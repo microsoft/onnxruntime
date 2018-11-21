08a3677ce87b079841cea2195a46912743006707
Raymond Yang <zihao.yang@microsoft.com>
2018-11-21 12:51:42 -0800

    Add pipeline for building python wheels for Windows/Linux CPU and GPU

--- tools/ci_build/github/linux/run_build.sh
+++ tools/ci_build/github/linux/run_build.sh
@@ -9,16 +9,19 @@ while getopts c:d:x: parameter_Option
 do case "${parameter_Option}"
 in
 d) BUILD_DEVICE=${OPTARG};;
+x) BUILD_EXTR_PAR=${OPTARG};;
 esac
 done
 
 if [ -z "$AZURE_BLOB_KEY" ]; then
   echo "AZURE_BLOB_KEY is blank"
+  BUILD_EXTR_PAR="${BUILD_EXTR_PAR}"
+  echo "Extra parameters: ${BUILD_EXTR_PAR}"
 else
   echo "Downloading test data from azure"
   mkdir -p /home/onnxruntimedev/models/
   azcopy --recursive --source:https://onnxruntimetestdata.blob.core.windows.net/onnx-model-zoo-20181018 --destination:/home/onnxruntimedev/models/ --source-key:$AZURE_BLOB_KEY
-  BUILD_EXTR_PAR="--enable_onnx_tests"
+  BUILD_EXTR_PAR="${BUILD_EXTR_PAR} --enable_onnx_tests"
 fi
 
 if [ $BUILD_DEVICE = "gpu" ]; then
@@ -28,7 +31,7 @@ if [ $BUILD_DEVICE = "gpu" ]; then
         --parallel --build_shared_lib \
         --use_cuda \
         --cuda_home /usr/local/cuda \
-        --cudnn_home /usr/local/cudnn-7.0/cuda --build_shared_lib $BUILD_EXTR_PAR
+        --cudnn_home /usr/local/cudnn-7.1/cuda --build_shared_lib $BUILD_EXTR_PAR
     /home/onnxruntimedev/Release/onnx_test_runner -e cuda /data/onnx
 else
     python3 $SCRIPT_DIR/../../build.py --build_dir /home/onnxruntimedev \
