093331822a1511f89b9494350b16a95ffa7615d5
Du Li <duli1@microsoft.com>
2018-11-26 21:02:18 -0800

    Accomodating PR comments.

--- onnxruntime/test/contrib_ops/expand_dims_test.cc
+++ onnxruntime/test/contrib_ops/expand_dims_test.cc
@@ -9,7 +9,7 @@ namespace test {
 
 TEST(ContribOpTest, ExpandDims_0) {
   OpTester test("ExpandDims", 1, onnxruntime::kMSDomain);
-  test.AddShapeToTensorData(true);  // TODO: re-enable shape inference test
+  test.AddShapeToTensorData(true);
   test.AddInput<float>("X", {2, 3}, std::vector<float>(6, 1.0f));
   test.AddInput<int32_t>("axis", {}, {-1});
   test.AddOutput<float>("Y", {2, 3, 1}, std::vector<float>(6, 1.0f));
@@ -18,7 +18,7 @@ TEST(ContribOpTest, ExpandDims_0) {
 
 TEST(ContribOpTest, ExpandDims_1) {
   OpTester test("ExpandDims", 1, onnxruntime::kMSDomain);
-  test.AddShapeToTensorData(true);  // TODO: re-enable shape inference test
+  test.AddShapeToTensorData(true);
   test.AddInput<float>("X", {2, 3}, std::vector<float>(6, 1.0f));
   test.AddInput<int32_t>("axis", {}, {1});
   test.AddOutput<float>("Y", {2, 1, 3}, std::vector<float>(6, 1.0f));
