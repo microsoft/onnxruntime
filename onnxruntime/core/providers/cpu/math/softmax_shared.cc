c530064ebe912621ac5627bc486c62356fbd1eeb
Lei Zhang <zhalei@microsoft.com>
2018-11-28 11:15:31 -0800

    Better opemmp parallel group count calculation in Softmax parallel running.

--- onnxruntime/core/providers/cpu/math/softmax_shared.cc
+++ onnxruntime/core/providers/cpu/math/softmax_shared.cc
@@ -36,29 +36,49 @@
 #include "gsl/gsl_algorithm"
 #include "gsl/gsl_util"
 
+#if defined(_OPENMP)
+#include <omp.h>
+#endif
+
+#ifdef __GNUC__
+#define UNUSED __attribute__((unused))
+#else
+#define UNUSED
+#endif
+
 namespace onnxruntime {
 
 common::Status SoftmaxCore(const int n,
-                          const int d,
-                          const float* Xdata,
-                          float* Ydata,
-                          float* scale,
-                          const float* sum_multiplier,
-                          bool logarithmic,
-                          float* rowmax) {
-
+                           const int d,
+                           const float* Xdata,
+                           float* Ydata,
+                           const float* sum_multiplier,
+                           float* rowmax) {
   const int nd = n * d;
 
   math::RowwiseMax<float, CPUMathUtil>(n, d, Xdata, rowmax, nullptr);
-
   // Put the intermediate result X - max(X) into Y by first copying X to Y, and then subtracting max from each entry
   gsl::copy(gsl::make_span(Xdata, nd), gsl::make_span(Ydata, nd));
-
   math::Gemm<float, CPUMathUtil>(CblasNoTrans, CblasNoTrans, n, d, 1, -1, rowmax, sum_multiplier, 1, Ydata, nullptr);
   // Exponentiation
   math::Exp<float, CPUMathUtil>(nd, Ydata, Ydata, nullptr);
-
   return Status::OK();
+}
+
+static int GetParallelGroupCount(int UNUSED n, int UNUSED d) {
+#if defined(_OPENMP)
+  int omp_num_threads = omp_get_num_threads();
+  int group_count = std::min(omp_num_threads, n);
+  if (group_count <= 1) return 1;
+
+  // 2048 * sizeof(float) is size of 2 cache page
+  static const int min_elements_per_group = 2048;
+  int max_groups = gsl::narrow_cast<int>((int64_t{n} * d + min_elements_per_group-1) / min_elements_per_group);
+ 
+  return std::min(group_count, max_groups);
+#else
+  return 1;
+#endif
 }
 
 common::Status SoftmaxCPU(const int64_t N,
@@ -80,17 +100,16 @@ common::Status SoftmaxCPU(const int64_t N,
 
   const int n = gsl::narrow_cast<int>(N);
   const int d = gsl::narrow_cast<int>(D);
-  const int nd = gsl::narrow_cast<int>(N * D);
 
-  static const int kGROUP = 8;
-  int g = (n + (kGROUP-1)) / kGROUP;
+  int parallel_group_count = GetParallelGroupCount(n, d);
+  int n_per_group = (n + (parallel_group_count-1)) / parallel_group_count;
 
   #pragma omp parallel for
-  for (int i = 0; i < kGROUP; ++i) {
-    int s = g * i;
+  for (int i = 0; i < parallel_group_count; ++i) {
+    int s = n_per_group * i;
     if (s < n) {
-      int c = (n - s >= g)?g : (n-s);
-      SoftmaxCore(c, d, Xdata + (s*d), Ydata + (s*d), scale + s, sum_multiplier, logarithmic, rowmax+s);
+      int c = (n - s >= n_per_group) ? n_per_group : (n-s);
+      SoftmaxCore(c, d, Xdata + (s*d), Ydata + (s*d), sum_multiplier, rowmax+s);
     }
   }
 
