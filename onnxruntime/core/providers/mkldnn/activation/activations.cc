69fb110261386d8d4500a434dc36ce3ff212ffd8
Yalachigere, Sreekanth V <sreekanth.v.yalachigere@intel.com>
2018-12-05 15:15:18 -0800

    modified license

--- onnxruntime/core/providers/mkldnn/activation/activations.cc
+++ onnxruntime/core/providers/mkldnn/activation/activations.cc
@@ -1,17 +1,5 @@
-/* Copyright(C) 2018 Intel Corporation
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-http ://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing,
-software distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions
-and limitations under the License.
-==============================================================================*/
+// Copyright(C) 2018 Intel Corporation
+// 
 
 #ifdef _WIN32
 #pragma warning(disable : 4244)
@@ -215,7 +203,7 @@ Status Relu<T>::Compute(OpKernelContext* context) const {
     ReluPrimitive<T>* relulPrimitive = ReluPrimitivePool<T>::Get(pool_params);
 
     relulPrimitive->Compute(src_data, dst_data);
-  } catch (mkldnn::error& e) {
+  } catch (const mkldnn::error& e) {
     return ONNXRUNTIME_MAKE_STATUS(ONNXRUNTIME, FAIL, "Status: ", e.status, 
 		", message: ", e.message.c_str());
   }
