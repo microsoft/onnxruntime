42a710ab64903c37884b3d3c4f73c53d4f2f917f
Yalachigere, Sreekanth V <sreekanth.v.yalachigere@intel.com>
2018-12-10 10:31:25 -0800

    check provider_ for nullptr

--- onnxruntime/core/providers/mkldnn/nn/conv.cc
+++ onnxruntime/core/providers/mkldnn/nn/conv.cc
@@ -406,9 +406,16 @@ Status Conv<T>::Compute(OpKernelContext* context) const {
     // Reorder filter memory layout if necessary
     // Avoid data reordering. Save filter memory in mkldnn format from first iteration 
     // in execution provider mapped by weight name.
+<<<<<<< HEAD
     std::string weightKey = OpKernel::Node().InputDefs()[1]->Name();
     std::shared_ptr<mkldnn::memory> filter_dst_mem = nullptr;
     filter_dst_mem = provider_->GetWeightMemory(weightKey);
+=======
+    std::string weightName = OpKernel::Node().InputDefs()[1]->Name();
+    std::shared_ptr<mkldnn::memory> filter_dst_mem = nullptr;
+    if(provider_ != nullptr)
+      filter_dst_mem = provider_->GetWeightMemory(weightName);
+>>>>>>> check provider_ for nullptr
 
     if (filter_dst_mem == nullptr) {
       if (filter_format != conv_primitive->GetFilterMemoryFormat()) {
@@ -421,11 +428,16 @@ Status Conv<T>::Compute(OpKernelContext* context) const {
         MemoryReorderParams params(src, *filter_dst_mem);
         DoReorder<T>(params);
         filter_data = static_cast<T*>(filter_dst_mem->get_data_handle());
+<<<<<<< HEAD
         {
           // make assignment threadsafe
           std::lock_guard<std::mutex> lock(mutex_);
           provider_->GetWeightsMap()[weightKey] = filter_dst_mem;
         }
+=======
+        if (provider_ != nullptr)
+          provider_->weights_mem_map[weightName] = filter_dst_mem;
+>>>>>>> check provider_ for nullptr
       }
     } else {
       filter_data = static_cast<T*>(filter_dst_mem->get_data_handle());
