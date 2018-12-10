c45d0cb86869d6040ff91bfbbf5f7c0f70aca7a3
Yalachigere, Sreekanth V <sreekanth.v.yalachigere@intel.com>
2018-12-09 21:58:54 -0800

    Filter data (Weights) reorder optimization

--- onnxruntime/core/providers/mkldnn/nn/conv.h
+++ onnxruntime/core/providers/mkldnn/nn/conv.h
@@ -4,18 +4,26 @@
 #pragma once
 #include "core/framework/op_kernel.h"
 #include "core/providers/cpu/nn/conv.h"
+#include "../mkldnn_execution_provider.h"
 
 namespace onnxruntime {
 namespace mkl_dnn {
+
 template <typename T>
 class Conv final : public onnxruntime::Conv<T> {
  public:
   Conv(const OpKernelInfo& info) : onnxruntime::Conv<T>(info) {
+    if (info.GetExecutionProvider()->Type() == kMklDnnExecutionProvider) {
+      provider_ = (const_cast<MKLDNNExecutionProvider*>(
+        dynamic_cast<const MKLDNNExecutionProvider*>(info.GetExecutionProvider())));
+    }
   }
 
   Status Compute(OpKernelContext* context) const override;
 
  private:
+   MKLDNNExecutionProvider * provider_;
+
 };
 }  // namespace mkl_dnn
 }  // namespace onnxruntime
