c60920a5c070ac6eb645aaa31e353ae5a0950a46
Changming Sun <chasun@microsoft.com>
2018-12-10 19:15:03 -0800

    Enable data cache (#144)

--- tools/ci_build/github/linux/run_build.sh
+++ tools/ci_build/github/linux/run_build.sh
@@ -13,22 +13,11 @@ x) BUILD_EXTR_PAR=${OPTARG};;
 esac
 done
 
-if [ -z "$AZURE_BLOB_KEY" ]; then
-  echo "AZURE_BLOB_KEY is blank"
-  BUILD_EXTR_PAR="${BUILD_EXTR_PAR}"
-  echo "Extra parameters: ${BUILD_EXTR_PAR}"
-else
-  echo "Downloading test data from azure"
-  mkdir -p /home/onnxruntimedev/models/
-  azcopy --recursive --source:https://onnxruntimetestdata.blob.core.windows.net/onnx-model-zoo-20181018 --destination:/home/onnxruntimedev/models/ --source-key:$AZURE_BLOB_KEY
-  BUILD_EXTR_PAR="${BUILD_EXTR_PAR} --enable_onnx_tests"
-fi
-
 if [ $BUILD_DEVICE = "gpu" ]; then
     _CUDNN_VERSION=$(echo $CUDNN_VERSION | cut -d. -f1-2)
     python3 $SCRIPT_DIR/../../build.py --build_dir /home/onnxruntimedev \
         --config Debug Release \
-        --skip_submodule_sync \
+        --skip_submodule_sync --enable_onnx_tests \
         --parallel --build_shared_lib \
         --use_cuda \
         --cuda_home /usr/local/cuda \
@@ -37,7 +26,7 @@ if [ $BUILD_DEVICE = "gpu" ]; then
 else
     python3 $SCRIPT_DIR/../../build.py --build_dir /home/onnxruntimedev \
         --config Debug Release --build_shared_lib \
-        --skip_submodule_sync \
+        --skip_submodule_sync --enable_onnx_tests \
         --enable_pybind \
         --parallel --use_mkldnn --build_shared_lib $BUILD_EXTR_PAR
     /home/onnxruntimedev/Release/onnx_test_runner /data/onnx
