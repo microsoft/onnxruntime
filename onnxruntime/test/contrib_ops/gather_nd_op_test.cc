75f6673fea93c88044a8b8007cddb32c39cb9ff2
RandyShuai <rashuai@microsoft.com>
2018-12-11 16:26:35 -0800

    add test file

---
+++ onnxruntime/test/contrib_ops/gather_nd_op_test.cc
@@ -0,0 +1,123 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+#include "gtest/gtest.h"
+#include "test/providers/provider_test_utils.h"
+
+namespace onnxruntime {
+namespace test {
+
+TEST(GatherNDOpTest, GatherND_simple_int64_int64) {
+  OpTester test("GatherND", 1, onnxruntime::kMSDomain);
+  test.AddInput<int64_t> ("data", {2,2}, {0LL,1LL,2LL,3LL});
+  test.AddInput<int64_t> ("indices", {2,2}, {0LL,0LL,1LL,1LL});
+  test.AddOutput<int64_t>("output", {2}, {0LL,3LL});
+  test.Run();
+}
+
+TEST(GatherNDOpTest, GatherND_simple_string_int64) {
+  OpTester test("GatherND", 1, onnxruntime::kMSDomain);
+  test.AddInput<std::string>("data", {2,2}, {"a","b","c","d"});
+  test.AddInput<int64_t>("indices", {2,2}, {0LL,0LL,1LL,1LL});
+  test.AddOutput<std::string>("output", {2}, {"a","d"});
+  test.Run();
+}
+
+TEST(GatherNDOpTest, GatherND_simple_int64_int32) {
+  OpTester test("GatherND", 1, onnxruntime::kMSDomain);
+  test.AddInput<int64_t>("data", {2,2}, {0LL,1LL,2LL,3LL});
+  test.AddInput<int32_t>("indices", {2,2}, {0,0,1,1});
+  test.AddOutput<int64_t>("output", {2}, {0LL,3LL});
+  test.Run();
+}
+
+TEST(GatherNDOpTest, GatherND_slice_float_int64_t) {
+  OpTester test("GatherND", 1, onnxruntime::kMSDomain);
+  test.AddInput<float>("data", {2,2}, {0.0f,0.1f,0.2f,0.3f});
+  test.AddInput<int64_t>("indices", {2,1}, {1LL,0LL});
+  test.AddOutput<float>("output", {2,2}, {0.2f,0.3f,0.0f,0.1f});
+  test.Run();
+}
+
+TEST(GatherNDOpTest, GatherND_slice_double_int32_t) {
+  OpTester test("GatherND", 1, onnxruntime::kMSDomain);
+  test.AddInput<double>("data", {2,2}, {0.0f,0.1f,0.2f,0.3f});
+  test.AddInput<int32_t>("indices", {2,1}, {1LL,0LL});
+  test.AddOutput<double>("output", {2,2}, {0.2f,0.3f,0.0f,0.1f});
+  test.Run();
+}
+
+TEST(GatherNDOpTest, GatherND_3tensor_int64) {
+  OpTester test0("GatherND", 1, onnxruntime::kMSDomain);
+  test0.AddInput<int16_t>("data", {2,2,2}, {0,1,2,3,4,5,6,7});
+  test0.AddInput<int64_t>("indices", {1,1}, {1LL});
+  test0.AddOutput<int16_t>("output", {1,2,2}, {4,5,6,7});
+  test0.Run();
+
+  OpTester test1("GatherND", 1, onnxruntime::kMSDomain);
+  test1.AddInput<int64_t>("data", {2,2,2}, {0LL,1LL,2LL,3LL,4LL,5LL,6LL,7LL});
+  test1.AddInput<int64_t>("indices", {2,2}, {0LL,1LL,1LL,0LL});
+  test1.AddOutput<int64_t>("output", {2,2}, {2LL,3LL,4LL,5LL});
+  test1.Run();
+
+  OpTester test2("GatherND", 1, onnxruntime::kMSDomain);
+  test2.AddInput<int8_t>("data", {2,2,2}, {0,1,2,3,4,5,6,7});
+  test2.AddInput<int32_t>("indices", {2,3}, {0,0,1,1,0,1});
+  test2.AddOutput<int8_t>("output", {2}, {1,5});
+  test2.Run();
+}
+
+TEST(GatherNDOpTest, GatherND_batched_index_int64) {
+  OpTester test("GatherND", 1, onnxruntime::kMSDomain);
+  test.AddInput<int64_t>("data", {2,2}, {0LL,1LL,2LL,3LL});
+  test.AddInput<int64_t>("indices", {2,1,2}, {0LL,0LL,0LL,1LL});
+  test.AddOutput<int64_t>("output", {2,1}, {0LL,1LL});
+  test.Run();
+}
+
+TEST(GatherNDOpTest, GatherND_batched_index_bool_int64) {
+  OpTester test("GatherND", 1, onnxruntime::kMSDomain);
+  test.AddInput<bool>("data", {2,2}, {true,false,false,true});
+  test.AddInput<int64_t>("indices", {2,1,2}, {0LL,0LL,0LL,1LL});
+  test.AddOutput<bool>("output", {2,1}, {true,false});
+  test.Run();
+}
+
+TEST(GatherNDOpTest, GatherND_sliced_index_int64) {
+  OpTester test("GatherND", 1, onnxruntime::kMSDomain);
+  test.AddInput<int64_t>("data", {2,2}, {0LL,1LL,2LL,3LL});
+  test.AddInput<int64_t>("indices", {2,1,1}, {1LL,0LL});
+  test.AddOutput<int64_t>("output", {2,1,2}, {2LL,3LL,0LL,1LL});
+  test.Run();
+}
+
+TEST(GatherNDOpTest, GatherND_sliced_index_string_int32) {
+  OpTester test("GatherND", 1, onnxruntime::kMSDomain);
+  test.AddInput<std::string>("data", {2,2}, {"ab","cde","f","ghi"});
+  test.AddInput<int32_t>("indices", {2,1,1}, {1LL,0LL});
+  test.AddOutput<std::string>("output", {2,1,2}, {"f","ghi","ab","cde"});
+  test.Run();
+}
+
+TEST(GatherNDOpTest, GatherND_batched_3tensor_int64) {
+  OpTester test0("GatherND", 1, onnxruntime::kMSDomain);
+  test0.AddInput<int64_t>("data", {2,2,2}, {0LL,1LL,2LL,3LL,4LL,5LL,6LL,7LL});
+  test0.AddInput<int32_t>("indices", {2,1,1}, {1,0});
+  test0.AddOutput<int64_t>("output", {2,1,2,2}, {4LL,5LL,6LL,7LL,0LL,1LL,2LL,3LL});
+  test0.Run();
+
+  OpTester test1("GatherND", 1, onnxruntime::kMSDomain);
+  test1.AddInput<uint32_t>("data", {2,2,2}, {0,1,2,3,4,5,6,7});
+  test1.AddInput<int64_t>("indices", {2,2,2}, {0LL,1LL,1LL,0LL,0LL,0LL,1LL,1LL});
+  test1.AddOutput<uint32_t>("output", {2,2,2}, {2,3,4,5,0,1,6,7});
+  test1.Run();
+
+  OpTester test2("GatherND", 1, onnxruntime::kMSDomain);
+  test2.AddInput<uint32_t>("data", {2,2,2}, {0,1,2,3,4,5,6,7});
+  test2.AddInput<int32_t>("indices", {2,2,3}, {0,0,1,1,0,1,0,1,1,1,1,0});
+  test2.AddOutput<uint32_t>("output", {2,2}, {1,5,3,6});
+  test2.Run();
+}
+
+}  // namespace test
+}  // namespace onnxruntime
