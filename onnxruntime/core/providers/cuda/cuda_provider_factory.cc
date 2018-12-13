e7265b4a6f7f8d57d5d9022e2bcc33959d80b174
RyanUnderhill <ryanhill@microsoft.com>
2018-12-12 18:03:49 -0800

    Rename ONNXRUNTIME_API_STATUSCALL to ONNXRUNTIME_API_CALL since it's just for calling convention, there's no status return value implied.

--- onnxruntime/core/providers/cuda/cuda_provider_factory.cc
+++ onnxruntime/core/providers/cuda/cuda_provider_factory.cc
@@ -15,7 +15,7 @@ struct CUDAProviderFactory {
   CUDAProviderFactory();
 };
 
-ONNXStatus* ONNXRUNTIME_API_STATUSCALL CreateCuda(void* this_, ONNXRuntimeProvider** out) {
+ONNXStatus* ONNXRUNTIME_API_CALL CreateCuda(void* this_, ONNXRuntimeProvider** out) {
   CUDAExecutionProviderInfo info;
   CUDAProviderFactory* this_ptr = (CUDAProviderFactory*)this_;
   info.device_id = this_ptr->device_id;
@@ -24,14 +24,14 @@ ONNXStatus* ONNXRUNTIME_API_STATUSCALL CreateCuda(void* this_, ONNXRuntimeProvid
   return nullptr;
 }
 
-uint32_t ONNXRUNTIME_API_STATUSCALL ReleaseCuda(void* this_) {
+uint32_t ONNXRUNTIME_API_CALL ReleaseCuda(void* this_) {
   CUDAProviderFactory* this_ptr = (CUDAProviderFactory*)this_;
   if (--this_ptr->ref_count == 0)
     delete this_ptr;
   return 0;
 }
 
-uint32_t ONNXRUNTIME_API_STATUSCALL AddRefCuda(void* this_) {
+uint32_t ONNXRUNTIME_API_CALL AddRefCuda(void* this_) {
   CUDAProviderFactory* this_ptr = (CUDAProviderFactory*)this_;
   ++this_ptr->ref_count;
   return 0;
