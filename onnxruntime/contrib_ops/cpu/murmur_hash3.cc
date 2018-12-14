db31213312e3a61bf1c6f1e1cefd59400c12f433
RyanUnderhill <ryanhill@microsoft.com>
2018-12-14 11:25:49 -0800

    Oops, ORT is not part of StatusCategory

--- onnxruntime/contrib_ops/cpu/murmur_hash3.cc
+++ onnxruntime/contrib_ops/cpu/murmur_hash3.cc
@@ -188,7 +188,7 @@ Status MurmurHash3::Compute(OpKernelContext* ctx) const {
                            seed_,
                            reinterpret_cast<uint8_t*>(output) + static_cast<int64_t>(i) * output_element_bytes);
       } else {
-        return ORT_MAKE_STATUS(ORT, NOT_IMPLEMENTED, "Type not supported.");
+        return ORT_MAKE_STATUS(ONNXRUNTIME, NOT_IMPLEMENTED, "Type not supported.");
       }
     }
   }
