bdbebf0560088121b51f9d121373691be39c646f
Edward Chen <edch@microsoft.com>
2018-12-17 14:55:30 -0800

    Added copy assignment, move assignment/ctor to TensorShape.

--- include/onnxruntime/core/framework/tensor_shape.h
+++ include/onnxruntime/core/framework/tensor_shape.h
@@ -28,13 +28,17 @@ class TensorShape : private std::vector<int64_t> {
  public:
   TensorShape() = default;
 
+  TensorShape(const TensorShape& /*other*/) = default;
+  TensorShape& operator=(const TensorShape& /*other*/) = default;
+
+  TensorShape(TensorShape&& /*other*/) = default;
+  TensorShape& operator=(TensorShape&& /*other*/) = default;
+
   TensorShape(const int64_t* dimension_sizes, size_t dimension_count);
 
   TensorShape(const std::vector<int64_t>& dims);
 
   TensorShape(const std::initializer_list<int64_t>& dims);
-
-  TensorShape(const TensorShape& /*other*/) = default;
 
   TensorShape(const std::vector<int64_t>& dims, size_t start, size_t end);
 
