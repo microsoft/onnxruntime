93103edde8683f7e6d7c74936db8fb5d9d96aa49
Edward Chen <edch@microsoft.com>
2018-12-17 13:15:19 -0800

    Updated macro name.

--- onnxruntime/core/providers/cpu/ml/array_feature_extractor.cc
+++ onnxruntime/core/providers/cpu/ml/array_feature_extractor.cc
@@ -74,7 +74,7 @@ common::Status ArrayFeatureExtractorOp<T>::Compute(OpKernelContext* context) con
 
   for (int64_t i = 0; i < num_indices; ++i) {
     if (y_data[i] >= stride) {
-      return ONNXRUNTIME_MAKE_STATUS(
+      return ORT_MAKE_STATUS(
           ONNXRUNTIME, INVALID_ARGUMENT,
           "Invalid Y argument: index is out of range: Y[", i, "] (", y_data[i], ") >=", stride);
     }
