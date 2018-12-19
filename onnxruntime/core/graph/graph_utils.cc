2e7606553585c7b192b72ab413a32ffd05ff66d5
Scott McKay <scmckay@microsoft.com>
2018-12-19 17:49:03 +1000

    Re-structure the inference session initialization to   - apply any transforms to the main graph and any subgraphs first   - call Graph::Resolve() once on the main graph, which will recurse into the subgraphs     - previously it was called after the transform on each subgraph, which results in it traversing up to the main graph to call resolve, and that resolve call recursing into all subgraphs every time.
    This avoids lots of unnecessary Graph::Resolve calls, and prevents subgraphs from being broken by SessionStateInitializer::InitializeAndSave calling graph_.CleanAllInitializedTensors() prior to final Graph::Resolve call. If a subgraph has optional inputs the backing initializers were removed by CleanAllInitializedTensors causing the next Resolve to incorrectly turn them into required inputs.

--- onnxruntime/core/graph/graph_utils.cc
+++ onnxruntime/core/graph/graph_utils.cc
@@ -4,18 +4,70 @@
 namespace onnxruntime {
 
 namespace utils {
-  // fusion is only done for ONNX domain ops
-  bool IsSupportedOptypeVersionAndDomain(const Node& node,
-                                         const std::string& op_type,
-                                         ONNX_NAMESPACE::OperatorSetVersion version,
-                                         const std::string& domain) {
-    if (node.OpType() != op_type ||
-        node.Op()->Deprecated() || node.Op()->SinceVersion() != version ||
-        (!node.Domain().empty() && node.Domain() != domain)) {
-      return false;
-    }
-    return true;
+// fusion is only done for ONNX domain ops
+bool IsSupportedOptypeVersionAndDomain(const Node& node,
+                                       const std::string& op_type,
+                                       ONNX_NAMESPACE::OperatorSetVersion version,
+                                       const std::string& domain) {
+  if (node.OpType() != op_type ||
+      node.Op()->Deprecated() || node.Op()->SinceVersion() != version ||
+      (!node.Domain().empty() && node.Domain() != domain)) {
+    return false;
   }
+  return true;
 }
 
+Status ForAllMutableSubgraphs(Graph& graph, std::function<Status(Graph&)> func) {
+  Status status = Status::OK();
+
+  for (auto& node : graph.Nodes()) {
+    for (auto& attribute : node.GetAttributes()) {
+      auto& name = attribute.first;
+      auto& proto = attribute.second;
+
+      // check if it has a subgraph
+      if (proto.has_g()) {
+        Graph* subgraph = node.GetMutableGraphAttribute(name);
+        ORT_ENFORCE(subgraph, "Main Graph instance should have populated all subgraphs when being resolved.");
+
+        status = func(*subgraph);
+        ORT_RETURN_IF_ERROR(status);
+
+        // recurse
+        status = ForAllMutableSubgraphs(*subgraph, func);
+        ORT_RETURN_IF_ERROR(status);
+      }
+    }
+  }
+
+  return status;
+}
+
+Status ForAllSubgraphs(const Graph& graph, std::function<Status(const Graph&)> func) {
+  Status status = Status::OK();
+
+  for (auto& node : graph.Nodes()) {
+    for (auto& attribute : node.GetAttributes()) {
+      auto& name = attribute.first;
+      auto& proto = attribute.second;
+
+      // check if it has a subgraph
+      if (proto.has_g()) {
+        const Graph* subgraph = node.GetGraphAttribute(name);
+        ORT_ENFORCE(subgraph, "Main Graph instance should have populated all subgraphs when being resolved.");
+
+        status = func(*subgraph);
+        ORT_RETURN_IF_ERROR(status);
+
+        // recurse
+        status = ForAllSubgraphs(*subgraph, func);
+        ORT_RETURN_IF_ERROR(status);
+      }
+    }
+  }
+
+  return status;
+}
+
+}  // namespace utils
 }  // namespace onnxruntime
