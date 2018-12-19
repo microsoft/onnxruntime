06fe4e7c8e62085bcd927f8f48c7a3141883d855
Ashwin Kumar <ashku@microsoft.com>
2018-12-19 12:54:15 -0800

    Revert "remove tests from exclude list"
    This reverts commit 2112a30b57d5a899991de4847e948e700a44e85d.

--- onnxruntime/test/onnx/main.cc
+++ onnxruntime/test/onnx/main.cc
@@ -314,9 +314,13 @@ int real_main(int argc, char* argv[]) {
       {"upsample_nearest", "opset 9 not supported yet"},
       {"onehot_with_axis", "opset 9 not supported yet"},
       {"onehot_without_axis", "opset 9 not supported yet"},  // also has bug in current test re: output type. Spandan to fix.
+      {"sinh", "opset 9 not supported yet"},
+      {"cosh", "opset 9 not supported yet"},
       {"asinh", "opset 9 not supported yet"},
       {"acosh", "opset 9 not supported yet"},
       {"atanh", "opset 9 not supported yet"},
+      {"sinh_example", "opset 9 not supported yet"},
+      {"cosh_example", "opset 9 not supported yet"},
       {"asinh_example", "opset 9 not supported yet"},
       {"acosh_example", "opset 9 not supported yet"},
       {"atanh_example", "opset 9 not supported yet"},
