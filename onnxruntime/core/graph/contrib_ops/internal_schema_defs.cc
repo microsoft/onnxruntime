1599352d5c5e36290e01dddd2f91a0f3e55e7ff5
souptc <chenta@microsoft.com>
2018-12-18 17:52:07 -0800

    placeholder for internal contrib ops

---
+++ onnxruntime/core/graph/contrib_ops/internal_schema_defs.cc
@@ -0,0 +1,12 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+#include "range_schema_defs.h"
+
+namespace onnxruntime {
+namespace contrib {
+
+void RegisterInternalSchemas() {}
+
+}  // namespace contrib
+}  // namespace onnxruntime
