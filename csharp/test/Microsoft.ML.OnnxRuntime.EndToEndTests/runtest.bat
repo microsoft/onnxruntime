fb51686cb45d11507260cf7d999fdcf79b920b54
Shah Asaduzzaman (ASAD) <shahasad@microsoft.com>
2018-12-24 18:19:21 -0800

    fix in the runtest.bat

--- csharp/test/Microsoft.ML.OnnxRuntime.EndToEndTests/runtest.bat
+++ csharp/test/Microsoft.ML.OnnxRuntime.EndToEndTests/runtest.bat
@@ -1,5 +1,7 @@
 @echo off
 SETLOCAL EnableDelayedExpansion
+
+set LocalNuGetRepo=%1
 REM WorkingDirectory is Build.SourcesDirectory\csharp
 set /p MajorVersionNumber=<..\VERSION_NUMBER
 set VersionSuffix=
@@ -11,5 +13,5 @@ IF NOT DEFINED IsReleaseBuild (
 
 set CurrentOnnxRuntimeVersion=%MajorVersionNumber%%VersionSuffix%
 @echo %CurrentOnnxRuntimeVersion%
-dotnet restore test\Microsoft.ML.OnnxRuntime.EndToEndTests\Microsoft.ML.OnnxRuntime.EndToEndTests.csproj -s %buildDirectory%\NuGetRepo --configfile .\Nuget.CSharp.config
+dotnet restore test\Microsoft.ML.OnnxRuntime.EndToEndTests\Microsoft.ML.OnnxRuntime.EndToEndTests.csproj -s %LocalNuGetRepo% --configfile .\Nuget.CSharp.config
 dotnet test test\Microsoft.ML.OnnxRuntime.EndToEndTests\Microsoft.ML.OnnxRuntime.EndToEndTests.csproj --no-restore
