6a090985fbbf74c0fa3dda19537f3c5160a506b5
Ryan Hill <38674843+RyanUnderhill@users.noreply.github.com>
2018-12-28 14:53:19 -0800

    More C API changes (#259)
    * More API changes, remove 'Inference' from function names. Remove enum values. Make Status match other types.
    
    * Switch to bool instead of int, and remove stdbool

--- onnxruntime/test/shared_lib/test_io_types.cc
+++ onnxruntime/test/shared_lib/test_io_types.cc
@@ -9,18 +9,18 @@ using namespace onnxruntime;
 static void TestModelInfo(const OrtSession* inference_session, bool is_input, const std::vector<int64_t>& dims) {
   size_t input_count;
   if (is_input) {
-    ORT_THROW_ON_ERROR(OrtInferenceSessionGetInputCount(inference_session, &input_count));
+    ORT_THROW_ON_ERROR(OrtSessionGetInputCount(inference_session, &input_count));
   } else {
-    ORT_THROW_ON_ERROR(OrtInferenceSessionGetOutputCount(inference_session, &input_count));
+    ORT_THROW_ON_ERROR(OrtSessionGetOutputCount(inference_session, &input_count));
   }
   ASSERT_EQ(1, input_count);
   std::unique_ptr<OrtTypeInfo> input_type_info;
   {
     OrtTypeInfo* t;
     if (is_input) {
-      ORT_THROW_ON_ERROR(OrtInferenceSessionGetInputTypeInfo(inference_session, 0, &t));
+      ORT_THROW_ON_ERROR(OrtSessionGetInputTypeInfo(inference_session, 0, &t));
     } else {
-      ORT_THROW_ON_ERROR(OrtInferenceSessionGetOutputTypeInfo(inference_session, 0, &t));
+      ORT_THROW_ON_ERROR(OrtSessionGetOutputTypeInfo(inference_session, 0, &t));
     }
     input_type_info.reset(t);
   }
@@ -39,7 +39,7 @@ static void TestModelInfo(const OrtSession* inference_session, bool is_input, co
 TEST_F(CApiTest, input_output_type_info) {
   SessionOptionsWrapper sf(env);
   constexpr PATH_TYPE model_uri = TSTR("../models/opset8/test_squeezenet/model.onnx");
-  std::unique_ptr<OrtSession, decltype(&OrtReleaseSession)> inference_session(sf.OrtCreateInferenceSession(model_uri), OrtReleaseSession);
+  std::unique_ptr<OrtSession, decltype(&OrtReleaseSession)> inference_session(sf.OrtCreateSession(model_uri), OrtReleaseSession);
   TestModelInfo(inference_session.get(), true, {1, 3, 224, 224});
   TestModelInfo(inference_session.get(), false, {1, 1000, 1, 1});
 }
