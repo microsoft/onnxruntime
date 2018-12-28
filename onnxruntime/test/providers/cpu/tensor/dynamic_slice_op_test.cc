e6ef39b941dfa64f9a408f91c847ce022e645591
RandyShuai <rashuai@microsoft.com>
2018-12-28 13:07:00 -0800

    rename test cases

--- onnxruntime/test/providers/cpu/tensor/dynamic_slice_op_test.cc
+++ onnxruntime/test/providers/cpu/tensor/dynamic_slice_op_test.cc
@@ -7,7 +7,7 @@
 namespace onnxruntime {
 namespace test {
 
-TEST(DynamicSliceTest, dynamic_full_slice_varied_types) {
+TEST(DynamicSliceTest, dynamic_slice_varied_types) {
   OpTester test1 ("DynamicSlice", 1);
   test1.AddInput  <int32_t> ("data",   {3,3}, {1,2,3,4,5,6,7,8,9});
   test1.AddInput  <int32_t> ("starts", {2},   {1,1});
@@ -58,62 +58,66 @@ TEST(DynamicSliceTest, dynamic_slice_with_axes) {
                                                  10,11,12,13,14,15,16,17,18,
                                                  19,20,21,22,23,24,25,26,27});
   test2.AddInput  <int32_t> ("starts", {1},     {1});
-  test2.AddInput  <int32_t> ("ends",   {1},     {-1});
-  test2.AddInput  <int32_t> ("axes",   {1},     {1});
-  test2.AddOutput <int32_t> ("output", {3,1,3}, {4,5,6,13,14,15,22,23,24});
+  test2.AddInput  <int32_t> ("ends",   {1},     {2});
+  test2.AddInput  <int32_t> ("axes",   {1},     {2});
+  test2.AddOutput <int32_t> ("output", {3,3,1}, {2,5,8,11,14,17,20,23,26});
   test2.Run();
-
-  OpTester test3 ("DynamicSlice", 1);
-  test3.AddInput  <int32_t> ("data",   {3,3,3}, {1, 2, 3, 4, 5, 6, 7, 8, 9,
-                                                 10,11,12,13,14,15,16,17,18,
-                                                 19,20,21,22,23,24,25,26,27});
-  test3.AddInput  <int32_t> ("starts", {1},     {1});
-  test3.AddInput  <int32_t> ("ends",   {1},     {2});
-  test3.AddInput  <int32_t> ("axes",   {1},     {2});
-  test3.AddOutput <int32_t> ("output", {3,3,1}, {2,5,8,11,14,17,20,23,26});
-  test3.Run();
-
-  OpTester test4 ("DynamicSlice", 1);
-  test4.AddInput  <int32_t> ("data",   {3,3,3}, {1, 2, 3, 4, 5, 6, 7, 8, 9,
-                                                 10,11,12,13,14,15,16,17,18,
-                                                 19,20,21,22,23,24,25,26,27});
-  test4.AddInput  <int32_t> ("starts", {2},     {0,-2});
-  test4.AddInput  <int32_t> ("ends",   {2},     {2,1000});
-  test4.AddInput  <int32_t> ("axes",   {2},     {1,2});
-  test4.AddOutput <int32_t> ("output", {3,2,2}, {2,3,5,6,11,12,14,15,20,21,23,24});
-  test4.Run();
-
-  OpTester test5 ("DynamicSlice", 1);
-  test5.AddInput  <int32_t> ("data",   {3,3,3}, {1, 2, 3, 4, 5, 6, 7, 8, 9,
-                                                 10,11,12,13,14,15,16,17,18,
-                                                 19,20,21,22,23,24,25,26,27});
-  test5.AddInput  <int32_t> ("starts", {2},     {-3,0});
-  test5.AddInput  <int32_t> ("ends",   {2},     {-1,2});
-  test5.AddInput  <int32_t> ("axes",   {2},     {0,2});
-  test5.AddOutput <int32_t> ("output", {2,3,2}, {1,2,4,5,7,8,10,11,13,14,16,17});
-  test5.Run();
-
-  OpTester test6 ("DynamicSlice", 1);
-  test6.AddInput  <int32_t> ("data",   {3,3,3}, {1, 2, 3, 4, 5, 6, 7, 8, 9,
-                                                 10,11,12,13,14,15,16,17,18,
-                                                 19,20,21,22,23,24,25,26,27});
-  test6.AddInput  <int32_t> ("starts", {3},     {0,1,1});
-  test6.AddInput  <int32_t> ("ends",   {3},     {1,3,2});
-  test6.AddInput  <int32_t> ("axes",   {3},     {0,1,2});
-  test6.AddOutput <int32_t> ("output", {1,2,1}, {5,8});
-  test6.Run();
 }
 
-TEST(DynamicSliceTest, dynamic_slice_with_axes_7) {
-  OpTester test7 ("DynamicSlice", 1);
-  test7.AddInput  <int32_t> ("data",   {3,3,3}, {1, 2, 3, 4, 5, 6, 7, 8, 9,
+TEST(DynamicSliceTest, dynamic_slice_with_negative_axes) {
+  OpTester test1 ("DynamicSlice", 1);
+  test1.AddInput  <int32_t> ("data",   {3,3,3}, {1, 2, 3, 4, 5, 6, 7, 8, 9,
                                                  10,11,12,13,14,15,16,17,18,
                                                  19,20,21,22,23,24,25,26,27});
-  test7.AddInput  <int32_t> ("starts", {3},     {1,0,1});
-  test7.AddInput  <int32_t> ("ends",   {3},     {2,1,3});
-  test7.AddInput  <int32_t> ("axes",   {3},     {2,0,1});
-  test7.AddOutput <int32_t> ("output", {1,2,1}, {5,8});
-  test7.Run();
+  test1.AddInput  <int32_t> ("starts", {1},     {1});
+  test1.AddInput  <int32_t> ("ends",   {1},     {-1});
+  test1.AddInput  <int32_t> ("axes",   {1},     {1});
+  test1.AddOutput <int32_t> ("output", {3,1,3}, {4,5,6,13,14,15,22,23,24});
+  test1.Run();
+
+  OpTester test2 ("DynamicSlice", 1);
+  test2.AddInput  <int32_t> ("data",   {3,3,3}, {1, 2, 3, 4, 5, 6, 7, 8, 9,
+					       10,11,12,13,14,15,16,17,18,
+					       19,20,21,22,23,24,25,26,27});
+  test2.AddInput  <int32_t> ("starts", {2},     {-3,0});
+  test2.AddInput  <int32_t> ("ends",   {2},     {-1,2});
+  test2.AddInput  <int32_t> ("axes",   {2},     {0,2});
+  test2.AddOutput <int32_t> ("output", {2,3,2}, {1,2,4,5,7,8,10,11,13,14,16,17});
+  test2.Run();
+}
+
+TEST(DynamicSliceTest, dynamic_slice_ends_out_of_bounds) {
+  OpTester test ("DynamicSlice", 1);
+  test.AddInput  <int32_t> ("data",   {3,3,3}, {1, 2, 3, 4, 5, 6, 7, 8, 9,
+                                                 10,11,12,13,14,15,16,17,18,
+                                                 19,20,21,22,23,24,25,26,27});
+  test.AddInput  <int32_t> ("starts", {2},     {0,-2});
+  test.AddInput  <int32_t> ("ends",   {2},     {2,1000});
+  test.AddInput  <int32_t> ("axes",   {2},     {1,2});
+  test.AddOutput <int32_t> ("output", {3,2,2}, {2,3,5,6,11,12,14,15,20,21,23,24});
+  test.Run();
+}
+
+TEST(DynamicSliceTest, dynamic_slice_full_axes) {
+  OpTester test1 ("DynamicSlice", 1);
+  test1.AddInput  <int32_t> ("data",   {3,3,3}, {1, 2, 3, 4, 5, 6, 7, 8, 9,
+                                                 10,11,12,13,14,15,16,17,18,
+                                                 19,20,21,22,23,24,25,26,27});
+  test1.AddInput  <int32_t> ("starts", {3},     {0,1,1});
+  test1.AddInput  <int32_t> ("ends",   {3},     {1,3,2});
+  test1.AddInput  <int32_t> ("axes",   {3},     {0,1,2});
+  test1.AddOutput <int32_t> ("output", {1,2,1}, {5,8});
+  test1.Run();
+
+  OpTester test2 ("DynamicSlice", 1);
+  test2.AddInput  <int32_t> ("data",   {3,3,3}, {1, 2, 3, 4, 5, 6, 7, 8, 9,
+                                                 10,11,12,13,14,15,16,17,18,
+                                                 19,20,21,22,23,24,25,26,27});
+  test2.AddInput  <int32_t> ("starts", {3},     {1,0,1});
+  test2.AddInput  <int32_t> ("ends",   {3},     {2,1,3});
+  test2.AddInput  <int32_t> ("axes",   {3},     {2,0,1});
+  test2.AddOutput <int32_t> ("output", {1,2,1}, {5,8});
+  test2.Run();
 }
 
 }  // namespace Test
