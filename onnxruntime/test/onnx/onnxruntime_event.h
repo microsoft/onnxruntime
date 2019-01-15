fac501dd3f32b5cc1bcc7f94be6720967e144ef9
Ashwin Kumar <ashku@microsoft.com>
2019-01-15 14:41:04 -0800

    turn off eigenthreadpool by default and add todo comment

--- onnxruntime/test/onnx/onnxruntime_event.h
+++ onnxruntime/test/onnx/onnxruntime_event.h
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+//TODO: switch to nsync mutex and cv
 #include <mutex>
 #include <condition_variable>
 #include <core/common/common.h>
