0e6e1051012aab3680609f9cfb116ee5bd2f831f
Hector Li <hecli@microsoft.com>
2019-01-14 23:04:46 -0800

    Upsample opset 9 cuda implementation (#330)

--- onnxruntime/core/providers/cpu/tensor/upsample.h
+++ onnxruntime/core/providers/cpu/tensor/upsample.h
@@ -17,19 +17,32 @@ enum UpsampleMode {
 
 class UpsampleBase {
  protected:
-  UpsampleBase(OpKernelInfo info) {
+  UpsampleBase(OpKernelInfo info): scales_cached_(false) {
     std::string mode;
     ORT_ENFORCE(info.GetAttr<std::string>("mode", &mode).IsOK());
     mode_ = StringToUpsampleMode(mode);
 
-    if (info.GetInputCount() == 1) {
+    auto input_count = info.GetInputCount();
+    if (input_count == 1) {
       ORT_ENFORCE(info.GetAttrs<float>("scales", scales_).IsOK());
       ScalesValidation(scales_, mode_);
+    }
+
+    // opset 9
+    if (input_count > 1) {
+      const Tensor* scale;
+      bool get_scale = info.TryGetConstantInput(1, &scale);
+
+      if (get_scale) {
+        ParseScalesData(scale, scales_);
+        scales_cached_ = true;
+      }
     }
   }
 
   UpsampleMode mode_;
   std::vector<float> scales_;
+  bool scales_cached_;
 
   UpsampleMode StringToUpsampleMode(const std::string& mode) {
     if (strcmp(mode.c_str(), UpsampleModeNN) == 0) {
@@ -53,28 +66,7 @@ class UpsampleBase {
                           "Upsample: linear mode upsample only support bilinear, the first 2 scales should be 1.");
     }
   }
-};
 
-template <typename T>
-class Upsample : public UpsampleBase, public OpKernel {
- public:
-  Upsample(OpKernelInfo info) : UpsampleBase(info), OpKernel(info), scales_cached_(false) {
-    if (info.GetInputCount() > 1) {
-      const Tensor* scale;
-      bool get_scale = info.TryGetConstantInput(1, &scale);
-
-      if (get_scale) {
-        ParseScalesData(scale, scales_);
-        scales_cached_ = true;
-      }
-    }
-  }
-
-  Status Compute(OpKernelContext* context) const override;
-
-  Status BaseCompute(OpKernelContext* context, const std::vector<float>& scales) const;
-
-private:
   void ParseScalesData(const Tensor* scale, std::vector<float>& scales) const {
     const float* scale_data = scale->template Data<float>();
     int64_t scales_size = scale->Shape().Size();
@@ -82,9 +74,17 @@ private:
     memcpy(scales.data(), scale_data, scales_size * sizeof(float));
     ScalesValidation(scales, mode_);
   }
+};
 
-private:
-  bool scales_cached_;
+template <typename T>
+class Upsample : public UpsampleBase, public OpKernel {
+ public:
+  Upsample(OpKernelInfo info) : UpsampleBase(info), OpKernel(info) {
+  }
+
+  Status Compute(OpKernelContext* context) const override;
+
+  Status BaseCompute(OpKernelContext* context, const std::vector<float>& scales) const;
 };
 
 }  // namespace onnxruntime
