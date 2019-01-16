cf57f1cc4ac25ccbb7bd259472032d3889ed8a72
Changming Sun <chasun@microsoft.com>
2019-01-16 10:33:28 -0800

    Delete onnxruntime_exec

--- cmake/onnxruntime_unittests.cmake
+++ cmake/onnxruntime_unittests.cmake
@@ -418,21 +418,6 @@ if(WIN32)
   target_compile_options(onnx_test_runner_common PRIVATE -D_CRT_SECURE_NO_WARNINGS)
 endif()
 
-set(onnxruntime_exec_src_dir ${TEST_SRC_DIR}/onnxruntime_exec)
-file(GLOB onnxruntime_exec_src
-  "${onnxruntime_exec_src_dir}/*.cc"
-  "${onnxruntime_exec_src_dir}/*.h"
-  )
-
-add_executable(onnxruntime_exec ${onnxruntime_exec_src})
-
-target_include_directories(onnxruntime_exec PRIVATE ${ONNXRUNTIME_ROOT})
-# we need to force these dependencies to build first. just using target_link_libraries isn't sufficient
-add_dependencies(onnxruntime_exec ${onnxruntime_EXTERNAL_DEPENDENCIES})
-onnxruntime_add_include_to_target(onnxruntime_exec gsl)
-target_link_libraries(onnxruntime_exec PRIVATE ${onnx_test_libs})
-set_target_properties(onnxruntime_exec PROPERTIES FOLDER "ONNXRuntimeTest")
-
 add_test(NAME onnx_test_pytorch_converted
   COMMAND onnx_test_runner ${PROJECT_SOURCE_DIR}/external/onnx/onnx/backend/test/data/pytorch-converted)
 add_test(NAME onnx_test_pytorch_operator
