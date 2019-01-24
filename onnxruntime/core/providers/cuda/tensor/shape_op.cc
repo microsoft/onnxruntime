09ea51f567c26515683cfbb40dcd6a15812c81c3
RandyShuai <rashuai@microsoft.com>
2019-01-23 16:19:06 -0800

    add all fixed type support

--- onnxruntime/core/providers/cuda/tensor/shape_op.cc
+++ onnxruntime/core/providers/cuda/tensor/shape_op.cc
@@ -7,19 +7,6 @@
 namespace onnxruntime {
 namespace cuda {
 
-const std::vector<MLDataType> shapeOpTypeConstraints{
-    DataTypeImpl::GetTensorType<bool>(),
-    DataTypeImpl::GetTensorType<float>(),
-    DataTypeImpl::GetTensorType<double>(),
-    DataTypeImpl::GetTensorType<int8_t>(),
-    DataTypeImpl::GetTensorType<int16_t>(),
-    DataTypeImpl::GetTensorType<int32_t>(),
-    DataTypeImpl::GetTensorType<int64_t>(),
-    DataTypeImpl::GetTensorType<uint8_t>(),
-    DataTypeImpl::GetTensorType<uint16_t>(),
-    DataTypeImpl::GetTensorType<uint32_t>(),
-    DataTypeImpl::GetTensorType<uint64_t>()};
-
 ONNX_OPERATOR_KERNEL_EX(
     Shape,
     kOnnxDomain,
@@ -27,7 +14,7 @@ ONNX_OPERATOR_KERNEL_EX(
     kCudaExecutionProvider,
     KernelDefBuilder()
         .OutputMemoryType<OrtMemTypeCPUOutput>(0)
-        .TypeConstraint("T", shapeOpTypeConstraints)
+        .TypeConstraint("T",  DataTypeImpl::AllFixedSizeTensorTypes())
         .TypeConstraint("T1", DataTypeImpl::GetTensorType<int64_t>()),
     Shape);
 
