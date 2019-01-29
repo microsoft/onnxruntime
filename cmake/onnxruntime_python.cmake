1a63cfe13211cb7d443c297d8508d8707e136466
Xavier Dupré <xadupre@users.noreply.github.com>
2019-01-29 18:51:24 +0100

    Adds OnnxTransformer to plug onnxruntime in sckit-learn's pipeline (#389)
    Useful for transfer learning

--- cmake/onnxruntime_python.cmake
+++ cmake/onnxruntime_python.cmake
@@ -115,6 +115,9 @@ file(GLOB onnxruntime_python_datasets_data
     "${ONNXRUNTIME_ROOT}/python/datasets/*.pb"
     "${ONNXRUNTIME_ROOT}/python/datasets/*.onnx"
 )
+file(GLOB onnxruntime_python_sklapi_srcs
+    "${ONNXRUNTIME_ROOT}/python/sklapi/*.py"
+)
 
 # adjust based on what target/s onnxruntime_unittests.cmake created
 if (SingleUnitTestProject)
@@ -129,6 +132,7 @@ add_custom_command(
   COMMAND ${CMAKE_COMMAND} -E make_directory $<TARGET_FILE_DIR:${test_data_target}>/onnxruntime/capi
   COMMAND ${CMAKE_COMMAND} -E make_directory $<TARGET_FILE_DIR:${test_data_target}>/onnxruntime/datasets
   COMMAND ${CMAKE_COMMAND} -E make_directory $<TARGET_FILE_DIR:${test_data_target}>/onnxruntime/tools
+  COMMAND ${CMAKE_COMMAND} -E make_directory $<TARGET_FILE_DIR:${test_data_target}>/onnxruntime/sklapi
   COMMAND ${CMAKE_COMMAND} -E copy
       ${ONNXRUNTIME_ROOT}/__init__.py
       $<TARGET_FILE_DIR:${test_data_target}>/onnxruntime/
@@ -156,6 +160,9 @@ add_custom_command(
   COMMAND ${CMAKE_COMMAND} -E copy
       ${onnxruntime_python_datasets_data}
       $<TARGET_FILE_DIR:${test_data_target}>/onnxruntime/datasets/
+  COMMAND ${CMAKE_COMMAND} -E copy
+      ${onnxruntime_python_sklapi_srcs}
+      $<TARGET_FILE_DIR:${test_data_target}>/onnxruntime/sklapi/
   COMMAND ${CMAKE_COMMAND} -E copy
       ${onnxruntime_python_tools_srcs}
       $<TARGET_FILE_DIR:${test_data_target}>/onnxruntime/tools/
