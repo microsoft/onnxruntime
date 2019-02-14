7aa16431daf7c15cb2bb94d5a70c97a60822efc1
Dmitri Smirnov <dmitrism@microsoft.com>
2019-02-14 11:18:30 -0800

    Address review comment. Include a header to address protobuf errors   and warnings.

--- onnxruntime/contrib_ops/cpu/tokenizer.cc
+++ onnxruntime/contrib_ops/cpu/tokenizer.cc
@@ -1,10 +1,11 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-#include "onnx/defs/schema.h"
 #include "core/common/common.h"
 #include "core/framework/tensor.h"
 #include "core/framework/op_kernel.h"
+#include "core/graph/onnx_protobuf.h"
+#include "onnx/defs/schema.h"
 
 #include "core/common/utf8_util.h"
 #include "re2/re2.h"
