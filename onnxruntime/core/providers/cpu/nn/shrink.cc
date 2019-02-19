9519a96f60cef9d886b2099bf3bccb6d4889b957
hariharans29 <shariharan91@gmail.com>
2019-02-19 11:58:15 -0800

    Place files in the right location corresponding to def file location in onnx

--- onnxruntime/core/providers/cpu/nn/shrink.cc
+++ onnxruntime/core/providers/cpu/nn/shrink.cc
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-#include "core/providers/cpu/math/shrink.h"
+#include "core/providers/cpu/nn/shrink.h"
 
 #include "core/util/math.h"
 #include "core/util/math_cpuonly.h"
