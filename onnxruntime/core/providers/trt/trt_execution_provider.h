f19614cf33bfbc3130d89e154f5fb6e862e99929
steven <stevli>
2019-02-19 09:29:08 -0800

    removed trt kernel (use function compile instead)

--- onnxruntime/core/providers/trt/trt_execution_provider.h
+++ onnxruntime/core/providers/trt/trt_execution_provider.h
@@ -2,9 +2,68 @@
 // Licensed under the MIT License.
 
 #pragma once
-#include "core/providers/trt/trt_kernel.h"
+#include "core/framework/op_kernel.h"
+#include "NvInfer.h"
 
 namespace onnxruntime{
+
+static const int kBatchSize = 1;
+static const int max_batch_size = 1;
+static const int max_workspace_size = 1 << 30;
+
+#define CHECK(status)                             \
+    do                                            \
+    {                                             \
+        auto ret = (status);                      \
+        if (ret != 0)                             \
+        {                                         \
+            std::cout << "Cuda failure: " << ret; \
+            abort();                              \
+        }                                         \
+    } while (0)
+
+struct InferDeleter{
+    template<typename T>
+    void operator()(T* obj) const{
+        if( obj ){
+            obj->destroy();
+        }
+    }
+};
+
+template<typename T>
+inline std::shared_ptr<T> InferObject(T* obj){
+    if( !obj ){
+        throw std::runtime_error("Failed to create object");
+    }
+    return std::shared_ptr<T>(obj, InferDeleter());
+}
+
+class TRTLogger : public nvinfer1::ILogger{
+    nvinfer1::ILogger::Severity verbosity_;
+    std::ostream* ostream_;
+public:
+    TRTLogger(Severity verbosity=Severity::kWARNING,
+               std::ostream& ostream=std::cout)
+        : verbosity_(verbosity), ostream_(&ostream) {}
+    void log(Severity severity, const char* msg) override{
+        if( severity <= verbosity_ ){
+            time_t rawtime = std::time(0);
+            char buf[256];
+            strftime(&buf[0], 256,
+                     "%Y-%m-%d %H:%M:%S",
+                     std::gmtime(&rawtime));
+            const char* sevstr = (severity == Severity::kINTERNAL_ERROR ? "    BUG" :
+                                  severity == Severity::kERROR          ? "  ERROR" :
+                                  severity == Severity::kWARNING        ? "WARNING" :
+                                  severity == Severity::kINFO           ? "   INFO" :
+                                  "UNKNOWN");
+            (*ostream_) << "[" << buf << " " << sevstr << "] "
+                        << msg
+                        << std::endl;
+        }
+    }
+};
 
 // Information needed to construct trt execution providers.
 struct TRTExecutionProviderInfo{
