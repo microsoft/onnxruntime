55c3ec8b14572afad967835dd55b5cd30fabc17c
Dmitri Smirnov <dmitrism@microsoft.com>
2019-02-19 14:30:07 -0800

    Add RtlRunOnceExecuteOnce to cover all re2 cases

--- onnxruntime/core/platform/windows/debug_alloc.cc
+++ onnxruntime/core/platform/windows/debug_alloc.cc
@@ -210,7 +210,7 @@ Memory_LeakCheck::~Memory_LeakCheck() {
     //     empty_string = new string;
     //     empty_named_groups = new std::map<string, int>;
     //     empty_group_names = new std::map<int, string>; });
-    if (string.find("re2::RE2::Init") == std::string::npos &&
+    if (string.find("RtlRunOnceExecuteOnce") == std::string::npos &&
         string.find("testing::internal::Mutex::ThreadSafeLazyInit") == std::string::npos &&
         string.find("testing::internal::ThreadLocalRegistryImpl::GetThreadLocalsMapLocked") == std::string::npos &&
         string.find("testing::internal::ThreadLocalRegistryImpl::GetValueOnCurrentThread") == std::string::npos) {
