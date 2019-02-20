d00a511c2216122fe66f5b5b49364b1b69f88a5d
RandyShuai <rashuai@microsoft.com>
2019-02-19 17:58:32 -0800

    prototype python op

---
+++ onnxruntime/contrib_ops/cpu/python_op.cc
@@ -0,0 +1,35 @@
+#include "python_op.h"
+#include <vector>
+namespace onnxruntime {
+namespace contrib {
+
+PyObject* FromTensor(const Tensor* tensor)
+{
+    ORT_ENFORCE(tensor->DataType() == DataTypeImpl::GetType<int32_t>(), "input type not int32_t");
+    std::vector<npy_intp> dims(tensor->Shape().GetDims());
+    auto obj = PyArray_EMPTY(dims.size(), dims.data(), NPY_INT32, 0);
+    auto np_array = reinterpret_cast<PyArrayObject*>(obj);
+    memcpy(PyArray_DATA(np_array), tensor->DataRaw(), tensor->Size());
+    return PyArray_Return(np_array);
+}
+
+Status PyOp::Compute(OpKernelContext* context) const 
+{
+    auto pyArgs = PyTuple_New(context->InputCount());
+    for (int i = 0; i < context->InputCount(); ++i) {
+        PyTuple_SetItem(pyArgs, i, FromTensor(context->Input<Tensor>(i)));
+    }
+    auto pyResult = PyEval_CallObject(pyFunc_, pyArgs);
+    ORT_ENFORCE(PyArray_Check(pyResult));
+    auto np_array = reinterpret_cast<PyArrayObject*>(pyResult);
+    std::vector<int64_t> shape;
+    for (int i = 0; i < PyArray_NDIM(np_array); ++i) {
+        shape.push_back(PyArray_SHAPE(np_array)[i]);
+    }
+    auto output_tensor = context->Output(0, TensorShape(shape));
+    memcpy(output_tensor->MutableDataRaw(), PyArray_DATA(np_array), output_tensor->Size());
+    return Status::OK(); 
+}
+
+}
+}
---
+++ onnxruntime/contrib_ops/cpu/python_op.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include "core/common/common.h"
+#include "core/framework/op_kernel.h"
+#include "core/providers/common.h"
+#include <Python.h>
+#define NPY_NO_DEPRECATED_API NPY_1_7_API_VERSION
+#include "numpy/arrayobject.h"
+
+namespace onnxruntime {
+namespace contrib {
+
+class PyOp final: public OpKernel {
+public:
+    PyOp (const OpKernelInfo& info): OpKernel(info) {
+        Py_Initialize();
+        _import_array();
+        std::string module, script;
+        ORT_ENFORCE(info.GetAttr("module", &module).IsOK(), "module not specified");
+        ORT_ENFORCE(info.GetAttr("script", &script).IsOK(), "script not specified");
+        pyModule_ = PyImport_ImportModule(module.c_str());
+        ORT_ENFORCE(nullptr != pyModule_, "import python module failed");
+        pyFunc_ = PyObject_GetAttrString(pyModule_, script.c_str());
+        ORT_ENFORCE(nullptr != pyFunc_ && PyCallable_Check(pyFunc_), "script not callable");
+    }
+
+    Status Compute(OpKernelContext*) const override;
+
+    ~PyOp() {
+        if (nullptr != pyModule_) {
+            Py_XDECREF(pyModule_);
+            pyModule_ = nullptr;
+        }
+        if (nullptr != pyFunc_) {
+            Py_XDECREF(pyFunc_);
+            pyFunc_ = nullptr;
+        }
+        Py_Finalize();
+    }
+private:
+    PyObject* pyModule_ = nullptr;
+    PyObject* pyFunc_ = nullptr;
+};
+
+}
+}
