cfd5fcd6430965e4b4ea207a0e9c0beda4e5ff49
Tao Qin <taqin@microsoft.com>
2019-02-27 19:08:07 +0000

    Merged PR 3973: Tune cmake
    Tune cmake

--- cmake/onnxruntime_training.cmake
+++ cmake/onnxruntime_training.cmake
@@ -1,7 +1,7 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
-file(GLOB onnxruntime_training_srcs
+file(GLOB_RECURSE onnxruntime_training_srcs
     "${ONNXRUNTIME_ROOT}/core/training/*.h"
     "${ONNXRUNTIME_ROOT}/core/training/*.cc"
 )
@@ -9,9 +9,30 @@ file(GLOB onnxruntime_training_srcs
 add_library(onnxruntime_training ${onnxruntime_training_srcs})
 add_dependencies(onnxruntime_training ${onnxruntime_EXTERNAL_DEPENDENCIES} onnx)
 onnxruntime_add_include_to_target(onnxruntime_training  gsl onnx onnx_proto protobuf::libprotobuf)
-target_include_directories(onnxruntime_training PRIVATE ${ONNXRUNTIME_ROOT})
+target_include_directories(onnxruntime_training PRIVATE ${ONNXRUNTIME_ROOT} ${eigen_INCLUDE_DIRS})
+set_target_properties(onnxruntime_training PROPERTIES FOLDER "ONNXRuntime")
+source_group(TREE ${REPO_ROOT} FILES ${onnxruntime_training_srcs})
 
-if(WIN32)
-    # Add Code Analysis properties to enable C++ Core checks. Have to do it via a props file include.
-    set_target_properties(onnxruntime_training PROPERTIES VS_USER_PROPS ${PROJECT_SOURCE_DIR}/EnableVisualStudioCodeAnalysis.props)
-endif()
+# POC
+set(training_test_src_dir ${ONNXRUNTIME_ROOT}/test/training)
+add_executable(onnxruntime_training_poc ${training_test_src_dir}/poc/main.cc)
+onnxruntime_add_include_to_target(onnxruntime_training_poc gsl onnx onnx_proto protobuf::libprotobuf)
+target_include_directories(onnxruntime_training_poc PUBLIC ${ONNXRUNTIME_ROOT} ${eigen_INCLUDE_DIRS} ${extra_includes} ${onnxruntime_graph_header} ${onnxruntime_exec_src_dir} ${CMAKE_CURRENT_BINARY_DIR} ${CMAKE_CURRENT_BINARY_DIR}/onnx)
+
+set(ONNXRUNTIME_LIBS
+    onnxruntime_session
+    ${onnxruntime_libs}
+    ${PROVIDERS_CUDA}
+    ${PROVIDERS_MKLDNN}
+    onnxruntime_optimizer
+    onnxruntime_providers
+    onnxruntime_util
+    onnxruntime_framework
+    onnxruntime_util
+    onnxruntime_graph
+    onnxruntime_common
+    onnxruntime_mlas
+)
+
+target_link_libraries(onnxruntime_training_poc PRIVATE ${ONNXRUNTIME_LIBS} ${onnxruntime_EXTERNAL_LIBRARIES})
+set_target_properties(onnxruntime_training_poc PROPERTIES FOLDER "ONNXRuntimeTest")
