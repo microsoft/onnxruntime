15a240b429fddd5e4c094828aa49ffd893040a82
Randy Shuai <rashuai@microsoft.com>
2019-03-01 15:15:09 -0800

    remove mkldnn from linux packages

--- tools/ci_build/github/linux/run_build.sh
+++ tools/ci_build/github/linux/run_build.sh
@@ -28,6 +28,6 @@ else
         --config Debug Release --build_shared_lib \
         --skip_submodule_sync --enable_onnx_tests \
         --enable_pybind \
-        --parallel --use_openmp --use_mkldnn --build_shared_lib $BUILD_EXTR_PAR
+        --parallel --use_openmp --build_shared_lib $BUILD_EXTR_PAR
     /home/onnxruntimedev/Release/onnx_test_runner /data/onnx
 fi
