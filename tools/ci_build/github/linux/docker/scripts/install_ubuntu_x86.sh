8c841e858ecd2d5cd538c604089441f2321511fc
RandyShuai <rashuai@microsoft.com>
2019-03-08 21:17:19 -0800

    install x86 deps

--- tools/ci_build/github/linux/docker/scripts/install_ubuntu_x86.sh
+++ tools/ci_build/github/linux/docker/scripts/install_ubuntu_x86.sh
@@ -39,8 +39,7 @@ apt-get update && apt-get install -y --no-install-recommends \
         unzip \
         zip \
         rsync libunwind8 libpng16-dev \
-        python3-setuptools python3-numpy python3-wheel python python3-pip python3-pytest \
-        cmake
+        python3-setuptools python3-numpy python3-wheel python python3-pip python3-pytest
 
 locale-gen en_US.UTF-8
 update-locale LANG=en_US.UTF-8
