a83c89ba2b8d570c12192f93c8f88f8a503f1e6d
RandyShuai <rashuai@microsoft.com>
2019-03-11 16:33:19 -0700

    mergef with master yml

--- tools/ci_build/github/linux/docker/scripts/install_deps_x86.sh
+++ tools/ci_build/github/linux/docker/scripts/install_deps_x86.sh
@@ -28,11 +28,6 @@ INSTALLED_PYTHON_VERSION=$(python3 -c 'import sys; version=sys.version_info[:2];
 if [ "$INSTALLED_PYTHON_VERSION" = "3.7" ];then
   pip3 install --upgrade setuptools
 fi
-#if [ "$INSTALLED_PYTHON_VERSION" = "3.4" ];then
-#  echo "Python 3.5 and above is needed for running onnx tests!" 1>&2
-#else
-#    pip3 install onnx
-#fi
 
 #The last onnx version will be kept
 aria2c -q -d /tmp/src  http://bitbucket.org/eigen/eigen/get/3.3.7.tar.bz2
