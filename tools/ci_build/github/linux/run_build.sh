1433f87b26eb7a5665c5ad6a76d68e68ee25a516
RandyShuai <rashuai@microsoft.com>
2019-03-11 16:00:44 -0700

    disable onnx test on x86

--- tools/ci_build/github/linux/run_build.sh
+++ tools/ci_build/github/linux/run_build.sh
@@ -25,12 +25,17 @@ if [ $BUILD_DEVICE = "gpu" ]; then
         --cudnn_home /usr/local/cudnn-$_CUDNN_VERSION/cuda --build_shared_lib $BUILD_EXTR_PAR
     /home/onnxruntimedev/Release/onnx_test_runner -e cuda /data/onnx
 else
-    python3 $SCRIPT_DIR/../../build.py --build_dir /home/onnxruntimedev \
-        --config Debug Release --build_shared_lib \
-        --skip_submodule_sync --enable_onnx_tests \
-        --enable_pybind \
-        --parallel --use_openmp --build_shared_lib $BUILD_EXTR_PAR
     if [ MACHINE = "x86_64" ]; then
+        python3 $SCRIPT_DIR/../../build.py --build_dir /home/onnxruntimedev \
+            --config Debug Release --build_shared_lib \
+            --skip_submodule_sync --enable_onnx_tests \
+            --enable_pybind \
+            --parallel --use_openmp --build_shared_lib $BUILD_EXTR_PAR
         /home/onnxruntimedev/Release/onnx_test_runner /data/onnx
+    else
+        python3 $SCRIPT_DIR/../../build.py --build_dir /home/onnxruntimedev \
+            --config Debug Release --build_shared_lib \
+            --skip_submodule_sync --enable_pybind \
+            --parallel --use_openmp --build_shared_lib $BUILD_EXTR_PAR
     fi
 fi
