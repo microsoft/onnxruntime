0591c2cbc0ef23687a65d55036f693971ed7f75d
Sherlock Huang <bahuang@microsoft.com>
2019-03-12 22:19:54 +0000

    Dropout Kernel Impl

---
+++ onnxruntime/core/providers/cpu/nn/dropout.h
@@ -0,0 +1,59 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+#pragma once
+
+#ifdef _MSC_VER
+#pragma warning(push)
+#pragma warning(disable : 4996)
+#endif
+#include "core/common/common.h"
+#ifdef _MSC_VER
+#pragma warning(pop)
+#endif
+#include <random>
+#include "core/framework/op_kernel.h"
+
+namespace onnxruntime {
+
+class Dropout final : public OpKernel {
+ public:
+  Dropout(const OpKernelInfo& info) : OpKernel(info) {
+    ORT_ENFORCE(info.GetAttr<float>("ratio", &ratio_).IsOK());
+    keep_prob_ = 1.0f - ratio_;
+
+    // TODO: enable following when is_test is present
+    /*int64_t is_test = 1;
+    ORT_ENFORCE(info.GetAttr("is_test", &is_test).IsOK());
+    is_test_ = (is_test == 1);*/
+  }
+
+  Status Compute(OpKernelContext* context) const override;
+
+ private:
+  bool is_test_ = true;
+  float ratio_;
+  float keep_prob_;
+};
+
+class DrouputGrad final : public OpKernel {
+ public:
+  DrouputGrad(const OpKernelInfo& info) : OpKernel(info) {
+    ORT_ENFORCE(info.GetAttr<float>("ratio", &ratio_).IsOK());
+    keep_prob_ = 1.0f - ratio_;
+
+    // TODO: enable following when is_test is present
+    /*int64_t is_test = 1;
+    ORT_ENFORCE(info.GetAttr("is_test", &is_test).IsOK());
+    is_test_ = (is_test == 1);*/
+  }
+
+  Status Compute(OpKernelContext* context) const override;
+
+ private:
+  bool is_test_ = true;
+  float ratio_;
+  float keep_prob_;
+};
+
+}  //namespace onnxruntime
--- onnxruntime/core/providers/cpu/tensor/identity_op.cc
+++ onnxruntime/core/providers/cpu/tensor/identity_op.cc
@@ -6,12 +6,6 @@
 namespace onnxruntime {
 
 ONNX_CPU_OPERATOR_KERNEL(
-    Dropout,
-    7,
-    KernelDefBuilder().TypeConstraint("T", {DataTypeImpl::GetTensorType<MLFloat16>(), DataTypeImpl::GetTensorType<float>(), DataTypeImpl::GetTensorType<double>()}),
-    IdentityOp<true>);
-
-ONNX_CPU_OPERATOR_KERNEL(
     Identity,
     1,
     KernelDefBuilder().TypeConstraint("T", DataTypeImpl::AllTensorTypes()).Alias(0, 0),
