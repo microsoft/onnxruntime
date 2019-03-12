b8c5bf2e3637cc5e8e31e82ca1a92059ca0859f7
Raymond Yang <zihao.yang@microsoft.com>
2019-03-12 15:33:13 -0700

    Cherry pick x86 fix in release 0.3.0 (#606)
    * merge x86 linux build
    
    * remove wrong argu

--- tools/ci_build/build.py
+++ tools/ci_build/build.py
@@ -300,6 +300,7 @@ def generate_build_tree(cmake_path, source_dir, build_dir, cuda_home, cudnn_home
                  "-Donnxruntime_USE_NUPHAR=" + ("ON" if args.use_nuphar else "OFF"),
                  "-Donnxruntime_USE_EIGEN_THREADPOOL=" + ("ON" if args.use_eigenthreadpool else "OFF"), 
                  "-Donnxruntime_USE_TRT=" + ("ON" if args.use_trt else "OFF"),
+                 "-Donnxruntime_BUILD_x86=" + ("ON" if args.x86 else "OFF"),
                  ]
     if args.use_brainslice:
         bs_pkg_name = args.brain_slice_package_name.split('.', 1)
