bff09dc4cd69848ddebd2f572ff21e5141348303
Dmitri Smirnov <yuslepukhin@users.noreply.github.com>
2019-03-15 14:03:15 -0700

    Rework Transpose as a generic type agnostic implementation (#561)
      Make Transpose op impl generic and add std::string support.
      Un-templatize implementation functions that make use of memcpy().
      Support all types per spec. Add string tests.

--- onnxruntime/test/providers/cpu/tensor/transpose_test.cc
+++ onnxruntime/test/providers/cpu/tensor/transpose_test.cc
@@ -7,16 +7,17 @@
 namespace onnxruntime {
 namespace test {
 
+template <class T>
 void TransposeTest(std::vector<int64_t>& input_shape,
-                   std::vector<float>& input_vals,
+                   std::vector<T>& input_vals,
                    std::vector<int64_t>* p_perm,
                    std::vector<int64_t> expected_shape,
-                   std::initializer_list<float>& expected_vals) {
+                   std::initializer_list<T>& expected_vals) {
   OpTester test("Transpose");
   if (nullptr != p_perm)
     test.AddAttribute("perm", *p_perm);
-  test.AddInput<float>("X", input_shape, input_vals);
-  test.AddOutput<float>("Y", expected_shape, expected_vals);
+  test.AddInput<T>("X", input_shape, input_vals);
+  test.AddOutput<T>("Y", expected_shape, expected_vals);
   test.Run();
 }
 
@@ -36,6 +37,21 @@ TEST(TransposeOpTest, TwoDimNoAttr) {
   TransposeTest(input_shape, input_vals, nullptr, expected_shape, expected_vals);
 }
 
+TEST(TransposeOpTest, TwoDimNoAttrStr) {
+  std::vector<int64_t> input_shape({2, 3});
+  std::vector<std::string> input_vals = {
+      "1", "2", "3",
+      "4", "5", "6"};
+
+  std::vector<int64_t> expected_shape({3, 2});
+  std::initializer_list<std::string> expected_vals = {
+      "1", "4",
+      "2", "5",
+      "3", "6"};
+
+  TransposeTest(input_shape, input_vals, nullptr, expected_shape, expected_vals);
+}
+
 // Test 2 dimensional transpose, with permutation attribute specified
 TEST(TransposeOpTest, TwoDim) {
   std::vector<int64_t> input_shape({2, 3});
@@ -49,6 +65,22 @@ TEST(TransposeOpTest, TwoDim) {
       1.0f, 4.0f,
       2.0f, 5.0f,
       3.0f, 6.0f};
+
+  TransposeTest(input_shape, input_vals, &perm, expected_shape, expected_vals);
+}
+
+TEST(TransposeOpTest, TwoDimStr) {
+  std::vector<int64_t> input_shape({2, 3});
+  std::vector<std::string> input_vals = {
+      "1", "2", "3",
+      "4", "5", "6"};
+
+  std::vector<int64_t> perm = {1, 0};
+  std::vector<int64_t> expected_shape({3, 2});
+  std::initializer_list<std::string> expected_vals = {
+      "1", "4",
+      "2", "5",
+      "3", "6"};
 
   TransposeTest(input_shape, input_vals, &perm, expected_shape, expected_vals);
 }
@@ -99,6 +131,57 @@ TEST(TransposeOpTest, ThreeDim) {
       5.3f,
       3.3f,
       6.3f,
+
+  };
+
+  TransposeTest(input_shape, input_vals, &perm, expected_shape, expected_vals);
+}
+
+TEST(TransposeOpTest, ThreeDimStr) {
+  std::vector<int64_t> input_shape({4, 2, 3});
+  std::vector<std::string> input_vals = {
+      "1", "2", "3",
+      "4", "5", "6",
+
+      "1", "2", "3",
+      "4", "5", "6",
+
+      "1", "2", "3",
+      "4", "5", "6",
+
+      "1", "2", "3",
+      "4", "5", "6"};
+
+  std::vector<int64_t> perm = {0, 2, 1};
+  std::vector<int64_t> expected_shape({4, 3, 2});
+  std::initializer_list<std::string> expected_vals = {
+      "1",
+      "4",
+      "2",
+      "5",
+      "3",
+      "6",
+
+      "1",
+      "4",
+      "2",
+      "5",
+      "3",
+      "6",
+
+      "1",
+      "4",
+      "2",
+      "5",
+      "3",
+      "6",
+
+      "1",
+      "4",
+      "2",
+      "5",
+      "3",
+      "6"
 
   };
 
