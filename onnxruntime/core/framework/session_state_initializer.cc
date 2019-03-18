971058fc38b24c8582da032f1421840c8506046f
Scott McKay <Scott.McKay@microsoft.com>
2019-03-19 06:55:59 +1000

    Avoid copy of pre-existing value to subgraph output (#637)
    * Add AllocKind::kShare to allow copying the MLValue for a pre-existing value to a graph output when an Identity node is involved. Ideally we can make this handling for an Identity node more general purpose, however the current logic to free an MLValue during execution doesn't take into account a re-use point also needing a free. Due to that, limit the scope and start with a somewhat ugly hardcoded approach.
    
    Migrate some changes from PR497
    
    The existing Loop unit tests exercise the new code. Also manually stepped through the problematic model to verify the unnecessary copy was avoided.
    
    * Fix build error
    
    * Fix missing switch case in debug output of allocation plan
    
    * Limit optimization to Loop

--- onnxruntime/core/framework/session_state_initializer.cc
+++ onnxruntime/core/framework/session_state_initializer.cc
@@ -59,7 +59,8 @@ SessionStateInitializer::SessionStateInitializer(const std::basic_string<PATH_CH
       kernel_registry_manager_{kernel_registry_manager},
       logger_{session_state.Logger()} {}
 
-common::Status SessionStateInitializer::CreatePlan(const std::vector<NodeArg*>& outer_scope_node_args,
+common::Status SessionStateInitializer::CreatePlan(const Node* parent_node,
+                                                   const std::vector<NodeArg*>& outer_scope_node_args,
                                                    bool enable_sequential_execution) {
   auto graph_viewer = std::make_unique<onnxruntime::GraphViewer>(graph_);
 
@@ -83,7 +84,7 @@ common::Status SessionStateInitializer::CreatePlan(const std::vector<NodeArg*>& 
     // CreatePlan will create a new SequentialExecutionPlan instance that we will
     // save into the session state.
     ORT_RETURN_IF_ERROR(
-        SequentialPlanner::CreatePlan(*graph_viewer, valid_outer_scope_node_args, execution_providers_,
+        SequentialPlanner::CreatePlan(parent_node, *graph_viewer, valid_outer_scope_node_args, execution_providers_,
                                       kernel_registry_manager_, mlvalue_name_idx_map, exec_plan));
 
     session_state_.SetExecutionPlan(std::move(exec_plan));
@@ -91,7 +92,7 @@ common::Status SessionStateInitializer::CreatePlan(const std::vector<NodeArg*>& 
     // Parallel execution still uses same allocation plan, but has limitation of memory buffer reuse.
     SequentialPlannerContext context(true /* enable parallel execution */);
     ORT_RETURN_IF_ERROR(
-        SequentialPlanner::CreatePlan(*graph_viewer, valid_outer_scope_node_args, execution_providers_,
+        SequentialPlanner::CreatePlan(parent_node, *graph_viewer, valid_outer_scope_node_args, execution_providers_,
                                       kernel_registry_manager_, mlvalue_name_idx_map, context, exec_plan));
 
     session_state_.SetExecutionPlan(std::move(exec_plan));
@@ -112,12 +113,13 @@ common::Status SessionStateInitializer::InitializeAndSave(const std::vector<Node
   // lambda to save initialized tensors into SessionState directly
   const Env& env = Env::Default();
   ORT_RETURN_IF_ERROR(
-      SaveInitializedTensors(env, graph_loc_, graph_, exec_plan, execution_providers_, mlvalue_name_idx_map,
-                             session_state_.GetMutableWeightsBuffers(),
-                             [this](int idx, const onnxruntime::MLValue& value, const OrtCallback& d) -> Status {
-                               return session_state_.AddInitializedTensor(idx, value, &d);
-                             },
-                             logger_));
+      SaveInitializedTensors(
+          env, graph_loc_, graph_, exec_plan, execution_providers_, mlvalue_name_idx_map,
+          session_state_.GetMutableWeightsBuffers(),
+          [this](int idx, const onnxruntime::MLValue& value, const OrtCallback& d) -> Status {
+            return session_state_.AddInitializedTensor(idx, value, &d);
+          },
+          logger_));
   // remove weights from the graph now to save memory but in many cases it won't save memory, if the tensor was
   // preallocated with the some other tensors in a single 'allocate' call, which is very common.
   // TODO: make it better
