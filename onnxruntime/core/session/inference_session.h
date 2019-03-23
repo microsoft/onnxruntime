64809b624e534a4c6f16f6cb48a4bdcce814c015
Ashwini Khade <askhade@microsoft.com>
2019-03-22 17:32:24 -0700

    Set default optimization level to Level1

--- onnxruntime/core/session/inference_session.h
+++ onnxruntime/core/session/inference_session.h
@@ -60,7 +60,7 @@ struct SessionOptions {
   unsigned max_num_graph_transformation_steps = 5;  // TODO choose a good default here?
 
   // set graph optimization level
-  TransformerLevel graph_optimization_level = TransformerLevel::Default;
+  TransformerLevel graph_optimization_level = TransformerLevel::Level1;
 
   // How many threads in the session thread pool.
   int session_thread_pool_size = 0;
