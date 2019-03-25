bbf3437c02c4e4b40ecfaf748d772e572d6aede0
pengwa <pengwa@microsoft.com>
2019-03-25 14:23:20 +0800

    refine this op description/examples a bit

--- onnxruntime/core/graph/contrib_ops/reverse_sequence_schema_defs.cc
+++ onnxruntime/core/graph/contrib_ops/reverse_sequence_schema_defs.cc
@@ -10,44 +10,46 @@
 namespace onnxruntime {
 namespace contrib {
 
-using ::ONNX_NAMESPACE::AttributeProto;
-using ::ONNX_NAMESPACE::OPTIONAL;
-using ::ONNX_NAMESPACE::OpSchema;
-using ::ONNX_NAMESPACE::InferenceContext;
-using ::ONNX_NAMESPACE::TensorShapeProto;
-using ::ONNX_NAMESPACE::TensorProto;
-using ::ONNX_NAMESPACE::TensorProto_DataType;
+using ONNX_NAMESPACE::AttributeProto;
+using ONNX_NAMESPACE::OPTIONAL;
+using ONNX_NAMESPACE::OpSchema;
+using ONNX_NAMESPACE::InferenceContext;
+using ONNX_NAMESPACE::TensorShapeProto;
+using ONNX_NAMESPACE::TensorProto;
+using ONNX_NAMESPACE::TensorProto_DataType;
 
 static const char* ReverseSequence_ver1_doc = R"DOC(
 Reverse batch of sequences having different lengths specified by `sequence_lens`.
 
-For each slice i iterating on batch axis, the operator reverses the first sequence_lens[i] elements on time axis.
+For each slice i iterating on batch axis, the operator reverses the first sequence_lens[i] elements on time axis,
+and copies elements whose index's beyond sequence_lens[i] to the output. So the output slice i contains reversed
+sequences on the first sequence_lens[i] elements, then have original values copied for the other elements.
 
 Example 1:
-  input = [
-    [0.0, 1.0, 2.0],
-    [3.0, 4.0, 5.0],
-  ]
-  sequence_lens = [3, 3]
-  data_format = "batch_major"
+  input = [[0.0, 4.0, 8.0,  12.0],
+           [1.0, 5.0, 9.0,  13.0],
+           [2.0, 6.0, 10.0, 14.0],
+           [3.0, 7.0, 11.0, 15.0]]
+  sequence_lens = [4, 3, 2, 1]
+  data_format = "time_major"
 
-  output = [
-    [2.0, 1.0, 0.0],
-    [5.0, 4.0, 3.0],
-  ]
+  output = [[3.0, 6.0, 9.0,  12.0],
+            [2.0, 5.0, 8.0,  13.0],
+            [1.0, 4.0, 10.0, 14.0],
+            [0.0, 7.0, 11.0, 15.0]]
 
 Example 2:
-  input = [
-    [0.0, 1.0, 2.0],
-    [3.0, 4.0, 5.0],
-  ]
-  sequence_lens = [2, 1]
+  input = [[0.0,  1.0,  2.0,  3.0 ],
+           [4.0,  5.0,  6.0,  7.0 ],
+           [8.0,  9.0,  10.0, 11.0],
+           [12.0, 13.0, 14.0, 15.0]]
+  sequence_lens = [1, 2, 3, 4]
   data_format = "batch_major"
 
-  output = [
-    [1.0, 0.0, 2.0],
-    [3.0, 4.0, 5.0],
-  ]
+  output = [[0.0,  1.0,  2.0,  3.0 ],
+            [5.0,  4.0,  6.0,  7.0 ],
+            [10.0, 9.0,  8.0,  11.0],
+            [15.0, 14.0, 13.0, 12.0]]
 )DOC";
 
 static const char* Input_Data_Format_ver1_doc = R"DOC(
@@ -64,7 +66,7 @@ OpSchema& RegisterReverseSequenceOpSchema(OpSchema&& op_schema) {
       OpSchema::all_tensor_types(),
       "Input and output types can be of any tensor type.")
     .TypeConstraint(
-      "T1", {"tensor(int32)"}, "Constrain seq_lens to integer tensor.")
+      "T1", {"tensor(int32)"}, "Constrain sequence_lens to integer tensor.")
     .Attr(
       "data_format",
       Input_Data_Format_ver1_doc,
