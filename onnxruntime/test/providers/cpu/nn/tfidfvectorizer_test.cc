9f6ef775164a4217d1ca9bc2bbb880ee74d5c9f4
xavier dupré <xavier.dupre@gmail.com>
2019-03-28 19:16:04 +0100

    add unit test failure for an empty input {0, 1}

--- onnxruntime/test/providers/cpu/nn/tfidfvectorizer_test.cc
+++ onnxruntime/test/providers/cpu/nn/tfidfvectorizer_test.cc
@@ -136,6 +136,28 @@ TEST(TfIdfVectorizerTest, Int32_TF_onlyBigrams_Skip0_Empty_Dim2) {
   test.Run(OpTester::ExpectResult::kExpectFailure);
 }
 
+TEST(TfIdfVectorizerTest, Int32_TF_onlyBigrams_Skip01_Empty_Dim2) {
+  OpTester test("TfIdfVectorizer", opset_ver, domain);
+  // s=0, Min=Max=2, weights empty, int32
+  InitTestAttr(test, "TF", 2, 2, 0,
+               {0, 4},
+               {0, 1, 2, 3, 4, 5, 6},  //7 output indexes
+               {},
+               {2, 3, 5, 4,         //1-grams
+                5, 6, 7, 8, 6, 7},  //bi-grams
+               {});
+
+  std::vector<int64_t> dims{0, 1};
+  std::vector<int32_t> input = {};
+  test.AddInput<int32_t>("T", dims, input);
+
+  std::vector<int64_t> out_dims{7};
+  std::vector<float> output = {0, 0, 0, 0, 0, 0, 0};
+  test.AddOutput<float>("Y", out_dims, output);
+
+  test.Run(OpTester::ExpectResult::kExpectFailure);
+}
+
 TEST(TfIdfVectorizerTest, Int32_TF_onlyBigrams_Skip0_Empty_Dim2N) {
   OpTester test("TfIdfVectorizer", opset_ver, domain);
   // s=0, Min=Max=2, weights empty, int32
