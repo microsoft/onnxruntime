2624de906ff4daeaf6885996523c79bef8b5233c
Konstantinos Karanasos <kkaranasos@gmail.com>
2019-04-03 22:34:20 -0700

    Generalize node removal (#743)
    Generalize node removal method in graph_utils. This is a higher-level method that keeps the graph consistent so that no Resolve is needed after the removal of a node. 
    The new method supports the removal of nodes with a single input (be it an incoming node or an initializer) and a single output (but allowing multiple output edges of that output). It also takes into account the case that one of the output edges is fed to a subgraph.
    Also updated the rewrite rules to use this new, less restrictive method, and improved the rules' conditions. Introduced a GraphEdge struct to simplify various methods in graph_utils.

--- onnxruntime/core/graph/graph_utils.h
+++ onnxruntime/core/graph/graph_utils.h
@@ -9,6 +9,7 @@
 namespace onnxruntime {
 
 namespace graph_utils {
+
 bool IsSupportedOptypeVersionAndDomain(const Node& node,
                                        const std::string& op_type,
                                        ONNX_NAMESPACE::OperatorSetVersion version,
@@ -20,7 +21,9 @@ bool IsSupportedOptypeVersionAndDomain(const Node& node,
 bool IsSupportedProvider(const Node& node,
                          const std::unordered_set<std::string>& compatible_providers);
 
-/** Check whether the node has a single input and a single output. */
+/** Check whether the node has a single input and a single output. The single input can be either the output of
+    another node or an initializer, but not an implicit input from a parent subgraph. The single output can be 
+    fed to multiple downstream operators, i.e., it can have multiple output edges. */
 bool IsSingleInSingleOutNode(const Node& node);
 
 /** Returns true if the graph has the given input.*/
@@ -28,6 +31,12 @@ bool IsGraphInput(const Graph& graph, const NodeArg* input);
 
 /** Checks if the given node has only constant inputs (initializers). */
 bool AllNodeInputsAreConstant(const Graph& graph, const Node& node);
+
+/** Get the name of the incoming NodeArg with the specified index for the given node. */
+const std::string& GetNodeInputName(const Node& node, int index);
+
+/** Get the name of the outgoing NodeArg with the specified index for the given node. */
+const std::string& GetNodeOutputName(const Node& node, int index);
 
 /** Return the attribute of a Node with a given name. */
 const ONNX_NAMESPACE::AttributeProto* GetNodeAttribute(const Node& node, const std::string& attr_name);
@@ -49,8 +58,10 @@ bool GetRepeatedNodeAttributeValues(const Node& node,
 Status ForAllMutableSubgraphs(Graph& main_graph, std::function<Status(Graph&)> func);
 Status ForAllSubgraphs(const Graph& main_graph, std::function<Status(const Graph&)> func);
 
-/** Remove the given single-input-single-output Node from the Graph. */
-bool RemoveSingleInSingleOutNode(Graph& graph, Node& node);
+/** Remove the given single-input Node from the Graph. The single input might be either
+    another node or an initializer, but not an implicit input. The node should have a single
+    output but can have multiple output edges. */
+bool RemoveSingleInputNode(Graph& graph, Node& node);
 
 /** Remove all output edges from the given Node of the Graph. 
     This should probably be elevated to the Graph API eventually. */
