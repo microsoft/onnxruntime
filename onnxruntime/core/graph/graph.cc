92e1a1bf7a07168543ed11de96f05173fdd5609a
kkaranasos <kokarana@microsoft.com>
2019-04-03 17:05:52 -0700

    fixes for node removal in case of subgraphs whose removed edges appears in the graph's outputs

--- onnxruntime/core/graph/graph.cc
+++ onnxruntime/core/graph/graph.cc
@@ -875,7 +875,7 @@ void Graph::RemoveEdge(NodeIndex src_node_index, NodeIndex dst_node_index, int s
     // It means there was no edge between these two slots before.
     ORT_THROW("Argument type mismatch when removing edge.");
   }
-  
+
   nodes_[dst_node_index]->MutableRelationships().input_edges.erase(Node::EdgeEnd(*nodes_[src_node_index], src_arg_slot, dst_arg_slot));
   nodes_[src_node_index]->MutableRelationships().output_edges.erase(Node::EdgeEnd(*nodes_[dst_node_index], src_arg_slot, dst_arg_slot));
 }
@@ -887,6 +887,8 @@ Status Graph::BuildConnections(std::vector<std::string>& outer_scope_node_args_c
 
   // recurse into subgraphs first so we can update any nodes in this graph that are used by those subgraphs
   if (!resolve_context_.nodes_with_subgraphs.empty()) {
+    const bool loaded_from_model_file = GraphLoadedFromModelFile(graph_proto_);
+
     for (auto* node : resolve_context_.nodes_with_subgraphs) {
       for (auto& subgraph : node->MutableSubgraphs()) {
         std::vector<std::string> node_args_consumed;
@@ -938,6 +940,17 @@ Status Graph::BuildConnections(std::vector<std::string>& outer_scope_node_args_c
             AddEdge(output_node.Index(), node->Index(), entry->second.second, input_slot_index);
 
             inner_nodes.insert(&output_node);
+
+            // if this Graph was built manually, remove the implicit input from graph outputs if it is present there
+            // and not explicitly listed in the ordered graph outputs (as that implies we should leave it as an output).
+            // if the Graph was loaded from a GraphProto, honor the explicit graph outputs and leave as is.
+            if (!loaded_from_model_file) {
+              auto in_ordered_graph_outputs = find(graph_output_order_.cbegin(), graph_output_order_.cend(), node_arg);
+              if (in_ordered_graph_outputs == graph_output_order_.cend()) {
+                graph_outputs_.erase(std::remove(graph_outputs_.begin(), graph_outputs_.end(), node_arg),
+                                     graph_outputs_.end());
+              }
+            }
           }
         }
       }
