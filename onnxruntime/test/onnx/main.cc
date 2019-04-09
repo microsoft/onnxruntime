867b8a135bdfd5ef4dca109a30da35ea6d6f7659
Randy Shuai <rashuai@microsoft.com>
2019-04-08 17:59:19 -0700

    try enable fp16_tiny_yolov2 on linux

--- onnxruntime/test/onnx/main.cc
+++ onnxruntime/test/onnx/main.cc
@@ -349,7 +349,6 @@ int real_main(int argc, char* argv[], OrtEnv** p_env) {
 
 #if defined(__GNUG__) && !defined(__LP64__)
   broken_tests["nonzero_example"] = "failed: type mismatch";
-  broken_tests["fp16_tiny_yolov2"] = "Need to adjust the per_sample_tolerance: 0.2";
 #endif
 
   int result = 0;
