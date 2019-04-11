1a0ca16f60bd57d11645351c319a4a859d4f5b0f
Ashwini Khade <askhade@microsoft.com>
2019-04-10 17:20:35 -0700

    update onnx to bring in quantized ops (#808)
    * update onnx + move quantized ops kernels and test to onnx + remove exp ops
    
    * update onnx
    
    * Revert "update onnx"
    
    This reverts commit 533abfc297e75473a74505fb89921ffc05c46a1c.
    
    * add generated csharp test file

--- onnxruntime/core/providers/cpu/tensor/quantize_linear.h
+++ onnxruntime/core/providers/cpu/tensor/quantize_linear.h
@@ -8,34 +8,20 @@
 #include "core/util/math_cpuonly.h"
 
 namespace onnxruntime {
-namespace contrib {
 
 template <typename T>
 class DequantizeLinear final : public OpKernel {
  public:
-  DequantizeLinear(const OpKernelInfo& info) : OpKernel(info) {
-    has_axis_ = info.GetAttr<int64_t>("axis", &axis_).IsOK();
-  }
+  DequantizeLinear(const OpKernelInfo& info) : OpKernel(info) { }
 
-  Status Compute(OpKernelContext* context) const override;
-
- private:
-  int64_t axis_ = 0;
-  bool has_axis_;
+  Status Compute(OpKernelContext* context) const override; 
 };
 
 template <typename T>
 class QuantizeLinear final : public OpKernel {
  public:
-  QuantizeLinear(const OpKernelInfo& info) : OpKernel(info) {
-    has_axis_ = info.GetAttr<int64_t>("axis", &axis_).IsOK();
-  }
+  QuantizeLinear(const OpKernelInfo& info) : OpKernel(info) { }
 
   Status Compute(OpKernelContext* context) const override;
-
- private:
-  int64_t axis_ = 0;
-  bool has_axis_;
 };
-}  // namespace contrib
 }  // namespace onnxruntime
