92d67f90944509b0e43a3cf8739cb2da8ff48c79
tmccrmck <tjmcc01@gmail.com>
2019-04-16 14:22:26 -0700

    Improves documentation for GenerateResponseInJson function

--- onnxruntime/hosting/http/json_handling.h
+++ onnxruntime/hosting/http/json_handling.h
@@ -14,14 +14,12 @@ namespace hosting {
 namespace http = boost::beast::http;
 
 // Deserialize Json input to PredictRequest.
-// Unknown field in the json file will be ignored.
+// Unknown fields in the json file will be ignored.
 google::protobuf::util::Status GetRequestFromJson(const std::string& json_string, /* out */ onnxruntime::hosting::PredictRequest& request);
 
 // Serialize PredictResponse to json string
-// 1. To save the space, no whitespace will be added to prettify the string;
-// 2. Proto3 primitive fields with default values will be omitted in JSON output. Eg. int32 field with value 0 will be omitted
-// 3. Enums will be printed as string, not int, to improve the readability
-// 4. Not preserve proto field names
+// 1. Proto3 primitive fields with default values will be omitted in JSON output. Eg. int32 field with value 0 will be omitted
+// 2. Enums will be printed as string, not int, to improve readability
 google::protobuf::util::Status GenerateResponseInJson(const onnxruntime::hosting::PredictResponse& response, /* out */ std::string& json_string);
 
 // Constructs JSON error message from error code object and error message
