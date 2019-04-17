faccc57806979f810845987dadc92b005beb105a
daquexian <daquexian566@gmail.com>
2019-04-17 16:10:36 +0800

    Remove python in deps for android

--- tools/ci_build/github/linux/docker/scripts/install_deps_android.sh
+++ tools/ci_build/github/linux/docker/scripts/install_deps_android.sh
@@ -30,36 +30,6 @@ for build_type in 'Debug' 'Relwithdebinfo'; do
   popd
 done
 export ONNX_ML=1
-INSTALLED_PYTHON_VERSION=$(python3 -c 'import sys; version=sys.version_info[:2]; print("{0}.{1}".format(*version));')
-if [ "$INSTALLED_PYTHON_VERSION" = "3.7" ];then
-  pip3 install --upgrade setuptools
-fi
-if [ "$INSTALLED_PYTHON_VERSION" = "3.4" ];then
-  echo "Python 3.5 and above is needed for running onnx tests!" 1>&2
-else
-  #Install ONNX
-  #5af210ca8a1c73aa6bae8754c9346ec54d0a756e is v1.2.3
-  #bae6333e149a59a3faa9c4d9c44974373dcf5256 is v1.3.0
-  #9e55ace55aad1ada27516038dfbdc66a8a0763db is v1.4.1
-  #3a9a87871021a02d6b970c1e26ffa4c0dd720677 is v1.4.1 latest
-  for onnx_version in "5af210ca8a1c73aa6bae8754c9346ec54d0a756e" "bae6333e149a59a3faa9c4d9c44974373dcf5256" "9e55ace55aad1ada27516038dfbdc66a8a0763db" "3a9a87871021a02d6b970c1e26ffa4c0dd720677"; do
-    if [ -z ${lastest_onnx_version+x} ]; then
-      echo "first pass";
-    else
-      echo "deleting old onnx-${lastest_onnx_version}";
-      pip3 uninstall -y onnx
-    fi
-    lastest_onnx_version=$onnx_version
-    aria2c -q -d /tmp/src  https://github.com/onnx/onnx/archive/$onnx_version.tar.gz
-    tar -xf /tmp/src/onnx-$onnx_version.tar.gz -C /tmp/src
-    cd /tmp/src/onnx-$onnx_version
-    git clone https://github.com/pybind/pybind11.git third_party/pybind11
-    python3 setup.py bdist_wheel
-    pip3 install -q dist/*
-    mkdir -p /data/onnx/$onnx_version
-    backend-test-tools generate-data -o /data/onnx/$onnx_version
-  done
-fi
 
 #The last onnx version will be kept
 
