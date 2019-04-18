eedd3654422cebb30669dea3fd069c293f3eb490
Du Li <duli1@microsoft.com>
2019-04-17 21:16:00 -0700

    Adding resize cuda kernel

---
+++ onnxruntime/core/providers/cuda/tensor/resize.cc
@@ -0,0 +1,27 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+#include "resize.h"
+
+namespace onnxruntime {
+namespace cuda {
+#define REGISTER_KERNEL_TYPED(T)                                  \
+  ONNX_OPERATOR_TYPED_KERNEL_EX(                                  \
+      Resize,                                                     \
+      kOnnxDomain,                                                \
+      10,                                                         \
+      T,                                                          \
+      kCudaExecutionProvider,                                     \
+      KernelDefBuilder()                                          \
+          .InputMemoryType<OrtMemTypeCPUInput>(1)                 \
+          .TypeConstraint("T", DataTypeImpl::GetTensorType<T>()), \
+      Resize<T>);
+
+REGISTER_KERNEL_TYPED(float)
+REGISTER_KERNEL_TYPED(double)
+REGISTER_KERNEL_TYPED(MLFloat16)
+REGISTER_KERNEL_TYPED(int32_t)
+REGISTER_KERNEL_TYPED(uint8_t)
+
+}  // namespace cuda
+}  // namespace onnxruntime
---
+++ onnxruntime/core/providers/cuda/tensor/resize.h
@@ -0,0 +1,24 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+#pragma once
+
+#include "upsample.h"
+
+namespace onnxruntime {
+namespace cuda {
+
+template <typename T>
+class Resize : public Upsample<T> {
+ public:
+  Resize(OpKernelInfo info) : Upsample(info) {
+    UpsampleBase::is_resize = true;
+  }
+
+  Status ComputeInternal(OpKernelContext* context) const override {
+    return Upsample::ComputeInternal(context);
+  }
+};
+
+}  // namespace cuda
+}  // namespace onnxruntime
