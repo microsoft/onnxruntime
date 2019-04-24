ee8ab294e8edf16d8eb7bc7fb1b88b77faa27900
Changming Sun <chasun@microsoft.com>
2019-04-23 19:27:58 -0700

    Update onnx

--- tools/ci_build/github/linux/run_build.sh
+++ tools/ci_build/github/linux/run_build.sh
@@ -20,36 +20,23 @@ if [ $BUILD_OS = "android" ]; then
     /opt/cmake/bin/cmake -DCMAKE_TOOLCHAIN_FILE=/android-ndk/build/cmake/android.toolchain.cmake -DANDROID_CPP_FEATURES=exceptions -DANDROID_PLATFORM=android-28 -DANDROID_ABI=arm64-v8a -DCMAKE_BUILD_TYPE=Release -Donnxruntime_CROSS_COMPILING=ON -Donnxruntime_BUILD_x86=OFF -DONNX_CUSTOM_PROTOC_EXECUTABLE=/usr/bin/protoc ../cmake
     /opt/cmake/bin/cmake --build . -- -j$(nproc)
 else
+    COMMON_BUILD_ARGS="--skip_submodule_sync --enable_onnx_tests --parallel --build_shared_lib --build_wheel --use_openmp"
     if [ $BUILD_DEVICE = "gpu" ]; then
         _CUDNN_VERSION=$(echo $CUDNN_VERSION | cut -d. -f1-2)
         python3 $SCRIPT_DIR/../../build.py --build_dir /build \
-            --config Debug Release \
-            --skip_submodule_sync --enable_onnx_tests \
-            --parallel --build_shared_lib \
-            --use_cuda --use_openmp \
+            --config Debug Release $COMMON_BUILD_ARGS \
+            --use_cuda \
             --cuda_home /usr/local/cuda \
-            --cudnn_home /usr/local/cudnn-$_CUDNN_VERSION/cuda --build_shared_lib $BUILD_EXTR_PAR
+            --cudnn_home /usr/local/cudnn-$_CUDNN_VERSION/cuda $BUILD_EXTR_PAR
     elif [ $BUILD_DEVICE = "tensorrt" ]; then
         _CUDNN_VERSION=$(echo $CUDNN_VERSION | cut -d. -f1-2)
         python3 $SCRIPT_DIR/../../build.py --build_dir /build \
-            --config Release \
-            --enable_onnx_tests \
-            --parallel --build_shared_lib \
+            --config Release $COMMON_BUILD_ARGS \
             --use_tensorrt --tensorrt_home /workspace/tensorrt \
-            --use_openmp \
             --cuda_home /usr/local/cuda \
-            --cudnn_home /usr/local/cuda --build_shared_lib $BUILD_EXTR_PAR
-    elif [ $BUILD_DEVICE = "ngraph" ]; then
+            --cudnn_home /usr/local/cuda $BUILD_EXTR_PAR
+    else #cpu and ngraph
         python3 $SCRIPT_DIR/../../build.py --build_dir /build \
-            --config Debug Release --build_shared_lib \
-            --skip_submodule_sync --enable_onnx_tests \
-            --build_wheel \
-            --parallel --use_openmp $BUILD_EXTR_PAR
-    else
-        python3 $SCRIPT_DIR/../../build.py --build_dir /build \
-            --config Debug Release --build_shared_lib \
-            --skip_submodule_sync --enable_onnx_tests \
-            --build_wheel \
-            --parallel --use_openmp --build_shared_lib $BUILD_EXTR_PAR
+            --config Debug Release $COMMON_BUILD_ARGS $BUILD_EXTR_PAR
     fi
 fi
