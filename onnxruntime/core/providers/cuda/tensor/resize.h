2a204103b4e26992bd1ed750255d208e93cb7205
Du Li <duli1@microsoft.com>
2019-04-24 21:43:11 -0700

    Fix CI errors

--- onnxruntime/core/providers/cuda/tensor/resize.h
+++ onnxruntime/core/providers/cuda/tensor/resize.h
@@ -9,13 +9,13 @@ namespace onnxruntime {
 namespace cuda {
 
 template <typename T>
-class Resize : public Upsample<T> {
+class Resize : public Upsample {
  public:
   Resize(OpKernelInfo info) : Upsample(info) {
   }
 
   Status ComputeInternal(OpKernelContext* context) const override {
-    return Upsample::ComputeInternal(context);
+    return Upsample<T>::ComputeInternal(context);
   }
 };
 
