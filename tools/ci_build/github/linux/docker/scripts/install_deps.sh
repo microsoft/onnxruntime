7cf3580feb112c9682b8e26f858d2c9abf363921
Raymond Yang <zihao.yang@microsoft.com>
2019-04-30 08:44:32 -0700

    Bump up onnx version (#936)
    * bump up onnx version

--- tools/ci_build/github/linux/docker/scripts/install_deps.sh
+++ tools/ci_build/github/linux/docker/scripts/install_deps.sh
@@ -38,8 +38,8 @@ else
   #5af210ca8a1c73aa6bae8754c9346ec54d0a756e is v1.2.3
   #bae6333e149a59a3faa9c4d9c44974373dcf5256 is v1.3.0
   #9e55ace55aad1ada27516038dfbdc66a8a0763db is v1.4.1
-  #27d4b617e7097cda7d0d4c45ff2b09d248f33179 is v1.4.1 latest
-    for onnx_version in "5af210ca8a1c73aa6bae8754c9346ec54d0a756e" "bae6333e149a59a3faa9c4d9c44974373dcf5256" "9e55ace55aad1ada27516038dfbdc66a8a0763db" "27d4b617e7097cda7d0d4c45ff2b09d248f33179"; do
+  #c1c04af4e9fa0c96fbc1fda7b330bb994118f3c5 is v1.4.1 latest
+    for onnx_version in "5af210ca8a1c73aa6bae8754c9346ec54d0a756e" "bae6333e149a59a3faa9c4d9c44974373dcf5256" "9e55ace55aad1ada27516038dfbdc66a8a0763db" "c1c04af4e9fa0c96fbc1fda7b330bb994118f3c5"; do
     if [ -z ${lastest_onnx_version+x} ]; then
       echo "first pass";
     else
