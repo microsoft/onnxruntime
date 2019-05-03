a545501eabc3a2897a05d8506f27c423a0496b52
Luis Daniel Castellanos <luis.daniel.castellanos@intel.com>
2019-05-03 10:35:15 -0700

    Added OPENVINO ci pipeline
    Added new pipeline for openvino provider,
    made changes to support the docker build and
    onnxruntime build with openvino.
    
    Signed-off-by: Luis Daniel Castellanos <luis.daniel.castellanos@intel.com>

---
+++ tools/ci_build/github/linux/docker/scripts/install_openvino.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -e
+while getopts o: parameter_Option
+do case "${parameter_Option}"
+in
+o) OPENVINO_VERSION=${OPTARG};;
+esac
+done
+
+git clone https://github.com/opencv/dldt.git /data/dldt
+
+export INTEL_CVSDK_DIR=/data/dldt
+
+cd ${INTEL_CVSDK_DIR}/inference-engine
+git submodule init
+git submodule update --recursive
+git checkout tags/$OPENVINO_VERSION -b $OPENVINO_VERSION
+
+mkdir -p build
+cd build
+cmake ..
+make -j$(nproc)
+
+cd ${INTEL_CVSDK_DIR}
+mkdir -p deployment_tools
+mv inference-engine inference_engine && mv inference_engine deployment_tools/
+
+cd ${INTEL_CVSDK_DIR}/deployment_tools/inference_engine
+mkdir -p lib/ubuntu_16.04/intel64
+mv bin/intel64/Release/lib/* lib/ubuntu_16.04/intel64
+
+cd ~
