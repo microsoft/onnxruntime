862e8255628a0aaf9d596e85219f08703ffac69c
Ryan Hill <38674843+RyanUnderhill@users.noreply.github.com>
2019-05-03 19:32:46 -0700

    C++ wrapper for ABI (#958)

--- onnxruntime/test/util/include/test/compare_mlvalue.h
+++ onnxruntime/test/util/include/test/compare_mlvalue.h
@@ -23,5 +23,5 @@ std::pair<COMPARE_RESULT, std::string> CompareMLValue(const MLValue& real, const
                                                       double relative_per_sample_tolerance, bool post_processing);
 
 //verify if the 'value' matches the 'expected' ValueInfoProto. 'value' is a model output
-std::pair<COMPARE_RESULT, std::string> VerifyValueInfo(const ONNX_NAMESPACE::ValueInfoProto& expected, const OrtValue* value);
+std::pair<COMPARE_RESULT, std::string> VerifyValueInfo(const ONNX_NAMESPACE::ValueInfoProto& expected, const Ort::Value& value);
 }  // namespace onnxruntime
