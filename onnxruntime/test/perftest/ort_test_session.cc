bd396b5485c1b3166e109bf022daf8a34fbb0624
Changming Sun <chasun@microsoft.com>
2019-05-07 11:50:29 -0700

    perf test runner: support NCHW->NHWC rotation (#976)
    1. Support NCHW->NHWC rotation. For some models, they have different input data for ONNX and TF. Like the mlperf resnet50 model, the onnx model expects NCHW input but TF expects NHWC. NHWC is the TensorFlow default, however NCHW is the optimal format for both MKL and CUDNN. Unless I can get another TF model in NCHW format, I have to rotate the input by myself.
    
    2. Previously, the perf test tool only randomly pick one test data set, and repeatedly run with it again and again. This PR will load all the test data sets and do the random choice for each InferenceSession::Run().

--- onnxruntime/test/perftest/ort_test_session.cc
+++ onnxruntime/test/perftest/ort_test_session.cc
@@ -11,10 +11,12 @@
 namespace onnxruntime {
 namespace perftest {
 
-std::chrono::duration<double> OnnxRuntimeTestSession::Run(const OrtValue* const* input) {
+std::chrono::duration<double> OnnxRuntimeTestSession::Run() {
+  const std::uniform_int_distribution<int>::param_type p(0, input_length);
+  const size_t id = static_cast<size_t>(dist_(rand_engine_, p));
+  OrtValueArray* const input = test_inputs.at(id);
   auto start = std::chrono::high_resolution_clock::now();
-
-  ORT_THROW_ON_ERROR(OrtRun(session_object_, nullptr, input_names_.data(), input, input_names_.size(),
+  ORT_THROW_ON_ERROR(OrtRun(session_object_, nullptr, input_names_.data(), input->Data(), input_names_.size(),
                             output_names_raw_ptr.data(), output_names_raw_ptr.size(), output_values_.data()));
   auto end = std::chrono::high_resolution_clock::now();
   std::chrono::duration<double> duration_seconds = end - start;
@@ -25,9 +27,10 @@ std::chrono::duration<double> OnnxRuntimeTestSession::Run(const OrtValue* const*
   return duration_seconds;
 }
 
-OnnxRuntimeTestSession::OnnxRuntimeTestSession(OrtEnv* env, const PerformanceTestConfig& performance_test_config,
+OnnxRuntimeTestSession::OnnxRuntimeTestSession(OrtEnv* env, std::random_device& rd,
+                                               const PerformanceTestConfig& performance_test_config,
                                                const TestModelInfo* m)
-    : input_names_(m->GetInputCount()) {
+    : rand_engine_(rd()), input_names_(m->GetInputCount()), input_length(m->GetInputCount()) {
   SessionOptionsWrapper sf(env);
   const bool enable_cpu_mem_arena = true;
   const std::string& provider_name = performance_test_config.machine_config.provider_type_name;
