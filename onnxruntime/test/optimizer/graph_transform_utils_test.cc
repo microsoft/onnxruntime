ed33633e326ce164c5f55f8d0bbcf617be3419d2
Konstantinos Karanasos <kkaranasos@gmail.com>
2019-05-07 17:23:04 -0700

    Avoid using transformers that rely on contrib ops in tests (#983)

--- onnxruntime/test/optimizer/graph_transform_utils_test.cc
+++ onnxruntime/test/optimizer/graph_transform_utils_test.cc
@@ -56,8 +56,8 @@ TEST(GraphTransformerUtilsTests, TestGenerateGraphTransformers_CustomList) {
   // custom list of rules and transformers
   std::string l1_rule1 = "EliminateIdentity";
   std::string l1_transformer = "ConstantFolding";
-  std::string l2_transformer = "GemmActivationFusion";
-  std::vector<std::string> custom_list = {l1_rule1, l1_transformer, l2_transformer};
+  std::string l2_rule1 = "ConvBNFusion";
+  std::vector<std::string> custom_list = {l1_rule1, l1_transformer, l2_rule1};
 
   auto transformers = transformer_utils::GenerateTransformers(TransformerLevel::Level1, custom_list);
   ASSERT_TRUE(transformers.size() == 2);
@@ -72,7 +72,8 @@ TEST(GraphTransformerUtilsTests, TestGenerateGraphTransformers_CustomList) {
   
   transformers = transformer_utils::GenerateTransformers(TransformerLevel::Level2, custom_list);
   ASSERT_TRUE(transformers.size() == 1);
-  ASSERT_TRUE(transformers[0]->Name() == l2_transformer);
+  rule_transformer = dynamic_cast<RuleBasedGraphTransformer*>(transformers[0].get());
+  ASSERT_TRUE(rule_transformer->RulesCount() == 1);
 }
 
 }  // namespace test
