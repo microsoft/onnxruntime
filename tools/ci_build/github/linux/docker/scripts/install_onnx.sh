5b7e68051851cd871687fd0f47eda50896bdd4bc
Dmitri Smirnov <dmitrism@microsoft.com>
2019-05-17 15:56:19 -0700

    Uncomment ConstantOfShape tests.   Advance ONNX submodule to 5c51f0dbbe88ee1536f17ee7bd462b2ab3772c52   Update test script, make sure exclusions are uniform.

--- tools/ci_build/github/linux/docker/scripts/install_onnx.sh
+++ tools/ci_build/github/linux/docker/scripts/install_onnx.sh
@@ -4,13 +4,13 @@
 #bae6333e149a59a3faa9c4d9c44974373dcf5256 is v1.3.0
 #9e55ace55aad1ada27516038dfbdc66a8a0763db is v1.4.1
 #7d7bc83d29a328233d3e8affa4c4ea8b3e3599ef is v1.5.0
-#5d0975f43c1224edce17177783b859fca4b8e2b1 is master
+#5c51f0dbbe88ee1536f17ee7bd462b2ab3772c52 is master
 declare -A version2tag
 version2tag+=(["5af210ca8a1c73aa6bae8754c9346ec54d0a756e"]="onnx123"
               ["bae6333e149a59a3faa9c4d9c44974373dcf5256"]="onnx130"
               ["9e55ace55aad1ada27516038dfbdc66a8a0763db"]="onnx141"
               ["7d7bc83d29a328233d3e8affa4c4ea8b3e3599ef"]="onnx150"
-              ["5d0975f43c1224edce17177783b859fca4b8e2b1"]="onnxtip")
+              ["5c51f0dbbe88ee1536f17ee7bd462b2ab3772c52"]="onnxtip")
 for onnx_version in ${!version2tag[@]}; do
   if [ -z ${lastest_onnx_version+x} ]; then
     echo "first pass";
