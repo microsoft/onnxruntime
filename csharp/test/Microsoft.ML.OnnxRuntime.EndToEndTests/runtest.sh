87e61ae28a20b8f752a31774d32c1f3162482dbb
Jignesh Parmar <jignparm@microsoft.com>
2019-05-21 12:39:01 -0700

    Minor update for pipeline tests

--- csharp/test/Microsoft.ML.OnnxRuntime.EndToEndTests/runtest.sh
+++ csharp/test/Microsoft.ML.OnnxRuntime.EndToEndTests/runtest.sh
@@ -10,12 +10,12 @@ TestDataChecksum=$5
 
 #TestDataUrl and TestDataChecksum comes from the build env variable
 
-echo "Downloading test data"
-python3 $SourceRoot/tools/ci_build/build.py --update --download_test_data --build_dir $BuildDir --test_data_url $TestDataUrl --test_data_checksum $TestDataChecksum
-if [ $? -ne 0 ]; then
-    echo "Failed to download test data"
-    exit 1
-fi
+#echo "Downloading test data"
+#python3 $SourceRoot/tools/ci_build/build.py --update --download_test_data --build_dir $BuildDir --test_data_url $TestDataUrl --test_data_checksum $TestDataChecksum
+#if [ $? -ne 0 ]; then
+#    echo "Failed to download test data"
+#    exit 1
+#fi
 
 OldDir=`pwd`
 cd $SourceRoot
