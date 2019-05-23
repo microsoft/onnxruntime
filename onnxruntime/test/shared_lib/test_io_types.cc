b768a9c59768cefaff3b5a43d734f48aa9b10e26
RyanUnderhill <ryanhill@microsoft.com>
2019-05-23 13:54:04 -0700

    Rename fix

--- onnxruntime/test/shared_lib/test_io_types.cc
+++ onnxruntime/test/shared_lib/test_io_types.cc
@@ -4,19 +4,19 @@
 #include "core/session/onnxruntime_cxx_api.h"
 #include "test_fixture.h"
 
-static void TestModelInfo(const Ort::Session& inference_session, bool is_input, const std::vector<int64_t>& dims) {
+static void TestModelInfo(const Ort::Session& session, bool is_input, const std::vector<int64_t>& dims) {
   size_t input_count;
   if (is_input) {
-    input_count = inference_session.GetInputCount();
+    input_count = session.GetInputCount();
   } else {
-    input_count = inference_session.GetOutputCount();
+    input_count = session.GetOutputCount();
   }
   ASSERT_EQ(1, input_count);
   Ort::TypeInfo input_type_info{nullptr};
   if (is_input) {
-    inference_session.GetInputTypeInfo(0);
+    session.GetInputTypeInfo(0);
   } else {
-    inference_session.GetOutputTypeInfo(0);
+    session.GetOutputTypeInfo(0);
   }
 
   ASSERT_NE(nullptr, input_type_info);
@@ -37,6 +37,6 @@ TEST_F(CApiTest, input_output_type_info) {
   constexpr PATH_TYPE model_uri = TSTR("../models/opset8/test_squeezenet/model.onnx");
   Ort::SessionOptions session_options;
   Ort::Session session(env_, model_uri, session_options);
-  TestModelInfo(inference_session, true, {1, 3, 224, 224});
-  TestModelInfo(inference_session, false, {1, 1000, 1, 1});
+  TestModelInfo(session, true, {1, 3, 224, 224});
+  TestModelInfo(session, false, {1, 1000, 1, 1});
 }
