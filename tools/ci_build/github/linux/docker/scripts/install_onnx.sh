49cf520bb9c0906c606d701c6ac2f8e1005ec542
RandyShuai <rashuai@microsoft.com>
2019-05-29 15:20:04 -0700

    fix x86 compile err

--- tools/ci_build/github/linux/docker/scripts/install_onnx.sh
+++ tools/ci_build/github/linux/docker/scripts/install_onnx.sh
@@ -24,7 +24,7 @@ for onnx_version in ${!version2tag[@]}; do
   cd /tmp/src/onnx-$onnx_version
   git clone https://github.com/pybind/pybind11.git third_party/pybind11
   python3 setup.py bdist_wheel
-  pip3 install -q dist/*
+  pip3 install -q dist/*`uname -p`".whl"
   mkdir -p /data/onnx/${version2tag[$onnx_version]}
   backend-test-tools generate-data -o /data/onnx/${version2tag[$onnx_version]}
   echo $onnx_version":"${version2tag[$onnx_version]} >> /data/onnx/version2tag
