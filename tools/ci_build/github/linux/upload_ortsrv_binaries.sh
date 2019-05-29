30f8f05bf7a113b0fd9a56da82e8bb685d0283bf
Klein Hu <bohu@microsoft.com>
2019-05-28 17:09:09 -0700

    Temporarily disable binary split

--- tools/ci_build/github/linux/upload_ortsrv_binaries.sh
+++ tools/ci_build/github/linux/upload_ortsrv_binaries.sh
@@ -20,11 +20,11 @@ echo "Creating temp folder $BINARY_DIR/$BUILD_ID ... "
 mkdir $BINARY_DIR/$BUILD_ID
 cp $BINARY_DIR/onnxruntime_server $BINARY_DIR/$BUILD_ID
 
-echo "Split binary and symbol ..."
-cd $BINARY_DIR/$BUILD_ID
-objcopy --only-keep-debug onnxruntime_server onnxruntime_server.symbol
-strip --strip-debug --strip-unneeded onnxruntime_server
-objcopy --add-gnu-debuglink=onnxruntime_server.symbol onnxruntime_server
+# echo "Split binary and symbol ..."
+# cd $BINARY_DIR/$BUILD_ID
+# objcopy --only-keep-debug onnxruntime_server onnxruntime_server.symbol
+# strip --strip-debug --strip-unneeded onnxruntime_server
+# objcopy --add-gnu-debuglink=onnxruntime_server.symbol onnxruntime_server
 
 echo "Create build info file ..."
 echo "Build parameters: $BUILD_PARAMETERS" >> build_info.txt
