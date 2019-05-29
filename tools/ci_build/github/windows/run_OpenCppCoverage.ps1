59e9e935fb8f2811f7e3fe1603c903393e3b3b91
Shah Asaduzzaman (ASAD) <shahasad@microsoft.com>
2019-05-28 17:03:02 -0700

    updated the run coverage script. added installation to the windows CI for trying

--- tools/ci_build/github/windows/run_OpenCppCoverage.ps1
+++ tools/ci_build/github/windows/run_OpenCppCoverage.ps1
@@ -8,58 +8,74 @@ Param(
     [Parameter(Mandatory=$true, HelpMessage="Build root.")][string]$BuildRoot
 )
 
-"OpenCppCoverageExe=$OpenCppCoverageExe"
-"SourceRoot=$SourceRoot"
-"BuildRoot=$BuildRoot"
+#"OpenCppCoverageExe=$OpenCppCoverageExe"
+#"SourceRoot=$SourceRoot"
+#"BuildRoot=$BuildRoot"
 
-$sources = Join-Path $SourceRoot "onnxruntime\core" 
+$coreSources = Join-Path $SourceRoot "onnxruntime" 
+$headerSources = Join-Path $SourceRoot "include" 
 $buildDir = Join-Path $BuildRoot "Debug"
+
+Write-Host "sources = " $coreSources $headerSources
+Write-Host "buildDir = " $buildDir
+Write-Host "OpenCppCoverageExe = " $OpenCppCoverageExe
+Write-Host "SourceRoot = " $SourceRoot
+Write-Host "BuildRoot = " $BuildRoot
+
+
 
 function RunTest([string]$test_cmd, [string[]]$export_types, [string[]]$inputs)
 {
-    $cmd = "$OpenCppCoverageExe --sources=$sources --modules=$buildDir --working_dir=$buildDir "
+    $cmd = "$OpenCppCoverageExe"
+    $cmdParams = @("--sources=$headerSources","--sources=$coreSources","--modules=$buildDir","--working_dir=$buildDir")
+#    Write-Host "cmd = " $cmd
 
     foreach($input in $inputs)
     {
         $inputPath = Join-Path $buildDir $input
-        $cmd += "--input_coverage=$inputPath "
+        $cmdParams += "--input_coverage=$inputPath"
     }
 
     foreach($export_type in $export_types)
     {
-        $cmd += "--export_type=$export_type "
+        $cmdParams += "--export_type=$export_type"
     }
 
-    $cmd += "-- $test_cmd"
-    $cmd
-    Invoke-Expression -Command:$cmd
+    $cmdParams += @("--","$test_cmd")
+    # $cmd += $cmdParams
+    #Write-Host "final cmd = " $cmd
+    #Write-Host "final cmdParams = " $cmdParams
+
+    # Invoke-Expression -Command:$cmd  
+    # echoargs.exe "$cmdParams"
+    #& $cmd $cmdParams
+    Start-Process -Wait -FilePath $cmd -ArgumentList $cmdParams 
 }
 
 # generate cobertura xml output and html report
 $outputXml = Join-Path $buildDir "cobertura.xml"
 $outputDir = Join-Path $buildDir "OpenCppCoverageResults"
+$modelDir = Join-Path $BuildRoot "..\models" 
 
 # Lotus unit tests
 $onnxruntime_test_all = Join-Path $buildDir "onnxruntime_test_all.exe"
 RunTest $onnxruntime_test_all ("binary:" + (Join-Path $buildDir "onnxruntime_test_all.cov"))
 
-# Set to true to use vstest.console.exe to run the model tests. Requires a 'models' directory to have been created
-# in $BuildRoot by running 'generate-data' from the ONNX installation.
-# Set to false to use onnx_test_runner.exe and the models from the cmake/external directory.
-$useVSTest = $false
+# ONNX test runner tests. 
+$onnx_test_runner = Join-Path $buildDir "onnx_test_runner.exe" 
+$otr_1 = "$onnx_test_runner " + $modelDir #(Join-Path $sourceRoot "cmake/external/onnx/onnx/backend/test/data/pytorch-converted")
+#$otr_2 = "$onnx_test_runner " + $modelDir #(Join-Path $sourceRoot "cmake/external/onnx/onnx/backend/test/data/pytorch-operator")
+RunTest $otr_1 ("binary:"  + (Join-Path $buildDir "otr_1.cov"))
+#RunTest $otr_2 ("cobertura:$outputXml", "html:$outputDir") ("onnxruntime_test_all.cov", "otr_1.cov")
 
-if ($haveModelsInBuildDir)
-{
-    # ONNX tests via the vstest onnx_test_runner. merge output from onnxruntime_test_all.cov. 
-    $onnx_test_models = "vstest.console.exe " + (Join-Path $buildDir "onnx_test_runner_vstest.dll")
-    RunTest $onnx_test_models ("cobertura:$outputXml", "html:$outputDir") ("onnxruntime_test_all.cov")
-}
-else
-{
-    # ONNX test runner tests. 
-    $onnx_test_runner = Join-Path $buildDir "onnx_test_runner.exe" 
-    $otr_1 = "$onnx_test_runner " + (Join-Path $sourceRoot "cmake/external/onnx/onnx/backend/test/data/pytorch-converted")
-    $otr_2 = "$onnx_test_runner " + (Join-Path $sourceRoot "cmake/external/onnx/onnx/backend/test/data/pytorch-operator")
-    RunTest $otr_1 ("binary:"  + (Join-Path $buildDir "otr_1.cov"))
-    RunTest $otr_2 ("cobertura:$outputXml", "html:$outputDir") ("onnxruntime_test_all.cov", "otr_1.cov")
-}
+# C-API/Shared-lib test
+$shared_lib_test = Join-Path $buildDir "onnxruntime_shared_lib_test.exe"
+RunTest $shared_lib_test ("binary:" + (Join-Path $buildDir "onnxruntime_shared_lib_test.cov"))
+
+# MLAS test
+$mlas_test = Join-Path $buildDir "onnxruntime_mlas_test.exe"
+RunTest $mlas_test ("binary:" + (Join-Path $buildDir "onnxruntime_mlas_test.cov"))
+
+# Session Without Environment test
+$session_test = Join-Path $buildDir "onnxruntime_test_framework_session_without_environment_standalone.exe"
+RunTest $session_test ("binary:" + (Join-Path $buildDir "onnxruntime_session_without_environment_test.cov"))
