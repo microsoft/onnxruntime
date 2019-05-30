4757933afe56ea4332141ebd8e2feed204cd915f
RandySheriffH <48490400+RandySheriffH@users.noreply.github.com>
2019-05-30 16:19:06 -0700

    Exclude test by onnx version tag (#1073)
    * add version filter to failed tests
    
    * exclude test from backend
    
    * exclude shrink from opset 9
    
    * fix compile err
    
    * exclude certain version of constant shape
    
    * enable flatten test
    
    * fix compile err
    
    * comment mvn test
    
    * disable constantofshape test in x86
    
    * disable x86 test
    
    * get model version from imported opset
    
    * test linux x86 case
    
    * disable nonzero opset 10
    
    * make mutex const
    
    * test filter by commit id
    
    * adjust substr offset
    
    * Limit test platform
    
    * remove change impacting TFModleInfo.h
    
    * refactoring
    
    * refactoring
    
    * test x86 pipeline with filter
    
    * add comment
    
    * restrict version extraction on non-win
    
    * restrict version extraction on non-win
    
    * add tag
    
    * exclude case from backend test
    
    * remove dup
    
    * remove dup
    
    * make script runnable
    
    * hard code adsolute path
    
    * refactor log
    
    * fix x86 compile err
    
    * fix x86 compile err
    
    * fix x86 compile err
    
    * sync with latest tensorrt
    
    * switch to regex
    
    * fix cpu pipeline err
    
    * test filter
    
    * disable nonzero from all versions

--- tools/ci_build/github/linux/docker/scripts/install_deps_x86.sh
+++ tools/ci_build/github/linux/docker/scripts/install_deps_x86.sh
@@ -28,29 +28,7 @@ INSTALLED_PYTHON_VERSION=$(python3 -c 'import sys; version=sys.version_info[:2];
 if [ "$INSTALLED_PYTHON_VERSION" = "3.7" ];then
   pip3 install --upgrade setuptools
 else
-  #Install ONNX
-  #5af210ca8a1c73aa6bae8754c9346ec54d0a756e is v1.2.3
-  #bae6333e149a59a3faa9c4d9c44974373dcf5256 is v1.3.0
-  #9e55ace55aad1ada27516038dfbdc66a8a0763db is v1.4.1
-  #7d7bc83d29a328233d3e8affa4c4ea8b3e3599ef is v1.5.0
-  #5d0975f43c1224edce17177783b859fca4b8e2b1 is master
-  for onnx_version in "5af210ca8a1c73aa6bae8754c9346ec54d0a756e" "bae6333e149a59a3faa9c4d9c44974373dcf5256" "9e55ace55aad1ada27516038dfbdc66a8a0763db" "7d7bc83d29a328233d3e8affa4c4ea8b3e3599ef" "5d0975f43c1224edce17177783b859fca4b8e2b1"; do
-    if [ -z ${lastest_onnx_version+x} ]; then
-      echo "first pass";
-    else
-      echo "deleting old onnx-${lastest_onnx_version}";
-      pip3 uninstall -y onnx
-    fi
-    lastest_onnx_version=$onnx_version
-    aria2c -q -d /tmp/src  https://github.com/onnx/onnx/archive/$onnx_version.tar.gz
-    tar -xf /tmp/src/onnx-$onnx_version.tar.gz -C /tmp/src
-    cd /tmp/src/onnx-$onnx_version
-    git clone https://github.com/pybind/pybind11.git third_party/pybind11
-    python3 setup.py bdist_wheel
-    pip3 install onnx
-    mkdir -p /data/onnx/$onnx_version
-    backend-test-tools generate-data -o /data/onnx/$onnx_version
-  done
+  source /tmp/scripts/install_onnx.sh x86
 fi
 
 #The last onnx version will be kept
