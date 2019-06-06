c2215166c979cb24bfdffcd365e6ccd792d7d3cf
RyanUnderhill <ryanhill@microsoft.com>
2019-06-05 17:30:13 -0700

    Change SessionOptions APIs to always return a status, for consistency and ease of use (a couple returned 0 or -1 for success/failure)

--- onnxruntime/core/framework/error_code_helper.h
+++ onnxruntime/core/framework/error_code_helper.h
@@ -8,3 +8,10 @@
 namespace onnxruntime {
 OrtStatus* ToOrtStatus(const onnxruntime::common::Status& st);
 };
+
+#define API_IMPL_BEGIN try {
+#define API_IMPL_END                                          \
+  }                                                           \
+  catch (std::exception & ex) {                               \
+    return OrtCreateStatus(ORT_RUNTIME_EXCEPTION, ex.what()); \
+  }
