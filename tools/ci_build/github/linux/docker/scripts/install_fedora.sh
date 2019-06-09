e653b20fd54cc3b05ca52b0a34103f64b76bfe85
Changming Sun <chasun@microsoft.com>
2019-06-09 15:45:38 -0700

    update fedora docker scripts

--- tools/ci_build/github/linux/docker/scripts/install_fedora.sh
+++ tools/ci_build/github/linux/docker/scripts/install_fedora.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -e
-dnf install -y --best --allowerasing redhat-lsb-core expat-devel libcurl-devel protobuf-compiler protobuf-devel protobuf rpmdevtools tar unzip ccache curl gcc gcc-c++ zlib-devel make git python2-devel python3-devel python3-numpy libunwind icu aria2 rsync python3-setuptools python3-wheel bzip2
+dnf install -y --best --allowerasing libjpeg-turbo-devel redhat-lsb-core expat-devel libcurl-devel protobuf-compiler protobuf-devel protobuf rpmdevtools tar unzip ccache curl gcc gcc-c++ zlib-devel make git python2-devel python3-devel python3-numpy libunwind icu aria2 rsync python3-setuptools python3-wheel bzip2
