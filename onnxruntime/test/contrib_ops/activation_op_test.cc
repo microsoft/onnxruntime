3a691fe5da7b96b08fac20ab25e8fbe3456479e5
Ashwini Khade <askhade@microsoft.com>
2019-06-12 19:12:22 -0700

    bug fix

--- onnxruntime/test/contrib_ops/activation_op_test.cc
+++ onnxruntime/test/contrib_ops/activation_op_test.cc
@@ -9,22 +9,16 @@ using namespace onnxruntime::test;
 namespace onnxruntime {
 namespace test {
 
-std::vector<float> input_vals = {
+std::vector<float> input_values = {
     -1.0f, 0, 1.0f,                                              // normal input values for activation
     100.0f, -100.0f, 1000.0f, -1000.0f,                          // input values that leads to exp() overflow
     FLT_MIN, FLT_MIN / 10, -FLT_MIN / 10,                        // min, denorm, -denorm
     FLT_MAX, -FLT_MAX, std::numeric_limits<float>::infinity()};  // max, -max, inf
 
-std::vector<float> no_inf_input_vals = {
-    -1.0f, 0, 1.0f,                        // normal input values for activation
-    FLT_MIN, FLT_MIN / 10, -FLT_MIN / 10,  // min, denorm, -denorm
-    FLT_MAX, -FLT_MAX};                    // max, -max
-
-
 TEST(ActivationOpTest, ThresholdedRelu_version_1_to_9) {
   float alpha = 0.1f;
   TestUnaryElementwiseOp("ThresholdedRelu",
-                         input_vals,
+                         input_values,
                          [alpha](float x) { return (x >= alpha) ? x : 0; },
                          {{"alpha", alpha}}, true, 1);
 }
@@ -34,7 +28,7 @@ TEST(ActivationOpTest, ScaledTanh) {
   static constexpr float beta = 1.5f;
 
   TestUnaryElementwiseOp("ScaledTanh",
-                         input_vals,
+                         input_values,
                          [](float x) { return alpha * tanh(beta * x); },
                          {{"alpha", alpha}, {"beta", beta}});
 }
@@ -44,7 +38,7 @@ TEST(ActivationOpTest, ParametricSoftplus) {
   static constexpr float beta = 1.5f;
 
   TestUnaryElementwiseOp("ParametricSoftplus",
-                         input_vals,
+                         input_values,
                          [](float x) {
                            float bx = beta * x;
                            if (bx > 0)
