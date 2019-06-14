b332eea9e28ba49fdb1eecd852ce99c2ab063196
Randy Shuai <rashuai@microsoft.com>
2019-06-14 13:34:59 -0700

    fix cmake issue

--- cmake/onnxruntime_language_interop_ops.cmake
+++ cmake/onnxruntime_language_interop_ops.cmake
@@ -5,5 +5,5 @@ file (GLOB onnxruntime_language_interop_ops_src "${ONNXRUNTIME_ROOT}/core/langua
 add_library(onnxruntime_language_interop ${onnxruntime_language_interop_ops_src})
 onnxruntime_add_include_to_target(onnxruntime_language_interop onnxruntime_common onnxruntime_framework onnxruntime_pyop gsl onnx onnx_proto protobuf::libprotobuf)
 target_include_directories(onnxruntime_language_interop PRIVATE ${ONNXRUNTIME_ROOT} ${eigen_INCLUDE_DIRS})
-
+install(DIRECTORY DESTINATION)
 
