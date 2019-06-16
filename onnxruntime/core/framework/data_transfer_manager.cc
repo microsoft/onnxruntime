46a4e8363b8bd4ebf75854a72e35fcbc5c53b0ce
linkerzhang <kezhan@microsoft.com>
2019-06-16 10:54:36 -0700

    fix linux build break.

--- onnxruntime/core/framework/data_transfer_manager.cc
+++ onnxruntime/core/framework/data_transfer_manager.cc
@@ -1,8 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-#pragma once
-
 #include "core/framework/data_transfer_manager.h"
 
 namespace onnxruntime {
