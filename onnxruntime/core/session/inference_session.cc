d62802ae5c93f444ded0eebeaa04a4a34581bef9
Maik Riechert <maik.riechert@microsoft.com>
2019-06-26 15:45:49 +0100

    add fall-back for hardware_concurrency() if 0

--- onnxruntime/core/session/inference_session.cc
+++ onnxruntime/core/session/inference_session.cc
@@ -106,6 +106,10 @@ InferenceSession::InferenceSession(const SessionOptions& session_options, loggin
     int pool_size = session_options_.session_thread_pool_size <= 0
                         ? std::thread::hardware_concurrency() / 2
                         : session_options_.session_thread_pool_size;
+    if (pool_size == 0) {
+      // hardware_concurrency() may have returned 0, reset to safe default
+      pool_size = 1;
+    }
 
     thread_pool_ = std::make_unique<onnxruntime::concurrency::ThreadPool>("SESSION", pool_size);
   }
