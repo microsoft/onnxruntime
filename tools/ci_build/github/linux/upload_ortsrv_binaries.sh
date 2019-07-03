fb04697a119f3c076c87473f74e510ecb2bb5279
Ubuntu <colin@onnx.2mtiqnnbmiiuvf5ckej1en3z3a.xx.internal.cloudapp.net>
2019-07-03 23:57:29 +0000

    fix typo

--- tools/ci_build/github/linux/upload_ortsrv_binaries.sh
+++ tools/ci_build/github/linux/upload_ortsrv_binaries.sh
@@ -22,7 +22,7 @@ mkdir $BINARY_DIR/$BUILD_ID
 cp $BINARY_DIR/onnxruntime_server $BINARY_DIR/$BUILD_ID
 cp $BINARY_DIR/onnxruntime_server.symbol $BINARY_DIR/$BUILD_ID
 cp $BINARY_DIR/libonnxruntime.so.* $BINARY_DIR/$BUILD_ID
-cp $BINARY_DIR/libonxruntime.symbol $BINARY_DIR/$BUILD_ID
+cp $BINARY_DIR/libonnxruntime.symbol $BINARY_DIR/$BUILD_ID
 
 echo "Create build info file ..."
 echo "Build parameters: $BUILD_PARAMETERS" >> $BINARY_DIR/$BUILD_ID/build_info.txt
