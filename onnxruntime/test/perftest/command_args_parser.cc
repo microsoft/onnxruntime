a7217b778fdc8d51c3bf3b0d321b91c142fe2088
Manohar Karlapalem <manohar.karlapalem@intel.com>
2019-07-09 11:54:55 -0700

    Add missing 'openvino' option in perftest Usage message

--- onnxruntime/test/perftest/command_args_parser.cc
+++ onnxruntime/test/perftest/command_args_parser.cc
@@ -31,7 +31,7 @@ namespace perftest {
       "\t-M: Disable memory pattern.\n"
       "\t-A: Disable memory arena\n"
       "\t-c [parallel runs]: Specifies the (max) number of runs to invoke simultaneously. Default:1.\n"
-      "\t-e [cpu|cuda|mkldnn|tensorrt|ngraph]: Specifies the provider 'cpu','cuda','mkldnn','tensorrt', 'ngraph' or 'openvino'. "
+      "\t-e [cpu|cuda|mkldnn|tensorrt|ngraph|openvino]: Specifies the provider 'cpu','cuda','mkldnn','tensorrt', 'ngraph' or 'openvino'. "
       "Default:'cpu'.\n"
       "\t-b [tf|ort]: backend to use. Default:ort\n"
       "\t-r [repeated_times]: Specifies the repeated times if running in 'times' test mode.Default:1000.\n"
