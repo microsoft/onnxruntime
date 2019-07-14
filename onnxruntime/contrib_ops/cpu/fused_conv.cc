9eaab1fd0cbac684414572d4cf16ae54c205fc09
Tracy Sharpe <tracysh@microsoft.com>
2019-07-13 19:00:12 -0700

    cleanup fused conv activation handling

--- onnxruntime/contrib_ops/cpu/fused_conv.cc
+++ onnxruntime/contrib_ops/cpu/fused_conv.cc
@@ -1,16 +1,26 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-#include "fused_conv.h"
+#include "core/providers/cpu/nn/conv.h"
+#include "contrib_ops/cpu/fused_activation.h"
 
 namespace onnxruntime {
 namespace contrib {
+
+class FusedConvFloat final : public ConvFloat {
+ public:
+  FusedConvFloat(const OpKernelInfo& info) : ConvFloat(info) {
+    ORT_ENFORCE(GetFusedActivationAttr(info, activation_).IsOK());
+  }
+};
+
 ONNX_CPU_OPERATOR_TYPED_MS_KERNEL(
     FusedConv,
     1,
     float,
     KernelDefBuilder()
         .TypeConstraint("T", DataTypeImpl::GetTensorType<float>()),
-    FusedConv<float>);
+    FusedConvFloat);
+
 }  // namespace contrib
 }  // namespace onnxruntime
