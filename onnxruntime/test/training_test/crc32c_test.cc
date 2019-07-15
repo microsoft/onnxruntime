8acc8386425107a7febbf72170a2714de85b4a6c
Jesse Benson <jesseb@microsoft.com>
2019-07-15 20:28:57 +0000

    Merged PR 4733: Fix signed/unsigned warnings for Windows CPU devbox builds
    Fix warnings treated as errors that some people hit when building locally (CI machines don't hit these, for some reason).

--- onnxruntime/test/training_test/crc32c_test.cc
+++ onnxruntime/test/training_test/crc32c_test.cc
@@ -14,20 +14,20 @@ TEST(Crc32cTest, ChecksumTests) {
   char data[32];
 
   memset(data, 0x00, sizeof(data));
-  ASSERT_EQ(0x8a9136aa, Crc32c(data, sizeof(data)));
+  ASSERT_EQ(static_cast<uint32_t>(0x8a9136aa), Crc32c(data, sizeof(data)));
 
   memset(data, 0xff, sizeof(data));
-  ASSERT_EQ(0x62a8ab43, Crc32c(data, sizeof(data)));
+  ASSERT_EQ(static_cast<uint32_t>(0x62a8ab43), Crc32c(data, sizeof(data)));
 
-  for (int i = 0; i < sizeof(data); i++) {
-    data[i] = i;
+  for (size_t i = 0; i < sizeof(data); i++) {
+    data[i] = static_cast<char>(i);
   }
-  ASSERT_EQ(0x46dd794e, Crc32c(data, sizeof(data)));
+  ASSERT_EQ(static_cast<uint32_t>(0x46dd794e), Crc32c(data, sizeof(data)));
 
-  for (int i = 0; i < sizeof(data); i++) {
-    data[i] = 31 - i;
+  for (size_t i = 0; i < sizeof(data); i++) {
+    data[i] = 31 - static_cast<char>(i);
   }
-  ASSERT_EQ(0x113fdb5c, Crc32c(data, sizeof(data)));
+  ASSERT_EQ(static_cast<uint32_t>(0x113fdb5c), Crc32c(data, sizeof(data)));
 
   uint8_t iscsi[] = {
     0x01, 0xc0, 0x00, 0x00,
@@ -43,7 +43,7 @@ TEST(Crc32cTest, ChecksumTests) {
     0x02, 0x00, 0x00, 0x00,
     0x00, 0x00, 0x00, 0x00,
   };
-  ASSERT_EQ(0xd9963a56, Crc32c(reinterpret_cast<const char*>(iscsi), sizeof(iscsi)));
+  ASSERT_EQ(static_cast<uint32_t>(0xd9963a56), Crc32c(reinterpret_cast<const char*>(iscsi), sizeof(iscsi)));
 }
 
 }  // namespace test
