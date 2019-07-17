20cd1aa0c74a2a9a7c168b7520c8bbeffe14ff32
daquexian <daquexian566@gmail.com>
2019-07-17 17:48:07 +0800

    suppress the verbose adb output

--- tools/ci_build/github/android/adb-push.sh
+++ tools/ci_build/github/android/adb-push.sh
@@ -11,7 +11,7 @@ src=`realpath $1`
 src_basename=`basename $1`
 pushd `dirname $src`
 if [[ -d $src ]]; then
-    find $src_basename -type d -print -exec adb shell mkdir -p $2/{} \;
+    find $src_basename -type d -print -exec adb shell mkdir -p $2/{} \; > /dev/null
 fi
-find $src_basename -type f -exec adb push {} $2/{} \;
+find $src_basename -type f -exec adb push {} $2/{} \; > /dev/null
 popd
