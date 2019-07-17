04f156a074e466acad84dcaaffa0c157d22b5d13
daquexian <daquexian566@gmail.com>
2019-07-17 18:15:44 +0800

    Set default ANDROID_HOME to ~/Android/Sdk, remove TERM

--- tools/ci_build/github/android/start_android_emulator.sh
+++ tools/ci_build/github/android/start_android_emulator.sh
@@ -3,7 +3,7 @@
 
 set -e
 
-export TERM=xterm
+ANDROID_HOME="${ANDROID_HOME:-$HOME/Android/Sdk}"
 
 echo "y" | $ANDROID_HOME/tools/bin/sdkmanager --install 'system-images;android-28;google_apis;x86_64'
 
