c1603c2ddebaab536fdc4c1940dd8b56f93ce610
daquexian <daquexian566@gmail.com>
2019-07-20 13:17:49 +0800

    Build static lib for android

--- tools/ci_build/github/linux/run_build.sh
+++ tools/ci_build/github/linux/run_build.sh
@@ -14,7 +14,10 @@ o) BUILD_OS=${OPTARG};;
 esac
 done
 
-COMMON_BUILD_ARGS="--skip_submodule_sync --enable_onnx_tests --parallel --build_shared_lib --use_openmp"
+COMMON_BUILD_ARGS="--skip_submodule_sync --enable_onnx_tests --parallel --use_openmp"
+if [ $BUILD_OS != "android" ]; then
+    COMMON_BUILD_ARGS="$COMMON_BUILD_ARGS --build_shared_lib"
+fi
 if [ $BUILD_OS = "manylinux2010" ]; then
     # FindPython3 does not work on manylinux2010 image, define things manually
     # ask python where to find includes
