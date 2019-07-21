a0036e31c637bdf4f4e321e800e539176537e4e2
daquexian <daquexian566@gmail.com>
2019-07-21 14:11:58 +0800

    Use android ndk r20 instead of r19c

--- tools/ci_build/github/linux/docker/scripts/install_deps_android.sh
+++ tools/ci_build/github/linux/docker/scripts/install_deps_android.sh
@@ -12,6 +12,6 @@ wget -qO- -O temp.zip https://dl.google.com/android/repository/sdk-tools-linux-4
 mkdir /android-sdk/platforms /android-sdk/platform-tools
 
 # Download Android NDK r19c, move /temp_ndk/android-ndk-<version> to /android_ndk
-wget -qO- -O temp.zip https://dl.google.com/android/repository/android-ndk-r19c-linux-x86_64.zip && unzip -oq temp.zip -d /temp-ndk && mv /temp-ndk/* /android-ndk && rm temp.zip && rm -rf /temp-ndk && ls /android-ndk
+wget -qO- -O temp.zip https://dl.google.com/android/repository/android-ndk-r20-linux-x86_64.zip && unzip -oq temp.zip -d /temp-ndk && mv /temp-ndk/* /android-ndk && rm temp.zip && rm -rf /temp-ndk && ls /android-ndk
 
 apt-get -y remove libprotobuf-dev protobuf-compiler
