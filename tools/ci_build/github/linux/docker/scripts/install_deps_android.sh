1f82ea0a7230282c0d3f06979a2c4a10ac5e099d
daquexian <daquexian566@gmail.com>
2019-07-21 14:11:58 +0800

    Use android ndk r20 instead of r19c, fix conflicts in install_deps_android.sh

--- tools/ci_build/github/linux/docker/scripts/install_deps_android.sh
+++ tools/ci_build/github/linux/docker/scripts/install_deps_android.sh
@@ -6,10 +6,7 @@ python3 -m pip install cmake==3.13.2.post1
 
 cmake --version
 
-#download Android NDK r19c
-aria2c -q -d /tmp https://dl.google.com/android/repository/android-ndk-r19c-linux-x86_64.zip
-unzip -oq /tmp/android-ndk-r19c-linux-x86_64.zip -d /tmp/android-ndk && mv /tmp/android-ndk/* /android-ndk
-cd /
-rm -rf /tmp/src
+# Download Android NDK r20, move /temp_ndk/android-ndk-<version> to /android_ndk
+wget -qO- -O temp.zip https://dl.google.com/android/repository/android-ndk-r20-linux-x86_64.zip && unzip -oq temp.zip -d /temp-ndk && mv /temp-ndk/* /android-ndk && rm temp.zip && rm -rf /temp-ndk && ls /android-ndk
 
 apt-get -y remove libprotobuf-dev protobuf-compiler
