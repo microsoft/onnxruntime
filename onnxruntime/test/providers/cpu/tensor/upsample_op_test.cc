c5f8c29d30c79a599dad5e9c9e32e0a5d9ff0795
Yufeng Li <liyufeng1987@gmail.com>
2019-07-24 11:29:01 -0700

    refactor unit tests

--- onnxruntime/test/providers/cpu/tensor/upsample_op_test.cc
+++ onnxruntime/test/providers/cpu/tensor/upsample_op_test.cc
@@ -295,37 +295,6 @@ TEST(UpsampleOpTest, UpsampleOpBilinearTest) {
   test.Run();
 }
 
-TEST(UpsampleOpTest, UpsampleOpBilinearTest2) {
-  OpTester test("Upsample");
-
-  std::vector<float> scales{1.0f, 1.0f, 2.0f, 4.0f};
-  test.AddAttribute("mode", "linear");
-  test.AddAttribute("scales", scales);
-
-  const int64_t N = 2, C = 1, H = 2, W = 2;
-  std::vector<float> X = {1.0f, 3.0f,
-                          4.0f, 8.0f,
-
-                          6.0f, 2.0f,
-                          7.0f, 11.0f};
-
-  test.AddInput<float>("X", {N, C, H, W}, X);
-
-  std::vector<float> Y = {
-      1.0f, 1.5f, 2.0f, 2.5f, 3.0f, 3.0f, 3.0f, 3.0f,
-      2.5f, 3.25f, 4.0f, 4.75f, 5.5f, 5.5f, 5.5f, 5.5f,
-      4.0f, 5.0f, 6.0f, 7.0f, 8.0f, 8.0f, 8.0f, 8.0f,
-      4.0f, 5.0f, 6.0f, 7.0f, 8.0f, 8.0f, 8.0f, 8.0f,
-
-      6.0f, 5.0f, 4.0f, 3.0f, 2.0f, 2.0f, 2.0f, 2.0f,
-      6.5f, 6.5f, 6.5f, 6.5f, 6.5f, 6.5f, 6.5f, 6.5f,
-      7.0f, 8.0f, 9.0f, 10.0f, 11.0f, 11.0f, 11.0f, 11.0f,
-      7.0f, 8.0f, 9.0f, 10.0f, 11.0f, 11.0f, 11.0f, 11.0f};
-
-  test.AddOutput<float>("Y", {N, C, (int64_t)(H * scales[2]), (int64_t)(W * scales[3])}, Y);
-  test.Run();
-}
-
 TEST(UpsampleOpTest, UpsampleOpBilinearTest_NoScale) {
   OpTester test("Upsample");
 
