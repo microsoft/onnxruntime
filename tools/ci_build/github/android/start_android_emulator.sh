5eb4654bc949fe8d44b83d1b53f01f75fbdc959b
daquexian <daquexian566@gmail.com>
2019-08-01 18:27:14 +0800

    no need to set TERM=xterm anymore, it is only needed for watch

--- tools/ci_build/github/android/start_android_emulator.sh
+++ tools/ci_build/github/android/start_android_emulator.sh
@@ -3,8 +3,6 @@
 
 set -e
 
-export TERM=xterm
-
 echo "y" | $ANDROID_HOME/tools/bin/sdkmanager --install 'system-images;android-28;google_apis;x86_64'
 
 echo "no" | $ANDROID_HOME/tools/bin/avdmanager create avd -n android_emulator -k 'system-images;android-28;google_apis;x86_64' --force
