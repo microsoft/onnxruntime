89615c22338fba5e6d9269f142caa2b8ba47294a
Adam Malowiecki <adam.malowiecki@gmail.com>
2019-08-05 08:17:15 +0900

    Moved MetricRegistry to a class

--- onnxruntime/server/core/metric_registry.h
+++ onnxruntime/server/core/metric_registry.h
@@ -4,7 +4,8 @@
 
 namespace onnxruntime {
 namespace server {
-struct MetricRegistry {
+class MetricRegistry {
+public:
   // returns the global metric registry instance
   // use this handle to get the instance to increment/count requests
   static MetricRegistry& Get() {
@@ -31,23 +32,23 @@ struct MetricRegistry {
   // The three metrics we're currently recording
   // Inference Time (model and version can be handled by labels so you can)
   // aggregate total onnxruntime server performance if desired
-  std::shared_ptr<prometheus::Family<prometheus::Histogram>*> inferenceTimer;
+  std::unique_ptr<prometheus::Family<prometheus::Histogram>*> inferenceTimer;
   // Run Time (model and version again handled by labels)
-  std::shared_ptr<prometheus::Family<prometheus::Histogram>*> runTimer;
+  std::unique_ptr<prometheus::Family<prometheus::Histogram>*> runTimer;
   // Total number of HTTP requests split by path, this includes pinging the metric
   // endpoint and the health checker
-  std::shared_ptr<prometheus::Family<prometheus::Counter>*> totalHTTPRequests;
+  std::unique_ptr<prometheus::Family<prometheus::Counter>*> totalHTTPRequests;
   // Total number of gRPC requests received by the server
-  std::shared_ptr<prometheus::Family<prometheus::Counter>*> totalGRPCRequests;
+  std::unique_ptr<prometheus::Family<prometheus::Counter>*> totalGRPCRequests;
   // Total number of erroneous HTTP requests, includes error code and
   // for more in-depth analysis e.g bad inputs, protobuffer errors
-  std::shared_ptr<prometheus::Family<prometheus::Counter>*> totalHTTPErrors;
+  std::unique_ptr<prometheus::Family<prometheus::Counter>*> totalHTTPErrors;
   // Total number of erroneous gRPC requests, including error code
-  std::shared_ptr<prometheus::Family<prometheus::Counter>*> totalGRPCErrors;
+  std::unique_ptr<prometheus::Family<prometheus::Counter>*> totalGRPCErrors;
   // Request Sizes of HTTP Requests
-  std::shared_ptr<prometheus::Family<prometheus::Histogram>*> httpRequestSize;
+  std::unique_ptr<prometheus::Family<prometheus::Histogram>*> httpRequestSize;
   // Request Sizes of gRPC Requests
-  std::shared_ptr<prometheus::Family<prometheus::Histogram>*> grpcRequestSize;
+  std::unique_ptr<prometheus::Family<prometheus::Histogram>*> grpcRequestSize;
 
 private:
   MetricRegistry() {
@@ -57,56 +58,56 @@ private:
     // This is purely runtime/model performance so json/protobuf serde
     // shouldn't be considered, actual API Server performance can be done
     // through network tracing
-    inferenceTimer = std::make_shared<prometheus::Family<prometheus::Histogram>*>(
+    inferenceTimer = std::make_unique<prometheus::Family<prometheus::Histogram>*>(
       &prometheus::BuildHistogram().
         Name("ortserver_model_inference_time_milliseconds").
         Help("How long it took to perform inference").
         Register(registry)
     );
 
-    runTimer = std::make_shared<prometheus::Family<prometheus::Histogram>*>(
+    runTimer = std::make_unique<prometheus::Family<prometheus::Histogram>*>(
       &prometheus::BuildHistogram().
         Name("ortserver_model_session_run_time_milliseconds").
         Help("How long it took to perform Session.Run on the input, includes casting").
         Register(registry)
     );
     
-    totalHTTPRequests = std::make_shared<prometheus::Family<prometheus::Counter>*>(
+    totalHTTPRequests = std::make_unique<prometheus::Family<prometheus::Counter>*>(
       &prometheus::BuildCounter().
         Name("ortserver_http_requests_total").
         Help("How many requests over HTTP the onnxruntime server has received").
         Register(registry)
     );
 
-    totalGRPCRequests = std::make_shared<prometheus::Family<prometheus::Counter>*>(
+    totalGRPCRequests = std::make_unique<prometheus::Family<prometheus::Counter>*>(
       &prometheus::BuildCounter().
         Name("ortserver_grpc_requests_total").
         Help("How many requests over gRPC the onnxruntime server has received").
         Register(registry)
     );
 
-    totalHTTPErrors = std::make_shared<prometheus::Family<prometheus::Counter>*>(
+    totalHTTPErrors = std::make_unique<prometheus::Family<prometheus::Counter>*>(
       &prometheus::BuildCounter().
         Name("ortserver_http_errors_total").
         Help("How many bad requests or errors the server has handled over HTTP").
         Register(registry)
     );
     
-    totalGRPCErrors = std::make_shared<prometheus::Family<prometheus::Counter>*>(
+    totalGRPCErrors = std::make_unique<prometheus::Family<prometheus::Counter>*>(
       &prometheus::BuildCounter().
         Name("ortserver_grpc_errors_total").
         Help("How many bad requests or errors the server has handled over gRPC").
         Register(registry)
     );
 
-    httpRequestSize = std::make_shared<prometheus::Family<prometheus::Histogram>*>(
+    httpRequestSize = std::make_unique<prometheus::Family<prometheus::Histogram>*>(
       &prometheus::BuildHistogram().
         Name("ortserver_http_request_size_bytes").
         Help("File sizes of http requests in bytes").
         Register(registry)
     );
 
-    grpcRequestSize = std::make_shared<prometheus::Family<prometheus::Histogram>*>(
+    grpcRequestSize = std::make_unique<prometheus::Family<prometheus::Histogram>*>(
       &prometheus::BuildHistogram().
         Name("ortserver_grpc_request_size_bytes").
         Help("File sizes of grpc requests in bytes").
