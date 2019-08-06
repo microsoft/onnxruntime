f500ee7891ff64ac1d4317dbe8e09d1c921ae6c8
Pulkit Tomar <putomar@microsoft.com>
2019-08-06 00:30:27 -0700

    Fixed failing tests

--- csharp/src/Microsoft.ML.OnnxRuntime/NativeMethods.cs
+++ csharp/src/Microsoft.ML.OnnxRuntime/NativeMethods.cs
@@ -131,7 +131,7 @@ namespace Microsoft.ML.OnnxRuntime
         public static extern IntPtr /*(OrtStatus*)*/ OrtDisableSequentialExecution(IntPtr /*(OrtSessionOptions*)*/ options);
 
         [DllImport(nativeLib, CharSet = charSet)]
-        public static extern IntPtr /*(OrtStatus*)*/ OrtSetOptimizedModelFilePath(IntPtr /* OrtSessionOptions* */ options, string optimizedModelFilepath);
+        public static extern IntPtr /*(OrtStatus*)*/ OrtSetOptimizedModelFilePath(IntPtr /* OrtSessionOptions* */ options, [MarshalAs(UnmanagedType.LPWStr)]string optimizedModelFilepath);
 
         [DllImport(nativeLib, CharSet = charSet)]
         public static extern IntPtr /*(OrtStatus*)*/ OrtEnableProfiling(IntPtr /* OrtSessionOptions* */ options, string profilePathPrefix);
