604acd61a9aea1dd55b964782521847f9a6a2689
Agni Kumar <agni.kumar@yahoo.com>
2019-08-08 15:50:56 -0700

    update calibrate.py graph augmentation

--- onnxruntime/python/tools/quantization/calibrate.py
+++ onnxruntime/python/tools/quantization/calibrate.py
@@ -7,34 +7,33 @@
 import onnx
 from onnx import helper, TensorProto
 
+# Adding ability to record intermediate graph outputs
+selected_types = ["Conv", "MatMul"] # node types to extend
+
 def augment_graph(model):
     '''
-    Adds ReduceMin and ReduceMax nodes to all Conv and MatMul nodes_list in
+    Adds ReduceMin and ReduceMax nodes to all Conv and MatMul nodes in
     model and ensures their outputs are stored as part of the graph output
-        parameter model: loaded ONNX model (not yet quantized)
+        parameter model: FP32 ONNX model to quantize
         return: augmented ONNX model
     '''
     added_nodes = []
     added_outputs = []
-
     for node in model.graph.node:
-        if (node.op_type == "Conv") or (node.op_type == "MatMul"):
+        if node.op_type in selected_types:
             input_name = node.name
             # Adding ReduceMin nodes
             reduce_min_name = input_name + "_ReduceMin"
             reduce_min_node = onnx.helper.make_node("ReduceMin", [input_name + ":0"],
                             [reduce_min_name + ":0"], reduce_min_name, keepdims=0)
             added_nodes.append(reduce_min_node)
+            added_outputs.append(helper.make_tensor_value_info(reduce_min_node.name + ":0", TensorProto.FLOAT, ()))
             # Adding ReduceMax nodes
             reduce_max_name = input_name + "_ReduceMax"
             reduce_max_node = onnx.helper.make_node("ReduceMax", [input_name + ":0"],
                             [reduce_max_name + ":0"], reduce_max_name, keepdims=0)
             added_nodes.append(reduce_max_node)
+            added_outputs.append(helper.make_tensor_value_info(reduce_max_node.name + ":0", TensorProto.FLOAT, ()))
     model.graph.node.extend(added_nodes)
-
-    for node in model.graph.node:
-        if (node.op_type == "ReduceMin") or (node.op_type == "ReduceMax"):
-            intermediate_output = helper.make_tensor_value_info(node.name + ":0", TensorProto.FLOAT, ())
-            added_outputs.append(intermediate_output)
     model.graph.output.extend(added_outputs)
     return model
