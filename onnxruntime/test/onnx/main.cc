865047e4c75ab2a518d5ec968f30e9dcb60560c3
Ashwini Khade <askhade@microsoft.com>
2019-08-14 20:36:58 -0700

    disable tests

--- onnxruntime/test/onnx/main.cc
+++ onnxruntime/test/onnx/main.cc
@@ -389,12 +389,12 @@ int real_main(int argc, char* argv[], Ort::Env& env) {
       {"cumsum_1d_reverse", "not implemented yet"},
       {"cumsum_1d_exclusive", "not implemented yet"},
       {"cumsum_1d", "not implemented yet"},
-      {"clip_splitbounds", "not implemented yet for opset 11", {"onnxtip"}},
-      {"clip_outbounds", "not implemented yet for opset 11", {"onnxtip"}},
-      {"clip_example", "not implemented yet for opset 11", {"onnxtip"}},
-      {"clip_default_min", "not implemented yet for opset 11", {"onnxtip"}},
-      {"clip_default_max", "not implemented yet for opset 11", {"onnxtip"}},
-      {"clip", "not implemented yet for opset 11", {"onnxtip"}},
+      {"clip_splitbounds", "not implemented yet for opset 11"},
+      {"clip_outbounds", "not implemented yet for opset 11"},
+      {"clip_example", "not implemented yet for opset 11"},
+      {"clip_default_min", "not implemented yet for opset 11"},
+      {"clip_default_max", "not implemented yet for opset 11"},
+      {"clip", "not implemented yet for opset 11"},
   };
 
 #ifdef USE_NGRAPH
