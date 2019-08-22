40f0455503967496e9564e043c5ab15d1d1f31c3
Hector Li <hecli@microsoft.com>
2019-08-22 15:27:37 -0700

    update device option for openvino-edge"

--- tools/ci_build/github/linux/run_dockerbuild.sh
+++ tools/ci_build/github/linux/run_dockerbuild.sh
@@ -56,7 +56,7 @@ else
         IMAGE="ubuntu16.04-cuda10.0-cudnn7.4-tensorrt5.0"
         DOCKER_FILE=Dockerfile.ubuntu_tensorrt
         docker build -t "onnxruntime-$IMAGE" --build-arg BUILD_USER=onnxruntimedev --build-arg BUILD_UID=$(id -u) --build-arg PYTHON_VERSION=${PYTHON_VER} -f $DOCKER_FILE .
-    elif [ $BUILD_DEVICE = "openvino" ]; then
+    elif [ $BUILD_DEVICE = "openvino" ] || [ $BUILD_DEVICE = "openvino-edge" ]; then
         IMAGE="ubuntu16.04"
         DOCKER_FILE=Dockerfile.ubuntu_openvino
         docker build -t "onnxruntime-$IMAGE" --build-arg BUILD_USER=onnxruntimedev --build-arg BUILD_UID=$(id -u) --build-arg OS_VERSION=16.04 --build-arg PYTHON_VERSION=${PYTHON_VER} --build-arg OPENVINO_VERSION=${OPENVINO_VERSION} -f Dockerfile.ubuntu_openvino .
