03d700db8920510ac21d2b18673b215744972f90
hariharans29 <shariharan91@gmail.com>
2019-09-04 13:14:25 -0700

    Fix build

--- onnxruntime/test/providers/cpu/tensor/gather_elements_op_test.cc
+++ onnxruntime/test/providers/cpu/tensor/gather_elements_op_test.cc
@@ -49,12 +49,40 @@ void RunTypedTest()
                         {1, 1,
                          4, 3});
   test3.Run();
+
+  // negative indices - axis 1
+  OpTester test4("GatherElements", 11);
+  test4.AddAttribute<int64_t>("axis", 1LL);
+  test4.AddInput<T>("data", {2, 2},
+                    {1, 2,
+                     3, 4});
+  test4.AddInput<int64_t>("indices", {2, 2},
+                          {0, 0,
+                           -1, -1});
+  test4.AddOutput<T>("output", {2, 2},
+                     {1, 1,
+                      4, 4});
+  test4.Run();
+
+  // indices out of bounds
+  OpTester test5("GatherElements", 11);
+  test5.AddAttribute<int64_t>("axis", 1LL);
+  test5.AddInput<T>("data", {2, 2},
+                    {1, 2,
+                     3, 4});
+  test5.AddInput<int64_t>("indices", {2, 2},
+                          {0, 0,
+                           2, 2});
+  test5.AddOutput<T>("output", {2, 2},
+                     {1, 1,
+                      4, 4});
+  test5.Run(OpTester::ExpectResult::kExpectFailure, "GatherElements op: Value in indices must be within bounds [-2 , 1]");
 }
 
 template <>
 void RunTypedTest<std::string>() {
 
-  // non-inner dimension 
+  // int32_t indices - axis 0
   OpTester test1("GatherElements", 11);
   test1.AddAttribute<int64_t>("axis", 0LL);
   test1.AddInput<std::string>("data", {2, 3},
@@ -64,7 +92,7 @@ void RunTypedTest<std::string>() {
                               {"a", "e"});
   test1.Run();
 
-  // inner-dimension
+  // int32_t indices - axis 1
   OpTester test2("GatherElements", 11);
   test2.AddAttribute<int64_t>("axis", 1LL);
   test2.AddInput<std::string>("data", {2, 2},
@@ -77,6 +105,34 @@ void RunTypedTest<std::string>() {
                               {"a", "a",
                                "d", "c"});
   test2.Run();
+
+    // negative indices - axis 1
+  OpTester test3("GatherElements", 11);
+  test3.AddAttribute<int64_t>("axis", 1LL);
+  test3.AddInput<std::string>("data", {2, 2},
+                              {"a", "b",
+                               "c", "d"});
+  test3.AddInput<int32_t>("indices", {2, 2},
+                          {0, 0,
+                           -1, -1});
+  test3.AddOutput<std::string>("output", {2, 2},
+                               {"a", "a",
+                                "d", "d"});
+  test3.Run();
+
+  // indices out of bounds
+  OpTester test4("GatherElements", 11);
+  test4.AddAttribute<int64_t>("axis", 1LL);
+  test4.AddInput<std::string>("data", {2, 2},
+                              {"a", "b",
+                               "c", "d"});
+  test4.AddInput<int32_t>("indices", {2, 2},
+                          {0, 0,
+                           -3, -3});
+  test4.AddOutput<std::string>("output", {2, 2},
+                               {"a", "a",
+                                "d", "d"});
+  test4.Run(OpTester::ExpectResult::kExpectFailure, "GatherElements op: Value in indices must be within bounds [-2 , 1]");
 }
 
 TEST(GatherElementsOpTest, int8_t) {
@@ -95,6 +151,21 @@ TEST(GatherElementsOpTest, int64_t) {
   RunTypedTest<int64_t>();
 }
 
+TEST(GatherElementsOpTest, uint8_t) {
+  RunTypedTest<uint8_t>();
+}
+
+TEST(GatherElementsOpTest, uint16_t) {
+  RunTypedTest<uint16_t>();
+}
+
+TEST(GatherElementsOpTest, uint32_t) {
+  RunTypedTest<uint32_t>();
+}
+
+TEST(GatherElementsOpTest, uint64_t) {
+  RunTypedTest<uint64_t>();
+}
 TEST(GatherElementsOpTest, string) {
   RunTypedTest<std::string>();
 }
