62c010779046ac6f2ef30abe91c765cd36d64eaf
Hector Li <hecli@microsoft.com>
2019-09-06 10:56:56 -0700

    remove the constructor which doesn't need the device_id parameter in CudaAsyncBuffer in case someone forget to pass the device_id to it. The device_id is always required otherwise it could cause issue if running on device other than device 0

--- onnxruntime/core/providers/cuda/cuda_common.h
+++ onnxruntime/core/providers/cuda/cuda_common.h
@@ -66,8 +66,6 @@ class CudaKernel : public OpKernel {
   template <typename T>
   class CudaAsyncBuffer {
    public:
-    CudaAsyncBuffer(const CudaKernel* op_kernel) : gpu_copy_(nullptr), count_(0), op_kernel_(op_kernel) {}
-
     CudaAsyncBuffer(const CudaKernel* op_kernel, int device_id, size_t count) : CudaAsyncBuffer(op_kernel) {
       AllocCpuPtr(device_id, count);
     }
