dbd0a735944a7a39118c7aafdc52c9fb64d3b9f6
tomdol <tomasz.dolbniak@intel.com>
2019-09-19 15:51:37 +0200

    Enable the quantizelinear and dequantizelinear for NGEP

--- onnxruntime/test/onnx/main.cc
+++ onnxruntime/test/onnx/main.cc
@@ -448,13 +448,11 @@ int real_main(int argc, char* argv[], Ort::Env& env) {
   };
 
 #ifdef USE_NGRAPH
-  broken_tests.insert({"dequantizelinear", "ambiguity in scalar dimensions [] vs [1]", {"onnx150"}});
   broken_tests.insert({"qlinearconv", "ambiguity in scalar dimensions [] vs [1]"});
-  broken_tests.insert({"quantizelinear", "ambiguity in scalar dimensions [] vs [1]", {"onnx150"}});
   broken_tests.insert({"clip_splitbounds", "not implemented yet for opset 11"});
-  broken_tests.insert({"clip_outbounds", "not implemented yet for opset 11"});	
-  broken_tests.insert({"clip_example", "not implemented yet for opset 11"});	
-  broken_tests.insert({"clip_default_min", "not implemented yet for opset 11"});	
+  broken_tests.insert({"clip_outbounds", "not implemented yet for opset 11"});
+  broken_tests.insert({"clip_example", "not implemented yet for opset 11"});
+  broken_tests.insert({"clip_default_min", "not implemented yet for opset 11"});
   broken_tests.insert({"clip_default_max", "not implemented yet for opset 11"});
   broken_tests.insert({"clip", "not implemented yet for opset 11"});
 #endif
