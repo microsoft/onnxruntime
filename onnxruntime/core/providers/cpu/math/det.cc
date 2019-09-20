635be937cf65d253a7e5c00e1caab40edc0577e3
Pranav Sharma <prs@microsoft.com>
2019-09-19 19:49:13 -0700

    Det op

--- onnxruntime/core/providers/cpu/math/det.cc
+++ onnxruntime/core/providers/cpu/math/det.cc
@@ -15,28 +15,25 @@ ONNX_CPU_OPERATOR_KERNEL(
     Det<float>);
 
 template <typename T>
-Status Det::Compute(OpKernelContext* context) const {
+Status Det<T>::Compute(OpKernelContext* context) const {
   const auto* X = context->Input<Tensor>(0);
   ORT_ENFORCE(X != nullptr);
 
   const auto& X_shape = X->Shape();
-  int X_num_dims = X_shape.size();
+  int X_num_dims = static_cast<int>(X_shape.NumDimensions());
 
   // input validation
-  if (X_shape.empty()) {
-    return Status(ONNXRUNTIME, INVALID_ARGUMENT, "Got empty shape for the input tensor");
-  }
-  if (X_shape.size() < 2) {
+  if (X_num_dims < 2) {  // this is getting capture by shape inference code as well
     return Status(ONNXRUNTIME, INVALID_ARGUMENT, "Input tensor should have a rank of at least 2");
   }
-  if (X_shape[X_num_dims - 1] != X_shape[X_num_dims - 2]) {
+  if (X_shape[X_num_dims - 1] != X_shape[X_num_dims - 2]) {  // this is getting capture by shape inference code as well
     return Status(ONNXRUNTIME, INVALID_ARGUMENT, "Matrix dimensions are not equal. Square matrix is expected");
   }
 
-  const auto* X_data = X->Data();
-  int matrix_dim = X_shape[X_num_dims - 1];
+  const auto* X_data = X->Data<T>();
+  int matrix_dim = static_cast<int>(X_shape[X_num_dims - 1]);
 
-  auto get_determinant = [= matrix_dim](const T* matrix_ptr) -> T {
+  auto get_determinant = [matrix_dim](const T* matrix_ptr) -> T {
     auto one_eigen_mat = ConstEigenMatrixMapRowMajor<T>(matrix_ptr, matrix_dim, matrix_dim);
     return one_eigen_mat.determinant();
   };
@@ -46,12 +43,12 @@ Status Det::Compute(OpKernelContext* context) const {
     auto* Y_data = Y->template MutableData<T>();
     *Y_data = get_determinant(X_data);
   } else {
-    int batch_size = X_shape[0];
+    int batch_size = static_cast<int>(X_shape[0]);
     int num_matrix_elems = matrix_dim * matrix_dim;
     auto* Y = context->Output(0, {batch_size});
     auto* Y_data = Y->template MutableData<T>();
-    for (int b = 0; b < batch_size; ++b) {
-      const auto* one_matrix = X_data[b * num_matrix_elems];
+    for (int b = 0; b < batch_size; ++b) {  // can be parallelized if need to
+      const T* one_matrix = X_data + (b * num_matrix_elems);
       *Y_data++ = get_determinant(one_matrix);
     }
   }
