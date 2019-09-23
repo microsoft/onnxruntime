d7155f4275582d42401ffa9b9fcdd98391cf6b11
Adrian Tsai <adtsai@microsoft.com>
2019-09-23 14:07:07 -0700

    Build fixes

--- cmake/external/dml.cmake
+++ cmake/external/dml.cmake
@@ -1,11 +1,13 @@
 
-if (onnxruntime_BUILD_x86)
-    set(dml_LIB_DIR "${PROJECT_SOURCE_DIR}/external/dml/x86")
-else ()
-    set(dml_LIB_DIR "${PROJECT_SOURCE_DIR}/external/dml/x64")
-endif()
+if (NOT onnxruntime_USE_CUSTOM_DIRECTML)
+    if (onnxruntime_BUILD_x86)
+        set(dml_LIB_DIR "${PROJECT_SOURCE_DIR}/external/dml/x86")
+    else ()
+        set(dml_LIB_DIR "${PROJECT_SOURCE_DIR}/external/dml/x64")
+    endif()
 
-set(dml_INCLUDE_DIR "${PROJECT_SOURCE_DIR}/external/dml")
+    set(dml_INCLUDE_DIR "${PROJECT_SOURCE_DIR}/external/dml")
+endif()
 
 include_directories(${dml_INCLUDE_DIR})
 link_directories(${dml_LIB_DIR})
