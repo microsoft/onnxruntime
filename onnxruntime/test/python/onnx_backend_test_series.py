a06a81ee3d8bed123cb0f35f50ce2a444ba45008
Scott McKay <skottmckay@gmail.com>
2019-09-25 12:59:45 +1000

    Fix a couple of merge issues.

--- onnxruntime/test/python/onnx_backend_test_series.py
+++ onnxruntime/test/python/onnx_backend_test_series.py
@@ -125,7 +125,7 @@ def create_backend_test(testname=None):
                                  '^test_top_k*',
                                  '^test_unique_*',
                                  '^test_mod_float_mixed_sign_example_cpu.*', #onnxruntime::Mod::Compute fmod_ was false. fmod attribute must be true for float, float16 and double types
-                                 '^test_gru_seq_length_cpu.*')
+                                 '^test_shrink_cpu.*', #Invalid rank for input: x Got: 1 Expected: 2 Please fix either the inputs or the model.
                                  '^test_range_float_type_positive_delta_cpu.*',
                                  '^test_range_float_type_positive_delta_expanded_cpu.*',
                                  '^test_range_int32_type_negative_delta_cpu.*',
