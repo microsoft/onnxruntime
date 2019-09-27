70e905d9780afcd66cd8a1b84a487bb21c895eaa
stevenlix <38092805+stevenlix@users.noreply.github.com>
2019-09-27 11:55:38 -0700

    Update run_dockerbuild.sh

--- tools/ci_build/github/linux/run_dockerbuild.sh
+++ tools/ci_build/github/linux/run_dockerbuild.sh
@@ -53,7 +53,7 @@ else
         fi
         docker build -t "onnxruntime-$IMAGE" --build-arg BUILD_USER=onnxruntimedev --build-arg BUILD_UID=$(id -u) --build-arg PYTHON_VERSION=${PYTHON_VER} -f $DOCKER_FILE .
     elif [ $BUILD_DEVICE = "tensorrt" ]; then
-        IMAGE="ubuntu16.04-cuda10.0-cudnn7.4-tensorrt5.0"
+        IMAGE="ubuntu18.04-cuda10.1-cudnn7.6-tensorrt6.0"
         DOCKER_FILE=Dockerfile.ubuntu_tensorrt
         docker build -t "onnxruntime-$IMAGE" --build-arg BUILD_USER=onnxruntimedev --build-arg BUILD_UID=$(id -u) --build-arg PYTHON_VERSION=${PYTHON_VER} -f $DOCKER_FILE .
     elif [ $BUILD_DEVICE = "openvino" ]; then
