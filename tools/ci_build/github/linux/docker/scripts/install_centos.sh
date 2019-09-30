8331c75097a07e83a712fa019b16a9b23e632f78
Dmitri Smirnov <dmitrism@microsoft.com>
2019-09-30 10:15:42 -0700

    Remote git as it is installed separate.

--- tools/ci_build/github/linux/docker/scripts/install_centos.sh
+++ tools/ci_build/github/linux/docker/scripts/install_centos.sh
@@ -8,5 +8,5 @@ yum install -y cmake3 git2u-all
 ln -s /usr/bin/cmake3 /usr/bin/cmake
 ln -s /usr/bin/ctest3 /usr/bin/ctest
 
-yum install -y redhat-lsb-core expat-devel libcurl-devel protobuf-compiler protobuf-devel protobuf rpmdevtools tar unzip ccache curl gcc gcc-c++ zlib-devel make git python2-devel python3-devel python3-numpy libunwind icu aria2 rsync python3-setuptools python3-wheel bzip2
+yum install -y redhat-lsb-core expat-devel libcurl-devel protobuf-compiler protobuf-devel protobuf rpmdevtools tar unzip ccache curl gcc gcc-c++ zlib-devel make  python2-devel python3-devel python3-numpy libunwind icu aria2 rsync python3-setuptools python3-wheel bzip2
 
