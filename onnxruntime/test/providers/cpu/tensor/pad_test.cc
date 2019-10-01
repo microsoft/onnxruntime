aeaa802c23082c1a0bd9e703a97d185f4a732e91
hariharans29 <shariharan91@gmail.com>
2019-10-01 11:48:47 -0700

    Update

--- onnxruntime/test/providers/cpu/tensor/pad_test.cc
+++ onnxruntime/test/providers/cpu/tensor/pad_test.cc
@@ -32,7 +32,7 @@ static void RunTest(
   test2.AddInput<float>("value", {1}, {value});
   test2.AddOutput<float>("output", output_dims, output);
   // NGraph does not yet support opset-11 and builds break on this test, hence exclude the EP
-  test2.Run(OpTester::ExpectResult::kExpectSuccess, "", {kNGraphDomain});
+  test2.Run(OpTester::ExpectResult::kExpectSuccess, "", {kNGraphExecutionProvider});
 
   #ifndef DISABLE_CONTRIB_OPS
   
