8ad0985d50fdbe28c8a50d7fea5ed43c81639393
Adrian Tsai <adtsai@microsoft.com>
2019-10-01 13:04:37 -0700

    Preliminary work for DirectML nuget package ingestion

--- cmake/external/dml.cmake
+++ cmake/external/dml.cmake
@@ -1,13 +1,38 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
 
 if (NOT onnxruntime_USE_CUSTOM_DIRECTML)
-    if (onnxruntime_BUILD_x86)
-        set(dml_LIB_DIR "${PROJECT_SOURCE_DIR}/external/dml/x86")
-    else ()
-        set(dml_LIB_DIR "${PROJECT_SOURCE_DIR}/external/dml/x64")
-    endif()
+  if (NOT(MSVC) OR NOT(WIN32))
+    message(FATAL_ERROR "NuGet packages are only supported for MSVC on Windows.")
+  endif()
 
-    set(dml_INCLUDE_DIR "${PROJECT_SOURCE_DIR}/external/dml")
+  # Retrieve the latest version of nuget
+  include(ExternalProject)
+  ExternalProject_Add(nuget
+    PREFIX nuget
+    URL "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe"
+    DOWNLOAD_NO_EXTRACT 1
+    CONFIGURE_COMMAND ""
+    BUILD_COMMAND ""
+    UPDATE_COMMAND ""
+    INSTALL_COMMAND "")
+
+  set(PACKAGES_CONFIG ${PROJECT_SOURCE_DIR}/../packages.config)
+  set(PACKAGES_DIR ${CMAKE_CURRENT_BINARY_DIR}/packages)
+
+  # Restore nuget packages, which will pull down the DirectML redist package
+  add_custom_command(
+    OUTPUT restore_packages.stamp
+    DEPENDS ${PROJECT_SOURCE_DIR}/../packages.config
+    COMMAND ${CMAKE_CURRENT_BINARY_DIR}/nuget/src/nuget restore ${PACKAGES_CONFIG} -PackagesDirectory ${PACKAGES_DIR}
+    COMMAND ${CMAKE_COMMAND} -E touch restore_packages.stamp
+    VERBATIM)
+
+  add_custom_target(RESTORE_PACKAGES ALL DEPENDS restore_packages.stamp)
+  add_dependencies(RESTORE_PACKAGES nuget)
+
+  link_libraries(${CMAKE_CURRENT_BINARY_DIR}/packages/DirectML.0.0.1/build/DirectML.targets)
+else()
+  include_directories(${dml_INCLUDE_DIR})
+  link_directories(${dml_LIB_DIR})
 endif()
-
-include_directories(${dml_INCLUDE_DIR})
-link_directories(${dml_LIB_DIR})
