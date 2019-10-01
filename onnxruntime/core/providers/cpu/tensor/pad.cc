447ba95441a4b77e81d9f1e6ba92e009b514b499
hariharans29 <shariharan91@gmail.com>
2019-09-30 18:27:22 -0700

    Update

--- onnxruntime/core/providers/cpu/tensor/pad.cc
+++ onnxruntime/core/providers/cpu/tensor/pad.cc
@@ -270,7 +270,7 @@ Status PadCpuImpl<float>(OpKernelContext* ctx,
 }
 
 template <>
-Status Pad<float>::Compute(OpKernelContext* ctx) const {
+Status PadCpu<float>::Compute(OpKernelContext* ctx) const {
   // kOnnxDomain Pad opset >= 11 (Or) kMsDomain opset == 1
   if (is_dynamic_) {
     const Tensor& input_tensor = *ctx->Input<Tensor>(0);
