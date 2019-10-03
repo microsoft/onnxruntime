7f0f32cdfbd3f9a5b8b296a8e31b9df8ef9e0498
Hariharan Seshadri <shariharan91@gmail.com>
2019-10-02 19:27:03 -0700

    Update

--- onnxruntime/test/python/onnx_backend_test_series.py
+++ onnxruntime/test/python/onnx_backend_test_series.py
@@ -162,7 +162,8 @@ def create_backend_test(testname=None):
             current_failing_tests = current_failing_tests + ('|^test_depthtospace_crd*',)
             current_failing_tests = current_failing_tests + ('|^test_argmax_negative_axis*',)
             current_failing_tests = current_failing_tests + ('|^test_argmin_negative_axis*',)
-            current_failing_tests = current_failing_tests + ('|^test_hadmax_negative_axis*',)            
+            current_failing_tests = current_failing_tests + ('|^test_hadmax_negative_axis*',) 
+            current_failing_tests = current_failing_tests + ('|^test_gemm_default_no_bias_cpu.*',)
             current_failing_tests = current_failing_tests + ('|^test_constant_pad_cpu.*',)
 
         if c2.supports_device('OPENVINO_GPU_FP32') or c2.supports_device('OPENVINO_GPU_FP16'):
