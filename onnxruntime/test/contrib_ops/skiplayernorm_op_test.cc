4a087a740250d675712a250d596642e6ff31f3c6
Tianlei Wu <tlwu@microsoft.com>
2019-10-04 15:08:18 -0700

    add float16 test for skiplayernorm

--- onnxruntime/test/contrib_ops/skiplayernorm_op_test.cc
+++ onnxruntime/test/contrib_ops/skiplayernorm_op_test.cc
@@ -2,6 +2,7 @@
 // Licensed under the MIT License.
 
 #include "gtest/gtest.h"
+#include "test/common/tensor_op_test_utils.h"
 #include "test/providers/provider_test_utils.h"
 #include "test/util/include/default_providers.h"
 
@@ -13,6 +14,15 @@
 namespace onnxruntime {
 namespace test {
 
+static const std::vector<MLFloat16> ToFloat16(const std::vector<float>& data) {
+  std::vector<MLFloat16> result;
+  result.reserve(data.size());
+  for (int i = 0; i < data.size(); i++) {
+    result.push_back(MLFloat16(math::floatToHalf(data[i])));
+  }
+  return result;
+}
+
 static void RunTest(
     const std::vector<float>& input_data,
     const std::vector<float>& skip_data,
@@ -21,32 +31,40 @@ static void RunTest(
     const std::vector<float>& output_data,
     int batch_size,
     int sequence_length,
-    int hidden_size) {
-  OpTester test("SkipLayerNormalization", 1, onnxruntime::kMSDomain);
-
-  // Input and output shapes
-  //   Input 0 - input: (batch_size, sequence_length, hidden_size)
-  //   Input 1 - skip : (batch_size, sequence_length, hidden_size)
-  //   Input 2 - gamma: (hidden_size)
-  //   Input 3 - beta : (hidden_size)
-  //   Output         : (batch_size, sequence_length, hidden_size)
-  std::vector<int64_t> input_dims = {batch_size, sequence_length, hidden_size};
-  std::vector<int64_t> skip_dims = input_dims;
-  std::vector<int64_t> gamma_dims = {hidden_size};
-  std::vector<int64_t> beta_dims = gamma_dims;
-  std::vector<int64_t> output_dims = input_dims;
-
-  test.AddInput<float>("input", input_dims, input_data);
-  test.AddInput<float>("skip", skip_dims, skip_data);
-  test.AddInput<float>("gamma", gamma_dims, gamma_data);
-  test.AddInput<float>("beta", beta_dims, beta_data);
-  test.AddOutput<float>("output", output_dims, output_data);
-
+    int hidden_size,
+    bool use_float16 = false) {
   // Run test on CUDA provider since Attention only have CUDA kernel.
   std::vector<std::unique_ptr<IExecutionProvider>> execution_providers;
   execution_providers.push_back(DefaultCudaExecutionProvider());
-  if (execution_providers[0].get() != nullptr)
-  {
+  if (execution_providers[0].get() != nullptr) {
+    OpTester test("SkipLayerNormalization", 1, onnxruntime::kMSDomain);
+
+    // Input and output shapes
+    //   Input 0 - input: (batch_size, sequence_length, hidden_size)
+    //   Input 1 - skip : (batch_size, sequence_length, hidden_size)
+    //   Input 2 - gamma: (hidden_size)
+    //   Input 3 - beta : (hidden_size)
+    //   Output         : (batch_size, sequence_length, hidden_size)
+    std::vector<int64_t> input_dims = {batch_size, sequence_length, hidden_size};
+    std::vector<int64_t> skip_dims = input_dims;
+    std::vector<int64_t> gamma_dims = {hidden_size};
+    std::vector<int64_t> beta_dims = gamma_dims;
+    std::vector<int64_t> output_dims = input_dims;
+
+    if (use_float16) {
+      test.AddInput<MLFloat16>("input", input_dims, ToFloat16(input_data));
+      test.AddInput<MLFloat16>("skip", skip_dims, ToFloat16(skip_data));
+      test.AddInput<MLFloat16>("gamma", gamma_dims, ToFloat16(gamma_data));
+      test.AddInput<MLFloat16>("beta", beta_dims, ToFloat16(beta_data));
+      test.AddOutput<MLFloat16>("output", output_dims, ToFloat16(output_data));
+    } else {
+      test.AddInput<float>("input", input_dims, input_data);
+      test.AddInput<float>("skip", skip_dims, skip_data);
+      test.AddInput<float>("gamma", gamma_dims, gamma_data);
+      test.AddInput<float>("beta", beta_dims, beta_data);
+      test.AddOutput<float>("output", output_dims, output_data);
+    }
+
     test.Run(OpTester::ExpectResult::kExpectSuccess, "", {}, nullptr, &execution_providers);
   }
 }
@@ -58,29 +76,51 @@ TEST(SkipLayerNormTest, SkipLayerNormBatch1) {
 
   std::vector<float> input_data = {
       0.8f, -0.5f, 0.0f, 1.f,
-      0.5f, 0.2f, 0.3f, -0.6f
-  };
+      0.5f, 0.2f, 0.3f, -0.6f};
 
   std::vector<float> skip_data = {
       0.1f, -0.2f, 0.3f, 1.0f,
-      0.5f, 0.1f, 0.4f, 1.6f
-  };
+      0.5f, 0.1f, 0.4f, 1.6f};
 
   std::vector<float> gamma_data = {
-      0.3f, 0.2f, 4.0f, 2.2f
-  };
+      0.3f, 0.2f, 4.0f, 2.2f};
 
   std::vector<float> beta_data = {
-      0.2f, 0.1f, 0.4f, 1.6f
-  };
+      0.2f, 0.1f, 0.4f, 1.6f};
 
   std::vector<float> output_data = {
       0.28433859348297119, -0.17090578377246857, -0.92897164821624756, 4.6924152374267578,
-      0.46111652255058289, -0.21333980560302734, -0.29631003737449646, 3.5148544311523438
-  };
-  
+      0.46111652255058289, -0.21333980560302734, -0.29631003737449646, 3.5148544311523438};
+
   RunTest(input_data, skip_data, gamma_data, beta_data, output_data,
           batch_size, sequence_length, hidden_size);
+}
+
+TEST(SkipLayerNormTest, SkipLayerNormBatch1_Float16) {
+  int batch_size = 1;
+  int sequence_length = 2;
+  int hidden_size = 4;
+
+  std::vector<float> input_data = {
+      0.8f, -0.5f, 0.0f, 1.f,
+      0.5f, 0.2f, 0.3f, -0.6f};
+
+  std::vector<float> skip_data = {
+      0.1f, -0.2f, 0.3f, 1.0f,
+      0.5f, 0.1f, 0.4f, 1.6f};
+
+  std::vector<float> gamma_data = {
+      0.3f, 0.2f, 4.0f, 2.2f};
+
+  std::vector<float> beta_data = {
+      0.2f, 0.1f, 0.4f, 1.6f};
+
+  std::vector<float> output_data = {
+      0.28433859348297119, -0.17090578377246857, -0.92897164821624756, 4.6924152374267578,
+      0.46111652255058289, -0.21333980560302734, -0.29631003737449646, 3.5148544311523438};
+
+  RunTest(input_data, skip_data, gamma_data, beta_data, output_data,
+          batch_size, sequence_length, hidden_size, true);
 }
 
 TEST(SkipLayerNormTest, SkipLayerNormBatch2) {
@@ -92,30 +132,25 @@ TEST(SkipLayerNormTest, SkipLayerNormBatch2) {
       0.8f, -0.5f, 0.0f, 1.f,
       0.5f, 0.2f, 0.3f, -0.6f,
       0.8f, -0.5f, 0.0f, 1.f,
-      0.5f, 0.2f, 0.3f, -0.6f
-  };
+      0.5f, 0.2f, 0.3f, -0.6f};
 
   std::vector<float> skip_data = {
       0.1f, -0.2f, 0.3f, 1.0f,
       0.5f, 0.1f, 0.4f, 1.6f,
       1.8f, -0.3f, 0.0f, 1.f,
-      -0.5f, 0.4f, 0.8f, -0.6f
-  };
+      -0.5f, 0.4f, 0.8f, -0.6f};
 
   std::vector<float> gamma_data = {
-      0.3f, 0.2f, 4.0f, 2.2f
-  };
+      0.3f, 0.2f, 4.0f, 2.2f};
 
   std::vector<float> beta_data = {
-      0.2f, 0.1f, 0.4f, 1.6f
-  };
+      0.2f, 0.1f, 0.4f, 1.6f};
 
   std::vector<float> output_data = {
       0.28433859348297119, -0.17090578377246857, -0.92897164821624756, 4.6924152374267578,
       0.46111652255058289, -0.21333980560302734, -0.29631003737449646, 3.5148544311523438,
       0.55470430850982666, -0.15080101788043976, -2.3229825496673584, 3.255286693572998,
-      0.15631480515003204, 0.21066918969154358, 4.9432611465454102, -1.7957965135574341
-  };
+      0.15631480515003204, 0.21066918969154358, 4.9432611465454102, -1.7957965135574341};
 
   RunTest(input_data, skip_data, gamma_data, beta_data, output_data,
           batch_size, sequence_length, hidden_size);
