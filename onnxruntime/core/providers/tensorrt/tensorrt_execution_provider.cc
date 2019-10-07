836054c4fcd86a64058c6c2e4b9cb17e81d46582
George Wu <jywu@microsoft.com>
2019-10-07 12:44:24 -0700

    status and code cleanup.

--- onnxruntime/core/providers/tensorrt/tensorrt_execution_provider.cc
+++ onnxruntime/core/providers/tensorrt/tensorrt_execution_provider.cc
@@ -21,19 +21,10 @@
 #include "core/graph/model.h"
 #include "core/providers/cuda/gpu_data_transfer.h"
 
-using namespace std;
 using namespace ONNX_NAMESPACE;
 using namespace ::onnxruntime::logging;
 
 namespace onnxruntime {
-
-#define CHECK_CUDA(call)                                        \
-  do {                                                          \
-    cudaError_t status = call;                                  \
-    if (status != cudaSuccess) {                                \
-      return common::Status(common::ONNXRUNTIME, common::FAIL); \
-    }                                                           \
-  } while (0)
 
 ONNX_OPERATOR_KERNEL_EX(
     MemcpyFromHost,
@@ -252,7 +243,7 @@ SubGraphCollection_t TensorrtExecutionProvider::GetSupportedList(SubGraphCollect
 
         // Serialize modelproto to string
         ONNX_NAMESPACE::ModelProto model_proto = model_build.ToProto();
-        string string_buf;
+        std::string string_buf;
         model_proto.SerializeToString(&string_buf);
 
         // Get supported node list recursively
@@ -312,7 +303,7 @@ TensorrtExecutionProvider::GetCapability(const onnxruntime::GraphViewer& graph,
   model_proto.set_ir_version(ONNX_NAMESPACE::Version::IR_VERSION);
 
   // Serialize modelproto to string
-  string string_buf;
+  std::string string_buf;
   model_proto.SerializeToString(&string_buf);
 
   // Get supported node list
@@ -382,7 +373,7 @@ common::Status TensorrtExecutionProvider::Compile(const std::vector<onnxruntime:
     ONNX_NAMESPACE::ModelProto model_proto = model.ToProto();
     *(model_proto.mutable_graph()) = graph_body.ToGraphProto();
     model_proto.set_ir_version(ONNX_NAMESPACE::Version::IR_VERSION);
-    string string_buf;
+    std::string string_buf;
     model_proto.SerializeToString(&string_buf);
 
     // Create TensorRT engine
@@ -436,11 +427,17 @@ common::Status TensorrtExecutionProvider::Compile(const std::vector<onnxruntime:
     }
 
     auto trt_engine = unique_pointer<nvinfer1::ICudaEngine>(trt_builder->buildEngineWithConfig(*trt_network, *trt_config));
-    ORT_ENFORCE(trt_engine != nullptr);
+    if (trt_engine == nullptr) {
+      return ORT_MAKE_STATUS(ONNXRUNTIME, EP_FAIL,
+                             "TensorRT EP could not build Engine for fused node: " + fused_node->Name());
+    }
 
     // Build TensorRT context
     auto trt_context = unique_pointer<nvinfer1::IExecutionContext>(trt_engine->createExecutionContext());
-    ORT_ENFORCE(trt_context != nullptr);
+    if (trt_context == nullptr) {
+      return ORT_MAKE_STATUS(ONNXRUNTIME, EP_FAIL,
+                             "TensorRT EP could not build Execution Context for fused node: " + fused_node->Name());
+    }
 
     // Get input shape and binding index
     int num_inputs = trt_network->getNbInputs();
@@ -565,10 +562,11 @@ common::Status TensorrtExecutionProvider::Compile(const std::vector<onnxruntime:
           for (int j = 0, end = dimensions.nbDims; j < end; ++j) {
             input_dim_size *= tensor_shape[j];
           }
-          CHECK_CUDA(cudaMalloc(&buffers[i], input_dim_size * sizeof(int32_t)));
+          CUDA_RETURN_IF_ERROR(cudaMalloc(&buffers[i], input_dim_size * sizeof(int32_t)));
           cuda::Impl_Cast<int64_t, int32_t>(ort.GetTensorData<int64_t>(input_tensor), reinterpret_cast<int32_t*>(buffers[i]), input_dim_size);
         } else {
-          return common::Status(common::ONNXRUNTIME, common::NOT_IMPLEMENTED);
+          return ORT_MAKE_STATUS(ONNXRUNTIME, EP_FAIL,
+                                 "TensorRT EP input onnx tensor data type: " + std::to_string(tensor_type) + " not supported.");
         }
       }
 
@@ -596,16 +594,20 @@ common::Status TensorrtExecutionProvider::Compile(const std::vector<onnxruntime:
           for (int j = 0, end = dimensions.nbDims; j < end; ++j) {
             output_dim_size[i] *= dimensions.d[j];
           }
-          CHECK_CUDA(cudaMalloc(&buffers[i + num_binding_inputs], output_dim_size[i] * sizeof(int32_t)));
+          CUDA_RETURN_IF_ERROR(cudaMalloc(&buffers[i + num_binding_inputs], output_dim_size[i] * sizeof(int32_t)));
 
         } else {
-          return common::Status(common::ONNXRUNTIME, common::NOT_IMPLEMENTED);
+          return ORT_MAKE_STATUS(ONNXRUNTIME, EP_FAIL,
+                                 "TensorRT EP output onnx tensor data type: " + std::to_string(output_types[i]) + " not supported.");
         }
       }
 
       // Run TRT inference
       std::lock_guard<OrtMutex> lock(*(trt_state->tensorrt_mu_ptr));
-      trt_state->context->enqueueV2(&buffers[0], nullptr, nullptr);
+      if (!trt_state->context->enqueueV2(&buffers[0], nullptr, nullptr)) {
+        // return FAIL rather than EP_FAIL because the enqueue failure may be transient.
+        return ORT_MAKE_STATUS(ONNXRUNTIME, FAIL, "TensorRT EP Execution Context Enqueue Failed.");
+      }
 
       // Cast INT64 input to INT32 because TensorRT doesn't fully support INT64
       for (int i = 0, end = num_binding_outputs; i < end; ++i) {
@@ -623,4 +625,3 @@ common::Status TensorrtExecutionProvider::Compile(const std::vector<onnxruntime:
   return Status::OK();
 }
 }  // namespace onnxruntime
-
