e9bed8b23b518b88bfd0480611a3ac295228c03a
Changming Sun <chasun@microsoft.com>
2019-10-08 10:03:54 -0700

    Change python packaging pipeline to use manylinux1 (#2035)
    1. Change the python packaing pipeline to use manylinux1
    2. Temporarily disable model test in the python pipeline.

--- tools/ci_build/github/linux/docker/scripts/install_centos.sh
+++ tools/ci_build/github/linux/docker/scripts/install_centos.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 set -e
 
-yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+os_major_version=$(cat /etc/redhat-release | tr -dc '0-9.'|cut -d \. -f1)
 
-yum install -y redhat-lsb-core expat-devel libcurl-devel rpmdevtools tar unzip ccache curl gcc gcc-c++ zlib-devel make  python2-devel python3-devel python3-pip python3-numpy libunwind icu aria2 rsync python3-setuptools python3-wheel bzip2 git
 
+yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-$os_major_version.noarch.rpm
+
+if [ "$os_major_version" == "5" ]; then
+ yum install -y redhat-lsb expat-devel libcurl-devel rpmdevtools tar unzip curl zlib-devel make  python2-devel icu  rsync bzip2 git bzip2-devel
+else
+ yum install -y redhat-lsb-core expat-devel libcurl-devel rpmdevtools tar unzip curl zlib-devel make  python2-devel  libunwind icu aria2 rsync bzip2 git bzip2-devel
+fi
+
+
+#If the /opt/python folder exists, we assume this is the manylinux docker image
+if [ ! -d "/opt/python/cp35-cp35m"  ] 
+then
+yum install -y ccache gcc gcc-c++ python3-devel python3-pip python3-numpy python3-setuptools python3-wheel
+fi
