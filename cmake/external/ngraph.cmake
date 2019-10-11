99e1ff8b9c0f5579b5bb6fffa986a989cc2c8e39
tomdol <tomasz.dolbniak@intel.com>
2019-10-12 00:54:20 +0200

    Update ngraph to RC8 - bigobj flag for Windows builds

--- cmake/external/ngraph.cmake
+++ cmake/external/ngraph.cmake
@@ -12,7 +12,7 @@ set(prebuilt_ONNX_SOURCE_DIR "${PROJECT_SOURCE_DIR}/external/onnx")
 set(prebuilt_ONNX_BINARY_DIR "${CMAKE_CURRENT_BINARY_DIR}")
 set(ngraph_URL "https://github.com/NervanaSystems/ngraph.git")
 # TODO: change to v0.26.0 tag when it's ready
-set(ngraph_TAG "v0.26.0-rc.7")
+set(ngraph_TAG "v0.26.0-rc.8")
 
 # Libraries for python package.
 if (WIN32)
