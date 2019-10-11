bf1aeda87508aca14ff059d346b3c6abc19ad1e3
Ryan Hill <38674843+RyanUnderhill@users.noreply.github.com>
2019-10-11 11:12:51 -0700

    Ryanunderhill/customop dll (#2002)
    * Add OrtApiBase
    * Add RegisterCustomOpsLibrary API

--- onnxruntime/test/perftest/main.cc
+++ onnxruntime/test/perftest/main.cc
@@ -9,8 +9,8 @@
 
 using namespace onnxruntime;
 
-const OrtApi* g_ort = OrtGetApi(ORT_API_VERSION);
-const OrtApi* Ort::g_api = OrtGetApi(ORT_API_VERSION);
+const OrtApi* g_ort = OrtGetApiBase()->GetApi(ORT_API_VERSION);
+const OrtApi* Ort::g_api = OrtGetApiBase()->GetApi(ORT_API_VERSION);
 
 #ifdef _WIN32
 int real_main(int argc, wchar_t* argv[]) {
