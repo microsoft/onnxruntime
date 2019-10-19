4efa5c4585ead3b0419ed00b162c7f91ee9121af
Shah Asaduzzaman <shahasad@microsoft.com>
2019-10-19 21:14:17 +0000

    should compile now, check for errors

--- onnxruntime/test/testdata/custom_op_library/custom_op_library.cc
+++ onnxruntime/test/testdata/custom_op_library/custom_op_library.cc
@@ -36,17 +36,17 @@
 #include <cmath>
 
 static const char* c_OpDomain = "test.customop";
-static const char* c_OpOneName = "CustomOpOne";
-static const char* c_OpTwoName = "CustomOpTwo";
+// static const char* c_OpOneName = "CustomOpOne";
+// static const char* c_OpTwoName = "CustomOpTwo";
 static const char* c_EPType = nullptr; // CPU
-ONNXTensorElementDataType c_dataElemTypeOne = ONNX_TENSOR_ELEMENT_DATA_TYPE_FLOAT;
-ONNXTensorElementDataType c_dataElemTypeTwo = ONNX_TENSOR_ELEMENT_DATA_TYPE_INT32;
+// ONNXTensorElementDataType c_dataElemTypeOne = ONNX_TENSOR_ELEMENT_DATA_TYPE_FLOAT;
+// ONNXTensorElementDataType c_dataElemTypeTwo = ONNX_TENSOR_ELEMENT_DATA_TYPE_INT32;
 
-#define ORT_API_RETURN_IF_ERROR(expr) \
-  do {                                \
-    auto _status = (expr);            \
-    if (_status) return _status;      \
-  } while (0)
+// #define ORT_API_RETURN_IF_ERROR(expr) \
+//   do {                                \
+//     auto _status = (expr);            \
+//     if (_status) return _status;      \
+//   } while (0)
 
 
 
@@ -61,11 +61,106 @@ struct OrtTensorDimensions : std::vector<int64_t> {
 };
 
 
-struct CustomKernelOne {
-  CustomKernelOne(Ort::OrtApi ort, const OrtKernelInfo* /*info*/) : ort_(ort) {
-  }
+// struct CustomKernelOne {
+//   CustomKernelOne(Ort::OrtApi ort, const OrtKernelInfo* /*info*/) : ort_(ort) {
+//   }
+
+//   void Compute(OrtKernelContext* context) {
+//     // Setup inputs
+//     const OrtValue* input_X = ort_.KernelContext_GetInput(context, 0);
+//     const OrtValue* input_Y = ort_.KernelContext_GetInput(context, 1);
+//     const float* X = ort_.GetTensorData<float>(input_X);
+//     const float* Y = ort_.GetTensorData<float>(input_Y);
+
+//     // Setup output
+//     OrtTensorDimensions dimensions(ort_, input_X);
+
+//     OrtValue* output = ort_.KernelContext_GetOutput(context, 0, dimensions.data(), dimensions.size());
+//     float* out = ort_.GetTensorMutableData<float>(output);
+
+//     OrtTensorTypeAndShapeInfo* output_info = ort_.GetTensorTypeAndShape(output);
+//     int64_t size = ort_.GetTensorShapeElementCount(output_info);
+//     ort_.ReleaseTensorTypeAndShapeInfo(output_info);
+
+//     // Do computation
+//     for (int64_t i = 0; i < size; i++) {
+//       out[i] = X[i] + Y[i];
+//     }
+//   }
+
+//   static const char* GetName()
+//   {
+//       return "CustomOpOne";
+//   }
+//   static size_t GetInputCount()
+//   {
+//       return 2;
+//   }
+
+//   static ONNXTensorElementDataType GetInputType(size_t /*i*/)
+//   {
+//       return ONNX_TENSOR_ELEMENT_DATA_TYPE_FLOAT;
+//   }
+
+//   static size_t GetOutputCount()
+//   {
+//       return 1;
+//   }
+
+  
+
+//   static   
+//  private:
+//   Ort::OrtApi ort_;
+// };
+
+
+// struct CustomKernelTwo {
+//   CustomKernelTwo(Ort::OrtApi ort, const OrtKernelInfo* /*info*/) : ort_(ort) {
+//   }
+
+//   void Compute(OrtKernelContext* context) {
+//     // Setup inputs
+//     const OrtValue* input_X = ort_.KernelContext_GetInput(context, 0);
+//     const float* X = ort_.GetTensorData<float>(input_X);
+
+//     // Setup output
+//     OrtTensorDimensions dimensions(ort_, input_X);
+
+//     OrtValue* output = ort_.KernelContext_GetOutput(context, 0, dimensions.data(), dimensions.size());
+//     int32_t* out = ort_.GetTensorMutableData<int32_t>(output);
+
+//     OrtTensorTypeAndShapeInfo* output_info = ort_.GetTensorTypeAndShape(output);
+//     int64_t size = ort_.GetTensorShapeElementCount(output_info);
+//     ort_.ReleaseTensorTypeAndShapeInfo(output_info);
+
+//     // Do computation
+//     for (int64_t i = 0; i < size; i++) {
+//       out[i] = (int32_t)(round(X[i]));
+//     }
+//   }
+  
+
+//  private:
+//   Ort::OrtApi ort_;
+// };
+
+
+
+
+struct CustomOpOne : Ort::CustomOpBase<CustomOpOne, CustomOpOne> {
+  void* CreateKernel(Ort::OrtApi api, const OrtKernelInfo* info) { return this; };
+  const char* GetName() const { return "CustomOpOne"; };
+
+  size_t GetInputTypeCount() const { return 2; };
+  ONNXTensorElementDataType GetInputType(size_t /*index*/) const { return ONNX_TENSOR_ELEMENT_DATA_TYPE_FLOAT; };
+
+  size_t GetOutputTypeCount() const { return 1; };
+  ONNXTensorElementDataType GetOutputType(size_t /*index*/) const { return ONNX_TENSOR_ELEMENT_DATA_TYPE_FLOAT; };
 
   void Compute(OrtKernelContext* context) {
+    auto ort_ = OrtGetApiBase()->GetApi(version);
+
     // Setup inputs
     const OrtValue* input_X = ort_.KernelContext_GetInput(context, 0);
     const OrtValue* input_Y = ort_.KernelContext_GetInput(context, 1);
@@ -88,16 +183,21 @@ struct CustomKernelOne {
     }
   }
 
- private:
-  Ort::OrtApi ort_;
-};
+} c_CustomOpOne;
 
+struct CustomOpTwo : Ort::CustomOpBase<CustomOpTwo, CustomOpTwo> {
+  void* CreateKernel(Ort::OrtApi api, const OrtKernelInfo* info) { return this; };
+  const char* GetName() const { return "CustomOpTwo"; };
 
-struct CustomKernelTwo {
-  CustomKernelTwo(Ort::OrtApi ort, const OrtKernelInfo* /*info*/) : ort_(ort) {
-  }
+  size_t GetInputTypeCount() const { return 1; };
+  ONNXTensorElementDataType GetInputType(size_t /*index*/) const { return ONNX_TENSOR_ELEMENT_DATA_TYPE_FLOAT; };
+
+  size_t GetOutputTypeCount() const { return 1; };
+  ONNXTensorElementDataType GetOutputType(size_t /*index*/) const { return ONNX_TENSOR_ELEMENT_DATA_TYPE_INT32; };
 
   void Compute(OrtKernelContext* context) {
+    auto ort_ = OrtGetApiBase()->GetApi(version);
+
     // Setup inputs
     const OrtValue* input_X = ort_.KernelContext_GetInput(context, 0);
     const float* X = ort_.GetTensorData<float>(input_X);
@@ -117,194 +217,177 @@ struct CustomKernelTwo {
       out[i] = (int32_t)(round(X[i]));
     }
   }
-  
+} c_CustomOpTwo;
 
- private:
-  Ort::OrtApi ort_;
-};
-
-
-
-
-struct CustomOpOne : Ort::CustomOpBase<CustomOpOne, MyCustomKernel> {
-  void* CreateKernel(Ort::OrtApi api, const OrtKernelInfo* info) { return new MyCustomKernel(api, info); };
-  const char* GetName() const { return "Foo"; };
-
-  size_t GetInputTypeCount() const { return 2; };
-  ONNXTensorElementDataType GetInputType(size_t /*index*/) const { return ONNX_TENSOR_ELEMENT_DATA_TYPE_FLOAT; };
-
-  size_t GetOutputTypeCount() const { return 1; };
-  ONNXTensorElementDataType GetOutputType(size_t /*index*/) const { return ONNX_TENSOR_ELEMENT_DATA_TYPE_FLOAT; };
-};
 
 
 
 //===================
 
-// This callback creates the kernel, which is a user defined parameter that is passed to the Kernel* callbacks below.
-void* ORT_API_CALL CreateKernel_One(_In_ struct OrtCustomOp* op, _In_ const OrtApi* api, _In_ const OrtKernelInfo* info)
-{
-    return nullptr;
-}
+// // This callback creates the kernel, which is a user defined parameter that is passed to the Kernel* callbacks below.
+// void* ORT_API_CALL CreateKernel_One(_In_ struct OrtCustomOp* op, _In_ const OrtApi* api, _In_ const OrtKernelInfo* info)
+// {
+//     return nullptr;
+// }
 
-// Returns the name of the op
-const char* ORT_API_CALL GetName_One(_In_ struct OrtCustomOp* op)
-{
-    return c_OpOneName;   
-}
+// // Returns the name of the op
+// const char* ORT_API_CALL GetName_One(_In_ struct OrtCustomOp* op)
+// {
+//     return c_OpOneName;   
+// }
 
-// Returns the type of the execution provider, return nullptr to use CPU execution provider
-const char* ORT_API_CALL GetExecutionProviderType_One(_In_ struct OrtCustomOp* op)
-{
-    return c_EPType;
-}
+// // Returns the type of the execution provider, return nullptr to use CPU execution provider
+// const char* ORT_API_CALL GetExecutionProviderType_One(_In_ struct OrtCustomOp* op)
+// {
+//     return c_EPType;
+// }
 
-// Returns the count and types of the input & output tensors
-ONNXTensorElementDataType ORT_API_CALL GetInputType_One(_In_ struct OrtCustomOp* op, _In_ size_t index)
-{
-    return c_dataElemTypeOne;
-}
+// // Returns the count and types of the input & output tensors
+// ONNXTensorElementDataType ORT_API_CALL GetInputType_One(_In_ struct OrtCustomOp* op, _In_ size_t index)
+// {
+//     return c_dataElemTypeOne;
+// }
   
-size_t ORT_API_CALL GetInputTypeCount_One(_In_ struct OrtCustomOp* op)
-{
-    return 2;
-}
+// size_t ORT_API_CALL GetInputTypeCount_One(_In_ struct OrtCustomOp* op)
+// {
+//     return 2;
+// }
 
 
-ONNXTensorElementDataType ORT_API_CALL GetOutputType_One(_In_ struct OrtCustomOp* op, _In_ size_t index)
-{
-    return c_dataElemTypeOne;
-}
+// ONNXTensorElementDataType ORT_API_CALL GetOutputType_One(_In_ struct OrtCustomOp* op, _In_ size_t index)
+// {
+//     return c_dataElemTypeOne;
+// }
 
-size_t ORT_API_CALL GetOutputTypeCount_One(_In_ struct OrtCustomOp* op)
-{
-    return 1;
-}
+// size_t ORT_API_CALL GetOutputTypeCount_One(_In_ struct OrtCustomOp* op)
+// {
+//     return 1;
+// }
 
-// Op kernel callbacks
-void ORT_API_CALL KernelCompute_One(_In_ void* op_kernel, _In_ OrtKernelContext* context)
-{
+// // Op kernel callbacks
+// void ORT_API_CALL KernelCompute_One(_In_ void* op_kernel, _In_ OrtKernelContext* context)
+// {
     
 
 
-}
+// }
 
-void ORT_API_CALL KernelDestroy_One(_In_ void* op_kernel)
-{
-    // nothing to do
-}
+// void ORT_API_CALL KernelDestroy_One(_In_ void* op_kernel)
+// {
+//     // nothing to do
+// }
 
 
-// This callback creates the kernel, which is a user defined parameter that is passed to the Kernel* callbacks below.
-void* ORT_API_CALL CreateKernel_Two(_In_ struct OrtCustomOp* op, _In_ const OrtApi* api, _In_ const OrtKernelInfo* info)
-{
-    return nullptr;
-}
+// // This callback creates the kernel, which is a user defined parameter that is passed to the Kernel* callbacks below.
+// void* ORT_API_CALL CreateKernel_Two(_In_ struct OrtCustomOp* op, _In_ const OrtApi* api, _In_ const OrtKernelInfo* info)
+// {
+//     return nullptr;
+// }
 
-// Returns the name of the op
-const char* ORT_API_CALL GetName_Two(_In_ struct OrtCustomOp* op)
-{
-    return c_OpTwoName;
-}
+// // Returns the name of the op
+// const char* ORT_API_CALL GetName_Two(_In_ struct OrtCustomOp* op)
+// {
+//     return c_OpTwoName;
+// }
 
-// Returns the type of the execution provider, return nullptr to use CPU execution provider
-const char* ORT_API_CALL GetExecutionProviderType_Two(_In_ struct OrtCustomOp* op)
-{
-    return c_EPType;
-}
+// // Returns the type of the execution provider, return nullptr to use CPU execution provider
+// const char* ORT_API_CALL GetExecutionProviderType_Two(_In_ struct OrtCustomOp* op)
+// {
+//     return c_EPType;
+// }
 
-// Returns the count and types of the input & output tensors
-ONNXTensorElementDataType ORT_API_CALL GetInputType_Two(_In_ struct OrtCustomOp* op, _In_ size_t index)
-{
-    return c_dataElemTypeTwo;
-}
+// // Returns the count and types of the input & output tensors
+// ONNXTensorElementDataType ORT_API_CALL GetInputType_Two(_In_ struct OrtCustomOp* op, _In_ size_t index)
+// {
+//     return c_dataElemTypeTwo;
+// }
   
-size_t ORT_API_CALL GetInputTypeCount_Two(_In_ struct OrtCustomOp* op)
-{
-    return 2;
-}
+// size_t ORT_API_CALL GetInputTypeCount_Two(_In_ struct OrtCustomOp* op)
+// {
+//     return 2;
+// }
 
 
-ONNXTensorElementDataType ORT_API_CALL GetOutputType_Two(_In_ struct OrtCustomOp* op, _In_ size_t index)
-{
-    return c_dataElemTypeTwo;
-}
+// ONNXTensorElementDataType ORT_API_CALL GetOutputType_Two(_In_ struct OrtCustomOp* op, _In_ size_t index)
+// {
+//     return c_dataElemTypeTwo;
+// }
 
-size_t ORT_API_CALL GetOutputTypeCount_Two(_In_ struct OrtCustomOp* op)
-{
-    return 1;
-}
+// size_t ORT_API_CALL GetOutputTypeCount_Two(_In_ struct OrtCustomOp* op)
+// {
+//     return 1;
+// }
 
-// Op kernel callbacks
-void ORT_API_CALL KernelCompute_Two(_In_ void* op_kernel, _In_ OrtKernelContext* context)
-{
-    auto api = OrtGetApiBase()->GetApi(ORT_API_VERSION);
-    size_t inputCount, outputCount;
+// // Op kernel callbacks
+// void ORT_API_CALL KernelCompute_Two(_In_ void* op_kernel, _In_ OrtKernelContext* context)
+// {
+//     auto api = OrtGetApiBase()->GetApi(ORT_API_VERSION);
+//     size_t inputCount, outputCount;
 
-    if (auto status = api->KernelContext_GetInputCount(context, &inputCount))
-    {
-        return; // throw exception? where to log the error?
-    }
+//     if (auto status = api->KernelContext_GetInputCount(context, &inputCount))
+//     {
+//         return; // throw exception? where to log the error?
+//     }
 
-    if (auto status = api->KernelContext_GetOutputCount(context, &outputCount))
-    {
-        return;
-    }
+//     if (auto status = api->KernelContext_GetOutputCount(context, &outputCount))
+//     {
+//         return;
+//     }
 
-    std::vector<OrtValue*> inputs(inputCount);
-    std::vector<OrtValue*> outputs(outputCount);
+//     std::vector<OrtValue*> inputs(inputCount);
+//     std::vector<OrtValue*> outputs(outputCount);
 
-    for (int i = 0; i < inputCount; i++)
-    {
-        if (auto status = api->KernelContext_GetInput(context, i, (const OrtValue**)inputs.data()+i))
-        {
-            return;
-        }
-    }
+//     for (int i = 0; i < inputCount; i++)
+//     {
+//         if (auto status = api->KernelContext_GetInput(context, i, (const OrtValue**)inputs.data()+i))
+//         {
+//             return;
+//         }
+//     }
 
-    // for (int i = 0; i < outputCount; i++)
-    // {
-    //     if (auto status = api->KernelContext_GetOutput(context, i, (const OrtValue**)outputs.data()+i))
-    //     {
-    //         return;
-    //     }
-    // }
+//     // for (int i = 0; i < outputCount; i++)
+//     // {
+//     //     if (auto status = api->KernelContext_GetOutput(context, i, (const OrtValue**)outputs.data()+i))
+//     //     {
+//     //         return;
+//     //     }
+//     // }
 
 
 
-}
+// }
 
-void ORT_API_CALL KernelDestroy_Two(_In_ void* op_kernel)
-{
+// void ORT_API_CALL KernelDestroy_Two(_In_ void* op_kernel)
+// {
 
-}
+// }
 
-OrtCustomOp c_CustomOpOne =
-{
-ORT_API_VERSION, 
-CreateKernel_One, 
-GetName_One,
-GetExecutionProviderType_One,
-GetInputType_One,
-GetInputTypeCount_One,
-GetOutputType_One,
-GetOutputTypeCount_One,
-KernelCompute_One,
-KernelDestroy_One
-},
-c_CustomOpTwo = 
-{
-ORT_API_VERSION, 
-CreateKernel_Two, 
-GetName_Two,
-GetExecutionProviderType_Two,
-GetInputType_Two,
-GetInputTypeCount_Two,
-GetOutputType_Two,
-GetOutputTypeCount_Two,
-KernelCompute_Two,
-KernelDestroy_Two
-};
+// OrtCustomOp c_CustomOpOne =
+// {
+// ORT_API_VERSION, 
+// CreateKernel_One, 
+// GetName_One,
+// GetExecutionProviderType_One,
+// GetInputType_One,
+// GetInputTypeCount_One,
+// GetOutputType_One,
+// GetOutputTypeCount_One,
+// KernelCompute_One,
+// KernelDestroy_One
+// },
+// c_CustomOpTwo = 
+// {
+// ORT_API_VERSION, 
+// CreateKernel_Two, 
+// GetName_Two,
+// GetExecutionProviderType_Two,
+// GetInputType_Two,
+// GetInputTypeCount_Two,
+// GetOutputType_Two,
+// GetOutputTypeCount_Two,
+// KernelCompute_Two,
+// KernelDestroy_Two
+// };
 
 
 
