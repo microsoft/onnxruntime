30adbe64e2380736804003fe542b76f9fc65f2a6
Sreekanth Yalachigere <17345104+sreekanth-yalachigere@users.noreply.github.com>
2019-10-22 07:23:41 -0700

    update libmkldnn.so version (#2220)

--- setup.py
+++ setup.py
@@ -115,7 +115,7 @@ except ImportError:
 
 # Additional binaries
 if platform.system() == 'Linux':
-  libs = ['onnxruntime_pybind11_state.so', 'libmkldnn.so.0', 'libmklml_intel.so', 'libiomp5.so', 'mimalloc.so']
+  libs = ['onnxruntime_pybind11_state.so', 'libmkldnn.so.1', 'libmklml_intel.so', 'libiomp5.so', 'mimalloc.so']
   # nGraph Libs
   libs.extend(['libngraph.so', 'libcodegen.so', 'libcpu_backend.so', 'libmkldnn.so', 'libtbb_debug.so', 'libtbb_debug.so.2', 'libtbb.so', 'libtbb.so.2'])
   # Nuphar Libs
@@ -123,7 +123,7 @@ if platform.system() == 'Linux':
   if nightly_build:
     libs.extend(['libonnxruntime_pywrapper.so'])
 elif platform.system() == "Darwin":
-  libs = ['onnxruntime_pybind11_state.so', 'libmkldnn.0.dylib', 'mimalloc.so'] # TODO add libmklml and libiomp5 later.
+  libs = ['onnxruntime_pybind11_state.so', 'libmkldnn.1.dylib', 'mimalloc.so'] # TODO add libmklml and libiomp5 later.
   if nightly_build:
     libs.extend(['libonnxruntime_pywrapper.dylib'])
 else:
