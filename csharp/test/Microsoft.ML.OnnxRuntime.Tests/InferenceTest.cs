53e535d764619da079b3aec299d6877ad76ba8ff
Shah Asaduzzaman (ASAD) <shahasad@microsoft.com>
2019-10-23 08:37:47 -0700

    enable pretrained model test

--- csharp/test/Microsoft.ML.OnnxRuntime.Tests/InferenceTest.cs
+++ csharp/test/Microsoft.ML.OnnxRuntime.Tests/InferenceTest.cs
@@ -390,7 +390,7 @@ namespace Microsoft.ML.OnnxRuntime.Tests
         }
 
 
-        [Theory(Skip = "TestPreTrainedModels is flaky and is blocking CI build progress. Enable it once this is fixed.")]
+        [Theory]
         [MemberData(nameof(GetModelsForTest))]
         [MemberData(nameof(GetSkippedModelForTest), Skip = "Skipped due to Error, please fix the error and enable the test")]
         private void TestPreTrainedModels(string opset, string modelName)
