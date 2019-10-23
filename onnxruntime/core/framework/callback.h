0ed99807f07c9a548b18e1ac8997baab1622b0d8
Edward Chen <18449977+edgchen1@users.noreply.github.com>
2019-10-23 14:16:49 -0700

    Fixed CentOS build.

--- onnxruntime/core/framework/callback.h
+++ onnxruntime/core/framework/callback.h
@@ -18,7 +18,13 @@ void OrtRunCallback(OrtCallback* f) noexcept;
  * Useful for something like a std::unique_ptr<> deleter.
  */
 struct OrtCallbackInvoker {
-  OrtCallback callback{nullptr, nullptr};
+  OrtCallbackInvoker()
+      : callback{nullptr, nullptr} {}
+
+  OrtCallbackInvoker(OrtCallback callback_to_invoke)
+      : callback(callback_to_invoke) {}
+
+  OrtCallback callback;
 
   template <typename T>
   void operator()(T) {
