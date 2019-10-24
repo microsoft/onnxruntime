bf664575252a17346b704f682d9bfd179e563fdb
Tracy Sharpe <42477615+tracysh@users.noreply.github.com>
2019-10-23 19:12:40 -0700

    handle Concat-11 (#2240)

--- onnxruntime/core/optimizer/nchwc_transformer.cc
+++ onnxruntime/core/optimizer/nchwc_transformer.cc
@@ -684,7 +684,7 @@ void NchwcTransformerImpl::Transform(Node& node) {
     if (graph_utils::IsSupportedOptypeVersionAndDomain(node, "Add", {7}) ||
         graph_utils::IsSupportedOptypeVersionAndDomain(node, "Sum", {6, 8})) {
       TransformAdd(node);
-    } else if (graph_utils::IsSupportedOptypeVersionAndDomain(node, "Concat", {4})) {
+    } else if (graph_utils::IsSupportedOptypeVersionAndDomain(node, "Concat", {4, 11})) {
       TransformConcat(node);
     } else if (graph_utils::IsSupportedOptypeVersionAndDomain(node, "Relu", {6})) {
       TransformActivation(node);
