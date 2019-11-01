66e4815ac67efdcdc3250b50848d10250bb93bfe
Tianlei Wu <tlwu@microsoft.com>
2019-11-01 13:49:55 -0700

    Fix mask in EmbedLayerNormalization (#2300)

--- onnxruntime/contrib_ops/cuda/bert/embed_layer_norm_impl.cu
+++ onnxruntime/contrib_ops/cuda/bert/embed_layer_norm_impl.cu
@@ -182,7 +182,7 @@ bool LaunchEmbedLayerNormKernel(
     const size_t element_size) {
   const cudaStream_t stream = nullptr;  // default stream
 
-  if (!ComputeMaskIndex(stream, sequence_length, hidden_size, input_mask, static_cast<int*>(mask_index))) {
+  if (!ComputeMaskIndex(stream, sequence_length, batch_size, input_mask, static_cast<int*>(mask_index))) {
     return false;
   }
 
