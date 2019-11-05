64d550ed3a48929fb5c6cbe226be84bcc292ecfd
CeciliaLiu <ziyue.liu7@gmail.com>
2019-11-04 16:11:17 -0800

    tests

--- onnxruntime/core/optimizer/layer_norm_fusion.cc
+++ onnxruntime/core/optimizer/layer_norm_fusion.cc
@@ -12,16 +12,16 @@ namespace onnxruntime {
 
 static const Node* first_child_by_type(Node& node, std::string child_type) {
   //const Node* p_child_found = nullptr;
-	for (auto it = node.OutputNodesBegin(); it != node.OutputNodesEnd(); ++it) {
-	std::cout << "children optype " << (*it).OpType() << "\n";
-	if ((*it).OpType().compare(child_type) == 0) {
-		//p_child_found = &(*it);
-		std::cout << "Matched node found! \n";
-		//break;
-		return &(*it);
-	}
-	}
-  
+  for (auto it = node.OutputNodesBegin(); it != node.OutputNodesEnd(); ++it) {
+    std::cout << "children optype " << (*it).OpType() << "\n";
+    if ((*it).OpType().compare(child_type) == 0) {
+      //p_child_found = &(*it);
+      std::cout << "Matched node found! \n";
+      //break;
+      return &(*it);
+    }
+  }
+
   return nullptr;
 }
 
@@ -61,11 +61,14 @@ Status LayerNormFusion::ApplyImpl(Graph& graph, bool& modified, int graph_level)
   for (auto node_index : node_topology_list) {
     nodes_to_remove.clear();
     std::cout << "entering fusion\n";
-	auto* p_reduce_mean = graph.GetNode(node_index);
+    auto* p_reduce_mean = graph.GetNode(node_index);
     if (p_reduce_mean == nullptr)
       continue;  // we removed the node as part of an earlier fusion
     Node& reduce_mean_node = *p_reduce_mean;
     ORT_RETURN_IF_ERROR(Recurse(reduce_mean_node, modified, graph_level));
+    std::cout << reduce_mean_node.OpType() << "\n";
+    std::cout << graph_utils::IsSupportedOptypeVersionAndDomain(reduce_mean_node, "ReduceMean", {1});
+    std::cout << "node.op since version " << reduce_mean_node.Op()->SinceVersion() << "\n";
 
     if (!graph_utils::IsSupportedOptypeVersionAndDomain(reduce_mean_node, "ReduceMean", {1}) ||
         !graph_utils::IsSupportedProvider(reduce_mean_node, GetCompatibleExecutionProviders()) ||
@@ -75,7 +78,7 @@ Status LayerNormFusion::ApplyImpl(Graph& graph, bool& modified, int graph_level)
     }
     nodes_to_remove.push_back(reduce_mean_node);
 
-	// Loop through the children of current "ReduceMean" node. See if they match ["Sub"] or ["Sub", "Sub"]
+    // Loop through the children of current "ReduceMean" node. See if they match ["Sub"] or ["Sub", "Sub"]
     int subCnt = 0;
     const Node* p_sub_node = nullptr;
     const Node* p_sub_node_dup = nullptr;
@@ -87,28 +90,27 @@ Status LayerNormFusion::ApplyImpl(Graph& graph, bool& modified, int graph_level)
         } else {
           p_sub_node_dup = &(*iter);
         }
-		subCnt++;
+        subCnt++;
       } else {
-		// doesn't match layer norm pattern. break.
+        // doesn't match layer norm pattern. break.
         subCnt = -1;
-		break;
-	  }
+        break;
+      }
     }
 
     if (subCnt != 1 && subCnt != 2) {
-		continue;
-	}
+      continue;
+    }
     Node& sub_node = *graph.GetNode(p_sub_node->Index());
     if (!graph_utils::IsSupportedOptypeVersionAndDomain(sub_node, "Sub", {7}) ||
         sub_node.GetExecutionProviderType() != reduce_mean_node.GetExecutionProviderType() ||
-        sub_node.GetOutputEdgesCount() != 1 ||
         !IsSupportedDataType(sub_node)) {
-        continue;
+      continue;
     }
     nodes_to_remove.push_back(sub_node);
     std::cout << "sub found\n";
     const Node* p_div = nullptr;
-	p_div = first_child_by_type(sub_node, "Div");
+    p_div = first_child_by_type(sub_node, "Div");
     if (p_div == nullptr) {
       std::cout << "sub_node has not p_div. Check if sub node has a dup. \n";
       // Find the sub_dup node if exist
@@ -123,18 +125,18 @@ Status LayerNormFusion::ApplyImpl(Graph& graph, bool& modified, int graph_level)
         }
         std::cout << "sub dup found\n";
         p_div = first_child_by_type(sub_node_dup, "Div");
-      } else {     
-		  std::cout << "sub dup not found. exit.\n";
-		  continue;
+      } else {
+        std::cout << "sub dup not found. exit.\n";
+        continue;
       }
     }
 
-	if (p_div == nullptr) {
+    if (p_div == nullptr) {
       std::cout << "no div from two subs. exit.\n ";
       continue;
-	}
+    }
     std::cout << "get div node.\n";
-	Node& div_node = *graph.GetNode(p_div ->Index());
+    Node& div_node = *graph.GetNode(p_div->Index());
     std::cout << "div here. Checking.\n ";
     if (!graph_utils::IsSupportedOptypeVersionAndDomain(div_node, "Div", {7}) ||
         div_node.GetExecutionProviderType() != reduce_mean_node.GetExecutionProviderType() ||
@@ -146,20 +148,20 @@ Status LayerNormFusion::ApplyImpl(Graph& graph, bool& modified, int graph_level)
     nodes_to_remove.push_back(div_node);
     std::cout << "div found\n";
 
-	// Traceback the div node to see if sqrt --> div
+    // Traceback the div node to see if sqrt --> div
     const Node* p_sqrt = first_parent_by_type(div_node, "Sqrt");
     if (p_sqrt == nullptr) {
       std::cout << "no sqrt found \n";
       continue;
-	}
-    Node& sqrt_node = *graph.GetNode(p_sqrt ->Index());
+    }
+    Node& sqrt_node = *graph.GetNode(p_sqrt->Index());
     std::cout << sqrt_node.OpType() << "\n";
     std::cout << graph_utils::IsSupportedOptypeVersionAndDomain(sqrt_node, "Sqrt", {6});
     std::cout << "node.op since version " << sqrt_node.Op()->SinceVersion() << "\n";
 
-	std::cout << IsSupportedDataType(sqrt_node);
+    std::cout << IsSupportedDataType(sqrt_node);
     if (!graph_utils::IsSupportedOptypeVersionAndDomain(sqrt_node, "Sqrt", {6}) ||
-            sqrt_node.GetExecutionProviderType() != reduce_mean_node.GetExecutionProviderType() ||
+        sqrt_node.GetExecutionProviderType() != reduce_mean_node.GetExecutionProviderType() ||
         sqrt_node.GetOutputEdgesCount() != 1 ||
         !IsSupportedDataType(sqrt_node)) {
       std::cout << "sqrt check failed. \n";
@@ -168,7 +170,7 @@ Status LayerNormFusion::ApplyImpl(Graph& graph, bool& modified, int graph_level)
     nodes_to_remove.push_back(sqrt_node);
     std::cout << "sqrt found\n";
 
-	// add --> sqrt
+    // add --> sqrt
     Node& add2_node = *graph.GetNode(sqrt_node.InputNodesBegin()->Index());
     std::cout << "Matches Op " << graph_utils::MatchesOpSinceVersion(add2_node, {7}) << "\n";
     std::cout << "node.op since version " << add2_node.Op()->SinceVersion() << "\n";
@@ -181,10 +183,10 @@ Status LayerNormFusion::ApplyImpl(Graph& graph, bool& modified, int graph_level)
     nodes_to_remove.push_back(add2_node);
     std::cout << "add2 found\n";
 
-	// reduceMean --> add
+    // reduceMean --> add
     const Node* p_reduce_mean2 = nullptr;
-    
-	p_reduce_mean2 = first_parent_by_type(add2_node, "ReduceMean");
+
+    p_reduce_mean2 = first_parent_by_type(add2_node, "ReduceMean");
     Node& reduce_mean2_node = *graph.GetNode(p_reduce_mean2->Index());
     std::cout << "Matches Op " << graph_utils::MatchesOpSinceVersion(reduce_mean2_node, {1}) << "\n";
     std::cout << "node.op since version " << reduce_mean2_node.Op()->SinceVersion() << "\n";
@@ -197,7 +199,7 @@ Status LayerNormFusion::ApplyImpl(Graph& graph, bool& modified, int graph_level)
     nodes_to_remove.push_back(reduce_mean2_node);
     std::cout << "reduce mean found\n";
 
-	// pow --> reduceMean
+    // pow --> reduceMean
     Node& pow_node = *graph.GetNode(reduce_mean2_node.InputNodesBegin()->Index());
     if (!graph_utils::IsSupportedOptypeVersionAndDomain(pow_node, "Pow", {7}) ||
         pow_node.GetExecutionProviderType() != reduce_mean_node.GetExecutionProviderType() ||
@@ -208,26 +210,25 @@ Status LayerNormFusion::ApplyImpl(Graph& graph, bool& modified, int graph_level)
     nodes_to_remove.push_back(pow_node);
     std::cout << "pow found";
 
-	// sub --> pow
+    // sub --> pow
     const Node* p_sub2_node = first_parent_by_type(pow_node, "Sub");
-    Node& sub2_node = *graph.GetNode(p_sub2_node ->Index());
+    Node& sub2_node = *graph.GetNode(p_sub2_node->Index());
     if (!graph_utils::IsSupportedOptypeVersionAndDomain(sub2_node, "Sub", {7}) ||
         sub2_node.GetExecutionProviderType() != reduce_mean_node.GetExecutionProviderType() ||
-        sub2_node.GetOutputEdgesCount() != 1 ||
         !IsSupportedDataType(sub2_node)) {
       continue;
     }
     nodes_to_remove.push_back(sub2_node);
     std::cout << "sub2 found\n";
 
-	// add --> sub
+    // add --> sub
     const Node* p_reduce_mean_check = first_parent_by_type(sub2_node, "ReduceMean");
     if (p_reduce_mean_check == nullptr || p_reduce_mean_check != &reduce_mean_node) {
       continue;
     }
     std::cout << "is the same add!!!";
 
-	// div --> mul
+    // div --> mul
     Node& mul_node = *graph.GetNode(div_node.OutputNodesBegin()->Index());
     if (!graph_utils::IsSupportedOptypeVersionAndDomain(mul_node, "Mul", {7}) ||
         mul_node.GetExecutionProviderType() != reduce_mean_node.GetExecutionProviderType() ||
@@ -238,7 +239,7 @@ Status LayerNormFusion::ApplyImpl(Graph& graph, bool& modified, int graph_level)
     nodes_to_remove.push_back(mul_node);
     std::cout << "Mul found (last).\n";
 
-	// mul --> add
+    // mul --> add
     Node& last_add_node = *graph.GetNode(mul_node.OutputNodesBegin()->Index());
     if (!graph_utils::IsSupportedOptypeVersionAndDomain(last_add_node, "Add", {7}) ||
         last_add_node.GetExecutionProviderType() != reduce_mean_node.GetExecutionProviderType() ||
@@ -249,15 +250,34 @@ Status LayerNormFusion::ApplyImpl(Graph& graph, bool& modified, int graph_level)
     nodes_to_remove.push_back(last_add_node);
     std::cout << "Add found (last).\n";
 
-    const std::vector<NodeArg*> layer_norm_input_defs{reduce_mean_node.MutableInputDefs()[0], 
-		mul_node.MutableInputDefs()[1], 
-		last_add_node.MutableInputDefs()[1]
-	};
+	// Get the inputs for the new layernorm node
+	NodeArg* scale = nullptr;
+    NodeArg* bias = nullptr;
+    for (int i = 0; i < mul_node.MutableInputDefs().size(); i++) {
+	  if (graph_utils::NodeArgIsConstant(graph, *(mul_node.MutableInputDefs()[i])) || 
+		  graph_utils::IsGraphInput(graph, mul_node.MutableInputDefs()[i])) {
+        scale = mul_node.MutableInputDefs()[i];
+	  }
+	}
+
+	for (int i = 0; i < last_add_node.MutableInputDefs().size(); i++) {
+      if (graph_utils::NodeArgIsConstant(graph, *(last_add_node.MutableInputDefs()[i])) ||
+		  graph_utils::IsGraphInput(graph, last_add_node.MutableInputDefs()[i])) {
+        bias = mul_node.MutableInputDefs()[i];
+	  }
+    }
+    if (scale == nullptr || bias == nullptr) {
+      std::cout << "Cannot find inputs for the new node\n";
+		continue;
+	}
+    const std::vector<NodeArg*> layer_norm_input_defs{reduce_mean_node.MutableInputDefs()[0],
+                                                    scale,
+                                                    bias};
     Node& layer_norm_node = graph.AddNode(graph.GenerateNodeName("LayerNormalization"),
-                                    "LayerNormalization",
-                                    "fused LayerNorm subgraphs ",
-                                    layer_norm_input_defs,
-                                    {}, {}, kOnnxDomain);
+                                          "LayerNormalization",
+                                          "fused LayerNorm subgraphs ",
+                                          layer_norm_input_defs,
+                                          {}, {}, kOnnxDomain);
 
     // Assign provider to this new node. Provider should be same as the provider for old node.
     layer_norm_node.SetExecutionProviderType(reduce_mean_node.GetExecutionProviderType());
--- onnxruntime/test/optimizer/graph_transform_test.cc
+++ onnxruntime/test/optimizer/graph_transform_test.cc
@@ -833,7 +833,28 @@ TEST(GraphTransformationTests, LayerNormFusionTest) {
 
   std::map<std::string, int> op_to_count = CountOpsInGraph(graph);
   ASSERT_TRUE(op_to_count["Div"] == 0);
-  ASSERT_TRUE(op_to_count["Add"] == 1);
+  ASSERT_TRUE(op_to_count["Add"] == 0);
+  ASSERT_TRUE(op_to_count["Sub"] == 0);
+  ASSERT_TRUE(op_to_count["ReduceMean"] == 0);
+  ASSERT_TRUE(op_to_count["Pow"] == 0);
+  ASSERT_TRUE(op_to_count["Sqrt"] == 0);
+  ASSERT_TRUE(op_to_count["LayerNormalization"] == 1);
+}
+
+TEST(GraphTransformationTests, LayerNormWithSubDupFusionTest) {
+  string model_uri = MODEL_FOLDER + "fusion/layer_norm_sub_dup.onnx";
+  std::shared_ptr<Model> p_model;
+  ASSERT_TRUE(Model::Load(model_uri, p_model).IsOK());
+  Graph& graph = p_model->MainGraph();
+
+  onnxruntime::GraphTransformerManager graph_transformation_mgr{5};
+  graph_transformation_mgr.Register(onnxruntime::make_unique<LayerNormFusion>(), TransformerLevel::Level2);
+  auto ret = graph_transformation_mgr.ApplyTransformers(graph, TransformerLevel::Level2);
+  ASSERT_TRUE(ret.IsOK());
+
+  std::map<std::string, int> op_to_count = CountOpsInGraph(graph);
+  ASSERT_TRUE(op_to_count["Div"] == 0);
+  ASSERT_TRUE(op_to_count["Add"] == 0);
   ASSERT_TRUE(op_to_count["Sub"] == 0);
   ASSERT_TRUE(op_to_count["ReduceMean"] == 0);
   ASSERT_TRUE(op_to_count["Pow"] == 0);
