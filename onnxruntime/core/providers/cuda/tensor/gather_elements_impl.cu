05af169e8a08c0120992ae5c8f777fb448127b1e
hariharans29 <shariharan91@gmail.com>
2019-11-06 11:51:50 -0800

    Fix build

--- onnxruntime/core/providers/cuda/tensor/gather_elements_impl.cu
+++ onnxruntime/core/providers/cuda/tensor/gather_elements_impl.cu
@@ -92,6 +92,32 @@ void GatherElementsImpl(
   }
 }
 
+template void GatherElementsImpl<int32_t>(
+    const int64_t rank,
+    const void* input_data,
+    const int64_t input_size,
+    const int64_t input_dim_along_axis,
+    const int64_t* input_strides,
+    const int32_t* indices_data,
+    const int64_t indices_size,
+    const fast_divmod* indices_strides,
+    const int64_t axis,
+    void* output_data,
+    size_t element_size);
+
+template void GatherElementsImpl<int64_t>(
+    const int64_t rank,
+    const void* input_data,
+    const int64_t input_size,
+    const int64_t input_dim_along_axis,
+    const int64_t* input_strides,
+    const int64_t* indices_data,
+    const int64_t indices_size,
+    const fast_divmod* indices_strides,
+    const int64_t axis,
+    void* output_data,
+    size_t element_size);
+
 }  // namespace cuda
 }  // namespace onnxruntime
 
