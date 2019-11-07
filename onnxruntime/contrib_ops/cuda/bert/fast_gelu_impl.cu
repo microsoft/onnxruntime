1ae3e0b82865f1b04b6bcd381ed20b8174e4b366
Tianlei Wu <tlwu@microsoft.com>
2019-11-07 15:07:52 -0800

    move _Tanh to common.cuh

--- onnxruntime/contrib_ops/cuda/bert/fast_gelu_impl.cu
+++ onnxruntime/contrib_ops/cuda/bert/fast_gelu_impl.cu
@@ -24,8 +24,6 @@ limitations under the License.
 #include "core/providers/cuda/cuda_common.h"
 #include "core/providers/cuda/cu_inc/common.cuh"
 #include "core/providers/cuda/shared_inc/cuda_call.h"
-#include <cuda_fp16.h>
-#include <cublas_v2.h>
 #include "fast_gelu_impl.h"
 
 using namespace onnxruntime::cuda;
@@ -41,23 +39,6 @@ constexpr float B = 0.7978845608028654;  // sqrt(2.0/M_PI)
 
 constexpr float C = 0.035677408136300125;  // 0.044715 * sqrt(2.0/M_PI)
 
-__device__ inline float Tanh(const float& x) {
-  return tanhf(x);
-}
-
-__device__ inline half Tanh(const half& x) {
-  const float tmp = tanhf(__half2float(x));
-  return __float2half(tmp);
-}
-
-__device__ inline half2 Tanh(const half2& x) {
-  // at the moment, there is no half2 tanh builtin
-  float2 tmp = (__half22float2(x));
-  tmp.x = tanhf(tmp.x);
-  tmp.y = tanhf(tmp.y);
-  return __float22half2_rn(tmp);
-}
-
 template <typename T, unsigned TPB>
 __global__ void FastGeluKernel(const T a, const T b, const T c, int input_length, int bias_length, const T* input, const T* bias, T* output) {
   const int idx = blockIdx.x * TPB + threadIdx.x;
@@ -65,20 +46,21 @@ __global__ void FastGeluKernel(const T a, const T b, const T c, int input_length
   if (idx < input_length) {
     const T x = input[idx];
     const T in = (bias == nullptr) ? x : (x + bias[idx % bias_length]);
-    const T cdf = a + a * Tanh(in * (c * in * in + b));
+    const T cdf = a + a * _Tanh(in * (c * in * in + b));
     output[idx] = in * cdf;
   }
 }
 
 template <unsigned TPB>
 __global__ void FastGeluKernel2(const half2 a, const half2 b, const half2 c, int input_length, int bias_length, const half2* input, const half2* bias, half2* output) {
-// half2 Arithmetic functions requires cuda architecture >= 5.3
+// half2 arithmetic functions requires cuda architecture >= 5.3
 #if __CUDA_ARCH__ >= 530
   const int idx = blockIdx.x * TPB + threadIdx.x;
+
   if (idx < input_length) {
     const half2 x = input[idx];
     const half2 in = (bias == nullptr) ? x : (x + bias[idx % bias_length]);
-    const half2 cdf = a + a * Tanh(in * (c * in * in + b));
+    const half2 cdf = a + a * _Tanh(in * (c * in * in + b));
     output[idx] = in * cdf;
   }
 #endif
