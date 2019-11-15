5380a3fc478e2099d63057200a1304e65137bada
Manohar Karlapalem <manohar.karlapalem@intel.com>
2019-11-15 15:12:32 -0800

    Backend logic cleanup
    Cleans the backend(intel_graph.*) code in the following ways:-
    
    1. Minimize global usage: Since all the IR graphs need to be
    re-generated on every Infer, it is bad practice to rely on globals
    for their saving and usage as there would be multiple readers and
    writers to the same global variable leading to incorrect usages or
    contentions. This change replaces globals with locals where possible.
     This change also fixes an existing bug with due to
    incorrect global usage.
    
    2. Remove all unused functions.
    
    3. Remove all unused headers and prepocessor directives.

--- onnxruntime/core/providers/intel/intel_graph.h
+++ onnxruntime/core/providers/intel/intel_graph.h
@@ -2,34 +2,12 @@
 // Licensed under the MIT License
 
 #pragma once
-#if defined(_MSC_VER)
-#pragma warning(disable : 4244 4245)
-#elif __GNUC__
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wunused-parameter"
-#endif
-#include <ngraph/ngraph.hpp>
-#if defined(_MSC_VER)
-#pragma warning(default : 4244 4245)
-#elif __GNUC__
-#pragma GCC diagnostic pop
-#endif
 
-#include "core/session/onnxruntime_c_api.h"
-#include "core/framework/func_api.h"
-#include "core/graph/onnx_protobuf.h"
-
-#include <map>
 #include <memory>
 
 #include <inference_engine.hpp>
-#include <ie_builders.hpp>
-#include <cpp/ie_infer_request.hpp>
-//#include "intel_custom_op.h"
 
-#include "core/framework/func_api.h"
 #include "core/session/onnxruntime_cxx_api.h"
-#include "core/graph/graph.h"
 
 namespace onnxruntime {
 namespace intel_ep {
@@ -40,63 +18,32 @@ class IntelGraph {
 
   void Infer(const ONNX_NAMESPACE::ModelProto& model_proto, Ort::CustomOpApi ort, OrtKernelContext* context);
 
-  static void ConvertONNXModelToIntelIR(const std::string& onnx_model, std::string& intel_xml, std::string& intel_bin, bool precision_fp32);
-
   static const std::string log_tag;
-  void CreateNGraphFunc(const ONNX_NAMESPACE::ModelProto& model_proto);  //, Ort::CustomOpApi api, OrtKernelContext* context) const;
-
-  //static std::shared_ptr<InferenceEngine::CNNNetwork> cnetwork;
-  static InferenceEngine::CNNNetwork cnetwork;
 
  private:
-  mutable std::shared_ptr<ngraph::runtime::Executable> ng_curr_exe_ = nullptr;
-
-  //AllocateFunc allocate_func_ = nullptr;
-
-  //DestroyFunc release_func_ = nullptr;
-
-  //AllocatorHandle allocator_ = nullptr;
-
-  std::string name_ = "test";
-  mutable std::unordered_map<std::string, std::shared_ptr<ngraph::runtime::Executable>> ng_exe_map_;
-  mutable std::list<std::string> keyCache;
-
-  mutable std::mutex compute_lock_;
-
-  mutable ONNX_NAMESPACE::ModelProto model_proto_;
-
-  std::shared_ptr<InferenceEngine::CNNNetwork> BuildIntelNetworkWithMO();
+  std::shared_ptr<InferenceEngine::CNNNetwork> CreateCNNNetwork(const ONNX_NAMESPACE::ModelProto& model_proto);
 
   InferenceEngine::Precision ConvertPrecisionONNXToIntel(ONNX_NAMESPACE::DataType onnx_type);
 
-  void GetExecutableHandle(
-      std::shared_ptr<InferenceEngine::CNNNetwork> network);
+  void SetIODefs(std::shared_ptr<InferenceEngine::CNNNetwork> network);
 
-  size_t DeduceBatchSize(Ort::CustomOpApi ort, const OrtValue* input_tensor,
-                         InferenceEngine::SizeVector graph_dims);
+  size_t DeduceBatchSize(Ort::CustomOpApi ort, const OrtValue* input_tensor, InferenceEngine::SizeVector graph_dims);
 
-  void GetInputTensors(Ort::CustomOpApi ort, OrtKernelContext* context, const OrtValue* input_tensors[]);
+  void GetInputTensors(Ort::CustomOpApi ort, OrtKernelContext* context, const OrtValue* input_tensors[], std::shared_ptr<InferenceEngine::CNNNetwork> ie_cnn_network);
 
-  void GetOutputTensors(Ort::CustomOpApi ort, OrtKernelContext* context, OrtValue* output_tensors[], size_t batch_size);
+  void GetOutputTensors(Ort::CustomOpApi ort, OrtKernelContext* context, OrtValue* output_tensors[], size_t batch_size, std::vector<InferenceEngine::InferRequest::Ptr>& infer_requests, std::shared_ptr<InferenceEngine::CNNNetwork> ie_cnn_network);
 
-  void StartAsyncInference(Ort::CustomOpApi ort, const OrtValue* input_tensors[], size_t batch_slice_idx, size_t infer_req_idx);
+  void StartAsyncInference(Ort::CustomOpApi ort, const OrtValue* input_tensors[], size_t batch_slice_idx, size_t infer_req_idx, std::vector<InferenceEngine::InferRequest::Ptr>& infer_requests, std::shared_ptr<InferenceEngine::CNNNetwork> ie_cnn_network);
 
-  void CompleteAsyncInference(Ort::CustomOpApi ort, OrtValue* output_tensors[], size_t batch_slice_idx, size_t infer_req_idx);
+  void CompleteAsyncInference(Ort::CustomOpApi ort, OrtValue* output_tensors[], size_t batch_slice_idx, size_t infer_req_idx, std::vector<InferenceEngine::InferRequest::Ptr>& infer_requests, std::shared_ptr<InferenceEngine::CNNNetwork> ie_cnn_network);
 
-  std::vector<std::string> GetEnvLdLibraryPath() const;
-
+  std::string name_ = "test";
   const onnxruntime::Node* fused_node_;
-  std::shared_ptr<InferenceEngine::CNNNetwork> intel_network_;
   size_t num_inf_reqs_;
-  IE_SUPPRESS_DEPRECATED_START
-  InferenceEngine::InferencePlugin plugin_;
-  IE_SUPPRESS_DEPRECATED_END
-  std::vector<InferenceEngine::InferRequest::Ptr> infer_requests_;
+  mutable std::mutex compute_lock_;
   std::string device_id_;
-  //  mutable std::mutex compute_lock_;
   std::vector<int> input_indexes_;
   InferenceEngine::Precision precision_;
-  const onnxruntime::Graph* onnx_graph_;
 };
 }  // namespace intel_ep
 }  // namespace onnxruntime
