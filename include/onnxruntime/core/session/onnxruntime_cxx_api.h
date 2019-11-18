2155f5b317106727d2a982e99d741ed2a56bc081
Shah Asaduzzaman <shahasad@microsoft.com>
2019-11-18 19:56:51 +0000

    use a stub ortapi

--- include/onnxruntime/core/session/onnxruntime_cxx_api.h
+++ include/onnxruntime/core/session/onnxruntime_cxx_api.h
@@ -44,13 +44,15 @@ struct Global {
   static const OrtApi& api_;
 };
 
-template <typename T>
-const OrtApi& Global<T>::api_ =
 #ifdef EXCLUDE_REFERENCE_TO_ORT_DLL
-    *(static_cast<OrtApi*>(0xDEADBEEFul));
+OrtApi stub_api;
+template <typename T>
+const OrtApi& Global<T>::api_ = stub_api;
 #else
-    *OrtGetApiBase()->GetApi(ORT_API_VERSION);
+template <typename T>
+const OrtApi& Global<T>::api_ = *OrtGetApiBase()->GetApi(ORT_API_VERSION);
 #endif
+
 
 // This returns a reference to the OrtApi interface in use, in case someone wants to use the C API functions
 inline const OrtApi& GetApi() { return Global<void>::api_; }
