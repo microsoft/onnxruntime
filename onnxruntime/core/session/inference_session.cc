b0200319f7c666856fea2928da0ec66844dad5ba
Scott McKay <skottmckay@gmail.com>
2019-11-21 07:58:47 +1000

    Remove temporary change

--- onnxruntime/core/session/inference_session.cc
+++ onnxruntime/core/session/inference_session.cc
@@ -838,9 +838,7 @@ Status InferenceSession::Run(const RunOptions& run_options, const std::vector<st
 
   // info all execution providers InferenceSession:Run ended
   for (auto& xp : execution_providers_) {
-    auto s = xp->OnRunEnd();
-    ORT_CHECK_AND_SET_RETVAL(s);
-    //ORT_CHECK_AND_SET_RETVAL(xp->OnRunEnd());
+    ORT_CHECK_AND_SET_RETVAL(xp->OnRunEnd());
   }
 
   --current_num_runs_;
