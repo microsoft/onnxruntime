e84986992cfe84ea7662480f85ee4eeaf5d80061
Yang Chen <yanchen@microsoft.com>
2019-11-20 17:13:53 -0800

    don't rely on pointer values for iterating std::set
    Previously, openvino relied on iterating a set of NodeArg pointers
    to construct inputs and outputs for a fused graph. It could cause
    non-determinism. The reason was that although iterating std::set by
    itself is stable, pointer values of NodeArgs may vary. Consequently,
    we could end up visiting the set's elements in different orders for
    different runs for the same test, which resulted in constructing
    inputs (and outputs) with different orders to the fused graph.
    For example, for the same test, we may have inputs [A, B] in some
    runs but inputs[B, A] in others.
    
    Let's use std::string as the key type to avoid such nondeterminism.
    
    This commit also added implicit inputs into meta->inputs while returning
    the capability from the openvino provider.

--- onnxruntime/core/providers/openvino/openvino_execution_provider.cc
+++ onnxruntime/core/providers/openvino/openvino_execution_provider.cc
@@ -488,7 +488,15 @@ std::vector<std::unique_ptr<ComputeCapability>> OpenVINOExecutionProvider::GetCa
 
   auto model_proto = GetModelProtoFromFusedNode(graph_viewer);
 
-  std::set<const onnxruntime::NodeArg *> fused_inputs, fused_outputs;
+  // Make sure *not* to use a pointer type for the key, which can cause nondeterminism
+  // for iterating the set elements. The reason is that although iterating std::set by
+  // itself is stable, pointer values of NodeArgs may vary. Consequently, we may end up
+  // visiting the set's elements in different orders for different runs for the same test,
+  // which will result in constructing inputs (and outputs) with different orders to
+  // the fused graph. For example, for the same test, we may have inputs [A, B] in some
+  // runs but inputs[B, A] in others.
+  // Let's use std::string as the key type to avoid such nondeterminism.
+  std::set<std::string> fused_inputs, fused_outputs;
 
   try {
     CheckGraphSupported(graph_viewer, device_id);
@@ -517,21 +525,30 @@ std::vector<std::unique_ptr<ComputeCapability>> OpenVINOExecutionProvider::GetCa
     sub_graph->nodes.push_back(index);
     const auto node = graph_viewer.GetNode(index);
 
-    // Track graph inputs and initializers
-    for (const auto& input_def : node->InputDefs()) {
-      if (fused_outputs.find(input_def) == fused_outputs.end()) {
-        fused_inputs.insert(input_def);
-      } else {
-        fused_outputs.erase(input_def);
-      }
-    }
+    auto process_input_fn =
+      [&fused_inputs, &fused_outputs, &node](const onnxruntime::NodeArg& input_def, size_t) {
+        const auto& name = input_def.Name();
+        if (fused_outputs.find(name) == fused_outputs.end()) {
+          fused_inputs.insert(name);
+        } else {
+          fused_outputs.erase(name);
+        }
+        return Status::OK();
+      };
+
+    // Track graph inputs (both explicit and implicit) and initializers
+    node->ForEachWithIndex(node->InputDefs(), process_input_fn);
+    // Implicit inputs will be promoted to be explicit inputs of the fused
+    // graph later by ORT.
+    node->ForEachWithIndex(node->ImplicitInputDefs(), process_input_fn);
 
     // Track graph outputs
     for (const auto& output_def : node->OutputDefs()) {
-      if (fused_inputs.find(output_def) == fused_inputs.end()) {
-        fused_outputs.insert(output_def);
+      const auto& name = output_def->Name();
+      if (fused_inputs.find(name) == fused_inputs.end()) {
+        fused_outputs.insert(name);
       } else {
-        fused_inputs.erase(output_def);
+        fused_inputs.erase(name);
       }
     }
   }
@@ -553,12 +570,12 @@ std::vector<std::unique_ptr<ComputeCapability>> OpenVINOExecutionProvider::GetCa
   meta_def->domain = "OpenVINO";
   meta_def->since_version = 1;
 
-  for (auto input : fused_inputs) {
-    meta_def->inputs.push_back(input->Name());
+  for (const auto& name : fused_inputs) {
+    meta_def->inputs.push_back(name);
   }
 
-  for (auto output : fused_outputs) {
-    meta_def->outputs.push_back(output->Name());
+  for (const auto& name : fused_outputs) {
+    meta_def->outputs.push_back(name);
   }
 
   sub_graph->SetMetaDef(meta_def);
