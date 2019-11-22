5deb0861d1f8d4564d3a27a050963b8fc5b060b1
Tiago Koji Castro Shibata <ticastro@microsoft.com>
2019-11-21 17:28:02 -0800

    Add scenario tests

--- cmake/winml_unittests.cmake
+++ cmake/winml_unittests.cmake
@@ -65,6 +65,17 @@ add_winml_test(
 )
 target_precompiled_header(winml_test_api testPch.h)
 
+file(GLOB winml_test_scenario_src CONFIGURE_DEPENDS "${WINML_TEST_SRC_DIR}/scenario/cppwinrt/*.cpp")
+add_winml_test(
+  TARGET winml_test_scenario
+  SOURCES ${winml_test_scenario_src}
+  LIBS winml_test_common onnxruntime_providers_dml
+)
+target_precompiled_header(winml_test_scenario testPch.h)
+set_target_properties(winml_test_scenario PROPERTIES LINK_FLAGS
+  "/DELAYLOAD:d2d1.dll /DELAYLOAD:d3d11.dll /DELAYLOAD:dxgi.dll"
+)
+
 # During build time, copy any modified collaterals.
 # configure_file(source destination COPYONLY), which configures CMake to copy the file whenever source is modified,
 # can't be used here because we don't know the destination during configure time (in multi-configuration generators,
@@ -88,3 +99,4 @@ add_winml_collateral("${WINML_TEST_SRC_DIR}/collateral/images/*.png")
 add_winml_collateral("${WINML_TEST_SRC_DIR}/collateral/models/*.onnx")
 add_winml_collateral("${WINML_TEST_SRC_DIR}/common/testdata/squeezenet/*")
 add_winml_collateral("${WINML_TEST_SRC_DIR}/scenario/cppwinrt/*.onnx")
+add_winml_collateral("${WINML_TEST_SRC_DIR}/scenario/models/*.onnx")
