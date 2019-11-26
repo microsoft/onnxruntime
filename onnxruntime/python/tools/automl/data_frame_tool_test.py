0b6b493bc702b41e05c9c0c50a2d6b7c3e3fab13
Dmitri Smirnov <dmitrism@microsoft.com>
2019-11-26 15:20:11 -0800

    Rework the class. Add proper docs.   Add model creation and tests for all the types.   Unsigned types apparently not supported.

--- onnxruntime/python/tools/automl/data_frame_tool_test.py
+++ onnxruntime/python/tools/automl/data_frame_tool_test.py
@@ -7,32 +7,29 @@ import argparse
 import onnxruntime as onnxrt
 import numpy as np
 import pandas as pd
-from feed_inputs import DataFrameTool
+from data_frame_tool import DataFrameTool
 import os
 import sys
 
 def main():
     parser = argparse.ArgumentParser(description='Test Feed Inputs utility')
     parser.add_argument('model_path', help='model path')
-    parser.add_argument('-debug', action='store_true',
-                        help='pause execution to allow attaching a debugger.')
     parser.add_argument('-profile', action='store_true',
                         help='enable chrome timeline trace profiling.')
     args = parser.parse_args()
 
-    # Load model
+    # Create options and the tool
     sess_options = onnxrt.SessionOptions()
     sess_options.enable_profiling = args.profile
 
-    sess = onnxrt.InferenceSession(args.model_path, sess_options)
+    df_tool = DataFrameTool(args.model_path, sess_options)
 
     # Create a DataFrame that holds 3 inputs, string, bool, float in their respective columns
-    df = pd.DataFrame([['string_input', True, np.float32(0.25)]], index=[0], columns=['F2', 'Label', 'F1'])
+    df = pd.DataFrame([['string_input', 3.25, 8, 16, 32, 64, True, 0.25]], 
+                      columns=['StringInput', 'DoubleInput', 'Int8Input', 'Int16Input', 'Int32Input', 'Int64Input', 'BoolInput', 'Float32Input'])
 
-    feed_helper = DataFrameTool(sess)
-    feeds = feed_helper.feed_nputs(df)
-    sess.run([], feeds)
-    print('Run complete')
+    outputs = df_tool.execute(df)
+    print('Outputs: ', outputs)
 
 if __name__ == "__main__":
     sys.exit(main())
