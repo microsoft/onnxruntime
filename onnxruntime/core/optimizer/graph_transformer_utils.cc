e57b735bb9a4912eb38f9de9d272d42c03779569
Tianlei Wu <tlwu@microsoft.com>
2019-11-27 10:15:50 -0800

    Add a transformer to use Gelu approximation for cuda provider (#2480)
    * Add Gelu Approximation Transformer to convert Gelu or AddGeluFusion to FastGelu to get better inference performance.

--- onnxruntime/core/optimizer/graph_transformer_utils.cc
+++ onnxruntime/core/optimizer/graph_transformer_utils.cc
@@ -20,6 +20,7 @@
 #include "core/optimizer/free_dim_override_transformer.h"
 #include "core/optimizer/add_gelu_fusion.h"
 #include "core/optimizer/gelu_fusion.h"
+#include "core/optimizer/gelu_approximation.h"
 #include "core/optimizer/layer_norm_fusion.h"
 #include "core/optimizer/skip_layer_norm_fusion.h"
 #include "core/optimizer/reshape_fusion.h"
@@ -130,6 +131,7 @@ std::vector<std::unique_ptr<GraphTransformer>> GenerateTransformers(TransformerL
 
       std::unordered_set<std::string> cuda_execution_providers = {onnxruntime::kCudaExecutionProvider};
       transformers.emplace_back(onnxruntime::make_unique<AddGeluFusion>(cuda_execution_providers));
+      transformers.emplace_back(onnxruntime::make_unique<GeluApproximation>(cuda_execution_providers));
       transformers.emplace_back(onnxruntime::make_unique<SkipLayerNormFusion>(cuda_execution_providers));
 
 #endif
@@ -142,7 +144,6 @@ std::vector<std::unique_ptr<GraphTransformer>> GenerateTransformers(TransformerL
         transformers.emplace_back(onnxruntime::make_unique<NchwcTransformer>());
       }
 #endif
-
     } break;
 
     default:
