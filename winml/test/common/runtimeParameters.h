9169c95a0ed234ae8e24550e7024b641ace4b226
Tiago Koji Castro Shibata <tiago.shibata@gmail.com>
2019-11-27 10:33:00 -0800

    Add CLI parameters to test runner, build WinML in ARM and x86 CI (#2479)
    * Support test parameters through CLI arguments
    
    * Add WinML do Windows x86/ARM CI builds
    
    * Code style fixes
    
    * Update googletest
    
    Remove GPUTEST macros everywhere now that GTEST_SKIP is supported
    
    * Refactor main.cpp
    
    * Build scenario tests without DML

---
+++ winml/test/common/runtimeParameters.h
@@ -0,0 +1,9 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+#pragma once
+namespace RuntimeParameters
+{
+    // Runtime parameters passed through CLI arguments
+    extern std::unordered_map<std::string, std::string> Parameters;
+}
