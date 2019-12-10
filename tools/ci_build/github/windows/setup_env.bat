90a2da17102078ff229a5ec424202a98ffa87639
Changming Sun <chasun@microsoft.com>
2019-12-10 13:46:32 -0800

    update

--- tools/ci_build/github/windows/setup_env.bat
+++ tools/ci_build/github/windows/setup_env.bat
@@ -1,1 +1,1 @@
-set PATH=%BUILD_BINARIESDIRECTORY%\packages\python;%BUILD_BINARIESDIRECTORY%\packages\python\DLLs;%BUILD_BINARIESDIRECTORY%\packages\python\Library\bin;%BUILD_BINARIESDIRECTORY%\packages\python\script;%PATH%
+set PATH=C:\azcopy;%BUILD_BINARIESDIRECTORY%\packages\python;%BUILD_BINARIESDIRECTORY%\packages\python\DLLs;%BUILD_BINARIESDIRECTORY%\packages\python\Library\bin;%BUILD_BINARIESDIRECTORY%\packages\python\script;%PATH%
