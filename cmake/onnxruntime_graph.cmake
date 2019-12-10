a680d1800dcdaecda74b58d9ad5f0c77b31dea88
Dmitri Smirnov <dmitrism@microsoft.com>
2019-12-09 16:36:58 -0800

    Fix compile issue in automl_defs   Make sure we do not compile automl_defs when automl is disabled.

--- cmake/onnxruntime_graph.cmake
+++ cmake/onnxruntime_graph.cmake
@@ -15,10 +15,13 @@ if (onnxruntime_DISABLE_CONTRIB_OPS)
 endif()
 
 if(NOT onnxruntime_USE_AUTOML)
-  list(REMOVE_ITEM onnxruntime_graph_src
+  file(GLOB_RECURSE automl_to_remove_graph_src
     "${ONNXRUNTIME_ROOT}/core/graph/automl_ops/*.h"
     "${ONNXRUNTIME_ROOT}/core/graph/automl_ops/*.cc"
     )
+  foreach(I in ${automl_to_remove_graph_src})
+    list(REMOVE_ITEM onnxruntime_graph_src ${I})
+  endforeach()
 endif()
 
 if(NOT onnxruntime_USE_DML)
