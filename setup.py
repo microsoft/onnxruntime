7cf82acc2d4265402f92336a9af684febdda6f5e
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2019-12-16 10:51:16 +0100

    Add 3.8 classifer

--- setup.py
+++ setup.py
@@ -230,5 +230,7 @@ setup(
         'Programming Language :: Python :: 3 :: Only',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7'],
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
     )
