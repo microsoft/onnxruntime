29eab92158e61f5684891a50591bffc54673d282
RyanUnderhill <ryanhill@microsoft.com>
2019-12-19 16:56:12 -0800

    Update to v1.1.0

--- python/_static/documentation_options.js
+++ python/_static/documentation_options.js
@@ -1,10 +1,9 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.5.9994',
+    VERSION: '1.1.0',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
     HAS_SOURCE: true,
-    SOURCELINK_SUFFIX: '.txt',
-    NAVIGATION_WITH_KEYS: false
+    SOURCELINK_SUFFIX: '.txt'
 };
