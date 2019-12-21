204214f6aca0bb241a3e3d9d05ae0354f97b3c39
RyanUnderhill <ryanhill@microsoft.com>
2019-12-20 16:59:21 -0800

    Update Python docs to v1.1.0

--- python/_static/documentation_options.js
+++ python/_static/documentation_options.js
@@ -3,7 +3,9 @@ var DOCUMENTATION_OPTIONS = {
     VERSION: '1.1.0',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
+    BUILDER: 'html',
     FILE_SUFFIX: '.html',
     HAS_SOURCE: true,
-    SOURCELINK_SUFFIX: '.txt'
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false
 };
