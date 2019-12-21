959a525b02caa6119fed5931033129f6cbc0f690
Xiang Zhang <xianz@microsoft.com>
2019-12-20 16:09:39 -0800

    add comment

--- onnxruntime/core/session/inference_session.h
+++ onnxruntime/core/session/inference_session.h
@@ -511,6 +511,7 @@ class InferenceSession {
   std::string event_name_;                                          // where the model is loaded from: ["model_loading_uri", "model_loading_proto", "model_loading_istream"]
 
   TimePoint time_sent_last_evalutation_start_;
+                                                                      // Event Rate per provider < 20 peak events per second
   const long long kDurationBetweenSendingEvaluationStart = 1000 * 50; // duration in (us). send a EvaluationStop Event every 50 ms;
   bool isEvaluationStart = false;
 
