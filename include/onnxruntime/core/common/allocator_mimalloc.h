f6b70e9ac6351d15b7f5b4fe5efae80ef89c079b
Kirsten Lee <kile@microsoft.com>
2020-01-03 11:14:44 -0800

    polish alloctor_mimalloc

--- include/onnxruntime/core/common/allocator_mimalloc.h
+++ include/onnxruntime/core/common/allocator_mimalloc.h
@@ -7,41 +7,27 @@ template <class T>
 struct allocator_mimalloc {
   typedef T value_type;
   
-  using propagate_on_container_copy_assignment = std::true_type; // for consistency
-	using propagate_on_container_move_assignment = std::true_type; // to avoid the pessimization
-	using propagate_on_container_swap = std::true_type; // to avoid the undefined behavior
-
-	// to get the C++17 optimization: add this line for non-empty allocators which are always equal
+  using propagate_on_container_copy_assignment = std::true_type;
+	using propagate_on_container_move_assignment = std::true_type;
+	using propagate_on_container_swap = std::true_type;
 	using is_always_equal = std::true_type;
 
   allocator_mimalloc() noexcept {}
-  allocator_mimalloc(const allocator_mimalloc& other) noexcept {
-  }
-
+  allocator_mimalloc(const allocator_mimalloc& other) noexcept {}
   template <class U>
-  allocator_mimalloc(const allocator_mimalloc<U>& other) noexcept {
+  allocator_mimalloc(const allocator_mimalloc<U>& other) noexcept {}
+
+
+  T* allocate(std::size_t n, const void* hint = 0) {
+    return (T*)mi_mallocn(n, sizeof(T));
   }
 
-
-  T* allocate(std::size_t n, const void* hint = 0);
-  void deallocate(T* p, std::size_t n);
+  void deallocate(T* p, std::size_t n) {
+    mi_free(p);
+  }
 };
 
-template <class T>
-T* allocator_mimalloc<T>::allocate(std::size_t n, const void* hint) {
-  return (T*)mi_mallocn(n, sizeof(T));
-}
-
-template <class T>
-void allocator_mimalloc<T>::deallocate(T* p, std::size_t n) {
-  mi_free(p);
-}
-
 template <class T1, class T2>
-bool operator==(const allocator_mimalloc<T1>& lhs, const allocator_mimalloc<T2>& rhs) noexcept { 
-  return true; }
+bool operator==(const allocator_mimalloc<T1>& lhs, const allocator_mimalloc<T2>& rhs) noexcept { return true; }
 template <class T1, class T2>
 bool operator!=(const allocator_mimalloc<T1>& lhs, const allocator_mimalloc<T2>& rhs) noexcept { return false; }
-
-template <typename T>
-using Ty_Alloc = allocator_mimalloc<T>;
