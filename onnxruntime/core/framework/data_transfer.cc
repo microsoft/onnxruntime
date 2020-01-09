4cfecc56ab2f8552b1282664f8ef0bf726623f89
linkerzhang <kezhan@microsoft.com>
2020-01-09 11:26:02 -0800

    add interface to copy batch tensors.

--- onnxruntime/core/framework/data_transfer.cc
+++ onnxruntime/core/framework/data_transfer.cc
@@ -9,6 +9,13 @@ common::Status IDataTransfer::CopyTensor(const Tensor& src, Tensor& dst) const {
   return CopyTensor(src, dst, 0);
 }
 
+common::Status IDataTransfer::CopyTensors(const Tensor*& src, Tensor*& dst, int size) const {
+  for (int i = 0; i < size; ++i) {
+    ORT_RETURN_IF_ERROR(CopyTensor(src[i], dst[i], 0));
+  }
+  return Status::OK();
+}
+
 bool CPUDataTransfer::CanCopy(const OrtDevice& src_device, const OrtDevice& dst_device) const {
   return src_device.Type() == OrtDevice::CPU && dst_device.Type() == OrtDevice::CPU;
 }
