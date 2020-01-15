1feddabdcd7c501e046d0040cd45fd033fbb8777
Tiago Koji Castro Shibata <ticastro@microsoft.com>
2020-01-15 14:35:23 -0800

    Fix test warnings and delayload linking

--- cmake/winml.cmake
+++ cmake/winml.cmake
@@ -442,15 +442,15 @@ endif(onnxruntime_USE_DML)
 
 # The default libraries to link with in Windows are kernel32.lib;user32.lib;gdi32.lib;winspool.lib;shell32.lib;ole32.lib;oleaut32.lib;uuid.lib;comdlg32.lib;advapi32.lib
 # Remove them and use the onecore umbrella library instead
-set(CMAKE_C_STANDARD_LIBRARIES "onecoreuap_apiset.lib")
-set(CMAKE_CXX_STANDARD_LIBRARIES "onecoreuap_apiset.lib")
 foreach(default_lib kernel32.lib user32.lib gdi32.lib winspool.lib shell32.lib ole32.lib oleaut32.lib uuid.lib comdgl32.lib advapi32.lib)
   set(removed_libs "${removed_libs} /NODEFAULTLIB:${default_lib}")
 endforeach()
+set(CMAKE_C_STANDARD_LIBRARIES "${removed_libs} onecoreuap.lib")
+set(CMAKE_CXX_STANDARD_LIBRARIES "${removed_libs} onecoreuap.lib")
 set_target_properties(winml_dll
     PROPERTIES
     LINK_FLAGS
-    "/DEF:${WINML_DIR}/windows.ai.machinelearning.def ${os_component_link_flags} /DELAYLOAD:d3d12.dll /DELAYLOAD:d3d11.dll /DELAYLOAD:dxgi.dll ${delayload_dml} ${removed_libs}")
+    "/DEF:${WINML_DIR}/windows.ai.machinelearning.def ${os_component_link_flags} /DELAYLOAD:d3d12.dll /DELAYLOAD:d3d11.dll /DELAYLOAD:dxgi.dll ${delayload_dml}")
 
 
 set_target_properties(winml_dll
