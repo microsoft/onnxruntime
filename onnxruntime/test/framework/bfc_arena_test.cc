724ff0753bc3c7a1931e58ccb092df41b262d61f
Scott McKay <skottmckay@gmail.com>
2020-01-17 07:41:48 +1000

    Use IArenaAllocator::Reserve for initializers and mem pattern planner blocks (#2835)
    * Use IArenaAllocator::Reserve for initializers and mem pattern planner blocks.

--- onnxruntime/test/framework/bfc_arena_test.cc
+++ onnxruntime/test/framework/bfc_arena_test.cc
@@ -2,7 +2,10 @@
 // Licensed under the MIT License.
 
 #include "core/framework/bfc_arena.h"
+#include "core/framework/utils.h"
 #include "gtest/gtest.h"
+#include "test_utils.h"
+
 #include <cstdlib>
 
 namespace onnxruntime {
@@ -234,5 +237,42 @@ TEST(BFCArenaTest, TestReserve) {
   a.GetStats(&stats);
   EXPECT_EQ(stats.total_allocated_bytes, 1048576);
 }
+
+// arena is disabled for CPUExecutionProvider on x86 and JEMalloc
+#if (defined(__amd64__) || defined(_M_AMD64)) && !defined(USE_JEMALLOC)
+TEST(BFCArenaTest, UtilsAllocateBlockTest) {
+  // use a local CPUExecutionProvider so it has a clean arena.
+  CPUExecutionProviderInfo info;
+  info.create_arena = true;
+  CPUExecutionProvider cpu_provider(info);
+
+  auto cpu_arena = cpu_provider.GetAllocator(0, OrtMemTypeDefault);
+
+  EXPECT_EQ(cpu_arena->Info().alloc_type, OrtAllocatorType::OrtArenaAllocator);
+
+  // AllocateBlock should use Reserve so the growth rate of the arena doesn't change
+  size_t block_size = 4 * 1024 * 1024;
+  void* block = utils::AllocateBlock(*cpu_arena, block_size);
+  EXPECT_TRUE(block);
+
+  // if Reserve was called the initial allocation via Alloc should allocate 1MB of memory
+  // if Reserve was not called it would grow by another 4MB
+  size_t initial_extend_size = 1024 * 1024;
+  size_t size = 1024;
+  auto bytes = cpu_arena->Alloc(size);
+  EXPECT_TRUE(bytes);
+
+  AllocatorStats stats;
+  BFCArena* arena = dynamic_cast<BFCArena*>(&*cpu_arena);
+  EXPECT_TRUE(arena);
+  arena->GetStats(&stats);
+
+  EXPECT_EQ(size_t(stats.total_allocated_bytes), block_size + initial_extend_size);
+
+  cpu_arena->Free(block);
+  cpu_arena->Free(bytes);
+}
+#endif
+
 }  // namespace test
 }  // namespace onnxruntime
