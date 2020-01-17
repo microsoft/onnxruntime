2018b9656af1eaa7da367ab82bb5a3385c814986
James Yuzawa <jtyuzawa@gmail.com>
2020-01-17 19:11:54 +0000

    refactor loader

--- java/src/main/java/ai/onnxruntime/OnnxRuntime.java
+++ java/src/main/java/ai/onnxruntime/OnnxRuntime.java
@@ -5,11 +5,10 @@
 package ai.onnxruntime;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Properties;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /** Static loader for the JNI binding. */
@@ -18,9 +17,6 @@ final class OnnxRuntime {
 
   // The initial release of the ORT API.
   private static final int ORT_API_VERSION_1 = 1;
-
-  /** Turns on debug logging during library loading. */
-  public static final String LIBRARY_LOAD_LOGGING = "ORT_LOAD_LOGGING";
 
   private static boolean loaded = false;
 
@@ -35,91 +31,49 @@ final class OnnxRuntime {
    * @throws IOException If it can't write to disk to copy out the library from the jar file.
    */
   static synchronized void init() throws IOException {
-    if (!loaded) {
-      // Check system properties for load time configuration.
-      Properties props = System.getProperties();
-      boolean debug = props.containsKey(LIBRARY_LOAD_LOGGING);
-      String libraryFromJar =
-          "/ai/onnxruntime/native/"
-              + System.mapLibraryName("onnxruntime").replace("dylib", "jnilib");
-      try {
-        String tempLibraryPath = createTempFileFromResource(libraryFromJar, debug);
-        if (debug) {
-          logger.info("Copied resource " + libraryFromJar + " to location " + tempLibraryPath);
-        }
-        System.load(tempLibraryPath);
-      } catch (Exception e) {
-        if (debug) {
-          logger.info("Library onnxruntime not loaded From jar.");
-        }
-      }
-      String jniFromJar =
-          "/ai/onnxruntime/native/"
-              + System.mapLibraryName("onnxruntime4j_jni").replace("dylib", "jnilib");
-      String tempJniPath = createTempFileFromResource(jniFromJar, debug);
-      if (debug) {
-        logger.info("Copied resource " + jniFromJar + " to location " + tempJniPath);
-      }
-      System.load(tempJniPath);
-      ortApiHandle = initialiseAPIBase(ORT_API_VERSION_1);
-      loaded = true;
+    if (loaded) {
+      return;
     }
+    load("onnxruntime");
+    load("onnxruntime4j_jni");
+    ortApiHandle = initialiseAPIBase(ORT_API_VERSION_1);
+    loaded = true;
   }
 
   /**
-   * Copies out the named file from the class path into a temporary directory so it can be loaded by
-   * {@link System#load}.
+   * Finds a named library in classpath resources, copies to temporary file on the filesystem to be
+   * loaded using {@link System#load}.
    *
-   * <p>The file is marked delete on exit. Throws {@link IllegalArgumentException} if the supplied
-   * path is not absolute.
-   *
-   * @param path The path to the file in the classpath.
-   * @param debugLogging If true turn on debug logging.
-   * @return The path to the extracted file on disk.
+   * @param library The bare name of the library.
    * @throws IOException If the file failed to read or write.
    */
-  private static String createTempFileFromResource(String path, boolean debugLogging)
-      throws IOException {
-    if (!path.startsWith("/")) {
-      throw new IllegalArgumentException("The path has to be absolute (start with '/').");
-    } else {
-      String[] parts = path.split("/");
-      String filename = parts.length > 1 ? parts[parts.length - 1] : null;
-      String prefix = "";
-      String suffix = null;
-      if (filename != null) {
-        parts = filename.split("\\.", 2);
-        prefix = parts[0];
-        suffix = parts.length > 1 ? "." + parts[parts.length - 1] : null;
-      }
+  private static void load(String library) throws IOException {
+    // generate a platform specific library name
+    // replace Mac's jnilib extension to dylib
+    String libraryFileName = System.mapLibraryName(library).replace("jnilib", "dylib");
+    String resourcePath = "/ai/onnxruntime/native/" + libraryFileName;
+    logger.log(
+        Level.FINE,
+        "Attempting to load native library '" + library + "' from resource path " + resourcePath);
+    InputStream is = OnnxRuntime.class.getResourceAsStream(resourcePath);
+    if (is == null) {
+      logger.log(Level.FINE, "Native library '" + library + "' was not found in resources");
+      return;
+    }
 
-      if (filename != null && prefix.length() >= 3) {
-        File temp = File.createTempFile(prefix, suffix);
-        if (debugLogging) {
-          logger.info("Writing " + path + " out to " + temp.getAbsolutePath());
+    File temp = File.createTempFile(library, "lib");
+    try {
+      byte[] buffer = new byte[1024];
+      int readBytes;
+      try (FileOutputStream os = new FileOutputStream(temp)) {
+        while ((readBytes = is.read(buffer)) != -1) {
+          os.write(buffer, 0, readBytes);
         }
-        temp.deleteOnExit();
-        if (!temp.exists()) {
-          throw new FileNotFoundException("File " + temp.getAbsolutePath() + " does not exist.");
-        } else {
-          byte[] buffer = new byte[1024];
-          try (InputStream is = OnnxRuntime.class.getResourceAsStream(path)) {
-            if (is == null) {
-              throw new FileNotFoundException("File " + path + " was not found inside JAR.");
-            } else {
-              int readBytes;
-              try (FileOutputStream os = new FileOutputStream(temp)) {
-                while ((readBytes = is.read(buffer)) != -1) {
-                  os.write(buffer, 0, readBytes);
-                }
-              }
-              return temp.getAbsolutePath();
-            }
-          }
-        }
-      } else {
-        throw new IllegalArgumentException("The filename has to be at least 3 characters long.");
       }
+      System.load(temp.getAbsolutePath());
+      logger.log(Level.FINE, "Loaded native library '" + library + "'");
+    } finally {
+      temp.delete();
     }
   }
 
