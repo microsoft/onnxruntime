ed82f30a3ec659a3f74277ef25d9da907a98ba40
Andrey Lompart <andrey.lompart@gmail.com>
2020-02-03 14:40:00 +0800

    fix build of onnx server on ubuntu 16 and 18

--- tools/ci_build/github/linux/docker/scripts/install_server_deps.sh
+++ tools/ci_build/github/linux/docker/scripts/install_server_deps.sh
@@ -2,14 +2,14 @@
 set -e
 
 SYS_LONG_BIT=$(getconf LONG_BIT)
-
+apt-get update
 #see: https://docs.microsoft.com/en-us/dotnet/core/linux-prerequisites?tabs=netcore21
 if [ "$OS_VERSION" = "16.04" ]; then
-  apt-get remove -y libprotobuf9v5 libprotobuf-lite9v5
+  apt-get remove libprotobuf9v5 libprotobuf-lite9v5
 else # ubuntu18.04
-  apt-get remove -y libprotobuf-dev libprotobuf10
+  apt-get remove libprotobuf-dev libprotobuf10
 fi
-apt-get update && apt-get install -y --no-install-recommends libre2-dev
+apt-get install -y --no-install-recommends libre2-dev
 rm -rf /var/lib/apt/lists/*
 
 if [ $SYS_LONG_BIT = "64" ]; then
