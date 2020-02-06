3fcb173ef6f97e5f10b188aefa1ed761a5f6b5ca
Ori Levari <orlevari@microsoft.com>
2020-02-06 15:25:28 -0800

    winml will conform to ort major and minor versioning

--- tools/ci_build/build.py
+++ tools/ci_build/build.py
@@ -403,14 +403,11 @@ def generate_build_tree(cmake_path, source_dir, build_dir, cuda_home, cudnn_home
                 # BuildPart = 1911
                 # PrivatePart = 123
                 # String = 191101-2300.1.master.0bce7ae
-                cmake_args += ["-DWINML_VERSION_MAJOR_PART=1",
-                            "-DWINML_VERSION_MINOR_PART=0",
-                            "-DORT_VERSION_MAJOR_PART={}".format(ort_major),
-                            "-DORT_VERSION_MINOR_PART={}".format(ort_minor),
+                cmake_args += ["-DVERSION_MAJOR_PART={}".format(ort_major),
+                            "-DVERSION_MINOR_PART={}".format(ort_minor),
                             "-DVERSION_BUILD_PART={}{}".format(YY, MM),
                             "-DVERSION_PRIVATE_PART={}{}".format(DD, HH),
-                            "-DWINML_VERSION_STRING=1.0.{}.{}".format(build_number, source_version[0:7]),
-                            "-DORT_VERSION_STRING={}.{}.{}.{}".format(ort_major, ort_minor, build_number, source_version[0:7])]
+                            "-DVERSION_STRING={}.{}.{}.{}".format(ort_major, ort_minor, build_number, source_version[0:7])]
     
     for config in configs:                
         config_build_dir = get_config_build_dir(build_dir, config)
