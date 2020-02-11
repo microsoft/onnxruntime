f0040da9c08187dbc02a61c3293d1e9924178144
Tiago Koji Castro Shibata <ticastro@microsoft.com>
2020-02-11 11:01:10 -0800

    Use target_add_dml function

--- cmake/winml.cmake
+++ cmake/winml.cmake
@@ -180,7 +180,7 @@ add_dependencies(winml_lib_ort winml_api_native)
 add_dependencies(winml_lib_ort winml_api_native_internal)
 
 # Link libraries
-target_link_libraries(winml_lib_ort PRIVATE ${CMAKE_CURRENT_BINARY_DIR}/packages/DirectML.0.0.1/build/DirectML.targets)
+target_add_dml(winml_lib_ort)
 target_link_libraries(winml_lib_ort PRIVATE wil)
 
 
