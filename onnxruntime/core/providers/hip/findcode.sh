6225147abc718505a58432ab7c203ab73f224270
Weixing Zhang <wezhan@microsoft.com>
2020-02-22 05:15:33 +0800

    delete files.

--- onnxruntime/core/providers/hip/findcode.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-SEARCH_DIRS=$@
-
-find $SEARCH_DIRS -name '*.cu'
-find $SEARCH_DIRS -name '*.cpp' -o -name '*.cxx' -o -name '*.c' -o -name '*.cc'
-find $SEARCH_DIRS -name '*.cuh'
-find $SEARCH_DIRS -name '*.h' -o -name '*.hpp' -o -name '*.inc' -o -name '*.inl' -o -name '*.hxx' -o -name '*.hdl'
