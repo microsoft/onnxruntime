a1b2f36d8961bcdddca0f30e8e196c0959848ae3
souptc <chenta@microsoft.com>
2020-02-28 16:22:15 -0800

    fix training build break

--- orttraining/orttraining/core/graph/mixed_precision_transformer.cc
+++ orttraining/orttraining/core/graph/mixed_precision_transformer.cc
@@ -407,7 +407,7 @@ Status TransformGraphForMixedPrecision(Graph& graph,
   // Add new FP16 initializers to the graph
   for (const auto& kv : fp16_initializers) {
     const ONNX_NAMESPACE::TensorProto* tensor_proto = kv.second;
-    Initializer initializer(*tensor_proto);
+    Initializer initializer(*tensor_proto, graph.ModelPath());
     ONNX_NAMESPACE::TensorProto weight_tensor_proto = initializer.ToFP16(kv.first);
     graph.AddInitializedTensor(weight_tensor_proto);
   }
