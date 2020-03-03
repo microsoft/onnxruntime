12605f05d14f29a8741432d32db983a3c89c7c9e
Changming Sun <chasun@microsoft.com>
2020-03-03 14:34:19 -0800

    Fix CUDA PATH (#3131)
    Previously, we put the "bin" folder of all the CUDA verions in the system PATH. And 10.2 is in the front. It's a mess.
    So I've removed all of them from the system PATH env. But I need to add one of them back through build scripts.
    
    (The problem only affect the C# test, not the C/C++ tests that forked from build.py).

---
+++ tools/ci_build/github/windows/setup_env_cuda.bat
@@ -0,0 +1,2 @@
+set PATH=C:\azcopy;C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v10.1\bin;C:\local\cudnn-10.1-windows10-x64-v7.6.5.32\cuda\bin;%PATH%
+set GRADLE_OPTS=-Dorg.gradle.daemon=false
