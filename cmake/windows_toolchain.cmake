d2edecc56a481fa6652949c597c4f9ace3c7112d
Tiago Koji Castro Shibata <ticastro@microsoft.com>
2020-03-03 09:40:26 -0800

    Use library list instead of string

--- cmake/windows_toolchain.cmake
+++ cmake/windows_toolchain.cmake
@@ -8,18 +8,19 @@
 # CMake doesn't support multiple toolchains in the same project, so in this case we use a small hack clearing
 # CMAKE_CXX_STANDARD_LIBRARIES and applying the Win32 or WCOS libraries to each target manually.
 
-set(wcos_libs "windowsapp.lib")
 foreach(default_lib kernel32.lib user32.lib gdi32.lib winspool.lib shell32.lib ole32.lib oleaut32.lib uuid.lib comdgl32.lib advapi32.lib)
-  set(wcos_libs "${wcos_libs} /NODEFAULTLIB:${default_lib}")
+  set(wcos_options "${wcos_options} /NODEFAULTLIB:${default_lib}")
 endforeach()
 
 if (onnxruntime_ENABLE_WCOS)
-    set(CMAKE_CXX_STANDARD_LIBRARIES ${wcos_libs})
+    set(CMAKE_CXX_STANDARD_LIBRARIES "${wcos_options} windowsapp.lib")
     return()
 endif()
 
 set(win32_libs "${CMAKE_CXX_STANDARD_LIBRARIES}")
 set(CMAKE_CXX_STANDARD_LIBRARIES "")
+separate_arguments(win32_libs)
+separate_arguments(wcos_options)
 get_directory_property(all_targets BUILDSYSTEM_TARGETS)
 foreach(target ${all_targets})
     get_target_property(target_type ${target} TYPE)
@@ -27,7 +28,8 @@ foreach(target ${all_targets})
         get_target_property(target_language ${target} LINKER_LANGUAGE)
         if (target_language STREQUAL CXX OR target_language MATCHES NOTFOUND)
             if (target MATCHES "winml_")
-                target_link_libraries(${target} PRIVATE ${wcos_libs})
+                target_link_libraries(${target} PRIVATE windowsapp.lib)
+                target_link_options(${target} PRIVATE ${wcos_options})
             else()
                 target_link_libraries(${target} PRIVATE ${win32_libs})
             endif()
