a5d8a864b3612fdc83803d103a54af64586e9cd9
Weixing Zhang <Weixing.Zhang@microsoft.com>
2020-03-03 13:23:35 -0800

    fix build issue on rocm3.1

--- orttraining/orttraining/training_ops/hip/math/isfinite.cu
+++ orttraining/orttraining/training_ops/hip/math/isfinite.cu
@@ -1,8 +1,6 @@
-#include "hip/hip_runtime.h"
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-#include <hip/hip_fp16.h>
 #include "core/providers/hip/cu_inc/common.cuh"
 #include "isfinite.cuh"
 
