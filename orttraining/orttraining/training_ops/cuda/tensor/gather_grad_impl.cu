b3ef1627761fc97b251c97240d5ced6846f72fee
Edward Chen <edch@microsoft.com>
2020-03-05 01:05:29 +0000

    Merged PR 5657: Fixed GatherGrad const correctness, replaced thrust with cub.
    Fixed GatherGrad const correctness, replaced thrust with cub.

--- orttraining/orttraining/training_ops/cuda/tensor/gather_grad_impl.cu
+++ orttraining/orttraining/training_ops/cuda/tensor/gather_grad_impl.cu
@@ -3,22 +3,30 @@
 
 #include "orttraining/training_ops/cuda/tensor/gather_grad_impl.h"
 #include "core/providers/cuda/cu_inc/common.cuh"
+#include "core/providers/cuda/shared_inc/cuda_call.h"
 
-#include <thrust/device_ptr.h>
-#include <thrust/execution_policy.h>
-#include <thrust/unique.h>
-#include <thrust/sort.h>
+#include <cub/device/device_radix_sort.cuh>
+#include <cub/iterator/counting_input_iterator.cuh>
 
 namespace onnxruntime {
 namespace cuda {
 
-static const int WARP_SIZE = 32;
+static constexpr int WARP_SIZE = 32;
+
+template <typename T>
+__global__ void _Iota(
+    cub::CountingInputIterator<T> input,
+    size_t length,
+    T* output) {
+  CALCULATE_ELEMENTWISE_INDEX_OR_EXIT(idx, length);
+  output[idx] = input[idx];
+}
 
 template <typename T, typename Tin>
 __global__ void _GatherGradImpl(
-    Tin* input,
-    Tin* indices,
-    T* grad_output,
+    const Tin* input,
+    const Tin* indices,
+    const T* grad_output,
     T* grad_weight,
     int64_t numel,
     int64_t input_numel,
@@ -64,40 +72,55 @@ __global__ void _GatherGradImpl(
   }
 }
 
-template <typename T>
-__host__ __device__ __forceinline__ T CeilDiv(T a, T b) {
-  return (a + b - 1) / b;
-}
-
 template <typename T, typename Tin>
 void GatherGradImpl(
+    const CudaKernel& cuda_kernel,
     const T* grad_data,
     const Tin* indices_data,
     const int64_t num_indices,
     const int64_t num_weights,
     const int64_t stride,
-    Tin* origin_indices,
     T* output_data,
     const int64_t num_inputs,  //The number of input elements starting from the gathering dimension
-    const int64_t param_itrs,  //The size of dimensions of the data before gathering dimension
-    ThrustAllocator& allocator) {
-  // sort the index
-  auto count_iter = thrust::counting_iterator<Tin>(0);
-  auto origin_data = thrust::device_ptr<Tin>(origin_indices);
+    const int64_t param_itrs   //The size of dimensions of the data before gathering dimension
+    ) {
+  // allocate intermediate buffers
+  auto original_indices = cuda_kernel.template GetScratchBuffer<Tin>(num_indices);
 
-  auto policy = thrust::cuda::par(allocator);
-  thrust::copy(policy, count_iter, count_iter + num_indices, origin_data);
-  //TODO: remove the const_cast
-  auto sorted_data = thrust::device_ptr<Tin>(const_cast<Tin*>(indices_data));
-  thrust::sort_by_key(policy, sorted_data, sorted_data + num_indices, origin_data, thrust::less<Tin>());
+  // initialize original_indices with [0, num_indices)
+  {
+    const auto blocks_per_grid = CeilDiv(num_indices, GridDim::maxThreadsPerBlock);
+    cub::CountingInputIterator<Tin> counting_input(Tin{});
+    _Iota<<<blocks_per_grid, GridDim::maxThreadsPerBlock>>>(
+        counting_input, num_indices, original_indices.get());
+  }
 
-  dim3 grid(CeilDiv(num_indices, (int64_t)4), CeilDiv(stride, (int64_t)128));
+  auto indices_data_sorted = cuda_kernel.template GetScratchBuffer<Tin>(num_indices);
+  auto original_indices_sorted = cuda_kernel.template GetScratchBuffer<Tin>(num_indices);
+
+  // sort indices and original indices
+  size_t sort_temp_storage_size_bytes = 0;
+  CUDA_CALL_THROW(cub::DeviceRadixSort::SortPairs(
+      nullptr, sort_temp_storage_size_bytes,
+      indices_data, indices_data_sorted.get(),
+      original_indices.get(), original_indices_sorted.get(),
+      num_indices));
+
+  auto sort_temp_storage = cuda_kernel.GetScratchBuffer<void>(sort_temp_storage_size_bytes);
+
+  CUDA_CALL_THROW(cub::DeviceRadixSort::SortPairs(
+      sort_temp_storage.get(), sort_temp_storage_size_bytes,
+      indices_data, indices_data_sorted.get(),
+      original_indices.get(), original_indices_sorted.get(),
+      num_indices));
+
+  dim3 grid(CeilDiv(num_indices, 4), CeilDiv(stride, 128));
   dim3 block(WARP_SIZE, 4);
 
   _GatherGradImpl<<<grid, block>>>(
-      const_cast<Tin*>(indices_data),
-      origin_indices,
-      const_cast<T*>(grad_data),
+      indices_data_sorted.get(),
+      original_indices_sorted.get(),
+      grad_data,
       output_data,
       num_indices,
       num_inputs,
@@ -105,15 +128,27 @@ void GatherGradImpl(
       stride);
 }
 
-#define SPECIALIZED_GRAD_IMPL2(T)                                                                                      \
-  template void GatherGradImpl<T, int64_t>(const T* grad_data, const int64_t* indices_data,                            \
-                                           const int64_t num_indices, const int64_t num_weights, const int64_t stride, \
-                                           int64_t* origin_indices, T* output_data, const int64_t num_inputs,          \
-                                           const int64_t params_itrs, ThrustAllocator& allocator);                     \
-  template void GatherGradImpl<T, int32_t>(const T* grad_data, const int32_t* indices_data,                            \
-                                           const int64_t num_indices, const int64_t num_weights, const int64_t stride, \
-                                           int32_t* origin_indices, T* output_data, const int64_t num_inputs,          \
-                                           const int64_t params_itrs, ThrustAllocator& allocator);
+#define SPECIALIZED_GRAD_IMPL2(T)           \
+  template void GatherGradImpl<T, int64_t>( \
+      const CudaKernel& cuda_kernel,        \
+      const T* grad_data,                   \
+      const int64_t* indices_data,          \
+      const int64_t num_indices,            \
+      const int64_t num_weights,            \
+      const int64_t stride,                 \
+      T* output_data,                       \
+      const int64_t num_inputs,             \
+      const int64_t param_itrs);            \
+  template void GatherGradImpl<T, int32_t>( \
+      const CudaKernel& cuda_kernel,        \
+      const T* grad_data,                   \
+      const int32_t* indices_data,          \
+      const int64_t num_indices,            \
+      const int64_t num_weights,            \
+      const int64_t stride,                 \
+      T* output_data,                       \
+      const int64_t num_inputs,             \
+      const int64_t param_itrs);
 
 SPECIALIZED_GRAD_IMPL2(float)
 SPECIALIZED_GRAD_IMPL2(half)
--- orttraining/orttraining/training_ops/cuda/tensor/gather_grad_impl.h
+++ orttraining/orttraining/training_ops/cuda/tensor/gather_grad_impl.h
@@ -3,24 +3,23 @@
 
 #pragma once
 #include <stdint.h>
+#include "core/providers/cuda/cuda_common.h"
 #include "core/providers/cuda/shared_inc/cuda_utils.h"
-#include "orttraining/training_ops/cuda/tensor/thrustallocator.h"
 
 namespace onnxruntime {
 namespace cuda {
 
 template <typename T, typename Tin>
 void GatherGradImpl(
+    const CudaKernel& cuda_kernel,
     const T* grad_data,
     const Tin* indices_data,
     const int64_t num_indices,
     const int64_t num_weights,
     const int64_t stride,
-    Tin* origin_indices,
     T* output_data,
     const int64_t num_inputs,
-    const int64_t param_itrs,
-    ThrustAllocator& allocator);
+    const int64_t param_itrs);
 
 }  // namespace cuda
 }  // namespace onnxruntime
