189e01ffbbf886d9318130f6ebbf3988b0951bbc
Tiago Koji Castro Shibata <tiago.shibata@gmail.com>
2020-03-10 00:54:44 -0700

    Enable Win32 APIs in featurizer

--- cmake/external/featurizers.cmake
+++ cmake/external/featurizers.cmake
@@ -16,5 +16,12 @@ if (WIN32)
     # Add Code Analysis properties to enable C++ Core checks. Have to do it via a props file include.
     set_target_properties(FeaturizersCode PROPERTIES VS_USER_PROPS ${PROJECT_SOURCE_DIR}/ConfigureVisualStudioCodeAnalysis.props)
 endif()
+if(WINDOWS_STORE)
+    # protobuf and FeaturizersLibrary use Win32 desktop APIs; this must be fixed!
+    # See https://dev.azure.com/onnxruntime/2a773b67-e88b-4c7f-9fc0-87d31fea8ef2/_apis/build/builds/119296/logs/25
+    # target_compile_options(libprotoc PRIVATE "-DWINAPI_FAMILY=WINAPI_FAMILY_DESKTOP_APP")
+    target_compile_options(FeaturizersCode PRIVATE "/FI ${CMAKE_CURRENT_SOURCE_DIR}\\set_winapi_family.h")
+    target_link_libraries(FeaturizersCode kernel32.lib)
+endif()
 
 add_library(onnxruntime_featurizers ALIAS FeaturizersCode)
