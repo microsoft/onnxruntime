1db300045dca3528eec7064e67ab25c633ee238a
Ke Deng <kedeng@microsoft.com>
2020-03-12 01:24:42 +0000

    Initial implementation of graph cut and pipeline
    This is a draft of graph cut and wait/record to demonstrate cut and Wait/Record design. You may find sub models and profiling json under onnxruntime/test if you run "onnxruntime_test_all --gtest_filter=GradientGraphBuilderTest.TrainingSession_WithPipeline"

--- orttraining/orttraining/training_ops/cpu/controlflow/record.cc
+++ orttraining/orttraining/training_ops/cpu/controlflow/record.cc
@@ -2,9 +2,19 @@
 // Licensed under the MIT License.
 
 #include "record.h"
+#include "core/providers/cpu/tensor/utils.h"
 
 namespace onnxruntime {
 namespace contrib {
+
+template <int input_start, int output_start>
+std::vector<std::pair<int, int>> AliasRange(int start, int end) {
+  std::vector<std::pair<int, int>> aliases;
+  for (int i = start; i < end; i++) {
+    aliases.push_back(std::pair<int, int>(input_start + i, output_start + i));
+  }
+  return aliases;
+}
 
 ONNX_OPERATOR_KERNEL_EX(
     RecordEvent,
@@ -13,23 +23,24 @@ ONNX_OPERATOR_KERNEL_EX(
     kCpuExecutionProvider,
     KernelDefBuilder()
         .TypeConstraint("TInt64", DataTypeImpl::GetTensorType<int64_t>())
-        .TypeConstraint("TBool", DataTypeImpl::GetTensorType<bool>()),
+        .TypeConstraint("T", DataTypeImpl::AllFixedSizeTensorTypes())
+        .Alias(AliasRange<1, 0>(0, 1024)),
     RecordEvent);
 
 Status RecordEvent::Compute(OpKernelContext* ctx) const {
   const Tensor* event_id_tensor = ctx->Input<Tensor>(0);
   const int64_t event_id = *event_id_tensor->template Data<int64_t>();
 
-  const Tensor* input_signal_tensor = ctx->Input<Tensor>(1);
-  const bool input_signal = *input_signal_tensor->template Data<bool>();
+  ORT_RETURN_IF_NOT(event_id != -1, "-1 is reserved for skip wait, so cannot be used in RecordEvent");
 
-  ORT_ENFORCE(input_signal, "Input signal must be true to trigger RecordEvent operator.");
+  OrtEventPool::GetInstance().SignalEvent(event_id);
 
-  OrtEventPool::GetInstance().CreateEvent(event_id);
-  OrtEventPool::GetInstance().RecordEvent(event_id);
-
-  Tensor* output_signal_tensor = ctx->Output(0, {});
-  *output_signal_tensor->template MutableData<bool>() = true;
+  for (int i_out = 0; i_out < ctx->OutputCount(); ++i_out) {
+    const Tensor* X = ctx->Input<Tensor>(i_out + 1);
+    const TensorShape& data_shape = X->Shape();
+    Tensor* Y = ctx->Output(i_out, data_shape);
+    CopyCpuTensor(X, Y);
+  }
   return Status::OK();
 }
 
