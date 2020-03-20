84171f93d1df73c9996633b59f5bf64e3e101f4e
Tianlei Wu <tlwu@microsoft.com>
2020-03-19 17:26:17 -0700

    update bat file

--- onnxruntime/ReformatSourcePython.bat
+++ onnxruntime/ReformatSourcePython.bat
@@ -1,4 +1,16 @@
 :: Copyright (c) Microsoft Corporation. All rights reserved.
 :: Licensed under the MIT License.
 
-autopep8 --in-place -r python
+:: Before running this, please make sure python.exe is in path, and yapf is installed like the following
+::    pip install --upgrade yapf
+
+:: If you use Visual Studio Code, you can configure like the following:
+::    "python.formatting.provider": "yapf"
+::    "python.formatting.yapfArgs": ["--style", "{based_on_style: google, column_limit: 120}"]
+:: See https://code.visualstudio.com/docs/python/editing for more info
+
+:: The style configuration file is .style.yapf in current directory.
+
+yapf -ir ./python
+
+if errorlevel 1 echo please install python, then pip install yapf
