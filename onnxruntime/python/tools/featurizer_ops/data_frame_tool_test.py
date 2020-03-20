403f99cd77e596b8d161c2f3d2859548bc0d3bb2
Tianlei Wu <tlwu@microsoft.com>
2020-03-20 14:34:10 -0700

    Use yapf to format python (#3276)
    Update ReformatSourcePython.bat to use YAPF to format python code, and add onnxruntime\test directory to be formatted.
    
    Add onnxruntime\.style.yapf for configuration. The style is based on google, except max column width 120.
    
    Format python scripts using ReformatSourcePython.bat.

--- onnxruntime/python/tools/featurizer_ops/data_frame_tool_test.py
+++ onnxruntime/python/tools/featurizer_ops/data_frame_tool_test.py
@@ -11,11 +11,11 @@ from data_frame_tool import DataFrameTool
 import os
 import sys
 
+
 def main():
     parser = argparse.ArgumentParser(description='Test Feed Inputs utility')
     parser.add_argument('model_path', help='model path')
-    parser.add_argument('-profile', action='store_true',
-                        help='enable chrome timeline trace profiling.')
+    parser.add_argument('-profile', action='store_true', help='enable chrome timeline trace profiling.')
     args = parser.parse_args()
 
     # Create options and the tool
@@ -25,11 +25,15 @@ def main():
     df_tool = DataFrameTool(args.model_path, sess_options)
 
     # Create a DataFrame that holds 3 inputs, string, bool, float in their respective columns
-    df = pd.DataFrame([['string_input', 3.25, 8, 16, 32, 64, True, 0.25]], 
-                      columns=['StringInput', 'DoubleInput', 'Int8Input', 'Int16Input', 'Int32Input', 'Int64Input', 'BoolInput', 'Float32Input'])
+    df = pd.DataFrame([['string_input', 3.25, 8, 16, 32, 64, True, 0.25]],
+                      columns=[
+                          'StringInput', 'DoubleInput', 'Int8Input', 'Int16Input', 'Int32Input', 'Int64Input',
+                          'BoolInput', 'Float32Input'
+                      ])
 
     outputs = df_tool.execute(df, [])
     print('Outputs: ', outputs)
+
 
 if __name__ == "__main__":
     sys.exit(main())
--- onnxruntime/test/testdata/transform/fusion/gelu_gen.py
+++ onnxruntime/test/testdata/transform/fusion/gelu_gen.py
@@ -3,7 +3,6 @@ from onnx import helper
 from onnx import AttributeProto, TensorProto, GraphProto, OperatorSetIdProto
 from onnx import numpy_helper
 import numpy as np
-
 """
 Generate test model for Gelu subgraph pattern 2:
                    +------------------------------------+
@@ -13,9 +12,9 @@ Generate test model for Gelu subgraph pattern 2:
                           (B=1.4142...)        (1)            (0.5)
 """
 
-has_bias = True # change it to True to generate gelu_format2_*_with_bias.onnx
-gelu_use_graph_input = False # change it to False to let Gelu don't have graph inputs as inputs.
-switch_order = True # switch order of inputs for Mul and Add
+has_bias = True  # change it to True to generate gelu_format2_*_with_bias.onnx
+gelu_use_graph_input = False  # change it to False to let Gelu don't have graph inputs as inputs.
+switch_order = True  # switch order of inputs for Mul and Add
 
 X = helper.make_tensor_value_info('input', TensorProto.FLOAT, ["batch", "seqlen", 64])
 Y = helper.make_tensor_value_info('output', TensorProto.FLOAT, ["batch", "seqlen", 64])
@@ -32,77 +31,40 @@ initializer_0_5 = numpy_helper.from_array(value, "mul3_init")
 value = np.asarray([1.0]).astype(np.float32).reshape(())
 initializer_1 = numpy_helper.from_array(value, "add1_init")
 
-
 nodes = []
 gelu_input = "input"
 if not gelu_use_graph_input:
-    leading_identity = helper.make_node(
-        'Identity', 
-        [gelu_input], 
-        ['identity_leading'], 
-        name="identity_leading"
-    )
+    leading_identity = helper.make_node('Identity', [gelu_input], ['identity_leading'], name="identity_leading")
     gelu_input = "identity_leading"
     nodes.append(leading_identity)
 
 gelu_root = gelu_input
 if has_bias:
     add0 = helper.make_node(
-        'Add', 
-        [gelu_input, bias_initializer.name] if switch_order else [bias_initializer.name, gelu_input],
-        ['add0'], 
-        name="add0_node"
-    )
+        'Add', [gelu_input, bias_initializer.name] if switch_order else [bias_initializer.name, gelu_input], ['add0'],
+        name="add0_node")
     gelu_root = "add0"
     nodes.append(add0)
 
-
-div = helper.make_node(
-    'Div', 
-    [gelu_root, initializer_sqrt_2.name], 
-    ['div'], 
-    name="div_node"
-)
+div = helper.make_node('Div', [gelu_root, initializer_sqrt_2.name], ['div'], name="div_node")
 nodes.append(div)
 
-erf = helper.make_node(
-    'Erf', 
-    ['div'], 
-    ['erf'], 
-    name="erf_node"
-)
+erf = helper.make_node('Erf', ['div'], ['erf'], name="erf_node")
 nodes.append(erf)
 
-add1 = helper.make_node(
-    'Add', 
-    ['erf', initializer_1.name] if switch_order else [initializer_1.name, 'erf'],
-    ['add1'], 
-    name="add1"
-)
+add1 = helper.make_node('Add', ['erf', initializer_1.name] if switch_order else [initializer_1.name, 'erf'], ['add1'],
+                        name="add1")
 nodes.append(add1)
 
-mul = helper.make_node(
-    'Mul', 
-    [gelu_root, 'add1'] if switch_order else ['add1', gelu_root],
-    ['mul'], 
-    name="mul_node"
-)
+mul = helper.make_node('Mul', [gelu_root, 'add1'] if switch_order else ['add1', gelu_root], ['mul'], name="mul_node")
 nodes.append(mul)
 
-mul2 = helper.make_node(
-    'Mul', 
-    ['mul', initializer_0_5.name] if switch_order else [initializer_0_5.name, 'mul'],
-    ['mul2'], 
-    name="mul2_node"
-)
+mul2 = helper.make_node('Mul', ['mul', initializer_0_5.name] if switch_order else [initializer_0_5.name, 'mul'],
+                        ['mul2'],
+                        name="mul2_node")
 nodes.append(mul2)
 
-ending_identity = helper.make_node(
-    'Identity', 
-    ['mul2'], 
-    ['output'], 
-    name="identity_ending"
-)
+ending_identity = helper.make_node('Identity', ['mul2'], ['output'], name="identity_ending")
 nodes.append(ending_identity)
 
 initializers = []
@@ -112,18 +74,12 @@ if has_bias:
 initializers.extend([initializer_sqrt_2, initializer_1, initializer_0_5])
 
 # Create the graph (GraphProto)
-graph_def = helper.make_graph(
-    nodes,
-    'gelu_pattern_2',
-    [X],
-    [Y],
-    initializers
-)
+graph_def = helper.make_graph(nodes, 'gelu_pattern_2', [X], [Y], initializers)
 
 opsets = []
 onnxdomain = OperatorSetIdProto()
 onnxdomain.version = 10
-onnxdomain.domain = "" # The empty string ("") or absence of this field implies the operator set that is defined as part of the ONNX specification.
+onnxdomain.domain = ""  # The empty string ("") or absence of this field implies the operator set that is defined as part of the ONNX specification.
 opsets.append(onnxdomain)
 
 msdomain = OperatorSetIdProto()
@@ -131,7 +87,7 @@ msdomain.version = 1
 msdomain.domain = "com.microsoft"
 
 opsets.append(msdomain)
-kwargs={}
+kwargs = {}
 kwargs["opset_imports"] = opsets
 
 model_def = helper.make_model(graph_def, producer_name='onnx-example', **kwargs)
@@ -146,4 +102,3 @@ if gelu_use_graph_input:
 file_name += ".onnx"
 onnx.save(model_def, file_name)
 print(file_name)
-
