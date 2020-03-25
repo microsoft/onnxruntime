a031af8cfcb499d371d635657305e15fe46fc326
Xueyun Zhu <xzhu1900@gmail.com>
2020-03-25 19:18:50 +0000

    fix windows build

--- onnxruntime/test/platform/path_lib_test.cc
+++ onnxruntime/test/platform/path_lib_test.cc
@@ -1,15 +1,18 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-#include "core/platform/path_lib.h"
-
-#include "gtest/gtest.h"
 #include <string>
 
 #include "gtest/gtest.h"
 
 #include "core/platform/env.h"
 #include "test/framework/test_utils.h"
+
+// Windows.h reserves the word OPTIONAL in macro, which introduces
+// conflict on variables declared from onnx external library. Currently workaround
+// this by re-ordering the included files.
+// TODO: undefine OPTIONAL in path_lib.h and move path_lib.h to the first include
+#include "core/platform/path_lib.h"
 
 namespace onnxruntime {
 namespace test {
