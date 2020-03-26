3a7440de7fe3410cf068f7db3074a09a2e1bd8e0
CircleCI <tix@microsoft.com>
2020-03-25 18:23:19 -0700

    fix windows build

--- orttraining/orttraining/test/gradient/optimizer_ops_test.cc
+++ orttraining/orttraining/test/gradient/optimizer_ops_test.cc
@@ -1,6 +1,8 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+#include <random>
+
 #include "gtest/gtest.h"
 #include "test/providers/provider_test_utils.h"
 
