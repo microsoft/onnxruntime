5278f73202757547482534eab685ecaf2084f18b
Yang Chen <40417152+yangchen-MS@users.noreply.github.com>
2020-03-26 23:27:37 -0700

    Fixed two issues in symbolic_shape_infer script (#3332)
    * Fixed two issues in symbolic_shape_infer script
    
    This change addressed #3293
    
    There were two issues in the script:
    
    * We need to handle a special case for infer_Reshape, where input_shape
    is empty and target shape_value is [-1]. In such case, we need to
    get sympy data for the output dim (or create one if it doesn't exist).
    
    * We need to update computed dims for newly-created shape for Range op
    
    * also call _update_computed_dims for _infer_Expand
    
    addressed CR feedback
    
    * added ai.onnx into opset list
    
    * instead of manipulating _infer_Reshape, call _update_computed_dims
    from _infer_Expand to update newly-computed dims

--- onnxruntime/core/providers/nuphar/scripts/symbolic_shape_infer.py
+++ onnxruntime/core/providers/nuphar/scripts/symbolic_shape_infer.py
@@ -34,7 +34,7 @@ def handle_negative_axis(axis, rank):
     assert axis < rank and axis >= -rank
     return axis if axis >= 0 else rank + axis
 
-def get_opset(mp, domain=['', 'onnx']):
+def get_opset(mp, domain=['', 'onnx', 'ai.onnx']):
     if type(domain) != list:
         domain = [domain]
     for opset in mp.opset_import:
@@ -645,9 +645,12 @@ class SymbolicShapeInference:
     def _infer_Expand(self, node):
         expand_to_shape = self._try_get_value(node, 1)
         if expand_to_shape is not None:
-            input_shape = self._get_shape(node, 0)
-            target_shape = get_shape_from_sympy_shape(expand_to_shape)
-            new_shape = self._broadcast_shapes(input_shape, target_shape)
+            sympy_shape = self._get_sympy_shape(node, 0)
+            new_sympy_shape = self._broadcast_shapes(sympy_shape, expand_to_shape)
+
+            # new_shape's dim can come from 'Expand' computation
+            self._update_computed_dims(new_sympy_shape)
+            new_shape = get_shape_from_sympy_shape(new_sympy_shape)
             vi = self.known_vi_[node.output[0]]
             vi.CopyFrom(helper.make_tensor_value_info(node.output[0], self.known_vi_[node.input[0]].type.tensor_type.elem_type, new_shape))
 
@@ -805,6 +808,7 @@ class SymbolicShapeInference:
         else:
             new_dim = self._new_symbolic_dim_from_output(node)
             new_shape = [self.symbolic_dims_[new_dim]]
+        self._update_computed_dims(new_shape)
         vi.CopyFrom(helper.make_tensor_value_info(node.output[0], self.known_vi_[node.input[0]].type.tensor_type.elem_type, get_shape_from_sympy_shape(new_shape)))
 
     def _infer_ReduceProd(self, node):
