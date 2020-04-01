caa30d2821ea5d8e8586999748d09af039bf96d3
Zeeshan Siddiqui <mzs@microsoft.com>
2020-04-01 03:06:30 -0700

    update submodule.

--- tools/ci_build/github/linux/docker/scripts/install_onnx.sh
+++ tools/ci_build/github/linux/docker/scripts/install_onnx.sh
@@ -30,7 +30,7 @@ version2tag=(5af210ca8a1c73aa6bae8754c9346ec54d0a756e-onnx123
              bae6333e149a59a3faa9c4d9c44974373dcf5256-onnx130
              9e55ace55aad1ada27516038dfbdc66a8a0763db-onnx141
              7d7bc83d29a328233d3e8affa4c4ea8b3e3599ef-onnx150
-             8d15705ec29c2867847d1a8affad9fe19dedb2e3-onnxtip) #1.6.1
+             20b3e10e6c3a9cdab90d2bb864d1c36d3e3651cd-onnxtip) #1.7.0
 for v2t in ${version2tag[*]}; do
   onnx_version="$(cut -d'-' -f1<<<${v2t})"
   onnx_tag="$(cut -d'-' -f2<<<${v2t})"
