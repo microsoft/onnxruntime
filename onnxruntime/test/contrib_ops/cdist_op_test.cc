bbd22b12321ec145393d5ec3ffadfe4788372ffd
Scott McKay <skottmckay@gmail.com>
2020-04-01 21:48:26 +1000

    Add sqeuclidean test

--- onnxruntime/test/contrib_ops/cdist_op_test.cc
+++ onnxruntime/test/contrib_ops/cdist_op_test.cc
@@ -7,7 +7,7 @@
 namespace onnxruntime {
 namespace test {
 
-TEST(CDistOpTest, Basic) {
+TEST(CDistOpTest, Euclidean) {
   OpTester test("CDist", 1, onnxruntime::kMSDomain);
   test.AddAttribute("metric", "euclidean");
 
@@ -26,6 +26,28 @@ TEST(CDistOpTest, Basic) {
                          1.1727045f, 1.7081447f, 1.4874904f,
                          3.1214628f, 1.749023f, 3.0871522f,
                          3.718481f, 1.7794584f, 3.923692f});
+  test.Run();
+}
+
+TEST(CDistOpTest, Sqeuclidean) {
+  OpTester test("CDist", 1, onnxruntime::kMSDomain);
+  test.AddAttribute("metric", "sqeuclidean");
+
+  test.AddInput<float>("A", {4, 2},
+                       {-1.0856307f, 0.99734545f,
+                        0.2829785f, -1.5062947f,
+                        -0.5786002f, 1.6514366f,
+                        -2.4266791f, -0.42891264f});
+  test.AddInput<float>("B", {3, 2},
+                       {1.2659363f, -0.8667404f,
+                        -0.6788862f, -0.09470897f,
+                        1.4913896f, -0.638902f});
+
+  test.AddOutput<float>("y", {4, 3},
+                        {9.004683f, 1.3580238f, 9.318338f,
+                         1.3752356f, 2.917758f, 2.2126276f,
+                         9.74353f, 3.0590816f, 9.530509f,
+                         13.827101f, 3.1664724f, 15.395359f});
   test.Run();
 }
 
