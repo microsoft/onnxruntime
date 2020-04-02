dda97212b961dccdfe1366624fc3a39b393eedb7
wangyems <wangye@microsoft.com>
2020-04-01 20:39:14 -0700

    fix mac os and linux build error

--- onnxruntime/featurizers_ops/cpu/forecasting_pivot_transformer.cc
+++ onnxruntime/featurizers_ops/cpu/forecasting_pivot_transformer.cc
@@ -26,7 +26,7 @@ struct ForecastingPivotTransformerImpl {
     const auto* state_tensor(ctx->Input<Tensor>(0));
     const uint8_t* const state_data(state_tensor->Data<uint8_t>());
     Microsoft::Featurizer::Archive archive(state_data, state_tensor->Shape().GetDims()[0]);
-    typename TransformerT transformer(archive);
+    TransformerT transformer(archive);
 
     // Get the Number of Rows
     const auto* input_tensor_temp(ctx->Input<Tensor>(1));
