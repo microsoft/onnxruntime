f4376653605bedee222c79a1335dc3cbec379f83
Sherlock <baihan.huang@gmail.com>
2020-04-03 11:59:47 -0700

    Revert "Addressing PR comments (#3334)" (#3412)
    This reverts commit 131c65d23d040079d50a6dc3e85f72f0de401346.

--- onnxruntime/core/providers/cuda/tensor/slice.cc
+++ onnxruntime/core/providers/cuda/tensor/slice.cc
@@ -156,7 +156,7 @@ template <bool dynamic>
 void Slice<dynamic>::FillInputVectors(OpKernelContext* ctx, std::vector<int64_t>& input_starts,
                                       std::vector<int64_t>& input_ends, std::vector<int64_t>& input_axes,
                                       std::vector<int64_t>& input_steps) const {
-  FillVectorsFromInput(*ctx->Input<Tensor>(1), *ctx->Input<Tensor>(2), ctx->Input<Tensor>(3),
+  FillVectorsFromInput(ctx->Input<Tensor>(1), ctx->Input<Tensor>(2), ctx->Input<Tensor>(3),
                        ctx->Input<Tensor>(4), input_starts, input_ends, input_axes, input_steps);
 }
 
