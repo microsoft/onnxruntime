316dbc9ead3f80b8d727b1371954443323ee2198
Ryan Lai <ryalai96@gamil.com>
2020-04-08 13:58:53 -0700

    Comment with default allocator

--- onnxruntime/test/perftest/ort_test_session.cc
+++ onnxruntime/test/perftest/ort_test_session.cc
@@ -155,7 +155,7 @@ bool OnnxRuntimeTestSession::PopulateGeneratedInputTestData()
             dim = 1;
           }
         }
-
+        // default allocator doesn't have to be freed by user
         auto allocator = static_cast<OrtAllocator*>(Ort::AllocatorWithDefaultOptions());
         Ort::Value newInput_Tensor = Ort::Value::CreateTensor(allocator, (const int64_t*)input_node_dim.data(), input_node_dim.size(), tensor_info.GetElementType());
         PreLoadTestData(0, i, newInput_Tensor.release());
