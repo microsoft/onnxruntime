eaa3f652dfc75d95b6f71133cebc04acceadcdf4
Sergii Dymchenko <sedymche@microsoft.com>
2020-04-09 14:35:36 -0700

    Fix dynamicslice.cc after merge.

--- onnxruntime/contrib_ops/cpu/dynamicslice.cc
+++ onnxruntime/contrib_ops/cpu/dynamicslice.cc
@@ -17,19 +17,5 @@ ONNX_CPU_OPERATOR_KERNEL(
         .TypeConstraint("Tind", {DataTypeImpl::GetTensorType<int32_t>(), DataTypeImpl::GetTensorType<int64_t>()}),
     Slice10);
 
-ADD_TYPED_DYNAMIC_SLICE_OP(uint8_t);
-ADD_TYPED_DYNAMIC_SLICE_OP(uint16_t);
-ADD_TYPED_DYNAMIC_SLICE_OP(uint32_t);
-ADD_TYPED_DYNAMIC_SLICE_OP(uint64_t);
-ADD_TYPED_DYNAMIC_SLICE_OP(int8_t);
-ADD_TYPED_DYNAMIC_SLICE_OP(int16_t);
-ADD_TYPED_DYNAMIC_SLICE_OP(int32_t);
-ADD_TYPED_DYNAMIC_SLICE_OP(int64_t);
-ADD_TYPED_DYNAMIC_SLICE_OP(float);
-ADD_TYPED_DYNAMIC_SLICE_OP(double);
-ADD_TYPED_DYNAMIC_SLICE_OP(MLFloat16);
-ADD_TYPED_DYNAMIC_SLICE_OP(bool);
-ADD_TYPED_DYNAMIC_SLICE_OP(string);
-
 }  // namespace contrib
 }  // namespace onnxruntime
