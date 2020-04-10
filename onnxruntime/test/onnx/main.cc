4b5f66ad17f4433425439e10810470e844bff532
Sergii Dymchenko <sedymche@microsoft.com>
2020-04-10 13:27:00 -0700

    Put dropout_default, dropout_random, celu back in the list of broken tests.

--- onnxruntime/test/onnx/main.cc
+++ onnxruntime/test/onnx/main.cc
@@ -479,6 +479,9 @@ int real_main(int argc, char* argv[], Ort::Env& env) {
       {"resize_upsample_sizes_nearest_round_prefer_ceil_asymmetric", "Bad onnx test output. Needs test fix."},
       {"bitshift_right_uint16", "BitShift(11) uint16 support not enabled currently"},
       {"bitshift_left_uint16", "BitShift(11) uint16 support not enabled currently"},
+      {"dropout_default", "result differs", {"onnxtip"}},
+      {"dropout_random", "result differs", {"onnxtip"}},
+      {"celu", "invalid model", {"onnxtip"}},
       {"maxunpool_export_with_output_shape", "Invalid output in ONNX test. See https://github.com/onnx/onnx/issues/2398"}
   };
 
