7a2ab6bc26cddfc2953baf99e34159152b32a7ac
liqun <liqun@OrtTrainingDev4.af05slrtruoetgaxwwjv5nsq5e.px.internal.cloudapp.net>
2020-04-13 17:52:38 +0000

    prepare e2e transformer tests

---
+++ orttraining/orttraining/test/python/helpers/bert_postprocess.py
@@ -0,0 +1,36 @@
+def postprocess_model(model):
+    # TODO: postprocess transforms are to be moved to either pytorch exporter or onnxruntime front end (ort_trainer.py)
+    from helpers.model_transform import add_name, process_concat, handle_expand_input_is_not_constant_case, fix_expand, fix_dim, fix_transpose, process_dropout, add_expand_shape
+
+    add_name(model)
+    #replace garther&concat to reshape
+    process_concat(model)
+
+    # will be longer needed after Range is supported in ORT.
+    handle_expand_input_is_not_constant_case(model)
+    
+    # fix the expand with dynamic shape
+    # will be longer needed after Range is supported in ORT.
+    fix_expand(model)
+
+    #use dynamic batch/sequence
+    fix_dim(model)
+
+    #constant fold transpose
+    #fix_transpose(model)
+
+    #replace dropout with trainable dropout
+    process_dropout(model)
+    
+    #add output shape of expand
+    # will be longer needed after Range is supported in ORT.
+    add_expand_shape(model)
+    #set opset version to 10
+    #model.opset_import[0].version = 10
+
+
+    ################
+    # layer_norm
+    ################
+    from helpers.layer_norm_transform import layer_norm_transform
+    layer_norm_transform(model)
