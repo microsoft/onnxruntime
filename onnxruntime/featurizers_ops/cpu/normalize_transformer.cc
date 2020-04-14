bc88d4b361102e27dfac9c6988d2dca87c0ad86a
wangyems <wangye@microsoft.com>
2020-04-13 20:47:56 -0700

    address some comments

--- onnxruntime/featurizers_ops/cpu/normalize_transformer.cc
+++ onnxruntime/featurizers_ops/cpu/normalize_transformer.cc
@@ -23,7 +23,7 @@ struct NormalizeTransformerImpl {
           const auto* state_tensor(ctx->Input<Tensor>(0));
           const uint8_t* const state_data(state_tensor->Data<uint8_t>());
 
-          Microsoft::Featurizer::Archive archive(state_data, state_tensor->Shape().GetDims()[0]);
+          Microsoft::Featurizer::Archive archive(state_data, state_tensor->Shape().Size());
           return Transformer(archive);
         }());
 
@@ -58,6 +58,7 @@ struct NormalizeTransformerImpl {
       std::copy(result.cbegin(), result.cend(), output_data);
       output_data += row_size;
     }
+    transformer.flush(callback);
   }
 };
 
