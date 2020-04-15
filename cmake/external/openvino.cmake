18177a30fe01f1a10e37deec05ac39a509a93257
Manohar Karlapalem <manohar.karlapalem@intel.com>
2020-04-15 02:52:17 -0700

    CMake code cleanup

--- cmake/external/openvino.cmake
+++ cmake/external/openvino.cmake
@@ -15,17 +15,7 @@ set(ngraph_branch "origin/rov.2020.2")
 
 # Libraries for python package.
 if (WIN32)
-    #set(NGRAPH_STATIC_LIB ngraph.lib)
     set(NGRAPH_SHARED_LIB ovep_ngraph.dll)
-#    set(NGRAPH_CPU_BACKEND_SHARED_LIB cpu_backend.dll)
-#    set(NGRAPH_IOMP5MD_SHARED_LIB libiomp5md.dll)
-#    set(NGRAPH_MKLDNN_SHARED_LIB mkldnn.dll)
-#    set(NGRAPH_MKLML_SHARED_LIB mklml.dll)
-#    if("${CMAKE_BUILD_TYPE}" STREQUAL "Debug")
-#        set(NGRAPH_TBB_SHARED_LIB tbb_debug.dll)
-#    else()
-#        set(NGRAPH_TBB_SHARED_LIB tbb.dll)
-#    endif()
 else()
     set(NGRAPH_SHARED_LIB libovep_ngraph.so)
 endif()
@@ -87,24 +77,24 @@ else()
             COMMAND git apply --ignore-space-change --ignore-whitespace ${PROJECT_SOURCE_DIR}/patches/openvino/ngraph_protobuf.patch
             COMMAND git apply --ignore-space-change --ignore-whitespace ${PROJECT_SOURCE_DIR}/patches/openvino/lib_name.patch
             CMAKE_ARGS
-            -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
-            -DNGRAPH_ONNX_IMPORT_ENABLE=TRUE
-            -DNGRAPH_JSON_ENABLE=FALSE
-            -DNGRAPH_USE_SYSTEM_PROTOBUF=FALSE
-            -DNGRAPH_UNIT_TEST_ENABLE=FALSE
-            -DNGRAPH_TEST_UTIL_ENABLE=FALSE
-            -DNGRAPH_TOOLS_ENABLE=FALSE
-            -DNGRAPH_CPU_ENABLE=FALSE
-            -DNGRAPH_INTERPRETER_ENABLE=FALSE
-            -DNGRAPH_NOP_ENABLE=FALSE
-            -DNGRAPH_GPU_ENABLE=FALSE
-            -DNGRAPH_GENERIC_CPU_ENABLE=FALSE
-            -DNGRAPH_PLAIDML_ENABLE=FALSE
-            -DNGRAPH_ENABLE_CPU_CONV_AUTO=FALSE
-            -DNGRAPH_PYTHON_BUILD_ENABLE=FALSE
-            -DNGRAPH_FAST_MATH_ENABLE=FALSE
-            -DNGRAPH_DYNAMIC_COMPONENTS_ENABLE=FALSE
-            -DNGRAPH_NATIVE_ARCH_ENABLE=FALSE
+                -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
+                -DNGRAPH_ONNX_IMPORT_ENABLE=TRUE
+                -DNGRAPH_JSON_ENABLE=FALSE
+                -DNGRAPH_USE_SYSTEM_PROTOBUF=FALSE
+                -DNGRAPH_UNIT_TEST_ENABLE=FALSE
+                -DNGRAPH_TEST_UTIL_ENABLE=FALSE
+                -DNGRAPH_TOOLS_ENABLE=FALSE
+                -DNGRAPH_CPU_ENABLE=FALSE
+                -DNGRAPH_INTERPRETER_ENABLE=FALSE
+                -DNGRAPH_NOP_ENABLE=FALSE
+                -DNGRAPH_GPU_ENABLE=FALSE
+                -DNGRAPH_GENERIC_CPU_ENABLE=FALSE
+                -DNGRAPH_PLAIDML_ENABLE=FALSE
+                -DNGRAPH_ENABLE_CPU_CONV_AUTO=FALSE
+                -DNGRAPH_PYTHON_BUILD_ENABLE=FALSE
+                -DNGRAPH_FAST_MATH_ENABLE=FALSE
+                -DNGRAPH_DYNAMIC_COMPONENTS_ENABLE=FALSE
+                -DNGRAPH_NATIVE_ARCH_ENABLE=FALSE
                 -DCMAKE_INSTALL_PREFIX=${ngraph_INSTALL_DIR}
                 -Dprebuilt_ONNX_BINARY_DIR=${prebuilt_ONNX_BINARY_DIR}
                 -Dprebuilt_ONNX_SOURCE_DIR=${prebuilt_ONNX_SOURCE_DIR}
