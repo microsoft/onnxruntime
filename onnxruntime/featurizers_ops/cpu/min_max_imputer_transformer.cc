7c9f9f42fc067143e932dfa18d98db77b2ea9232
Ye Wang <52801275+wangyems@users.noreply.github.com>
2020-04-15 11:10:24 -0700

    Resolve comments and make minor changes to Featurizer transformers

--- onnxruntime/featurizers_ops/cpu/min_max_imputer_transformer.cc
+++ onnxruntime/featurizers_ops/cpu/min_max_imputer_transformer.cc
@@ -45,7 +45,7 @@ struct MinMaxImputerTransformerImpl {
           const auto* state_tensor(ctx->Input<Tensor>(0));
           const uint8_t* const state_data(state_tensor->Data<uint8_t>());
 
-          Microsoft::Featurizer::Archive archive(state_data, state_tensor->Shape().GetDims()[0]);
+          Microsoft::Featurizer::Archive archive(state_data, state_tensor->Shape().Size());
           return MinMaxImputerTransformerT<T>(archive);
         }());
 
