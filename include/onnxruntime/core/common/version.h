7cce8c4e3084c42bf0eb8fa6988a2a96aaf72fa1
Xiang Zhang <xianz@microsoft.com>
2020-04-15 17:19:30 -0700

    deleted version.h

--- include/onnxruntime/core/common/version.h
+++
@@ -1,6 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
-#pragma once
-#include "onnxruntime_config.h"
-#define ONNXRUNTIME_VERSION_STRING ORT_VERSION
