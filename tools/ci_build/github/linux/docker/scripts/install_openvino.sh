9619a272451ee2f3a96c59a2cdaa8458a7dfa740
Aravind <aravindx.gunda@intel.com>
2020-04-16 23:29:45 -0700

    Updated DLDT instructions

--- tools/ci_build/github/linux/docker/scripts/install_openvino.sh
+++ tools/ci_build/github/linux/docker/scripts/install_openvino.sh
@@ -10,31 +10,30 @@ done
 OPENVINO_VERSION=${OPENVINO_VERSION:=2020.2}
 git clone https://github.com/opencv/dldt.git /data/dldt/openvino_2020.2.120
 
-export INTEL_CVSDK_DIR=/data/dldt/openvino_2020.2.120
+export INTEL_OPENVINO_DIR=/data/dldt/openvino_2020.2.120
 apt-get update && apt-get -y  install libusb-1.0-0-dev
 
-cd ${INTEL_CVSDK_DIR}/inference-engine
 git checkout tags/$OPENVINO_VERSION -b $OPENVINO_VERSION
 git submodule init
 git submodule update --recursive
+chmod +x install_dependencies.sh
+./install_dependencies.sh
+
 
 mkdir -p build
 cd build
 #cmake ..
-cmake -DNGRAPH_ONNX_IMPORT_ENABLE=ON -DNGRAPH_JSON_ENABLE=ON DNGRAPH_UNIT_TEST_ENABLE=OFF -DCMAKE_BUILD_TYPE=Release ..
-make -j$(nproc)
+cmake -DNGRAPH_ONNX_IMPORT_ENABLE=ON -DNGRAPH_UNIT_TEST_ENABLE=OFF -DCMAKE_BUILD_TYPE=Release ..
+make --jobs=$(nproc --all)
 
-cd ${INTEL_CVSDK_DIR}
+cd ${INTEL_OPENVINO_DIR}
 mkdir -p deployment_tools
 mv inference-engine inference_engine && mv inference_engine deployment_tools/
 ln -s deployment_tools/inference_engine inference-engine
 mv model-optimizer model_optimizer && mv model_optimizer deployment_tools/
+mv ngraph deployment_tools/
 
-cd ${INTEL_CVSDK_DIR}/deployment_tools/model_optimizer/install_prerequisites && ./install_prerequisites_onnx.sh
-pip3 install test-generator==0.1.1
-export InferenceEngine_DIR=${INTEL_CVSDK_DIR}/deployment_tools/inference_engine/build
-
-cd ${INTEL_CVSDK_DIR}/deployment_tools/inference_engine
+cd ${INTEL_OPENVINO_DIR}/deployment_tools/inference_engine
 mkdir -p lib/intel64
 mkdir -p external/tbb/lib
 cp -R bin/intel64/Release/lib/* lib/intel64
