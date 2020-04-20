2087f044d309bcc0ac82455147a37e861d718d8a
liqunfu <liqfu@microsoft.com>
2020-04-20 11:19:31 -0700

    Add front-end MNIST test (#3231)
    * add frontend minst test
    
    * to use torch nightly with torchvision
    
    * remove incorrect comment per reviewer's comment
    
    * experiment torchvision import failure
    
    * experiment install_deps.sh
    
    * more experiment install_deps.sh
    
    * experiment install_deps.sh with --upgrade
    
    * Experiment with install_deps.sh.
    
    * Experiment with install_ubuntu.sh.
    
    * Use Ubuntu 18.04 and Python 3.6 for CI.
    
    * Update cmake version for CI.
    
    * Install MPI on Ubuntu 18.04 for CI.
    
    * Increase tolerance for MNIST test.
    
    * Go back to Ubuntu 16.04 for CI, fix installing from deadsnakes ppa.
    
    * Clean-up.
    
    * Update ort_trainer.py from ort_training.
    
    * Get default Ubuntu Python ver back to 3.5.
    
    * Add underscore to opset_version parameter name in ORTTrainer constructor.
    
    * Move loss/model wrap before the call for sample output.
    
    * Update expected values for MNIST test.
    
    Co-authored-by: liqun <liqun@OrtTrainingDev4.af05slrtruoetgaxwwjv5nsq5e.px.internal.cloudapp.net>
    Co-authored-by: Sergii Dymchenko <sedymche@microsoft.com>

--- tools/ci_build/github/linux/run_dockerbuild.sh
+++ tools/ci_build/github/linux/run_dockerbuild.sh
@@ -14,7 +14,7 @@ o) BUILD_OS=${OPTARG};;
 #cpu, gpu, tensorrt
 d) BUILD_DEVICE=${OPTARG};;
 r) BUILD_DIR=${OPTARG};;
-#python version: 3.6 3.7 (absence means default 3.5)
+#python version: 3.6 3.7 (absence means default 3.6)
 p) PYTHON_VER=${OPTARG};;
 # "--build_wheel --use_openblas"
 x) BUILD_EXTR_PAR=${OPTARG};;
@@ -30,7 +30,7 @@ esac
 done
 
 EXIT_CODE=1
-PYTHON_VER=${PYTHON_VER:=3.5}
+PYTHON_VER=${PYTHON_VER:=3.6}
 echo "bo=$BUILD_OS bd=$BUILD_DEVICE bdir=$BUILD_DIR pv=$PYTHON_VER bex=$BUILD_EXTR_PAR"
 
 cd $SCRIPT_DIR/docker
