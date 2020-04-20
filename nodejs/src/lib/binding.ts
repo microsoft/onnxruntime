b2ae15159b05e898b9c832ca850e4672abdf4d2e
Yulong Wang <yulongw@microsoft.com>
2020-04-20 15:21:40 -0700

    adjust binding declaration

--- nodejs/src/lib/binding.ts
+++ nodejs/src/lib/binding.ts
@@ -1,11 +1,10 @@
 import {InferenceSession as InferenceSessionInterface} from './inference-session';
 import {OnnxValue} from './onnx-value';
 
+/**
+ * Binding exports a simple synchronized inference session object wrap.
+ */
 export declare namespace Binding {
-  type FeedsType = {[name: string]: OnnxValue};
-  type FetchesType = {[name: string]: OnnxValue | null};
-  type ReturnType = {[name: string]: OnnxValue};
-
   export interface InferenceSession {
     loadModel(modelPath: string): void;
     loadModel(buffer: ArrayBuffer, byteOffset: number, byteLength: number): void;
@@ -13,7 +12,15 @@ export declare namespace Binding {
     readonly inputNames: string[];
     readonly outputNames: string[];
 
-    run(feeds: FeedsType, fetches: FetchesType, options: InferenceSessionInterface.RunOptions): ReturnType;
+    run(feeds: InferenceSession.FeedsType, fetches: InferenceSession.FetchesType,
+        options: InferenceSession.RunOptions): InferenceSession.ReturnType;
+  }
+
+  export namespace InferenceSession {
+    type FeedsType = {[name: string]: OnnxValue};
+    type FetchesType = {[name: string]: OnnxValue | null};
+    type ReturnType = {[name: string]: OnnxValue};
+    type RunOptions = InferenceSessionInterface.RunOptions;
   }
 
   export interface InferenceSessionConstructor {
@@ -21,9 +28,6 @@ export declare namespace Binding {
   }
 }
 
-// construct binding file path
-const GPU_ENABLED = false;  // TODO: handle GPU
-
+// export native binding
 export const binding =
-    require(`../bin/napi-v4/${GPU_ENABLED ? 'gpu' : 'cpu'}/onnxruntime_binding${GPU_ENABLED ? '_gpu' : ''}.node`) as
-    {InferenceSession: Binding.InferenceSessionConstructor};
+    require(`../bin/napi-v4/cpu/onnxruntime_binding.node`) as {InferenceSession: Binding.InferenceSessionConstructor};
