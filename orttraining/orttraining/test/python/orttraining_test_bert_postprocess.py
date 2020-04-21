8a06d59618fc6e89c348778a49e20bba12854607
liqun <liqun@OrtTrainingDev4.af05slrtruoetgaxwwjv5nsq5e.px.internal.cloudapp.net>
2020-04-21 21:20:34 +0000

    skip a few un-needed post-process steps

--- orttraining/orttraining/test/python/orttraining_test_bert_postprocess.py
+++ orttraining/orttraining/test/python/orttraining_test_bert_postprocess.py
@@ -5,8 +5,8 @@ from orttraining_test_layer_norm_transform import layer_norm_transform
 def postprocess_model(model):
 
     add_name(model)
-    #replace garther&concat to reshape
-    process_concat(model)
+    # #replace garther&concat to reshape
+    # process_concat(model)
 
     # # will be longer needed after Range is supported in ORT.
     # handle_expand_input_is_not_constant_case(model)
@@ -15,14 +15,14 @@ def postprocess_model(model):
     # # will be longer needed after Range is supported in ORT.
     # fix_expand(model)
 
-    #use dynamic batch/sequence
-    fix_dim(model)
+    # #use dynamic batch/sequence
+    # fix_dim(model)
 
     #constant fold transpose
-    #fix_transpose(model)
+    fix_transpose(model)
 
-    #replace dropout with trainable dropout
-    process_dropout(model)
+    # #replace dropout with trainable dropout
+    # process_dropout(model)
     
     #add output shape of expand
     # will be longer needed after Range is supported in ORT.
