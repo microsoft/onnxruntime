cfb747a3d28353773d3104ba2a06fee470d2a1aa
Peng Wang (pengwa) <pengwa@microsoft.com>
2020-04-22 04:03:56 +0000

    Revert "use MLTypeCallDispatcherRet to refactor"
    This reverts commit b487b19047bf1af97f3e22effaf48865d92130ec.

--- onnxruntime/core/providers/cuda/tensor/scatter_elements.h
+++ onnxruntime/core/providers/cuda/tensor/scatter_elements.h
@@ -19,11 +19,9 @@ class ScatterElements final : public CudaKernel {
   Status ComputeInternal(OpKernelContext* context) const override;
 
  private:
-  template <typename T>
-  struct ComputeImpl;
-
   int64_t axis_;
 };
 
 }  // namespace cuda
 }  // namespace onnxruntime
+
