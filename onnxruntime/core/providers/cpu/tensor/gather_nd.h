5ba22fa8b05954f8cd7ae3dba7b106ac8736b46a
Peng Wang (pengwa) <pengwa@microsoft.com>
2020-04-24 13:45:22 +0000

    Refactor GatherND CPU Kernel (Renaming & Simplify)

--- onnxruntime/core/providers/cpu/tensor/gather_nd.h
+++ onnxruntime/core/providers/cpu/tensor/gather_nd.h
@@ -16,19 +16,19 @@ class GatherNDBase {
     const std::string* input_str_base;
     uint8_t* output_base;
     std::string* output_str_base;
-    uint64_t bytes_to_copy;
+    uint64_t bytes_per_slice;
     uint64_t element_bytes;
-    uint64_t element_to_copy;
-    std::vector<uint64_t> element_offsets;
+    uint64_t element_count_per_slice;
+    std::vector<uint64_t> slice_offsets;
 
     Prepare() : input_base(nullptr),
                 input_str_base(nullptr),
                 output_base(nullptr),
                 output_str_base(nullptr),
-                bytes_to_copy(0),
+                bytes_per_slice(0),
                 element_bytes(0),
-                element_to_copy(0),
-                element_offsets(0) {}
+                element_count_per_slice(0),
+                slice_offsets(0) {}
   };  // struct Prepare
 
   template <typename Tind>
