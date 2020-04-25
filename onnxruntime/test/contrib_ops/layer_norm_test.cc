14ce9bbd6090530e8916c3bba0ca4d730674474b
Changming Sun <chasun@microsoft.com>
2020-04-25 11:07:32 -0700

    update

--- onnxruntime/test/contrib_ops/layer_norm_test.cc
+++ onnxruntime/test/contrib_ops/layer_norm_test.cc
@@ -1,7 +1,16 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+#pragma once
+#include "onnxruntime_config.h"
+#if defined(__GNUC__)
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+#endif
 #include "Eigen/Core"
+#if defined(__GNUC__)
+#pragma GCC diagnostic pop
+#endif
 
 #include "test/providers/compare_provider_test_utils.h"
 
