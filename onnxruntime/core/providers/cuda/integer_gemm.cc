c8e9d93b40f237b3fdaaf155a30851b5b45a0403
Yufeng Li <liyufeng1987@gmail.com>
2020-04-25 21:41:21 -0700

    fix warning

--- onnxruntime/core/providers/cuda/integer_gemm.cc
+++ onnxruntime/core/providers/cuda/integer_gemm.cc
@@ -25,18 +25,18 @@ Status GemmInt8(int m, int n, int k,
   // 1. leading dimension is multiples of 4
   // 2. A, B is 32-bit aligned
 
-  const int64_t mask = 0x1F;
-  int64_t lda_aligned = lda;
+  const int mask = 0x1F;
+  int lda_aligned = lda;
   IAllocatorUniquePtr<int8_t> a_padded;
-  if (mask & lda_aligned != 0) {
+  if ((mask & lda_aligned) != 0) {
     lda_aligned = roundoff(lda, 32);
     a_padded = cuda_kernel->GetScratchBuffer<int8_t>(m * lda_aligned);
     cudaMemcpy2DAsync(a_padded.get(), lda_aligned, a, lda, k, m, cudaMemcpyDeviceToDevice, 0);
   }
 
-  int64_t ldb_aligned = ldb;
+  int ldb_aligned = ldb;
   IAllocatorUniquePtr<int8_t> b_padded;
-  if (mask & ldb_aligned) {
+  if ((mask & ldb_aligned) != 0) {
     ldb_aligned = roundoff(ldb, 32);
     b_padded = cuda_kernel->GetScratchBuffer<int8_t>(k * ldb_aligned);
     cudaMemcpy2DAsync(b_padded.get(), ldb_aligned, b, ldb, n, k, cudaMemcpyDeviceToDevice, 0);
