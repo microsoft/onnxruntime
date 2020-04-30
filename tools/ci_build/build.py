ea6242e6d55ef5d573c0c694a27eca53aa94e997
Edward Chen <18449977+edgchen1@users.noreply.github.com>
2020-04-30 02:01:56 +0000

    Disable Python training tests.

--- tools/ci_build/build.py
+++ tools/ci_build/build.py
@@ -153,6 +153,9 @@ def parse_arguments():
     parser.add_argument(
         "--skip-keras-test", action='store_true',
         help="Skip tests with Keras if keras is installed")
+    parser.add_argument(
+        "--skip_python_training_tests", action='store_true',
+        help="Skip Python training tests.")
 
     # C-Sharp bindings
     parser.add_argument(
@@ -1090,7 +1093,7 @@ def run_onnxruntime_tests(args, source_dir, ctest_path, build_dir, configs,
                 [sys.executable, 'onnxruntime_test_python.py'],
                 cwd=cwd, dll_path=dll_path)
 
-            if args.enable_training and args.use_cuda:
+            if not args.skip_python_training_tests and args.enable_training and args.use_cuda:
                 # run basic frontend tests
                 run_subprocess(
                     [sys.executable, 'onnxruntime_test_ort_trainer.py'],
