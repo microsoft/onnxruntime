44b1f28a9491bfa1fa1247f940ce814865209cb1
root <root@525204a066204ea794f942530b05ae7f000000.axlncovkyjne5caro2tmz3zryb.xx.internal.cloudapp.net>
2020-05-01 22:24:42 +0000

    fixed seg fault when using concrete shape disable gradient as output

--- orttraining/orttraining/python/orttraining_pybind_state.cc
+++ orttraining/orttraining/python/orttraining_pybind_state.cc
@@ -115,7 +115,7 @@ TrainingConfigurationResult ConfigureSessionForTraining(
   config.loss_name = parameters.loss_output_name;
 
   if (!parameters.training_optimizer_name.empty()) {
-    config.set_gradients_as_graph_outputs = true;
+    config.set_gradients_as_graph_outputs = false;
     training::TrainingSession::TrainingConfiguration::OptimizerConfiguration opt{};
     opt.name = parameters.training_optimizer_name;
     opt.learning_rate_input_name = parameters.lr_params_feed_name;
