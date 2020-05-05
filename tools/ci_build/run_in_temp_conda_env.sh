f4f5265555767ffebcfd9c88146384b81c7282a9
Edward Chen <18449977+edgchen1@users.noreply.github.com>
2020-05-05 19:57:19 +0000

    clean up run in conda env script.

--- tools/ci_build/run_in_temp_conda_env.sh
+++ tools/ci_build/run_in_temp_conda_env.sh
@@ -1,23 +1,32 @@
 #!/bin/bash
 
-ENV_DIR=$(mktemp -d)
+# use environment variables for input, command line arguments are run directly
 PYTHON_VERSION=${PYTHON_VERSION:-3.6}
+CONDA_INSTALL_DIR=${CONDA_INSTALL_DIR:?"Please specify the conda install directory in \$CONDA_INSTALL_DIR."}
 
-# enable "conda activate" from bash script
-eval "$(conda shell.bash hook)"
+# enable conda commands from bash script
+source ${CONDA_INSTALL_DIR}/etc/profile.d/conda.sh
 
-conda create --yes --prefix ${ENV_DIR} python=${PYTHON_VERSION} || exit 1
+function clean_up_and_exit() {
+  local env_dir=$1
+  local exit_code=$2
 
-conda activate ${ENV_DIR}
+  if [ "${CONDA_DEFAULT_ENV}" == "${env_dir}" ]; then
+    conda deactivate
+  fi
+
+  if [ -d "${env_dir}" ]; then
+    conda env remove --yes --quiet --prefix ${env_dir}
+    rm -rf ${env_dir}
+  fi
+
+  exit ${exit_code}
+}
+
+ENV_DIR=$(mktemp -d)
+conda create --yes --quiet --prefix ${ENV_DIR} python=${PYTHON_VERSION} || clean_up_and_exit ${ENV_DIR} 1
+conda activate ${ENV_DIR} || clean_up_and_exit ${ENV_DIR} 1
 
 $@
-COMMAND_RESULT=$?
 
-conda deactivate
-
-conda env remove --yes --prefix ${ENV_DIR}
-
-# clean up just in case "conda env remove" doesn't
-rm -rf "${ENV_DIR}"
-
-exit $COMMAND_RESULT
+clean_up_and_exit ${ENV_DIR} $?
