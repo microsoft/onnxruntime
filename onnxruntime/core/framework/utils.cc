edaf8a542c21b7f38cfc871fff9540ceb0e56951
airockchip <57983059+airockchip@users.noreply.github.com>
2020-05-06 11:36:47 +0800

    Initial PR for RKNPU execution provider (#3609)
    * Initial RKNPU execution provider
    
        * Init
    
        * Support Ops:
            Conv, Relu, Clip, LeakyRelu,
            MaxPool, AveragePool, GlobalAveragePool,
            Concat, Softmax, BatchNormalization, Gemm,
            Add, Mul, Sub,
            Reshape, Squeeze, Unsqueeze,
            Flatten, Transpose,
            QLinearConv, DequantizeLinear
    
        * Add rknpu unittest
    
        * Update BUILD.md and Add RKNPU-ExecutionProvider.md
    
    * misc code update
    
    * fix CLIP accuracy issue.
    
    * fix "Error: Duplicate definition of name".
    
    * move rknpu_ddk out of onnxruntime submodule.
    
    * remove temporary code.
    
    * add rknpu namespace.
    
    * update misc of node_attr_helper
    
    * add const & comment for onnx_converter
    
    * add const & comment for shaper
    
    * unify variable name
    
    Co-authored-by: dkm <dkm@rock-chips.com>
    Co-authored-by: George Wu <jywu@microsoft.com>

--- onnxruntime/core/framework/utils.cc
+++ onnxruntime/core/framework/utils.cc
@@ -100,7 +100,8 @@ bool ProviderIsCpuBased(const std::string& provider_type) {
          provider_type == onnxruntime::kNGraphExecutionProvider ||
          provider_type == onnxruntime::kNupharExecutionProvider ||
          provider_type == onnxruntime::kOpenVINOExecutionProvider ||
-         provider_type == onnxruntime::kNnapiExecutionProvider;
+         provider_type == onnxruntime::kNnapiExecutionProvider ||
+         provider_type == onnxruntime::kRknpuExecutionProvider;
 }
 
 common::Status AllocateHelper(const IExecutionProvider& execution_provider, const OrtDevice& device,
