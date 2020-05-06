f7ec1f929e3c1d955336af317b21645dbd6d6f24
liqun <liqun@OrtTrainingDev4.af05slrtruoetgaxwwjv5nsq5e.px.internal.cloudapp.net>
2020-05-06 14:44:13 +0000

    try pip install transformers in build.sh

--- tools/ci_build/github/linux/run_build.sh
+++ tools/ci_build/github/linux/run_build.sh
@@ -67,8 +67,8 @@ else
                 --cuda_home /usr/local/cuda \
                 --cudnn_home /usr/local/cuda $BUILD_EXTR_PAR
         else
-            # pip install /transformers/ --upgrade
-            python3 -m pip install /transformers/ --upgrade
+            pip install /transformers/ --upgrade
+            # python3 -m pip install /transformers/ --upgrade
 
             _CUDNN_VERSION=$(echo $CUDNN_VERSION | cut -d. -f1-2)
             python3 $SCRIPT_DIR/../../build.py --build_dir /build \
