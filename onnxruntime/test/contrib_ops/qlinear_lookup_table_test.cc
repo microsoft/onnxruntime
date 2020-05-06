b46b9f28aa45b9282ef5ecb6928a4b7ff112b149
Lei Zhang <zhang.huanning@hotmail.com>
2020-05-06 15:14:00 -0700

    Fix test case rounding result related with rounding mode.

--- onnxruntime/test/contrib_ops/qlinear_lookup_table_test.cc
+++ onnxruntime/test/contrib_ops/qlinear_lookup_table_test.cc
@@ -2,6 +2,8 @@
 #include "test/common/tensor_op_test_utils.h"
 #include "test/providers/provider_test_utils.h"
 
+#include <cfenv>
+
 namespace onnxruntime {
 namespace test {
 
@@ -14,13 +16,16 @@ TEST(QLinearLookupTableBasedOperatorTests, QLinearLeakyRelu_Int8) {
   int8_t Y_zero_point = -100;
 
   std::vector<int64_t> dims = {16};
-  test.AddInput<int8_t>("X", dims, {0, 16, 17,  18, 19, 90, 91, 127, -128, -110, -108, -100, -16, -17, -18, -1});
+  test.AddInput<int8_t>("X", dims, {0, 16, 17, 18, 19, 90, 91, 127, -128, -110, -108, -100, -16, -17, -18, -1});
   test.AddInput<float>("X_scale", {}, {X_scale});
   test.AddInput<int8_t>("X_zero_point", {}, {X_zero_point});
   test.AddInput<float>("Y_scale", {}, {Y_scale});
   test.AddInput<int8_t>("Y_zero_point", {}, {Y_zero_point});
-  test.AddOutput<int8_t>("Y", dims, {-100, -60, -57, -55, -52, 125, 127, 127, -128, -128, -127, -125, -104, -104, -105, -100});
+  test.AddOutput<int8_t>("Y", dims, {-100, -60, -58, -55, -52, 125, 127, 127, -128, -128, -127, -125, -104, -104, -104, -100});
+  auto origin_round_mode = std::fegetround();
+  std::fesetround(FE_TONEAREST);
   test.Run();
+  std::fesetround(origin_round_mode);
 }
 
 
@@ -38,8 +43,11 @@ TEST(QLinearLookupTableBasedOperatorTests, QLinearLeakyRelu_UInt8) {
   test.AddInput<uint8_t>("X_zero_point", {}, {X_zero_point});
   test.AddInput<float>("Y_scale", {}, {Y_scale});
   test.AddInput<uint8_t>("Y_zero_point", {}, {Y_zero_point});
-  test.AddOutput<uint8_t>("Y", dims, {0, 2, 2, 2, 3, 20, 21, 30, 30, 50, 53, 55, 250, 253, 255, 255});
+  test.AddOutput<uint8_t>("Y", dims, {0, 2, 2, 2, 3, 20, 21, 30, 30, 50, 52, 55, 250, 252, 255, 255});
+  auto origin_round_mode = std::fegetround();
+  std::fesetround(FE_TONEAREST);
   test.Run();
+  std::fesetround(origin_round_mode);
 }
 
 }
