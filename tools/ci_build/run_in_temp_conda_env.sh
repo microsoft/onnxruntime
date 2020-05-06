9a362156b2dbb94ea267afd9b64b1c7b24e46992
Edward Chen <18449977+edgchen1@users.noreply.github.com>
2020-05-06 00:49:23 +0000

    clean up and copy over changes from test build

--- tools/ci_build/run_in_temp_conda_env.sh
+++ tools/ci_build/run_in_temp_conda_env.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-# use environment variables for input, command line arguments are run directly
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+# Creates and activates a temporary conda environment and runs the specified command.
+# Usage example:
+#   $ PYTHON_VERSION=3.7 CONDA_INSTALL_DIR=/opt/miniconda3 run_in_temp_conda_env.sh python --version
+
 PYTHON_VERSION=${PYTHON_VERSION:-3.6}
 CONDA_INSTALL_DIR=${CONDA_INSTALL_DIR:?"Please specify the conda install directory in \$CONDA_INSTALL_DIR."}
 
 # enable conda commands from bash script
-source ${CONDA_INSTALL_DIR}/etc/profile.d/conda.sh
+source ${CONDA_INSTALL_DIR}/etc/profile.d/conda.sh || exit 1
 
 function clean_up_and_exit() {
   local env_dir=$1
