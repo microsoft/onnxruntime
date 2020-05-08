20c015ffe12c778e4b413ef2ff283f555a07c7fa
hariharans29 <shariharan91@gmail.com>
2020-05-07 22:39:32 -0700

    Remove unnecessary header inclusion

--- onnxruntime/core/optimizer/constant_folding.cc
+++ onnxruntime/core/optimizer/constant_folding.cc
@@ -5,7 +5,6 @@
 #include "core/graph/graph_utils.h"
 #include "core/optimizer/optimizer_execution_frame.h"
 #include "core/framework/op_kernel.h"
-#include "core/framework/kernel_registry.h"
 #include "core/framework/tensorprotoutils.h"
 
 using namespace onnxruntime::common;
