7e90a2b46bd08ec2224cd63a3fee21d52a5e57ec
Thiago Crepaldi <thiago.crepaldi@microsoft.com>
2020-05-08 14:43:22 -0700

    Address review

--- orttraining/pytorch_frontend_examples/mnist_training.py
+++ orttraining/pytorch_frontend_examples/mnist_training.py
@@ -37,41 +37,6 @@ class NeuralNet(nn.Module):
 def my_loss(x, target):
     return F.nll_loss(F.log_softmax(x, dim=1), target)
 
-def train_with_model(args, model, device, train_loader, optimizer, epoch):
-    model.train()
-    for batch_idx, (data, target) in enumerate(train_loader):
-        data, target = data.to(device), target.to(device)
-        data = data.reshape(data.shape[0], -1)
-        loss, pred = model.run(data, target)
-        if batch_idx % args.log_interval == 0:
-            optimizer.step()
-            optimizer.zero_grad()
-            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                epoch, batch_idx * len(data), len(train_loader.dataset),
-                100. * batch_idx / len(train_loader), loss.item()))
-
-def test_with_model(args, model, device, test_loader, optimizer, epoch):
-    model.eval()
-    test_loss = 0
-    correct = 0
-    with torch.no_grad():
-        for data, target in test_loader:
-            data, target = data.to(device), target.to(device)
-            data = data.reshape(data.shape[0], -1)
-
-            pred = model.run(data, target, )
-
-            output = F.log_softmax(model.eval(data), dim=1)
-            test_loss += F.nll_loss(output, target, reduction='sum').item()     # sum up batch loss
-            pred = output.argmax(dim=1, keepdim=True)   # get the index of the max log-probability
-            correct += pred.eq(target.view_as(pred)).sum().item()
-
-    test_loss /= len(test_loader.dataset)
-
-    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
-        test_loss, correct, len(test_loader.dataset),
-        100. * correct / len(test_loader.dataset)))
-
 def train_with_trainer(args, trainer, device, train_loader, epoch):
     for batch_idx, (data, target) in enumerate(train_loader):
         data, target = data.to(device), target.to(device)
