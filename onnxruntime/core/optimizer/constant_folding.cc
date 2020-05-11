06985a992226ba57036f503e709839e0b6504d0d
Hariharan Seshadri <shariharan91@gmail.com>
2020-05-10 19:08:37 -0700

    Stop proceeding with constant folding if a CPU kernel is not found found (#3836)
    * Stop proceeding with constant folding if a CPU kernel is not found for a node
    
    * Fix build
    
    * PR feedback
    
    * Fix typo
    
    * Refine
    
    * Remove unnecessary header inclusion
    
    * Refine
    
    * Fix build
    
    * More changes
    
    * More changes
    
    * More changes
    
    * Fix CentOS build

--- onnxruntime/core/optimizer/constant_folding.cc
+++ onnxruntime/core/optimizer/constant_folding.cc
@@ -74,10 +74,10 @@ Status ConstantFolding::ApplyImpl(Graph& graph, bool& modified, int graph_level,
       InitializedTensorSet constant_inputs;
 
       // we currently constant fold using the CPU EP only.
-      // if the node is assigned to a different EP we can run it if it's an ONNX op as we have CPU based implementations
-      // for all ONNX ops. if it's from a different domain we can't.
-      // NOTE: This is in addition to the IsSupportedProvider check below which will optionally do further filtering
-      // on the EPs we constant fold for.
+      // if the node is assigned to a different EP we can run it if it's an ONNX op as we have CPU based
+      // implementations for all ONNX ops. If the node/op is from a different op domain or if the CPU implementation
+      // does not support the specific input type(s) required by the node (currently we only support a subset of
+      // types in some CPU kernels) then we can't proceed with constant folding for the node.
       auto ep_type = node->GetExecutionProviderType();
       bool cpu_ep = ep_type == kCpuExecutionProvider;
       if (!cpu_ep && node->Domain() != kOnnxDomain) {
@@ -94,32 +94,41 @@ Status ConstantFolding::ApplyImpl(Graph& graph, bool& modified, int graph_level,
         continue;
       }
 
-      // override the EP while setting up OptimizerExecutionFrame::Info so that it will use the CPU kernel for Compute.
-      if (!cpu_ep) {
-        node->SetExecutionProviderType(kCpuExecutionProvider);
-      }
+      // Create execution frame for executing constant nodes.
+      std::unique_ptr<CPUExecutionProvider> cpu_execution_provider =
+          onnxruntime::make_unique<CPUExecutionProvider>(CPUExecutionProviderInfo());
 
       // Create execution frame for executing constant nodes.
-      OptimizerExecutionFrame::Info info({node}, constant_inputs);
-
-      // undo the EP change in case something fails prior to node removal
-      if (!cpu_ep) {
-        node->SetExecutionProviderType(ep_type);
-      }
+      OptimizerExecutionFrame::Info info({node}, constant_inputs, std::move(cpu_execution_provider));
 
       std::vector<int> fetch_mlvalue_idxs;
       for (const auto* node_out : node->OutputDefs()) {
         fetch_mlvalue_idxs.push_back(info.GetMLValueIndex(node_out->Name()));
       }
 
-      OptimizerExecutionFrame frame(info, fetch_mlvalue_idxs);
+      // override the EP assigned to the node so that it will use the CPU kernel for Compute.
+      if (!cpu_ep) {
+        node->SetExecutionProviderType(kCpuExecutionProvider);
+      }
 
-      auto* kernel = info.GetKernel(node->Index());
+      auto kernel = info.CreateKernel(node);
+
+      // undo the EP change to the value that was assigned at graph partitioning time
+      if (!cpu_ep) {
+        node->SetExecutionProviderType(ep_type);
+      }
+
       if (kernel == nullptr) {
+        LOGS(logger, WARNING) << "Could not find a CPU kernel and hence "
+                              << "can't constant fold " << node->OpType() << " node '" << node->Name() << "'";
+
+        // Move on to the next candidate node
         continue;
       }
 
-      OpKernelContext op_kernel_context(&frame, kernel, nullptr, logger);
+      OptimizerExecutionFrame frame(info, fetch_mlvalue_idxs);
+
+      OpKernelContext op_kernel_context(&frame, kernel.get(), nullptr, logger);
       ORT_RETURN_IF_ERROR(kernel->Compute(&op_kernel_context));
 
       std::vector<OrtValue> fetches;
