e73c790cd01ad389c93a595acf026d72a0b4be6c
Hariharan Seshadri <shariharan91@gmail.com>
2020-05-11 19:49:51 -0700

    More changes

--- onnxruntime/python/_pybind_state.py
+++ onnxruntime/python/_pybind_state.py
@@ -4,11 +4,18 @@
 #--------------------------------------------------------------------------
 import sys
 import os
+import platform
 import warnings
 import onnxruntime.capi._ld_preload
 
 try:
     from onnxruntime.capi.onnxruntime_pybind11_state import *  # noqa
 except ImportError as e:
-    warnings.warn("Cannot load onnxruntime.capi. Error: '{0}'.".format(str(e)) +
-                  "Please install the Visual C++ 2019 runtime if you haven't already done so")
+    warnings.warn("Cannot load onnxruntime.capi. Error: '{0}'.".format(str(e)))
+    
+	# If on Windows, check if this import error is caused by the user not installing the 2019 VC Runtime
+    if platform.system().lower() == 'windows':
+        # The VC Redist installer usually puts the VC Runtime dlls in the System32 folder
+        if not os.path.isfile('c:\\Windows\\System32\\vcruntime140_1.dll'):
+            warnings.warn("Please install the Visual C++ 2019 runtime and then try again")
+            
