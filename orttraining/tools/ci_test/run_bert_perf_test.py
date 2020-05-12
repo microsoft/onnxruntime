ecc053003b0ae5817f6ed364ed58b3c587d5bde7
Ethan Tao <ettao@microsoft.com>
2020-05-11 20:13:03 -0700

    yaml/perf scripts for new pipeline

--- orttraining/tools/ci_test/run_bert_perf_test.py
+++ orttraining/tools/ci_test/run_bert_perf_test.py
@@ -65,34 +65,6 @@ def main():
 
         subprocess.run(cmds).check_returncode()
 
-    # run BERT training
-    # subprocess.run([
-    #     os.path.join(args.binary_dir, "onnxruntime_training_bert"),
-    #     "--model_name", os.path.join(
-    #         args.model_root, "nv/bert-large/bert-large-uncased_L_24_H_1024_A_16_V_30528_S_512_Dp_0.1_optimized_layer_norm"),
-    #     "--train_data_dir", os.path.join(
-    #         args.training_data_root, "128/books_wiki_en_corpus/train"),
-    #     "--test_data_dir", os.path.join(
-    #         args.training_data_root, "128/books_wiki_en_corpus/test"),
-    #     "--train_batch_size", "64",
-    #     "--mode", "train",
-    #     "--num_train_steps", "100",
-    #     "--display_loss_steps", "5",
-    #     "--optimizer", "adam",
-    #     "--learning_rate", "5e-4",
-    #     "--warmup_ratio", "0.1",
-    #     "--warmup_mode", "Linear",
-    #     "--gradient_accumulation_steps", "1",
-    #     "--max_predictions_per_seq=20",
-    #     "--use_mixed_precision",
-    #     "--allreduce_in_fp16",
-    #     "--lambda", "0",
-    #     "--use_nccl",
-    #     "--seed", "42",
-    #     "--enable_grad_norm_clip=false",
-    #     "--perf_output_dir", os.path.join(SCRIPT_DIR, "results"), 
-    # ]).check_returncode()
-
     return 0
 
 if __name__ == "__main__":
