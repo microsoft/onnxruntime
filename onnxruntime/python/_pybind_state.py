79b1f1b0851ff2e6c76070029c7da669036e92cf
Hariharan Seshadri <shariharan91@gmail.com>
2020-05-11 20:02:00 -0700

    More changes

--- onnxruntime/python/_pybind_state.py
+++ onnxruntime/python/_pybind_state.py
@@ -18,7 +18,8 @@ except ImportError as e:
     # This may not always paint the true picture as anyone building from source using VS 2017 might hit this error
    	# because the machine might be missing the 2019 VC Runtime but it is not actually needed in that case and the 
     # import error might actually be due to some other reason.
-    # TODO: Guard against False Positive error message
+    # TODO: Add a guard against False Positive error message
+    # As a proxy for checking if the 2019 VC Runtime is installed, we look for a specific dll only shipped with the 2019 VC Runtime
     if platform.system().lower() == 'windows' and not os.path.isfile('c:\\Windows\\System32\\vcruntime140_1.dll'):
             warnings.warn("Please install the Visual C++ 2019 runtime and then try again")
             
