9e6d2c2ebb3e5713dea58d8a80bb54e0e8f7b162
Edward Chen <18449977+edgchen1@users.noreply.github.com>
2020-05-13 02:37:13 +0000

    disable testBertCheckpointingBasic test

--- tools/ci_build/run_python_tests_with_wheel.py
+++ tools/ci_build/run_python_tests_with_wheel.py
@@ -12,6 +12,7 @@ SCRIPT_DIR = os.path.realpath(os.path.dirname(__file__))
 REPO_DIR = os.path.normpath(os.path.join(SCRIPT_DIR, "..", ".."))
 PYTHON_TEST_DIR = os.path.join(REPO_DIR, "onnxruntime", "test", "python")
 
+
 def run_python_tests_with_wheel(wheel_path):
     if not os.path.exists(wheel_path):
         raise ValueError("Wheel file not found: {}".format(wheel_path))
@@ -35,8 +36,10 @@ def run_python_tests_with_wheel(wheel_path):
     pip("install", "pytest")
 
     if training_enabled:
-        pip("install", "--pre", "torch", "torchvision",
-            "-f", "https://download.pytorch.org/whl/nightly/cu101/torch_nightly.html")
+        pip(
+            "install", "--pre", "torch", "torchvision", "-f",
+            "https://download.pytorch.org/whl/nightly/cu101/torch_nightly.html"
+        )
 
     # run tests
     test_paths = [
@@ -45,17 +48,24 @@ def run_python_tests_with_wheel(wheel_path):
 
     if training_enabled:
         test_paths += [
-            os.path.join(PYTHON_TEST_DIR, "onnxruntime_test_training_unit_tests.py"),
+            os.path.join(PYTHON_TEST_DIR,
+                         "onnxruntime_test_training_unit_tests.py"),
             os.path.join(PYTHON_TEST_DIR, "onnxruntime_test_ort_trainer.py"),
         ]
 
     for test_path in test_paths:
-        run(sys.executable, "-m", "pytest", test_path)
+        run(sys.executable, "-m", "pytest", test_path, "-k",
+            "not testBertCheckpointingBasic")
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description="Installs the specified onnxruntime wheel and test dependencies and runs Python tests.")
-    parser.add_argument("--wheel-path", required=True, help="Path to the wheel file.")
+        description=
+        "Installs the specified onnxruntime wheel and test dependencies and runs Python tests."
+    )
+    parser.add_argument("--wheel-path",
+                        required=True,
+                        help="Path to the wheel file.")
 
     args = parser.parse_args()
 
