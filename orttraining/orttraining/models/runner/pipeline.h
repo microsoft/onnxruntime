33208c9f6bad07512a67bc953ef9886953736fa3
Wei-Sheng Chin <wschin@outlook.com>
2020-05-15 18:27:19 -0700

    Modify Pipeline Facilities to Fix PipeDream Deadlock (#3823)
    * Prepare utils for adding Wait's and Record's
    
    * Have a running PipeDream
    
    * Add comments
    
    * Polish comments
    
    * Clean code
    
    * Fix test
    
    * Polish names
    
    * Polish names
    
    * Remove debug headers
    
    * Fix a shape inference bug (not related to pipeline code)
    
    * Fix a warning
    
    * Address some comments
    
    * Address comments
    
    * Only touch consumers of outputs when re-wire edges

--- orttraining/orttraining/models/runner/pipeline.h
+++ orttraining/orttraining/models/runner/pipeline.h
@@ -17,33 +17,61 @@ namespace training {
 namespace pipeline {
 
 struct Slot {
-  enum Type { Empty, Forward, Backward };
-  
-  Slot(); 
-  bool IsEmpty() const; 
-  bool IsForward() const; 
-  bool IsBackward() const; 
-  void Show() const; 
+  enum Type { Empty,
+              Forward,
+              Backward };
+
+  Slot();
+  bool IsEmpty() const;
+  bool IsForward() const;
+  bool IsBackward() const;
+  void Show() const;
 
   Type type;
   int batch_id;
+
+  // If type is Forward,
+  //  waited_events[0]: The first (before forward Recv)
+  //  waited event in forward pass.
+  //  waited_events[1]: The second (after forward Recv)
+  //  waited event in forward pass.
+  // If type is Backward,
+  //  waited_events[0]: The first (before backward Recv)
+  //  waited event in backward pass.
+  //  waited_events[1]: The second (after backward Recv)
+  //  waited event in backward pass.
   std::vector<int> waited_events;
+
+  // If type is Forward,
+  //  recorded_events[0]: The first (before forward Send)
+  //  recorded event in forward pass.
+  //  recorded_events[1]: The second (after forward Send)
+  //  recorded event in forward pass.
+  // If type is Backward,
+  //  recorded_events[0]: The first (before backward Send)
+  //  recorded event in backward pass.
+  //  recorded_events[1]: The second (after backward Send)
+  //  recorded event in backward pass.
   std::vector<int> recorded_events;
 };
 
 class PipelineSchedule {
-public:
+ public:
   PipelineSchedule() = default;
   PipelineSchedule(int num_stages);
   void Add(int batch_id);
   void Add(int batch_id_begin, int batch_id_end);
   int GetForwardWaitedEventId(int stage_id, int batch_id) const;
+  int GetForwardWaitedEventIdAfterRecv(int stage_id, int batch_id) const;
+  int GetForwardRecordedEventIdBeforeSend(int stage_id, int batch_id) const;
   int GetForwardRecordedEventId(int stage_id, int batch_id) const;
   int GetBackwardWaitedEventId(int stage_id, int batch_id) const;
+  int GetBackwardWaitedEventIdAfterRecv(int stage_id, int batch_id) const;
+  int GetBackwardRecordedEventIdBeforeSend(int stage_id, int batch_id) const;
   int GetBackwardRecordedEventId(int stage_id, int batch_id) const;
   void Show() const;
 
-private:
+ private:
   std::vector<int> SearchLastRecordedEvents(int time_id, int stage_id) const;
 
   // 2-D table of pipeline schedule. table_[i][j] is the computation happening in
@@ -53,12 +81,12 @@ private:
   //   3. table_[1][0].batch_id is 1 and table_[1][0].type is Forward.
   std::vector<std::vector<Slot>> table_;
   // Number of active batches per time slot. batch_count_[i] is the number of active
-  // batches at the i-th time slot. 
+  // batches at the i-th time slot.
   std::vector<int> batch_count_;
-  // Total number of stages of this pipeline. 
+  // Total number of stages of this pipeline.
   // It equals to table_.size().
   int num_stages_;
-  // Total number of batches scheduled in this pipeline. 
+  // Total number of batches scheduled in this pipeline.
   // It equals to table_[i].size(), for i = 0, ..., num_stages_.
   int num_batches_;
 };
@@ -72,13 +100,12 @@ struct PipelineWorkerState {
 
 struct PipelineWorkerPool {
   PipelineWorkerPool() = default;
-  PipelineWorkerPool(size_t num_workers) : workers(num_workers), worker_states(num_workers) {};
+  PipelineWorkerPool(size_t num_workers) : workers(num_workers), worker_states(num_workers){};
   void Join(size_t worker_id);
   void JoinAll();
 
   std::vector<std::thread> workers;
   std::vector<PipelineWorkerState> worker_states;
-
 };
 
 struct PipelineContext {
@@ -93,24 +120,19 @@ struct PipelineContext {
   // Name of scheduling event in graph's input list.
   // If an event name is an empty string, it means no event
   // should be waited or recorded.
-  // [TODO] Add events for communication stages and computation stages independently.
-  // std::string forward_comm_waited_event_name;
-  // std::string forward_comm_recorded_event_name;
-  // std::string forward_comp_waited_event_name;
-  // std::string forward_comp_recorded_event_name;
-  // std::string backward_comm_waited_event_name;
-  // std::string backward_comm_recorded_event_name;
-  // std::string backward_comp_waited_event_name;
-  // std::string backward_comp_recorded_event_name;
   std::string forward_waited_event_name;
+  std::string forward_waited_event_after_recv_name;
+  std::string forward_recorded_event_before_send_name;
   std::string forward_recorded_event_name;
   std::string backward_waited_event_name;
+  std::string backward_waited_event_after_recv_name;
+  std::string backward_recorded_event_before_send_name;
   std::string backward_recorded_event_name;
 
-  std::string forward_waited_output_name;
-  std::string forward_recorded_output_name;
-  std::string backward_waited_output_name;
-  std::string backward_recorded_output_name;
+  std::string forward_wait_output_name;
+  std::string forward_record_output_name;
+  std::string backward_wait_output_name;
+  std::string backward_record_output_name;
 
   // Allowed feed names.
   // It stands for inputs of a graph partition at this stage.
@@ -118,7 +140,7 @@ struct PipelineContext {
   // Allowed fetch names.
   // Values can be fetched at this pipeline stage.
   std::vector<std::string> fetch_names;
-}; 
+};
 
 }  // namespace pipeline
 }  // namespace training
