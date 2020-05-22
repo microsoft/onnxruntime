7a82d70aaba421d623372220ad788f754c937732
Wei-Sheng Chin <wschin@outlook.com>
2020-05-22 06:59:00 +0000

    Address a comment

--- onnxruntime/core/profile/context.h
+++ onnxruntime/core/profile/context.h
@@ -11,6 +11,7 @@
 namespace onnxruntime {
 namespace profile {
 
+// Singleton class of managing global NVTX profiling information. 
 class Context {
  public:
   static Context& GetInstance() {
@@ -18,13 +19,13 @@ class Context {
     return instance_;
   }
 
-  std::string GetThreadTag(const std::thread::id thread_id) {
+  std::string GetThreadTag(const std::thread::id& thread_id) {
     const std::lock_guard<std::mutex> lock(mtx_);
     return thread_tag_.at(thread_id);
   }
 
   void SetThreadTag(
-      const std::thread::id thread_id, const std::string tag) {
+      const std::thread::id& thread_id, const std::string& tag) {
     const std::lock_guard<std::mutex> lock(mtx_);
     thread_tag_[thread_id] = tag;
   }
@@ -35,6 +36,7 @@ class Context {
   Context(const Context&) = delete;
   Context& operator=(const Context&) = delete;
 
+  // map from thread's id to its human-readable tag.
   std::unordered_map<std::thread::id, std::string> thread_tag_;
   std::mutex mtx_;
 };
