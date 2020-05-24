f6612dcf600370e47f7dc54dab4f333330649248
Weixing Zhang <Weixing.Zhang@microsoft.com>
2020-05-24 02:21:35 -0700

    polish run_perf.sh

--- orttraining/tools/docker/script/run_perf.sh
+++ orttraining/tools/docker/script/run_perf.sh
@@ -1,19 +1,22 @@
-#!/bin/bash
+if [ "$#" -ne 6 ]; then
+  echo "Usage: $0 ngpu min_batch_size max_batch_size seq_len max_predictions_per_seq num_train_steps"
+  exit 1
+fi
 
-ngpu=1
-seq_len=512
-max_predictions_per_seq=80
-batch_size=8
+ngpu=$1
+min_batch_size=$2
+max_batch_size=$3
+seq_len=$4
+max_predictions_per_seq=$5
+num_train_steps=$6
+
 grad_acc=1
-num_train_steps=400
 optimizer=adam
 lr=5e-4
 warmup_ratio=0.1
-warmup_mode=Linear
+warmup_mode=Poly
 effective_batch_size=$((ngpu * batch_size * grad_acc))
-commit=$(git rev-parse HEAD | cut -c1-8)
 time_now=$(date +%m%d%H%M)
-run_name=ort_${commit}_nvbertbase_bookwiki128_fp32_${optimizer}_lr${lr}_${warmup_mode}${warmup_ratio}_g${ngpu}_bs${batch_size}_acc${grad_acc}_efbs${effective_batch_size}_steps${num_train_steps}_${time_now}_mi50
 
 work_dir=/data/wezhan
 #model_dir=${work_dir}/bert/model/bert-tiny-uncased_L_3_H_128_A_2_V_30528_S_512_Dp_0.1_optimized_layer_norm
@@ -23,9 +26,13 @@ data_dir=${work_dir}/bert/${seq_len}/train
 mpi_dir=${OMPI_DIR}
 training_bert_dir=${work_dir}/onnxruntime/build/RelWithDebInfo
 
-#export LD_LIBRARY_PATH=${mpi_dir}/lib
+commit=$(git -C ${work_dir}/onnxruntime rev-parse HEAD | cut -c1-8)
+run_name=ort_${commit}_nvbertlarge_fp32_${optimizer}_g${ngpu}_bs${min_batch_size}_acc${grad_acc}_efbs${effective_batch_size}_steps${num_train_steps}_${time_now}_mi50
 
-nohup ${mpi_dir}/bin/mpirun --allow-run-as-root -n ${ngpu} ${training_bert_dir}/onnxruntime_training_bert --model_name ${model_dir} --train_data_dir ${data_dir} --test_data_dir ${data_dir} --train_batch_size ${batch_size} --mode train --num_train_steps ${num_train_steps} --optimizer ${optimizer} --learning_rate ${lr} --warmup_ratio ${warmup_ratio} --warmup_mode ${warmup_mode} --gradient_accumulation_steps ${grad_acc} --max_seq_length ${seq_len} --max_predictions_per_seq=${max_predictions_per_seq} --data_parallel_size ${ngpu} --use_nccl --lambda 0 --display_loss_steps 1  > ${run_name}.log 2>&1 &
+batch_size=${min_batch_size}
+#while [ $batch_size -le ${max_batch_size} ]; do
+  nohup ${mpi_dir}/bin/mpirun --allow-run-as-root -n ${ngpu} ${training_bert_dir}/onnxruntime_training_bert --model_name ${model_dir} --train_data_dir ${data_dir} --test_data_dir ${data_dir} --train_batch_size ${batch_size} --mode train --num_train_steps ${num_train_steps} --optimizer ${optimizer} --learning_rate ${lr} --warmup_ratio ${warmup_ratio} --warmup_mode ${warmup_mode} --gradient_accumulation_steps ${grad_acc} --max_seq_length ${seq_len} --max_predictions_per_seq=${max_predictions_per_seq} --data_parallel_size ${ngpu} --use_nccl --lambda 0 --display_loss_steps 1 >> ${run_name}.log 2>&1 &
 
-tail -f ${run_name}.log
-
+  tail -f ${run_name}.log
+  #batch_size=$((${batch_size}+2))
+#done
