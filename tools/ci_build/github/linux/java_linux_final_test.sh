103a03fcaeb12638d35453a74e392f5b4cc00fdd
Dmitri Smirnov <dmitrism@microsoft.com>
2020-05-26 14:17:45 -0700

    Fix up GPU pipeline. Run final test on Linux under GPU docker.

---
+++ tools/ci_build/github/linux/java_linux_final_test.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -e -o -x
+
+while getopts r:v: parameter_Option
+do case "${parameter_Option}"
+in
+r) BINARY_DIR=${OPTARG};;
+v) VERSION_NUMBER=${OPTARG};;
+esac
+done
+
+EXIT_CODE=1
+
+uname -a
+
+mkdir test
+echo "Directories created"
+pushd test
+jar xf $BINARY_DIR/final-jar/testing.jar
+popd
+
+wget https://oss.sonatype.org/service/local/repositories/releases/content/org/junit/platform/junit-platform-console-standalone/1.6.2/junit-platform-console-standalone-1.6.2.jar -P ./
+wget https://oss.sonatype.org/service/local/repositories/google-releases/content/com/google/protobuf/protobuf-java/3.9.2/protobuf-java-3.9.2.jar -P ./
+java -jar ./junit-platform-console-standalone-1.6.2.jar -cp .:./test:./protobuf-java-3.9.2.jar:./onnxruntime-${VERSION_NUMBER=}.jar --scan-class-path --fail-if-no-tests --disable-banner
+
+EXIT_CODE=$?
+
+set -e
+exit $EXIT_CODE
