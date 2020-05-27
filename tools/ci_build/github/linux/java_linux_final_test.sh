998822ac2b3c7d16790a6e15927ade4f8fd4b4da
Dmitri Smirnov <dmitrism@microsoft.com>
2020-05-27 10:20:45 -0700

    Adjust library path.

--- tools/ci_build/github/linux/java_linux_final_test.sh
+++ tools/ci_build/github/linux/java_linux_final_test.sh
@@ -19,6 +19,7 @@ pushd test
 jar xf $BINARY_DIR/final-jar/testing.jar
 popd
 
+LD_LIBRARY_PATH=./test:${LD_LIBRARY_PATH}
 echo  "Library path:" $LD_LIBRARY_PATH
 wget https://oss.sonatype.org/service/local/repositories/releases/content/org/junit/platform/junit-platform-console-standalone/1.6.2/junit-platform-console-standalone-1.6.2.jar -P ./
 wget https://oss.sonatype.org/service/local/repositories/google-releases/content/com/google/protobuf/protobuf-java/3.9.2/protobuf-java-3.9.2.jar -P ./
