e2ce3070e5f83de25cc25bbfd3b321142b4b26f4
Sherlock Huang <bahuang@OrtTrainingDev3.af05slrtruoetgaxwwjv5nsq5e.px.internal.cloudapp.net>
2020-05-28 23:21:33 +0000

    Draft for LayerNorm Optimization

--- orttraining/orttraining/training_ops/cpu/nn/layer_norm.cc
+++ orttraining/orttraining/training_ops/cpu/nn/layer_norm.cc
@@ -36,12 +36,13 @@ LayerNormGrad<T>::LayerNormGrad(const OpKernelInfo& op_kernel_info)
 template <typename T>
 Status LayerNormGrad<T>::Compute(OpKernelContext* op_kernel_context) const {
   const Tensor* Y_grad = op_kernel_context->Input<Tensor>(0);
-  const Tensor* X = op_kernel_context->Input<Tensor>(1);
+  const Tensor* Y = op_kernel_context->Input<Tensor>(1);
   const Tensor* scale = op_kernel_context->Input<Tensor>(2);
-  const Tensor* mean = op_kernel_context->Input<Tensor>(3);
+  const Tensor* bias = op_kernel_context->Input<Tensor>(3);
   const Tensor* inv_std_var = op_kernel_context->Input<Tensor>(4);
 
-  const auto& X_shape = X->Shape();
+  const auto& Y_shape = Y_grad->Shape();
+  const auto& X_shape = Y_shape;
   const auto axis = HandleNegativeAxis(axis_, X_shape.NumDimensions());
   const auto N = X_shape.SizeToDimension(axis);
   const auto M = X_shape.SizeFromDimension(axis);
@@ -54,9 +55,10 @@ Status LayerNormGrad<T>::Compute(OpKernelContext* op_kernel_context) const {
 
   // Note: Eigen has column-major storage order by default
   ConstEigenArrayMap<T> Y_grad_arr{Y_grad->Data<T>(), M, N};
-  ConstEigenArrayMap<T> X_arr{X->Data<T>(), M, N};
+  ConstEigenArrayMap<T> Y_arr{Y->Data<T>(), M, N};
   ConstEigenVectorArrayMap<T> scale_vec{scale->Data<T>(), M};
-  ConstEigenVectorArrayMap<float> mean_vec{mean->Data<float>(), N};
+  ConstEigenVectorArrayMap<T> bias_vec{bias->Data<T>(), M};
+  //ConstEigenVectorArrayMap<float> mean_vec{mean->Data<float>(), N};
   ConstEigenVectorArrayMap<float> inv_std_var_vec{inv_std_var->Data<float>(), N};
 
   EigenArrayMap<T> X_grad_arr{X_grad->MutableData<T>(), M, N};
@@ -72,9 +74,8 @@ Status LayerNormGrad<T>::Compute(OpKernelContext* op_kernel_context) const {
   // C = Y_grad * scale * inv_std_var * (X - mean(X)) * inv_std_var
 
   // A, B, and C are M x N
-
-  Array X_mean_difference_over_std_var =
-      (X_arr.rowwise() - mean_vec.cast<T>().transpose()).rowwise() * inv_std_var_vec.cast<T>().transpose();
+  Array X_mean_difference_over_std_var = (Y_arr.colwise() - bias_vec).colwise() / scale_vec;
+  //    (X_arr.rowwise() - mean_vec.cast<T>().transpose()).rowwise() * inv_std_var_vec.cast<T>().transpose();
   Array A = Y_grad_arr * X_mean_difference_over_std_var;
   Array B = (Y_grad_arr.colwise() * scale_vec).rowwise() * inv_std_var_vec.cast<T>().transpose();
   Array C = B * X_mean_difference_over_std_var;
