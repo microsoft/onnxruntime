1d441f89acc7c4d17ecda1fb41ca0a2310ef2a57
Scott McKay <skottmckay@gmail.com>
2020-05-30 09:10:05 +1000

    Re-enable PEP8 check in Win CI build  (#4075)
    * Add flake8 to Win CI build so it's re-enabled. It was in the static analysis build that is currently disabled so checks are not running.
    Fix build.py to be compliant again.
    Add prefix to flake8 output so it's (hopefully) easier to identify the errors in build output.
    
    * Add to all builds in Windows CPU CI so they all fail quickly if there's an issue.

--- tools/ci_build/build.py
+++ tools/ci_build/build.py
@@ -1036,11 +1036,13 @@ def run_training_python_frontend_e2e_tests(args, cwd):
     # frontend tests are to be added here:
     log.info("Running python frontend e2e tests.")
 
-    # with orttraining_run_glue.py. 
+    # with orttraining_run_glue.py.
     # 1. we like to force to use single GPU (with CUDA_VISIBLE_DEVICES) for fine-tune tests.
     # 2. need to run test separately (not to mix between fp16 and full precision runs. this need to be investigated).
-    run_subprocess([sys.executable, 'orttraining_run_glue.py', 'ORTGlueTest.test_bert_with_mrpc', '-v'], cwd=cwd, env={'CUDA_VISIBLE_DEVICES': '0'})
-    run_subprocess([sys.executable, 'orttraining_run_glue.py', 'ORTGlueTest.test_bert_fp16_with_mrpc', '-v'], cwd=cwd, env={'CUDA_VISIBLE_DEVICES': '0'})
+    run_subprocess([sys.executable, 'orttraining_run_glue.py', 'ORTGlueTest.test_bert_with_mrpc', '-v'],
+                   cwd=cwd, env={'CUDA_VISIBLE_DEVICES': '0'})
+    run_subprocess([sys.executable, 'orttraining_run_glue.py', 'ORTGlueTest.test_bert_fp16_with_mrpc', '-v'],
+                   cwd=cwd, env={'CUDA_VISIBLE_DEVICES': '0'})
 
     run_subprocess([sys.executable, 'orttraining_test_transformers.py'], cwd=cwd)
 
