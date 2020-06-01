ff16ca54e1c0d08bc1c6ebaabd4ce6cf2425f738
Changming Sun <chasun@microsoft.com>
2020-06-01 10:32:22 -0700

    Fix the flake8 warning in generate_nuspec_for_native_nuget.py (#4089)

--- tools/nuget/generate_nuspec_for_native_nuget.py
+++ tools/nuget/generate_nuspec_for_native_nuget.py
@@ -167,6 +167,7 @@ def generate_files(list, args):
                           '" target="build\\native\\include" />')
 
     if includes_winml:
+        mlop_path = 'onnxruntime\\core\\providers\\dml\\dmlexecutionprovider\\inc\\mloperatorauthor.h'
         # Add microsoft.ai.machinelearning headers
         files_list.append('<file src=' + '"' + os.path.join(args.ort_build_path, args.build_config,
                                                             'microsoft.ai.machinelearning.h') +
@@ -183,8 +184,7 @@ def generate_files(list, args):
                           '" target="lib\\uap10.0\\Microsoft.AI.MachineLearning.winmd" />')
         # Add custom operator headers
         files_list.append('<file src=' + '"' +
-                          os.path.join(args.sources_path,
-                                       'onnxruntime\\core\\providers\\dml\\dmlexecutionprovider\\inc\\mloperatorauthor.h') +
+                          os.path.join(args.sources_path, mlop_path) +
                           '" target="build\\native\\include" />')
 
     # Process runtimes
