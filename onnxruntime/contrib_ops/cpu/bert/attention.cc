04393aced94caf106084eaf4708603e88b45e6a8
Yufeng Li <liyufeng1987@gmail.com>
2020-06-01 16:57:41 -0700

    refine attention code

--- onnxruntime/contrib_ops/cpu/bert/attention.cc
+++ onnxruntime/contrib_ops/cpu/bert/attention.cc
@@ -2,6 +2,7 @@
 // Licensed under the MIT License.
 
 #include "attention.h"
+#include "attention_helper.h"
 #include "core/framework/tensorprotoutils.h"
 #include "core/graph/onnx_protobuf.h"
 #include "core/util/math.h"
@@ -121,221 +122,53 @@ Status Attention<T>::Compute(OpKernelContext* context) const {
 
   auto* tp = context->GetOperatorThreadPool();
   // STEP.1: gemm_data(BS, 3NH) = input(BS, NH) x weights(NH, 3NH) + bias(3NH)
+  const auto input_data = input->template Data<T>();
+  const auto weights_data = weights->template Data<T>();
+  const auto bias_data = bias->template Data<T>();
+
   auto gemm_data = allocator->Alloc(SafeInt<size_t>(batch_size) * sequence_length * 3 * hidden_size * element_size);
   BufferUniquePtr gemm_buffer(gemm_data, BufferDeleter(allocator));
-  auto Q = reinterpret_cast<T*>(gemm_data);
-  auto K = Q + batch_size * sequence_length * hidden_size;
-  auto V = K + batch_size * sequence_length * hidden_size;
+  T* Q = reinterpret_cast<T*>(gemm_data);
+  T* K = Q + batch_size * sequence_length * hidden_size;
+  T* V = K + batch_size * sequence_length * hidden_size;
 
-  T* QKV[3] = {Q, K, V};
+  GenerateQKV<T>(Q, K, V, input_data, weights_data, bias_data,
+                 batch_size, sequence_length, head_size, num_heads_, hidden_size, tp);
 
-  {
-    const int loop_len = 3 * batch_size * num_heads_;
-    const auto input_data = input->template Data<T>();
-    const auto weights_data = weights->template Data<T>();
-    const auto bias_data = bias->template Data<T>();
+  // STEP.2: compute the attention score. It does 2 things:
+  //         I. attention_probs(B, N, S, S) = 1/sqrt(H) x Q(B, N, S, H) x K'(B, N, S, H -> B, N, H, S) +
+  //                                         1 x mask_data(B, N, S, S)
+  //         II.attention_probs(B, N, S, S) = Softmax(attention_probs)
+  size_t attention_probs_bytes = SafeInt<size_t>(batch_size) * num_heads_ * sequence_length * sequence_length * element_size;
+  auto attention_probs = allocator->Alloc(attention_probs_bytes);
+  BufferUniquePtr scratch_buffer(attention_probs, BufferDeleter(allocator));
 
-    const double cost =
-        static_cast<double>(sequence_length) * static_cast<double>(head_size) * static_cast<double>(hidden_size);
-    ThreadPool::TryParallelFor(tp, loop_len, cost, [&](std::ptrdiff_t begin, std::ptrdiff_t end) {
-      for (std::ptrdiff_t i = begin; i != end; ++i) {
-        const int batch_index = static_cast<int>((i / 3) / num_heads_);
-        const int head_index = static_cast<int>((i / 3) % num_heads_);
-        const int qkv_index = static_cast<int>(i % 3);
-
-        int input_offset = batch_index * sequence_length * hidden_size;
-        int weights_offset = qkv_index * hidden_size + head_index * head_size;
-        T* qkv_dest = QKV[qkv_index];
-        int qkv_offset = (batch_index * num_heads_ + head_index) * (sequence_length * head_size);
-
-        // broadcast 3NH -> (3.B.N.S.H)
-        const T* broadcast_data_src = bias_data + weights_offset;
-        T* broadcast_data_dest = QKV[qkv_index] + qkv_offset;
-        for (int seq_index = 0; seq_index < sequence_length; seq_index++) {
-          memcpy(broadcast_data_dest, broadcast_data_src, head_size * sizeof(T));
-          broadcast_data_dest += head_size;
-        }
-
-        //                   original           transposed            iteration
-        // A: input          (BxSxNxH)          (B.)S x NH            S x NH
-        // B: weights        (NxHx3xNxH)        NH  x (3.N.)H         NH x H
-        // C: QKV[qkv_index] (3xBxNxSxH)        (3.B.N.)S x H         S x H
-
-        math::GemmEx<float, ThreadPool>(CblasNoTrans,                   // TransA = no
-                                        CblasNoTrans,                   // TransB = no
-                                        sequence_length,                // M      = S
-                                        head_size,                      // N      = H
-                                        hidden_size,                    // K      = NH
-                                        1.0f,                           // alpha
-                                        input_data + input_offset,      // A
-                                        hidden_size,                    // lda    = NH
-                                        weights_data + weights_offset,  // B
-                                        3 * hidden_size,                // ldb    = 3NH
-                                        1.0f,                           // beta
-                                        qkv_dest + qkv_offset,          // C
-                                        head_size,                      // ldc
-                                        nullptr                         // use single-thread
-        );
-      }
-    });
+  size_t mask_data_bytes = 0;
+  if (mask_index != nullptr) {
+    mask_data_bytes = SafeInt<size_t>(batch_size) * sequence_length * sequence_length * element_size;
+  } else if (is_unidirectional_) {
+    mask_data_bytes = SafeInt<size_t>(sequence_length) * sequence_length * element_size;
   }
 
-  // STEP.2: scratch(B, N, S, S) = 1/sqrt(H) x Q(B, N, S, H) x K'(B, N, S, H -> B, N, H, S) + 1 x mask_index(B -> B, 1,
-  // 1, 1)
-  size_t scratch_data_bytes = SafeInt<size_t>(batch_size) * num_heads_ * sequence_length * sequence_length * element_size;
-  auto scratch_data = allocator->Alloc(scratch_data_bytes);
-  BufferUniquePtr scratch_buffer(scratch_data, BufferDeleter(allocator));
-
-  {
-    size_t mask_data_bytes = 0;
-    if (mask_index != nullptr) {
-      mask_data_bytes = SafeInt<size_t>(batch_size) * sequence_length * sequence_length * element_size;
-    } else if (is_unidirectional_) {
-      mask_data_bytes = SafeInt<size_t>(sequence_length) * sequence_length * element_size;
-    }
-
-    void* mask_data = nullptr;
-    if (mask_data_bytes > 0) {
-      mask_data = allocator->Alloc(mask_data_bytes);
-      memset(mask_data, 0, mask_data_bytes);
-    }
-    BufferUniquePtr mask_data_buffer(mask_data, BufferDeleter(allocator));
-
-    if (mask_index != nullptr && mask_data != nullptr) {
-      T* p_mask = reinterpret_cast<T*>(mask_data);
-      for (int b_i = 0; b_i < batch_size; b_i++) {
-        // TODO: mask_index can be used in softmax to save some calculation.
-        // Convert mask_index to mask (-10000 means out of range, which will be 0 after softmax): B => BxS
-        int valid_length = mask_index->template Data<int32_t>()[b_i];
-        for (int m_i = valid_length; m_i < sequence_length; m_i++) {
-          p_mask[m_i] = static_cast<T>(-10000.0);
-        }
-
-        // Broadcast mask from BxS to BxSxS
-        for (int s_i = 1; s_i < sequence_length; s_i++) {
-          memcpy(p_mask + s_i * sequence_length, p_mask, sequence_length * sizeof(T));
-        }
-        p_mask += sequence_length * sequence_length;
-      }
-    } else if (is_unidirectional_ && mask_data != nullptr) {  // unidirectional mask
-      T* p_mask = reinterpret_cast<T*>(mask_data);
-      for (int s_i = 0; s_i < sequence_length - 1; s_i++) {
-        for (int m_i = s_i + 1; m_i < sequence_length; m_i++) {
-          p_mask[s_i * sequence_length + m_i] = static_cast<T>(-10000.0);
-        }
-      }
-    } else {  // no any mask
-      memset(scratch_data, 0, scratch_data_bytes);
-    }
-
-    const int loop_len = batch_size * num_heads_;
-    const float alpha = 1.0f / sqrt(static_cast<float>(head_size));
-
-    // The cost of Gemm
-    const double cost =
-        static_cast<double>(head_size) * static_cast<double>(sequence_length) * static_cast<double>(sequence_length);
-    ThreadPool::TryParallelFor(tp, loop_len, cost, [&](std::ptrdiff_t begin, std::ptrdiff_t end) {
-      for (std::ptrdiff_t i = begin; i != end; ++i) {
-        const std::ptrdiff_t batch_index = i / num_heads_;
-
-        // broadcast mask data: SxS or (Bx)SxS -> (BxNx)SxS
-        if (mask_data != nullptr) {
-          const T* broadcast_data_src = is_unidirectional_ ? reinterpret_cast<T*>(mask_data) : reinterpret_cast<T*>(mask_data) + batch_index * sequence_length * sequence_length;
-          T* broadcast_data_dest = reinterpret_cast<T*>(scratch_data) + sequence_length * sequence_length * i;
-          memcpy(broadcast_data_dest, broadcast_data_src, sequence_length * sequence_length * sizeof(T));
-        }
-
-        // gemm
-
-        //                   original           transposed            iteration
-        // A: Q              (BxNxSxH)          (B.N.)S x H            S x H
-        // B: K'             (BxNxSxH)          (B.N.)H x S            H x S
-        // C: scratch_data   (BxNxSxS)          (B.N.)S x S            S x S
-
-        math::Gemm<T, ThreadPool>(CblasNoTrans, CblasTrans, sequence_length, sequence_length, head_size, alpha,
-                                  Q + sequence_length * head_size * i, K + sequence_length * head_size * i, 1.0,
-                                  reinterpret_cast<T*>(scratch_data) + sequence_length * sequence_length * i, nullptr);
-      }
-    });
+  void* mask_data = nullptr;
+  if (mask_data_bytes > 0) {
+    mask_data = allocator->Alloc(mask_data_bytes);
+    memset(mask_data, 0, mask_data_bytes);
   }
+  BufferUniquePtr mask_data_buffer(mask_data, BufferDeleter(allocator));
 
-  // STEP.3: P(B, N, S, S) = Softmax(scratch)
-  {
-    const int N = batch_size * num_heads_ * sequence_length;
-    const int D = sequence_length;
+  const int32_t* mask_index_data = mask_index != nullptr ? mask_index->template Data<int32_t>() : nullptr;
 
-    if (std::is_same<T, float>::value) {
-      float* x = reinterpret_cast<float*>(scratch_data);
-      MlasComputeSoftmax(x, x, N, D, false, tp);
-    } else {
-      ThreadPool::TryParallelFor(tp, N, sequence_length * 2.0, [&](std::ptrdiff_t begin, std::ptrdiff_t end) {
-        for (std::ptrdiff_t j = begin; j != end; ++j) {
-          float* x = reinterpret_cast<T*>(scratch_data) + j * D;
-          float* y = x;
+  ComputeAttentionProbs<T>(static_cast<T*>(attention_probs), Q, K, mask_index_data, static_cast<T*>(mask_data),
+                           batch_size, sequence_length, head_size, num_heads_, is_unidirectional_, tp);
 
-          // e^x is represented as infinity if x is large enough, like 100.f.
-          // Infinity divided by Infinity is a NAN. Thus, softmax gets a NAN if
-          // one or more item are large enough. a math transform as below is
-          // leveraged to get a stable softmax: e^xi/(e^x1 + ...e^xn) = e^(xi -
-          // max) / (e^(x1 - max) + ... + e^(xn - max))
-          float max = -std::numeric_limits<float>::infinity();
-          for (int i = 0; i < D; i++) {
-            if (max < x[i])
-              max = x[i];
-          }
-          for (int i = 0; i < D; i++) {
-            y[i] = expf(x[i] - max);
-          }
-
-          double sum = 0.0;
-
-          for (int i = 0; i < D; i++) {
-            sum += x[i];
-          }
-
-          if (sum == 0) {
-            for (int i = 0; i < D; i++) {
-              y[i] = 1.0f / (float)D;
-            }
-          } else {
-            for (int i = 0; i < D; i++) {
-              y[i] = x[i] / (float)sum;
-            }
-          }
-        }
-      });
-    }
-  }
-
-  // STEP.4: out_tmp(B, N, S, H) = P(B, N, S, S) x V(B, N, S, H)
+  // STEP.3: compute the attentionScore * Value. It does: out_tmp(B, N, S, H) = attention_probs(B, N, S, S) x V(B, N, S, H)
   auto out_tmp_data =
       allocator->Alloc(SafeInt<size_t>(batch_size) * num_heads_ * sequence_length * head_size * element_size);
   BufferUniquePtr out_tmp_buffer(out_tmp_data, BufferDeleter(allocator));
-  // cost of MatMul
-  const double cost =
-      static_cast<double>(sequence_length) * static_cast<double>(head_size) * static_cast<double>(sequence_length);
-  ThreadPool::TryParallelFor(tp, batch_size * num_heads_, cost, [&](std::ptrdiff_t begin, std::ptrdiff_t end) {
-    const int sequence_length_mul_head_size = sequence_length * head_size;
-    for (std::ptrdiff_t i = begin; i != end; ++i) {
-      T* current_tmp_data = reinterpret_cast<T*>(out_tmp_data) + sequence_length_mul_head_size * i;
-      math::MatMul<T>(sequence_length, head_size, sequence_length,
-                      reinterpret_cast<T*>(scratch_data) + sequence_length * sequence_length * i,
-                      V + sequence_length_mul_head_size * i, current_tmp_data, nullptr);
 
-      // transpose: out(B, S, N, H) = transpose out_tmp(B, N, S, H)
-      const int batch_index = static_cast<int>(i / num_heads_);
-      const int head_index = static_cast<int>(i % num_heads_);
-      T* src = current_tmp_data;
-      T* dest =
-          output->template MutableData<T>() + (batch_index * sequence_length * num_heads_ + head_index) * head_size;
-      const auto bytes_to_copy = SafeInt<size_t>(head_size) * sizeof(T);
-      for (int j = 0; j < sequence_length; j++) {
-        memcpy(dest, src, bytes_to_copy);
-        src += head_size;
-        dest += hidden_size;
-      }
-    }
-  });
+  ComputeVxAttentionScore(output->template MutableData<T>(), static_cast<T*>(out_tmp_data), static_cast<T*>(attention_probs), V,
+                          batch_size, sequence_length, head_size, num_heads_, hidden_size, tp);
 
   return Status::OK();
 }
