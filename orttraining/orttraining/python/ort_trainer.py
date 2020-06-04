d3e5b1e8b411dd237b0abc4c69ed83dab999b76e
CircleCI <tix@microsoft.com>
2020-06-04 11:03:05 -0700

    fixed bug

--- orttraining/orttraining/python/ort_trainer.py
+++ orttraining/orttraining/python/ort_trainer.py
@@ -862,7 +862,7 @@ class ORTTrainer():
 
         if fetches is not None:
             results = [session_run_results[fetch] for fetch in fetches]
-        elif has_if_all_finite and self.loss_scaler_ is None:
+        else:
             results = [session_run_results[output.name_] for output in output_desc]
         #     # return descripted outputs plus the all_finite flag so that the training script can handle loss scaling.
         #     results = [session_run_results[output_desc.name_] for output_desc in self.output_desc_with_all_fp_16_or_fp32_gradients_finite]
