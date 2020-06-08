9215194c348364eceb93e68cdec160206e843830
Peng Wang (pengwa) <pengwa@microsoft.com>
2020-06-08 10:52:04 +0000

    fix uts

--- orttraining/orttraining/test/training_ops/cpu/tensor/gather_grad_op_test.cc
+++ orttraining/orttraining/test/training_ops/cpu/tensor/gather_grad_op_test.cc
@@ -2,6 +2,7 @@
 // Licensed under the MIT License.
 
 #include "gtest/gtest.h"
+#include "test/common/cuda_op_test_utils.h"
 #include "test/common/tensor_op_test_utils.h"
 #include "test/providers/provider_test_utils.h"
 #include "core/util/math.h"
