50277551cde04bf6d161bddb1b50d974e58e6e95
Chi Lo <Chi.Lo@microsoft.com>
2020-06-16 10:36:43 -0700

    Add cuda provider option default setting

---
+++ onnxruntime/core/providers/cuda/cuda_provider_options.h
@@ -0,0 +1,22 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+#pragma once
+
+namespace onnxruntime {
+
+/**
+  * Configuration information for a cuda provider.
+  */
+struct CudaProviderOptions {
+
+  // use cuda device with id=0 as default device.
+  OrtDevice::DeviceId device_id = 0;
+
+  // set default cuda memory limitation to maximum finite value of size_t.
+  size_t cuda_mem_limit = std::numeric_limits<size_t>::max();
+
+  // set default area extend strategy to KNextPowerOfTwo.
+  onnxruntime::ArenaExtendStrategy arena_extend_strategy = onnxruntime::ArenaExtendStrategy::kNextPowerOfTwo;
+};
+}  // namespace onnxruntime
