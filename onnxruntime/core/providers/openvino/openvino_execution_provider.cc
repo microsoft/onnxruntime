4d79fa38f30f7a5cad6f4c491cb7da7145571b8b
suryasidd <surya.siddharth.pemmaraju@intel.com>
2020-06-18 17:32:23 -0700

    Softmax restriction removed

--- onnxruntime/core/providers/openvino/openvino_execution_provider.cc
+++ onnxruntime/core/providers/openvino/openvino_execution_provider.cc
@@ -57,7 +57,7 @@ int GetInputCount(const Node* node, const InitializedTensorSet& initializer_set)
   return count;
 }
 
-bool IsDimensionSupported(const Node* node, std::string device) {
+bool IsDimensionSupported(const Node* node) {
   auto node_inputs = node->InputDefs();
   size_t input_dims = 0;
   if (node_inputs[0]->Shape() == nullptr) {
@@ -76,13 +76,6 @@ bool IsDimensionSupported(const Node* node, std::string device) {
         return false;
     }
 
-    if (node->OpType() == "Softmax") {
-      //3D input not supported on GPU, MYRIAD and HDDL
-      if (device == "GPU" || device == "MYRIAD" || device == "HDDL") {
-        if (input_dims == 3)
-          return false;
-      }
-    }
   }
   return true;
 }
@@ -195,7 +188,7 @@ bool IsOpSupported(std::string name, std::string device) {
 }
 
 // Returns true only if op is in a mode that is not currently supported
-static bool IsUnsupportedOpMode(const Node* node, const onnxruntime::GraphViewer& graph_viewer, const std::string& device_id) {
+static bool IsUnsupportedOpMode(const Node* node, const onnxruntime::GraphViewer& graph_viewer) {
   const auto& optype = node->OpType();
 
   const auto& initializers = graph_viewer.GetAllInitializedTensors();
@@ -217,7 +210,7 @@ static bool IsUnsupportedOpMode(const Node* node, const onnxruntime::GraphViewer
     if (attributes.find("dilations") != attributes.end()) {
       return true;
     }
-    if (!IsDimensionSupported(node, device_id))
+    if (!IsDimensionSupported(node))
       return true;
   } else if (optype == "Add" || optype == "Sub" || optype == "Mul") {
     for (size_t i = 0; i < node->InputDefs().size(); i++) {
@@ -299,11 +292,8 @@ static bool IsUnsupportedOpMode(const Node* node, const onnxruntime::GraphViewer
       }
     }
     return true;
-  } else if (optype == "Softmax") {
-    if (!IsDimensionSupported(node, device_id))
-      return true;
   } else if (optype == "Unsqueeze") {
-    if (!IsDimensionSupported(node, device_id))
+    if (!IsDimensionSupported(node))
       return true;
   } else if (optype == "Pad") {
     // Pad is only supported only up to opset 10 (in opset 11 more inputs were added)
@@ -369,7 +359,7 @@ static bool IsUnsupportedOpMode(const Node* node, const onnxruntime::GraphViewer
     if (ceil_attr != attributes.end() && ceil_attr->second.i() != 0) {
       return true;
     }
-    if (!IsDimensionSupported(node, device_id))
+    if (!IsDimensionSupported(node))
       return true;
   } else if (optype == "QLinearMatMul") {
     const auto& a_zero_point = node->InputDefs()[2];
@@ -578,7 +568,7 @@ static bool IsNodeSupported(const std::map<std::string, std::set<std::string>>& 
   }
 
   //Check 3a
-  if (domain == kOnnxDomain && IsUnsupportedOpMode(node, graph_viewer, device_id)) {
+  if (domain == kOnnxDomain && IsUnsupportedOpMode(node, graph_viewer)) {
 #ifndef NDEBUG
     if (openvino_ep::backend_utils::IsDebugEnabled()) {
       std::cout << "Failed in unsupported op mode" << std::endl;
