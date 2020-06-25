d234b55db3fdbd43a30143ba9674d0467603dd38
EJ Omiyi eromiyi@microsoft.com <emo5925@g.rit.edu>
2020-06-25 16:19:47 -0700

    Added src files and build instructions for the main fuzzing engine

--- cmake/onnxruntime_fuzz_test.cmake
+++ cmake/onnxruntime_fuzz_test.cmake
@@ -21,5 +21,42 @@ if (onnxruntime_FUZZ_ENABLED)
 	target_compile_options(protobuf-mutator PRIVATE "/wd4244" "/wd4245" "/wd4267" "/wd4100" "/wd4456")
 	target_compile_options(protobuf-mutator-libfuzzer PRIVATE "/wd4146" "/wd4267")
 	
+	# add Fuzzing Engine Build Configuration 
+	message(STATUS "Building Fuzzing engine")
 	
+	# set Fuzz root directory
+	set(SEC_FUZZ_ROOT ${TEST_SRC_DIR}/fuzzing)
+	
+	# Security fuzzing engine src file reference 
+	set(SEC_FUZ_SRC "${SEC_FUZZ_ROOT}/src/BetaDistribution.cpp" 
+					"${SEC_FUZZ_ROOT}/src/OnnxPrediction" 
+					"${SEC_FUZZ_ROOT}/src/testlog.cpp" 
+					"${SEC_FUZZ_ROOT}/src/test.cpp")
+					
+	# compile the executables
+	add_executable(onnxruntime_security_fuzz ${SEC_FUZ_SRC})
+	
+	# compile with c++17
+	target_compile_features(onnxruntime_security_fuzz PUBLIC cxx_std_17)
+	
+	# Security fuzzing engine header file reference
+	onnxruntime_add_include_to_target(onnxruntime_security_fuzz libprotobuf onnx onnxruntime)
+	
+	# Assign all include to one variable
+	set(SEC_FUZ_INC "${SEC_FUZZ_ROOT}/include")
+	set(INCLUDE_FILES ${SEC_FUZ_INC} "$<TARGET_PROPERTY:protobuf-mutator,INCLUDE_DIRECTORIES>")
+	
+	# add all these include directory to the Fuzzing engine
+	target_include_directories(onnxruntime_security_fuzz PRIVATE ${INCLUDE_FILES})
+	
+	# add link libraries the project
+	target_link_libraries(onnxruntime_security_fuzz libprotobuf onnx_proto onnxruntime protobuf-mutator)
+	
+	# add the dependencies
+	add_dependencies(onnxruntime_security_fuzz libprotobuf onnx_proto onnxruntime protobuf-mutator)
+	
+	# copy the dlls to the execution directory
+	add_custom_command(TARGET onnxruntime_security_fuzz POST_BUILD
+		COMMAND ${CMAKE_COMMAND} -E copy_if_different $<TARGET_FILE:onnxruntime>  $<TARGET_FILE_DIR:onnxruntime_security_fuzz>
+		COMMAND ${CMAKE_COMMAND} -E copy_if_different $<TARGET_FILE:libprotobuf>  $<TARGET_FILE_DIR:onnxruntime_security_fuzz>)
 endif()
