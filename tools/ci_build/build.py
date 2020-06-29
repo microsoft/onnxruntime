11357d6831f806dcb2dd2c22fb62eb484820fba6
EJ Omiyi eromiyi@microsoft.com <emo5925@g.rit.edu>
2020-06-29 12:42:42 -0700

    Removed all pep8 violations introduced by this change and other E501 violations

--- tools/ci_build/build.py
+++ tools/ci_build/build.py
@@ -409,8 +409,8 @@ def run_subprocess(args, cwd=None, capture=False, dll_path=None,
     completed_process = subprocess.run(
         args, cwd=cwd, check=True, stdout=stdout, stderr=stderr,
         env=my_env, shell=shell)
-    log.debug("Subprocess completed. Return code="
-              + str(completed_process.returncode))
+    log.debug("Subprocess completed. Return code=" +
+              str(completed_process.returncode))
     return completed_process
 
 
@@ -647,8 +647,8 @@ def generate_build_tree(cmake_path, source_dir, build_dir, cuda_home, cudnn_home
         cmake_args += ["-Donnxruntime_NCCL_HOME=" + nccl_home]
 
     if args.winml_root_namespace_override:
-        cmake_args += ["-Donnxruntime_WINML_NAMESPACE_OVERRIDE="
-                       + args.winml_root_namespace_override]
+        cmake_args += ["-Donnxruntime_WINML_NAMESPACE_OVERRIDE=" +
+                       args.winml_root_namespace_override]
 
     # temp turn on only for linux gpu build
     if not is_windows():
@@ -678,8 +678,8 @@ def generate_build_tree(cmake_path, source_dir, build_dir, cuda_home, cudnn_home
 
     if args.android:
         cmake_args += [
-            "-DCMAKE_TOOLCHAIN_FILE=" + args.android_ndk_path
-            + "/build/cmake/android.toolchain.cmake",
+            "-DCMAKE_TOOLCHAIN_FILE=" + args.android_ndk_path +
+            "/build/cmake/android.toolchain.cmake",
             "-DANDROID_PLATFORM=android-" + str(args.android_api),
             "-DANDROID_ABI=" + str(args.android_abi)
         ]
@@ -765,18 +765,18 @@ def generate_build_tree(cmake_path, source_dir, build_dir, cuda_home, cudnn_home
     if path_to_protoc_exe:
         cmake_args += [
             "-DONNX_CUSTOM_PROTOC_EXECUTABLE=%s" % path_to_protoc_exe]
-			
+
     if args.fuzz_testing:
-        if not (args.build_shared_lib 
-				and is_windows() 
-				and args.cmake_generator == 'Visual Studio 16 2019'
-				and args.use_full_protobuf):
-            raise BuildError("Fuzz test has only be tested with build shared libs option using MSVC on windows")
+        if not (args.build_shared_lib and
+                is_windows() and
+                args.cmake_generator == 'Visual Studio 16 2019' and
+                args.use_full_protobuf):
+            raise BuildError(
+             "Fuzz test has only be tested with build shared libs option using MSVC on windows")
         cmake_args += [
-            "-Donnxruntime_BUILD_UNIT_TESTS=ON", 
-			"-Donnxruntime_FUZZ_TEST=ON",
-			"-Donnxruntime_USE_FULL_PROTOBUF=ON"]
-	
+            "-Donnxruntime_BUILD_UNIT_TESTS=ON",
+            "-Donnxruntime_FUZZ_TEST=ON",
+            "-Donnxruntime_USE_FULL_PROTOBUF=ON"]
 
     if args.gen_doc:
         cmake_args += ["-Donnxruntime_PYBIND_EXPORT_OPSCHEMA=ON"]
@@ -838,11 +838,11 @@ def generate_build_tree(cmake_path, source_dir, build_dir, cuda_home, cudnn_home
 
         run_subprocess(
             cmake_args + [
-                "-Donnxruntime_ENABLE_MEMLEAK_CHECKER="
-                + ("ON" if config.lower() == 'debug' and not args.use_tvm
-                   and not args.use_ngraph and not args.use_openvino and
-                   not args.enable_msvc_static_runtime
-                   else "OFF"), "-DCMAKE_BUILD_TYPE={}".format(config)],
+                "-Donnxruntime_ENABLE_MEMLEAK_CHECKER=" +
+                ("ON" if config.lower() == 'debug' and not args.use_tvm and not
+                 args.use_ngraph and not args.use_openvino and not
+                 args.enable_msvc_static_runtime
+                 else "OFF"), "-DCMAKE_BUILD_TYPE={}".format(config)],
             cwd=config_build_dir)
 
 
@@ -932,8 +932,8 @@ def setup_cuda_vars(args):
 
             cuda_bin_path = os.path.join(cuda_home, 'bin')
             os.environ["CUDA_BIN_PATH"] = cuda_bin_path
-            os.environ["PATH"] += (os.pathsep + cuda_bin_path + os.pathsep
-                                   + os.path.join(cudnn_home, 'bin'))
+            os.environ["PATH"] += (os.pathsep + cuda_bin_path + os.pathsep +
+                                   os.path.join(cudnn_home, 'bin'))
             # Add version specific CUDA_PATH_Vx_y value as the
             # Visual Studio build files require that.
             version_file = os.path.join(cuda_home, 'version.txt')
@@ -950,8 +950,8 @@ def setup_cuda_vars(args):
                 m = re.match(r"CUDA Version (\d+).(\d+)", first_line)
                 if not m:
                     raise BuildError(
-                        "Couldn't read version from first line of "
-                        + version_file)
+                        "Couldn't read version from first line of " +
+                        version_file)
 
                 cuda_major_version = m.group(1)
                 minor = m.group(2)
@@ -966,8 +966,8 @@ def setup_cuda_vars(args):
                     "compatible with CUDA. Will attempt to use.")
                 log.warning(
                     "Failed to get valid Visual C++ Tools version from "
-                    "VCToolsVersion environment variable value of '"
-                    + vc_ver_str + "'")
+                    "VCToolsVersion environment variable value of '" +
+                    vc_ver_str + "'")
                 log.warning(
                     "VCToolsVersion is set in a VS 2017 Developer Command "
                     "shell, or by running "
@@ -977,8 +977,8 @@ def setup_cuda_vars(args):
                     "instructions on installing the Visual C++ 2017 14.11 "
                     "toolset if needed.")
 
-            elif (cuda_major_version == "9" and vc_ver[0] == "14"
-                  and int(vc_ver[1]) > 11):
+            elif (cuda_major_version == "9" and vc_ver[0] == "14" and
+                  int(vc_ver[1]) > 11):
                 raise BuildError(
                     "Visual C++ Tools version not supported by CUDA v9. You "
                     "must setup the environment to use the 14.11 toolset.",
@@ -1570,8 +1570,8 @@ def generate_documentation(source_dir, build_dir, configs):
         # dependent on build configuration for including
         # execution propviders
         log.warning(
-            'The updated opkernel document file ' + str(opkernel_doc_path)
-            + ' is different from the checked in version. Consider '
+            'The updated opkernel document file ' + str(opkernel_doc_path) +
+            ' is different from the checked in version. Consider '
             'regenerating the file with CPU, DNNL and CUDA providers enabled.')
         log.debug('diff:\n' + str(docdiff))
 
@@ -1582,9 +1582,9 @@ def generate_documentation(source_dir, build_dir, configs):
         print('git diff returned non-zero error code')
     if len(docdiff) > 0:
         raise BuildError(
-            'The updated operator document file '
-            + str(operator_doc_path) + ' must be checked in.\n diff:\n'
-            + str(docdiff))
+            'The updated operator document file ' +
+            str(operator_doc_path) + ' must be checked in.\n diff:\n' +
+            str(docdiff))
 
 
 def main():
