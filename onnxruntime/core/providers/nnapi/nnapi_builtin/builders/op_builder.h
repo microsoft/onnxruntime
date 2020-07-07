292432666fa8e520f2fa8d4f71a37797206cbacf
gwang-msft <62914304+gwang-msft@users.noreply.github.com>
2020-07-06 18:44:04 -0700

    Change the input to NNAPI EP ModelBuilder from ModelProto to GraphViewer (#4389)
    * init version to use graph instead of model_proto for IsOpSupported
    
    * move add to modelbuilder to use graph node
    
    * move the rest of model_builder to use graph instead of modelproto
    
    * remove redundant code
    
    * Clear some redundant code
    
    * merge master and some minor style changes
    
    * move check if an initializer is external to individual op instead the whole graph
    
    * Addressed comments
    
    * Change the GetType and GetShape to log waring info inside to simplify the caller, remove some redundant onnxruntime namespace
    
    * add squeeze op support, some more code style clean up
    
    * fix a bug where duplicate output can be added to a subgraph, some other minor logging changes

--- onnxruntime/core/providers/nnapi/nnapi_builtin/builders/op_builder.h
+++ onnxruntime/core/providers/nnapi/nnapi_builtin/builders/op_builder.h
@@ -13,27 +13,22 @@ class IOpBuilder {
   virtual ~IOpBuilder() = default;
 
   // Check if an operator is supported
-  virtual bool IsOpSupported(ModelBuilder& model_builder,
-                             const ONNX_NAMESPACE::NodeProto& node) = 0;
+  virtual bool IsOpSupported(ModelBuilder& model_builder, const Node& node) = 0;
 
   // Check if the initializers of this operator need preprocess
   // which will not be copied
-  virtual void AddInitializersToSkip(ModelBuilder& model_builder,
-                                     const ONNX_NAMESPACE::NodeProto& node) = 0;
+  virtual void AddInitializersToSkip(ModelBuilder& model_builder, const Node& node) = 0;
 
   // Add the operator to NNAPI model
-  virtual void AddToModelBuilder(ModelBuilder& model_builder,
-                                 const ONNX_NAMESPACE::NodeProto& node) = 0;
+  virtual void AddToModelBuilder(ModelBuilder& model_builder, const Node& node) = 0;
 };
 
 // Generate a lookup table with IOpBuilder delegates
 // for different onnx operators
-std::unordered_map<std::string, std::shared_ptr<IOpBuilder>>
-CreateOpBuilders();
+std::unordered_map<std::string, std::shared_ptr<IOpBuilder>> CreateOpBuilders();
 
-void TransposeNHWCToNCHW(ModelBuilder& model_builder,
-                         const std::string& input,
-                         const std::string& output);
+// Transpose the NHWCinput to NCHW output
+void TransposeNHWCToNCHW(ModelBuilder& model_builder, const std::string& input, const std::string& output);
 
 }  // namespace nnapi
 }  // namespace onnxruntime
