af4623d97024adebacb3d70e558ab0788ed0b84f
gwang <wanggy@outlook.com>
2020-07-10 00:54:03 -0700

    make android elumator boot faster in android ci

--- tools/ci_build/github/android/start_android_emulator.sh
+++ tools/ci_build/github/android/start_android_emulator.sh
@@ -10,7 +10,7 @@ echo "no" | $ANDROID_HOME/tools/bin/avdmanager create avd -n android_emulator -k
 echo "Starting emulator"
 
 # Start emulator in background
-nohup $ANDROID_HOME/emulator/emulator -avd android_emulator -partition-size 2048 -no-snapshot -no-audio &
+nohup $ANDROID_HOME/emulator/emulator -avd android_emulator -partition-size 2048 -no-snapshot -no-audio -memory 4096 -timezone America/Los_Angeles -no-boot-anim -no-window &
 
 $ANDROID_HOME/platform-tools/adb start-server
 
