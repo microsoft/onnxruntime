da8b45b7a950c6cfd1f505f370fb13542c325e0a
Thiago Crepaldi <thiago.crepaldi@microsoft.com>
2020-07-17 10:21:04 -0700

    Add Optimizer classes to the new frontend (#4280)

--- orttraining/orttraining/python/training/__init__.py
+++ orttraining/orttraining/python/training/__init__.py
@@ -1,12 +1,11 @@
-#-------------------------------------------------------------------------
+# -------------------------------------------------------------------------
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
-#--------------------------------------------------------------------------
+# --------------------------------------------------------------------------
 from onnxruntime.capi._pybind_state import TrainingParameters
 from onnxruntime.capi.training.training_session import TrainingSession
 
 
 from .orttrainer_options import ORTTrainerOptions
-from . import model_desc_validation
-from . import amp
 from .orttrainer import TrainStepInfo
+from . import amp, optim, model_desc_validation
