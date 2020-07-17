af85799415ebb701286d33878aeb526b2371ef1b
Xueyun Zhu <xzhu1900@gmail.com>
2020-07-17 02:26:47 +0000

    address feedback

--- onnxruntime/core/common/path_utils.h
+++ onnxruntime/core/common/path_utils.h
@@ -10,12 +10,14 @@ namespace onnxruntime {
 
 namespace path_utils {
 
-/** Return a PathString with concatenated args. */
+/** Return a PathString with concatenated args.
+ *  TODO: Add support for std::wstring for type std::wstring. Currently it is not supported as the underneath
+ *  MakeString doesn't support this type.
+*/
 template <typename... Args>
 PathString MakePathString(const Args&... args) {
   const std::string str = onnxruntime::MakeString(args...);
   return ToPathString(str);
 }
-
 }
 }
