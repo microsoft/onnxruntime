3281124812cd58dfbed1f9c0158b3e46ae358de7
Thiago Crepaldi <thiago.crepaldi@microsoft.com>
2020-07-17 14:55:28 -0700

    Add basic ORTTrainer API (#4435)
    This PR presents the public API for ORTTrainer for the short term
    development.
    
    It also validates and saves input parameters, which will be used in the
    next stages, such as building ONNX model, post processing the model and
    configuring the training session

--- orttraining/orttraining/python/training/__init__.py
+++ orttraining/orttraining/python/training/__init__.py
@@ -5,7 +5,6 @@
 from onnxruntime.capi._pybind_state import TrainingParameters
 from onnxruntime.capi.training.training_session import TrainingSession
 
-
 from .orttrainer_options import ORTTrainerOptions
-from .orttrainer import TrainStepInfo
+from .orttrainer import ORTTrainer, TrainStepInfo
 from . import amp, optim, model_desc_validation
