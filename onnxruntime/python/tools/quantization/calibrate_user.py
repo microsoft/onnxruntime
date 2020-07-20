b1cb51e0b6b127b9c46714107351481e4016ec5f
t-yguo <t-yguo@microsoft.com>
2020-07-20 15:11:26 -0700

    modify calibration tool

---
+++ onnxruntime/python/tools/quantization/calibrate_user.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# coding: utf-8
+# -------------------------------------------------------------------------
+# Copyright (c) Microsoft, Intel Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for
+# license information.
+# --------------------------------------------------------------------------
+
+import os
+import sys
+import numpy as np
+import re
+import subprocess
+import json
+
+import abc
+
+import onnx
+import onnxruntime
+from onnx import helper, TensorProto, numpy_helper
+from quantize import quantize, QuantizationMode
+from calibrate import calibrate
+
+#user-implement preprocess func
+from data_preprocess import preprocess_func
+
+
+class DataRetrieverInterface(metaclass=abc.ABCMeta):
+    @classmethod
+    def __subclasshook__(cls,subclass):
+        return (hasattr(subclass,'get_next') and callable(subclass.get_next) or NotImplemented)
+
+    @abc.abstractmethod
+    def get_next(self) -> dict:
+        """generate the input data dict for ONNXinferenceSession run"""
+        raise NotImplementedError
+
+class CalibrationDataRetriever(DataRetrieverInterface):
+    def __init__(self,calibration_image_folder,augmented_model_path='augmented_model.onnx'): 
+        self.image_folder = calibration_image_folder
+        self.augmented_model_path = augmented_model_path
+        self.preprocess_flag = True
+        self.enum_data_dicts = []
+        self.datasize = 0
+
+    def get_next(self):
+        if self.preprocess_flag:
+            self.preprocess_flag = False
+            session = onnxruntime.InferenceSession(self.augmented_model_path, None)
+            (_,height,width,_) = session.get_inputs()[0].shape
+            nhwc_data_list = preprocess_func(self.image_folder,height,width,size_limit = 0)
+            input_name = session.get_inputs()[0].name
+            self.datasize = len(nhwc_data_list)           
+            self.enum_data_dicts = iter([{input_name:nhwc_data_list[i]} for i in range(self.datasize)])
+        return next(self.enum_data_dicts,None)
+
+def main():
+    model_path = 'path/to/model.onnx'
+    calibration_dataset_path = 'path/to/calibration_data_set'
+    dr = CalibrationDataRetriever(calibration_dataset_path)
+    #call calibrate to generate quantization dictionary containing the zero point and scale values
+    quantization_params_dict = calibrate(model_path,dr)
+    calibrated_quantized_model = quantize(onnx.load(model_path),
+                                          quantization_mode=QuantizationMode.QLinearOps,
+                                          force_fusions=False,
+                                          quantization_params=quantization_params_dict)
+    output_model_path = 'path/to/output_model.onnx'
+    onnx.save(calibrated_quantized_model, output_model_path)
+    print('Calibrated and quantized model saved.')
+
+if __name__ == '__main__':
+   main()
--- onnxruntime/python/tools/quantization/data_preprocess.py
+++ onnxruntime/python/tools/quantization/data_preprocess.py
@@ -10,61 +10,15 @@ import os
 import sys
 import numpy as np
 
+#user implement preprocess_func
 
-def set_preprocess(preprocess_func_name):
+def preprocess_func(images_folder, height, width, size_limit=0):
     '''
-    Set up the data preprocess function name and function dict. 
-        parameter preprocess_func_name: name of the preprocess function 
-        return: function pointer 
-    '''
-    funcdict = {'preprocess_method1': preprocess_method1, 'preprocess_method2': preprocess_method2}
-    return funcdict[preprocess_func_name]
-
-
-def preprocess_method1(image_filepath, height, width):
-    '''
-    Resizes image to NCHW format. Image is scaled to range [-1, 1].
-    This method is suitable for the mobilenet model from mlperf inference git repo.
-        parameter image_filepath: path to image files
-        parameter height: image height in pixels
-        parameter width: image width in pixels
-        return: matrix characterizing image
-    '''
-    pillow_img = Image.new("RGB", (width, height))
-    pillow_img.paste(Image.open(image_filepath).resize((width, height)))
-    input_data = np.float32(pillow_img) / 127.5 - 1.0  # normalization
-    input_data -= np.mean(input_data)  # normalization
-    nhwc_data = np.expand_dims(input_data, axis=0)
-    nchw_data = nhwc_data.transpose(0, 3, 1, 2)  # ONNX Runtime standard
-    return nchw_data
-
-
-def preprocess_method2(image_filepath, height, width):
-    '''
-    Resizes and normalizes image to NCHW format. 
-    This method is suitable for the resnet50 model from mlperf inference git repo. 
-        parameter image_filepath: path to image files
-        parameter height: image height in pixels
-        parameter width: image width in pixels
-        return: matrix characterizing image
-    '''
-    pillow_img = Image.new("RGB", (width, height))
-    pillow_img.paste(Image.open(image_filepath).resize((width, height)))
-    input_data = np.float32(pillow_img) - \
-        np.array([123.68, 116.78, 103.94], dtype=np.float32)
-    nhwc_data = np.expand_dims(input_data, axis=0)
-    nchw_data = nhwc_data.transpose(0, 3, 1, 2)  # ONNX Runtime standard
-    return nchw_data
-
-
-def load_batch(images_folder, height, width, preprocess_func_name, size_limit=0):
-    '''
-    Loads a batch of images
+    Loads a batch of images and preprocess them
     parameter images_folder: path to folder storing images
     parameter height: image height in pixels
     parameter width: image width in pixels
     parameter size_limit: number of images to load. Default is 0 which means all images are picked.
-    parameter preprocess_func_name: name of the preprocess function
     return: list of matrices characterizing multiple images
     '''
     image_names = os.listdir(images_folder)
@@ -74,10 +28,14 @@ def load_batch(images_folder, height, width, preprocess_func_name, size_limit=0)
         batch_filenames = image_names
     unconcatenated_batch_data = []
 
-    preprocess_func = set_preprocess(preprocess_func_name)
     for image_name in batch_filenames:
         image_filepath = images_folder + '/' + image_name
-        nchw_data = preprocess_func(image_filepath, height, width)
-        unconcatenated_batch_data.append(nchw_data)
+        pillow_img = Image.new("RGB", (width, height))
+        pillow_img.paste(Image.open(image_filepath).resize((width, height)))
+        input_data = np.float32(pillow_img) - \
+        np.array([123.68, 116.78, 103.94], dtype=np.float32)
+        nhwc_data = np.expand_dims(input_data, axis=0)
+        unconcatenated_batch_data.append(nhwc_data)
     batch_data = np.concatenate(np.expand_dims(unconcatenated_batch_data, axis=0), axis=0)
     return batch_data
+
