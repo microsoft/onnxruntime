6e09f620bba7eccf462a470940ef456985cf1556
Ksenija Stanojevic <ksenija.stanojevic@gmail.com>
2020-07-20 09:54:42 -0700

    update trilu

--- onnxruntime/contrib_ops/cpu/trilu.cc
+++ onnxruntime/contrib_ops/cpu/trilu.cc
@@ -16,16 +16,16 @@ ONNX_OPERATOR_KERNEL_EX(
     kMSDomain,
     1,
     kCpuExecutionProvider,
-    KernelDefBuilder().TypeConstraint("T", BuildKernelDefConstraints<float, double, int64_t>()),
+    KernelDefBuilder().MayInplace(0, 0).TypeConstraint("T", BuildKernelDefConstraints<float, double, int64_t>()),
     Trilu);
 
 template <typename T>
 struct Trilu::ComputeImpl {
-  void operator()(const Tensor* X, Tensor* Y, int64_t k_val, int64_t up) const {
+  void operator()(const Tensor* X, Tensor* Y, int64_t k_val, bool up) const {
     const auto& X_shape = X->Shape();
     int64_t X_num_dims = static_cast<int64_t>(X_shape.NumDimensions());
 
-    const auto* X_data = X->Data<T>();
+    const auto* X_data = reinterpret_cast<const T*>(X->DataRaw());//X->Data<T>();
     int64_t matrix_h = static_cast<int64_t>(X_shape[X_num_dims - 2]);
     int64_t matrix_w = static_cast<int64_t>(X_shape[X_num_dims - 1]);
 
@@ -35,7 +35,7 @@ struct Trilu::ComputeImpl {
     }
 
     int64_t num_matrix_elems = matrix_h * matrix_w;
-    auto* Y_data = Y->template MutableData<T>();
+    auto* Y_data = reinterpret_cast<T*>(Y->MutableDataRaw());//Y->template MutableData<T>();
     for (int64_t b = 0; b < batch_size; b++) {  // can be parallelized if need to
       auto X_batch_data = X_data + (b * num_matrix_elems);
       auto Y_batch_data = Y_data + (b * num_matrix_elems);
@@ -50,8 +50,21 @@ struct Trilu::ComputeImpl {
           matrix_w);
 
       output_mat = input_mat;
-      for (int64_t i = -1 * matrix_h; i < up * k_val; i++){
-        output_mat.diagonal(up * i).array() = static_cast<T>(0);
+
+      if (up) {
+        int64_t start_i = k_val > 0 ? 0 : 1 - k_val;
+        for (int64_t i = start_i; i < matrix_h ; i++){
+          for (int64_t j = 0; j < i + k_val && j < matrix_w; j ++) {
+            output_mat(i, j) = static_cast<int64_t>(0);
+          }
+        }
+      } else {
+        int64_t start_j = k_val < 0 ? 0 : 1 + k_val;
+        for (int64_t j = start_j; j < matrix_w ; j++){
+          for (int64_t i = 0; i < j - k_val && i < matrix_h; i ++) {
+            output_mat(i, j) = static_cast<int64_t>(0);
+          }
+        }
       }
     }    
   }
@@ -61,7 +74,7 @@ Status Trilu::Compute(OpKernelContext* ctx) const {
   const auto* X = ctx->Input<Tensor>(0);
   const auto* k = ctx->Input<Tensor>(1);
 
-  int64_t up = upper_ ? 1 : -1;
+  bool up = upper_;
   int64_t k_val = 0;
   if (k) {
     ORT_ENFORCE(IsScalarOr1ElementVector(k), "k should be a 1-D or 0-D tensor.");
