60bc3ca2f8dbc523c948f76f95c16b11173a7e7e
Ksenija Stanojevic <ksenija.stanojevic@gmail.com>
2020-07-20 16:11:18 -0700

    add tests for tril

--- tools/test/test_custom_ops_pytorch_exporter.py
+++ tools/test/test_custom_ops_pytorch_exporter.py
@@ -122,6 +122,21 @@ class ONNXExporterTest(unittest.TestCase):
             self.run_test(model, x, custom_opsets={'com.microsoft': 1})
 
         for i in range(-5, 5):
+            class Module(torch.nn.Module):
+                def forward(self, input):
+                    return input.tril(diagonal=i)
+
+            model = Module()
+            x = torch.randn(5, 4, 7, dtype=torch.float32)
+            self.run_test(model, x, custom_opsets={'com.microsoft': 1})
+
+            x = torch.randn(5, 4, 0, dtype=torch.float32)
+            self.run_test(model, x, custom_opsets={'com.microsoft': 1})
+
+            x = torch.randn(5, 0, 0, dtype=torch.float32)
+            self.run_test(model, x, custom_opsets={'com.microsoft': 1})
+
+        for i in range(-5, 5):
             class Module2D(torch.nn.Module):
                 def forward(self, input):
                     return input.triu(diagonal=i)
@@ -135,6 +150,22 @@ class ONNXExporterTest(unittest.TestCase):
 
             x = torch.randn(0, 0, dtype=torch.float32)
             self.run_test(model, x, custom_opsets={'com.microsoft': 1})
+
+        for i in range(-5, 5):
+            class Module2D(torch.nn.Module):
+                def forward(self, input):
+                    return input.tril(diagonal=i)
+
+            model = Module2D()
+            x = torch.randn(4, 7, dtype=torch.float32)
+            self.run_test(model, x, custom_opsets={'com.microsoft': 1})
+
+            x = torch.randn(0, 7, dtype=torch.float32)
+            self.run_test(model, x, custom_opsets={'com.microsoft': 1})
+
+            x = torch.randn(0, 0, dtype=torch.float32)
+            self.run_test(model, x, custom_opsets={'com.microsoft': 1})
+
 
 
 # opset 10 tests
