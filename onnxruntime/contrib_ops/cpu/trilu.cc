dd67b34dbc28626ae39805ed53298ffcb979e211
Ksenija Stanojevic <ksenija.stanojevic@gmail.com>
2020-07-21 11:02:22 -0700

    update trilu

--- onnxruntime/contrib_ops/cpu/trilu.cc
+++ onnxruntime/contrib_ops/cpu/trilu.cc
@@ -20,57 +20,54 @@ ONNX_OPERATOR_KERNEL_EX(
     Trilu);
 
 template <typename T>
-struct Trilu::ComputeImpl {
-  void operator()(const Tensor* X, Tensor* Y, int64_t k_val, bool up) const {
-    const auto& X_shape = X->Shape();
-    int64_t X_num_dims = static_cast<int64_t>(X_shape.NumDimensions());
+static Status TriluImpl(OpKernelContext* ctx, const Tensor* X, Tensor* Y, int64_t k_val, bool up) {
+  //void operator()(const Tensor* X, Tensor* Y, int64_t k_val, bool up) const {
+  const auto& X_shape = X->Shape();
+  int64_t X_num_dims = static_cast<int64_t>(X_shape.NumDimensions());
 
-    const auto* X_data = reinterpret_cast<const T*>(X->DataRaw());
-    int64_t matrix_h = static_cast<int64_t>(X_shape[X_num_dims - 2]);
-    int64_t matrix_w = static_cast<int64_t>(X_shape[X_num_dims - 1]);
+  const auto* X_data = reinterpret_cast<const T*>(X->DataRaw());
+  int64_t matrix_h = static_cast<int64_t>(X_shape[X_num_dims - 2]);
+  int64_t matrix_w = static_cast<int64_t>(X_shape[X_num_dims - 1]);
 
-    int64_t batch_size = 1;
-    for (int64_t i = 0; i < X_num_dims - 2; ++i) {
-      batch_size *= X_shape[i];
+  int64_t batch_size = 1;
+  for (int64_t i = 0; i < X_num_dims - 2; ++i) {
+    batch_size *= X_shape[i];
+  }
+
+  int64_t num_matrix_elems = matrix_h * matrix_w;
+  auto* Y_data = reinterpret_cast<T*>(Y->MutableDataRaw());
+  for (int64_t b = 0; b < batch_size; b++) {  // can be parallelized if need to
+    auto X_batch_data = X_data + (b * num_matrix_elems);
+    auto Y_batch_data = Y_data + (b * num_matrix_elems);
+
+    auto input_mat = ConstEigenMatrixMapRowMajor<T>(X_batch_data, matrix_h, matrix_w);
+    auto output_mat = EigenMatrixMapRowMajor<T>(Y_batch_data, matrix_h, matrix_w);
+
+    if (X_batch_data != Y_batch_data) {
+      output_mat = input_mat;
     }
 
-    int64_t num_matrix_elems = matrix_h * matrix_w;
-    auto* Y_data = reinterpret_cast<T*>(Y->MutableDataRaw());
-    for (int64_t b = 0; b < batch_size; b++) {  // can be parallelized if need to
-      auto X_batch_data = X_data + (b * num_matrix_elems);
-      auto Y_batch_data = Y_data + (b * num_matrix_elems);
-
-      auto input_mat = ConstEigenMatrixMapRowMajor<T>(
-          X_batch_data,
-          matrix_h,
-          matrix_w);
-      auto output_mat = EigenMatrixMapRowMajor<T>(
-          Y_batch_data,
-          matrix_h,
-          matrix_w);
-
-      output_mat = input_mat;
-
-      if (up) {
-        int64_t start_i = k_val > 0 ? 0 : 1 - k_val;
-        for (int64_t i = start_i; i < matrix_h; i++) {
-          for (int64_t j = 0; j < i + k_val && j < matrix_w; j++) {
-            output_mat(i, j) = static_cast<int64_t>(0);
-          }
+    if (up) {
+      int64_t start_i = k_val > 0 ? 0 : 1 - k_val;
+      for (int64_t i = start_i; i < matrix_h; i++) {
+        for (int64_t j = 0; j < i + k_val && j < matrix_w; j++) {
+          output_mat(i, j) = 0;
         }
-      } else {
-        int64_t start_j = k_val < 0 ? 0 : 1 + k_val;
-        for (int64_t j = start_j; j < matrix_w; j++) {
-          for (int64_t i = 0; i < j - k_val && i < matrix_h; i++) {
-            output_mat(i, j) = static_cast<int64_t>(0);
-          }
+      }
+    } else {
+      int64_t end_i = std::min(matrix_h, matrix_w - k_val);
+      for (int64_t i = 0; i < end_i; i++) {
+        for (int64_t j = std::max(static_cast<int64_t>(0), i + k_val + 1); j < matrix_w; j++) {
+          output_mat(i, j) = 0;
         }
       }
     }
   }
+  return Status::OK();
 };
 
 Status Trilu::Compute(OpKernelContext* ctx) const {
+  Status status;
   const auto* X = ctx->Input<Tensor>(0);
   const auto* k = ctx->Input<Tensor>(1);
 
@@ -90,10 +87,19 @@ Status Trilu::Compute(OpKernelContext* ctx) const {
     return Status(ONNXRUNTIME, INVALID_ARGUMENT, "Input tensor should have a rank of at least 2");
   }
 
-  utils::MLTypeCallDispatcher<ComputeImpl, float, double, int64_t> t_disp(X->GetElementType());
-  t_disp.Invoke(X, Y, k_val, up);
-
-  return Status::OK();
+  MLDataType data_type = X->DataType();
+  const auto element_size = data_type->Size();
+  switch (element_size) {
+    case sizeof(float):
+      status = TriluImpl<float>(ctx, X, Y, k_val, up);
+      break;
+    case sizeof(double):
+      status = TriluImpl<double>(ctx, X, Y, k_val, up);
+      break;
+    default:
+      ORT_THROW("Unsupported input data type of ", data_type);
+  }
+  return status;
 }
 
 }  // namespace contrib
