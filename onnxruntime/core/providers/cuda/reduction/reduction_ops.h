cb5040ced044a53f79ee8395c1f5da1a8474c3f1
Ethan Tao <ettao@microsoft.com>
2020-07-22 12:58:33 -0700

    code clean up

--- onnxruntime/core/providers/cuda/reduction/reduction_ops.h
+++ onnxruntime/core/providers/cuda/reduction/reduction_ops.h
@@ -179,18 +179,6 @@ class ReduceSum final : public ReduceKernel<true> {
   }
 };
 
-//template <typename T>
-//class ReduceSumTraining final : public ReduceKernel<true> {
-// public:
-//  ReduceSumTraining(const OpKernelInfo& info) : ReduceKernel<true>(info) {
-//    fast_reduction_ = true;
-//  }
-//
-//  Status ComputeInternal(OpKernelContext* ctx) const override {
-//    return ComputeImpl<T>(ctx, CUDNN_REDUCE_TENSOR_ADD);
-//  }
-//};
-
 template <typename T>
 class ReduceLogSum final : public ReduceKernel<true> {
  public:
