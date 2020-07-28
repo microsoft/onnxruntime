25798e1b5d1b06d4c213d4b8b734a79b7d5e5a32
Ksenija Stanojevic <ksenija.stanojevic@gmail.com>
2020-07-28 13:35:40 -0700

    add onnx_test_custom_ops as part of ORT CI

--- tools/ci_build/github/linux/test_custom_ops_pytorch_export.sh
+++ tools/ci_build/github/linux/test_custom_ops_pytorch_export.sh
@@ -7,3 +7,5 @@ pip3 install --user /build/Release/dist/*.whl
 export PYTHONPATH=/onnxruntime_src/tools:/usr/local/lib/python3.6/site-packages:$PYTHONPATH
 
 python3 -m pytest -v /onnxruntime_src/tools/test/test_custom_ops_pytorch_exporter.py || exit 1
+
+cd /build/Release python3 -m pytest -v ../../onnxruntime/test/python/onnx_test_custom_ops.py
