b555fe5f72f720dd3ce5611e3090128d151b3852
liqun <liqun@OrtTrainingDev4.af05slrtruoetgaxwwjv5nsq5e.px.internal.cloudapp.net>
2020-07-29 19:32:27 +0000

    move mpi test to e2e

--- tools/ci_build/build.py
+++ tools/ci_build/build.py
@@ -1112,22 +1112,6 @@ def run_training_python_frontend_tests(cwd):
         sys.executable, 'orttraining_test_transformers.py',
         'BertModelTest.test_for_pretraining_full_precision_list_and_dict_input'], cwd=cwd)
 
-    def mpirun_subprocess(mpicmd, cwd):
-        mpi = subprocess.Popen(
-            mpicmd,
-            cwd=cwd,
-            shell=True)
-        mpi.wait()
-        if mpi.returncode != 0:
-            raise TestFailure(mpicmd, "Test failed with returncode={}.".format(mpi.returncode))
-
-    import torch
-    ngpus = torch.cuda.device_count()
-    if ngpus > 1:
-        run_glue_mpicmd = 'mpirun -n {} python orttraining_run_glue.py'.format(ngpus)
-        print("RUN: ", run_glue_mpicmd)
-        mpirun_subprocess(run_glue_mpicmd, cwd)
-
 
 def run_training_python_frontend_e2e_tests(cwd):
     # frontend tests are to be added here:
@@ -1163,6 +1147,22 @@ def run_training_python_frontend_e2e_tests(cwd):
     run_subprocess([
         sys.executable, 'orttraining_test_transformers.py',
         'BertModelTest.test_for_pretraining_mixed_precision_all'], cwd=cwd)
+
+    def mpirun_subprocess(mpicmd, cwd):
+        mpi = subprocess.Popen(
+            mpicmd,
+            cwd=cwd,
+            shell=True)
+        mpi.wait()
+        if mpi.returncode != 0:
+            raise TestFailure(mpicmd, "Test failed with returncode={}.".format(mpi.returncode))
+
+    import torch
+    ngpus = torch.cuda.device_count()
+    if ngpus > 1:
+        run_glue_mpicmd = 'mpirun -n {} python orttraining_run_glue.py'.format(ngpus)
+        print("RUN: ", run_glue_mpicmd)
+        mpirun_subprocess(run_glue_mpicmd, cwd)
 
 
 def run_onnxruntime_tests(args, source_dir, ctest_path, build_dir, configs):
