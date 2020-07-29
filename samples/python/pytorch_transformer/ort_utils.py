ac3cba14bac958c8dbad4e37f8b34baeb6f5c34f
Rayan-Krishnan <rayankrishnan@live.com>
2020-07-29 16:40:06 -0700

    Add ONNX Model Export to New Frontend (#4612)
    Co-authored-by: Rayan Krishnan <t-rakr@OrtDevTest2v100.af05slrtruoetgaxwwjv5nsq5e.px.internal.cloudapp.net>
    Co-authored-by: Thiago Crepaldi <thiago.crepaldi@microsoft.com>

---
+++ samples/python/pytorch_transformer/ort_utils.py
@@ -0,0 +1,18 @@
+import torch
+
+
+def my_loss(x, target):
+    x = x.view(-1, 28785)
+    return torch.nn.CrossEntropyLoss()(x, target)
+
+
+def transformer_model_description():
+    bptt = 35
+    ntokens = 28785
+    batch_size = 20
+
+    model_desc = {'inputs':  [('input1', [bptt, batch_size]),
+                              ('label', [bptt, batch_size, ntokens],)],
+                  'outputs': [('loss', [], True),
+                              ('predictions', [bptt, batch_size, ntokens])]}
+    return model_desc
