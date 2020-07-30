935207ceee1f997ef3df9bedab7b35044eb42b2c
hariharans29 <shariharan91@gmail.com>
2020-07-29 21:53:38 -0700

    Fix training build

--- onnxruntime/core/providers/cuda/tensor/slice.cc
+++ onnxruntime/core/providers/cuda/tensor/slice.cc
@@ -85,11 +85,10 @@ static Status SliceImpCore(const void* input_data, void* output_data,
 
 namespace SliceCuda {
 
-Status Impl(const void* input_data,
-            const TensorShape& input_shape,
-            void* output_data,
-            SliceOp::PrepareForComputeMetadata& compute_metadata,
-            size_t element_size) {
+static Status ComputeSliceStrides(const TensorShape& input_shape,
+                                  TArray<int64_t>& input_strides,
+                                  TArray<fast_divmod>& output_strides,
+                                  SliceOp::PrepareForComputeMetadata& compute_metadata) {
   const auto& input_dimensions = input_shape.GetDims();
   size_t dimension_count = input_dimensions.size();
   // if we are able to flatten the output dims we updated 'starts' and 'steps' to match the smaller number of dims.
@@ -98,9 +97,7 @@ Status Impl(const void* input_data,
     dimension_count = compute_metadata.p_flattened_output_dims_->size();
   }
 
-  TArray<int64_t> starts_buffer(compute_metadata.starts_);
-  TArray<int64_t> steps_buffer(compute_metadata.steps_);
-  TArray<int64_t> input_strides(gsl::narrow_cast<int32_t>(dimension_count));
+  input_strides.SetSize(gsl::narrow_cast<int32_t>(dimension_count));
   const gsl::span<int64_t> input_strides_span = gsl::make_span(input_strides.Data(), input_strides.Size());
   if (compute_metadata.p_flattened_output_dims_ != nullptr) {
     // we were able to flatten the innermost dimensions as they're being copied in full to the output.
@@ -121,12 +118,31 @@ Status Impl(const void* input_data,
 
   TensorPitches original_output_strides(
       compute_metadata.p_flattened_output_dims_ != nullptr ? compute_metadata.flattened_output_dims_ : compute_metadata.output_dims_);
-  TArray<fast_divmod> output_strides(gsl::narrow_cast<int32_t>(original_output_strides.size()));
+  output_strides.SetSize(gsl::narrow_cast<int32_t>(original_output_strides.size()));
   for (int32_t i = 0; i < static_cast<int32_t>(original_output_strides.size()); ++i) {
     output_strides[i] = fast_divmod(gsl::narrow_cast<int>(original_output_strides[i]));
   }
 
+  return Status::OK();
+}
+
+Status Impl(const void* input_data,
+            const TensorShape& input_shape,
+            void* output_data,
+            SliceOp::PrepareForComputeMetadata& compute_metadata,
+            size_t element_size) {
+  const auto& input_dimensions = input_shape.GetDims();
+  size_t dimension_count = input_dimensions.size();
+
+  TArray<int64_t> starts_buffer(compute_metadata.starts_);
+  TArray<int64_t> steps_buffer(compute_metadata.steps_);
+  TArray<int64_t> input_strides;
+  TArray<fast_divmod> output_strides;
+
+  ORT_RETURN_IF_ERROR(ComputeSliceStrides(input_shape, input_strides, output_strides, compute_metadata));
+
   TensorShape output_shape(compute_metadata.output_dims_);
+
   ORT_RETURN_IF_ERROR(SliceImpCore(input_data,
                                    output_data,
                                    element_size,
@@ -145,7 +161,8 @@ template <bool dynamic>
 Status Slice<dynamic>::ComputeInternal(OpKernelContext* ctx) const {
   const Tensor* input_tensor = GetSlicedOrUnslicedTensor(ctx);
   ORT_ENFORCE(nullptr != input_tensor);
-  auto& input_dimensions = input_tensor->Shape().GetDims();
+  const auto& input_shape = input_tensor->Shape();
+  const auto& input_dimensions = input_shape.GetDims();
   if (input_dimensions.empty()) return ORT_MAKE_STATUS(ONNXRUNTIME, INVALID_ARGUMENT, "Cannot slice scalars");
 
   SliceOp::PrepareForComputeMetadata compute_metadata(input_dimensions);
@@ -160,13 +177,21 @@ Status Slice<dynamic>::ComputeInternal(OpKernelContext* ctx) const {
     ORT_RETURN_IF_ERROR(PrepareForCompute(StartsAttribute(), EndsAttribute(), AxesAttribute(), compute_metadata));
   }
 
-  auto* output_tensor = ctx->Output(0, compute_metadata.output_dims_);
+  TensorShape output_shape(compute_metadata.output_dims_);
 
-  return SliceCuda::Impl(input_tensor->DataRaw(),
-                         input_tensor->Shape(),
-                         output_tensor->MutableDataRaw(),
-                         compute_metadata,
-                         input_tensor->DataType()->Size());
+  TArray<int64_t> starts_buffer(compute_metadata.starts_);
+  TArray<int64_t> steps_buffer(compute_metadata.steps_);
+  TArray<int64_t> input_strides;
+  TArray<fast_divmod> output_strides;
+
+  ORT_RETURN_IF_ERROR(SliceCuda::ComputeSliceStrides(input_shape, input_strides, output_strides, compute_metadata));
+
+  ORT_RETURN_IF_ERROR(CallSliceImp(input_tensor->DataType()->Size(), input_dimensions.size(), starts_buffer,
+                                   steps_buffer, input_strides,
+                                   output_strides, ctx,
+                                   output_shape));
+
+  return Status::OK();
 }
 
 template <bool dynamic>
@@ -186,7 +211,7 @@ template <bool dynamic>
 Status Slice<dynamic>::CallSliceImp(size_t element_size, size_t dimension_count, const TArray<int64_t>& starts_buffer,
                                     const TArray<int64_t>& steps_buffer, const TArray<int64_t>& input_strides,
                                     const TArray<fast_divmod>& output_strides, OpKernelContext* ctx,
-                                    TensorShape output_shape) const {
+                                    TensorShape& output_shape) const {
   const auto* input_tensor = ctx->Input<Tensor>(0);
   auto* output_tensor = ctx->Output(0, output_shape);
 
