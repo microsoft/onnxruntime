01ca6392cb165f3ebad6fc3c28016adad5a26688
Changming Sun <chasun@microsoft.com>
2020-08-03 10:18:10 -0700

    Avoid building ONNX of every history ONNX versions in our CI (#4678)
    1. Avoid building ONNX of every history ONNX versions in our CI, it is costly and easy to fail.
    2. Run docker command without sudo. Previously the user is not in docker group, now Azure DevOps Service have added it in.

--- tools/ci_build/github/linux/docker/scripts/install_ubuntu.sh
+++ tools/ci_build/github/linux/docker/scripts/install_ubuntu.sh
@@ -54,7 +54,6 @@ if [ "$OS_VERSION" = "16.04" ]; then
         zip \
         rsync libunwind8 libpng16-dev libexpat1-dev \
         python3-setuptools python3-numpy python3-wheel python python3-pip python3-pytest \
-        libprotobuf-dev libprotobuf9v5 protobuf-compiler \
         openjdk-8-jdk"
 else # ubuntu18.04
     PACKAGE_LIST="autotools-dev \
@@ -88,7 +87,6 @@ else # ubuntu18.04
         zip \
         rsync libunwind8 libpng-dev libexpat1-dev \
         python3-setuptools python3-numpy python3-wheel python python3-pip python3-pytest \
-        libprotobuf-dev libprotobuf10 protobuf-compiler \
         openjdk-11-jdk"
 fi
 
