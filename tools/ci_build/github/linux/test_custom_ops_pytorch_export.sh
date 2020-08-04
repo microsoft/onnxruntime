2d7d1509089749be3d7e35e991c0b5eadde40a31
neginraoof <neginmr@utexas.edu>
2020-08-04 15:07:55 -0700

    Fix documentation for tests

--- tools/ci_build/github/linux/test_custom_ops_pytorch_export.sh
+++ tools/ci_build/github/linux/test_custom_ops_pytorch_export.sh
@@ -8,6 +8,6 @@ export PYTHONPATH=/onnxruntime_src/tools:/usr/local/lib/python3.6/site-packages:
 
 python3 -m pytest -v /onnxruntime_src/tools/test/test_custom_ops_pytorch_exporter.py || exit 1
 
-cd /build/Release && python3 -m pytest -v ../../onnxruntime/test/python/onnx_test_trilu.py
+cd /build/Release && python3 -m pytest -v /onnxruntime_src/onnxruntime/test/python/onnx_test_trilu.py
 
 cd /build/Release && ./onnxruntime_test_all --gtest_filter=ShapeInferenceTests.*
