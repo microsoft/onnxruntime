5eaa67ccf0faf9365d8412afde0a35b81efff147
gwang-msft <62914304+gwang-msft@users.noreply.github.com>
2020-08-06 14:11:59 -0700

    [Android NNAPI EP] Enable test for BatchNormalization, enable dev_mode for Android, fix some issues in concat (#4715)
    * update batch_norm test, enable dev_mode for nnapi, ignore onnx protobuf warning for nnapi ep
    
    * fix some issues in concat and mark input without shape as not supported for now
    
    * address review comments
    
    * addressed comments

--- tools/ci_build/build.py
+++ tools/ci_build/build.py
@@ -556,7 +556,7 @@ def generate_build_tree(cmake_path, source_dir, build_dir, cuda_home, cudnn_home
             "OFF" if args.skip_winml_tests else "ON"),
         "-Donnxruntime_GENERATE_TEST_REPORTS=ON",
         "-Donnxruntime_DEV_MODE=" + (
-            "OFF" if args.android or args.use_acl or args.use_armnn or
+            "OFF" if args.use_acl or args.use_armnn or
             (args.ios and is_macOS()) else "ON"),
         "-DPYTHON_EXECUTABLE=" + sys.executable,
         "-Donnxruntime_USE_CUDA=" + ("ON" if args.use_cuda else "OFF"),
