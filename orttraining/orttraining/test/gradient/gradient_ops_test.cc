7af92b9b0402544231f4ac9d8200742401b30ee9
suffian khan <sukha@OrtTrainingDev1.af05slrtruoetgaxwwjv5nsq5e.px.internal.cloudapp.net>
2020-08-06 14:39:04 +0000

    revert disable for debug builds

--- orttraining/orttraining/test/gradient/gradient_ops_test.cc
+++ orttraining/orttraining/test/gradient/gradient_ops_test.cc
@@ -1,7 +1,7 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-// #ifdef NDEBUG  // disable for debug builds because some of these tests are slow
+#ifdef NDEBUG  // disable for debug builds because some of these tests are slow
 
 #include <algorithm>
 #include <bitset>
@@ -1908,5 +1908,5 @@ TEST(GradientCheckerTest, ExpandGrad) {
 }  // namespace test
 }  // namespace onnxruntime
 
-// #endif  // NDEBUG
+#endif  // NDEBUG
 
