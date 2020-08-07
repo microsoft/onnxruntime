8812b57bd4034fef19e2967a6c7e7e0378062d24
Sheil Kumar <sheilk@microsoft.com>
2020-08-06 17:12:47 -0700

    Add experimental winrt api idl with dummy type to satisfy the build

---
+++ winml/lib/Api.Experimental/Dummy.cpp
@@ -0,0 +1,10 @@
+﻿#include "pch.h"
+#include "Dummy.h"
+
+namespace winrt::Microsoft::AI::MachineLearning::Experimental::implementation
+{
+    void Dummy::Test()
+    {
+        throw hresult_not_implemented();
+    }
+}
---
+++ winml/lib/Api.Experimental/Dummy.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+#include "Dummy.g.h"
+
+namespace winrt::Microsoft::AI::MachineLearning::Experimental::implementation
+{
+    struct Dummy : DummyT<Dummy>
+    {
+        Dummy() = default;
+
+        void Test();
+    };
+}
+
+namespace winrt::Microsoft::AI::MachineLearning::Experimental::factory_implementation
+{
+    struct Dummy : DummyT<Dummy, implementation::Dummy>
+    {
+    };
+}
