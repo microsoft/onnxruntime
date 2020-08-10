7242476f754a52b119b833ffdc0f4f97f4085c71
gwang <wanggy@outlook.com>
2020-08-10 15:01:27 -0700

    fix traning ci build break

--- orttraining/orttraining/models/runner/training_runner.cc
+++ orttraining/orttraining/models/runner/training_runner.cc
@@ -46,6 +46,7 @@ static SessionOptions SESSION_OPTION = {
     true,                              //use_per_session_threads
     true,                              //thread_pool_allow_spinning
     false,                             //use_deterministic_compute
+    true,                              //use_prepacking
     {},                                //session_configurations
 };
 
