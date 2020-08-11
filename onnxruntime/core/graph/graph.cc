77a09b4a0e51ba1df1f5bbcd390d02a58db66c52
Derek Murray <Derek.Murray@microsoft.com>
2020-08-11 16:23:34 -0700

    Set `GraphResolveNeeded(true)` after adding a node
    At present, after calling `Graph::AddNode()` (e.g. in an optimization pass) the `Node::op_` field for the added nodes may be nullptr. This can cause segfaults in subsequent code that accesses this field.

--- onnxruntime/core/graph/graph.cc
+++ onnxruntime/core/graph/graph.cc
@@ -2569,7 +2569,8 @@ Node& Graph::AddNode(const std::string& name,
   node->Init(name, op_type, description, inputs, outputs, attributes, domain);
   if (0 != op_type.compare(kNoOp)) {
     GraphProtoSyncNeeded(true);
-  }
+  } 
+  GraphResolveNeeded(true);
 
   return *node;
 }
