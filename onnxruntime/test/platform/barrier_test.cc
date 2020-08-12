194130638e4f3c89e8e8e1384d8065700d337b1a
Tim Harris <tiharr@microsoft.com>
2020-08-12 13:08:02 +0100

    Fix for case sensitive FS

--- onnxruntime/test/platform/barrier_test.cc
+++ onnxruntime/test/platform/barrier_test.cc
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-#include "core/platform/barrier.h"
+#include "core/platform/Barrier.h"
 #include "core/platform/threadpool.h"
 
 #include <core/common/make_unique.h>
