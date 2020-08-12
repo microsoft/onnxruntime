191de261900eeb8479e47dbcb10bf367f941f3b3
harshithapv <havenka@microsoft.com>
2020-08-12 17:15:13 +0000

    Changes in SSCE for all tokens ignored case.

--- orttraining/orttraining/python/ort_trainer.py
+++ orttraining/orttraining/python/ort_trainer.py
@@ -788,7 +788,19 @@ class ORTTrainer():
             input_descs = self.input_desc_with_lr
 
         self.current_step += 1
-
+        if 320 <= self.global_step_ <= 322: 
+            counts = []
+            lengths = []
+            # get data properties
+            for t in args[0]:
+                if len(t.size()) == 1:
+                    frequency = t.eq(-1).sum()
+                else:
+                    frequency = t.eq(-1).sum(dim = 1)
+                total_size = t.size()
+                lengths.append(total_size)
+                counts.append(frequency)
+            print('SystemLog: train_step: counts of -1 = ', counts,"total_length = ",lengths, "current_step = ",self.current_step, "global_step = ", self.global_step_ )
         # handle gradient accumulation in fully optimized mode
         run_options = None
         has_if_all_finite = False
@@ -829,22 +841,32 @@ class ORTTrainer():
             # optimization has done, increase self.global_step_
             self.global_step_ = self.global_step_ + 1
         
-        if self.current_step % self.gradient_accumulation_steps == 0:
-            # print grad norms
-            loss_scale_to_print = args[0][-1]
-            if  self.global_step_ % 1000 == 0 and self.world_rank == 0 and loss_scale_to_print:
+        if (self.current_step % self.gradient_accumulation_steps == 0):#or (self.global_step_ >= 320 and self.global_step_ <= 330):
+            # print grad norm
+            if not self.use_mixed_precision:
+                loss_scale_to_print = 1.0
+            else:
+                loss_scale_to_print = args[0][-1]
+            if  (self.global_step_ % 10 == 0 and self.world_rank == 0) or ( self.world_rank == 0 and self.global_step_ >= 320 and self.global_step_ <= 330 ) :
                 states = self.session.get_state()
+                print('SystemLog: train_step: with SSCE change loss_scale = '+ str(loss_scale_to_print) + ", global_step = " + str(self.global_step_) )
                 for name in states:
                     tensor = states[name]
-                    if name.startswith('model_') and name.endswith('grad_accumulate_buffer') and 'fp16' not in name:
+                    if name.startswith('model_') and name.endswith('grad_accumulate_buffer'):
                         p_name = name[7:-23]
-                        #self.writer.add_scalar('train/summary/scalar/'+p_name+'_grad', torch.norm(tensor), self.total_steps)
-                        print('train/summary/scalar/'+ p_name+'_grad', torch.norm(torch.from_numpy(tensor))/loss_scale_to_print, self.global_step_)
-                    if name.startswith('model_') and ('weight' in name or 'bias' in name) and 'fp16' not in name:
+                        if 'fp16' not in name:
+                            #self.writer.add_scalar('train/summary/scalar/'+p_name+'_grad', torch.norm(tensor), self.total_steps)
+                            print('SystemLog: train_step: grad_norm/'+ p_name, torch.norm(torch.from_numpy(tensor))/loss_scale_to_print, self.global_step_)
+                        else:
+                            print('SystemLog: train_step: grad_norm/' + p_name, torch.norm(torch.from_numpy(tensor).to('cuda').to(torch.float16))/loss_scale_to_print,  self.global_step_)
+                    if name.startswith('model_') and ('weight' in name or 'bias' in name):
                         p_name = name[7:]
-                        #self.writer.add_scalar('train/summary/scalar/'+p_name+'_grad', torch.norm(tensor), self.total_steps)
-                        print('train/summary/scalar/'+ p_name , torch.norm(torch.from_numpy(tensor))/loss_scale_to_print, self.global_step_)   
-
+                        if 'fp16' not in name:
+                            #self.writer.add_scalar('train/summary/scalar/'+p_name+'_grad', torch.norm(tensor), self.total_steps)
+                            print('SystemLog: train_step: weight_norm/'+ p_name , torch.norm(torch.from_numpy(tensor))/loss_scale_to_print, self.global_step_)   
+                        else:
+                            print('SystemLog: train_step: weight_norm/' + p_name, torch.norm(torch.from_numpy(tensor).to('cuda').to(torch.float16))/loss_scale_to_print,  self.global_step_)
+ 
         if fetches is not None:
             results = [session_run_results[fetch] for fetch in fetches]
         elif has_if_all_finite and self.loss_scaler_ is None:
@@ -852,7 +874,11 @@ class ORTTrainer():
             results = [session_run_results[output_desc.name_] for output_desc in self.output_desc_with_all_fp_16_or_fp32_gradients_finite]
         else:
             results = [session_run_results[output_desc.name_] for output_desc in self.model_desc_.outputs_]
-
+        
+        if  (self.global_step_ % 10 == 0 and self.world_rank == 0) or ( self.world_rank == 0 and self.global_step_ >= 320 and self.global_step_ <= 330 ) :
+            loss = results[0] if len(results) == 1 else results
+            print("SystemLog: train_step: loss = " ,loss[0], self.global_step_)
+            
         return results[0] if len(results) == 1 else results
 
     def __call__(self, *args, **kwargs):
