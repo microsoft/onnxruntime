6f9636c15dc5c9df3beb4abf62a279e65b54b95d
Tim Harris <tiharr@microsoft.com>
2020-08-12 12:34:52 +0100

    Add missing include

--- include/onnxruntime/core/platform/Barrier.h
+++ include/onnxruntime/core/platform/Barrier.h
@@ -7,7 +7,10 @@
 // Public License v. 2.0. If a copy of the MPL was not distributed
 // with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+#include <assert.h>
+
 #include "core/platform/ort_mutex.h"
+
 #include <mutex>
 #include <atomic>
 
