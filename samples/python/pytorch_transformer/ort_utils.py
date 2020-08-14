046be7f8f051ee04e526d73f667c5532b9583bcf
Thiago Crepaldi <thiago.crepaldi@microsoft.com>
2020-08-13 19:06:51 -0700

    Fix Dynamic Axes feature and add unit test (#4795)

--- samples/python/pytorch_transformer/ort_utils.py
+++ samples/python/pytorch_transformer/ort_utils.py
@@ -10,19 +10,34 @@ def my_loss(x, target):
 
 
 def transformer_model_description(bptt=35, batch_size=20, ntokens=28785):
-    # TODO: Update to Dynamic Axis when backend is fixed
     model_desc = {'inputs':  [('input1', [bptt, batch_size]),
-                              ('label', [bptt, batch_size, ntokens],)],
+                              ('label', [bptt * batch_size])],
                   'outputs': [('loss', [], True),
                               ('predictions', [bptt, batch_size, ntokens])]}
     return model_desc
 
 
+def transformer_model_description_dynamic_axes(ntokens=28785):
+    model_desc = {'inputs':  [('input1', ['bptt', 'batch_size']),
+                              ('label', ['bptt_x_batch_size'])],
+                  'outputs': [('loss', [], True),
+                              ('predictions', ['bptt', 'batch_size', ntokens])]}
+    return model_desc
+
+
 def legacy_transformer_model_description(bptt=35, batch_size=20, ntokens=28785):
-    # TODO: Update to Dynamic Axis when backend is fixed
     input_desc = Legacy_IODescription('input1', [bptt, batch_size])
-    label_desc = Legacy_IODescription('label', [bptt, batch_size, ntokens])
+    label_desc = Legacy_IODescription('label', [bptt * batch_size])
     loss_desc = Legacy_IODescription('loss', [])
     predictions_desc = Legacy_IODescription('predictions', [bptt, batch_size, ntokens])
+    return Legacy_ModelDescription([input_desc, label_desc],[loss_desc, predictions_desc]),\
+           Legacy_IODescription('__learning_rate', [1])
+
+
+def legacy_transformer_model_description_dynamic_axes(ntokens=28785):
+    input_desc = Legacy_IODescription('input1', ['bptt', 'batch_size'])
+    label_desc = Legacy_IODescription('label', ['bptt_x_batch_size'])
+    loss_desc = Legacy_IODescription('loss', [])
+    predictions_desc = Legacy_IODescription('predictions', ['bptt', 'batch_size', ntokens])
     return Legacy_ModelDescription([input_desc, label_desc],[loss_desc, predictions_desc]),\
            Legacy_IODescription('__learning_rate', [1])
