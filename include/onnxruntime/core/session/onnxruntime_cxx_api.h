0f70c155204363df310a21212971629900bcb7e2
Joshua Bradley <jgbradley1@gmail.com>
2020-08-18 02:42:28 -0400

    reformat code

--- include/onnxruntime/core/session/onnxruntime_cxx_api.h
+++ include/onnxruntime/core/session/onnxruntime_cxx_api.h
@@ -83,7 +83,6 @@ ORT_DEFINE_RELEASE(IoBinding);
 // This is used internally by the C++ API. This is the common base class used by the wrapper objects.
 template <typename T>
 struct Base {
-
   using contained_type = T;
 
   Base() = default;
@@ -119,7 +118,6 @@ struct Base {
 
 template <typename T>
 struct Base<const T> {
-
   using contained_type = const T;
 
   Base() = default;
@@ -142,7 +140,7 @@ struct Base<const T> {
   const T* p_{};
 };
 
-template<typename T> 
+template <typename T>
 struct Unowned : T {
   Unowned(decltype(T::p_) p) : T{p} {}
   Unowned(Unowned&& v) : T{v.p_} {}
@@ -159,7 +157,7 @@ struct ModelMetadata;
 struct Env : Base<OrtEnv> {
   Env(std::nullptr_t) {}
   Env(OrtLoggingLevel default_logging_level = ORT_LOGGING_LEVEL_WARNING, _In_ const char* logid = "");
-  Env(const OrtThreadingOptions* tp_options, OrtLoggingLevel default_logging_level = ORT_LOGGING_LEVEL_WARNING, _In_ const char* logid = "");  
+  Env(const OrtThreadingOptions* tp_options, OrtLoggingLevel default_logging_level = ORT_LOGGING_LEVEL_WARNING, _In_ const char* logid = "");
   Env(OrtLoggingLevel default_logging_level, const char* logid, OrtLoggingFunction logging_function, void* logger_param);
   Env(const OrtThreadingOptions* tp_options, OrtLoggingFunction logging_function, void* logger_param, OrtLoggingLevel default_logging_level = ORT_LOGGING_LEVEL_WARNING, _In_ const char* logid = "");
   explicit Env(OrtEnv* p) : Base<OrtEnv>{p} {}
@@ -229,7 +227,6 @@ struct SessionOptions : Base<OrtSessionOptions> {
 
   SessionOptions& EnablePrePacking();
   SessionOptions& DisablePrePacking();
-
 };
 
 struct ModelMetadata : Base<OrtModelMetadata> {
@@ -329,7 +326,7 @@ struct Value : Base<OrtValue> {
   template <typename T>
   T* GetTensorMutableData();
 
-  template<typename T>
+  template <typename T>
   const T* GetTensorData() const;
 
   template <typename T>
@@ -358,7 +355,6 @@ struct MemoryAllocation {
   size_t size() const { return size_; }
 
  private:
-
   OrtAllocator* allocator_;
   void* p_;
   size_t size_;
@@ -393,7 +389,7 @@ struct BaseMemoryInfo : B {
   OrtAllocatorType GetAllocatorType() const;
   int GetDeviceId() const;
   OrtMemType GetMemoryType() const;
-  template<typename U>
+  template <typename U>
   bool operator==(const BaseMemoryInfo<U>& o) const;
 };
 
@@ -424,6 +420,7 @@ struct IoBinding : public Base<OrtIoBinding> {
  private:
   std::vector<std::string> GetOutputNamesHelper(OrtAllocator*) const;
   std::vector<Value> GetOutputValuesHelper(OrtAllocator*) const;
+
  public:
   explicit IoBinding(Session& session);
   void BindInput(const char* name, const Value&);
