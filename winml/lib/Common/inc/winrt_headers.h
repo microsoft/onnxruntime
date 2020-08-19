56ac5541c14ce45732d49b1bfc6d6b4c9883a3f6
Tiago Koji Castro Shibata <tiago.shibata@gmail.com>
2020-08-19 10:32:27 -0700

    Remove downlevel workaround

--- winml/lib/Common/inc/winrt_headers.h
+++ winml/lib/Common/inc/winrt_headers.h
@@ -3,14 +3,6 @@
 
 #pragma once
 
-#include <windows.h>
-
-#if WINVER < _WIN32_WINNT_WIN8
-// Stubs for WRL to build for Windows 7
-constexpr BOOL RoTransformError(HRESULT, HRESULT, void*) { return true; }
-constexpr HRESULT SetRestrictedErrorInfo(void*) { return S_OK; }
-#endif
-
 #include "common.h"
 
 #include <winrt/windows.media.h>
