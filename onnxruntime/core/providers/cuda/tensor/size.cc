de1b51ebae2a4c391d15bc89da4786d37ec8f4d7
Ethan Tao <ettao@microsoft.com>
2020-08-19 21:18:50 -0700

    on comments

--- onnxruntime/core/providers/cuda/tensor/size.cc
+++ onnxruntime/core/providers/cuda/tensor/size.cc
@@ -14,19 +14,7 @@ ONNX_OPERATOR_KERNEL_EX(
     kCudaExecutionProvider,
     KernelDefBuilder()
         .OutputMemoryType<OrtMemTypeCPUOutput>(0)
-        .TypeConstraint("T",
-                        std::vector<MLDataType>({DataTypeImpl::GetTensorType<float>(),
-                                                 DataTypeImpl::GetTensorType<double>(),
-                                                 DataTypeImpl::GetTensorType<int8_t>(),
-                                                 DataTypeImpl::GetTensorType<int16_t>(),
-                                                 DataTypeImpl::GetTensorType<int32_t>(),
-                                                 DataTypeImpl::GetTensorType<int64_t>(),
-                                                 DataTypeImpl::GetTensorType<uint8_t>(),
-                                                 DataTypeImpl::GetTensorType<uint16_t>(),
-                                                 DataTypeImpl::GetTensorType<uint32_t>(),
-                                                 DataTypeImpl::GetTensorType<uint64_t>(),
-                                                 DataTypeImpl::GetTensorType<std::string>(),
-                                                 DataTypeImpl::GetTensorType<bool>()}))
+        .TypeConstraint("T", DataTypeImpl::AllTensorTypes())
         .TypeConstraint("T1", DataTypeImpl::GetTensorType<int64_t>()),
     Size);
 
