7e40f597d95db144d7bc99df0d10831655bb3f37
t-yguo <t-yguo@microsoft.com>
2020-08-20 16:22:22 -0700

    qat support and quant tool improvement

---
+++ onnxruntime/python/tools/quantization/registry.py
@@ -0,0 +1,42 @@
+from .quant_utils import QuantizationMode
+from .operators.base_operator import QuantOperatorBase
+from .operators.matmul import MatMulInteger, QLinearMatMul
+from .operators.attention import AttentionQuant
+from .operators.embed_layernorm import EmbedLayerNormalizationQuant
+from .operators.gather import GatherQuant
+from .operators.conv import QLinearCov, ConInteger
+from .operators.activation import QLinearActivation
+from .operators.binary_op import QLinearBinaryOp
+from .operators.maxpool import QMaxPool
+
+CommonOpsRegistry ={
+    "Gather":GatherQuant,
+    "Attention":AttentionQuant,
+    "EmbedLayerNormalization":EmbedLayerNormalizationQuant
+}
+
+IntegerOpsRegistry = {
+    "Conv":ConInteger,
+    "MatMul":MatMulInteger
+}
+IntegerOpsRegistry.update(CommonOpsRegistry)
+
+QLinearOpsRegistry = {
+    "Conv":QLinearCov,
+    "MatMul":QLinearMatMul,
+    "Add":QLinearBinaryOp,
+    "Mul":QLinearBinaryOp,
+    "Relu":QLinearActivation,
+    "Clip":QLinearActivation,
+    "MaxPool":QMaxPool,
+}
+QLinearOpsRegistry.update(CommonOpsRegistry)
+
+def CreateDefaultOpQuantizer(onnx_quantizer, node):
+    return QuantOperatorBase(onnx_quantizer, node)
+
+def CreateOpQuantizer(onnx_quantizer, node):
+    registry = IntegerOpsRegistry if onnx_quantizer.mode == QuantizationMode.IntegerOps else QLinearOpsRegistry
+    if node.op_type in registry.keys():
+        return registry[node.op_type](onnx_quantizer, node)
+    return QuantOperatorBase(onnx_quantizer, node)
