e715856bc1b94ea12ad35cc2da128141e7e73a23
BowenBao <bowbao@microsoft.com>
2020-08-24 12:31:16 -0700

    fix indent

--- orttraining/orttraining/test/python/orttraining_test_orttrainer_frontend.py
+++ orttraining/orttraining/test/python/orttraining_test_orttrainer_frontend.py
@@ -771,8 +771,7 @@ def testORTTrainerDynamicShape(dynamic_axes):
     for i in range(total_steps):
         data, targets = batcher_fn(train_data, i)
         if dynamic_axes:
-        # Forcing batches with different sizes to exercise dynamic shapes
-            # Enforce different batch size between steps, to test dynamic axes feature.
+            # Forcing batches with different sizes to exercise dynamic shapes
             data = data[:-(i+1)]
             targets = targets[:-(i+1)*data.size(1)]
         _, _ = trainer.train_step(data, targets)
