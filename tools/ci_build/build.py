901b63eb3079c1a577a0161efbf629edf3e21779
Tiago Koji Castro Shibata <tiago.shibata@gmail.com>
2020-08-25 10:37:46 -0700

    PR feedback

--- tools/ci_build/build.py
+++ tools/ci_build/build.py
@@ -1606,7 +1606,8 @@ def main():
                 ]
             if args.enable_wcos:
                 cmake_extra_args.append(
-                    '-DCMAKE_USER_MAKE_RULES_OVERRIDE=wcos_rules_override.cmake')
+                    '-DCMAKE_TOOLCHAIN_FILE=' + os.path.join(
+                        source_dir, 'cmake', 'wcos_toolchain.cmake'))
         elif args.cmake_generator is not None and not (is_macOS() and args.use_xcode):
             cmake_extra_args += ['-G', args.cmake_generator]
         elif is_macOS() and args.use_xcode:
