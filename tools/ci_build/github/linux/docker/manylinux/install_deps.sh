ac2c40c80a4249e107d2937a0bf2bc0b254609a1
Changming Sun <chasun@microsoft.com>
2020-09-01 14:47:37 -0700

    Build system enhancements

--- tools/ci_build/github/linux/docker/manylinux/install_deps.sh
+++ tools/ci_build/github/linux/docker/manylinux/install_deps.sh
@@ -36,6 +36,7 @@ function GetFile {
 }
 
 PYTHON_EXES=("/opt/python/cp35-cp35m/bin/python3.5" "/opt/python/cp36-cp36m/bin/python3.6" "/opt/python/cp37-cp37m/bin/python3.7" "/opt/python/cp38-cp38/bin/python3.8")
+os_major_version=$(cat /etc/redhat-release | tr -dc '0-9.'|cut -d \. -f1)
 
 SYS_LONG_BIT=$(getconf LONG_BIT)
 mkdir -p /tmp/src
@@ -51,29 +52,34 @@ fi
 
 cd /tmp/src
 
-  echo "Installing azcopy"
-  mkdir -p /tmp/azcopy
-  GetFile https://aka.ms/downloadazcopy-v10-linux /tmp/azcopy/azcopy.tar.gz
-  tar --strip 1 -xf /tmp/azcopy/azcopy.tar.gz -C /tmp/azcopy
-  cp /tmp/azcopy/azcopy /usr/bin
-  echo "Installing cmake"
-  GetFile https://github.com/Kitware/CMake/releases/download/v3.18.1/cmake-3.18.1-Linux-x86_64.tar.gz /tmp/src/cmake-3.18.1-Linux-x86_64.tar.gz
-  tar -zxf /tmp/src/cmake-3.18.1-Linux-x86_64.tar.gz --strip=1 -C /usr
-  echo "Installing Ninja"
-  GetFile https://github.com/ninja-build/ninja/archive/v1.10.0.tar.gz /tmp/src/ninja-linux.tar.gz
-  tar -zxf ninja-linux.tar.gz
-  cd ninja-1.10.0
-  cmake -Bbuild-cmake -H.
-  cmake --build build-cmake
-  mv ./build-cmake/ninja /usr/bin
-  echo "Installing Node.js"
+echo "Installing azcopy"
+mkdir -p /tmp/azcopy
+GetFile https://aka.ms/downloadazcopy-v10-linux /tmp/azcopy/azcopy.tar.gz
+tar --strip 1 -xf /tmp/azcopy/azcopy.tar.gz -C /tmp/azcopy
+cp /tmp/azcopy/azcopy /usr/bin
+echo "Installing cmake"
+GetFile https://github.com/Kitware/CMake/releases/download/v3.18.1/cmake-3.18.1-Linux-x86_64.tar.gz /tmp/src/cmake-3.18.1-Linux-x86_64.tar.gz
+tar -zxf /tmp/src/cmake-3.18.1-Linux-x86_64.tar.gz --strip=1 -C /usr
+echo "Installing Ninja"
+GetFile https://github.com/ninja-build/ninja/archive/v1.10.0.tar.gz /tmp/src/ninja-linux.tar.gz
+tar -zxf ninja-linux.tar.gz
+cd ninja-1.10.0
+cmake -Bbuild-cmake -H.
+cmake --build build-cmake
+mv ./build-cmake/ninja /usr/bin
+if [[ "$os_major_version" == "6" ]]; then
+  echo "Installing Node.js from source"
   GetFile https://nodejs.org/dist/v12.16.3/node-v12.16.3.tar.xz /tmp/src/node-v12.16.3.tar.xz
   tar -xf /tmp/src/node-v12.16.3.tar.xz
   cd node-v12.16.3
   LDFLAGS=-lrt /opt/python/cp27-cp27m/bin/python configure --ninja
   LDFLAGS=-lrt make -j$(getconf _NPROCESSORS_ONLN)
   LDFLAGS=-lrt make install
-
+else
+  echo "Installing Node.js from source"
+  GetFile https://nodejs.org/dist/v12.16.3/node-v12.16.3-linux-x64.tar.gz /tmp/src/node-v12.16.3-linux-x64.tar.gz
+  tar --strip 1 -xf /tmp/src/node-v12.16.3-linux-x64.tar.gz -C /usr
+fi
 cd /tmp/src
 GetFile https://downloads.gradle-dn.com/distributions/gradle-6.3-bin.zip /tmp/src/gradle-6.3-bin.zip
 unzip /tmp/src/gradle-6.3-bin.zip
