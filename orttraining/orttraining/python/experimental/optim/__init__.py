05ed66e57eac65be39b36a5e7ecfcc27617d8fa4
Thiago Crepaldi <thiago.crepaldi@microsoft.com>
2020-09-01 10:12:28 -0700

    Update CODEOWNERS and minor docstring fix
    Previous CODEOWNERS was encompassing more files than just training files
    Polynomial optimizer config is missing part of its docstring

--- orttraining/orttraining/python/experimental/optim/__init__.py
+++ orttraining/orttraining/python/experimental/optim/__init__.py
@@ -1,3 +1,3 @@
-from .config import _OptimizerConfig, AdamConfig, LambConfig, SGDConfig
+from .optim import AdamConfig, LambConfig, SGDConfig
 from .lr_scheduler import _LRScheduler, ConstantWarmupLRScheduler, CosineWarmupLRScheduler,\
     LinearWarmupLRScheduler, PolyWarmupLRScheduler
