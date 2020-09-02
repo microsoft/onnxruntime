d5d5e37e76cd3ed72925601861871322f6745971
Changming Sun <chasun@microsoft.com>
2020-09-02 10:13:26 -0700

    Build system enhancements (#5012)
    1. Add a docker file for CUDA11
    2. Support setting CUDA_ARCHITECTURES from command line.

---
+++ tools/ci_build/github/linux/docker/manylinux2014_build_scripts/build_env.sh
@@ -0,0 +1,28 @@
+# source me
+
+PYTHON_DOWNLOAD_URL=https://www.python.org/ftp/python
+# of the form <maj>.<min>.<rev> or <maj>.<min>.<rev>rc<n>
+CPYTHON_VERSIONS="3.5.9 3.6.12 3.7.9 3.8.5 3.9.0rc1"
+
+PATCHELF_VERSION=0.12
+PATCHELF_HASH=3dca33fb862213b3541350e1da262249959595903f559eae0fbc68966e9c3f56
+
+AUTOMAKE_ROOT=automake-1.16.2
+AUTOMAKE_HASH=b2f361094b410b4acbf4efba7337bdb786335ca09eb2518635a09fb7319ca5c1
+AUTOMAKE_DOWNLOAD_URL=http://ftp.gnu.org/gnu/automake
+
+LIBTOOL_ROOT=libtool-2.4.6
+LIBTOOL_HASH=e3bd4d5d3d025a36c21dd6af7ea818a2afcd4dfc1ea5a17b39d7854bcd0c06e3
+LIBTOOL_DOWNLOAD_URL=http://ftp.gnu.org/gnu/libtool
+
+SQLITE_AUTOCONF_VERSION=sqlite-autoconf-3320300
+SQLITE_AUTOCONF_HASH=a31507123c1c2e3a210afec19525fd7b5bb1e19a6a34ae5b998fbd7302568b66
+SQLITE_AUTOCONF_DOWNLOAD_URL=https://www.sqlite.org/2020
+
+LIBXCRYPT_VERSION=4.4.16
+LIBXCRYPT_DOWNLOAD_URL=https://codeload.github.com/besser82/libxcrypt/tar.gz
+LIBXCRYPT_HASH=a98f65b8baffa2b5ba68ee53c10c0a328166ef4116bce3baece190c8ce01f375
+
+GIT_ROOT=2.26.2
+GIT_HASH=21b48d1e7e03d0b325967e31a30f4fa878e4fbb6dac6e997b5dc3f41f950a6d3
+GIT_DOWNLOAD_URL=https://github.com/git/git/archive
