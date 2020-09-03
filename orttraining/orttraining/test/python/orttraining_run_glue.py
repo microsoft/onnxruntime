bb13b522911be49918790ba12f9420fbf4451cbd
liqunfu <liqfu@microsoft.com>
2020-09-03 12:47:12 -0700

    to allow parallel training with mpi4py (#4942)
    to allow parallel training with mpi4py
    Co-authored-by: liqun <liqun@OrtTrainingDev4.af05slrtruoetgaxwwjv5nsq5e.px.internal.cloudapp.net>

--- orttraining/orttraining/test/python/orttraining_run_glue.py
+++ orttraining/orttraining/test/python/orttraining_run_glue.py
@@ -67,6 +67,7 @@ class ORTGlueTest(unittest.TestCase):
         self.learning_rate = 2e-5
         self.num_train_epochs = 3.0
         self.local_rank = -1
+        self.world_size = 1
         self.overwrite_output_dir = True
         self.gradient_accumulation_steps = 1
         self.data_dir = "/bert_data/hf_data/glue_data/"
@@ -279,7 +280,8 @@ class ORTGlueTest(unittest.TestCase):
             train_dataset=train_dataset,
             eval_dataset=eval_dataset,
             compute_metrics=compute_metrics,
-            use_new_api=use_new_api
+            use_new_api=use_new_api,
+            world_size=self.world_size,
         )
 
         # Training
@@ -303,17 +305,17 @@ class ORTGlueTest(unittest.TestCase):
         return results
 
 if __name__ == "__main__":
-    if get_mpi_context_world_size() > 1:
+    local_rank = get_mpi_context_local_rank()
+    world_size = get_mpi_context_world_size()
+    if world_size > 1:
         # mpi launch
+        logger.warning("mpirun launch, local_rank / world_size: %s : % s", local_rank, world_size)
 
-        print("mpirun launch")
         # TrainingArguments._setup_devices will call torch.distributed.init_process_group(backend="nccl")
         # pytorch expects following environment settings (which would be set if launched with torch.distributed.launch).
 
-        local_rank = get_mpi_context_local_rank()
-        print("get_mpi_context_local_rank(): ", local_rank)
         os.environ['RANK'] = str(local_rank)
-        os.environ['WORLD_SIZE'] = str(get_mpi_context_world_size())
+        os.environ['WORLD_SIZE'] = str(world_size)
         os.environ['MASTER_ADDR'] = '127.0.0.1'
         os.environ['MASTER_PORT'] = '29500'
 
@@ -323,6 +325,7 @@ if __name__ == "__main__":
         test = ORTGlueTest()
         test.setUp()
         test.local_rank = local_rank
+        test.world_size = world_size
         test.test_bert_with_mrpc()
     else:
         unittest.main()
