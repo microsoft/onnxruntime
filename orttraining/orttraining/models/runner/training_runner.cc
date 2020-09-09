4ccca20def4c8a99b2afdb55ff109cbc5fc72b2e
Wei-Sheng Chin <wschin@outlook.com>
2020-09-09 09:39:56 -0700

    Replace MPI Send and Recv with NCCL Send and Recv (#5054)
    * Prototype NCCL P2P
    
    * Clean code
    
    * Fix NCCL path and some minor bugs
    
    * Add path
    
    * Fix path
    
    * Try fix path
    
    * Add missed files
    
    * Address some comments
    
    * Clean code
    
    * Rename files
    
    * Add MPI path back and fix a path
    
    * Put MPI path under USE_NCCL flag
    
    * not to build Send and Recv when MPI is not installed

--- orttraining/orttraining/models/runner/training_runner.cc
+++ orttraining/orttraining/models/runner/training_runner.cc
@@ -20,6 +20,7 @@
 #include "orttraining/core/framework/distributed_run_context.h"
 #include "orttraining/core/graph/optimizer_graph_builder.h"
 #include "orttraining/models/runner/training_util.h"
+#include "orttraining/training_ops/cuda/communication/nccl_service.h"
 #include "single_include/nlohmann/json.hpp"
 #include "test/perftest/utils.h"
 
@@ -705,6 +706,30 @@ Status TrainingRunner::TrainingLoop(IDataLoader& training_data_loader, IDataLoad
   auto end_to_end_start = std::chrono::high_resolution_clock::now();
   bool end_to_end_measurement_started = false;
 
+#ifdef USE_NCCL
+  // NCCL-P2P
+  auto& nccl_service = cuda::NcclService::GetInstance();
+
+  nccl_service.PlanStart();
+  for (auto& slot : pipeline_schedule_.GetSchedule(pipeline_context_.pipeline_stage_id)) {
+    if (!slot.HasCommute()) {
+      continue;
+    }
+    nccl_service.PlanNewGroupStart();
+    for (auto& task : slot.GetTasks()) {
+      if (task.type == pipeline::PipelineTask::Type::Send) {
+        nccl_service.PlanSend(task.peer_rank);
+      } else if (task.type == pipeline::PipelineTask::Type::Recv) {
+        nccl_service.PlanRecv(task.peer_rank);
+      }
+    }
+    nccl_service.PlanNewGroupEnd();
+  }
+  nccl_service.PlanEnd();
+
+  nccl_service.Launch();
+#endif
+
   auto all_steps_time_start = std::chrono::high_resolution_clock::now();
   while (step_ < params_.num_train_steps) {
     for (size_t shard_it = 0; shard_it < num_shards_to_visit; ++shard_it) {
@@ -754,6 +779,9 @@ Status TrainingRunner::TrainingLoop(IDataLoader& training_data_loader, IDataLoad
                                           fetch_names,
                                           fetches));
           RunWithUpdate(feed_names, fetch_names, feeds, fetches);
+#ifdef USE_NCCL
+          nccl_service.Reset();
+#endif
         } else {
           ORT_RETURN_IF_ERROR(PrepareFeedNamesAndFeeds(GradientAccumulateStep,
                                                        training_data_loader,
@@ -876,6 +904,9 @@ Status TrainingRunner::TrainingLoop(IDataLoader& training_data_loader, IDataLoad
             << "Average Step Time: " << all_steps_duration_seconds.count() / (step_ - step_start) << " Second\n"
             << "Average Step Throughput: " << params_.batch_size * (step_ - step_start) / (all_steps_duration_seconds.count()) << " Examples / Second\n";
 
+#ifdef USE_NCCL
+  nccl_service.Terminate();
+#endif
   return Status::OK();
 }
 
