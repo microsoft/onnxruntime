d6bb89d68e8b1986f46d7279bc26177260c30207
Rayan-Krishnan <rayankrishnan@live.com>
2020-09-11 12:16:07 -0700

    [Debuggability] Add feature to ORTTrainer Frontend (#5124)
    * add option, feature to orttrainer and test
    
    * address comments
    
    * minor fixes
    
    * further address comments
    
    * minor changes
    
    Co-authored-by: Rayan Krishnan <t-rakr@OrtDevTest2v100.af05slrtruoetgaxwwjv5nsq5e.px.internal.cloudapp.net>

--- orttraining/orttraining/python/training/orttrainer_options.py
+++ orttraining/orttraining/python/training/orttrainer_options.py
@@ -144,6 +144,10 @@ class ORTTrainerOptions(object):
                             'type' : 'boolean',
                             'default' : False
                         },
+                        'check_model_export' : {
+                            'type' : 'boolean',
+                            'default' : False
+                        },
                     }
                 },
                 '_internal_use' : {
@@ -225,6 +229,9 @@ class ORTTrainerOptions(object):
             debug options
         debug.deterministic_compute (bool, default is False)
             forces compute to be deterministic accross runs
+        debug.check_model_export (bool, default is False)
+            compares PyTorch outputs with InferenceSession outputs before the first
+            train step to ensure successful model export
         _internal_use (dict):
             internal options, possibly undocumented, that might be removed without notice
         _internal_use.enable_internal_postprocess (bool, default is True):
@@ -447,6 +454,10 @@ _ORTTRAINER_OPTIONS_SCHEMA = {
         'required': False,
         'schema': {
             'deterministic_compute': {
+                'type': 'boolean',
+                'default': False
+            },
+            'check_model_export': {
                 'type': 'boolean',
                 'default': False
             },
