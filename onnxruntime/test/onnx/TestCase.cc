b2a3492ddbce60c4819ec828d82767bbcf0621ef
Guoyu Wang <62914304+gwang-msft@users.noreply.github.com>
2020-09-11 01:58:11 -0700

    Enable onnxruntime_perf_test for ORT minimal build (#5126)
    * Enable onnxruntime_perf_test for ort minimal build
    
    * Add error message
    
    Co-authored-by: gwang0000 <62914304+gwang0000@users.noreply.github.com>

--- onnxruntime/test/onnx/TestCase.cc
+++ onnxruntime/test/onnx/TestCase.cc
@@ -284,11 +284,11 @@ void LoopDataFile(int test_data_pb_fd, bool is_input, const TestModelInfo& model
 
 }  // namespace
 
+#if !defined(ORT_MINIMAL_BUILD)
 std::unique_ptr<TestModelInfo> TestModelInfo::LoadOnnxModel(_In_ const PATH_CHAR_TYPE* model_url) {
   return std::unique_ptr<TestModelInfo>(new OnnxModelInfo(model_url));
 }
-
-#if defined(ORT_MINIMAL_BUILD)
+#else
 std::unique_ptr<TestModelInfo> TestModelInfo::LoadOrtModel(_In_ const PATH_CHAR_TYPE* model_url) {
   return std::unique_ptr<TestModelInfo>(new OrtModelInfo(model_url));
 }
