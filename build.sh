b3b0052bb4ce14971c8769ae6b765d1a87f5eb5b
Wenbing Li <wenli@microsoft.com>
2020-09-14 10:32:01 -0700

    build for ios shared library.

--- build.sh
+++ build.sh
@@ -12,5 +12,11 @@ else
     DIR_OS="Linux"
 fi
 
+if [[ "$*" == *"--ios"* ]]; then
+    DIR_OS="iOS"
+elif [[ "$*" == *"--android"* ]]; then
+    DIR_OS="Android"
+fi
+
 #requires python3.6 or higher
 python3 $DIR/tools/ci_build/build.py --build_dir $DIR/build/$DIR_OS "$@"
