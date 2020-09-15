17e5e0284a63361721f1720ef8b76f4752c465f1
gwang <wanggy@outlook.com>
2020-09-14 21:00:05 -0700

    revert change to build.sh

--- build.sh
+++ build.sh
@@ -5,11 +5,5 @@
 # Get directory this script is in
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# Get OS type
-PLATFORM='Linux'
-if [[ "$OSTYPE" == "darwin"* ]]; then
-    PLATFORM='macOS'
-fi
-
 #requires python3.6 or higher
-python3 $DIR/tools/ci_build/build.py --build_dir $DIR/build/$PLATFORM "$@"
+python3 $DIR/tools/ci_build/build.py --build_dir $DIR/build/Linux "$@"
