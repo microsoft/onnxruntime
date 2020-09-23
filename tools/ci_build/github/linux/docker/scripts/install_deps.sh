cce9def0efaaf02d3ccd443ed426128d090789e4
Changming Sun <chasun@microsoft.com>
2020-09-23 12:15:44 -0700

    a

--- tools/ci_build/github/linux/docker/scripts/install_deps.sh
+++ tools/ci_build/github/linux/docker/scripts/install_deps.sh
@@ -106,6 +106,8 @@ if ! [ -x "$(command -v protoc)" ]; then
   source ${0/%install_deps\.sh/install_protobuf\.sh}
 fi
 
+export ONNX_ML=1
+export CMAKE_ARGS="-DONNX_GEN_PB_TYPE_STUBS=OFF -DONNX_WERROR=OFF"
 ${PYTHON_EXE} -m pip install -r ${0/%install_deps\.sh/requirements\.txt}
 if [ $DEVICE_TYPE = "gpu" ]; then
     if [[ $BUILD_EXTR_PAR = *--enable_training* ]]; then
