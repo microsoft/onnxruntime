83f00f296698ae27a9b00e4be9874986a95a732d
wangye <wangye@microsoft.com>
2020-09-28 19:39:03 +0000

    refactor

--- onnxruntime/python/tools/transformers/convert_tf_models_to_pytorch.py
+++ onnxruntime/python/tools/transformers/convert_tf_models_to_pytorch.py
@@ -3,22 +3,47 @@
 # Licensed under the MIT License.
 #--------------------------------------------------------------------------
 
-import os
 import glob
+import os
+import requests
+
 
 TFMODELS = {
-    "bert_base_uncased":
+    "bert-base-uncased":
     ("bert", "BertConfig", "", "https://storage.googleapis.com/bert_models/2018_10_18/uncased_L-12_H-768_A-12.zip"),
-    "bert_base_cased":
+    "bert-base-cased":
     ("bert", "BertConfig", "", "https://storage.googleapis.com/bert_models/2019_05_30/wwm_cased_L-24_H-1024_A-16.zip"),
-    "bert_large_uncased":
+    "bert-large-uncased":
     ("bert", "BertConfig", "", "https://storage.googleapis.com/bert_models/2018_10_18/uncased_L-24_H-1024_A-16.zip"),
-    "albert_base": ("albert", "AlbertConfig", "", "https://storage.googleapis.com/albert_models/albert_base_v1.tar.gz"),
-    "albert_large":
+    "albert-base": ("albert", "AlbertConfig", "", "https://storage.googleapis.com/albert_models/albert_base_v1.tar.gz"),
+    "albert-large":
     ("albert", "AlbertConfig", "", "https://storage.googleapis.com/albert_models/albert_large_v1.tar.gz"),
     "gpt-2-117M": ("gpt2", "GPT2Config", "GPT2Model", "https://storage.googleapis.com/gpt-2/models/117M"),
     "gpt-2-124M": ("gpt2", "GPT2Config", "GPT2Model", "https://storage.googleapis.com/gpt-2/models/124M")
 }
+
+
+def download_compressed_file(tf_ckpt_url, ckpt_dir):
+    r = requests.get(tf_ckpt_url)
+    compressed_file_name = tf_ckpt_url.split("/")[-1]
+    compressed_file_dir = os.path.join(ckpt_dir, compressed_file_name)
+    with open(compressed_file_dir, 'wb') as f:
+        f.write(r.content)
+    return compressed_file_dir
+
+
+def get_ckpt_prefix_path(ckpt_dir):
+    # get prefix
+    sub_folder_dir = None
+    for o in os.listdir(ckpt_dir):
+        sub_folder_dir = os.path.join(ckpt_dir, o)
+        break
+    if os.path.isfile(sub_folder_dir):
+        sub_folder_dir = ckpt_dir
+    unique_file_name = str(glob.glob(sub_folder_dir + "/*data-00000-of-00001"))
+    prefix = (unique_file_name.rpartition('.')[0]).split("/")[-1]
+
+    return os.path.join(sub_folder_dir, prefix)
 
 
 def download_tf_checkpoint(model_name, tf_models_dir="tf_models"):
@@ -32,13 +57,8 @@ def download_tf_checkpoint(model_name, tf_models_dir="tf_models"):
     tf_ckpt_url = TFMODELS[model_name][3]
 
     import re
-    import requests
     if (re.search('.zip$', tf_ckpt_url) != None):
-        r = requests.get(tf_ckpt_url)
-        zip_name = tf_ckpt_url.split("/")[-1]
-        zip_dir = os.path.join(ckpt_dir, zip_name)
-        with open(zip_dir, 'wb') as f:
-            f.write(r.content)
+        zip_dir = download_compressed_file(tf_ckpt_url, ckpt_dir)
 
         # unzip file
         import zipfile
@@ -46,48 +66,26 @@ def download_tf_checkpoint(model_name, tf_models_dir="tf_models"):
             zip_ref.extractall(ckpt_dir)
             os.remove(zip_dir)
 
-        # get prefix
-        for o in os.listdir(ckpt_dir):
-            folder_dir = os.path.join(ckpt_dir, o)
-            break
-        unique_file_name = str(glob.glob(folder_dir + "/*data-00000-of-00001"))
-        prefix = (unique_file_name.rpartition('.')[0]).split("/")[-1]
-
-        return os.path.join(folder_dir, prefix)
+        return get_ckpt_prefix_path(ckpt_dir)
 
     elif (re.search('.tar.gz$', tf_ckpt_url) != None):
-        r = requests.get(tf_ckpt_url)
-        tar_name = tf_ckpt_url.split("/")[-1]
-        tar_dir = os.path.join(ckpt_dir, tar_name)
-        with open(tar_dir, 'wb') as f:
-            f.write(r.content)
+        tar_dir = download_compressed_file(tf_ckpt_url, ckpt_dir)
+
         # untar file
         import tarfile
         with tarfile.open(tar_dir, 'r') as tar_ref:
             tar_ref.extractall(ckpt_dir)
             os.remove(tar_dir)
 
-        # get prefix
-        for o in os.listdir(ckpt_dir):
-            folder_dir = os.path.join(ckpt_dir, o)
-            break
-        unique_file_name = str(glob.glob(folder_dir + "/*data-00000-of-00001"))
-        prefix = (unique_file_name.rpartition('.')[0]).split("/")[-1]
-
-        return os.path.join(folder_dir, prefix)
+        return get_ckpt_prefix_path(ckpt_dir)
 
     else:
         for filename in ['checkpoint', 'model.ckpt.data-00000-of-00001', 'model.ckpt.index', 'model.ckpt.meta']:
             r = requests.get(tf_ckpt_url + "/" + filename)
-
             with open(os.path.join(ckpt_dir, filename), 'wb') as f:
                 f.write(r.content)
 
-        # get prefix
-        unique_file_name = str(glob.glob(ckpt_dir + "/*data-00000-of-00001"))
-        prefix = (unique_file_name.rpartition('.')[0]).split("/")[-1]
-
-        return os.path.join(ckpt_dir, prefix)
+        return get_ckpt_prefix_path(ckpt_dir)
 
 
 def init_pytorch_model(model_name, tf_checkpoint_path):
@@ -110,7 +108,7 @@ def init_pytorch_model(model_name, tf_checkpoint_path):
     return config, init_model
 
 
-def convert_tf_checkpoint_to_pytorch(config, init_model, tf_checkpoint_path):
+def convert_tf_checkpoint_to_pytorch(model_name, config, init_model, tf_checkpoint_path):
     load_tf_weight_func_name = "load_tf_weights_in_" + TFMODELS[model_name][0]
 
     module = __import__("transformers", fromlist=[load_tf_weight_func_name])
@@ -121,16 +119,17 @@ def convert_tf_checkpoint_to_pytorch(config, init_model, tf_checkpoint_path):
     return model
 
 
-def pipeline(model_name):
+def tf2pt_pipeline(model_name):
     if model_name not in TFMODELS:
-        raise NotImplementedError()
+        raise NotImplementedError(model_name + " not implemented")
     tf_checkpoint_path = download_tf_checkpoint(model_name)
     config, init_model = init_pytorch_model(model_name, tf_checkpoint_path)
-    model = convert_tf_checkpoint_to_pytorch(config, init_model, tf_checkpoint_path)
+    model = convert_tf_checkpoint_to_pytorch(model_name, config, init_model, tf_checkpoint_path)
+    # Could then use the model in Benchmark
+    return config, model
 
 
 if __name__ == '__main__':
-    # For test
+    # For test on linux only
     for model_name in TFMODELS.keys():
-        pipeline(model_name)
-    print("finished")
+        config, model = tf2pt_pipeline(model_name)
