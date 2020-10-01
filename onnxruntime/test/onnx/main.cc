42bc1232dd9f97cd27f8e869e9a3776195593845
Du Li <duli1@microsoft.com>
2020-09-30 17:24:13 -0700

    remove a redundnant include.

--- onnxruntime/test/onnx/main.cc
+++ onnxruntime/test/onnx/main.cc
@@ -1,7 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-#include <core/session/onnxruntime_cxx_api.h>
 #include <set>
 #include <iostream>
 #include <fstream>
