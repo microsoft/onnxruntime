333a5a9432f10f8879c50547ce156f3f4cba7a74
Chi Lo <Chi.Lo@microsoft.com>
2020-10-01 18:10:50 -0700

    Fix bug

--- onnxruntime/python/tools/tensorrt/perf/perf.sh
+++ onnxruntime/python/tools/tensorrt/perf/perf.sh
@@ -3,24 +3,24 @@
 # many models 
 if [ "$1" == "many-models" ]
 then
-    FAILING_MODEL_LIST = "/home/hcsuser/perf/failing_model_list.json"
-    python3 benchmark.py -r validate -m /home/hcsuser/mount/ -o result/"$1" -e FAILING_MODEL_LIST
-    python3 benchmark.py -r benchmark -i random -t 10 -m /home/hcsuser/mount -o result/"$1" -e FAILING_MODEL_LIST
+    FAILING_MODEL_LIST="/home/hcsuser/perf/failing_model_list.json"
+    python3 benchmark.py -r validate -m /home/hcsuser/mount/ -o result/"$1" -e $FAILING_MODEL_LIST
+    python3 benchmark.py -r benchmark -i random -t 10 -m /home/hcsuser/mount -o result/"$1" -e $FAILING_MODEL_LIST
 fi
 
 # ONNX model zoo
 if [ "$1" == "onnx-zoo-models" ]
 then
-    MODEL_LIST = "model_list.json"
-    python3 benchmark.py -r validate -m MODEL_LIST -o result/"$1"
-    python3 benchmark.py -r benchmark -i random -t 10 -m MODEL_LIST -o result/"$1"
+    MODEL_LIST="model_list.json"
+    python3 benchmark.py -r validate -m $MODEL_LIST -o result/"$1"
+    python3 benchmark.py -r benchmark -i random -t 10 -m $MODEL_LIST -o result/"$1"
 fi
 
 # 1P models 
 if [ "$1" == "partner-models" ]
 then
-    MODEL_LIST = "/home/hcsuser/perf/partner_model_list.json"
-    python3 benchmark.py -r validate -m MODEL_LIST -o result/"$1"
-    python3 benchmark.py -r benchmark -i random -t 10 -m MODEL_LIST -o result/"$1"
+    MODEL_LIST="/home/hcsuser/perf/partner_model_list.json"
+    python3 benchmark.py -r validate -m $MODEL_LIST -o result/"$1"
+    python3 benchmark.py -r benchmark -i random -t 10 -m $MODEL_LIST -o result/"$1"
 fi
 
