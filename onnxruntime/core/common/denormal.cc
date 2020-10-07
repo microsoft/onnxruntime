d901586f58d06ebfb605815f550b53d40e300cb7
Sunghoon <35605090+hanbitmyths@users.noreply.github.com>
2020-10-07 15:50:45 -0700

    Clean up and utilize SSE3 detection logic from existeing cpuid_info.h

--- onnxruntime/core/common/denormal.cc
+++ onnxruntime/core/common/denormal.cc
@@ -1,17 +1,22 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-// Microsoft C/C++-compatible compiler
+#if defined(_M_IX86) || defined(_M_X64) || defined(__i386__) || defined(__x86_64__)
+#define PLATFORM_X86
+#endif
+
+#if defined(PLATFORM_X86)
 #if defined(_MSC_VER)
 #include <intrin.h>
-// GCC-compatible compiler targeting x86/x86-64
-#elif defined(__GNUC__) && (defined(__x86_64__) || defined(__i386__))
+#elif defined(__GNUC__)
 #include <cpuid.h>
 #include <x86intrin.h>
+#endif
 #endif
 
 #include <mutex>
 
+#include "core/common/cpuid_info.h"
 #include "core/common/denormal.h"
 
 namespace onnxruntime {
@@ -38,25 +43,7 @@ void InitializeWithDenormalAsZero(bool on) {
 #endif
 
 bool SetDenormalAsZero(bool on) {
-#if defined(__SSE3__) || defined(_M_X64) || (defined(_M_IX86_FP) && _M_IX86_FP >= 1)
-  static std::once_flag once;
-  static volatile bool has_sse3 = true;
-  std::call_once(once, [&] {
-#if defined(_M_AMD64) || defined(__x86_64__) || defined(_M_IX86) || defined(__i386__)
-    unsigned cpuid[4];
-#if defined(_WIN32)
-    __cpuid((int*)cpuid, 1);
-#else
-        __cpuid(1, cpuid[0], cpuid[1], cpuid[2], cpuid[3]);
-#endif
-    has_sse3 = (cpuid[2] & 0x1) == 0x1;
-#else
-        has_sse3 = false;
-#endif
-  });
-
-  // runtime check for denormal-as-zero support.
-  if (has_sse3) {
+  if (CPUIDInfo::GetCPUIDInfo().HasSSE3()) {
     if (on) {
       _MM_SET_DENORMALS_ZERO_MODE(_MM_DENORMALS_ZERO_ON);
       _MM_SET_FLUSH_ZERO_MODE(_MM_FLUSH_ZERO_ON);
@@ -66,7 +53,6 @@ bool SetDenormalAsZero(bool on) {
     }
     return true;
   }
-#endif
   return false;
 }
 
