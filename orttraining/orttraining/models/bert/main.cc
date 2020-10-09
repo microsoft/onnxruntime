71090a3942117840f5db80b542b73221b7b9cc3b
KeDengMS <kedeng@microsoft.com>
2020-10-09 23:09:41 +0000

    Add CUDA option to run copy in default stream
    This change fixes #4829. Thanks @maherzog for providing the repro!
    
    The bug is caused by memory reuse in BFC arena, where copy and
    compute stream in CUDA has a racing condition.
    
    BFC arena is an arena allocator on top of cudaMalloc/Free to
    reduce the cost in syncing CPU and GPU when alloc/free. It means
    when CPU alloc/free the memory, GPU might not finished previous
    work on the memory, so that CPU and GPU could run asynchronously.
    
    This is OK if there's only one stream, where the execution order
    in CPU and GPU are consistent. For example, if we have two kernels
    A and B, CPU runs allocA->computeA->freeA->allocB->computeB->freeB,
    A and B could shares the same memory since computeA and computeB
    will not have racing as long as they run in the same GPU compute
    stream.
    
    However, if CPU runs allocA->CopyA->freeA->allocB->computeB->freeB,
    the order of execution in GPU could have copyA happen after computeB,
    if copy and compute happens in different GPU streams.
    
    This change makes copy to run in default compute stream, while adding
    an option to fall back to previous behavior if there's perf hit. This
    is a short term fix before BFC arena could support multiple streams.
    
    User may use following options to revert to previous behavior:
    C API:
      struct OrtCUDAProviderOptions cudaProviderOpt;
      cudaProviderOpt.do_copy_in_default_stream = false;
    C++ API:
      CUDAExecutionProviderInfo cudaEPInfo;
      cudaEPInfo.do_copy_in_default_stream = false;
    C# API:
      pending...
    Python:
      import onnxruntime
      onnxruntime.capi._pybind_state.set_do_copy_in_default_stream(False)

--- orttraining/orttraining/models/bert/main.cc
+++ orttraining/orttraining/models/bert/main.cc
@@ -22,7 +22,8 @@ namespace onnxruntime {
 std::shared_ptr<IExecutionProviderFactory> CreateExecutionProviderFactory_CUDA(OrtDevice::DeviceId device_id,
                                                                                OrtCudnnConvAlgoSearch cudnn_conv_algo_search = OrtCudnnConvAlgoSearch::EXHAUSTIVE,
                                                                                size_t cuda_mem_limit = std::numeric_limits<size_t>::max(),
-                                                                               onnxruntime::ArenaExtendStrategy arena_extend_strategy = ArenaExtendStrategy::kNextPowerOfTwo);
+                                                                               onnxruntime::ArenaExtendStrategy arena_extend_strategy = ArenaExtendStrategy::kNextPowerOfTwo,
+                                                                               bool do_copy_in_default_stream = true);
 }
 
 using namespace onnxruntime;
