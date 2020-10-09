50547406c54d19d4d81f82f92693c4c561f28912
Yufeng Li <liyufeng1987@gmail.com>
2020-10-09 02:37:19 -0700

    Prepacking MatMulInteger (#5403)
    * prepack matmulinteger
    Prepacking constant matrix B for MatMulInteger to get better performance.

--- onnxruntime/contrib_ops/cpu/quantization/dynamic_quantize_matmul.cc
+++ onnxruntime/contrib_ops/cpu/quantization/dynamic_quantize_matmul.cc
@@ -1,27 +1,21 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-#include "core/framework/op_kernel.h"
 #include "core/common/safeint.h"
-#include "core/providers/common.h"
 #include "core/providers/cpu/math/matmul_helper.h"
+#include "core/providers/cpu/math/matmul_integer_base.h"
 #include "core/util/math_cpuonly.h"
 #include "core/util/qmath.h"
-#include "core/mlas/inc/mlas.h"
 
 #include <algorithm>
 
 namespace onnxruntime {
 namespace contrib {
 
-class MatMulIntegerToFloatBase : public OpKernel {
+class MatMulIntegerToFloatBase : public MatMulIntegerBase {
  public:
-  MatMulIntegerToFloatBase(const OpKernelInfo& info) : OpKernel(info) {
+  MatMulIntegerToFloatBase(const OpKernelInfo& info) : MatMulIntegerBase(info) {
   }
-
-#ifdef MLAS_SUPPORTS_PACKED_GEMM_U8X8
-  Status PrePack(const Tensor& tensor, int input_idx, bool& is_packed) override;
-#endif
 
  protected:
   Status ComputeCommon(OpKernelContext* ctx,
@@ -32,45 +26,7 @@ class MatMulIntegerToFloatBase : public OpKernel {
                        uint8_t b_zero_point,
                        float multiplier,
                        const Tensor* bias_tensor) const;
-
-  bool b_is_signed_;
-  TensorShape b_shape_;
-  BufferUniquePtr packed_b_;
 };
-
-#ifdef MLAS_SUPPORTS_PACKED_GEMM_U8X8
-Status MatMulIntegerToFloatBase::PrePack(const Tensor& tensor, int input_idx, bool& is_packed) {
-  is_packed = false;
-
-  // only pack Matrix B
-  if (input_idx == 1) {
-    // Only handle the common case of a 2D weight matrix. Additional matrices
-    // could be handled by stacking the packed buffers.
-    b_shape_ = tensor.Shape();
-    if (b_shape_.NumDimensions() != 2) {
-      return Status::OK();
-    }
-
-    const size_t K = static_cast<size_t>(b_shape_[0]);
-    const size_t N = static_cast<size_t>(b_shape_[1]);
-
-    const auto* b_data = static_cast<const uint8_t*>(tensor.DataRaw());
-    b_is_signed_ = tensor.IsDataType<int8_t>();
-
-    const size_t packed_b_size = MlasGemmPackBSize(N, K, b_is_signed_);
-    if (packed_b_size == 0) {
-      return Status::OK();
-    }
-
-    auto alloc = Info().GetAllocator(0, OrtMemTypeDefault);
-    auto* packed_b_data = alloc->Alloc(packed_b_size);
-    packed_b_ = BufferUniquePtr(packed_b_data, BufferDeleter(alloc));
-    MlasGemmPackB(N, K, b_data, N, b_is_signed_, packed_b_data);
-    is_packed = true;
-  }
-  return Status::OK();
-}
-#endif
 
 Status MatMulIntegerToFloatBase::ComputeCommon(OpKernelContext* ctx,
                                                const uint8_t* a_data,
