a4cfd78595ddaac12dcdf6554a5c14c997c020c2
Thiago Crepaldi <thiago.crepaldi@microsoft.com>
2020-10-08 18:07:32 -0700

    Basic plumbing for backward pass. Not fully working

--- orttraining/orttraining/python/training/ortmodule.py
+++ orttraining/orttraining/python/training/ortmodule.py
@@ -17,55 +17,37 @@ class ORTModule(torch.nn.Module):
     def __init__(self, module):
         print(f'ORTModule.__init__() was called')
         super(ORTModule, self).__init__()
-
         # User will interact with it (debugging, etc)
         self._original_module = module
 
         # Forward pass
         self._onnx_forward = None
+        self._forward_session = None
         self._onnx_forward_initializers_desc = []
         self._onnx_forward_inputs_desc = []
         self._onnx_forward_outputs_desc = []
 
         # Backward pass
         self._onnx_backward = None
+        self._backward_session = None
 
     def forward(self, *input, **kwargs):
         print(f'ORTModule.forward() was called')
 
         if not self._onnx_forward:
             original_forward_graph = ORTModule._get_forward_graph(self._original_module, *input, **kwargs)
-            # gradient_graph = ORTModule._build_gradient_graph(original_forward_graph)
+            gradient_graph = ORTModule._build_gradient_graph(original_forward_graph)
+            # TODO: Remove manual split after MVP
             # self.forward_graph, self.backward_graph = ORTModule._split_forward_and_backward(gradient_graph)
-            self._onnx_forward = original_forward_graph # TODO: hard-coding for MVP
-            # import pdb; pdb.set_trace()
-            self.forward_session = onnxruntime.InferenceSession(self._onnx_forward.SerializeToString())
+            self._onnx_forward = original_forward_graph  # TODO: hard-coding for MVP
+            self._onnx_backward = gradient_graph  # TODO: hard-coding for MVP
+            self._forward_session = onnxruntime.InferenceSession(self._onnx_forward.SerializeToString())
+            self._backward_session = onnxruntime.InferenceSession(self._onnx_backward.SerializeToString())
 
+            # TODO: debug only
+            self._save_onnx_graph(self._onnx_forward, 'ortmodule_forward_mnist.onnx')
+            self._save_onnx_graph(self._onnx_backward, 'ortmodule_backward_mnist.onnx')
 
-            # TrainingParameters
-            # ort_parameters = onnxruntime.TrainingParameters()
-            # ort_parameters.loss_output_name = "loss"
-            # ort_parameters.use_mixed_precision = False
-            # ort_parameters.world_rank = 0
-            # ort_parameters.world_size = 1
-            # ort_parameters.gradient_accumulation_steps = 1
-            # ort_parameters.allreduce_post_accumulation = False
-            # ort_parameters.deepspeed_zero_stage = 0
-            # ort_parameters.enable_grad_norm_clip = False
-            # ort_parameters.set_gradients_as_graph_outputs = False
-            # ort_parameters.use_invertible_layernorm_grad = False
-            # ort_parameters.training_optimizer_name = "SGDOptimizer"
-            # ort_parameters.lr_params_feed_name = "Learning_Rate"
-            # ort_parameters.weights_to_train = trainable_params
-            # ort_parameters.optimizer_attributes_map = optimizer_attributes_map
-            # ort_parameters.optimizer_int_attributes_map = optimizer_int_attributes_map
-
-            # # SessionOptions
-            # session_options = onnxruntime.SessionOptions()
-            # session_options.use_deterministic_compute = self.options.debug.deterministic_compute
-            # self.forward_session = onnxruntime.TrainingSession(self._onnx_forward.SerializeToString(), ort_parameters, session_options)
-
-            self._save_onnx_graph(self._onnx_forward, 'forward_mnist.onnx')
         if not self._onnx_forward_initializers_desc:
             self._onnx_forward_initializers_desc = self._get_initializer_from_graph(self._onnx_forward)
         if not self._onnx_forward_inputs_desc:
@@ -73,6 +55,7 @@ class ORTModule(torch.nn.Module):
         if not self._onnx_forward_outputs_desc:
             self._onnx_forward_outputs_desc = self._get_output_from_graph(self._onnx_forward)
 
+        # TODO: debug only
         print(f'Initializers: {self._onnx_forward_initializers_desc}')
         print(f'Inputs: {self._onnx_forward_inputs_desc}')
         print(f'Outpus: {self._onnx_forward_outputs_desc}')
@@ -86,13 +69,12 @@ class ORTModule(torch.nn.Module):
                 # Note: A potential optimization would be to detect which of inputs and weights
                 # require a gradient.
                 # intermediates, outputs = self._run_forward_graph(inputs) # inputs, weights)
-                # import pdb; pdb.set_trace()
-                outputs = self._run_forward_graph(*input, **kwargs) # inputs, weights)
+                outputs = self._run_forward_graph(*input, **kwargs)  # inputs, weights)
                 outputs = [torch.from_numpy(out).requires_grad_(True) for out in outputs]
 
                 # TODO: Properly save intermediate tensors and remove them from model output
-                ctx.save_for_backward(outputs[1])
-                outputs = [outputs[0]]
+                ctx.save_for_backward([(input, kwargs), outputs[1]])
+                # outputs = [outputs[0]]
 
                 # TODO: Properly support original module output format
                 if len(outputs) == 1:
@@ -100,42 +82,67 @@ class ORTModule(torch.nn.Module):
                 return tuple(outputs)
 
             @staticmethod
-            def backward(ctx, grad_output):
+            def backward(ctx, *grad_output):
                 print(f'_ORTModuleFunction.backward() was called')
-                ...
-                # intermediates = ctx.saved_tensors
+                input_and_kwargs, intermediates = ctx.saved_tensors
                 # grad_inputs, grad_weights = self._run_backward_graph(
                 #     grad_output, intermediates)
                 # return grad_inputs, grad_weights
 
-        return _ORTModuleFunction.apply(self._prepare_model_input(*input, **kwargs))
+        return _ORTModuleFunction.apply(self._prepare_forward_input(*input, **kwargs))
 
-    def _prepare_model_input(self, *input, **kwargs):
+    def _prepare_forward_input(self, *input, **kwargs):
         # Dictionary containing both inputs and initializers
         input_with_initializer = {}
 
-        # import pdb; pdb.set_trace()
         # Inputs
-        for idx, input_data in enumerate(self.forward_session.get_inputs()):
-            input_with_initializer.update({input_data.name : input[idx].cpu().numpy()})
+        for idx, input_data in enumerate(self._forward_session.get_inputs()):
+            input_with_initializer.update({input_data.name: input[idx].cpu().numpy()})
 
         # Initializers
         for idx, param in enumerate(self._original_module.named_parameters()):
-            input_with_initializer.update({param[0] : param[1].detach().numpy()})
+            input_with_initializer.update({param[0]: param[1].detach().numpy()})
 
         return input_with_initializer
 
-    def _run_forward_graph(self, data_with_initializer): #input, weights):
-        # import pdb; pdb.set_trace()
-        return self.forward_session.run(None, data_with_initializer)
+    def _prepare_backward_input(self, grad_output, intermediates, *inputs, **kwargs):
+        # Dictionary containing initializers
+        input_with_initializer = {}
 
-    def _run_backward_graph(self, grad_output, intermediates):
+        # User input
+        # TODO: How to determine which user input to feed to backward
+        for idx, input_data in enumerate(self._forward_session.get_inputs()):
+            input_with_initializer.update({input_data.name: inputs[idx].cpu().numpy()})
+
+        # Initializers
+        # TODO: How to determine which initializer (subset) to be used
+        for idx, param in enumerate(self._original_module.named_parameters()):
+            if param[0] == 'fc2.weight':
+                input_with_initializer.update({param[0]: param[1].detach().numpy()})
+
+        # Grad output
+        # TODO: How to determine grad_output name?
+        input_with_initializer.update({'probability_grad': grad_output.detach().numpy()})
+
+        # Intermediates
+        # TODO: How to determine intermediates name?
+        input_with_initializer.update({'7': intermediates.detach().numpy()})
+        return input_with_initializer
+
+    def _run_forward_graph(self, data_with_initializer):  # input, weights):
+        print(f'_run_forward_graph was called...')
+        return self._forward_session.run(None, data_with_initializer)
+
+    def _run_backward_graph(self, grad_output, intermediates, *inputs, **kwargs):
         # Use an InferenceSession to execute self.backward_graph.
         # Return gradient tensors for inputs and weights.
-        ...
+        print(f'_run_backward_graph was called...')
+        data = self._prepare_backward_input(grad_output, intermediates, *inputs, **kwargs)
+        return self._backward_session.run(None, data)
 
     @staticmethod
     def _get_forward_graph(module, module_input):
+        print(f'_get_forward_graph was called...')
         # TODO: Pytorch module must be exported to ONNX and splitted
         #       Hard-coding with MNIST stub for MVP
         # Export torch.nn.Module to ONNX with initializers as input
@@ -148,9 +155,10 @@ class ORTModule(torch.nn.Module):
         #                   export_params=True)
         # return onnx.load_model_from_string(f.getvalue())
         return onnx.load('/home/thiagofc/mnist_onnx/mnist_with_training_forward_sliced.onnx')
+        # return onnx.load('/home/thiagofc/mnist_onnx/mnist_with_training.onnx')
 
     def _get_initializer_from_graph(self, graph):
-        # TODO: There is a tradefoo between memory footprint and total model export time
+        # TODO: There is a tradeoff between memory footprint and total model export time
         #   Ideally we want to export the model using torch.onnx.export(.., export_params=False, keep_initializers_as_inputs=True)
         #   to obtain an ONNX model with minimal size and initializers as input.
         #   However, this results in (guessing) assuming only initializer's name end with '.weight' and '.bias'.
@@ -168,7 +176,7 @@ class ORTModule(torch.nn.Module):
             # TODO: Dynamic shape is not being handled yet
             shape = initializer.dims
             dtype = _utils.dtype_onnx_to_torch(initializer.data_type)
-            initializers.append({'name' : name, 'shape' : shape, 'dtype' : dtype})
+            initializers.append({'name': name, 'shape': shape, 'dtype': dtype})
         return initializers
 
     def _get_input_from_graph(self, graph):
@@ -182,7 +190,7 @@ class ORTModule(torch.nn.Module):
                 # TODO: Dynamic shape is not being handled yet
                 shape = [dim.dim_value for dim in elem.type.tensor_type.shape.dim]
                 dtype = _utils.dtype_onnx_to_torch(elem.type.tensor_type.elem_type)
-                inputs.append({'name' : name, 'shape' : shape, 'dtype' : dtype})
+                inputs.append({'name': name, 'shape': shape, 'dtype': dtype})
         return inputs
 
     def _get_output_from_graph(self, graph):
@@ -196,7 +204,7 @@ class ORTModule(torch.nn.Module):
                 # TODO: Dynamic shape is not being handled yet
                 shape = [dim.dim_value for dim in elem.type.tensor_type.shape.dim]
                 dtype = _utils.dtype_onnx_to_torch(elem.type.tensor_type.elem_type)
-                outputs.append({'name' : name, 'shape' : shape, 'dtype' : dtype})
+                outputs.append({'name': name, 'shape': shape, 'dtype': dtype})
         return outputs
 
     @staticmethod
@@ -226,18 +234,20 @@ class ORTModule(torch.nn.Module):
 
     @staticmethod
     def _build_gradient_graph(forward_graph):
-        # Invoke the C++ GradientBuilder implementation via pybind.
+        print(f'_build_gradient_graph was called...')
+        # TODO: Invoke the C++ GradientBuilder implementation via pybind.
         # Return an ONNX graph that contains the forward and backward nodes, which takes the
         # following inputs:
         # * Module inputs
         # * Module weights
         # * Gradients with respect to the module outputs
         # …and produces gradients with respect to the module inputs and weights.
-        ...
+        return onnx.load('/home/thiagofc/mnist_onnx/mnist_with_training_backward_sliced.onnx')
 
     @staticmethod
     def _split_forward_and_backward(gradient_graph):
-        # Split the result of _build_gradient_graph into two subgraphs:
+        print(f'_split_forward_and_backward was called...')
+        # TODO: Split the result of _build_gradient_graph into two subgraphs:
         # * A forward graph that takes module inputs and weights as input, and produces module
         #   outputs and (“stashed”) intermediate tensors as output.
         # * A backward graph that takes intermediate tensors, module weights, and gradients
--- orttraining/orttraining/test/python/orttraining_test_ortmodule_basic_transform_model.py
+++ orttraining/orttraining/test/python/orttraining_test_ortmodule_basic_transform_model.py
@@ -1,4 +1,5 @@
 # coding=utf8
+import copy
 import sys
 import onnx
 from onnx import helper, shape_inference
@@ -14,11 +15,11 @@ input_model_name = sys.argv[1]
 output_forward_model_name = input_model_name[:-5] + '_forward_sliced.onnx'
 output_backward_model_name = input_model_name[:-5] + '_backward_sliced.onnx'
 
-def add_model_input_from_initializer(model, initializer, docstring=None):
+def add_input_from_initializer(model, initializer, docstring=None):
     new_input = onnx.helper.make_tensor_value_info(initializer.name, initializer.data_type, initializer.dims, docstring)
     model.graph.input.append(new_input)
 
-def add_model_input(model, name, data_type, dims, docstring=None):
+def add_input(model, name, data_type = None, dims = None, docstring = None):
     new_input = onnx.helper.make_tensor_value_info(name, data_type, dims, docstring)
     model.graph.input.append(new_input)
 
@@ -61,73 +62,45 @@ def find_node(model, name):
 model = onnx.load(input_model_name)
 
 # Remove model inputs
-#   They are: label
-node = find_model_input(model, 'label')
-model.graph.input.remove(node)
+nodes = ['label']
+for node in nodes:
+    node = find_model_input(model, node)
+    model.graph.input.remove(node)
 
 # Remove model outputs
-#   They are: loss
-node = find_model_output(model, 'loss')
-model.graph.output.remove(node)
-node = find_model_output(model, 'fc1.bias_grad')
-model.graph.output.remove(node)
-node = find_model_output(model, 'fc1.weight_grad')
-model.graph.output.remove(node)
-node = find_model_output(model, 'fc2.bias_grad')
-model.graph.output.remove(node)
-node = find_model_output(model, 'fc2.weight_grad')
-model.graph.output.remove(node)
+nodes = ['loss', 'fc1.bias_grad', 'fc1.weight_grad', 'fc2.bias_grad', 'fc2.weight_grad']
+for node in nodes:
+    node = find_model_output(model, node)
+    model.graph.output.remove(node)
 
 # Add input with same name, type and shape as the initializers
 #   They are: [fc1.bias, fc1.weight, fc2.bias, fc2.weight]
-node = find_initializer(model, 'fc1.bias')
-add_model_input_from_initializer(model, node, 'thiagofc: add fc1.bias as model input')
-node = find_initializer(model, 'fc1.weight')
-add_model_input_from_initializer(model, node, 'thiagofc: add fc1.weight as model input')
-node = find_initializer(model, 'fc2.bias')
-add_model_input_from_initializer(model, node, 'thiagofc: add fc2.bias as model input')
-node = find_initializer(model, 'fc2.weight')
-add_model_input_from_initializer(model, node, 'thiagofc: add fc2.weight as model input')
+forward_initializer_names = ['fc1.bias', 'fc1.weight', 'fc2.bias', 'fc2.weight']
+forward_initializer = {}
+for node in forward_initializer_names:
+    node = find_initializer(model, node)
+    add_input_from_initializer(model, node, f'thiagofc: add {node.name} as model input')
+    forward_initializer.update({node.name : copy.deepcopy(node)})
 
 # Remove initializers from model
-#   They are: [fc1.bias, fc1.weight, fc2.bias, fc2.weight]
 # TODO: Do this when we are able to distinguish inputs from initializers
-# model.graph.initializer.remove(node)
-# model.graph.initializer.remove(node)
-# model.graph.initializer.remove(node)
-# model.graph.initializer.remove(node)
+# for node in forward_initializer_names:
+#    node = find_initializer(model, init)
+#    model.graph.initializer.remove(node)
 
 # Remove backward-related initializers
-#   They are: [loss_grad, ZeroConstant]
-node = find_initializer(model, 'loss_grad')
-model.graph.initializer.remove(node)
-node = find_initializer(model, 'ZeroConstant')
-model.graph.initializer.remove(node)
+nodes = ['loss_grad', 'ZeroConstant']
+for node in nodes:
+    node = find_initializer(model, node)
+    model.graph.initializer.remove(node)
 
 # Remove OPs
-#   They are: [SoftmaxCrossEntropyLoss_3, SoftmaxCrossEntropyLoss_3_Grad/SoftmaxCrossEntropyLossGrad_0,
-#              Gemm_2_Grad/ReduceSum_3, Gemm_2_Grad/Identity_4, Gemm_2_Grad/Gemm_2, Gemm_2_Grad/Gemm_1,
-#              Relu_1_Grad/ReluGrad_0, Gemm_0_Grad/Gemm_1, Gemm_0_Grad/ReduceSum_2, Gemm_0_Grad/Identity_3]
-node = find_node(model, 'SoftmaxCrossEntropyLoss_3')
-model.graph.node.remove(node)
-node = find_node(model, 'SoftmaxCrossEntropyLoss_3_Grad/SoftmaxCrossEntropyLossGrad_0')
-model.graph.node.remove(node)
-node = find_node(model, 'Gemm_2_Grad/ReduceSum_3')
-model.graph.node.remove(node)
-node = find_node(model, 'Gemm_2_Grad/Identity_4')
-model.graph.node.remove(node)
-node = find_node(model, 'Gemm_2_Grad/Gemm_2')
-model.graph.node.remove(node)
-node = find_node(model, 'Gemm_2_Grad/Gemm_1')
-model.graph.node.remove(node)
-node = find_node(model, 'Relu_1_Grad/ReluGrad_0')
-model.graph.node.remove(node)
-node = find_node(model, 'Gemm_0_Grad/Gemm_1')
-model.graph.node.remove(node)
-node = find_node(model, 'Gemm_0_Grad/ReduceSum_2')
-model.graph.node.remove(node)
-node = find_node(model, 'Gemm_0_Grad/Identity_3')
-model.graph.node.remove(node)
+nodes = ['SoftmaxCrossEntropyLoss_3', 'SoftmaxCrossEntropyLoss_3_Grad/SoftmaxCrossEntropyLossGrad_0',
+         'Gemm_2_Grad/ReduceSum_3', 'Gemm_2_Grad/Identity_4', 'Gemm_2_Grad/Gemm_2', 'Gemm_2_Grad/Gemm_1',
+         'Relu_1_Grad/ReluGrad_0', 'Gemm_0_Grad/Gemm_1', 'Gemm_0_Grad/ReduceSum_2', 'Gemm_0_Grad/Identity_3']
+for node in nodes:
+    node = find_node(model, node)
+    model.graph.node.remove(node)
 
 # Add new outputs:
 #   They are: 7
@@ -138,11 +111,38 @@ with open(output_forward_model_name, "wb") as f:
 
 
 ###############################################################################
-# FORWARD PASS GRAPH ##########################################################
+# BACKWARD PASS GRAPH ##########################################################
 ###############################################################################
 model = onnx.load(input_model_name)
 
+# Add new inputs:
+# TODO: Should we specify types here? ORT graph doesn't have that info available, but ONNX API needs it
+add_input_from_initializer(model, forward_initializer['fc2.weight'], 'thiagofc: add fc2.weight as model input')
+add_input(model, '7', 1, None, 'thiagofc: add 7 as model input')
+add_input(model, 'probability_grad', 1, None, 'thiagofc: add probability_grad as model input')
 
+# Remove model inputs
+#   They are: label
+node = find_model_input(model, 'label')
+model.graph.input.remove(node)
+
+# Remove model outputs
+nodes = ['loss', 'probability']
+for node in nodes:
+    node = find_model_output(model, node)
+    model.graph.output.remove(node)
+
+# Remove OP nodes from forward pass
+nodes = ['Gemm_0', 'Relu_1', 'Gemm_2', 'SoftmaxCrossEntropyLoss_3', 'SoftmaxCrossEntropyLoss_3_Grad/SoftmaxCrossEntropyLossGrad_0']
+for node in nodes:
+    node = find_node(model, node)
+    model.graph.node.remove(node)
+
+# Remove initializers
+forward_initializer_names.extend(['loss_grad'])
+for node in forward_initializer_names:
+   node = find_initializer(model, node)
+   model.graph.initializer.remove(node)
 
 with open(output_backward_model_name, "wb") as f:
     f.write(model.SerializeToString())
