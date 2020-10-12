d0390e82a660e51070020c5a41e475bf79d32db5
Pranav Sharma <prs@microsoft.com>
2020-10-12 10:09:38 -0700

    Register opset13 ops: Dropout, Flatten, LRN, MeanVarianceNormalization, ArgMax, ArgMin, Reshape, Shape, Concat. (#5451)

--- onnxruntime/core/providers/cpu/tensor/shape_op.cc
+++ onnxruntime/core/providers/cpu/tensor/shape_op.cc
@@ -5,9 +5,16 @@
 
 namespace onnxruntime {
 
-ONNX_CPU_OPERATOR_KERNEL(
+ONNX_CPU_OPERATOR_VERSIONED_KERNEL(
     Shape,
     1,
+    12,
+    KernelDefBuilder().TypeConstraint("T", DataTypeImpl::AllFixedSizeTensorTypes()).TypeConstraint("T1", DataTypeImpl::GetTensorType<int64_t>()),
+    Shape);
+
+ONNX_CPU_OPERATOR_KERNEL(
+    Shape,
+    13,
     KernelDefBuilder().TypeConstraint("T", DataTypeImpl::AllFixedSizeTensorTypes()).TypeConstraint("T1", DataTypeImpl::GetTensorType<int64_t>()),
     Shape);
 
