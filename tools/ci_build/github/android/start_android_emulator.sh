f25c9e1757dfdf545faae1ffad4668747d80323b
gwang <wanggy@outlook.com>
2020-10-13 12:08:10 -0700

    Update Android CI

--- tools/ci_build/github/android/start_android_emulator.sh
+++ tools/ci_build/github/android/start_android_emulator.sh
@@ -3,6 +3,14 @@
 
 set -e
 
+# we are trying to create a device with port 5682 here, so the device will have serial number emulator-5682
+# if it exists, we will use it directly, without running a new emulator instance
+if adb devices | grep -e '^emulator-5682';
+then
+    echo "emulator-5682 is running, exiting without starting new emulator"
+    exit 0
+fi
+
 echo "y" | $ANDROID_HOME/tools/bin/sdkmanager --install 'system-images;android-29;google_apis;x86_64'
 
 echo "no" | $ANDROID_HOME/tools/bin/avdmanager create avd -n android_emulator -k 'system-images;android-29;google_apis;x86_64' --force
@@ -10,7 +18,7 @@ echo "no" | $ANDROID_HOME/tools/bin/avdmanager create avd -n android_emulator -k
 echo "Starting emulator"
 
 # Start emulator in background
-nohup $ANDROID_HOME/emulator/emulator -avd android_emulator -partition-size 2048 -memory 3072 -timezone America/Los_Angeles -no-snapshot -no-audio -no-boot-anim -no-window &
+nohup $ANDROID_HOME/emulator/emulator -avd android_emulator -partition-size 2048 -memory 3072 -timezone America/Los_Angeles -no-snapshot -no-audio -no-boot-anim -no-window -port 5682 &
 
 $ANDROID_HOME/platform-tools/adb start-server
 
