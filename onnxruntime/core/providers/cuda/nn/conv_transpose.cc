ff127ce3f9172b51dfadb0a09ef083c5ce1740b2
Marijn Stollenga <mstollenga@gmail.com>
2020-10-17 20:46:18 +0200

    Update dimensions check to enable 3D Conv Transpose

--- onnxruntime/core/providers/cuda/nn/conv_transpose.cc
+++ onnxruntime/core/providers/cuda/nn/conv_transpose.cc
@@ -6,7 +6,7 @@
 namespace onnxruntime {
 namespace cuda {
 
-// Op Set 11 for ConvTranspose only update document to clearify default dilations and strides value.
+// Op Set 11 for ConvTranspose only update document to clarify default dilations and strides value.
 // which are already covered by op set 11 cpu version, so simply add declaration.
 #define REGISTER_KERNEL_TYPED(T)                                                \
   ONNX_OPERATOR_VERSIONED_TYPED_KERNEL_EX(                                      \
@@ -45,10 +45,10 @@ Status ConvTranspose<T>::DoConvTranspose(OpKernelContext* context, bool dynamic_
   auto x_data = reinterpret_cast<const CudaT*>(X->template Data<T>());
 
   auto x_dimensions = X->Shape().NumDimensions();
-  if (x_dimensions != 4 && x_dimensions != 3) {
+  if (x_dimensions != 5 && x_dimensions != 4 && x_dimensions != 3) {
     // This condition is not true for test_convtranspose_3d in ONNX tests series.
     // TODO: the error message should tell which operator raises it.
-    return ORT_MAKE_STATUS(ONNXRUNTIME, INVALID_ARGUMENT, "Input X must be 3- or 4-dimensional.",
+    return ORT_MAKE_STATUS(ONNXRUNTIME, INVALID_ARGUMENT, "Input X must be 3-, 4- or 5-dimensional.",
                            " X: ", X->Shape().ToString().c_str());
   }
   const Tensor* W = context->Input<Tensor>(1);
