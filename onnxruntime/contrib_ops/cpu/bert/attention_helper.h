0b7eb05c8b9184dacdd9839d89ab3d9eff315550
wangye <wangye@microsoft.com>
2020-10-21 21:40:53 +0000

    support attention 3D input mask

--- onnxruntime/contrib_ops/cpu/bert/attention_helper.h
+++ onnxruntime/contrib_ops/cpu/bert/attention_helper.h
@@ -72,7 +72,7 @@ void PrepareMask(const int32_t* mask_index,
   // mask_data has been filled with 0, and its shape is BxSxS*
   T* p_mask = mask_data;
 
-  bool is_raw_attention_mask = (nullptr != mask_index_dims && mask_index_dims->size() == 2);
+  bool is_raw_attention_mask = (nullptr != mask_index_dims && (mask_index_dims->size() == 2 || mask_index_dims->size() == 3));
   bool has_mask_start_position = (nullptr != mask_index_dims && mask_index_dims->size() == 1 && static_cast<int>(mask_index_dims->at(0)) == 2 * batch_size);
 
   for (int b_i = 0; b_i < batch_size; b_i++) {
