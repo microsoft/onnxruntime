76cdb872587372d1315001bd8f07cc37340b290f
suryasidd <surya.siddharth.pemmaraju@intel.com>
2020-10-21 17:56:03 -0700

    Fixed batching logic for 2020.4 and above

--- onnxruntime/core/providers/openvino/backend_manager.cc
+++ onnxruntime/core/providers/openvino/backend_manager.cc
@@ -22,7 +22,7 @@ GlobalContext& BackendManager::GetGlobalContext() {
 }
 
 BackendManager::BackendManager(const onnxruntime::Node* fused_node, const logging::Logger& logger) {
-  auto prec_str = GetGlobalContext().precision_str; 
+  auto prec_str = GetGlobalContext().precision_str;
   if (prec_str == "FP32") {
     subgraph_context_.precision = InferenceEngine::Precision::FP32;
   } else if (prec_str == "FP16") {
@@ -33,17 +33,10 @@ BackendManager::BackendManager(const onnxruntime::Node* fused_node, const loggin
 
   // Save the indexes of graph inputs among fused_node's inputDefs
   // (which also contains initializers).
-  #if (defined OPENVINO_2020_2) || (defined OPENVINO_2020_3)
-    std::map<std::string, int> inputdef_index_map;
-  #endif
   auto node_input_defs = fused_node->InputDefs();
   int i = 0;
   for (auto idef : node_input_defs) {
-    #if (defined OPENVINO_2020_2) || (defined OPENVINO_2020_3)
-    inputdef_index_map.insert({idef->Name(), i});
-    #else
     subgraph_context_.input_names.insert({idef->Name(), i});
-    #endif
     i++;
   }
 
@@ -57,15 +50,12 @@ BackendManager::BackendManager(const onnxruntime::Node* fused_node, const loggin
         }
       }
     }
-    #if (defined OPENVINO_2020_2) || (defined OPENVINO_2020_3)
-    auto it = inputdef_index_map.find(input->Name());
-    if (it == inputdef_index_map.end()) {
+    auto it = subgraph_context_.input_names.find(input->Name());
+    if(it == subgraph_context_.input_names.end()){
       ORT_THROW("Input not found in the input defs list");
     }
-
     int index = it->second;
     subgraph_context_.input_indexes.push_back(index);
-    #endif
   }
 
   auto graph_outputs_defs = fused_node->OutputDefs();
@@ -99,15 +89,9 @@ BackendManager::BackendManager(const onnxruntime::Node* fused_node, const loggin
 
 bool BackendManager::ModelHasBatchedInputs(const ONNX_NAMESPACE::ModelProto& model_proto) const {
   bool has_batched_inputs = true;
-  
-  #if (defined OPENVINO_2020_2) || (defined OPENVINO_2020_3)
+
   for (int i = 0; i < (int)subgraph_context_.input_indexes.size(); i++) {
     auto input = model_proto.graph().input(subgraph_context_.input_indexes[i]);
-  #else
-  for (auto input_info_iter = subgraph_context_.input_names.begin();
-       input_info_iter != subgraph_context_.input_names.end(); ++input_info_iter) {
-    auto input = model_proto.graph().input(input_info_iter->second);
-  #endif
 
     // Batch-process only raw image inputs (NCHW or NHWC layouts)
     auto shape = input.type().tensor_type().shape();
@@ -268,16 +252,9 @@ void BackendManager::Compute(Ort::CustomOpApi api, OrtKernelContext* context) {
     auto key = MakeMapKeyString(tensor_shapes, GetGlobalContext().device_type);
 
     if(GetGlobalContext().device_type.find("MYRIAD") != std::string::npos){
-      
-      #if (defined OPENVINO_2020_2) || (defined OPENVINO_2020_3)
+
       for(size_t i = 0; i < subgraph_context_.input_indexes.size(); i++){
         if(tensor_shapes[i].size() != 4)
-      #else
-      for (auto input_info_iter = subgraph_context_.input_names.begin();
-          input_info_iter  != subgraph_context_.input_names.end(); ++input_info_iter) {
-        if(tensor_shapes[input_info_iter->second].size() != 4)
-      #endif
-
           subgraph_context_.set_vpu_config = true;
       }
     }
