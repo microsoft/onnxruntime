a7d08e1c867e8b83ad485c8332d13886fd65e644
gwang <wanggy@outlook.com>
2020-10-21 23:28:17 -0700

    Fix Android CI failure

--- onnxruntime/test/providers/nnapi/nnapi_basic_test.cc
+++ onnxruntime/test/providers/nnapi/nnapi_basic_test.cc
@@ -58,7 +58,7 @@ TEST(NnapiExecutionProviderTest, ReshapeFlattenTest) {
   std::string model_file_name = "testdata/reshape_flatten_test.onnx";
 
   std::vector<int64_t> dims_mul_x = {2, 1, 2};
-  std::vector<float> values_mul_x = {1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f};
+  std::vector<float> values_mul_x = {1.0f, 2.0f, 3.0f, 4.0f};
   std::vector<int64_t> dims_mul_y = {3, 2, 2};
   std::vector<float> values_mul_y = {1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f, 7.0f, 8.0f, 9.0f, 10.0f, 11.0f, 12.0f};
   OrtValue ml_value_x;
