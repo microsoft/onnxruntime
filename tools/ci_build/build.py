e00dfdbce80db430e516c8436f5f25ab2e156f51
Changming Sun <chasun@microsoft.com>
2020-10-22 16:27:36 -0700

    update

--- tools/ci_build/build.py
+++ tools/ci_build/build.py
@@ -1355,7 +1355,7 @@ def run_onnxruntime_tests(args, source_dir, ctest_path, build_dir, configs):
                      source_dir, 'cmake\\codeconv.runsettings')] + executables,
                 cwd=cwd2, dll_path=dll_path)
         else:
-            ctest_cmd = [ctest_path, "-T", "memcheck", "--build-config", config, "--verbose", "--timeout", "3600"]
+            ctest_cmd = [ctest_path, "-T", "memcheck", "--build-config", config, "--verbose", "--timeout", "10800"]
             run_subprocess(ctest_cmd, cwd=cwd, dll_path=dll_path)
 
         if args.enable_pybind:
