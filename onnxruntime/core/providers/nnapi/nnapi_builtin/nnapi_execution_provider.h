ca0f92ad2eaee2358dec2fc31f64b0463b863aca
gwang <wanggy@outlook.com>
2020-10-22 18:56:08 -0700

    add comments

--- onnxruntime/core/providers/nnapi/nnapi_builtin/nnapi_execution_provider.h
+++ onnxruntime/core/providers/nnapi/nnapi_builtin/nnapi_execution_provider.h
@@ -19,11 +19,13 @@ class NnapiExecutionProvider : public IExecutionProvider {
                 const std::vector<const KernelRegistry*>& /*kernel_registries*/) const override;
   common::Status Compile(const std::vector<onnxruntime::Node*>& fused_nodes,
                          std::vector<NodeComputeInfo>& node_compute_funcs) override;
-
   const std::bitset<NNAPI_FLAG_MAX>& GetNNAPIFlags() const { return nnapi_flags_; }
 
  private:
   std::unordered_map<std::string, std::unique_ptr<onnxruntime::nnapi::Model>> nnapi_models_;
+
+  // The bit set which defined bool options for NNAPI EP, bits are defined as
+  // NNAPIFlag in include/onnxruntime/core/providers/nnapi/nnapi_provider_factory.h
   std::bitset<NNAPI_FLAG_MAX> nnapi_flags_;
 };
 }  // namespace onnxruntime
