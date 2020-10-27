2274b5d8ddb6f1c52353e839c48056e72f42edbb
Gani Nazirov <ganaziro@OrtTrainingDev3.af05slrtruoetgaxwwjv5nsq5e.px.internal.cloudapp.net>
2020-10-27 22:37:45 +0000

    dum accumulate buffer

--- orttraining/orttraining/core/session/training_session.cc
+++ orttraining/orttraining/core/session/training_session.cc
@@ -1011,14 +1011,15 @@ std::unordered_set<std::string> TrainingSession::GetStateTensorNames() const {
       opt_state_initializer_names_.begin(), opt_state_initializer_names_.end());
   checkpointed_tensor_names.insert(
       mixed_precision_weight_initializer_names_.begin(), mixed_precision_weight_initializer_names_.end());
-  auto& initializers = session_state_->GetInitializedTensors();
-  auto& name_map = session_state_->GetOrtValueNameIdxMap();
+  auto& initializers = GetSessionState().GetInitializedTensors();
+  auto& name_map = GetSessionState().GetOrtValueNameIdxMap();
   for (auto& kv : initializers) {
     std::string name;
     if (name_map.GetName(kv.first, name).IsOK() && name.find("accumulate") != std::string::npos) {
       checkpointed_tensor_names.insert(name);
     }
-  }  return checkpointed_tensor_names;
+  }
+  return checkpointed_tensor_names;
 }
 
 bool TrainingSession::IsUntrainable(const Node* node, const std::string& initializer_name,
