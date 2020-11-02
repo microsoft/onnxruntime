0df5f6e25784eb84b53319f6dc2859c7ee33922e
satyajandhyala <satya.k.jandhyala@gmail.com>
2020-11-02 12:13:55 -0800

    Added model checkpoint dump after loading.

--- onnxruntime/test/fuzzing/src/test.cpp
+++ onnxruntime/test/fuzzing/src/test.cpp
@@ -280,6 +280,11 @@ int main(int argc, char* argv[])
     }
 
     std::wstring& modelFileName{opt.modelFileName};
+    std::filesystem::path p(modelFileName);
+
+    std::wstring modelOutFileName = (p.parent_path() / p.stem()).wstring();
+    modelOutFileName += L".out";
+    modelOutFileName += p.extension().wstring();
     std::wstring& mutateModelDirName{opt.mutateModelDirName};
     bool& repoMode{opt.repoMode};
     int& testTimeOut{opt.testTimeOut};
@@ -295,6 +300,7 @@ int main(int argc, char* argv[])
     // Create a stream to hold the model
     //
     std::ifstream modelStream{modelFile, std::ios::in | std::ios::binary};
+    std::ofstream modelOutStream{modelOutFileName, std::ios::out | std::ios::binary};
     // Create an onnx protobuf object
     //
     onnx::ModelProto model_proto;
@@ -303,6 +309,7 @@ int main(int argc, char* argv[])
     //
     if( model_proto.ParseFromIstream(&modelStream) )
     {
+      model_proto.SerializeToOstream(&modelOutStream);
       if(repoMode)
       {
         Logger::testLog<< L"Running Prediction for: " << modelFileName 
