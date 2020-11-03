366ea5b1d346b97494525943147f4613db244364
Thiago Crepaldi <thiago.crepaldi@microsoft.com>
2020-11-03 15:36:49 -0800

    Refactor BERT classifier fine tune for better debugging

--- orttraining/orttraining/test/python/orttraining_test_ortmodule_bert_classifier.py
+++ orttraining/orttraining/test/python/orttraining_test_ortmodule_bert_classifier.py
@@ -22,205 +22,15 @@ import datetime
 import onnxruntime
 from onnxruntime.training import ORTModule
 
+def train(model, optimizer, scheduler, train_dataloader, epoch, device, args):
+    # ========================================
+    #               Training
+    # ========================================
+    # This training code is based on the `run_glue.py` script here:
+    # https://github.com/huggingface/transformers/blob/5bfcd0485ece086ebcbed2d008813037968a9e58/examples/run_glue.py#L128
 
-# 0. Common stuff
-parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
-parser.add_argument('--pytorch-only', action='store_true', default=False,
-                    help='disables ONNX Runtime training')
-parser.add_argument('--view-graphs', action='store_true', default=False,
-                    help='views forward and backward graphs')
-parser.add_argument('--no-cuda', action='store_true', default=False,
-                    help='disables CUDA training')
-parser.add_argument('--epochs', type=int, default=4, metavar='N',
-                        help='number of epochs to train (default: 4)')
-args = parser.parse_args()
-
-
-# 1. Device setup
-if torch.cuda.is_available() and not args.no_cuda:
-    device = torch.device("cuda")
-    print('There are %d GPU(s) available.' % torch.cuda.device_count())
-    print('We will use the GPU:', torch.cuda.get_device_name(0))
-else:
-    print('No GPU available, using the CPU instead.')
-    device = torch.device("cpu")
-
-# 2. Loading CoLA Dataset
-print('Downloading dataset...')
-url = 'https://nyu-mll.github.io/CoLA/cola_public_1.1.zip'
-
-# Download the file (if we haven't already)
-if not os.path.exists('./cola_public_1.1.zip'):
-    wget.download(url, './cola_public_1.1.zip')
-else:
-    print('Reusing cached dataset')
-
-# Unzip it
-if not os.path.exists('./cola_public'):
-    print('Extracting cached dataset')
-    with zipfile.ZipFile('./cola_public_1.1.zip', 'r') as zip_ref:
-        zip_ref.extractall('./')
-else:
-    print('Reusing extracted dataset')
-
-# Load the dataset into a pandas dataframe.
-df = pd.read_csv("./cola_public/raw/in_domain_train.tsv", delimiter='\t', header=None, names=['sentence_source', 'label', 'label_notes', 'sentence'])
-
-# Get the lists of sentences and their labels.
-sentences = df.sentence.values
-labels = df.label.values
-
-# 3. Tokenization & Input Formatting
-
-# Load the BERT tokenizer.
-tokenizer = BertTokenizer.from_pretrained('bert-base-uncased', do_lower_case=True)
-
-# Tokenize all of the sentences and map the tokens to their word IDs.
-input_ids = []
-for sent in sentences:
-    # `encode` will:
-    #   (1) Tokenize the sentence.
-    #   (2) Prepend the `[CLS]` token to the start.
-    #   (3) Append the `[SEP]` token to the end.
-    #   (4) Map tokens to their IDs.
-    encoded_sent = tokenizer.encode(
-                        sent,                      # Sentence to encode.
-                        add_special_tokens = True, # Add '[CLS]' and '[SEP]'
-                   )
-
-    # Add the encoded sentence to the list.
-    input_ids.append(encoded_sent)
-
-# We'll borrow the `pad_sequences` utility function to do this.
-# 64 is slightly larger than the maximum training sentence length of 47...
-MAX_LEN = 64
-
-# Pad our input tokens with value 0.
-input_ids = pad_sequences(input_ids, maxlen=MAX_LEN, dtype="long",
-                          value=0, truncating="post", padding="post")
-
-# Create attention masks
-attention_masks = []
-
-# For each sentence...
-for sent in input_ids:
-    # Create the attention mask.
-    #   - If a token ID is 0, then it's padding, set the mask to 0.
-    #   - If a token ID is > 0, then it's a real token, set the mask to 1.
-    att_mask = [int(token_id > 0) for token_id in sent]
-
-    # Store the attention mask for this sentence.
-    attention_masks.append(att_mask)
-
-# Use 90% for training and 10% for validation.
-train_inputs, validation_inputs, train_labels, validation_labels = train_test_split(input_ids, labels,
-                                                            random_state=2018, test_size=0.1)
-# Do the same for the masks.
-train_masks, validation_masks, _, _ = train_test_split(attention_masks, labels,
-                                             random_state=2018, test_size=0.1)
-
-# Convert all inputs and labels into torch tensors, the required datatype
-# for our model.
-train_inputs = torch.tensor(train_inputs)
-validation_inputs = torch.tensor(validation_inputs)
-
-train_labels = torch.tensor(train_labels)
-validation_labels = torch.tensor(validation_labels)
-
-train_masks = torch.tensor(train_masks)
-validation_masks = torch.tensor(validation_masks)
-
-# The DataLoader needs to know our batch size for training, so we specify it
-batch_size = 32
-
-# Create the DataLoader for our training set.
-train_data = TensorDataset(train_inputs, train_masks, train_labels)
-train_sampler = RandomSampler(train_data)
-train_dataloader = DataLoader(train_data, sampler=train_sampler, batch_size=batch_size)
-
-# Create the DataLoader for our validation set.
-validation_data = TensorDataset(validation_inputs, validation_masks, validation_labels)
-validation_sampler = SequentialSampler(validation_data)
-validation_dataloader = DataLoader(validation_data, sampler=validation_sampler, batch_size=batch_size)
-
-# 4. Train (fine-tune)
-
-# TODO: try to use a smaller model
-
-# Load BertForSequenceClassification, the pretrained BERT model with a single
-# linear classification layer on top.
-model = BertForSequenceClassification.from_pretrained(
-    "bert-base-uncased", # Use the 12-layer BERT model, with an uncased vocab.
-    # '/home/thiagofc/dev/github/onnxruntime/orttraining/orttraining/test/python/',
-    num_labels = 2, # The number of output labels--2 for binary classification.
-    output_attentions = False, # Whether the model returns attentions weights.
-    output_hidden_states = False, # Whether the model returns all hidden-states.
-)
-
-if not args.pytorch_only:
-    model = ORTModule(model)
-
-# TODO: change it to False to stop saving ONNX models
-model._save_onnx = True
-model._save_onnx_prefix = 'BertForSequenceClassification'
-
-# Tell pytorch to run this model on the GPU.
-if torch.cuda.is_available() and not args.no_cuda:
-    model.cuda()
-
-# Note: AdamW is a class from the huggingface library (as opposed to pytorch)
-optimizer = AdamW(model.parameters(),
-                  lr = 2e-5, # args.learning_rate - default is 5e-5, our notebook had 2e-5
-                  eps = 1e-8 # args.adam_epsilon  - default is 1e-8.
-                )
-
-# Authors recommend between 2 and 4 epochs
-# Total number of training steps is number of batches * number of epochs.
-total_steps = len(train_dataloader) * args.epochs
-
-# Create the learning rate scheduler.
-scheduler = get_linear_schedule_with_warmup(optimizer,
-                                            num_warmup_steps = 0, # Default value in run_glue.py
-                                            num_training_steps = total_steps)
-
-# Function to calculate the accuracy of our predictions vs labels
-def flat_accuracy(preds, labels):
-    pred_flat = np.argmax(preds, axis=1).flatten()
-    labels_flat = labels.flatten()
-    return np.sum(pred_flat == labels_flat) / len(labels_flat)
-
-def format_time(elapsed):
-    '''Takes a time in seconds and returns a string hh:mm:ss'''
-    # Round to the nearest second.
-    elapsed_rounded = int(round((elapsed)))
-
-    # Format as hh:mm:ss
-    return str(datetime.timedelta(seconds=elapsed_rounded))
-
-# This training code is based on the `run_glue.py` script here:
-# https://github.com/huggingface/transformers/blob/5bfcd0485ece086ebcbed2d008813037968a9e58/examples/run_glue.py#L128
-
-
-# Set the seed value all over the place to make this reproducible.
-seed_val = 42
-
-random.seed(seed_val)
-np.random.seed(seed_val)
-torch.manual_seed(seed_val)
-onnxruntime.set_seed(seed_val)
-if torch.cuda.is_available() and not args.no_cuda:
-    torch.cuda.manual_seed_all(seed_val)
-
-# Store the average loss after each epoch so we can plot them.
-loss_values = []
-
-# ========================================
-#               Training
-# ========================================
-for epoch_i in range(0, args.epochs):
     # Perform one full pass over the training set.
-    print("")
-    print('======== Epoch {:} / {:} ========'.format(epoch_i + 1, args.epochs))
+    print('\n======== Epoch {:} / {:} ========'.format(epoch + 1, args.epochs))
 
     # Measure how long the training epoch takes.
     t0 = time.time()
@@ -237,8 +47,11 @@ for epoch_i in range(0, args.epochs):
     # For each batch of training data...
     for step, batch in enumerate(train_dataloader):
 
+        if step == args.train_steps:
+            break
+
         # Progress update every 40 batches.
-        if step % 40 == 0 and not step == 0:
+        if step % args.log_interval == 0 and not step == 0:
             # Calculate elapsed time in minutes.
             elapsed = format_time(time.time() - t0)
 
@@ -288,13 +101,10 @@ for epoch_i in range(0, args.epochs):
         # calculate the average loss at the end. `loss` is a Tensor containing a
         # single value; the `.item()` function just returns the Python value
         # from the tensor.
-        print(loss.shape)
-        print(loss)
         total_loss += loss.item()
         # total_loss += loss
 
         # Perform a backward pass to calculate the gradients.
-        print(loss)
         loss.backward()
 
         # Clip the norm of the gradients to 1.0.
@@ -312,21 +122,16 @@ for epoch_i in range(0, args.epochs):
     # Calculate the average loss over the training data.
     avg_train_loss = total_loss / len(train_dataloader)
 
-    # Store the loss value for plotting the learning curve.
-    loss_values.append(avg_train_loss)
-
-    print("")
-    print("  Average training loss: {0:.2f}".format(avg_train_loss))
+    print("\n  Average training loss: {0:.2f}".format(avg_train_loss))
     print("  Training epoch took: {:}".format(format_time(time.time() - t0)))
 
+def test(model, validation_dataloader, device):
     # ========================================
     #               Validation
     # ========================================
     # After the completion of each training epoch, measure our performance on
     # our validation set.
-
-    print("")
-    print("Running Validation...")
+    print("\nRunning Validation...")
 
     t0 = time.time()
 
@@ -383,5 +188,199 @@ for epoch_i in range(0, args.epochs):
     print("  Accuracy: {0:.2f}".format(eval_accuracy/nb_eval_steps))
     print("  Validation took: {:}".format(format_time(time.time() - t0)))
 
-print("")
-print("Training complete!")
+def load_dataset():
+    # 2. Loading CoLA Dataset
+    print('Downloading dataset...')
+
+    # Download the file (if we haven't already)
+    url = 'https://nyu-mll.github.io/CoLA/cola_public_1.1.zip'
+    if not os.path.exists('./cola_public_1.1.zip'):
+        wget.download(url, './cola_public_1.1.zip')
+    else:
+        print('Reusing cached dataset')
+
+    # Unzip it
+    if not os.path.exists('./cola_public'):
+        print('Extracting cached dataset')
+        with zipfile.ZipFile('./cola_public_1.1.zip', 'r') as zip_ref:
+            zip_ref.extractall('./')
+    else:
+        print('Reusing extracted dataset')
+
+    # Load the dataset into a pandas dataframe.
+    df = pd.read_csv("./cola_public/raw/in_domain_train.tsv", delimiter='\t', header=None, names=['sentence_source', 'label', 'label_notes', 'sentence'])
+
+    # Get the lists of sentences and their labels.
+    sentences = df.sentence.values
+    labels = df.label.values
+
+    # 3. Tokenization & Input Formatting
+
+    # Load the BERT tokenizer.
+    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased', do_lower_case=True)
+
+    # Tokenize all of the sentences and map the tokens to their word IDs.
+    input_ids = []
+    for sent in sentences:
+        # `encode` will:
+        #   (1) Tokenize the sentence.
+        #   (2) Prepend the `[CLS]` token to the start.
+        #   (3) Append the `[SEP]` token to the end.
+        #   (4) Map tokens to their IDs.
+        encoded_sent = tokenizer.encode(
+                            sent,                      # Sentence to encode.
+                            add_special_tokens = True, # Add '[CLS]' and '[SEP]'
+                    )
+
+        # Add the encoded sentence to the list.
+        input_ids.append(encoded_sent)
+
+    # We'll borrow the `pad_sequences` utility function to do this.
+    # 64 is slightly larger than the maximum training sentence length of 47...
+    MAX_LEN = 64
+
+    # Pad our input tokens with value 0.
+    input_ids = pad_sequences(input_ids, maxlen=MAX_LEN, dtype="long",
+                            value=0, truncating="post", padding="post")
+
+    # Create attention masks
+    attention_masks = []
+
+    # For each sentence...
+    for sent in input_ids:
+        # Create the attention mask.
+        #   - If a token ID is 0, then it's padding, set the mask to 0.
+        #   - If a token ID is > 0, then it's a real token, set the mask to 1.
+        att_mask = [int(token_id > 0) for token_id in sent]
+
+        # Store the attention mask for this sentence.
+        attention_masks.append(att_mask)
+
+    # Use 90% for training and 10% for validation.
+    train_inputs, validation_inputs, train_labels, validation_labels = train_test_split(input_ids, labels,
+                                                                random_state=2018, test_size=0.1)
+    # Do the same for the masks.
+    train_masks, validation_masks, _, _ = train_test_split(attention_masks, labels,
+                                                random_state=2018, test_size=0.1)
+
+    # Convert all inputs and labels into torch tensors, the required datatype
+    # for our model.
+    train_inputs = torch.tensor(train_inputs)
+    validation_inputs = torch.tensor(validation_inputs)
+
+    train_labels = torch.tensor(train_labels)
+    validation_labels = torch.tensor(validation_labels)
+
+    train_masks = torch.tensor(train_masks)
+    validation_masks = torch.tensor(validation_masks)
+
+    # The DataLoader needs to know our batch size for training, so we specify it
+    batch_size = 32
+
+    # Create the DataLoader for our training set.
+    train_data = TensorDataset(train_inputs, train_masks, train_labels)
+    train_sampler = RandomSampler(train_data)
+    train_dataloader = DataLoader(train_data, sampler=train_sampler, batch_size=batch_size)
+
+    # Create the DataLoader for our validation set.
+    validation_data = TensorDataset(validation_inputs, validation_masks, validation_labels)
+    validation_sampler = SequentialSampler(validation_data)
+    validation_dataloader = DataLoader(validation_data, sampler=validation_sampler, batch_size=batch_size)
+
+    return train_dataloader, validation_dataloader
+
+# Function to calculate the accuracy of our predictions vs labels
+def flat_accuracy(preds, labels):
+    pred_flat = np.argmax(preds, axis=1).flatten()
+    labels_flat = labels.flatten()
+    return np.sum(pred_flat == labels_flat) / len(labels_flat)
+
+def format_time(elapsed):
+    '''Takes a time in seconds and returns a string hh:mm:ss'''
+    # Round to the nearest second.
+    elapsed_rounded = int(round((elapsed)))
+
+    # Format as hh:mm:ss
+    return str(datetime.timedelta(seconds=elapsed_rounded))
+
+def main():
+    # 1. Basic setup
+    parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
+    parser.add_argument('--pytorch-only', action='store_true', default=False,
+                        help='disables ONNX Runtime training')
+    parser.add_argument('--view-graphs', action='store_true', default=False,
+                        help='views forward and backward graphs')
+    parser.add_argument('--no-cuda', action='store_true', default=False,
+                        help='disables CUDA training')
+    parser.add_argument('--epochs', type=int, default=4, metavar='N',
+                        help='number of epochs to train (default: 4)')
+    parser.add_argument('--seed', type=int, default=42, metavar='S',
+                        help='random seed (default: 42)')
+    parser.add_argument('--log-interval', type=int, default=40, metavar='N',
+                        help='how many batches to wait before logging training status (default: 40)')
+    parser.add_argument('--train-steps', type=int, default=-1, metavar='N',
+                        help='number of steps to train. Set -1 to run through whole dataset (default: -1)')
+    args = parser.parse_args()
+
+    # Device (CPU vs CUDA)
+    if torch.cuda.is_available() and not args.no_cuda:
+        device = torch.device("cuda")
+        print('There are %d GPU(s) available.' % torch.cuda.device_count())
+        print('We will use the GPU:', torch.cuda.get_device_name(0))
+    else:
+        print('No GPU available, using the CPU instead.')
+        device = torch.device("cpu")
+
+    # 2. Dataloader
+    train_dataloader, validation_dataloader = load_dataset()
+
+    # 3. Modeling
+    # Load BertForSequenceClassification, the pretrained BERT model with a single
+    # linear classification layer on top.
+    model = BertForSequenceClassification.from_pretrained(
+        "bert-base-uncased", # Use the 12-layer BERT model, with an uncased vocab.
+        num_labels = 2, # The number of output labels--2 for binary classification.
+        output_attentions = False, # Whether the model returns attentions weights.
+        output_hidden_states = False, # Whether the model returns all hidden-states.
+    )
+
+    if not args.pytorch_only:
+        model = ORTModule(model)
+
+    # TODO: change it to False to stop saving ONNX models
+    model._save_onnx = True
+    model._save_onnx_prefix = 'BertForSequenceClassification'
+
+    # Tell pytorch to run this model on the GPU.
+    if torch.cuda.is_available() and not args.no_cuda:
+        model.cuda()
+
+    # Note: AdamW is a class from the huggingface library (as opposed to pytorch)
+    optimizer = AdamW(model.parameters(),
+                    lr = 2e-5, # args.learning_rate - default is 5e-5, our notebook had 2e-5
+                    eps = 1e-8 # args.adam_epsilon  - default is 1e-8.
+                    )
+
+    # Authors recommend between 2 and 4 epochs
+    # Total number of training steps is number of batches * number of epochs.
+    total_steps = len(train_dataloader) * args.epochs
+
+    # Create the learning rate scheduler.
+    scheduler = get_linear_schedule_with_warmup(optimizer,
+                                                num_warmup_steps = 0, # Default value in run_glue.py
+                                                num_training_steps = total_steps)
+    # Seed
+    random.seed(args.seed)
+    np.random.seed(args.seed)
+    torch.manual_seed(args.seed)
+    onnxruntime.set_seed(args.seed)
+    if torch.cuda.is_available() and not args.no_cuda:
+        torch.cuda.manual_seed_all(args.seed)
+
+    # 4. Train loop (fine-tune)
+    for epoch_i in range(0, args.epochs):
+        train(model, optimizer, scheduler, train_dataloader, epoch_i, device, args)
+        test(model, validation_dataloader, device)
+
+if __name__ == '__main__':
+    main()
