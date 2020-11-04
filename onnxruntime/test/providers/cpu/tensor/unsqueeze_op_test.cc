9c47e5e62931aca1908d22040f02e8f6de8db6ff
hariharans29 <shariharan91@gmail.com>
2020-11-04 11:11:27 -0800

    fix TensorRT build

--- onnxruntime/test/providers/cpu/tensor/unsqueeze_op_test.cc
+++ onnxruntime/test/providers/cpu/tensor/unsqueeze_op_test.cc
@@ -8,9 +8,9 @@ namespace onnxruntime {
 namespace test {
 
 // Disable TensorRT on the tests because of SegFault errors in the parser
-// DIsable OpenVINO for opset-13 because it expects axes to be an attribute
+
 TEST(TensorOpTest, Unsqueeze_1) {
-  OpTester test("Unsqueeze", 12);
+  OpTester test("Unsqueeze");
 
   test.AddAttribute("axes", std::vector<int64_t>{1});
   test.AddInput<float>("input", {2, 3, 4}, std::vector<float>(2 * 3 * 4, 1.0f));
@@ -19,7 +19,7 @@ TEST(TensorOpTest, Unsqueeze_1) {
 }
 
 TEST(TensorOpTest, Unsqueeze_1_int32) {
-  OpTester test("Unsqueeze", 12);
+  OpTester test("Unsqueeze");
 
   test.AddAttribute("axes", std::vector<int64_t>{1});
   test.AddInput<int32_t>("input", {2, 3, 4}, std::vector<int32_t>(2 * 3 * 4, 1));
@@ -28,7 +28,7 @@ TEST(TensorOpTest, Unsqueeze_1_int32) {
 }
 
 TEST(TensorOpTest, Unsqueeze_2) {
-  OpTester test("Unsqueeze", 12);
+  OpTester test("Unsqueeze");
 
   test.AddAttribute("axes", std::vector<int64_t>{0, 4});
   test.AddInput<float>("input", {2, 3, 4}, std::vector<float>(2 * 3 * 4, 1.0f));
@@ -37,7 +37,7 @@ TEST(TensorOpTest, Unsqueeze_2) {
 }
 
 TEST(TensorOpTest, Unsqueeze_3) {
-  OpTester test("Unsqueeze", 12);
+  OpTester test("Unsqueeze");
 
   test.AddAttribute("axes", std::vector<int64_t>{2, 1, 0});
   test.AddInput<float>("input", {2, 3, 4}, std::vector<float>(2 * 3 * 4, 1.0f));
@@ -57,7 +57,7 @@ TEST(TensorOpTest, Unsqueeze_Duplicate) {
              {kTensorrtExecutionProvider});  //TensorRT failed
   }
   {
-    OpTester test("Unsqueeze", 13);  // use latest opset with axes input
+    OpTester test("Unsqueeze", -1);  // use latest opset with axis input
 
     test.AddInput<float>("input", {2, 3, 4}, std::vector<float>(2 * 3 * 4, 1.0f));
     test.AddInput<int64_t>("axes", {4}, std::vector<int64_t>{2, 1, 0, 2}, true);  //set as initializer to enable shape inference
@@ -78,13 +78,14 @@ TEST(TensorOpTest, Unsqueeze_OutOfRange) {
              "[ShapeInferenceError] values in 'axes' are beyond the bounds of the computed output shape");
   }
   {
-    OpTester test("Unsqueeze", 13);  // use latest opset with axes input
+    OpTester test("Unsqueeze", -1);  // use latest opset with axis input
     test.AddInput<float>("input", {2, 3, 4}, std::vector<float>(2 * 3 * 4, 1.0f));
     test.AddInput<int64_t>("axes", {1}, std::vector<int64_t>{4}, true);  //set as initializer to enable shape inference
     test.AddOutput<float>("output", {2, 1, 3, 4}, std::vector<float>(2 * 3 * 4, 1.0f));
     // nGraph and TensorRT does not support negative axis.
     test.Run(OpTester::ExpectResult::kExpectFailure,
-             "[ShapeInferenceError] values in 'axes' are beyond the bounds of the computed output shape", {kOpenVINOExecutionProvider});
+             "[ShapeInferenceError] values in 'axes' are beyond the bounds of the computed output shape",
+             {kTensorrtExecutionProvider});  //TensorRT expects 'axes' attribute
   }
 }
 
@@ -95,34 +96,37 @@ TEST(TensorOpTest, UnsqueezeNegAxis_3) {
     test.AddInput<float>("input", {2, 3, 4}, std::vector<float>(2 * 3 * 4, 1.0f));
     test.AddOutput<float>("output", {1, 1, 1, 2, 3, 4}, std::vector<float>(2 * 3 * 4, 1.0f));
     // nGraph and TensorRT does not support negative axis.
-    test.Run(OpTester::ExpectResult::kExpectSuccess, "", {kNGraphExecutionProvider, kTensorrtExecutionProvider, kOpenVINOExecutionProvider});
+    test.Run(OpTester::ExpectResult::kExpectSuccess, "", {kNGraphExecutionProvider, kTensorrtExecutionProvider});
   }
   {
-    OpTester test("Unsqueeze", 13);  // use latest opset with axes input
+    OpTester test("Unsqueeze", 13);  // use latest opset with axis input
     test.AddInput<float>("input", {2, 3, 4}, std::vector<float>(2 * 3 * 4, 1.0f));
     test.AddInput<int64_t>("axes", {3}, std::vector<int64_t>{-4, 1, -6});
     test.AddOutput<float>("output", {1, 1, 1, 2, 3, 4}, std::vector<float>(2 * 3 * 4, 1.0f));
     // nGraph and TensorRT does not support negative axis.
+    // TODO: TensorRT, OpenVINO dont support "axes" input in opset 13, re-enable after
     test.Run(OpTester::ExpectResult::kExpectSuccess, "", {kNGraphExecutionProvider, kTensorrtExecutionProvider, kOpenVINOExecutionProvider});
   }
 }
 
 TEST(TensorOpTest, Unsqueeze_1_int32_axes_input) {
-  OpTester test("Unsqueeze", 13);  // use latest opset with axes input
+  OpTester test("Unsqueeze", 13);
 
   test.AddInput<int32_t>("input", {2, 3, 4}, std::vector<int32_t>(2 * 3 * 4, 1));
   test.AddInput<int64_t>("axes", {1}, std::vector<int64_t>{1});
   test.AddOutput<int32_t>("output", {2, 1, 3, 4}, std::vector<int32_t>(2 * 3 * 4, 1));
+  // TODO: TensorRT and OpenVINO dont support "axes" input in opset 13, re-enable after
   test.Run(OpTester::ExpectResult::kExpectSuccess, "", {kTensorrtExecutionProvider, kOpenVINOExecutionProvider});
 }
 
 TEST(TensorOpTest, Unsqueeze_3_axes_input) {
-  OpTester test("Unsqueeze", 13);  // use latest opset with axes input
+  OpTester test("Unsqueeze", 13);
 
   test.AddInput<float>("input", {2, 3, 4}, std::vector<float>(2 * 3 * 4, 1.0f));
   test.AddInput<int64_t>("axes", {3}, std::vector<int64_t>{2, 1, 0});
   test.AddOutput<float>("output", {1, 1, 1, 2, 3, 4}, std::vector<float>(2 * 3 * 4, 1.0f));
-  test.Run(OpTester::ExpectResult::kExpectSuccess, "", {kOpenVINOExecutionProvider});
+  // TODO: TensorRT and OpenVINO dont support "axes" input in opset 13, re-enable after
+  test.Run(OpTester::ExpectResult::kExpectSuccess, "", {kTensorrtExecutionProvider, kOpenVINOExecutionProvider});
 }
 
 }  // namespace test
