4393fc101e7ec5da925a9656a53884d390bdee7b
Sherlock Huang <bahuang@OrtTrainingDev3.af05slrtruoetgaxwwjv5nsq5e.px.internal.cloudapp.net>
2020-11-06 08:36:34 +0000

    Introduce PassThrough op  to wait for all gradient ready before weight update

--- orttraining/orttraining/core/graph/optimizer_graph_builder.cc
+++ orttraining/orttraining/core/graph/optimizer_graph_builder.cc
@@ -79,6 +79,29 @@ ArgDef BuildGroupNode(const std::string& group_output_name,
   return group_output;
 }
 
+Status OptimizerGraphBuilder::AddGradientPassThroughNode(
+    const NodeArgNameGeneratorFn& nodearg_name_generator,
+    std::vector<ArgDef>& gradient_argdefs,  // update argdefs in place
+    GraphAugmenter::GraphDefs& graph_defs) {
+  std::vector<ArgDef> outputs_gradient_argdefs;
+  for (size_t i = 0; i < gradient_argdefs.size(); ++i) {
+    ArgDef& gradient_argdef = gradient_argdefs[i];
+    TypeProto* output_gradient_type_proto = graph_defs.CopyTypeProto(gradient_argdef);
+    outputs_gradient_argdefs.emplace_back(nodearg_name_generator(gradient_argdef.name + "_passthrough"),
+                                          output_gradient_type_proto);
+  }
+
+  graph_defs.AddNodeDefs({NodeDef(OpDef{"PassThrough", kMSDomain, 1},
+                                  gradient_argdefs,
+                                  outputs_gradient_argdefs,
+                                  NodeAttributes(),
+                                  nodearg_name_generator("GradientPassThrought"))});
+
+  gradient_argdefs = outputs_gradient_argdefs;
+
+  return Status::OK();
+}
+
 Status OptimizerGraphBuilder::AddGradientScalingNodes(
     const NodeArgNameGeneratorFn& nodearg_name_generator,
     const float scale,
@@ -132,13 +155,13 @@ Status OptimizerGraphBuilder::AddGradientScalingNodes(
 Status OptimizerGraphBuilder::AddGradientScalingNodes(
     const NodeArgNameGeneratorFn& nodearg_name_generator,
     const float scale,
-    std::vector<ArgDef>& input_gradient_argdefs,       // update argdefs in place
+    std::vector<ArgDef>& input_gradient_argdefs,  // update argdefs in place
     std::vector<ArgDef>& output_gradient_argdef,  // update argdef in place
     GraphAugmenter::GraphDefs& graph_defs,
     ONNX_NAMESPACE::TensorProto_DataType target_type) {
   ArgDef pre_allreduce_scale(nodearg_name_generator("pre_allreduce_scale"),
                              graph_defs.CreateTypeProto({}, ONNX_NAMESPACE::TensorProto_DataType_FLOAT));
-                             
+
   graph_defs.AddInitializers({CreateTensorProto<float>(pre_allreduce_scale.name, scale, {})});
 
   TypeProto* fused_gradient_type_proto = graph_defs.CreateTypeProto();
@@ -149,7 +172,7 @@ Status OptimizerGraphBuilder::AddGradientScalingNodes(
   for (size_t i = 0; i < input_gradient_argdefs.size(); ++i) {
     inputs.emplace_back(input_gradient_argdefs[i]);
   }
-  
+
   for (size_t i = 0; i < input_gradient_argdefs.size(); ++i) {
     ArgDef& gradient_argdef = input_gradient_argdefs[i];
 
@@ -453,7 +476,13 @@ Status OptimizerGraphBuilder::BuildInternal(
       ORT_RETURN_IF_ERROR(AddFiniteGradientCheck(
           nodearg_name_generator, {global_grad_norm_argdef}, graph_defs, global_grad_norm_finite_argdef));
       optimizer_graph_outputs[OptimizerOutputKey::GradientAllIsFinite] = global_grad_norm_finite_argdef.name;
+    } else if (opt_graph_config_.mixed_precision_type == MixedPrecisionDataType::BF16) {
+      // add gradient pass through node for bf16 run
+      ORT_RETURN_IF_ERROR(AddGradientPassThroughNode(nodearg_name_generator, gradient_argdefs, graph_defs));
     }
+  } else {
+    // add gradient pass through node for fp32 run
+    ORT_RETURN_IF_ERROR(AddGradientPassThroughNode(nodearg_name_generator, gradient_argdefs, graph_defs));
   }
 
   // add weight update
--- orttraining/orttraining/training_ops/cpu/controlflow/group.cc
+++ orttraining/orttraining/training_ops/cpu/controlflow/group.cc
@@ -1,7 +1,8 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-#include "group.h"
+#include "orttraining/training_ops/cpu/controlflow/group.h"
+#include "orttraining/training_ops/cpu/controlflow/common.h"
 
 namespace onnxruntime {
 namespace contrib {
@@ -23,6 +24,28 @@ ONNX_OPERATOR_KERNEL_EX(
         .TypeConstraint("B", DataTypeImpl::GetTensorType<bool>())
         .TypeConstraint("T", DataTypeImpl::AllTensorTypes()),
     Group);
+
+Status PassThrough::Compute(OpKernelContext* context) const {
+  ORT_ENFORCE(context->InputCount() <= passthrough_input_count_limit, "Number of inputs for PassThrough node exceeds the limit.");
+
+  for (int i = 0; i < context->InputCount(); ++i) {
+    const auto* X = context->Input<Tensor>(i);
+    ORT_ENFORCE(X != nullptr);
+    Tensor* Y = context->Output(i, X->Shape());
+    ORT_ENFORCE(X->DataRaw() == Y->DataRaw(), "PassThrough input and outpu are not sharing the same buffer.");
+  }
+  return Status::OK();
+}
+
+ONNX_OPERATOR_KERNEL_EX(
+    PassThrough,
+    kMSDomain,
+    1,
+    kCpuExecutionProvider,
+    KernelDefBuilder()
+        .TypeConstraint("T", DataTypeImpl::AllTensorTypes())
+        .Alias(AliasRange<0, 0>(0, passthrough_input_count_limit)),  // outputs and inputs are mapped one to one
+    PassThrough);
 
 }  // namespace contrib
 }  // namespace onnxruntime
