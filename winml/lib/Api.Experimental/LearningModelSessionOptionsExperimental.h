e0a99c22d54babdff42894246025dc13a6cc2c0a
Ori Levari <orlevari@microsoft.com>
2020-11-13 15:30:53 -0800

    add missing changes from previous commit

--- winml/lib/Api.Experimental/LearningModelSessionOptionsExperimental.h
+++ winml/lib/Api.Experimental/LearningModelSessionOptionsExperimental.h
@@ -15,10 +15,3 @@ struct LearningModelSessionOptionsExperimental : LearningModelSessionOptionsExpe
 };
 
 }  // namespace WINML_EXPERIMENTALP
-
-namespace WINML_EXPERIMENTAL::factory_implementation {
-
-struct LearningModelSessionOptionsExperimental : LearningModelSessionOptionsExperimentalT<LearningModelSessionOptionsExperimental, implementation::LearningModelSessionOptionsExperimental> {
-};
-
-}  // namespace WINML_EXPERIMENTAL::factory_implementation
