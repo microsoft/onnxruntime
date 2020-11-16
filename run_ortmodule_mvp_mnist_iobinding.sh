4122f05e2ef3c8c4c2e11af054f6419969fcdc8e
Vincent Wang <weicwang@microsoft.com>
2020-11-16 09:17:27 +0000

    add io binding

---
+++ run_ortmodule_mvp_mnist_iobinding.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+cur_dir=$(basename `pwd`)
+
+if [[ ${cur_dir} != "RelWithDebInfo" ]]
+then
+    echo "Going to build folder (aka build/Linux/RelWithDebInfo)"
+    cd build/Linux/RelWithDebInfo
+fi
+
+echo "Exporting PYTHONPATH to use build dir as onnxruntime package"
+export PYTHONPATH=$(pwd)
+
+echo "Copying PyTorch frontend source-code to build folder"
+cp -Rf ../../../orttraining/orttraining/python/training/* ../../../build/Linux/RelWithDebInfo/onnxruntime/training/
+
+echo "Running Flexible API (ORTModule)"
+python ../../../orttraining/orttraining/test/python/orttraining_test_ortmodule_iobinding.py --epochs 10 --log-interval 100 --log-level=DEBUG --use_iobinding
