7b76b57fc83363acdb02a67834e01fbe1c02443f
Scott McKay <skottmckay@gmail.com>
2020-11-17 13:52:22 +1000

    Support EPs that compile nodes in a minimal build. (#5776)
    * Support EPs that compile nodes in a minimal build. This enables NNAPI being used.

--- onnxruntime/core/providers/nnapi/nnapi_builtin/nnapi_execution_provider.h
+++ onnxruntime/core/providers/nnapi/nnapi_builtin/nnapi_execution_provider.h
@@ -19,11 +19,21 @@ class NnapiExecutionProvider : public IExecutionProvider {
   std::vector<std::unique_ptr<ComputeCapability>>
   GetCapability(const onnxruntime::GraphViewer& graph_view,
                 const std::vector<const KernelRegistry*>& /*kernel_registries*/) const override;
-  common::Status Compile(const std::vector<onnxruntime::Node*>& fused_nodes,
+
+  // we implement the Compile that takes FusedNodeAndGraph instances
+  FusionStyle GetFusionStyle() const override { return FusionStyle::FilteredGraphViewer; }
+
+#if !defined(ORT_MINIMAL_BUILD) || defined(ORT_EXTENDED_MINIMAL_BUILD)
+  common::Status Compile(const std::vector<FusedNodeAndGraph>& fused_nodes,
                          std::vector<NodeComputeInfo>& node_compute_funcs) override;
+#endif
+
   unsigned long GetNNAPIFlags() const { return nnapi_flags_; }
 
  private:
+  // unique counter to name each fused kernel across the entire model
+  mutable int metadef_id_{0};
+
   // The bit flags which define bool options for NNAPI EP, bits are defined as
   // NNAPIFlags in include/onnxruntime/core/providers/nnapi/nnapi_provider_factory.h
   const unsigned long nnapi_flags_;
