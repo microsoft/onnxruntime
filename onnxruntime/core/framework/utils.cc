0657bdfbb759705f56a5fea2d165f4856397efaf
Scott McKay <skottmckay@gmail.com>
2020-11-19 16:25:36 +1000

    Exclude some training specific code around the allocation planning and initializer handling from the minimal build. Simplify the code around tracking start/end usage of a value.

--- onnxruntime/core/framework/utils.cc
+++ onnxruntime/core/framework/utils.cc
@@ -571,6 +571,7 @@ int32_t ONNXTensorElementDataTypeToProtoTensorType(ONNXTensorElementDataType onn
   }
 }
 
+#ifdef ENABLE_TRAINING
 common::Status VerifyInputTensorsAllocatedContiguously(OpKernelContext* context) {
   const Tensor* prev_input = context->Input<Tensor>(0);
   for (int i = 1; i < context->InputCount(); i++) {
@@ -582,15 +583,18 @@ common::Status VerifyInputTensorsAllocatedContiguously(OpKernelContext* context)
     size_t input_element_size = prev_input->DataType()->Size();
     size_t input_aligned_bytes = 0;
 
-    ORT_RETURN_IF_NOT(IAllocator::CalcMemSizeForArrayWithAlignment<256>(input_element_count, input_element_size, &input_aligned_bytes));
+    ORT_RETURN_IF_NOT(IAllocator::CalcMemSizeForArrayWithAlignment<256>(input_element_count, input_element_size,
+                                                                        &input_aligned_bytes));
 
-    ORT_RETURN_IF_NOT(curr_input->DataRaw() == static_cast<const int8_t*>(prev_input->DataRaw()) + input_aligned_bytes ||
-                      curr_input->DataRaw() == static_cast<const int8_t*>(prev_input->DataRaw()) + prev_input->SizeInBytes());
+    ORT_RETURN_IF_NOT(
+        curr_input->DataRaw() == static_cast<const int8_t*>(prev_input->DataRaw()) + input_aligned_bytes ||
+        curr_input->DataRaw() == static_cast<const int8_t*>(prev_input->DataRaw()) + prev_input->SizeInBytes());
 
     prev_input = curr_input;
   }
   return Status::OK();
 }
+#endif
 
 }  // namespace utils
 }  // namespace onnxruntime
