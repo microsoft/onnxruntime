ef094e4375d259b08ad5295d56e8c0d8aca85169
Lei Zhang <zhang.huanning@hotmail.com>
2020-11-20 04:43:32 -0800

    Qlinear GlobalAveragePool result precision checker.

--- onnxruntime/test/contrib_ops/qlinear_global_average_pool_test.cc
+++ onnxruntime/test/contrib_ops/qlinear_global_average_pool_test.cc
@@ -38,7 +38,7 @@ static void CalculateGlobalAvgPoolU8(
 
 void RunQLinearGlobalAveragePoolU8(
     bool channels_last, int64_t batch, int64_t channel, int64_t h, int64_t w,
-    uint8_t x_zero_point, float x_scale, uint8_t y_zero_point, float y_scale, int32_t seed) {
+    uint8_t x_zero_point, float x_scale, uint8_t y_zero_point, float y_scale, int32_t seed = 0) {
   std::vector<int64_t> x_dims = channels_last ? std::vector<int64_t>{batch, h, w, channel} : std::vector<int64_t>{batch, channel, h, w};
   std::vector<int64_t> y_dims = channels_last ? std::vector<int64_t>{batch, 1, 1, channel} : std::vector<int64_t>{batch, channel, 1, 1};
   int64_t x_size = batch * channel * h * w;
@@ -46,7 +46,7 @@ void RunQLinearGlobalAveragePoolU8(
   std::vector<uint8_t> x_data((size_t)x_size);
   std::vector<uint8_t> y_data((size_t)y_size);
 
-  RandomValueGenerator random{ seed ? optional<RandomValueGenerator::RandomSeedType>{seed} : optional<RandomValueGenerator::RandomSeedType>{}};
+  RandomValueGenerator random{seed ? optional<RandomValueGenerator::RandomSeedType>{seed} : optional<RandomValueGenerator::RandomSeedType>{}};
   std::vector<int> tmp_x_data = random.Uniform<int32_t>(x_dims, 0, 255);
   std::transform(tmp_x_data.begin(), tmp_x_data.end(), x_data.data(), [](int32_t v) -> uint8_t {
     return static_cast<uint8_t>(v);
@@ -64,83 +64,102 @@ void RunQLinearGlobalAveragePoolU8(
   test.AddInput<uint8_t>("y_zero_point", {}, {y_zero_point});
   test.AddOutput<uint8_t>("Y", y_dims, y_data);
 
+  auto q8checker = [&](const std::vector<OrtValue>& fetches, const std::string& provider_type) {
+    const OrtValue& ort_value = fetches[0];
+    if (ort_value.Fence()) {
+      ort_value.Fence()->BeforeUsingAsInput(onnxruntime::kCpuExecutionProvider, 0);
+    }
+
+    auto y_shape = TensorShape(y_dims);
+    const Tensor& output_tensor = ort_value.Get<Tensor>();
+    ORT_ENFORCE(y_shape == output_tensor.Shape(),
+                "Expected output shape [" + y_shape.ToString() + "] did not match run output shape [" +
+                    output_tensor.Shape().ToString() + "] for Y @" + provider_type);
+    auto* output = output_tensor.Data<uint8_t>();
+    auto size = static_cast<int>(output_tensor.Shape().Size());
+    for (int i = 0; i < size; ++i) {
+      int diff = abs(y_data[i] - output[i]);
+      EXPECT_LE(diff, 1) << "i:" << i << " expected:" << y_data[i] << " " << (int)y_data[i]
+                         << ", got:" << output[i] << " " << (int)output[i] << ", provider_type: " << provider_type;
+    }
+  };
+  test.SetCustomOutputVerifier(q8checker);
+
   test.Run();
 }
 
-static int32_t seed = 1234569;
-
 TEST(QLinearGlobalAveragePool, Nchw_1x1x32x32) {
-  RunQLinearGlobalAveragePoolU8(true, 1, 1, 32, 32, 128, 1.0, 64, 2.0, seed);
+  RunQLinearGlobalAveragePoolU8(true, 1, 1, 32, 32, 128, 1.0, 64, 2.0);
 }
 
 TEST(QLinearGlobalAveragePool, Nhwc_1x32x32x1) {
-  RunQLinearGlobalAveragePoolU8(false, 1, 1, 32, 32, 128, 1.0, 64, 2.0, seed);
+  RunQLinearGlobalAveragePoolU8(false, 1, 1, 32, 32, 128, 1.0, 64, 2.0);
 }
 
 TEST(QLinearGlobalAveragePool, Nchw_1x256x8x8) {
-  RunQLinearGlobalAveragePoolU8(true, 1, 256, 8, 8, 128, 1.0, 64, 3.0, 89);
+  RunQLinearGlobalAveragePoolU8(true, 1, 256, 8, 8, 128, 1.0, 64, 3.0);
 }
 
 TEST(QLinearGlobalAveragePool, Nhwc_1x8x8x256) {
-  RunQLinearGlobalAveragePoolU8(false, 1, 256, 8, 8, 128, 1.0, 64, 3.0, seed);
+  RunQLinearGlobalAveragePoolU8(false, 1, 256, 8, 8, 128, 1.0, 64, 3.0);
 }
 
 TEST(QLinearGlobalAveragePool, Nchw_1x255x7x7) {
-  RunQLinearGlobalAveragePoolU8(true, 1, 255, 7, 7, 128, 7.0, 128, 21.0, seed);
+  RunQLinearGlobalAveragePoolU8(true, 1, 255, 7, 7, 128, 7.0, 128, 21.0);
 }
 
 TEST(QLinearGlobalAveragePool, Nhwc_1x7x7x255) {
-  RunQLinearGlobalAveragePoolU8(false, 1, 255, 7, 7, 128, 7.0, 128, 21.0, seed);
+  RunQLinearGlobalAveragePoolU8(false, 1, 255, 7, 7, 128, 7.0, 128, 21.0);
 }
 
 TEST(QLinearGlobalAveragePool, Nchw_1x255x8x8) {
-  RunQLinearGlobalAveragePoolU8(true, 1, 255, 8, 8, 128, 1.0, 128, 2.0, 89);
+  RunQLinearGlobalAveragePoolU8(true, 1, 255, 8, 8, 128, 1.0, 128, 2.0);
 }
 
 TEST(QLinearGlobalAveragePool, Nhwc_1x8x8x255) {
-  RunQLinearGlobalAveragePoolU8(false, 1, 255, 8, 8, 128, 1.0, 128, 2.0, seed);
+  RunQLinearGlobalAveragePoolU8(false, 1, 255, 8, 8, 128, 1.0, 128, 2.0);
 }
 
 TEST(QLinearGlobalAveragePool, Nchw_1x256x7x7) {
-  RunQLinearGlobalAveragePoolU8(true, 1, 256, 7, 7, 128, 7.0, 128, 21.0, seed);
+  RunQLinearGlobalAveragePoolU8(true, 1, 256, 7, 7, 128, 7.0, 128, 21.0);
 }
 
 TEST(QLinearGlobalAveragePool, Nhwc_1x7x7x256) {
-  RunQLinearGlobalAveragePoolU8(false, 1, 256, 7, 7, 128, 7.0, 128, 21.0, seed);
+  RunQLinearGlobalAveragePoolU8(false, 1, 256, 7, 7, 128, 7.0, 128, 21.0);
 }
 
 // tests for BatchSize > 1
 TEST(QLinearGlobalAveragePool, Nchw_3x256x8x8) {
-  RunQLinearGlobalAveragePoolU8(true, 3, 256, 8, 8, 128, 1.0, 64, 3.0, 99);
+  RunQLinearGlobalAveragePoolU8(true, 3, 256, 8, 8, 128, 1.0, 64, 3.0);
 }
 
 TEST(QLinearGlobalAveragePool, Nhwc_3x8x8x256) {
-  RunQLinearGlobalAveragePoolU8(false, 3, 256, 8, 8, 128, 1.0, 64, 3.0, seed);
+  RunQLinearGlobalAveragePoolU8(false, 3, 256, 8, 8, 128, 1.0, 64, 3.0);
 }
 
 TEST(QLinearGlobalAveragePool, Nchw_3x255x7x7) {
-  RunQLinearGlobalAveragePoolU8(true, 3, 255, 7, 7, 128, 7.0, 128, 21.0, seed);
+  RunQLinearGlobalAveragePoolU8(true, 3, 255, 7, 7, 128, 7.0, 128, 21.0);
 }
 
 TEST(QLinearGlobalAveragePool, Nhwc_3x7x7x255) {
-  RunQLinearGlobalAveragePoolU8(false, 3, 255, 7, 7, 128, 7.0, 128, 21.0, seed);
+  RunQLinearGlobalAveragePoolU8(false, 3, 255, 7, 7, 128, 7.0, 128, 21.0);
 }
 
 TEST(QLinearGlobalAveragePool, Nchw_3x255x8x8) {
-  RunQLinearGlobalAveragePoolU8(true, 3, 255, 8, 8, 128, 1.0, 128, 2.0, 89);
+  RunQLinearGlobalAveragePoolU8(true, 3, 255, 8, 8, 128, 1.0, 128, 2.0);
 }
 
 TEST(QLinearGlobalAveragePool, Nhwc_3x8x8x255) {
-  RunQLinearGlobalAveragePoolU8(false, 3, 255, 8, 8, 128, 1.0, 128, 2.0, 686);
+  RunQLinearGlobalAveragePoolU8(false, 3, 255, 8, 8, 128, 1.0, 128, 2.0);
 }
 
 TEST(QLinearGlobalAveragePool, Nchw_3x256x7x7) {
-  RunQLinearGlobalAveragePoolU8(true, 3, 256, 7, 7, 128, 7.0, 128, 21.0, seed);
+  RunQLinearGlobalAveragePoolU8(true, 3, 256, 7, 7, 128, 7.0, 128, 21.0);
 }
 
 TEST(QLinearGlobalAveragePool, Nhwc_3x7x7x256) {
-  RunQLinearGlobalAveragePoolU8(false, 3, 256, 7, 7, 128, 7.0, 128, 21.0, seed);
+  RunQLinearGlobalAveragePoolU8(false, 3, 256, 7, 7, 128, 7.0, 128, 21.0);
 }
 
-}
-}
+}  // namespace test
+}  // namespace onnxruntime
