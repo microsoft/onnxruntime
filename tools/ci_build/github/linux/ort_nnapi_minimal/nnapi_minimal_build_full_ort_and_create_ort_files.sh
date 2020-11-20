7b95fc434261fdb7b5c332042db80d132377bf60
gwang <wanggy@outlook.com>
2020-11-19 20:24:20 -0800

    update

--- tools/ci_build/github/linux/ort_nnapi_minimal/nnapi_minimal_build_full_ort_and_create_ort_files.sh
+++ tools/ci_build/github/linux/ort_nnapi_minimal/nnapi_minimal_build_full_ort_and_create_ort_files.sh
@@ -9,13 +9,13 @@ set -e
 python3 -m pip install --user numpy
 
 ORT_ROOT=$1
-BUILD_DIR=$ORT_ROOT/full_build
+FULL_BUILD_DIR=$ORT_ROOT/full_build
 
 # Run a full build of ORT
 # Since we need the ORT python package to generate the ORT format files and the include ops config files
 # Will not run tests since those are covered by other CIs
 python3 $ORT_ROOT/tools/ci_build/build.py \
-    --build_dir $BUILD_DIR \
+    --build_dir $FULL_BUILD_DIR \
     --config Debug \
     --skip_submodule_sync \
     --parallel \
@@ -26,13 +26,13 @@ python3 $ORT_ROOT/tools/ci_build/build.py \
     --use_nnapi
 
 # Install the ORT python wheel
-python3 -m pip install --user $BUILD_DIR/Debug/dist/*
+pip3 install -U $FULL_BUILD_DIR/Debug/dist/*
 
 # Copy the test data to a separated folder
 cp -Rf $ORT_ROOT/onnxruntime/test/testdata/ort_minimal_e2e_test_data $TMPDIR/.test_data
 
 # Convert onnx file to ort requires onnx
-python3 -m pip install --user onnx
+pip3 install -U onnx
 
 # Convert all the onnx models in the $HOME/.test_data/ort_minimal_e2e_test_data to ort model
 # and generate the included ops config file as $HOME/.test_data/ort_minimal_e2e_test_data/required_operators.config
@@ -40,4 +40,25 @@ python3 $ORT_ROOT/tools/python/convert_onnx_models_to_ort.py --use_nnapi \
     $ORT_ROOT/.test_data/ort_minimal_e2e_test_data
 
 # Uninstall the ORT python wheel
-python3 -m pip uninstall -y onnxruntime_noopenmp
+pip3 uninstall -y onnxruntime_noopenmp
+
+MIN_BUILD_DIR=$ORT_ROOT/min_build
+
+# build minimal package for Android x86_64 Emulator
+# Since this is a minimal build with reduced ops, we will only run e2e test using onnx_test_runner
+python3 $ORT_ROOT/tools/ci_build/build.py \
+    --build_dir $MIN_BUILD_DIR \
+    --config Debug \
+    --skip_submodule_sync \
+    --parallel \
+    --cmake_generator=Ninja \
+    --skip_tests \
+    --use_nnapi \
+    --android \
+    --android_sdk_path $ANDROID_HOME \
+    --android_ndk_path $ANDROID_HOME/ndk-bundle \
+    --android_abi=x86_64 \
+    --android_api=29 \
+    --build_java \
+    --include_ops_by_config $TMPDIR/.test_data/ort_minimal_e2e_test_data/required_operators.config
+
