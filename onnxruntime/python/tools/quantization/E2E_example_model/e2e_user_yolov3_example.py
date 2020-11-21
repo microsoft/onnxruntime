3050f48b639ead7f0872fdc97ef131a324b9f302
Chi Lo <Chi.Lo@microsoft.com>
2020-11-21 07:58:36 -0800

    minor modification

--- onnxruntime/python/tools/quantization/E2E_example_model/e2e_user_yolov3_example.py
+++ onnxruntime/python/tools/quantization/E2E_example_model/e2e_user_yolov3_example.py
@@ -23,15 +23,15 @@ def get_prediction_evaluation(model_path, augmented_model_path, validation_datas
 
 
 def get_calibration_table(model_path, augmented_model_path, calibration_dataset):
-    # data_reader = YoloV3DataReader(calibration_dataset, augmented_model_path=augmented_model_path)
-    data_reader = YoloV3VisionDataReader(calibration_dataset, augmented_model_path=augmented_model_path)
+    data_reader = YoloV3DataReader(calibration_dataset, augmented_model_path=augmented_model_path)
+    # data_reader = YoloV3VisionDataReader(calibration_dataset, augmented_model_path=augmented_model_path)
     generate_calibration_table(model_path, augmented_model_path, data_reader, calibration_dataset=calibration_dataset)
 
 
 if __name__ == '__main__':
 
     model_path = 'yolov3_new.onnx'
-    model_path = 'yolov3_merge_coco_openimage_500200_288x512_batch_nms_obj_300_score_0p35_iou_0p35_shape.onnx'
+    # model_path = 'yolov3_merge_coco_openimage_500200_288x512_batch_nms_obj_300_score_0p35_iou_0p35_shape.onnx'
     # model_path = 'yolov3_merge_coco_openimage_500200_384x608_batch_nms_obj_300_score_0p35_iou_0p35_shape.onnx'
     augmented_model_path = 'augmented_model.onnx'
     # calibration_dataset = './val2017'
@@ -39,7 +39,7 @@ if __name__ == '__main__':
     validation_dataset = './val2017'
 
     get_calibration_table(model_path, augmented_model_path, calibration_dataset)
-    # get_prediction_evaluation(model_path, augmented_model_path, validation_dataset, ["CUDAExecutionProvider"])
+    get_prediction_evaluation(model_path, augmented_model_path, validation_dataset, ["CUDAExecutionProvider"])
 
 
 
