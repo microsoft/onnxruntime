054e0c497fd1339e35368905096d8c896ecb1178
gwang <wanggy@outlook.com>
2020-11-23 13:44:51 -0800

    Remove build java from NNAPI minimal build to fix build break

--- tools/ci_build/github/linux/ort_minimal/nnapi_minimal_build_minimal_ort_and_run_tests.sh
+++ tools/ci_build/github/linux/ort_minimal/nnapi_minimal_build_minimal_ort_and_run_tests.sh
@@ -39,7 +39,6 @@ python3 $ORT_ROOT/tools/ci_build/build.py \
     --android_ndk_path $ANDROID_HOME/ndk-bundle \
     --android_abi=x86_64 \
     --android_api=29 \
-    --build_java \
     --minimal_build extended \
     --disable_rtti \
     --disable_ml_ops \
