7c1bf4ec74dcb1af0f13e95d134374aac6eb87fd
gwang <wanggy@outlook.com>
2020-11-22 22:00:59 -0800

    Address CR comment

--- tools/ci_build/github/linux/ort_minimal/nnapi_minimal_build_minimal_ort_and_run_tests.sh
+++ tools/ci_build/github/linux/ort_minimal/nnapi_minimal_build_minimal_ort_and_run_tests.sh
@@ -3,30 +3,32 @@
 # This script will run a full ORT build and use the python package built to generate ort format test files,
 # and the exclude ops config file, which will be used in the build_minimal_ort_and_run_tests.sh
 
+# This script takes one command line argument, the root of the current Onnx Runtime project
+
 set -e
 set -x
 
-ORT_ROOT=$1
+# Build with reduced ops requires setuptools wheel onnx
+python3 -m pip install -U --user setuptools wheel onnx
 
-MIN_BUILD_DIR=$ORT_ROOT/min_build
+ORT_ROOT=$1
+MIN_BUILD_DIR=$ORT_ROOT/build_nnapi_minimal
 
 # Copy all the models containing the required ops to pass the UT
 mkdir -p $TMPDIR/.test_data/models_to_include
 cp $ORT_ROOT/onnxruntime/test/testdata/ort_github_issue_4031.onnx $TMPDIR/.test_data/models_to_include
 cp $ORT_ROOT/onnxruntime/test/testdata/mnist.onnx $TMPDIR/.test_data/models_to_include
-cp $ORT_ROOT/onnxruntime/test/testdata/mnist.level1_opt.onnx $TMPDIR/.test_data/models_to_include
 cp $ORT_ROOT/onnxruntime/test/testdata/ort_minimal_test_models/*.onnx $TMPDIR/.test_data/models_to_include
 
 # Build minimal package for Android x86_64 Emulator
 # No test will be triggered in the build process
-# UT and e2e tests are triggered separately below
+# UT will be triggered separately below
 python3 $ORT_ROOT/tools/ci_build/build.py \
     --build_dir $MIN_BUILD_DIR \
     --config Debug \
     --skip_submodule_sync \
     --parallel \
     --cmake_generator=Ninja \
-    --skip_tests \
     --use_nnapi \
     --android \
     --android_sdk_path $ANDROID_HOME \
@@ -39,17 +41,11 @@ python3 $ORT_ROOT/tools/ci_build/build.py \
     --disable_ml_ops \
     --disable_exceptions \
     --include_ops_by_model $TMPDIR/.test_data/models_to_include/ \
-    --include_ops_by_config $TMPDIR/.test_data/ort_minimal_e2e_test_data/required_operators.config
+    --include_ops_by_config $ORT_ROOT/onnxruntime/test/testdata/reduced_ops_via_config.config \
+    --skip_tests
 
 # Start the Android Emulator
 /bin/bash $ORT_ROOT/tools/ci_build/github/android/start_android_emulator.sh
-
-# Push onnx_test_runner and test models/data to emulator
-adb push $MIN_BUILD_DIR/Debug/onnx_test_runner /data/local/tmp/
-adb push $TMPDIR/.test_data/ort_minimal_e2e_test_data /data/local/tmp/
-
-# Perform the e2e tests
-adb shell 'cd /data/local/tmp/ && ./onnx_test_runner -e nnapi ./ort_minimal_e2e_test_data'
 
 # Push onnxruntime_test_all and testdata to emulator
 adb push $MIN_BUILD_DIR/Debug/onnxruntime_test_all /data/local/tmp/
