d5b0cbe89040f77e28f2cbc39351bb54840b4f22
gwang <wanggy@outlook.com>
2020-11-25 16:19:22 -0800

    Add a couple license headers

--- onnxruntime/core/providers/nnapi/nnapi_builtin/builders/shaper.cc
+++ onnxruntime/core/providers/nnapi/nnapi_builtin/builders/shaper.cc
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 #include "core/providers/common.h"
 #include "core/providers/nnapi/nnapi_builtin/nnapi_lib/NeuralNetworksWrapper.h"
 
