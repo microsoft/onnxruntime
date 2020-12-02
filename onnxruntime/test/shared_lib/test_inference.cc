485a8ea7a4ca72d401968e253baf0cc5ffa7048a
Randy Shuai <rashuai@microsoft.com>
2020-12-02 10:57:35 -0800

    check type

--- onnxruntime/test/shared_lib/test_inference.cc
+++ onnxruntime/test/shared_lib/test_inference.cc
@@ -233,26 +233,29 @@ struct MyCustomKernel {
     // Setup inputs
     const OrtValue* input_X = ort_.KernelContext_GetInput(context, 0);
     const OrtValue* input_Y = ort_.KernelContext_GetInput(context, 1);
-    const float* X = ort_.GetTensorData<float>(input_X);
-    const float* Y = ort_.GetTensorData<float>(input_Y);
+    OrtTensorTypeAndShapeInfo* input_X_info = ort_.GetTensorTypeAndShape(input_X);
+    if (ort_.GetTensorElementType(input_X_info) == ONNX_TENSOR_ELEMENT_DATA_TYPE_FLOAT) {
+        const float* X = ort_.GetTensorData<float>(input_X);
+        const float* Y = ort_.GetTensorData<float>(input_Y);
 
-    // Setup output
-    OrtTensorDimensions dimensions(ort_, input_X);
-    OrtValue* output = ort_.KernelContext_GetOutput(context, 0, dimensions.data(), dimensions.size());
-    float* out = ort_.GetTensorMutableData<float>(output);
+        // Setup output
+        OrtTensorDimensions dimensions(ort_, input_X);
+        OrtValue* output = ort_.KernelContext_GetOutput(context, 0, dimensions.data(), dimensions.size());
+        float* out = ort_.GetTensorMutableData<float>(output);
 
-    OrtTensorTypeAndShapeInfo* output_info = ort_.GetTensorTypeAndShape(output);
-    int64_t size = ort_.GetTensorShapeElementCount(output_info);
-    ort_.ReleaseTensorTypeAndShapeInfo(output_info);
+        OrtTensorTypeAndShapeInfo* output_info = ort_.GetTensorTypeAndShape(output);
+        int64_t size = ort_.GetTensorShapeElementCount(output_info);
+        ort_.ReleaseTensorTypeAndShapeInfo(output_info);
 
-    // Do computation
-#ifdef USE_CUDA
-    cuda_add(size, out, X, Y); 
-#else
-    for (int64_t i = 0; i < size; i++) {
-      out[i] = X[i] + Y[i];
+        // Do computation
+    #ifdef USE_CUDA
+        cuda_add(size, out, X, Y); 
+    #else
+        for (int64_t i = 0; i < size; i++) {
+          out[i] = X[i] + Y[i];
+        }
+    #endif
     }
-#endif
   }
 
  private:
