f7d671a388d3939a0e42f40c64071426d9be85a9
Changming Sun <chasun@microsoft.com>
2020-12-01 16:53:58 -0800

    Revert some pipeline changes in #5874

--- csharp/test/Microsoft.ML.OnnxRuntime.EndToEndTests/runtest-docker-gpu.sh
+++ csharp/test/Microsoft.ML.OnnxRuntime.EndToEndTests/runtest-docker-gpu.sh
@@ -27,6 +27,6 @@ docker run --gpus all --rm \
         -e "PackageName=$PackageName" \
         -e "RunTestCsharp=$RunTestCsharp" \
         -e "RunTestNative=$RunTestNative" \
-        onnxruntimebuildcache.azurecr.io/internal/azureml/onnxruntimegpubuild:ch4 \
+        onnxruntimebuildcache.azurecr.io/internal/azureml/onnxruntimecentosgpubuild:cht \
         /bin/bash /onnxruntime_src/csharp/test/Microsoft.ML.OnnxRuntime.EndToEndTests/runtest.sh \
         /home/onnxruntimedev/$NUGET_REPO_DIRNAME /onnxruntime_src /home/onnxruntimedev $CurrentOnnxRuntimeVersion
