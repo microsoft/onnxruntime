5b5bd488b75d6088594046283468f44707f7ba94
Sergii Dymchenko <sedymche@microsoft.com>
2020-12-04 15:52:58 -0800

    More logs.

--- include/onnxruntime/core/graph/graph.h
+++ include/onnxruntime/core/graph/graph.h
@@ -753,8 +753,10 @@ class Graph {
   NodeArg& GetOrCreateNodeArg(const std::string& name, const ONNX_NAMESPACE::TypeProto* p_arg_type) {
     auto iter = node_args_.find(name);
     if (iter != node_args_.end()) {
+      std::cout << "Found existing node" << std::endl;
       return *(iter->second);
     }
+    std::cout << "Creating new node" << std::endl;
     auto result = node_args_.insert(std::make_pair(name, onnxruntime::make_unique<NodeArg>(name, p_arg_type)));
     return *(result.first->second);
   }
