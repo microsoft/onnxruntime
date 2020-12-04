af3dc87121d2c262a981adadc075b26ea1810eb1
Changming Sun <chasun@microsoft.com>
2020-12-04 10:40:09 -0800

    Revert python 3.8 changes

--- cgmanifests/submodules/generate_submodule_cgmanifest.py
+++ cgmanifests/submodules/generate_submodule_cgmanifest.py
@@ -4,53 +4,9 @@ import json
 import os
 import subprocess
 import sys
-import re
 
 SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
 REPO_DIR = os.path.normpath(os.path.join(SCRIPT_DIR, "..", ".."))
-
-package_name = None
-package_filename = None
-package_url = None
-
-registrations = []
-
-with open(os.path.join(REPO_DIR,'tools','ci_build','github','linux','docker','manylinux2014_build_scripts','build_env.sh'), "r") as f:
-  for line in f:
-    if not line.strip():      
-      package_name = None
-      package_filename = None
-      package_url = None
-    if package_filename is None:
-      m = re.match("(.+?)_ROOT=(.*)$",line)
-      if m != None:
-        package_name = m.group(1)
-        package_filename = m.group(2)
-      else:
-        m = re.match("(.+?)_AUTOCONF_VERSION=(.*)$",line)
-        if m != None:
-          package_name = m.group(1)
-          package_filename = m.group(2)        
-    elif package_url is None:
-      m = re.match("(.+?)_DOWNLOAD_URL=(.+)$",line)
-      if m != None:
-        package_url = m.group(2) + "/" + package_filename + ".tar.gz"
-        registration = {
-          "Component": {
-            "Type": "other",
-            "other": {
-                "Name": package_name.lower(),
-                "Version": package_filename.split("-")[-1],
-                "DownloadUrl": package_url,
-            },
-            "comments": "manylinux dependency"
-          }
-        }
-        registrations.append(registration)
-        package_name = None
-        package_filename = None
-        package_url = None
-
 
 proc = subprocess.run(
     ["git", "submodule", "foreach", "--quiet", "--recursive", "{} {} $toplevel/$sm_path".format(
@@ -61,7 +17,7 @@ proc = subprocess.run(
     stderr=subprocess.PIPE,
     universal_newlines=True)
 
-
+registrations = []
 submodule_lines = proc.stdout.splitlines()
 for submodule_line in submodule_lines:
     (absolute_path, url, commit) = submodule_line.split(" ")
