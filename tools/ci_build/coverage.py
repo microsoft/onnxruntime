3872fd85ec4f8e879fe61f3b669c2e562a0a2e96
Satya Jandhyala <satya.k.jandhyala@gmail.com>
2020-12-05 13:36:59 -0800

    Fixed PEP8 issues.

--- tools/ci_build/coverage.py
+++ tools/ci_build/coverage.py
@@ -7,12 +7,21 @@
 #     1. The Onnxruntime build with coverage option to compile/link the source files using --coverage optoin
 #     2. The tests are run on the target emulator and *.gcda files are available on the emulator
 #
+
 import os
+import sys
 import argparse
-from build import *
+from build import run_subprocess, adb_shell
+
+SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
+REPO_DIR = os.path.normpath(os.path.join(SCRIPT_DIR, "..", ".."))
+
+sys.path.append(os.path.join(REPO_DIR, "tools", "python"))
+
 
 def adb_pull(src, dest, **kwargs):
     return run_subprocess(['adb', 'pull', src, dest], **kwargs)
+
 
 def parse_arguments():
     parser = argparse.ArgumentParser()
@@ -24,21 +33,25 @@ def parse_arguments():
         help="Configuration(s) to run code coverage.")
     return parser.parse_args()
 
+
 def start_android_emulator(args):
     run_subprocess([os.path.join(
         args.source_dir, 'tools', 'ci_build', 'github', 'android',
         'start_android_emulator.sh')])
 
+
 def main():
     args = parse_arguments()
     script_dir = os.path.realpath(os.path.dirname(__file__))
     source_dir = os.path.normpath(os.path.join(script_dir, "..", ".."))
-    cwd = os.path.abspath(get_config_build_dir(args.build_dir, args.config))
+    cwd = os.path.abspath(os.path.join(args.build_dir, args.config))
     start_android_emulator(args)
     adb_shell('cd /data/local/tmp && tar zcvf gcda_files.tar.gz *.dir', cwd=cwd)
-    adb_pull('data/local/tmp/gcda_files.tar.gz','.', cwd=cwd)
+    adb_pull('data/local/tmp/gcda_files.tar.gz', '.', cwd=cwd)
     run_subprocess("tar -zxvf gcda_files.tar.gz -C CMakeFiles".split(' '))
-    run_subprocess("gcovr -s -r {} .".format(os.path.join(source_dir, "onnxruntime")).split(' '), cwd=os.path.join(cwd, "CMakeFiles"))
+    run_subprocess("gcovr -s -r {} .".format(os.path.join(source_dir, "onnxruntime")).split(' '),
+                   cwd=os.path.join(cwd, "CMakeFiles"))
+
 
 if __name__ == "__main__":
     main()
