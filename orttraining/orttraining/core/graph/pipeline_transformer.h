de15ac841735a9fa7950744c85687d0565ab1577
Juliana Franco <jufranc@microsoft.com>
2020-12-07 16:52:39 +0000

    more doc improvements

--- orttraining/orttraining/core/graph/pipeline_transformer.h
+++ orttraining/orttraining/core/graph/pipeline_transformer.h
@@ -16,6 +16,9 @@ Status TransformGraphForPipeline(
     const std::unordered_set<std::string>& initializer_names_to_preserve,
     pipeline::PipelineTensorNames& pipeline_tensor_names);
 
+// Partitions the graph into num_stages subgraphs, as defined in op_to_stage map,
+// which maps operators to stage ids. After the graph is partitioned, it drops
+// all the tensors and operators that do not belong to the subgraph pipeline_stage_id.
 // TODO(jufranc): when adapting this code to partition training graphs, add
 // a boolean is_training as parameter.
 Status ApplyPipelinePartitionToMainGraph(Graph& graph,
@@ -28,8 +31,8 @@ Status ApplyPipelinePartitionToMainGraph(Graph& graph,
 //   - graph is the graph being partitioned into multiple pipeline stages.
 //   - id_to_stage maps string identifiers of operators and stage ids. Each
 // operator is identified with the name of any of its outputs.
-//   - op_to_stage maps pointers to operators and stage ids. This is the output
-// of this function.
+//   - op_to_stage keeps the output of this function, where op_to_stage[node_ptr]
+// is the pipeline stage ID of the pointed node.
 //   - num_stages is the total number of stages.
 Status GetDeviceAssignmentMap(Graph& graph,
     const std::map<std::string, int>& id_to_stage,
@@ -43,8 +46,8 @@ Status GetDeviceAssignmentMap(Graph& graph,
 //   - graph is the graph being partitioned into multiple pipeline stages.
 //   - cuts describes all the cut points as defined by the user (c.f., CutInfo
 // type definition.
-//   - op_to_stage maps pointers to operators and stage ids. This is the output
-// of this function.
+//   - op_to_stage keeps the output of this function, where op_to_stage[node_ptr]
+// is the pipeline stage ID of the pointed node.
 //   - num_stages is the total number of stages.
 Status GetDeviceAssignmentMap(Graph& graph,
     const std::vector<TrainingSession::TrainingConfiguration::CutInfo>& cuts,
