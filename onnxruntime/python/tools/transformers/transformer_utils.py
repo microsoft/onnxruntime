8ff88669cbf572801284589f6f0cd2a64aaba45a
Yufeng Li <liyufeng1987@gmail.com>
2020-12-10 23:52:22 -0800

    fix indent

--- onnxruntime/python/tools/transformers/transformer_utils.py
+++ onnxruntime/python/tools/transformers/transformer_utils.py
@@ -1,6 +1,8 @@
+import onnx
+import numpy as np
 class TransformerUtils:
     @staticmethod
     def make_initializer(name, data_type, dims, vals):
-    data_arry = np.asarray(vals,
-                            dtype=onnx.mapping.TENSOR_TYPE_TO_NP_TYPE[data_type])
-    return onnx.numpy_helper.from_array(data_arry.reshape(dims), name)
+        data_arry = np.asarray(vals,
+                                dtype=onnx.mapping.TENSOR_TYPE_TO_NP_TYPE[data_type])
+        return onnx.numpy_helper.from_array(data_arry.reshape(dims), name)
