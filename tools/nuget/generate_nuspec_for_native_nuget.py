9ca7539b01999b4041436a29f17d278659249e3d
Sheil Kumar <sheilk@microsoft.com>
2020-12-10 17:15:37 -0800

    disable c# builds for non-x64 builds

--- tools/nuget/generate_nuspec_for_native_nuget.py
+++ tools/nuget/generate_nuspec_for_native_nuget.py
@@ -282,14 +282,15 @@ def generate_files(list, args):
         files_list.append('<file src=' + '"' + os.path.join(args.ort_build_path, args.build_config,
                                                             'microsoft.ai.machinelearning.winmd') +
                           '" target="lib\\uap\\Microsoft.AI.MachineLearning.winmd" />')
-        interop_dll_path = 'Microsoft.AI.MachineLearning.Interop\\net5.0-windows10.0.19041.0'
-        interop_dll = interop_dll_path + '\\Microsoft.AI.MachineLearning.Interop.dll'
-        files_list.append('<file src=' + '"' + os.path.join(args.native_build_path, interop_dll) +
-                          '" target="lib\\net5.0\\Microsoft.AI.MachineLearning.Interop.dll" />')
-        interop_pdb_path = 'Microsoft.AI.MachineLearning.Interop\\net5.0-windows10.0.19041.0'
-        interop_pdb = interop_pdb_path + '\\Microsoft.AI.MachineLearning.Interop.pdb'
-        files_list.append('<file src=' + '"' + os.path.join(args.native_build_path, interop_pdb) +
-                          '" target="lib\\net5.0\\Microsoft.AI.MachineLearning.Interop.pdb" />')
+        if args.target_architecture == 'x64':
+          interop_dll_path = 'Microsoft.AI.MachineLearning.Interop\\net5.0-windows10.0.19041.0'
+          interop_dll = interop_dll_path + '\\Microsoft.AI.MachineLearning.Interop.dll'
+          files_list.append('<file src=' + '"' + os.path.join(args.native_build_path, interop_dll) +
+                            '" target="lib\\net5.0\\Microsoft.AI.MachineLearning.Interop.dll" />')
+          interop_pdb_path = 'Microsoft.AI.MachineLearning.Interop\\net5.0-windows10.0.19041.0'
+          interop_pdb = interop_pdb_path + '\\Microsoft.AI.MachineLearning.Interop.pdb'
+          files_list.append('<file src=' + '"' + os.path.join(args.native_build_path, interop_pdb) +
+                            '" target="lib\\net5.0\\Microsoft.AI.MachineLearning.Interop.pdb" />')
 
     # Process runtimes
     # Process onnxruntime import lib, dll, and pdb
@@ -403,11 +404,12 @@ def generate_files(list, args):
         files_list.append('<file src=' + '"' + windowsai_native_targets + '" target="' + build + '" />')
         # Process rules
         files_list.append('<file src=' + '"' + windowsai_native_rules + '" target="' + build + '" />')
-        # Process .net standard 2.0 targets
-        interop_src = 'Microsoft.AI.MachineLearning.Interop'
-        interop_targets = 'Microsoft.AI.MachineLearning.targets'
-        windowsai_net50_targets = os.path.join(args.sources_path, 'csharp', 'src', interop_src, interop_targets)
-        files_list.append('<file src=' + '"' + windowsai_net50_targets + '" target="build\\net5.0" />')
+        # Process .net5.0 targets
+        if args.target_architecture == 'x64':
+          interop_src = 'Microsoft.AI.MachineLearning.Interop'
+          interop_targets = 'Microsoft.AI.MachineLearning.targets'
+          windowsai_net50_targets = os.path.join(args.sources_path, 'csharp', 'src', interop_src, interop_targets)
+          files_list.append('<file src=' + '"' + windowsai_net50_targets + '" target="build\\net5.0" />')
 
     if is_cpu_package or is_cuda_gpu_package or is_dml_package or is_mklml_package:
         # Process props file
