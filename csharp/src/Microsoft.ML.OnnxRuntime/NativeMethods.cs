a538692921de8bd7c5f26b2d729295a5b5327dcb
ZoroDerVonCodier <daveb@signalpop.com>
2020-12-15 10:30:48 -0800

    Made internals visible to Microsoft.ML.OnnxTraining assembly.

--- csharp/src/Microsoft.ML.OnnxRuntime/NativeMethods.cs
+++ csharp/src/Microsoft.ML.OnnxRuntime/NativeMethods.cs
@@ -3,6 +3,16 @@
 
 using System;
 using System.Runtime.InteropServices;
+using System.Runtime.CompilerServices;
+
+// Making this assembly's internals visible to the internal training assembly
+[assembly: InternalsVisibleTo("Microsoft.ML.OnnxTraining," +
+                              "PublicKey=002400000480000094000000060200000024000052534131000400000100010059013e94e4bc70" +
+                              "136ca4c35f33acd6b62974536b698f9c7a21cee18d805c7ad860ad9eebfdc47a96ba2f8d03f4cf" +
+                              "1c36b9d30787e276c7b9833b5bf2a6eba7e919e6b90083078a352262aed1d842e5f70a3085cbcf" +
+                              "4c56ae851b161137920961c23fcc246598d61d258ccc615c927b2441359eea666a99ce1c3c07dc" +
+                              "a18fb0e1")]
+
 
 namespace Microsoft.ML.OnnxRuntime
 {
