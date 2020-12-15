fe6545ab4ab1957eb3d909e3e2ae1fa7f8cd4188
Vincent Wang <wangwchpku@outlook.com>
2020-12-15 17:19:17 +0800

    ort's to_dlpack.

--- onnxruntime/python/onnxruntime_inference_collection.py
+++ onnxruntime/python/onnxruntime_inference_collection.py
@@ -441,3 +441,6 @@ class OrtValue:
         Valid only for OrtValues holding Tensors. Throws for OrtValues holding non-Tensors.
         '''
         return self._ortvalue.numpy()
+
+    def to_dlpack(self):
+        return self._ortvalue.to_dlpack()
