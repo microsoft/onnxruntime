2d4fce500155935742ef18d9aedfedfb6f0a9b55
Wei-Sheng Chin <wschin@outlook.com>
2021-01-04 08:11:41 +0000

    Add ort path
    Add os package

--- orttraining/orttraining/test/python/dhp_parallel/orttraining_test_parallel_train_simple_model.py
+++ orttraining/orttraining/test/python/dhp_parallel/orttraining_test_parallel_train_simple_model.py
@@ -1,3 +1,9 @@
+# Add onnxruntime path as system path.
+# Otherwise, "import onnxruntime" may fail.
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 import onnxruntime
 import torch
 from onnxruntime.capi._pybind_state import get_mpi_context_world_rank, get_mpi_context_world_size
