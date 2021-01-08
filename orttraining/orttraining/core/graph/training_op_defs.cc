6084a9870713ec4e9aa25e8f4252cc3f3f5961ad
Cheng Tang <chenta@microsoft.com>
2021-01-07 16:21:59 -0800

    ort training fix

--- orttraining/orttraining/core/graph/training_op_defs.cc
+++ orttraining/orttraining/core/graph/training_op_defs.cc
@@ -1978,11 +1978,11 @@ Return true if all elements are true and false otherwise.
       .Output(0, "Y", "output", "TOut")
       .TypeConstraint(
           "TIn",
-          {"tensor(float16)", "tensor(float)", "tensor(double)"},
+          {"tensor(float16)", "tensor(float)", "tensor(double)", "tensor(bfloat16)"},
           "Constrain input types to float tensors.")
       .TypeConstraint(
           "TOut",
-          {"tensor(float16)", "tensor(float)", "tensor(double)"},
+          {"tensor(float16)", "tensor(float)", "tensor(double)", "tensor(bfloat16)"},
           "Constrain scale types to float tensors.")
       .TypeAndShapeInferenceFunction([](ONNX_NAMESPACE::InferenceContext& ctx) {
         updateOutputShape(ctx, 0, {});
