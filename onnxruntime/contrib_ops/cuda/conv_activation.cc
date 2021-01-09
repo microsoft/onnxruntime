1e233c19c87fd8b4bfbce3655d4c9a44611331f0
RandySheriffH <randysheriff@hotmail.com>
2021-01-08 17:16:33 -0800

    commit all

---
+++ onnxruntime/contrib_ops/cuda/conv_activation.cc
@@ -0,0 +1,11 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+#include "core/providers/cuda/nn/conv.h"
+#include "core/providers/cuda/cudnn_common.h"
+
+namespace onnxruntime {
+namespace contrib {
+
+}  // namespace contrib
+}  // namespace onnxruntime
---
+++ onnxruntime/contrib_ops/cuda/fused_conv.cc
@@ -0,0 +1,22 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+#include "core/providers/cuda/nn/conv.h"
+#include "core/providers/cuda/cudnn_common.h"
+
+namespace onnxruntime {
+namespace contrib {
+namespace cuda {
+
+/*
+template <typename T>
+class FusedConv : public Conv<T> {
+ public:
+  FusedConv();
+  ~FusedConv();
+};
+*/
+
+}  // namespace cuda
+}  // namespace contrib
+}  // namespace onnxruntime
