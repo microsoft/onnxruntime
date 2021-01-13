2d546770c6479c31412b8fe4026b050d52b68478
baijumeswani <bmeswani@microsoft.com>
2021-01-13 12:34:04 -0800

    Add ORTModule BERT classifier to CI the pipeline (#6330)

--- orttraining/orttraining/test/python/orttraining_ortmodule_tests.py
+++ orttraining/orttraining/test/python/orttraining_ortmodule_tests.py
@@ -37,6 +37,14 @@ def run_ortmodule_poc_net(cwd, log):
     run_subprocess(command, cwd=cwd, log=log).check_returncode()
 
 
+def run_ort_module_hf_bert_for_sequence_classification_from_pretrained(cwd, log):
+    log.debug('Running: ORTModule HuggingFace BERT for sequence classification.')
+
+    command = [sys.executable, 'orttraining_test_ortmodule_bert_classifier.py']
+
+    run_subprocess(command, cwd=cwd, log=log).check_returncode()
+
+
 def main():
     args = parse_arguments()
     cwd = args.cwd
@@ -46,6 +54,8 @@ def main():
     run_ortmodule_api_tests(cwd, log)
 
     run_ortmodule_poc_net(cwd, log)
+
+    run_ort_module_hf_bert_for_sequence_classification_from_pretrained(cwd, log)
 
     return 0
 
