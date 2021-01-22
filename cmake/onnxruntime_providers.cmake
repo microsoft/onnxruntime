987038d9ceaa3a43892dee3b37666bd12eb6c5af
Edward Chen <18449977+edgchen1@users.noreply.github.com>
2021-01-21 16:07:06 -0800

    Update CMake files to use Mp11 submodule.

--- cmake/onnxruntime_providers.cmake
+++ cmake/onnxruntime_providers.cmake
@@ -166,7 +166,6 @@ if(HAS_DEPRECATED_COPY)
 endif()
 
 target_include_directories(onnxruntime_providers PRIVATE ${ONNXRUNTIME_ROOT} ${eigen_INCLUDE_DIRS} ${gemmlowp_src} ${RE2_INCLUDE_DIR})
-target_include_directories(onnxruntime_providers PRIVATE "D:/source/mp11/include") # test - include mp11
 
 add_dependencies(onnxruntime_providers onnx ${onnxruntime_EXTERNAL_DEPENDENCIES})
 
