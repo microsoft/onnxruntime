aef14b8a3deaf9bab360504cec096ea25d6feed7
liqun <liqun@OrtTrainingDev4.af05slrtruoetgaxwwjv5nsq5e.px.internal.cloudapp.net>
2021-01-26 23:56:05 +0000

    a

--- onnxruntime/test/python/onnxruntime_test_python_nuphar.py
+++ onnxruntime/test/python/onnxruntime_test_python_nuphar.py
@@ -338,7 +338,7 @@ class TestNuphar(unittest.TestCase):
     def test_bidaf(self):
         cwd = os.getcwd()
 
-        bidaf_dir_src = '/data/models/opset9/test_bidaf'
+        bidaf_dir_src = '/build/models/opset9/test_bidaf'
         bidaf_dir = os.path.join(cwd, 'bidaf')
         shutil.copytree(bidaf_dir_src, bidaf_dir)
 
@@ -419,7 +419,7 @@ class TestNuphar(unittest.TestCase):
 
         # run symbolic shape inference on this model
         # set int_max to 1,000,000 to simplify symbol computes for things like min(1000000, seq_len) -> seq_len
-        bert_squad_dir_src = '/data/models/opset10/BERT_Squad'
+        bert_squad_dir_src = '/build/models/opset10/BERT_Squad'
         bert_squad_dir = os.path.join(cwd, 'BERT_Squad')
         shutil.copytree(bert_squad_dir_src, bert_squad_dir)
 
@@ -710,5 +710,6 @@ class TestNuphar(unittest.TestCase):
         ], check=True)
 
         validate_with_ort(loop_model_filename, scan_model_filename)
+
 if __name__ == '__main__':
     unittest.main()
