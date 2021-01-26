08a0c799aca761e2a7ffe7faefe568ee45569d54
stevenlix <38092805+stevenlix@users.noreply.github.com>
2021-01-26 10:41:31 -0800

    Expose graph ModelPath to TensorRT shared library (#6353)
    * Update graph_viewer.cc
    
    * Update tensorrt_execution_provider.cc
    
    * Update graph_viewer.h
    
    * Update tensorrt_execution_provider.cc
    
    * Update tensorrt_execution_provider.cc
    
    * Update provider_api.h
    
    * Update provider_bridge_ort.cc
    
    * Update provider_interfaces.h
    
    * Update provider_interfaces.h
    
    * expose GraphViewer ModelPath API to TRT shared lib
    
    * add modelpath to compile
    
    * update
    
    * add model_path to onnx tensorrt parser
    
    * use GenerateMetaDefId to generate unique TRT kernel name
    
    * use GenerateMetaDefId to generate unique TRT engine name
    
    * fix issue
    
    * Update tensorrt_execution_provider.cc
    
    * remove GetVecHash
    
    * Update tensorrt_execution_provider.h
    
    * convert wchar_t to char for tensorrt parser
    
    * update tensorrt parser to include latest changes
    
    * fix issues
    
    * Update tensorrt_execution_provider.cc
    
    * merge trt parser latest change
    
    * add PROVIDER_DISALLOW_ALL(Path)

--- onnxruntime/core/framework/provider_bridge_ort.cc
+++ onnxruntime/core/framework/provider_bridge_ort.cc
@@ -514,6 +514,7 @@ struct ProviderHostImpl : ProviderHost {
   }
 
   const std::string& GraphViewer__Name(const GraphViewer* p) noexcept override { return p->Name(); }
+  const Path& GraphViewer__ModelPath(const GraphViewer* p) noexcept override { return p->ModelPath(); }
 
   const Node* GraphViewer__GetNode(const GraphViewer* p, NodeIndex node_index) override { return p->GetNode(node_index); }
   const NodeArg* GraphViewer__GetNodeArg(const GraphViewer* p, const std::string& name) override { return p->GetNodeArg(name); }
@@ -534,6 +535,10 @@ struct ProviderHostImpl : ProviderHost {
 
   const std::vector<NodeIndex>& GraphViewer__GetNodesInTopologicalOrder(const GraphViewer* p) override { return p->GetNodesInTopologicalOrder(); }
   const std::vector<const NodeArg*>& GraphViewer__GetInputsIncludingInitializers(const GraphViewer* p) noexcept override { return p->GetInputsIncludingInitializers(); }
+
+  //Path
+  //Gets a string representation of the path.
+  PathString Path__ToPathString(const Path* p) noexcept override { return p->ToPathString(); }
 
   // Provider_OpKernel_Base
   const OpKernelInfo& Provider_OpKernel_Base__GetInfo(const Provider_OpKernel_Base* p) override { return reinterpret_cast<const OpKernel*>(p)->Info(); }
