b36bbf181a4d934fe5ce0e29d1731d6fb9894831
liqun <liqun@OrtTrainingDev4.af05slrtruoetgaxwwjv5nsq5e.px.internal.cloudapp.net>
2021-01-26 19:16:00 +0000

    a

--- tools/ci_build/build.py
+++ tools/ci_build/build.py
@@ -160,9 +160,6 @@ def parse_arguments():
     parser.add_argument(
         "--enable_training", action='store_true', help="Enable training in ORT.")
     parser.add_argument(
-        "--enable_training_pipeline_e2e_tests", action="store_true",
-        help="Enable the pipeline c++ e2e tests.")
-    parser.add_argument(
         "--disable_nccl", action='store_true', help="Disable Nccl.")
     parser.add_argument(
         "--mpi_home", help="Path to MPI installation dir")
