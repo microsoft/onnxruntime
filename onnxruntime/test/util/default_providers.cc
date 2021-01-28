752627c5bb72eabbab73425199d83c4004ba23ab
Guoyu Wang <62914304+gwang-msft@users.noreply.github.com>
2021-01-28 12:25:46 -0800

    [CoreML EP] Add CI for CoreML EP (macOS) and add coreml_flags for EP options (#6481)
    * Add macos coreml CI and coreml_flags
    
    * Move save debuggubg model to use environment var
    
    * Move pipeline off from macos CI template
    
    * Fix an issue building using unix make, add parallel to build script
    
    * Fixed build break for shared_lib and cmpile warning
    
    * Fix a compile warning
    
    * test
    
    * Revert the accidental push from another branch
    
    This reverts commit 472029ba25d50f9508474c9eeceb3454cead7877.

--- onnxruntime/test/util/default_providers.cc
+++ onnxruntime/test/util/default_providers.cc
@@ -10,6 +10,9 @@
 #ifdef USE_ROCM
 #include "core/providers/rocm/rocm_provider_factory_creator.h"
 #endif
+#ifdef USE_COREML
+#include "core/providers/coreml/coreml_provider_factory.h"
+#endif
 #include "core/session/onnxruntime_cxx_api.h"
 
 namespace onnxruntime {
@@ -26,7 +29,7 @@ std::shared_ptr<IExecutionProviderFactory> CreateExecutionProviderFactory_Tensor
 std::shared_ptr<IExecutionProviderFactory> CreateExecutionProviderFactory_MIGraphX(int device_id);
 std::shared_ptr<IExecutionProviderFactory> CreateExecutionProviderFactory_ACL(int use_arena);
 std::shared_ptr<IExecutionProviderFactory> CreateExecutionProviderFactory_ArmNN(int use_arena);
-std::shared_ptr<IExecutionProviderFactory> CreateExecutionProviderFactory_CoreML();
+std::shared_ptr<IExecutionProviderFactory> CreateExecutionProviderFactory_CoreML(uint32_t);
 
 // EP for internal testing
 std::shared_ptr<IExecutionProviderFactory> CreateExecutionProviderFactory_InternalTesting(
@@ -136,7 +139,10 @@ std::unique_ptr<IExecutionProvider> DefaultRocmExecutionProvider() {
 
 std::unique_ptr<IExecutionProvider> DefaultCoreMLExecutionProvider() {
 #if defined(USE_COREML)
-  return CreateExecutionProviderFactory_CoreML()->CreateProvider();
+  // We want to run UT on CPU only to get output value without losing precision
+  uint32_t coreml_flags = 0;
+  coreml_flags |= COREML_FLAG_USE_CPU_ONLY;
+  return CreateExecutionProviderFactory_CoreML(coreml_flags)->CreateProvider();
 #else
   return nullptr;
 #endif
