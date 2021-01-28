7ecdb6e31ecdf3c9b15f933cd69cd99d15ea142a
Sheil Kumar <smk2007@gmail.com>
2021-01-28 14:37:15 -0800

    Fix test breaks in Windows ingestion pipeline (#6476)
    * fix various build breaks with Windows build
    
    * fix runtime errors loading libraries from system32
    
    * add build_inbox check to winml_test_common
    
    * use raw string
    
    * cleanup
    
    * fix dll load
    
    Co-authored-by: Sheil Kumar <sheilk@microsoft.com>

--- cmake/winml_unittests.cmake
+++ cmake/winml_unittests.cmake
@@ -171,6 +171,9 @@ file(GLOB winml_test_common_src CONFIGURE_DEPENDS
     "${WINML_TEST_SRC_DIR}/common/*.cpp")
 add_library(winml_test_common STATIC ${winml_test_common_src})
 target_compile_options(winml_test_common PRIVATE /wd5205)  # workaround cppwinrt SDK bug https://github.com/microsoft/cppwinrt/issues/584
+if (onnxruntime_WINML_NAMESPACE_OVERRIDE STREQUAL "Windows")
+  target_compile_definitions(winml_test_common PRIVATE "BUILD_INBOX=1")
+endif()
 add_dependencies(winml_test_common
   onnx
   winml_api
@@ -192,7 +195,7 @@ add_winml_test(
   SOURCES ${winml_test_api_src} ${winml_test_api_redist_only_src}
   LIBS winml_test_common
 )
-target_delayload(winml_test_api d3d11.dll dxgi.dll d3d12.dll api-ms-win-core-file-l1-2-2.dll api-ms-win-core-synch-l1-2-1.dll)
+target_delayload(winml_test_api dxgi.dll d3d12.dll api-ms-win-core-file-l1-2-2.dll api-ms-win-core-synch-l1-2-1.dll)
 if (onnxruntime_USE_DML)
   target_delayload(winml_test_api directml.dll)
 endif()
@@ -229,9 +232,6 @@ if(onnxruntime_RUN_MODELTEST_IN_DEBUG_MODE)
   target_compile_definitions(winml_test_image PUBLIC -DRUN_MODELTEST_IN_DEBUG_MODE)
 endif()
 target_delayload(winml_test_image d3d12.dll api-ms-win-core-file-l1-2-2.dll api-ms-win-core-synch-l1-2-1.dll)
-if (EXISTS ${dxcore_header})
-  target_delayload(winml_test_image ext-ms-win-dxcore-l1-*.dll)
-endif()
 
 get_winml_test_concurrency_src(${WINML_TEST_SRC_DIR} winml_test_concurrency_src)
 add_winml_test(
