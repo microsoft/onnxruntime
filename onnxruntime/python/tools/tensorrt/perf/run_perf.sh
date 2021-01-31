7bc1b973a343280f3533064e85f34681cd134a77
Olivia Jain <oljain@microsoft.com>
2021-01-31 15:43:42 -0800

    correcting many-models path

--- onnxruntime/python/tools/tensorrt/perf/run_perf.sh
+++ onnxruntime/python/tools/tensorrt/perf/run_perf.sh
@@ -7,7 +7,7 @@ fi
 
 if [ "$1" == "many-models" ]
 then 
-    sudo docker run --gpus all -v /home/hcsuser/repos/onnxruntime/onnxruntime/python/tools/tensorrt/perf/:/usr/share/perf/ -v /home/hcsuser/mount/models:/usr/share/mount/many-models onnxruntime-trt-perf /bin/bash /usr/share/perf/perf.sh -d /usr/share/perf/ -o many-models -m /usr/share/mount/models
+    sudo docker run --gpus all -v /home/hcsuser/repos/onnxruntime/onnxruntime/python/tools/tensorrt/perf/:/usr/share/perf/ -v /home/hcsuser/mount/models:/usr/share/mount/many-models onnxruntime-trt-perf /bin/bash /usr/share/perf/perf.sh -d /usr/share/perf/ -o many-models -m /usr/share/mount/many-models
 fi 
 
 if [ "$1" == "partner-models" ]
