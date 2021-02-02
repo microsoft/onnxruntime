da7a97ff9fe7a452f6d3b32a45e443a06f9d3662
Chi Lo <Chi.Lo@microsoft.com>
2021-02-02 01:35:08 -0800

    Add entropy support for quantization calibration

--- onnxruntime/python/tools/quantization/calibrate.py
+++ onnxruntime/python/tools/quantization/calibrate.py
@@ -19,7 +19,166 @@ from .registry import QLinearOpsRegistry
 
 import abc
 import itertools
+import copy
 
+def smooth_distribution(p, eps=0.0001):
+    """Given a discrete distribution (may have not been normalized to 1),
+    smooth it by replacing zeros with eps multiplied by a scaling factor
+    and taking the corresponding amount off the non-zero values.
+    Ref: http://web.engr.illinois.edu/~hanj/cs412/bk3/KL-divergence.pdf
+         https://github.com//apache/incubator-mxnet/blob/master/python/mxnet/contrib/quantization.py
+    """
+    is_zeros = (p == 0).astype(np.float32)
+    is_nonzeros = (p != 0).astype(np.float32)
+    n_zeros = is_zeros.sum()
+    n_nonzeros = p.size - n_zeros
+
+    if not n_nonzeros:
+        # raise ValueError('The discrete probability distribution is malformed. All entries are 0.')
+        return -1
+    eps1 = eps * float(n_zeros) / float(n_nonzeros)
+    assert eps1 < 1.0, 'n_zeros=%d, n_nonzeros=%d, eps1=%f' % (n_zeros, n_nonzeros, eps1)
+
+    hist = p.astype(np.float32)
+    hist += eps * is_zeros + (-eps1) * is_nonzeros
+    assert (hist <= 0).sum() == 0
+
+    return hist
+
+class CalibrationDataCollector(metaclass=abc.ABCMeta):
+
+    @abc.abstractmethod
+    def collect(self, name_to_arr):
+        """
+        Get histogram based on given data.
+            name_to_arr : dict 
+                tensor name to data as NDArray 
+        """
+
+class HistogramCollector(CalibrationDataCollector):
+    def __init__(self, num_quantized_bins=128):
+        self.histogram_dict = {}
+        self.num_quantized_bins= num_quantized_bins
+
+    def get_histogram_dict(self):
+        return self.histogram_dict
+
+    def collect(self, name_to_arr):
+        for tensor, data_arr in name_to_arr.items():
+            data_arr = np.asarray(data_arr)
+            data_arr = data_arr.flatten()
+
+            if data_arr.size > 0:
+                min_value = np.min(data_arr)
+                max_value = np.max(data_arr)
+            else:
+                min_value = 0
+                max_value = 0
+
+            threshold = max(abs(min_value), abs(max_value))
+
+            if tensor in self.histogram_dict:
+                old_histogram = self.histogram_dict[tensor]
+                self.histogram_dict[tensor] = self.merge_histogram(old_histogram, data_arr, min_value, max_value, threshold)
+            else:
+                # hist, hist_edges = np.histogram(data_arr, self.num_quantized_bins, range=(min_value, max_value))
+                hist, hist_edges = np.histogram(data_arr, self.num_quantized_bins, range=(-threshold, threshold))
+                self.histogram_dict[tensor] = (hist, hist_edges, min_value, max_value, threshold)
+
+    def merge_histogram(self, old_histogram, data_arr, new_min, new_max, new_threshold):
+
+        (old_hist, old_hist_edges, old_min, old_max, old_threshold) = old_histogram
+
+        if new_threshold <= old_threshold:
+            new_hist, _ = np.histogram(data_arr, len(old_hist), range=(-old_threshold, old_threshold))
+            return (new_hist + old_hist, old_hist_edges, min(old_min, new_min), max(old_max, new_max), old_threshold)
+        else:
+            old_num_bins = len(old_hist)
+            old_stride = 2 * old_threshold / old_num_bins
+            half_increased_bins = 0 if old_stride == 0 else int((new_threshold - old_threshold) // old_stride + 1) 
+            new_num_bins = old_num_bins + 2 * half_increased_bins
+            new_threshold = half_increased_bins * old_stride + old_threshold
+            hist, hist_edges = np.histogram(data_arr, new_num_bins, range=(-new_threshold, new_threshold))
+            hist[half_increased_bins:new_num_bins-half_increased_bins] += old_hist
+            return (hist, hist_edges, min(old_min, new_min), max(old_max, new_max), new_threshold)
+
+    def get_optimal_collection_result(self):
+        return self.get_optimal_thresholds(self.histogram_dict, self.num_quantized_bins)
+
+
+    def get_optimal_thresholds(self, histogram_dict, num_quantized_bins=128):
+        thresholds_dict = {} # per tensor thresholds
+
+        for tensor, histogram in histogram_dict.items():
+            optimal_threshold = self.get_optimal_threshold(histogram, num_quantized_bins)
+            thresholds_dict[tensor] = optimal_threshold
+
+        return thresholds_dict
+
+    def get_optimal_threshold(self, histogram, num_quantized_bins):
+        from scipy.stats import entropy
+
+        hist, hist_edges, _, _, _ = histogram
+        num_bins = hist.size
+        zero_bin_index = num_bins // 2
+        num_half_quantized_bin = num_quantized_bins // 2
+        
+        kl_divergence = np.zeros(zero_bin_index - num_half_quantized_bin + 1)
+        thresholds = [(0, 0) for i in range(kl_divergence.size)] 
+
+        for i in range(num_half_quantized_bin, zero_bin_index + 1, 1):
+            start_index = zero_bin_index - i 
+            end_index = zero_bin_index + i + 1 if (zero_bin_index + i + 1) <= num_bins else num_bins
+
+            thresholds[i - num_half_quantized_bin] = (float(hist_edges[start_index]), float(hist_edges[end_index]))
+
+            sliced_distribution = copy.deepcopy(hist[start_index:end_index])
+
+            # reference distribution p
+            p = sliced_distribution.copy() # a copy of np array
+            left_outliers_count = sum(hist[:start_index]) 
+            right_outliers_count = sum(hist[end_index:])
+            p[0] += left_outliers_count
+            p[-1] += right_outliers_count
+
+            # nonzeros[i] incidates whether p[i] is non-zero
+            nonzeros = (p != 0).astype(np.int64)
+            
+            # quantize p.size bins into quantized bins (default 128 bins) 
+            quantized_bins = np.zeros(num_quantized_bins, dtype=np.int64)
+            num_merged_bins = sliced_distribution.size // num_quantized_bins
+
+            # merge bins into quantized bins
+            for index in range(num_quantized_bins):
+                start = index * num_merged_bins 
+                end = start + num_merged_bins
+                quantized_bins[index] = sum(sliced_distribution[start:end]) 
+                # print("start %s, end %s, sum %s" % (start, end, sum(sliced_distribution[start:end])))
+            quantized_bins[-1] += sum(sliced_distribution[num_quantized_bins * num_merged_bins:])
+
+            # in order to compare p and q, we need to make length of q equals to length of p
+            # expand quantized bins into p.size bins
+            q = np.zeros(p.size, dtype=np.int64)
+            for index in range(num_quantized_bins):
+                start = index * num_merged_bins
+                end = start + num_merged_bins
+
+                norm = sum(nonzeros[start:end])
+                if norm != 0:
+                    q[start:end] = float(quantized_bins[index]) / float(norm)
+            
+            p = smooth_distribution(p)
+            q = smooth_distribution(q)
+
+            if isinstance(q, np.ndarray):
+                kl_divergence[i - num_half_quantized_bin] = entropy(p, q)
+            else:
+                kl_divergence[i - num_half_quantized_bin] = float('inf')
+
+        min_kl_divergence_idx = np.argmin(kl_divergence)
+        optimal_threshold = thresholds[min_kl_divergence_idx] 
+
+        return optimal_threshold
 
 class CalibrationDataReader(metaclass=abc.ABCMeta):
     @classmethod
@@ -58,14 +217,24 @@ class ONNXCalibrater:
         self.augmented_model_path = augmented_model_path
         self.input_name_to_nodes = {}
         self.calibration_cache = {}  # save temporary calibration table
+        self.collector = None
 
     def set_data_reader(self, data_reader):
         self.data_reader = data_reader
 
     def get_calibration_cache(self):
+        if len(self.calibration_cache) > 0:
+            return self.calibration_cache
+
+        if not self.collector:
+            print("No collector created and can't generate calibration data.")
+            return None
+
+        self.calibration_cache = self.collector.get_optimal_collection_result()
+
         return self.calibration_cache
 
-    def augment_graph(self):
+    def augment_graph(self, calib_mode='naive'):
         '''
         Adds ReduceMin and ReduceMax nodes to all quantization_candidates op type nodes in
         model and ensures their outputs are stored as part of the graph output
@@ -94,31 +263,37 @@ class ONNXCalibrater:
                         if vi.type.HasField('tensor_type') and (vi.type.tensor_type.elem_type in tensor_type_to_calibrate) and (tensor_name not in initializer):
                             tensors_to_calibrate.add(tensor_name)
 
-        # If augmenting all ops, it's possible that some nodes' input value are 0.
-        # Can't reduce on dim with value of 0 if 'keepdims' is false, therefore set keepdims to 1.
-        if self.calibrate_op_types:
-            keepdims_value = 0
-        else:
-            keepdims_value = 1
 
         for tensor in tensors_to_calibrate:
-            # Adding ReduceMin nodes
-            reduce_min_name = tensor + '_ReduceMin'
-            reduce_min_node = onnx.helper.make_node('ReduceMin', [tensor], [tensor + '_ReduceMin'],
-                                                    reduce_min_name,
-                                                    keepdims=keepdims_value)
+            if calib_mode == 'naive':
 
-            added_nodes.append(reduce_min_node)
-            added_outputs.append(helper.make_tensor_value_info(reduce_min_node.output[0], TensorProto.FLOAT, ()))
+                # If augmenting all ops, it's possible that some nodes' input value are 0.
+                # Can't reduce on dim with value of 0 if 'keepdims' is false, therefore set keepdims to 1.
+                if self.calibrate_op_types:
+                    keepdims_value = 0
+                else:
+                    keepdims_value = 1
 
-            # Adding ReduceMax nodes
-            reduce_max_name = tensor + '_ReduceMax'
-            reduce_max_node = onnx.helper.make_node('ReduceMax', [tensor], [tensor + '_ReduceMax'],
-                                                    reduce_max_name,
-                                                    keepdims=keepdims_value)
+                # Adding ReduceMin nodes
+                reduce_min_name = tensor + '_ReduceMin'
+                reduce_min_node = onnx.helper.make_node('ReduceMin', [tensor], [tensor + '_ReduceMin'],
+                                                        reduce_min_name,
+                                                        keepdims=keepdims_value)
 
-            added_nodes.append(reduce_max_node)
-            added_outputs.append(helper.make_tensor_value_info(reduce_max_node.output[0], TensorProto.FLOAT, ()))
+                added_nodes.append(reduce_min_node)
+                added_outputs.append(helper.make_tensor_value_info(reduce_min_node.output[0], TensorProto.FLOAT, ()))
+
+                # Adding ReduceMax nodes
+                reduce_max_name = tensor + '_ReduceMax'
+                reduce_max_node = onnx.helper.make_node('ReduceMax', [tensor], [tensor + '_ReduceMax'],
+                                                        reduce_max_name,
+                                                        keepdims=keepdims_value)
+
+                added_nodes.append(reduce_max_node)
+                added_outputs.append(helper.make_tensor_value_info(reduce_max_node.output[0], TensorProto.FLOAT, ()))
+
+            elif calib_mode == 'entropy':
+                added_outputs.append(value_infos[tensor])
 
         model.graph.node.extend(added_nodes)
         model.graph.output.extend(added_outputs)
@@ -139,15 +314,16 @@ class ONNXCalibrater:
         :return: dictionary mapping: {added node names: (ReduceMin, ReduceMax) pairs }
         '''
 
+        print(calib_mode)
         #conduct inference session and get intermediate outputs
-        sess_options = onnxruntime.SessionOptions()
         if ort_graph_optimization_enable:
-            sess_options.graph_optimization_level = onnxruntime.GraphOptimizationLevel.ORT_ENABLE_BASIC
+            session = onnxruntime.InferenceSession(self.augmented_model_path, None) 
         else:            
-            sess_options.graph_optimization_level = onnxruntime.GraphOptimizationLevel.ORT_DISABLE_ALL
-        session = onnxruntime.InferenceSession(self.augmented_model_path,
-                                               sess_options=sess_options,
-                                               providers=providers)
+            sess_options = onnxruntime.SessionOptions()
+            sess_options.graph_optimization_level = onnxruntime.GraphOptimizationLevel.ORT_DISABLE_ALL  #ORT_ENABLE_BASIC
+            session = onnxruntime.InferenceSession(self.augmented_model_path,
+                                                   sess_options=sess_options,
+                                                   providers=providers)
 
         #number of outputs in original model
         num_model_outputs = len(self.model.graph.output)
@@ -171,13 +347,14 @@ class ONNXCalibrater:
             for k, v in d.items():
                 merged_dict.setdefault(k, []).append(v)
         added_node_output_names = node_output_names[num_model_outputs:]
-        node_names = [added_node_output_names[i].rpartition('_')[0]
-                      for i in range(0, len(added_node_output_names), 2)]  #output names
 
         # Characterizing distribution of a node's values across test data sets
         clean_merged_dict = dict((i, merged_dict[i]) for i in merged_dict if i not in model_original_outputs)
 
         if calib_mode == 'naive':
+
+            node_names = [added_node_output_names[i].rpartition('_')[0]
+                          for i in range(0, len(added_node_output_names), 2)]  #output names
 
             pairs = []
             for i in range(0, len(added_node_output_names), 2):
@@ -192,17 +369,25 @@ class ONNXCalibrater:
 
                 pairs.append(tuple([min_value, max_value]))
 
+            final_dict = dict(zip(node_names, pairs))
+
+            # merge new calibration data with previous calibration data
+            if len(self.calibration_cache) > 0:
+                for key, value in self.calibration_cache.items():
+                    min_value = min(value[0], final_dict[key][0])
+                    max_value = max(value[1], final_dict[key][1])
+                    final_dict[key] = (min_value, max_value)
+
+        elif calib_mode == 'entropy':
+            if not self.collector:
+                self.collector = HistogramCollector()
+            self.collector.collect(clean_merged_dict)
+
+            return
+
         else:
             raise ValueError('Unknown value for calib_mode. Currently only naive mode is supported.')
 
-        final_dict = dict(zip(node_names, pairs))
-
-        # merge new calibration data with previous calibration data
-        if len(self.calibration_cache) > 0:
-            for key, value in self.calibration_cache.items():
-                min_value = min(value[0], final_dict[key][0])
-                max_value = max(value[1], final_dict[key][1])
-                final_dict[key] = (min_value, max_value)
 
         self.calibration_cache = final_dict
 
@@ -297,7 +482,7 @@ class ONNXCalibrater:
 
 def get_calibrator(model,
                    data_reader: CalibrationDataReader,
-                   op_types=['Conv', 'MatMul'],
+                   op_types=[],
                    black_nodes=[],
                    white_nodes=[],
                    augmented_model_path='augmented_model.onnx'):
@@ -309,6 +494,7 @@ def get_calibrator(model,
 
 def calculate_calibration_data(model,
                                calibrator=None,
+                               calib_mode='entropy',
                                calibration_data_reader: CalibrationDataReader = None,
                                op_types_to_quantize=[],
                                activation_type=QuantType.QUInt8,
@@ -333,10 +519,10 @@ def calculate_calibration_data(model,
                                     augmented_model_path=augmented_model_path)
 
     if not os.path.exists(augmented_model_path):
-        augmented_model = calibrator.augment_graph()
+        augmented_model = calibrator.augment_graph(calib_mode=calib_mode)
         onnx.save(augmented_model, augmented_model_path)
 
-    calibrator.get_intermediate_outputs(providers=["CUDAExecutionProvider"], ort_graph_optimization_enable=False)
+    calibrator.get_intermediate_outputs(calib_mode=calib_mode, providers=["CUDAExecutionProvider"], ort_graph_optimization_enable=False)
 
 
 def generate_calibration_table(calibrator,
@@ -344,9 +530,7 @@ def generate_calibration_table(calibrator,
                                augmented_model_path,
                                remove_previous_flag,
                                data_reader,
-                               calibration_dataset=None,
-                               stride=5000,
-                               batch_size=20):
+                               calib_mode='entropy'):
 
     if remove_previous_flag and os.path.exists(augmented_model_path):
         os.remove(augmented_model_path)
@@ -354,9 +538,7 @@ def generate_calibration_table(calibrator,
 
     if not calibrator:
         calibrator = get_calibrator(model, data_reader, augmented_model_path=augmented_model_path)
-    calculate_calibration_data(model, calibrator, augmented_model_path=augmented_model_path)
-
-    return calibrator.get_calibration_cache()
+    calculate_calibration_data(model, calibrator, calib_mode, augmented_model_path=augmented_model_path)
 
 
 def calibrate(model,
