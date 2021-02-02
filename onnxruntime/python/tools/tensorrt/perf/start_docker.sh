7bbe44b22293f71bbf96891f6b2a576719d62e02
Olivia Jain <oljain@microsoft.com>
2021-02-02 07:36:55 -0800

    Delete start_docker.sh

--- onnxruntime/python/tools/tensorrt/perf/start_docker.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sudo systemctl start docker.service    
-sudo docker run --gpus all -it -v /home/hcsuser/:/usr/share/ 36f6f9268db2
-cd /usr/share/repos/onnxruntime/onnxruntime/python/tools/tensorrt/perf
