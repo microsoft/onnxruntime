59041ab79e555fd78034c5839be6189adfe69cb8
Wei-Sheng Chin <wschin@outlook.com>
2021-02-03 07:00:47 +0000

    Remove unused MP model

--- orttraining/orttraining/test/python/orttraining_test_ort_module_pytorch_ddp.py
+++ orttraining/orttraining/test/python/orttraining_test_ort_module_pytorch_ddp.py
@@ -143,21 +143,6 @@ def demo_checkpoint(rank, world_size, use_ort_module):
 
     cleanup()
 
-class ToyMpModel(nn.Module):
-    def __init__(self, dev0, dev1):
-        super(ToyMpModel, self).__init__()
-        self.dev0 = dev0
-        self.dev1 = dev1
-        self.net1 = torch.nn.Linear(10, 10).to(dev0)
-        self.relu = torch.nn.ReLU()
-        self.net2 = torch.nn.Linear(10, 5).to(dev1)
-
-    def forward(self, x):
-        x = x.to(self.dev0)
-        x = self.relu(self.net1(x))
-        x = x.to(self.dev1)
-        return self.net2(x)
-
 def run_demo(demo_fn, world_size, use_ort_module):
     mp.spawn(demo_fn,
              args=(world_size, use_ort_module),
