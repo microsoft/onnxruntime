ccfd90291bda29492b420037ad57237b0470798b
Scott McKay <skottmckay@gmail.com>
2021-02-06 11:33:29 +1000

    Remove condition from ORT_RETURN_IF[_NOT] macro output. (#6563)
    Remove condition from ORT_RETURN_IF[_NOT] macro output as repeating the condition doesn't add much value compared to the explicit error message, and the error message includes the file and line anyway so it's easy enough to find the condition if needed.
    Update the few places where the macros were used without an explicit error message to provide an explicit error message.
    
    Saves 12.5KB in a minimal MinSizeRel build with all DNN ops, 16KB in full release build.

--- onnxruntime/core/providers/cuda/object_detection/non_max_suppression.cc
+++ onnxruntime/core/providers/cuda/object_detection/non_max_suppression.cc
@@ -33,15 +33,13 @@ ONNX_OPERATOR_KERNEL_EX(
 
 Status NonMaxSuppression::ComputeInternal(OpKernelContext* ctx) const {
   PrepareContext pc;
-  auto ret = PrepareCompute(ctx, pc);
-  ORT_RETURN_IF_NOT(ret.IsOK(), ret.ErrorMessage());
+  ORT_RETURN_IF_ERROR(PrepareCompute(ctx, pc));
 
   int64_t max_output_boxes_per_class = 0;
   float iou_threshold = .0f;
   float score_threshold = .0f;
 
-  ret = GetThresholdsFromInputs(pc, max_output_boxes_per_class, iou_threshold, score_threshold);
-  ORT_RETURN_IF_NOT(ret.IsOK(), ret.ErrorMessage());
+  ORT_RETURN_IF_ERROR(GetThresholdsFromInputs(pc, max_output_boxes_per_class, iou_threshold, score_threshold));
 
   if (0 == pc.num_boxes_ || 0 == max_output_boxes_per_class) {
     ctx->Output(0, {0, 3});
