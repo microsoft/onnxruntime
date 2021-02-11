1b0b1e9bfd08e98d4e167166c23aac4a4102861b
Changming Sun <chasun@microsoft.com>
2021-02-10 23:23:16 -0800

    csharp

--- csharp/test/Microsoft.ML.OnnxRuntime.EndToEndTests/runtest.sh
+++ csharp/test/Microsoft.ML.OnnxRuntime.EndToEndTests/runtest.sh
@@ -3,7 +3,6 @@
 # Licensed under the MIT License.
 
 LocalNuGetRepo=$1
-SourceRoot=$2
 BuildDir=$3
 export CurrentOnnxRuntimeVersion=$4
 IsMacOS=${5:-false}
@@ -15,22 +14,23 @@ RunTestNative=${RunTestNative:-true}
 set -x
 
 OldDir=`pwd`
-cd $SourceRoot
+cd $BUILD_SOURCESDIRECTORY
 
 echo "Current NuGet package version is $CurrentOnnxRuntimeVersion"
 
 if [ $RunTestCsharp = "true" ]; then
   if [ $IsMacOS = "true" ]; then
     mkdir -p $BUILD_BINARIESDIRECTORY/models
+	ln -s $BUILD_SOURCESDIRECTORY/cmake/external/onnx/onnx/backend/test/data/node $BUILD_BINARIESDIRECTORY/models/opset14
   fi
   # Run C# tests
-  dotnet restore $SourceRoot/csharp/test/Microsoft.ML.OnnxRuntime.EndToEndTests/Microsoft.ML.OnnxRuntime.EndToEndTests.csproj -s $LocalNuGetRepo -s https://api.nuget.org/v3/index.json
+  dotnet restore $BUILD_SOURCESDIRECTORY/csharp/test/Microsoft.ML.OnnxRuntime.EndToEndTests/Microsoft.ML.OnnxRuntime.EndToEndTests.csproj -s $LocalNuGetRepo -s https://api.nuget.org/v3/index.json
   if [ $? -ne 0 ]; then
     echo "Failed to restore nuget packages for the test project"
     exit 1
   fi
 
-  dotnet test $SourceRoot/csharp/test/Microsoft.ML.OnnxRuntime.EndToEndTests/Microsoft.ML.OnnxRuntime.EndToEndTests.csproj --no-restore --verbosity detailed
+  dotnet test $BUILD_SOURCESDIRECTORY/csharp/test/Microsoft.ML.OnnxRuntime.EndToEndTests/Microsoft.ML.OnnxRuntime.EndToEndTests.csproj --no-restore --verbosity detailed
   if [ $? -ne 0 ]; then
     echo "Failed to build or execute the end-to-end test"
     exit 1
@@ -51,20 +51,20 @@ if [ $RunTestNative = "true" ]; then
   if [ $IsMacOS = "true" ]; then
     export DYLD_FALLBACK_LIBRARY_PATH=$LocalNuGetRepo/_tmp:${DYLD_FALLBACK_LIBRARY_PATH}
     libs="-L runtimes/osx-x64/native -l onnxruntime"
-    g++ -std=c++11 $SourceRoot/csharp/test/Microsoft.ML.OnnxRuntime.EndToEndTests.Capi/C_Api_Sample.cpp $inc $libs -Wunused-result -Wformat=0 -o sampletest
+    g++ -std=c++11 $BUILD_SOURCESDIRECTORY/csharp/test/Microsoft.ML.OnnxRuntime.EndToEndTests.Capi/C_Api_Sample.cpp $inc $libs -Wunused-result -Wformat=0 -o sampletest
     libName=$(otool -L ./sampletest | grep onnxruntime | xargs | cut -d' ' -f1 | cut -d'/' -f2)
     ln -sf runtimes/osx-x64/native/libonnxruntime.dylib $libName
   else
     export LD_LIBRARY_PATH=$LocalNuGetRepo/_tmp:${LD_LIBRARY_PATH}
     libs="-L runtimes/linux-x86/native -L runtimes/linux-x64/native -l onnxruntime"
-    g++ -std=c++11 $SourceRoot/csharp/test/Microsoft.ML.OnnxRuntime.EndToEndTests.Capi/C_Api_Sample.cpp $inc $libs -Wunused-result -o sampletest
+    g++ -std=c++11 $BUILD_SOURCESDIRECTORY/csharp/test/Microsoft.ML.OnnxRuntime.EndToEndTests.Capi/C_Api_Sample.cpp $inc $libs -Wunused-result -o sampletest
     # Create link to versioned shared object required at runtime
     libname=`ldd sampletest | grep onnxruntime | xargs | cut -d" " -f1`
     ln -sf runtimes/linux-x64/native/libonnxruntime.so $libname
   fi
 
   # Copy Sample Model
-  cp $SourceRoot/csharp/testdata/squeezenet.onnx .
+  cp $BUILD_SOURCESDIRECTORY/csharp/testdata/squeezenet.onnx .
 
   # Run the sample model
   ./sampletest
