b5b950b6a7cc5e29ab3c376d9bd2ca812853e1ba
Olivia Jain <oljain@microsoft.com>
2021-02-12 13:58:21 -0800

    no cache

--- onnxruntime/python/tools/tensorrt/perf/build/build_images.sh
+++ onnxruntime/python/tools/tensorrt/perf/build/build_images.sh
@@ -10,5 +10,5 @@ i) IMAGE_NAME=${OPTARG};;
 esac
 done 
 
-sudo docker build -no-cache -t onnxruntime-trt --build-arg ONNXRUNTIME_BRANCH=$ORT_BRANCH -f $ORT_DOCKERFILE_PATH ..
-sudo docker build -no-cache -t $IMAGE_NAME -f $PERF_DOCKERFILE_PATH ..
+sudo docker build --no-cache -t onnxruntime-trt --build-arg ONNXRUNTIME_BRANCH=$ORT_BRANCH -f $ORT_DOCKERFILE_PATH ..
+sudo docker build --no-cache -t $IMAGE_NAME -f $PERF_DOCKERFILE_PATH ..
