84eaabd1aa7c26aa71a93eb31898d863c1f85168
Edward Chen <18449977+edgchen1@users.noreply.github.com>
2021-02-14 15:16:07 -0800

    Change BuildKernelDefConstraintsFunctorFromTypeList struct to BuildKernelDefConstraintsFromTypeList function. (#6674)

--- onnxruntime/core/providers/cpu/tensor/slice.cc
+++ onnxruntime/core/providers/cpu/tensor/slice.cc
@@ -33,10 +33,10 @@ using EnabledDataTypes = ORT_OP_KERNEL_ARG_ENABLED_TYPE_LIST_ALL_OPSETS(kCpuExec
 using EnabledIndicesTypes = ORT_OP_KERNEL_ARG_ENABLED_TYPE_LIST_ALL_OPSETS(kCpuExecutionProvider, kOnnxDomain,
                                                                            Slice, Input, 1);
 
-const auto supported_data_type_constraints = BuildKernelDefConstraintsFunctorFromTypeList<SupportedDataTypes>{}();
-const auto supported_indices_type_constraints = BuildKernelDefConstraintsFunctorFromTypeList<SupportedIndicesTypes>{}();
-const auto enabled_data_type_constraints = BuildKernelDefConstraintsFunctorFromTypeList<EnabledDataTypes>{}();
-const auto enabled_indices_type_constraints = BuildKernelDefConstraintsFunctorFromTypeList<EnabledIndicesTypes>{}();
+const auto supported_data_type_constraints = BuildKernelDefConstraintsFromTypeList<SupportedDataTypes>();
+const auto supported_indices_type_constraints = BuildKernelDefConstraintsFromTypeList<SupportedIndicesTypes>();
+const auto enabled_data_type_constraints = BuildKernelDefConstraintsFromTypeList<EnabledDataTypes>();
+const auto enabled_indices_type_constraints = BuildKernelDefConstraintsFromTypeList<EnabledIndicesTypes>();
 
 // std::clamp doesn't exist until C++17 so create a local version
 template <typename T>
