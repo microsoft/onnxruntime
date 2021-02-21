64beed8f5d0806dd1f3536c9e707a6dc6f884ff9
Adam Pocock <adam.pocock@oracle.com>
2021-02-20 20:47:37 -0500

    Adding the new CoreML flag.

--- java/src/main/java/ai/onnxruntime/providers/CoreMLFlags.java
+++ java/src/main/java/ai/onnxruntime/providers/CoreMLFlags.java
@@ -7,7 +7,8 @@ package ai.onnxruntime.providers;
 /** Flags for the CoreML provider. */
 public enum CoreMLFlags implements OrtFlags {
   CPU_ONLY(1), // COREML_FLAG_USE_CPU_ONLY(0x001)
-  ENABLE_ON_SUBGRAPH(2); // COREML_FLAG_ENABLE_ON_SUBGRAPH(0x002)
+  ENABLE_ON_SUBGRAPH(2), // COREML_FLAG_ENABLE_ON_SUBGRAPH(0x002)
+  ONLY_ENABLE_DEVICE_WITH_ANE(4); // COREML_FLAG_ONLY_ENABLE_DEVICE_WITH_ANE(0x004),
 
   public final int value;
 
