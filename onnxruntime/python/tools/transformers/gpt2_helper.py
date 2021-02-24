937427b2c669915da49f1e8581bbf4b1f5f774c0
Tianlei Wu <tlwu@microsoft.com>
2021-02-24 09:59:17 -0800

    Fix gpt2 onnx conversion for transformers 4.3.*. Add a check of transformer version >= 3.1.

--- onnxruntime/python/tools/transformers/gpt2_helper.py
+++ onnxruntime/python/tools/transformers/gpt2_helper.py
@@ -31,7 +31,7 @@ class GPT2ModelNoPastState(GPT2Model):
         super().__init__(config)
 
     def forward(self, input_ids):
-        return super().forward(input_ids, use_cache=False)
+        return super().forward(input_ids, use_cache=False, return_dict=False)
 
 
 class MyGPT2Model(GPT2Model):
@@ -40,11 +40,26 @@ class MyGPT2Model(GPT2Model):
     def __init__(self, config):
         super().__init__(config)
 
+    @staticmethod
+    def post_process(result, num_layer):
+        if isinstance(result[1][0], tuple) or isinstance(result[1][0], list):
+            assert len(result[1]) == num_layer and len(result[1][0]) == 2 #and len(result[1][0][0].shape) == 4 and result[1][0][0].shape == result[1][0][1].shape
+            present = []
+            for i in range(num_layer):
+                # Since transformers v4.*, past key and values are separated outputs.
+                # Here we concate them into one tensor to be compatible with Attention operator.
+                present.append(torch.cat((result[1][i][0].unsqueeze(0), result[1][i][1].unsqueeze(0)), dim=0))
+            return (result[0], tuple(present))
+
+        return result
+
     def forward(self, input_ids, position_ids, attention_mask, *past):
-        return super().forward(input_ids,
-                               position_ids=position_ids,
-                               attention_mask=attention_mask,
-                               past_key_values=past)
+        result = super().forward(input_ids,
+                                 position_ids=position_ids,
+                                 attention_mask=attention_mask,
+                                 past_key_values=past,
+                                 return_dict=False)
+        return MyGPT2Model.post_process(result, self.config.n_layer)
 
 
 class MyGPT2LMHeadModel(GPT2LMHeadModel):
@@ -54,10 +69,13 @@ class MyGPT2LMHeadModel(GPT2LMHeadModel):
         super().__init__(config)
 
     def forward(self, input_ids, position_ids, attention_mask, *past):
-        return super().forward(input_ids,
-                               position_ids=position_ids,
-                               attention_mask=attention_mask,
-                               past_key_values=past)
+        result = super().forward(input_ids,
+                                 position_ids=position_ids,
+                                 attention_mask=attention_mask,
+                                 past_key_values=past,
+                                 return_dict=False)
+
+        return MyGPT2Model.post_process(result, self.config.n_layer)
 
 
 class MyGPT2LMHeadModel_NoPadding(GPT2LMHeadModel):
@@ -216,6 +234,7 @@ class Gpt2Helper:
 
         is_all_close = is_close
         num_layers = len(ort_outputs) - 1
+
         for layer in range(num_layers):
             is_close = numpy.allclose(ort_outputs[1 + layer],
                                       torch_outputs[1][layer].cpu().numpy(),
@@ -288,10 +307,12 @@ class Gpt2Helper:
             input_names.append('attention_mask')
         input_names.extend(past_names)
 
+        assert len(outputs) == 2 and len(outputs[1]) == num_layer
+
         logger.info(
             f"Shapes: input_ids={dummy_inputs.input_ids.shape} past={dummy_inputs.past[0].shape} output={outputs[0].shape} present={outputs[1][0].shape}"
         )
-
+    
         Path(onnx_model_path).parent.mkdir(parents=True, exist_ok=True)
 
         torch.onnx.export(model,
