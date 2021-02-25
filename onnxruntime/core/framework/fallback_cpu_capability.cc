9f70f5a1f5095f3ee2fcb39ff6a6d3bc9db62ba0
Hariharan Seshadri <shariharan91@gmail.com>
2021-02-25 10:21:31 -0800

    Trial

--- onnxruntime/core/framework/fallback_cpu_capability.cc
+++ onnxruntime/core/framework/fallback_cpu_capability.cc
@@ -24,8 +24,8 @@ bool IsSmallInitializerWithSingleConsumer(const onnxruntime::GraphViewer& graph,
   for (auto& dim : initializer_tensor->dims()) {
     size *= dim;
   }
-  return size <= Small_Initializer_Threshold &&
-         graph.GetConsumerNodes(arg->Name()).size() == 1;
+
+  return size <= Small_Initializer_Threshold;
 }
 }  // namespace
 
@@ -100,6 +100,11 @@ std::unordered_set<NodeIndex> GetCpuPreferredNodes(const onnxruntime::GraphViewe
       continue;
 
     auto* node = graph.GetNode(cur);
+    if (node->Name() == "na_speech_model/parallel_0_3/na_speech_model/na_speech_model/body/s_decoder/layer_5/ffn/conv_relu_conv/conv1_7/Tensordot/GatherV2") {
+      float f = 1.f / 2;
+      ORT_IGNORE_RETURN_VALUE(f);
+    }
+
     bool place_in_cpu = true;
     for (size_t i = 0; i < node->InputDefs().size(); ++i) {
       auto* input = node->InputDefs()[i];
