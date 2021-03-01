06b2a216b31e6a470269b714997243572380db15
Vincent Wang <wangwchpku@outlook.com>
2021-03-01 13:13:36 +0800

    fix ut after merge from master

--- onnxruntime/test/framework/execution_frame_test.cc
+++ onnxruntime/test/framework/execution_frame_test.cc
@@ -270,7 +270,13 @@ TEST_F(ExecutionFrameTest, MemPatternWithExternalOutputsTest) {
       yield_out_def("T", &tensor_float),
       gemm_out_def("Y", &tensor_float);
 
-  graph.AddNode("node1", "YieldOp", "yield", ArgMap{&input_def}, ArgMap{&yield_out_def}, nullptr, kMSDomain)
+  ONNX_NAMESPACE::AttributeProto required_grad;
+  const std::string attribute_name = "required_grad";
+  required_grad.set_name(attribute_name);
+  required_grad.set_type(ONNX_NAMESPACE::AttributeProto::INTS);
+  required_grad.add_ints(static_cast<int64_t>(0));
+  NodeAttributes attributes({{attribute_name, required_grad}});
+  graph.AddNode("node1", "YieldOp", "yield", ArgMap{&input_def}, ArgMap{&yield_out_def}, &attributes, kMSDomain)
       .SetExecutionProviderType(xp_type);
   // Add another node after YieldOp as YieldOp should not be graph output.
   graph.AddNode("node2", "MatMul", "gemm1", ArgMap{&yield_out_def, &input_def}, ArgMap{&gemm_out_def})
