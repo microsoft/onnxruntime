4cd2c7ad755087e39a581180b00959955d547b70
Chi Lo <Chi.Lo@microsoft.com>
2021-03-04 02:01:44 -0800

    Fix bug of "No module named:onnxruntime.quantization.CalTableFlatBuffers"

---
+++ onnxruntime/python/tools/quantization/setup.py
@@ -0,0 +1,8 @@
+# setup.py
+from setuptools import setup
+
+setup(
+   name='quantization',
+   description='Depedencies of ORT quantization',
+   install_requires=['flatbuffers'], #external packages as dependencies
+)
