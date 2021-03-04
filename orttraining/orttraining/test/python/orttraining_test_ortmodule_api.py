bc222aaa715149a9c3a344cab219faa7ac3c5f91
Sherlock Huang <bahuang@OrtTrainingDev3.af05slrtruoetgaxwwjv5nsq5e.px.internal.cloudapp.net>
2021-03-04 01:00:35 +0000

    Found one more failing case

--- orttraining/orttraining/test/python/orttraining_test_ortmodule_api.py
+++ orttraining/orttraining/test/python/orttraining_test_ortmodule_api.py
@@ -649,7 +649,7 @@ def test_input_requires_grad_backward_creates_input_grad_as_required1(x1_require
     N, D_in, H, D_out = 32, 784, 500, 10
     device = 'cuda'
     pt_model = NeuralNetMultiplePositionalArgumentsMultipleOutputs1(D_in, H, D_out).to(device)
-    ort_model = ORTModule(pt_model)
+    ort_model = ORTModule(copy.deepcopy(pt_model))
     pt_x1 = torch.randn(N, D_in, device=device, requires_grad=x1_requires_grad)
     pt_x2 = torch.randn(N, D_in, device=device, requires_grad=x2_requires_grad)
 
@@ -658,9 +658,11 @@ def test_input_requires_grad_backward_creates_input_grad_as_required1(x1_require
     ort_x1.requires_grad = x1_requires_grad
     ort_x2.requires_grad = x2_requires_grad
 
-    run_step(pt_model, pt_x1, pt_x2)
-    run_step(ort_model, ort_x1, ort_x2)
+    pt_y1, pt_y2 = run_step(pt_model, pt_x1, pt_x2)
+    ort_y1, ort_y2 = run_step(ort_model, ort_x1, ort_x2)
 
+    # assert torch.allclose(ort_y1, pt_y1)  # TODO: this assert is failing, need to investigate!!
+    assert torch.allclose(ort_y2, pt_y2)
     assert not x1_requires_grad or ort_x1.grad is not None
     assert not x2_requires_grad or ort_x2.grad is not None
     assert not x1_requires_grad or torch.allclose(ort_x1.grad, pt_x1.grad)
