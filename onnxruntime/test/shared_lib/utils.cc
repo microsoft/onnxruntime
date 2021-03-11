32839ee9afc7d72da183c0cd8bddd166ad0cca44
Hariharan Seshadri <shariharan91@gmail.com>
2021-03-11 03:03:26 -0800

    fix nit2

--- onnxruntime/test/shared_lib/utils.cc
+++ onnxruntime/test/shared_lib/utils.cc
@@ -1,8 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-#pragma once
-
 #include "utils.h"
 
 OrtCUDAProviderOptions CreateDefaultOrtCudaProviderOptionsWithCustomStream(void* cuda_compute_stream) {
