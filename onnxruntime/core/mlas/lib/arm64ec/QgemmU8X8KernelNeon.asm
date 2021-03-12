fabda28523126073952c6d69391ba8988d6d938c
Ben Niu <beniu@microsoft.com>
2021-03-12 11:27:04 -0800

    Add ARM64EC assemblies

---
+++ onnxruntime/core/mlas/lib/arm64ec/QgemmU8X8KernelNeon.asm
@@ -0,0 +1,18 @@
+/*++
+
+Copyright (c) Microsoft Corporation. All rights reserved.
+
+Licensed under the MIT License.
+
+Module Name:
+
+    QgemmU8X8KernelNeon.asm
+
+Abstract:
+
+    This module implements the kernels for the quantized integer matrix/matrix
+    multiply operation (QGEMM).
+
+--*/
+
+#include "..\arm64\QgemmU8X8KernelNeon.asm"
---
+++ onnxruntime/core/mlas/lib/arm64ec/SgemmKernelNeon.asm
@@ -0,0 +1,18 @@
+;++
+;
+; Copyright (c) Microsoft Corporation. All rights reserved.
+;
+; Licensed under the MIT License.
+;
+; Module Name:
+;
+;   SgemmKernelNeon.asm
+;
+; Abstract:
+;
+;   This module implements the kernels for the single precision matrix/matrix
+;   multiply operation (SGEMM).
+;
+;--
+
+#include "..\arm64\SgemmKernelNeon.asm"
