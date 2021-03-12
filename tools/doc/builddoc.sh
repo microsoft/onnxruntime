5773b16381f4e0d59ab756223211804f521678fc
Xavier Dupré <xadupre@users.noreply.github.com>
2021-03-12 04:02:45 +0100

    Automate generation of python documentation (#6909)
    Co-authored-by: xavier dupré <xavier.dupre@gmail.com>

--- tools/doc/builddoc.sh
+++ tools/doc/builddoc.sh
@@ -1,13 +1,10 @@
 # This script must be executed from this folder.
-pip install -r ../../docs/python/requirements.txt
 
-# Delete old docs
-rm -rf ../../build/docs
-
-# Inference doc
-python -m sphinx -j2 -v -T -b html -d ../../build/docs/inference/_doctrees/html ../../docs/python/inference/ ../../build/docs/inference/html
-python -u rename_folders.py ../../build/docs/inference/html
-
-# Training doc
-python -m sphinx -j2 -v -T -b html -d ../../build/docs/training/_doctrees/html ../../docs/python/training ../../build/docs/training/html
-python -u rename_folders.py ../../build/docs/training/html
+# $1 python path
+# $2 source folder
+# $3 build folder
+$1/python -m pip install -r $2/docs/python/requirements.txt
+export PYTHONPATH=$3/Release/Release:$PYTHONPATH
+$1/python -m sphinx -j2 -v -T -b html -d $3/docs/_doctrees/html $2/docs/python $3/docs/html
+$1/python -u rename_folders.py $3/docs/html
+zip -r $3/python_doc.zip $3/docs/html
