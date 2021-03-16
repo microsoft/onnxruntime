8866e5256c7f5ba84b8d2430fd82189dcd74215b
xavier dupré <xavier.dupre@gmail.com>
2021-03-16 22:37:29 +0100

    update path

--- tools/doc/builddoc.sh
+++ tools/doc/builddoc.sh
@@ -2,8 +2,11 @@
 # $1 python path
 # $2 source folder
 # $3 build folder
+echo $1
+echo $2
+echo $3
 $1/python -m pip install -r $2/docs/python/requirements.txt
 export PYTHONPATH=$3/Release/Release:$PYTHONPATH
 $1/python -m sphinx -j2 -v -T -b html -d $3/docs/_doctrees/html $2/docs/python $3/docs/html
-$1/python -u rename_folders.py $3/docs/html
-zip -r $3/python_doc.zip $3/docs/html
+$1/python -u $2/tools/docs/rename_folders.py $3/docs/html
+# zip -r $3/python_doc.zip $3/docs/html
