332ac419fb0fe407f54dbb4d0b56244d9ecfa316
Peng Wang <pengwa@microsoft.com>
2021-03-18 11:38:23 +0000

    Format for making subsquent changes cleaner

--- onnxruntime/core/language_interop_ops/pyop/pyop.cc
+++ onnxruntime/core/language_interop_ops/pyop/pyop.cc
@@ -36,7 +36,6 @@
 #include <unordered_map>
 
 using namespace std;
-
 namespace onnxruntime {
 
 PyOpLibProxy& PyOpLibProxy::GetInstance() {
@@ -44,193 +43,193 @@ PyOpLibProxy& PyOpLibProxy::GetInstance() {
   return proxy;
 }
 
-class Scope
-{
-public:
-    Scope(const vector<PyObject*>& objs = {}): objs_(objs) {
-        mtx_.lock();
+class Scope {
+ public:
+  Scope(const vector<PyObject*>& objs = {}) : objs_(objs) {
+    mtx_.lock();
+  }
+  ~Scope() {
+    for (auto obj : objs_) {
+      Py_XDECREF(obj);
     }
-    ~Scope() {
-        for (auto obj: objs_) {
-            Py_XDECREF(obj);
-        }
-        mtx_.unlock();
-    }
-    void Add(PyObject* obj) {
-        objs_.push_back(obj);
-    }
-private:
-    static std::mutex mtx_;
-    vector<PyObject*> objs_;
+    mtx_.unlock();
+  }
+  void Add(PyObject* obj) {
+    objs_.push_back(obj);
+  }
+
+ private:
+  static std::mutex mtx_;
+  vector<PyObject*> objs_;
 };
 
 PyOpLibProxy::PyOpLibProxy() {
-    Scope scope;
-    Py_Initialize();
-    if (_import_array() < 0) {
-        return;
-    }
-    auto path_list = PySys_GetObject("path");//do not release it
-    if (nullptr == path_list || !PyList_Check(path_list) ||
-        PyList_Append(path_list, PyUnicode_FromString(".")) != 0) {
-        return;
-    }
-    initialized_ = true;
+  Scope scope;
+  Py_Initialize();
+  if (_import_array() < 0) {
+    return;
+  }
+  auto path_list = PySys_GetObject("path");  //do not release it
+  if (nullptr == path_list || !PyList_Check(path_list) ||
+      PyList_Append(path_list, PyUnicode_FromString(".")) != 0) {
+    return;
+  }
+  initialized_ = true;
 }
 
 PyOpLibProxy::~PyOpLibProxy() {
-    if (initialized_) {
-        Py_Finalize();
-    }
+  if (initialized_) {
+    Py_Finalize();
+  }
 }
 
 std::mutex Scope::mtx_;
 
 const char* PyOpLibProxy::GetLastErrorMessage(std::string& err) {
-    Scope scope;
-    if (PyErr_Occurred()) {
-        PyObject *type, *value, *trace;
-        PyErr_Fetch(&type, &value, &trace);
-        if (nullptr != value) {
-            auto pyVal = PyObject_Repr(value);
-            scope.Add(pyVal);
-            auto pyStr = PyUnicode_AsEncodedString(pyVal, "utf-8", "Error ~");
-            scope.Add(pyStr);
-            err = PyBytes_AS_STRING(pyStr);
-        }
-        PyErr_Restore(type, value, trace);
+  Scope scope;
+  if (PyErr_Occurred()) {
+    PyObject *type, *value, *trace;
+    PyErr_Fetch(&type, &value, &trace);
+    if (nullptr != value) {
+      auto pyVal = PyObject_Repr(value);
+      scope.Add(pyVal);
+      auto pyStr = PyUnicode_AsEncodedString(pyVal, "utf-8", "Error ~");
+      scope.Add(pyStr);
+      err = PyBytes_AS_STRING(pyStr);
     }
-    return err.c_str();
+    PyErr_Restore(type, value, trace);
+  }
+  return err.c_str();
 }
 
 int32_t PyOpLibProxy::GetGil() const {
-    return PyGILState_Ensure();
+  return PyGILState_Ensure();
 }
 
 void PyOpLibProxy::PutGil(int32_t state) const {
-   PyGILState_Release((PyGILState_STATE)state);
+  PyGILState_Release((PyGILState_STATE)state);
 }
 
 PyObject* MakePyObj(const void* data, int32_t type, const vector<int64_t>& dim) {
-    std::vector<npy_intp> np_dim;
-    for (auto d: dim) {
-        np_dim.push_back(static_cast<npy_intp>(d));
-    }
-    auto pyObj = static_cast<PyObject*>(PyArray_EMPTY(static_cast<int>(np_dim.size()), np_dim.data(), type, 0));
-    auto data_len = std::accumulate(begin(np_dim), end(np_dim),
-                                    static_cast<int64_t>(PyArray_DescrFromType(type)->elsize),
-                                    std::multiplies<int64_t>());
-    auto np_array = reinterpret_cast<PyArrayObject*>(pyObj);
-    memcpy(PyArray_DATA(np_array), data, data_len);
-    return pyObj;
+  std::vector<npy_intp> np_dim;
+  for (auto d : dim) {
+    np_dim.push_back(static_cast<npy_intp>(d));
+  }
+  auto pyObj = static_cast<PyObject*>(PyArray_EMPTY(static_cast<int>(np_dim.size()), np_dim.data(), type, 0));
+  auto data_len = std::accumulate(begin(np_dim), end(np_dim),
+                                  static_cast<int64_t>(PyArray_DescrFromType(type)->elsize),
+                                  std::multiplies<int64_t>());
+  auto np_array = reinterpret_cast<PyArrayObject*>(pyObj);
+  memcpy(PyArray_DATA(np_array), data, data_len);
+  return pyObj;
 }
 
-bool ExtractOutput(PyObject*                   pyObj,
+bool ExtractOutput(PyObject* pyObj,
                    vector<unique_ptr<char[]>>& outputs,
-                   vector<int32_t>&            outputs_elem_size,
-                   vector<vector<int64_t>>&    outputs_dim) {
-    if (!PyArray_Check(pyObj)) {
-        return false;
-    }
+                   vector<int32_t>& outputs_elem_size,
+                   vector<vector<int64_t>>& outputs_dim) {
+  if (!PyArray_Check(pyObj)) {
+    return false;
+  }
 
-    outputs_dim.push_back({});
-    auto np_array = reinterpret_cast<PyArrayObject*>(pyObj);
-    outputs_elem_size.push_back(static_cast<int32_t>(PyArray_ITEMSIZE(np_array)));
+  outputs_dim.push_back({});
+  auto np_array = reinterpret_cast<PyArrayObject*>(pyObj);
+  outputs_elem_size.push_back(static_cast<int32_t>(PyArray_ITEMSIZE(np_array)));
 
-    for (int i = 0; i < PyArray_NDIM(np_array); ++i) {
-        outputs_dim.back().push_back(PyArray_SHAPE(np_array)[i]);
-    }
+  for (int i = 0; i < PyArray_NDIM(np_array); ++i) {
+    outputs_dim.back().push_back(PyArray_SHAPE(np_array)[i]);
+  }
 
-    auto data_len = std::accumulate(begin(outputs_dim.back()),
-                                    end(outputs_dim.back()),
-                                    static_cast<int64_t>(outputs_elem_size.back()),
-                                    std::multiplies<int64_t>());
+  auto data_len = std::accumulate(begin(outputs_dim.back()),
+                                  end(outputs_dim.back()),
+                                  static_cast<int64_t>(outputs_elem_size.back()),
+                                  std::multiplies<int64_t>());
 
-    outputs.push_back(unique_ptr<char[]>(new char[data_len]));
-    memcpy(static_cast<void*>(outputs.back().get()), PyArray_DATA(np_array), data_len);
-    return true;
+  outputs.push_back(unique_ptr<char[]>(new char[data_len]));
+  memcpy(static_cast<void*>(outputs.back().get()), PyArray_DATA(np_array), data_len);
+  return true;
 }
 
 void* PyOpLibProxy::NewInstance(const char* module, const char* class_name, const unordered_map<string, string>& args) {
-    Scope scope; 
-    auto pyModule = PyImport_ImportModule(module);
-    if (nullptr == pyModule) {
-        return nullptr;
-    }
+  Scope scope;
+  auto pyModule = PyImport_ImportModule(module);
+  if (nullptr == pyModule) {
+    return nullptr;
+  }
 
-    scope.Add(pyModule);
-    auto pyClass = PyObject_GetAttrString(pyModule, class_name);
-    if (nullptr == pyClass) {
-        return nullptr;
-    }
+  scope.Add(pyModule);
+  auto pyClass = PyObject_GetAttrString(pyModule, class_name);
+  if (nullptr == pyClass) {
+    return nullptr;
+  }
 
-    scope.Add(pyClass);
-    auto empty_args = PyTuple_New(0);
-    scope.Add(empty_args);
-    auto named_args = PyDict_New();
-    scope.Add(named_args);
-    for (const auto& iter: args) {
-        PyDict_SetItemString(named_args, iter.first.c_str(), PyUnicode_FromString(iter.second.c_str()));
-    }
+  scope.Add(pyClass);
+  auto empty_args = PyTuple_New(0);
+  scope.Add(empty_args);
+  auto named_args = PyDict_New();
+  scope.Add(named_args);
+  for (const auto& iter : args) {
+    PyDict_SetItemString(named_args, iter.first.c_str(), PyUnicode_FromString(iter.second.c_str()));
+  }
 
-    return PyObject_Call(pyClass, empty_args, named_args);
+  return PyObject_Call(pyClass, empty_args, named_args);
 }
 
 void PyOpLibProxy::ReleaseInstance(void* instance) {
-    Scope scope({static_cast<PyObject*>(instance)});
+  Scope scope({static_cast<PyObject*>(instance)});
 }
 
-bool PyOpLibProxy::InvokePythonFunc(void*                            raw_inst,
-                                    const char*                      function,
-                                    const vector<const void*>&       inputs,
-                                    const vector<int32_t>&           inputs_type,
-                                    const vector<vector<int64_t>>&   inputs_dim,
-                                    vector<unique_ptr<char[]>>&      outputs,
-                                    vector<int32_t>&                 outputs_elem_size,
-                                    vector<vector<int64_t>>&         outputs_dim,
+bool PyOpLibProxy::InvokePythonFunc(void* raw_inst,
+                                    const char* function,
+                                    const vector<const void*>& inputs,
+                                    const vector<int32_t>& inputs_type,
+                                    const vector<vector<int64_t>>& inputs_dim,
+                                    vector<unique_ptr<char[]>>& outputs,
+                                    vector<int32_t>& outputs_elem_size,
+                                    vector<vector<int64_t>>& outputs_dim,
                                     std::function<void(const char*)> logging_func) {
-    Scope scope;
-    auto instance = static_cast<PyObject*>(raw_inst);
-    if (nullptr == instance || nullptr == function) {
-        logging_func("InvokePythonFunc: found invalid instance or function");
-        return false;
-    }
+  Scope scope;
+  auto instance = static_cast<PyObject*>(raw_inst);
+  if (nullptr == instance || nullptr == function) {
+    logging_func("InvokePythonFunc: found invalid instance or function");
+    return false;
+  }
 
-    auto pyFunc = PyObject_GetAttrString(instance, function);
-    if (nullptr == pyFunc) {
-        logging_func("InvokePythonFunc: failed to create function object");
-        return false;
-    }
+  auto pyFunc = PyObject_GetAttrString(instance, function);
+  if (nullptr == pyFunc) {
+    logging_func("InvokePythonFunc: failed to create function object");
+    return false;
+  }
 
-    scope.Add(pyFunc);
-    auto pyArgs = PyTuple_New(inputs.size());
-    for (size_t i = 0; i < inputs.size(); ++i) {
-        PyTuple_SetItem(pyArgs, i, MakePyObj(inputs[i], inputs_type[i], inputs_dim[i]));
-    }
+  scope.Add(pyFunc);
+  auto pyArgs = PyTuple_New(inputs.size());
+  for (size_t i = 0; i < inputs.size(); ++i) {
+    PyTuple_SetItem(pyArgs, i, MakePyObj(inputs[i], inputs_type[i], inputs_dim[i]));
+  }
 
-    scope.Add(pyArgs);
-    auto pyResult = PyEval_CallObject(pyFunc, pyArgs);
-    if (nullptr == pyResult) {
-        logging_func("InvokePythonFunc: no result");
-        return false;
-    }
+  scope.Add(pyArgs);
+  auto pyResult = PyEval_CallObject(pyFunc, pyArgs);
+  if (nullptr == pyResult) {
+    logging_func("InvokePythonFunc: no result");
+    return false;
+  }
 
-    scope.Add(pyResult);
-    if (PyArray_Check(pyResult)) {
-        ExtractOutput(pyResult, outputs, outputs_elem_size, outputs_dim);
-    } else if (PyTuple_Check(pyResult)) {
-        for (int32_t i = 0; i < PyTuple_Size(pyResult); ++i) {
-            if (!ExtractOutput(PyTuple_GetItem(pyResult, i), outputs, outputs_elem_size, outputs_dim)) {
-                logging_func("InvokePythonFunc: failed to extract output");
-                return false;
-            }
-        }
-    } else {
-        logging_func("InvokePythonFunc: returned value must be numpy(s)");
+  scope.Add(pyResult);
+  if (PyArray_Check(pyResult)) {
+    ExtractOutput(pyResult, outputs, outputs_elem_size, outputs_dim);
+  } else if (PyTuple_Check(pyResult)) {
+    for (int32_t i = 0; i < PyTuple_Size(pyResult); ++i) {
+      if (!ExtractOutput(PyTuple_GetItem(pyResult, i), outputs, outputs_elem_size, outputs_dim)) {
+        logging_func("InvokePythonFunc: failed to extract output");
         return false;
+      }
     }
-    return true;
-}//bool InvokePythonFunc
+  } else {
+    logging_func("InvokePythonFunc: returned value must be numpy(s)");
+    return false;
+  }
+  return true;
+}  //bool InvokePythonFunc
 
 PyCustomKernel::PyCustomKernel(Ort::CustomOpApi ort,
                                const OnnxAttrs& attrs,
--- onnxruntime/core/language_interop_ops/pyop/pyop.h
+++ onnxruntime/core/language_interop_ops/pyop/pyop.h
@@ -1,6 +1,6 @@
 #pragma once
 #include "core/platform/env.h"
-#define LOAD_PYOP_SYM(n,v,m) ORT_ENFORCE(Env::Default().GetSymbolFromLibrary(handle_,n,reinterpret_cast<void**>(&v))==Status::OK(),m)
+#define LOAD_PYOP_SYM(n, v, m) ORT_ENFORCE(Env::Default().GetSymbolFromLibrary(handle_, n, reinterpret_cast<void**>(&v)) == Status::OK(), m)
 #include "core/framework/ml_value.h"
 #include "core/session/onnxruntime_cxx_api.h"
 #include "core/framework/op_kernel_context_internal.h"
@@ -15,81 +15,81 @@
 
 namespace onnxruntime {
 
-using OnnxTypes   = std::vector<ONNXTensorElementDataType>;
-using OnnxAttrs   = std::unordered_map<std::string, std::string>;
+using OnnxTypes = std::vector<ONNXTensorElementDataType>;
+using OnnxAttrs = std::unordered_map<std::string, std::string>;
 using PyOpLogFunc = std::function<void(const char*)>;
 
 class PyOpLibProxy {
+ public:
+  static PyOpLibProxy& GetInstance();
+  void ReleaseInstance(void*);
+  bool InvokePythonFunc(void*,
+                        const char*,
+                        const std::vector<const void*>&,
+                        const std::vector<int32_t>&,
+                        const std::vector<std::vector<int64_t>>&,
+                        std::vector<std::unique_ptr<char[]>>&,
+                        std::vector<int32_t>&,
+                        std::vector<std::vector<int64_t>>&,
+                        std::function<void(const char*)>);
+  const char* GetLastErrorMessage(std::string&);
+  void* NewInstance(const char*, const char*, const OnnxAttrs&);
+  bool Initialized() const { return initialized_; }
+  int32_t GetGil() const;
+  void PutGil(int32_t) const;
 
-public:
-    static PyOpLibProxy& GetInstance();
-    void ReleaseInstance(void*);
-    bool InvokePythonFunc(void*,
-                          const char*,
-                          const std::vector<const void*>&,
-                          const std::vector<int32_t>&,
-                          const std::vector<std::vector<int64_t>>&,
-                          std::vector<std::unique_ptr<char[]>>&,
-                          std::vector<int32_t>&,
-                          std::vector<std::vector<int64_t>>&,
-                          std::function<void(const char*)>);
-    const char* GetLastErrorMessage(std::string&);
-    void* NewInstance(const char*, const char*, const OnnxAttrs&);
-    bool Initialized() const { return initialized_; }
-    int32_t GetGil() const;
-    void PutGil(int32_t) const;
-private:
-    PyOpLibProxy();
-    ~PyOpLibProxy();
-    bool initialized_ = false;
+ private:
+  PyOpLibProxy();
+  ~PyOpLibProxy();
+  bool initialized_ = false;
 };
 
 struct PyCustomKernel {
+  PyCustomKernel(Ort::CustomOpApi ort,
+                 const OnnxAttrs& attrs,
+                 const std::string& module,
+                 const std::string& class_name,
+                 const std::string& compute,
+                 PyOpLogFunc logging_func);
+  ~PyCustomKernel();
+  void GetOutputShape(OrtKernelContext*, size_t, OrtTensorTypeAndShapeInfo*);
+  void Compute(OrtKernelContext* context);
+  int32_t GetType(const OrtValue* input) const;
 
-    PyCustomKernel(Ort::CustomOpApi   ort,
-                   const OnnxAttrs&   attrs,
-                   const std::string& module,
-                   const std::string& class_name,
-                   const std::string& compute,
-                   PyOpLogFunc        logging_func);
-    ~PyCustomKernel();
-    void    GetOutputShape(OrtKernelContext*, size_t, OrtTensorTypeAndShapeInfo*);
-    void    Compute(OrtKernelContext* context);
-    int32_t GetType(const OrtValue* input) const;
-private:
-    Ort::CustomOpApi ort_;
-    OnnxAttrs        attrs_;
-    std::string      module_;
-    std::string      class_name_;
-    std::string      compute_;
-    void*            instance_ = nullptr;
-    PyOpLogFunc      logging_func_;
+ private:
+  Ort::CustomOpApi ort_;
+  OnnxAttrs attrs_;
+  std::string module_;
+  std::string class_name_;
+  std::string compute_;
+  void* instance_ = nullptr;
+  PyOpLogFunc logging_func_;
 };
 
-struct PyCustomOp: Ort::CustomOpBase<PyCustomOp, PyCustomKernel> {
+struct PyCustomOp : Ort::CustomOpBase<PyCustomOp, PyCustomKernel> {
+  PyCustomOp(const OnnxAttrs& attrs,
+             const OnnxTypes& inputs_type,
+             const OnnxTypes& outputs_type,
+             const std::string& module,
+             const std::string& class_name,
+             const std::string& compute = "compute",
+             PyOpLogFunc logging_func = [](const char*) {});
+  void* CreateKernel(Ort::CustomOpApi api, const OrtKernelInfo*) const;
+  const char* GetName() const;
+  size_t GetInputTypeCount() const;
+  ONNXTensorElementDataType GetInputType(size_t index) const;
+  size_t GetOutputTypeCount() const;
+  ONNXTensorElementDataType GetOutputType(size_t index) const;
 
-    PyCustomOp(const OnnxAttrs&    attrs,
-               const OnnxTypes&    inputs_type,
-               const OnnxTypes&    outputs_type,
-               const std::string&  module,
-               const std::string&  class_name,
-               const std::string&  compute      = "compute",
-               PyOpLogFunc         logging_func = [](const char*){});
-    void* CreateKernel(Ort::CustomOpApi api, const OrtKernelInfo*) const;
-    const char* GetName() const;
-    size_t GetInputTypeCount() const;
-    ONNXTensorElementDataType GetInputType(size_t index) const;
-    size_t GetOutputTypeCount() const;
-    ONNXTensorElementDataType GetOutputType(size_t index) const;
-private:
-    OnnxAttrs      attrs_;
-    OnnxTypes      inputs_type_;
-    OnnxTypes      outputs_type_;
-    std::string    module_;
-    std::string    class_name_;
-    std::string    compute_;
-    PyOpLogFunc    logging_func_;
-};//struct PyCustomOp
+ private:
+  OnnxAttrs attrs_;
+  OnnxTypes inputs_type_;
+  OnnxTypes outputs_type_;
+  std::string module_;
+  std::string class_name_;
+  std::string compute_;
+  PyOpLogFunc logging_func_;
+};  //struct PyCustomOp
 
 PyCustomOp* LoadPyOp(const ONNX_NAMESPACE::NodeProto& node_proto, PyOpLogFunc log_func);
-}//namespace onnxruntime
+}  //namespace onnxruntime
