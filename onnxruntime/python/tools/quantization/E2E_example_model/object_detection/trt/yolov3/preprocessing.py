8c3b59a0263eca2c45939fbaa46a47e757f94d9a
Chi Lo <54722500+chilo-ms@users.noreply.github.com>
2021-03-19 01:09:11 -0700

    Quantization calibration refactor (#6893)
    * Code refactor
    
    * Modify code to tackle OOM when calibrating on larget dataset
    
    * Fix mismatch issue when setting keepdims on ReduceMin/ReduceMax
    
    * Add COCO val 2017 annotation
    
    * Fix mismatch issue when setting keepdims on ReduceMin/ReduceMax
    
    * Fix bug of "No module named:onnxruntime.quantization.CalTableFlatBuffers"
    
    * Check and install flatbuffers module
    
    * Add script to donwload coco dataset image and refactor example
    
    * Fix bug of "No module
    named:onnxruntime.quantization.CalTableFlatBuffers"
    
    * Add CalTableFaltBuffers as module
    
    * Remove annotation, user can download by themselves.
    
    * Uncommet code
    
    * Add back instances_val2017.json
    
    * Make sure flatbuffers installed when ORT is installed
    
    * Refactor code to call coco api
    
    * Enable FP16 for example

--- onnxruntime/python/tools/quantization/E2E_example_model/object_detection/trt/yolov3/preprocessing.py
+++ onnxruntime/python/tools/quantization/E2E_example_model/object_detection/trt/yolov3/preprocessing.py
@@ -133,65 +133,6 @@ def yolov3_variant_preprocess_func(images_folder, height, width, start_index=0, 
     parameter size_limit: number of images to load. Default is 0 which means all images are picked.
     return: list of matrices characterizing multiple images
     '''
-    # this function is from yolo3.utils.letterbox_image
-    # https://github.com/qqwweee/keras-yolo3/blob/master/yolo3/utils.py
-    def letterbox_image(image, size):
-        '''resize image with unchanged aspect ratio using padding'''
-        iw, ih = image.size
-        w, h = size
-        scale = min(w/iw, h/ih)
-        nw = int(iw*scale)
-        nh = int(ih*scale)
-
-        image = image.resize((nw,nh), Image.BICUBIC)
-        new_image = Image.new('RGB', size, (128,128,128))
-        new_image.paste(image, ((w-nw)//2, (h-nh)//2))
-        return new_image
-
-    image_names = os.listdir(images_folder)
-    if start_index >= len(image_names):
-        return np.asanyarray([]), np.asanyarray([]), np.asanyarray([])
-    elif size_limit > 0 and len(image_names) >= size_limit:
-        end_index = start_index + size_limit
-        if end_index > len(image_names):
-            end_index = len(image_names)
-
-        batch_filenames = [image_names[i] for i in range(start_index, end_index)]
-    else:
-        batch_filenames = image_names
-
-
-    unconcatenated_batch_data = []
-    image_size_list = []
-
-    print(batch_filenames)
-    print("size: %s" % str(len(batch_filenames)))
-
-    for image_name in batch_filenames:
-        image_filepath = images_folder + '/' + image_name
-        img = Image.open(image_filepath) 
-        model_image_size = (height, width)
-        boxed_image = letterbox_image(img, tuple(reversed(model_image_size)))
-        image_data = np.array(boxed_image, dtype='float32')
-        image_data /= 255.
-        image_data = np.transpose(image_data, [2, 0, 1])
-        image_data = np.expand_dims(image_data, 0)
-        unconcatenated_batch_data.append(image_data)
-        image_size_list.append((img.size[1], img.size[0]))  # img.shape is h, w, c
-        # image_size_list.append(np.array([img.size[1], img.size[0]], dtype=np.float32).reshape(1, 2))
-
-    batch_data = np.concatenate(np.expand_dims(unconcatenated_batch_data, axis=0), axis=0)
-    return batch_data, batch_filenames, image_size_list
-
-def yolov3_variant_preprocess_func_2(images_folder, height, width, start_index=0, size_limit=0):
-    '''
-    Loads a batch of images and preprocess them
-    parameter images_folder: path to folder storing images
-    parameter height: image height in pixels
-    parameter width: image width in pixels
-    parameter size_limit: number of images to load. Default is 0 which means all images are picked.
-    return: list of matrices characterizing multiple images
-    '''
 
     # reference from here:
     # https://github.com/jkjung-avt/tensorrt_demos/blob/3fb15c908b155d5edc1bf098c6b8c31886cd8e8d/utils/yolo.py#L60
@@ -244,7 +185,7 @@ def yolov3_variant_preprocess_func_2(images_folder, height, width, start_index=0
 
 
 # This is for special tuned yolov3 model
-def yolov3_variant_preprocess_func_3(images_folder, height, width, start_index=0, size_limit=0):
+def yolov3_variant_preprocess_func_2(images_folder, height, width, start_index=0, size_limit=0):
     def letterbox(img, new_shape=(416, 416), color=(114, 114, 114), auto=True, scaleFill=False, scaleup=True):
         # Resize image to a 32-pixel-multiple rectangle https://github.com/ultralytics/yolov3/issues/232
         shape = img.shape[:2]  # current shape [height, width]
