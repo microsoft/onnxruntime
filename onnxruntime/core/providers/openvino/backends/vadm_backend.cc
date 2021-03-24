225aa8eee07192f244dd90cddd711f6d14043bac
MaajidKhan <n.maajidkhan@gmail.com>
2021-03-24 16:54:02 +0530

    Removing Logging Information
    Signed-off-by: MaajidKhan <n.maajidkhan@gmail.com>

--- onnxruntime/core/providers/openvino/backends/vadm_backend.cc
+++ onnxruntime/core/providers/openvino/backends/vadm_backend.cc
@@ -293,7 +293,6 @@ void VADMBackend::Infer(Ort::CustomOpApi& ort, OrtKernelContext* context) {
     }
   }
   LOGS_DEFAULT(INFO) << log_tag << "Inference successful";
-  std::cout << "Inference successful" << std::endl;
 }
 
 }  // namespace openvino_ep
--- onnxruntime/core/providers/openvino/ov_versions/capability.cc
+++ onnxruntime/core/providers/openvino/ov_versions/capability.cc
@@ -110,7 +110,6 @@ std::vector<std::unique_ptr<ComputeCapability>> GetCapability::Execute() {
     AppendClusterToSubGraph(graph_viewer_.GetNodesInTopologicalOrder(), inputs, outputs, result);
 
     LOGS_DEFAULT(INFO) << "[OpenVINO-EP] Model is fully supported by OpenVINO";
-    std::cout << "Model is fully supported on OpenVINO" << std::endl;
     openvino_ep::BackendManager::GetGlobalContext().is_wholly_supported_graph = true;
 
   } else {  // unsupported_nodes_idx.empty()
