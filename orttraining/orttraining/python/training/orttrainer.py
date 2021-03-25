9be25800c47a8a83b12bf894361d92f518d3a45b
Tang, Cheng <souptc@gmail.com>
2021-03-25 10:41:25 -0700

    Update orttraining/orttraining/python/training/orttrainer.py
    Co-authored-by: Thiago Crepaldi <thiago.crepaldi@microsoft.com>

--- orttraining/orttraining/python/training/orttrainer.py
+++ orttraining/orttraining/python/training/orttrainer.py
@@ -665,7 +665,7 @@ class ORTTrainer(object):
         ort_parameters.model_with_training_graph_path = self.options.debug.graph_save_paths.model_with_training_graph_path
 
         # SessionOptions
-        session_options = ort.SessionOptions() if session_option is None else session_option
+        session_options = ort.SessionOptions() if session_options is None else session_options
         session_options.use_deterministic_compute = self.options.debug.deterministic_compute
         if (self.options.graph_transformer.attn_dropout_recompute or
             self.options.graph_transformer.gelu_recompute or
