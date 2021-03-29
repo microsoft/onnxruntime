d7b3a6a295953185b895fa329903e2ce7c0eaa3a
Ravi shankar Kolli <rakoll@microsoft.com>
2021-03-29 11:25:12 -0700

    Changed gpu identifier from ROCM to CUDA

--- onnxruntime/core/providers/rocm/rocm_execution_provider.cc
+++ onnxruntime/core/providers/rocm/rocm_execution_provider.cc
@@ -63,7 +63,7 @@ AllocatorPtr ROCMExecutionProvider::CreateRocmAllocator(OrtDevice::DeviceId devi
   if (external_allocator_info.UseExternalAllocator()) {
     AllocatorCreationInfo default_memory_info(
         [external_allocator_info](OrtDevice::DeviceId id) {
-          return onnxruntime::make_unique<ROCMExternalAllocator>(id, ROCM, external_allocator_info.alloc, external_allocator_info.free);
+          return onnxruntime::make_unique<ROCMExternalAllocator>(id, CUDA, external_allocator_info.alloc, external_allocator_info.free);
         },
         device_id,
         false);
@@ -98,7 +98,7 @@ ROCMExecutionProvider::PerThreadContext::PerThreadContext(OrtDevice::DeviceId de
 
   AllocatorCreationInfo default_memory_info(
       [](OrtDevice::DeviceId id) {
-        return onnxruntime::make_unique<ROCMAllocator>(id, ROCM);
+        return onnxruntime::make_unique<ROCMAllocator>(id, CUDA);
       },
       device_id,
       true,
