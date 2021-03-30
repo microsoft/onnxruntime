5328c03f893527faf40b17f5136561d24b816dc1
Chi Lo <Chi.Lo@microsoft.com>
2021-03-30 06:03:49 -0700

    Fix bug due to build fail

--- include/onnxruntime/core/providers/tensorrt/tensorrt_provider_factory.h
+++ include/onnxruntime/core/providers/tensorrt/tensorrt_provider_factory.h
@@ -9,7 +9,6 @@ extern "C" {
 #endif
 
 ORT_API_STATUS(OrtSessionOptionsAppendExecutionProvider_Tensorrt, _In_ OrtSessionOptions* options, int device_id);
-ORT_API_STATUS(OrtApis::SessionOptionsAppendExecutionProvider_TensorRT, _In_ OrtSessionOptions* options, _In_ const OrtTensorRTProviderOptions* tensorrt_options);
 
 #ifdef __cplusplus
 }
