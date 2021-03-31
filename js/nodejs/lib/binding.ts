b8e3e2611f51318b6181cfea58b6a52e742453bb
Yulong Wang <yulongw@microsoft.com>
2021-03-31 15:51:22 -0700

    remove dup files

--- js/nodejs/lib/binding.ts
+++ js/nodejs/lib/binding.ts
@@ -1,31 +1,33 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-import {InferenceSession as InferenceSessionInterface} from './inference-session';
-import {OnnxValue} from './onnx-value';
+import {InferenceSession, OnnxValue} from 'onnxruntime-common';
+
+type SessionOptions = InferenceSession.SessionOptions;
+type FeedsType = {
+  [name: string]: OnnxValue;
+};
+type FetchesType = {
+  [name: string]: OnnxValue|null;
+};
+type ReturnType = {
+  [name: string]: OnnxValue;
+};
+type RunOptions = InferenceSession.RunOptions;
+
 
 /**
  * Binding exports a simple synchronized inference session object wrap.
  */
 export declare namespace Binding {
   export interface InferenceSession {
-    loadModel(modelPath: string, options: InferenceSessionInterface.SessionOptions): void;
-    loadModel(
-        buffer: ArrayBuffer, byteOffset: number, byteLength: number,
-        options: InferenceSessionInterface.SessionOptions): void;
+    loadModel(modelPath: string, options: SessionOptions): void;
+    loadModel(buffer: ArrayBuffer, byteOffset: number, byteLength: number, options: SessionOptions): void;
 
     readonly inputNames: string[];
     readonly outputNames: string[];
 
-    run(feeds: InferenceSession.FeedsType, fetches: InferenceSession.FetchesType,
-        options: InferenceSession.RunOptions): InferenceSession.ReturnType;
-  }
-
-  export namespace InferenceSession {
-    type FeedsType = {[name: string]: OnnxValue};
-    type FetchesType = {[name: string]: OnnxValue | null};
-    type ReturnType = {[name: string]: OnnxValue};
-    type RunOptions = InferenceSessionInterface.RunOptions;
+    run(feeds: FeedsType, fetches: FetchesType, options: RunOptions): ReturnType;
   }
 
   export interface InferenceSessionConstructor {
@@ -35,6 +37,7 @@ export declare namespace Binding {
 
 // export native binding
 export const binding =
-    // eslint-disable-next-line @typescript-eslint/no-require-imports, import/no-internal-modules
+    // eslint-disable-next-line @typescript-eslint/no-require-imports, @typescript-eslint/no-var-requires
     require(`../bin/napi-v3/${process.platform}/${process.arch}/onnxruntime_binding.node`) as
+    // eslint-disable-next-line @typescript-eslint/naming-convention
     {InferenceSession: Binding.InferenceSessionConstructor};
