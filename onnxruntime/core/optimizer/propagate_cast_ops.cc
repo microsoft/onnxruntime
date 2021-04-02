1fea1e53daec081d3b62401e421e9bfd4ddc965a
Satya Jandhyala <satya.k.jandhyala@gmail.com>
2021-04-02 16:43:19 +0000

    Added code to add/remove edges when adding/removingCasts, in addition to many miscellanious changes.

--- onnxruntime/core/optimizer/propagate_cast_ops.cc
+++ onnxruntime/core/optimizer/propagate_cast_ops.cc
@@ -3,6 +3,7 @@
 #include "core/optimizer/initializer.h"
 #include "core/optimizer/propagate_cast_ops.h"
 #include "core/graph/graph_utils.h"
+#include "core/optimizer/utils.h"
 #include <deque>
 
 using namespace ONNX_NAMESPACE;
@@ -16,63 +17,105 @@ std::vector<std::string> fp16_safe = { "LayerNorm", "Gelu", "FastGelu", "Tanh", 
 // Insert a Cast node after each NodeArg
 static Status InsertCastNodes(Graph& graph, const std::set<NodeArg*>& require_cast, bool is_fp16, std::deque<onnxruntime::NodeIndex>& removed_nodes)
 {
-  TensorProto_DataType data_type = is_fp16 ? TensorProto_DataType_FLOAT16 : TensorProto_DataType_FLOAT;
-  TypeProto fp16_type_proto;
-  fp16_type_proto.mutable_tensor_type()->set_elem_type(data_type);
-  std::vector<std::pair<NodeArg*, NodeArg*>> substitute_list; // List of cast_input and cast_output nodes
-  std::set<Node*> consumer_nodes; // Nodes that need to be modified due to inserting Cast nodes
+  (void) removed_nodes;
 
   //Create requirred new Cast nodes.
   for (NodeArg* node_arg : require_cast) {
-    NodeArg& cast_input = *node_arg;
-    NodeArg& cast_output = graph.GetOrCreateNodeArg(graph.GenerateNodeArgName(node_arg->Name()), &fp16_type_proto);
-    const std::vector<NodeArg*> new_cast_input = {&cast_input};
-    const std::vector<NodeArg*> new_cast_output = {&cast_output};
+    if (!node_arg->Exists()) {
+      continue;
+    }
+    // data_type is the data type of the Cast output.
+    TensorProto_DataType data_type = is_fp16 ? TensorProto_DataType_FLOAT16 : TensorProto_DataType_FLOAT;
+    TypeProto type_proto;
+    bool is_node_arg_cast_output = node_arg->TypeAsProto()->tensor_type().elem_type() == data_type;
+    TensorProto_DataType new_node_arg_data_type = data_type;;
+    if (is_node_arg_cast_output) {
+      new_node_arg_data_type = (data_type == TensorProto_DataType_FLOAT) ? TensorProto_DataType_FLOAT16 : TensorProto_DataType_FLOAT;
+    }
+    type_proto.mutable_tensor_type()->set_elem_type(new_node_arg_data_type);
+    NodeArg& new_node_arg = graph.GetOrCreateNodeArg(graph.GenerateNodeArgName(node_arg->Name()), &type_proto);
+    // Expect that a NodeArg is not both a graph input as well as a graph output
+    ORT_ENFORCE(!(graph.IsInputsIncludingInitializers(node_arg) && graph.IsOutput(node_arg)));
+    NodeArg& cast_input = !is_node_arg_cast_output ? *node_arg : new_node_arg;
+    NodeArg& cast_output = is_node_arg_cast_output ? *node_arg : new_node_arg;
+    const std::vector<NodeArg*> cast_inputs = {&cast_input};
+    const std::vector<NodeArg*> cast_outputs = {&cast_output};
     ONNX_NAMESPACE::AttributeProto to_attribute;
     to_attribute.set_name("to");
     to_attribute.set_type(ONNX_NAMESPACE::AttributeProto::INT);
     to_attribute.set_i(static_cast<int64_t>(data_type));
     NodeAttributes attributes({{"to", to_attribute}});
 
-    (void) graph.AddNode(graph.GenerateNodeName(node_arg->Name() + "_cast"),
-                        "Cast",
-                        "Created a new Cast node",
-                        new_cast_input,
-                        new_cast_output,
-                        &attributes);
-    substitute_list.push_back(std::make_pair(&cast_input, &cast_output));
+    Node& cast = graph.AddNode(graph.GenerateNodeName(node_arg->Name() + "_cast"),
+                               "Cast",
+                               "Created a new Cast node",
+                               cast_inputs,
+                               cast_outputs,
+                               &attributes);
+    Node* producer = graph.GetMutableProducerNode(node_arg->Name());
     std::vector<Node*> consumers = graph.GetMutableConsumerNodes(node_arg->Name());
-    std::copy(consumers.begin(), consumers.end(), std::inserter(consumer_nodes, consumer_nodes.end()));
-  }
-  for (Node* consumer : consumer_nodes) {
-    if (consumer) {
-      std::vector<NodeArg*> consumer_inputs = consumer->MutableInputDefs();
-      auto consumer_outputs = consumer->MutableOutputDefs();
-      for (std::pair<NodeArg*, NodeArg*> p : substitute_list) {
-        std::replace(consumer_inputs.begin(), consumer_inputs.end(), p.first, p.second);
+    int output_index = (producer != nullptr) ? optimizer_utils::IndexOfNodeOutput(*producer, *node_arg) : -1;
+    // Update consumers of node_arg to use the output of the cast node
+    int cast_output_index = optimizer_utils::IndexOfNodeOutput(cast, cast_output);
+    for (Node* consumer : graph.GetMutableConsumerNodes(node_arg->Name())) {
+      if (consumer != nullptr) {
+        auto& consumer_inputs = consumer->MutableInputDefs();
+        int input_index = optimizer_utils::IndexOfNodeInput(*consumer, *node_arg);
+        if (producer) {
+          graph.RemoveEdge(producer->Index(), consumer->Index(), output_index, input_index);
+        }
+        std::replace(consumer_inputs.begin(), consumer_inputs.end(), &cast_input, &cast_output);
+        graph.AddEdge(cast.Index(), consumer->Index(), cast_output_index, input_index);
       }
-      Node& new_consumer =  graph.AddNode(graph.GenerateNodeName(consumer->Name() + "_replace"),
-                                          consumer->OpType(),
-                                          "Created to replace a node",
-                                          consumer_inputs,
-                                          consumer_outputs,
-                                          &consumer->GetAttributes(),
-                                          consumer->Domain());
-      (void) new_consumer;
-      removed_nodes.push_front(consumer->Index());
     }
+    if (producer != nullptr) {
+      auto& producer_outputs = producer->MutableOutputDefs();
+      std::replace(producer_outputs.begin(), producer_outputs.end(), node_arg, &cast_input);
+      graph.UpdateProducerNode(cast_input.Name(), producer->Index());
+      int input_index = optimizer_utils::IndexOfNodeInput(cast, cast_input);
+      graph.AddEdge(producer->Index(), cast.Index(), output_index, input_index);
+    }
+    graph.UpdateProducerNode(cast_output.Name(), cast.Index());
   }
   return Status::OK();
 }
 
-static Status RemoveCastNode(Graph& graph, Node* cast, std::deque<onnxruntime::NodeIndex>& removed_nodes)
+static Status RemoveCastNodes(Graph& graph, std::vector<Node*> casts, std::deque<onnxruntime::NodeIndex>& removed_nodes)
 {
-  NodeArg* cast_input = cast->MutableInputDefs()[0];
-  auto parent = graph.GetMutableProducerNode(cast_input->Name());
-  if (parent) {
-    graph_utils::FinalizeNodeFusion(graph, *parent, *cast);
-  } else {
-    removed_nodes.push_front(cast->Index());
+  (void) removed_nodes;
+  ORT_ENFORCE(casts.size()>0);
+  Node* lead_cast = casts.front();
+  Node* trail_cast = casts.back();
+  NodeArg* cast_input = lead_cast->MutableInputDefs()[0];
+  NodeArg* cast_output = trail_cast->MutableOutputDefs()[0];
+  // Update producer node
+  Node* producer = graph.GetMutableProducerNode(cast_input->Name());
+  auto consumers = graph.GetMutableConsumerNodes(cast_output->Name());
+  int output_index = (producer != nullptr) ? optimizer_utils::IndexOfNodeOutput(*producer, *cast_input) : -1;
+  if (producer) {
+    auto& outputs = producer->MutableOutputDefs();
+    int input_index = optimizer_utils::IndexOfNodeInput(*lead_cast, *cast_input);
+    graph.RemoveEdge(producer->Index(), lead_cast->Index(), output_index, input_index);
+    std::replace(outputs.begin(), outputs.end(), cast_input, cast_output);
+    graph.UpdateProducerNode(cast_output->Name(), producer->Index());
+  }
+  // Update consumer nodes
+  if (consumers.size()>0) {
+    int cast_output_index = optimizer_utils::IndexOfNodeOutput(*trail_cast, *cast_output);
+    for (Node* consumer : consumers) {
+      auto& consumer_inputs = consumer->MutableInputDefs();
+      int input_index = optimizer_utils::IndexOfNodeInput(*consumer, *cast_output);
+      graph.RemoveEdge(trail_cast->Index(), consumer->Index(), cast_output_index, input_index);
+      std::replace(consumer_inputs.begin(), consumer_inputs.end(), cast_output, cast_input);
+      if (producer) {
+        graph.AddEdge(producer->Index(), consumer->Index(), output_index, input_index);
+      }
+    }
+    graph.UpdateConsumerNodes(cast_input->Name(), consumers);
+  }
+  for (auto cast : casts) {
+    graph_utils::RemoveNodeOutputEdges(graph, *cast);
+    graph.RemoveNode(cast->Index());
   }
   return Status::OK();
 }
@@ -95,12 +138,11 @@ static bool RemoveBackToBackCasts(Graph& graph, std::deque<onnxruntime::NodeInde
             bool is_child_fp16 = child_attributes.at("to").i() == static_cast<int64_t> (TensorProto::FLOAT16);
             if ((is_fp && is_child_fp16) || (is_fp16 && is_child_fp)) {
               // The parent and child cancell out
-              RemoveCastNode(graph, &node, removed_nodes);
-              RemoveCastNode(graph, child, removed_nodes);
+              RemoveCastNodes(graph, {&node, child}, removed_nodes);
               modified = true;
             } else if ((is_fp16 && is_child_fp16) || (is_fp && is_child_fp)) {
               // Child is a duplicate of parent
-              RemoveCastNode(graph, child, removed_nodes);
+              RemoveCastNodes(graph, {child}, removed_nodes);
               modified = true;
             }
           }
@@ -164,18 +206,18 @@ static bool PropagateForwards(Graph& graph, Node* node, std::deque<onnxruntime::
       std::set<NodeArg*> require_cast;
       NodeArg* cast_output = node->MutableOutputDefs()[0];
       SearchDownstream(graph, cast_output, require_cast);
-      if (require_cast.find(cast_output) == require_cast.end()) {
+      if (require_cast.size() > 0 && require_cast.find(cast_output) == require_cast.end()) {
         // Remove Cast operation
-        RemoveCastNode(graph, node, removed_nodes);
+        RemoveCastNodes(graph,{node}, removed_nodes);
         InsertCastNodes(graph, require_cast, false, removed_nodes);
         modified = true;
       }
     }
-  } else if (std::find(fp16_safe.begin(), fp16_safe.end(), node->OpType()) == fp16_safe.end()) {
+  } else if (std::find(fp16_safe.begin(), fp16_safe.end(), node->OpType()) != fp16_safe.end()) {
     bool all_inputs_have_casts = true;
     for (NodeArg* input : node->MutableInputDefs()) {
       Node* producer = graph.GetMutableProducerNode(input->Name());
-      if (producer->OpType() == "Cast") {
+      if (producer && producer->OpType() == "Cast") {
         const NodeAttributes& attributes = producer->GetAttributes();
         ORT_ENFORCE(attributes.find("to") != attributes.end());
         if (attributes.at("to").i() == static_cast<int64_t> (TensorProto::FLOAT)) {
@@ -188,10 +230,12 @@ static bool PropagateForwards(Graph& graph, Node* node, std::deque<onnxruntime::
     if (all_inputs_have_casts) {
       for (NodeArg* input : node->MutableInputDefs()) {
         Node* producer = graph.GetMutableProducerNode(input->Name());
-        RemoveCastNode(graph, producer, removed_nodes);
+        RemoveCastNodes(graph, {producer}, removed_nodes);
       }
+      NodeArg* node_arg = node->MutableOutputDefs()[0];
+      InsertCastNodes(graph, {node_arg}, false, removed_nodes);
+      modified = true;
     }
-    InsertCastNodes(graph, {node->MutableOutputDefs()[0]}, false, removed_nodes);
   } else {
     for (NodeArg* output: node->MutableOutputDefs()) {
       for (Node* consumer : graph.GetMutableConsumerNodes(output->Name())) {
@@ -217,7 +261,7 @@ static bool PropagateBackwards(Graph& graph, Node* node, std::deque<onnxruntime:
       SearchUpstream(graph, cast_input, require_cast);
       if (require_cast.find(cast_input) == require_cast.end()) {
         // Remove Cast operation
-        RemoveCastNode(graph, node, removed_nodes);
+        RemoveCastNodes(graph, {node}, removed_nodes);
         InsertCastNodes(graph, require_cast, true, removed_nodes);
         modified = true;
       }
@@ -294,16 +338,14 @@ Status PropagateCastOps::ApplyImpl(Graph& graph, bool& modified, int graph_level
   (void) logger;
   modified = false;
   // Propagate FP32 Casts forward
-  for (const NodeArg* input: graph.GetInputs()) {
-    for (Node* node : graph.GetMutableConsumerNodes(input->Name())) {
-      modified |= PropagateForwards(graph, node, removed_nodes);
-    }
+  for (Node& node : graph.Nodes()) {
+      modified |= PropagateForwards(graph, &node, removed_nodes);
   }
 
-  RemoveBackToBackCasts(graph, removed_nodes);
+  modified |= RemoveBackToBackCasts(graph, removed_nodes);
 
   // Propagate FP16 Casts backward
-  for (const NodeArg* output: graph.GetOutputs()) {
+  if (!modified) for (const NodeArg* output: graph.GetOutputs()) {
     Node* node = graph.GetMutableProducerNode(output->Name());
     modified |= PropagateBackwards(graph, node, removed_nodes);
   }
