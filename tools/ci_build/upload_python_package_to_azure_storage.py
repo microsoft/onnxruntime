f15cd4a38a46a4079ede1777ce4a489f5476bf2d
liqun <liqun@OrtTrainingDev4.af05slrtruoetgaxwwjv5nsq5e.px.internal.cloudapp.net>
2021-04-06 18:56:08 +0000

    .

---
+++ tools/ci_build/upload_python_package_to_azure_storage.py
@@ -0,0 +1,46 @@
+import os
+import argparse
+from azure.storage.blob import BlockBlobService
+
+
+def upload_whl(python_wheel_path, account_name, account_key, container_name):
+    block_blob_service = BlockBlobService(
+        account_name=account_name,
+        account_key=account_key
+    )
+
+    python_wheel_path = whl_files[0]
+
+    blob_name = os.path.basename(python_wheel_path)
+    block_blob_service.create_blob_from_path(container_name, blob_name, python_wheel_path)
+
+    html_blob_name = 'onnxruntime_nightly.html'
+    download_path_to_html = "./onnxruntime_nightly.html"
+    block_blob_service.get_blob_to_path(container_name, html_blob_name, download_path_to_html)
+
+    blob_name_plus_replaced = blob_name.replace('+', '%2B')
+    with open(download_path_to_html) as f:
+        lines = f.read().splitlines()
+
+    new_line = '<a href="{blobname}">{blobname}</a><br>'.format(blobname=blob_name_plus_replaced)
+    lines.append(new_line)
+    lines.sort()
+
+    with open(download_path_to_html, 'w') as f:
+        for item in lines:
+            f.write("%s\n" % item)
+
+    block_blob_service.create_blob_from_path(container_name, html_blob_name, download_path_to_html)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Upload python whl to azure storage.")
+
+    parser.add_argument("--python_wheel_path", type=str, help="path to python wheel")
+    parser.add_argument("--account_name", type=str, help="account name")
+    parser.add_argument("--account_key", type=str, help="account key")
+    parser.add_argument("--container_name", type=str, help="container name")
+
+    args = parser.parse_args()
+
+    upload_whl(args.python_wheel_path, args.account_name, args.account_key, args.container_name)
