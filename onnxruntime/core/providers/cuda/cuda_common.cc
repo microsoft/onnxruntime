e20c9ef32e99e3b61cc1b01939160f3d26c402b5
Tianlei Wu <tlwu@microsoft.com>
2021-04-05 18:09:10 -0700

    change half gemm to use compute_32f as default; add env variable for configuring gemm options

---
+++ onnxruntime/core/providers/cuda/cuda_common.cc
@@ -0,0 +1,40 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+#pragma once
+
+#include "core/providers/cuda/cuda_common.h"
+#include "core/common/logging/logging.h"
+#include "core/common/logging/severity.h"
+
+namespace onnxruntime {
+namespace cuda {
+
+// Initialize the singleton instance
+HalfGemmOptions HalfGemmOptions::instance;
+
+const HalfGemmOptions* HalfGemmOptions::GetInstance() {
+  if (!instance.initialized_) {
+    // We do not use critical section here since it is fine to initialize multiple times by different threads.
+
+    // The environment variable is for testing purpose only, and it might be removed in the future.
+    constexpr const char* kCudaGemmOptions = "ORT_CUDA_GEMM_OPTIONS";
+    int value = ParseEnvironmentVariableWithDefault<int>(kCudaGemmOptions, 0);
+    instance.compute_16f_ = (value & 0x01) > 0;
+#if defined(CUDA_VERSION) && CUDA_VERSION >= 11000
+    instance.disallow_reduced_precision_reduction_ = (value & 0x02) > 0;
+    instance.pedantic_ = (value & 0x04) > 0;
+    LOGS_DEFAULT(INFO) << "ORT_CUDA_GEMM_OPTIONS: compute_16f=" << instance.compute_16f_
+                       << " disallow_reduced_precision_reduction=" << instance.disallow_reduced_precision_reduction_
+                       << " pedantic=" << instance.pedantic_;
+#else
+    LOGS_DEFAULT(INFO) << "ORT_CUDA_GEMM_OPTIONS: compute_16f=" << instance.compute_16f_;
+#endif
+    instance.initialized_ = true;
+  }
+
+  return &instance;
+}
+
+}  // namespace cuda
+}  // namespace onnxruntime
