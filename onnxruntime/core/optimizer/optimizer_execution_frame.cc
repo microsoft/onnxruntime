77471b666d6c8771cd299f4d001f4e8ecc9eb99c
Sumit Agarwal <sumitagarwal@microsoft.com>
2021-04-07 14:51:31 -0700

    Fixed test and addressed comment on the PR

--- onnxruntime/core/optimizer/optimizer_execution_frame.cc
+++ onnxruntime/core/optimizer/optimizer_execution_frame.cc
@@ -121,7 +121,7 @@ OptimizerExecutionFrame::OptimizerExecutionFrame(const Info& info,
 }
 
 const logging::Logger* OptimizerExecutionFrame::GetLogger() {
-  return NULL;
+  return nullptr;
 }
 
 AllocatorPtr OptimizerExecutionFrame::GetAllocatorImpl(const OrtMemoryInfo& info) const {
