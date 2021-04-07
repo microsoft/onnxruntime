37e129dfdf52e2b1b24b58651e66ae6983d8470e
liqun <liqun@OrtTrainingDev4.af05slrtruoetgaxwwjv5nsq5e.px.internal.cloudapp.net>
2021-04-07 21:49:17 +0000

    .

--- tools/ci_build/upload_python_package_to_azure_storage.py
+++ tools/ci_build/upload_python_package_to_azure_storage.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python3
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
 import os
 import argparse
 from azure.storage.blob import BlockBlobService
