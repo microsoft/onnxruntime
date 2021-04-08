461e707c1d8aecc8c049d5095260aa70b70992c9
Chen Fu <fuchen@microsoft.com>
2021-04-08 09:04:38 -0700

    update benchmark

--- onnxruntime/test/onnx/microbenchmark/quantize.cc
+++ onnxruntime/test/onnx/microbenchmark/quantize.cc
@@ -2,7 +2,15 @@
 
 #include <benchmark/benchmark.h>
 #include "core/util/qmath.h"
+#include "core/util/thread_utils.h"
 
+static void BenchSize(benchmark::internal::Benchmark* b) {
+  for (int size : {80000, 160000, 320000, 640000, 1280000}) {
+    for (int threads : {2, 4, 6, 8}) {
+      b->Args({size, threads});
+    }
+  }
+}
 
 // qmath.h GetQuantizationParameter
 struct quant_params {
@@ -10,18 +18,27 @@ struct quant_params {
   uint8_t zero;
 };
 
-static quant_params GetQuantParams(const float* data, const int64_t data_size) {
+static quant_params GetQuantParams(const float* data, const int64_t data_size, onnxruntime::concurrency::ThreadPool* tp) {
   quant_params params;
-  onnxruntime::GetQuantizationParameter<uint8_t>(data, data_size, params.scale, params.zero);
+  onnxruntime::GetQuantizationParameter<uint8_t>(data, data_size, params.scale, params.zero, tp);
   return params;
 }
 
 static void BM_GetQuantParams(benchmark::State& state) {
   const int64_t batch_size = state.range(0);
+  const int64_t threads = state.range(1);
+
   float* data = GenerateArrayWithRandomValue<float>(batch_size, -1, 1);
 
+  OrtThreadPoolParams tpo;
+  tpo.thread_pool_size = int(threads);
+  tpo.auto_set_affinity = true;
+  std::unique_ptr<onnxruntime::concurrency::ThreadPool> tp(
+      onnxruntime::concurrency::CreateThreadPool(&onnxruntime::Env::Default(),
+                                                 tpo, onnxruntime::concurrency::ThreadPoolType::INTRA_OP));
+
   for (auto _ : state) {
-    benchmark::DoNotOptimize(GetQuantParams(data, batch_size));
+    benchmark::DoNotOptimize(GetQuantParams(data, batch_size, tp.get()));
   }
   aligned_free(data);
 }
@@ -29,24 +46,27 @@ static void BM_GetQuantParams(benchmark::State& state) {
 BENCHMARK(BM_GetQuantParams)
     ->UseRealTime()
     ->Unit(benchmark::TimeUnit::kNanosecond)
-    ->Arg(40000)
-    ->Arg(80000)
-    ->Arg(98304)
-    ->Arg(160000)
-    ->Arg(320000)
-    ->Arg(640000)
-    ->Arg(1280000);
+    ->Apply(BenchSize);
 
 static void BM_Quantize(benchmark::State& state) {
   const int64_t batch_size = state.range(0);
+  const int64_t threads = state.range(1);
   float* a_data = GenerateArrayWithRandomValue<float>(batch_size, -1, 1);
   uint8_t* a_data_quant = static_cast<uint8_t*>(aligned_alloc(sizeof(uint8_t) * batch_size, 64));
+
+  OrtThreadPoolParams tpo;
+  tpo.thread_pool_size = int(threads);
+  tpo.auto_set_affinity = true;
+  std::unique_ptr<onnxruntime::concurrency::ThreadPool> tp(
+      onnxruntime::concurrency::CreateThreadPool(&onnxruntime::Env::Default(),
+                                                 tpo, onnxruntime::concurrency::ThreadPoolType::INTRA_OP));
+
   float scale = 1.23456f;
   uint8_t zero = 129;
 
   for (auto _ : state) {
     benchmark::DoNotOptimize(a_data_quant);
-    MlasQuantizeLinear(a_data, a_data_quant, batch_size, scale, zero);
+    onnxruntime::ParQuantizeLinear(a_data, a_data_quant, batch_size, scale, zero, tp.get());
     benchmark::ClobberMemory();
   }
   aligned_free(a_data_quant);
@@ -56,10 +76,4 @@ static void BM_Quantize(benchmark::State& state) {
 BENCHMARK(BM_Quantize)
     ->UseRealTime()
     ->Unit(benchmark::TimeUnit::kNanosecond)
-    ->Arg(40000)
-    ->Arg(80000)
-    ->Arg(98304)
-    ->Arg(160000)
-    ->Arg(320000)
-    ->Arg(640000)
-    ->Arg(1280000);
+    ->Apply(BenchSize);
