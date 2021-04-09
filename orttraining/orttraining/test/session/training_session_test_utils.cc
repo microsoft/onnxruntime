75c0192e4fb05ffbcbaf9bb96ec16bca89151010
Weixing Zhang <weixingzhang@users.noreply.github.com>
2021-04-09 15:15:13 -0700

    enable more unit tests for ROCM EP (#7307)

--- orttraining/orttraining/test/session/training_session_test_utils.cc
+++ orttraining/orttraining/test/session/training_session_test_utils.cc
@@ -99,7 +99,7 @@ void VerifyState(const DataTransferManager& data_transfer_mgr, const NameMLValMa
     const auto& e_state_it = expected_state.find(key);
     ORT_ENFORCE(e_state_it != expected_state.end());
     auto& expected_tensor = e_state_it->second.Get<Tensor>();
-#ifdef USE_CUDA
+#if defined(USE_CUDA) || defined(USE_ROCM)
     auto& actual_gpu_tensor = a_state_it.second.Get<Tensor>();
 
     // Copying tensor to CPU when cuda is enabled.
@@ -182,6 +182,9 @@ std::unique_ptr<TrainingSession> BuildAndRunTrainingSessionWithChecks(
 #ifdef USE_CUDA
   CUDAExecutionProviderInfo xp_info;
   ORT_THROW_IF_ERROR(training_session->RegisterExecutionProvider(onnxruntime::make_unique<CUDAExecutionProvider>(xp_info)));
+#elif USE_ROCM
+  ROCMExecutionProviderInfo xp_info;
+  ORT_THROW_IF_ERROR(training_session->RegisterExecutionProvider(onnxruntime::make_unique<ROCMExecutionProvider>(xp_info)));
 #endif
   ORT_THROW_IF_ERROR(training_session->Initialize());
 
