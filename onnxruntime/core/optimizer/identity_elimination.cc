4cebce896b3a6eced949b4006ff1f2be09596aa6
Ethan Tao <ettao@OrtTrainingDev4.af05slrtruoetgaxwwjv5nsq5e.px.internal.cloudapp.net>
2021-04-13 18:12:21 +0000

    fix build

--- onnxruntime/core/optimizer/identity_elimination.cc
+++ onnxruntime/core/optimizer/identity_elimination.cc
@@ -21,7 +21,6 @@ namespace onnxruntime {
   X ---> graph output
  */
 Status EliminateIdentity::Apply(Graph& graph, Node& node, RewriteRuleEffect& rule_effect, const logging::Logger&) const {
-
   // regular case - when Identity's output has no graph output 
   if (graph.GetNodeOutputsInGraphOutputs(node).empty()) {
     if (graph_utils::RemoveNode(graph, node)) {
@@ -32,19 +31,16 @@ Status EliminateIdentity::Apply(Graph& graph, Node& node, RewriteRuleEffect& rul
 
     // keep a reference of output def to the graph output
     NodeArg* output = node.MutableOutputDefs()[0];
-
     const Node* p_input_node = graph_utils::GetInputNode(node, 0);
     // get mutable input node
     Node& input_node = *graph.GetNode(p_input_node->Index());
-
+    int output_idx = graph_utils::GetNodeOutputIndexFromOutputName(input_node, node.MutableInputDefs()[0]->Name());
     // remove Identity node and its input edge
     graph.RemoveNode(node.Index());
     // update input node's output def to the graph output
-    input_node.MutableOutputDefs()[0] = output;
-
-    rule_effect = RewriteRuleEffect::kRemovedCurrentNode;
+    input_node.MutableOutputDefs()[output_idx] = output;
+    rule_effect = RewriteRuleEffect::kRemovedCurrentNode; 
   }
-
   return Status::OK();
 }
 
@@ -53,6 +49,27 @@ bool EliminateIdentity::SatisfyCondition(const Graph& graph, const Node& node, c
     return true;
   }
   
+  // check the input nodearg of Identity - it must only have one consumer, which is the Identity itself
+  const Node* p_input_node = graph_utils::GetInputNode(node, 0);
+  // find the edge between input node and this Identity node, and then get its src arg from input node
+  int src_arg_index = -1;
+  for (auto it = p_input_node->OutputEdgesBegin(), end = p_input_node->OutputEdgesEnd(); it != end; ++it) {
+    if (it->GetNode().Index() == node.Index()) {
+      src_arg_index = it->GetSrcArgIndex();
+      break;
+    }
+  }
+  // count how many consumers are sharing the same src arg
+  int count = 0;
+  for (auto it = p_input_node->OutputEdgesBegin(), end = p_input_node->OutputEdgesEnd(); it != end; ++it) {
+    if (it->GetSrcArgIndex() == src_arg_index) {
+      count++;
+    }
+  }
+  // condition not met if there are more than 1 consumer for the same src arg
+  if (count > 1) 
+    return false;
+
   // relax the condition if Identity is connecting to graph output
   if (node.GetOutputEdgesCount() == 0 && node.OutputDefs().size() == 1 &&
     !graph.GetNodeOutputsInGraphOutputs(node).empty()) {
