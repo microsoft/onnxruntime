cdb372767609bd99ef1bca7e4cc05c61f99c7d7e
G. Ramalingam <grama@microsoft.com>
2021-04-13 14:31:53 -0700

    Add DropoutGrad function body (#7310)
    * Add DropoutGrad function body
    
    * Add DropoutGrad function body
    
    * Fix documentation and add test cases
    
    * Fix template specialization
    
    * Check expansion for float16 and bfloat16

--- orttraining/orttraining/core/graph/training_op_defs.cc
+++ orttraining/orttraining/core/graph/training_op_defs.cc
@@ -1407,18 +1407,15 @@ Example 4:
       .AllowUncheckedAttributes()
       .Input(0, "dy", "The gradient tensor from output.", "T")
       .Input(1, "mask",
-             "The mask tensor of the dropout. ", "T2")
+             "The mask output of the dropout. ", "T2")
       .Input(2, "ratio",
-             "The ratio of random dropout, with value in [0, 1). If this input was not set, "
-             "or if it was set to 0, the output would be a simple copy of the input. "
-             "If it's non-zero, output will be a random dropout of input, which is typically "
-             "the case during training.",
+             "Same value as the ratio input supplied to the dropout op with value in [0, 1). "
+             "If this input is not specified, a default value of 0.5 is used.",
              "T1",
              OpSchema::Optional)
       .Input(3, "training_mode",
-             "If set to true then it indicates dropout is being used for training. It is an optional value hence unless "
-             "specified explicitly, it is false. If it is false, ratio is ignored and the operation mimics inference mode where "
-             "nothing will be dropped from the input data and if mask is requested as output it will contain all ones.",
+             "Same value as the training_mode input supplied to the dropout op. "
+             "If this input is not specified, a default value of false is used.",
              "T2",
              OpSchema::Optional)
       .Output(0, "dx", "Gradient of the input.", "T")
@@ -1436,7 +1433,52 @@ Example 4:
           "Constrain 'mask' and 'training_mode' types to boolean tensors.")
       .TypeAndShapeInferenceFunction([](ONNX_NAMESPACE::InferenceContext& ctx) {
         propagateShapeAndTypeFromFirstInput(ctx);
-      });
+      })
+      .SetContextDependentFunctionBodyBuilder(
+          [](const FunctionBodyBuildContext& ctx, const OpSchema& schema, FunctionProto& functionProto) {
+            /* DropoutGrad (dy, mask, optional ratio, optional training_mode) => dX 
+                 dX = Where (mask, dY / (1-ratio), 0)
+              where ratio = 0.5 if not specified.
+
+              TODO: Note that the above doesn't handle the case where training_mode=false and a non-zero
+              value is specified for ratio. In general, it is unclear why we need the training_mode as an
+              input here, since the Gradient will be used only for training.
+            */
+            OperatorSetIdProto onnx_opset_13;
+            onnx_opset_13.set_domain("");
+            onnx_opset_13.set_version(13);
+
+            auto* tp = ctx.getInputType(0);
+            if ((tp == nullptr) || (!tp->has_tensor_type()))
+              return false;
+            auto elem_type = (ONNX_NAMESPACE::TensorProto_DataType)tp->tensor_type().elem_type();
+            if (elem_type == ONNX_NAMESPACE::TensorProto_DataType::TensorProto_DataType_BFLOAT16)
+              return false; // ONNX op Where doesn't support bfloat16 yet.
+
+            if (ctx.hasInput(2)) {
+              // ratio specified.
+              std::vector<FunctionBodyHelper::NodeDef> body{
+                  ONNX_NAMESPACE::Const("C0", 0.0f, elem_type),
+                  ONNX_NAMESPACE::Const("C1", 1.0f, elem_type),
+                  {{"ratio_elem_type"}, "Cast", {"ratio"}, {MakeAttribute("to", int64_t(elem_type))}},
+                  {{"scale"}, "Sub", {"C1", "ratio_elem_type"}},
+                  {{"scaled_dy"}, "Div", {"dy", "scale"}},
+                  {{"dx"}, "Where", {"mask", "scaled_dy", "C0"}}};
+
+              return ONNX_NAMESPACE::BuildFunctionProto(functionProto, schema, body, {onnx_opset_13});
+            } else {
+              // ratio not specified. Use a value of 0.5
+              std::vector<FunctionBodyHelper::NodeDef> body{
+                  ONNX_NAMESPACE::Const("C0", 0.0f, elem_type),
+                  ONNX_NAMESPACE::Const("C1", 1.0f, elem_type),
+                  ONNX_NAMESPACE::Const("ratio_elem_type", 0.5f, elem_type),
+                  {{"scale"}, "Sub", {"C1", "ratio_elem_type"}},
+                  {{"scaled_dy"}, "Div", {"dy", "scale"}},
+                  {{"dx"}, "Where", {"mask", "scaled_dy", "C0"}}};
+
+              return ONNX_NAMESPACE::BuildFunctionProto(functionProto, schema, body, {onnx_opset_13});
+            }
+          });
 
   ONNX_CONTRIB_OPERATOR_SCHEMA(BroadcastGradientArgs)
       .SetDomain(kMSDomain)
@@ -1614,7 +1656,7 @@ Example 4:
                 ONNX_NAMESPACE::Const("C_One", 1.0f, elem_type),
                 ONNX_NAMESPACE::Const("C_SqrtHalf", float(M_SQRT1_2), elem_type),
                 ONNX_NAMESPACE::Const("C_MinusHalf", -0.5f, elem_type),
-                ONNX_NAMESPACE::Const("C_alpha", kAlpha, elem_type),              
+                ONNX_NAMESPACE::Const("C_alpha", kAlpha, elem_type),
                 {{"ErfArg"}, "Mul", {"X", "C_SqrtHalf"}},
                 {{"ErfTerm"}, "Erf", {"ErfArg"}},
                 {{"PartialSum"}, "Add", {"ErfTerm", "C_One"}},
