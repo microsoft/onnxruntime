9bcf68fcc61d0ea4b0d726d994213f48c89e3ae1
edgchen1 <18449977+edgchen1@users.noreply.github.com>
2021-04-13 18:26:10 -0700

    updates and fixes

--- cmake/onnxruntime_objc.cmake
+++ cmake/onnxruntime_objc.cmake
@@ -1,3 +1,10 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+if (${CMAKE_VERSION} VERSION_LESS "3.16")
+    message(FATAL_ERROR "CMake 3.16+ is required when building the Objective-C API.")
+endif()
+
 check_language(OBJC)
 if (CMAKE_OBJC_COMPILER)
     enable_language(OBJC)
@@ -24,15 +31,18 @@ file(GLOB onnxruntime_objc_srcs
     "${REPO_ROOT}/objc/src/onnxruntime/*.m"
     "${REPO_ROOT}/objc/src/onnxruntime/*.mm")
 
-find_library(FOUNDATION_LIBRARY Foundation)
+source_group(TREE "${REPO_ROOT}/objc"
+    FILES ${onnxruntime_objc_headers} ${onnxruntime_objc_srcs})
 
 add_library(onnxruntime_objc SHARED ${onnxruntime_objc_headers} ${onnxruntime_objc_srcs})
+
 target_include_directories(onnxruntime_objc
     PUBLIC
         "${REPO_ROOT}/objc/include"
     PRIVATE
         "${OPTIONAL_LITE_INCLUDE_DIR}"
         "${REPO_ROOT}/objc/src")
+
 target_link_libraries(onnxruntime_objc PUBLIC onnxruntime)
 
 set_target_properties(onnxruntime_objc PROPERTIES
@@ -40,27 +50,28 @@ set_target_properties(onnxruntime_objc PROPERTIES
     VERSION "1.0.0"
     SOVERSION "1.0.0"
     FRAMEWORK_VERSION "A"
-    #MACOSX_FRAMEWORK_INFO_PLIST ${CMAKE_CURRENT_SOURCE_DIR}/FrameworkExample/Info.plist
-    PUBLIC_HEADER "${onnxruntime_objc_headers}")
+    PUBLIC_HEADER "${onnxruntime_objc_headers}"
+    FOLDER "ONNXRuntime")
 
 if (onnxruntime_BUILD_UNIT_TESTS)
     find_package(XCTest REQUIRED)
 
     # onnxruntime_test_objc
     file(GLOB onnxruntime_objc_test_srcs
-        "${REPO_ROOT}/objc/test/*.h"
-        "${REPO_ROOT}/objc/test/*.m"
-        "${REPO_ROOT}/objc/test/*.mm")
+        "${REPO_ROOT}/objc/test/onnxruntime/*.h"
+        "${REPO_ROOT}/objc/test/onnxruntime/*.m"
+        "${REPO_ROOT}/objc/test/onnxruntime/*.mm")
+
+    source_group(TREE "${REPO_ROOT}/objc"
+        FILES ${onnxruntime_objc_test_srcs})
 
     xctest_add_bundle(onnxruntime_objc_test onnxruntime_objc
-        ${onnxruntime_objc_test_srcs}
-        #FrameworkExampleTests/Info.plist
-        )
-      
-    # set_target_properties(FrameworkExampleTests PROPERTIES
-    #     MACOSX_BUNDLE_INFO_PLIST ${CMAKE_CURRENT_SOURCE_DIR}/FrameworkExampleTests/Info.plist
-    #     )
-      
+        ${onnxruntime_objc_test_srcs})
+
+    set_target_properties(onnxruntime_objc_test PROPERTIES FOLDER "ONNXRuntimeTest")
+
     xctest_add_test(XCTest.onnxruntime_objc_test onnxruntime_objc_test)
-      
+
+    set_property(TEST XCTest.onnxruntime_objc_test APPEND PROPERTY
+        ENVIRONMENT DYLD_LIBRARY_PATH=$<TARGET_FILE_DIR:onnxruntime>)
 endif()
--- objc/src/onnxruntime/error_utils.mm
+++ objc/src/onnxruntime/error_utils.mm
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 #import "onnxruntime/error_utils.h"
 
 static NSString* const kOrtErrorDomain = @"org.onnxruntime";
