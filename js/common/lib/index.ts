4a4d0d824a325bc6ffa233b1cedbfed56c0fb89c
Yulong Wang <yulongw@microsoft.com>
2021-04-15 15:15:01 -0700

    add env

--- js/common/lib/index.ts
+++ js/common/lib/index.ts
@@ -2,6 +2,7 @@
 // Licensed under the MIT License.
 
 export * from './backend';
+export * from './env';
 export * from './inference-session';
 export * from './tensor';
 export * from './onnx-value';
