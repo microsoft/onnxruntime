b8268428ec56c18d7d75a4bf825dcd6dfa7c70b8
Dwayne Robinson <dwayner@microsoft.com>
2021-04-16 01:37:41 -0700

    Set max to 0, not 1

--- onnxruntime/core/providers/cpu/object_detection/roialign.cc
+++ onnxruntime/core/providers/cpu/object_detection/roialign.cc
@@ -162,8 +162,8 @@ void RoiAlignForward(const TensorShape& output_shape, const T* bottom_data, floa
       T roi_end_h = offset_bottom_rois[3] * spatial_scale;
 
       // Note that 0 size ROI's are legal, meaning the sample a single point in the input.
-      T roi_width = roi_end_w - roi_start_w;
-      T roi_height = roi_end_h - roi_start_h;
+      T roi_width = std::max(roi_end_w - roi_start_w, (T)0);
+      T roi_height = std::max(roi_end_h - roi_start_h, (T)0);
       T bin_size_h = static_cast<T>(roi_height) / static_cast<T>(pooled_height);
       T bin_size_w = static_cast<T>(roi_width) / static_cast<T>(pooled_width);
 
