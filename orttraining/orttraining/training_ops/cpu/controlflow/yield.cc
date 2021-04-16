90f682687353fe74a5b4b53df17c6a383e751b76
M. Zeeshan Siddiqui <mzs@microsoft.com>
2021-04-16 06:52:02 +0000

    Delete ORT Task et al.

--- orttraining/orttraining/training_ops/cpu/controlflow/yield.cc
+++ orttraining/orttraining/training_ops/cpu/controlflow/yield.cc
@@ -2,7 +2,6 @@
 // Licensed under the MIT License.
 
 #include "orttraining/training_ops/cpu/controlflow/yield.h"
-#include "orttraining/training_ops/cpu/controlflow/ort_tasks.h"
 #include "core/framework/op_kernel_context_internal.h"
 
 namespace onnxruntime {
