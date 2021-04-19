6b65b7828a508ff186b47de3a9c63ac9b1106b78
Thiago Crepaldi <thiago.crepaldi@microsoft.com>
2021-04-19 16:44:18 -0700

    Add pytorch version check before loading ORTModule API

--- orttraining/orttraining/python/training/__init__.py
+++ orttraining/orttraining/python/training/__init__.py
@@ -2,6 +2,23 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 # --------------------------------------------------------------------------
+
+
+# Verify proper PyTorch is installed before proceding to ONNX Runtime initializetion
+from packaging import version
+MINIMUM_TORCH_VERSION_STR = '1.8.1'
+try:
+    import torch
+    torch_version = version.parse(torch.__version__.split('+')[0])
+    minimum_torch_version = version.parse(MINIMUM_TORCH_VERSION_STR)
+    if torch_version < minimum_torch_version:
+        raise RuntimeError(
+            f'ONNXRuntime ORTModule frontend requires PyTorch version greater or equal to {MINIMUM_TORCH_VERSION_STR}, '
+            f'but version {torch.__version__} was found instead!')
+except:
+    raise(f'PyTorch {MINIMUM_TORCH_VERSION_STR} must be installed in order to run ONNXRuntime ORTModule frontend!')
+
+
 from onnxruntime.capi._pybind_state import TrainingParameters
 from onnxruntime.capi.training.training_session import TrainingSession
 
