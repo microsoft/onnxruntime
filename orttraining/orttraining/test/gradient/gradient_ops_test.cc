446c4ccfc5e4389aba7c71cda29e735e76f3d0e3
mindest <30493312+mindest@users.noreply.github.com>
2021-04-20 18:14:31 +0800

    Add gradient registration and tests for Min/Max (#7217)
    * Add gradient registration and tests for Min/Max
    
    * Add helper function for min/max grad test
    
    * limit Min/Max Grad to accept at most two inputs; modify test case accordingly
    
    * resolve merge error

--- orttraining/orttraining/test/gradient/gradient_ops_test.cc
+++ orttraining/orttraining/test/gradient/gradient_ops_test.cc
@@ -2439,6 +2439,44 @@ TEST(GradientCheckerTest, ClipGrad) {
   }
 }
 
+void GradientCheckerMinMaxGradHelper(const std::string op) {
+  float max_error;
+  GradientChecker<float, float, float> gradient_checker;
+  OpDef op_def{op, kOnnxDomain, 11};
+
+  // Exclude equal inputs, since Min/Max is not smooth in such case
+  {
+    TensorInfo x_info({2, 3}, true);
+    TensorInfo y_info({2, 3}, true);
+    gradient_checker.ComputeGradientError(op_def, {x_info}, {y_info}, &max_error);
+    EXPECT_IS_TINY(max_error);
+  }
+
+  {
+    TensorInfo x1_info({2, 3}, true);
+    TensorInfo x2_info({2, 3}, true);
+    TensorInfo y_info({2, 3}, true);
+    gradient_checker.ComputeGradientError(op_def, {x1_info, x2_info}, {y_info}, &max_error);
+    EXPECT_IS_TINY(max_error);
+  }
+
+  {
+    TensorInfo x1_info({2, 3}, true);
+    TensorInfo x2_info({3}, true);
+    TensorInfo y_info({2, 3}, true);
+    gradient_checker.ComputeGradientError(op_def, {x1_info, x2_info}, {y_info}, &max_error);
+    EXPECT_IS_TINY(max_error);
+  }
+}
+
+TEST(GradientCheckerTest, MinGrad) {
+  GradientCheckerMinMaxGradHelper("Min");
+}
+
+TEST(GradientCheckerTest, MaxGrad) {
+  GradientCheckerMinMaxGradHelper("Max");
+}
+
 TEST(GradientCheckerTest, TileGrad) {
   float max_error;
   GradientChecker<float, float, float> gradient_checker;
