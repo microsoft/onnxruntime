2e2e62d1e77c39967fa3877daba6190034099920
liqun <liqun@OrtTrainingDev4.af05slrtruoetgaxwwjv5nsq5e.px.internal.cloudapp.net>
2021-04-22 17:45:34 +0000

    .

--- js/node/test/test-main.ts
+++ js/node/test/test-main.ts
@@ -1,22 +1,22 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
+// // Copyright (c) Microsoft Corporation. All rights reserved.
+// // Licensed under the MIT License.
 
-import {NODE_TESTS_ROOT, warmup} from './test-utils';
+// import {NODE_TESTS_ROOT, warmup} from './test-utils';
 
-// warmup
-//
-// for unknown reason, the first call to native InferenceSession::Run() is very slow.
-// we need this warmup call so that coming test cases will not fail because of timeout.
-warmup();
+// // warmup
+// //
+// // for unknown reason, the first call to native InferenceSession::Run() is very slow.
+// // we need this warmup call so that coming test cases will not fail because of timeout.
+// warmup();
 
-// unittests
-require('./unittests/lib/index');
-require('./unittests/lib/inference-session');
-require('./unittests/lib/tensor');
+// // unittests
+// require('./unittests/lib/index');
+// require('./unittests/lib/inference-session');
+// require('./unittests/lib/tensor');
 
-// E2E tests
-require('./e2e/simple-e2e-tests');
-require('./e2e/inference-session-run');
+// // E2E tests
+// require('./e2e/simple-e2e-tests');
+// require('./e2e/inference-session-run');
 
 // // Test ONNX spec tests
 // import {run as runTestRunner} from './test-runner';
