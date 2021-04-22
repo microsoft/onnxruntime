745ad0a0a4c7ac04de433312d842018e6377b375
Changming Sun <chasun@microsoft.com>
2021-04-21 21:28:04 -0700

    Increase unit tests timeout to 2 hours
    Because TRT is timed out.

--- tools/ci_build/build.py
+++ tools/ci_build/build.py
@@ -1380,7 +1380,7 @@ def run_onnxruntime_tests(args, source_dir, ctest_path, build_dir, configs):
                      source_dir, 'cmake\\codeconv.runsettings')] + executables,
                 cwd=cwd2, dll_path=dll_path)
         else:
-            ctest_cmd = [ctest_path, "--build-config", config, "--verbose", "--timeout", "3600"]
+            ctest_cmd = [ctest_path, "--build-config", config, "--verbose", "--timeout", "7200"]
             run_subprocess(ctest_cmd, cwd=cwd, dll_path=dll_path)
 
         if args.enable_pybind:
