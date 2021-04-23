e007b79b4b1c920cbbd6f14724c887cfcdd0363f
Changming Sun <chasun@microsoft.com>
2021-04-22 19:14:39 -0700

    Reduce the number of TensorRT tests needed to run (#7419)

--- tools/ci_build/github/linux/run_build.sh
+++ tools/ci_build/github/linux/run_build.sh
@@ -45,12 +45,7 @@ elif [ $BUILD_OS = "yocto" ]; then
     make -j$(nproc)
 else
     COMMON_BUILD_ARGS="--skip_submodule_sync --enable_onnx_tests --parallel --build_shared_lib --cmake_path /usr/bin/cmake --ctest_path /usr/bin/ctest"
-    # For the nocontribops pipeline we don't need openmp as it is used by the Edge browser team and
-    # (going forward) the vscode team. Both these teams don't want their users to install any external dependency to use
-    # ORT.
-    if [[ $BUILD_EXTR_PAR != *--disable_contrib_ops* ]]; then
-        COMMON_BUILD_ARGS="${COMMON_BUILD_ARGS} --use_openmp "
-    fi
+
     if [ $BUILD_OS = "manylinux2010" ]; then
         # FindPython3 does not work on manylinux2010 image, define things manually
         # ask python where to find includes
