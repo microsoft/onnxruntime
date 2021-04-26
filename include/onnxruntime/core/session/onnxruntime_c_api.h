f0d3703e42147368cdb05a114cd6f5d07d528a82
Nat Kershaw <nakersha@microsoft.com>
2021-04-26 16:35:35 -0700

    Revert comment style

--- include/onnxruntime/core/session/onnxruntime_c_api.h
+++ include/onnxruntime/core/session/onnxruntime_c_api.h
@@ -1,5 +1,5 @@
-/* Copyright (c) Microsoft Corporation. All rights reserved. */
-/* Licensed under the MIT License. */
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
 
 /** @file onnxruntime_c_api.h
 
