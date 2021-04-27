19d7155d1f3dae2573d768abce16035e3de93426
Rachel Guo <guorachel@microsoft.com>
2021-04-27 14:42:03 -0700

    add simple sigmoid test android project and cmake

---
+++ java/src/test/android/app/src/main/java/ai/onnxruntime/example/javavalidater/MainActivity.kt
@@ -0,0 +1,22 @@
+package ai.onnxruntime.example.javavalidater
+
+import ai.onnxruntime.*
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
