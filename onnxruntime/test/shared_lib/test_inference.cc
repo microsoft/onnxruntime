82da5233cc91ea67741e460d94fb57dbccfae352
Changming Sun <chasun@microsoft.com>
2021-04-28 14:47:24 -0700

    update

--- onnxruntime/test/shared_lib/test_inference.cc
+++ onnxruntime/test/shared_lib/test_inference.cc
@@ -788,8 +788,7 @@ TEST(CApiTest, io_binding) {
   binding.ClearBoundOutputs();
 }
 
-//TODO: Replace the function call OrtSessionOptionsAppendExecutionProvider_CUDA
-#if defined(USE_CUDA)
+#if defined(USE_CUDA) || defined(USE_TENSORRT)
 TEST(CApiTest, io_binding_cuda) {
   struct CudaMemoryDeleter {
     explicit CudaMemoryDeleter(const Ort::Allocator* alloc) {
@@ -803,7 +802,11 @@ TEST(CApiTest, io_binding_cuda) {
   };
 
   Ort::SessionOptions session_options;
+#ifdef USE_TENSORRT
+  Ort::ThrowOnError(OrtSessionOptionsAppendExecutionProvider_Tensorrt(session_options, 0));
+#else
   Ort::ThrowOnError(OrtSessionOptionsAppendExecutionProvider_CUDA(session_options, 0));
+#endif
   Ort::Session session(*ort_env, MODEL_URI, session_options);
 
   Ort::MemoryInfo info_cuda("Cuda", OrtAllocatorType::OrtArenaAllocator, 0, OrtMemTypeDefault);
