cf1d02b9b146388a65ab95959f065a20044d2c1e
Chandru Ramakrishnan <chandrur@microsoft.com>
2021-04-28 10:59:04 -0400

    Updated to noexcept.

--- onnxruntime/core/providers/shared_library/provider_bridge_provider.cc
+++ onnxruntime/core/providers/shared_library/provider_bridge_provider.cc
@@ -17,12 +17,13 @@
 #define _Post_writable_byte_size_(n)
 #endif
 
+#ifndef _GLIBCXX_USE_NOEXCEPT 
+#define _GLIBCXX_USE_NOEXCEPT 
+#endif
+
 // Override default new/delete so that we match the host's allocator
 _Ret_notnull_ _Post_writable_byte_size_(n) void* operator new(size_t n) { return Provider_GetHost()->HeapAllocate(n); }
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wimplicit-exception-spec-mismatch"
-void operator delete(void* p) { return Provider_GetHost()->HeapFree(p); }
-#pragma clang diagnostic pop
+void operator delete(void* p) _GLIBCXX_USE_NOEXCEPT { return Provider_GetHost()->HeapFree(p); }
 void operator delete(void* p, size_t /*size*/) { return Provider_GetHost()->HeapFree(p); }
 
 namespace onnxruntime {
