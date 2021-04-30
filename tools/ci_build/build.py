54db6648afbeecf1208dbb6c3308a1106f3e7826
Tang, Cheng <souptc@gmail.com>
2021-04-30 13:33:58 -0700

    kerne invoker api for eager mode (#7473)
    * initial draft for kernel invoke api
    
    * initial implementation of kernel invoker
    
    * [eager] fix build on Mac
    
    * [eager] increment input name in kernel invoker
    
    * temp fix for type in eager mode
    
    * use global default log manager
    
    * rollback the previous commit since it break linux build
    
    * Revert "rollback the previous commit since it break linux build"
    
    This reverts commit 58c2c3423ae19cd53459be75618e17935d572ab3.
    
    * Eager Mode: fix linking on macOS
    
    * optimizer_execution_frame: ignore unused lambda capture (model_path)
    
    * fix link issue
    
    * ORTInvoker: set correct input argument tensor element proto types
    
    Do not set a type proto on output arguments to allow ORT to deduce them
    
    * ORTInvoker: create only one logging manager
    
    * Minor fix to set execution provider type correctly. (#7000)
    
    Co-authored-by: Chandru Ramakrishnan <chandru-r@github.com>
    
    * training fix
    
    * support config output ml values in frame, so we can use it to implement inplace update
    
    * Fix range loop error while building. (#7087)
    
    Co-authored-by: Chandru Ramakrishnan <chandru-r@github.com>
    
    * Conditionally link with nsync_cpp if not windows. (#7151)
    
    Co-authored-by: Chandru Ramakrishnan <chandru-r@github.com>
    
    * Fixed initialization order in ORT kernel invoker (#7342)
    
    * Updated constructor of ort_kernel_invoker to take a logger.
    
    * Changed linking order.
    
    * Updated test.
    
    * add inplace ut
    
    * add build option
    
    * Update include/onnxruntime/core/eager/ort_kernel_invoker.h
    
    Co-authored-by: Derek Murray <Derek.Murray@microsoft.com>
    
    * resolve comments in pr
    
    * fix build break;merge from master
    
    * fix build break
    
    Co-authored-by: Cheng Tang <chenta@microsoft.com>
    Co-authored-by: Aaron Bockover <abock@microsoft.com>
    Co-authored-by: Chandru Ramakrishnan <41447659+chandru-r@users.noreply.github.com>
    Co-authored-by: Chandru Ramakrishnan <chandru-r@github.com>
    Co-authored-by: Derek Murray <Derek.Murray@microsoft.com>

--- tools/ci_build/build.py
+++ tools/ci_build/build.py
@@ -519,7 +519,11 @@ def parse_arguments():
     parser.add_argument("--code_coverage", action='store_true',
                         help="Generate code coverage when targetting Android (only).")
     parser.add_argument(
-        "--ms_experimental", action='store_true', help="Build microsoft experimental operators.")
+        "--ms_experimental", action='store_true', help="Build microsoft experimental operators.")\
+
+    parser.add_argument(
+        "--build_eager_mode", action='store_true',
+        help="Build ONNXRuntime micro-benchmarks.")
 
     return parser.parse_args()
 
@@ -740,6 +744,7 @@ def generate_build_tree(cmake_path, source_dir, build_dir, cuda_home, cudnn_home
         "-Donnxruntime_ENABLE_WEBASSEMBLY_EXCEPTION_CATCHING=" + ("OFF" if args.disable_wasm_exception_catching
                                                                   else "ON"),
         "-Donnxruntime_ENABLE_WEBASSEMBLY_THREADS=" + ("ON" if args.enable_wasm_threads else "OFF"),
+        "-Donnxruntime_ENABLE_EAGER_MODE=" + ("ON" if args.build_eager_mode else "OFF"),
         "-Donnxruntime_ENABLE_WEBASSEMBLY_SOURCEMAP=" + ("ON" if args.enable_wasm_sourcemap else "OFF"),
         "-Donnxruntime_WEBASSEMBLY_SOURCEMAP_BASE=" + (args.wasm_sourcemap_base if args.enable_wasm_sourcemap else ""),
     ]
