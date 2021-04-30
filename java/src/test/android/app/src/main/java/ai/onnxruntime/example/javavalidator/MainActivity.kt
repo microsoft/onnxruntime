15d3af9eeb84a2ebbb5d37ed669a89c9e129fd0d
Rachel Guo <guorachel@microsoft.com>
2021-04-29 17:51:49 -0700

    fix typo in project folder

---
+++ java/src/test/android/app/src/main/java/ai/onnxruntime/example/javavalidator/MainActivity.kt
@@ -0,0 +1,12 @@
+package ai.onnxruntime.example.javavalidator
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+/*Empty activity app mainly used for testing*/
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
