eeaef974f671812deaf5c5f27de692da1990b285
Sherlock Huang <bahuang@OrtTrainingDev3.af05slrtruoetgaxwwjv5nsq5e.px.internal.cloudapp.net>
2021-05-01 07:47:11 +0000

    Improve tol value logging in ORTModule test

--- orttraining/orttraining/test/python/orttraining_test_ortmodule_api.py
+++ orttraining/orttraining/test/python/orttraining_test_ortmodule_api.py
@@ -432,7 +432,6 @@ def test_model_and_input_without_device():
     out = model(x)
     out is not None
 
-#TODO: Re-enable this Test when .to(), .cpu() and .cuda() are fixed
 def test_model_with_different_devices_same_session():
     N, D_in, H, D_out = 64, 784, 500, 10
     model = NeuralNetSinglePositionalArgument(D_in, H, D_out)
@@ -486,7 +485,7 @@ def test_gradient_correctness():
         pt_prediction = run_step(pt_model, x)
         ort_prediction = run_step(ort_model, x)
         
-        assert torch.allclose(ort_prediction, pt_prediction)
+        _test_helpers.assert_values_are_close(ort_prediction, pt_prediction)
         _test_helpers.assert_gradients_match_and_reset_gradient(ort_model, pt_model)
 
 @pytest.mark.parametrize("use_fp16", [False, True])
@@ -525,11 +524,11 @@ def test_gradient_correctness_conv1d(use_fp16, input_requires_grad):
         ort_prediction = run_step(ort_model, x)
         
         if use_fp16:
-            assert torch.allclose(ort_prediction, pt_prediction, atol=1e-3, rtol=1e-3)
+            _test_helpers.assert_values_are_close(ort_prediction, pt_prediction, atol=1e-3, rtol=1e-3)
             _test_helpers.assert_gradients_match_and_reset_gradient(ort_model, pt_model, rtol=1e-2, atol=1e-2)
         else:
-            assert torch.allclose(ort_prediction, pt_prediction, atol=1e-5)
-            _test_helpers.assert_gradients_match_and_reset_gradient(ort_model, pt_model, rtol=5e-3, atol=1e-3)
+            _test_helpers.assert_values_are_close(ort_prediction, pt_prediction, atol=1e-5)
+            _test_helpers.assert_gradients_match_and_reset_gradient(ort_model, pt_model, rtol=5e-3, atol=4e-3)
 
 def test_module_with_non_differential_output():
     device = 'cuda'
@@ -548,12 +547,12 @@ def test_module_with_non_differential_output():
         pt_prediction1, pt_mask1, pt_prediction2, pt_mask2 = run_step(pt_model, x)
         ort_prediction1, ort_mask1, ort_prediction2, ort_mask2 = run_step(ort_model, x)
 
-        # assert torch.allclose(ort_prediction1, pt_prediction1)   # TODO: this is failing, need to investigate!
-                                                                   # This will be no reproducible if we change the model forward to 
-                                                                   # mask1 = torch.gt(out, 0.01)
-        assert torch.allclose(ort_prediction2, pt_prediction2)
-        assert torch.allclose(ort_mask1, pt_mask1)
-        assert torch.allclose(ort_mask2, pt_mask2)
+        # _test_helpers.assert_values_are_close(ort_prediction1, pt_prediction1)   # TODO: this is failing, need to investigate!
+                                                                                   # This will be no reproducible if we change the model forward to 
+                                                                                   # mask1 = torch.gt(out, 0.01)
+        _test_helpers.assert_values_are_close(ort_prediction2, pt_prediction2)
+        _test_helpers.assert_values_are_close(ort_mask1, pt_mask1)
+        _test_helpers.assert_values_are_close(ort_mask2, pt_mask2)
         _test_helpers.assert_gradients_match_and_reset_gradient(ort_model, pt_model)
 
 def test_multiple_forward_only_calls():
@@ -567,7 +566,7 @@ def test_multiple_forward_only_calls():
         pt_prediction = pt_model(x)
         ort_prediction = ort_model(x)
 
-        assert torch.allclose(ort_prediction, pt_prediction)
+        _test_helpers.assert_values_are_close(ort_prediction, pt_prediction)
 
 def test_nesting_forward_backward_calls():
     device = 'cuda'
@@ -580,7 +579,7 @@ def test_nesting_forward_backward_calls():
     pt_x1 = copy.deepcopy(ort_x1)
     ort_prediction1 = ort_model(ort_x1)
     pt_prediction1 = pt_model(pt_x1)
-    assert torch.allclose(ort_prediction1, pt_prediction1)
+    _test_helpers.assert_values_are_close(ort_prediction1, pt_prediction1)
     ort_loss1 = ort_prediction1.sum()
     pt_loss1 = pt_prediction1.sum()
     # forward2
@@ -590,16 +589,16 @@ def test_nesting_forward_backward_calls():
     ort_loss2 = ort_prediction2.sum()
     pt_prediction2 = pt_model(pt_x2)
     pt_loss2 = pt_prediction2.sum()
-    assert torch.allclose(ort_prediction2, pt_prediction2)
+    _test_helpers.assert_values_are_close(ort_prediction2, pt_prediction2)
     # backward2
     ort_loss2.backward()
     pt_loss2.backward()
-    assert torch.allclose(ort_x2.grad, ort_x2.grad)
+    _test_helpers.assert_values_are_close(ort_x2.grad, ort_x2.grad)
     _test_helpers.assert_gradients_match_and_reset_gradient(ort_model, pt_model)
     # backward1
     ort_loss1.backward()
     pt_loss1.backward()
-    assert torch.allclose(ort_x1.grad, pt_x1.grad)
+    _test_helpers.assert_values_are_close(ort_x1.grad, pt_x1.grad)
     _test_helpers.assert_gradients_match_and_reset_gradient(ort_model, pt_model)
 
 def test_multiple_overlapping_forward_backward_calls():
@@ -631,10 +630,10 @@ def test_multiple_overlapping_forward_backward_calls():
         pt_prediction1, pt_prediction2 = run_step(pt_model, pt_x1, pt_x2)
         ort_prediction1, ort_prediction2 = run_step(ort_model, ort_x1, ort_x2)
         
-        assert torch.allclose(ort_prediction1, pt_prediction1)
-        assert torch.allclose(ort_prediction2, pt_prediction2)
-        assert torch.allclose(ort_x1.grad, pt_x1.grad)
-        assert torch.allclose(ort_x2.grad, pt_x2.grad)
+        _test_helpers.assert_values_are_close(ort_prediction1, pt_prediction1)
+        _test_helpers.assert_values_are_close(ort_prediction2, pt_prediction2)
+        _test_helpers.assert_values_are_close(ort_x1.grad, pt_x1.grad)
+        _test_helpers.assert_values_are_close(ort_x2.grad, pt_x2.grad)
         _test_helpers.assert_gradients_match_and_reset_gradient(ort_model, pt_model)
 
 def test_multiple_ortmodules_training():
@@ -661,8 +660,8 @@ def test_multiple_ortmodules_training():
         pt_prediction1, pt_prediction2 = run_step(pt_model1, pt_model2, x1, x2)
         ort_prediction1, ort_prediction2 = run_step(ort_model1, ort_model2, x1, x2)
 
-        assert torch.allclose(ort_prediction1, pt_prediction1, atol=1e-6)
-        assert torch.allclose(ort_prediction2, pt_prediction2, atol=1e-6)
+        _test_helpers.assert_values_are_close(ort_prediction1, pt_prediction1, atol=1e-6)
+        _test_helpers.assert_values_are_close(ort_prediction2, pt_prediction2, atol=1e-6)
         _test_helpers.assert_gradients_match_and_reset_gradient(ort_model1, pt_model1)
         _test_helpers.assert_gradients_match_and_reset_gradient(ort_model2, pt_model2)
 
@@ -689,7 +688,7 @@ def test_multiple_ortmodules_common_backbone_training():
         pt_prediction = run_step(pt_model0, pt_model1, x1)
         ort_prediction = run_step(ort_model0, ort_model1, x1)
 
-        assert torch.allclose(ort_prediction, pt_prediction)
+        _test_helpers.assert_values_are_close(ort_prediction, pt_prediction)
         _test_helpers.assert_gradients_match_and_reset_gradient(ort_model0, pt_model0, reset_gradient=False)
         _test_helpers.assert_gradients_match_and_reset_gradient(ort_model1, pt_model1)
 
@@ -698,8 +697,8 @@ def test_multiple_ortmodules_common_backbone_training():
         pt_prediction = run_step(pt_model0, pt_model2, x1)
         ort_prediction = run_step(ort_model0, ort_model2, x1)
 
-        assert torch.allclose(ort_prediction, pt_prediction)
-        _test_helpers.assert_gradients_match_and_reset_gradient(ort_model0, pt_model0, reset_gradient=True)
+        _test_helpers.assert_values_are_close(ort_prediction, pt_prediction)
+        _test_helpers.assert_gradients_match_and_reset_gradient(ort_model0, pt_model0, reset_gradient=True, atol=1.5e-6)
         _test_helpers.assert_gradients_match_and_reset_gradient(ort_model2, pt_model2)
 
 def test_multiple_chained_ortmodules_training():
@@ -721,7 +720,7 @@ def test_multiple_chained_ortmodules_training():
         pt_prediction = run_step(pt_model1, pt_model2, x)
         ort_prediction = run_step(ort_model1, ort_model2, x)
 
-        assert torch.allclose(ort_prediction, pt_prediction)
+        _test_helpers.assert_values_are_close(ort_prediction, pt_prediction)
         _test_helpers.assert_gradients_match_and_reset_gradient(ort_model1, pt_model1)
         _test_helpers.assert_gradients_match_and_reset_gradient(ort_model2, pt_model2)
 
@@ -749,10 +748,10 @@ def test_mixed_nnmodule_ortmodules_training():
         pt_p1, pt_p2, pt_p3 = run_step(pt_model1, pt_model2, pt_model3, x1, x2)
         ort_p1, ort_p2, ort_p3 = run_step(ort_model1, ort_model2, ort_model3, x1, x2)
 
-        assert torch.allclose(ort_p1, pt_p1, atol=1e-06)
-        assert torch.allclose(ort_p2, pt_p2, atol=1e-06)
-        assert torch.allclose(ort_p3, pt_p3, atol=1e-06)
-        _test_helpers.assert_gradients_match_and_reset_gradient(ort_model1, pt_model1)
+        _test_helpers.assert_values_are_close(ort_p1, pt_p1, atol=1e-06)
+        _test_helpers.assert_values_are_close(ort_p2, pt_p2, atol=1e-06)
+        _test_helpers.assert_values_are_close(ort_p3, pt_p3, atol=1e-06)
+        _test_helpers.assert_gradients_match_and_reset_gradient(ort_model1, pt_model1, atol=2e-6)
         _test_helpers.assert_gradients_match_and_reset_gradient(ort_model2, pt_model2)
         _test_helpers.assert_gradients_match_and_reset_gradient(ort_model3, pt_model3)
 
@@ -800,7 +799,7 @@ def test_ortmodule_inputs_with_dynamic_shape():
         pt_p = run_step(pt_model, x)
         ort_p = run_step(ort_model, x)
 
-        assert torch.allclose(ort_p, pt_p, atol=1e-6)    # relaxing tolerance, 1e-7 or less would fail
+        _test_helpers.assert_values_are_close(ort_p, pt_p, atol=1e-6)    # relaxing tolerance, 1e-7 or less would fail
         _test_helpers.assert_gradients_match_and_reset_gradient(ort_model, pt_model)
 
 
@@ -824,7 +823,7 @@ def test_bert_inputs_with_dynamic_shape():
         pt_p = run_step(pt_model, x, y, z)
         ort_p = run_step(ort_model, x, y, z)
 
-        assert torch.allclose(ort_p, pt_p, atol=1e-02)      # TODO: this assert is failing with smaller tolerance, need to investigate!!
+        _test_helpers.assert_values_are_close(ort_p, pt_p, atol=1e-02)      # TODO: this assert is failing with smaller tolerance, need to investigate!!
         # _test_helpers.assert_gradients_match_and_reset_gradient(ort_model, pt_model)  #TODO - enable this check after the investigation
 
 
@@ -868,9 +867,9 @@ def test_input_requires_grad_backward_creates_input_grad_as_required0(device):
     pt_y1 = run_step0(pt_model, pt_x1, pt_x2)
     ort_y1 = run_step0(ort_model, ort_x1, ort_x2)
 
-    assert torch.allclose(pt_y1, ort_y1, atol=1e-06)
-    assert torch.allclose(ort_x1.grad, pt_x1.grad)
-    assert torch.allclose(ort_x2.grad, pt_x2.grad)
+    _test_helpers.assert_values_are_close(pt_y1, ort_y1, atol=1e-06)
+    _test_helpers.assert_values_are_close(ort_x1.grad, pt_x1.grad)
+    _test_helpers.assert_values_are_close(ort_x2.grad, pt_x2.grad)
     # backward() is from y1, so grad of fc2.weight and fc2.bias will not be calculated.
     _test_helpers.assert_gradients_match_and_reset_gradient(ort_model, pt_model, none_pt_params=['fc2.weight', 'fc2.bias'], reset_gradient=True)
 
@@ -883,9 +882,9 @@ def test_input_requires_grad_backward_creates_input_grad_as_required0(device):
     pt_y2 = run_step1(pt_model, pt_x1, pt_x2)
     ort_y2 = run_step1(ort_model, ort_x1, ort_x2)
 
-    assert torch.allclose(pt_y2, ort_y2, atol=1e-06)
-    assert torch.allclose(ort_x1.grad, pt_x1.grad)
-    assert torch.allclose(ort_x2.grad, pt_x2.grad)
+    _test_helpers.assert_values_are_close(pt_y2, ort_y2, atol=1e-06)
+    _test_helpers.assert_values_are_close(ort_x1.grad, pt_x1.grad)
+    _test_helpers.assert_values_are_close(ort_x2.grad, pt_x2.grad)
     # backward() is from y2, so grad of fc1.weight and fc1.bias will not be calculated.
     _test_helpers.assert_gradients_match_and_reset_gradient(ort_model, pt_model, none_pt_params=['fc1.weight', 'fc1.bias'])
 
@@ -911,8 +910,8 @@ def test_loss_combines_two_outputs_with_dependency(device):
     pt_y1, pt_y2 = run_step(pt_model, pt_x1, pt_x2)
     ort_y1, ort_y2 = run_step(ort_model, ort_x1, ort_x2)
 
-    assert torch.allclose(pt_y1, ort_y1, atol=1e-06)
-    assert torch.allclose(pt_y2, ort_y2, atol=1e-06)
+    _test_helpers.assert_values_are_close(pt_y1, ort_y1, atol=1e-06)
+    _test_helpers.assert_values_are_close(pt_y2, ort_y2, atol=1e-06)
     _test_helpers.assert_gradients_match_and_reset_gradient(ort_model, pt_model)
 
 @pytest.mark.parametrize("x1_requires_grad", [True, False])
@@ -940,8 +939,8 @@ def test_input_requires_grad_backward_creates_input_grad_as_required1(x1_require
     pt_y1, pt_y2 = run_step(pt_model, pt_x1, pt_x2)
     ort_y1, ort_y2 = run_step(ort_model, ort_x1, ort_x2)
 
-    assert torch.allclose(ort_y1, pt_y1, atol=1e-06)
-    assert torch.allclose(ort_y2, pt_y2, atol=1e-06)
+    _test_helpers.assert_values_are_close(ort_y1, pt_y1, atol=1e-06)
+    _test_helpers.assert_values_are_close(ort_y2, pt_y2, atol=1e-06)
     assert not x1_requires_grad or ort_x1.grad is not None
     assert not x2_requires_grad or ort_x2.grad is not None
     assert not x1_requires_grad or torch.allclose(ort_x1.grad, pt_x1.grad)
@@ -1770,7 +1769,7 @@ def test_eval_with_dropout():
     assert output is not None
     assert output_pt is not None
     # Assert that the output from torch is the same as the one from ORTModule
-    assert torch.equal(output, output_pt)
+    _test_helpers.assert_values_are_close(output, output_pt)
 
 def test_with_torch_no_grad_context():
     device = 'cuda'
@@ -1792,7 +1791,7 @@ def test_with_torch_no_grad_context():
     assert output is not None
     assert output_pt is not None
     # Assert that the output from torch is the same as the one from ORTModule
-    assert torch.equal(output, output_pt)
+    _test_helpers.assert_values_are_close(output, output_pt)
     assert output.grad is None and output_pt.grad is None
 
 def test_unused_layer():
