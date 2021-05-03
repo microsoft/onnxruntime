50abaab4cb49e52c50a2e21005369cf425076bdc
Rachel Guo <guorachel@microsoft.com>
2021-05-03 16:17:01 -0700

    address comments

--- java/src/test/android/app/src/androidTest/java/ai/onnxruntime/example/javavalidator/SimpleTest.kt
+++ java/src/test/android/app/src/androidTest/java/ai/onnxruntime/example/javavalidator/SimpleTest.kt
@@ -20,29 +20,35 @@ class SimpleTest {
         val env = OrtEnvironment.getEnvironment()
         env.use {
             val opts = SessionOptions()
-            val session = env.createSession(readModel("sigmoid.ort"), opts)
-            val inputName = session.inputNames.iterator().next()
-            val testdata = Array(3) { Array(4) { FloatArray(5) } }
-            val expected =  Array(3) { Array(4) { FloatArray(5) } }
-            for (i in 0..2) {
-                for (j in 0..3) {
-                    for (k in 0..4) {
-                        testdata[i][j][k] = (i+j+k).toFloat()
-                        //expected sigmoid output is y = 1.0 / (1.0 + exp(-x))
-                        expected[i][j][k] = (1.0 / (1.0 + kotlin.math.exp(-testdata[i][j][k]))).toFloat()
+            opts.use {
+                val session = env.createSession(readModel("sigmoid.ort"), opts)
+                session.use {
+                    val inputName = session.inputNames.iterator().next()
+                    val testdata = Array(3) { Array(4) { FloatArray(5) } }
+                    val expected =  Array(3) { Array(4) { FloatArray(5) } }
+                    for (i in 0..2) {
+                        for (j in 0..3) {
+                            for (k in 0..4) {
+                                testdata[i][j][k] = (i+j+k).toFloat()
+                                //expected sigmoid output is y = 1.0 / (1.0 + exp(-x))
+                                expected[i][j][k] = (1.0 / (1.0 + kotlin.math.exp(-testdata[i][j][k]))).toFloat()
+                            }
+                        }
                     }
-                }
-            }
-            val inputTensor = OnnxTensor.createTensor(env, testdata)
-            inputTensor.use {
-                val output = session.run(Collections.singletonMap(inputName, inputTensor))
-                val rawOutput = output[0].value as Array<Array<FloatArray>>
-                for (i in 0..2) {
-                    for (j in 0..3) {
-                        for (k in 0..4) {
-                            val actual = (kotlin.math.round(rawOutput[i][j][k] * 1000.0) / 1000.0).toFloat()
-                            val expected = (kotlin.math.round(expected[i][j][k] * 1000.0) / 1000.0).toFloat()
-                            Assert.assertEquals(actual, expected)
+                    val inputTensor = OnnxTensor.createTensor(env, testdata)
+                    inputTensor.use {
+                        val output = session.run(Collections.singletonMap(inputName, inputTensor))
+                        output.use {
+                            val rawOutput = output[0].value as Array<Array<FloatArray>>
+                            for (i in 0..2) {
+                                for (j in 0..3) {
+                                    for (k in 0..4) {
+                                        val actual = (kotlin.math.round(rawOutput[i][j][k] * 1000.0) / 1000.0).toFloat()
+                                        val expected = (kotlin.math.round(expected[i][j][k] * 1000.0) / 1000.0).toFloat()
+                                        Assert.assertEquals(actual, expected, 1e-15.toFloat())
+                                    }
+                                }
+                            }
                         }
                     }
                 }
