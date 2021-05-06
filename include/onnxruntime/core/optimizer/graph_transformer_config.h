12ece2ace9edeec539a45a86dd32b6a934025191
Satya Jandhyala <satya.k.jandhyala@gmail.com>
2021-05-06 19:29:09 +0000

    Removed unnecessary comments.

--- include/onnxruntime/core/optimizer/graph_transformer_config.h
+++ include/onnxruntime/core/optimizer/graph_transformer_config.h
@@ -12,7 +12,7 @@ struct GraphTransformerConfiguration {
     // Cast propagation strategy. One strategy is to insert casts around all the nodes with the allowed opcodes
     // and reduce, remove redundent and back-to-back casts, and
     // the other is to propagate casts using flood-fill approach, expanding float16 regions in the graph
-    // traversinvg the graph up/down.
+    // traversing the graph up/down.
     enum class Strategy {
       InsertAndReduce = 0,
       FloodFill = 1
@@ -20,8 +20,7 @@ struct GraphTransformerConfiguration {
     Strategy strategy = Strategy::InsertAndReduce;
     // List of allowed opcodes to consider as safe to execute in float16, while moving cast operations
     std::vector<std::string> allow;
-    // Whether allow fusion of layer norm subgraph if doing so will cause modified precision.
-  };  // Whether to enable GELU approximation which is faster but produces different results.
+  };
   PropagateCastOpsConfiguration propagate_cast_ops_config;
 };
 
