1fcdf723485f572e6a80147198ead5292da7f3e0
ashbhandare <ash.bhandare@gmail.com>
2021-05-06 09:39:34 -0700

    Improve CPU node placement logic to avoid Memcpys (#7427)
    * Modify CPU fallback logic
    
    * Review comments, failing test
    
    * Add test for topological order
    
    * review comment
    
    * Fix test for amd ci
    
    * fix build
    
    * Fix amd test

--- onnxruntime/core/framework/fallback_cpu_capability.cc
+++ onnxruntime/core/framework/fallback_cpu_capability.cc
@@ -7,7 +7,10 @@
 
 #include "onnx/defs/data_type_utils.h"
 
+#include "core/framework/execution_providers.h"
+#include "core/framework/kernel_registry_manager.h"
 #include "core/framework/op_kernel.h"
+#include "core/providers/cpu/cpu_execution_provider.h"
 
 using namespace ONNX_NAMESPACE::Utils;
 
@@ -45,18 +48,43 @@ std::unordered_set<NodeIndex> GetCpuPreferredNodes(const onnxruntime::GraphViewe
     return node_id_to_order_map[n1] > node_id_to_order_map[n2];
   };
 
-  std::priority_queue<NodeIndex, std::vector<NodeIndex>, decltype(greater_order_comp)> candidates(greater_order_comp);
+  // If return false, n2 will be output first; If return true, n1 will be output first
+  auto lesser_order_comp = [&](const NodeIndex n1, const NodeIndex n2) {
+    return node_id_to_order_map[n1] < node_id_to_order_map[n2];
+  };
+
+  std::priority_queue<NodeIndex, std::vector<NodeIndex>, decltype(greater_order_comp)> candidates_fw(greater_order_comp);
+  std::priority_queue<NodeIndex, std::vector<NodeIndex>, decltype(lesser_order_comp)> candidates_bw(lesser_order_comp);
   std::unordered_set<NodeIndex> visited;
 
-  std::unordered_set<const NodeArg*> cpu_output_args;
+  std::unordered_set<const NodeArg*> cpu_args;
   std::unordered_set<NodeIndex> provider_nodes;
   std::unordered_map<NodeIndex, const KernelCreateInfo*> node_to_kernel;
+  std::unordered_set<NodeIndex> cpu_kernel_available;
+
+  // create a temp CPU kernel registry
+  KernelRegistryManager mgr;
+  ExecutionProviders cpu_ep;
+  CPUExecutionProviderInfo epi{false};
+  ORT_ENFORCE(cpu_ep.Add(kCpuExecutionProvider, std::make_unique<CPUExecutionProvider>(epi)).IsOK());
+  ORT_ENFORCE(mgr.RegisterKernels(cpu_ep).IsOK());
+  std::vector<const KernelRegistry*> cpu_kernel_registries = mgr.GetKernelRegistriesByProviderType(kCpuExecutionProvider);
 
   for (auto& node_id : tentative_nodes) {
     provider_nodes.insert(node_id);
     const Node* node = graph.GetNode(node_id);
 
     const KernelCreateInfo* kernel_info = nullptr;
+
+    // Get the CPU kernel availability for this node
+    for (auto registry : cpu_kernel_registries) {
+      auto st = registry->TryFindKernel(*node, kCpuExecutionProvider, &kernel_info);
+      if (st.IsOK()) {
+        cpu_kernel_available.insert(node_id);
+        break;
+      }
+    }
+
     for (auto registry : kernel_registries) {
       auto st = registry->TryFindKernel(*node, provider_type, &kernel_info);
       if (st.IsOK())
@@ -71,11 +99,26 @@ std::unordered_set<NodeIndex> GetCpuPreferredNodes(const onnxruntime::GraphViewe
         node->OutputDefs(),
         [&](const NodeArg& node_arg, size_t out_index) {
           if (kernel_info->kernel_def->IsOutputOnCpu(out_index)) {
-            cpu_output_args.insert(&node_arg);
+            cpu_args.insert(&node_arg);
             auto consumer_nodes = graph.GetConsumerNodes(node_arg.Name());
             for (auto& consumer_node : consumer_nodes) {
-              candidates.push(consumer_node->Index());
-              LOGS_DEFAULT(INFO) << "Candidate for fallback CPU execution: " << consumer_node->Name();
+              candidates_fw.push(consumer_node->Index());
+              LOGS_DEFAULT(INFO) << "Candidate for fallback CPU execution in forward trace: " << consumer_node->Name();
+            }
+          }
+          return Status::OK();
+        }));
+
+    // then, find all the direct producers of cpu tensors.
+    ORT_THROW_IF_ERROR(node->ForEachWithIndex(
+        node->InputDefs(),
+        [&](const NodeArg& node_arg, size_t in_index) {
+          if (kernel_info->kernel_def->IsInputOnCpu(in_index)) {
+            cpu_args.insert(&node_arg);
+            auto producer_node = graph.GetProducerNode(node_arg.Name());
+            if (producer_node != nullptr) {
+              candidates_bw.push(producer_node->Index());
+              LOGS_DEFAULT(INFO) << "Candidate for fallback CPU execution in backward trace: " << producer_node->Name();
             }
           }
           return Status::OK();
@@ -89,9 +132,9 @@ std::unordered_set<NodeIndex> GetCpuPreferredNodes(const onnxruntime::GraphViewe
   // The detail:
   // for each candidate, if one of its input is a cpu tensor and the Non-CPU kernel doesn't mark it as cpu input,
   // force the node to CPU to avoid memory cpu and add its output to the small cpu tensors.
-  while (!candidates.empty()) {
-    NodeIndex cur = candidates.top();
-    candidates.pop();
+  while (!candidates_fw.empty()) {
+    NodeIndex cur = candidates_fw.top();
+    candidates_fw.pop();
     if (visited.count(cur) != 0)
       continue;
     visited.insert(cur);
@@ -118,7 +161,7 @@ std::unordered_set<NodeIndex> GetCpuPreferredNodes(const onnxruntime::GraphViewe
       }
 
       // the input is not a CPU tensor
-      if (cpu_output_args.find(input) == cpu_output_args.end()) {
+      if (cpu_args.find(input) == cpu_args.end()) {
         place_in_cpu = false;
         break;
       }
@@ -130,16 +173,90 @@ std::unordered_set<NodeIndex> GetCpuPreferredNodes(const onnxruntime::GraphViewe
       }
     }
 
-    if (place_in_cpu) {
+    if (place_in_cpu && cpu_kernel_available.count(cur) != 0) {
       cpu_nodes.insert(cur);
       LOGS_DEFAULT(INFO) << "ORT optimization- Force fallback to CPU execution for node: " << node->Name()
                          << " because the CPU execution path is deemed faster than overhead involved with execution on other EPs "
                          << " capable of executing this node";
       for (auto* output : node->OutputDefs()) {
-        cpu_output_args.insert(output);
+        cpu_args.insert(output);
       }
       for (auto it = node->OutputNodesBegin(); it != node->OutputNodesEnd(); ++it) {
-        candidates.push((*it).Index());
+        candidates_fw.push((*it).Index());
+      }
+    }
+  }
+  // clear the visited to prepare for backward trace
+  visited.clear();
+  // Trace the graph backwards to find additional CPU nodes
+  // Starting from nodes that must produce an output on CPU, trace the producer nodes
+  // The trace stops when we find that
+  // 1) The node is already picked for CPU
+  // 2) Input/Output type is unsupported on CPU(float16/bfloat16)
+  // 3) The output is not a CPU tensor
+  // 4) The search hits a node that produces a CPU output
+  while (!candidates_bw.empty()) {
+    NodeIndex cur = candidates_bw.top();
+    candidates_bw.pop();
+    if (visited.count(cur) != 0)
+      continue;
+    visited.insert(cur);
+
+    // node is already picked for CPU
+    if (cpu_nodes.count(cur) != 0)
+      continue;
+
+    if (provider_nodes.find(cur) == provider_nodes.end())
+      continue;
+
+    auto* node = graph.GetNode(cur);
+    bool place_in_cpu = true;
+    for (size_t i = 0; i < node->OutputDefs().size(); ++i) {
+      auto* output = node->OutputDefs()[i];
+
+      // skip placing on CPU if the data typs is float16 or bfloat16
+      if (output->Type() == DataTypeUtils::ToType("float16") ||
+          output->Type() == DataTypeUtils::ToType("bfloat16")) {
+        place_in_cpu = false;
+        break;
+      }
+
+      // the output is not a CPU tensor
+      if (cpu_args.find(output) == cpu_args.end()) {
+        place_in_cpu = false;
+        break;
+      }
+
+      // output is a CPU tensor, but it's intended to be consumed as CPU output by the target EP
+      if (node_to_kernel[cur]->kernel_def->IsOutputOnCpu(i)) {
+        place_in_cpu = false;
+        break;
+      }
+    }
+    // Next, check if the node inputs are of supported type
+    if (place_in_cpu) {
+      for (size_t i = 0; i < node->InputDefs().size(); ++i) {
+        auto* input = node->InputDefs()[i];
+
+        // skip placing on CPU if the data typs is float16 or bfloat16
+        if (input->Type() == DataTypeUtils::ToType("float16") ||
+            input->Type() == DataTypeUtils::ToType("bfloat16")) {
+          place_in_cpu = false;
+          break;
+        }
+      }
+    }
+
+    if (place_in_cpu && cpu_kernel_available.count(cur) != 0) {
+      cpu_nodes.insert(cur);
+      LOGS_DEFAULT(INFO) << "ORT optimization- Force fallback to CPU execution for node: " << node->Name()
+                         << " because the CPU execution path is deemed faster than overhead involved with execution on other EPs "
+                         << " capable of executing this node";
+      for (auto* input : node->InputDefs()) {
+        cpu_args.insert(input);
+      }
+      for (auto it = node->InputNodesBegin(); it != node->InputNodesEnd(); ++it) {
+        candidates_bw.push((*it).Index());
       }
     }
   }
