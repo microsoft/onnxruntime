5cf5354348c3c31f3c39e57896b4fbece13273e3
Maajid khan <n.maajidkhan@gmail.com>
2021-05-06 19:20:18 -0700

    [OpenVINO-EP] Remove support for 2020.4 (#7580)
    * [OpenVINO-EP] Remove support for 2020.4
    
    Signed-off-by: MaajidKhan <n.maajidkhan@gmail.com>
    
    * Minor changes added
    
    Signed-off-by: MaajidKhan <n.maajidkhan@gmail.com>
    
    * Minor changes added
    
    Signed-off-by: MaajidKhan <n.maajidkhan@gmail.com>
    
    Co-authored-by: suryasidd <surya.siddharth.pemmaraju@intel.com>

--- onnxruntime/core/providers/openvino/backends/basic_backend.cc
+++ onnxruntime/core/providers/openvino/backends/basic_backend.cc
@@ -78,8 +78,7 @@ BasicBackend::BasicBackend(const ONNX_NAMESPACE::ModelProto& model_proto,
   if(!openvino_ep::BackendManager::GetGlobalContext().is_wholly_supported_graph) {
     ie_cnn_network_ = CreateCNNNetwork(model_proto, global_context_, subgraph_context_, const_outputs_map_);
     SetIODefs(model_proto, ie_cnn_network_, subgraph_context_.output_names, const_outputs_map_, global_context_.device_type);
-  #if defined(OPENVINO_2020_4) || defined(OPENVINO_2021_1) || defined(OPENVINO_2021_2) || \
-      defined(OPENVINO_2021_3) || defined(OPENVINO_2021_4)
+  #if defined(OPENVINO_2021_1) || defined(OPENVINO_2021_2) || defined(OPENVINO_2021_3) || defined(OPENVINO_2021_4)
     if (const_outputs_map_.size() == subgraph_context_.output_names.size())
       subgraph_context_.is_constant = true;
   #endif
@@ -117,8 +116,7 @@ BasicBackend::BasicBackend(const ONNX_NAMESPACE::ModelProto& model_proto,
     if(!openvino_ep::backend_utils::UseCompiledNetwork()) {
       ie_cnn_network_ = CreateCNNNetwork(model_proto, global_context_, subgraph_context_, const_outputs_map_);
       SetIODefs(model_proto, ie_cnn_network_, subgraph_context_.output_names, const_outputs_map_, global_context_.device_type);
-    #if defined(OPENVINO_2020_4) || defined(OPENVINO_2021_1) || defined(OPENVINO_2021_2) || \
-        defined(OPENVINO_2021_3) || defined(OPENVINO_2021_4)
+    #if defined(OPENVINO_2021_1) || defined(OPENVINO_2021_2) || defined(OPENVINO_2021_3) || defined(OPENVINO_2021_4)
       if (const_outputs_map_.size() == subgraph_context_.output_names.size())
         subgraph_context_.is_constant = true;
     #endif
@@ -133,22 +131,21 @@ BasicBackend::BasicBackend(const ONNX_NAMESPACE::ModelProto& model_proto,
       }
     #endif
       if (global_context_.device_type.find("MYRIAD") != std::string::npos) {
-    #if defined(OPENVINO_2021_1) || defined(OPENVINO_2021_2) || \
-        defined(OPENVINO_2021_3) || defined(OPENVINO_2021_4)
-        if (subgraph_context_.set_vpu_config) {
-          config["MYRIAD_DETECT_NETWORK_BATCH"] = CONFIG_VALUE(NO);
-        }
-        if (global_context_.enable_vpu_fast_compile) {
-          config["MYRIAD_HW_INJECT_STAGES"] = CONFIG_VALUE(NO);
-          config["MYRIAD_COPY_OPTIMIZATION"] = CONFIG_VALUE(NO);
-        }
-    #else
+    #if defined(OPENVINO_2020_3)
         if (subgraph_context_.set_vpu_config) {
           config["VPU_DETECT_NETWORK_BATCH"] = CONFIG_VALUE(NO);
         }
         if (global_context_.enable_vpu_fast_compile) {
           config["VPU_HW_INJECT_STAGES"] = CONFIG_VALUE(NO);
           config["VPU_COPY_OPTIMIZATION"] = CONFIG_VALUE(NO);
+        }
+    #else
+        if (subgraph_context_.set_vpu_config) {
+          config["MYRIAD_DETECT_NETWORK_BATCH"] = CONFIG_VALUE(NO);
+        }
+        if (global_context_.enable_vpu_fast_compile) {
+          config["MYRIAD_HW_INJECT_STAGES"] = CONFIG_VALUE(NO);
+          config["MYRIAD_COPY_OPTIMIZATION"] = CONFIG_VALUE(NO);
         }
     #endif
     #if defined(OPENVINO_2021_4)
@@ -248,8 +245,7 @@ void BasicBackend::CompleteAsyncInference(Ort::CustomOpApi& ort, OrtKernelContex
     size_t batch_slice = 0;
     FillOutputBlob(graph_output_blob, output_tensor, ort, precision, batch_slice);
   }
-#if defined(OPENVINO_2020_4) || defined(OPENVINO_2021_1) || defined(OPENVINO_2021_2) || \
-    defined(OPENVINO_2021_3) || defined(OPENVINO_2021_4)
+#if defined(OPENVINO_2021_1) || defined(OPENVINO_2021_2) || defined(OPENVINO_2021_3) || defined(OPENVINO_2021_4)
   if (!const_outputs_map_.empty()) {
     for (auto item : const_outputs_map_) {
       auto out_name = item.first;
@@ -269,8 +265,7 @@ void BasicBackend::Infer(Ort::CustomOpApi& ort, OrtKernelContext* context) {
   LOGS_DEFAULT(INFO) << log_tag << "In Infer";
 
   if (subgraph_context_.is_constant) {
-#if defined(OPENVINO_2020_4) || defined(OPENVINO_2021_1)  || defined(OPENVINO_2021_2) || \
-    defined(OPENVINO_2021_3) || defined(OPENVINO_2021_4)
+#if defined(OPENVINO_2021_1)  || defined(OPENVINO_2021_2) || defined(OPENVINO_2021_3) || defined(OPENVINO_2021_4)
     for (auto item : const_outputs_map_) {
       auto out_name = item.first;
       auto node = item.second;
