23f326ae3e51095100617a318ea070ce39390495
Guoyu Wang <wanggy@outlook.com>
2021-05-10 22:08:31 -0700

    update variable names

--- tools/ci_build/github/apple/build_ios_framework.py
+++ tools/ci_build/github/apple/build_ios_framework.py
@@ -23,21 +23,21 @@ DEFAULT_BUILD_OSX_ARCHS = [
 
 
 def _parse_build_settings(args):
-    _setting_file = args.build_settings_file.resolve()
+    setting_file = args.build_settings_file.resolve()
 
-    if not _setting_file.is_file():
-        raise FileNotFoundError('Build config file {} is not a file.'.format(_setting_file))
+    if not setting_file.is_file():
+        raise FileNotFoundError('Build config file {} is not a file.'.format(setting_file))
 
-    with open(_setting_file) as f:
-        _build_settings_data = json.load(f)
+    with open(setting_file) as f:
+        build_settings_data = json.load(f)
 
     build_settings = {}
 
-    build_settings["build_osx_archs"] = _build_settings_data.get("build_osx_archs", DEFAULT_BUILD_OSX_ARCHS)
+    build_settings["build_osx_archs"] = build_settings_data.get("build_osx_archs", DEFAULT_BUILD_OSX_ARCHS)
 
     build_params = []
-    if 'build_params' in _build_settings_data:
-        build_params += _build_settings_data['build_params']
+    if 'build_params' in build_settings_data:
+        build_params += build_settings_data['build_params']
     else:
         raise ValueError('build_params is required in the build config file')
 
@@ -46,65 +46,65 @@ def _parse_build_settings(args):
 
 
 def _build_package(args):
-    _include_ops_by_config_file = args.include_ops_by_config.resolve()
-    if not _include_ops_by_config_file.is_file():
-        raise FileNotFoundError('Include ops config file {} is not a file.'.format(_include_ops_by_config_file))
+    include_ops_by_config_file = args.include_ops_by_config.resolve()
+    if not include_ops_by_config_file.is_file():
+        raise FileNotFoundError('Include ops config file {} is not a file.'.format(include_ops_by_config_file))
 
     build_settings = _parse_build_settings(args)
     build_dir = os.path.abspath(args.build_dir)
 
     # Temp dirs to hold building results
-    _intermediates_dir = os.path.join(build_dir, 'intermediates')
-    _build_config = args.config
-    _base_build_command = [sys.executable, BUILD_PY, '--config=' + _build_config] + build_settings['build_params']
+    intermediates_dir = os.path.join(build_dir, 'intermediates')
+    build_config = args.config
+    base_build_command = [sys.executable, BUILD_PY, '--config=' + build_config] + build_settings['build_params']
 
     # paths of the onnxruntime libraries for different archs
-    _ort_libs = []
-    _info_plist = ''
+    ort_libs = []
+    info_plist_path = ''
 
     # Build binary for each arch, one by one
-    for _osx_arch in build_settings['build_osx_archs']:
-        _sysroot = _osx_arch['sysroot']
-        _arch = _osx_arch['arch']
-        _build_dir_current_arch = os.path.join(_intermediates_dir, _sysroot + "_" + _arch)
-        _build_command = _base_build_command + [
-            '--ios_sysroot=' + _sysroot,
-            '--osx_arch=' + _arch,
-            '--build_dir=' + _build_dir_current_arch
+    for osx_arch in build_settings['build_osx_archs']:
+        sysroot = osx_arch['sysroot']
+        current_arch = osx_arch['arch']
+        build_dir_current_arch = os.path.join(intermediates_dir, sysroot + "_" + current_arch)
+        build_command = base_build_command + [
+            '--ios_sysroot=' + sysroot,
+            '--osx_arch=' + current_arch,
+            '--build_dir=' + build_dir_current_arch
         ]
 
         if args.include_ops_by_config is not None:
-            _build_command += ['--include_ops_by_config=' + str(_include_ops_by_config_file)]
+            build_command += ['--include_ops_by_config=' + str(include_ops_by_config_file)]
 
         # the actual build process for current arch
-        subprocess.run(_build_command, shell=False, check=True, cwd=REPO_DIR)
+        subprocess.run(build_command, shell=False, check=True, cwd=REPO_DIR)
 
         # get the compiled lib path
-        _framework_dir = os.path.join(
-            _build_dir_current_arch, _build_config, _build_config + "-" + _sysroot, 'onnxruntime.framework')
-        _ort_libs.append(os.path.join(_framework_dir, 'onnxruntime'))
+        framework_dir = os.path.join(
+            build_dir_current_arch, build_config, build_config + "-" + sysroot, 'onnxruntime.framework')
+        ort_libs.append(os.path.join(framework_dir, 'onnxruntime'))
 
         # We actually only need to define the info.plist and headers once since they are all the same
-        if not _info_plist:
-            _info_plist = os.path.join(_build_dir_current_arch, _build_config, 'info.plist')
-            _headers = glob.glob(os.path.join(_framework_dir, 'Headers', '*.h'))
+        if not info_plist_path:
+            info_plist_path = os.path.join(build_dir_current_arch, build_config, 'info.plist')
+            headers = glob.glob(os.path.join(framework_dir, 'Headers', '*.h'))
 
     # manually create the fat framework
-    _framework_dir = os.path.join(build_dir, 'framework_out', 'onnxruntime.framework')
-    pathlib.Path(_framework_dir).mkdir(parents=True, exist_ok=True)
+    framework_dir = os.path.join(build_dir, 'framework_out', 'onnxruntime.framework')
+    pathlib.Path(framework_dir).mkdir(parents=True, exist_ok=True)
 
     # copy the header files and info.plist
-    shutil.copy(_info_plist, _framework_dir)
-    _header_dir = os.path.join(_framework_dir, 'Headers')
-    pathlib.Path(_header_dir).mkdir(parents=True, exist_ok=True)
-    for _header in _headers:
-        shutil.copy(_header, _header_dir)
+    shutil.copy(info_plist_path, framework_dir)
+    header_dir = os.path.join(framework_dir, 'Headers')
+    pathlib.Path(header_dir).mkdir(parents=True, exist_ok=True)
+    for _header in headers:
+        shutil.copy(_header, header_dir)
 
     # use lipo to create a fat ort library
-    _lipo_command = ['lipo', '-create']
-    _lipo_command += _ort_libs
-    _lipo_command += ['-output', os.path.join(_framework_dir, 'onnxruntime')]
-    subprocess.run(_lipo_command, shell=False, check=True)
+    lipo_command = ['lipo', '-create']
+    lipo_command += ort_libs
+    lipo_command += ['-output', os.path.join(framework_dir, 'onnxruntime')]
+    subprocess.run(lipo_command, shell=False, check=True)
 
 
 def parse_args():
