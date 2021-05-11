b455e6b30cabdcef2f191538638551464b26cc18
liqun <liqun@OrtTrainingDev4.af05slrtruoetgaxwwjv5nsq5e.px.internal.cloudapp.net>
2021-05-11 20:38:44 +0000

    .

--- onnxruntime/__init__.py
+++ onnxruntime/__init__.py
@@ -10,13 +10,29 @@ or the `Github project <https://github.com/microsoft/onnxruntime/>`_.
 __version__ = "1.7.0"
 __author__ = "Microsoft"
 
+# we need to do device version validation (for example to check Cuda version for an onnxruntime-training package).
+# in order to know whether the onnxruntime package is for training it needs
+# to do import onnxruntime.training.ortmodule first.
+# onnxruntime.capi._pybind_state is required before import onnxruntime.training.ortmodule.
+# however, import onnxruntime.capi._pybind_state will already raise an exception if a required Cuda version
+# is not found.
+# here we need to save the exception and continue with Cuda version validation in order to post
+# meaningful messages to the user.
+# the saved exception is raised after device version validation.
+try:
+    from onnxruntime.capi._pybind_state import get_all_providers, get_available_providers, get_device, set_seed, \
+        RunOptions, SessionOptions, set_default_logger_severity, enable_telemetry_events, disable_telemetry_events, \
+        NodeArg, ModelMetadata, GraphOptimizationLevel, ExecutionMode, ExecutionOrder, OrtDevice, SessionIOBinding, \
+        OrtAllocatorType, OrtMemType, OrtArenaCfg, OrtMemoryInfo, create_and_register_allocator
+    import_capi_exception = None
+except Exception as e:
+    import_capi_exception = e
+
 from onnxruntime.capi import onnxruntime_validation
 from onnxruntime.capi.onnxruntime_validation import has_ortmodule
 
-from onnxruntime.capi._pybind_state import get_all_providers, get_available_providers, get_device, set_seed, \
-    RunOptions, SessionOptions, set_default_logger_severity, enable_telemetry_events, disable_telemetry_events, \
-    NodeArg, ModelMetadata, GraphOptimizationLevel, ExecutionMode, ExecutionOrder, OrtDevice, SessionIOBinding, \
-    OrtAllocatorType, OrtMemType, OrtArenaCfg, OrtMemoryInfo, create_and_register_allocator
+if import_capi_exception:
+    raise import_capi_exception
 
 from onnxruntime.capi.onnxruntime_inference_collection import InferenceSession, IOBinding, OrtValue
 
