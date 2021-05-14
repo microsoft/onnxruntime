a27ef39d8f0eff2738d1b8f68c379ca7659c0857
Dwayne Robinson <dwayner@microsoft.com>
2021-05-14 04:16:12 +0000

    Merged PR 6047693: DML EP fix GetSupportedDeviceDataTypeMask for int64 (and some float64 issues)
    Fix one issue with int64/uint64 not being returned when getting the supported data types. Also add various fixes for float64 (do not actually *enable* any float64 kernels in this CR, just fix some things that enable it to be enabled later.)
    
    Related work items: #28761231

--- onnxruntime/core/providers/dml/DmlExecutionProvider/src/DmlCommon.cpp
+++ onnxruntime/core/providers/dml/DmlExecutionProvider/src/DmlCommon.cpp
@@ -20,7 +20,7 @@ DML_TENSOR_DATA_TYPE GetDmlDataTypeFromMlDataTypeNoThrow(MLOperatorTensorDataTyp
     case MLOperatorTensorDataType::String: return DML_TENSOR_DATA_TYPE_UNKNOWN;
     case MLOperatorTensorDataType::Bool: return DML_TENSOR_DATA_TYPE_UINT8;
     case MLOperatorTensorDataType::Float16: return DML_TENSOR_DATA_TYPE_FLOAT16;
-    case MLOperatorTensorDataType::Double: return DML_TENSOR_DATA_TYPE_UNKNOWN;
+    case MLOperatorTensorDataType::Double: return DML_TENSOR_DATA_TYPE_FLOAT64;
     case MLOperatorTensorDataType::UInt32: return DML_TENSOR_DATA_TYPE_UINT32;
     case MLOperatorTensorDataType::UInt64: return DML_TENSOR_DATA_TYPE_UINT64;
     case MLOperatorTensorDataType::Complex64: return DML_TENSOR_DATA_TYPE_UNKNOWN;
@@ -113,7 +113,7 @@ uint32_t GetSupportedDeviceDataTypeMask(IDMLDevice* dmlDevice)
     uint32_t deviceTypeMask = 0u;
 
     // Form the bitmask of all supported data types.
-    for (uint32_t i = 0; i <= DML_TENSOR_DATA_TYPE_INT8; ++i)
+    for (uint32_t i = 0; i <= DML_TENSOR_DATA_TYPE_INT64; ++i)
     {
         DML_FEATURE_QUERY_TENSOR_DATA_TYPE_SUPPORT dataTypeQuery = { static_cast<DML_TENSOR_DATA_TYPE>(i) };
         DML_FEATURE_DATA_TENSOR_DATA_TYPE_SUPPORT dataTypeSupport = {};
