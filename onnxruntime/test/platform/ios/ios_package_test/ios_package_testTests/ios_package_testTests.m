25e6e3644397950f09645e62f0509604c1400c74
Guoyu Wang <wanggy@outlook.com>
2021-05-21 21:21:06 -0700

    Working script

--- onnxruntime/test/platform/ios/ios_package_test/ios_package_testTests/ios_package_testTests.m
+++ onnxruntime/test/platform/ios/ios_package_test/ios_package_testTests/ios_package_testTests.m
@@ -6,31 +6,91 @@
 //
 
 #import <XCTest/XCTest.h>
+#include <math.h>
+#include <onnxruntime/onnxruntime_c_api.h>
 
-@interface ios_package_testTests : XCTestCase
+#define ASSERT_ON_ERROR(expr)                                                                          \
+  do {                                                                                                 \
+    OrtStatus* status = (expr);                                                                        \
+    XCTAssertEqual(NULL, status, @"Failed with error message: %@", @(g_ort->GetErrorMessage(status))); \
+  } while (0);
+
+@interface ios_package_testTests : XCTestCase {
+  const OrtApi* g_ort;
+}
 
 @end
 
 @implementation ios_package_testTests
 
 - (void)setUp {
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+  // Put setup code here. This method is called before the invocation of each test method in the class.
+  g_ort = OrtGetApiBase()->GetApi(ORT_API_VERSION);
 }
 
 - (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
+  // Put teardown code here. This method is called after the invocation of each test method in the class.
 }
 
-- (void)testExample {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
-}
+- (void)testCAPI {
+  // This is an example of a functional test case.
+  // Use XCTAssert and related functions to verify your tests produce the correct results.
 
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
+  OrtEnv* env = NULL;
+  ASSERT_ON_ERROR(g_ort->CreateEnv(ORT_LOGGING_LEVEL_WARNING, "test", &env));
+
+  // initialize session options if needed
+  OrtSessionOptions* so;
+  ASSERT_ON_ERROR(g_ort->CreateSessionOptions(&so));
+
+  OrtSession* session;
+  NSString* path = [[NSBundle mainBundle] pathForResource:@"sigmoid" ofType:@"ort"];
+  const char* cPath = [path cStringUsingEncoding:NSUTF8StringEncoding];
+  ASSERT_ON_ERROR(g_ort->CreateSession(env, cPath, so, &session));
+
+  OrtAllocator* allocator;
+  ASSERT_ON_ERROR(g_ort->GetAllocatorWithDefaultOptions(&allocator));
+
+  size_t input_tensor_size = 3 * 4 * 5;
+  float input_tensor_values[input_tensor_size];
+  float expected_values[input_tensor_size];
+  const char* input_node_names[] = {"x"};
+  const char* output_node_names[] = {"y"};
+  const int64_t input_node_dims[] = {3, 4, 5};
+
+  for (size_t i = 0; i < input_tensor_size; i++) {
+    input_tensor_values[i] = (float)i - 30;
+    expected_values[i] = 1.0f / (1 + exp(-input_tensor_values[i]));
+  }
+
+  OrtMemoryInfo* memory_info;
+  ASSERT_ON_ERROR(g_ort->CreateCpuMemoryInfo(OrtArenaAllocator, OrtMemTypeDefault, &memory_info));
+  OrtValue* input_tensor = NULL;
+  ASSERT_ON_ERROR(g_ort->CreateTensorWithDataAsOrtValue(memory_info, input_tensor_values, input_tensor_size * sizeof(float), input_node_dims, 3, ONNX_TENSOR_ELEMENT_DATA_TYPE_FLOAT, &input_tensor));
+  int is_tensor;
+  ASSERT_ON_ERROR(g_ort->IsTensor(input_tensor, &is_tensor));
+  XCTAssertNotEqual(is_tensor, 0);
+  g_ort->ReleaseMemoryInfo(memory_info);
+
+  OrtValue* output_tensor = NULL;
+  ASSERT_ON_ERROR(g_ort->Run(session, NULL, input_node_names, (const OrtValue* const*)&input_tensor, 1, output_node_names, 1, &output_tensor));
+  ASSERT_ON_ERROR(g_ort->IsTensor(output_tensor, &is_tensor));
+  XCTAssertNotEqual(is_tensor, 0);
+
+  // Get pointer to output tensor float values
+  float* output_values;
+  ASSERT_ON_ERROR(g_ort->GetTensorMutableData(output_tensor, (void**)&output_values));
+
+  for (size_t i = 0; i < input_tensor_size; i++) {
+    NSLog(@"%1.10f\t%1.10f", expected_values[i], output_values[i]);
+    XCTAssertEqualWithAccuracy(expected_values[i], output_values[i], 1e-6);
+  }
+
+  g_ort->ReleaseValue(output_tensor);
+  g_ort->ReleaseValue(input_tensor);
+  g_ort->ReleaseSession(session);
+  g_ort->ReleaseSessionOptions(so);
+  g_ort->ReleaseEnv(env);
 }
 
 @end
--- tools/ci_build/github/apple/test_ios_packages.py
+++ tools/ci_build/github/apple/test_ios_packages.py
@@ -27,10 +27,6 @@ def _test_ios_packages(args):
     if not base_dir.is_dir():
         raise FileNotFoundError('c_framework_dir {} is not a folder.'.format(base_dir))
 
-    podspec_path = os.path.join(base_dir, 'OnnxRuntimeBase.podspec')
-    if not pathlib.Path(podspec_path).exists():
-        raise FileNotFoundError('{} does not have OnnxRuntimeBase.podspec file'.format(base_dir))
-
     framework_path = os.path.join(base_dir, 'onnxruntime.framework')
     if not pathlib.Path(framework_path).exists():
         raise FileNotFoundError('{} does not have onnxruntime.framework'.format(base_dir))
@@ -40,26 +36,42 @@ def _test_ios_packages(args):
     with tempfile.TemporaryDirectory() as temp_dir:
         temp_dir = '/Users/gwang/temp/eee'
 
-        # create a zip file contains the framework and the podspec file
-        zip_content_dir = os.path.join(temp_dir, 'zip_content')
-        # zip_filename = os.path.join(temp_dir, 'OnnxRuntimeCAndCPP.zip')
-        zip_base_filename = '/Users/gwang/temp/ddd'
+        # create a zip file contains the framework
+        local_pods_dir = os.path.join(temp_dir, 'local_pods')
+        os.makedirs(local_pods_dir, exist_ok=True)
+        zip_base_filename = os.path.join(local_pods_dir, 'OnnxRuntimeBase')
         zip_filename = zip_base_filename + '.zip'
-        os.makedirs(zip_content_dir, exist_ok=True)
-        shutil.copy(podspec_path, zip_content_dir)
-        framework_target_dir = os.path.join(zip_content_dir, 'onnxruntime.framework')
-        if pathlib.Path(framework_target_dir).exists():
-            shutil.rmtree(framework_target_dir)
-        shutil.copytree(framework_path, framework_target_dir)
-        shutil.make_archive(zip_base_filename, 'zip', zip_content_dir)
+        shutil.make_archive(zip_base_filename, 'zip', root_dir=base_dir, base_dir='onnxruntime.framework')
 
         # copy the test project to the temp_dir
+        test_proj_path = os.path.join(REPO_DIR, 'onnxruntime', 'test', 'platform', 'ios', 'ios_package_test')
+        target_proj_path = os.path.join(temp_dir, 'ios_package_test')
+        if pathlib.Path(target_proj_path).exists():
+            shutil.rmtree(target_proj_path)
+        shutil.copytree(test_proj_path, target_proj_path)
 
-        # Update the Podfile of the project and run pod install
+        # update the podspec to point to the local framework zip file
+        local_podspec_path = os.path.join(target_proj_path, 'OnnxRuntimeBase.podspec')
+        with open(local_podspec_path, 'r') as file:
+            file_data = file.read()
 
-        # Run the test
+        # Replace the target string
+        file_data = file_data.replace('${ORT_BASE_FRAMEWORK_FILE}', zip_filename)
+        with open(os.path.join(REPO_DIR, 'VERSION_NUMBER')) as version_file:
+            file_data = file_data.replace('${ORT_VERSION}', version_file.readline().strip())
 
-    print(args)
+        # Write the file out again
+        with open(local_podspec_path, 'w') as file:
+            file.write(file_data)
+
+        # install pods first
+        subprocess.run(['pod', 'install'], shell=False, check=True, cwd=target_proj_path)
+        # run the test
+        subprocess.run(['xcodebuild', 'test',
+                        '-workspace', 'ios_package_test.xcworkspace',
+                        '-scheme', 'ios_package_test',
+                        '-destination', 'platform=iOS Simulator,OS=latest,name=iPhone SE (2nd generation)'],
+                       shell=False, check=True, cwd=target_proj_path)
 
 
 def parse_args():
