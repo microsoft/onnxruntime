4eb8511482c9729ed5de1c002c2790632aa2719a
Guoyu Wang <wanggy@outlook.com>
2021-05-22 02:35:11 -0700

    minor update

--- onnxruntime/test/platform/ios/ios_package_test/ios_package_testTests/ios_package_test_c_api.m
+++ onnxruntime/test/platform/ios/ios_package_test/ios_package_testTests/ios_package_test_c_api.m
@@ -9,14 +9,16 @@
 #include <math.h>
 #include <onnxruntime/onnxruntime_c_api.h>
 
-#define ASSERT_ON_ERROR(expr)                                                              \
-  do {                                                                                     \
-    OrtStatus* status = (expr);                                                            \
-    XCTAssertEqual(NULL, status,                                                           \
-                   @"Failed with error message: %@", @(ort_api->GetErrorMessage(status))); \
+#define ASSERT_ON_ERROR(expr)                                                            \
+  do {                                                                                   \
+    OrtStatus* status = (expr);                                                          \
+    XCTAssertEqual(NULL, status,                                                         \
+                   @"Failed with error message: %@", @(g_ort->GetErrorMessage(status))); \
   } while (0);
 
-@interface ios_package_test_c_api : XCTestCase
+@interface ios_package_test_c_api : XCTestCase {
+  const OrtApi* g_ort;
+}
 
 @end
 
@@ -24,6 +26,7 @@
 
 - (void)setUp {
   // Put setup code here. This method is called before the invocation of each test method in the class.
+  g_ort = OrtGetApiBase()->GetApi(ORT_API_VERSION);
 }
 
 - (void)tearDown {
@@ -32,18 +35,17 @@
 
 - (void)testCAPI {
   // This is an e2e test for ORT C API
-  const OrtApi* ort_api = OrtGetApiBase()->GetApi(ORT_API_VERSION);
   OrtEnv* env = NULL;
-  ASSERT_ON_ERROR(ort_api->CreateEnv(ORT_LOGGING_LEVEL_WARNING, "testCAPI", &env));
+  ASSERT_ON_ERROR(g_ort->CreateEnv(ORT_LOGGING_LEVEL_WARNING, "testCAPI", &env));
 
   // initialize session options if needed
   OrtSessionOptions* so;
-  ASSERT_ON_ERROR(ort_api->CreateSessionOptions(&so));
+  ASSERT_ON_ERROR(g_ort->CreateSessionOptions(&so));
 
   OrtSession* session;
   NSString* path = [[NSBundle mainBundle] pathForResource:@"sigmoid" ofType:@"ort"];
   const char* cPath = [path cStringUsingEncoding:NSUTF8StringEncoding];
-  ASSERT_ON_ERROR(ort_api->CreateSession(env, cPath, so, &session));
+  ASSERT_ON_ERROR(g_ort->CreateSession(env, cPath, so, &session));
 
   size_t input_tensor_size = 3 * 4 * 5;
   float input_tensor_values[input_tensor_size];
@@ -58,37 +60,37 @@
   }
 
   OrtMemoryInfo* memory_info;
-  ASSERT_ON_ERROR(ort_api->CreateCpuMemoryInfo(OrtArenaAllocator, OrtMemTypeDefault, &memory_info));
+  ASSERT_ON_ERROR(g_ort->CreateCpuMemoryInfo(OrtArenaAllocator, OrtMemTypeDefault, &memory_info));
   OrtValue* input_tensor = NULL;
-  ASSERT_ON_ERROR(ort_api->CreateTensorWithDataAsOrtValue(
+  ASSERT_ON_ERROR(g_ort->CreateTensorWithDataAsOrtValue(
       memory_info, input_tensor_values, input_tensor_size * sizeof(float),
       input_node_dims, 3, ONNX_TENSOR_ELEMENT_DATA_TYPE_FLOAT, &input_tensor));
   int is_tensor;
-  ASSERT_ON_ERROR(ort_api->IsTensor(input_tensor, &is_tensor));
+  ASSERT_ON_ERROR(g_ort->IsTensor(input_tensor, &is_tensor));
   XCTAssertNotEqual(is_tensor, 0);
-  ort_api->ReleaseMemoryInfo(memory_info);
+  g_ort->ReleaseMemoryInfo(memory_info);
 
   OrtValue* output_tensor = NULL;
-  ASSERT_ON_ERROR(ort_api->Run(session, NULL, input_node_names,
-                               (const OrtValue* const*)&input_tensor, 1,
-                               output_node_names, 1, &output_tensor));
-  ASSERT_ON_ERROR(ort_api->IsTensor(output_tensor, &is_tensor));
+  ASSERT_ON_ERROR(g_ort->Run(session, NULL, input_node_names,
+                             (const OrtValue* const*)&input_tensor, 1,
+                             output_node_names, 1, &output_tensor));
+  ASSERT_ON_ERROR(g_ort->IsTensor(output_tensor, &is_tensor));
   XCTAssertNotEqual(is_tensor, 0);
 
   // Get pointer to output tensor float values
   float* output_values;
-  ASSERT_ON_ERROR(ort_api->GetTensorMutableData(output_tensor, (void**)&output_values));
+  ASSERT_ON_ERROR(g_ort->GetTensorMutableData(output_tensor, (void**)&output_values));
 
   for (size_t i = 0; i < input_tensor_size; i++) {
     NSLog(@"%1.10f\t%1.10f", expected_output_values[i], output_values[i]);
     XCTAssertEqualWithAccuracy(expected_output_values[i], output_values[i], 1e-6);
   }
 
-  ort_api->ReleaseValue(output_tensor);
-  ort_api->ReleaseValue(input_tensor);
-  ort_api->ReleaseSession(session);
-  ort_api->ReleaseSessionOptions(so);
-  ort_api->ReleaseEnv(env);
+  g_ort->ReleaseValue(output_tensor);
+  g_ort->ReleaseValue(input_tensor);
+  g_ort->ReleaseSession(session);
+  g_ort->ReleaseSessionOptions(so);
+  g_ort->ReleaseEnv(env);
 }
 
 @end
