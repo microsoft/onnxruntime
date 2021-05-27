fe4fbe8f7e3759e07502cd61f4eb48113f4b7dd9
edgchen1 <18449977+edgchen1@users.noreply.github.com>
2021-05-27 11:30:00 -0700

    Update gen_models.sh to be usable from other directories.

--- objectivec/test/testdata/gen_models.sh
+++ objectivec/test/testdata/gen_models.sh
@@ -2,6 +2,11 @@
 
 set -e
 
-python ./single_add_gen.py
-python -m onnxruntime.tools.convert_onnx_models_to_ort --optimization_level basic .
+# Get directory this script is in
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+cd ${DIR}
+
+python3 ./single_add_gen.py
+python3 -m onnxruntime.tools.convert_onnx_models_to_ort --optimization_level basic .
 
