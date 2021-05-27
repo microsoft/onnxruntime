9ac139b08144741ae19a50a57abfd73cec2a53f0
Edward Chen <18449977+edgchen1@users.noreply.github.com>
2021-05-27 10:03:02 -0700

    Add Apple log sink. (#7820)
    Add a log sink for Apple platforms. This version uses NSLog().

--- cmake/onnxruntime_objectivec.cmake
+++ cmake/onnxruntime_objectivec.cmake
@@ -5,18 +5,22 @@ if(${CMAKE_VERSION} VERSION_LESS "3.18")
     message(FATAL_ERROR "CMake 3.18+ is required when building the Objective-C API.")
 endif()
 
+if(NOT APPLE)
+    message(FATAL_ERROR "Objective-C API must be built on an Apple platform.")
+endif()
+
 check_language(OBJC)
 if(CMAKE_OBJC_COMPILER)
-    enable_language(OBJC)
+  enable_language(OBJC)
 else()
-    message(FATAL_ERROR "Objective-C is not supported.")
+  message(FATAL_ERROR "Objective-C is not supported.")
 endif()
 
 check_language(OBJCXX)
 if(CMAKE_OBJCXX_COMPILER)
-    enable_language(OBJCXX)
+  enable_language(OBJCXX)
 else()
-    message(FATAL_ERROR "Objective-C++ is not supported.")
+  message(FATAL_ERROR "Objective-C++ is not supported.")
 endif()
 
 add_compile_options(
@@ -28,8 +32,6 @@ if(onnxruntime_DEV_MODE)
 endif()
 
 set(OBJC_ROOT "${REPO_ROOT}/objectivec")
-
-set(OBJC_ARC_COMPILE_OPTIONS "-fobjc-arc" "-fobjc-arc-exceptions")
 
 # onnxruntime_objc target
 
@@ -57,7 +59,7 @@ source_group(TREE "${OBJC_ROOT}" FILES
     ${onnxruntime_objc_srcs}
     ${onnxruntime_objc_common_srcs})
 
-add_library(onnxruntime_objc SHARED
+onnxruntime_add_shared_library(onnxruntime_objc
     ${onnxruntime_objc_headers}
     ${onnxruntime_objc_srcs}
     ${onnxruntime_objc_common_srcs})
@@ -77,8 +79,6 @@ target_link_libraries(onnxruntime_objc
         safeint_interface
         ${FOUNDATION_LIB})
 
-target_compile_options(onnxruntime_objc PRIVATE ${OBJC_ARC_COMPILE_OPTIONS})
-
 set_target_properties(onnxruntime_objc PROPERTIES
     FRAMEWORK TRUE
     VERSION "1.0.0"
@@ -88,6 +88,8 @@ set_target_properties(onnxruntime_objc PROPERTIES
     FOLDER "ONNXRuntime"
     CXX_STANDARD 17 # TODO remove when everything else moves to 17
     )
+
+set_property(TARGET onnxruntime_objc APPEND PROPERTY COMPILE_OPTIONS "-fvisibility=default")
 
 target_link_options(onnxruntime_objc PRIVATE "-Wl,-headerpad_max_install_names")
 
@@ -122,11 +124,11 @@ if(onnxruntime_BUILD_UNIT_TESTS)
         ${onnxruntime_objc_test_srcs}
         ${onnxruntime_objc_common_srcs})
 
+    onnxruntime_configure_target(onnxruntime_objc_test)
+
     target_include_directories(onnxruntime_objc_test
         PRIVATE
             "${OBJC_ROOT}")
-
-    target_compile_options(onnxruntime_objc_test PRIVATE ${OBJC_ARC_COMPILE_OPTIONS})
 
     set_target_properties(onnxruntime_objc_test PROPERTIES
         FOLDER "ONNXRuntimeTest")
