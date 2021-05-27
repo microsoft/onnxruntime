4ad3dd0982225eab93eccc26dd887b64ab1a4162
Chen Fu <fuchen@microsoft.com>
2021-05-27 08:34:24 -0700

    fix linux compilation error

--- onnxruntime/core/mlas/lib/qlgavgpool.cpp
+++ onnxruntime/core/mlas/lib/qlgavgpool.cpp
@@ -121,7 +121,8 @@ MlasQLinearGlobalAveragePoolNchw(
         int32x2_t vacc = vadd_s32(vget_high_s32(vacc_lo), vget_low_s32(vacc_lo));
         *sum_buffer++ = vget_lane_s32(vpadd_s32(vacc, vacc), 0);
     }
-    MlasRequantizeOutput(AccumulateBuffer, Channels, Output, Channels, nullptr, &Scale, false,
+
+    MlasRequantizeOutput(AccumulateBuffer, Channels, Output, Channels, nullptr, &scale, false,
                          static_cast<uint8_t>(ZeroPointOutput), 0, 0, 1, Channels);
 }
 
