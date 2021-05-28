3f43a84e10776fada10c94ea68ad2334b09ddc58
Sumit Agarwal <sumitagarwal@microsoft.com>
2021-05-28 17:44:37 +0000

    Merged PR 6093117: Fix test_DynamicQuantizedLinear_max_adjusted_expanded by allowing Identity operator to run on non-float inputs
    Motivation:
    As part of the OnnxConformance Backend tests, DynamicQuantizedLinear_max_adjusted_expanded is failing.
    
    Root Cause:
    - The test model has `Identity` operator as one of the node. The input of this node is of non-float data type.
    - In DML, `Identity` operator is registered as operator which requires floating input.
    - As per `DirectMLSchema.h`, support for non-float input has been added for `Identity` operator in DML but the same has not been reflected in the `OperatorRegistration.cpp`.
    
    Changes:
    - Removed all traces of the requiresFloatFormatsForGraph flag from it's definition and usage. This flag was only used for Identity and it's related operator.
    - Added null check for the graphOutput nodeArg in GraphDescBuilder.cpp to stop the crash of the test.
    
    Related work items: #33076298

--- onnxruntime/core/providers/dml/DmlExecutionProvider/src/GraphPartitioner.cpp
+++ onnxruntime/core/providers/dml/DmlExecutionProvider/src/GraphPartitioner.cpp
@@ -139,7 +139,7 @@ namespace Dml
         }
     };
 
-    bool NodeArgSupportedInGraph(const onnxruntime::NodeArg* arg, bool requiresFloatFormats, uint32_t supportedDeviceDataTypeMask)
+    bool NodeArgSupportedInGraph(const onnxruntime::NodeArg* arg, uint32_t supportedDeviceDataTypeMask)
     {            
         if (arg->Exists())
         {
@@ -164,14 +164,6 @@ namespace Dml
                         }
                     }
 
-                    if (requiresFloatFormats)
-                    {
-                        if (mlDataType != MLOperatorTensorDataType::Float &&
-                            mlDataType != MLOperatorTensorDataType::Float16)
-                        {
-                            return false;
-                        }
-                    }
                 }
             }
         }
@@ -189,7 +181,6 @@ namespace Dml
             if (!isConstantCpuInput &&
                 !NodeArgSupportedInGraph(
                     node.InputDefs()[i],
-                    registration.graphNodeFactoryRegistration->requiresFloatFormatsExceptConstInputs,
                     supportedDeviceDataTypeMask
                 ))
             {
@@ -201,7 +192,6 @@ namespace Dml
         {
             if (!NodeArgSupportedInGraph(
                     arg,
-                    registration.graphNodeFactoryRegistration->requiresFloatFormatsExceptConstInputs,
                     supportedDeviceDataTypeMask
                 ))
             {
