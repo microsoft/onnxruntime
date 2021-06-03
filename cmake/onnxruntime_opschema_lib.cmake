e99f55f281c2f4ca795b12af2e3003e4ed345e7b
Ganesan Ramalingam <grama@microsoft.com>
2021-06-03 09:16:55 -0700

    Add cmake dependency

--- cmake/onnxruntime_opschema_lib.cmake
+++ cmake/onnxruntime_opschema_lib.cmake
@@ -13,13 +13,15 @@ file(GLOB_RECURSE contrib_ops_schema_src
 
 add_library(ort_opschema_lib ${contrib_ops_schema_src})
 
+set (OPSCHEMA_LIB_DEPENDENCIES onnxruntime_mlas onnxruntime_common onnxruntime_util onnx onnx_proto protobuf::libprotobuf flatbuffers)
+
 # ${CMAKE_CURRENT_BINARY_DIR} is so that #include "onnxruntime_config.h" is found
 target_include_directories(ort_opschema_lib PRIVATE ${ONNXRUNTIME_ROOT} ${ORTTRAINING_ROOT} ${CMAKE_CURRENT_BINARY_DIR})
-add_dependencies(ort_opschema_lib onnx_proto flatbuffers)
+add_dependencies(ort_opschema_lib ${OPSCHEMA_LIB_DEPENDENCIES})
 onnxruntime_add_include_to_target(ort_opschema_lib onnxruntime_common onnx onnx_proto protobuf::libprotobuf flatbuffers)
 
 # Test schema library using toy application
-set (OPSCHEMA_LIB_DEPENDENCIES onnxruntime_mlas onnxruntime_common onnxruntime_util onnx onnx_proto protobuf::libprotobuf flatbuffers)
+
 set(OPSCHEMA_LIB_TEST ${REPO_ROOT}/samples/c_cxx/opschema_lib_use)
 
 file(GLOB_RECURSE opschema_lib_test_src "${OPSCHEMA_LIB_TEST}/*.cc")
