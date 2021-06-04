ceb6a2def0b3670cbef64ac353c3d205164b3969
Nick Kreeger <nickkreeger@microsoft.com>
2021-06-04 09:22:25 -0500

    save

--- onnxruntime/core/optimizer/quantized_embed_layer_norm_fusion.cc
+++ onnxruntime/core/optimizer/quantized_embed_layer_norm_fusion.cc
@@ -4,3 +4,10 @@
 #include "core/optimizer/initializer.h"
 #include "core/optimizer/quantized_embed_layer_norm_fusion.h"
 
+namespace onnxruntime {
+
+Status QuantizedEmbedLayerNormFusion::ApplyImpl(Graph& graph, bool& modified, int graph_level, const logging::Logger& logger) const {
+  return Status::OK();
+}
+
+}  // namespace onnxruntime
