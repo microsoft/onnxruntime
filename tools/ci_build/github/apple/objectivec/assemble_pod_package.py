e7b1f43ed07003b067b09d9d8447e106f283f3b3
edgchen1 <18449977+edgchen1@users.noreply.github.com>
2021-06-07 15:47:30 -0700

    Add comments.

--- tools/ci_build/github/apple/objectivec/assemble_pod_package.py
+++ tools/ci_build/github/apple/objectivec/assemble_pod_package.py
@@ -75,6 +75,17 @@ _template_variable_pattern = re.compile(r"@(\w+)@")  # match "@var@"
 
 def gen_file_from_template(template_file: pathlib.Path, output_file: pathlib.Path,
                            variable_substitutions: dict[str, str]):
+    '''
+    Generates a file from a template file.
+    The template file may contain template variables that will be substituted
+    with the provided values in the generated output file.
+    In the template file, template variable names are delimited by "@"'s,
+    e.g., "@var@".
+
+    :param template_file The template file path.
+    :param output_file The generated output file path.
+    :variable_substitutions The mapping from template variable name to value.
+    '''
     with open(template_file, mode="r") as template:
         content = template.read()
 
@@ -89,6 +100,14 @@ def gen_file_from_template(template_file: pathlib.Path, output_file: pathlib.Pat
 
 
 def copy_to_staging_dir(patterns: list[str], staging_dir: pathlib.Path):
+    '''
+    Copies files to a staging directory.
+    The files are relative to the repo root, and the repo root-relative
+    intermediate directory structure is maintained.
+
+    :param patterns The paths or path patterns relative to the repo root.
+    :param staging_dir The staging directory.
+    '''
     paths = [path for pattern in patterns for path in _repo_root.glob(pattern)]
     for path in paths:
         repo_relative_path = path.relative_to(_repo_root)
@@ -105,26 +124,30 @@ def main():
     if staging_dir.exists():
         print("Warning: staging directory already exists", file=sys.stderr)
 
+    # copy the necessary files to the staging directory
     copy_to_staging_dir(
         [license_file] + source_files + test_source_files + test_resource_files,
         staging_dir)
 
-    def as_variable_value(patterns: list[str]):
+    # generate the podspec file from the template
+
+    def path_patterns_as_variable_value(patterns: list[str]):
         return ", ".join([f'"{pattern}"' for pattern in patterns])
 
     variable_substitutions = {
         "VERSION": args.pod_version,
         "SOURCE_HTTP": args.pod_source_http,
-        "LICENSE_FILE": as_variable_value([license_file]),
-        "INCLUDE_DIR_LIST": as_variable_value(include_dirs),
-        "PUBLIC_HEADER_FILE_LIST": as_variable_value(public_header_files),
-        "SOURCE_FILE_LIST": as_variable_value(source_files),
-        "TEST_SOURCE_FILE_LIST": as_variable_value(test_source_files),
-        "TEST_RESOURCE_FILE_LIST": as_variable_value(test_resource_files),
+        "LICENSE_FILE": path_patterns_as_variable_value([license_file]),
+        "INCLUDE_DIR_LIST": path_patterns_as_variable_value(include_dirs),
+        "PUBLIC_HEADER_FILE_LIST": path_patterns_as_variable_value(public_header_files),
+        "SOURCE_FILE_LIST": path_patterns_as_variable_value(source_files),
+        "TEST_SOURCE_FILE_LIST": path_patterns_as_variable_value(test_source_files),
+        "TEST_RESOURCE_FILE_LIST": path_patterns_as_variable_value(test_resource_files),
     }
 
     podspec_template = _script_dir / "onnxruntime-mobile-objc.podspec.template"
     podspec = staging_dir / "onnxruntime-mobile-objc.podspec"
+
     gen_file_from_template(podspec_template, podspec, variable_substitutions)
 
     return 0
