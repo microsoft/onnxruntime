f888f3228202285ec2a0c9b48be6b7f016d16e83
pengwa <pengwa@microsoft.com>
2021-06-08 17:31:22 +0000

    fix builds

--- cmake/onnxruntime_interop_torch.cmake
+++ cmake/onnxruntime_interop_torch.cmake
@@ -3,27 +3,22 @@
 
 set (CXXOPTS ${PROJECT_SOURCE_DIR}/external/cxxopts/include)
 
-# python depedent files for torch interop
+# python dependent files for torch interop
 file(GLOB onnxruntime_interop_torch_srcs
     "${ORTTRAINING_SOURCE_DIR}/core/training_ops/cpu/torch/*.h"
     "${ORTTRAINING_SOURCE_DIR}/core/training_ops/cpu/torch/*.cc"
     "${ONNXRUNTIME_ROOT}/core/language_interop_ops/torch/*.h"
     "${ONNXRUNTIME_ROOT}/core/language_interop_ops/torch/*.cc"
-    "${ONNXRUNTIME_ROOT}/core/dlpack/dlpack_python.cc"
-    "${ONNXRUNTIME_ROOT}/core/dlpack/dlpack_python.h"
-    "${ONNXRUNTIME_ROOT}/core/dlpack/python_common.h"
 )
 
 onnxruntime_add_static_library(onnxruntime_interop_torch ${onnxruntime_interop_torch_srcs})
-add_dependencies(onnxruntime_interop_torch onnx  ${onnxruntime_EXTERNAL_DEPENDENCIES})
+add_dependencies(onnxruntime_interop_torch onnx ${onnxruntime_EXTERNAL_DEPENDENCIES})
 onnxruntime_add_include_to_target(onnxruntime_interop_torch onnxruntime_common onnx onnx_proto ${PROTOBUF_LIB} flatbuffers)
 
 onnxruntime_add_include_to_target(onnxruntime_interop_torch Python::Module)
 target_include_directories(onnxruntime_interop_torch PRIVATE ${CMAKE_CURRENT_BINARY_DIR} ${ONNXRUNTIME_ROOT} ${ORTTRAINING_ROOT} PUBLIC ${onnxruntime_graph_header})
 
-if (onnxruntime_USE_CUDA)
-  target_include_directories(onnxruntime_interop_torch PRIVATE ${onnxruntime_CUDNN_HOME}/include ${CMAKE_CUDA_TOOLKIT_INCLUDE_DIRECTORIES})
-endif()
+target_link_libraries(onnxruntime_interop_torch PRIVATE onnxruntime_python_interface)
 
 # DLPack is a header-only dependency
 set(DLPACK_INCLUDE_DIR ${PROJECT_SOURCE_DIR}/external/dlpack/include)
--- cmake/onnxruntime_python.cmake
+++ cmake/onnxruntime_python.cmake
@@ -49,10 +49,23 @@ endif()
 if (onnxruntime_USE_NCCL)
   target_include_directories(onnxruntime_pybind11_state PRIVATE ${NCCL_INCLUDE_DIRS})
 endif()
+
 if (onnxruntime_ENABLE_TRAINING)
   # DLPack is a header-only dependency
   set(DLPACK_INCLUDE_DIR ${PROJECT_SOURCE_DIR}/external/dlpack/include)
   target_include_directories(onnxruntime_pybind11_state PRIVATE ${ORTTRAINING_ROOT} ${DLPACK_INCLUDE_DIR})
+
+  file(GLOB onnxruntime_python_interface_cc_srcs
+    "${ONNXRUNTIME_ROOT}/core/dlpack/dlpack_python.cc"
+    "${ONNXRUNTIME_ROOT}/core/dlpack/dlpack_python.h"
+    "${ONNXRUNTIME_ROOT}/core/dlpack/python_common.h"
+  )
+
+  onnxruntime_add_static_library(onnxruntime_python_interface ${onnxruntime_python_interface_cc_srcs})
+  add_dependencies(onnxruntime_python_interface onnx  ${onnxruntime_EXTERNAL_DEPENDENCIES})
+  target_include_directories(onnxruntime_python_interface PRIVATE ${DLPACK_INCLUDE_DIR})
+  onnxruntime_add_include_to_target(onnxruntime_python_interface onnxruntime_common onnx onnx_proto ${PROTOBUF_LIB} flatbuffers Python::Module)
+  target_link_libraries(onnxruntime_pybind11_state PRIVATE onnxruntime_python_interface)
 endif()
 
 if(APPLE)
