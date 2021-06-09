f34335ad7a1eaab71404319a58403aea49bed826
Suffian Khan <sukha@microsoft.com>
2021-06-08 17:24:28 -0700

    fix script

--- tools/ci_build/github/pai/wrap_rocm_python_doc_publisher.sh
+++ tools/ci_build/github/pai/wrap_rocm_python_doc_publisher.sh
@@ -2,4 +2,4 @@ echo "HIP_VISIBLE_DEVICES=$HIP_VISIBLE_DEVICES"
 echo "PythonManylinuxDir=$PythonManylinuxDir"
 
 $PythonManylinuxDir/bin/python3 -m pip install /build/Release/dist/*.whl 
-/onnxruntime_src/tools/doc/builddoc.sh $PythonManylinuxDir/bin/ /onnxruntime_src /build Release " ; 
+/onnxruntime_src/tools/doc/builddoc.sh $PythonManylinuxDir/bin/ /onnxruntime_src /build Release  
