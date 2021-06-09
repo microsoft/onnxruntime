f6e295c098b72f197235362653d38eee67858854
Nick Kreeger <nickkreeger@microsoft.com>
2021-06-09 13:05:59 -0500

    save

--- onnxruntime/core/optimizer/quantized_embed_layer_norm_fusion.cc
+++ onnxruntime/core/optimizer/quantized_embed_layer_norm_fusion.cc
@@ -1,31 +1,125 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-#include "core/optimizer/initializer.h"
 #include "core/optimizer/quantized_embed_layer_norm_fusion.h"
+
+#include "core/graph/graph_utils.h"
+#include "core/optimizer/initializer.h"
 
 namespace onnxruntime {
 
-Status QuantizedEmbedLayerNormFusion::ApplyImpl(Graph& graph, bool& modified, int graph_level, const logging::Logger& logger) const {
+namespace {
+
+// NOTE: These are not in the proper order. I wonder if there is a way to loop
+// through all edges of a node and do it that way.
+constexpr int kEmbedLayerNormWordDequantizeInputIdx = 2;
+constexpr int kEmbedLayerNormPositionDequantizeInputIdx = 3;
+constexpr int kEmbedLayerNormSegmentDequantizeInputIdx = 4;  // Also known as "token"
+
+//void GetNodesFromEmbedLayerNormalizationNode(const Node& node) {
+//  //
+//  // TODO - write me.
+//  //
+//}
+
+}  // namespace
+
+Status QuantizedEmbedLayerNormFusion::ApplyImpl(Graph& graph,
+                                                bool& modified,
+                                                int graph_level,
+                                                const logging::Logger& logger) const {
   GraphViewer graph_viewer(graph);
 
   const auto& node_topology_list = graph_viewer.GetNodesInTopologicalOrder();
   for (auto node_index : node_topology_list) {
-    auto* p_layer_norm = graph.GetNode(node_index);
-    if (p_layer_norm == nullptr) {
-      // This node was removed as part of an earlier fusion?
+    auto* node = graph.GetNode(node_index);
+    if (node == nullptr) {
+      // TODO - validate that the node was removed as part of an earlier fusion...
       continue;
     }
 
-    //
-    // TODO(kreeger): LEFT OFF RIGHT HERE. NEED TO WRITE THIS FUNCTION!
-    //
+    ORT_RETURN_IF_ERROR(Recurse(*node, modified, graph_level, logger));
 
-    Node& layer_norm_node = *p_layer_norm;
-    ORT_RETURN_IF_ERROR(Recurse(layer_norm_node, modified, graph_level, logger));
+    // 1.) Find the "EmbedLayerNormalization" node:
+    if (node->OpType().compare("EmbedLayerNormalization") != 0 ||
+        !graph_utils::IsSupportedProvider(*node, GetCompatibleExecutionProviders())) {
+      continue;
+    }
+
+    // Try and find the correct input thing here for a node?
+    auto inputs = node->InputDefs();
+    for (size_t i = 0; i < inputs.size(); ++i) {
+      auto* input_arg = inputs.at(i);
+      printf("  - input_arg: %s\n", input_arg->Name().c_str());
+    }
+
+    // Find and assing the 
+    //Node* p_word_dequantize_node = nullptr;
+    //Node* p_position_dequantize_node = nullptr;
+    //Node* p_segment_dequantize_node = nullptr;
+
+    auto node_inputs_begin = node->InputNodesBegin();
+    auto node_inputs_end = node->InputNodesEnd();
+    for (auto it = node_inputs_begin; it != node_inputs_end; ++it) {
+      if (it->OpType().compare("DequantizeLinear") == 0) {
+        if (it->Index() == 2) {
+        }
+      }
+    }
+
+    std::vector<graph_utils::EdgeEndToMatch> dequantize_inputs_edges{
+        {0, 2, "DequantizeLinear", {10, 13}, ""},
+        {0, 3, "DequantizeLinear", {10, 13}, ""},
+        {0, 4, "DequantizeLinear", {10, 13}, ""},
+    };
+
+    std::vector<const Node::EdgeEnd*> edges;
+    if (!graph_utils::FindPath(*node,
+                               /*is_input_edge=*/true,
+                               dequantize_inputs_edges,
+                               edges,
+                               logger)) {
+      continue;
+    }
+
+    // Node edges are inputs to the operator.
+    if (edges.size() > 0) {
+    }
+
+    // 2.) Find the "DequantizeLinear" Node for the position embedding:
+    // 3.) Find the "DequantizeLinear" Node for the segment embedding:
+    // 4.) Find the "DequantizeLinear" Node for the word embedding:
   }
 
   return Status::OK();
+}
+
+Node* QuantizedEmbedLayerNormFusion::FindNodeFromPath(const Node& node,
+                                                      Graph& graph,
+                                                      const char* op_type,
+                                                      const logging::Logger& logger) const {
+
+  //
+  // TODO(kreeger): Left off right here. Need to determine why this method is not finding
+  // any of the DequantizeLinear operators in the graph. Might have to look at other APIs 
+  // to examine the usage of graph_utils::FindPath()...
+  //
+
+  std::vector<const Node::EdgeEnd*> edges;
+  if (!graph_utils::FindPath(node,
+                             /*is_input_edge=*/true,
+                             {{/*src_arg_index=*/0,
+                               /*dst_arg_index=*/0,
+                               op_type,
+                               /*version=*/{10},
+                               kMSDomain}},
+                             edges,
+                             logger)) {
+    return nullptr;
+  }
+
+  return graph.GetNode(edges[0]->GetNode().Index());
+
 }
 
 }  // namespace onnxruntime
