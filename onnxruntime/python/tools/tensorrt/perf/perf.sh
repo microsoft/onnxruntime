3f0884170f1063952cc948c19224e5fe16a7682a
Olivia Jain <oljain@microsoft.com>
2021-06-14 16:02:34 -0700

    remove symbolic from perf.sh

--- onnxruntime/python/tools/tensorrt/perf/perf.sh
+++ onnxruntime/python/tools/tensorrt/perf/perf.sh
@@ -32,8 +32,6 @@ METRICS_FILE=".metrics_map"
 PROFILE="*onnxruntime_profile*"
 
 # files to download info
-SYMBOLIC_SHAPE_INFER="symbolic_shape_infer.py"
-SYMBOLIC_SHAPE_INFER_LINK="https://raw.githubusercontent.com/microsoft/onnxruntime/master/onnxruntime/python/tools/symbolic_shape_infer.py"
 FLOAT_16="float16.py"
 FLOAT_16_LINK="https://raw.githubusercontent.com/microsoft/onnxconverter-common/master/onnxconverter_common/float16.py"
 
@@ -41,14 +39,12 @@ cleanup_files() {
     rm -f $FAIL_MODEL_FILE
     rm -f $LATENCY_FILE
     rm -f $METRICS_FILE
-    rm -f $SYMBOLIC_SHAPE_INFER
     rm -f $FLOAT_16
     rm -rf result/$OPTION
     find -name $PROFILE -delete
 }
 
 download_files() {
-    wget --no-check-certificate -c $SYMBOLIC_SHAPE_INFER_LINK
     wget --no-check-certificate -c $FLOAT_16_LINK 
 }
 
