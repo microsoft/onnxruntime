0fa56f7a34be4b3bb7e7d3adbbc37bead0520b6a
Olivia Jain <oljain@microsoft.com>
2021-06-15 12:29:45 -0700

    simplify perf docker

--- onnxruntime/python/tools/tensorrt/perf/run_perf_docker.sh
+++ onnxruntime/python/tools/tensorrt/perf/run_perf_docker.sh
@@ -16,18 +16,6 @@ done
 DOCKER_PERF_DIR='/perf/'
 HOME_PERF_DIR='/home/hcsuser/perf/'
 WORKSPACE='/'
-
-# Volumes
-VOLUME=$PERF_DIR:$DOCKER_PERF_DIR
-
-if [ $OPTION == "many-models" ]
-then 
-    MANY_MODELS_VOLUME=' -v /home/hcsuser/mount/many-models:/mount/many-models'
-    VOLUME=$VOLUME$MANY_MODELS_VOLUME
-else 
-    ADD_VOLUME=' -v '$HOME_PERF_DIR$OPTION':'$DOCKER_PERF_DIR$OPTION
-    VOLUME=$VOLUME$ADD_VOLUME
-fi 
-
 MODEL_PATH=$WORKSPACE$MODEL_PATH
-sudo docker run --gpus all -v $VOLUME $DOCKER_IMAGE /bin/bash $DOCKER_PERF_DIR'perf.sh' -d $DOCKER_PERF_DIR -o $OPTION -m $MODEL_PATH -w $WORKSPACE -e "$EP_LIST"
+
+sudo docker run --gpus all -v $PERF_DIR:$OPTION -v $HOME_PERF_DIR$OPTION:$DOCKER_PERF_DIR$OPTION $DOCKER_IMAGE /bin/bash $DOCKER_PERF_DIR'perf.sh' -d $DOCKER_PERF_DIR -o $OPTION -m $MODEL_PATH -w $WORKSPACE -e "$EP_LIST"
