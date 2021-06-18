e61d41c0d6bc96f391e901efe2559492c1f55c89
Olivia Jain <oljain@microsoft.com>
2021-06-18 11:11:19 -0700

    Make Perf Test Configurable (#7836)
    - Allow anyone to kick off a perf test here. Customize: branch, eps, model selection, cuda version.
    - Only run shape inference when required.
    - Kill errored out memory processes.
    - Remove warmup run.
    - Clean up script.
    - Standalone_TRT is it's own "EP" vs as an additional run with TRT EP

--- onnxruntime/python/tools/tensorrt/perf/perf.sh
+++ onnxruntime/python/tools/tensorrt/perf/perf.sh
@@ -1,13 +1,29 @@
 #!/bin/bash
 
-while getopts d:o:m: parameter
+while getopts d:o:m:w:e: parameter
 do case "${parameter}"
-in 
+in
 d) PERF_DIR=${OPTARG};;
 o) OPTION=${OPTARG};;
 m) MODEL_PATH=${OPTARG};;
+w) WORKSPACE=${OPTARG};;
+e) EP_LIST=${OPTARG};;
 esac
 done 
+
+# add ep list
+RUN_EPS=""
+if [ ! -z "$EP_LIST" ]
+then 
+    RUN_EPS="--ep_list $EP_LIST"
+fi
+
+# change dir if docker
+if [ ! -z $PERF_DIR ]
+then 
+    echo 'changing to '$PERF_DIR
+    cd $PERF_DIR 
+fi 
 
 # metadata
 FAIL_MODEL_FILE=".fail_model_map"
@@ -16,8 +32,6 @@ METRICS_FILE=".metrics_map"
 PROFILE="*onnxruntime_profile*"
 
 # files to download info
-SYMBOLIC_SHAPE_INFER="symbolic_shape_infer.py"
-SYMBOLIC_SHAPE_INFER_LINK="https://raw.githubusercontent.com/microsoft/onnxruntime/master/onnxruntime/python/tools/symbolic_shape_infer.py"
 FLOAT_16="float16.py"
 FLOAT_16_LINK="https://raw.githubusercontent.com/microsoft/onnxconverter-common/master/onnxconverter_common/float16.py"
 
@@ -25,23 +39,20 @@ cleanup_files() {
     rm -f $FAIL_MODEL_FILE
     rm -f $LATENCY_FILE
     rm -f $METRICS_FILE
-    rm -f $SYMBOLIC_SHAPE_INFER
     rm -f $FLOAT_16
     rm -rf result/$OPTION
     find -name $PROFILE -delete
 }
 
 download_files() {
-    wget --no-check-certificate -c $SYMBOLIC_SHAPE_INFER_LINK
     wget --no-check-certificate -c $FLOAT_16_LINK 
 }
 
 setup() {
-    cd $PERF_DIR
     cleanup_files
     download_files
 }
 
 setup
-python3 benchmark_wrapper.py -r validate -m $MODEL_PATH -o result/$OPTION
-python3 benchmark_wrapper.py -r benchmark -i random -t 10 -m $MODEL_PATH -o result/$OPTION
+python3 benchmark_wrapper.py -r validate -m $MODEL_PATH -o result/$OPTION -w $WORKSPACE $RUN_EPS
+python3 benchmark_wrapper.py -r benchmark -t 10 -m $MODEL_PATH -o result/$OPTION -w $WORKSPACE $RUN_EPS
