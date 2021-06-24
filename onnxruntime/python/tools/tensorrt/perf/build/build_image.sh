47974e49663bdf4155f2e1594a0f84677ba15a17
Olivia Jain <oljain@microsoft.com>
2021-06-24 12:24:38 -0700

    allow for configurable trt

--- onnxruntime/python/tools/tensorrt/perf/build/build_image.sh
+++ onnxruntime/python/tools/tensorrt/perf/build/build_image.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-while getopts p:b:i:t: parameter
+while getopts p:b:i:t:c: parameter
 do case "${parameter}"
 in 
 p) PERF_DOCKERFILE_PATH=${OPTARG};;
 b) ORT_BRANCH=${OPTARG};;
 i) IMAGE_NAME=${OPTARG};;
-t) TRT_CONTAINER=${OPTARG};;
+t) TRT=${OPTARG};;
+c) CUDA_VERSION=${OPTARG};;
 esac
 done 
 
-sudo docker build --no-cache -t $IMAGE_NAME --build-arg TRT_CONTAINER=$TRT_CONTAINER --build-arg ONNXRUNTIME_BRANCH=$ORT_BRANCH -f $PERF_DOCKERFILE_PATH .
+sudo docker build --no-cache -t $IMAGE_NAME --build-arg TRT=$TRT --build-arg CUDA_VERSION=$CUDA_VERSION --build-arg ONNXRUNTIME_BRANCH=$ORT_BRANCH -f $PERF_DOCKERFILE_PATH .
