98305295e2e8aa51b01454102e06c36f1b125143
Changming Sun <chasun@microsoft.com>
2021-06-25 11:07:28 -0700

    updtae

--- onnxruntime/core/providers/cuda/cuda_call.cc
+++ onnxruntime/core/providers/cuda/cuda_call.cc
@@ -93,6 +93,7 @@ bool CudaCall(ERRTYPE retCode, const char* exprString, const char* libName, ERRT
       std::unique_ptr<char, decltype(del)> hostname_ptr(nullptr, del);
       size_t hostname_len = 0;
       char* hostname = nullptr;
+      //TODO: avoid using const_cast
       if (-1 == _dupenv_s(&hostname, &hostname_len, "COMPUTERNAME"))
         hostname = const_cast<char*>("?");
       else
