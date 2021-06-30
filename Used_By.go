df843951c4942d9fc00ea669562a273302d1f630
Michael Glenn <mbg198618@gmail.com>
2021-06-29 22:51:28 -0500

    Create Used_By.go

---
+++ Used_By.go
@@ -0,0 +1,39 @@
+# Used By
+NuGet packages (9)
+Showing the top 5 NuGet packages that depend on Microsoft.ML.OnnxRuntime:
+
+Package	Downloads
+FaceONNX
+Face analytics library based on deep neural networks and ONNX runtime.
+ 3.6K
+Aspose.Ocr.Cpp
+Aspose.OCR for C and C++ is a robust optical character recognition API. Developers can easily add OCR functionalities in their applications. API is extensible, easy to use, compact and provides a simple set of classes for controlling character recognition. It supports commonly used image formats and provides noise removal filters, determine text fields and automatic alignment of the document. The library requires onnxruntime.dll installed in the system.
+ 3.0K
+MxNet.Sharp
+C# Binding for the Apache MxNet library. NDArray, Symbolic and Gluon Supported MxNet is a deep learning framework designed for both efficiency and flexibility. It allows you to mix symbolic and imperative programming to maximize efficiency and productivity. At its core, MXNet contains a dynamic dependency scheduler that automatically parallelizes both symbolic and imperative operations on the fly. A graph optimization layer on top of that makes symbolic execution fast and memory efficient. MXNet is portable and lightweight, scaling effectively to multiple GPUs and multiple machines. MXNet is more than a deep learning project. It is a collection of blue prints and guidelines for building deep learning systems, and interesting insights of DL systems for hackers.
+ 2.7K
+SmartSelectors
+A set of machine learning models to predict if an image is a certain type of web element.
+ 2.3K
+RusstressNet
+Automatic accent (stress) prediction for Russian language
+ 1.0K
+GitHub repositories (5)
+Showing the top 5 popular GitHub repositories that depend on Microsoft.ML.OnnxRuntime:
+
+Repository	Stars
+dotnet/machinelearning
+ML.NET is an open source and cross-platform machine learning framework for .NET.
+ 7.6K
+microsoft/psi
+Platform for Situated Intelligence
+ 327
+Azure/azure-stream-analytics
+Azure Stream Analytics
+ 192
+microsoft/CryptoNets
+CryptoNets is a demonstration of the use of Neural-Networks over data encrypted with Homomorphic Encryption. Homomorphic Encryptions allow performing operations such as addition and multiplication over data while it is encrypted. Therefore, it allows keeping data private while outsourcing computation (see here and here for more about Homomorphic Encryptions and its applications). This project demonstrates the use of Homomorphic Encryption for outsourcing neural-network predictions. The scenario in mind is a provider that would like to provide Prediction as a Service (PaaS) but the data for which predictions are needed may be private. This may be the case in fields such as health or finance. By using CryptoNets, the user of the service can encrypt their data using Homomorphic Encryption and send only the encrypted message to the service provider. Since Homomorphic Encryptions allow the provider to operate on the data while it is encrypted, the provider can make predictions using a pre-trained Neural-Network while the data remains encrypted throughout the process and finaly send the prediction to the user who can decrypt the results. During the process the service provider does not learn anything about the data that was used, the prediction that was made or any intermediate result since everything is encrypted throughout the process. This project uses the Simple Encrypted Arithmetic Library SEAL version 3.2.1 implementation of Homomorphic Encryption developed in Microsoft Research.
+ 160
+deepakkumar1984/MxNet.Sharp
+.NET Standard bindings for Apache MxNet with Imperative, Symbolic and Gluon Interface for developing, training and deploying Machine Learning models in C#. https://mxnet.tech-quantum.com/
+ 137
