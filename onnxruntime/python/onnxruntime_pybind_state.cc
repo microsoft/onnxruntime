4e5e2dfab921a07afe10d09ea6f6028afe85e58f
Cheng Tang <chenta@microsoft.com>
2021-06-30 13:11:44 -0700

    fix the test execution provider for training build

--- onnxruntime/python/onnxruntime_pybind_state.cc
+++ onnxruntime/python/onnxruntime_pybind_state.cc
@@ -1680,15 +1680,14 @@ void CreatePybindStateModule(py::module& m) {
   addOrtValueMethods(m);
   addIoBindingMethods(m);
 
-#if !defined(ENABLE_TRAINING) && !defined(__APPLE__) && \
+#if !defined(__APPLE__) && \
     (!defined(ORT_MINIMAL_BUILD) || defined(ORT_EXTENDED_MINIMAL_BUILD) || defined(ORT_MINIMAL_BUILD_CUSTOM_OPS))
-  
-#endif
   Ort::SessionOptions tmp_options;
   if (!InitProvidersSharedLibrary()) {
     const logging::Logger& default_logger = logging::LoggingManager::DefaultLogger();
     LOGS(default_logger, WARNING) << "Init provider bridge failed.";
   }
+#endif
 
 #ifdef ENABLE_TRAINING
   addObjectMethodsForTraining(m);
