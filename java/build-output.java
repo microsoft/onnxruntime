0c75057c5cd971d2d2894c3955bf789d3d6bac2c
Michael Glenn <mbg198618@gmail.com>
2021-06-30 18:59:02 -0500

    Create build-output.java

---
+++ java/build-output.java
@@ -0,0 +1,7 @@
+Build Output
+The build will generate output in $REPO_ROOT/build/$OS/$CONFIGURATION/java/build:
+
+docs/javadoc/ - HTML javadoc
+reports/ - detailed test results and other reports
+libs/onnxruntime-VERSION.jar - JAR with compiled classes, platform-specific JNI shared library,
+and platform-specific onnxruntime shared library
