ce943fb8f7cfe7566fef856936adbc6b7572a927
Michael Glenn <mbg198618@gmail.com>
2021-06-30 01:03:24 -0500

    Create Load-Pretrained-Model-Weights.c

---
+++ Load-Pretrained-Model-Weights.c
@@ -0,0 +1,12 @@
+# Load pretrained model weights
+model_url = 'https://s3.amazonaws.com/pytorch/test_data/export/superres_epoch100-44c6958e.pth'
+batch_size = 1    # just a random number
+
+# Initialize model with the pretrained weights
+map_location = lambda storage, loc: storage
+if torch.cuda.is_available():
+    map_location = None
+torch_model.load_state_dict(model_zoo.load_url(model_url, map_location=map_location))
+
+# set the model to inference mode
+torch_model.eval()
