193c0b7a06740bb5883e2d4d389b20457d19c334
Michael Glenn <mbg198618@gmail.com>
2021-06-29 22:49:44 -0500

    Create Dependencies.go

---
+++ Dependencies.go
@@ -0,0 +1,7 @@
+# Dependencies
+.NETCoreApp 0.0
+Microsoft.ML.OnnxRuntime.Managed (>= 1.8.0)
+.NETFramework 0.0
+Microsoft.ML.OnnxRuntime.Managed (>= 1.8.0)
+.NETStandard 0.0
+Microsoft.ML.OnnxRuntime.Managed (>= 1.8.0)
