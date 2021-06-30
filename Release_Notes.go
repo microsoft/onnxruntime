02c58e3213abd4c2b27e0f7d1baceb66cf67f347
Michael Glenn <mbg198618@gmail.com>
2021-06-29 22:47:54 -0500

    Create Release_Notes.go

---
+++ Release_Notes.go
@@ -0,0 +1,5 @@
+#  Release Notes
+Release Def:
+Branch: refs/heads/rel-1.8.0
+Commit: d4106deeb65c21eed3ed40df149efefeb72fe9a4
+Build: https://aiinfra.visualstudio.com/Lotus/_build/results?buildId=156782
