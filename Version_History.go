6ba07cddd62f6b4a1821221300c8c1b2b857e6b3
Michael Glenn <mbg198618@gmail.com>
2021-06-29 22:53:22 -0500

    Create Version_History.go

---
+++ Version_History.go
@@ -0,0 +1,7 @@
+# Version History
+Version	Downloads	Last updated	
+1.8.0	4,373	a month ago	
+1.7.0	24,831	4 months ago	
+1.6.0	17,222	7 months ago	
+1.5.2	32,154	8 months ago	
+1.5.1	7,094	9 months ago 
