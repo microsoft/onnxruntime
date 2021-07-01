672f97a6c4de0608e84acb9624ca5d54bb1c0d12
Chi Lo <Chi.Lo@microsoft.com>
2021-07-01 01:15:51 -0700

    modify for test

---
+++ csharp/test/Microsoft.ML.OnnxRuntime.EndToEndTests/runtest_tensorrt.bat
@@ -0,0 +1,55 @@
+REM Copyright (c) Microsoft Corporation. All rights reserved.
+REM Licensed under the MIT License.
+
+@ECHO ON
+SETLOCAL EnableDelayedExpansion
+
+SET TargetFramework=netcoreapp2.1
+SET TargetArch=x64
+SET dn="C:\Program Files\dotnet\dotnet"
+
+SET LocalNuGetRepo=%1
+IF NOT "%2"=="" (SET TargetFramework=%2)
+IF NOT "%3"=="" (SET TargetArch=%3)
+IF NOT "%4"=="" (
+    SET CurrentOnnxRuntimeVersion=%4
+) ELSE (
+    echo "Usage: runtest.bat LocalNuGetRepoPath TargetFramework TargetArch NuGetPackageVersion"
+)
+
+IF "%TargetArch%"=="x64" (
+  SET RuntimeIdentifier=win-x64
+  SET PlatformTarget=x64
+)
+
+IF "%TargetArch%"=="x86" (
+  SET dn="C:\Program Files (x86)\dotnet\dotnet"
+  SET RuntimeIdentifier=win-x86
+  SET PlatformTarget=x86
+)
+
+ECHO Target Framework is %TargetFramework%
+
+REM Update if CUDA lib paths if set
+SET PATH=%CUDA_PATH%\bin;%CUDNN_PATH%\bin;%PATH%
+
+IF EXIST test\Microsoft.ML.OnnxRuntime.EndToEndTests\packages RMDIR /S /Q test\Microsoft.ML.OnnxRuntime.EndToEndTests\packages
+IF EXIST test\Microsoft.ML.OnnxRuntime.EndToEndTests\bin RMDIR /S /Q test\Microsoft.ML.OnnxRuntime.EndToEndTests\bin
+IF EXIST test\Microsoft.ML.OnnxRuntime.EndToEndTests\obj RMDIR /S /Q test\Microsoft.ML.OnnxRuntime.EndToEndTests\obj
+
+@echo %CurrentOnnxRuntimeVersion%
+%dn% clean test\Microsoft.ML.OnnxRuntime.EndToEndTests\Microsoft.ML.OnnxRuntime.EndToEndTests.csproj
+%dn% restore test\Microsoft.ML.OnnxRuntime.EndToEndTests\Microsoft.ML.OnnxRuntime.EndToEndTests.csproj --configfile .\Nuget.CSharp.config --no-cache --packages test\Microsoft.ML.OnnxRuntime.EndToEndTests\packages --source https://api.nuget.org/v3/index.json --source  %LocalNuGetRepo%
+
+IF NOT errorlevel 0 (
+    @echo "Failed to restore nuget packages for the test project"
+    EXIT 1
+)
+
+%dn% test test\Microsoft.ML.OnnxRuntime.EndToEndTests\Microsoft.ML.OnnxRuntime.EndToEndTests.csproj --no-restore -p:DefineConstants=USE_TENSORRT
+%dn% test test\Microsoft.ML.OnnxRuntime.EndToEndTests\Microsoft.ML.OnnxRuntime.EndToEndTests.csproj --no-restore -t -p:DefineConstants=USE_TENSORRT
+%dn% restore test\Microsoft.ML.OnnxRuntime.EndToEndTests\Microsoft.ML.OnnxRuntime.EndToEndTests.csproj -p:DefineConstants=USE_TENSORRT --configfile .\Nuget.CSharp.config --no-cache --packages test\Microsoft.ML.OnnxRuntime.EndToEndTests\packages --source https://api.nuget.org/v3/index.json --source  %LocalNuGetRepo%
+IF NOT errorlevel 0 (
+    @echo "Failed to build or execute the end-to-end test"
+    EXIT 1
+)
