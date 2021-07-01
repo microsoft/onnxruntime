89e64a2dccca915dced85cca28b807827d6b8ec6
Michael Glenn <mbg198618@gmail.com>
2021-06-30 19:02:52 -0500

    Create onnxruntime-node.js

---
+++ js/onnxruntime-node.js
@@ -0,0 +1,9 @@
+onnxruntime-node
+language: typescript/C++
+
+dependency: onnxruntime-common, ONNXRuntime.dll
+
+folder: <ORT_ROOT>/js/node
+
+This project is designed to be used as a NPM package to enable Node.js users 
+to consume ONNX Runtime via Node.js binding, in Node.js or any Node.js compatible environment.
