215617f77a3335b6139f1896c7951ce83bac8c9e
Suffian Khan <sukha@microsoft.com>
2021-07-05 14:02:11 -0700

    first attempt share docker image across python and torch versons

--- tools/ci_build/github/linux/docker/scripts/install_python_deps.sh
+++ tools/ci_build/github/linux/docker/scripts/install_python_deps.sh
@@ -59,9 +59,6 @@ if [ $DEVICE_TYPE = "gpu" ]; then
         # Due to a [bug on DeepSpeed](https://github.com/microsoft/DeepSpeed/issues/663), we install it separately through ortmodule/stage2/requirements.txt
         ${PYTHON_EXE} -m pip install -r ${0/%install_python_deps.sh/training\/ortmodule\/stage2\/requirements.txt}
       else
-        ${PYTHON_EXE} -m pip install \
-          --pre -f https://download.pytorch.org/whl/nightly/rocm4.2/torch_nightly.html \
-          torch torchvision torchtext
         ${PYTHON_EXE} -m pip install -r ${0/%install_python_deps.sh/training\/ortmodule\/stage1\/requirements-torch${TORCH_VERSION}_rocm.txt}
         ${PYTHON_EXE} -m pip install fairscale
 	      # remove triton requirement from getting triggered in requirements-sparse_attn.txt
