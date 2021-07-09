c4d471c13ba7ac9b5b5178830a66c7e34dd9b2ab
Nat Kershaw <nakersha@microsoft.com>
2021-07-09 15:09:53 -0700

    Add ROCm torch-ort installation instructions

--- js/script.js
+++ js/script.js
@@ -545,7 +545,7 @@ var ot_validCombos = {
     "Follow sample notebook from <a href='https://github.com/microsoft/onnxruntime-training-examples' target='_blank'>here</a>",
 
     "ot_linux,ot_PyTorch18,ot_X64,ot_AMD":
-    "This combination is in preview. Please <a href=https://github.com/Microsoft/onnxruntime/issues>reach out</a> for preview packages",
+    "This combination requires an AMD docker image. See <a href=https://github.com/pytorch/ort>the torch-ort repo</a> for details",
    
     "ot_linux,ot_PyTorch18,ot_X64,ot_DNNL":
     "This combination of resources is not fully tested. It may be possible to&nbsp;<a href='https://www.onnxruntime.ai/docs/how-to/build/training' target='_blank'>build from source</a>.",
@@ -557,7 +557,7 @@ var ot_validCombos = {
     "pip install torch-ort -f https://onnxruntimepackages.z14.web.core.windows.net/onnxruntime_stable_torch190.cu111.html<br/> python -m torch_ort.configure",
 
     "ot_linux,ot_PyTorch19,ot_X64,ot_AMD":
-    "This combination is in preview. Please <a href=https://github.com/Microsoft/onnxruntime/issues>reach out</a> for preview packages",
+    "(Preview) pip install torch -f https://download.pytorch.org/whl/rocm4.2/torch_stable.html<br/>pip install torch-ort -f https://onnxruntimepackages.z14.web.core.windows.net/onnxruntime_stable_torch190.rocm42.html<br/>python -m torch_ort.configure</br>",
    
     "ot_linux,ot_C++,ot_X64,ot_CUDA10":
     "This combination of resources is not fully tested. It may be possible to&nbsp;<a href='https://www.onnxruntime.ai/docs/how-to/build/training' target='_blank'>build from source</a>.",
