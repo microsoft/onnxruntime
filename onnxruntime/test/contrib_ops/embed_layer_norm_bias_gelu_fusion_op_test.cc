7a7058f97aff585bd5151f73549e4ca75aa21314
Nick Kreeger <nickkreeger@microsoft.com>
2021-07-12 15:49:45 -0500

    add unit test.

---
+++ onnxruntime/test/contrib_ops/embed_layer_norm_bias_gelu_fusion_op_test.cc
@@ -0,0 +1,20 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+//#include "embed_layer_norm_test_vectors.h"
+#include "gtest/gtest.h"
+#include "test/common/tensor_op_test_utils.h"
+#include "test/providers/provider_test_utils.h"
+
+namespace onnxruntime {
+namespace test {
+
+TEST(EmbedLayerNormBiasGelu, ShouldWork) {
+  //
+  // TODO(kreeger): write me!
+  //
+}
+
+}  // namespace test
+}  // namespace onnxruntime
+
