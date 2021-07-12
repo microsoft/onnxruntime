43fb07e51219346c50bea3c6faabf0773712f3df
Nick Kreeger <nickkreeger@microsoft.com>
2021-07-12 10:00:50 -0500

    wip

--- onnxruntime/test/testdata/transform/fusion/skip_layer_norm_to_name_gen.py
+++ onnxruntime/test/testdata/transform/fusion/skip_layer_norm_to_name_gen.py
@@ -2,70 +2,67 @@ import onnx
 from onnx import helper
 from onnx import TensorProto
 from enum import Enum
+import numpy as np
 
 #
 # TWO SUBGRAPHS: These should all be fused together before quantization!
 #    SkipLayerNormGelu
 #    SkipLayerNormAttention
+#
 
-
-#   <SkipLayerNormalization/EmbedLayerNormalization>   <MatMul>
-#                              \                        /
-#                               \                      /
-#                                \                    /
-#                               <SkipLayerNormalization>
-#                                   /             \
-#                                  /               \
-#                                 /                 \
-#                   <SkipLayerNormalization>   <Attention/MatMul>
-
-
-def generate_nodes(model_name, has_cast, suffix=''):
-    #
-    # TODO(kreeger): Write me!
-    #
-
-    # TODO(kreeger): LEFT OFF RIGHT HERE. NEED TO STITCH TOGETHER A TRANSFORMER SUBGRAPH TO PREP
-    #                for fusing!
+# Graph for SubLayerNorm with a MatMul -> BiasGelu -> MatMul subgraph
+# TODO(kreeger): Eventually rename this one.
+def generate_nodes():
+    kwargs = {}
+    kwargs['epsilon'] = 1e-12
     nodes = [
-        # make_node(op_type, inputs, outputs, name, doc_string, domain, **kwargs)
-        helper.make_node("EmbedLayerNormalization", ["input_ids" + suffix], ["shape1_out" + suffix], "shape1" + suffix),
-        helper.make_node("SkipLayerNormalization"),
-        helper.make_node("Attention"),
-        helper.make_node("MatMul"),
-        helper.make_node("SkipLayerNormalization"),
-        helper.make_node("MatMul"),
-        helper.make_node("BiasGelu"),
-        helper.make_node("MatMul"),
-        helper.make_node("SkipLayerNormalization"),
-        helper.make_node("Attention"),
-        helper.make_node("SkipLayerNormalization"),
-        helper.make_node("MatMul"),
-        helper.make_node("BiasGelu"),
-        helper.make_node("MatMul"),
-        helper.make_node("SkipLayerNormalization"),
-        helper.make_node("Gather"),
-        helper.make_node("MatMul"),
-        helper.make_node("MatMul"),
-        helper.make_node("MatMul"),
+        helper.make_node('SkipLayerNormalization',
+                         ['sln_1_input', 'sln_1_skip', 'sln_1_gamma', 'sln_1_beta', 'sln_1_bias'], ['sln_1_output'],
+                         **kwargs),
+        helper.make_node('MatMul', ['sln_1_output', 'matmul_1_b'], ['matmul_1_output']),
+        helper.make_node('BiasGelu', ['matmul_1_output', 'bias_gelu_1_bias'], ['bias_gelu_1_output']),
+        helper.make_node('MatMul', ['bias_gelu_1_output', 'matmul_2_b'], ['matmul_2_output']),
+        helper.make_node('SkipLayerNormalization',
+                         ['sln_2_input', 'matmul_2_output', 'sln_2_gamma', 'sln_2_beta', 'sln_2_bias'],
+                         ['sln_2_output'],
+                         **kwargs),
     ]
     return nodes
 
 
+# TODO(kreeger): Eventually rename this one.
 def generate_initializers():
-    #
-    # TODO(kreeger): Write me!
-    #
     initializers = [
+        helper.make_tensor('sln_1_gamma', TensorProto.FLOAT, [4], np.random.rand(4)),
+        helper.make_tensor('sln_1_beta', TensorProto.FLOAT, [4], np.random.rand(4)),
+        helper.make_tensor('sln_1_bias', TensorProto.FLOAT, [4], np.random.rand(4)),
+
+        helper.make_tensor('matmul_1_b', TensorProto.FLOAT, [4, 16], np.random.rand(4 * 16)),
+
+        helper.make_tensor('bias_gelu_1_bias', TensorProto.FLOAT, [16], np.random.rand(16)),
+
+        helper.make_tensor('matmul_2_b', TensorProto.FLOAT, [16, 4], np.random.rand(16 * 4)),
+
+        helper.make_tensor('sln_2_gamma', TensorProto.FLOAT, [4], np.random.rand(4)),
+        helper.make_tensor('sln_2_beta', TensorProto.FLOAT, [4], np.random.rand(4)),
+        helper.make_tensor('sln_2_bias', TensorProto.FLOAT, [4], np.random.rand(4)),
     ]
     return initializers
-    pass
+
 
 def generate_model(model_name):
-    #
-    # TODO(kreeger): Write me!
-    #
+    # TODO(kreeger): where do attributes go?
+
+    graph = helper.make_graph(
+        generate_nodes(),
+        'SkipLayerNorm_MatMulBiasGeluMatMul_Subgraph',
+        [],  # inputs
+        [],  # outputs
+        generate_initializers())
+
+    model = helper.make_model(graph);
+    onnx.save(model, model_name)
     pass
 
 
-generate_model('skip_layer_norm_subgraph.py')
+generate_model('skip_layer_norm_matmul_biasgelu_matmul_subgraph.onnx')
