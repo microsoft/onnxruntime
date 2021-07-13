06477c2a8c5c0df864d39570e599af6d822dc2f1
Olivia Jain <oljain@microsoft.com>
2021-07-13 10:14:01 -0700

    Update post.py

--- onnxruntime/python/tools/tensorrt/perf/post.py
+++ onnxruntime/python/tools/tensorrt/perf/post.py
@@ -2,7 +2,6 @@ import argparse
 import mysql.connector
 import sys
 import os
-import subprocess
 import pandas as pd
 from sqlalchemy import create_engine
 
@@ -21,64 +20,29 @@ def parse_arguments():
         "-c", "--commit_hash", help="Commit id", required=True)
     parser.add_argument(
         "-u", "--report_url", help="Report Url", required=True)
-    
     return parser.parse_args()
 
 def parse_csv(report_file):
     table = pd.read_csv(report_file)
     return table
 
-def insert_latency(commit_hash, report_url, latency): 
+def get_latency_over_time(commit_hash, report_url, latency):
+    if not latency.empty:
+        to_drop = ['TrtGain-CudaFp32', 'EpGain-TrtFp32', 'TrtGain-CudaFp16', 'EpGain-TrtFp16']
+        over_time = latency.drop(to_drop, axis='columns')
+        over_time = over_time.melt(id_vars=['Model', 'Group'], var_name='Ep', value_name='Latency')
+            
+        import time   
+        datetime = time.strftime('%Y-%m-%d %H:%M:%S')
+        over_time = over_time.assign(UploadTime=datetime)
+        over_time = over_time.assign(CommitId=commit_hash)
+        over_time = over_time.assign(ReportUrl=report_url)
+            
+        over_time = over_time[['UploadTime', 'CommitId', 'Model', 'Ep', 'Latency', 'ReportUrl', 'Group']]
+        over_time.rename(columns={"Group":"ModelGroup"}, inplace=True)
+        over_time.fillna('', inplace=True)
+        return over_time
     
-    # connect to database
-    cnx = mysql.connector.connect(
-            user=user,
-            password=password,
-            host=host,
-            database=database)
-    
-    try:
-        cursor = cnx.cursor()
-
-        # delete old records
-        delete_query = ('DELETE FROM onnxruntime.ep_latency_over_time '
-                        'WHERE UploadTime < DATE_SUB(Now(), INTERVAL 100 DAY);'
-                        )
-
-        cursor.execute(delete_query)
-        if not latency.empty:
-            print('posting latency over time results to dashboard')
-            to_drop = ['TrtGain-CudaFp32', 'EpGain-TrtFp32', 'TrtGain-CudaFp16', 'EpGain-TrtFp16']
-            over_time = latency.drop(to_drop, axis='columns')
-            over_time = over_time.melt(id_vars=['Model', 'Group'], var_name='Ep', value_name='Latency')
-            
-            import time   
-            datetime = time.strftime('%Y-%m-%d %H:%M:%S')
-            over_time = over_time.assign(UploadTime=datetime)
-            over_time = over_time.assign(CommitId=commit_hash)
-            over_time = over_time.assign(ReportUrl=report_url)
-            
-            over_time = over_time[['UploadTime', 'CommitId', 'Model', 'Ep', 'Latency', 'ReportUrl', 'Group']]
-            over_time.fillna('', inplace=True)
-            tuples = list(over_time.to_records(index=False))
-            tuples = str(tuples)[1:-1] # cut off list brackets
-
-            # insert current record
-            insert_query = ('INSERT INTO onnxruntime.ep_latency_over_time '
-                            '''(UploadTime, CommitId, Model, Ep, Latency, ReportUrl, ModelGroup) '''
-                            '''VALUES %s; ''')
-            
-            query = insert_query % tuples
-            cursor.execute(query)
-        
-        cnx.commit()
-
-        cursor.close()
-        cnx.close()
-    except BaseException as e:
-        cnx.close()
-        raise e
-
 def adjust_columns(table, columns, db_columns, model_group): 
     table = table[columns]
     table = table.set_axis(db_columns, axis=1)
@@ -129,29 +93,42 @@ def get_status(status, model_group):
     status = adjust_columns(status, status_columns, status_db_columns, model_group)
     return status
 
-def get_database_cert(): 
-    cert = 'BaltimoreCyberTrustRoot.crt.pem'
-    #if not os.path.exists(cert):
-    p = subprocess.run(["curl", "-O", 'https://cacerts.digicert.com/BaltimoreCyberTrustRoot.crt.pem'], check=True)
-    return cert 
+def write_table(engine, table, table_name, drop_duplicates=True):   
+    table.to_sql(table_name, con=engine, if_exists='append', index=False, chunksize=1)
+    if drop_duplicates: 
+        full_table = pd.read_sql("SELECT * FROM " + table_name,  engine)
+        full_table.drop_duplicates(keep='last', inplace=True)
+        table.to_sql(table_name, con=engine, if_exists='replace', index=False, chunksize=1)
 
-def write_table(engine, table, table_name): 
-    table.to_sql(table_name, con=engine, if_exists='replace', index=False, chunksize=1)
+def write_latency_over_time(engine, table, table_name):
 
+    # delete using cursor for large table
+    conn = engine.raw_connection()
+    cursor = conn.cursor()
+    delete_query = ('DELETE FROM onnxruntime.ep_latency_over_time '
+                    'WHERE UploadTime < DATE_SUB(Now(), INTERVAL 100 DAY);'
+                    )
+
+    cursor.execute(delete_query)
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+    # write table 
+    write_table(engine, table, table_name, drop_duplicates=False)
+    
 def main():
     
-    # connect to database 
-    cert = get_database_cert()
-    ssl_args = {'ssl_ca': cert}
+    # connect to database
+    args = parse_arguments()
     connection_string = sql_connector + \
                         user + ':' + \
                         password + \
                         '@' + host + '/' + \
                         database
-    engine = create_engine(connection_string, connect_args=ssl_args)
+    engine = create_engine(connection_string)
 
-    try: 
-        args = parse_arguments()
+    try:
         result_file = args.report_folder
 
         folders = os.listdir(result_file)
@@ -161,17 +138,19 @@ def main():
         memory = pd.DataFrame()
         latency = pd.DataFrame()
         status = pd.DataFrame()
+        latency_over_time = pd.DataFrame()
        
         for model_group in folders: 
             os.chdir(model_group)
             csv_filenames = os.listdir()
-            for csv in csv_filenames: 
+            for csv in csv_filenames:
                 table = parse_csv(csv)
                 if "fail" in csv:
                     fail = fail.append(get_failures(table, model_group), ignore_index=True)
                 if "latency" in csv:
                     memory = memory.append(get_memory(table, model_group), ignore_index=True)
                     latency = latency.append(get_latency(table, model_group), ignore_index=True)
+                    latency_over_time = latency_over_time.append(get_latency_over_time(args.commit_hash, args.report_url, latency), ignore_index=True)
                 if "status" in csv: 
                     status = status.append(get_status(table, model_group), ignore_index=True)
             os.chdir(result_file)
@@ -185,7 +164,7 @@ def main():
         print('writing status to database')
         write_table(engine, status, 'ep_models_status')
         print('writing latency over time to database')
-        insert_latency(args.commit_hash, args.report_url, latency)
+        write_latency_over_time(engine, latency_over_time, 'ep_latency_over_time')
 
     except BaseException as e: 
         print(str(e))
