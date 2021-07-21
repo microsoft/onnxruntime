4534667be7b9cc0a6602078231989d156ff9e905
hwangdeyu <dejack953@outlook.com>
2021-07-21 16:32:18 +0800

    disable the tests until the kernel added

--- onnxruntime/core/graph/contrib_ops/contrib_defs.cc
+++ onnxruntime/core/graph/contrib_ops/contrib_defs.cc
@@ -2988,12 +2988,11 @@ It's an extension of Gelu. It takes the sum of input A and bias input B as the i
 
   static const char* GridSampler_ver1_doc = R"DOC(
       Given an `input` and a flow-field `grid`, computes the `output` using `input` values and pixel locations from `grid`.
-      Currently, only spatial (4-D) inputs are supported. For `input` with shap (N, C, H, W) and `grid` with shape (N, H_out, W_out, 2),
+      Currently, only spatial (4-D) inputs are supported. For `input` with shape (N, C, H, W) and `grid` with shape (N, H_out, W_out, 2),
       the `output` will have shape (N, C, H_out, W_out).
       For each output location `output[n, :, h, w]`, the size-2 vector `grid[n, h, w]` specifies `input` pixel locations `x` and `y`,
       which are used to interpolate the output value `output[n, :, h, w]`.
-      The GridSampler operator is often used in conjunction with affine_grid doing grid generator
-      and sampler in the [Spatial Transformer Networks](https://arxiv.org/abs/1506.02025).
+      The GridSampler operator is often used in doing grid generator and sampler in the [Spatial Transformer Networks](https://arxiv.org/abs/1506.02025).
       See also in [torch.nn.functional.grid_sample](https://pytorch.org/docs/master/generated/torch.nn.functional.grid_sample.html#torch-nn-functional-grid-sample).
       )DOC";
 
