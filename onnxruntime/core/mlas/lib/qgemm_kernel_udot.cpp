1f4ba8643eeadec1a16463511431a20a376e30a7
Chen Fu <fuchen@microsoft.com>
2021-07-21 10:12:17 -0700

    process 8 rows

--- onnxruntime/core/mlas/lib/qgemm_kernel_udot.cpp
+++ onnxruntime/core/mlas/lib/qgemm_kernel_udot.cpp
@@ -90,6 +90,7 @@ MlasGemmU8X8CopyPackA<MLAS_GEMM_U8X8_KERNEL_UDOT>(
     // interleaved with the following pattern:
     //
     //      [ A0 A1 A2 A3 B0 B1 B2 B3 C0 C1 C2 C3 D0 D1 D2 D3 ]
+    // TODO [ E0 E1 E2 E3 F0 F1 F2 F3 G0 G1 G2 G3 H0 H1 H2 H3 ]
     //      [ A4 A5 A6 A7 B4 B5 B6 B7 C4 C5 C6 C7 D4 D5 D6 D7 ]
     //
     // This pattern is repeated (CountK / 8) times.
