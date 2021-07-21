55b26b69513d3972e7c0a70f0d54f3fbb8adc054
Adam Pocock <adam.pocock@oracle.com>
2021-07-21 01:33:15 -0400

    [Java] Adds support for DNNL, OpenVINO, TensorRT shared providers and refactors the CUDA shared provider loader (#8013)

--- tools/ci_build/github/linux/run_build.sh
+++ tools/ci_build/github/linux/run_build.sh
@@ -69,7 +69,6 @@ else
             --cuda_home /usr/local/cuda \
             --cudnn_home /usr/local/cuda $BUILD_EXTR_PAR
     else #cpu and openvino
-        export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
         python3 $SCRIPT_DIR/../../build.py --build_dir /build \
             --config Release $COMMON_BUILD_ARGS $BUILD_EXTR_PAR
     fi
