ef3118fd634d3a2b2f65bbf8e86e492236495d5c
Nick Kreeger <nickkreeger@microsoft.com>
2021-07-20 22:12:33 -0500

    quant tool

--- onnxruntime/python/tools/quantization/operators/embed_layer_norm_bias_gelu.py
+++ onnxruntime/python/tools/quantization/operators/embed_layer_norm_bias_gelu.py
@@ -16,58 +16,60 @@ class EmbedLayerNormBiasGeluQuant(QuantOperatorBase):
 
         '''
         Pre-quantization EmbedLayerNorm inputs:
-        [0] input_ids (int32)
-        [1] segment_ids (int32)
-        [2] word_embedding (float32)
-        [3] position_embedding (float32)
-        [4] segment_embedding (float32)
-        [5] gamma (float32)
-        [6] beta (float32)
-        [7] mask (int32) (optional)
+        [0] SLN Input 0 (input)
+        [1] SLN Input 1 (skip)
+        [2] SLN Input 2 (gamma)
+        [3] SLN Input 3 (beta)
+        [4] SLN Input 4 (bias)
+        [5] MatMul #1 Input 1
+        [6] BiasGelu Input 1
+        [7] MatMul #2 Input 1
         '''
         (quantized_input_names, zero_point_names, scale_names, nodes) = \
-            self.quantizer.quantize_inputs(node, [2, 3, 4, 5, 6])
+            self.quantizer.quantize_inputs(node, [2, 3, 4, 5, 6, 7])
 
-        qembed_layer_norm_name = "" if node.name == "" else node.name + "_quant"
+        name = "" if node.name == "" else node.name + "_quant"
 
         '''
         Quantized Input Tensor List
-        [0] input_ids (int32)
-        [1] segment_ids (int32)
-        [2] word_embedding (uint8)
-        [3] position_embedding (uint8)
-        [4] segment_embedding (uint8)
-        [5] gamma (uint8)
-        [6] beta (uint8)
-        [7] mask (int32) (optional)
-        [8] word_embedding_scale (float)
-        [9] position_embedding_scale (float)
-        [10] segment_embedding_scale (float)
-        [11] gamma_scale (float)
-        [12] beta_scale (float)
-        [13] word_embedding_zero_point (uint8)
-        [14] position_embedding_zero_point (uint8)
-        [15] segment_embedding_zero_point (uint8)
-        [16] gamma_zero_point (uint8)
-        [17] beta_zero_point (uint8)
+        [0] SLN Input 0 (input)
+        [1] SLN Input 1 (skip)
+        [2] SLN Input 2 (gamma)
+        [3] SLN Input 3 (beta)
+        [4] SLN Input 4 (bias)
+        [5] MatMul #1 Input 1
+        [6] BiasGelu Input 1
+        [7] MatMul #2 Input 1
+	[8] SLN Input 2 (gamma) scale (float)
+	[9] SLN Input 3 (beta) scale (float)
+	[10] SLN Input 4 (bias) scale (float)
+	[11] MatMul #1 Input 1 scale (float)
+	[12] BiasGelu Input 1 scale (float)
+	[13] MatMul #2 Input 1 scale (float)
+	[14] SLN Input 2 (gamma) zero point (uint8)
+	[15] SLN Input 3 (beta) zero point (uint8)
+	[10] SLN Input 4 (bias) zero point (uint8)
+	[11] MatMul #1 Input 1 zero point (uint8)
+	[12] BiasGelu Input 1 zero point (uint8)
+	[13] MatMul #2 Input 1 zero point (uint8)
         '''
         inputs = []
-        # 'input_ids'
+        # 'SLN Input 0 (input)'
         inputs.extend([node.input[0]])
-        # 'segment_ids'
+        # 'SLN Input 1 (skip)'
         inputs.extend([node.input[1]])
-        # 'word_embedding_quant'
+        # 'SLN Input 2 (gamma)'
         inputs.extend([quantized_input_names[0]])
-        # 'position_embedding_quant'
+        # 'SLN Input 3 (beta)'
         inputs.extend([quantized_input_names[1]])
-        # 'segment_embedding_quant'
+        # 'SLN Input 4 (bias)'
         inputs.extend([quantized_input_names[2]])
-        # 'gamma_quant'
+        # 'MatMul #1 Input 1'
         inputs.extend([quantized_input_names[3]])
-        # 'beta_quant'
+        # 'BiasGelu Input 1'
         inputs.extend([quantized_input_names[4]])
-        # 'mask' (optional)
-        inputs.extend([node.input[7] if len(node.input) > 7 else ""])
+        # 'MatMul #2 Input 1'
+        inputs.extend([quantized_input_names[4]])
 
         # Add all scales:
         inputs.extend([scale_names[0]])
@@ -75,6 +77,7 @@ class EmbedLayerNormBiasGeluQuant(QuantOperatorBase):
         inputs.extend([scale_names[2]])
         inputs.extend([scale_names[3]])
         inputs.extend([scale_names[4]])
+        inputs.extend([scale_names[5]])
 
         # Add all zero points:
         inputs.extend([zero_point_names[0]])
@@ -82,15 +85,14 @@ class EmbedLayerNormBiasGeluQuant(QuantOperatorBase):
         inputs.extend([zero_point_names[2]])
         inputs.extend([zero_point_names[3]])
         inputs.extend([zero_point_names[4]])
+        inputs.extend([zero_point_names[5]])
 
         kwargs = {}
         for attribute in node.attribute:
             kwargs.update(attribute_to_kwarg(attribute))
         kwargs["domain"] = ms_domain
 
-        qembed_layer_norm_node = onnx.helper.make_node("QEmbedLayerNormalization", inputs, node.output,
-                                                       qembed_layer_norm_name, **kwargs)
-        nodes.append(qembed_layer_norm_node)
+        quantized_node = onnx.helper.make_node("QEmbedLayerNormBiasGelu", inputs, node.output, name, **kwargs)
+        nodes.append(quantized_node)
 
         self.quantizer.new_nodes += nodes
-
