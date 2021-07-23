bd20a9d8d4e06e4411a79a466f694d994fac2a56
Tiago Koji Castro Shibata <tiago.shibata@gmail.com>
2021-07-23 14:56:21 -0700

    Remove incompatible tests from Store

--- cmake/uwp_stubs.h
+++ cmake/uwp_stubs.h
@@ -1,6 +1,10 @@
 #pragma once
-
+// Part of the standard C runtime and the STL are missing when targeting UWP, and some applications that rely on standard,
+// cross-platform headers fail to build.
+// Here we provide stubs for functions required by some onnxruntime dependencies.
 #ifdef __cplusplus
+// Extending the std namespace is undefined behavior
+// NOLINTNEXTLINE
 namespace std {
     inline char *getenv(const char*) { return nullptr; }
 }
