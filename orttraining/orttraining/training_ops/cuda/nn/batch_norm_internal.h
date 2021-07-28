a71dab691d1d67469654e8e35043aa8bceea8b9e
mindest <30493312+mindest@users.noreply.github.com>
2021-07-28 16:04:49 +0800

    Implement BatchNormInternal for cuda (#8172)
    * correct batchnorm replacement output order;
    
    remove bn replacement in grad graph builder
    
    * update op defs and kernel class
    
    * implement batch norm internal and grad.
    
    * change saved_var into saved_inv_std
    
    * cuda test case: bn internal
    
    * remove redundant include
    
    * fix comment; add support and UT for 1d input.
    
    * exclude batch_norm_internal in amd_hipify
    
    * run BNInternal UT for CUDA only
    
    * fix CI error
    
    * fix comment errors
    
    * fix error
    
    * add comment for inconsistency with cudnnBN doc
    
    * additional comments for cudnnBN inconsistency

--- orttraining/orttraining/training_ops/cuda/nn/batch_norm_internal.h
+++ orttraining/orttraining/training_ops/cuda/nn/batch_norm_internal.h
@@ -4,7 +4,6 @@
 #pragma once
 
 #include "gsl/gsl"
-
 #include "core/providers/cuda/cuda_kernel.h"
 #include "core/providers/cuda/cudnn_common.h"
 
@@ -12,23 +11,29 @@ namespace onnxruntime {
 namespace cuda {
 
 template <typename T, typename T1, typename T2>
-class BatchNormalizationGrad final : public CudaKernel {
+class BatchNormInternal final : public CudaKernel {
  public:
-  BatchNormalizationGrad(const OpKernelInfo& info)
-      : CudaKernel{info},
-        cudnn_batch_norm_mode_(CUDNN_BATCHNORM_SPATIAL) {
+  BatchNormInternal(const OpKernelInfo& op_kernel_info)
+      : CudaKernel{op_kernel_info},
+        cudnn_batch_norm_mode_(CUDNN_BATCHNORM_SPATIAL),
+        momentum_(0.9) {
     float tmp_epsilon;
-    ORT_ENFORCE(info.GetAttr<float>("epsilon", &tmp_epsilon).IsOK());
+    ORT_ENFORCE(op_kernel_info.GetAttr<float>("epsilon", &tmp_epsilon).IsOK());
     epsilon_ = ClampCudnnBatchNormEpsilon(static_cast<double>(tmp_epsilon));
 
     // spatial or not
     int64_t tmp_spatial;
-    if (info.GetAttr<int64_t>("spatial", &tmp_spatial).IsOK()) {
+    if (op_kernel_info.GetAttr<int64_t>("spatial", &tmp_spatial).IsOK()) {
       spatial_ = tmp_spatial;
     }
 
     if (spatial_ == 0) {
       cudnn_batch_norm_mode_ = CUDNN_BATCHNORM_PER_ACTIVATION;
+    }
+
+    float tmp_momentum;
+    if (op_kernel_info.GetAttr<float>("momentum", &tmp_momentum).IsOK()) {
+      momentum_ = static_cast<double>(tmp_momentum);
     }
   }
 
@@ -36,8 +41,9 @@ class BatchNormalizationGrad final : public CudaKernel {
 
  private:
   double epsilon_;
-  int64_t spatial_ = 1;
+  int64_t spatial_ = 1;  // default as per spec
   cudnnBatchNormMode_t cudnn_batch_norm_mode_;
+  double momentum_;
 };
 
 }  // namespace cuda
