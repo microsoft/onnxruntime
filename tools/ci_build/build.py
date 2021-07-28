b42bad2c306b7b1b64ec2832d97099dab22ab3f4
wangye <wangye@microsoft.com>
2021-07-28 21:19:35 +0000

    update

--- tools/ci_build/build.py
+++ tools/ci_build/build.py
@@ -1541,10 +1541,7 @@ def run_onnxruntime_tests(args, source_dir, ctest_path, build_dir, configs):
                         run_subprocess([sys.executable, '-m', 'pip', 'install', '-r', 'requirements.txt'],
                                        cwd=SCRIPT_DIR)
                         run_subprocess([sys.executable, '-m', 'pytest', 'transformers'], cwd=cwd)
-                        # Restore initial environment
-                        run_subprocess([sys.executable, '-m', 'pip', 'uninstall', '-r', 'requirements.txt', '-y'],
-                                       cwd=SCRIPT_DIR)
-                        # Restore initial numpy version in case other tests use it
+                        # Restore initial numpy/protobuf version in case other tests use it
                         run_subprocess([sys.executable, '-m', 'pip', 'install', 'numpy==' + numpy_init_version])
                         run_subprocess([sys.executable, '-m', 'pip', 'install', 'protobuf==' + protobuf_init_version])
 
