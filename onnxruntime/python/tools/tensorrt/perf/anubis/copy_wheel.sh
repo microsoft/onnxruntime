e7aad8aea9acf6fa76659c7ef5980e75be2e4a27
Olivia Jain <oljain@microsoft.com>
2021-08-03 16:46:43 -0700

    remove sudo

--- onnxruntime/python/tools/tensorrt/perf/anubis/copy_wheel.sh
+++ onnxruntime/python/tools/tensorrt/perf/anubis/copy_wheel.sh
@@ -8,10 +8,10 @@ esac
 done 
 
 # copying wheel over
-id=$(sudo docker create ort-master)
-sudo docker cp $id:/code/onnxruntime/build/Linux/Release/dist/ ../
-sudo docker rm -v $id
+id=$(docker create ort-master)
+docker cp $id:/code/onnxruntime/build/Linux/Release/dist/ ../
+docker rm -v $id
 
 # adding trt container version
 wheel_name=$(echo ../dist/*|sed 's/-cp/.'$TRT_CONTAINER'-cp/')
-sudo mv ../dist/* $wheel_name
+mv ../dist/* $wheel_name
