6e7eb6fd8135f0c7acf7c920cd755b00561d79d3
Yulong Wang <yulongw@microsoft.com>
2021-08-03 19:25:37 -0700

    [js/web] support override wasm file path

---
+++ js/web/test/e2e/browser-test-wasm-path-override-prefix.js
@@ -0,0 +1,14 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+it('Browser E2E testing - WebAssembly backend (path override prefix)', async function () {
+
+  // disable SIMD and multi-thread
+  ort.env.wasm.numThreads = 1;
+  ort.env.wasm.simd = false;
+
+  // override .wasm file path prefix
+  ort.env.wasm.wasmPaths = new URL('./test-wasm-path-override/', document.baseURI).href;
+
+  await testFunction(ort, { executionProviders: ['wasm'] });
+});
