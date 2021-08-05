4161a423c737fe567fbfc49f6e3d29c56da075f3
Chi Lo <Chi.Lo@microsoft.com>
2021-08-05 07:46:47 +0000

    Change back from CUDA11.4 + TRT8 to CUDA11.1 + TRT7 for testing

--- tools/ci_build/github/windows/setup_env_gpu.bat
+++ tools/ci_build/github/windows/setup_env_gpu.bat
@@ -1,2 +1,2 @@
-set PATH=C:\azcopy;C:\local\TensorRT-8.0.1.6.Windows10.x86_64.cuda-11.3.cudnn8.2;C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v11.1\bin;C:\local\cudnn-11.4-windows-x64-v8.2.2.26\cuda\bin;%PATH%
+set PATH=C:\azcopy;C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v11.1\bin;C:\local\cudnn-11.1-windows-x64-v8.0.5.39\cuda\bin;C:\local\TensorRT-7.2.2.3\lib;%PATH%
 set GRADLE_OPTS=-Dorg.gradle.daemon=false
