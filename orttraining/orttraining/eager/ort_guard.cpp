6d3c2c85ef0b663c9beead6db52e5c0551d639bf
Tang, Cheng <souptc@gmail.com>
2021-08-06 08:30:27 -0700

    Integrate eager mode source code into onnxruntime repo (#8584)
    * integrate eager mode source codde; build with cmake and integrate the python test
    
    * Adding the python path for importing libraries in the Eager mode
    
    * fix clang break;check if training and python enabled
    
    * handling the linking of torch libraries across multiple platforms
    
    * merge and fix the naming
    
    * add build instruction
    
    Co-authored-by: Abhishek Jindal <abjindal@OrtTrainingDev0.af05slrtruoetgaxwwjv5nsq5e.px.internal.cloudapp.net>
    Co-authored-by: ajindal1 <abjindal@microsoft.com>

---
+++ orttraining/orttraining/eager/ort_guard.cpp
@@ -0,0 +1,112 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+#include <torch/extension.h>
+
+#include "ort_backends.h"
+#include "ort_log.h"
+
+namespace torch_ort {
+namespace eager {
+
+struct ORTGuardImpl final : public c10::impl::DeviceGuardImplInterface {
+  ORTGuardImpl() {
+    ORT_LOG_FN();
+  }
+
+  explicit ORTGuardImpl(at::DeviceType t) {
+    ORT_LOG_FN(t);
+    AT_ASSERT(t == at::DeviceType::ORT);
+  }
+
+  at::DeviceType type() const override {
+    ORT_LOG_FN();
+    return at::DeviceType::ORT;
+  }
+
+  at::Device exchangeDevice(at::Device d) const override {
+    ORT_LOG_FN(d);
+    AT_ASSERT(d.type() == at::DeviceType::ORT);
+    auto old_device = getDevice();
+    if (old_device.index() != d.index()) {
+      current_device_ = d.index();
+    }
+    return old_device;
+  }
+
+  at::Device getDevice() const override {
+    ORT_LOG_FN();
+    return at::Device(at::DeviceType::ORT, current_device_);
+  }
+
+  void setDevice(at::Device d) const override {
+    ORT_LOG_FN(d);
+    AT_ASSERT(d.type() == at::DeviceType::ORT);
+    AT_ASSERT(d.index() >= 0);
+    current_device_ = d.index();
+  }
+
+  void uncheckedSetDevice(at::Device d) const noexcept override {
+    ORT_LOG_FN(d);
+    current_device_ = d.index();
+  }
+
+  at::Stream getStream(at::Device d) const noexcept override {
+    ORT_LOG_FN(d);
+    return at::Stream(at::Stream::UNSAFE, d, current_streams_[d.index()]);
+  }
+
+  at::Stream exchangeStream(at::Stream s) const noexcept override {
+    ORT_LOG_FN(s);
+    auto old_id = current_streams_[s.device_index()];
+    current_streams_[s.device_index()] = s.id();
+    return at::Stream(at::Stream::UNSAFE, s.device(), old_id);
+  }
+
+  at::DeviceIndex deviceCount() const noexcept override {
+    ORT_LOG_FN();
+    return 1;
+  }
+
+//  #pragma region events
+
+  #define EVENTS_NIEX TORCH_CHECK(false, "ORT backend doesn't support events.")
+
+  void record(void** event,
+    const at::Stream& stream,
+    const at::DeviceIndex device_index,
+    const at::EventFlag flag) const override {
+    EVENTS_NIEX;
+  }
+
+  void block(
+    void* event,
+    const at::Stream& stream) const override {
+    EVENTS_NIEX;
+  }
+
+  bool queryEvent(void* event) const override {
+    EVENTS_NIEX;
+  }
+
+  void destroyEvent(
+    void* event,
+    const at::DeviceIndex device_index) const noexcept override {
+  }
+
+  #undef EVENTS_NIEX
+
+  //#pragma endregion events
+
+ private:
+  thread_local static at::DeviceIndex current_device_;
+  thread_local static std::map<at::DeviceIndex, at::StreamId> current_streams_;
+};
+
+thread_local at::DeviceIndex ORTGuardImpl::current_device_ = 0;
+thread_local std::map<at::DeviceIndex, at::StreamId> ORTGuardImpl::current_streams_ = {};
+
+C10_REGISTER_GUARD_IMPL(ORT, ORTGuardImpl);
+
+} // namespace eager
+} // namespace torch_ort
