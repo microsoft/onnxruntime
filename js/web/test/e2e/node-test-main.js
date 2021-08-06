f3a1aebb3385a446acf6239a1350e162dfaa1ccd
Yulong Wang <yulongw@microsoft.com>
2021-08-05 18:01:03 -0700

    [js/web] support override wasm file path (#8610)

--- js/web/test/e2e/node-test-main.js
+++ js/web/test/e2e/node-test-main.js
@@ -4,7 +4,7 @@
 const ort = require('onnxruntime-web');
 const testFunction = require('./common');
 
-it('Browser E2E testing - WebAssembly backend', async function () {
+it('Node.js E2E testing - WebAssembly backend', async function () {
   await testFunction(ort, { executionProviders: ['wasm'] });
 
   process.exit();
