14e93ceab01a9dd2b0ef51468c02c2834847aa59
rachguo <rachguo@rachguos-Mini.attlocal.net>
2021-08-06 17:36:46 -0700

    wip

--- onnxruntime/core/providers/coreml/builders/impl/transpose_op_builder.cc
+++ onnxruntime/core/providers/coreml/builders/impl/transpose_op_builder.cc
@@ -13,9 +13,9 @@
 namespace onnxruntime {
 namespace coreml {
 
+#ifdef __APPLE__
 class TransposeOpBuilder : public BaseOpBuilder {
   // Add operator related
-#ifdef __APPLE__
  private:
   Status AddToModelBuilderImpl(ModelBuilder& model_builder, const Node& node,
                                const logging::Logger& logger) const override ORT_MUST_USE_RESULT;
