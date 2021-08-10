02682ea4c09b126196f35e971032ec1ce15e7e42
rachguo <rachguo@rachguos-Mini.attlocal.net>
2021-08-10 11:50:06 -0700

    wip

--- tools/python/util/convert_onnx_models_to_ort.py
+++ tools/python/util/convert_onnx_models_to_ort.py
@@ -219,12 +219,8 @@ def convert_onnx_models_to_ort():
     if args.use_nnapi and 'NnapiExecutionProvider' not in ort.get_available_providers():
         raise ValueError('The NNAPI Execution Provider was not included in this build of ONNX Runtime.')
 
-    if args.use_coreml:
-        if not is_macOS():
-            # Check if the script is run on a Mac Device in this case
-            raise ValueError('--use_coreml option requires a MacOS environment.')
-        if 'CoreMLExecutionProvider' not in ort.get_available_providers():
-            raise ValueError('The CoreML Execution Provider was not included in this build of ONNX Runtime.')
+    if args.use_coreml and 'CoreMLExecutionProvider' not in ort.get_available_providers():
+        raise ValueError('The CoreML Execution Provider was not included in this build of ONNX Runtime.')
 
     _convert(model_path_or_dir, args.optimization_level, args.use_nnapi, args.use_coreml, custom_op_library,
              args.save_optimized_onnx_model, args.allow_conversion_failures)
