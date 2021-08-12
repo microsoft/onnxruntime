7ff6a5e50374ff2d583f3788023cbe497ed14521
George Wu <jywu@microsoft.com>
2021-08-12 08:37:25 -0700

    work around build warning on jetson (#8701)

--- onnxruntime/test/providers/kernel_def_hash_test.cc
+++ onnxruntime/test/providers/kernel_def_hash_test.cc
@@ -61,10 +61,15 @@
 #ifdef _WIN32
 #pragma warning(push)
 #pragma warning(disable : 28020)
+#elif __aarch64__
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wformat-truncation"
 #endif
 #include "nlohmann/json.hpp"
 #ifdef _WIN32
 #pragma warning(pop)
+#elif __aarch64__
+#pragma GCC diagnostic pop
 #endif
 
 #include "asserts.h"
