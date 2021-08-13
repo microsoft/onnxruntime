4172c181d18d520568553a25b1405f068583bfcc
rachguo <rachguo@rachguos-Mini.attlocal.net>
2021-08-13 14:34:10 -0700

    enable nnapi/coreml in one build

--- tools/ci_build/github/linux/ort_minimal/build_full_ort_and_create_ort_files.sh
+++ tools/ci_build/github/linux/ort_minimal/build_full_ort_and_create_ort_files.sh
@@ -26,16 +26,6 @@ python3 /onnxruntime_src/tools/ci_build/build.py \
     --enable_training_ops \
     --enable_pybind --cmake_extra_defines PYTHON_INCLUDE_DIR=/opt/python/cp37-cp37m/include/python3.7m PYTHON_LIBRARY=/usr/lib64/librt.so \
     --use_nnapi
-
-python3 /onnxruntime_src/tools/ci_build/build.py \
-    --build_dir /build --cmake_generator Ninja \
-    --config Debug \
-    --skip_submodule_sync \
-    --parallel \
-    --build_wheel \
-    --skip_tests \
-    --enable_training_ops \
-    --enable_pybind --cmake_extra_defines PYTHON_INCLUDE_DIR=/opt/python/cp37-cp37m/include/python3.7m PYTHON_LIBRARY=/usr/lib64/librt.so \
     --use_coreml
 
 # Run kernel def hash verification test
