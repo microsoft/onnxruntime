f8508e069af2c77af2db192ceb2fe61e437f8fe2
Guoyu Wang <wanggy@outlook.com>
2021-08-12 18:48:56 -0700

    Add missing __aarch64__

--- onnxruntime/test/providers/kernel_def_hash_test.cc
+++ onnxruntime/test/providers/kernel_def_hash_test.cc
@@ -62,14 +62,14 @@
 #ifdef _WIN32
 #pragma warning(push)
 #pragma warning(disable : 28020)
-#elif defined(HAS_FORMAT_TRUNCATION)
+#elif __aarch64__ && defined(HAS_FORMAT_TRUNCATION)
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wformat-truncation"
 #endif
 #include "nlohmann/json.hpp"
 #ifdef _WIN32
 #pragma warning(pop)
-#elif defined(HAS_FORMAT_TRUNCATION)
+#elif __aarch64__ && defined(HAS_FORMAT_TRUNCATION)
 #pragma GCC diagnostic pop
 #endif
 
