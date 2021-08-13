71f97109d31020732cb36184e3ae7615d2f1b3fb
Chi Lo <Chi.Lo@microsoft.com>
2021-08-13 07:13:52 +0000

    Modify for test

--- csharp/test/Microsoft.ML.OnnxRuntime.EndToEndTests/runtest.bat
+++ csharp/test/Microsoft.ML.OnnxRuntime.EndToEndTests/runtest.bat
@@ -53,10 +53,18 @@ dir test\Microsoft.ML.OnnxRuntime.EndToEndTests\packages\
 
 IF "%PACKAGENAME%"=="Microsoft.ML.OnnxRuntime.Gpu" (
   set TESTONGPU=ON
-  %dn% test -p:DefineConstants=USE_CUDA%2CUSE_TENSORRT test\Microsoft.ML.OnnxRuntime.EndToEndTests\Microsoft.ML.OnnxRuntime.EndToEndTests.csproj --no-restore
+  %dn% test -p:DefineConstants=USE_TENSORRT test\Microsoft.ML.OnnxRuntime.EndToEndTests\Microsoft.ML.OnnxRuntime.EndToEndTests.csproj --no-restore --filter TensorRT 
+
+  IF NOT errorlevel 0 (
+    @echo "Failed to build or execute the end-to-end test"
+    EXIT 1
+  )
+
+  %dn% test -p:DefineConstants=USE_CUDA test\Microsoft.ML.OnnxRuntime.EndToEndTests\Microsoft.ML.OnnxRuntime.EndToEndTests.csproj --no-restore
 ) ELSE (
   %dn% test test\Microsoft.ML.OnnxRuntime.EndToEndTests\Microsoft.ML.OnnxRuntime.EndToEndTests.csproj --no-restore
 )
+
 IF NOT errorlevel 0 (
     @echo "Failed to build or execute the end-to-end test"
     EXIT 1
