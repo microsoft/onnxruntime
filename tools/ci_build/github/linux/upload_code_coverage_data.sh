bd9b5c9231e7c6cd9ba7c3d12234ddfd2c60deba
Changming Sun <chasun@microsoft.com>
2021-08-13 07:34:14 -0700

    update

--- tools/ci_build/github/linux/upload_code_coverage_data.sh
+++ tools/ci_build/github/linux/upload_code_coverage_data.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
-$BUILD_SOURCESDIRECTORY/tools/ci_build/github/windows/post_code_coverage_to_dashboard.py --commit_hash=$BUILD_SOURCEVERSION --report_file="$BUILD_BINARIESDIRECTORY/report.json" --report_url=$1 --branch $BUILD_SOURCEBRANCHNAME --arch $2 --os $3 --build_config default
+$BUILD_SOURCESDIRECTORY/tools/ci_build/github/windows/post_code_coverage_to_dashboard.py --commit_hash=$BUILD_SOURCEVERSION --report_file=$1 --report_url=$2 --branch $BUILD_SOURCEBRANCHNAME --arch $3 --os $4 --build_config default
