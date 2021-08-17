63a3499dbfb00f2fa26369e7ce5f952e13e8b4d3
Wei-Sheng Chin <wschin@outlook.com>
2021-08-17 05:47:12 +0000

    Remove print's

--- tools/ci_build/build.py
+++ tools/ci_build/build.py
@@ -1021,12 +1021,6 @@ def generate_build_tree(cmake_path, source_dir, build_dir, cuda_home, cudnn_home
     else:
         add_cmake_define_without_override(cmake_extra_defines, "onnxruntime_PYBIND_EXPORT_OPSCHEMA", "OFF")
 
-    print('build.py: 3')
-    print(cmake_extra_defines)
-
-    print('build.py: 4 (cmake_args)')
-    print(cmake_args)
-
     if args.build_eager_mode:
         import torch
         cmake_args += ["-Donnxruntime_PREBUILT_PYTORCH_PATH=%s" % os.path.dirname(torch.__file__)]
@@ -1034,9 +1028,6 @@ def generate_build_tree(cmake_path, source_dir, build_dir, cuda_home, cudnn_home
     cmake_args += ["-D{}".format(define) for define in cmake_extra_defines]
 
     cmake_args += cmake_extra_args
-
-    print('build.py: 5 (cmake_args)')
-    print(cmake_args)
 
     # ADO pipelines will store the pipeline build number
     # (e.g. 191101-2300.1.master) and source version in environment
@@ -1085,8 +1076,6 @@ def generate_build_tree(cmake_path, source_dir, build_dir, cuda_home, cudnn_home
                 config_build_dir, "external", "tvm",
                 config) + os.pathsep + os.path.dirname(sys.executable) + os.pathsep + os.environ["PATH"]
 
-        print('build.py: 6 (cmake_args)')
-        print(cmake_args)
         run_subprocess(
             cmake_args + [
                 "-Donnxruntime_ENABLE_MEMLEAK_CHECKER=" +
@@ -1919,8 +1908,6 @@ def main():
     args = parse_arguments()
     cmake_extra_defines = (args.cmake_extra_defines
                            if args.cmake_extra_defines else [])
-    print('build.py: 1')
-    print(cmake_extra_defines)
     cross_compiling = args.arm or args.arm64 or args.arm64ec or args.android
 
     # If there was no explicit argument saying what to do, default
@@ -2160,8 +2147,6 @@ def main():
         if args.use_rocm and args.rocm_version is None:
             args.rocm_version = ""
 
-        print('build.py: 2')
-        print(cmake_extra_defines)
         if args.build_eager_mode:
             # generate the ort aten backend code
             def gen_ort_aten_ops(eager_root_dir):
