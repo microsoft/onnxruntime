98c3edf1c6df773694538804344665c3e7cd00a5
Wei-Sheng Chin <wschin@outlook.com>
2021-08-17 10:55:13 -0700

    Packaging pipeline now builds with PythonOp (aka running autograd.Function) (#8652)
    This PR disable UTs in training's package pipelines 
    for building packages with PythonOp (torch.autograd.Function).

--- tools/ci_build/build.py
+++ tools/ci_build/build.py
@@ -161,6 +161,8 @@ def parse_arguments():
     parser.add_argument(
         "--enable_training_ops", action='store_true', help="Enable training ops in inference graph.")
     parser.add_argument(
+        "--enable_training_torch_interop", action='store_true', help="Enable training kernels interop with torch.")
+    parser.add_argument(
         "--disable_nccl", action='store_true', help="Disable Nccl.")
     parser.add_argument(
         "--mpi_home", help="Path to MPI installation dir")
@@ -763,6 +765,7 @@ def generate_build_tree(cmake_path, source_dir, build_dir, cuda_home, cudnn_home
         "-Donnxruntime_ENABLE_NVTX_PROFILE=" + ("ON" if args.enable_nvtx_profile else "OFF"),
         "-Donnxruntime_ENABLE_TRAINING=" + ("ON" if args.enable_training else "OFF"),
         "-Donnxruntime_ENABLE_TRAINING_OPS=" + ("ON" if args.enable_training_ops else "OFF"),
+        "-Donnxruntime_ENABLE_TRAINING_TORCH_INTEROP=" + ("ON" if args.enable_training_torch_interop else "OFF"),
         # Enable advanced computations such as AVX for some traininig related ops.
         "-Donnxruntime_ENABLE_CPU_FP16_OPS=" + ("ON" if args.enable_training else "OFF"),
         "-Donnxruntime_USE_NCCL=" + ("OFF" if args.disable_nccl else "ON"),
