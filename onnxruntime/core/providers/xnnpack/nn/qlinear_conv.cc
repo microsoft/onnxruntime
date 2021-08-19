8d38a70461b167b57bc5ed0ff22708b1db20723b
Yufeng Li <liyufeng1987@gmail.com>
2021-08-19 10:19:09 -0700

    create operator when constructing operator

--- onnxruntime/core/providers/xnnpack/nn/qlinear_conv.cc
+++ onnxruntime/core/providers/xnnpack/nn/qlinear_conv.cc
@@ -15,12 +15,98 @@ class QLinearConv : public OpKernel {
   explicit QLinearConv(const OpKernelInfo& info) : OpKernel(info),
                                                    conv_attrs_(info) {
     channels_last_ = (info.GetAttrOrDefault<int64_t>("channels_last", static_cast<int64_t>(0)) != 0);
+    CreateOperator(info);
   }
 
   Status Compute(OpKernelContext* context) const override;
 
+  void CreateOperator(const OpKernelInfo& info) {
+    const Tensor* W = nullptr;
+    if (!info.TryGetConstantInput(3, &W)) return;
+    const auto& W_shape = W->Shape();
+
+    const int64_t M = W_shape[0];
+    const int64_t C = W_shape[1];
+
+    const Tensor* X_zero_point;
+    const Tensor* W_zero_point;
+    const Tensor* Y_zero_point;
+    if (!info.TryGetConstantInput(2, &X_zero_point)) return;
+    if (!info.TryGetConstantInput(5, &W_zero_point)) return;
+    if (!info.TryGetConstantInput(7, &Y_zero_point)) return;
+
+    auto X_zero_point_value = *(X_zero_point->template Data<uint8_t>());
+    auto W_zero_point_value = *(W_zero_point->template Data<uint8_t>());
+    auto Y_zero_point_value = *(Y_zero_point->template Data<uint8_t>());
+
+    const Tensor* X_scale;
+    const Tensor* W_scale;
+    const Tensor* Y_scale;
+    const Tensor* B;
+    if (!info.TryGetConstantInput(1, &X_scale)) return;
+    if (!info.TryGetConstantInput(4, &W_scale)) return;
+    if (!info.TryGetConstantInput(6, &Y_scale)) return;
+    if (!info.TryGetConstantInput(8, &B)) return;
+
+    auto X_scale_value = *(X_scale->template Data<float>());
+    auto W_scale_value = *(W_scale->template Data<float>());
+    auto Y_scale_value = *(Y_scale->template Data<float>());
+
+    std::vector<int64_t> kernel_shape;
+    ORT_ENFORCE(conv_attrs_.ComputeKernelShape(W_shape, kernel_shape).IsOK());
+
+    const size_t kernel_rank = kernel_shape.size();
+
+    std::vector<int64_t> pads(conv_attrs_.pads);
+    if (pads.empty()) {
+      pads.resize(kernel_rank * 2, 0);
+    }
+    std::vector<int64_t> dilations(conv_attrs_.dilations);
+    if (dilations.empty()) {
+      dilations.resize(kernel_rank, 1);
+    }
+    std::vector<int64_t> strides(conv_attrs_.strides);
+    if (strides.empty()) {
+      strides.resize(kernel_rank, 1);
+    }
+
+    int64_t group_count = conv_attrs_.group;
+    int64_t group_input_channels = C;
+    int64_t group_output_channels = M / group_count;
+
+    const auto* Wdata = W->Data<uint8_t>();
+    const auto* Bdata = B != nullptr ? B->template Data<int32_t>() : nullptr;
+
+    auto status = xnn_create_convolution2d_nhwc_qu8(
+        static_cast<uint32_t>(pads[0]),
+        static_cast<uint32_t>(pads[3]),
+        static_cast<uint32_t>(pads[2]),
+        static_cast<uint32_t>(pads[1]),
+        static_cast<uint32_t>(kernel_shape[0]),
+        static_cast<uint32_t>(kernel_shape[1]),
+        static_cast<uint32_t>(strides[0]),
+        static_cast<uint32_t>(strides[1]),
+        static_cast<uint32_t>(dilations[0]),
+        static_cast<uint32_t>(dilations[1]),
+        static_cast<uint32_t>(group_count),
+        static_cast<size_t>(group_input_channels),
+        static_cast<size_t>(group_output_channels),
+        static_cast<size_t>(C * group_count),
+        static_cast<size_t>(M),
+        X_zero_point_value, X_scale_value,
+        W_zero_point_value, W_scale_value,
+        Wdata, Bdata,
+        Y_zero_point_value, Y_scale_value,
+        0, 255,
+        0 /* flags */,
+        &conv_op_);
+    ORT_ENFORCE(status == xnn_status_success);
+  }
+
+ private:
   ConvAttributes conv_attrs_;
   bool channels_last_;
+  xnn_operator_t conv_op_{nullptr};
 };
 
 Status QLinearConv::Compute(OpKernelContext* context) const {
@@ -99,31 +185,33 @@ Status QLinearConv::Compute(OpKernelContext* context) const {
   const auto* Bdata = B != nullptr ? B->template Data<int32_t>() : nullptr;
   auto* Ydata = Y->MutableData<uint8_t>();
 
-  xnn_operator_t conv_op = nullptr;
-  auto status = xnn_create_convolution2d_nhwc_qu8(
-      static_cast<uint32_t>(pads[0]),
-      static_cast<uint32_t>(pads[3]),
-      static_cast<uint32_t>(pads[2]),
-      static_cast<uint32_t>(pads[1]),
-      static_cast<uint32_t>(kernel_shape[0]),
-      static_cast<uint32_t>(kernel_shape[1]),
-      static_cast<uint32_t>(strides[0]),
-      static_cast<uint32_t>(strides[1]),
-      static_cast<uint32_t>(dilations[0]),
-      static_cast<uint32_t>(dilations[1]),
-      static_cast<uint32_t>(group_count),
-      static_cast<size_t>(group_input_channels),
-      static_cast<size_t>(group_output_channels),
-      static_cast<size_t>(C * group_count),
-      static_cast<size_t>(M),
-      X_zero_point_value, X_scale_value,
-      W_zero_point_value, W_scale_value,
-      Wdata, Bdata,
-      Y_zero_point_value, Y_scale_value,
-      0, 255,
-      0 /* flags */,
-      &conv_op);
-  ORT_ENFORCE(status == xnn_status_success);
+  xnn_operator_t conv_op = conv_op_;
+  if (conv_op == nullptr) {
+    auto status = xnn_create_convolution2d_nhwc_qu8(
+        static_cast<uint32_t>(pads[0]),
+        static_cast<uint32_t>(pads[3]),
+        static_cast<uint32_t>(pads[2]),
+        static_cast<uint32_t>(pads[1]),
+        static_cast<uint32_t>(kernel_shape[0]),
+        static_cast<uint32_t>(kernel_shape[1]),
+        static_cast<uint32_t>(strides[0]),
+        static_cast<uint32_t>(strides[1]),
+        static_cast<uint32_t>(dilations[0]),
+        static_cast<uint32_t>(dilations[1]),
+        static_cast<uint32_t>(group_count),
+        static_cast<size_t>(group_input_channels),
+        static_cast<size_t>(group_output_channels),
+        static_cast<size_t>(C * group_count),
+        static_cast<size_t>(M),
+        X_zero_point_value, X_scale_value,
+        W_zero_point_value, W_scale_value,
+        Wdata, Bdata,
+        Y_zero_point_value, Y_scale_value,
+        0, 255,
+        0 /* flags */,
+        &conv_op);
+    ORT_ENFORCE(status == xnn_status_success);
+  }
 
   ORT_ENFORCE(xnn_status_success == xnn_setup_convolution2d_nhwc_qu8(conv_op, N, input_shape[0], input_shape[1], Xdata, Ydata, nullptr));
   ORT_ENFORCE(xnn_status_success == xnn_run_operator(conv_op, nullptr));
