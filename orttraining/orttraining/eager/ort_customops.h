91a3619e2681b933e1aa2ad356278192ad55810d
Ashwin Hari <ashari@microsoft.com>
2021-08-20 16:50:36 -0700

    Revert "Renamed module_ to module."
    This reverts commit b17fc114b3db20d174283811d90592b5b8154c19.

--- orttraining/orttraining/eager/ort_customops.h
+++ orttraining/orttraining/eager/ort_customops.h
@@ -3,7 +3,7 @@
 namespace torch_ort {
 namespace eager {
 
-void GenerateCustomOpsBindings(pybind11::module module);
+void GenerateCustomOpsBindings(pybind11::module_ module);
 
 } // namespace eager
 } // namespace torch_ort
