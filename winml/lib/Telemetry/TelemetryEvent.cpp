c4813193d07df99c3281ccffd71e63d80fd9b68e
Tiago Koji Castro Shibata <ticastro@microsoft.com>
2021-08-23 11:19:03 -0700

    Fix Windows Store build (#8753)
    * Remove APIs unavailable in Store in #8349, #8178, #8065
    
    * Add UWP stubs of C runtime functions
    
    * Remove UWP incompatible tests from UWP build
    
    * Remove incompatible tests from Store
    
    * Use UWP stubs in store only
    
    * Skip partition check outside of Windows
    
    * Remove unused WRL include
    
    * Workaround Windows header not including what it uses
    
    * Fix precompiled header name clash
    
    * Workaround SDK bugs
    
    * DXCore workaround in Win7
    
    * Fix warning
    
    * Fix more warnings
    
    * Bump WinML to target Windows 8
    
    * Fix more warnings
    
    * Remove unnecessary workarounds
    
    * Remove Desktop only APIs from DML adapter

--- winml/lib/Telemetry/TelemetryEvent.cpp
+++ winml/lib/Telemetry/TelemetryEvent.cpp
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-#include "pch.h"
+#include "lib/Telemetry/pch.h"
 
 #include "inc/TelemetryEvent.h"
 
