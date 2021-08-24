05630cfdc91d6f5bb4828bbd5763d70a43ed6cc6
Tianlei Wu <tlwu@microsoft.com>
2021-08-24 16:14:02 -0700

    update option to disable half2

--- onnxruntime/contrib_ops/cuda/bert/transformer_common.cc
+++ onnxruntime/contrib_ops/cuda/bert/transformer_common.cc
@@ -25,7 +25,7 @@ const TransformerOptions* TransformerOptions::GetInstance() {
     if (value > 0)
       std::cout << "ORT_TRANSFORMER_OPTIONS: IsPrecisionMode=" << instance.IsPrecisionMode()
                 << ",DisablePersistentSoftmax=" << instance.DisablePersistentSoftmax()
-                << ",UseHalf2=" << instance.UseHalf2()
+                << ",DisableHalf2=" << instance.DisableHalf2()
                 << std::endl;
   }
 
