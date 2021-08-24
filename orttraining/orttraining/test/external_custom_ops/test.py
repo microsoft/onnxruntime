819e4c69882466704bf0a246df2addf1233f26bd
Satya Jandhyala <satya.k.jandhyala@gmail.com>
2021-08-24 17:40:49 +0000

    Changed static function registerOps to non-static and added function to the module.

--- orttraining/orttraining/test/external_custom_ops/test.py
+++ orttraining/orttraining/test/external_custom_ops/test.py
@@ -1,4 +1,6 @@
 import onnxruntime
 import orttraining_external_custom_ops
+
+orttraining_external_custom_ops.register_custom_ops()
 so = onnxruntime.SessionOptions()
 sess = onnxruntime.InferenceSession("testdata/model.onnx", so)
