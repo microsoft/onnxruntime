2228f8bb192b953bb10ece735eeb7a5466a073f8
Chi Lo <Chi.Lo@microsoft.com>
2021-08-24 00:04:11 -0700

    Modify for test

---
+++ tools/ci_build/github/linux/extract_and_bundle_gpu_package.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e -o -x
+
+while getopts a: parameter_Option
+do case "${parameter_Option}"
+in
+a) ARTIFACT_DIR=${OPTARG};;
+esac
+done
+
+EXIT_CODE=1
+
+uname -a
+
+mkdir -p onnxruntime-linux-x64-gpu-tensorrt
+tar zxvf $ARTIFACT_DIR/onnxruntime-linux-x64-gpu-tensorrt-*.tgz -C onnxruntime-linux-x64-gpu-tensorrt
+rm $ARTIFACT_DIR/onnxruntime-linux-x64-gpu-tensorrt-*.tgz
+
+mkdir -p onnxruntime-linux-x64-gpu
+TAR_NAME=`ls $ARTIFACT_DIR/onnxruntime-linux-x64-gpu-*.tgz`
+tar zxvf $ARTIFACT_DIR/onnxruntime-linux-x64-gpu-*.tgz -C onnxruntime-linux-x64-gpu
+rm $ARTIFACT_DIR/onnxruntime-linux-x64-gpu-*.tgz
+
+cp onnxruntime-linux-x64-gpu-tensorrt/*/lib/libonnxruntime.so onnxruntime-linux-x64-gpu/*/lib
+cp onnxruntime-linux-x64-gpu-tensorrt/*/lib/libonnxruntime_providers_tensorrt.so onnxruntime-linux-x64-gpu/*/lib
+cp onnxruntime-linux-x64-gpu-tensorrt/*/lib/libonnxruntime_providers_shared.so onnxruntime-linux-x64-gpu/*/lib
+cp onnxruntime-linux-x64-gpu-tensorrt/*/lib/libonnxruntime.so.1.8.2 onnxruntime-linux-x64-gpu/*/lib
