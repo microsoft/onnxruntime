0cfbc07220ff9939a4fdc67b196c3d321d17516e
Satya Jandhyala <satya.k.jandhyala@gmail.com>
2021-08-25 23:36:06 +0000

    Revert "Changed static function registerOps to non-static and added function to the module."
    This reverts commit 819e4c69882466704bf0a246df2addf1233f26bd.

--- orttraining/orttraining/test/external_custom_ops/test.py
+++ orttraining/orttraining/test/external_custom_ops/test.py
@@ -1,6 +1,4 @@
 import onnxruntime
 import orttraining_external_custom_ops
-
-orttraining_external_custom_ops.register_custom_ops()
 so = onnxruntime.SessionOptions()
 sess = onnxruntime.InferenceSession("testdata/model.onnx", so)
