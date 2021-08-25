f3c61cad0c53d260cbac11ebaa212ed8eecd270e
Olivia Jain <oljain@microsoft.com>
2021-08-24 23:35:52 -0700

    no onnxruntime extensions

--- onnxruntime/python/tools/tensorrt/perf/benchmark.py
+++ onnxruntime/python/tools/tensorrt/perf/benchmark.py
@@ -1040,8 +1040,8 @@ def run_onnxruntime(args, models):
                 options = onnxruntime.SessionOptions()
                 options.graph_optimization_level = onnxruntime.GraphOptimizationLevel.ORT_ENABLE_ALL
                 
-                from onnxruntime_extensions import get_library_path as _lib_path
-                options.register_custom_ops_library(_lib_path())
+                #from onnxruntime_extensions import get_library_path as _lib_path
+                #options.register_custom_ops_library(_lib_path())
                 
                 # create onnxruntime inference session
                 try:
