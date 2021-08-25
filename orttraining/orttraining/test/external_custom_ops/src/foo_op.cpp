de6ec9fc434fbd1974d4a3a2b87c2ec33a3f4a3e
Satya Jandhyala <satya.k.jandhyala@gmail.com>
2021-08-25 13:36:35 +0000

    Make schema a local variable.

--- orttraining/orttraining/test/external_custom_ops/src/foo_op.cpp
+++ orttraining/orttraining/test/external_custom_ops/src/foo_op.cpp
@@ -3,46 +3,8 @@
 #include <onnx/defs/function.h>
 #include <onnx/defs/shape_inference.h>
 #include <pybind11/pybind11.h>
-namespace ONNX_NAMESPACE {
-static const char FooDoc[] = "Foo copies input tensor to the output tensor.";
-ONNX_OPERATOR_SET_SCHEMA_EX(
-    Foo,
-    comExamples,
-    "com.examples",
-    1,
-    false,
-    OpSchema()
-        .SetDoc(FooDoc)
-        .Input(0, "X", "Input tensor", "T")
-        .Output(0, "Y", "Output tensor", "T")
-        .TypeConstraint(
-            "T",
-            {"tensor(float)", "tensor(int32)", "tensor(float16)"},
-            "Constrain input and output types to signed numeric tensors.")
-        .TypeAndShapeInferenceFunction([](InferenceContext& ctx) {
-          propagateShapeAndTypeFromFirstInput(ctx);
-        })
-        .SetContextDependentFunctionBodyBuilder(
-            [](const FunctionBodyBuildContext& ctx,
-               const OpSchema& schema,
-               FunctionProto& functionProto) -> bool {
-              if (ctx.getInputType(0) == nullptr) {
-                // we cannot create a correct function body without knowing the input type
-                return false;
-              }
-              std::vector<ONNX_NAMESPACE::FunctionBodyHelper::NodeDef> body{
-                                                {{"Y"}, "Identity", { "X"}}
-                                                };
-              auto func_nodes = ONNX_NAMESPACE::FunctionBodyHelper::BuildNodes(body);
-              for (const auto& node : func_nodes) {
-                auto new_node = functionProto.add_node();
-                new_node->CopyFrom(node);
-              }
-              schema.BuildFunction(functionProto);
-              return true;
-            }));
 
-} // namespace ONNX_NAMESPACE
+static const char FooDoc[] = "Foo copies input tensor to the output tensor.";
 
 using namespace ONNX_NAMESPACE;
 bool registerOps() {
@@ -50,8 +12,39 @@ bool registerOps() {
   auto &d = OpSchemaRegistry::DomainToVersionRange::Instance();
   std::cout << "AddDomainToVersion" << std::endl;
   d.AddDomainToVersion("com.examples", 1, 1);
-  std::cout << "GetOpSchema" << std::endl;
-  auto schema = GetOpSchema<ONNX_OPERATOR_SET_SCHEMA_CLASS_NAME(comExamples, 1, Foo)>();
+  OpSchema schema;
+  schema.SetName("Foo")
+    .SetDomain("com.examples")
+    .SinceVersion(1)
+    .SetDoc(FooDoc)
+    .Input(0, "X", "Input tensor", "T")
+    .Output(0, "Y", "Output tensor", "T")
+    .TypeConstraint(
+        "T",
+        {"tensor(float)", "tensor(int32)", "tensor(float16)"},
+        "Constrain input and output types to signed numeric tensors.")
+    .TypeAndShapeInferenceFunction([](InferenceContext& ctx) {
+      propagateShapeAndTypeFromFirstInput(ctx);
+    })
+    .SetContextDependentFunctionBodyBuilder(
+        [](const FunctionBodyBuildContext& ctx,
+            const OpSchema& schema,
+            FunctionProto& functionProto) -> bool {
+          if (ctx.getInputType(0) == nullptr) {
+            // we cannot create a correct function body without knowing the input type
+            return false;
+          }
+          std::vector<ONNX_NAMESPACE::FunctionBodyHelper::NodeDef> body{
+                                            {{"Y"}, "Identity", { "X"}}
+                                            };
+          auto func_nodes = ONNX_NAMESPACE::FunctionBodyHelper::BuildNodes(body);
+          for (const auto& node : func_nodes) {
+            auto new_node = functionProto.add_node();
+            new_node->CopyFrom(node);
+          }
+          schema.BuildFunction(functionProto);
+          return true;
+        });
   std::cout << "RegisterSchema" << std::endl;
   RegisterSchema(schema);
   std::cout << "Successfully registered custom op" << std::endl;
