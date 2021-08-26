6967389fd171f3445ba03d887b4978690f36964a
ajindal1 <abjindal@microsoft.com>
2021-08-26 21:34:17 +0000

    changing docker file

--- tools/ci_build/github/linux/docker/scripts/install_python_deps_eager.sh
+++ tools/ci_build/github/linux/docker/scripts/install_python_deps_eager.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#set -e -x
+set -e -x
 
-# PYTHON_EXE="/usr/bin/python3"
+PYTHON_EXE="/usr/bin/python3"
 
-# export ONNX_ML=1
-# export CMAKE_ARGS="-DONNX_GEN_PB_TYPE_STUBS=OFF -DONNX_WERROR=OFF"
+export ONNX_ML=1
+export CMAKE_ARGS="-DONNX_GEN_PB_TYPE_STUBS=OFF -DONNX_WERROR=OFF"
 /usr/bin/python3 -m pip install -r ${0/%install_python_deps\.sh/requirements\.txt}
 /usr/bin/python3 -m pip install -r ${0/%install_python_deps.sh/training\/ortmodule\/stage1\/requirements_torch_eager_cpu.txt}
