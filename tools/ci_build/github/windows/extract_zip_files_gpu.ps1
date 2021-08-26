54edc96103b6bb3b9ca3479e31f6ff38fd494d5d
Chi Lo <Chi.Lo@microsoft.com>
2021-08-25 21:12:06 -0700

    modify for test

--- tools/ci_build/github/windows/extract_zip_files_gpu.ps1
+++ tools/ci_build/github/windows/extract_zip_files_gpu.ps1
@@ -1,6 +1,7 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
+# extract *-cuda.zip and *-tensorrt.zip
 Get-ChildItem $Env:BUILD_BINARIESDIRECTORY\zip-artifacts -Filter *.zip | 
 Foreach-Object {
  $cmd = "7z.exe x $($_.FullName) -y -o$Env:BUILD_BINARIESDIRECTORY\zip-artifacts"
@@ -8,13 +9,16 @@ Foreach-Object {
  Invoke-Expression -Command $cmd
 }
 
-Get-ChildItem $Env:BUILD_BINARIESDIRECTORY\zip-artifacts -Filter *onnxruntime-win-x64-cuda*.zip |
-Foreach-Object {
- $($_.FullName) -match '.*onnxruntime-win-x64-cuda-(.*).zip'
- $version=$matches[1]
- Rename-Item -Path $($_.FullName) -NewName onnxruntime-win-x64-gpu-$version.zip
-}
-
-Get-ChildItem $Env:BUILD_BINARIESDIRECTORY\zip-artifacts | Where-Object { $_.Name -match 'onnxruntime-win-x64-cuda-\d{1,}\.\d{1,}\.\d{1,}$' } | Rename-Item -NewName $Env:BUILD_BINARIESDIRECTORY\zip-artifacts\onnxruntime-win-x64-gpu
-
 Get-ChildItem $Env:BUILD_BINARIESDIRECTORY\zip-artifacts | Where-Object { $_.Name -match 'onnxruntime-win-x64-tensorrt-\d{1,}\.\d{1,}\.\d{1,}$' } | Rename-Item -NewName $Env:BUILD_BINARIESDIRECTORY\zip-artifacts\onnxruntime-win-x64-tensorrt
+Remove-Item *.zip
+
+# Rename cuda directory to gpu directory and compress it for later use
+Get-ChildItem $Env:BUILD_BINARIESDIRECTORY\zip-artifacts -Filter *cuda* |
+Foreach-Object {
+ $($_.FullName) -match '.*onnxruntime-win-x64-cuda-(.*)'
+ $version=$matches[1]
+ Rename-Item -Path $($_.FullName) -NewName onnxruntime-win-x64-gpu-$version
+ $cmd = "7z.exe a onnxruntime-win-x64-gpu-$version -y -o$Env:BUILD_BINARIESDIRECTORY\zip-artifacts"
+ Write-Output $cmd
+ Invoke-Expression -Command $cmd
+}
