82c5238c6204ae7f1db9bcf8b5775b39cb4f7338
Satya Jandhyala <satya.k.jandhyala@gmail.com>
2021-08-26 20:09:25 +0000

    Run onnxruntime inference session using custom op

--- orttraining/orttraining/test/external_custom_ops/test.py
+++ orttraining/orttraining/test/external_custom_ops/test.py
@@ -6,3 +6,6 @@ sys.setdlopenflags(os.RTLD_LOCAL|os.RTLD_NOW|os.RTLD_DEEPBIND)
 import orttraining_external_custom_ops
 so = onnxruntime.SessionOptions()
 sess = onnxruntime.InferenceSession("testdata/model.onnx", so)
+input = np.random.rand(2, 2).astype(np.float32)
+output  = sess.run(None, {"input1" : input})[0]
+np.testing.assert_equal(input, output)
