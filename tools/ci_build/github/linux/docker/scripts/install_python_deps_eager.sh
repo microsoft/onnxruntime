859dc1ec7903120b183d30b3c94c56ad2d027adf
ajindal1 <abjindal@microsoft.com>
2021-08-26 22:39:25 +0000

    changing paths

--- tools/ci_build/github/linux/docker/scripts/install_python_deps_eager.sh
+++ tools/ci_build/github/linux/docker/scripts/install_python_deps_eager.sh
@@ -48,5 +48,5 @@ fi
 
 export ONNX_ML=1
 export CMAKE_ARGS="-DONNX_GEN_PB_TYPE_STUBS=OFF -DONNX_WERROR=OFF"
-${PYTHON_EXE} -m pip install -r ${0/%install_python_deps\.sh/requirements\.txt}
-${PYTHON_EXE} -m pip install -r ${0/%install_python_deps.sh/training\/ortmodule\/stage1\/requirements_torch_cpu.txt}
+${PYTHON_EXE} -m pip install -r ${0/%install_python_deps_eager\.sh/requirements\.txt}
+${PYTHON_EXE} -m pip install -r ${0/%install_python_deps_eager.sh/training\/ortmodule\/stage1\/requirements_torch_cpu.txt}
