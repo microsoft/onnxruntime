606d2af23aefc5e6f5a745bfb3eb2f0139689e4b
Scott McKay <skottmckay@gmail.com>
2021-08-26 13:55:04 +1000

    Add doco for new C# class Update GPU build to use msbuild for C# project

--- csharp/src/Microsoft.ML.OnnxRuntime/PreserveAttribute.shared.cs
+++ csharp/src/Microsoft.ML.OnnxRuntime/PreserveAttribute.shared.cs
@@ -3,13 +3,29 @@ using System.ComponentModel;
 
 namespace Microsoft.ML.OnnxRuntime
 {
+	/// <summary>
+	/// Preserve attribute to prevent the MonoTouch linker from linking the target.
+	/// <see cref="https://docs.microsoft.com/en-us/dotnet/api/foundation.preserveattribute?view=xamarin-ios-sdk-12"/>
+	/// </summary>
 	[AttributeUsage(AttributeTargets.All)]
 	[EditorBrowsable(EditorBrowsableState.Never)]
 	public sealed class PreserveAttribute : Attribute
 	{
+		/// <summary>
+		/// Ensures that all members of this type are preserved.
+		/// </summary>
 		public bool AllMembers;
+
+		/// <summary>
+		/// Flags the method as a method to preserve during linking if the container class is pulled in.
+		/// </summary>
 		public bool Conditional;
 
+		/// <summary>
+		/// Instruct the MonoTouch linker to preserve the decorated code
+		/// </summary>
+		/// <param name="allMembers">Ensures that all members of this type are preserved.</param>
+		/// <param name="conditional">Flags the method as a method to preserve during linking if the container class is pulled in.</param>
 		public PreserveAttribute(bool allMembers, bool conditional)
 		{
 			AllMembers = allMembers;
