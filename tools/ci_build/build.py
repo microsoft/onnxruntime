e02c339786e49a262ea024c67023710a2d4acec5
Satya Jandhyala <satya.k.jandhyala@gmail.com>
2021-08-29 21:16:01 +0000

    External custom ops is available on Linux

--- tools/ci_build/build.py
+++ tools/ci_build/build.py
@@ -588,11 +588,6 @@ def is_ubuntu_1604():
     return dist == 'Ubuntu' and ver.startswith('16.04')
 
 
-def is_ubuntu():
-    dist, _ = get_linux_distro()
-    return dist == 'Ubuntu'
-
-
 def get_config_build_dir(build_dir, config):
     # build directory per configuration
     return os.path.join(build_dir, config)
@@ -2215,8 +2210,8 @@ def main():
                         args.eager_customop_header, args.eager_customop_module, True)
 
             gen_ort_ops()
-        if args.enable_external_custom_op_schemas and not is_ubuntu():
-            raise BuildError("Registering external custom op schemas is only supported on Ubuntu.")
+        if args.enable_external_custom_op_schemas and not is_linux():
+            raise BuildError("Registering external custom op schemas is only supported on Linux.")
 
         generate_build_tree(
             cmake_path, source_dir, build_dir, cuda_home, cudnn_home, rocm_home, mpi_home, nccl_home,
