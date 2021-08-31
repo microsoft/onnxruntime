bedb53634de2106f662f33325a7ba7b9a1ea6f01
suffian khan <sukha@microsoft.com>
2021-08-31 23:20:03 +0000

    add more models

--- orttraining/tools/ci_test/compare_huggingface.py
+++ orttraining/tools/ci_test/compare_huggingface.py
@@ -12,13 +12,12 @@ with open(expect) as file_expect:
 
 # loss curve match
 def almost_equal(x, y, threshold=0.0001):
-  print(abs(x-y))
   return abs(x-y) < threshold
 
 for i in range(len(json_actual['steps'])): 
   step_actual = json_actual['steps'][i]
   step_expect = json_expect['steps'][i]
-  print(step_actual['step'])
+  print('step {} loss actual {:.6f} expected {:.6f}'.format(step_actual['step'], step_actual['loss'], step_expect['loss']))
   assert(step_actual['step'] == step_expect['step'])
   assert(almost_equal(step_actual['loss'], step_expect['loss']))
 
