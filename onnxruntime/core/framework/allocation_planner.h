acd9db7fadb8da618c77aef31a4df626448343e9
Hariharan Seshadri <shariharan91@gmail.com>
2021-09-01 00:02:08 -0700

    Fix location planning for initializers used only in nested subgraphs (#8642)

--- onnxruntime/core/framework/allocation_planner.h
+++ onnxruntime/core/framework/allocation_planner.h
@@ -15,10 +15,22 @@ class TensorShapeProto;
 }
 namespace onnxruntime {
 
+namespace NestedSubgraphInfoDetails {
+
+// Used to compose a unique key to identify a nested subgraph
+// relative to a current graph level (which in turn is identified using a "base")
+std::string ComposeNestedSubgraphInfoKeyHelper(const std::string& base, size_t graph_depth,
+                                               NodeIndex node_index, const std::string& attr_name);
+
+}  // namespace NestedSubgraphInfoDetails
+
 class ExecutionProviders;
 struct KernelCreateInfo;
 class KernelRegistryManager;
 class OrtValueNameIdxMap;
+
+using KernelCreateInfoMap = std::unordered_map<onnxruntime::NodeIndex, gsl::not_null<const KernelCreateInfo*>>;
+using SubgraphsKernelCreateInfoMaps = std::unordered_map<std::string, KernelCreateInfoMap>;
 
 // ISequentialPlannerContext abstracts how the planner accesses information (such as inferred shape)
 // to do the planning.
@@ -65,7 +77,8 @@ class SequentialPlanner {
       const Node* parent_node, const onnxruntime::GraphViewer& graph,
       const std::vector<const NodeArg*>& outer_scope_node_args,
       const ExecutionProviders& providers,
-      const std::unordered_map<NodeIndex, gsl::not_null<const KernelCreateInfo*>>& kernel_create_info_map,
+      const KernelCreateInfoMap& kernel_create_info_map,
+      const SubgraphsKernelCreateInfoMaps& subgraphs_kernel_create_info_maps,
       const std::unordered_map<OrtValueName, OrtMemoryInfo>& outer_scope_arg_to_location_map,
       const OrtValueNameIdxMap& ort_value_name_idx_map,
       const ISequentialPlannerContext& context,
