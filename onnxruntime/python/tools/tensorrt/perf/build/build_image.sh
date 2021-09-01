6586999553c1ace9087486696f6d010949b91c44
Olivia Jain <oljain@microsoft.com>
2021-09-01 10:43:45 -0700

    Passing in cuda architecture to reduce build time

--- onnxruntime/python/tools/tensorrt/perf/build/build_image.sh
+++ onnxruntime/python/tools/tensorrt/perf/build/build_image.sh
@@ -3,15 +3,16 @@
 while getopts o:p:b:i:t: parameter
 do case "${parameter}"
 in 
-o) ORT_TRT_DOCKERFILE_PATH=${OPTARG};;
+o) TRT_DOCKERFILE_PATH=${OPTARG};;
 p) PERF_DOCKERFILE_PATH=${OPTARG};;
 b) ORT_BRANCH=${OPTARG};;
 i) IMAGE_NAME=${OPTARG};;
-t) TRT=${OPTARG};;
+t) TRT_VERSION=${OPTARG};;
 esac
 done 
 
 IMAGE=onnxruntime
+CMAKE_CUDA_ARCHITECTURES=75 
 
-docker build --no-cache -t $IMAGE --build-arg TRT=$TRT --build-arg ONNXRUNTIME_BRANCH=$ORT_BRANCH -f $ORT_TRT_DOCKERFILE_PATH . 
-docker build --no-cache --build-arg IMAGE=$IMAGE --build-arg ONNXRUNTIME_BRANCH=$ORT_BRANCH --build-arg TRT_VERSION=$TRT -t $IMAGE_NAME -f $PERF_DOCKERFILE_PATH .
+docker build --no-cache -t $IMAGE --build-arg CMAKE_CUDA_ARCHITECTURES=$CMAKE_CUDA_ARCHITECTURES --build-arg TRT_VERSION=$TRT_VERSION --build-arg ONNXRUNTIME_BRANCH=$ORT_BRANCH -f $TRT_DOCKERFILE_PATH . 
+docker build --no-cache --build-arg IMAGE=$IMAGE --build-arg CMAKE_CUDA_ARCHITECTURES=$CMAKE_CUDA_ARCHITECTURES --build-arg ONNXRUNTIME_BRANCH=$ORT_BRANCH --build-arg TRT_VERSION=$TRT_VERSION -t $IMAGE_NAME -f $PERF_DOCKERFILE_PATH .
