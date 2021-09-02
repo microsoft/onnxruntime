02b7290a54cb04e495ac831391c4582ae0b8e927
satyajandhyala <satya.k.jandhyala@gmail.com>
2021-09-02 09:54:32 -0700

    Move setdlopenflags calls into _pybind_state.py (#8916)
    * Use PROTOBUF_LIB instead of protobuf::libprotbuf
    
    * Moved setdlopenflags to _pybind_state.py
    
    * Copy the generated _pybind_state.py to required location for Windows.

--- orttraining/orttraining/test/external_custom_ops/test.py
+++ orttraining/orttraining/test/external_custom_ops/test.py
@@ -8,10 +8,8 @@ import numpy as np
 # Expose available (onnx::* and protobuf::*) symbols from onnxruntime to resolve references in 
 # the custom ops shared library. Deepbind flag is required to avoid conflicts with other
 # instances of onnx/protobuf libraries.
-sys.setdlopenflags(os.RTLD_GLOBAL|os.RTLD_NOW|os.RTLD_DEEPBIND)
 import onnxruntime
 # Restore dlopen flags.
-sys.setdlopenflags(os.RTLD_LOCAL|os.RTLD_NOW|os.RTLD_DEEPBIND)
 import orttraining_external_custom_ops
 so = onnxruntime.SessionOptions()
 sess = onnxruntime.InferenceSession("testdata/model.onnx", so)
