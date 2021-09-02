9f5978c2ceeea6f8c2ccba079cab320dfe3de2be
Arseny Yanchenko <a.yanchenko@expasoft.tech>
2021-09-02 11:13:46 +0700

    fix: import error in TrtTable::Dict method

--- onnxruntime/python/tools/quantization/CalTableFlatBuffers/TrtTable.py
+++ onnxruntime/python/tools/quantization/CalTableFlatBuffers/TrtTable.py
@@ -28,7 +28,7 @@ class TrtTable(object):
             x = self._tab.Vector(o)
             x += flatbuffers.number_types.UOffsetTFlags.py_type(j) * 4
             x = self._tab.Indirect(x)
-            from CalTableFlatBuffers.KeyValue import KeyValue
+            from onnxruntime.quantization.CalTableFlatBuffers.KeyValue import KeyValue
             obj = KeyValue()
             obj.Init(self._tab.Bytes, x)
             return obj
