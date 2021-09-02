d2cd63773d200b2f0c5eb56268be0cdf7d89e9e6
Edward Chen <18449977+edgchen1@users.noreply.github.com>
2021-09-01 18:06:28 -0700

    Add CMake logic.

--- include/onnxruntime/core/common/make_string.h
+++ include/onnxruntime/core/common/make_string.h
@@ -28,7 +28,9 @@ namespace detail {
 inline void MakeStringImpl(std::ostringstream& /*ss*/) noexcept {
 }
 
-#if 0
+// we get some binary size savings with the C++17 implementation
+#if defined(ORT_USE_PRE_CXX17_IMPL)
+
 template <typename T>
 inline void MakeStringImpl(std::ostringstream& ss, const T& t) noexcept {
   ss << t;
@@ -39,12 +41,15 @@ inline void MakeStringImpl(std::ostringstream& ss, const T& t, const Args&... ar
   MakeStringImpl(ss, t);
   MakeStringImpl(ss, args...);
 }
+
 #else
+
 template <typename... Args>
 inline void MakeStringImpl(std::ostringstream& ss, const Args&... args) noexcept {
   (ss << ... << args);
 }
-#endif
+
+#endif  // defined(ORT_USE_PRE_CXX17_IMPL)
 
 // see MakeString comments for explanation of why this is necessary
 template <typename... Args>
