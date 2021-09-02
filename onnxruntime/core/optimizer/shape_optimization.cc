77f37fc62758179fe09e8d8ec5f30223a964d75f
aishwarya bhandare <aibhanda@microsoft.com>
2021-09-02 21:38:42 +0000

    Cast-Shape optimization

---
+++ onnxruntime/core/optimizer/shape_optimization.cc
@@ -0,0 +1,78 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+#include "core/optimizer/shape_optimization.h"
+#include "core/graph/graph.h"
+#include "core/graph/graph_utils.h"
+#include "core/graph/op.h"
+#include "core/optimizer/initializer.h"
+
+namespace onnxruntime {
+
+Status ShapeOptimization::Apply(Graph& graph, Node& shape_node, RewriteRuleEffect& rule_effect, const logging::Logger&) const {
+  Node& previous_node = *graph.GetNode(shape_node.InputNodesBegin()->Index());
+
+  auto op_type = previous_node.OpType();
+  if (op_type == "Cast") {
+    Node& cast_node = previous_node;
+
+    const Node* cast_input_node = graph_utils::GetInputNode(cast_node, 0);
+    NodeArg* new_input_arg_for_shape = cast_node.MutableInputDefs()[0];
+
+    int output_idx = -1;
+    if (cast_input_node) {
+      output_idx = graph_utils::GetNodeOutputIndexFromOutputName(*cast_input_node, new_input_arg_for_shape->Name());
+      graph.RemoveEdge(cast_input_node->Index(), cast_node.Index(), output_idx, 0);
+    }
+    graph.RemoveEdge(cast_node.Index(), shape_node.Index(), 0, 0);
+    graph_utils::ReplaceNodeInput(shape_node, 0, *new_input_arg_for_shape);
+
+    if (cast_input_node) {
+      graph.AddEdge(cast_input_node->Index(), shape_node.Index(), output_idx, 0);
+    }
+    // remove cast_node
+    graph.RemoveNode(cast_node.Index());
+
+    rule_effect = RewriteRuleEffect::kUpdatedCurrentNode;
+
+  } else if (op_type == "Transpose") {
+    rule_effect = RewriteRuleEffect::kNone;
+  } else {
+    rule_effect = RewriteRuleEffect::kNone;
+  }
+
+  return Status::OK();
+}
+
+bool ShapeOptimization::SatisfyCondition(const Graph& graph, const Node& node, const logging::Logger& logger) const {
+  // TODO: Support Opset-15 Shape. There needs to be some additional consideration for
+  // modifying the 'perm' with added attributes of shape 'start' and 'end'.
+  if (!graph_utils::IsSupportedOptypeVersionAndDomain(node, "Shape", {1, 13})) {
+    return false;
+  }
+
+  // There has to be an input edge as we are going to look at the previous node.
+  if (node.GetInputEdgesCount() != 1) {
+    return false;
+  }
+
+  // The previous nodes should be one of the implemented types,
+  // and shouldn't have more than one Consumer.
+  // In case of Transpose, where both the nodes are kept and just the node order is swapped,
+  // the execution providers must match.
+  const auto& previous_node = *node.InputNodesBegin();
+  if (!(graph_utils::IsSupportedOptypeVersionAndDomain(previous_node, "Cast", {1, 6, 9, 13}) ||
+        graph_utils::IsSupportedOptypeVersionAndDomain(previous_node, "Transpose", {1, 13})) ||
+      previous_node.GetOutputEdgesCount() > 1 ||
+      previous_node.GetExecutionProviderType() != node.GetExecutionProviderType()) {
+    return false;
+  }
+
+  if (!graph_utils::CanRemoveNode(graph, previous_node, logger)) {
+    return false;
+  }
+
+  return true;
+}
+
+}  // namespace onnxruntime
---
+++ onnxruntime/core/optimizer/shape_optimization.h
@@ -0,0 +1,47 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+#pragma once
+
+#include "core/optimizer/rewrite_rule.h"
+
+namespace onnxruntime {
+
+/**
+@Class ShapeOptimization
+
+Rewrite rule that removes redundant compute preceding a Shape op.
+For example, :
+1)
+Cast -> Shape
+Here, if the Cast has no other consumers, it is Redundant.
+
+2)
+Transpose -> Shape
+If the Transpose has no other consumers, we can transpose the shape instead of 
+transposing the larger-sized activation.
+Shape -> Transpose
+
+Similar patterns can be added to this Rewrite Rule as required.
+*/
+class ShapeOptimization : public RewriteRule {
+ public:
+  ShapeOptimization() noexcept : RewriteRule("ShapeOptimization") {}
+
+  std::vector<std::string> TargetOpTypes() const noexcept override {
+    return {"Shape"};
+  }
+
+  // To support additional op patterns, the new op must be added to this list.
+  std::vector<std::string> PreviousOpTypes() const noexcept {
+    return {"Transpose", "Cast"};
+  }
+
+
+ private:
+  bool SatisfyCondition(const Graph& graph, const Node& node, const logging::Logger& logger) const override;
+
+  Status Apply(Graph& graph, Node& node, RewriteRuleEffect& rule_effect, const logging::Logger& logger) const override;
+};
+
+}  // namespace onnxruntime
