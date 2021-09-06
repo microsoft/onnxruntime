135ed977d5935e1f907215b21da94da0c4d8aa05
Zuwei Zhao <zuzhao@microsoft.com>
2021-09-06 18:30:19 +0800

    Enable linking in exception throwing support library when build onnxruntime webassembly containing onnxruntime-extensions.

--- cmake/onnxruntime_webassembly.cmake
+++ cmake/onnxruntime_webassembly.cmake
@@ -61,6 +61,12 @@ else()
   set_property(TARGET onnxruntime_webassembly APPEND_STRING PROPERTY LINK_FLAGS " -s ASSERTIONS=0 -s SAFE_HEAP=0 -s STACK_OVERFLOW_CHECK=0 -s DEMANGLE_SUPPORT=0")
 endif()
 
+# Set link flag for onnxruntime-extensions because now it contains submodules using exceptions
+# Would remove this link flag when all submodules have supported disabling c++ exceptions
+if (onnxruntime_USE_EXTENSIONS)
+  set_property(TARGET onnxruntime_webassembly APPEND_STRING PROPERTY LINK_FLAGS " -s DISABLE_EXCEPTION_THROWING=0")
+endif()
+
 if (onnxruntime_ENABLE_WEBASSEMBLY_THREADS)
   if (onnxruntime_ENABLE_WEBASSEMBLY_SIMD)
     set_property(TARGET onnxruntime_webassembly APPEND_STRING PROPERTY LINK_FLAGS " -s EXPORT_NAME=ortWasmSimdThreaded -s USE_PTHREADS=1")
