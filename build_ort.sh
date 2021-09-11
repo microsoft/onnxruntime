0ec7dade96269c8aed231656b01b72aa23c773fa
zhijxu <zhijxu@microsoft.com>
2021-09-11 10:14:45 +0000

    add script to build ort with amd ep

---
+++ build_ort.sh
@@ -0,0 +1,1 @@
+bash build.sh --config Release --enable_training --build_wheel --use_rocm --rocm_home /opt/rocm-4.2.0 --nccl_home /opt/rocm-4.2.0 --mpi_home /opt/ompi --update --build --parallel --skip_tests
