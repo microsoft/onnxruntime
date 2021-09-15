35d9ef29ea1d8f8faf4e170c5bb12ef91636d5dc
Olivia Jain <oljain@microsoft.com>
2021-09-15 16:11:31 -0700

    Update run_perf_docker.sh

--- onnxruntime/python/tools/tensorrt/perf/run_perf_docker.sh
+++ onnxruntime/python/tools/tensorrt/perf/run_perf_docker.sh
@@ -18,4 +18,4 @@ HOME_PERF_DIR='/home/hcsuser/perf/'
 WORKSPACE='/'
 MODEL_PATH=$WORKSPACE$MODEL_PATH
 
-docker run --gpus all -v $PERF_DIR:$DOCKER_PERF_DIR -v $HOME_PERF_DIR$OPTION:$DOCKER_PERF_DIR$OPTION $DOCKER_IMAGE /bin/bash $DOCKER_PERF_DIR'perf.sh' -d $DOCKER_PERF_DIR -o $OPTION -m $MODEL_PATH -w $WORKSPACE -e "$EP_LIST"
+docker run --gpus all -v $PERF_DIR:$DOCKER_PERF_DIR -v /data/models:/perf/models $DOCKER_IMAGE /bin/bash $DOCKER_PERF_DIR'perf.sh' -d $DOCKER_PERF_DIR -o $OPTION -m $MODEL_PATH -w $WORKSPACE -e "$EP_LIST"
