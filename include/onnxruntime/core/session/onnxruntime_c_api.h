6ae5f7a24478bf34b402db3a176020024b5d0567
Ryan Hill <38674843+RyanUnderhill@users.noreply.github.com>
2021-09-17 18:40:27 -0700

    C API Docs - Add build instructions (#9106)
    * Update Doxyfile, add build instructions to header
    * Update paths in README.md

--- include/onnxruntime/core/session/onnxruntime_c_api.h
+++ include/onnxruntime/core/session/onnxruntime_c_api.h
@@ -1,6 +1,8 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+// See docs\c_cxx\README.md on generating the Doxygen documentation from this file
+
 /** \mainpage C & C++ APIs
 *
 * <h1>C</h1>
