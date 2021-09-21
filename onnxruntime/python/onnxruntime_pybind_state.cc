5e7eb049423a663c38a2c4c52c4033a296dabe28
Changming Sun <chasun@microsoft.com>
2021-09-20 20:39:17 -0700

    Fix a bug for Openvino Python binding (#9130)

--- onnxruntime/python/onnxruntime_pybind_state.cc
+++ onnxruntime/python/onnxruntime_pybind_state.cc
@@ -576,10 +576,9 @@ std::unique_ptr<IExecutionProvider> CreateExecutionProviderInstance(
         }
       }
     }
-
-    return onnxruntime::CreateExecutionProviderFactory_OpenVINO(&params)->CreateProvider();
     // Reset global variables config to avoid it being accidentally passed on to the next session
     openvino_device_type.clear();
+    return onnxruntime::CreateExecutionProviderFactory_OpenVINO(&params)->CreateProvider();
 #endif
   } else if (type == kNupharExecutionProvider) {
 #if USE_NUPHAR
