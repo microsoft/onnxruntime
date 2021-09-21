153767bab42cb5b51296c8fb269be6ccdeacf99d
Thiago Crepaldi <thiago.crepaldi@microsoft.com>
2021-09-21 15:11:41 -0400

    Add internal determinism flag configuration for ORTModule (#9074)

--- orttraining/orttraining/python/training/ortmodule/__init__.py
+++ orttraining/orttraining/python/training/ortmodule/__init__.py
@@ -5,11 +5,11 @@
 
 import os
 import sys
+import torch
 
+from onnxruntime import set_seed
 from packaging import version
-
-from ._fallback import (_FallbackManager,
-                        _FallbackPolicy,
+from ._fallback import (_FallbackPolicy,
                         ORTModuleFallbackException,
                         ORTModuleInitException,
                         wrap_exception)
@@ -30,6 +30,7 @@ ORTMODULE_FALLBACK_POLICY = _FallbackPolicy.FALLBACK_UNSUPPORTED_DEVICE |\
                             _FallbackPolicy.FALLBACK_UNSUPPORTED_ONNX_MODEL |\
                             _FallbackPolicy.FALLBACK_BAD_INITIALIZATION
 ORTMODULE_FALLBACK_RETRY = False
+ORTMODULE_IS_DETERMINISTIC = torch.are_deterministic_algorithms_enabled()
 
 # Verify minimum PyTorch version is installed before proceding to ONNX Runtime initialization
 try:
@@ -39,41 +40,59 @@ try:
     if runtime_pytorch_version < minimum_runtime_pytorch_version:
         raise wrap_exception(ORTModuleInitException,
                              RuntimeError(
-                                 f'ONNX Runtime ORTModule frontend requires PyTorch version greater or equal to {MINIMUM_RUNTIME_PYTORCH_VERSION_STR}, '
-                                 f'but version {torch.__version__} was found instead.'))
+                                 'ONNX Runtime ORTModule frontend requires PyTorch version greater'
+                                 f' or equal to {MINIMUM_RUNTIME_PYTORCH_VERSION_STR},'
+                                 f' but version {torch.__version__} was found instead.'))
 except ORTModuleFallbackException as e:
     # Initialization fallback is handled at ORTModule.__init__
     _FALLBACK_INIT_EXCEPTION = e
 except ImportError as e:
-    raise RuntimeError(f'PyTorch {MINIMUM_RUNTIME_PYTORCH_VERSION_STR} must be installed in order to run ONNX Runtime ORTModule frontend!') from e
+    raise RuntimeError(f'PyTorch {MINIMUM_RUNTIME_PYTORCH_VERSION_STR} must be '
+                       'installed in order to run ONNX Runtime ORTModule frontend!') from e
 
 # Verify whether PyTorch C++ extensions are already compiled
 
 if not is_torch_cpp_extensions_installed(TORCH_CPP_DIR) and '-m' not in sys.argv:
-    _FALLBACK_INIT_EXCEPTION = wrap_exception(ORTModuleInitException,
-                                                        EnvironmentError(
-                                                            f"ORTModule's extensions were not detected at '{TORCH_CPP_DIR}' folder. "
-                                                            "Run `python -m torch_ort.configure` before using `ORTModule` frontend."))
+    _FALLBACK_INIT_EXCEPTION = wrap_exception(
+        ORTModuleInitException,
+        EnvironmentError(
+            f"ORTModule's extensions were not detected at '{TORCH_CPP_DIR}' folder. "
+            "Run `python -m torch_ort.configure` before using `ORTModule` frontend."))
 
 # Initalized ORT's random seed with pytorch's initial seed
 # in case user has set pytorch seed before importing ORTModule
-import sys
-from onnxruntime import set_seed
 set_seed((torch.initial_seed() % sys.maxsize))
+
 
 # Override torch.manual_seed and torch.cuda.manual_seed
 def override_torch_manual_seed(seed):
     set_seed(int(seed % sys.maxsize))
     return torch_manual_seed(seed)
+
+
 torch_manual_seed = torch.manual_seed
 torch.manual_seed = override_torch_manual_seed
+
 
 def override_torch_cuda_manual_seed(seed):
     set_seed(int(seed % sys.maxsize))
     return torch_cuda_manual_seed(seed)
+
+
 torch_cuda_manual_seed = torch.cuda.manual_seed
 torch.cuda.manual_seed = override_torch_cuda_manual_seed
 
+
+def _use_deterministic_algorithms(enabled):
+    global ORTMODULE_IS_DETERMINISTIC
+    ORTMODULE_IS_DETERMINISTIC = enabled
+
+
+def _are_deterministic_algorithms_enabled():
+    global ORTMODULE_IS_DETERMINISTIC
+    return ORTMODULE_IS_DETERMINISTIC
+
+
 # ORTModule must be loaded only after all validation passes
-from .ortmodule import ORTModule
-from .debug_options import DebugOptions, LogLevel
+from .ortmodule import ORTModule  # noqa: E402
+from .debug_options import DebugOptions, LogLevel  # noqa: E402
