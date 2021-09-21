39b87a98abaab70fe15a7d0f08d12b3ddbfbd364
wangye <wangye@microsoft.com>
2021-09-21 20:58:51 +0000

    Revert "Fix nightly CI pipeline to generate ROCm 4.2 wheels and add ROCm 4.3.1 wheels (#9101)"
    This reverts commit 47888392abd6b8065fe7b73863df99b6bdd7a07c.

--- onnxruntime/core/providers/rocm/reduction/reduction_ops.cc
+++ onnxruntime/core/providers/rocm/reduction/reduction_ops.cc
@@ -186,9 +186,14 @@ Status ReduceKernel<allow_multi_axes>::ReduceKernelShared(
   else
     ORT_RETURN_IF_ERROR(reduce_desc.Set(miopen_reduce_op, miopen_type_X, ReduceTensorIndices));
 
-  const auto one = ReduceConsts<HipT>::One;
-  const auto zero = ReduceConsts<HipT>::Zero;
-
+  // As of ROCm 4.2, miopenReduceTensor() requires alpha/beta to be the same data
+  // type as the input type. This differs from cudnnReduceTensor() and other
+  // MIOpen/cuDNN APIs where alpha/beta are float when input type is half (float16).
+  //
+  // NOTE: this workaround can be removed in ROCm 4.3:
+  //       https://github.com/ROCmSoftwarePlatform/MIOpen/pull/914
+  const auto one = Consts<float>::One;
+  const auto zero = Consts<float>::Zero;
   MiopenTensor input_tensor;
   MiopenTensor output_tensor;
   ORT_RETURN_IF_ERROR(input_tensor.Set(input_dims_miopen, miopen_type_X));
@@ -510,9 +515,14 @@ Status ReduceComputeCore(ROCMExecutionProvider& rocm_ep, const Tensor& input, Pr
     ORT_RETURN_IF_ERROR(reduce_desc.Set(miopen_reduce_op, miopen_type_X, ReduceTensorIndices));
   }
 
-  const auto one = ReduceConsts<HipT>::One;
-  const auto zero = ReduceConsts<HipT>::Zero;
-
+  // As of ROCm 4.2, miopenReduceTensor() requires alpha/beta to be the same data
+  // type as the input type. This differs from cudnnReduceTensor() and other
+  // MIOpen/cuDNN APIs where alpha/beta are float when input type is half (float16).
+  //
+  // NOTE: this workaround can be removed in ROCm 4.3:
+  //       https://github.com/ROCmSoftwarePlatform/MIOpen/pull/914
+  const float one = Consts<float>::One; 
+  const float zero = Consts<float>::Zero; 
   MiopenTensor input_tensor;
   MiopenTensor output_tensor;
   ORT_RETURN_IF_ERROR(input_tensor.Set(input_dims_miopen, miopen_type_X));
