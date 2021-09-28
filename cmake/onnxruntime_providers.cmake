a647c859e7b84c1efb593d5366a134d318a0ae36
L <91376574+LeoTzahal18@users.noreply.github.com>
2021-09-28 19:43:40 -0300

    Update onnxruntime_providers.cmake

--- cmake/onnxruntime_providers.cmake
+++ cmake/onnxruntime_providers.cmake
@@ -1,6 +1,10 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
+#include <onnxruntime/core/providers/cuda/cuda_provider_factory.h>
+#include <onnxruntime/core/session/onnxruntime_cxx_api.h>
+
+
 file(GLOB_RECURSE onnxruntime_providers_srcs CONFIGURE_DEPENDS
   "${ONNXRUNTIME_ROOT}/core/providers/cpu/*.h"
   "${ONNXRUNTIME_ROOT}/core/providers/cpu/*.cc"
