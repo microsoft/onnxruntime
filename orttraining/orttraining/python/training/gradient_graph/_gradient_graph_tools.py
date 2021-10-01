5ad361d1809121f6f4ba8341ab5f3cde67ff5a37
Justin D. Harris <juharris@users.noreply.github.com>
2021-10-01 17:59:27 -0400

    Force giving model params as input to the gradient graph.

--- orttraining/orttraining/python/training/gradient_graph/_gradient_graph_tools.py
+++ orttraining/orttraining/python/training/gradient_graph/_gradient_graph_tools.py
@@ -14,22 +14,29 @@ def export_gradient_graph(
         example_input: torch.Tensor,
         example_labels: torch.Tensor,
         gradient_graph_path: Union[Path, str], intermediate_graph_path: Optional[Union[Path, str]] = None,
-        # FIXME Use a clearer name that involes "parameters". `use_parameters_as_input`?
-        input_weights=True):
+        opset_version=12):
     r"""
     Build a gradient graph for `model` so that you can output gradients when given a specific input and labels.
 
     Args:
         model (torch.nn.Module): A gradient will be built for this model.
+
         loss_fn (Callable[[Any, Any], Any]): A function to compute the loss given the model's output and the `example_labels`.
+
         example_input (torch.Tensor): Example input that you would give your model for inference/prediction.
+
         example_labels (torch.Tensor): The expected labels for `example_input`.
-            This could be the output of your model when given `example_input` but it might be different if your loss function expects labels to be different (e.g. when using CrossEntropyLoss).
+        This could be the output of your model when given `example_input` but it might be different if your loss function expects labels to be different (e.g. when using CrossEntropyLoss).
+
         gradient_graph_path (Union[Path, str]): The path to where you would like to save the gradient graph.
-        intermediate_graph_path (Optional[Union[Path, str]): The path to where you would like to save any intermediate graphs that are needed to make the gradient graph. Defaults to `gradient_graph_path` and it will be overwritten if this function executes successfully.
-        input_weights (bool): `True` if the gradient graph should have inputs for the model weights. Useful if TODO...
+
+        intermediate_graph_path (Optional[Union[Path, str]): The path to where you would like to save any intermediate graphs that are needed to make the gradient graph.
+        Defaults to `gradient_graph_path` and it will be overwritten if this function executes successfully.
+
+        opset_version (int): See `torch.onnx.export`.
     """
 
+    # Is this needed?
     model.train()
 
     # Make sure that loss nodes that expect multiple outputs are set up.
@@ -45,14 +52,14 @@ def export_gradient_graph(
         gradient_graph_path = str(gradient_graph_path)
 
     class WrapperModule(torch.nn.Module):
-        def __init__(self, model: torch.nn.Module):
-            super(WrapperModule, self).__init__()
-            self.model = model
+        def forward(self, model_input, expected_labels, *model_params):
+                for param, set_param in zip(model.parameters(), model_params):
+                    param.data = set_param.data
+                output = model(model_input)
+                loss = loss_fn(output, expected_labels)
+                return output, loss
 
-        def forward(self, model_input, expected_labels):
-            output = self.model(model_input)
-            loss = loss_fn(output, expected_labels)
-            return output, loss
+    wrapped_model = WrapperModule()
 
     dynamic_axes = {
         'input': {0: 'batch_size', },
@@ -60,43 +67,19 @@ def export_gradient_graph(
         'output': {0: 'batch_size', },
     }
 
-    if input_weights:
-        class WeightsWrapperModule(WrapperModule):
-            def forward(self, model_input, expected_labels, *model_params):
-                for param, set_param in zip(model.parameters(), model_params):
-                    param.data = set_param.data
-                output = self.model(model_input)
-                loss = loss_fn(output, expected_labels)
-                return output, loss
-        wrapped_model = WeightsWrapperModule(model)
-        args = (example_input, example_labels, *tuple(model.parameters()))
-        model_param_names = tuple(name for name, _ in model.named_parameters())
-        input_names = ['input', 'labels', *model_param_names]
-        nodes_needing_gradients = set()
-        for name, _ in model.named_parameters():
-            # Should we check `if _.requires_grad:` first?
-            nodes_needing_gradients.add(name)
-    else:
-        wrapped_model = WrapperModule(model)
-        args = (example_input, example_labels)
-        input_names = ['input', 'labels']
-        # 'model.' will be prepended to nodes because we wrapped `model` using a member called `model`.
-        node_name_prefix = 'model.'
-         # TODO Allow customizing.
-        nodes_needing_gradients = set()
-        for name, _ in model.named_parameters():
-            # Should we check `if _.requires_grad:` first?
-            nodes_needing_gradients.add(name)
-
-        nodes_needing_gradients = set(
-            node_name_prefix + name for name in nodes_needing_gradients)
-        
+    args = (example_input, example_labels, *tuple(model.parameters()))
+    model_param_names = tuple(name for name, _ in model.named_parameters())
+    input_names = ['input', 'labels', *model_param_names]
+    nodes_needing_gradients = set()
+    for name, _ in model.named_parameters():
+        # Should we check `if _.requires_grad:` first?
+        nodes_needing_gradients.add(name)
 
     torch.onnx.export(
         wrapped_model, args,
         intermediate_graph_path,
         export_params=True,
-        opset_version=12, do_constant_folding=False,
+        opset_version=opset_version, do_constant_folding=False,
         training=TrainingMode.TRAINING,
         input_names=input_names,
         output_names=['output', 'loss'],
