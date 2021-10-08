32a60a4d49de1163f0afc05790882ca48b9c5809
Abhishek Kulkarni <abkulkarni@microsoft.com>
2021-10-08 10:21:00 -0700

    Add a test case with cast elimination inside a function

--- onnxruntime/test/optimizer/graph_transform_test.cc
+++ onnxruntime/test/optimizer/graph_transform_test.cc
@@ -2195,6 +2195,36 @@ TEST_F(GraphTransformationTests, CastElimination) {
   ASSERT_TRUE(op_to_count["Cast"] == 4);
 }
 
+TEST_F(GraphTransformationTests, RuleBasedGraphTransformerInsideFunction) {
+  auto model_uri = MODEL_FOLDER "cast_elim_inside_func.onnx";
+  std::shared_ptr<Model> model;
+  ASSERT_TRUE(Model::Load(model_uri, model, nullptr, *logger_).IsOK());
+  Graph& graph = model->MainGraph();
+
+  const std::function<int(const Graph& graph)> count_cast_ops = [&](const Graph& graph) {
+    int count = 0;
+    for (auto& node : graph.Nodes()) {
+      if (node.GetFunctionBody() != nullptr) {
+        count += count_cast_ops(node.GetFunctionBody()->Body());
+      }
+      if (node.OpType() == "Cast") {
+        count++;
+      }
+    }
+    return count;
+  };
+
+  ASSERT_TRUE(count_cast_ops(graph) == 3);
+
+  auto rule_transformer_L1 = std::make_unique<RuleBasedGraphTransformer>("RuleTransformer1");
+  rule_transformer_L1->Register(std::make_unique<CastElimination>());
+  onnxruntime::GraphTransformerManager graph_transformation_mgr{5};
+  graph_transformation_mgr.Register(std::move(rule_transformer_L1), TransformerLevel::Level1);
+  ASSERT_TRUE(graph_transformation_mgr.ApplyTransformers(graph, TransformerLevel::Level1, *logger_).IsOK());
+
+  ASSERT_TRUE(count_cast_ops(graph) == 2);
+}
+
 #ifndef DISABLE_CONTRIB_OPS
 
 static void ValidateAttention(Graph& graph) {
