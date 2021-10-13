58f6c6c9d4f220b6028e5cd2c72a6f8658004b4c
Satya Jandhyala <satya.k.jandhyala@gmail.com>
2021-10-13 22:27:19 +0000

    Use ATenOp to implement ReduceSum

--- orttraining/orttraining/training_ops/cuda/reduction/reduction_ops.h
+++ orttraining/orttraining/training_ops/cuda/reduction/reduction_ops.h
@@ -6,6 +6,9 @@
 #include "core/providers/cuda/cuda_common.h"
 #include "core/providers/cuda/reduction/reduction_ops.h"
 #include "core/providers/cuda/reduction/reduction_functions.h"
+#include "core/framework/op_kernel_context_internal.h"
+#include "core/dlpack/dlpack_converter.h"
+#include "orttraining/training_ops/cpu/aten_ops/aten_op.h"
 
 namespace onnxruntime {
 namespace cuda {
@@ -18,7 +21,28 @@ class ReduceSumTraining final : public ReduceKernel<true> {
   }
 
   Status ComputeInternal(OpKernelContext* ctx) const override {
+#if 0
     return ComputeImplEx<T>(ctx, CUDNN_REDUCE_TENSOR_ADD);
+#else
+    auto* p_ctx_internal = static_cast<OpKernelContextInternal*>(p_ctx);
+    std::vector<DLManagedTensor*> dlpacks;
+    for (int i = 0; i < p_ctx_internal->InputCount(); i++) {
+      const OrtValue* p_ort_value = p_ctx_internal->GetInputMLValue(i);
+      if (!p_ort_value) {
+        dlpacks.emplace_back(nullptr);
+      } else {
+        OrtValue ort_value = *p_ort_value;
+        dlpacks.emplace_back(dlpack::OrtValueToDlpack(ort_value));
+      }
+    }
+
+    auto result = aten_ops::ATenOperatorExecutor::Instance()("ReduceSum", "", dlpacks);
+    for (size_t i = 0; i < result.size(); i++) {
+      ORT_RETURN_IF_ERROR(p_ctx_internal->SetOutputMLValue(static_cast<int>(i), dlpack::DlpackToOrtValue(result[i])));
+    }
+
+    return Status::OK();
+#endif
   }
 };
 
