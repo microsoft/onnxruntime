b76e55fa18658d31e26a510bb1f25755ec4d2059
Guoyu Wang <wanggy@outlook.com>
2021-10-14 19:14:57 -0700

    Fix integer overflow for large step for Slice OP

--- onnxruntime/test/providers/cpu/tensor/slice_op.test.cc
+++ onnxruntime/test/providers/cpu/tensor/slice_op.test.cc
@@ -604,5 +604,26 @@ TEST(SliceTest, Slice5D_SubsetOfAxes_Flatten2Dims_OffsetInput) {
                       {-5.f, -6.f, -7.f, -8.f},
                       true);
 }
+
+// test where we use a ridiculously large step, using a large enough step has caused
+// ORT crash due to integer overflow on 32bit system
+TEST(SliceTest, Slice5D_LargeStep) {
+  RunSliceTest<float>({1, 2, 2, 2, 2},
+                      {1.f, 2.f, 3.f, 4.f,
+                       5.f, 6.f, 7.f, 8.f,
+                       -1.f, -2.f, -3.f, -4.f,
+                       -5.f, -6.f, -7.f, -8.f},
+                      {0},
+                      {1},
+                      {0},
+                      {std::numeric_limits<int64_t>::max()},
+                      {1, 2, 2, 2, 2},
+                      {1.f, 2.f, 3.f, 4.f,
+                       5.f, 6.f, 7.f, 8.f,
+                       -1.f, -2.f, -3.f, -4.f,
+                       -5.f, -6.f, -7.f, -8.f},
+                      true);
+}
+
 }  // namespace test
 }  // namespace onnxruntime
