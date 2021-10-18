c6c68d2e5d44d73c8e36ddf62e85c81941e02c7f
L <91376574+LeoTzahal18@users.noreply.github.com>
2021-10-18 00:27:51 -0300

    Update onnxruntime_pybind_ortvalue.cc

--- onnxruntime/python/onnxruntime_pybind_ortvalue.cc
+++ onnxruntime/python/onnxruntime_pybind_ortvalue.cc
@@ -5,7 +5,7 @@
 #include "python/onnxruntime_pybind_mlvalue.h"
 #include "python/onnxruntime_pybind_state_common.h"
 
-#define NO_IMPORT_ARRAY
+#define IMPORT_ARRAY
 #define NPY_NO_DEPRECATED_API NPY_1_7_API_VERSION
 #define PY_ARRAY_UNIQUE_SYMBOL onnxruntime_python_ARRAY_API
 #include <numpy/arrayobject.h>
