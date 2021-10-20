ab894935c5adc8d5794a2a17b97caa5ac126abbc
Rajalakshmi Srinivasaraghavan <rajis@linux.ibm.com>
2021-10-20 12:26:08 -0500

    POWER: Add Dgemm kernel for POWER processor
    This patch adds new dgemm kernel specific to POWER processor.

--- onnxruntime/core/mlas/lib/platform.cpp
+++ onnxruntime/core/mlas/lib/platform.cpp
@@ -370,6 +370,7 @@ Return Value:
 #endif // MLAS_TARGET_ARM64
 #if defined(MLAS_TARGET_POWER)
     this->GemmFloatKernel = MlasSgemmKernel;
+    this->GemmDoubleKernel = MlasDgemmKernel;
 #if defined(__linux__)  && defined(POWER10)
 #if (defined(__GNUC__) && ((__GNUC__ > 10) || (__GNUC__== 10 && __GNUC_MINOR__ >= 2))) || \
     (defined(__clang__) && (__clang_major__ >= 12))
