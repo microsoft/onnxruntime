c0f81ef21201450a8ded603ef0d39d85bc6d9b00
Tom Wildenhain <tomwi@microsoft.com>
2021-10-20 00:03:21 -0700

    Comments for nhwc_transformer

--- onnxruntime/core/optimizer/nhwc_transformer.cc
+++ onnxruntime/core/optimizer/nhwc_transformer.cc
@@ -16,16 +16,25 @@ namespace onnxruntime {
 
 LayoutHandlerResult QLinearConvHandler(api::Graph& graph, api::Node& node) {
   ORT_UNUSED_PARAMETER(graph);
-  if (node.Domain() != "") {
+
+  // Skip if domain is incorrect
+  auto domain = node.Domain();
+  if (domain != "" && domain != "com.microsoft") {
     return {false, 0, std::nullopt, std::nullopt};
   }
+
+  // Skip if already transformed
   if (node.GetAttributeIntDefault("channels_last", 0) == 1) {
     return {false, 0, std::nullopt, std::nullopt};
   }
+
+  // Skip if unknown rank
   auto shape = NodeFromApiNode(node).InputDefs()[0]->Shape();
   if (shape == nullptr) {
     return {false, 0, std::nullopt, std::nullopt};
   }
+
+  // Convert to channels last
   size_t rank = shape->dim_size();
   node.SetAttributeInt("channels_last", 1);
   return {true, rank, std::nullopt, "com.microsoft"};
@@ -38,13 +47,17 @@ Status NhwcTransformer::ApplyImpl(Graph& graph, bool& modified, int graph_level,
     ORT_RETURN_IF_ERROR(Recurse(node, modified, graph_level, logger));
   }
 
+  // Only QLinearConv needs to be handled explicitly. The rest will be transformed if needed during transpose
+  // optimization.
   std::unordered_map<std::string_view, LayoutHandler> handler_map = {
       {"QLinearConv", &QLinearConvHandler}
   };
+
   auto api_graph = MakeApiGraph(graph, std::move(cpu_allocator_), logger, kCpuExecutionProvider);
   if (ChannelFirstToChannelLast(*api_graph, handler_map, /*allow_extended_ops*/ true)) {
     modified = true;
   }
+
   return Status::OK();
 }
 
