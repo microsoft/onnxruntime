2ae91388d395930991e68c979f6a8bf2d7a58e3a
L <91376574+LeoTzahal18@users.noreply.github.com>
2021-10-20 08:08:37 -0300

    Create Tensornuph.py

---
+++ include/onnxruntime/core/eager/Tensornuph.py
@@ -0,0 +1,30 @@
+# -------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation.  All rights reserved.
+# Licensed under the MIT License.  See License.txt in the project root for
+# license information. 20/10/2021. By: LeoTzahal18
+# --------------------------------------------------------------------------
+
+
+Import Linux CPU CI Pipeline from Microsoft
+Import Linux CPU Minimal Build E2E CI Pipeline from Microsoft
+Import Linux CPU x64 NoContribops CI Pipeline from Microsoft
+Import Linux GPU CI Pipeline from Microsoft
+Import Linux GPU TensorRT CI Pipeline from Microsoft
+Import Linux Nuphar CI Pipeline from Microsoft
+Import Linux OpenVINO CI Pipeline from Microsoft
+Import MacOS CI Pipeline from Linux Nuphar
+Import MacOS NoContribops CI Pipeline from Linux OpenVINO
+Import Windows CPU CI Pipeline from Microsoft
+Import Windows GPU CI Pipeline from Linux
+Import Windows GPU TensorRT CI Pipeline from Linux TensorRT
+Import Windows WebAssembly CI Pipeline from Assembly
+Import orttraining-amd-gpu-ci-pipeline from run
+Import orttraining-linux-ci-pipeline from run
+Import orttraining-linux-gpu-ci-pipeline from run
+Import orttraining-ortmodule from run
+Import orttraining-ortmodule-distributed from run
+
+Import to Áustria
+Import to Israel
+Import to United States
+Import to leocloud
