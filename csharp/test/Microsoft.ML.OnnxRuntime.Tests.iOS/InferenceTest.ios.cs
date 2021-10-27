6ccacd812752d056478f6703a30737babd45ebbe
Scott McKay <skottmckay@gmail.com>
2021-10-27 20:07:07 +1000

    Add Xamarin support (#9436)
    Add Xamarin support to the ORT nuget packages.
      - Update C# code to support Xamarin builds for iOS and Android
      - refactor some things to split out common code
      - include iOS and Android ORT native shared library in native nuget package

---
+++ csharp/test/Microsoft.ML.OnnxRuntime.Tests.iOS/InferenceTest.ios.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+
+namespace Microsoft.ML.OnnxRuntime.Tests
+{
+    public partial class InferenceTest
+    {
+        [Fact(DisplayName = "TestPlatformSessionOptions")]
+        public void TestPlatformSessionOptions()
+        {
+            var opt = new SessionOptions();
+            opt.AppendExecutionProvider_CoreML(CoreMLFlags.COREML_FLAG_ONLY_ENABLE_DEVICE_WITH_ANE);
+            Assert.NotNull(opt);
+        }
+    }
+}
