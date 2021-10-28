f754c603f43b4abce85840508a0f8165a89b5703
Abhishek Jindal <abjindal@microsoft.com>
2021-10-28 16:32:19 -0700

    print OS environ path variables

--- onnxruntime/test/python/onnxruntime_test_python.py
+++ onnxruntime/test/python/onnxruntime_test_python.py
@@ -7,7 +7,9 @@ import os
 import numpy as np
 import gc
 
-import torch
+import os
+print("OS path print for eager mode:", os.environ["PATH"])
+print("OS path print for eager mode PYTHONPATH:", os.environ["PYTHONPATH"])
 import onnxruntime as onnxrt
 import threading
 import sys
