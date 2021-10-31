71e2028a39f396ee8e5b1f29364d70ebcd3be6a7
Yulong Wang <7679871+fs-eire@users.noreply.github.com>
2021-10-30 18:35:00 -0700

    update version format

--- tools/ci_build/github/js/generate-npm-package-dev-version.js
+++ tools/ci_build/github/js/generate-npm-package-dev-version.js
@@ -14,11 +14,9 @@ const now = new Date().toISOString();
 const YYYY = now.slice(0, 4);
 const MM = now.slice(5, 7);
 const DD = now.slice(8, 10);
-const hh = now.slice(11, 13);
-const mm = now.slice(14, 16);
 
 const GIT_COMMIT_ID = child_process.execSync('git rev-parse --short HEAD').toString().trim();
 
-const DATE_STRING = `${YYYY}${MM}${DD}-${hh}${mm}`;
-const VERSION = `${VERSION_BASE}-dev.${DATE_STRING}.${GIT_COMMIT_ID}`;
+const DATE_STRING = `${YYYY}${MM}${DD}`;
+const VERSION = `${VERSION_BASE}-dev.${DATE_STRING}-${GIT_COMMIT_ID}`;
 console.log(`##vso[task.setvariable variable=NpmPackageVersionNumber;]${VERSION}`);
