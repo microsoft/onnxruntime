7610387c4f36aaf84a8f453ff2d97813a286289f
Abhishek Jindal <abjindal@microsoft.com>
2021-11-02 15:39:01 -0700

    remove enter

--- onnxruntime/__init__.py
+++ onnxruntime/__init__.py
@@ -19,7 +19,6 @@ __author__ = "Microsoft"
 # here we need to save the exception and continue with Cuda version validation in order to post
 # meaningful messages to the user.
 # the saved exception is raised after device version validation.
-
 try:
     from onnxruntime.capi._pybind_state import get_all_providers, get_available_providers, get_device, set_seed, \
         RunOptions, SessionOptions, set_default_logger_severity, enable_telemetry_events, disable_telemetry_events, \
