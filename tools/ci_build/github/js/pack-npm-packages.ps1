db30640479cfbcb8d627a26c0089478be09ef5db
Yulong Wang <7679871+fs-eire@users.noreply.github.com>
2021-11-02 01:11:54 -0700

    use powershell to generate version

--- tools/ci_build/github/js/pack-npm-packages.ps1
+++ tools/ci_build/github/js/pack-npm-packages.ps1
@@ -14,15 +14,14 @@
 #
 
 if ($Args.Count -ne 3) {
-    Write-Error "This script requires 3 arguments, but got $($Args.Count)"
-    exit 1
+    throw "This script requires 3 arguments, but got $($Args.Count)"
 }
 $MODE=$Args[0] # "dev" or "release"
-$JS_ROOT=$Args[1] # eg. D:\source\onnxruntime\js
+$ORT_ROOT=$Args[1] # eg. D:\source\onnxruntime
 $TARGET=$Args[2] # "node" or "web"
 
-$JS_COMMON_DIR="$JS_ROOT\common"
-$JS_TARGET_DIR="$JS_ROOT\$TARGET"
+$JS_COMMON_DIR="$ORT_ROOT\js\common"
+$JS_TARGET_DIR="$ORT_ROOT\js\$TARGET"
 
 if ($MODE -eq "dev") {
     # For @dev builds, we compares the following 2 package versions for onnxruntime-common:
@@ -34,29 +33,42 @@ if ($MODE -eq "dev") {
     # 'latest'.
 
     # check latest @dev version
+    Write-Host "Start checking version for onnxruntime-common@dev"
     npm view onnxruntime-common@dev --json | Out-File -Encoding utf8 ./ort_common_latest_info.json
     $ort_common_latest_version=node -p "require('./ort_common_latest_info.json').version"
     $ort_common_latest_dist_tarball=node -p "require('./ort_common_latest_info.json').dist.tarball"
     $ort_common_latest_dist_shasum=node -p "require('./ort_common_latest_info.json').dist.shasum"
+    Write-Host "latest version is: $ort_common_latest_version"
 
     # download package latest@dev
     Invoke-WebRequest $ort_common_latest_dist_tarball -OutFile ./latest.tgz
     if ($(Get-FileHash -Algorithm SHA1 .\latest.tgz).Hash -ne "$ort_common_latest_dist_shasum") {
-        Write-Error "SHASUM mismatch"
-        exit 1
+        throw "SHASUM mismatch"
     }
+    Write-Host "Tarball downloaded"
+
+    # generate @dev version
+    pushd $ORT_ROOT
+    $dev_version_base=Get-Content .\VERSION_NUMBER
+    $dev_version_date=$(Get-Date).ToUniversalTime().ToString("yyyyMMdd")
+    $dev_version_commit=git rev-parse HEAD
+    $dev_version_commit_short=git rev-parse --short HEAD
+    popd
+    $dev_version_number="$dev_version_base-dev.$dev_version_date-$dev_version_commit_short"
+    Write-Host "Generated @dev current version: $dev_version_number"
 
     # make package for latest
     pushd $JS_COMMON_DIR
     npm version $ort_common_latest_version
+    echo $dev_version_commit | Out-File -Encoding ascii -NoNewline -FilePath ./__commit.txt
     npm pack
     popd
 
     $current_tgz_compare_only="$JS_COMMON_DIR\onnxruntime-common-$ort_common_latest_version.tgz"
     if (!(Test-Path $current_tgz_compare_only)) {
-        Write-Error "File is not generated: $current_tgz_compare_only"
-        exit 1
+        throw "File is not generated: $current_tgz_compare_only"
     }
+    Write-Host "Package file ready for comparing: $current_tgz_compare_only"
 
     # extract packages
     mkdir ./latest
@@ -77,9 +89,10 @@ if ($MODE -eq "dev") {
     npm init -y
     npm install "dir-compare-cli@1.0.1" "json-diff@0.5.4"
 
-    # compare package.json
+    Write-Host "Compare package.json"
     npx json-diff ..\latest\package\package.json ..\current\package\package.json
     $use_latest=$?
+    Write-Host "Result: $use_latest"
     if ($use_latest) {
         # package.json matches. now check package contents.
 
@@ -90,20 +103,20 @@ if ($MODE -eq "dev") {
         rm ../latest/package/package.json
         rm ../current/package/package.json
 
-        # compare whole dictionary
+        Write-Host "Compare package contents"
         npx dircompare -c ../latest/package/ ../current/package/
         $use_latest=$?
+        Write-Host "Result: $use_latest"
     }
 
     popd
 
-    # generate @dev version
-    $dev_version_number=node $PSScriptRoot\generate-npm-package-dev-version.js
-
     if (!$use_latest) {
+        Write-Host "Need update to onnxruntime-common@dev"
         # need to publish a new version for onnxruntime-common
         pushd $JS_COMMON_DIR
         npm version $dev_version_number
+        # file __commit.txt is already generated
         npm pack
         popd
     }
@@ -111,6 +124,7 @@ if ($MODE -eq "dev") {
     # make package for target
     pushd $JS_TARGET_DIR
     npm version $dev_version_number
+    echo $dev_version_commit | Out-File -Encoding ascii -NoNewline -FilePath ./__commit.txt
     npm pack
     popd
 } else {
