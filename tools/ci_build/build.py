aa70a9191925b71619f2580a518c22df6331fd94
Abhishek Jindal <abjindal@microsoft.com>
2021-11-02 01:29:57 -0700

    adding details for dll directory more

--- tools/ci_build/build.py
+++ tools/ci_build/build.py
@@ -1582,7 +1582,7 @@ def run_onnxruntime_tests(args, source_dir, ctest_path, build_dir, configs):
                      '/Logger:trx', '/Enablecodecoverage', '/Platform:x64',
                      "/Settings:%s" % os.path.join(
                          source_dir, 'cmake\\codeconv.runsettings')] + executables,
-                    cwd=cwd2, dll_path=dll_path, add_dll_dir_path=add_dll_dir_path)
+                    cwd=cwd2, dll_path=dll_path)
             else:
                 executables = ['onnxruntime_test_all', 'onnxruntime_mlas_test']
                 if args.build_shared_lib:
@@ -1594,7 +1594,7 @@ def run_onnxruntime_tests(args, source_dir, ctest_path, build_dir, configs):
 
         else:
             ctest_cmd = [ctest_path, "--build-config", config, "--verbose", "--timeout", "7200"]
-            run_subprocess(ctest_cmd, cwd=cwd, dll_path=dll_path, add_dll_dir_path=add_dll_dir_path)
+            run_subprocess(ctest_cmd, cwd=cwd, dll_path=dll_path)
 
         if args.enable_pybind:
             # Disable python tests for TensorRT because many tests are
