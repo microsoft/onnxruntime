64342e56647c0836bbbcc5e99a75bcd93b61aea3
Guoyu Wang <wanggy@outlook.com>
2021-11-02 16:01:26 -0700

    Add logging of system abi

--- java/src/test/android/app/src/androidTest/java/ai/onnxruntime/example/javavalidator/SimpleTest.kt
+++ java/src/test/android/app/src/androidTest/java/ai/onnxruntime/example/javavalidator/SimpleTest.kt
@@ -2,16 +2,14 @@ package ai.onnxruntime.example.javavalidator
 
 import ai.onnxruntime.*
 import ai.onnxruntime.OrtSession.SessionOptions
+import android.os.Build;
 import android.util.Log
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
 import com.microsoft.appcenter.espresso.Factory
 import com.microsoft.appcenter.espresso.ReportHelper
-import org.junit.After
-import org.junit.Assert
-import org.junit.Rule
-import org.junit.Test
+import org.junit.*
 import org.junit.runner.RunWith
 import java.io.IOException
 import java.util.*
@@ -25,6 +23,12 @@ class SimpleTest {
 
     @get:Rule
     var reportHelper: ReportHelper = Factory.getReportHelper()
+
+    @Before
+    fun Start() {
+        reportHelper.label("Starting App")
+        Log.println(Log.INFO, TAG, "SystemABI=" + Build.SUPPORTED_ABIS[0])
+    }
 
     @After
     fun TearDown() {
