a2c6658f07e3f3bb928656e5792b41d5ddeb163f
Yufeng Li <liyufeng1987@gmail.com>
2021-11-05 08:00:23 -0700

    Update graph_transformer_utils.cc

--- onnxruntime/core/optimizer/graph_transformer_utils.cc
+++ onnxruntime/core/optimizer/graph_transformer_utils.cc
@@ -174,7 +174,7 @@ std::vector<std::unique_ptr<GraphTransformer>> GenerateTransformers(
 
       if (!disable_quant_qdq) {
         transformers.emplace_back(std::make_unique<QDQS8ToU8Transformer>(cpu_ep));
-        transformers.emplace_back(std::make_unique<QDQPropagationTransformer>(cpu_ep));
+        // transformers.emplace_back(std::make_unique<QDQPropagationTransformer>(cpu_ep));
         transformers.emplace_back(std::make_unique<QDQSelectorActionTransformer>());
       }
 
