84df340b2f88b81190f39a6434e35c11f68adbc1
Abhishek Jindal <abjindal@microsoft.com>
2021-11-05 09:09:09 -0700

    Abjindal/eager windows ci pipeline (#9587)
    * adding eager ci pipelines files
    
    * adding import torch before onnxruntime
    
    * finding os environ path
    
    * finding os environ path corrected
    
    * print OS environ path variables
    
    * adding environ path for torch
    
    * changing python version
    
    * changing python python for torch libs
    
    * removing import torch statements
    
    * removing unncecessary torch path
    
    * removing path variable
    
    * add dll_path
    
    * test for python 3.7
    
    * adding dll directory path for python 3.8+
    
    * print dll directory path for python 3.8+
    
    * adding requirements file
    
    * change requirements directory
    
    * print more
    
    * adding dll dir path
    
    * removing setup eager file
    
    * adding details for dll directory
    
    * adding details for dll directory more
    
    * adding import torch in onnxruntime init file
    
    * removing dll dir path and moving requirements file
    
    * enabling pipeline for py3.7
    
    * remove enter
    
    * removing debug build
    
    * removing openmp
    
    * adding comments for torch dll loading and cases of failure
    
    * cleaning up the pipeline

--- tools/ci_build/build.py
+++ tools/ci_build/build.py
@@ -1547,6 +1547,12 @@ def run_onnxruntime_tests(args, source_dir, ctest_path, build_dir, configs):
                 build_dir, config, "external", "tvm", config))
         if args.use_tensorrt:
             dll_path_list.append(os.path.join(args.tensorrt_home, 'lib'))
+        # Adding the torch lib path for loading DLLs for onnxruntime in eager mode
+        # This works for Python 3.7 and below, and doesn't work for Python 3.8+
+        # User will need to import torch before onnxruntime and it will work for all versions
+        if args.build_eager_mode and is_windows():
+            import torch
+            dll_path_list.append(os.path.join(os.path.dirname(torch.__file__), 'lib'))
 
         dll_path = None
         if len(dll_path_list) > 0:
