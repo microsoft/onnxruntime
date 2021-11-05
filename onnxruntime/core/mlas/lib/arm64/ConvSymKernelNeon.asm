f97cda4cb6b9081234fc5985c92dec4c2eb105cd
Chen Fu <fuchen@microsoft.com>
2021-11-05 13:07:53 -0700

    from ldp to ldr

--- onnxruntime/core/mlas/lib/arm64/ConvSymKernelNeon.asm
+++ onnxruntime/core/mlas/lib/arm64/ConvSymKernelNeon.asm
@@ -96,17 +96,15 @@ Return Value:
         PROLOG_SAVE_REG_PAIR  d10,d11,#16
         PROLOG_SAVE_REG_PAIR  d12,d13,#32
         PROLOG_SAVE_REG_PAIR  d14,d15,#48
-
-        mov     x9, x3                  // save kernel size
-        mov     x16,x4                  // save input channels
+        mov     x9,x3                   // save kernel size
         ldr     x11,[x8,#ConvSymPostProcessParams_Bias]
+        mov     x16,x4                  // save input channels
         ldr     x12,[x8,#ConvSymPostProcessParams_Scale]
-        cmp     x7, 2                   // if OutputCount < 2
-        add     x5, x2, x5              // c1 = c0 + ldc
-        add     x4, x4, 7               // kc = (kc + 7) & ~7
-        csel    x5, x2, x5, lo          // if OutputCount < 2  c1 = c0
-        bic     x4, x4, 7
-
+        cmp     x7,2                    // if OutputCount < 2
+        add     x5,x2,x5                // c1 = c0 + ldc
+        add     x4,x4,7                 // kc = (kc + 7) & ~7
+        csel    x5,x2,x5,lo             // if OutputCount < 2  c1 = c0
+        bic     x4,x4,7
         ldp     s16,s18,[x11],8         // init accumulators with bias
         ldp     s20,s22,[x11],8
         ldp     s24,s26,[x11],8
@@ -119,7 +117,6 @@ Return Value:
         mov     v27.16b,v26.16b
         mov     v29.16b,v28.16b
         mov     v31.16b,v30.16b
-        ldr     w18, [x8, #ConvSymPostProcessParams_ZeroPoint]
 
 // Nested loops, inner loop: input channel; outter loop: kernel size
 // Each inner iteration processes 8 input channels, 2 output pixels, 8 output channels.
@@ -143,191 +140,200 @@ KernelSizeLoop
 
         // Load next 2 A pointers
         tst     w10,#MLAS_CONV_SYM_FLAG_INPUT_DIRECT
+        ldr     d4,[x1]
+        ldr     d5,[x1,8]
         beq     InputIndirection
+
 InputDirect
-        cmp     x7,2                    // test if OutputCount < 2
         mov     x13,x0                  // x13 -> A0
         add     x15,x0,x16              // x15 -> A1 = A0 + input channels
-        csel    x15, x13, x15, LO       // if OutputCount < 2  x15 -> A0
-        b       BlockloopPrologue
+        b       BlockLoopPrologue
+
 InputIndirection
         cmp     x7,2                    // test if OutputCount < 2
         ldr     x13,[x0]                // x13 -> A0
-        mov     x15,x13                 // x15 -> A0
         blo     SkipLoadA1
         ldr     x15,[x0,x3,lsl#3]       // x15 -> A1
 SkipLoadA1
+
+BlockLoopPrologue
+        subs    x14,x4,16               // input channel - 16
         add     x0,x0,8                 // indirect A advance to next pointer, prepare for kernel size loop
+        movi    v12.8b,128
+        blo     InputChannel8           // less than 16 deep, no unroll
 
-BlockloopPrologue
-        movi    v2.8b, 128
-        subs     x14,x4,16               // input channel - 16
-        blo     InChannel8     // less than 16 deep, no unroll
-
-        ldp     d0,d6,[x13],16
-        ldp     d1,d7,[x15],16
-        ldp     d4,d5,[x1]
-        ldp     d8,d9,[x1,64]
-
+        cmp     x7,2                    // test if OutputCount < 2
+        ldr     d8,[x1,64]
+        ldr     d9,[x1,72]
+        ldr     d0,[x13],8
+        csel    x15,x13,x15,lo          // if OutputCount < 2  x15 -> A0
+        ldr     d6,[x13],8
         subs    x14,x14,16              // input channel - 16
-        blo     BlockLoopEpilogue  // need 32 input channel for full unrolled loop
+        ldr     d1,[x15],8
+        ldr     d7,[x15],8
+        blo     BlockLoopEpilogue       // need 32 input channel for full unrolled loop
 
 Blockloop
-        eor     v0.8b, v0.8b, v2.8b
-        eor     v6.8b, v6.8b, v2.8b
-        eor     v1.8b, v1.8b, v2.8b
-        eor     v7.8b, v7.8b, v2.8b
-        smull   v2.8h, v4.8b, v0.8b
-        smull   v3.8h, v4.8b, v1.8b
-        prfm    PLDL1KEEP, [x1, 448]
-        smull   v10.8h, v5.8b, v0.8b
-        smull   v11.8h, v5.8b, v1.8b
-        ldp     d4, d5, [x1, 16]
-        smlal   v2.8h, v8.8b, v6.8b
-        smlal   v3.8h, v8.8b, v7.8b
-        prfm    PLDL1KEEP, [x1, 512]
-        smlal   v10.8h, v9.8b, v6.8b
-        smlal   v11.8h, v9.8b, v7.8b
-
-        ldp     d8, d9, [x1, 80]
-        smull   v12.8h, v4.8b, v0.8b
-        sadalp  v16.4s,  v2.8h
-        smull   v13.8h, v4.8b, v1.8b
-        sadalp  v17.4s,  v3.8h
-        smull   v14.8h, v5.8b, v0.8b
-        sadalp  v18.4s, v10.8h
-        smull   v15.8h, v5.8b, v1.8b
-        sadalp  v19.4s, v11.8h
-        ldp     d4, d5, [x1, 32]
-        smlal   v12.8h, v8.8b, v6.8b
-        smlal   v13.8h, v8.8b, v7.8b
-        prfm    PLDL1KEEP, [x13, 128]
-        smlal   v14.8h, v9.8b, v6.8b
-        smlal   v15.8h, v9.8b, v7.8b
-
-        ldp     d8, d9, [x1, 96]
-        smull   v2.8h, v4.8b, v0.8b
-        sadalp  v20.4s, v12.8h
-        smull   v3.8h, v4.8b, v1.8b
-        sadalp  v21.4s, v13.8h
-        smull   v10.8h, v5.8b, v0.8b
-        sadalp  v22.4s, v14.8h
-        smull   v11.8h, v5.8b, v1.8b
+        eor     v0.8b,v0.8b,v12.8b
+        eor     v1.8b,v1.8b,v12.8b
+        smull   v2.8h,v4.8b,v0.8b
+        smull   v3.8h,v4.8b,v1.8b
+        ldr     d4,[x1,16]
+        smull   v10.8h,v5.8b,v0.8b
+        smull   v11.8h,v5.8b,v1.8b
+        ldr     d5,[x1,24]
+        eor     v6.8b,v6.8b,v12.8b
+        eor     v7.8b,v7.8b,v12.8b
+        smlal   v2.8h,v8.8b,v6.8b
+        smlal   v3.8h,v8.8b,v7.8b
+        ldr     d8,[x1,80]
+        smlal   v10.8h,v9.8b,v6.8b
+        smlal   v11.8h,v9.8b,v7.8b
+        ldr     d9,[x1,88]
+        smull   v12.8h,v4.8b,v0.8b
+        sadalp  v16.4s,v2.8h
+        smull   v13.8h,v4.8b,v1.8b
+        ldr     d4,[x1,32]
+        sadalp  v17.4s,v3.8h
+        smull   v14.8h,v5.8b,v0.8b
+        sadalp  v18.4s,v10.8h
+        smull   v15.8h,v5.8b,v1.8b
+        ldr     d5,[x1,40]
+        sadalp  v19.4s,v11.8h
+        smlal   v12.8h,v8.8b,v6.8b
+        smlal   v13.8h,v8.8b,v7.8b
+        ldr     d8,[x1,96]
+        smlal   v14.8h,v9.8b,v6.8b
+        smlal   v15.8h,v9.8b,v7.8b
+        ldr     d9,[x1,104]
+        smull   v2.8h,v4.8b,v0.8b
+        sadalp  v20.4s,v12.8h
+        smull   v3.8h,v4.8b,v1.8b
+        ldr     d4,[x1,48]
+        sadalp  v21.4s,v13.8h
+        smull   v10.8h,v5.8b,v0.8b
+        sadalp  v22.4s,v14.8h
+        smull   v11.8h,v5.8b,v1.8b
+        ldr     d5,[x1,56]
         sadalp  v23.4s, v15.8h
-        ldp     d4, d5, [x1, 48]
-        smlal   v2.8h, v8.8b, v6.8b
-        smlal   v3.8h, v8.8b, v7.8b
-        prfm    PLDL1KEEP, [x15, 128]
-        smlal   v10.8h, v9.8b, v6.8b
-        smlal   v11.8h, v9.8b, v7.8b
-
-        ldp     d8, d9, [x1, 112]
-        smull   v12.8h, v4.8b, v0.8b
-        add     x1, x1, 128
-        sadalp  v24.4s,  v2.8h
-        smull   v13.8h, v4.8b, v1.8b
-        sadalp  v25.4s,  v3.8h
-        smull   v14.8h, v5.8b, v0.8b
-        sadalp  v26.4s, v10.8h
-        smull   v15.8h, v5.8b, v1.8b
-        sadalp  v27.4s, v11.8h
-        smlal   v12.8h, v8.8b, v6.8b
-        ldp     d4, d5, [x1]            // Read B
-        smlal   v13.8h, v8.8b, v7.8b
-        subs    x14, x14, 16
-        smlal   v14.8h, v9.8b, v6.8b
-        ldp     d0, d6, [x13], 16       // Read A0
-        smlal   v15.8h, v9.8b, v7.8b
-
-        ldp     d1, d7, [x15], 16       // Read A1
-        sadalp  v28.4s, v12.8h
-        sadalp  v29.4s, v13.8h
-        sadalp  v30.4s, v14.8h
-        sadalp  v31.4s, v15.8h
-        ldp     d8, d9, [x1, 64]        // Read B
-        movi    v2.8b, 128
+        smlal   v2.8h,v8.8b,v6.8b
+        smlal   v3.8h,v8.8b,v7.8b
+        ldr     d8,[x1,112]
+        smlal   v10.8h,v9.8b,v6.8b
+        smlal   v11.8h,v9.8b,v7.8b
+        ldr     d9,[x1,120]
+        smull   v12.8h,v4.8b,v0.8b
+        add     x1,x1,128
+        sadalp  v24.4s,v2.8h
+        smull   v13.8h,v4.8b,v1.8b
+        ldr     d4,[x1]                 // Read B
+        sadalp  v25.4s,v3.8h
+        smull   v14.8h,v5.8b,v0.8b
+        ldr     d0,[x13],8              // Read A0
+        sadalp  v26.4s,v10.8h
+        smull   v15.8h,v5.8b,v1.8b
+        ldr     d1,[x15],8              // Read A1
+        sadalp  v27.4s,v11.8h
+        smlal   v12.8h,v8.8b,v6.8b
+        ldr     d5,[x1,8]               // Read B
+        smlal   v13.8h,v8.8b,v7.8b
+        ldr     d8,[x1,64]              // Read B
+        smlal   v14.8h,v9.8b,v6.8b
+        ldr     d6,[x13],8              // Read A0
+        smlal   v15.8h,v9.8b,v7.8b
+        ldr     d7,[x15],8              // Read A1
+        sadalp  v28.4s,v12.8h
+        ldr     d9,[x1,72]              // Read B
+        sadalp  v29.4s,v13.8h
+        subs    x14,x14,16
+        sadalp  v30.4s,v14.8h
+        movi    v12.8b,128
+        sadalp  v31.4s,v15.8h
         b.hs    Blockloop
 
-BlockLoopEpilogue                       // remaining 16 input channels
-        eor     v0.8b, v0.8b, v2.8b
-        eor     v6.8b, v6.8b, v2.8b
-        eor     v1.8b, v1.8b, v2.8b
-        eor     v7.8b, v7.8b, v2.8b
-        smull   v2.8h, v4.8b, v0.8b
-        smull   v3.8h, v4.8b, v1.8b
-        smull   v10.8h, v5.8b, v0.8b
-        smull   v11.8h, v5.8b, v1.8b
-        ldp     d4, d5, [x1, 16]
-        smlal   v2.8h, v8.8b, v6.8b
-        smlal   v3.8h, v8.8b, v7.8b
-        smlal   v10.8h, v9.8b, v6.8b
-        smlal   v11.8h, v9.8b, v7.8b
+BlockLoopEpilogue            // remaining 16 input channels
+        eor     v0.8b,v0.8b,v12.8b
+        eor     v1.8b,v1.8b,v12.8b
+        smull   v2.8h,v4.8b,v0.8b
+        smull   v3.8h,v4.8b,v1.8b
+        ldr     d4,[x1,16]
+        smull   v10.8h,v5.8b,v0.8b
+        smull   v11.8h,v5.8b,v1.8b
+        ldr     d5,[x1,24]
+        eor     v6.8b,v6.8b,v12.8b
+        eor     v7.8b,v7.8b,v12.8b
+        smlal   v2.8h,v8.8b,v6.8b
+        smlal   v3.8h,v8.8b,v7.8b
+        ldr     d8,[x1,80]
+        smlal   v10.8h,v9.8b,v6.8b
+        smlal   v11.8h,v9.8b,v7.8b
+        ldr     d9,[x1,88]
+        smull   v12.8h,v4.8b,v0.8b
+        sadalp  v16.4s,v2.8h
+        smull   v13.8h,v4.8b,v1.8b
+        ldr     d4,[x1,32]
+        sadalp  v17.4s,v3.8h
+        smull   v14.8h,v5.8b,v0.8b
+        sadalp  v18.4s,v10.8h
+        smull   v15.8h,v5.8b,v1.8b
+        sadalp  v19.4s,v11.8h
+        ldr     d5,[x1,40]
+        smlal   v12.8h,v8.8b,v6.8b
+        smlal   v13.8h,v8.8b,v7.8b
+        ldr     d8,[x1,96]
+        smlal   v14.8h,v9.8b,v6.8b
+        smlal   v15.8h,v9.8b,v7.8b
+        ldr     d9,[x1,104]
+        smull   v2.8h,v4.8b,v0.8b
+        sadalp  v20.4s,v12.8h
+        smull   v3.8h,v4.8b,v1.8b
+        ldr     d4,[x1,48]
+        sadalp  v21.4s,v13.8h
+        smull   v10.8h,v5.8b,v0.8b
+        sadalp  v22.4s,v14.8h
+        smull   v11.8h,v5.8b,v1.8b
+        sadalp  v23.4s,v15.8h
+        ldr     d5,[x1,56]
+        smlal   v2.8h,v8.8b,v6.8b
+        smlal   v3.8h,v8.8b,v7.8b
+        ldr     d8,[x1,112]
+        smlal   v10.8h,v9.8b,v6.8b
+        smlal   v11.8h,v9.8b,v7.8b
+        ldr     d9,[x1,120]
+        smull   v12.8h,v4.8b,v0.8b
+        sadalp  v24.4s,v2.8h
+        smull   v13.8h,v4.8b,v1.8b
+        sadalp  v25.4s,v3.8h
+        smull   v14.8h,v5.8b,v0.8b
+        sadalp  v26.4s,v10.8h
+        smull   v15.8h,v5.8b,v1.8b
+        sadalp  v27.4s,v11.8h
+        smlal   v12.8h,v8.8b,v6.8b
+        smlal   v13.8h,v8.8b,v7.8b
+        smlal   v14.8h,v9.8b,v6.8b
+        smlal   v15.8h,v9.8b,v7.8b
+        add     x1,x1,128
 
-        ldp     d8, d9, [x1, 80]
-        smull   v12.8h, v4.8b, v0.8b
-        sadalp  v16.4s,  v2.8h
-        smull   v13.8h, v4.8b, v1.8b
-        sadalp  v17.4s,  v3.8h
-        smull   v14.8h, v5.8b, v0.8b
-        sadalp  v18.4s, v10.8h
-        smull   v15.8h, v5.8b, v1.8b
-        sadalp  v19.4s, v11.8h
-        ldp     d4, d5, [x1, 32]
-        smlal   v12.8h, v8.8b, v6.8b
-        smlal   v13.8h, v8.8b, v7.8b
-        smlal   v14.8h, v9.8b, v6.8b
-        smlal   v15.8h, v9.8b, v7.8b
+        sadalp  v28.4s,v12.8h
+        sadalp  v29.4s,v13.8h
+        sadalp  v30.4s,v14.8h
+        sadalp  v31.4s,v15.8h
+        movi    v12.8b,128
+        tbnz    x14,3,InputChannel8
 
-        ldp     d8, d9, [x1, 96]
-        smull   v2.8h, v4.8b, v0.8b
-        sadalp  v20.4s, v12.8h
-        smull   v3.8h, v4.8b, v1.8b
-        sadalp  v21.4s, v13.8h
-        smull   v10.8h, v5.8b, v0.8b
-        sadalp  v22.4s, v14.8h
-        smull   v11.8h, v5.8b, v1.8b
-        sadalp  v23.4s, v15.8h
-        ldp     d4, d5, [x1, 48]
-        smlal   v2.8h, v8.8b, v6.8b
-        smlal   v3.8h, v8.8b, v7.8b
-        smlal   v10.8h, v9.8b, v6.8b
-        smlal   v11.8h, v9.8b, v7.8b
-
-        ldp     d8, d9, [x1, 112]
-        smull   v12.8h, v4.8b, v0.8b
-        sadalp  v24.4s,  v2.8h
-        smull   v13.8h, v4.8b, v1.8b
-        sadalp  v25.4s,  v3.8h
-        smull   v14.8h, v5.8b, v0.8b
-        sadalp  v26.4s, v10.8h
-        smull   v15.8h, v5.8b, v1.8b
-        sadalp  v27.4s, v11.8h
-        smlal   v12.8h, v8.8b, v6.8b
-        smlal   v13.8h, v8.8b, v7.8b
-        smlal   v14.8h, v9.8b, v6.8b
-        smlal   v15.8h, v9.8b, v7.8b
-        add     x1, x1, 128
-
-        sadalp  v28.4s, v12.8h
-        sadalp  v29.4s, v13.8h
-        sadalp  v30.4s, v14.8h
-        sadalp  v31.4s, v15.8h
-        movi    v2.8b, 128
-        tbnz    x14, 3, InChannel8
-
-        subs    x9, x9, 1
+        subs    x9,x9,1
         b.hi    KernelSizeLoop
 
 Requantize
-        dup    v9.8h,w18
+        ldr     w11,[x8,#ConvSymPostProcessParams_ZeroPoint]
         tst     w10,#MLAS_CONV_SYM_FLAG_PER_CHANNEL_SCALE
         beq     BroadcastScaleValue
-        ld1     {v4.4s,v5.4s}, [x12]   // load scale vector
+        ld1     {v4.4s,v5.4s},[x12]     // load scale vector
         b       AccumulatorsToFloat
 
 BroadcastScaleValue
-        ld1r   {v4.4s},[x12]                // load scale Value
-        mov    v5.16b, v4.16b
+        ld1r    {v4.4s},[x12]           // load scale Value
+        mov     v5.16b, v4.16b
 
 AccumulatorsToFloat
         addp    v16.4s,v16.4s,v18.4s
@@ -342,29 +348,29 @@ AccumulatorsToFloat
         addp    v1.4s,v24.4s,v28.4s
         addp    v2.4s,v17.4s,v21.4s
         addp    v3.4s,v25.4s,v29.4s
-        scvtf    v0.4s,v0.4s         // convert to float
-        scvtf    v1.4s,v1.4s
-        scvtf    v2.4s,v2.4s
-        scvtf    v3.4s,v3.4s
-        fmul v0.4s,v0.4s,v4.4s          // multiply by scale
-        fmul v1.4s,v1.4s,v5.4s
-        fmul v2.4s,v2.4s,v4.4s
-        fmul v3.4s,v3.4s,v5.4s
-        fcvtns   v0.4s,v0.4s          // convert to int
-        fcvtns   v1.4s,v1.4s
-        fcvtns   v2.4s,v2.4s
-        fcvtns   v3.4s,v3.4s
+        scvtf   v0.4s,v0.4s             // convert to float
+        scvtf   v1.4s,v1.4s
+        scvtf   v2.4s,v2.4s
+        scvtf   v3.4s,v3.4s
+        fmul    v0.4s,v0.4s,v4.4s       // multiply by scale
+        fmul    v1.4s,v1.4s,v5.4s
+        fmul    v2.4s,v2.4s,v4.4s
+        fmul    v3.4s,v3.4s,v5.4s
+        fcvtns  v0.4s,v0.4s             // convert to int
+        fcvtns  v1.4s,v1.4s
+        dup     v9.8h,w11
+        fcvtns  v2.4s,v2.4s
+        fcvtns  v3.4s,v3.4s
         sqxtn   v0.4h,v0.4s
         sqxtn2  v0.8h,v1.4s
         sqxtn   v2.4h,v2.4s
         sqxtn2  v2.8h,v3.4s
         sqadd   v0.8h,v0.8h,v9.8h
         sqadd   v2.8h,v2.8h,v9.8h
-        sqxtun   v0.8b,v0.8h                 // shorten to int8
-        sqxtun2  v0.16b,v2.8h
-
-        st1     {v0.d}[1],[x5]              // full 2x8 store to c 
-        st1     {v0.8b}, [x2]
+        sqxtun  v0.8b,v0.8h             // shorten to int8
+        sqxtun2 v0.16b,v2.8h
+        st1     {v0.d}[1],[x5]          // full 2x8 store to c 
+        st1     {v0.8b},[x2]
 
 ExitKernel
         EPILOG_RESTORE_REG_PAIR  d14,d15,#48
@@ -373,51 +379,55 @@ ExitKernel
         EPILOG_RESTORE_REG_PAIR  d8,d9,#64!
         EPILOG_RETURN
 
-InChannel8
-        ldr     d0, [x13]
-        ldr     d1, [x15]
-        ldp     d4, d5, [x1]
-        ldp     d6, d7, [x1, 16]
-        eor     v0.8b, v0.8b, v2.8b
-        eor     v1.8b, v1.8b, v2.8b
-        smull   v2.8h, v4.8b, v0.8b
-        smull   v3.8h, v4.8b, v1.8b
-        smull   v10.8h, v5.8b, v0.8b
-        smull   v11.8h, v5.8b, v1.8b
-        smull   v12.8h, v6.8b, v0.8b
-        sadalp  v16.4s,  v2.8h
-        smull   v13.8h, v6.8b, v1.8b
-        sadalp  v17.4s,  v3.8h
-        smull   v14.8h, v7.8b, v0.8b
-        sadalp  v18.4s, v10.8h
-        smull   v15.8h, v7.8b, v1.8b
-        sadalp  v19.4s, v11.8h
-        ldp     d4, d5, [x1, 32]
-        smull   v2.8h, v4.8b, v0.8b
-        sadalp  v20.4s, v12.8h
-        smull   v3.8h, v4.8b, v1.8b
-        sadalp  v21.4s, v13.8h
-        smull   v10.8h, v5.8b, v0.8b
-        sadalp  v22.4s, v14.8h
-        smull   v11.8h, v5.8b, v1.8b
-        sadalp  v23.4s, v15.8h
-        ldp     d6, d7, [x1, 48]
-        smull   v12.8h, v6.8b, v0.8b
-        sadalp  v24.4s,  v2.8h
-        smull   v13.8h, v6.8b, v1.8b
-        sadalp  v25.4s,  v3.8h
-        smull   v14.8h, v7.8b, v0.8b
-        sadalp  v26.4s, v10.8h
-        smull   v15.8h, v7.8b, v1.8b
-        sadalp  v27.4s, v11.8h
-        add     x1, x1, 64
-        sadalp  v28.4s, v12.8h
-        sadalp  v29.4s, v13.8h
-        sadalp  v30.4s, v14.8h
-        sadalp  v31.4s, v15.8h
+InputChannel8
+        ldr     d0,[x13]
+        ldr     d1,[x15]
+        ldr     d4,[x1]
+        ldr     d5,[x1,8]
+        ldr     d6,[x1,16]
+        ldr     d7,[x1,24]
+        eor     v0.8b,v0.8b,v12.8b
+        eor     v1.8b,v1.8b,v12.8b
+        smull   v2.8h,v4.8b,v0.8b
+        smull   v3.8h,v4.8b,v1.8b
+        ldr     d4,[x1,32]
+        smull   v10.8h,v5.8b,v0.8b
+        smull   v11.8h,v5.8b,v1.8b
+        ldr     d5,[x1,40]
+        smull   v12.8h,v6.8b,v0.8b
+        sadalp  v16.4s,v2.8h
+        smull   v13.8h,v6.8b,v1.8b
+        ldr     d6,[x1,48]
+        sadalp  v17.4s,v3.8h
+        smull   v14.8h,v7.8b,v0.8b
+        sadalp  v18.4s,v10.8h
+        smull   v15.8h,v7.8b,v1.8b
+        ldr     d7,[x1,56]
+        sadalp  v19.4s,v11.8h
+        smull   v2.8h,v4.8b,v0.8b
+        sadalp  v20.4s,v12.8h
+        smull   v3.8h,v4.8b,v1.8b
+        sadalp  v21.4s,v13.8h
+        smull   v10.8h,v5.8b,v0.8b
+        sadalp  v22.4s,v14.8h
+        smull   v11.8h,v5.8b,v1.8b
+        sadalp  v23.4s,v15.8h
+        smull   v12.8h,v6.8b,v0.8b
+        sadalp  v24.4s,v2.8h
+        smull   v13.8h,v6.8b,v1.8b
+        sadalp  v25.4s,v3.8h
+        smull   v14.8h,v7.8b,v0.8b
+        sadalp  v26.4s,v10.8h
+        smull   v15.8h,v7.8b,v1.8b
+        sadalp  v27.4s,v11.8h
+        add     x1,x1,64
+        sadalp  v28.4s,v12.8h
+        sadalp  v29.4s,v13.8h
+        sadalp  v30.4s,v14.8h
+        sadalp  v31.4s,v15.8h
 
         // ks loop
-        subs    x9, x9, 1
+        subs    x9,x9,1
         b.hi    KernelSizeLoop
         b       Requantize
 
