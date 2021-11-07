89c5cbcb2fa0a33f7193e1b9fb99a21518137c0b
Chi Lo <Chi.Lo@microsoft.com>
2021-11-07 19:57:29 +0000

    remove the exposure of APIs

--- onnxruntime/python/tools/quantization/__init__.py
+++ onnxruntime/python/tools/quantization/__init__.py
@@ -2,5 +2,3 @@ from .quantize import quantize, quantize_static, quantize_dynamic, quantize_qat
 from .quantize import QuantizationMode
 from .calibrate import CalibrationDataReader, CalibraterBase, MinMaxCalibrater, create_calibrator, CalibrationMethod
 from .quant_utils import QuantType, QuantFormat, write_calibration_table
-from .registry import QLinearOpsRegistry
-from .qdq_quantizer import QDQQuantizer
