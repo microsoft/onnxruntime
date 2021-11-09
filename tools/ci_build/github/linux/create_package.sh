a48530635ce170826caf5a720a5302226a179bf9
Lifang Zhang <lifa08.zhang@gmail.com>
2021-11-09 05:07:45 +0200

    Split long line and replace legacy backticks with "$()"

--- tools/ci_build/github/linux/create_package.sh
+++ tools/ci_build/github/linux/create_package.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
-SCRIPT=`realpath $0`
-SCRIPT_DIR=`dirname $SCRIPT`
-TOP_SRC_DIR=`realpath $SCRIPT_DIR/../../../../`
+SCRIPT=$(realpath $0)
+SCRIPT_DIR=$(dirname $SCRIPT)
+TOP_SRC_DIR=$(realpath $SCRIPT_DIR/../../../../)
 mkdir -p $HOME/.cache/onnxruntime
 if [ -z "$BUILD_ARTIFACTSTAGINGDIRECTORY" ]; then
   sudo mkdir -p /data/a
@@ -14,10 +14,21 @@ if [ -z "$BUILD_BINARIESDIRECTORY" ]; then
   BUILD_BINARIESDIRECTORY="/data/b"
 fi
 
+docker_run_options=(
+  --rm
+  -e AZURESASKEY
+  --volume "$HOME/.cache/onnxruntime:/root/.cache/onnxruntime"
+  --volume "${BUILD_BINARIESDIRECTORY}:/root/rpmbuild"
+  --volume "${BUILD_ARTIFACTSTAGINGDIRECTORY}:/data/a"
+  --volume "${HOME}/.ccache:/root/.ccache"
+  --volume "${TOP_SRC_DIR}:/data/onnxruntime"
+  --workdir /data/b
+)
 
+run_script=/data/onnxruntime/tools/ci_build/github/linux/create_package_inside_docker.sh
 for version in '23'; do
   docker_image=fedora$version
-  cd $SCRIPT_DIR/docker
+  cd "${SCRIPT_DIR}/docker"
   docker build --pull -t $docker_image --build-arg OS_VERSION=$version -f Dockerfile.fedora .
-  docker run --rm -e AZURESASKEY --volume "$HOME/.cache/onnxruntime:/root/.cache/onnxruntime" -v $BUILD_BINARIESDIRECTORY:/root/rpmbuild -v $BUILD_ARTIFACTSTAGINGDIRECTORY:/data/a -v $HOME/.ccache:/root/.ccache -v $TOP_SRC_DIR:/data/onnxruntime -w /data/b $docker_image /data/onnxruntime/tools/ci_build/github/linux/create_package_inside_docker.sh
+  docker run  "${docker_run_options[@]}" $docker_image ${run_script}
 done
