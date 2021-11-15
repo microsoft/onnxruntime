d4541425f1ae009b86615168f88b251c73e3a983
Chen Fu <1316708+chenfucn@users.noreply.github.com>
2021-11-15 12:18:43 -0800

    Adding ARM64 depthwise convolution kernel for symmetric quantization (#9655)
    Adding ARM64 depthwise convolution kernel for symmetric quantization
    
    Motivation and Context
    Two improvements against current kernel code :
    
    1. Signed int8 based instructions, no need to extend from 8b to 16b before multiplication.
    2. Unrolled loop with manual software pipelining
    
    Co-authored-by: Chen Fu <fuchen@microsoft.com>

--- tools/ci_build/github/linux/ort_minimal/build_minimal_ort_android_baseline_and_report_bin_size.sh
+++ tools/ci_build/github/linux/ort_minimal/build_minimal_ort_android_baseline_and_report_bin_size.sh
@@ -32,7 +32,7 @@ python3 /onnxruntime_src/tools/ci_build/build.py \
     --include_ops_by_config /home/onnxruntimedev/.test_data/include_no_operators.config
 
 # set current size limit to BINARY_SIZE_LIMIT_IN_BYTES.
-BINARY_SIZE_LIMIT_IN_BYTES=1305000
+BINARY_SIZE_LIMIT_IN_BYTES=1306224
 echo "The current preset binary size limit is $BINARY_SIZE_LIMIT_IN_BYTES"
 python3 /onnxruntime_src/tools/ci_build/github/linux/ort_minimal/check_build_binary_size.py \
     --threshold=$BINARY_SIZE_LIMIT_IN_BYTES \
