c32f32bcf0e83e437782d60552446a3c894c77fa
sfatimar <sahar.fatima@intel.com>
2021-11-16 03:11:12 +0530

    Openvino ep 2021.4 v3.3 (#9588)
    * Added checks for Hetero/Multi
    
    Signed-off-by: MaajidKhan <n.maajidkhan@gmail.com>
    
    * Remote Context Plugin
    
    * changes for IO Buffer plugin
    
    * erronous couts added
    
    * erronous entry rectified
    
    * Set the Openvino OP Buffer also as output
    
    * Enable AUTO plugin in OpenVINO EP
    
    Signed-off-by: MaajidKhan <n.maajidkhan@gmail.com>
    
    * Remote Context Plugin
    
    * changes for IO Buffer plugin
    
    * erronous couts added
    
    * erronous entry rectified
    
    * Added checks for Hetero/Multi
    
    Signed-off-by: MaajidKhan <n.maajidkhan@gmail.com>
    
    * Set the Openvino OP Buffer also as output
    
    * Enable AUTO plugin in OpenVINO EP
    
    Signed-off-by: MaajidKhan <n.maajidkhan@gmail.com>
    
    * Please commit error message and rectification of param.context
    
    * Alignment fixed
    
    Signed-off-by: MaajidKhan <n.maajidkhan@gmail.com>
    
    * Changed the string to OpenVINO_GPU
    
    * hanged OpenVINO to to OpenVINO_CPU
    
    * Onnxruntime updated API for memory location
    
    * Removing Duplicate LOG Error
    
    * Tensor.h removed DeviceType function. Updated comment
    
    * API Comments updated
    
    * Removing changes to Provider Indo
    
    * Erronous commit
    
    * Removing Extra logs
    
    * Merge CMAKE
    
    * Not copy from a  local location
    
    * Duplicate Entry
    
    * Remove extra line
    
    Co-authored-by: MaajidKhan <n.maajidkhan@gmail.com>

--- include/onnxruntime/core/framework/allocator.h
+++ include/onnxruntime/core/framework/allocator.h
@@ -40,6 +40,9 @@ constexpr const char* CUDA_PINNED = "CudaPinned";
 constexpr const char* DML = "DML";
 constexpr const char* MIGRAPHX = "MIGraphX";
 constexpr const char* MIGRAPHX_PINNED = "MIGraphXPinned";
+constexpr const char* OpenVINO_CPU = "OpenVINO_CPU";
+constexpr const char* OpenVINO_GPU = "OpenVINO_GPU";
+
 
 constexpr size_t kAllocAlignment = 256;
 
