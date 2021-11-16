16813d3fcec47da05d359d32722d823dd4c0afa1
Yufeng Li <liyufeng1987@gmail.com>
2021-11-16 14:27:48 -0800

    fix test KernelDefHashTest failure

--- onnxruntime/test/providers/kernel_def_hash_test.cc
+++ onnxruntime/test/providers/kernel_def_hash_test.cc
@@ -174,6 +174,9 @@ TEST(KernelDefHashTest, ExpectedCpuKernelDefHashes) {
 #if defined(ENABLE_TRAINING_OPS)
     AppendKernelDefHashesFromFile(ORT_TSTR("testdata/kernel_def_hashes/training_ops.cpu.json"), result);
 #endif  // ENABLE_TRAINING_OPS
+#if defined(MLAS_TARGET_ARM_ANY)
+    AppendKernelDefHashesFromFile(ORT_TSTR("testdata/kernel_def_hashes/arm_ops.cpu.json"), result);
+#endif  // MLAS_TARGET_ARM_ANY
     // TODO also handle kernels enabled by these symbols: BUILD_MS_EXPERIMENTAL_OPS
     std::sort(result.begin(), result.end());
     return result;
