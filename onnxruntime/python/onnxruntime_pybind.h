8c518db0eb0895d6d4ea06038e73ec131be7b358
Scott McKay <skottmckay@gmail.com>
2021-11-18 12:19:20 +1000

    Add a little more detail to the comment in the intermediate header.

--- onnxruntime/python/onnxruntime_pybind.h
+++ onnxruntime/python/onnxruntime_pybind.h
@@ -3,11 +3,14 @@
 
 #pragma once
 
-// need to include corecrt.h before pybind so that the _STL_ASSERT macros are defined.
-// pybind11/pybind11.h includes pybind11/detail/common.h, which undefines _DEBUG
-// whilst including the Python headers, which alters how the _STL_ASSERT macros are created.
+// If performing a debug build with VS2022 (_MSC_VER == 1930) we need to include corecrt.h before pybind
+// so that the _STL_ASSERT macro is defined in a compatible way.
+//
+// pybind11/pybind11.h includes pybind11/detail/common.h, which undefines _DEBUG whilst including the Python headers
+// (which in turn include corecrt.h). This alters how the _STL_ASSERT macro is defined and causes the build to fail.
 //
 // see https://github.com/microsoft/onnxruntime/issues/9735
+//
 #if defined(_MSC_VER) && defined(_DEBUG) && _MSC_VER >= 1930
 #include <corecrt.h>
 #endif
