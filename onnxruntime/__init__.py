830812071ec9007d6e7f2f4c86835f453740f42d
Han Guangyun <guangyunhan@microsoft.com>
2021-11-18 15:42:28 +0800

    Add `set_default_logger_verbosity` api.

--- onnxruntime/__init__.py
+++ onnxruntime/__init__.py
@@ -21,7 +21,7 @@ __author__ = "Microsoft"
 # the saved exception is raised after device version validation.
 try:
     from onnxruntime.capi._pybind_state import get_all_providers, get_available_providers, get_device, set_seed, \
-        RunOptions, SessionOptions, set_default_logger_severity, enable_telemetry_events, disable_telemetry_events, \
+        RunOptions, SessionOptions, set_default_logger_severity, set_default_logger_verbosity, enable_telemetry_events, disable_telemetry_events, \
         NodeArg, ModelMetadata, GraphOptimizationLevel, ExecutionMode, ExecutionOrder, SessionIOBinding, \
         OrtAllocatorType, OrtMemType, OrtArenaCfg, OrtMemoryInfo, create_and_register_allocator,  OrtSparseFormat
     import_capi_exception = None
