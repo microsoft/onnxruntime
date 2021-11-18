3fd0ab53107661e33dbde4983104671639f53582
Sergii Dymchenko <sedymche@microsoft.com>
2021-11-18 00:55:36 +0000

    Simplify and update performance investigation script.

--- orttraining/tools/scripts/performance_investigation.py
+++ orttraining/tools/scripts/performance_investigation.py
@@ -1,106 +1,76 @@
 import argparse
-import glob
-import sys
-
 import onnx
 
 parser = argparse.ArgumentParser(description='ONNX file analyzer for performance investigation.')
-parser.add_argument('onnx_file', type=str, nargs='?',
-                    help='ONNX file to analyze; if empty, the program looks for ONNX files saved by ORTModule in the current dir')
+parser.add_argument('onnx_file', type=str, help='ONNX file to analyze')
 args = parser.parse_args()
 
-def process_file(onnx_file, torch_exported_file=False, pre_grad_optimized_file=False, optimized_file=False, execution_model_file=False):
-    print(f"Processing {onnx_file}...")
+
+
+def process_file(onnx_file):
     model = onnx.load(onnx_file)
 
-    input_to_nodes = {}
+    # Map from output arg to the producer of the output.
     output_to_node = {}
     for node in model.graph.node:
-        for i in node.input:
-            input_to_nodes[i] = input_to_nodes.get(i, []) + [node]
         for o in node.output:
             output_to_node[o] = node
 
     aten_ops = []
+    python_ops = []
+    memcpu_ops = []
+    cast_ops = []
     msgs = []
 
-    visited = set([])
-    def dfs(node, path):
-        if node.name in visited:
-            return
-        visited.add(node.name)
-
+    for node in model.graph.node:
+        if "Memcpy" in node.op_type:
+            memcpu_ops.append(f"{node.op_type} {node.name}")
+        if node.op_type == "Cast":
+            cast_ops.append(f"{node.name}")
         if node.op_type == "ATenOp":
-            aten_ops.append(f"{node.name}: {node.attribute[0].s.decode('utf-8')}")
-
-        # Search for 'memcpy' in the *_execution_model_<mode>.onnx.
-        # In the ideal case, there should zero memcpy node in the final optimized training graph.
-        if execution_model_file:
-            if "Memcpy" in node.op_type:
-                msgs.append(f"Memcpy node {node.name} found.")
-
-        # Look for a node sandwiched by MemcpyToHost and MemcpyFromHost in the *_optimized_<mode>.onnx graph
-        if optimized_file:
-            if node.op_type == "MemcpyFromHost" and len(path) > 1 and path[-2].op_type == "MemcpyToHost":
-                msgs.append(f"CUDA Kernel Missing for an Op {path[-1].op_type} for {path[-1].name} node.")
-
-        # Look for node surrounded (sandwiched) by Casts node, see if ORT has already implemented fp16-saft kernels for them
-        if optimized_file:
-            if node.op_type == "Cast" and len(path) > 1 and path[-2].op_type == "Cast":
-                msgs.append(f"Excessive casts around {path[-1].name} node.")        
-
-        # Look for (Simplified)LayerNormalization in *_pre_grad_optimized_<mode>.onnx graph.
-        # The layernorm subgraph (search for Pow node to begin with) should be fused into a single node.
-        if pre_grad_optimized_file:
-            if node.op_type == "LayerNormalization" or node.op_type == "SimplifiedLayerNormalization":
-                for prev in path[::-1]:
-                    if prev.op_type == "Pow":
-                        msgs.append(f"Standalone {node.op_type} {node.name} found. The layernorm subgraph starting with {prev.name} should be fused into a single node.")
-                    break
-
-        # Look for (Fast)Gelu in *_pre_grad_optimized_<mode>.onnx graph.
-        # The gelu subgraph (search for Erf node to begin with) should be fused into a single node.
-        if pre_grad_optimized_file:
-            if node.op_type == "Gelu" or node.op_type == "FastGelu":
-                for prev in path[::-1]:
-                    if prev.op_type == "Erf":
-                        msgs.append(f"Standalone {node.op_type} {node.name} found. The gelu subgraph starting with {prev.name} should be fused into a single node.")
-                    break
+            for attr in node.attribute:
+                if attr.name == "name":
+                    aten_ops.append(f"{node.name}: {attr.s.decode('utf-8')}")
+        if node.op_type == "PythonOp":
+            for attr in node.attribute:
+                if attr.name == "name":
+                    python_ops.append(f"{node.name}: {attr.s.decode('utf-8')}")
 
         # Look for stand-alone Dropout node in *_execution_model_<mode>.onnx graph.
         # Examine whether it should be fused with surrounding Add ops into BiasDropout node.
-        if execution_model_file:
-            if node.op_type == "Dropout" and path and path[-1].op_type == "Add":
-                msgs.append(f"Examine whether {node.name} should be fused with the leading {path[-1].name} op into BiasDropout node.")
+        if node.op_type == "Dropout" and len(node.input) == 1:
+            prev = output_to_node[node.input[0]]
+            if prev.op_type == "Add":
+                msgs.append(f"Examine whether {node.name} should be fused with the leading {prev.name} op into BiasDropout node.")
 
         # Look for stand-alone Softmax node in *_execution_model_<mode>.onnx graph.
         # Examine whether it should be fused with the leading Add ops into BiasSoftmax node.
-        if execution_model_file:
-            if node.op_type == "Softmax" and path and path[-1].op_type == "Add":
-                msgs.append(f"Examine whether {node.name} should be fused with the leading {path[-1].name} op into BiasSoftmax node.")
-
-        for o in node.output:
-            for next_node in input_to_nodes.get(o, []):
-                dfs(next_node, path + [node])
-
-    for i in model.graph.input:
-        if i.name in output_to_node:
-            continue
-        for node in input_to_nodes.get(i.name, []):
-            if node.name not in visited:
-                dfs(node, [])
-
-    for i in model.graph.input:
-        if i.name in output_to_node:
-            continue
-        for node in input_to_nodes.get(i.name, []):
-            if node.name not in visited:
-                dfs(node, [])
+        if node.op_type == "Softmax" and len(node.input) == 1:
+            prev = output_to_node[node.input[0]]
+            if prev.op_type == "Add":
+                msgs.append(f"Examine whether {node.name} should be fused with the leading {prev.name} op into BiasSoftmax node.")
 
     if aten_ops:
-        print("ATenOp(s) found:")
-        print(10 * '-')
+        print("ATenOp found:")
         for line in aten_ops:
+            print(line)
+        print(10 * '-')
+
+    if python_ops:
+        print("PythonOp found:")
+        for line in python_ops:
+            print(line)
+        print(10 * '-')
+
+    if memcpu_ops:
+        print("Memcpu ops found:")
+        for line in memcpu_ops:
+            print(line)
+        print(10 * '-')
+
+    if cast_ops:
+        print("Cast ops found:")
+        for line in cast_ops:
             print(line)
         print(10 * '-')
 
@@ -109,25 +79,7 @@ def process_file(onnx_file, torch_exported_file=False, pre_grad_optimized_file=F
 
 
 def main():
-    if args.onnx_file:
-        process_file(args.onnx_file, True, True, True, True)
-    else:
-        torch_exported_files = glob.glob("*_torch_exported_*.onnx")
-        for file in torch_exported_files:
-            process_file(file, torch_exported_file=True)
-
-        pre_grad_optimized_files = glob.glob("*_pre_grad_optimized_*.onnx")
-        for file in pre_grad_optimized_files:
-            process_file(file, pre_grad_optimized_file=True)
-
-        optimized_files = glob.glob("*_optimized_*.onnx")
-        for file in optimized_files:
-            if file not in pre_grad_optimized_files:
-                process_file(file, optimized_file=True)
-
-        execution_model_files = glob.glob("*_execution_model_file_*.onnx")
-        for file in execution_model_files:
-            process_file(file, execution_model_file=True)
+    process_file(args.onnx_file)
 
 if __name__ == "__main__":
     main()
