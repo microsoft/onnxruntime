745cb1d1e9468e0b80caf1f1205448c3f12b68d5
xavier dupré <xavier.dupre@gmail.com>
2021-11-18 16:41:46 +0100

    removes ignored and deprecated parameter

--- orttraining/orttraining/python/ort_trainer.py
+++ orttraining/orttraining/python/ort_trainer.py
@@ -363,7 +363,6 @@ def convert_model_loss_fn_to_onnx(model, loss_fn, model_desc, device, inputs, op
                        output_names=output_names,
                        opset_version=opset_version,
                        dynamic_axes=dynamic_axes,
-                       _retain_param_name=True,
                        example_outputs=tuple(sample_outputs),
                        do_constant_folding=False,
                        **other_export_options)
