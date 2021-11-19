aa86bbd01be7fa4a6aa3ec0f07277d2039db1964
hariharans29 <shariharan91@gmail.com>
2021-11-19 01:23:33 -0800

    More changes

--- onnxruntime/core/providers/cuda/tensor/identity_op.h
+++ onnxruntime/core/providers/cuda/tensor/identity_op.h
@@ -43,7 +43,7 @@ class IdentityOp final : public CudaKernel {
             float f = a[i];
             delete[] a;
             //return Status(common::ONNXRUNTIME, common::FAIL, std::to_string(f));
-            std::cout << a[i;]
+            std::cout << a[i];
           }
         }
 
