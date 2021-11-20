fb4e2f52050560a1fb2e4d206c31d7bfb8b99b36
hariharans29 <shariharan91@gmail.com>
2021-11-19 16:57:13 -0800

    a

--- onnxruntime/core/providers/cuda/tensor/identity_op.h
+++ onnxruntime/core/providers/cuda/tensor/identity_op.h
@@ -34,7 +34,6 @@ class IdentityOp final : public CudaKernel {
       void* target = Y->MutableDataRaw(X_type);
       //If source and target pointers are not equal, we need to copy the data.
       if (target != source) {
-        /*
         size_t s = X->Shape().Size() * X->DataType()->Size();
         float* a = new float[s];
         cudaMemcpy(a, source, s, cudaMemcpyDeviceToHost);
@@ -50,8 +49,7 @@ class IdentityOp final : public CudaKernel {
           return Status(common::ONNXRUNTIME, common::FAIL,
                         "IdentityOp cuda: found non-one.");
         }
-        */
-        cudaDeviceSynchronize();
+        //cudaDeviceSynchronize();
         CUDA_RETURN_IF_ERROR(cudaMemcpyAsync(target, source, X->Shape().Size() * X->DataType()->Size(), cudaMemcpyDeviceToDevice, Stream()));
       }
 
