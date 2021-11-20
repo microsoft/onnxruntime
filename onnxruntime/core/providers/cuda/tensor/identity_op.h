5350daea09a14433000eee52a3ebae66cf75ad06
hariharans29 <shariharan91@gmail.com>
2021-11-19 17:45:45 -0800

    a

--- onnxruntime/core/providers/cuda/tensor/identity_op.h
+++ onnxruntime/core/providers/cuda/tensor/identity_op.h
@@ -51,7 +51,7 @@ class IdentityOp final : public CudaKernel {
                         "IdentityOp cuda: found non-one.");
         }
         */
-        cudaDeviceSynchronize();
+        cudaStreamSynchronize(0);
         CUDA_RETURN_IF_ERROR(cudaMemcpyAsync(target, source, X->Shape().Size() * X->DataType()->Size(), cudaMemcpyDeviceToDevice, Stream()));
       }
 
