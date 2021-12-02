8c90709b5f636b721289f66f0423111f5e6bcc72
Lei Zhang <zhang.huanning@hotmail.com>
2021-12-01 16:38:52 -0800

    Add quantization tool with s8s8 support   * Add unittest for existing s8s8 support operators   * Comment ready unittest for upcomming s8s8 operator (ConvInteger, and Resize)   * Minor change on quantization tools

--- onnxruntime/test/python/quantization/test_conv_dynamic.py
+++ onnxruntime/test/python/quantization/test_conv_dynamic.py
@@ -11,17 +11,18 @@ import onnx
 import onnxruntime
 import numpy as np
 from onnx import helper, TensorProto, numpy_helper
-from onnxruntime.quantization import quantize_dynamic
-from op_test_utils import TestDataFeeds, check_model_correctness, check_op_type_count, check_op_type_order
+from onnxruntime.quantization import quantize_dynamic, QuantType
+from op_test_utils import TestDataFeeds, check_model_correctness, check_op_type_count, check_op_type_order, check_qtype_by_node_type
 
 
 def generate_input_initializer(tensor_shape, tensor_dtype, input_name):
-  '''
-  Helper function to generate initializers for test inputs
-  '''
-  tensor = np.random.normal(0, 0.3, tensor_shape).astype(tensor_dtype)
-  init = numpy_helper.from_array(tensor, input_name)
-  return init
+    '''
+    Helper function to generate initializers for test inputs
+    '''
+    tensor = np.random.normal(0, 0.3, tensor_shape).astype(tensor_dtype)
+    init = numpy_helper.from_array(tensor, input_name)
+    return init
+
 
 class TestONNXModel(unittest.TestCase):
     def construct_model(self, model_path):
@@ -52,19 +53,31 @@ class TestONNXModel(unittest.TestCase):
         model = helper.make_model(graph, opset_imports=[helper.make_opsetid("", 13)])
         onnx.save(model, model_path)
 
-    def dynamic_quant_conv(self, model_fp32_path, model_int8_path):
-        quantize_dynamic(model_fp32_path, model_int8_path)
-        quant_nodes = {'ConvInteger' : 2}
+    def dynamic_quant_conv_test(self, activation_type, weight_type, extra_options={}):
+        np.random.seed(1)
+        model_fp32_path = 'conv_bias.fp32.onnx'
+        self.construct_model(model_fp32_path)
+
+        activation_proto_qtype = TensorProto.UINT8 if activation_type == QuantType.QUInt8 else TensorProto.INT8
+        activation_type_str = 'u8' if (activation_type == QuantType.QUInt8) else 's8'
+        weight_type_str = 'u8' if (weight_type == QuantType.QUInt8) else 's8'
+        model_int8_path = 'conv_bias.quant.{}{}.onnx'.format(activation_type_str, weight_type_str)
+
+        quantize_dynamic(model_fp32_path, model_int8_path,
+                         activation_type=activation_type, weight_type=weight_type, extra_options=extra_options)
+        quant_nodes = {'ConvInteger': 2}
         check_op_type_count(self, model_int8_path, **quant_nodes)
+        qnode_io_qtypes = {'ConvInteger': [['i', 2, activation_proto_qtype]]}
+        check_qtype_by_node_type(self, model_int8_path, qnode_io_qtypes)
         check_model_correctness(self, model_fp32_path, model_int8_path, {'input': np.random.rand(4, 2, 8, 8).astype(np.float32)})
 
     def test_quant_conv(self):
-        np.random.seed(1)
-        model_fp32_path = 'conv_bias.fp32.onnx'
-        model_int8_path = 'conv_bias.quant.onnx'
-        self.construct_model(model_fp32_path)
+        self.dynamic_quant_conv_test(QuantType.QUInt8, QuantType.QUInt8, extra_options={})
 
-        self.dynamic_quant_conv(model_fp32_path, model_int8_path)
+    # TODO: uncomment following after ConvInteger s8 supportted
+    # def test_quant_conv_s8s8(self):
+    #    self.dynamic_quant_conv_test(QuantType.QInt8, QuantType.QInt8, extra_options={'ActivationSymmetric': True})
+
 
 if __name__ == '__main__':
     unittest.main()
