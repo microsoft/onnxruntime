02aa16e3ea2faa2a893d279c8e323ec3f672bc0b
Chi Lo <54722500+chilo-ms@users.noreply.github.com>
2021-12-03 10:09:34 -0800

    QDQ tool modification part3 (#9904)
    * refine per channel quantization for qdq
    
    * remove old option
    
    * add comment
    
    * add import itertools

--- onnxruntime/python/tools/quantization/onnx_quantizer.py
+++ onnxruntime/python/tools/quantization/onnx_quantizer.py
@@ -47,8 +47,6 @@ class ONNXQuantizer:
         is_weight_int8 = weight_qType == QuantType.QInt8
         self.is_weight_symmetric = is_weight_int8 if 'WeightSymmetric' not in self.extra_options else self.extra_options['WeightSymmetric']
         self.is_activation_symmetric = False if 'ActivationSymmetric' not in self.extra_options else self.extra_options['ActivationSymmetric']
-        self.op_types_support_per_channel_quantization = [] if 'OpTypesSupportPerChannelQuantization' not in extra_options \
-                                                        else extra_options['OpTypesSupportPerChannelQuantization']
 
         self.input_qType = onnx_proto.TensorProto.INT8 if input_qType == QuantType.QInt8 else onnx_proto.TensorProto.UINT8
         self.weight_qType = onnx_proto.TensorProto.INT8 if weight_qType == QuantType.QInt8 else onnx_proto.TensorProto.UINT8
