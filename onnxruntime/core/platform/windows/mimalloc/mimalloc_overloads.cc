82462f2457f07e10a73e3f13bd39431c80ca4a57
Dmitri Smirnov <dmitrism@microsoft.com>
2021-12-06 15:00:04 -0800

    Adjust defines

--- onnxruntime/core/platform/windows/mimalloc/mimalloc_overloads.cc
+++ onnxruntime/core/platform/windows/mimalloc/mimalloc_overloads.cc
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-#ifdef USE_MIMALLOC_ARENA_ALLOCATOR
+#ifdef USE_MIMALLOC
 // Add definition for new/delete
 #include <mimalloc-new-delete.h>
 #endif
