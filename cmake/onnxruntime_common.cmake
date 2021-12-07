3695c120a9310fa4e5623de13f77edffefee96b0
RandySheriffH <randysheriff@hotmail.com>
2021-12-07 13:01:39 -0800

    add submodule

--- cmake/onnxruntime_common.cmake
+++ cmake/onnxruntime_common.cmake
@@ -105,6 +105,9 @@ target_include_directories(onnxruntime_common
         ${OPTIONAL_LITE_INCLUDE_DIR})
 
 target_link_libraries(onnxruntime_common safeint_interface Boost::mp11)
+target_include_directories(onnxruntime_common PUBLIC "${CMAKE_CURRENT_SOURCE_DIR}/external/pthreadpool/include")
+add_subdirectory("${CMAKE_CURRENT_SOURCE_DIR}/external/pthreadpool" EXCLUDE_FROM_ALL)
+target_link_libraries(onnxruntime_common pthreadpool)
 
 if(NOT WIN32)
   target_include_directories(onnxruntime_common PUBLIC "${CMAKE_CURRENT_SOURCE_DIR}/external/nsync/public")
