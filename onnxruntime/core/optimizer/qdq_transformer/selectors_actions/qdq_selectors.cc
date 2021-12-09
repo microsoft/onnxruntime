bc0f2d173ace7be561c4b828f93fa55574205302
Yufeng Li <liyufeng1987@gmail.com>
2021-12-08 18:13:13 -0800

    enable s8s8 QDQ fusion on ARM (#9961)
    * enable s8s8 QDQ fusion on ARM devices

--- onnxruntime/core/optimizer/qdq_transformer/selectors_actions/qdq_selectors.cc
+++ onnxruntime/core/optimizer/qdq_transformer/selectors_actions/qdq_selectors.cc
@@ -114,10 +114,8 @@ bool UnarySelector::Check(const GraphViewer& graph_viewer, const Node& node,
   int32_t dt_input = dq_nodes[0]->InputDefs()[0]->TypeAsProto()->tensor_type().elem_type();
   int32_t dt_output = q_nodes[0]->OutputDefs()[0]->TypeAsProto()->tensor_type().elem_type();
 
-  return ((dt_input == ONNX_NAMESPACE::TensorProto_DataType::TensorProto_DataType_UINT8 ||
-           (int8_allowed_ && dt_input == ONNX_NAMESPACE::TensorProto_DataType::TensorProto_DataType_INT8))) &&
-         ((dt_output == ONNX_NAMESPACE::TensorProto_DataType::TensorProto_DataType_UINT8 ||
-           (int8_allowed_ && dt_output == ONNX_NAMESPACE::TensorProto_DataType::TensorProto_DataType_INT8)));
+  return dt_input == dt_output &&
+         (int8_allowed_ || dt_input == ONNX_NAMESPACE::TensorProto_DataType::TensorProto_DataType_UINT8);
 }
 
 bool BinarySelector::Check(const GraphViewer& graph_viewer,
@@ -128,7 +126,6 @@ bool BinarySelector::Check(const GraphViewer& graph_viewer,
     return false;
   }
 
-  // Currently QLinearAdd and QLinearMul only support activation type uint8_t
   int32_t dt_input_1 = dq_nodes[0]->InputDefs()[0]->TypeAsProto()->tensor_type().elem_type();
   int32_t dt_input_2 = dq_nodes[1]->InputDefs()[0]->TypeAsProto()->tensor_type().elem_type();
   int32_t dt_output = q_nodes[0]->OutputDefs()[0]->TypeAsProto()->tensor_type().elem_type();
@@ -168,12 +165,18 @@ bool ConvSelector::Check(const GraphViewer& graph_viewer,
     return false;
   }
 
-  // Currently QLinearConv only support activation type uint8_t and output type uint8_t
+  // input and output types need to be same
   int32_t dt_input = dq_nodes[0]->InputDefs()[0]->TypeAsProto()->tensor_type().elem_type();
+  int32_t dt_weight = dq_nodes[1]->InputDefs()[0]->TypeAsProto()->tensor_type().elem_type();
   int32_t dt_output = q_nodes[0]->OutputDefs()[0]->TypeAsProto()->tensor_type().elem_type();
-  if (dt_input != ONNX_NAMESPACE::TensorProto_DataType::TensorProto_DataType_UINT8 ||
-      dt_output != ONNX_NAMESPACE::TensorProto_DataType::TensorProto_DataType_UINT8) {
+  if (dt_input != dt_output) {
     return false;
+  }
+
+  if(dt_input == ONNX_NAMESPACE::TensorProto_DataType::TensorProto_DataType_INT8) {
+    if(!int8_allowed_ || dt_weight != dt_input) {
+      return false;
+    }
   }
 
   if (dq_nodes.size() < 3) {  // no bias
@@ -196,6 +199,15 @@ bool MatMulSelector::Check(const GraphViewer& graph_viewer,
     return false;
   }
 
+  int32_t dt_input = dq_nodes[0]->InputDefs()[0]->TypeAsProto()->tensor_type().elem_type();
+  int32_t dt_weight = dq_nodes[1]->InputDefs()[0]->TypeAsProto()->tensor_type().elem_type();
+
+  if(dt_input == ONNX_NAMESPACE::TensorProto_DataType::TensorProto_DataType_INT8) {
+    if(!int8_allowed_ || dt_weight != dt_input) {
+      return false;
+    }
+  }
+
   // potential match for QLinearMatMul or MatMulIntegerToFloat
   bool qlinear = !q_nodes.empty();
 
@@ -206,16 +218,10 @@ bool MatMulSelector::Check(const GraphViewer& graph_viewer,
     }
 
     int32_t dt_output = q_nodes[0]->OutputDefs()[0]->TypeAsProto()->tensor_type().elem_type();
-    if (dt_output != ONNX_NAMESPACE::TensorProto_DataType::TensorProto_DataType_UINT8) {
-      return false;
-    }
-  } else {
-    // MatMulIntegerToFloat has no Q node, so no call to CheckQDQNodes
+    return dt_input == dt_output;
   }
 
-  // Currently Quant MatMul only support activation type uint8_t
-  int32_t dt_input = dq_nodes[0]->InputDefs()[0]->TypeAsProto()->tensor_type().elem_type();
-  return (dt_input == ONNX_NAMESPACE::TensorProto_DataType::TensorProto_DataType_UINT8);
+  return true;
 }
 
 }  // namespace QDQ
