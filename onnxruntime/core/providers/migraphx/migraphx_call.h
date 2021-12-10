4331221a0c67947f0145b0cfc836d3650501afb8
Shucai Xiao <Shucai.Xiao@amd.com>
2021-12-10 11:39:52 -0600

    onnxruntime/core/providers/migraphx/migraphx_call.h

--- onnxruntime/core/providers/migraphx/migraphx_call.h
+++ onnxruntime/core/providers/migraphx/migraphx_call.h
@@ -3,4 +3,19 @@
 
 #pragma once
 #include "migraphx_inc.h"
-#include "core/providers/rocm/shared_inc/rocm_call.h"
+
+#pragma once
+
+namespace onnxruntime {
+
+// -----------------------------------------------------------------------
+// Error handling
+// -----------------------------------------------------------------------
+
+template <typename ERRTYPE, bool THRW>
+bool RocmCall(ERRTYPE retCode, const char* exprString, const char* libName, ERRTYPE successCode, const char* msg = "");
+
+#define HIP_CALL(expr) (RocmCall<hipError_t, false>((expr), #expr, "HIP", hipSuccess))
+#define HIP_CALL_THROW(expr) (RocmCall<hipError_t, true>((expr), #expr, "HIP", hipSuccess))
+
+}
