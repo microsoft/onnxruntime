777a80fbc15eaf1b79e06b86dcd57cdf1d156f50
Abhishek Jindal <abjindal@microsoft.com>
2021-12-13 13:23:46 -0800

    Abjindal/eager onnx operators fix (#9968)
    * adding view operator changes
    
    * adding the slice operator definition
    
    * moving to opgen script for slice op and removing redundant steps in view op and reshape_copy
    
    * adding for at definition
    
    * adding for at::infer_size definition
    
    * changing template style for reshape_copy to ensure int64_t type

--- orttraining/orttraining/eager/ort_ops.cpp
+++ orttraining/orttraining/eager/ort_ops.cpp
@@ -8,26 +8,6 @@
 namespace torch_ort {
 namespace eager {
 
-OrtValue reshape_copy(
-  onnxruntime::ORTInvoker& invoker,
-  const OrtValue& input,
-  std::vector<int64_t> shape) {
-  
-  // TODO: actual reshape on buffer
-  const onnxruntime::Tensor& input_tensor = input.Get<onnxruntime::Tensor>();
-  auto new_shape = at::infer_size(shape, input_tensor.Shape().Size());
-  OrtValue shape_tensor;
-  //todo: avoid the copy on this small shape vector;
-  auto element_type = onnxruntime::DataTypeImpl::GetType<int64_t>();
-  CreateMLValue(invoker.GetCurrentExecutionProvider().GetAllocator(0, OrtMemTypeDefault),
-                element_type, {(int64_t)new_shape.size(),}, &shape_tensor);
-  auto* ort_shape_tensor = shape_tensor.GetMutable<onnxruntime::Tensor>();
-  CopyVectorToTensor<int64_t>(invoker, new_shape, *ort_shape_tensor);
-  std::vector<OrtValue> result(1);
-  ORT_THROW_IF_ERROR(invoker.Invoke("Reshape", {input, shape_tensor}, result, nullptr));
-  return result[0];
-}
-
 void copy(onnxruntime::ORTInvoker& invoker, 
           const OrtValue& src, OrtValue& dst){
   auto& ort_ep = invoker.GetCurrentExecutionProvider();
