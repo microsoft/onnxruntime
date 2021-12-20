2cfec1182224451e03fcdc76f92f7520a2f29753
Vincent Wang <wangwchpku@outlook.com>
2021-12-20 11:01:09 +0800

    remove reducesum ut from rocm

--- onnxruntime/test/providers/cpu/reduction/reduction_ops_test.cc
+++ onnxruntime/test/providers/cpu/reduction/reduction_ops_test.cc
@@ -1410,18 +1410,6 @@ TEST(ReductionOpTest, ReduceSumHalfHalf) {
   test.Run();
 }
 
-TEST(ReductionOpTest, ReduceSumBFloat16) {
-  OpTester test("ReduceSum", 14);
-  test.AddAttribute("keepdims", (int64_t)0);
-  test.AddInput<BFloat16>("data", {3, 2, 2},
-                          MakeBFloat16({1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f, 7.0f, 8.0f, 9.0f, 10.0f, 11.0f, 12.0f}));
-  test.AddInput<int64_t>("axes", {2}, std::vector<int64_t>{0, 1});
-  test.AddOutput<BFloat16>("reduced", {2}, MakeBFloat16({36.0f, 42.0f}));
-  std::vector<std::unique_ptr<IExecutionProvider>> execution_providers;
-  execution_providers.push_back(DefaultCudaExecutionProvider());
-  test.Run(OpTester::ExpectResult::kExpectSuccess, "", {}, nullptr, &execution_providers);
-}
-
 void test_half_reduce_sum(
     int64_t m, int64_t n) {
   OpTester test("ReduceSum");
@@ -1461,6 +1449,20 @@ TEST(ReductionOpTest, ReduceSum_half_bert) {
 }
 
 // Add more UTs for half as needed
+#endif
+
+#ifdef USE_CUDA
+TEST(ReductionOpTest, ReduceSumBFloat16) {
+  OpTester test("ReduceSum", 14);
+  test.AddAttribute("keepdims", (int64_t)0);
+  test.AddInput<BFloat16>("data", {3, 2, 2},
+                          MakeBFloat16({1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f, 7.0f, 8.0f, 9.0f, 10.0f, 11.0f, 12.0f}));
+  test.AddInput<int64_t>("axes", {2}, std::vector<int64_t>{0, 1});
+  test.AddOutput<BFloat16>("reduced", {2}, MakeBFloat16({36.0f, 42.0f}));
+  std::vector<std::unique_ptr<IExecutionProvider>> execution_providers;
+  execution_providers.push_back(DefaultCudaExecutionProvider());
+  test.Run(OpTester::ExpectResult::kExpectSuccess, "", {}, nullptr, &execution_providers);
+}
 #endif
 
 TEST(ReductionOpTest, ReduceSum_apex_reduction) {
