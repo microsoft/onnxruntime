996ac220ba7091189a329cf7da85ec0328c98d19
Olivia Jain <oljain@microsoft.com>
2021-12-21 11:18:29 -0800

    Update checkout_trt.sh

--- onnxruntime/python/tools/tensorrt/perf/build/checkout_trt.sh
+++ onnxruntime/python/tools/tensorrt/perf/build/checkout_trt.sh
@@ -7,10 +7,12 @@ then
     git remote update
     if [ "$1" = "8.0" ]
     then 
-        git checkout "$1"'-GA'
+        echo "8.0"
+        git checkout 1f416bb462689f3ef9e3f1057a113d9c6aba6972
     fi
     if [ "$1" = "7.2" ]
     then 
+        ehco "7.2"
         git checkout "$1"'.1'
     fi
     cd $CUR_PWD 
