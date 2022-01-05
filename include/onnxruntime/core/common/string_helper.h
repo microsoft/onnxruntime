6a67e84e4d9c475533fa2844d984992baa200054
hariharans29 <shariharan91@gmail.com>
2022-01-04 16:30:25 -0800

    More fixes

--- include/onnxruntime/core/common/string_helper.h
+++ include/onnxruntime/core/common/string_helper.h
@@ -6,10 +6,12 @@
 #include "core/session/onnxruntime_c_api.h"
 
 namespace onnxruntime {
+#if defined(USE_TENSORRT) || defined(USE_CUDA)
 static char* StrDup(const std::string& str, _Inout_ OrtAllocator* allocator) {
   char* output_string = reinterpret_cast<char*>(allocator->Alloc(allocator, str.size() + 1));
   memcpy(output_string, str.c_str(), str.size());
   output_string[str.size()] = '\0';
   return output_string;
 }
+#endif
 }  // namespace onnxruntime
