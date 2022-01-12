312d78a26aec86b66f191ef3acb1b102a5193b62
Baiju Meswani <bmeswani@microsoft.com>
2022-01-12 18:45:19 +0000

    Add cuda version to install dependencies argument

--- tools/ci_build/github/linux/run_dockerbuild.sh
+++ tools/ci_build/github/linux/run_dockerbuild.sh
@@ -91,6 +91,7 @@ elif [ $BUILD_DEVICE = "gpu" ]; then
         if [[ $ORTMODULE_BUILD = true ]]; then
             INSTALL_DEPS_EXTRA_ARGS="${INSTALL_DEPS_EXTRA_ARGS} -u"
         fi
+        INSTALL_DEPS_EXTRA_ARGS="${INSTALL_DEPS_EXTRA_ARGS} -v 11.3"
         $GET_DOCKER_IMAGE_CMD --repository "onnxruntime-$IMAGE" \
             --docker-build-args="--build-arg BASEIMAGE=nvcr.io/nvidia/cuda:11.3.1-cudnn8-devel-${BUILD_OS} --build-arg BUILD_USER=onnxruntimedev --build-arg BUILD_UID=$(id -u) --build-arg PYTHON_VERSION=${PYTHON_VER} --build-arg INSTALL_DEPS_EXTRA_ARGS=\"${INSTALL_DEPS_EXTRA_ARGS}\" --build-arg USE_CONDA=${USE_CONDA} --network=host" \
             --dockerfile Dockerfile.ubuntu_gpu_training --context .
