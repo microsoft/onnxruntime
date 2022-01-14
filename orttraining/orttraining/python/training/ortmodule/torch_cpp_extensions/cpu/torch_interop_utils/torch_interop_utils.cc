8e96b56bf8704d691aad9b7f603964a8568c7269
Peng Wang <pengwa@microsoft.com>
2022-01-14 12:24:49 +0000

    refine code comments

--- orttraining/orttraining/python/training/ortmodule/torch_cpp_extensions/cpu/torch_interop_utils/torch_interop_utils.cc
+++ orttraining/orttraining/python/training/ortmodule/torch_cpp_extensions/cpu/torch_interop_utils/torch_interop_utils.cc
@@ -114,13 +114,15 @@ void unregister_grad_fn(size_t ctx_address)
   PyNodeSharedPointerPool::GetInstance().UnRegisterGradFunc(ctx_address);
 }
 
-// Supposed to be cleared on python program exit or before every forward run to resolve following bug:
-// When training program exits, PyNodeSharedPointerPool destructor is called, if grad_fns_ is not empty,
-// PyNode::release_variables() will be called.
-// (https://github.com/pytorch/pytorch/blob/15532595209d2daf34d35e10f8d3d3b64966aea2/torch/csrc/autograd/python_function.cpp#L168)
-// The other hand, there is known issue when acquiring GIL in pybind11 destructors, there will be probabbly deadlock issue.
-// (https://github.com/pybind/pybind11/issues/1446)
-// The resolution here, we remove all maintained states before every forward run.
+// Supposed to be cleared on python program exit or before every forward run to resolve following issues:
+// 1. When training program exits, PyNodeSharedPointerPool destructor is called, if grad_fns_ is not empty,
+//    PyNode::release_variables() will be called.
+//    (https://github.com/pytorch/pytorch/blob/15532595209d2daf34d35e10f8d3d3b64966aea2/torch/csrc/autograd/python_function.cpp#L168)
+//    The other hand, there is known issue when acquiring GIL in pybind11 destructors, there will be probabbly deadlock issue.
+//    (https://github.com/pybind/pybind11/issues/1446)
+//    The resolution here, we remove all maintained states before program exits.
+// 2. When forward functions is called repeated without corresponding backward calls, grad functions keeps accumulating without releasing
+//    (happening in backward)
 void clear_all_grad_fns(){
   PyNodeSharedPointerPool::GetInstance().ClearAll();
 }
