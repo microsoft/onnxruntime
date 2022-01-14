4dd5b9d28335af5f1f5744f970ac6c77db10af69
Olivia Jain <oljain@microsoft.com>
2022-01-14 11:31:14 -0800

    post results only once

--- onnxruntime/python/tools/tensorrt/perf/perf.sh
+++ onnxruntime/python/tools/tensorrt/perf/perf.sh
@@ -25,10 +25,34 @@ then
     cd $PERF_DIR 
 fi 
 
+# metadata
+FAIL_MODEL_FILE=".fail_model_map"
+LATENCY_FILE=".latency_map"
+METRICS_FILE=".metrics_map"
+PROFILE="*onnxruntime_profile*"
+
 # files to download info
 FLOAT_16="float16.py"
 FLOAT_16_LINK="https://raw.githubusercontent.com/microsoft/onnxconverter-common/master/onnxconverter_common/float16.py"
 
-wget --no-check-certificate -c $FLOAT_16_LINK 
+cleanup_files() {
+    rm -f $FAIL_MODEL_FILE
+    rm -f $LATENCY_FILE
+    rm -f $METRICS_FILE
+    rm -f $FLOAT_16
+    rm -rf result/$OPTION
+    find -name $PROFILE -delete
+}
+
+download_files() {
+    wget --no-check-certificate -c $FLOAT_16_LINK 
+}
+
+setup() {
+    cleanup_files
+    download_files
+}
+
+setup
 python3 benchmark_wrapper.py -r validate -m $MODEL_PATH -o result/$OPTION -w $WORKSPACE $RUN_EPS
 python3 benchmark_wrapper.py -r benchmark -t 1200 -m $MODEL_PATH -o result/$OPTION -w $WORKSPACE $RUN_EPS
