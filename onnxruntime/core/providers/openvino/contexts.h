59cf12848c1fd861910b8bcb73a0dfc5eedeeb23
MaajidKhan <n.maajidkhan@gmail.com>
2022-02-02 22:57:47 +0530

    Implement GPU Throttling feature
    *Added GPU Throttling feature for iGPU's.
    when user enables it as a runtime option,
    it helps in reducing overall CPU usage
    of the application
    
    *Added changes to exercise this option
    using onnxruntime_perf_test application.
    
    Signed-off-by: MaajidKhan <n.maajidkhan@gmail.com>

--- onnxruntime/core/providers/openvino/contexts.h
+++ onnxruntime/core/providers/openvino/contexts.h
@@ -14,6 +14,7 @@ struct GlobalContext {
   bool is_wholly_supported_graph = false;
   bool enable_vpu_fast_compile = false;
   bool use_compiled_network = false;
+  bool enable_gpu_throttling = false;
   size_t num_of_threads;
   std::string device_type;
   std::string precision_str;
