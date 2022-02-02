6076a262dccf9b8dc024e73d7401e1183bdb234d
Sunghoon <35605090+hanbitmyths@users.noreply.github.com>
2022-02-02 15:19:40 -0800

    upgrade react-native packages to latest (#10454)

--- js/react_native/example/metro.config.js
+++ js/react_native/example/metro.config.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const path = require('path');
-const blacklist = require('metro-config/src/defaults/blacklist');
+const exclusionList = require('metro-config/src/defaults/exclusionList');
 const escape = require('escape-string-regexp');
 const pak = require('../package.json');
 
@@ -16,9 +16,9 @@ module.exports = {
   watchFolders: [root],
 
   // We need to make sure that only one version is loaded for peerDependencies
-  // So we blacklist them at the root, and alias them to the versions in example's node_modules
+  // So we exclusionList them at the root, and alias them to the versions in example's node_modules
   resolver: {
-    blacklistRE: blacklist(
+    exclusionListRE: exclusionList(
       modules.map(
         (m) =>
           new RegExp(`^${escape(path.join(root, 'node_modules', m))}\\/.*$`)
