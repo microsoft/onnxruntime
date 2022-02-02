8fc3bb45193279302f61fa2621137156e9caee68
Yao Zhang <zhanyao@microsoft.com>
2022-02-01 23:01:22 -0800

    Support specifying execution providers.

--- onnxruntime/python/tools/transformers/benchmark_helper.py
+++ onnxruntime/python/tools/transformers/benchmark_helper.py
@@ -39,11 +39,11 @@ IO_BINDING_DATA_TYPE_MAP = {
 
 def create_onnxruntime_session(onnx_model_path,
                                use_gpu,
+                               provider='cpu',
                                enable_all_optimization=True,
                                num_threads=-1,
                                enable_profiling=False,
-                               verbose=False,
-                               use_dml=False):
+                               verbose=False):
     session = None
     try:
         from onnxruntime import SessionOptions, InferenceSession, GraphOptimizationLevel, __version__ as onnxruntime_version
@@ -68,8 +68,14 @@ def create_onnxruntime_session(onnx_model_path,
 
         logger.debug(f"Create session for onnx model: {onnx_model_path}")
         if use_gpu:
-            if use_dml:
+            if provider == 'dml':
                 execution_providers = ['DmlExecutionProvider', 'CPUExecutionProvider']
+            elif provider == 'rocm':
+                execution_providers = ['ROCMExecutionProvider', 'CPUExecutionProvider']
+            elif provider == 'migraphx':
+                execution_providers = ['MIGraphXExecutionProvider', 'CPUExecutionProvider']
+            elif provider == 'tensorrt':
+                execution_providers = ['TensorrtExecutionProvider', 'CPUExecutionProvider']
             else:
                 execution_providers = ['CUDAExecutionProvider', 'CPUExecutionProvider']
         else:
