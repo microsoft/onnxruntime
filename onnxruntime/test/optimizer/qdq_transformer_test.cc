a405658370cc05989209c6608ff9f591799c84ae
Yi-Hong Lyu <yilyu@microsoft.com>
2022-02-03 10:29:30 +0800

    Fuse Clip->Q to Q (#10434)
    * Fuse Clip->Q to Q
    
    * Remove unused variable argmax_node
    
    * Remove braces around scalar initializer
    
    * Move GetClipConstantMinMax under ORT_MINIMAL_BUILD
    
    * Consider epsilon so we can fuse more cases

--- onnxruntime/test/optimizer/qdq_transformer_test.cc
+++ onnxruntime/test/optimizer/qdq_transformer_test.cc
@@ -1616,6 +1616,81 @@ TEST(QDQTransformerTests, DQForward_MutilpleSteps) {
   test_case({1, 13, 13, 23}, {30, 23, 3, 3}, {0, 3, 1, 2});
 }
 
+TEST(QDQTransformerTests, Clip) {
+  constexpr float epsilon = std::numeric_limits<float>::epsilon();
+
+  auto test_case = [&](float scale, auto zero_point, int clip_count, int opset_version = 12) {
+    auto build_test_case = [&](ModelTestBuilder& builder) {
+      auto* input_arg = builder.MakeInput<int8_t>({1, 32, 112, 112},
+                                                  std::numeric_limits<int8_t>::min(),
+                                                  std::numeric_limits<int8_t>::max());
+      auto* output_arg = builder.MakeOutput();
+
+      // add DQ
+      auto* dq_output = builder.MakeIntermediate();
+      builder.AddDequantizeLinearNode<int8_t>(input_arg, .0035f, 7, dq_output);
+
+      // add Clip
+      auto* clip_output = builder.MakeIntermediate();
+      constexpr float min = .0f;
+      constexpr float max = 6.0f;
+      if (opset_version >= 11) {
+        auto* min_initializer = builder.MakeScalarInitializer<float>(min);
+        auto* max_initializer = builder.MakeScalarInitializer<float>(max);
+        builder.AddNode("Clip", {dq_output, min_initializer, max_initializer}, {clip_output});
+      } else {
+        Node& argmax_node = builder.AddNode("Clip", {dq_output}, {clip_output});
+        argmax_node.AddAttribute("min", min);
+        argmax_node.AddAttribute("max", max);
+      }
+
+      // add Q + DQ
+      auto* q_output = builder.MakeIntermediate();
+      builder.AddQuantizeLinearNode(clip_output, scale, zero_point, q_output);
+      builder.AddDequantizeLinearNode(q_output, scale, zero_point, output_arg);
+    };
+
+    auto check_clip_graph = [&](InferenceSessionWrapper& session) {
+      auto op_to_count = CountOpsInGraph(session.GetGraph());
+      EXPECT_EQ(op_to_count["QuantizeLinear"], 1);
+      EXPECT_EQ(op_to_count["Clip"], clip_count);
+      EXPECT_EQ(op_to_count["DequantizeLinear"], 2);
+    };
+
+    TransformerTester(build_test_case, check_clip_graph,
+                      TransformerLevel::Default,
+                      TransformerLevel::Level1,
+                      opset_version,
+                      epsilon,
+                      epsilon);
+  };
+
+  test_case(.0235294122248888f, static_cast<int8_t>(-128), 0);  // [0, 6]
+  test_case(.02f, static_cast<int8_t>(-128), 0);                // [0, 5.1]
+  test_case(.03f, static_cast<int8_t>(-128), 1);                // [0, 7.65]
+  test_case(.02f, static_cast<int8_t>(127), 1);                 // [-5.1 , 0]
+  test_case(.02f, static_cast<int8_t>(0), 1);                   // [-2.56, 2.54]
+  test_case(.04f, static_cast<int8_t>(-97), 1);                 // [-1.24, 8.96]
+  test_case(.02352941176f, static_cast<uint8_t>(0), 0);         // [0, 6]
+  test_case(.02f, static_cast<uint8_t>(0), 0);                  // [0, 5.1]
+  test_case(.03f, static_cast<uint8_t>(0), 1);                  // [0, 7.65]
+  test_case(.02f, static_cast<uint8_t>(255), 1);                // [-5.1, 0]
+  test_case(.02f, static_cast<uint8_t>(128), 1);                // [-2.56, 2.54]
+  test_case(.04f, static_cast<uint8_t>(31), 1);                 // [-1.24, 8.96]
+
+  // opset_version = 10
+  test_case(.02f, static_cast<int8_t>(-128), 0, 10);  // [0, 5.1]
+  test_case(.03f, static_cast<int8_t>(-128), 1, 10);  // [0, 7.65]
+  test_case(.02f, static_cast<uint8_t>(0), 0, 10);    // [0, 5.1]
+  test_case(.03f, static_cast<uint8_t>(0), 1, 10);    // [0, 7.65]
+
+  // difference between lower/upper and min/max are within epsilon
+  test_case(epsilon, static_cast<int8_t>(-127), 0);              // [-epsilon, x] (x <= 6 + epsilon)
+  test_case((6 + epsilon) / 255, static_cast<int8_t>(-128), 0);  // [0, 6 + epsilon]
+  test_case(epsilon, static_cast<uint8_t>(1), 0);                // [-epsilon, x] (x <= 6 + epsilon)
+  test_case((6 + epsilon) / 255, static_cast<uint8_t>(0), 0);    // [0, 6 + epsilon]
+}
+
 TEST(QDQTransformerTests, Concat) {
   auto test_case = [&](const std::vector<std::vector<int64_t>>& input_shapes,
                        int64_t axis,
