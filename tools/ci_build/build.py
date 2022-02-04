13ab2cf59a6146a3c11b12c7547b3d8855f91e95
Justin D. Harris <juharris@users.noreply.github.com>
2022-02-04 09:54:30 -0800

    build.py: Don't run run_training_python_utility_tests in Windows.

--- tools/ci_build/build.py
+++ tools/ci_build/build.py
@@ -1622,10 +1622,11 @@ def run_onnxruntime_tests(args, source_dir, ctest_path, build_dir, configs):
             if not args.disable_ml_ops:
                 run_subprocess([sys.executable, 'onnxruntime_test_python_mlops.py'], cwd=cwd, dll_path=dll_path)
 
-            if args.enable_training:
+            # The following test has multiple failures on Windows.
+            # PyTorch is not installed in Windows.
+            if args.enable_training and not is_windows():
                 run_training_python_utility_tests(cwd=cwd)
-                # The following test has multiple failures on Windows.
-                if args.use_cuda and not is_windows():
+                if args.use_cuda:
                     # Run basic frontend tests.
                     run_training_python_frontend_tests(cwd=cwd)
 
