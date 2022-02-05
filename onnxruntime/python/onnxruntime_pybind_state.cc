c25f4a112fbf16992a7cd0d2e50f540dd847164f
Chi Lo <54722500+chilo-ms@users.noreply.github.com>
2022-02-05 11:15:12 -0800

    Make user capable of adding new field in OrtTensorRTProviderOptionsV2 as new provider option (#10450)
    * modify code for add additional field in OrtTensorRTProviderOptionsV2
    
    * add include file
    
    * fix typo
    
    * fix bug
    
    * add comment
    
    * fix code
    
    * revert change

--- onnxruntime/python/onnxruntime_pybind_state.cc
+++ onnxruntime/python/onnxruntime_pybind_state.cc
@@ -27,6 +27,7 @@
 #include "core/session/abi_session_options_impl.h"
 #include "core/session/onnxruntime_session_options_config_keys.h"
 #include "core/session/provider_bridge_ort.h"
+#include "core/providers/tensorrt/tensorrt_provider_options.h"
 
 // Explicitly provide a definition for the static const var 'GPU' in the OrtDevice struct,
 // GCC 4.x doesn't seem to define this and it breaks the pipelines based on CentOS as it uses
@@ -374,7 +375,7 @@ std::unique_ptr<IExecutionProvider> CreateExecutionProviderInstance(
       std::string calibration_table, cache_path, lib_path;
       auto it = provider_options_map.find(type);
       if (it != provider_options_map.end()) {
-        OrtTensorRTProviderOptions params{
+        OrtTensorRTProviderOptionsV2 params{
             0,
             0,
             nullptr,
