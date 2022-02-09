23730b1143e02d8ccf38815843fc592f02f47fc1
Changming Sun <chasun@microsoft.com>
2022-02-08 23:04:40 -0800

    Add copyright

--- onnxruntime/core/graph/contrib_ops/bert_defs.cc
+++ onnxruntime/core/graph/contrib_ops/bert_defs.cc
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 #include "core/graph/constants.h"
 #include "core/graph/contrib_ops/contrib_defs.h"
 #include "core/graph/contrib_ops/quantization_defs.h"
