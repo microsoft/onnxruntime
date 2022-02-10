4d6d4dfb9de610fbb09e23b36379473b4005a99d
Ye Wang <52801275+wangyems@users.noreply.github.com>
2022-02-10 08:51:01 -0800

    Add TRT ep perf benchmark (#10470)

--- onnxruntime/python/tools/transformers/benchmark_helper.py
+++ onnxruntime/python/tools/transformers/benchmark_helper.py
@@ -31,6 +31,17 @@ class Precision(Enum):
         return self.value
 
 
+class OptimizerInfo(Enum):
+    # no_opt means using the raw ONNX model, but OnnxRuntime might still apply optimization as long as
+    # graph optimization level is not 0 (disable all).
+    NOOPT = 'no_opt'
+    BYORT = 'by_ort'
+    BYSCRIPT = 'by_script'
+
+    def __str__(self):
+        return self.value
+
+
 IO_BINDING_DATA_TYPE_MAP = {
     "float32": numpy.float32,
     # TODO: Add more.
@@ -114,7 +125,6 @@ def prepare_environment(cache_dir, output_dir, use_gpu, provider=None):
             assert 'CUDAExecutionProvider' in onnxruntime.get_available_providers(
             ), "Please install onnxruntime-gpu package to test GPU inference."
 
-
     import transformers
     logger.info(f'PyTorch Version:{torch.__version__}')
     logger.info(f'Transformers Version:{transformers.__version__}')
@@ -146,9 +156,9 @@ def get_latency_result(runtimes, batch_size):
 def output_details(results, csv_filename):
     with open(csv_filename, mode="a", newline='') as csv_file:
         column_names = [
-            "engine", "version", "device", "precision", "optimizer", "io_binding", "model_name", "inputs", "threads",
-            "batch_size", "sequence_length", "datetime", "test_times", "QPS", "average_latency_ms", "latency_variance",
-            "latency_90_percentile", "latency_95_percentile", "latency_99_percentile"
+            "engine", "version", "providers", "device", "precision", "optimizer", "io_binding", "model_name", "inputs",
+            "threads", "batch_size", "sequence_length", "datetime", "test_times", "QPS", "average_latency_ms",
+            "latency_variance", "latency_90_percentile", "latency_95_percentile", "latency_99_percentile"
         ]
 
         csv_writer = csv.DictWriter(csv_file, fieldnames=column_names)
@@ -162,7 +172,8 @@ def output_details(results, csv_filename):
 def output_summary(results, csv_filename, args):
     with open(csv_filename, mode="a", newline='') as csv_file:
         header_names = [
-            "model_name", "inputs", "engine", "version", "device", "precision", "optimizer", "io_binding", "threads"
+            "model_name", "inputs", "engine", "version", "providers", "device", "precision", "optimizer", "io_binding",
+            "threads"
         ]
         data_names = []
         for batch_size in args.batch_sizes:
@@ -213,8 +224,9 @@ def output_fusion_statistics(model_fusion_statistics, csv_filename):
     logger.info(f"Fusion statistics is saved to csv file: {csv_filename}")
 
 
-def inference_ort(ort_session, ort_inputs, result_template, repeat_times, batch_size):
+def inference_ort(ort_session, ort_inputs, result_template, repeat_times, batch_size, warm_up_repeat=0):
     result = {}
+    timeit.repeat(lambda: ort_session.run(None, ort_inputs), number=1, repeat=warm_up_repeat)  # Dry run
     runtimes = timeit.repeat(lambda: ort_session.run(None, ort_inputs), number=1, repeat=repeat_times)
     result.update(result_template)
     result.update({"io_binding": False})
@@ -232,7 +244,8 @@ def inference_ort_with_io_binding(ort_session,
                                   output_buffer_max_sizes,
                                   batch_size,
                                   device,
-                                  data_type=numpy.longlong):
+                                  data_type=numpy.longlong,
+                                  warm_up_repeat=0):
     result = {}
 
     # Bind inputs and outputs to onnxruntime session
@@ -250,6 +263,7 @@ def inference_ort_with_io_binding(ort_session,
     for i in range(len(ort_output_names)):
         io_binding.bind_output(ort_output_names[i], output_buffers[i].device.type, 0, numpy.float32,
                                ort_outputs[i].shape, output_buffers[i].data_ptr())
+    timeit.repeat(lambda: ort_session.run_with_iobinding(io_binding), number=1, repeat=warm_up_repeat)  # Dry run
     runtimes = timeit.repeat(lambda: ort_session.run_with_iobinding(io_binding), number=1, repeat=repeat_times)
     result.update(result_template)
     result.update({"io_binding": True})
