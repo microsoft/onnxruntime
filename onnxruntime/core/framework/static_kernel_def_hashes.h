c2cc662012bc74d2a3d1277c7a6efadf5e944b1b
Ashwini Khade <askhade@microsoft.com>
2022-02-09 23:37:22 -0800

    fix typo

--- onnxruntime/core/framework/static_kernel_def_hashes.h
+++ onnxruntime/core/framework/static_kernel_def_hashes.h
@@ -9,11 +9,11 @@
 #include "core/common/basic_types.h"
 namespace onnxruntime {
 /**
-   * @brief Gets the hash value for provided op type + version combination if it is available, otherwise
-   * returns a nullopt. The hash value is available if this node was added by layout transformer. For all other
-   * nodes, the hash values should be present either in the serialized sesison state obtained form ort format model
-   * or from compiled kernel hases map which is generated during partitioning.
-   * @return std::optional<HashValue>
-   */
+ * @brief Gets the hash value for provided op type + version combination if it is available, otherwise
+ * returns a nullopt. The hash value is available if this node was added by layout transformer. For all other
+ * nodes, the hash values should be present either in the serialized session state obtained form ort format model
+ * or from compiled kernel hash map which is generated during partitioning.
+ * @return std::optional<HashValue>
+ */
 std::optional<HashValue> GetHashValueFromStaticKernelHashMap(const std::string& op_type, int since_version);
-}
+}  // namespace onnxruntime
