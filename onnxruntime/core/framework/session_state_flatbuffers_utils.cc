532909b7db73e6f53a38cd816a91068171d1527d
Scott McKay <skottmckay@gmail.com>
2022-02-17 10:51:24 +1000

    Fix missing digit.

--- onnxruntime/core/framework/session_state_flatbuffers_utils.cc
+++ onnxruntime/core/framework/session_state_flatbuffers_utils.cc
@@ -87,19 +87,19 @@ void UpdateHashForBackwardsCompatibility(HashValue& hash) {
   // address that.
   static const std::unordered_map<HashValue, HashValue> hashes{
       // old                   new                          domain, operator, opset[, type]
-      {2832535737534577496ULL, 16708009824840936392ULL},   // kOnnxDomain, Dropout, 7
-      {12198479371038564912ULL, 1718418059112844640ULL},   // kOnnxDomain, Scan, 9
-      {2560955351529676608ULL, 3668627007850399040ULL},    // kOnnxDomain, Scan, 11
-      {10232409728231027688ULL, 5212043150202938416ULL},   // kOnnxDomain, Not, 1
-      {11912523891622051440ULL, 10225383741733918632ULL},  // kOnnxDomain, RoiAlign, 10, float
-      {18084231515768318048ULL, 17022700455473327752ULL},  // kOnnxDomain, RoiAlign, 10, double
-      {14033689580222898712ULL, 634727773751317256ULL},    // kOnnxDomain, GatherND, 11
-      {646512416908411600ULL, 3064028185911332496ULL},     // kOnnxDomain, GatherND, 12
-      {15019893097608892000ULL, 11311962292460032936ULL},  // kOnnxDomain, GatherND, 13
-      {14259324427750852648ULL, 7767393334034626736ULL},   // kOnnxDomain, StringNormalizer, 10
-                                                           // contrib ops
-      {7642430665819070720ULL, 8620498355864235632ULL},    // kMSDomain, CropAndResize, 1
-      {1501966609334176828ULL, 11924582339825775592ULL}};  // kMSDomain, GridSample, 1
+      {2832535737534577496ULL, 16708009824840936392ULL},    // kOnnxDomain, Dropout, 7
+      {12198479371038564912ULL, 1718418059112844640ULL},    // kOnnxDomain, Scan, 9
+      {2560955351529676608ULL, 3668627007850399040ULL},     // kOnnxDomain, Scan, 11
+      {10232409728231027688ULL, 5212043150202938416ULL},    // kOnnxDomain, Not, 1
+      {11912523891622051440ULL, 10225383741733918632ULL},   // kOnnxDomain, RoiAlign, 10, float
+      {18084231515768318048ULL, 17022700455473327752ULL},   // kOnnxDomain, RoiAlign, 10, double
+      {14033689580222898712ULL, 634727773751317256ULL},     // kOnnxDomain, GatherND, 11
+      {646512416908411600ULL, 3064028185911332496ULL},      // kOnnxDomain, GatherND, 12
+      {15019893097608892000ULL, 11311962292460032936ULL},   // kOnnxDomain, GatherND, 13
+      {14259324427750852648ULL, 7767393334034626736ULL},    // kOnnxDomain, StringNormalizer, 10
+                                                            // contrib ops
+      {7642430665819070720ULL, 8620498355864235632ULL},     // kMSDomain, CropAndResize, 1
+      {15019666093341768288ULL, 11924582339825775592ULL}};  // kMSDomain, GridSample, 1
 
   auto iter = hashes.find(hash);
   if (iter != hashes.cend()) {
