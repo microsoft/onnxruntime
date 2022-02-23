32ec949f220a035331e26358505a94c0cd4b0842
Chen Fu <fuchen@microsoft.com>
2022-02-23 13:29:54 -0800

    Add asm file

--- onnxruntime/core/mlas/lib/arm64/SymQgemmS8KernelSdotLd64.asm
+++ onnxruntime/core/mlas/lib/arm64/SymQgemmS8KernelSdotLd64.asm
@@ -78,17 +78,15 @@ Return Value:
 
         PROLOG_SAVE_REG_PAIR d8,d9,#-GemmS8S8KernelFrame_SavedRegisters!
         PROLOG_NOP    ldr    x8,[sp,#GemmS8S8KernelFrame_ColumnSumBuffer]
+        PROLOG_NOP    cmp    x4,#2                   // M < 2 ?
         PROLOG_SAVE_REG_PAIR d10,d11,#16
+        PROLOG_NOP    add    x16,x2,x6,lsl #2        // x16 -> C1
+        PROLOG_NOP    add    x17,x2,x6,lsl #3        // x17 -> C2
         PROLOG_SAVE_REG_PAIR x20,x21,#32
-
-        // compute C pointers: x2, x16, x17, x6
-        cmp     x4,#2                   // M < 2 ?
-        add     x16,x2,x6,lsl #2        // x16 -> C1
-        add     x17,x2,x6,lsl #3        // x17 -> C2
         csel    x16,x2,x16,lo           // if M < 2  x16/C1 -> C0
+        mov     x12,#4                  // set max M to 4
         csel    x17,x16,x17,ls          // if M <= 2  x17/C2 -> C1
         cmp     x4,#4                   // M < 4 ?
-        mov     x12,#4                  // set max M to 4
         add     x6,x16,x6,lsl #3        // x6 -> C3
         mov     x9,x0                   // save A0
         mov     x10,x3                  // save K
@@ -116,265 +114,265 @@ ProcessNextColumnLoop
         ldr     q28,[x8],#16
         mov     x0,x9                   // reload A0
         cmp     x4,#2                   // M < 2 ?
+        ldr     q4,[x1],#16             // Load B
         add     x12,x9,x7               // x12 -> A1
         add     x13,x0,x7,lsl #1        // x13 -> A2
-        ldr     q4,[x1],#16             // Load B
         csel    x12,x0,x12,lo           // if M < 2  A1 -> A0
+        ldr     d0,[x0],#8              // Load A0  1st/2nd block of 4
         csel    x13,x12,x13,ls          // if M <= 2  A2 -> A1
         cmp     x4,4                    // M < 4 ?
-        add     x14,x12,x7,lsl #1       // x14 -> A3
         ldr     d5,[x1],#8
-        ldr     x15,[x1],#8
-        ins     v5.d[1],x15
-        csel    x14,x13,x14,lo          // if M < 4  A3 -> A2
-        ldr     d0,[x0],#8              // Load A0  1st/2nd block of 4
-        mov     v17.16b,v16.16b
-        mov     v18.16b,v16.16b
+        add     x14,x12,x7,lsl #1       // x14 -> A3
         ldr     d1,[x12],#8             // Load A1
-        mov     v19.16b,v16.16b
-        mov     v21.16b,v20.16b
+        csel    x14,x13,x14,lo          // if M < 4  A3 -> A2
         ldr     d2,[x13],#8             // Load A2
+        mov     v17.16b,v16.16b
+        ldr     d3,[x14],#8             // Load A3
+        mov     v18.16b,v16.16b
+        ldr     x15,[x1],#8
+        mov     v19.16b,v16.16b
+        ldr     d6,[x1],#8
+        mov     v21.16b,v20.16b
+        ldr     x20,[x1],#8
         mov     v22.16b,v20.16b
         mov     v23.16b,v20.16b
-        ldr     d3,[x14],#8             // Load A3
         mov     v25.16b,v24.16b
         mov     v26.16b,v24.16b
-        ldr     d6,[x1],#8
-        ldr     x20,[x1],#8
-        ins     v6.d[1],x20
         mov     v27.16b,v24.16b
         mov     v29.16b,v28.16b
-        ldr     d7,[x1],#8
-        ldr     x21,[x1],#8
-        ins     v7.d[1],x21
         subs    x3,x10,#2               // one loop iteration and epilogue consume k = 32
         mov     v30.16b,v28.16b
         mov     v31.16b,v28.16b
         b.lo    BlockLoopEpilogue       // Need 32 k for main loop
 
 BlockLoop
+        ldr     d7,[x1],#8
         sdot    v16.4s,v4.16b,v0.4b[0]
+        ldr     x21,[x1],#8
         sdot    v17.4s,v4.16b,v1.4b[0]
-        ldr     d8,[x0],#8              // Load A0  3rd/4th block of 4
+        ins     v5.d[1],x15
         sdot    v18.4s,v4.16b,v2.4b[0]
+        ldr     d8,[x0],#8              // Load A0  3rd/4th block of 4
         sdot    v19.4s,v4.16b,v3.4b[0]
         ldr     d4,[x1],#8
-        ldr     x11,[x1],#8
-        ins     v4.d[1],x11
         sdot    v20.4s,v5.16b,v0.4b[0]
+        ldr     x11,[x1],#8
         sdot    v21.4s,v5.16b,v1.4b[0]
-        ldr     d9,[x12],#8
+        ins     v6.d[1],x20
         sdot    v22.4s,v5.16b,v2.4b[0]
+        ldr     d9,[x12],#8
         sdot    v23.4s,v5.16b,v3.4b[0]
         ldr     d5,[x1],#8
-        ldr     x15,[x1],#8
-        ins     v5.d[1],x15
         sdot    v24.4s,v6.16b,v0.4b[0]
+        ldr     x15,[x1],#8
         sdot    v25.4s,v6.16b,v1.4b[0]
-        ldr     d10,[x13],#8
+        ins     v7.d[1],x21
         sdot    v26.4s,v6.16b,v2.4b[0]
+        ldr     d10,[x13],#8
         sdot    v27.4s,v6.16b,v3.4b[0]
         ldr     d6,[x1],#8
-        ldr     x20,[x1],#8
-        ins     v6.d[1],x20
         sdot    v28.4s,v7.16b,v0.4b[0]
+        ldr     x20,[x1],#8
         sdot    v29.4s,v7.16b,v1.4b[0]
-        ldr     d11,[x14],#8
+        ins     v4.d[1],x11
         sdot    v30.4s,v7.16b,v2.4b[0]
+        ldr     d11,[x14],#8
         sdot    v31.4s,v7.16b,v3.4b[0]
         ldr     d7,[x1],#8
-        ldr     x21,[x1],#8
-        ins     v7.d[1],x21
         sdot    v16.4s,v4.16b,v0.4b[1]
+        ldr     x21,[x1],#8
         sdot    v17.4s,v4.16b,v1.4b[1]
+        ins     v5.d[1],x15
         sdot    v18.4s,v4.16b,v2.4b[1]
         sdot    v19.4s,v4.16b,v3.4b[1]
         ldr     d4,[x1],#8
-        ldr     x11,[x1],#8
-        ins     v4.d[1],x11
         sdot    v20.4s,v5.16b,v0.4b[1]
+        ldr     x11,[x1],#8
         sdot    v21.4s,v5.16b,v1.4b[1]
+        ins     v6.d[1],x20
         sdot    v22.4s,v5.16b,v2.4b[1]
         sdot    v23.4s,v5.16b,v3.4b[1]
         ldr     d5,[x1],#8
-        ldr     x15,[x1],#8
-        ins     v5.d[1],x15
         sdot    v24.4s,v6.16b,v0.4b[1]
+        ldr     x15,[x1],#8
         sdot    v25.4s,v6.16b,v1.4b[1]
+        ins     v7.d[1],x21
         sdot    v26.4s,v6.16b,v2.4b[1]
         sdot    v27.4s,v6.16b,v3.4b[1]
         ldr     d6,[x1],#8
-        ldr     x20,[x1],#8
-        ins     v6.d[1],x20
         sdot    v28.4s,v7.16b,v0.4b[1]
+        ldr     x20,[x1],#8
         sdot    v29.4s,v7.16b,v1.4b[1]
+        ins     v4.d[1],x11
         sdot    v30.4s,v7.16b,v2.4b[1]
         sdot    v31.4s,v7.16b,v3.4b[1]
         ldr     d7,[x1],#8
-        ldr     x21,[x1],#8
-        ins     v7.d[1],x21
         sdot    v16.4s,v4.16b,v8.4b[0]
+        ldr     x21,[x1],#8
         sdot    v17.4s,v4.16b,v9.4b[0]
-        ldr     d0,[x0],#8
+        ins     v5.d[1],x15
         sdot    v18.4s,v4.16b,v10.4b[0]
+        ldr     d0,[x0],#8
         sdot    v19.4s,v4.16b,v11.4b[0]
         ldr     d4,[x1],#8
-        ldr     x11,[x1],#8
-        ins     v4.d[1],x11
         sdot    v20.4s,v5.16b,v8.4b[0]
+        ldr     x11,[x1],#8
         sdot    v21.4s,v5.16b,v9.4b[0]
-        ldr     d1,[x12],#8
+        ins     v6.d[1],x20
         sdot    v22.4s,v5.16b,v10.4b[0]
+        ldr     d1,[x12],#8
         sdot    v23.4s,v5.16b,v11.4b[0]
         ldr     d5,[x1],#8
-        ldr     x15,[x1],#8
-        ins     v5.d[1],x15
         sdot    v24.4s,v6.16b,v8.4b[0]
+        ldr     x15,[x1],#8
         sdot    v25.4s,v6.16b,v9.4b[0]
-        ldr     d2,[x13],#8
+        ins     v7.d[1],x21
         sdot    v26.4s,v6.16b,v10.4b[0]
+        ldr     d2,[x13],#8
         sdot    v27.4s,v6.16b,v11.4b[0]
         ldr     d6,[x1],#8
-        ldr     x20,[x1],#8
-        ins     v6.d[1],x20
         sdot    v28.4s,v7.16b,v8.4b[0]
+        ldr     x20,[x1],#8
         sdot    v29.4s,v7.16b,v9.4b[0]
-        ldr     d3,[x14],#8
+        ins     v4.d[1],x11
         sdot    v30.4s,v7.16b,v10.4b[0]
+        ldr     d3,[x14],#8
         sdot    v31.4s,v7.16b,v11.4b[0]
         ldr     d7,[x1],#8
-        ldr     x21,[x1],#8
-        ins     v7.d[1],x21
         sdot    v16.4s,v4.16b,v8.4b[1]
+        ldr     x21,[x1],#8
         sdot    v17.4s,v4.16b,v9.4b[1]
+        ins     v5.d[1],x15
         sdot    v18.4s,v4.16b,v10.4b[1]
         sdot    v19.4s,v4.16b,v11.4b[1]
         ldr     d4,[x1],#8
-        ldr     x11,[x1],#8
-        ins     v4.d[1],x11
         sdot    v20.4s,v5.16b,v8.4b[1]
+        ldr     x11,[x1],#8
         sdot    v21.4s,v5.16b,v9.4b[1]
+        ins     v6.d[1],x20
         sdot    v22.4s,v5.16b,v10.4b[1]
         sdot    v23.4s,v5.16b,v11.4b[1]
         ldr     d5,[x1],#8
-        ldr     x15,[x1],#8
-        ins     v5.d[1],x15
         sdot    v24.4s,v6.16b,v8.4b[1]
+        ldr     x15,[x1],#8
         sdot    v25.4s,v6.16b,v9.4b[1]
+        ins     v7.d[1],x21
         sdot    v26.4s,v6.16b,v10.4b[1]
+        subs    x3,x3,#1                // k -= 16
         sdot    v27.4s,v6.16b,v11.4b[1]
         ldr     d6,[x1],#8
-        ldr     x20,[x1],#8
-        ins     v6.d[1],x20
         sdot    v28.4s,v7.16b,v8.4b[1]
+        ldr     x20,[x1],#8
         sdot    v29.4s,v7.16b,v9.4b[1]
-        subs    x3,x3,#1                // k -= 16
+        ins     v4.d[1],x11
         sdot    v30.4s,v7.16b,v10.4b[1]
         sdot    v31.4s,v7.16b,v11.4b[1]
-        ldr     d7,[x1],#8
-        ldr     x21,[x1],#8
-        ins     v7.d[1],x21
         b.hs    BlockLoop
 
 BlockLoopEpilogue
+        ldr     d7,[x1],#8
         sdot    v16.4s,v4.16b,v0.4b[0]
+        ldr     x21,[x1],#8
         sdot    v17.4s,v4.16b,v1.4b[0]
-        ldr     d8,[x0],#8
+        ins     v5.d[1],x15
         sdot    v18.4s,v4.16b,v2.4b[0]
+        ldr     d8,[x0],#8
         sdot    v19.4s,v4.16b,v3.4b[0]
         ldr     d4,[x1],#8
-        ldr     x11,[x1],#8
-        ins     v4.d[1],x11
         sdot    v20.4s,v5.16b,v0.4b[0]
+        ldr     x11,[x1],#8
         sdot    v21.4s,v5.16b,v1.4b[0]
-        ldr     d9,[x12],#8
+        ins     v6.d[1],x20
         sdot    v22.4s,v5.16b,v2.4b[0]
+        ldr     d9,[x12],#8
         sdot    v23.4s,v5.16b,v3.4b[0]
         ldr     d5,[x1],#8
-        ldr     x15,[x1],#8
-        ins     v5.d[1],x15
         sdot    v24.4s,v6.16b,v0.4b[0]
+        ldr     x15,[x1],#8
         sdot    v25.4s,v6.16b,v1.4b[0]
-        ldr     d10,[x13],#8
+        ins     v7.d[1],x21
         sdot    v26.4s,v6.16b,v2.4b[0]
+        ldr     d10,[x13],#8
         sdot    v27.4s,v6.16b,v3.4b[0]
         ldr     d6,[x1],#8
-        ldr     x20,[x1],#8
-        ins     v6.d[1],x20
         sdot    v28.4s,v7.16b,v0.4b[0]
+        ldr     x20,[x1],#8
         sdot    v29.4s,v7.16b,v1.4b[0]
-        ldr     d11,[x14],#8
+        ins     v4.d[1],x11
         sdot    v30.4s,v7.16b,v2.4b[0]
+        ldr     d11,[x14],#8
         sdot    v31.4s,v7.16b,v3.4b[0]
         ldr     d7,[x1],#8
-        ldr     x21,[x1],#8
-        ins     v7.d[1],x21
         sdot    v16.4s,v4.16b,v0.4b[1]
+        ldr     x21,[x1],#8
         sdot    v17.4s,v4.16b,v1.4b[1]
+        ins     v5.d[1],x15
         sdot    v18.4s,v4.16b,v2.4b[1]
         sdot    v19.4s,v4.16b,v3.4b[1]
         ldr     d4,[x1],#8
-        ldr     x11,[x1],#8
-        ins     v4.d[1],x11
         sdot    v20.4s,v5.16b,v0.4b[1]
+        ldr     x11,[x1],#8
         sdot    v21.4s,v5.16b,v1.4b[1]
+        ins     v6.d[1],x20
         sdot    v22.4s,v5.16b,v2.4b[1]
         sdot    v23.4s,v5.16b,v3.4b[1]
         ldr     d5,[x1],#8
-        ldr     x15,[x1],#8
-        ins     v5.d[1],x15
         sdot    v24.4s,v6.16b,v0.4b[1]
+        ldr     x15,[x1],#8
         sdot    v25.4s,v6.16b,v1.4b[1]
+        ins     v7.d[1],x21
         sdot    v26.4s,v6.16b,v2.4b[1]
         sdot    v27.4s,v6.16b,v3.4b[1]
         ldr     d6,[x1],#8
-        ldr     x20,[x1],#8
-        ins     v6.d[1],x20
         sdot    v28.4s,v7.16b,v0.4b[1]
+        ldr     x20,[x1],#8
         sdot    v29.4s,v7.16b,v1.4b[1]
+        ins     v4.d[1],x11
         sdot    v30.4s,v7.16b,v2.4b[1]
         sdot    v31.4s,v7.16b,v3.4b[1]
         ldr     d7,[x1],#8
-        ldr     x21,[x1],#8
-        ins     v7.d[1],x21
         sdot    v16.4s,v4.16b,v8.4b[0]
+        ldr     x21,[x1],#8
         sdot    v17.4s,v4.16b,v9.4b[0]
+        ins     v5.d[1],x15
         sdot    v18.4s,v4.16b,v10.4b[0]
         sdot    v19.4s,v4.16b,v11.4b[0]
         ldr     d4,[x1],#8
-        ldr     x11,[x1],#8
-        ins     v4.d[1],x11
         sdot    v20.4s,v5.16b,v8.4b[0]
+        ldr     x11,[x1],#8
         sdot    v21.4s,v5.16b,v9.4b[0]
+        ins     v6.d[1],x20
         sdot    v22.4s,v5.16b,v10.4b[0]
         sdot    v23.4s,v5.16b,v11.4b[0]
         ldr     d5,[x1],#8
-        ldr     x15,[x1],#8
-        ins     v5.d[1],x15
         sdot    v24.4s,v6.16b,v8.4b[0]
+        ldr     x15,[x1],#8
         sdot    v25.4s,v6.16b,v9.4b[0]
+        ins     v7.d[1],x21
         sdot    v26.4s,v6.16b,v10.4b[0]
         sdot    v27.4s,v6.16b,v11.4b[0]
         ldr     d6,[x1],#8
-        ldr     x20,[x1],#8
-        ins     v6.d[1],x20
         sdot    v28.4s,v7.16b,v8.4b[0]
+        ldr     x20,[x1],#8
         sdot    v29.4s,v7.16b,v9.4b[0]
+        ins     v4.d[1],x11
         sdot    v30.4s,v7.16b,v10.4b[0]
         sdot    v31.4s,v7.16b,v11.4b[0]
         ldr     d7,[x1],#8
-        ldr     x21,[x1],#8
-        ins     v7.d[1],x21
         sdot    v16.4s,v4.16b,v8.4b[1]
+        ldr     x21,[x1],#8
         sdot    v17.4s,v4.16b,v9.4b[1]
+        ins     v5.d[1],x15
         sdot    v18.4s,v4.16b,v10.4b[1]
         sdot    v19.4s,v4.16b,v11.4b[1]
         sdot    v20.4s,v5.16b,v8.4b[1]
         sdot    v21.4s,v5.16b,v9.4b[1]
+        ins     v6.d[1],x20
         sdot    v22.4s,v5.16b,v10.4b[1]
         sdot    v23.4s,v5.16b,v11.4b[1]
         sdot    v24.4s,v6.16b,v8.4b[1]
         sdot    v25.4s,v6.16b,v9.4b[1]
+        ins     v7.d[1],x21
         sdot    v26.4s,v6.16b,v10.4b[1]
         sdot    v27.4s,v6.16b,v11.4b[1]
         sdot    v28.4s,v7.16b,v8.4b[1]
