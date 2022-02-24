612f223e1080f801d458c52580ffc7e35a3a912d
Ryan Lai <rylai@microsoft.com>
2022-02-24 14:05:33 -0800

    include file

---
+++ csharp/test/Microsoft.AI.MachineLearning.Tests.Lib.DotNet5_0/Microsoft.AI.MachineLearning.Tests.Lib.DotNet5_0.csproj.pp
@@ -0,0 +1,15 @@
+<Project Sdk="Microsoft.NET.Sdk">
+    <PropertyGroup>
+      <TargetFramework>net5.0-windows10.0.19041.0</TargetFramework>
+      <Platforms>x86;x64</Platforms>
+    </PropertyGroup>
+  
+    <ItemGroup>
+      <PackageReference Include="Microsoft.AI.MachineLearning" Version="[PackageVersion]" />
+    </ItemGroup>
+  
+    <ItemGroup>
+      <PackageReference Include="Microsoft.Windows.CsWinRT" Version="1.5.0" />
+    </ItemGroup>
+  </Project>
+  
