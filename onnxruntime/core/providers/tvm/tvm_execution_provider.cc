a2a206b73965d277276edfbea3c221f42e899907
Cheng Tang <chenta@microsoft.com>
2022-03-01 14:56:55 -0800

    fix the type mismatch

--- onnxruntime/core/providers/tvm/tvm_execution_provider.cc
+++ onnxruntime/core/providers/tvm/tvm_execution_provider.cc
@@ -309,7 +309,7 @@ common::Status TvmExecutionProvider::Compile(const std::vector<FusedNodeAndGraph
     ONNX_NAMESPACE::ModelProto model_proto = model.ToProto();
     //TVM EP is using static lib approach, so invoke serializer directly.
     GraphProtoSerializer serializer(&graph_body_viewer);
-    serializer.ToProto(*model_proto->mutable_graph(), true);
+    serializer.ToProto(*model_proto.mutable_graph(), true);
     auto opset = model_proto.add_opset_import();
     opset->set_domain(kOnnxDomain);
     opset->set_version(graph_body_viewer.DomainToVersionMap().at(kOnnxDomain));
