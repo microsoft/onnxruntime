3e040c4c6bc8338e7334e731419404ba84fe5430
Yulong Wang <7679871+fs-eire@users.noreply.github.com>
2022-03-04 20:14:57 -0800

    fix

--- js/web/test/e2e/browser-test-wasm-multi-session-create.js
+++ js/web/test/e2e/browser-test-wasm-multi-session-create.js
@@ -6,5 +6,5 @@
 it('Browser E2E testing - WebAssembly backend (multiple inference session create calls)', async function () {
   const sessionPromiseA = createSession(ort);
   const sessionPromiseB = createSession(ort);
-  await Promise.all(sessionPromiseA, sessionPromiseB);
+  await Promise.all([sessionPromiseA, sessionPromiseB]);
 });
