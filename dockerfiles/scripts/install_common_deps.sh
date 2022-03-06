8bc8493536d15e450e03c93890e8e954e1bafc7b
Changming Sun <chasun@microsoft.com>
2022-03-06 09:24:10 +0000

    update

--- dockerfiles/scripts/install_common_deps.sh
+++ dockerfiles/scripts/install_common_deps.sh
@@ -11,14 +11,11 @@ apt-get update && apt-get install -y --no-install-recommends \
         python3-dev
 
 # Dependencies: conda
-wget --quiet https://repo.anaconda.com/miniconda/Miniconda3-4.5.11-Linux-x86_64.sh -O ~/miniconda.sh --no-check-certificate && /bin/bash ~/miniconda.sh -b -p /opt/miniconda
+wget --quiet https://repo.anaconda.com/miniconda/Miniconda3-4.11.0-Linux-x86_64.sh -O ~/miniconda.sh --no-check-certificate && /bin/bash ~/miniconda.sh -b -p /opt/miniconda
 rm ~/miniconda.sh
 /opt/miniconda/bin/conda clean -ya
 
-pip install numpy
+pip install numpy cmake
 rm -rf /opt/miniconda/pkgs
 
-# Dependencies: cmake
-wget --quiet https://github.com/Kitware/CMake/releases/download/v3.21.0/cmake-3.21.0-linux-x86_64.tar.gz
-tar zxf cmake-3.21.0-linux-x86_64.tar.gz
-rm -rf cmake-3.21.0-linux-x86_64.tar.gz
+
