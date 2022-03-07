8cd5e01b08e580e46d7962d00e6aabf101a41165
Hari Seshadri <hasesh@microsoft.com>
2022-03-07 01:31:48 -0800

    Fix builds

--- onnxruntime/core/providers/cuda/activation/activations_impl.cu
+++ onnxruntime/core/providers/cuda/activation/activations_impl.cu
@@ -40,7 +40,7 @@ struct OP_Relu : public CtxRelu {
 template <typename T>
 struct OP_Selu : public CtxSelu {
   __device__ __inline__ T operator()(const T& a) const {
-    return a > (T)0 ? (T)gamma * a : (T)gamma * (T)alpha * (_Exp(a) - 1.0f);
+    return a > (T)0 ? (T)gamma * a : (T)gamma * (T)alpha * (_Exp(a) - (T)1);
   }
 };
 
