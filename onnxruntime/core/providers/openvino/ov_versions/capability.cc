0c6a86f1c8c5a5038f26ab007d9f95e5c90eaeaa
MaajidKhan <n.maajidkhan@gmail.com>
2022-03-07 10:48:55 +0530

    Fix windows build
    ->Fixed windows build in debug mode
    ->Disabled scatternd3_tensor_int64
    
    Signed-off-by: MaajidKhan <n.maajidkhan@gmail.com>

--- onnxruntime/core/providers/openvino/ov_versions/capability.cc
+++ onnxruntime/core/providers/openvino/ov_versions/capability.cc
@@ -8,7 +8,7 @@
 #include "utils.h"
 
 #if defined(_MSC_VER)
-#pragma warning(disable : 4244 4245 5208)
+#pragma warning(disable : 4244 4245 5208 4702)
 #elif __GNUC__
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wunused-parameter"
