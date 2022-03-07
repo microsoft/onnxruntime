c14ffcd9da528b721bf55029d4e2dc0199a7e651
Chi Lo <Chi.Lo@microsoft.com>
2022-03-07 07:46:51 +0000

    fix bug for running c-api test for package

--- tools/capi_test/run_capi_application.bat
+++ tools/capi_test/run_capi_application.bat
@@ -1,22 +1,23 @@
 set ONNX_MODEL_URL="https://media.githubusercontent.com/media/onnx/models/main/vision/classification/squeezenet/model/squeezenet1.0-7.onnx"
 set ONNX_MODEL="squeezenet.onnx"
 SET ORT_PACKAGE=%1
-SET WORKSPACE=%2
- 
+SET ORT_DIR=%2
+SET WORKSPACE=%3
+
 cd %WORKSPACE%
 
 echo The current directory is %CD%
 
 7z.exe x %ORT_PACKAGE% -y
-set ORT_LIB=%ORT_PACKAGE:~0,-4%\lib
-echo %ORT_LIB%
+REM set ORT_LIB=%ORT_PACKAGE:~0,-4%\lib
+REM echo %ORT_LIB%
 
 
 cmake.exe -S . -B build\ -G "Visual Studio 16 2019"
 cd build
-powershell -Command "cp %ORT_LIB%\* ."
+powershell -Command "cp ..\%ORT_DIR%\lib\* ."
 MSBuild.exe .\capi_test.sln /property:Configuration=Release
-powershell -Command "cp %ORT_LIB%\* Release"
+powershell -Command "cp ..\%ORT_DIR%\lib\* Release"
 cd Release
 powershell -Command "Invoke-WebRequest %ONNX_MODEL_URL% -Outfile %ONNX_MODEL%"
 capi_test.exe
