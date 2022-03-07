b7f00b9682256c0baf291de8cf620bb5656faad1
zhangyaobit <1034716+zhangyaobit@users.noreply.github.com>
2022-03-07 13:15:49 -0800

    Refactor the common code per operator into an abstract base class. (#10785)

--- onnxruntime/python/tools/microbench/benchmark.py
+++ onnxruntime/python/tools/microbench/benchmark.py
@@ -1,21 +1,9 @@
+from abc import ABC, abstractmethod
 from argparse import ArgumentParser
 import time
 import numpy
 import onnxruntime as ort
 import torch
-
-
-def add_arguments(parser: ArgumentParser):
-    parser.add_argument("--provider", required=False, type=str, default="rocm", help="Execution provider to use")
-    parser.add_argument("--precision", required=False, type=str, default="fp16", help="Number format to use")
-    parser.add_argument('--profiling', type=bool, default=False, help='If enable profiling')
-
-
-def create_input_output_tensors(inputs, outputs):
-    device = "cuda"
-    input_tensors = {name: torch.from_numpy(array).to(device) for name, array in inputs.items()}
-    output_tensors = {name: torch.from_numpy(array).to(device) for name, array in outputs.items()}
-    return input_tensors, output_tensors
 
 
 def numpy_type(torch_type):
@@ -24,53 +12,100 @@ def numpy_type(torch_type):
     return type_map[torch_type]
 
 
-def create_io_binding(sess, input_tensors, output_tensors):
-    io_binding = sess.io_binding()
-
-    for name, tensor in input_tensors.items():
-        io_binding.bind_input(name, tensor.device.type, 0, numpy_type(tensor.dtype), tensor.shape, tensor.data_ptr())
-
-    for name, tensor in output_tensors.items():
-        io_binding.bind_output(name, tensor.device.type, 0, numpy_type(tensor.dtype), tensor.shape, tensor.data_ptr())
-  
-    return io_binding
+def add_arguments(parser: ArgumentParser):
+    parser.add_argument("--provider", required=False, type=str, default="rocm", help="Execution provider to use")
+    parser.add_argument("--precision", required=False, type=str, default="fp16", help="Number format to use")
+    parser.add_argument('--profiling', type=bool, default=False, help='If enable profiling')
 
 
-def create_session(onnx_file, args):
-    sess_opt = ort.SessionOptions()
-    sess_opt.enable_profiling = args.profiling
-    if args.provider == "rocm":
-        execution_provider = ["ROCMExecutionProvider"] 
-    elif args.provider == "cuda":
-        execution_provider = ["CUDAExecutionProvider"] 
-    else:
-        raise ValueError(f"The script doesn't support provider type '{args.provider}' yet.")
+class Benchmark:
+    def __init__(self, model, inputs, outputs, args):
+        self.provider = args.provider
+        self.profiling = args.profiling
+        self.model = model
+        self.inputs = inputs
+        self.outputs = outputs
 
-    sess = ort.InferenceSession(onnx_file, sess_options=sess_opt, providers=execution_provider)
+    def create_input_output_tensors(self):
+        device = "cuda"
+        input_tensors = {name: torch.from_numpy(array).to(device) for name, array in self.inputs.items()}
+        output_tensors = {name: torch.from_numpy(array).to(device) for name, array in self.outputs.items()}
+        return input_tensors, output_tensors
 
-    if args.provider == "rocm":
-        assert 'ROCMExecutionProvider' in sess.get_providers()
-    elif args.provider == "cuda":
-        assert 'CUDAExecutionProvider' in sess.get_providers()
+    @classmethod
+    def create_io_binding(cls, sess, input_tensors, output_tensors):
+        io_binding = sess.io_binding()
+        for name, tensor in input_tensors.items():
+            io_binding.bind_input(name, tensor.device.type, 0, numpy_type(tensor.dtype), tensor.shape, tensor.data_ptr())
+        for name, tensor in output_tensors.items():
+            io_binding.bind_output(name, tensor.device.type, 0, numpy_type(tensor.dtype), tensor.shape, tensor.data_ptr())
+        return io_binding
 
-    return sess
- 
+    def create_session(self):
+        sess_opt = ort.SessionOptions()
+        sess_opt.enable_profiling = self.profiling
+        if self.provider == "rocm":
+            execution_provider = ["ROCMExecutionProvider"]
+        elif self.provider == "cuda":
+            execution_provider = ["CUDAExecutionProvider"]
+        else:
+            raise ValueError(f"The script doesn't support provider type '{self.provider}' yet.")
 
-def benchmark(onnx_file, inputs, outputs, args):
-    sess = create_session(onnx_file, args)
-    input_tensors, output_tensors = create_input_output_tensors(inputs, outputs)
-    io_binding = create_io_binding(sess, input_tensors, output_tensors)
+        sess = ort.InferenceSession(self.model, sess_options=sess_opt, providers=execution_provider)
 
-    # warm up    
-    for iter in range(10):
-      sess.run_with_iobinding(io_binding)    
+        if self.provider == "rocm":
+            assert 'ROCMExecutionProvider' in sess.get_providers()
+        elif self.provider == "cuda":
+            assert 'CUDAExecutionProvider' in sess.get_providers()
+
+        return sess
+
+    def benchmark(self):
+        sess = self.create_session()
+        input_tensors, output_tensors = self.create_input_output_tensors()
+        io_binding = self.create_io_binding(sess, input_tensors, output_tensors)
     
-    # measure 
-    max_iters = 100
-    start_time = time.time()
-    for iter in range(max_iters):
-        sess.run_with_iobinding(io_binding)    
-    
-    # time is in milliseconds
-    elapsed_time = (time.time() - start_time) * 1000 / max_iters
-    return elapsed_time
+        # warm up
+        for iter in range(10):
+          sess.run_with_iobinding(io_binding)
+
+        # measure
+        max_iters = 100
+        start_time = time.time()
+        for iter in range(max_iters):
+            sess.run_with_iobinding(io_binding)
+
+        # time is in milliseconds
+        elapsed_time = (time.time() - start_time) * 1000 / max_iters
+        return elapsed_time
+
+
+class BenchmarkOp(ABC):
+    def __init__(self, args):
+        self.args = args
+        self.cases = []
+
+    @classmethod
+    @abstractmethod
+    def create_inputs_outputs(cls, op_param):
+        ...
+
+    def add_case(self, op_param, model):
+        self.cases += [(op_param, model)]
+
+    @abstractmethod
+    def create_cases(self):
+        ...
+
+    @classmethod
+    @abstractmethod
+    def case_profile(cls, op_param, time):
+        ...
+
+    def benchmark(self):
+        self.create_cases()
+        for op_param, model in self.cases:
+            inputs, outputs = self.create_inputs_outputs(op_param)
+            bm = Benchmark(model, inputs, outputs, self.args)
+            time = bm.benchmark()
+            print(self.case_profile(op_param, time))
