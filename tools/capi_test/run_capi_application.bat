3de8d72912c58d74ae719e99bb80f2814a19f694
Chi Lo <Chi.Lo@microsoft.com>
2022-03-07 19:33:29 +0000

    refine run application script

--- tools/capi_test/run_capi_application.bat
+++ tools/capi_test/run_capi_application.bat
@@ -1,23 +1,25 @@
 set ONNX_MODEL_URL="https://media.githubusercontent.com/media/onnx/models/main/vision/classification/squeezenet/model/squeezenet1.0-7.onnx"
 set ONNX_MODEL="squeezenet.onnx"
 SET ORT_PACKAGE=%1
-SET ORT_DIR=%2
-SET WORKSPACE=%3
-
-cd %WORKSPACE%
+SET WORKSPACE=%2
 
 echo The current directory is %CD%
 
 7z.exe x %ORT_PACKAGE% -y
-REM set ORT_LIB=%ORT_PACKAGE:~0,-4%\lib
-REM echo %ORT_LIB%
+set ORT_LIB=%ORT_PACKAGE:~0,-4%\lib
+echo %ORT_LIB%
 
-
+cd %WORKSPACE%
 cmake.exe -S . -B build\ -G "Visual Studio 16 2019"
+
+REM Copy ORT libraries to same folder for linker to build.
+REM For some reasons, setting "LINK" or "LIBPATH" env variables won't help. 
 cd build
-powershell -Command "cp ..\%ORT_DIR%\lib\* ."
+powershell -Command "cp %ORT_LIB%\* ."
 MSBuild.exe .\capi_test.sln /property:Configuration=Release
-powershell -Command "cp ..\%ORT_DIR%\lib\* Release"
+
+REM Copy ORT libraries to same folder for executable to run.
 cd Release
+powershell -Command "cp %ORT_LIB%\* ."
 powershell -Command "Invoke-WebRequest %ONNX_MODEL_URL% -Outfile %ONNX_MODEL%"
 capi_test.exe
