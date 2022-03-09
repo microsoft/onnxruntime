2e3ebe8918de687f749c77279ced28053441ef51
hariharans29 <shariharan91@gmail.com>
2022-03-09 04:12:51 -0800

    Fix comment

--- include/onnxruntime/core/framework/op_kernel_context.h
+++ include/onnxruntime/core/framework/op_kernel_context.h
@@ -131,7 +131,7 @@ class OpKernelContext {
   Status GetTempSpaceAllocator(AllocatorPtr* output) const ORT_MUST_USE_RESULT;
 
   /**
-   Return the allocator associated with the EP with memtype of OrtMemTypeDefault.
+   Return the allocator associated with the CPU EP with memtype of OrtMemTypeDefault.
    @remarks Use SafeInt when calculating the size of memory to allocate using AllocatorPtr->Alloc.
    */
   Status GetCPUAllocator(AllocatorPtr* output) const ORT_MUST_USE_RESULT;
