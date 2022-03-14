8b9af47069ccfcd42c45f6cec8125099b383e0ae
Guangyun Han <guangyunhan@microsoft.com>
2022-03-14 16:53:50 +0800

    Add license header to embed.py

--- cmake/embed.py
+++ cmake/embed.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
 """This file implement `xxd -i` funcationality with additional features."""
 
 import io
