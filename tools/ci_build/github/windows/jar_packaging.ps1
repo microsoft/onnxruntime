9630e0112f7765b6f60d8821dbdab29a3a14b660
leqiao <leqiao@microsoft.com>
2022-03-21 11:11:20 +0800

    fix error

--- tools/ci_build/github/windows/jar_packaging.ps1
+++ tools/ci_build/github/windows/jar_packaging.ps1
@@ -7,12 +7,10 @@ dir
 pushd onnxruntime-java-linux-x64
 Write-Output "Run 7z"
 7z a $Env:BUILD_BINARIESDIRECTORY\java-artifact\onnxruntime-java-win-x64\testing.jar libcustom_op_library.so
-Remove-Item -Path libcustom_op_library.so
 7z a $Env:BUILD_BINARIESDIRECTORY\java-artifact\onnxruntime-java-win-x64\onnxruntime-$Env:ONNXRUNTIMEVERSION.jar .
 popd
 pushd onnxruntime-java-osx-x86_64
 7z a $Env:BUILD_BINARIESDIRECTORY\java-artifact\onnxruntime-java-win-x64\testing.jar libcustom_op_library.dylib
-Remove-Item -Path libcustom_op_library.dylib
 7z a $Env:BUILD_BINARIESDIRECTORY\java-artifact\onnxruntime-java-win-x64\onnxruntime-$Env:ONNXRUNTIMEVERSION.jar .
 popd
 pushd onnxruntime-java-linux-aarch64
@@ -22,6 +20,7 @@ Remove-Item -Path libcustom_op_library.so
 7z a $Env:BUILD_BINARIESDIRECTORY\java-artifact\onnxruntime-java-win-x64\onnxruntime-$Env:ONNXRUNTIMEVERSION.jar .
 popd
 pushd onnxruntime-java-osx-arm64
+Write-Output "Run 7z"
 7z a $Env:BUILD_BINARIESDIRECTORY\java-artifact\onnxruntime-java-win-x64\testing.jar libcustom_op_library.dylib
 Remove-Item -Path libcustom_op_library.dylib
 7z a $Env:BUILD_BINARIESDIRECTORY\java-artifact\onnxruntime-java-win-x64\onnxruntime-$Env:ONNXRUNTIMEVERSION.jar .
