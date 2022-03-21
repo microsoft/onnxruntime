711e679c2353a234060c062f3d4a2912c912aea4
FAREAST\zhanyi <zhanyi@microsoft.com>
2022-03-21 23:33:20 +0800

    'rsync'

--- tools/ci_build/github/android/update_gcno_path.sh
+++ tools/ci_build/github/android/update_gcno_path.sh
@@ -10,5 +10,9 @@ do
   parent_path=$( echo $my_path | grep -Eo "(onnxruntime/).*(.gcno)")
   prefix_path=$( echo $my_path | grep -Eo ".*(.dir)" )
   mkdir -p "$prefix_path"/"$ORT_ROOT"
-  cp --parents parent_path "$prefix_path"/"$ORT_ROOT"/
+  # https://stackoverflow.com/questions/11246070/cp-parents-option-on-mac
+  parent_dir=$(echo $my_dir | grep -Po ".*(?=onnxruntime/)")
+  pushd $parent_dir
+  rsync -R "$parent_path" "$prefix_path"/"$ORT_ROOT"/
+  popd
 done
