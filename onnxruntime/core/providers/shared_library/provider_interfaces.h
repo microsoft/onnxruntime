28e3d66df6428d3e0c60596277f2b75190718ca8
Cheng Tang <chenta@microsoft.com@orttrainingdev9.d32nl1ml4oruzj4qz3bqlggovf.px.internal.cloudapp.net>
2022-03-21 02:43:10 +0000

    fix linux break

--- onnxruntime/core/providers/shared_library/provider_interfaces.h
+++ onnxruntime/core/providers/shared_library/provider_interfaces.h
@@ -368,7 +368,7 @@ struct ProviderHost {
   virtual bool TensorProto__has_raw_data(const ONNX_NAMESPACE::TensorProto* p) = 0;
   virtual const std::string& TensorProto__raw_data(const ONNX_NAMESPACE::TensorProto* p) = 0;
   virtual int32_t TensorProto__data_type(const ONNX_NAMESPACE::TensorProto* p) = 0;
-  virtual void TensorProto__CopyFrom(ONNX_NAMESPACE::TensorProto* p, const ONNX_NAMESPACE::TensorProto* other);
+  virtual void TensorProto__CopyFrom(ONNX_NAMESPACE::TensorProto* p, const ONNX_NAMESPACE::TensorProto* other) = 0;
 
   virtual bool TensorProto_DataType_IsValid(int value) = 0;
 
