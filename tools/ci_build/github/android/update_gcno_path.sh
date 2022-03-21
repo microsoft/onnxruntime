020fb16196e177e3b676798d85a82d72f9cf621c
FAREAST\zhanyi <zhanyi@microsoft.com>
2022-03-21 22:01:30 +0800

    ORT_ROOT

--- tools/ci_build/github/android/update_gcno_path.sh
+++ tools/ci_build/github/android/update_gcno_path.sh
@@ -2,12 +2,13 @@
 set -ex
 
 pwd
+echo $ORT_ROOT
 
 files=$(find . -name "*.gcno")
 for my_path in "${files[@]}"
 do 
   parent_path=$( echo $my_path | grep -Eo "(onnxruntime/).*(.gcno)")
   prefix_path=$( echo $my_path | grep -Eo ".*(.dir)" )
-  mkdir -p "$prefix_path"/"$(Build.SourcesDirectory)"
-  cp --parents parent_path "$prefix_path"/"$(Build.SourcesDirectory)"/
+  mkdir -p "$prefix_path"/"$ORT_ROOT"
+  cp --parents parent_path "$prefix_path"/"$ORT_ROOT"/
 done
