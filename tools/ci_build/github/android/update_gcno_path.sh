a1c146f077ff81e8fab7466f87add8f31951b7a4
FAREAST\zhanyi <zhanyi@microsoft.com>
2022-03-22 12:32:26 +0800

    more check

--- tools/ci_build/github/android/update_gcno_path.sh
+++ tools/ci_build/github/android/update_gcno_path.sh
@@ -8,17 +8,21 @@ files=($(find . -name "*.gcno"))
 for my_path in "${files[@]}"
 do
   parent_path=$( echo $my_path | grep -Eo "(onnxruntime/).*(.gcno)" )
-  if [ -n $parent_path ]; then
+  if [ -n "$parent_path" ]; then
 	  prefix_path=$( echo $my_path | grep -Eo ".*(.dir)" )
 	  dest_dir="$ORT_ROOT"/"$prefix_path"/"$ORT_ROOT"
 	  mkdir -p $dest_dir
 	  # Mac doesn't support -Po
 	  parent_dir=$(echo $my_path | grep -Eo ".*(onnxruntime/)")
 	  parent_dir1=${parent_dir%%'onnxruntime/'}
-	  pushd $parent_dir1
-	  # https://stackoverflow.com/questions/11246070/cp-parents-option-on-mac
-	  rsync -R "$parent_path" "$dest_dir"
-	  popd
+	  if [ -n "$parent_dir1" ]; then
+		  pushd $parent_dir1
+	      # https://stackoverflow.com/questions/11246070/cp-parents-option-on-mac
+	      rsync -R "$parent_path" "$dest_dir"
+		  popd
+	  else
+	      echo $my_path
+	  fi
   else
       echo $my_path
   fi
