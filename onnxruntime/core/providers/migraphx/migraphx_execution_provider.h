0624c7cc0b3e05682e4004628102b5f5120744e7
Shucai Xiao <shucai@gmail.com>
2022-03-23 21:17:19 -0500

    amdmigraphx_ep-add ops to be supported by migraphx and fixed a bug in check ops to be supported (#10496)
    * backup debugging information related to debugging a jira ticket
    
    * fixed a bug in checking whether an input can be constand folded
    
    * added more operators that are supported by migraphx
    
    * revert unnecessary changes
    
    * remove unused logger parameter
    
    * rename function to make name style consistent
    
    * backup code changes
    
    * fix review comments
    
    * refactor graph utility functions to add unit tests
    
    * backup additional changes
    
    * fixed a link error in build migraphx_basic_test
    
    * add unit test for some migraphx utility functions
    
    * add more supported ops in migraphx

--- onnxruntime/core/providers/migraphx/migraphx_execution_provider.h
+++ onnxruntime/core/providers/migraphx/migraphx_execution_provider.h
@@ -16,6 +16,7 @@ namespace onnxruntime {
 
 namespace migraphx_env_vars {
 static const std::string kFP16Enable = "ORT_MIGRAPHX_FP16_ENABLE";
+static const std::string dumpModelOps = "ORT_MIGRAPHX_DUMP_MODEL_OPS";
 };
 
 // Information to construct kernel function state.
@@ -31,6 +32,7 @@ struct MIGraphXFuncState {
   OrtMutex* mgx_mu_ptr = nullptr;
   bool no_input_shape = false;
   bool fp16_enable = false;
+  bool dump_model_ops = false;
 };
 
 // Logical device representation.
@@ -58,6 +60,7 @@ class MIGraphXExecutionProvider : public IExecutionProvider {
 
 private:
   bool fp16_enable_ = false;
+  bool dump_model_ops_ = false;
   int device_id_;
   migraphx::target t_; 
   OrtMutex mgx_mu_;
