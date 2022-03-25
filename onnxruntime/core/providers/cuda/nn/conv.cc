4ea2685cc13f12a5d9b93c69deceb06be2821ff5
Vincent Wang <wangwchpku@outlook.com>
2022-03-25 17:21:51 +0800

    add provider option to control

--- onnxruntime/core/providers/cuda/nn/conv.cc
+++ onnxruntime/core/providers/cuda/nn/conv.cc
@@ -185,6 +185,9 @@ Status Conv<T>::UpdateState(OpKernelContext* context, bool bias_expected) const 
       s_.y_data = reinterpret_cast<CudaT*>(s_.Y->template MutableData<T>());
     }
 
+    const CUDAExecutionProvider* cuda_ep =
+        static_cast<const CUDAExecutionProvider*>(this->Info().GetExecutionProvider());
+
     TensorShapeVector x_dims_cudnn{x_dims.begin(), x_dims.end()};
     TensorShapeVector y_dims_cudnn = !post_slicing_required ? y_dims : y_dims_with_adjusted_pads;
     if (rank < 2) {
@@ -196,25 +199,25 @@ Status Conv<T>::UpdateState(OpKernelContext* context, bool bias_expected) const 
       // PyTorch also pads to [N,C,1,D]. For inference build, we still pad it to [N, C, D, 1] as this seems
       // to be the sweet spot for all algo search options: EXHAUSTIVE, HEURISTIC, and DEFAULT.
       // See PR #7348 and #7702 for more context.
-#ifdef ENABLE_TRAINING
-      x_dims_cudnn.insert(x_dims_cudnn.begin() + 2, 1);
-      y_dims_cudnn.insert(y_dims_cudnn.begin() + 2, 1);
-      w_dims.insert(w_dims.begin() + 2, 1);
-      pads.insert(pads.begin() + rank, 0);
-      pads.insert(pads.begin(), 0);
-      kernel_shape.insert(kernel_shape.begin(), 1);
-      strides.insert(strides.begin(), 1);
-      dilations.insert(dilations.begin(), 1);
-#else
-      x_dims_cudnn.push_back(1);
-      y_dims_cudnn.push_back(1);
-      w_dims.push_back(1);
-      pads.insert(pads.begin() + rank, 0);
-      pads.insert(pads.end(), 0);
-      kernel_shape.push_back(1);
-      strides.push_back(1);
-      dilations.push_back(1);
-#endif
+      if (cuda_ep->GetCudnnConv1dPadToNc1d()) {
+        x_dims_cudnn.insert(x_dims_cudnn.begin() + 2, 1);
+        y_dims_cudnn.insert(y_dims_cudnn.begin() + 2, 1);
+        w_dims.insert(w_dims.begin() + 2, 1);
+        pads.insert(pads.begin() + rank, 0);
+        pads.insert(pads.begin(), 0);
+        kernel_shape.insert(kernel_shape.begin(), 1);
+        strides.insert(strides.begin(), 1);
+        dilations.insert(dilations.begin(), 1);
+      } else {
+        x_dims_cudnn.push_back(1);
+        y_dims_cudnn.push_back(1);
+        w_dims.push_back(1);
+        pads.insert(pads.begin() + rank, 0);
+        pads.insert(pads.end(), 0);
+        kernel_shape.push_back(1);
+        strides.push_back(1);
+        dilations.push_back(1);
+      }
     }
 
     if (w_dims_changed)
@@ -259,7 +262,6 @@ Status Conv<T>::UpdateState(OpKernelContext* context, bool bias_expected) const 
 
       cudnnConvolutionFwdAlgoPerf_t perf;
       int algo_count = 1;
-      const CUDAExecutionProvider* cuda_ep = static_cast<const CUDAExecutionProvider*>(this->Info().GetExecutionProvider());
       int cudnn_conv_algo = cuda_ep->GetCudnnConvAlgo();
       ORT_ENFORCE(cudnn_conv_algo > -1 && cudnn_conv_algo < 3, "cudnn_conv_algo should be 0, 1 or 2, but got ", cudnn_conv_algo);
       switch (cudnn_conv_algo) {
