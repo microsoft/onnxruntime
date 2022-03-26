385025e7d01a73b52145e8ac6e40b6c56a2013e4
Justin Stoecker <justoeck@microsoft.com>
2022-03-26 00:14:30 -0700

    build helper

--- build_dml.ps1
+++ build_dml.ps1
@@ -1,11 +1,20 @@
-python $PSScriptRoot\tools\ci_build\build.py `
-    --build_dir $PSScriptRoot\build\Windows `
-    --config Debug `
-    --build_shared_lib `
-    --parallel `
-    --skip_tests `
-    --skip_submodule_sync `
-    --target install `
-    --gdk `
-    --gdk_edition 210602 `
-    --cmake_extra_defines CMAKE_INSTALL_PREFIX=$PSScriptRoot/build/install `
+param
+(
+    [string]$DmlInstallPath = 'S:/dml_gdk210602'
+)
+
+$pythonArgs = @(
+    "--build_dir", "$PSScriptRoot\build\Windows",
+    "--config", "Debug",
+    "--build_shared_lib",
+    "--parallel",
+    "--skip_tests",
+    "--skip_submodule_sync",
+    "--target", "install",
+    "--use_dml",
+    "--gdk",
+    "--gdk_edition", "210602",
+    "--cmake_extra_defines", "CMAKE_INSTALL_PREFIX=$PSScriptRoot\build\install onnxruntime_USE_CUSTOM_DIRECTML=ON dml_INCLUDE_DIR=$DmlInstallPath/include"
+)
+
+python $PSScriptRoot\tools\ci_build\build.py $pythonArgs
