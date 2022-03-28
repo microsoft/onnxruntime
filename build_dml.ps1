c40c3ea88e02c72894e1abc3ba192c573245974f
Justin Stoecker <justoeck@microsoft.com>
2022-03-28 16:29:56 -0700

    script fixes

--- build_dml.ps1
+++ build_dml.ps1
@@ -14,7 +14,10 @@ $pythonArgs = @(
     "--use_dml",
     "--gdk",
     "--gdk_edition", "210602",
-    "--cmake_extra_defines", "CMAKE_INSTALL_PREFIX=$PSScriptRoot\build\install onnxruntime_USE_CUSTOM_DIRECTML=ON dml_INCLUDE_DIR=$DmlInstallPath/include"
+    "--cmake_extra_defines", 
+        "CMAKE_INSTALL_PREFIX=$PSScriptRoot\build\install", 
+        "onnxruntime_USE_CUSTOM_DIRECTML=ON", 
+        "dml_INCLUDE_DIR=`"$DmlInstallPath/include`""
 )
 
 python $PSScriptRoot\tools\ci_build\build.py $pythonArgs
