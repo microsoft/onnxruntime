2700261f7ce7ea83749e73fcf414994d7e46d4c3
Dmitri Smirnov <yuslepukhin@users.noreply.github.com>
2022-04-07 12:21:53 -0700

    Provide an API to supply external initializers data from user buffers (#11109)
    Imlpement AddExternalInitializers

--- onnxruntime/python/onnxruntime_pybind_state.cc
+++ onnxruntime/python/onnxruntime_pybind_state.cc
@@ -9,6 +9,7 @@
 #define PY_ARRAY_UNIQUE_SYMBOL onnxruntime_python_ARRAY_API
 #include <numpy/arrayobject.h>
 
+#include "core/common/inlined_containers.h"
 #include "core/common/logging/logging.h"
 #include "core/common/logging/severity.h"
 #include "core/common/optional.h"
@@ -1234,7 +1235,28 @@ Applies to session load, initialization, etc. Default is 0.)pbdoc")
             // This is no different than the native APIs
             const OrtValue* ml_value = ml_value_pyobject.attr(PYTHON_ORTVALUE_NATIVE_OBJECT_ATTR).cast<OrtValue*>();
             ORT_THROW_IF_ERROR(options->AddInitializer(name, ml_value));
-          });
+          })
+      .def("add_external_initializers", [](PySessionOptions* options, py::list& names, 
+                                                    const py::list& ort_values) -> void {
+#if !defined(ORT_MINIMAL_BUILD) && !defined(DISABLE_EXTERNAL_INITIALIZERS)
+          const auto init_num = ort_values.size();
+          ORT_ENFORCE(init_num == names.size(), "Expecting names and ort_values lists to have equal length");
+          InlinedVector<std::string> names_ptrs;
+          InlinedVector<OrtValue> values_ptrs;
+          names_ptrs.reserve(init_num);
+          values_ptrs.reserve(init_num);
+          for (size_t i = 0; i < init_num; ++i) {
+            names_ptrs.emplace_back(py::str(names[i]));
+            values_ptrs.emplace_back(*ort_values[i].attr(PYTHON_ORTVALUE_NATIVE_OBJECT_ATTR).cast<const OrtValue*>());
+          }
+          ORT_THROW_IF_ERROR(options->AddExternalInitializers(names_ptrs, values_ptrs));
+#else
+          ORT_UNUSED_PARAMETER(options);
+          ORT_UNUSED_PARAMETER(names);
+          ORT_UNUSED_PARAMETER(ort_values);
+          ORT_THROW("External initializers are not supported in this build.");
+#endif
+      });
 
   py::class_<RunOptions>(m, "RunOptions", R"pbdoc(Configuration information for a single Run.)pbdoc")
       .def(py::init())
