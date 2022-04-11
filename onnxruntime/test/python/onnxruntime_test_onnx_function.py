7684e39a67e0286c558dc148ab8f4bde55c9c3e8
Liqun Fu <liqfu@microsoft.com>
2022-04-11 12:23:17 -0700

    add onnx script function tests - it fails in Debug build with current master

---
+++ onnxruntime/test/python/onnxruntime_test_onnx_function.py
@@ -0,0 +1,45 @@
+import onnx
+from onnx import parser, GraphProto, checker
+from onnxruntime import InferenceSession
+import unittest
+
+class TestOnnxFunctions(unittest.TestCase):
+
+    def test_parse_model(self) -> None:
+        input = '''
+            <
+            ir_version: 8,
+            opset_import: [ "" : 10, "local" : 1 ]
+            >
+            agraph (float[N, 128] X, float[128,10] W, float[10] B) => (float[N] C)
+            {
+            T = local.foo (X, W, B)
+            C = local.square(T)
+            }
+
+            <
+            opset_import: [ "" : 10 ],
+            domain: "local",
+            doc_string: "Function foo."
+            >
+            foo (x, w, b) => (c) {
+            T = MatMul(x, w)
+            S = Add(T, b)
+            c = Softmax(S)
+            }
+
+            <
+            opset_import: [ "" : 10 ],
+            domain: "local",
+            doc_string: "Function square."
+            >
+            square (x) => (y) {
+            y = Mul (x, x)
+            }
+           '''
+        model = onnx.parser.parse_model(input)
+        checker.check_model(model)
+        sess = InferenceSession(model.SerializeToString(), providers=['CPUExecutionProvider'])
+
+if __name__ == '__main__':
+    unittest.main()
