eebc5b7374ee66e0d4b1d71f2140dedd570bbf22
Mina Asham <mina.asham@hotmail.com>
2022-04-13 11:01:48 +0100

    Move utility function to OrtUtil and drop MapUtil, also add Java doc to method

--- java/src/test/java/ai/onnxruntime/OrtUtilTest.java
+++ java/src/test/java/ai/onnxruntime/OrtUtilTest.java
@@ -4,15 +4,15 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-class MapUtilTest {
+class OrtUtilTest {
 
   @Test
   void capacityFromSize() {
     // Capacity remainder below 0.5
-    assertEquals(10, MapUtil.capacityFromSize(7));
+    assertEquals(10, OrtUtil.capacityFromSize(7));
     // Capacity remainder above 0.5
-    assertEquals(11, MapUtil.capacityFromSize(8));
+    assertEquals(11, OrtUtil.capacityFromSize(8));
     // Capacity remainder equals 0
-    assertEquals(13, MapUtil.capacityFromSize(9));
+    assertEquals(13, OrtUtil.capacityFromSize(9));
   }
 }
