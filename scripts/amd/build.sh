d1437814cc3ab28ad351a4ba28e5bbc9f672d8cd
Michael Melesse <micmelesse@gmail.com>
2022-04-14 06:55:56 +0000

    update build script

--- scripts/amd/build.sh
+++ scripts/amd/build.sh
@@ -1,3 +1,3 @@
 rm -rf build
-python3 tools/ci_build/build.py --build_dir build --config RelWithDebInfo --parallel --use_hip --hip_home /opt/rocm --enable_training \
-> build_log.txt
+# ./build.sh --build_dir build --config RelWithDebInfo --parallel --use_hip --hip_home /opt/rocm --enable_training
+./build.sh --build_dir build --config Release --use_migraphx --migraphx_home  /opt/rocm 
