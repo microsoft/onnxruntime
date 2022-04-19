8f5c81bb0519bf570bce144238ceb8436509ca78
Chi Lo <54722500+chilo-ms@users.noreply.github.com>
2022-04-19 16:28:10 -0700

    Patch Release 1.11.1 cherry pick (#11255)
    * Update tools/ci_build/upload_python_package_to_azure_storage.py to not use the azure blob storage python package (#11114)
    
    * Fix the rocm packaging pipeline package upload problem (#11174)
    
    In #11114 , I changed the script to use azcopy instead of azure blob storage's python APIs. However, it doesn't work for the AMD rocm pipeline, because:
    
    1. The machines do not have azcopy installed
    2. The machines are not in Azure, so they don't have Azure managed identity. So they still need to use SAS.
    
    Therefore in this PR I get the old python file back, but only use it in the AMD pipeline.
    
    * Scoped GIL release in run_with_iobinding (#11248)
    
    * [js/web] disable test_tan temorarily (#11048)
    
    * [js/web] fix output type mapping (#11049)
    
    Co-authored-by: Changming Sun <chasun@microsoft.com>
    Co-authored-by: Dmitri Smirnov <yuslepukhin@users.noreply.github.com>
    Co-authored-by: Yulong Wang <7679871+fs-eire@users.noreply.github.com>

--- tools/ci_build/upload_python_package_to_azure_storage_with_python.py
+++ tools/ci_build/upload_python_package_to_azure_storage_with_python.py
@@ -5,10 +5,7 @@
 import os
 import argparse
 import warnings
-import subprocess
-import logging
-
-log = logging.getLogger("Build")
+from azure.storage.blob import BlockBlobService, ContentSettings
 
 
 def parse_nightly_and_local_version_from_whl_name(blob_name):
@@ -24,14 +21,14 @@ def parse_nightly_and_local_version_from_whl_name(blob_name):
     return night_build, blob_name[start:end]
 
 
-def run_subprocess(args, cwd=None):
-    log.debug("Running subprocess in '{0}'\n{1}".format(cwd or os.getcwd(), args))
-    return subprocess.run(args, cwd=cwd, check=True)
+def upload_whl(python_wheel_path, account_name, account_key, container_name):
+    block_blob_service = BlockBlobService(
+        account_name=account_name,
+        account_key=account_key
+    )
 
-
-def upload_whl(python_wheel_path):
     blob_name = os.path.basename(python_wheel_path)
-    run_subprocess(['azcopy', 'cp', python_wheel_path, 'https://onnxruntimepackages.blob.core.windows.net/$web/'])
+    block_blob_service.create_blob_from_path(container_name, blob_name, python_wheel_path)
 
     nightly_build, local_version = parse_nightly_and_local_version_from_whl_name(blob_name)
     if local_version:
@@ -41,8 +38,7 @@ def upload_whl(python_wheel_path):
 
     download_path_to_html = "./onnxruntime_{}.html".format(nightly_build)
 
-    run_subprocess(['azcopy', 'cp', 'https://onnxruntimepackages.blob.core.windows.net/$web/'+html_blob_name,
-                    download_path_to_html])
+    block_blob_service.get_blob_to_path(container_name, html_blob_name, download_path_to_html)
 
     blob_name_plus_replaced = blob_name.replace('+', '%2B')
     with open(download_path_to_html) as f:
@@ -58,16 +54,25 @@ def upload_whl(python_wheel_path):
                 f.write("%s\n" % item)
     else:
         warnings.warn("'{}' exists in {}. The html file is not updated.".format(new_line, download_path_to_html))
-    run_subprocess(['azcopy', 'cp', download_path_to_html,
-                    'https://onnxruntimepackages.blob.core.windows.net/$web/'+html_blob_name,
-                    '--content-type', 'text/html', '--overwrite', 'true'])
+
+    content_settings = ContentSettings(content_type='text/html')
+    block_blob_service.create_blob_from_path(
+        container_name,
+        html_blob_name,
+        download_path_to_html,
+        content_settings=content_settings)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Upload python whl to azure storage.")
 
     parser.add_argument("--python_wheel_path", type=str, help="path to python wheel")
+    parser.add_argument("--account_name", type=str, help="account name")
+    parser.add_argument("--account_key", type=str, help="account key")
+    parser.add_argument("--container_name", type=str, help="container name")
 
+    # TODO: figure out a way to secure args.account_key to prevent later code changes
+    # that may accidentally print out it to the console.
     args = parser.parse_args()
 
-    upload_whl(args.python_wheel_path)
+    upload_whl(args.python_wheel_path, args.account_name, args.account_key, args.container_name)
