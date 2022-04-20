1d96cbec73a2febff99661f8187a07954555ac52
Tianlei Wu <tlwu@microsoft.com>
2022-04-20 11:09:26 -0700

    Move gpt2 script to models\gpt2 sub-directory (#11256)
    * move gpt-2 scripts to models\gpt2
    * change gpt2 beam search helper to make test_gpt2 passes

--- onnxruntime/python/tools/transformers/__init__.py
+++ onnxruntime/python/tools/transformers/__init__.py
@@ -1,4 +1,13 @@
-import os
-import sys
+#-------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation.  All rights reserved.
+# Licensed under the MIT License.
+#--------------------------------------------------------------------------
 
-sys.path.append(os.path.dirname(__file__))
+import sys
+import os
+
+sys.path.append(os.path.join(os.path.dirname(__file__), 'models', 'gpt2'))
+
+# added for backward compatible
+import gpt2_helper
+import convert_to_onnx
