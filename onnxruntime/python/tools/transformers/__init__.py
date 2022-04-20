eb3f58bcf6a7f0449f6bd77a045de7454753f905
Tianlei Wu <tlwu@microsoft.com>
2022-04-19 18:18:17 -0700

    undo __init__.py

--- onnxruntime/python/tools/transformers/__init__.py
+++ onnxruntime/python/tools/transformers/__init__.py
@@ -2,3 +2,12 @@
 # Copyright (c) Microsoft Corporation.  All rights reserved.
 # Licensed under the MIT License.
 #--------------------------------------------------------------------------
+
+import sys
+import os
+
+sys.path.append(os.path.join(os.path.dirname(__file__), 'models', 'gpt2'))
+
+# added for backward compatible
+import gpt2_helper
+import convert_to_onnx
