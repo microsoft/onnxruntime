366f4ebcb425b6a47c2b0decd3b39fa14eb9dbf6
Chi Lo <54722500+chilo-ms@users.noreply.github.com>
2022-04-22 13:35:16 -0700

    Update version number for patch release v1.11.1 (#11317)
    * Bump version number to 1.11.1
    
    * Bump up version to 1.11.1

--- onnxruntime/core/session/onnxruntime_c_api.cc
+++ onnxruntime/core/session/onnxruntime_c_api.cc
@@ -2538,7 +2538,7 @@ static_assert(offsetof(OrtApi, SynchronizeBoundOutputs) / sizeof(void*) == 203, 
 static_assert(offsetof(OrtApi, SessionOptionsAppendExecutionProvider_MIGraphX) / sizeof(void*) == 209, "Size of version 11 API cannot change");
 
 // So that nobody forgets to finish an API version, this check will serve as a reminder:
-static_assert(std::string_view(ORT_VERSION) == "1.11.0", "ORT_Version change detected, please follow below steps to ensure OrtApi is updated properly");
+static_assert(std::string_view(ORT_VERSION) == "1.11.1", "ORT_Version change detected, please follow below steps to ensure OrtApi is updated properly");
 // 1. Update the hardcoded version string in above static_assert to silence it
 // 2. If there were any APIs added to ort_api_1_to_11 above:
 //    a. Add the 'End of version #' markers (pattern above should be obvious)
