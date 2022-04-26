c63ed8ebcb591161263ae0470ea0a68a56b024a0
pengwa@microsoft.com <pengwa@microsoft.com>
2022-04-26 06:59:54 +0000

    minor fix

--- orttraining/orttraining/python/training/ortmodule/_graph_execution_manager.py
+++ orttraining/orttraining/python/training/ortmodule/_graph_execution_manager.py
@@ -3,39 +3,38 @@
 # Licensed under the MIT License.
 # --------------------------------------------------------------------------
 
-from .debug_options import DebugOptions, LogLevel
-from . import (_utils,
-               _io,
-               _logger,
-               _onnx_models,
-               _are_deterministic_algorithms_enabled)
-from .torch_cpp_extensions.cpu.aten_op_executor import load_aten_op_executor_cpp_extension
-from ._custom_autograd_function import custom_autograd_function_enabler
-from ._custom_autograd_function_exporter import _post_process_after_export
-from ._graph_execution_interface import GraphExecutionInterface
-from ._fallback import (_FallbackManager,
-                        ORTModuleDeviceException,
-                        ORTModuleONNXModelException,
-                        ORTModuleTorchModelException,
-                        wrap_exception)
-from ._gradient_accumulation_manager import GradientAccumulationManager
-from onnxruntime.training import ortmodule
+import copy
+import inspect
+import io
+import os
+import warnings
+from abc import ABC, abstractmethod
+from enum import IntFlag
+from functools import reduce
 
+import onnx
+import torch
+from torch.utils.cpp_extension import ROCM_HOME
+
+import onnxruntime
 from onnxruntime.capi import _pybind_state as C
 from onnxruntime.tools.symbolic_shape_infer import SymbolicShapeInference
-from abc import ABC, abstractmethod
-import copy
-from functools import reduce
-import io
-import inspect
-import os
-import onnx
-import onnxruntime
-import torch
-import warnings
-from enum import IntFlag
+from onnxruntime.training import ortmodule
 
-from torch.utils.cpp_extension import ROCM_HOME
+from . import _are_deterministic_algorithms_enabled, _io, _logger, _onnx_models, _utils
+from ._custom_autograd_function import custom_autograd_function_enabler
+from ._custom_autograd_function_exporter import _post_process_after_export
+from ._fallback import (
+    ORTModuleDeviceException,
+    ORTModuleONNXModelException,
+    ORTModuleTorchModelException,
+    _FallbackManager,
+    wrap_exception,
+)
+from ._gradient_accumulation_manager import GradientAccumulationManager
+from ._graph_execution_interface import GraphExecutionInterface
+from .debug_options import DebugOptions, LogLevel
+from .torch_cpp_extensions.cpu.aten_op_executor import load_aten_op_executor_cpp_extension
 
 
 class _RunStateInfo(object):
@@ -148,7 +147,7 @@ class GraphExecutionManager(GraphExecutionInterface):
         self._enable_custom_autograd_function = custom_autograd_function_enabler.state
 
         # Allow using TensorRT + CUDA EPs as training backend.
-        self._use_tensorrt_backend = os.getenv('ORTMODULE_FALLBACK_RETRY',str('ORTMODULE_USE_TENSORRT_BACKEND')).lower() in ['true', '1', 'yes']
+        self._use_tensorrt_backend = os.getenv('ORTMODULE_USE_TENSORRT_BACKEND', 'false').lower() in ['true', '1', 'yes']
 
         self._input_info = None
         self._module_output_schema = None
