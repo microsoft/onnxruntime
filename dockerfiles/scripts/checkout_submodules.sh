75e0c95933e67e3c14152d194f3c7a04ca74a567
Olivia Jain <oljain@microsoft.com>
2022-04-25 17:32:59 -0700

    only checkout protobuf if we checkout older onnx-tensorrt

--- dockerfiles/scripts/checkout_submodules.sh
+++ dockerfiles/scripts/checkout_submodules.sh
@@ -5,10 +5,11 @@ echo "checking out submodule versions"
 CUR_PWD=$(pwd)
 cd onnxruntime/cmake/external
 
-# Point to correct onnx-tensorrt
+
 echo "$1"
 if [ ! "$1" = "8.2" ]
 then
+    # Point to correct onnx-tensorrt
     cd onnx-tensorrt
     git remote update
     if [ "$1" = "8.0" ]
@@ -16,19 +17,12 @@ then
         git checkout "$1"'-GA'
     fi
     cd ..
-fi
 
-# Point to correct protobuf
-PROTO_VER=$(apt list | grep -oP -m 1 'protobuf-compiler.* \K3[\.\d]+')
-echo "$PROTO_VER"
-
-#if awk 'BEGIN{exit !(ARGV[1]<ARGV[2])}' "$PROTO_VER" "3.11.0"
-if 0
-then 
+    # checkout correct protobuf to match onnx-tensorrt
     cd protobuf 
-    CHECKOUT_BRANCH=$(echo $PROTO_VER | grep -oP -m 1 '\d+\.\d+\.')x
     git checkout 3.10.x
     cd .. 
+
 fi
 
 cd $CUR_PWD 
