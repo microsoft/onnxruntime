1b0fc04d1deed29a69a867f6974cd4651e234fe1
Michael Melesse <micmelesse@gmail.com>
2022-04-26 19:11:45 +0000

    remove scripts/amd

--- scripts/amd/clean.sh
+++
@@ -1,4 +0,0 @@
-TMP_DIR=/tmp/onnxruntime
-
-rm -rf $TMP_DIR
-mkdir -p $TMP_DIR
--- scripts/amd/git_remote_add.sh
+++
@@ -1,3 +0,0 @@
-git remote add upstream https://github.com/microsoft/onnxruntime
-git remote add rocm_fork https://github.com/ROCmSoftwarePlatform/onnxruntime
-git remote add private_fork https://github.com/micmelesse/onnxruntime
