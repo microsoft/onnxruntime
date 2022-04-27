33658be1f2e4a1a207ba3c9c1e3b7e092eaf8c89
Vincent Wang <wangwchpku@outlook.com>
2022-04-27 09:47:47 +0800

    gather elements bugfix and perf improve

--- onnxruntime/core/providers/cuda/tensor/gather_elements.h
+++ onnxruntime/core/providers/cuda/tensor/gather_elements.h
@@ -8,16 +8,23 @@
 namespace onnxruntime {
 namespace cuda {
 
+void CoalesceDimensions(TensorShapeVector& input_shape, TensorShapeVector& indices_shape, int64_t axis,
+                        int64_t& new_axis, int64_t& new_rank, int64_t& input_stride_along_axis,
+                        TArray<int64_t>& masked_input_strides, TArray<fast_divmod>& indices_fdms);
+ONNX_NAMESPACE::TensorProto_DataType GetElementType(size_t element_size);
+
 class GatherElements final : public CudaKernel {
  public:
   GatherElements(const OpKernelInfo& info) : CudaKernel(info) {
-    ORT_ENFORCE(info.GetAttr<int64_t>("axis", &axis_).IsOK(),
-                "Missing/Invalid 'axis' attribute value");
+    ORT_ENFORCE(info.GetAttr<int64_t>("axis", &axis_).IsOK(), "Missing/Invalid 'axis' attribute value");
   }
   ~GatherElements() = default;
   Status ComputeInternal(OpKernelContext* context) const override;
 
  private:
+  template <typename T>
+  struct ComputeImpl;
+
   int64_t axis_;
 };
 
