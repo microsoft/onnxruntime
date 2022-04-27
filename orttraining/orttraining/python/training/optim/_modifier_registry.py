2c4ddc9837382faa6a401f37d5977d021fe79f47
zhijxu <zhijxu@microsoft.com>
2022-04-27 07:16:43 +0000

    support optimizer opt for deepspeed 0.5.9

--- orttraining/orttraining/python/training/optim/_modifier_registry.py
+++ orttraining/orttraining/python/training/optim/_modifier_registry.py
@@ -9,10 +9,12 @@ from ._apex_amp_modifier import ApexAMPModifier
 
 LEAGCY_MEGATRON_LM_OPTIMIZER_NAME = "megatron.fp16.fp16.FP16_Optimizer"
 DEEPSPEED_ZERO1_AND_ZERO2_OPTIMIZER_NAME = "deepspeed.runtime.zero.stage2.FP16_DeepSpeedZeroOptimizer"
+DEEPSPEED_ZERO1_AND_ZERO2_OPTIMIZER_NAME_1 = "deepspeed.runtime.zero.stage_1_and_2.DeepSpeedZeroOptimizer"
 APEX_AMP_OPTIMIZER_NAME = "apex.amp.optimizer.unique_name_as_id"
 
 OptimizerModifierTypeRegistry = {
     LEAGCY_MEGATRON_LM_OPTIMIZER_NAME: LegacyMegatronLMModifier,
     DEEPSPEED_ZERO1_AND_ZERO2_OPTIMIZER_NAME: DeepSpeedZeROModifier,
+    DEEPSPEED_ZERO1_AND_ZERO2_OPTIMIZER_NAME_1: DeepSpeedZeROModifier,
     APEX_AMP_OPTIMIZER_NAME: ApexAMPModifier,
 }
