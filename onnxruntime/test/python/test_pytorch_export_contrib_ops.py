8511f895547302b442aa7fb916b6e62db6a1128a
Justin Chu <justinchuby@users.noreply.github.com>
2022-04-27 18:53:15 +0000

    Typing

--- onnxruntime/test/python/test_pytorch_export_contrib_ops.py
+++ onnxruntime/test/python/test_pytorch_export_contrib_ops.py
@@ -132,7 +132,7 @@ class ONNXExporterTest(unittest.TestCase):
     @unittest.skipIf(_torch_version_lower_than("1.12"), "Gelu's approximate parameter unsupported in PyTorch < 1.12")
     @parameterized.parameterized.expand([("default_approximate", "none"), ("tanh_approximate", "tanh")])
     def test_gelu_supports_approximate_param(self, _, approximate: str):
-        model = torch.nn.GELU(approximate=approximate)
+        model = torch.nn.GELU(approximate=approximate)  # type: ignore
         x = torch.randn(3, 3)
         self.run_test(model, x, custom_opsets={"com.microsoft": 1})
 
