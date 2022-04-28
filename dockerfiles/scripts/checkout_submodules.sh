9982abe0a26d780b183413cb0537367f32a587b0
Olivia Jain <oljain@microsoft.com>
2022-04-28 15:32:41 -0700

    address shellcheck

--- dockerfiles/scripts/checkout_submodules.sh
+++ dockerfiles/scripts/checkout_submodules.sh
@@ -4,27 +4,29 @@ echo "checking out submodule versions"
 
 downgrade_protobuf=false
 CUR_PWD=$(pwd)
-cd onnxruntime/cmake/external
+cd onnxruntime/cmake/external || return
 
 echo "$1"
 if [ ! "$1" = "8.2" ]
 then
     # Point to correct onnx-tensorrt
-    cd onnx-tensorrt
+    downgrade_protobuf=true
+    ( 
+    cd onnx-tensorrt || return
     git remote update
     if [ "$1" = "8.0" ]
     then 
         git checkout "$1"'-GA'
-        downgrade_protobuf=true
-    fi
-    cd ..
+    fi 
+    )
 
     # checkout correct protobuf to match onnx-tensorrt
     if [ "$downgrade_protobuf" = true ]
     then
-        cd protobuf 
-        git checkout 3.10.x
-        cd .. 
+        ( 
+        cd protobuf || return
+        git checkout 3.10.x 
+        )
     fi
 
 fi
