93c1d11b9d5f8d376628599e802b19975fd3e5df
George Wu <jywu@microsoft.com>
2022-05-04 10:21:40 -0700

    use placeholder builder only when running tests

--- onnxruntime/core/providers/tensorrt/tensorrt_execution_provider.cc
+++ onnxruntime/core/providers/tensorrt/tensorrt_execution_provider.cc
@@ -256,8 +256,11 @@ std::unique_lock<OrtMutex> TensorrtExecutionProvider::GetApiLock() const {
   return std::unique_lock<OrtMutex>(singleton);
 }
 
-// global unused builder object - which keeps the TRT kernel library in memory so that subsequent builders avoid the expensive load / unload process.
+#ifdef ORT_RUN_EXTERNAL_ONNX_TESTS
+// instantiate global unused builder object which keeps the TRT kernel library in memory 
+// so that subsequent builders avoid the expensive load / unload process.
 auto const placeholder = tensorrt_ptr::unique_pointer<nvinfer1::IBuilder>(nvinfer1::createInferBuilder(GetTensorrtLogger()));
+#endif
 
 TensorrtExecutionProvider::TensorrtExecutionProvider(const TensorrtExecutionProviderInfo& info)
     : IExecutionProvider{onnxruntime::kTensorrtExecutionProvider, true}, info_(info), device_id_(info.device_id) {
