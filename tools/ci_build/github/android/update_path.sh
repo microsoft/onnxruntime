efa0ec25b68573f4fdb683c77756bef929393112
Yi Zhang <zhanyi@microsoft.com>
2022-05-05 04:16:04 -0700

    pass vars between jobs

--- tools/ci_build/github/android/update_path.sh
+++ tools/ci_build/github/android/update_path.sh
@@ -2,8 +2,10 @@
 
 set -x
 echo $1
+origin_repo=$2
+current_repo=$3
 find . -name "*$1" | while IFS= read -r pathname; do
-    new_pathname=${pathname//"/mnt/vss/_work/3/s"/"/Users/runner/work/1/s/"}
+    new_pathname=${pathname//$origi_repo/$current_repo}
     d1=$(dirname $new_pathname)
     if [ "$pathname" != "$new_pathname" ]; then
         mkdir -p "$d1"  && mv $pathname $new_pathname
