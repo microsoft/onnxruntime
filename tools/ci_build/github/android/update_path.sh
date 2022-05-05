6759b80c3ee58d17859ad4a7095654672e126817
Yi Zhang <zhanyi@microsoft.com>
2022-05-05 09:14:22 -0700

    fix typo

--- tools/ci_build/github/android/update_path.sh
+++ tools/ci_build/github/android/update_path.sh
@@ -5,7 +5,7 @@ echo $1
 origin_repo=$2
 current_repo=$3
 find . -name "*$1" | while IFS= read -r pathname; do
-    new_pathname=${pathname//$origi_repo/$current_repo}
+    new_pathname=${pathname//$origin_repo/$current_repo}
     d1=$(dirname $new_pathname)
     if [ "$pathname" != "$new_pathname" ]; then
         mkdir -p "$d1"  && mv $pathname $new_pathname
