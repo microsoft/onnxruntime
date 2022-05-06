947e8702e4b109c1e1e0e30184a5f5fb4906f5e7
Sheil Kumar <sheilk@microsoft.com>
2022-05-06 12:08:26 -0700

    make tests reuse device

--- winml/test/scenario/cppwinrt/scenariotestscppwinrt.cpp
+++ winml/test/scenario/cppwinrt/scenariotestscppwinrt.cpp
@@ -136,10 +136,10 @@ static void Scenario1LoadBindEvalDefault() {
   std::wstring filePath = FileHelpers::GetModulePath() + L"model.onnx";
   LearningModel model = LearningModel::LoadFromFilePath(filePath);
   // create a session on the default device
-  //auto device = LearningModelDevice(LearningModelDeviceKind::Default);
-  LearningModelSession session(model, LearningModelDevice(LearningModelDeviceKind::Default));
-  LearningModelSession session2(model, LearningModelDevice(LearningModelDeviceKind::Default));
-  LearningModelSession session3(model, LearningModelDevice(LearningModelDeviceKind::Default));
+  auto device = LearningModelDevice(LearningModelDeviceKind::Default);
+  LearningModelSession session(model, device);
+  LearningModelSession session2(model, device);
+  LearningModelSession session3(model, device);
   // create a binding set
   LearningModelBinding binding(session);
   // bind the input and the output buffers by name
