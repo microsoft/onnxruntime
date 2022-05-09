209f9444b15fbdedb5ac74382cc056bc4d7e1a9e
Changming Sun <chasun@microsoft.com>
2022-05-09 11:08:03 -0700

    update

--- onnxruntime/test/python/node/__init__.py
+++ onnxruntime/test/python/node/__init__.py
@@ -1,22 +1,23 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import sys
+from typing import List, Optional, Sequence, Text
 
-import onnx.defs
 import numpy as np  # type: ignore
+import onnx.defs
 from onnx import ModelProto
-from typing import List, Optional, Text, Sequence
-from onnx.backend.test.case.utils import import_recursive
 from onnx.backend.test.case.test_case import TestCase
+from onnx.backend.test.case.utils import import_recursive
 
 _SimpleModelTestCases = []
 
 
-def expect(model: ModelProto,
-           inputs: Sequence[np.ndarray],
-           outputs: Sequence[np.ndarray],
-           name: Optional[Text] = None,
-           ) -> None:
+def expect(
+    model: ModelProto,
+    inputs: Sequence[np.ndarray],
+    outputs: Sequence[np.ndarray],
+    name: Optional[Text] = None,
+) -> None:
     name = name or model.graph.name
     _SimpleModelTestCases.append(
         TestCase(
@@ -26,14 +27,14 @@ def expect(model: ModelProto,
             model_dir=None,
             model=model,
             data_sets=[(inputs, outputs)],
-            kind='simple',
+            kind="simple",
             rtol=1e-3,
             atol=1e-7,
-        ))
+        )
+    )
 
 
 def collect_testcases() -> List[TestCase]:
-    '''Collect model test cases defined in python/numpy code and in model zoo.
-    '''    
+    """Collect model test cases defined in python/numpy code and in model zoo."""
     import_recursive(sys.modules[__name__])
     return _SimpleModelTestCases
--- onnxruntime/test/python/node/depthwise_conv2d.py
+++ onnxruntime/test/python/node/depthwise_conv2d.py
@@ -1,46 +1,37 @@
 # SPDX-License-Identifier: Apache-2.0
 
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-from __future__ import unicode_literals
+from __future__ import absolute_import, division, print_function, unicode_literals
 
 import numpy as np  # type: ignore
-
 import onnx
 import tensorflow as tf
 import tf2onnx
-from onnx.backend.test.case.base import Base
-from . import expect
 from onnx.backend.sample.ops.abs import abs
+from onnx.backend.test.case.base import Base
 
-x_in = np.array([
-    [1., 2.],
-    [3., 4.],
-    [5., 6.]
-], dtype=np.float32).reshape((1, 3, 2, 1))
+from . import expect
 
-kernel_in = np.array([
-    [1., 2.],
-    [3., 4]
-], dtype=np.float32).reshape((2, 1, 1, 2))
+x_in = np.array([[1.0, 2.0], [3.0, 4.0], [5.0, 6.0]], dtype=np.float32).reshape((1, 3, 2, 1))
+
+kernel_in = np.array([[1.0, 2.0], [3.0, 4]], dtype=np.float32).reshape((2, 1, 1, 2))
 
 
 # Create a model using low-level tf.* APIs
 class SimpleDepthwiseConv2DValidPadding(tf.Module):
-  @tf.function(input_signature=[tf.TensorSpec(shape=x_in.shape, dtype=tf.float32)])
-  def __call__(self, x):
-    kernel = tf.constant(kernel_in, dtype=tf.float32)
-    return tf.nn.depthwise_conv2d(x, kernel, strides=[1, 1, 1, 1], padding='VALID');
+    @tf.function(input_signature=[tf.TensorSpec(shape=x_in.shape, dtype=tf.float32)])
+    def __call__(self, x):
+        kernel = tf.constant(kernel_in, dtype=tf.float32)
+        return tf.nn.depthwise_conv2d(x, kernel, strides=[1, 1, 1, 1], padding="VALID")
+
 
 class SimpleDepthwiseConv2DSamePadding(tf.Module):
-  @tf.function(input_signature=[tf.TensorSpec(shape=x_in.shape, dtype=tf.float32)])
-  def __call__(self, x):
-    kernel = tf.constant(kernel_in, dtype=tf.float32)
-    return tf.nn.depthwise_conv2d(x, kernel, strides=[1, 1, 1, 1], padding='SAME');
+    @tf.function(input_signature=[tf.TensorSpec(shape=x_in.shape, dtype=tf.float32)])
+    def __call__(self, x):
+        kernel = tf.constant(kernel_in, dtype=tf.float32)
+        return tf.nn.depthwise_conv2d(x, kernel, strides=[1, 1, 1, 1], padding="SAME")
 
-class Abs(Base):
 
+class DepthwiseConv2D(Base):
     @staticmethod
     def export() -> None:
         onnx_opset_number = 13
@@ -48,13 +39,17 @@ class Abs(Base):
         simple_conv2D_model = SimpleDepthwiseConv2DValidPadding()
         output_tensor = simple_conv2D_model(x_in)
         input_signature = [tf.TensorSpec(x_in.shape, x_in.dtype)]
-        onnx_model, _ = tf2onnx.convert.from_function(simple_conv2D_model.__call__,input_signature, opset=onnx_opset_number)
-        expect(onnx_model, inputs=[x_in], outputs=[output_tensor.numpy()],
-               name='test_SimpleDepthwiseConv2DValidPadding')
+        onnx_model, _ = tf2onnx.convert.from_function(
+            simple_conv2D_model.__call__, input_signature, opset=onnx_opset_number
+        )
+        expect(
+            onnx_model, inputs=[x_in], outputs=[output_tensor.numpy()], name="test_SimpleDepthwiseConv2DValidPadding"
+        )
 
         simple_conv2D_model = SimpleDepthwiseConv2DSamePadding()
         output_tensor = simple_conv2D_model(x_in)
         input_signature = [tf.TensorSpec(x_in.shape, x_in.dtype)]
-        onnx_model, _ = tf2onnx.convert.from_function(simple_conv2D_model.__call__,input_signature, opset=onnx_opset_number)
-        expect(onnx_model, inputs=[x_in], outputs=[output_tensor.numpy()],
-               name='test_SimpleDepthwiseConv2DSamePadding')
+        onnx_model, _ = tf2onnx.convert.from_function(
+            simple_conv2D_model.__call__, input_signature, opset=onnx_opset_number
+        )
+        expect(onnx_model, inputs=[x_in], outputs=[output_tensor.numpy()], name="test_SimpleDepthwiseConv2DSamePadding")
