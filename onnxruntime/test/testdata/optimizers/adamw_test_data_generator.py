a5a86d398f58e4e7759045b97adbb5f95049af8d
pengwa@microsoft.com <pengwa@microsoft.com>
2022-05-18 13:24:32 +0000

    fix python lint

--- onnxruntime/test/testdata/optimizers/adamw_test_data_generator.py
+++ onnxruntime/test/testdata/optimizers/adamw_test_data_generator.py
@@ -16,6 +16,7 @@ class SingleParameterModule(torch.nn.Module):
         out = self.fc1(input1)
         return out
 
+
 class MultipleParametersModule(torch.nn.Module):
     def __init__(self, input_size, hidden_size, num_classes):
         super().__init__()
@@ -28,6 +29,7 @@ class MultipleParametersModule(torch.nn.Module):
         out = self.relu(out)
         out = self.fc2(out)
         return out
+
 
 def generate_adamw_test_data(seed, _model_setup_func, data_func, train_step_count, adam_mode):
     def _run_step(model, x, target):
@@ -48,8 +50,7 @@ def generate_adamw_test_data(seed, _model_setup_func, data_func, train_step_coun
     def _to_adam_test_format(data, name_of_the_data):
         print(f"{name_of_the_data} ==================")
         for name, val in data.items():
-            print("{{ \"{}\", {{ {} }}, }},".format(name, _str_list_to_str(val)))
-
+            print('{{ "{}", {{ {} }}, }},'.format(name, _str_list_to_str(val)))
 
     torch.manual_seed(seed)
 
@@ -63,6 +64,7 @@ def generate_adamw_test_data(seed, _model_setup_func, data_func, train_step_coun
         adamw_optimizer = torch.optim.AdamW(pt_model.parameters(), lr=1e-3)
     elif adam_mode == 1:
         from transformers import AdamW
+
         adamw_optimizer = AdamW(pt_model.parameters(), lr=1e-3)
     else:
         raise ValueError(f"invalid adam_model: {adam_mode}")
@@ -76,8 +78,10 @@ def generate_adamw_test_data(seed, _model_setup_func, data_func, train_step_coun
 
     # Dump the optimizer configs, our adam tests should align with these.
     for group in adamw_optimizer.param_groups:
-        print(f"beta1, beta2 : {group['betas']}, lr={group['lr']},"
-               " weight_decay={group['weight_decay']}, eps={group['eps']})")
+        print(
+            f"beta1, beta2 : {group['betas']}, lr={group['lr']},"
+            " weight_decay={group['weight_decay']}, eps={group['eps']})"
+        )
 
     p_dict = {}
     g_dict = {}
@@ -98,7 +102,7 @@ def generate_adamw_test_data(seed, _model_setup_func, data_func, train_step_coun
             # Collect flattened parameter data.
             p_dict[name].append(_torch_tensor_to_str(param.view(-1)))
 
-            if step != train_step_count -1:
+            if step != train_step_count - 1:
                 # Collect flattened gradient data.
                 # Skip collecting the last step's gradients.
                 g_dict[name].append(_torch_tensor_to_str(param.grad.view(-1)))
@@ -107,7 +111,7 @@ def generate_adamw_test_data(seed, _model_setup_func, data_func, train_step_coun
 
         for group in adamw_optimizer.param_groups:
             p_index = 0
-            for p in group['params']:
+            for p in group["params"]:
                 state = adamw_optimizer.state[p]
                 name = param_index_to_name_mapping[p_index]
                 # Collect flattened optimizer state data.
@@ -115,8 +119,8 @@ def generate_adamw_test_data(seed, _model_setup_func, data_func, train_step_coun
                     m1_dict[name].append(_torch_tensor_to_str(torch.zeros_like(p).view(-1)))
                     m2_dict[name].append(_torch_tensor_to_str(torch.zeros_like(p).view(-1)))
                 else:
-                    m1_dict[name].append(_torch_tensor_to_str(state['exp_avg'].view(-1)))
-                    m2_dict[name].append(_torch_tensor_to_str(state['exp_avg_sq'].view(-1)))
+                    m1_dict[name].append(_torch_tensor_to_str(state["exp_avg"].view(-1)))
+                    m2_dict[name].append(_torch_tensor_to_str(state["exp_avg_sq"].view(-1)))
                 p_index += 1
 
         adamw_optimizer.step()
@@ -130,7 +134,7 @@ def generate_adamw_test_data(seed, _model_setup_func, data_func, train_step_coun
 
 
 def generate_torch_adamw_single_weight_tests(adam_mode, run_step_count):
-    seed=8888
+    seed = 8888
     device = "cuda"
     batch_size, dimension_in, dimension_hidden = 2, 2, 3
 
@@ -145,8 +149,9 @@ def generate_torch_adamw_single_weight_tests(adam_mode, run_step_count):
 
     generate_adamw_test_data(seed, _model_setup_func, _data_func, run_step_count, adam_mode)
 
+
 def generate_torch_adamw_multiple_weights_tests(adam_mode, run_step_count):
-    seed=6666
+    seed = 6666
     device = "cuda"
     batch_size, dimension_in, dimension_hidden, DIM_OUT = 2, 2, 3, 2
 
@@ -160,6 +165,7 @@ def generate_torch_adamw_multiple_weights_tests(adam_mode, run_step_count):
         return x1, target
 
     generate_adamw_test_data(seed, _model_setup_func, data_func, run_step_count, adam_mode)
+
 
 def main():
     test_data_step_count = 11
