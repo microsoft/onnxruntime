e80bada266c618b020bdfebe51e7e89b59a6cae9
Hari Seshadri <hasesh@microsoft.com>
2022-05-23 21:21:17 -0700

    e

--- onnxruntime/contrib_ops/cuda/bert/longformer_attention_impl.cu
+++ onnxruntime/contrib_ops/cuda/bert/longformer_attention_impl.cu
@@ -872,7 +872,7 @@ bool LongformerQkvToContext(
   T* temp_output = qkv;  // Q will be overwritten
 
   if (use_fast_kernel) {
-    assert(false);
+    /*
     if (!launchSoftmaxFastKernel(
             stream,
             cublas,
@@ -898,6 +898,7 @@ bool LongformerQkvToContext(
             element_size)) {
       return false;
     }
+    */
   } else {
     assert(false);
     /*
