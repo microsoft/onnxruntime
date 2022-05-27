0753293dff848276da000f86e35ecb660f265fd5
edgchen1 <18449977+edgchen1@users.noreply.github.com>
2022-05-27 14:01:44 -0700

    Update compile_schema.py to first delete generated Python files.

--- onnxruntime/core/flatbuffers/schema/compile_schema.py
+++ onnxruntime/core/flatbuffers/schema/compile_schema.py
@@ -4,9 +4,9 @@
 
 import argparse
 import pathlib
+import shutil
 import subprocess
 import tempfile
-
 
 SCRIPT_DIR = pathlib.Path(__file__).parent.resolve()
 
@@ -22,16 +22,16 @@ def update_namespace(schema_path: pathlib.Path, updated_schema_path: pathlib.Pat
             output.write(line.replace('onnxruntime.fbs', 'ort_flatbuffers_py.fbs'))
 
 
-def generate_python(flatc: pathlib.Path, schema_path: pathlib.Path):
+def generate_python(flatc: pathlib.Path, schema_path: pathlib.Path, output_dir: pathlib.Path):
     # run flatc to generate Python code
     cmd = [str(flatc), '--python', str(schema_path)]
-    subprocess.run(cmd, check=True, cwd=SCRIPT_DIR.parent)
+    subprocess.run(cmd, check=True, cwd=output_dir)
 
 
-def create_init_py():
+def create_init_py(output_dir: pathlib.Path):
     # create an __init__.py that imports all the py files so we can just 'import ort_flatbuffers_py.fbs'
     # in a script that wants to process an ORT format model
-    init_py_path = SCRIPT_DIR.parent / 'ort_flatbuffers_py/fbs/__init__.py'
+    init_py_path = output_dir / 'ort_flatbuffers_py/fbs/__init__.py'
     with open(init_py_path, 'w') as init_py:
         init_py.write('''from os.path import dirname, basename, isfile, join, splitext
 import glob
@@ -69,10 +69,20 @@ def main():
 
     if 'python' in languages:
         with tempfile.TemporaryDirectory() as temp_dir_name:
-            updated_schema_path = pathlib.Path(temp_dir_name, 'ort.py.fbs').resolve()
+            temp_dir = pathlib.Path(temp_dir_name).resolve()
+            updated_schema_path = temp_dir / 'ort.py.fbs'
             update_namespace(schema_path, updated_schema_path)
-            generate_python(flatc, updated_schema_path)
-        create_init_py()
+
+            output_dir = temp_dir / 'out'
+            output_dir.mkdir()
+            generate_python(flatc, updated_schema_path, output_dir)
+            create_init_py(output_dir)
+
+            # replace generated files in repo
+            target_dir = SCRIPT_DIR.parent / 'ort_flatbuffers_py'
+            if target_dir.is_dir():
+                shutil.rmtree(target_dir)
+            shutil.move(str(output_dir / 'ort_flatbuffers_py'), str(target_dir))
 
     if 'cpp' in languages:
         generate_cpp(flatc, schema_path)
