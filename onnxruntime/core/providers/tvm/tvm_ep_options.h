5cb30f022ebbbc112f53a6477cede535ae2bc381
Valery Chernov <valery.chernov@deelvin.com>
2022-05-31 13:58:57 +0300

    set_output_zero_copy was added to TVM EP options. it is true by default

--- onnxruntime/core/providers/tvm/tvm_ep_options.h
+++ onnxruntime/core/providers/tvm/tvm_ep_options.h
@@ -41,6 +41,7 @@ struct TvmEPOptions {
   unsigned int opt_level{tvm::default_opt_level};
   bool freeze_weights = true;
   bool to_nhwc = false;
+  bool set_output_zero_copy = true;
   std::string tuning_file_path{""};
   std::string tuning_type{tvm::default_tuning_type};
   std::string input_names_str{""};
