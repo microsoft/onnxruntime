61ad051f36af5ccbb8483f224c99b7ff09849eac
Yi-Hong Lyu <yilyu@microsoft.com>
2022-06-07 08:55:39 -0700

    Replace coefficient computation by fixed point

--- onnxruntime/core/providers/cpu/tensor/upsample.h
+++ onnxruntime/core/providers/cpu/tensor/upsample.h
@@ -69,6 +69,25 @@ struct BilinearParams {
   float* dy2;
 };
 
+struct BilinearParamsInteger {
+  std::vector<float> x_original;
+  std::vector<float> y_original;
+
+  BufferUniquePtr idx_scale_data_buffer_holder;
+
+  int32_t* input_width_mul_y1;
+  int32_t* input_width_mul_y2;
+
+  int32_t* in_x1;
+  int32_t* in_x2;
+
+  int32_t* dx1_scale_10;
+  int32_t* dx2_scale_10;
+
+  int32_t* dy1_scale_10;
+  int32_t* dy2_scale_10;
+};
+
 class UpsampleBase {
  protected:
   UpsampleBase(const OpKernelInfo& info) : scales_cached_(false), roi_cached_(false), use_extrapolation_(false) {
@@ -539,6 +558,105 @@ void NhwcUpsampleBilinear(const int32_t batch_size,
                                                           X21_coef * X21 +
                                                           X12_coef * X12 +
                                                           X22_coef * X22);
+              }
+            }
+          }
+        });
+  }
+}
+
+BilinearParamsInteger SetupUpsampleBilinearInteger(const int32_t input_height,
+                                                   const int32_t input_width,
+                                                   const int32_t output_height,
+                                                   const int32_t output_width,
+                                                   const float height_scale,
+                                                   const float width_scale,
+                                                   const std::vector<float>& roi,
+                                                   AllocatorPtr& alloc,
+                                                   const GetOriginalCoordinateFunc& get_original_coordinate,
+                                                   bool is_nchw);
+
+template <typename T, bool UseExtrapolation>
+void NhwcUpsampleBilinearInteger(const int32_t batch_size,
+                                 const int32_t num_channels,
+                                 const int32_t input_height,
+                                 const int32_t input_width,
+                                 const int32_t output_height,
+                                 const int32_t output_width,
+                                 const float height_scale,
+                                 const float width_scale,
+                                 const std::vector<float>& roi,
+                                 const float extrapolation_value,
+                                 const T* const XdataBase,
+                                 T* const YdataBase,
+                                 AllocatorPtr& alloc,
+                                 const GetOriginalCoordinateFunc& get_original_coordinate,
+                                 concurrency::ThreadPool* tp) {
+  BilinearParamsInteger p = SetupUpsampleBilinearInteger(input_height, input_width, output_height, output_width,
+                                                         height_scale, width_scale, roi,
+                                                         alloc, get_original_coordinate, false);
+  for (int64_t n = 0; n < batch_size; ++n) {
+    const T* Xdata = XdataBase + n * (input_height * input_width) * num_channels;
+    T* Ydata = YdataBase + n * (output_height * output_width) * num_channels;
+    concurrency::ThreadPool::TryParallelFor(
+        tp, output_height * output_width,
+        static_cast<double>(num_channels * 2),
+        [&](std::ptrdiff_t first, std::ptrdiff_t last) {
+          for (std::ptrdiff_t i = first; i < last; ++i) {
+            const int32_t x = static_cast<int32_t>(i % output_width);
+            const int32_t y = static_cast<int32_t>(i / output_width);
+
+            // when use_extrapolation is set and original index of x or y is out of the dim range
+            // then use extrapolation_value as the output value.
+            if constexpr (UseExtrapolation) {
+              if ((p.y_original[y] < 0 || p.y_original[y] > static_cast<float>(input_height - 1)) ||
+                  (p.x_original[x] < 0 || p.x_original[x] > static_cast<float>(input_width - 1))) {
+                for (int32_t c = 0; c < num_channels; ++c) {
+                  Ydata[(output_width * y + x) * num_channels + c] = static_cast<T>(extrapolation_value);
+                }
+              } else {
+                int32_t X11_offset = (p.input_width_mul_y1[y] + p.in_x1[x]) * num_channels;
+                int32_t X21_offset = (p.input_width_mul_y1[y] + p.in_x2[x]) * num_channels;
+                int32_t X12_offset = (p.input_width_mul_y2[y] + p.in_x1[x]) * num_channels;
+                int32_t X22_offset = (p.input_width_mul_y2[y] + p.in_x2[x]) * num_channels;
+                int32_t X11_coef_scale_20 = p.dx2_scale_10[x] * p.dy2_scale_10[y];
+                int32_t X21_coef_scale_20 = p.dx1_scale_10[x] * p.dy2_scale_10[y];
+                int32_t X12_coef_scale_20 = p.dx2_scale_10[x] * p.dy1_scale_10[y];
+                int32_t X22_coef_scale_20 = p.dx1_scale_10[x] * p.dy1_scale_10[y];
+                for (int32_t c = 0; c < num_channels; ++c) {
+                  T X11 = Xdata[X11_offset + c];
+                  T X21 = Xdata[X21_offset + c];
+                  T X12 = Xdata[X12_offset + c];
+                  T X22 = Xdata[X22_offset + c];
+
+                  Ydata[(output_width * y + x) * num_channels + c] = static_cast<T>((X11_coef_scale_20 * X11 +
+                                                                                     X21_coef_scale_20 * X21 +
+                                                                                     X12_coef_scale_20 * X12 +
+                                                                                     X22_coef_scale_20 * X22) /
+                                                                                    (1 << 20));
+                }
+              }
+            } else {
+              int32_t output_offset = (output_width * y + x) * num_channels;
+              int32_t X11_offset = (p.input_width_mul_y1[y] + p.in_x1[x]) * num_channels;
+              int32_t X21_offset = (p.input_width_mul_y1[y] + p.in_x2[x]) * num_channels;
+              int32_t X12_offset = (p.input_width_mul_y2[y] + p.in_x1[x]) * num_channels;
+              int32_t X22_offset = (p.input_width_mul_y2[y] + p.in_x2[x]) * num_channels;
+              int32_t X11_coef_scale_20 = p.dx2_scale_10[x] * p.dy2_scale_10[y];
+              int32_t X21_coef_scale_20 = p.dx1_scale_10[x] * p.dy2_scale_10[y];
+              int32_t X12_coef_scale_20 = p.dx2_scale_10[x] * p.dy1_scale_10[y];
+              int32_t X22_coef_scale_20 = p.dx1_scale_10[x] * p.dy1_scale_10[y];
+              for (int32_t c = 0; c < num_channels; ++c) {
+                T X11 = Xdata[X11_offset + c];
+                T X21 = Xdata[X21_offset + c];
+                T X12 = Xdata[X12_offset + c];
+                T X22 = Xdata[X22_offset + c];
+
+                Ydata[output_offset + c] = static_cast<T>((X11_coef_scale_20 * X11 +
+                                                           X21_coef_scale_20 * X21 +
+                                                           X12_coef_scale_20 * X12 +
+                                                           X22_coef_scale_20 * X22) /
+                                                          (1 << 20));
               }
             }
           }
