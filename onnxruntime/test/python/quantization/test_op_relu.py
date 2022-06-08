f6f457aa5734e095726bdec1e46eb6f427c5fb1d
Yufeng Li <liyufeng1987@gmail.com>
2022-06-07 18:02:31 -0700

    not remove relu/clip for symmetric activation (#11696)
    * not remove relu/clip for symmetric activation

--- onnxruntime/test/python/quantization/test_op_relu.py
+++ onnxruntime/test/python/quantization/test_op_relu.py
@@ -13,10 +13,10 @@ import onnx
 from onnx import TensorProto, helper
 from op_test_utils import TestDataFeeds, check_model_correctness, check_op_type_count, check_qtype_by_node_type
 
-from onnxruntime.quantization import QuantFormat, QuantType, quantize_dynamic, quantize_static
+from onnxruntime.quantization import QuantFormat, QuantType, quantize_static
 
 
-class TestOpGemm(unittest.TestCase):
+class TestOpRelu(unittest.TestCase):
     def input_feeds(self, n, name2shape):
         input_data_list = []
         for i in range(n):
@@ -32,7 +32,7 @@ class TestOpGemm(unittest.TestCase):
         #         |
         #        Gemm
         #         |
-        #        Clip
+        #        Relu
         #         |
         #        Gemm
         #         |
@@ -68,19 +68,13 @@ class TestOpGemm(unittest.TestCase):
             gemm1_output_name,
         )
 
-        # make Clip
-        clip_min_name = "clip_min"
-        clip_max_name = "clip_max"
-        clip_output_name = "clip_output"
-        clip_inputs = [gemm1_output_name, clip_min_name, clip_max_name]
-        clip_outputs = [clip_output_name]
-        initializers.append(onnx.numpy_helper.from_array(np.array(-1.0, dtype=np.float32), name=clip_min_name))
-        initializers.append(onnx.numpy_helper.from_array(np.array(1.0, dtype=np.float32), name=clip_max_name))
-        clip_node = onnx.helper.make_node("Clip", clip_inputs, clip_outputs)
+        # make Relu
+        relu_output = "relu_output"
+        relu_node = onnx.helper.make_node("Relu", [gemm1_output_name], [relu_output])
 
         # make gemm2 node
         gemm2_node = make_gemm(
-            clip_output_name,
+            relu_output,
             [10, 100],
             "linear2.weight",
             [10],
@@ -91,16 +85,16 @@ class TestOpGemm(unittest.TestCase):
         # make graph
         input_tensor = helper.make_tensor_value_info(input_name, TensorProto.FLOAT, [-1, 10])
         output_tensor = helper.make_tensor_value_info(output_name, TensorProto.FLOAT, [-1, 10])
-        graph_name = "gemm_test"
+        graph_name = "relu_test"
         graph = helper.make_graph(
-            [gemm1_node, clip_node, gemm2_node],
+            [gemm1_node, relu_node, gemm2_node],
             graph_name,
             [input_tensor],
             [output_tensor],
             initializer=initializers,
         )
         model = helper.make_model(graph, opset_imports=[helper.make_opsetid("", 13)])
-        model.ir_version = 7  # use stable onnx ir version
+        model.ir_version = onnx.IR_VERSION
 
         onnx.save(model, output_model_path)
 
@@ -115,7 +109,7 @@ class TestOpGemm(unittest.TestCase):
         activation_proto_qtype = TensorProto.UINT8 if activation_type == QuantType.QUInt8 else TensorProto.INT8
         activation_type_str = "u8" if (activation_type == QuantType.QUInt8) else "s8"
         weight_type_str = "u8" if (weight_type == QuantType.QUInt8) else "s8"
-        model_int8_path = "gemm_fp32.quant_{}{}.onnx".format(activation_type_str, weight_type_str)
+        model_int8_path = "relu_fp32.quant_{}{}.onnx".format(activation_type_str, weight_type_str)
 
         data_reader.rewind()
         quantize_static(
@@ -129,8 +123,8 @@ class TestOpGemm(unittest.TestCase):
         )
 
         qdq_count = 1 if activation_type == QuantType.QUInt8 else 2
-        clip_count = 0 if activation_type == QuantType.QUInt8 else 1
-        quant_nodes = {"QGemm": 2, "QuantizeLinear": qdq_count, "DequantizeLinear": qdq_count, "Clip": clip_count}
+        relu_count = 0 if activation_type == QuantType.QUInt8 else 1
+        quant_nodes = {"QGemm": 2, "QuantizeLinear": qdq_count, "DequantizeLinear": qdq_count, "Relu": relu_count}
         check_op_type_count(self, model_int8_path, **quant_nodes)
         qnode_io_qtypes = {
             "QuantizeLinear": [
@@ -154,7 +148,7 @@ class TestOpGemm(unittest.TestCase):
         activation_proto_qtype = TensorProto.UINT8 if activation_type == QuantType.QUInt8 else TensorProto.INT8
         activation_type_str = "u8" if (activation_type == QuantType.QUInt8) else "s8"
         weight_type_str = "u8" if (weight_type == QuantType.QUInt8) else "s8"
-        model_int8_path = "gemm_fp32.quant_dqd_{}{}.onnx".format(activation_type_str, weight_type_str)
+        model_int8_path = "relu_fp32.quant_dqd_{}{}.onnx".format(activation_type_str, weight_type_str)
 
         data_reader.rewind()
         quantize_static(
@@ -167,10 +161,10 @@ class TestOpGemm(unittest.TestCase):
             extra_options=extra_options,
         )
 
-        clip_count = 0 if activation_type == QuantType.QUInt8 else 1
+        relu_count = 0 if activation_type == QuantType.QUInt8 else 1
         q_count = 3 if activation_type == QuantType.QUInt8 else 4
         dq_count = 7 if activation_type == QuantType.QUInt8 else 8
-        quant_nodes = {"Gemm": 2, "QuantizeLinear": q_count, "DequantizeLinear": dq_count, "Clip": clip_count}
+        quant_nodes = {"Gemm": 2, "QuantizeLinear": q_count, "DequantizeLinear": dq_count, "Relu": relu_count}
         check_op_type_count(self, model_int8_path, **quant_nodes)
         qnode_io_qtypes = {
             "QuantizeLinear": [
@@ -182,40 +176,9 @@ class TestOpGemm(unittest.TestCase):
         data_reader.rewind()
         check_model_correctness(self, model_fp32_path, model_int8_path, data_reader.get_next())
 
-    def dynamic_quant_test(
-        self,
-        model_fp32_path,
-        data_reader,
-        activation_type,
-        weight_type,
-        extra_options={},
-    ):
-        activation_proto_qtype = TensorProto.UINT8 if activation_type == QuantType.QUInt8 else TensorProto.INT8
-        activation_type_str = "u8" if (activation_type == QuantType.QUInt8) else "s8"
-        weight_type_str = "u8" if (weight_type == QuantType.QUInt8) else "s8"
-        model_int8_path = "gemm_fp32.quant_dynamic_{}{}.onnx".format(activation_type_str, weight_type_str)
-
-        quantize_dynamic(
-            model_fp32_path,
-            model_int8_path,
-            weight_type=weight_type,
-            extra_options=extra_options,
-        )
-        quant_nodes = {"MatMulInteger": 2}
-        check_op_type_count(self, model_int8_path, **quant_nodes)
-        qnode_io_qtypes = {"MatMulInteger": [["i", 2, activation_proto_qtype]]}
-        check_qtype_by_node_type(self, model_int8_path, qnode_io_qtypes)
-        data_reader.rewind()
-        check_model_correctness(
-            self,
-            model_fp32_path,
-            model_int8_path,
-            {"input": np.random.rand(5, 10).astype(np.float32)},
-        )
-
     def test_quantize_gemm(self):
         np.random.seed(1)
-        model_fp32_path = "gemm_fp32.onnx"
+        model_fp32_path = "relu_fp32.onnx"
         self.construct_model_gemm(model_fp32_path)
         data_reader = self.input_feeds(1, {"input": [5, 10]})
 
@@ -231,16 +194,10 @@ class TestOpGemm(unittest.TestCase):
             activation_type=QuantType.QUInt8,
             weight_type=QuantType.QUInt8,
         )
-        self.dynamic_quant_test(
-            model_fp32_path,
-            data_reader,
-            activation_type=QuantType.QUInt8,
-            weight_type=QuantType.QUInt8,
-        )
 
-    def test_quantize_gemm_s8s8(self):
+    def test_quantize_relu_s8s8(self):
         np.random.seed(1)
-        model_fp32_path = "gemm_fp32.onnx"
+        model_fp32_path = "relu_fp32.onnx"
         self.construct_model_gemm(model_fp32_path)
         data_reader = self.input_feeds(1, {"input": [5, 10]})
 
@@ -258,10 +215,6 @@ class TestOpGemm(unittest.TestCase):
             weight_type=QuantType.QInt8,
             extra_options={"ActivationSymmetric": True},
         )
-
-        # dynamic quantization doesn't support activation:int8
-        # self.dynamic_quant_test(model_fp32_path, data_reader, activation_type=QuantType.QInt8, weight_type=QuantType.QInt8,
-        #                        extra_options={'ActivationSymmetric': True})
 
 
 if __name__ == "__main__":
