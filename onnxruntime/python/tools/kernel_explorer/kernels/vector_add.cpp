449ee722baac259948af66a5a2ca339795faa8fc
Yao Zhang <zhanyao@microsoft.com>
2022-06-07 21:47:18 -0700

    macro renaming

--- onnxruntime/python/tools/kernel_explorer/kernels/vector_add.cpp
+++ onnxruntime/python/tools/kernel_explorer/kernels/vector_add.cpp
@@ -35,24 +35,24 @@ class VectorAdd: public Operator<T> {
     .def("Profile", &name<type, threads_per_block, vec_size>::Profile)                                    \
     .def("Run", &name<type, threads_per_block, vec_size>::Run);
   
-#define REGISTER_OP_VEC_SIZES(name, type, threads_per_block)  \
+#define REGISTER_OP_FOR_ALL_VEC_SIZE(name, type, threads_per_block)  \
   REGISTER_OP(name, type, threads_per_block, 1)               \
   REGISTER_OP(name, type, threads_per_block, 2)               \
   REGISTER_OP(name, type, threads_per_block, 4)               \
   REGISTER_OP(name, type, threads_per_block, 8)
 
-#define REGISTER_OP_THRADS_PER_BLOCK(name, type)  \
-  REGISTER_OP_VEC_SIZES(name, type, 64)          \
-  REGISTER_OP_VEC_SIZES(name, type, 128)         \
-  REGISTER_OP_VEC_SIZES(name, type, 192)         \
-  REGISTER_OP_VEC_SIZES(name, type, 256)         \
-  REGISTER_OP_VEC_SIZES(name, type, 320)         \
-  REGISTER_OP_VEC_SIZES(name, type, 384)         \
-  REGISTER_OP_VEC_SIZES(name, type, 448)         \
-  REGISTER_OP_VEC_SIZES(name, type, 512)
+#define REGISTER_OP_FOR_ALL_THREADS_PER_BLOCK(name, type)  \
+  REGISTER_OP_FOR_ALL_VEC_SIZE(name, type, 64)          \
+  REGISTER_OP_FOR_ALL_VEC_SIZE(name, type, 128)         \
+  REGISTER_OP_FOR_ALL_VEC_SIZE(name, type, 192)         \
+  REGISTER_OP_FOR_ALL_VEC_SIZE(name, type, 256)         \
+  REGISTER_OP_FOR_ALL_VEC_SIZE(name, type, 320)         \
+  REGISTER_OP_FOR_ALL_VEC_SIZE(name, type, 384)         \
+  REGISTER_OP_FOR_ALL_VEC_SIZE(name, type, 448)         \
+  REGISTER_OP_FOR_ALL_VEC_SIZE(name, type, 512)
   
 
 void InitVectorAdd(py::module m) {
-  REGISTER_OP_THRADS_PER_BLOCK(VectorAdd, half);
-  REGISTER_OP_THRADS_PER_BLOCK(VectorAdd, float);
+  REGISTER_OP_FOR_ALL_THREADS_PER_BLOCK(VectorAdd, half);
+  REGISTER_OP_FOR_ALL_THREADS_PER_BLOCK(VectorAdd, float);
 }
