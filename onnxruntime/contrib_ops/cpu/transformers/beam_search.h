def78a1b815ae2b60d19df97c352230591e7d7a6
Tianlei Wu <tlwu@microsoft.com>
2022-06-10 15:06:57 -0700

    Support T5 in BeamSearch operator (#11450)
    (1) Support T5 in BeamSearch operator, and add both CPU and CUDA implementation.
    (2) Change BeamSearch op: rename encoder_decoder_init attribute to encoder, and add decoder_start_token_id attribute
    (3) Update convert_to_onnx for T5 to use int32 instead of int64 inputs as default.
    (4) Add more tests in best_beam_search.py
    (5) fix ORT_ENFORCE of hypothesis_buffer_offset_
    (6) Improve ONNX conversion:
       (a) Change encoder some dynamic axes to fixed dim value
       (b) add --separate_encoder_and_decoder_init
       (c) correct name t5-3B => t5-3b, t5-11B => t5-11b
       (d) Add --use_int32_inputs in convert t5 to onnx
       (e) Allow t5 beam search conversion in one step

--- onnxruntime/contrib_ops/cpu/transformers/beam_search.h
+++ onnxruntime/contrib_ops/cpu/transformers/beam_search.h
@@ -2,12 +2,16 @@
 // Licensed under the MIT License.
 
 #pragma once
+
+#include <memory>
 #include "core/common/common.h"
 #include "core/framework/op_kernel.h"
 #include "core/providers/cpu/controlflow/utils.h"
-#include "beam_search_parameters.h"
-#include "gpt_subgraph.h"
-#include "beam_search_device_helper.h"
+#include "contrib_ops/cpu/transformers/beam_search_parameters.h"
+#include "contrib_ops/cpu/transformers/subgraph_gpt.h"
+#include "contrib_ops/cpu/transformers/subgraph_t5_encoder.h"
+#include "contrib_ops/cpu/transformers/subgraph_t5_decoder.h"
+#include "contrib_ops/cpu/transformers/beam_search_device_helper.h"
 
 namespace onnxruntime {
 class FeedsFetchesManager;
@@ -20,7 +24,11 @@ using namespace onnxruntime::controlflow;  // namespace of IControlFlowKernel
 class BeamSearch : public IControlFlowKernel {
  public:
   BeamSearch(const OpKernelInfo& info)
-      : IControlFlowKernel(info), feeds_fetches_manager_(nullptr), cuda_stream_(nullptr), dumper_(nullptr) {
+      : IControlFlowKernel(info),
+        encoder_feeds_fetches_manager_(nullptr),
+        decoder_feeds_fetches_manager_(nullptr),
+        cuda_stream_(nullptr),
+        dumper_(nullptr) {
     Init(info);
   }
 
@@ -36,54 +44,76 @@ class BeamSearch : public IControlFlowKernel {
   void SetComputeStream(void* stream) { cuda_stream_ = stream; }
   void SetConsoleDumper(IConsoleDumper* dumper) { dumper_ = dumper; }
 
+  // device helpers that is same for both GPT and encoder-decoder models.
   void SetDeviceHelpers(
-      // const BeamSearchDeviceHelper::CreateInputsFunc& create_inputs_func,
       const BeamSearchDeviceHelper::AddToFeedsFunc& add_to_feeds_func,
-      const BeamSearchDeviceHelper::TopkFunc& topk_func) {
-    // create_inputs_func_ = create_inputs_func;
+      const BeamSearchDeviceHelper::TopkFunc& topk_func,
+      const BeamSearchDeviceHelper::DeviceCopyFunc<float>& device_copy_func,
+      const BeamSearchDeviceHelper::DeviceCopyFunc<int32_t>& device_copy_int32_func,
+      const BeamSearchDeviceHelper::ProcessLogitsFunc<float>& process_logits_func,
+      const BeamSearchDeviceHelper::ProcessLogitsFunc<MLFloat16>& process_logits_fp16_func,
+      const BeamSearchDeviceHelper::InitBeamStateFunc<float>& init_beam_state_func,
+      const BeamSearchDeviceHelper::InitBeamStateFunc<MLFloat16>& init_beam_state_fp16_func) {
     add_to_feeds_func_ = add_to_feeds_func;
     topk_func_ = topk_func;
-  }
-
-  // Type dependent helpers: float
-  void SetDeviceHelpers(
-      const BeamSearchDeviceHelper::ProcessLogitsFunc<float>& process_logits_func,
-      const BeamSearchDeviceHelper::InitBeamStateFunc<float>& init_beam_state_func,
-      const BeamSearchDeviceHelper::DeviceCopyFunc<float>& device_copy_func,
-      const BeamSearchDeviceHelper::UpdateFeedsFunc<float>& update_feeds_func) {
-    process_logits_func_ = process_logits_func;
-    init_beam_state_func_ = init_beam_state_func;
     device_copy_func_ = device_copy_func;
-    update_feeds_func_ = update_feeds_func;
+    device_copy_int32_func_ = device_copy_int32_func;
+    process_logits_func_ = process_logits_func;
+    process_logits_fp16_func_ = process_logits_fp16_func;
+    init_beam_state_func_ = init_beam_state_func;
+    init_beam_state_fp16_func_ = init_beam_state_fp16_func;
   }
 
-  // Type dependent helpers: MLFloat16
-  void SetDeviceHelpers(
-      const BeamSearchDeviceHelper::ProcessLogitsFunc<MLFloat16>& process_logits_func,
-      const BeamSearchDeviceHelper::InitBeamStateFunc<MLFloat16>& init_beam_state_func,
-      const BeamSearchDeviceHelper::UpdateFeedsFunc<MLFloat16>& update_feeds_func) {
-    process_logits_fp16_func_ = process_logits_func;
-    init_beam_state_fp16_func_ = init_beam_state_func;
-    update_feeds_fp16_func_ = update_feeds_func;
+  void SetDeviceHelpers_Gpt(
+      const BeamSearchDeviceHelper::UpdateGptFeedsFunc<float>& update_gpt_feeds_func,
+      const BeamSearchDeviceHelper::UpdateGptFeedsFunc<MLFloat16>& update_gpt_feeds_fp16_func) {
+    update_gpt_feeds_func_ = update_gpt_feeds_func;
+    update_gpt_feeds_fp16_func_ = update_gpt_feeds_fp16_func;
+  }
+
+  // device helpers for encoder-decoder model like T5
+  void SetDeviceHelpers_EncoderDecoder(
+      const BeamSearchDeviceHelper::UpdateDecoderFeedsFunc<float>& update_decoder_feeds_func,
+      const BeamSearchDeviceHelper::UpdateDecoderFeedsFunc<MLFloat16>& update_decoder_feeds_fp16_func) {
+    update_decoder_feeds_func_ = update_decoder_feeds_func;
+    update_decoder_feeds_fp16_func_ = update_decoder_feeds_fp16_func;
   }
 
  private:
   // Device specific functions
-  BeamSearchDeviceHelper::CreateInputsFunc create_inputs_func_;
   BeamSearchDeviceHelper::AddToFeedsFunc add_to_feeds_func_;
   BeamSearchDeviceHelper::TopkFunc topk_func_;
-  BeamSearchDeviceHelper::ProcessLogitsFunc<float> process_logits_func_;
-  BeamSearchDeviceHelper::InitBeamStateFunc<float> init_beam_state_func_;
   BeamSearchDeviceHelper::DeviceCopyFunc<float> device_copy_func_;
-  BeamSearchDeviceHelper::UpdateFeedsFunc<float> update_feeds_func_;
+  BeamSearchDeviceHelper::DeviceCopyFunc<int32_t> device_copy_int32_func_;
 
+  BeamSearchDeviceHelper::ProcessLogitsFunc<float> process_logits_func_;
   BeamSearchDeviceHelper::ProcessLogitsFunc<MLFloat16> process_logits_fp16_func_;
-  BeamSearchDeviceHelper::InitBeamStateFunc<MLFloat16> init_beam_state_fp16_func_;
-  BeamSearchDeviceHelper::UpdateFeedsFunc<MLFloat16> update_feeds_fp16_func_;
 
+  BeamSearchDeviceHelper::InitBeamStateFunc<float> init_beam_state_func_;
+  BeamSearchDeviceHelper::InitBeamStateFunc<MLFloat16> init_beam_state_fp16_func_;
+
+  //------------------------------------------------------------
+  // Device specific functions for GPT
+  //------------------------------------------------------------
+  BeamSearchDeviceHelper::UpdateGptFeedsFunc<float> update_gpt_feeds_func_;
+  BeamSearchDeviceHelper::UpdateGptFeedsFunc<MLFloat16> update_gpt_feeds_fp16_func_;
+
+  //------------------------------------------------------------
+  // Device specific functions for encoder-decoder model like T5
+  //------------------------------------------------------------
+  BeamSearchDeviceHelper::CreateEncoderInputsFunc create_encoder_inputs_func_;
+
+  BeamSearchDeviceHelper::UpdateDecoderFeedsFunc<float> update_decoder_feeds_func_;
+  BeamSearchDeviceHelper::UpdateDecoderFeedsFunc<MLFloat16> update_decoder_feeds_fp16_func_;
+
+  //------------------------------------------------------------
   // Subgraph and FeedsFetchesManager re-used for each subgraph execution.
+  //------------------------------------------------------------
   std::unique_ptr<GptSubgraph> gpt_subgraph_;
-  FeedsFetchesManager* feeds_fetches_manager_;
+  std::unique_ptr<T5EncoderSubgraph> t5_encoder_subgraph_;
+  std::unique_ptr<T5DecoderSubgraph> t5_decoder_subgraph_;
+  FeedsFetchesManager* encoder_feeds_fetches_manager_;
+  FeedsFetchesManager* decoder_feeds_fetches_manager_;
 
   void* cuda_stream_;
 
