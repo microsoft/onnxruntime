85fcaaea7ea277aef53c3306c3e293620b73e837
Yulong Wang <7679871+fs-eire@users.noreply.github.com>
2022-06-13 17:20:04 -0700

    consume onnx test data from github

--- js/node/test/test-utils.ts
+++ js/node/test/test-utils.ts
@@ -12,7 +12,7 @@ export const TEST_ROOT = __dirname;
 export const TEST_DATA_ROOT = path.join(TEST_ROOT, 'testdata');
 
 export const ORT_ROOT = path.join(__dirname, '../../..');
-export const NODE_TESTS_ROOT = path.join(ORT_ROOT, 'cmake/external/onnx/onnx/backend/test/data/node');
+export const NODE_TESTS_ROOT = path.join(ORT_ROOT, 'js/test/data/node');
 
 export const SQUEEZENET_INPUT0_DATA: number[] = require(path.join(TEST_DATA_ROOT, 'squeezenet.input0.json'));
 export const SQUEEZENET_OUTPUT0_DATA: number[] = require(path.join(TEST_DATA_ROOT, 'squeezenet.output0.json'));
