74e583ef3b9ea7fdaa9a15efc0c0de709e580416
Yi-Hong Lyu <yilyu@microsoft.com>
2022-06-14 14:15:51 -0700

    Separate UpsampleBase from upsample.h
    Revert CMakeLists.txt to support CUDA 10.2 on Linux

--- onnxruntime/test/providers/cpu/tensor/upsample_op_test.cc
+++ onnxruntime/test/providers/cpu/tensor/upsample_op_test.cc
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-#include "core/providers/cpu/tensor/upsample.h"
+#include "core/providers/cpu/tensor/upsamplebase.h"
 #include "gtest/gtest.h"
 #include "test/providers/provider_test_utils.h"
 
