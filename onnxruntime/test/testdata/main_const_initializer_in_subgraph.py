b228b2d8e709fd8aa79050e728b7a17bc3a4b4fd
Chi Lo <Chi.Lo@microsoft.com>
2022-06-15 23:10:16 +0000

    modify python format

--- onnxruntime/test/testdata/main_const_initializer_in_subgraph.py
+++ onnxruntime/test/testdata/main_const_initializer_in_subgraph.py
@@ -11,38 +11,37 @@ body = helper.make_graph(
     ],
     "Loop_body",
     [
-        helper.make_tensor_value_info('iteration_num', TensorProto.INT64, [1]),
-        helper.make_tensor_value_info('subgraph_keep_going_in', TensorProto.BOOL, [1]),
-        helper.make_tensor_value_info('loop_state_in', TensorProto.FLOAT, [1])
+        helper.make_tensor_value_info("iteration_num", TensorProto.INT64, [1]),
+        helper.make_tensor_value_info("subgraph_keep_going_in", TensorProto.BOOL, [1]),
+        helper.make_tensor_value_info("loop_state_in", TensorProto.FLOAT, [1]),
     ],
     [
-        helper.make_tensor_value_info('subgraph_keep_going_in', TensorProto.BOOL, [1]),
-        helper.make_tensor_value_info('loop_state_out', TensorProto.FLOAT, [1]),
+        helper.make_tensor_value_info("subgraph_keep_going_in", TensorProto.BOOL, [1]),
+        helper.make_tensor_value_info("loop_state_out", TensorProto.FLOAT, [1]),
     ],
     [
-        helper.make_tensor('sub_graph_initializer', TensorProto.FLOAT, [1], [1.]),
+        helper.make_tensor("sub_graph_initializer", TensorProto.FLOAT, [1], [1.0]),
     ]
 )
 
 # Create the main graph
 graph_proto = helper.make_graph(
     [
-        helper.make_node("Loop", ["max_trip_count", "keep_going", "state_var_in"],
-                         ["state_var_out"], "Loop1", body=body)
+        helper.make_node("Loop", ["max_trip_count", "keep_going", "state_var_in"], ["state_var_out"], "Loop1", body=body)
     ],
     "Main_graph",
     [
-        helper.make_tensor_value_info('state_var_in', TensorProto.FLOAT, [1]),
+        helper.make_tensor_value_info("state_var_in", TensorProto.FLOAT, [1]),
     ],
     [
-        helper.make_tensor_value_info('state_var_out', TensorProto.FLOAT, [1]),
+        helper.make_tensor_value_info("state_var_out", TensorProto.FLOAT, [1]),
     ],
     [
-        helper.make_tensor('max_trip_count', TensorProto.INT64, [1], [1]),
-        helper.make_tensor('main_graph_initializer', TensorProto.FLOAT, [1], [1.]),
-        helper.make_tensor('keep_going', TensorProto.BOOL, [1], [True]),
+        helper.make_tensor("max_trip_count", TensorProto.INT64, [1], [1]),
+        helper.make_tensor("main_graph_initializer", TensorProto.FLOAT, [1], [1.0]),
+        helper.make_tensor("keep_going", TensorProto.BOOL, [1], [True]),
     ]
 )
 
 model = helper.make_model(graph_proto)
-onnx.save(model, 'main_const_initializer_in_subgraph.onnx')
+onnx.save(model, "main_const_initializer_in_subgraph.onnx")
