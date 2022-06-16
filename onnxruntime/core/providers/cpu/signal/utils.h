1a380b3dc8750df699b30ea12c895c8299f1f63d
Gary Miguel <garymiguel@microsoft.com>
2022-06-16 00:05:09 +0000

    simplify get_scalar_value_from_tensor

--- onnxruntime/core/providers/cpu/signal/utils.h
+++ onnxruntime/core/providers/cpu/signal/utils.h
@@ -11,20 +11,7 @@ namespace signal {
 template <typename T>
 static T get_scalar_value_from_tensor(const Tensor* tensor) {
   ORT_ENFORCE(tensor->Shape().Size() == 1, "ratio input should have a single value.");
-
-  auto data_type = tensor->GetElementType();
-  switch (data_type) {
-    case ONNX_NAMESPACE::TensorProto_DataType_FLOAT:
-      return static_cast<T>(*reinterpret_cast<const float*>(tensor->DataRaw()));
-    case ONNX_NAMESPACE::TensorProto_DataType_DOUBLE:
-      return static_cast<T>(*reinterpret_cast<const double*>(tensor->DataRaw()));
-    case ONNX_NAMESPACE::TensorProto_DataType_INT32:
-      return static_cast<T>(*reinterpret_cast<const int32_t*>(tensor->DataRaw()));
-    case ONNX_NAMESPACE::TensorProto_DataType_INT64:
-      return static_cast<T>(*reinterpret_cast<const int64_t*>(tensor->DataRaw()));
-    default:
-      ORT_THROW("Unsupported input data type of ", data_type);
-  }
+  return *tensor->Data<T>();
 }
 
 }  // namespace signal
