477e0fbc436949c3977866a301fdeb9a75d111e3
Yi Zhang <zhanyi@microsoft.com>
2022-06-17 09:06:03 -0700

    add comments

--- tools/ci_build/github/linux/docker/build_scripts/install-entrypoint.sh
+++ tools/ci_build/github/linux/docker/build_scripts/install-entrypoint.sh
@@ -27,6 +27,7 @@ if [ "${AUDITWHEEL_POLICY}" = "musllinux_1_1" ]; then
     apk add --no-cache bash
 fi
 
+# https://www.shellcheck.net/wiki/SC3020
 if command -v yum 2>&1 ; then
     yum install -y yum-plugin-versionlock
     yum versionlock cuda* libcudnn* libnccl*
