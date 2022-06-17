4550383255e85a0e90124208bbadf7af6012f2c9
Yi-Hong Lyu <yilyu@microsoft.com>
2022-06-17 11:00:36 -0700

    NHWC Resize optimization (#11825)
    The optimization consists of:
    
    * Use int32_t instead of int64_t
    * Use different code path for tf_crop_and_resize or other
      coordinate_transformation_mode to avoid redundant conditions
    * Loop-invariant code motion of offset, coefficient and extrapolation_value
      check
    * Use fixed point to avoid floating-point computation
    
    Besides, it always transforms NCHW Resize to NHWC because it has higher perf in
    the NHWC variant when the input X is 4D int8/uint8 tensor and the mode is
    linear on ARM.
    
    It improves DeepLab V3 with int8 quantization by 26%~27% on big core and 37% on
    LITTLE core on AArch64. It also improves DeepLab V3 with uint8 quantization by
    24%~25% on big core and 34% on LITTLE core on AArch64.
    
    Co-authored-by: Yufeng Li liyufeng1987@gmail.com

--- onnxruntime/test/providers/cpu/tensor/upsample_op_test.cc
+++ onnxruntime/test/providers/cpu/tensor/upsample_op_test.cc
@@ -1,7 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-#include "core/providers/cpu/tensor/upsample.h"
 #include "gtest/gtest.h"
 #include "test/providers/provider_test_utils.h"
 
