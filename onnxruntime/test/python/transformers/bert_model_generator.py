955c1b59b8fe8f10ce9f2303aafc5fca823c603c
Gary Miguel <garymiguel@microsoft.com>
2022-06-20 20:24:44 +0000

    fix pylint violation

--- onnxruntime/test/python/transformers/bert_model_generator.py
+++ onnxruntime/test/python/transformers/bert_model_generator.py
@@ -253,8 +253,7 @@ def create_bert_attention(
     # Needed so that we don't see the new LayerNormalization function added in version 17.
     # TODO(https://github.com/microsoft/onnxruntime/issues/11916): Remove once fixed.
     opsetid = helper.make_opsetid("ai.onnx", min(onnx.defs.onnx_opset_version(), 16))
-    model = helper.make_model(graph, opset_imports=(opsetid,))
-    return model
+    return helper.make_model(graph, opset_imports=(opsetid,))
 
 
 def create_tf2onnx_attention_3d(input_hidden_size=16, num_heads=4, head_size=4, use_float_mask=False):
@@ -417,8 +416,7 @@ def create_tf2onnx_attention_3d(input_hidden_size=16, num_heads=4, head_size=4, 
         initializers,
     )
 
-    model = helper.make_model(graph)
-    return model
+    return helper.make_model(graph)
 
 
 if __name__ == "__main__":
--- onnxruntime/test/python/transformers/gpt2_model_generator.py
+++ onnxruntime/test/python/transformers/gpt2_model_generator.py
@@ -546,8 +546,7 @@ def create_gpt2_attention(hidden_size=64, num_heads=4, max_seq_len=32, switch_ad
     # Needed so that we don't see the new LayerNormalization function added in version 17.
     # TODO(https://github.com/microsoft/onnxruntime/issues/11916): Remove once fixed.
     opsetid = helper.make_opsetid("ai.onnx", min(onnx.defs.onnx_opset_version(), 16))
-    model = helper.make_model(graph, opset_imports=(opsetid,))
-    return model
+    return helper.make_model(graph, opset_imports=(opsetid,))
 
 
 if __name__ == "__main__":
