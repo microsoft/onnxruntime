1ecaa634010d1c48220a90fd4719c56032058c46
Gary Miguel <garymiguel@microsoft.com>
2022-06-22 17:25:48 +0000

    Fix get_scalar_value_from_tensor

--- onnxruntime/core/providers/cpu/signal/utils.h
+++ onnxruntime/core/providers/cpu/signal/utils.h
@@ -11,7 +11,19 @@ namespace signal {
 template <typename T>
 static T get_scalar_value_from_tensor(const Tensor* tensor) {
   ORT_ENFORCE(tensor->Shape().Size() == 1, "ratio input should have a single value.");
-  return *tensor->Data<T>();
+  const auto data_type = tensor->GetElementType();
+  switch (data_type) {
+    case ONNX_NAMESPACE::TensorProto_DataType_FLOAT:
+      return static_cast<T>(*tensor->Data<float>());
+    case ONNX_NAMESPACE::TensorProto_DataType_DOUBLE:
+      return static_cast<T>(*tensor->Data<double>());
+    case ONNX_NAMESPACE::TensorProto_DataType_INT32:
+      return static_cast<T>(*tensor->Data<int32_t>());
+    case ONNX_NAMESPACE::TensorProto_DataType_INT64:
+      return static_cast<T>(*tensor->Data<int64_t>());
+    default:
+      ORT_THROW("Unsupported input data type of ", data_type);
+  }
 }
 
 }  // namespace signal
