7dbab73afa2f8bf2c46c5d77e4ea0492f0a00784
Yi Zhang <zhanyi@microsoft.com>
2022-06-30 13:13:15 +0800

    add scripts

---
+++ tools/ci_build/github/apple/set_sccache.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+set -ex
+
+function write_sccache_stub() {
+  output=$1
+  binary=$(basename "${output}")
+
+  printf "#!/bin/sh\nif [ \$(ps auxc \$(ps auxc -o ppid \$\$ | grep \$\$ | rev | cut -d' ' -f1 | rev) | tr '\\\\n' ' ' | rev | cut -d' ' -f2 | rev) != sccache ]; then\n  exec sccache %s \"\$@\"\nelse\n  exec %s \"\$@\"\nfi" "$(which "${binary}")" "$(which "${binary}")" > "${output}"
+  chmod a+x "${output}"
+}
+
+if which sccache > /dev/null; then
+  # Create temp directory for sccache shims
+  tmp_dir=$(mktemp -d)
+  trap 'rm -rfv ${tmp_dir}' EXIT
+  write_sccache_stub "${tmp_dir}/clang++"
+  write_sccache_stub "${tmp_dir}/clang"
+
+  export PATH="${tmp_dir}:$PATH"
+fi
