975692f754fb52f5b0ec3d86486de7e9c4db8394
Yao Zhang <zhanyao@microsoft.com>
2022-07-02 23:55:26 -0700

    Check return error.

--- onnxruntime/contrib_ops/rocm/bert/timer.cc
+++ onnxruntime/contrib_ops/rocm/bert/timer.cc
@@ -3,35 +3,45 @@
 
 #include "contrib_ops/rocm/bert/timer.h"
 
+#define HIP_CHECKED_CALL(expr)                                                             \
+  do {                                                                                     \
+    auto status = expr;                                                                    \
+    if (status != hipSuccess) {                                                            \
+      std::printf("HIP Error at %s:%d\n    Error name  : %s\n    Error string: %s\n",      \
+                  __FILE__, __LINE__, hipGetErrorName(status), hipGetErrorString(status)); \
+      std::abort();                                                                        \
+    }                                                                                      \
+  } while (0)
+
 namespace onnxruntime {
 namespace contrib {
 namespace rocm {
 
 Timer::Timer() {
-  hipEventCreate(&start_);
-  hipEventCreate(&end_);
+  HIP_CHECKED_CALL(hipEventCreate(&start_));
+  HIP_CHECKED_CALL(hipEventCreate(&end_));
 }
 
 void Timer::Start() {
-  hipDeviceSynchronize();
-  hipEventRecord(start_, nullptr);
+  HIP_CHECKED_CALL(hipDeviceSynchronize());
+  HIP_CHECKED_CALL(hipEventRecord(start_, nullptr));
 }
 
 void Timer::End() {
-  hipEventRecord(end_, nullptr);
-  hipEventSynchronize(end_);
+  HIP_CHECKED_CALL(hipEventRecord(end_, nullptr));
+  HIP_CHECKED_CALL(hipEventSynchronize(end_));
 }
 
 float Timer::time() {
   float time;
   // time is in ms with a resolution of 1 us
-  hipEventElapsedTime(&time, start_, end_);
+  HIP_CHECKED_CALL(hipEventElapsedTime(&time, start_, end_));
   return time;
 }
 
 Timer::~Timer() {
-  hipEventDestroy(start_);
-  hipEventDestroy(end_);
+  HIP_CHECKED_CALL(hipEventDestroy(start_));
+  HIP_CHECKED_CALL(hipEventDestroy(end_));
 }
 
 }  // namespace rocm
