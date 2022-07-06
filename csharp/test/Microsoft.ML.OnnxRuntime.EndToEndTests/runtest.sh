8cd5ffbe468e089b1b4c2413e7e39cf4971f6567
Wei-Sheng Chin <wschin@outlook.com>
2022-07-06 14:19:47 -0700

    Fix path

--- csharp/test/Microsoft.ML.OnnxRuntime.EndToEndTests/runtest.sh
+++ csharp/test/Microsoft.ML.OnnxRuntime.EndToEndTests/runtest.sh
@@ -18,11 +18,15 @@ echo "Current NuGet package version is $CurrentOnnxRuntimeVersion"
 
 if [ $RunTestCsharp = "true" ]; then
   if [[ $IsMacOS == "True" || $IsMacOS == "true" ]]; then
+    # TODO(#12040): The test should figure out the opset version from the model file. Remove it from the path.
+    ONNX_DIR="${BUILD_SOURCESDIRECTORY}/cmake/external/onnx"
+    ONNX_VERSION_NUMBER=$(cat "${ONNX_DIR}/VERSION_NUMBER" | sed -E 's/([0-9]+\.[0-9]+\.[0-9]+).*/\1/')
+    OPSET_VERSION=$(grep "${ONNX_VERSION_NUMBER}" "${ONNX_DIR}/docs/Versioning.md" | sed -E "s/${ONNX_VERSION_NUMBER}\|[^|]+\|([0-9]+)\|.*/\1/")
     mkdir -p "${BUILD_BINARIESDIRECTORY}/models"
-    ln -s "${ONNX_DIR}/onnx/backend/test/data/node" "${BUILD_BINARIESDIRECTORY}/models/onnx_node_tests"
+    ln -s "${ONNX_DIR}/onnx/backend/test/data/node" "${BUILD_BINARIESDIRECTORY}/models/opset${OPSET_VERSION}"
+    export ORT_CSHARP_TEST_ONNX_MODEL_ROOT_PATH="${BUILD_BINARIESDIRECTORY}/models"
   fi
   # Run C# tests
-  export ORT_CSHARP_TEST_ONNX_MODEL_ROOT_PATH="${BUILD_BINARIESDIRECTORY}/models"
   dotnet restore $BUILD_SOURCESDIRECTORY/csharp/test/Microsoft.ML.OnnxRuntime.EndToEndTests/Microsoft.ML.OnnxRuntime.EndToEndTests.csproj -s $LocalNuGetRepo -s https://api.nuget.org/v3/index.json
   if [ $? -ne 0 ]; then
     echo "Failed to restore nuget packages for the test project"
