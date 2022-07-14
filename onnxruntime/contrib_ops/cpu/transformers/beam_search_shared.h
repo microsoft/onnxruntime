30c23e634d9841395bdc50163181bc71acee99bc
wangye <wangye@microsoft.com>
2022-07-14 00:18:24 +0000

    move bufferallocater to generate_impl_base

--- onnxruntime/contrib_ops/cpu/transformers/beam_search_shared.h
+++ onnxruntime/contrib_ops/cpu/transformers/beam_search_shared.h
@@ -145,26 +145,6 @@ class IConsoleDumper {
   bool is_enabled_;
 };
 
-template <typename T>
-gsl::span<T> AllocateBuffer(AllocatorPtr allocator,
-                            BufferUniquePtr& buffer,
-                            size_t elements,
-                            bool fill = false,
-                            T fill_value = T{}) {
-  size_t bytes = SafeInt<size_t>(sizeof(T)) * elements;
-  void* data = allocator->Alloc(bytes);
-  BufferUniquePtr temp_buffer(data, BufferDeleter(allocator));
-  buffer = std::move(temp_buffer);
-  T* first = reinterpret_cast<T*>(buffer.get());
-  auto span = gsl::make_span(first, elements);
-
-  if (fill) {
-    std::fill_n(first, elements, fill_value);
-  }
-
-  return span;
-}
-
 }  // namespace transformers
 }  // namespace contrib
 }  // namespace onnxruntime
