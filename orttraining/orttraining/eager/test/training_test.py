43530eff531a6451c5cb80bfd26cda0393962814
Wil Brady <25513670+WilBrady@users.noreply.github.com>
2022-07-14 13:30:46 -0400

    Small cleanup.

--- orttraining/orttraining/eager/test/training_test.py
+++ orttraining/orttraining/eager/test/training_test.py
@@ -2,18 +2,21 @@
 # Licensed under the MIT License.
 
 # pylint: disable=missing-docstring
+# pylint: disable=C0103
 
-import unittest
+# The following is a simple neural network trained and tested using FashinMINST data.
+# It is using eager mode targeting the ort device. After building eager mode run
+# PYTHONPATH=~/{repo root}/build/Linux/Debug python ~/{repo root}/orttraining/orttraining/eager/test/training_test.py
 
 import onnxruntime_pybind11_state as torch_ort
 import torch
 from torch import nn
 from torch.utils.data import DataLoader
 from torchvision import datasets
-from torchvision.transforms import Lambda, ToTensor
+from torchvision.transforms import ToTensor
 
+# we copy traing data to build folder as it is gitignored
 training_data = datasets.FashionMNIST(root="build/data", train=True, download=True, transform=ToTensor())
-
 test_data = datasets.FashionMNIST(root="build/data", train=False, download=True, transform=ToTensor())
 
 train_dataloader = DataLoader(training_data, batch_size=64)
@@ -34,17 +37,10 @@ class NeuralNetwork(nn.Module):
             nn.Linear(512, 10),
         )
 
-    def forward(self, x):
-        x = self.flatten(x)
-        logits = self.linear_relu_stack(x)
+    def forward(self, sample):
+        sample = self.flatten(sample)
+        logits = self.linear_relu_stack(sample)
         return logits
-
-
-model = NeuralNetwork().to(device)
-
-learning_rate = 1e-3
-batch_size = 64
-epochs = 5
 
 
 def train_loop(dataloader, model, loss_fn, optimizer):
@@ -84,10 +80,14 @@ def test_loop(dataloader, model, loss_fn):
     print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
 
 
+model = NeuralNetwork().to(device)
+learning_rate = 1e-3
+
 loss_fn = nn.CrossEntropyLoss().to(device)
 optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
 
-epochs = 10
+batch_size = 64
+epochs = 1
 for t in range(epochs):
     print(f"Epoch {t+1}\n-------------------------------")
     train_loop(train_dataloader, model, loss_fn, optimizer)
