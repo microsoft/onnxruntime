45f8856f76eb5b88f970b4572d62b21ec2e612d7
Guangyun Han <guangyunhan@microsoft.com>
2022-07-17 23:43:48 -0700

    Profile for bert param size only

--- onnxruntime/python/tools/kernel_explorer/kernels/gemm_test.py
+++ onnxruntime/python/tools/kernel_explorer/kernels/gemm_test.py
@@ -74,32 +74,51 @@ def test_gemm_all_cases(dtype, size, transab):
         _test_gemm(f, dtype, *size, *transab)
 
 
-def profile_gemm_func(size, dtype, func):
-    m, k, n = size
+def profile_gemm_func(func, dtype, m, n, k):
+    a_shape = (m, k)
+    b_shape = (k, n)
 
     np.random.seed(0)
-    a = np.random.rand(m, k).astype(dtype)
-    b = np.random.rand(k, n).astype(dtype)
-    c = np.zeros((m, n)).astype(dtype)
+    a = (np.random.rand(*a_shape) * 2 - 1).astype(dtype)
+    b = (np.random.rand(*b_shape) * 2 - 1).astype(dtype)
+    my_c = np.zeros((m, n), dtype=dtype)
 
     dev_a = ke.DeviceArray(a)
     dev_b = ke.DeviceArray(b)
-    dev_c = ke.DeviceArray(c)
+    dev_c = ke.DeviceArray(my_c)
 
     f = getattr(ke, func)
-    va = f(ke.blas_op.N, ke.blas_op.N, n, m, k, 1.0, dev_b, n, dev_a, k, 0.0, dev_c, n)
-    t = va.Profile()
+    opa = ke.blas_op.N
+    opb = ke.blas_op.N
+    lda = a_shape[1]
+    ldb = b_shape[1]
+    alpha = 1.0
+    beta = 0.0
+    my_gemm = f(opb, opa, n, m, k, alpha, dev_b, ldb, dev_a, lda, beta, dev_c, n)
+    time_ms = my_gemm.Profile()
 
-    print(dtype, size, f, f"{t*1000:.2f} us")  # , f"{size*3*(dtype_to_bytes(dtype))*1e3/t/1e9:.2f} GB/s")
+    time_us = time_ms * 1000
+    tflops = (m * k * n * 2) / (time_ms * 1e-3) / 1e12
+
+    print(f"RocBLAS GEMM {dtype} m={m:<4} k={k:<4} n={n:<4}, {time_us:>8.4f} us, {tflops:>5.2f} tflops")
 
 
 def profile():
-    sizes = product(list(range(64, 512 + 1, 64)), repeat=3)
-    dtypes = ["float16", "float32"]
-    for dt in dtypes:
-        for f in dtype_to_funcs(dt):
-            for s in sizes:
-                profile_gemm_func(s, dt, f)
+    dtypes = ["float32", "float16"]
+    bert_sizes = [
+        # m, k, n
+        (384, 384, 64),
+        (384, 768, 768),
+        (384, 768, 3072),
+        (384, 1024, 1024),
+        (384, 1024, 4096),
+        (384, 3072, 768),
+        (384, 4096, 1024),
+    ]
+    for dtype in dtypes:
+        for f in dtype_to_funcs(dtype):
+            for m, k, n in bert_sizes:
+                profile_gemm_func(f, dtype, m, k, n)
             print()
         print()
 
