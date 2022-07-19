a5b31f5e44fd3b5fe39cec263bfa0146b5c9ee0e
Guangyun Han <guangyunhan@microsoft.com>
2022-07-19 04:51:11 -0700

    Fix cpplint caused by includes

--- onnxruntime/python/tools/kernel_explorer/kernels/gemm.cc
+++ onnxruntime/python/tools/kernel_explorer/kernels/gemm.cc
@@ -1,15 +1,15 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-#include <type_traits>
+#include "python/tools/kernel_explorer/kernels/gemm.h"
 
+#include <type_traits>
 #include <pybind11/pybind11.h>
 
 #include "core/providers/rocm/rocm_common.h"
-#include "device_array.h"
-#include "operator.h"
-
 #include "core/providers/rocm/shared_inc/fpgeneric.h"
+#include "python/tools/kernel_explorer/device_array.h"
+#include "python/tools/kernel_explorer/operator.h"
 
 namespace py = pybind11;
 
