8f9eca8a374bfee22b9da5305eec17e1f2630970
Jameson Miller <jamill@microsoft.com>
2022-07-21 17:59:00 +0000

    Add ReformatSource.ps1 script for eager mode source
    For eager mode portion of the source tree we would like to run
    clang-format using the style rules defined in the projects main
    .clang-format file, but would also like to take into account the 120
    column limit on lines. Ideally we would like to add a .clang-format file
    based on the projects main .clang-format file, but this is not supported
    in the version of clang (clang-format version 10.0.0-4ubuntu1) included
    in base dev environment (ubuntu 20.04). The ability to have a
    .clang-format file inherit from another .clang-format file is available
    in later version of clang-format, so when that functionality is
    supported in our base dev environment, we can incorporate it then.
    
    In the meantime, we add another ReformatSource.ps1 script that uses the
    base .clang-format file, but adds the ColumnLimit style to 120.
    
    In addition, we add functionality to ensure trailing newline at the end
    of files (this does not appear to be directly supported in
    clang-format).

---
+++ orttraining/orttraining/eager/ReformatSource.ps1
@@ -0,0 +1,43 @@
+<#
+.SYNOPSIS
+Formats .h and .cpp files in current directory and below.
+
+.DESCRIPTION
+Formats .h and .cpp files in current directory and below. It will ensure trailing newline at EOF, and run
+clang-format in-place.
+
+This is very similar to onnxruntime/ReformatSource.ps1, but we want to tweak the rules on files in the eager
+directory to include the column limit in the style. In the version of clang-format included with ubuntu 20.04,
+clang-format does not support inheriting rules from another file, so we add the extra options to the clang-format
+command.
+
+Later versions of clang-format do support inheriting style from another .clang-format file. When the baseline
+environment includes a version of clang-format that supports this functionality, we can refactor this.
+#>
+
+
+function EnsureTrailingEmptyLine {
+
+    <#
+    .SYNOPSIS
+    Ensures trailing newline for file.
+
+    .DESCRIPTION
+    If file is missing trailing newline, will append newline to end of file
+    #>
+    param (
+        $Path
+    )
+
+    $content = [System.IO.File]::ReadAllText($Path)
+
+    if ($content[$content.Length - 1] -ne [Environment]::NewLine) {
+        [System.IO.File]::AppendAllText($Path, [Environment]::NewLine)
+    }
+}
+
+gci -Recurse -Include  *.h, *.cpp | foreach {
+    Write-Host "Updating " $_.FullName
+    EnsureTrailingEmptyLine $_
+    clang-format -i --style="{ColumnLimit: 120}" $_
+}
