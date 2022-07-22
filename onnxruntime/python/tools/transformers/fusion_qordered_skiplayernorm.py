4b47a9f61656f10680f3dac8df134e01834bc713
Hari Seshadri <hasesh@microsoft.com>
2022-07-22 13:27:40 -0700

    More changes

--- onnxruntime/python/tools/transformers/fusion_qordered_skiplayernorm.py
+++ onnxruntime/python/tools/transformers/fusion_qordered_skiplayernorm.py
@@ -34,11 +34,19 @@ class FusionQOrderedSkipLayerNormalization(Fusion):
         if len(children) != 1 or children[0].op_type != "QuantizeLinear":
             return
 
-        quantize_node = input_name_to_nodes[node.output[0]][0]
-        if quantize_node.op_type != "QuantizeLinear":
+        downstream_quantize_node = input_name_to_nodes[node.output[0]][0]
+        if downstream_quantize_node.op_type != "QuantizeLinear":
             return
 
-        # The first and second inputs should flow through DequantizeLinears
+        y_scale = self.model.get_constant_value(downstream_quantize_node.input[1])
+        if y_scale is None:
+            return
+
+        y_zero_point = self.model.get_constant_value(downstream_quantize_node.input[2])
+        if y_zero_point is None or y_zero_point != 0:
+            return
+
+        # The first and second inputs to SkipLayerNormalization should flow through DequantizeLinear nodes
         first_path_id, first_input_parent_nodes, _ = self.model.match_parent_paths(
             node,
             [
@@ -51,6 +59,14 @@ class FusionQOrderedSkipLayerNormalization(Fusion):
             return
 
         dequantize_node_0 = first_input_parent_nodes[0]
+
+        x_scale_0 = self.model.get_constant_value(dequantize_node_0.input[1])
+        if x_scale_0 is None:
+            return
+
+        x_zero_point_0 = self.model.get_constant_value(dequantize_node_0.input[2])
+        if x_zero_point_0 is None or x_zero_point_0 != 0:
+            return
 
         second_path_id, second_input_parent_nodes, _ = self.model.match_parent_paths(
             node,
@@ -65,14 +81,20 @@ class FusionQOrderedSkipLayerNormalization(Fusion):
 
         dequantize_node_1 = second_input_parent_nodes[0]
 
-        # TODO Make sure zero point(s) are scalar 0s for Q/DQ nodes
+        x_scale_1 = self.model.get_constant_value(dequantize_node_1.input[1])
+        if x_scale_1 is None:
+            return
+
+        x_zero_point_1 = self.model.get_constant_value(dequantize_node_1.input[2])
+        if x_zero_point_1 is None or x_zero_point_1 != 0:
+            return
 
         subgraph_nodes = [node]  #SkipLayerNormalization
-        subgraph_nodes.extend([quantize_node, dequantize_node_0, dequantize_node_1])  #Relevant Q, DQ nodes
+        subgraph_nodes.extend([downstream_quantize_node, dequantize_node_0, dequantize_node_1])  #Relevant Q, DQ nodes
 
         if not self.model.is_safe_to_fuse_nodes(
             subgraph_nodes,
-            quantize_node.output,
+            downstream_quantize_node.output,
             input_name_to_nodes,
             output_name_to_node,
         ):
@@ -85,12 +107,11 @@ class FusionQOrderedSkipLayerNormalization(Fusion):
             "QOrderedSkipLayerNormalization",
             inputs=[dequantize_node_0.input[0], dequantize_node_0.input[1], 
                     dequantize_node_1.input[0], dequantize_node_1.input[1], 
-                    node.input[2], node.input[3], quantize_node.input[1]],
-            outputs=[quantize_node.output[0]],
+                    node.input[2], node.input[3], downstream_quantize_node.input[1]],
+            outputs=[downstream_quantize_node.output[0]],
             name=self.model.create_node_name("QOrderedSkipLayerNormalization", name_prefix="QOrderedSkipLayerNormalization"),
         )
 
-        normalize_node.attribute.extend([helper.make_attribute("epsilon", float(add_weight))])
         #TODO: More attributes
         self.nodes_to_add.append(normalize_node)
         self.node_name_to_graph_name[normalize_node.name] = self.this_graph_name
