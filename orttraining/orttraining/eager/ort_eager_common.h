1293c6263459099eb0c6c7ad75a59c3e9ecb4f41
Jameson Miller <jamill@microsoft.com>
2022-07-21 21:47:13 -0400

    Update ort_eager_common.h
    Attempt to disable linting on line

--- orttraining/orttraining/eager/ort_eager_common.h
+++ orttraining/orttraining/eager/ort_eager_common.h
@@ -1,8 +1,8 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-// NOLINT
+
 #pragma once
 // include the pybind header first, it will disable linking to pythonX_d.lib on Windows in debug mode
-#include "python/onnxruntime_pybind_state_common.h"
+#include "python/onnxruntime_pybind_state_common.h" // NOLINT
 #include <ATen/Operators.h>
 #include <torch/extension.h>
