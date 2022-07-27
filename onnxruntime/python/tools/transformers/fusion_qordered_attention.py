97b62ac1b6d2ddf80793198f53b57a7bf65cdea9
hariharans29 <shariharan91@gmail.com>
2022-07-26 17:59:10 -0700

    Add initial commit for QOrderedAttention

---
+++ onnxruntime/python/tools/transformers/fusion_qordered_attention.py
@@ -0,0 +1,85 @@
+#-------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation.  All rights reserved.
+# Licensed under the MIT License.
+#--------------------------------------------------------------------------
+
+from logging import getLogger
+from typing import Dict
+
+from numpy import transpose
+
+from fusion_base import Fusion
+from onnx import helper
+from onnx_model import OnnxModel
+
+logger = getLogger(__name__)
+
+
+class FusionQOrderedAttention(Fusion):
+    def __init__(self, model: OnnxModel):
+        super().__init__(model, "QOrderedAttention", "QOrderedSkipLayerNormalization")
+
+    def fuse(self, node, input_name_to_nodes: Dict, output_name_to_node: Dict):
+        gelu_children = self.model.get_children(node, input_name_to_nodes)
+
+        # Should only have 1 child - QuantizeLinear
+        if len(gelu_children) != 1 or gelu_children[0].op_type != "QuantizeLinear":
+            return
+
+        downstream_quantize_node = gelu_children[0]
+ 
+        y_scale = self.model.get_constant_value(downstream_quantize_node.input[1])
+        if y_scale is None:
+            return
+
+        y_zero_point = self.model.get_constant_value(downstream_quantize_node.input[2])
+        if y_zero_point is None or y_zero_point != 0:
+            return
+
+        # The first and second inputs to SkipLayerNormalization should flow through DequantizeLinear nodes
+        first_path_id, first_input_parent_nodes, _ = self.model.match_parent_paths(
+            node,
+            [
+                (["DequantizeLinear"], [0])
+            ],
+            output_name_to_node,
+        )
+
+        if first_path_id < 0:
+            return
+
+        dequantize_node_0 = first_input_parent_nodes[0]
+
+        x_scale_0 = self.model.get_constant_value(dequantize_node_0.input[1])
+        if x_scale_0 is None:
+            return
+
+        x_zero_point_0 = self.model.get_constant_value(dequantize_node_0.input[2])
+        if x_zero_point_0 is None or x_zero_point_0 != 0:
+            return
+            
+        subgraph_nodes = [node]  #Gelu
+        subgraph_nodes.extend([downstream_quantize_node, dequantize_node_0])  #Relevant Q, DQ nodes
+
+        if not self.model.is_safe_to_fuse_nodes(
+            subgraph_nodes,
+            downstream_quantize_node.output,
+            input_name_to_nodes,
+            output_name_to_node,
+        ):
+            logger.debug(f"It is not safe to fuse QOrderedGelu node. Skip")
+            return
+
+        self.nodes_to_remove.extend(subgraph_nodes)
+
+        ordered_gelu_node = helper.make_node(
+            "QOrderedGelu",
+            inputs=[dequantize_node_0.input[0], dequantize_node_0.input[1],
+                    downstream_quantize_node.input[1]],
+            outputs=[downstream_quantize_node.output[0]],
+            name=self.model.create_node_name("QOrderedGelu", name_prefix="QOrderedGelu"),
+        )
+
+        # TODO 3: More attributes
+        self.nodes_to_add.append(ordered_gelu_node)
+        self.node_name_to_graph_name[ordered_gelu_node.name] = self.this_graph_name
