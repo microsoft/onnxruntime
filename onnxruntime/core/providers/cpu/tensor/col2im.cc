5b70c4a9379c6b79622cbe97678305c10dcc50f5
Thiago Crepaldi <thiago.crepaldi@microsoft.com>
2022-07-27 17:48:49 -0400

    Kernel impl

--- onnxruntime/core/providers/cpu/tensor/col2im.cc
+++ onnxruntime/core/providers/cpu/tensor/col2im.cc
@@ -7,6 +7,7 @@
 #include "core/framework/TensorSeq.h"
 #include "core/providers/common.h"
 #include "core/framework/copy.h"
+#include "core/common/safeint.h"
 #include "core/providers/op_kernel_type_control.h"
 
 namespace onnxruntime {
@@ -23,7 +24,59 @@ REGISTER_KERNEL_TYPED(float)
 
 template <typename T>
 Status Col2Im<T>::Compute(OpKernelContext* context) const {
-  (void) context;
+  const auto* col_input = context->Input<Tensor>(0);
+  const auto* image_shape = context->Input<Tensor>(1);
+  const auto* kernel_shape = context->Input<Tensor>(2);
+
+  TensorShape col_shape = col_input->Shape();
+  const auto num_image_channels = image_shape->Shape()[1];
+  const auto batch_size = col_shape[0];
+
+  const int64_t image_size = image_shape->Shape().Size();
+
+  AllocatorPtr alloc;
+  ORT_RETURN_IF_ERROR(context->GetTempSpaceAllocator(&alloc));
+  const int64_t col_buffer_size = col_input->Shape().Size();
+  auto col_data = alloc->Alloc(SafeInt<size_t>(sizeof(T)) * col_buffer_size);
+
+  BufferUniquePtr col_buffer(col_data, BufferDeleter(std::move(alloc)));
+  T* col_buffer_data = static_cast<T*>(col_buffer.get());
+
+  TensorShapeVector Y_dims;
+  Y_dims.insert(Y_dims.begin(), {batch_size, num_image_channels});
+  TensorShape Yshape(Y_dims);
+  Tensor* Y = context->Output(0, Yshape);
+  T* Ydata = Y->template MutableData<T>();
+
+  // template <typename T, class Provider, int order>
+  // void Col2imNd(
+  //     const T* data_col,
+  //     const int64_t* img_shape,
+  //     const int64_t* output_shape,
+  //     int64_t channels_col,
+  //     int64_t img_size,
+  //     const int64_t* kernel_shape,
+  //     const int64_t* stride,
+  //     const int64_t* dilation,
+  //     const int64_t* pad,
+  //     ptrdiff_t N,
+  //     T* data_img,
+  //     Provider* provider);
+
+  math::Col2imNd<T, CPUMathUtil, StorageOrder::NCHW>(
+    col_buffer_data,
+    image_shape->Shape().GetDims().data(),
+    col_shape.GetDims().data(),
+    num_image_channels,
+    image_size,
+    kernel_shape->Shape().GetDims().data(),
+    col2im_attrs_.strides.data(),
+    col2im_attrs_.dilations.data(),
+    col2im_attrs_.pads.data(),
+    static_cast<int>(kernel_shape->Shape().Size()),
+    Ydata,
+    &CPUMathUtil::Instance());
+
   return Status::OK();
 }
 
---
+++ onnxruntime/core/providers/cpu/tensor/col2im_attributes.h
@@ -0,0 +1,55 @@
+/**
+* Copyright (c) 2016-present, Facebook, Inc.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+/* Modifications Copyright (c) Microsoft. */
+
+#pragma once
+
+#ifndef SHARED_PROVIDER
+#include "core/common/common.h"
+#include "core/providers/common.h"
+#include "core/util/math.h"
+#endif
+
+#include "core/common/inlined_containers.h"
+#include "core/framework/op_kernel.h"
+#include "core/framework/op_node_proto_helper.h"
+
+namespace onnxruntime {
+
+struct Col2ImAttributes {
+  using Col2ImPadVector = InlinedVector<int64_t, kTensorShapeSmallBufferElementsSize * 2>;
+
+  explicit Col2ImAttributes(const OpKernelInfo& info) {
+    auto status = info.GetAttrs("strides", strides);
+    ORT_ENFORCE(status.IsOK());
+
+    gsl::span<const int64_t> pads_span;
+    status = info.GetAttrsAsSpan("pads", pads_span);
+    ORT_ENFORCE(status.IsOK());
+    pads.assign(pads_span.cbegin(), pads_span.cend());
+
+    status = info.GetAttrs("dilations", dilations);
+    ORT_ENFORCE(status.IsOK());
+  }
+
+  ~Col2ImAttributes() = default;
+
+  Col2ImPadVector pads;
+  TensorShapeVector dilations;
+  TensorShapeVector strides;
+};
+
+}  // namespace onnxruntime
