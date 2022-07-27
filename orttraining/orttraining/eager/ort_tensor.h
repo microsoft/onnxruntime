c0b7215f3230a8a356cc94b6d3478eaa6422f908
Lincoln Waller <Lincoln.Waller@microsoft.com>
2022-07-27 11:17:02 -0400

    fix ifdef

--- orttraining/orttraining/eager/ort_tensor.h
+++ orttraining/orttraining/eager/ort_tensor.h
@@ -3,12 +3,12 @@
 
 #pragma once
 
-#ifdef _WIN32
+#ifdef WIN32
 #pragma warning( push )
 #pragma warning( disable: 4805 )
 #endif
 #include <c10/core/TensorImpl.h>
-#ifdef _WIN32
+#ifdef WIN32
 #pragma warning ( pop )
 #endif
 #include <core/framework/ort_value.h>
