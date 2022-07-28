cd047dadbad280461642c9b223ac3daf29a6b6b4
shalvamist <94086448+shalvamist@users.noreply.github.com>
2022-07-28 14:06:06 -0700

    Update buildAll.py
    Addressed several issues with the PR - Here are the highlights of this change - 
    1. Now pylint test should pass
    2. Resolved syntax issues " vs. '
    3. Changed '-b' option to 'config' to comply with build.bat options - added RelWithDebInfo build option
    4. Added support on UNIX (wasn't tested yet) - still need to change path generation to comply with Win/UNIX/Linux
    5. Replaced system calls with thread calls (subprocess API)

--- js/buildAll.py
+++ js/buildAll.py
@@ -3,22 +3,21 @@ import os
 import shutil
 import subprocess
 import sys
-from os.path import abspath, dirname, join
+from os.path import abspath, dirname
 
-build_dir = ["wasm_SIMD", "wasm_threaded", "wasm_SIMD_threaded"]
-build_flags = [" --enable_wasm_simd", " --enable_wasm_threads", " --enable_wasm_simd --enable_wasm_threads "]
+build_script = "build.sh"
+if 'win' in sys.platform:
+    build_script = "build.bat"
 
-firsttime_flags = '--build_wasm --skip_tests --emsdk_version releases-upstream-2ddc66235392b37e5b33477fd86cbe01a14b8aa2-64bit --cmake_generator "Visual Studio 16 2019"'
-default_flags = ' --build_wasm --skip_tests --skip_submodule_sync --emsdk_version releases-upstream-2ddc66235392b37e5b33477fd86cbe01a14b8aa2-64bit --cmake_generator "Visual Studio 16 2019"'
+build_dir = ["wasm","wasm_SIMD", "wasm_threaded", "wasm_SIMD_threaded"]
+build_flags = ["", " --enable_wasm_simd", " --enable_wasm_threads", " --enable_wasm_simd --enable_wasm_threads "]
+wasm_file_names = ["ort-wasm.wasm","ort-wasm-simd.wasm","ort-wasm-threaded.wasm","ort-wasm-simd-threaded.wasm"]
+js_file_names = ["ort-wasm.js","ort-wasm-threaded.worker.js","ort-wasm-threaded.js"]
+
+default_flags = " --build_wasm --skip_tests --skip_submodule_sync --parallel "
 BinariesDirectory = ".\\build\\"
 Dist_Path = ".\\js\\web\\dist\\"
 Binding_Path = ".\\js\\web\\lib\\wasm\\binding\\"
-
-# Needed Arguments
-# -b - build WASM
-#      - Need to add partial build for only WASM artifacts - reduces the amount of time to build ~100x
-#      - Need to add full build for the entire WASM + Web App
-#      - configuration - Release/Debug
 
 ### generating the build folders
 if not os.path.isdir(BinariesDirectory):
@@ -37,22 +36,18 @@ if not os.path.isdir(Binding_Path):
 
 #### running the WASM build commands
 parser = argparse.ArgumentParser()
-parser.add_argument("-b", "--build", choices=["Release", "Debug"], help="build WASM - {Release,Debug}")
-parser.add_argument("-c", "--copy", choices=["Release", "Debug"], help="copy WASM artifacts - {Release,Debug}")
+parser.add_argument("--config", choices=["Release", "Debug","RelWithDebInfo"], help="build WASM artifactsfor the configuration - {Release,Debug,RelWithDebInfo}")
+parser.add_argument("--copy", choices=["Release", "Debug","RelWithDebInfo"], help="copy WASM artifacts to destination folders- {Release,Debug,RelWithDebInfo}")
 args = parser.parse_args()
 configuration = "none"
 
-if args.build:
-    configuration = args.build
-    command = (
-        "build.bat " + firsttime_flags + " --config " + configuration + " --build_dir " + BinariesDirectory + "\\wasm"
-    )
-    # print(command)
-    os.system(command)
+if args.config:
+    configuration = args.config
 
     for i, path in enumerate(build_dir):
         command = (
-            "build.bat --config "
+            build_script
+            + " --config "
             + configuration
             + default_flags
             + " --build_dir "
@@ -60,30 +55,25 @@ if args.build:
             + path
             + build_flags[i]
         )
-        # print(command)
-        os.system(command)
+        p = subprocess.Popen(command,shell=True)
+        p.wait()
 
 # copy the files to the right location
 if args.copy:
     configuration = "\\" + args.copy + "\\"
     ## Copying WASM artifacts
-    file_name = "ort-wasm.wasm"
-    shutil.copyfile(BinariesDirectory + "\\wasm\\" + configuration + file_name, Dist_Path + file_name)
-    file_name = "ort-wasm-simd.wasm"
-    shutil.copyfile(BinariesDirectory + build_dir[0] + configuration + file_name, Dist_Path + file_name)
-    file_name = "ort-wasm-threaded.wasm"
-    shutil.copyfile(BinariesDirectory + build_dir[1] + configuration + file_name, Dist_Path + file_name)
-    file_name = "ort-wasm-simd-threaded.wasm"
-    shutil.copyfile(BinariesDirectory + build_dir[2] + configuration + file_name, Dist_Path + file_name)
+    for i,file_name in enumerate(wasm_file_names):
+        shutil.copyfile(BinariesDirectory + build_dir[i] + configuration + file_name, Dist_Path + file_name)
 
     ## Copying JS binding files
     file_name = "ort-wasm.js"
     shutil.copyfile(BinariesDirectory + "\\wasm\\" + configuration + file_name, Binding_Path + file_name)
     file_name = "ort-wasm-threaded.worker.js"
-    shutil.copyfile(BinariesDirectory + build_dir[1] + configuration + file_name, Binding_Path + file_name)
+    shutil.copyfile(BinariesDirectory + build_dir[2] + configuration + file_name, Binding_Path + file_name)
     file_name = "ort-wasm-threaded.js"
-    shutil.copyfile(BinariesDirectory + build_dir[1] + configuration + file_name, Binding_Path + file_name)
+    shutil.copyfile(BinariesDirectory + build_dir[2] + configuration + file_name, Binding_Path + file_name)
 ####
+
 # build NPM
 path = abspath(dirname(__file__))
 p = subprocess.Popen("npm ci", shell=True, cwd=path)
