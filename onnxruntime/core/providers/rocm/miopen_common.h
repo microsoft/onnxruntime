eea8dbe9f99b682e26b62df2a548478f4d1f6274
Xinya Zhang <Xinya.Zhang@amd.com>
2022-08-03 16:36:36 -0500

    [ROCm] Add AveragePool, GlobalAveragePool, MaxPool, GlobalMaxPool Ops (#11968)
    * [ROCm] disable expected failure tests PoolTest.MaxPool_10_DilationPadding_?d
    
    * [ROCm] Add AveragePool, GlobalAveragePool, MaxPool, GlobalMaxPool Ops
    
    * (To squash after review) Replace rocm/nn/pool.cc with amd_hipify.py changes
    
    * [ROCM] Replace miCompat with Helper functions
    
    * (to squash) fix the compiling error of SetPoolingNdDescriptorHelper

--- onnxruntime/core/providers/rocm/miopen_common.h
+++ onnxruntime/core/providers/rocm/miopen_common.h
@@ -196,5 +196,28 @@ SetLRNDescriptorHelper(miopenLRNDescriptor_t normDesc,
   return miopenSetLRNDescriptor(normDesc, miopenLRNCrossChannel, lrnN, lrnAlpha, lrnBeta, lrnK);
 }
 
+inline miopenStatus_t
+PoolingForwardHelper(miopenHandle_t handle,
+                     const miopenPoolingDescriptor_t poolDesc,
+                     const void* alpha,
+                     const miopenTensorDescriptor_t xDesc,
+                     const void* x,
+                     const void* beta,
+                     const miopenTensorDescriptor_t yDesc,
+                     void* y) {
+  return miopenPoolingForward(handle, poolDesc, alpha, xDesc, x, beta, yDesc, y, false, nullptr, 0);
+}
+
+inline miopenStatus_t
+SetPoolingNdDescriptorHelper(miopenPoolingDescriptor_t poolDesc,
+                             const miopenPoolingMode_t mode,
+                             miopenNanPropagation_t /* unavailable */,
+                             int nbDims,
+                             int* windowDimA,
+                             int* padA,
+                             int* stridesA) {
+  return miopenSetNdPoolingDescriptor(poolDesc, mode, nbDims, windowDimA, padA, stridesA);
+}
+
 }  // namespace rocm
 }  // namespace onnxruntime
