1481b0969c88a7688f53bf38bd733c813bc66137
Chen Fu <1316708+chenfucn@users.noreply.github.com>
2022-08-04 16:24:04 -0700

    python module for dumping activation tensors when running an onnx model

---
+++ onnxruntime/python/tools/quantization/save_activations.py
@@ -0,0 +1,152 @@
+# --------------------------------------------------------------------------
+# Copyright (c) Microsoft, Intel Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for
+# license information.
+# --------------------------------------------------------------------------
+
+"""Utilities to run a given ONNX model, while saving input/output tensors of
+eligible operator nodes.
+
+A use case is to debug quantization induced accuracy drop. An AI engineer can
+run the original float32 model and the quantized model with the same inputs,
+then compare the corresponding activations between the two models to find
+where the divergence is. 
+
+Example Usage:
+
+.. code-block:: python
+    class ExampleDataReader(CalibrationDataReader):
+        def __init__(self):
+            ...
+        def get_next(self):
+            ...
+
+    input_data_reader = ExampleDataReader()
+
+    aug_model = aug_model_for_act_saving(path_to_onnx_model)
+    augmented_model_path = str(Path(self._tmp_model_dir.name).joinpath("augmented_model.onnx"))
+    onnx.save(
+        aug_model,
+        augmented_model_path,
+        save_as_external_data=False,
+    )
+
+    tensor_dict = run_collect_activations(augmented_model_path, data_reader)
+    # tensor_dict points to a dictionary where the keys are tensor names and each value
+    # is a list of tensors, one from each model run
+
+"""
+
+import uuid
+
+from typing import Union,List
+from pathlib import Path
+
+import numpy
+import onnx
+import onnxruntime
+
+from onnx import ModelProto, TensorProto, helper, numpy_helper
+from .calibrate import CalibrationDataReader, CalibraterBase
+from .quant_utils import clone_model_with_shape_infer
+
+
+def aug_model_for_act_saving(
+    model:Union[str,Path,ModelProto],
+    op_types_for_saving=[]
+) -> ModelProto:
+    r"""Augment a given ONNX model by adding activation tensors as outputs
+
+    Parameters
+    ----------
+    model : ModelProto or Path
+        an ONNX model or the path to load the model
+    op_types_for_saving : array
+        Optional list of operator types for which the input/output should be
+        saved. By default, saving all the float32/float16 tensors.
+
+    Returns
+    -------
+    Augmented ONNX model
+    """
+
+    saver = CalibraterBase(model, op_types_to_calibrate=op_types_for_saving)
+    model = clone_model_with_shape_infer(saver.model)
+    tensors, _ = saver.select_tensors_to_calibrate(model)
+    reshape_shape_name = str(uuid.uuid4())
+    reshape_shape = numpy_helper.from_array(
+        numpy.array([-1], dtype=numpy.int64), reshape_shape_name)
+    model.graph.initializer.append(reshape_shape)
+
+    for tensor_name in tensors:
+        reshape_output = tensor_name + "_ReshapedSavedOutput"
+        reshape_node = onnx.helper.make_node(
+            "Reshape",
+            inputs=[tensor_name, reshape_shape_name],
+            outputs=[reshape_output],
+            name=reshape_output,
+        )
+        model.graph.node.extend([reshape_node])
+        vinfo = helper.make_tensor_value_info(
+            reshape_output, TensorProto.FLOAT, [1])
+        model.graph.output.append(vinfo)
+    return model
+
+
+def run_collect_activations(
+    augmented_model:str,
+    input_reader: CalibrationDataReader,
+    session_options=None,
+    execution_providers:List[str]=["CPUExecutionProvider"]
+) -> dict:
+    r"""Run augmented model and collect activations tensors
+
+    Parameters
+    ----------
+    augmented_model : str
+        Path to augmented model created by aug_model_for_act_saving()
+    input_reader : CalibrationDataReader
+        Logic for reading input for the model, augmented model have the same
+        input with the original model.
+    session_options : 
+        Optional OnnxRuntime session options for controlling model run.
+        By default graph optimization is turned off
+    execution_providers : List[str]
+        Optional execution providers for running the model. CPU EP is used
+        by default.
+
+    Returns
+    -------
+    A dictionary where the key is tensor name and values are list of tensors from each batch
+    """
+
+    if (session_options is None):
+        session_options = onnxruntime.SessionOptions()
+        session_options.graph_optimization_level = onnxruntime.GraphOptimizationLevel.ORT_DISABLE_ALL
+
+    infer_session = onnxruntime.InferenceSession(
+        augmented_model,
+        sess_options=session_options,
+        providers=execution_providers,
+    )
+ 
+    intermediate_outputs = []
+    while True:
+        inputs = input_reader.get_next()
+        if not inputs:
+            break
+        intermediate_outputs.append(infer_session.run(None, inputs))
+
+        if len(intermediate_outputs) == 0:
+            raise ValueError("No data is collected while running augmented model!")
+
+    output_dict={}
+    output_info = infer_session.get_outputs();
+    for batch in intermediate_outputs:
+        for output, output_data in zip(output_info, batch):
+            if output.name.endswith("_ReshapedSavedOutput") :
+                oname = output.name[0:len(output.name)-len("_ReshapedSavedOutput")]
+                output_dict.setdefault(oname, []).append(output_data)
+
+    return output_dict
+
---
+++ onnxruntime/test/python/quantization/test_save_activations.py
@@ -0,0 +1,164 @@
+#!/usr/bin/env python
+# coding: utf-8
+# -------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for
+# license information.
+# --------------------------------------------------------------------------
+
+"""Testing 
+"""
+
+import tempfile
+import unittest
+from pathlib import Path
+
+import numpy as np
+import onnx
+from onnx import TensorProto, helper, numpy_helper
+
+import onnxruntime
+from onnxruntime.quantization.calibrate import CalibrationDataReader
+from onnxruntime.quantization.save_activations import aug_model_for_act_saving, run_collect_activations
+
+
+def generate_input_initializer(tensor_shape, tensor_dtype, input_name):
+    """
+    Helper function to generate initializers for test inputs
+    """
+    tensor = np.random.normal(0, 0.3, tensor_shape).astype(tensor_dtype)
+    init = numpy_helper.from_array(tensor, input_name)
+    return init
+
+
+class TestDataReader(CalibrationDataReader):
+    """Random Data Input Generator
+    """
+
+    def __init__(self):
+        self.preprocess_flag = True
+        self.enum_data_dicts = []
+        self.count = 2
+        self.input_data_list = []
+        for _ in range(self.count):
+            self.input_data_list.append(np.random.normal(0, 0.33, [1, 3, 1, 3]).astype(np.float32))
+
+    def get_next(self):
+        if self.preprocess_flag:
+            self.preprocess_flag = False
+            input_name = "input"
+            self.enum_data_dicts = iter([{input_name: input_data} for input_data in self.input_data_list])
+        return next(self.enum_data_dicts, None)
+
+    def rewind(self):
+        self.preprocess_flag = True
+
+
+class TestSaveActivations(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls._tmp_model_dir = tempfile.TemporaryDirectory(prefix="test_save_activations.")
+
+    @classmethod
+    def tearDownClass(cls):
+        cls._tmp_model_dir.cleanup()
+
+    def construct_test_model1(self, test_model_path):
+        #    (input)
+        #       |
+        #      Relu
+        #      /  \
+        #    Conv  \
+        #     |     \
+        #    Relu  Conv
+        #     |     |
+        #   Conv    |
+        #     \     /
+        #       Add
+        #        |
+        #       (X6)
+        input = helper.make_tensor_value_info("input", TensorProto.FLOAT, [1, 3, 1, 3])
+        x1_output = helper.make_tensor_value_info("X1", TensorProto.FLOAT, [1, 3, 1, 3])
+        x2_output = helper.make_tensor_value_info("X2", TensorProto.FLOAT, [1, 3, 1, 3])
+        x3_output = helper.make_tensor_value_info("X3", TensorProto.FLOAT, [1, 3, 1, 3])
+        x4_output = helper.make_tensor_value_info("X4", TensorProto.FLOAT, [1, 3, 1, 3])
+        x5_output = helper.make_tensor_value_info("X5", TensorProto.FLOAT, [1, 3, 1, 3])
+        x6_output = helper.make_tensor_value_info("X6", TensorProto.FLOAT, [1, 3, 1, 3])
+        w1 = generate_input_initializer([3, 3, 1, 1], np.float32, "W1")
+        b1 = generate_input_initializer([3], np.float32, "B1")
+        w3 = generate_input_initializer([3, 3, 1, 1], np.float32, "W3")
+        b3 = generate_input_initializer([3], np.float32, "B3")
+        w5 = generate_input_initializer([3, 3, 1, 1], np.float32, "W5")
+        b5 = generate_input_initializer([3], np.float32, "B5")
+        relu_node_1 = helper.make_node("Relu", ["input"], ["X1"], name="Relu1")
+        conv_node_1 = helper.make_node("Conv", ["X1", "W1", "B1"], ["X2"], name="Conv1")
+        relu_node_2 = helper.make_node("Relu", ["X2"], ["X3"], name="Relu2")
+        conv_node_2 = helper.make_node("Conv", ["X3", "W3", "B3"], ["X4"], name="Conv2")
+        conv_node_3 = helper.make_node("Conv", ["X1", "W5", "B5"], ["X5"], name="Conv3")
+        add_node = helper.make_node("Add", ["X4", "X5"], ["X6"], name="Add")
+
+        # we are keeping all tensors in the output anyway for verification purpose
+        graph = helper.make_graph(
+            [relu_node_1, conv_node_1, relu_node_2, conv_node_2, conv_node_3, add_node],
+            "test_graph_4",
+            [input],
+            [x1_output, x2_output, x3_output, x4_output, x5_output, x6_output],
+        )
+        graph.initializer.add().CopyFrom(w1)
+        graph.initializer.add().CopyFrom(b1)
+        graph.initializer.add().CopyFrom(w3)
+        graph.initializer.add().CopyFrom(b3)
+        graph.initializer.add().CopyFrom(w5)
+        graph.initializer.add().CopyFrom(b5)
+        model = helper.make_model(graph, opset_imports=[helper.make_opsetid("", 13)])
+        onnx.save(model, test_model_path)
+
+    def test_saved_act(self):
+        test_model_path = str(Path(self._tmp_model_dir.name).joinpath("test_model_1.onnx"))
+        self.construct_test_model1(test_model_path)
+        data_reader = TestDataReader()
+
+        aug_model = aug_model_for_act_saving(test_model_path)
+        augmented_model_path = str(Path(self._tmp_model_dir.name).joinpath("augmented_test_model_1.onnx"))
+
+        onnx.save(
+            aug_model,
+            augmented_model_path,
+            save_as_external_data=False,
+        )
+
+        tensor_dict = run_collect_activations(augmented_model_path, data_reader)
+
+        # run original model and compare the tensors
+        sess_options = onnxruntime.SessionOptions()
+        sess_options.graph_optimization_level = onnxruntime.GraphOptimizationLevel.ORT_DISABLE_ALL
+        infer_session = onnxruntime.InferenceSession(
+            test_model_path.as_posix(),
+            sess_options=sess_options,
+            providers=["CPUExecutionProvider"],
+        )
+
+        data_reader.rewind()
+        oracle_outputs = []
+        while True:
+            input = data_reader.get_next()
+            if not input:
+                break
+            oracle_outputs.append(infer_session.run(None, input))
+
+        output_dict={}
+        output_info = infer_session.get_outputs();
+        for batch in oracle_outputs:
+            for output, output_data in zip(output_info, batch):
+                output_dict.setdefault(output.name, []).append(output_data)
+        
+        for outputn, outputlist in output_dict.items():
+            test_output_list = tensor_dict.get(outputn)
+            for expected, actual in zip(outputlist, test_output_list):
+                exp = expected.reshape(-1)
+                act = actual.reshape(-1)
+                self.assertTrue(np.array_equal(exp, act))
+
+
+if __name__ == "__main__":
+    unittest.main()
