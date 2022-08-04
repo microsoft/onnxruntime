d00f2c5c0af888b407251d4cb81f7b759a9e29e1
Thiago Crepaldi <thiago.crepaldi@microsoft.com>
2022-08-04 17:36:52 -0400

    Add support to N>1

--- onnxruntime/core/providers/cpu/tensor/col2im.cc
+++ onnxruntime/core/providers/cpu/tensor/col2im.cc
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+#include <cassert>
 #include "core/providers/cpu/tensor/col2im.h"
 
 #include "core/framework/element_type_lists.h"
@@ -36,12 +37,15 @@ Status Col2Im<T>::Compute(OpKernelContext* context) const {
 
   int64_t image_shape_size = 1;
   int64_t kernel_shape_size = 1;
+  int64_t kernel_shape_rank = 0;
   for (auto i=0; i < image_shape->Shape().Size(); ++i) {
+    ++kernel_shape_rank;
     image_shape_size *=  image_shape->Data<int64_t>()[i];
     kernel_shape_size *=  kernel_shape->Data<int64_t>()[i];
     // col_input_C computed as => (C*n-ary-prod{kernel_shape}) / n-ary-prod{kernel_shape}
     col_input_C /= kernel_shape->Data<int64_t>()[i];
   }
+  const int64_t col_input_offset = col_input_C * image_shape_size;
 
   TensorShapeVector Y_dims;
   Y_dims.insert(Y_dims.begin(), {col_input_N, col_input_C});
@@ -51,8 +55,11 @@ Status Col2Im<T>::Compute(OpKernelContext* context) const {
   TensorShape Yshape(Y_dims);
   Tensor* Y = context->Output(0, Yshape);
   T* Ydata = Y->template MutableData<T>();
+  for (auto i=0; i < Yshape.Size(); ++i)
+    Ydata[i] = -1; // just for debug (to know what has been written to Ydata in the end)
+  const int64_t Y_offset = Yshape.Size() / Yshape[0];
 
-  std::cout << "\n\tInput 0: col_input = ("; for (auto i=0; i < Yshape.Size(); ++i) std::cout <<  col_input_data[i] << ", "; std::cout << ") with shape "<< Yshape << std::endl;
+  std::cout << "\n\tInput 0: col_input = ("; for (auto i=0; i < Yshape.Size(); ++i) std::cout <<  col_input_data[i] << ", "; std::cout << ") with shape "<< col_input_shape << std::endl;
   std::cout << "\tInput 1: image_shape = ("; for (auto i=0; i < image_shape->Shape().Size(); ++i) std::cout << image_shape->Data<int64_t>()[i] << ", "; std::cout << ")" << std::endl;
   std::cout << "\tInput 2: kernel_shape = ("; for (auto i=0; i < kernel_shape->Shape().Size(); ++i) std::cout << kernel_shape->Data<int64_t>()[i] << ", "; std::cout << ")" << std::endl;
   std::cout << "\tAttribute strides = ("; for (size_t i=0; i < col2im_attrs_.strides.size(); ++i) std::cout <<  col2im_attrs_.strides[i] << ", "; std::cout << ")"<< std::endl;
@@ -66,41 +73,46 @@ Status Col2Im<T>::Compute(OpKernelContext* context) const {
 
   std::cout << "\n\tStatus Col2Im<T>::Compute() --> math::Col2imNd<>()" << std::endl;
 
-  if (image_shape->Shape()[0] == 2) {
-    std::cout << "image_shape->Shape()[0] == 2 --> Col2Im" << std::endl;
-    math::Col2im<float, CPUMathUtil, StorageOrder::NCHW>(
-      col_input_data,
-      col_input_C,
-      image_shape->Data<int64_t>()[0],
-      image_shape->Data<int64_t>()[1],
-      kernel_shape->Data<int64_t>()[0],
-      kernel_shape->Data<int64_t>()[1],
-      col2im_attrs_.dilations[0],
-      col2im_attrs_.dilations[1],
-      col2im_attrs_.pads[0],
-      col2im_attrs_.pads[1],
-      col2im_attrs_.pads[2],
-      col2im_attrs_.pads[3],
-      col2im_attrs_.strides[0],
-      col2im_attrs_.strides[1],
-      Ydata,
-      &CPUMathUtil::Instance());
-  } else {
-    std::cout << "image_shape->Shape()[0] != 2 --> Col2ImNd (nd=" << image_shape->Shape()[0] << ") " << std::endl;
-    math::Col2imNd<T, CPUMathUtil, StorageOrder::NCHW>(
-      col_input_data,                                   // const T* data_col,
-      image_shape->Data<int64_t>(),                     // const int64_t* img_shape,
-      Yshape.Slice(2).GetDims().data(),                 // const int64_t* output_shape,
-      col_input_C,                                      // int64_t channels_col, --> output_num_channels * kernel_shape_size
-      image_shape_size,                                 // int64_t img_size,
-      kernel_shape->Data<int64_t>(),                    // const int64_t* kernel_shape,
-      col2im_attrs_.strides.data(),                     // const int64_t* stride,
-      col2im_attrs_.dilations.data(),                   // const int64_t* dilation,
-      col2im_attrs_.pads.data(),                        // const int64_t* pad,
-      kernel_shape->Shape().Size(),                     // ptrdiff_t N, --> number of spatial dims for image
-      Ydata,                                            // T* data_img,
-      &CPUMathUtil::Instance()                          // Provider* provider
-      );
+  assert(image_shape_size == Y_offset); // just for temp debug
+
+  for (auto image_id = 0; image_id < col_input_N; ++image_id) {
+    std::cout << "Image " << image_id+1 << " out of "<< col_input_N << std::endl;
+    if (image_shape->Shape()[0] == 2) {
+      std::cout << "image_shape->Shape()[0] == 2 --> Col2Im" << std::endl;
+      math::Col2im<float, CPUMathUtil, StorageOrder::NCHW>(
+        col_input_data + image_id * col_input_offset,
+        col_input_C,
+        image_shape->Data<int64_t>()[0],
+        image_shape->Data<int64_t>()[1],
+        kernel_shape->Data<int64_t>()[0],
+        kernel_shape->Data<int64_t>()[1],
+        col2im_attrs_.dilations[0],
+        col2im_attrs_.dilations[1],
+        col2im_attrs_.pads[0],
+        col2im_attrs_.pads[1],
+        col2im_attrs_.pads[2],
+        col2im_attrs_.pads[3],
+        col2im_attrs_.strides[0],
+        col2im_attrs_.strides[1],
+        Ydata + image_id * Y_offset,
+        &CPUMathUtil::Instance());
+    } else {
+      std::cout << "image_shape->Shape()[0] != 2 --> Col2ImNd (nd=" << image_shape->Shape()[0] << ") " << std::endl;
+      math::Col2imNd<T, CPUMathUtil, StorageOrder::NCHW>(
+        col_input_data + image_id * col_input_offset,     // const T* data_col,
+        image_shape->Data<int64_t>(),                     // const int64_t* img_shape,
+        Yshape.Slice(2).GetDims().data(),                 // const int64_t* output_shape,
+        col_input_C,                                      // int64_t channels_col,
+        image_shape_size,                                 // int64_t img_size,
+        kernel_shape->Data<int64_t>(),                    // const int64_t* kernel_shape,
+        col2im_attrs_.strides.data(),                     // const int64_t* stride,
+        col2im_attrs_.dilations.data(),                   // const int64_t* dilation,
+        col2im_attrs_.pads.data(),                        // const int64_t* pad,
+        kernel_shape->Shape().Size(),                     // ptrdiff_t N, --> #spatial_dims?
+        Ydata + image_id * Y_offset,                      // T* data_img,
+        &CPUMathUtil::Instance()                          // Provider* provider
+        );
+    }
   }
   std::cout << "\n\n Return Col2Im<T>::Compute() --> "; for (auto i=0; i < Yshape.Size(); ++i) std::cout <<  Ydata[i] << ", "; std::cout << ") with shape " << Yshape << std::endl << std::endl;
 
--- onnxruntime/test/contrib_ops/col2im_test.cc
+++ onnxruntime/test/contrib_ops/col2im_test.cc
@@ -10,26 +10,27 @@ namespace onnxruntime {
 namespace test {
 
 template <typename T>
-std::vector<T> _transpose_1dvector(std::vector<T> &input, size_t C, size_t H, size_t W)
+std::vector<T> _transpose_serialized_vector(std::vector<T> &input, size_t N, size_t C, size_t H, size_t W)
 {
-    size_t n = input.size();
-    if (n == 0){
+    size_t input_size = input.size();
+    if (input_size == 0){
         throw std::runtime_error("Invalid input");
     }
     std::vector<T> trans_vec(input);
 
     std::cout << "input: (";
-    for(size_t i = 0; i < n; ++i)
+    for(size_t i = 0; i < input_size; ++i)
       std::cout << trans_vec[i] << ", ";
     std::cout << ")" << std::endl;
 
-    for(size_t c = 0; c < C; ++c)
-      for(size_t i = 0; i < H; ++i)
-        for(size_t j = i+1; j < W; ++j)
-            std::swap(trans_vec[c*(H*W) + (H*i + j)], trans_vec[c*(H*W) + (W*j + i)]);
+    for(size_t n = 0; n < N; ++n)
+      for(size_t c = 0; c < C; ++c)
+        for(size_t i = 0; i < H; ++i)
+          for(size_t j = i+1; j < W; ++j)
+              std::swap(trans_vec[n*(C*H*W) + c*(H*W) + (H*i + j)], trans_vec[n*(C*H*W) + c*(H*W) + (W*j + i)]);
 
     std::cout << "trans_vec: (";
-    for(size_t i = 0; i < n; ++i)
+    for(size_t i = 0; i < input_size; ++i)
       std::cout << trans_vec[i] << ", ";
     std::cout << ")" << std::endl;
 
@@ -46,7 +47,7 @@ TEST(Col2ImContribOpTest, simple4dNCHW) {
   std::vector<float> input(25);
   std::vector<float> output(25);
   std::iota(output.begin(), output.end(), 1);
-  input = _transpose_1dvector(output, 1, 5, 5);
+  input = _transpose_serialized_vector(output, 1, 1, 5, 5);
   test.AddInput<float>("input", {1, 5, 5},  input);
   test.AddInput<int64_t>("image_shape", {2},  std::vector<int64_t>{5, 5});
   test.AddInput<int64_t>("block_shape", {2},  std::vector<int64_t>{1, 5});
@@ -65,12 +66,31 @@ TEST(Col2ImContribOpTest, with3channels4dNCHW) {
   std::vector<float> input(75);
   std::vector<float> output(75);
   std::iota(output.begin(), output.end(), 1);
-  input = _transpose_1dvector(output, 3, 5, 5);
+  input = _transpose_serialized_vector(output, 1, 3, 5, 5);
   test.AddInput<float>("input", {1, 15, 5},  input);
   test.AddInput<int64_t>("image_shape", {2},  std::vector<int64_t>{5, 5});
   test.AddInput<int64_t>("block_shape", {2},  std::vector<int64_t>{1, 5});
 
   test.AddOutput<float>("output", {1, 3, 5, 5}, output);
+  test.Run();
+}
+
+TEST(Col2ImContribOpTest, with2Images3channels4dNCHW) {
+  OpTester test("Col2Im", 1, kMSDomain);
+
+  test.AddAttribute("strides", std::vector<int64_t>{1, 1});
+  test.AddAttribute("dilations", std::vector<int64_t>{1, 1});
+  test.AddAttribute("pads", std::vector<int64_t>{0, 0, 0, 0});
+
+  std::vector<float> input(150);
+  std::vector<float> output(150);
+  std::iota(output.begin(), output.end(), 1);
+  input = _transpose_serialized_vector(output, 2, 3, 5, 5);
+  test.AddInput<float>("input", {2, 15, 5},  input);
+  test.AddInput<int64_t>("image_shape", {2},  std::vector<int64_t>{5, 5});
+  test.AddInput<int64_t>("block_shape", {2},  std::vector<int64_t>{1, 5});
+
+  test.AddOutput<float>("output", {2, 3, 5, 5}, output);
   test.Run();
 }
 
@@ -84,7 +104,7 @@ TEST(Col2ImContribOpTest, simple5dNCHWD) {
   std::vector<float> input(25);
   std::vector<float> output(25);
   std::iota(output.begin(), output.end(), 1);
-  input = _transpose_1dvector(output, 1, 5, 5);
+  input = _transpose_serialized_vector(output, 1, 1, 5, 5);
   test.AddInput<float>("input", {1, 5, 5},  input);
   test.AddInput<int64_t>("image_shape", {3},  std::vector<int64_t>{1, 5, 5});
   test.AddInput<int64_t>("block_shape", {3},  std::vector<int64_t>{1, 1, 5});
