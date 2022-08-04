a7501b35f0021d6f6ea6d00ff84ea210d9c9be2f
Adam Louly <adamlouly@microsoft.com@orttrainingdev7.d32nl1ml4oruzj4qz3bqlggovf.px.internal.cloudapp.net>
2022-08-04 21:27:14 +0000

    Revert "run bash inside docker"
    This reverts commit 2cea5255576e095a23834667448b25c8cd5a4994.

--- tools/ci_build/github/azure-pipelines/templates/orttraining-linux-gpu-ortmodule-test-ci-pipeline.sh
+++ tools/ci_build/github/azure-pipelines/templates/orttraining-linux-gpu-ortmodule-test-ci-pipeline.sh
@@ -6,14 +6,9 @@ if [ $5 == "Manual" ]; then
     REQDIR="requirements_torch_nightly"
 fi
 
-docker run \
-    --gpus all \
-    --shm-size=1024m \
-    --rm \
-    --volume $1:/onnxruntime_src \
-    --volume $2/$3:/build \
-    --volume /mnist:/mnist \
-    --volume /bert_data:/bert_data \
-    --volume /hf_models_cache:/hf_models_cache \
-    $4 \
-    bash -c "python3 -m pip uninstall -y -r /onnxruntime_src/tools/ci_build/github/linux/docker/scripts/training/requirements.txt && python3 -m pip install -r /onnxruntime_src/tools/ci_build/github/linux/docker/scripts/training/ortmodule/stage1/${REQDIR}/requirements.txt && python3 -m pip install -r /onnxruntime_src/tools/ci_build/github/linux/docker/scripts/training/ortmodule/stage2/requirements.txt && rm -rf /build/onnxruntime/ && python3 -m pip install /build/dist/onnxruntime*.whl && python3 -m onnxruntime.training.ortmodule.torch_cpp_extensions.install && /build/launch_test.py --cmd_line_with_args 'python orttraining_ortmodule_tests.py --mnist /mnist --bert_data /bert_data/hf_data/glue_data/CoLA/original/raw --transformers_cache /hf_models_cache/huggingface/transformers' --cwd /build" \
+docker run --gpus all --shm-size=1024m --rm --volume $1:/onnxruntime_src --volume $2/$3:/build --volume /mnist:/mnist --volume /bert_data:/bert_data --volume /hf_models_cache:/hf_models_cache $4
+
+python3 -m pip uninstall -y -r /onnxruntime_src/tools/ci_build/github/linux/docker/scripts/training/requirements.txt
+python3 -m pip install -r /onnxruntime_src/tools/ci_build/github/linux/docker/scripts/training/ortmodule/stage1/$REQDIR/requirements.txt
+python3 -m pip install -r /onnxruntime_src/tools/ci_build/github/linux/docker/scripts/training/ortmodule/stage2/requirements.txt
+rm -rf /build/onnxruntime/ && python3 -m pip install /build/dist/onnxruntime*.whl && python3 -m onnxruntime.training.ortmodule.torch_cpp_extensions.install && /build/launch_test.py --cmd_line_with_args 'python orttraining_ortmodule_tests.py --mnist /mnist --bert_data /bert_data/hf_data/glue_data/CoLA/original/raw --transformers_cache /hf_models_cache/huggingface/transformers' --cwd /build
