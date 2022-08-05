2cfa98b70adb21960311a3fdfec5373239445153
Shalva Mist <shalvamist@microsoft.com>
2022-08-05 16:42:48 -0700

    Adding a few improvments

--- js/buildAll.py
+++ js/buildAll.py
@@ -6,26 +6,40 @@ import sys
 from os.path import abspath, dirname
 
 build_script = "build.sh"
-if "win" in sys.platform:
+if 'win' in sys.platform:
     build_script = "build.bat"
 
-build_dir = ["wasm", "wasm_SIMD", "wasm_threaded", "wasm_SIMD_threaded"]
-build_flags = ["", " --enable_wasm_simd", " --enable_wasm_threads", " --enable_wasm_simd --enable_wasm_threads "]
-wasm_file_names = ["ort-wasm.wasm", "ort-wasm-simd.wasm", "ort-wasm-threaded.wasm", "ort-wasm-simd-threaded.wasm"]
-js_file_names = ["ort-wasm.js", "ort-wasm-threaded.worker.js", "ort-wasm-threaded.js"]
-build_dir_js = ["wasm", "wasm_threaded", "wasm_threaded"]
+builds = [
+    {'dir':"wasm", 'wasm_flags':"", 'wasm_file_name':"ort-wasm.wasm"},
+    {'dir':"wasm_SIMD", 'wasm_flags':" --enable_wasm_simd", 'wasm_file_name':"ort-wasm-simd.wasm"},
+    {'dir':"wasm_threaded", 'wasm_flags':" --enable_wasm_threads", 'wasm_file_name':"ort-wasm-threaded.wasm"},
+    {'dir':"wasm_SIMD_threaded", 'wasm_flags':" --enable_wasm_simd --enable_wasm_threads ", 'wasm_file_name':"ort-wasm-simd-threaded.wasm"},
+]
+
+js_file_names = [
+    {'dir':"wasm",'file_name':"ort-wasm.js"},
+    {'dir':"wasm_threaded", 'file_name':"ort-wasm-threaded.worker.js"},
+    {'dir':"wasm_threaded", 'file_name':"ort-wasm-threaded.js"}
+]
+
+npm_build_dir = [
+    abspath(dirname(__file__)),
+    os.path.join(abspath(dirname(__file__)),"common"),
+    os.path.join(abspath(dirname(__file__)),"web"),
+    os.path.join(abspath(dirname(__file__)),"web")
+]
 
 default_flags = " --build_wasm --skip_tests --skip_submodule_sync --parallel "
 BinariesDirectory = ".\\build\\"
 Dist_Path = ".\\js\\web\\dist\\"
 Binding_Path = ".\\js\\web\\lib\\wasm\\binding\\"
 
-# Stage #1 - Creating the folders for the build process
+### generating the build folders
 if not os.path.isdir(BinariesDirectory):
     os.mkdir(BinariesDirectory)
 
-for path in build_dir:
-    dirctory_name = BinariesDirectory + path
+for entry in builds:
+    dirctory_name = BinariesDirectory + entry['dir']
     if not os.path.isdir(dirctory_name):
         os.mkdir(dirctory_name)
 
@@ -35,62 +49,43 @@ if not os.path.isdir(Dist_Path):
 if not os.path.isdir(Binding_Path):
     os.mkdir(Binding_Path)
 
-# Stage #1.1 - reading the script parameters
+#### running the WASM build commands
 parser = argparse.ArgumentParser()
-parser.add_argument(
-    "--config",
-    choices=["Release", "Debug", "RelWithDebInfo"],
-    help="build WASM artifactsfor the configuration - {Release,Debug,RelWithDebInfo}",
-)
-parser.add_argument(
-    "--copy",
-    choices=["Release", "Debug", "RelWithDebInfo"],
-    help="copy WASM artifacts to destination folders - {Release,Debug,RelWithDebInfo}",
-)
+parser.add_argument("--config", choices=["Release", "Debug","RelWithDebInfo"], help="build WASM artifactsfor the configuration - {Release,Debug,RelWithDebInfo}")
+parser.add_argument("--copy", choices=["Release", "Debug","RelWithDebInfo"], help="copy WASM artifacts to destination folders - {Release,Debug,RelWithDebInfo}")
 args = parser.parse_args()
 configuration = "none"
 
-# Stage #2 - Building the WASM artifacts in 4 different folders listed in - build_dir
 if args.config:
     configuration = args.config
 
-    for i, path in enumerate(build_dir):
+    for i, entry in enumerate(builds):
         command = (
             build_script
             + " --config "
             + configuration
             + default_flags
             + " --build_dir "
-            + os.path.join(BinariesDirectory, path)
-            + build_flags[i]
+            + os.path.join('"'+BinariesDirectory,builds[i]['dir']+'"')
+            + builds[i]['wasm_flags']
         )
-        p = subprocess.Popen(command, shell=True)
+        p = subprocess.Popen(command,shell=True)
         p.wait()
 
-# Stage 3 - copy the files to the right location as instructed in the build process for ORT Web
+# copy the files to the right location
 if args.copy:
     configuration = args.copy
     ## Copying WASM artifacts
-    for i, file_name in enumerate(wasm_file_names):
-        shutil.copyfile(
-            os.path.join(BinariesDirectory, build_dir[i], configuration, file_name),
-            os.path.join(Dist_Path, file_name),
-        )
+    for entry in builds:
+        file_name = entry['wasm_file_name']
+        shutil.copyfile(os.path.join(BinariesDirectory, entry['dir'], configuration, entry['wasm_file_name']), os.path.join(Dist_Path, entry['wasm_file_name']))
 
     ## Copying JS binding files
-    for i, file_name in enumerate(js_file_names):
-        shutil.copyfile(
-            os.path.join(BinariesDirectory, build_dir_js[i], configuration, file_name),
-            os.path.join(Binding_Path, file_name),
-        )
+    for entry in js_file_names:
+        shutil.copyfile(os.path.join(BinariesDirectory, entry['dir'], configuration, entry['file_name']), os.path.join(Binding_Path, entry['file_name']))
 
-# Stage 4 - running the npm build
-path = abspath(dirname(__file__))
-p = subprocess.Popen("npm ci", shell=True, cwd=os.path.join(path))
-p.wait()
-p = subprocess.Popen("npm ci", shell=True, cwd=path + "\\common\\")
-p.wait()
-p = subprocess.Popen("npm ci", shell=True, cwd=path + "\\web")
-p.wait()
-p = subprocess.Popen("npm run build", shell=True, cwd=path + "\\web")
-p.wait()
+####
+# build NPM
+for path in npm_build_dir:
+    p = subprocess.Popen("npm ci", shell=True, cwd=path)
+    p.wait()
