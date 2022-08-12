61f823efae3963ac99d24924c0ca8563f80cf1b3
cao lei <jslhcl@gmail.com>
2022-08-12 15:26:44 -0700

    fix build break (#12585)
    * fix build break of GPU TensorRT CI pipeline in PE branch
    
    * fix build break for linux GPU tensorRT CI
    
    * fix error moving a temporary object prevents copy elision
    
    * fix build errors
    
    * fix the merge issue
    
    * build break for DEBUG_NODE_INPUTS_OUTPUTS enabled
    
    * resolve comments
    
    * resolve comments
    
    * something missing in local
    
    * rename kernel_dump_context_ to dump_context_
    
    * unordered_map to inlinedHashMap
    
    * replace unordered_map and unordered_set with Inlinedxx for allocation_planner
    
    * replace unordered_map and set wiht Inlinedxxx
    
    * build break
    
    * build break
    
    Co-authored-by: Lei Cao <leca@microsoft.com@orttrainingdev9.d32nl1ml4oruzj4qz3bqlggovf.px.internal.cloudapp.net>

--- onnxruntime/core/framework/partial_graph_execution_state.cc
+++ onnxruntime/core/framework/partial_graph_execution_state.cc
@@ -39,7 +39,7 @@ ProgramRegion& PartialGraphExecutionState::GetProgramRegions(const SessionState&
 
 ExecutionContext& PartialGraphExecutionState::GetExecutionContext(gsl::span<const int>& feed_mlvalue_idxs, gsl::span<const OrtValue>& feeds,
                                                                   gsl::span<const int>& fetch_mlvalue_idxs, std::vector<OrtValue>& fetches,
-    const std::unordered_map<size_t, IExecutor::CustomAllocator>& fetch_allocators,
+    const InlinedHashMap<size_t, IExecutor::CustomAllocator>& fetch_allocators,
     const SessionState& session_state,
     const logging::Logger& sess_logger,
     const DeviceStreamCollection& device_streams_map,
