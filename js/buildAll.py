3734963e68c1382073826efe449e3b55824e7503
Shalva Mist <shalvamist@microsoft.com>
2022-08-15 09:28:28 -0700

    Updated the OS support - tested locally - All tests passed - Note that the git clone of the onnxruntime repo should be recursive for this script to work!

--- js/buildAll.py
+++ js/buildAll.py
@@ -5,19 +5,16 @@ import subprocess
 import sys
 from os.path import abspath, dirname
 
-Build_Script = "build.sh"
-CONST_VALUE_Default_Flags = " --build_wasm --skip_tests --skip_submodule_sync --parallel "
-CONST_VALUE_Binaries_Directory = "./build/"
-CONST_VALUE_Dist_Path = "./js/web/dist/"
-CONST_VALUE_Binding_Path = "./js/web/lib/wasm/binding/"
+BUILD_SCRIPT = "build.sh"
+DEFAULT_FLAGS = " --build_wasm --skip_tests --skip_submodule_sync --parallel "
+BINARIES_DIR = os.path.normpath("./build/")
+DIST_PATH = os.path.normpath("./js/web/dist/")
+BINDING_PATH = os.path.normpath("./js/web/lib/wasm/binding/")
 
 if sys.platform.startswith("win"):
-    Build_Script = "build.bat"
-    CONST_VALUE_Binaries_Directory = ".\\build\\"
-    CONST_VALUE_Dist_Path = ".\\js\\web\\dist\\"
-    CONST_VALUE_Binding_Path = ".\\js\\web\\lib\\wasm\\binding\\"
+    BUILD_SCRIPT = "build.bat"
 
-CONST_VALUE_Builds = [
+BUILDS = [
     {"dir": "wasm", "wasm_flags": "", "wasm_file_name": "ort-wasm.wasm"},
     {"dir": "wasm_SIMD", "wasm_flags": "--enable_wasm_simd", "wasm_file_name": "ort-wasm-simd.wasm"},
     {"dir": "wasm_threaded", "wasm_flags": "--enable_wasm_threads", "wasm_file_name": "ort-wasm-threaded.wasm"},
@@ -28,13 +25,13 @@ CONST_VALUE_Builds = [
     },
 ]
 
-CONST_VALUE_JS_file_names = [
+JS_FILES = [
     {"dir": "wasm", "file_name": "ort-wasm.js"},
     {"dir": "wasm_threaded", "file_name": "ort-wasm-threaded.worker.js"},
     {"dir": "wasm_threaded", "file_name": "ort-wasm-threaded.js"},
 ]
 
-CONST_VALUE_Npm_Build_Dir = [
+NPM_BUILD_DIR = [
     {"command": "npm ci", "path": abspath(dirname(__file__))},
     {"command": "npm ci", "path": os.path.join(abspath(dirname(__file__)), "common")},
     {"command": "npm ci", "path": os.path.join(abspath(dirname(__file__)), "web")},
@@ -42,19 +39,19 @@ CONST_VALUE_Npm_Build_Dir = [
 ]
 
 #### generating the build folders
-if not os.path.isdir(CONST_VALUE_Binaries_Directory):
-    os.mkdir(CONST_VALUE_Binaries_Directory)
+if not os.path.isdir(BINARIES_DIR):
+    os.mkdir(BINARIES_DIR)
 
-for entry in CONST_VALUE_Builds:
-    dirctory_name = os.path.join(CONST_VALUE_Binaries_Directory, entry["dir"])
+for entry in BUILDS:
+    dirctory_name = os.path.join(BINARIES_DIR, entry["dir"])
     if not os.path.isdir(dirctory_name):
         os.mkdir(dirctory_name)
 
-if not os.path.isdir(CONST_VALUE_Dist_Path):
-    os.mkdir(CONST_VALUE_Dist_Path)
+if not os.path.isdir(DIST_PATH):
+    os.mkdir(DIST_PATH)
 
-if not os.path.isdir(CONST_VALUE_Binding_Path):
-    os.mkdir(CONST_VALUE_Binding_Path)
+if not os.path.isdir(BINDING_PATH):
+    os.mkdir(BINDING_PATH)
 
 #### running the WASM build commands
 parser = argparse.ArgumentParser()
@@ -74,15 +71,15 @@ configuration = "none"
 if args.config:
     configuration = args.config
 
-    for entry in CONST_VALUE_Builds:
+    for entry in BUILDS:
         command = (
-            Build_Script
+            BUILD_SCRIPT
             + " --config "
             + configuration
             + " "
-            + CONST_VALUE_Default_Flags
+            + DEFAULT_FLAGS
             + " --build_dir "
-            + os.path.join('"' + CONST_VALUE_Binaries_Directory, entry["dir"] + '"')
+            + os.path.join('"' + BINARIES_DIR, entry["dir"] + '"')
             + " "
             + entry["wasm_flags"]
         )
@@ -93,30 +90,26 @@ if args.config:
 if args.copy:
     configuration = args.copy
     ## Copying WASM artifacts
-    for entry in CONST_VALUE_Builds:
-        if not os.path.exists(
-            os.path.join(CONST_VALUE_Binaries_Directory, entry["dir"], configuration, entry["wasm_file_name"])
-        ):
+    for entry in BUILDS:
+        if not os.path.exists(os.path.join(BINARIES_DIR, entry["dir"], configuration, entry["wasm_file_name"])):
             print("Error - can find " + entry["wasm_file_name"] + " there might be an issue with the build\n")
             exit()
         shutil.copyfile(
-            os.path.join(CONST_VALUE_Binaries_Directory, entry["dir"], configuration, entry["wasm_file_name"]),
-            os.path.join(CONST_VALUE_Dist_Path, entry["wasm_file_name"]),
+            os.path.join(BINARIES_DIR, entry["dir"], configuration, entry["wasm_file_name"]),
+            os.path.join(DIST_PATH, entry["wasm_file_name"]),
         )
 
     ## Copying JS binding files
-    for entry in CONST_VALUE_JS_file_names:
-        if not os.path.exists(
-            os.path.join(CONST_VALUE_Binaries_Directory, entry["dir"], configuration, entry["file_name"])
-        ):
+    for entry in JS_FILES:
+        if not os.path.exists(os.path.join(BINARIES_DIR, entry["dir"], configuration, entry["file_name"])):
             print("Error - can find " + entry["file_name"] + " there might be an issue with the build\n")
             exit()
         shutil.copyfile(
-            os.path.join(CONST_VALUE_Binaries_Directory, entry["dir"], configuration, entry["file_name"]),
-            os.path.join(CONST_VALUE_Binding_Path, entry["file_name"]),
+            os.path.join(BINARIES_DIR, entry["dir"], configuration, entry["file_name"]),
+            os.path.join(BINDING_PATH, entry["file_name"]),
         )
 
 #### build NPM package
-for entry in CONST_VALUE_Npm_Build_Dir:
+for entry in NPM_BUILD_DIR:
     p = subprocess.Popen(entry["command"], shell=True, cwd=entry["path"])
     p.wait()
