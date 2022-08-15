accd13cf59672a01555e705500185618712fc742
Shalva Mist <shalvamist@microsoft.com>
2022-08-15 15:58:02 -0700

    Created a ORTWeb python script

---
+++ js/build_web.bat
@@ -0,0 +1,6 @@
+:: Copyright (c) Microsoft Corporation. All rights reserved.
+:: Licensed under the MIT License.
+
+@echo off
+rem Requires a python 3.6 or higher install to be available in your PATH
+python %~dp0\scripts\build_web.py --config Release --copy Release
