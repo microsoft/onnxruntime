6246662b1d1b0ba33a9eebfcc426397f62335f82
Chun-Wei Chen <jacky82226@gmail.com>
2022-08-22 15:35:34 -0700

    [Dup] Fix SAME_UPPER/SAME_LOWER (auto_pad attribute) in ConvTranspose (#12537)
    * Fix SAME_UPPER/SAME_LOWER (auto_pad attribute) in ConvTranspose
    
    * Bump ONNX 1.10.2 globally
    
    * load ONNX_VERSION from VERSION_NUMBER
    
    * /
    
    * revert deprecate warning in ORT 1.12
    
    * add a comment about why removing cntk_simple_seg
    
    * correct the implem in DML as well

--- onnxruntime/core/providers/dml/OperatorAuthorHelper/OperatorHelper.cpp
+++ onnxruntime/core/providers/dml/OperatorAuthorHelper/OperatorHelper.cpp
@@ -721,7 +721,7 @@ namespace OperatorHelper
                 int paddings = gsl::narrow_cast<int>((inputDimensions[i + dimOffset] - 1) * stride + windowSize - m_outputShapes[0].GetShape()[i + dimOffset]);
                 paddings = std::max<int>(0, paddings);
 
-                m_kernel.startPadding[i] = m_kernel.autoPadSameUpper ? (paddings + 1) / 2 : paddings / 2;
+                m_kernel.startPadding[i] = m_kernel.autoPadSameUpper ? paddings / 2 : (paddings + 1) / 2;
                 m_kernel.endPadding[i] = paddings - m_kernel.startPadding[i];
             }
         }
