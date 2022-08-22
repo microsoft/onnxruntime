cefdc39b628aa575de675f7e019bbeddfa317f13
Xiaoyu Liu <lixiaoyu@microsoft.com>
2022-08-22 06:47:39 +0000

    update unit test

--- onnxruntime/test/contrib_ops/ngram_repeat_block_op_test.cc
+++ onnxruntime/test/contrib_ops/ngram_repeat_block_op_test.cc
@@ -9,7 +9,7 @@
 namespace onnxruntime {
 namespace test {
 
-TEST(NGramRepeatBlockTest, NGramSize_3_BannedNoFurther_0) {
+TEST(NGramRepeatBlockTest, NGramSize_3_Recency_0) {
   OpTester tester("NGramRepeatBlock", 1, onnxruntime::kMSDomain);
 
   tester.AddInput<int64_t>("input_ids", {3, 6},
@@ -38,11 +38,13 @@ TEST(NGramRepeatBlockTest, NGramSize_3_BannedNoFurther_0) {
   tester.Run(OpTester::ExpectResult::kExpectSuccess, "", {}, nullptr, &execution_providers);
 }
 
-TEST(NGramRepeatBlockTest, NGramSize_5_BannedNoFurther_0) {
+TEST(NGramRepeatBlockTest, NGramSize_5_Recency_0) {
   OpTester tester("NGramRepeatBlock", 1, onnxruntime::kMSDomain);
 
-  tester.AddInput<int64_t>("input_ids", {1, 32},
-      {1, 6, 5, 4,
+  tester.AddInput<int64_t>("input_ids", {2, 20},
+      {17, 18, 19, 20,
+       21, 22, 23, 24,
+       1, 6, 5, 4,
        10, 9,  8, 7,
        1, 6, 5,  4,
        3,  2, 11,  12,
@@ -50,20 +52,20 @@ TEST(NGramRepeatBlockTest, NGramSize_5_BannedNoFurther_0) {
        1,  14, 10, 9,
        15, 16,  17,  7,
        1,  6,  5,  4});
-  tester.AddInput<float>("scores", {1, 25},
+  tester.AddInput<float>("scores", {2, 12},
       {0.0f, 1.0f, 2.0f, 3.0f, 4.0f,
        5.0f, 6.0f, 7.0f, 8.0f, 9.0f,
        10.0f, 11.0f, 12.0f, 13.0f, 14.0f,
        15.0f, 16.0f, 17.0f, 18.0f, 19.0f,
-       20.0f, 21.0f, 22.0f, 23.0f, 24.0f});
+       20.0f, 21.0f, 22.0f, 23.0f});
   tester.AddAttribute("ngram_size", (int64_t)5);
   tester.AddAttribute("recency_length", (int64_t)0);
-  tester.AddOutput<float>("scores_out", {1, 25},
-      {0.0f, 1.0f, 2.0f, -std::numeric_limits<float>::infinity(), 4.0f,
+  tester.AddOutput<float>("scores_out", {2, 12},
+      {0.0f, 1.0f, 2.0f, 3.0f, 4.0f,
        5.0f, 6.0f, 7.0f, 8.0f, 9.0f,
        -std::numeric_limits<float>::infinity(), 11.0f, 12.0f, 13.0f, 14.0f,
        15.0f, 16.0f, 17.0f, 18.0f, 19.0f,
-       20.0f, 21.0f, 22.0f, 23.0f, 24.0f});
+       20.0f, 21.0f, 22.0f, 23.0f});
 
   if (HasCudaEnvironment(0)) {
     std::vector<std::unique_ptr<IExecutionProvider>> execution_providers;
@@ -76,11 +78,13 @@ TEST(NGramRepeatBlockTest, NGramSize_5_BannedNoFurther_0) {
   tester.Run(OpTester::ExpectResult::kExpectSuccess, "", {}, nullptr, &execution_providers);
 }
 
-TEST(NGramRepeatBlockTest, NGramSize_5_BannedNoFurther_24) {
+TEST(NGramRepeatBlockTest, NGramSize_5_Recency_7) {
   OpTester tester("NGramRepeatBlock", 1, onnxruntime::kMSDomain);
 
-  tester.AddInput<int64_t>("input_ids", {1, 32},
-      {1, 6, 5, 4,
+  tester.AddInput<int64_t>("input_ids", {2, 20},
+      {17, 18, 19, 20,
+       21, 22, 23, 24,
+       1, 6, 5, 4,
        10, 9,  8, 7,
        1, 6, 5,  4,
        3,  2, 11,  12,
@@ -88,20 +92,20 @@ TEST(NGramRepeatBlockTest, NGramSize_5_BannedNoFurther_24) {
        1,  14, 10, 9,
        15, 16,  17,  7,
        1,  6,  5,  4});
-  tester.AddInput<float>("scores", {1, 25},
+  tester.AddInput<float>("scores", {2, 12},
       {0.0f, 1.0f, 2.0f, 3.0f, 4.0f,
        5.0f, 6.0f, 7.0f, 8.0f, 9.0f,
        10.0f, 11.0f, 12.0f, 13.0f, 14.0f,
        15.0f, 16.0f, 17.0f, 18.0f, 19.0f,
-       20.0f, 21.0f, 22.0f, 23.0f, 24.0f});
+       20.0f, 21.0f, 22.0f, 23.0f});
   tester.AddAttribute("ngram_size", (int64_t)5);
-  tester.AddAttribute("recency_length", (int64_t)25);
-  tester.AddOutput<float>("scores_out", {1, 25},
-      {0.0f, 1.0f, 2.0f, -std::numeric_limits<float>::infinity(), 4.0f,
+  tester.AddAttribute("recency_length", (int64_t)7);
+  tester.AddOutput<float>("scores_out", {2, 12},
+      {0.0f, 1.0f, 2.0f, 3.0f, 4.0f,
        5.0f, 6.0f, 7.0f, 8.0f, 9.0f,
        10.0f, 11.0f, 12.0f, 13.0f, 14.0f,
        15.0f, 16.0f, 17.0f, 18.0f, 19.0f,
-       20.0f, 21.0f, 22.0f, 23.0f, 24.0f});
+       20.0f, 21.0f, 22.0f, 23.0f});
 
   if (HasCudaEnvironment(0)) {
     std::vector<std::unique_ptr<IExecutionProvider>> execution_providers;
