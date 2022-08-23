4ed3856fe461d45783484d1462620921ae032d08
Hari Seshadri <hasesh@microsoft.com>
2022-08-23 14:31:45 -0700

    Add GPT2 pattern for QOrderedGelu

--- onnxruntime/python/tools/transformers/fusion_qordered_gelu.py
+++ onnxruntime/python/tools/transformers/fusion_qordered_gelu.py
@@ -16,15 +16,27 @@ logger = getLogger(__name__)
 
 class FusionQOrderedGelu(Fusion):
     def __init__(self, model: OnnxModel):
-        super().__init__(model, "QOrderedGelu", "Gelu")
+        super().__init__(model, "QOrderedGelu", ["Gelu", "FastGelu"])
 
     def fuse(self, node, input_name_to_nodes: Dict, output_name_to_node: Dict):
         """
+        INPUT PATTERN
         Fuse (quantized) Gelu subgraph into one node QOrderedGelu:
             -> quantized input  -> DQ -> Gelu -> Q ->
 
-        should become
+        (or)
+
+            -> quantized input  -> DQ -> FastGelu -> Q ->
+
+        (or)
+
+            -> quantized input  -> DQ -> Reshape -> FastGelu -> Q ->
+
+        OUTPUT PATTERN
             -> QOrderedGelu ->
+
+        (or)
+            -> Reshape -> QOrderedGelu ->
         """
         gelu_children = self.model.get_children(node, input_name_to_nodes)
 
@@ -37,23 +49,51 @@ class FusionQOrderedGelu(Fusion):
         if not FusionUtils.check_qdq_node_for_fusion(downstream_quantize_node, self.model):
             return
 
-        # The first input to Gelu should flow through a DequantizeLinear node
-        first_path_id, first_input_parent_nodes, _ = self.model.match_parent_paths(
-            node,
-            [(["DequantizeLinear"], [0])],
-            output_name_to_node,
-        )
+        # The first input to Gelu/FastGelu should flow through a DequantizeLinear node
+        
+        # In GPT2, there a Reshape between the DQ node and the FastGelu node to reshape
+        # the output of the upstream Gemm. In this case, we move the DQ node below the 
+        # Reshape node and then fuse the FastGelu node.
+        dq_paths = {
+            "path1": (["DequantizeLinear"], [0]),
+            "path2": (["Reshape", "DequantizeLinear"], [0, 0]),
+        }
 
-        if first_path_id < 0:
+        dq_path = None
+        has_reshape = False
+        for k, v in dq_paths.items():
+            dq_path = self.model.match_parent_path(dq_paths, v[0], v[1])
+            if dq_path is None:
+                continue
+            if k == "path2":
+                has_reshape = True
+            break
+
+        if dq_path is None:
+            logger.debug(f"It is not safe to fuse QOrderedGelu node. Skip")
             return
 
-        upstream_dequantize_node = first_input_parent_nodes[0]
+        upstream_reshape_node = dq_path[0] if has_reshape else None
+        upstream_dequantize_node = dq_path[1] if has_reshape else dq_path[0]
 
         if not FusionUtils.check_qdq_node_for_fusion(upstream_dequantize_node, self.model):
             return
 
+        # Push Reshape node above the DQ node if it exists
+        if has_reshape:
+            """
+            -> DQ -> Reshape -> FastGelu -> Q ->
+
+            should become
+
+            -> Reshape -> DQ -> FastGelu -> Q ->            
+            """
+            self.model.replace_node_input(upstream_reshape_node, upstream_reshape_node.input[0], upstream_dequantize_node.input[0])
+            self.model.replace_node_input(upstream_dequantize_node, upstream_dequantize_node.input[0], upstream_reshape_node.output[0])
+            self.model.replace_node_input(node, node.input[0], upstream_dequantize_node.output[0])
+
         # Fusion logic
-        subgraph_nodes = [node]  # Gelu
+        subgraph_nodes = [node]  # Gelu/FastGelu
         subgraph_nodes.extend([downstream_quantize_node, upstream_dequantize_node])  # Relevant Q, DQ nodes
 
         if not self.model.is_safe_to_fuse_nodes(
