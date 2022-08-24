1aeaa1cd454fee8a578ada4ac41459b405749d67
adrianlizarraga <adlizarraga@microsoft.com>
2022-08-24 15:21:55 -0700

    Add module doc string

--- onnxruntime/python/tools/tensorrt/perf/build/build_image.py
+++ onnxruntime/python/tools/tensorrt/perf/build/build_image.py
@@ -2,6 +2,10 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 # --------------------------------------------------------------------------
+"""
+Builds an Ubuntu-based Docker image with TensorRT.
+"""
+
 import argparse
 import os
 import shlex
