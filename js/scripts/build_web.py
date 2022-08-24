9793db35703cb3ff486891def918d52c1858f917
shalvamist <shalva.mist@microsoft.com>
2022-08-24 10:48:23 -0700

    Updates - the subprocess command use "with" statement - enabled to run the script from any folder - changed a few syntax issues

--- js/scripts/build_web.py
+++ js/scripts/build_web.py
@@ -1,3 +1,6 @@
+## Copyright (c) Microsoft Corporation. All rights reserved.
+## Licensed under the MIT License.
+
 import argparse
 import os
 import shutil
@@ -5,14 +8,17 @@ import subprocess
 import sys
 from os.path import abspath, dirname
 
-BUILD_SCRIPT = "source ../build.sh"
-DEFAULT_FLAGS = " --build_wasm --skip_tests --skip_submodule_sync --parallel "
-BINARIES_DIR = os.path.normpath("../build/")
-DIST_PATH = os.path.normpath("./web/dist/")
-BINDING_PATH = os.path.normpath("./web/lib/wasm/binding/")
+CURRENT_FOLDER = abspath(dirname(__file__))
+ROOT_DIR = os.path.normpath(os.path.join(CURRENT_FOLDER, "..\\..\\"))
 
+BUILD_SCRIPT = "source build.sh"
 if sys.platform.startswith("win"):
-    BUILD_SCRIPT = "call ..\\build.bat"
+    BUILD_SCRIPT = "build.bat"
+
+DEFAULT_FLAGS = " --build_wasm --skip_tests --skip_submodule_sync --parallel "
+BINARIES_DIR = os.path.normpath(os.path.join(ROOT_DIR, "./build/"))
+DIST_PATH = os.path.normpath(os.path.join(ROOT_DIR, "./js/web/dist/"))
+BINDING_PATH = os.path.normpath(os.path.join(ROOT_DIR, "./js/web/lib/wasm/binding/"))
 
 BUILDS = [
     {"dir": "wasm", "wasm_flags": "", "wasm_file_name": "ort-wasm.wasm"},
@@ -32,10 +38,10 @@ JS_FILES = [
 ]
 
 NPM_BUILD_DIR = [
-    {"command": "npm ci", "path": os.path.normpath(os.path.join(abspath(dirname(__file__)), "../"))},
-    {"command": "npm ci", "path": os.path.normpath(os.path.join(abspath(dirname(__file__)), "../common"))},
-    {"command": "npm ci", "path": os.path.normpath(os.path.join(abspath(dirname(__file__)), "../web"))},
-    {"command": "npm run build", "path": os.path.normpath(os.path.join(abspath(dirname(__file__)), "../web"))},
+    {"command": "npm ci", "path": os.path.normpath(os.path.join(CURRENT_FOLDER, "../"))},
+    {"command": "npm ci", "path": os.path.normpath(os.path.join(CURRENT_FOLDER, "../common"))},
+    {"command": "npm ci", "path": os.path.normpath(os.path.join(CURRENT_FOLDER, "../web"))},
+    {"command": "npm run build", "path": os.path.normpath(os.path.join(CURRENT_FOLDER, "../web"))},
 ]
 
 #### Generating the build folders
@@ -58,29 +64,31 @@ parser = argparse.ArgumentParser()
 parser.add_argument(
     "--config",
     choices=["Release", "Debug", "RelWithDebInfo"],
+    default="Release",
     help="build WASM artifactsfor the configuration - {Release,Debug,RelWithDebInfo}",
 )
 
 args = parser.parse_args()
 
-if args.config:
-    configuration = args.config
+CONFIGURATION = args.config
 
-    for entry in BUILDS:
-        command = (
-            BUILD_SCRIPT
-            + " --config "
-            + configuration
-            + " "
-            + DEFAULT_FLAGS
-            + " --build_dir "
-            + os.path.join('"' + BINARIES_DIR, entry["dir"] + '"')
-            + " "
-            + entry["wasm_flags"]
-        )
-        p = subprocess.Popen(command, shell=True)
+for entry in BUILDS:
+    command = (
+        BUILD_SCRIPT
+        + " --config "
+        + CONFIGURATION
+        + " "
+        + DEFAULT_FLAGS
+        + " --build_dir "
+        + os.path.join('"' + BINARIES_DIR, entry["dir"] + '"')
+        + " "
+        + entry["wasm_flags"]
+    )
+    print(ROOT_DIR)
+    print(command)
+    with subprocess.Popen(command, shell=True, cwd=ROOT_DIR) as p:
         p.wait()
-        if not os.path.exists(os.path.join(BINARIES_DIR, entry["dir"], configuration, entry["wasm_file_name"])) and (
+        if not os.path.exists(os.path.join(BINARIES_DIR, entry["dir"], CONFIGURATION, entry["wasm_file_name"])) and (
             p.returncode != 0
         ):
             print("Error - can find " + entry["wasm_file_name"] + " there might be an issue with the build\n")
@@ -88,25 +96,26 @@ if args.config:
 
 ## Copying WASM artifacts
 for entry in BUILDS:
-    if not os.path.exists(os.path.join(BINARIES_DIR, entry["dir"], configuration, entry["wasm_file_name"])):
+    if not os.path.exists(os.path.join(BINARIES_DIR, entry["dir"], CONFIGURATION, entry["wasm_file_name"])):
         print("Error - can find " + entry["wasm_file_name"] + " there might be an issue with the build\n")
         sys.exit()
     shutil.copyfile(
-        os.path.join(BINARIES_DIR, entry["dir"], configuration, entry["wasm_file_name"]),
+        os.path.join(BINARIES_DIR, entry["dir"], CONFIGURATION, entry["wasm_file_name"]),
         os.path.join(DIST_PATH, entry["wasm_file_name"]),
     )
 
 ## Copying JS binding files
 for entry in JS_FILES:
-    if not os.path.exists(os.path.join(BINARIES_DIR, entry["dir"], configuration, entry["file_name"])):
+    if not os.path.exists(os.path.join(BINARIES_DIR, entry["dir"], CONFIGURATION, entry["file_name"])):
         print("Error - can find " + entry["file_name"] + " there might be an issue with the build\n")
         sys.exit()
     shutil.copyfile(
-        os.path.join(BINARIES_DIR, entry["dir"], configuration, entry["file_name"]),
+        os.path.join(BINARIES_DIR, entry["dir"], CONFIGURATION, entry["file_name"]),
         os.path.join(BINDING_PATH, entry["file_name"]),
     )
 
 #### Build NPM package
 for entry in NPM_BUILD_DIR:
-    p = subprocess.Popen(entry["command"], shell=True, cwd=entry["path"])
-    p.wait()
+    print(command)
+    with subprocess.Popen(entry["command"], shell=True, cwd=entry["path"]) as p:
+        p.wait()
