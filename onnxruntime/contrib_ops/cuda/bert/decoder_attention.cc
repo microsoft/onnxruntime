9e5f90bbd5cd92cb4e62715f99f0f291f98d4fb5
Ye Wang <wangye@microsoft.com>
2022-08-25 10:00:15 -0700

    fix warnings

--- onnxruntime/contrib_ops/cuda/bert/decoder_attention.cc
+++ onnxruntime/contrib_ops/cuda/bert/decoder_attention.cc
@@ -85,7 +85,7 @@ Status CheckInputs(const TensorShape& query_shape,
     return ORT_MAKE_STATUS(ONNXRUNTIME, INVALID_ARGUMENT, "q_weights shall have shape (hidden size, hidden size)");
   }
 
-  if (kv_weights_dims[0] != hidden_size || kv_weights_dims[1] != 2 * hidden_size) {
+  if (kv_weights_dims[0] != hidden_size || kv_weights_dims[1] != static_cast<int64_t>(2 * hidden_size)) {
     return ORT_MAKE_STATUS(ONNXRUNTIME, INVALID_ARGUMENT, "kv_weights shall have shape (hidden size, 2 * hidden size)");
   }
 
