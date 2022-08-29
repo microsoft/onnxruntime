0ab9ee4ebf85aedd41ffa75719df0433ecb86fd8
Chen Fu <1316708+chenfucn@users.noreply.github.com>
2022-08-29 12:19:37 -0700

    expose pre process function

--- onnxruntime/python/tools/quantization/__init__.py
+++ onnxruntime/python/tools/quantization/__init__.py
@@ -2,3 +2,4 @@ from .calibrate import CalibraterBase, CalibrationDataReader, CalibrationMethod,
 from .qdq_quantizer import QDQQuantizer
 from .quant_utils import QuantFormat, QuantType, write_calibration_table
 from .quantize import QuantizationMode, quantize_dynamic, quantize_static
+from .shape_inference import quant_pre_process
