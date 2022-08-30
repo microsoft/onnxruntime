microsoft/onnxruntime

commit 10ea17c55aa2b86dd0b164fe4e35818d6ee9e761

Change cuda and rocm error checking helpers to return Status (#12699)
* CudaCall returns Status in non-throw and void in throw

* RocmCall returns Status in non-throw and void in throw

onnxruntime/contrib_ops/rocm/bert/fast_gelu_impl.cu
@@ -40,7 +40,7 @@ namespace contrib {
 namespace rocm {
 
 template <typename T>
-bool LaunchFastGeluKernel(hipStream_t stream, int input_length, int bias_length,
+Status LaunchFastGeluKernel(hipStream_t stream, int input_length, int bias_length,
                           const T* input, const T* bias, T* output, bool tuning) {
   static FastGeluTunableOp<T> op;
   if (tuning) {
@@ -50,13 +50,13 @@ bool LaunchFastGeluKernel(hipStream_t stream, int input_length, int bias_length,
   return op(&op_params).IsOK();
 }
 
-template bool LaunchFastGeluKernel<float>(hipStream_t stream, int input_length, int bias_length,
+template Status LaunchFastGeluKernel<float>(hipStream_t stream, int input_length, int bias_length,
                                           const float* input, const float* bias, float* output, bool tuning);
 
-template bool LaunchFastGeluKernel<BFloat16>(hipStream_t stream, int input_length, int bias_length,
+template Status LaunchFastGeluKernel<BFloat16>(hipStream_t stream, int input_length, int bias_length,
                                              const BFloat16* input, const BFloat16* bias, BFloat16* output, bool tuning);
 
-template bool LaunchFastGeluKernel<half>(hipStream_t stream, int input_length, int bias_length,
+template Status LaunchFastGeluKernel<half>(hipStream_t stream, int input_length, int bias_length,
                                          const half* input, const half* bias, half* output, bool tuning);
 
 }  // namespace rocm

onnxruntime/contrib_ops/cuda/bert/fast_gelu_impl.cu
@@ -65,18 +65,18 @@ __global__ void FastGeluKernel2(const half2 a, const half2 b, const half2 c, int
 }
 
 template <>
-bool LaunchFastGeluKernel(const cudaDeviceProp& prop, cudaStream_t stream, int input_length, int bias_length,
+Status LaunchFastGeluKernel(const cudaDeviceProp& prop, cudaStream_t stream, int input_length, int bias_length,
                           const float* input, const float* bias, float* output, bool /*use_half2*/) {
   constexpr int blockSize = 256;
   const int gridSize = (input_length + blockSize - 1) / blockSize;
   FastGeluKernel<float, blockSize><<<gridSize, blockSize, 0, stream>>>(A, B, C, input_length, bias_length,
                                                                        input, bias, output);
 
-  return CUDA_CALL(cudaPeekAtLastError());
+  return CUDA_CALL(cudaGetLastError());
 }
 
 template <>
-bool LaunchFastGeluKernel(const cudaDeviceProp& prop, cudaStream_t stream, int input_length, int bias_length,
+Status LaunchFastGeluKernel(const cudaDeviceProp& prop, cudaStream_t stream, int input_length, int bias_length,
                           const half* input, const half* bias, half* output, bool use_half2) {
   constexpr int blockSize = 256;
   if (use_half2 && 0 == (bias_length & 1) && prop.major >= 7) {
@@ -96,11 +96,11 @@ bool LaunchFastGeluKernel(const cudaDeviceProp& prop, cudaStream_t stream, int i
                                                                         input, bias, output);
   }
 
-  return CUDA_CALL(cudaPeekAtLastError());
+  return CUDA_CALL(cudaGetLastError());
 }
 
 template <>
-bool LaunchFastGeluKernel(const cudaDeviceProp& prop, cudaStream_t stream, int input_length, int bias_length,
+Status LaunchFastGeluKernel(const cudaDeviceProp& prop, cudaStream_t stream, int input_length, int bias_length,
                           const BFloat16* input, const BFloat16* bias, BFloat16* output, bool /*use_half2*/) {
   constexpr int blockSize = 256;
 
@@ -110,7 +110,7 @@ bool LaunchFastGeluKernel(const cudaDeviceProp& prop, cudaStream_t stream, int i
   FastGeluKernel<BFloat16, blockSize>
       <<<gridSize, blockSize, 0, stream>>>(A, B, C, input_length, bias_length, input, bias, output);
 
-  return CUDA_CALL(cudaPeekAtLastError());
+  return CUDA_CALL(cudaGetLastError());
 }
 
 }  // namespace cuda

commit 1e863759ebec88b99bf05106347ca8f2a1e5f205

Fix rocm build caused by #12699 (#12787)

onnxruntime/contrib_ops/rocm/bert/fast_gelu_impl.cu
@@ -47,7 +47,7 @@ Status LaunchFastGeluKernel(hipStream_t stream, int input_length, int bias_lengt
     op.EnableTuning();
   }
   FastGeluParams<T> op_params(stream, input, bias, output, input_length, bias_length);
-  return op(&op_params).IsOK();
+  return op(&op_params);
 }
 
 template Status LaunchFastGeluKernel<float>(hipStream_t stream, int input_length, int bias_length,