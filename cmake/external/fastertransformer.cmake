c84e00825e8e41e01b9c1b122e45e2c188dff19f
hariharans29 <shariharan91@gmail.com>
2022-09-06 18:01:37 -0700

    a

--- cmake/external/fastertransformer.cmake
+++ cmake/external/fastertransformer.cmake
@@ -3,7 +3,7 @@ set(fastertransformer_root_dir ${PROJECT_SOURCE_DIR}/external/FasterTransformer)
 add_definitions(-DUSE_FASTERTRANSFORMER)
 include_directories(${fastertransformer_root_dir}/src/fastertransformer/models/bert)
 
-# option(MI_OVERRIDE "" OFF)
+option(SM "" 75)
 
 add_subdirectory(${fastertransformer_root_dir} EXCLUDE_FROM_ALL)
 set_target_properties(transformer-static PROPERTIES RUNTIME_OUTPUT_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR})
