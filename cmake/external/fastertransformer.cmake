f6c503a84f63ce0bd4af3f91f278952f3044b2c0
hariharans29 <shariharan91@gmail.com>
2022-09-07 12:59:02 -0700

    More commits 5

--- cmake/external/fastertransformer.cmake
+++ cmake/external/fastertransformer.cmake
@@ -1,8 +1,6 @@
 set(FASTERTRANSFORMER_DIR ${PROJECT_SOURCE_DIR}/external/FasterTransformer)
 
-if (onnxruntime_BUILD_WEBASSEMBLY)
-  execute_process(COMMAND  git apply --ignore-space-change --ignore-whitespace ${PROJECT_SOURCE_DIR}/patches/fastertransformer/DisableBFloat16Support2.patch WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}/${FASTERTRANSFORMER_DIR})
-endif()
+execute_process(COMMAND  git apply --ignore-space-change --ignore-whitespace ${PROJECT_SOURCE_DIR}/patches/fastertransformer/DisableBFloat16Support2.patch WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}/${FASTERTRANSFORMER_DIR})
 
 add_definitions(-DUSE_FASTERTRANSFORMER)
 include_directories(${FASTERTRANSFORMER_DIR}/src/fastertransformer/models)
