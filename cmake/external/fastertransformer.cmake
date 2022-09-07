376ddb39bc86df5064185e0c50ebd5b28d4e30e1
hariharans29 <shariharan91@gmail.com>
2022-09-07 13:52:49 -0700

    More commits 6

--- cmake/external/fastertransformer.cmake
+++ cmake/external/fastertransformer.cmake
@@ -3,8 +3,9 @@ set(FASTERTRANSFORMER_DIR ${PROJECT_SOURCE_DIR}/external/FasterTransformer)
 execute_process(COMMAND  git apply --ignore-space-change --ignore-whitespace ${PROJECT_SOURCE_DIR}/patches/fastertransformer/DisableBFloat16Support.patch WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}/${FASTERTRANSFORMER_DIR})
 
 add_definitions(-DUSE_FASTERTRANSFORMER)
+
+include_directories(${FASTERTRANSFORMER_DIR})
 include_directories(${FASTERTRANSFORMER_DIR}/src/fastertransformer/models)
 
 add_subdirectory(${FASTERTRANSFORMER_DIR} EXCLUDE_FROM_ALL)
 set_target_properties(transformer-static PROPERTIES RUNTIME_OUTPUT_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR})
-
