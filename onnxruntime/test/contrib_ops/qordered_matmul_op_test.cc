4088289c7507c6f5478c0dc2e1a8a59aa6c12fe4
Hariharan Seshadri <shariharan91@gmail.com>
2022-09-08 14:58:55 -0700

    Update

--- onnxruntime/test/contrib_ops/qordered_matmul_op_test.cc
+++ onnxruntime/test/contrib_ops/qordered_matmul_op_test.cc
@@ -131,7 +131,7 @@ TEST(QOrderedTest, MatMul_bias_COL_16x64x32) {
                           1.0f / 32.0f, 1.0f / 32.0f, 0.0f /*scaleC*/, 2.0f,
                           true /* add bias */, false /* broadcast batch c */);
 }
-#endif
+#endif // ifndef _WIN32
 
 TEST(QOrderedTest, MatMul_addC_COL_16x64x32) {
   std::vector<int64_t> shape_A = {16, 32};
@@ -151,7 +151,7 @@ TEST(QOrderedTest, MatMul_bias_addC_COL_16x64x32) {
                           1.0f / 32.0f, 1.0f / 32.0f, 4.0f /*scaleC*/, 2.0f,
                           true /* add bias */, true /* broadcast batch c */);
 }
-#endif
+#endif // ifndef _WIN32
  
 TEST(QOrderedTest, MatMul_COL_16x64x32_b3_1) {
   std::vector<int64_t> shape_A = {3, 16, 32};
@@ -171,7 +171,7 @@ TEST(QOrderedTest, MatMul_bias_COL_16x64x32_b2_1) {
                           1.0f / 32.0f, 1.0f / 32.0f, 0.0f /*scaleC*/, 2.0f,
                           true /* add bias */, false /* broadcast batch c */);
 }
-#endif
+#endif // ifndef _WIN32
 
 TEST(QOrderedTest, MatMul_addC_COL_16x64x32_b2_1) {
   std::vector<int64_t> shape_A = {2, 16, 32};
@@ -191,7 +191,7 @@ TEST(QOrderedTest, MatMul_addC_broadcastC_COL_16x64x32_b2_1) {
                           false /* add bias */, true /* broadcast batch c */);
 }
 
-#ifndef _WIN32    
+#ifndef _WIN32
 TEST(QOrderedTest, MatMul_addC_bias_COL_16x64x32_b2_1) {
   std::vector<int64_t> shape_A = {2, 16, 32};
   std::vector<int64_t> shape_B = {1, 32, 64};
@@ -200,8 +200,9 @@ TEST(QOrderedTest, MatMul_addC_bias_COL_16x64x32_b2_1) {
                           1.0f / 32.0f, 1.0f / 32.0f, 0.0f /*scaleC*/, 2.0f,
                           true /* add bias */, false /* broadcast batch c */);
 }
-#endif
+#endif // ifndef _WIN32
 
+#ifndef _WIN32
 TEST(QOrderedTest, MatMul_bias_addC_broadcastC_COL_16x64x32_b2_1) {
   std::vector<int64_t> shape_A = {2, 16, 32};
   std::vector<int64_t> shape_B = {1, 32, 64};
@@ -210,8 +211,9 @@ TEST(QOrderedTest, MatMul_bias_addC_broadcastC_COL_16x64x32_b2_1) {
                           1.0f / 32.0f, 1.0f / 32.0f, 0.0f /*scaleC*/, 2.0f,
                           true /* add bias */, true /* broadcast batch c */);
 }
+#endif // ifndef _WIN32
 
-#endif
+#endif // USE_CUDA
 
 }  // namespace test
 }  // namespace onnxruntime
