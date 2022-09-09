75f7c8b41bb4e7150f0b8e17785993fcb818a9ff
Changming Sun <chasun@microsoft.com>
2022-09-08 21:42:00 -0700

    update

--- tools/ci_build/github/linux/docker/inference/x64/python/cpu/scripts/install_deps.sh
+++ tools/ci_build/github/linux/docker/inference/x64/python/cpu/scripts/install_deps.sh
@@ -2,11 +2,13 @@
 set -e -x
 
 PYTHON_EXES=("/opt/python/cp37-cp37m/bin/python3.7" "/opt/python/cp38-cp38/bin/python3.8" "/opt/python/cp39-cp39/bin/python3.9" "/opt/python/cp310-cp310/bin/python3.10")
-
+CURRENT_DIR=$(pwd)
 if ! [ -x "$(command -v protoc)" ]; then
   mkdir -p /tmp/src
   cd /tmp/src
-  ./install_protobuf.sh
+  $CURRENT_DIR/install_protobuf.sh
+  cd /
+  rm -rf /tmp/src
 fi
 
 export ONNX_ML=1
