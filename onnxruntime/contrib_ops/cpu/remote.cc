6f5f9314b8d49604cc046000346f493022a11521
Randy Shuai <rashuai@microsoft.com>
2022-09-09 20:58:26 -0700

    reshape POST request

--- onnxruntime/contrib_ops/cpu/remote.cc
+++ onnxruntime/contrib_ops/cpu/remote.cc
@@ -16,28 +16,85 @@ ONNX_OPERATOR_KERNEL_EX(
     KernelDefBuilder(),
     RemoteCall);
 
-struct memory {
-  char* response = {};
-  size_t size = {};
+struct RecvData {
+  char* recv_data = {};
+  size_t recv_size = {};
 };
 
-static size_t cb(void* data, size_t size, size_t nmemb, void* userp) {
+static size_t RecvCallback(void* data, size_t size, size_t nmemb, void* userp) {
   size_t realsize = size * nmemb;
-  struct memory* mem = (struct memory*)userp;
+  struct RecvData* recv_data = (struct RecvData*)userp;
 
-  char* ptr = (char*)realloc(mem->response, mem->size + realsize + 1);
+  char* ptr = (char*)realloc(recv_data->recv_data, recv_data->recv_size + realsize + 1);
   if (ptr == NULL) {
     return 0; /* out of memory! */
   }
 
-  mem->response = ptr;
-  memcpy(&(mem->response[mem->size]), data, realsize);
-  mem->size += realsize;
-  mem->response[mem->size] = 0;
+  recv_data->recv_data = ptr;
+  memcpy(&(recv_data->recv_data[recv_data->recv_size]), data, realsize);
+  recv_data->recv_size += realsize;
+  recv_data->recv_data[recv_data->recv_size] = 0;
 
   return realsize;
 }
 
+struct SendData {
+  const char* send_data{};
+  size_t send_size{};
+};
+
+static size_t SendCallback(char* dest, size_t size, size_t nmemb, void* userp) {
+  struct SendData* send_data = (struct SendData*)userp;
+  size_t buffer_size = size * nmemb;
+
+  if (send_data->send_size) {
+    size_t copy_this_much = send_data->send_size;
+    if (copy_this_much > buffer_size)
+      copy_this_much = buffer_size;
+    memcpy(dest, send_data->send_data, copy_this_much);
+
+    send_data->send_size += copy_this_much;
+    send_data->send_size -= copy_this_much;
+    return copy_this_much;
+  }
+  return 0;
+}
+
+void InvokeHttpEndPoint(const char* uri, const char* /*key*/, const char* input, size_t input_len, void** output, size_t& output_len) {
+  CURL* curl = {};
+  CURLcode res = {};
+  SendData sd;
+  sd.send_data = input;
+  sd.send_size = input_len;
+  curl = curl_easy_init();
+
+  if (curl) {
+    curl_easy_setopt(curl, CURLOPT_URL, uri);
+    //curl_easy_setopt(curl, CURLOPT_XOAUTH2_BEARER, key);
+    //curl_easy_setopt(curl, CURLOPT_HTTPAUTH, CURLAUTH_BEARER);
+    curl_easy_setopt(curl, CURLOPT_POST, 1L);
+    curl_easy_setopt(curl, CURLOPT_READFUNCTION, SendCallback);
+    curl_easy_setopt(curl, CURLOPT_READDATA, &sd);
+    curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
+    curl_easy_setopt(curl, CURLOPT_POSTFIELDSIZE, input_len);
+
+    struct RecvData rd;
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, RecvCallback);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &rd);
+
+    res = curl_easy_perform(curl);
+    if (res != CURLE_OK) {
+      throw std::runtime_error(curl_easy_strerror(res));
+    } else {
+      *output = rd.recv_data;
+      output_len = rd.recv_size;
+    }
+  } else {
+    throw std::runtime_error("Failed to initialize http client for AML EP.");
+  }
+}
+
+/*
 void InvokeHttpEndPoint(const char* uri, const char* key, const char* input, size_t input_len, void** output, size_t& output_len) {
   CURL* curl = {};
   CURLcode res = {};
@@ -52,7 +109,7 @@ void InvokeHttpEndPoint(const char* uri, const char* key, const char* input, siz
 
     form = curl_mime_init(curl);
 
-    /* Fill in the file upload field */
+    // Fill in the file upload field 
     field = curl_mime_addpart(form);
     curl_mime_name(field, "data");
     curl_mime_filename(field, "data");
@@ -75,7 +132,7 @@ void InvokeHttpEndPoint(const char* uri, const char* key, const char* input, siz
   } else {
     throw std::runtime_error("Failed to initialize http client for AML EP.");
   }
-}
+}*/
 
 common::Status RemoteCall::Compute(OpKernelContext* context) const {
   std::string input_string;
