c32bb092d526764635f41831a889e53361d8f01a
Jian Chen <cjian@microsoft.com>
2022-09-13 09:36:28 -0700

    Format Code

--- onnxruntime/python/tools/quantization/operators/where.py
+++ onnxruntime/python/tools/quantization/operators/where.py
@@ -1,8 +1,8 @@
 import onnx
 
+from ..quant_utils import TENSOR_NAME_QUANT_SUFFIX, QuantizedValue, QuantizedValueType, attribute_to_kwarg, ms_domain
 from .base_operator import QuantOperatorBase
 from .qdq_base_operator import QDQOperatorBase
-from ..quant_utils import TENSOR_NAME_QUANT_SUFFIX, QuantizedValue, QuantizedValueType, attribute_to_kwarg, ms_domain
 
 
 class QLinearWhere(QuantOperatorBase):
