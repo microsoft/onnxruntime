bf7c0f653eb9aa2e259d2c33fbf4c7b47a429389
Yi-Hong Lyu <yilyu@microsoft.com>
2022-09-15 18:39:00 -0700

    origin_sess and target_sess should use same opt
    Revert check_model_correctness

--- onnxruntime/test/python/quantization/op_test_utils.py
+++ onnxruntime/test/python/quantization/op_test_utils.py
@@ -85,24 +85,20 @@ def check_op_type_count(testcase, model_path, **kwargs):
 
 def check_model_correctness(testcase, model_path_origin, model_path_to_check, inputs, rtol=1e-2, atol=0.05):
     sess_options = onnxruntime.SessionOptions()
-    sess_options.graph_optimization_level = onnxruntime.GraphOptimizationLevel.ORT_ENABLE_ALL  # TODO: ENABLE_ALL?
-
+    sess_options.graph_optimization_level = onnxruntime.GraphOptimizationLevel.ORT_DISABLE_ALL
     origin_sess = onnxruntime.InferenceSession(
         model_path_origin, sess_options=sess_options, providers=["CPUExecutionProvider"]
     )
     origin_results = origin_sess.run([], inputs)
     # enable QDQ transformers
-    sess_options.graph_optimization_level = onnxruntime.GraphOptimizationLevel.ORT_ENABLE_EXTENDED
+    # sess_options.graph_optimization_level = onnxruntime.GraphOptimizationLevel.ORT_ENABLE_EXTENDED
     target_sess = onnxruntime.InferenceSession(
         model_path_to_check,
         sess_options=sess_options,
         providers=["CPUExecutionProvider"],
     )
-
     target_results = target_sess.run([], inputs)
     testcase.assertEqual(len(origin_results), len(target_results), "result count are different")
-    # np.set_printoptions(threshold=sys.maxsize)
-
     for idx, ref_output in enumerate(origin_results):
         output = target_results[idx]
         np.testing.assert_allclose(ref_output, output, rtol=rtol, atol=atol)
