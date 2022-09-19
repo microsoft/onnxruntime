471a66e29396d383796fa86037be07cff0ac4d42
Justin Chu <justinchuby@users.noreply.github.com>
2022-09-19 15:33:14 -0700

    Update pytorch_export_contrib_ops.py

--- onnxruntime/python/tools/pytorch_export_contrib_ops.py
+++ onnxruntime/python/tools/pytorch_export_contrib_ops.py
@@ -98,8 +98,11 @@ def unregister():
         try:
             torch.onnx.unregister_custom_op_symbolic(name, _OPSET_VERSION)
         except AttributeError:
+            # The symbolic_registry module was removed in PyTorch 1.13.
+            # We are importing it here for backwards compatibility
+            # because unregister_custom_op_symbolic is not available before PyTorch 1.12
             from torch.onnx import symbolic_registry
-            # unregister_custom_op_symbolic is not available before PyTorch 1.12
+
             namespace, kind = name.split("::")
             for version in symbolic_helper._onnx_stable_opsets:
                 if version >= _OPSET_VERSION and symbolic_registry.is_registered_op(kind, namespace, version):
