bbee533cd369d2de1decb06498d66bcb7755d21e
Wei-Sheng Chin <wschin@outlook.com>
2022-09-20 16:16:13 -0700

    Fix Deferred Release and Add New Test Framework for CUDA EP-specific Tests (#13016)
    Since CUDA EP became a shared library, most of internal functions are
    not accessible from `onnxruntime_test_all`, we need a new mechanism to
    write CUDA EP-specific tests. To this end, this PR introduces a general
    infra and an example test for deferred release in CUDA EP. When adding
    this test, we also found the current deferred release will cause error
    when pinned CPU buffer is not allocated by BFCArena, and this PR also
    makes a small fix (see changes in rocm_execution_provider.cc and
    cuda_execution_provider.cc).
    
    This PR also fixes a deferred release bug found by new tests.

--- onnxruntime/core/providers/cuda/cuda_execution_provider.cc
+++ onnxruntime/core/providers/cuda/cuda_execution_provider.cc
@@ -407,7 +407,19 @@ Status CUDAExecutionProvider::EnqueueDeferredRelease() {
     cpu_buffers_info->buffers = buffers;
     // Release the ownership of cpu_buffers_info so that the underlying
     // object will keep alive until the end of ReleaseCpuBufferCallback.
-    CUDA_RETURN_IF_ERROR(cudaLaunchHostFunc(stream, ReleaseCpuBufferCallback, cpu_buffers_info.release()));
+    if (cpu_buffers_info->allocator->Info().alloc_type == OrtArenaAllocator) {
+      // Release memory asynchronously to avoid blocking the compute stream.
+      CUDA_RETURN_IF_ERROR(cudaLaunchHostFunc(stream, ReleaseCpuBufferCallback, cpu_buffers_info.release()));
+    } else {
+      // Per
+      // https://docs.nvidia.com/cuda/cuda-c-programming-guide/index.html#implicit-synchronization
+      // cudaHostFree doesn't block stream, so a synchronitation is needed to make sure no kernels
+      // are using the host memory.
+      CUDA_RETURN_IF_ERROR(cudaStreamSynchronize(stream));
+      // cudaFreeHost and all other CUDA APIs cannot be called by cudaLaunchHostFunc per spec.
+      // So we just do synchrous release.
+      ReleaseCpuBufferCallback(cpu_buffers_info.release());
+    }
   }
   // All buffers are scheduled for release.
   // Let's clear releated information so that
