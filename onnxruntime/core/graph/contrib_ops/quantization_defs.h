d8010d7f141d1dc9c0f9a86e502da6e495d3cfac
ashari4 <70242157+ashari4@users.noreply.github.com>
2022-09-22 14:02:55 -0700

    QuantizeBFP and DequantizeBFP (#12833)
    * `QuantizeBFP` and `DequantizeBFP` schemas - similar to
    `QuantizeLinear` and `DeQuantizeLinear`.
    * BFP datatype is represented as a `uint8` tensor with shape and stride
    metadata. This is preferrable to adding a new datatype for BFP, which is
    more disruptive and [discouraged by
    PyTorch](https://discuss.pytorch.org/t/training-with-custom-quantized-datatype/152132/2).
    
    Context: 
    
    The Microsoft Floating Point (BFP) datatype shares an exponent for every
    n numbers called a “bounding box.” Each number still has its own
    mantissa and sign bits. BFP has been shown to incur 3-4 less cost
    (energy and area) than BFloat16 and INT8 counterparts without reductions
    in accuracy for the ImageNet benchmark as described in [Rouhani
    2020](https://proceedings.neurips.cc/paper/2020/file/747e32ab0fea7fbd2ad9ec03daa3f840-Paper.pdf).
    
    Requirements:
    
    * There are many variants of BFP (number of mantissa bits, number of
    shared exponent bits, size of bounding box, custom bit fields, etc.)
    * The size and layout of an BFP variant varies across hardware
    * bounding box can be over arbitrary dimensions; for example, for the
    channel "C" dimension in a N x C x H x W tensor for convolution
    
    Goals of this PR:
    
    * Add initial versions of QuantizeBFP and DequantizeBFP operators to
    enable QDQ-style quantization with BFP. Once the schemas stabilize, we
    can consider upstreaming to ONNX.
    * Add some basic type and shape inferencing tests; tests that run on an
    EP will be a follow-up.

--- onnxruntime/core/graph/contrib_ops/quantization_defs.h
+++ onnxruntime/core/graph/contrib_ops/quantization_defs.h
@@ -2,6 +2,8 @@
 // Licensed under the MIT License.
 #pragma once
 
+#include <stdexcept>
+
 #if !defined(ORT_MINIMAL_BUILD)
 #include "onnx/defs/schema.h"
 #else
@@ -13,12 +15,75 @@
 namespace onnxruntime {
 namespace contrib {
 
-void ValidateTypeAndShapeForScaleAndZP(
-    ONNX_NAMESPACE::InferenceContext& ctx,
-    int index,
-    ::google::protobuf::int32 expectedType,
-    bool isScalar,
-    int expectedTensorSize = 0);
+void ValidateTypeAndShapeForScaleAndZP(ONNX_NAMESPACE::InferenceContext& ctx, int index,
+                                       ::google::protobuf::int32 expectedType, bool isScalar,
+                                       int expectedTensorSize = 0);
 
+// Names follow the convention of BFP_<# sign bits>_<# mantissa bits>_<# exponent bits>_<size of bounding box>
+enum class BFPType : int64_t {
+  // 1 sign bit, 8 mantissa bits, 8 exponent bits, 16 numbers per bounding box.
+  BFP_1_8_8_16,
+
+  // 1 sign bit, 8 mantissa bits, 8 exponent bits, 16 numbers per bounding box.
+  BFP_1_4_8_16,
+
+  // No sign bit, 8 mantissa bits, 8 exponent bits, 64 numbers per bounding box. Mantissa and exponent are signed.
+  BFP_0_8_8_64,
+
+  // No sign bit, 8 mantissa bits, 8 exponent bits, 128 numbers per bounding box. Mantissa and exponent are signed.
+  BFP_0_8_8_128,
+
+  // No sign bit, 8 mantissa bits, 8 exponent bits, 256 numbers per bounding box. Mantissa and exponent are signed.
+  BFP_0_8_8_256,
+
+  // No sign bit, 4 mantissa bits, 8 exponent bits, 128 numbers per bounding box. Mantissa and exponent are signed.
+  BFP_0_4_8_128,
+
+  // No sign bit, 4 mantissa bits, 8 exponent bits, 256 numbers per bounding box. Mantissa and exponent are signed.
+  BFP_0_4_8_256,
+
+  // No sign bit, 16 mantissa bits, 8 exponent bits, 64 numbers per bounding box. Mantissa and exponent are signed.
+  BFP_0_16_8_64,
+
+  // No sign bit, 16 mantissa bits, 8 exponent bits, 128 numbers per bounding box. Mantissa and exponent are signed.
+  BFP_0_16_8_128,
+
+  // No sign bit, 16 mantissa bits, 8 exponent bits, 256 numbers per bounding box. Mantissa and exponent are signed.
+  BFP_0_16_8_256,
+
+  // No sign bit, 16 mantissa bits, 8 exponent bits, 1 number per bounding box. Mantissa and exponent are signed.
+  BFP_0_16_8_1,
+
+  // No sign bit, 24 mantissa bits, 8 exponent bits, 1 number per bounding box. Mantissa and exponent are signed.
+  BFP_0_24_8_1,
+
+  // Reserved for custom BFP types
+  Custom_BFP_0,
+  Custom_BFP_1
+};
+
+inline size_t get_bounding_box_size(const BFPType& bfp_type) {
+  switch (bfp_type) {
+    case BFPType::BFP_0_16_8_1:
+    case BFPType::BFP_0_24_8_1:
+      return 1u;
+    case BFPType::BFP_1_4_8_16:
+    case BFPType::BFP_1_8_8_16:
+      return 16u;
+    case BFPType::BFP_0_8_8_64:
+    case BFPType::BFP_0_16_8_64:
+      return 64u;
+    case BFPType::BFP_0_4_8_128:
+    case BFPType::BFP_0_8_8_128:
+    case BFPType::BFP_0_16_8_128:
+      return 128u;
+    case BFPType::BFP_0_4_8_256:
+    case BFPType::BFP_0_8_8_256:
+    case BFPType::BFP_0_16_8_256:
+      return 256u;
+    default:
+      ONNX_THROW_EX(std::invalid_argument("Unsuported bfp_type case."));
+  }
 }
+}  // namespace contrib
 }  // namespace onnxruntime
