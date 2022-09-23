c25e4d00484df05dbfa8f327756d92e852aa7b57
Hari Seshadri <hasesh@microsoft.com>
2022-09-22 17:54:55 -0700

    Add MatMul support

--- onnxruntime/core/providers/cuda/math/matmul.h
+++ onnxruntime/core/providers/cuda/math/matmul.h
@@ -4,6 +4,7 @@
 #pragma once
 
 #include "core/providers/cuda/cuda_kernel.h"
+#include "core/providers/cuda/math/gemm.h"
 
 namespace onnxruntime {
 namespace cuda {
@@ -18,7 +19,10 @@ class MatMul final : public CudaKernel {
         trans_A_{info.GetAttrOrDefault<int64_t>("transA", 0) != 0},
         trans_B_{info.GetAttrOrDefault<int64_t>("transB", 0) != 0},
         trans_batch_a_{info.GetAttrOrDefault<int64_t>("transBatchA", 0) != 0},
-        trans_batch_b_{info.GetAttrOrDefault<int64_t>("transBatchB", 0) != 0} {}
+        trans_batch_b_{info.GetAttrOrDefault<int64_t>("transBatchB", 0) != 0},
+        disable_cublaslt_matmul_{!std::is_same<T, MLFloat16>::value ||
+                                 ParseEnvironmentVariableWithDefault<bool>(matmul_detail::kDisableCublasLtMatmul, false)} {
+  }
 
   Status ComputeInternal(OpKernelContext* context) const override;
 
@@ -28,6 +32,7 @@ class MatMul final : public CudaKernel {
   const bool trans_B_;
   const bool trans_batch_a_;
   const bool trans_batch_b_;
+  const bool disable_cublaslt_matmul_;
 };
 }  // namespace cuda
 }  // namespace onnxruntime
