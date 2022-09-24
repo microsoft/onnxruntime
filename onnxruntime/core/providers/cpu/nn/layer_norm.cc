8e2528bad24fde3f5d3f90047408140be975f9f3
Scott McKay <skottmckay@gmail.com>
2022-09-24 13:22:44 +1000

    More LayoutNormalization opset 17 changes (#13066)
    ### Description
    Add CUDA kernel.
    Support double in CPU kernel and only write Mean and InvStdDev values if the optional outputs exist.
    
    ### Motivation and Context
    Complete opset 17 support for LayoutNormalization

--- onnxruntime/core/providers/cpu/nn/layer_norm.cc
+++ onnxruntime/core/providers/cpu/nn/layer_norm.cc
@@ -6,17 +6,14 @@
 #include "core/providers/common.h"
 
 namespace onnxruntime {
-// official onnx operator registration. originally LayerNormalization was a contrib op.
-// Only 2 type constraints (values using 'T' and 'U' in the contrib op all use 'T' in the ONNX spec)
-#define REGISTER_ONNX_KERNEL_TYPED(T)                                                        \
-  ONNX_CPU_OPERATOR_TYPED_KERNEL(LayerNormalization, 17, T,                                  \
-                                 KernelDefBuilder()                                          \
-                                     .TypeConstraint("T", DataTypeImpl::GetTensorType<T>())  \
-                                     .TypeConstraint("U", DataTypeImpl::GetTensorType<T>()), \
+#define REGISTER_ONNX_KERNEL_TYPED(T)                                                            \
+  ONNX_CPU_OPERATOR_TYPED_KERNEL(LayerNormalization, 17, T,                                      \
+                                 KernelDefBuilder()                                              \
+                                     .TypeConstraint("T", DataTypeImpl::GetTensorType<T>())      \
+                                     .TypeConstraint("U", DataTypeImpl::GetTensorType<float>()), \
                                  LayerNorm);
 
-// ONNX LayerNorm doesn't support 'double' for Mean/InvStdDev so we can only register a version with float
-// with our current implementation which originally handled 'double' and 'float' for the contrib op.
 REGISTER_ONNX_KERNEL_TYPED(float)
+REGISTER_ONNX_KERNEL_TYPED(double)
 
 }  // namespace onnxruntime
