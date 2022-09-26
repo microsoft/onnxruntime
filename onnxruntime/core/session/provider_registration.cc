9e65c0811c8eab8e69ce7b61d32d65da66f0e79c
adrianlizarraga <adlizarraga@microsoft.com>
2022-09-26 13:33:53 -0700

    Fix provider name check

--- onnxruntime/core/session/provider_registration.cc
+++ onnxruntime/core/session/provider_registration.cc
@@ -78,7 +78,7 @@ ORT_API_STATUS_IMPL(OrtApis::SessionOptionsAppendExecutionProvider,
 #else
     status = create_not_supported_status();
 #endif
-  } else if (strcmp(provider_name, "OpWrapper")) {
+  } else if (strcmp(provider_name, "OpWrapper") == 0) {
 #if defined(USE_OPWRAPPER)
     options->provider_factories.push_back(OpWrapperProviderFactoryCreator::Create(provider_options));
 #else
