a9a370f7627d945496a55297ec25518b63eb6d10
Peter Mcaughan <petermca@microsoft.com>
2022-09-27 16:22:17 -0700

    Simplify UT

--- onnxruntime/test/contrib_ops/attention_op_test.cc
+++ onnxruntime/test/contrib_ops/attention_op_test.cc
@@ -267,11 +267,7 @@ TEST(AttentionTest, AttentionBatch1WithQKVAttr1) {
   RunAttentionTest(input_data, weight_data, bias_data, mask_index_data, output_data,
                    batch_size, sequence_length, hidden_size, number_of_heads,
                    false, false, false, 0, nullptr, nullptr, kMaskIndexEnd, 0,
-                   0, false, true, qkv_sizes);
-  RunAttentionTest(input_data, weight_data, bias_data, mask_index_data, output_data,
-                   batch_size, sequence_length, hidden_size, number_of_heads,
-                   false, false, false, 0, nullptr, nullptr, kMaskIndexEnd, 0,
-                   0, true, false, qkv_sizes);
+                   0, false, false, qkv_sizes);
 }
 
 TEST(AttentionTest, AttentionBatch1WithQKVAttr2) {
@@ -308,11 +304,7 @@ TEST(AttentionTest, AttentionBatch1WithQKVAttr2) {
   RunAttentionTest(input_data, weight_data, bias_data, mask_index_data, output_data,
                    batch_size, sequence_length, hidden_size, number_of_heads,
                    false, false, false, 0, nullptr, nullptr, kMaskIndexEnd, 0,
-                   0, false, true, qkv_sizes);
-  RunAttentionTest(input_data, weight_data, bias_data, mask_index_data, output_data,
-                   batch_size, sequence_length, hidden_size, number_of_heads,
-                   false, false, false, 0, nullptr, nullptr, kMaskIndexEnd, 0,
-                   0, true, false, qkv_sizes);
+                   0, false, false, qkv_sizes);
 }
 
 TEST(AttentionTest, AttentionBatch1ExtraAdd) {
