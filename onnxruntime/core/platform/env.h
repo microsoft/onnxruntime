5450d56a194b5b2ffef403cda52bb438a1defda8
Randy Shuai <rashuai@microsoft.com>
2022-09-28 16:09:39 -0700

    resolve conflict

--- onnxruntime/core/platform/env.h
+++ onnxruntime/core/platform/env.h
@@ -34,10 +34,13 @@ limitations under the License.
 #include <sys/types.h>
 #include <unistd.h>
 #endif
+
 namespace Eigen {
 class ThreadPoolInterface;
 }
 namespace onnxruntime {
+
+using GroupAffinity = std::vector<std::pair<int64_t,int64_t>>;
 
 #ifdef _WIN32
 using PIDType = unsigned long;
@@ -79,7 +82,15 @@ struct ThreadOptions {
   void* custom_thread_creation_options = nullptr;
   OrtCustomJoinThreadFn custom_join_thread_fn = nullptr;
   int dynamic_block_base_ = 0;
+
+  GroupAffinity group_affinities;
 };
+
+struct ProcessorGroup {
+  int64_t processor_count = 0;
+  int64_t processor_mask = 0;
+};
+
 /// \brief An interface used by the onnxruntime implementation to
 /// access operating system functionality like the filesystem etc.
 ///
--- onnxruntime/core/util/thread_utils.cc
+++ onnxruntime/core/util/thread_utils.cc
@@ -4,6 +4,7 @@
 #include "core/util/thread_utils.h"
 
 #include <algorithm>
+#include <iostream>
 
 #ifdef _WIN32
 #include <Windows.h>
@@ -12,6 +13,33 @@
 #include "core/session/ort_apis.h"
 
 namespace onnxruntime {
+
+#ifdef _WIN32
+GroupAffinity GetGroupAffinities() {
+  GroupAffinity group_affinities;
+  LOGICAL_PROCESSOR_RELATIONSHIP relation = RelationGroup;
+  SYSTEM_LOGICAL_PROCESSOR_INFORMATION_EX processorInfos[128];
+  DWORD returnLength = 128;
+  WORD group_id = 0;
+  ORT_ENFORCE(GetLogicalProcessorInformationEx(relation, processorInfos, &returnLength),
+              "Failed to fetch processor info, error code: ", GetLastError());
+  auto numGroups = returnLength / sizeof(SYSTEM_LOGICAL_PROCESSOR_INFORMATION_EX);
+  for (int64_t i = 0; i < static_cast<int64_t>(numGroups); ++i) {
+    ORT_ENFORCE(processorInfos[i].Relationship == RelationGroup, "Returned processors not divided by group");
+    for (int64_t j = 0; j < static_cast<int>(processorInfos[i].Group.ActiveGroupCount); ++j) {
+      const auto& groupInfo = processorInfos[i].Group.GroupInfo[j];
+      KAFFINITY processor_affinity = 1UL;
+      for (int64_t k = 0; k < groupInfo.ActiveProcessorCount; ++k) {
+        group_affinities.push_back({static_cast<int64_t>(group_id), static_cast<int64_t>(processor_affinity)});
+        processor_affinity <<= 1;
+      }
+      group_id++;
+    }
+  }
+  return std::move(group_affinities);
+}
+#endif
+
 namespace concurrency {
 static std::unique_ptr<ThreadPool>
 CreateThreadPoolHelper(Env* env, OrtThreadPoolParams options) {
@@ -23,12 +51,22 @@ CreateThreadPoolHelper(Env* env, OrtThreadPoolParams options) {
     to.affinity.assign(options.affinity_vec, options.affinity_vec + options.affinity_vec_len);
   }
   if (options.thread_pool_size <= 0) {  // default
+#ifdef _WIN32
+    to.group_affinities = GetGroupAffinities();
+    options.thread_pool_size = static_cast<int>(to.group_affinities.size());
+    std::cout << "thread_pool size: " << options.thread_pool_size << std::endl;
+    int tid = 0;
+    for (const auto& affinity : to.group_affinities) {
+      std::cout << "thread " << tid++ << ", group " << affinity.first << ", core " << affinity.second << std::endl;
+    }
+#else
     cpu_list = Env::Default().GetThreadAffinityMasks();
     if (cpu_list.empty() || cpu_list.size() == 1)
       return nullptr;
     options.thread_pool_size = static_cast<int>(cpu_list.size());
     if (options.auto_set_affinity)
       to.affinity = cpu_list;
+#endif
   }
   to.set_denormal_as_zero = options.set_denormal_as_zero;
 
