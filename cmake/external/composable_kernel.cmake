6b0b911be53c4d37943bd228a230d3ee071c29ea
Guangyun Han <guangyunhan@microsoft.com>
2022-09-30 15:15:54 +0000

    Switch to use CMake's HIP LANGUAGE support

--- cmake/external/composable_kernel.cmake
+++ cmake/external/composable_kernel.cmake
@@ -1,7 +1,6 @@
 set(composable_kernel_URL https://github.com/ROCmSoftwarePlatform/composable_kernel.git)
 set(composable_kernel_TAG e1a3fff67510be2af023b31587e411230b994631) # 2022-08-25 07:43:43 +0800
 
-set(BUILD_DEV OFF)
 set(PATCH ${PROJECT_SOURCE_DIR}/patches/composable_kernel/Fix_Clang_Build.patch)
 
 include(FetchContent)
@@ -14,6 +13,7 @@ FetchContent_Declare(composable_kernel
 FetchContent_GetProperties(composable_kernel)
 if(NOT composable_kernel_POPULATED)
   FetchContent_Populate(composable_kernel)
+  set(BUILD_DEV OFF CACHE BOOL "Disable -Weverything, otherwise, error: 'constexpr' specifier is incompatible with C++98 [-Werror,-Wc++98-compat]" FORCE)
   add_subdirectory(${composable_kernel_SOURCE_DIR} ${composable_kernel_BINARY_DIR} EXCLUDE_FROM_ALL)
 
   add_library(onnxruntime_composable_kernel_includes INTERFACE)
