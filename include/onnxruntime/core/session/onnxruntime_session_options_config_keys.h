b4809036b3cf4a4e9923d9b1b832456421b9948c
Randy Shuai <rashuai@microsoft.com>
2022-10-04 16:24:18 -0700

    allow setting of group affinity for windows thread

--- include/onnxruntime/core/session/onnxruntime_session_options_config_keys.h
+++ include/onnxruntime/core/session/onnxruntime_session_options_config_keys.h
@@ -46,3 +46,10 @@ static const char* const kOrtSessionOptionsConfigSetDenormalAsZero = "session.se
 // "1": enable. ORT does fusion logic for QDQ format.
 // Its default value is "1"
 static const char* const kOrtSessionOptionsEnableQuantQDQ = "session.enable_quant_qdq";
+
+// string consist of pairs of "uint64,uint64", e.g. "uint64,uint64;uint64,uint64;uint64,uint64;..."
+// 1.For each pair, the first uint64 stands for processor group, the second represents processor mask.
+//   For example, if want to let a thread run on the first two processors of the second processor group, the pair is "1,3";
+//   to let it run on the forth processor of the first group, the pair is "0,8".
+// 2. The number of pairs must equal to intra_op_num_threads MINUS ONE, since we will not set affinity on the main thread.
+static const char* const kOrtSessionOptionsConfigThreadGroupAffinities = "session.thread_group_affinities";
