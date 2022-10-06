0b00d342026bfc615bd4d11a3842ca2b97799407
Prathik Rao <prathikrao@microsoft.com>
2022-10-06 13:02:33 -0700

    register cos grad impl

--- orttraining/orttraining/training_ops/cuda/math/cos_grad_impl.h
+++ orttraining/orttraining/training_ops/cuda/math/cos_grad_impl.h
@@ -9,8 +9,21 @@
 namespace onnxruntime {
 namespace cuda {
 
-template <typename T>
-Status CosGradImpl(cudaStream_t stream, const T* dy, const T* Y, T* output, size_t N);
+typedef onnxruntime::cuda::CtxNull CtxCosGrad;
+
+#define BINARY_ELEMENTWISE_IMPL_DECLARATION() \
+  template <typename T>                           \
+  void Impl_CosGrad(cudaStream_t stream,           \
+                   const T* lhs_data,             \
+                   const T* rhs_data,             \
+                   T* output_data,                \
+                   const CtxCosGrad* func_ctx,     \
+                   size_t count)
+
+BINARY_ELEMENTWISE_IMPL_DECLARATION();
+
+// template <typename T>
+// Status CosGradImpl(cudaStream_t stream, const T* dy, const T* Y, T* output, size_t N);
 
 }
 }  // namespace onnxruntime
