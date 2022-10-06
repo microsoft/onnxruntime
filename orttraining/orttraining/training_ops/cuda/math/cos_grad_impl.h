7710714eb15d0bbeb3b0fe947daf881346a85faa
Prathik Rao <prathikrao@microsoft.com>
2022-10-06 12:47:31 -0700

    fix typo

--- orttraining/orttraining/training_ops/cuda/math/cos_grad_impl.h
+++ orttraining/orttraining/training_ops/cuda/math/cos_grad_impl.h
@@ -9,7 +9,7 @@ namespace onnxruntime {
 namespace cuda {
 
 template <typename T>
-Status CosGradImpl(cudaStream_t stream, const T* dy, const T* Y, T* output, size_t N);
+Status Impl_CosGrad(cudaStream_t stream, const T* dy, const T* Y, T* output, size_t N);
 
 }
 }  // namespace onnxruntime
