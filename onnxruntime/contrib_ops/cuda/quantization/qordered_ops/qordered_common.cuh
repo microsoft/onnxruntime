microsoft/onnxruntime

commit 45c60c7fef82b0de075fca6c4d80fed5a0005930

Add more qordered int8 operators for CUDA provider (#12949)
Attention, Quantize/Dequantize etc.
Update QOrderedMatmul's schema, updated unittest.
Verified test data for QOrdered Attention.

Co-authored-by: Zhang Lei <phill.zhang@gmail.com>
Co-authored-by: Lei Zhang <zhalei@microsoft.com>

onnxruntime/contrib_ops/cuda/quantization/qordered_ops/qordered_common.cuh
@@ -1,17 +1,13 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+#pragma once
+
 #include "core/providers/cuda/cu_inc/common.cuh"
 
 namespace onnxruntime {
 namespace contrib {
 namespace cuda {
-
-using namespace onnxruntime::cuda;
-
-__device__ inline float ToFloat(const __half h) { return __half2float(h); }
-
-__device__ inline float ToFloat(const float f) { return f; }
 
 template <typename T>
 __inline__ __device__ T
@@ -22,6 +18,97 @@ WarpReduceSum(T val) {
   val += __shfl_xor_sync(0xFFFFFFFF, val, 8);
   val += __shfl_xor_sync(0xFFFFFFFF, val, 16);
   return val;
+}
+
+template <typename T>
+__inline__ __device__ T
+WarpReduceMax(T val) {
+  val = max(val, __shfl_xor_sync(0xFFFFFFFF, val, 1));
+  val = max(val, __shfl_xor_sync(0xFFFFFFFF, val, 2));
+  val = max(val, __shfl_xor_sync(0xFFFFFFFF, val, 4));
+  val = max(val, __shfl_xor_sync(0xFFFFFFFF, val, 8));
+  val = max(val, __shfl_xor_sync(0xFFFFFFFF, val, 16));
+  return val;
+}
+
+__device__ inline int8_t QuantizeFloatS8(const float val, const float rscale) {
+  float dqval = fmaxf(fminf(127.0f, val * rscale), -128.0f);
+  return static_cast<int8_t>(__float2int_rn(dqval));
+}
+
+// TODO: Const Ref ?
+__device__ inline char4 QuantizeFloat4Char4(const float4 val4, const float rscale) {
+  return char4{QuantizeFloatS8(val4.x, rscale), QuantizeFloatS8(val4.y, rscale),
+               QuantizeFloatS8(val4.z, rscale), QuantizeFloatS8(val4.w, rscale)};
+}
+
+// TODO: Const Ref ?
+__device__ inline int32_t Dp4a_Defined(const char4 input_1, const char4 input_2) {
+  return static_cast<int32_t>(input_1.x) * static_cast<int32_t>(input_2.x) +
+         static_cast<int32_t>(input_1.y) * static_cast<int32_t>(input_2.y) +
+         static_cast<int32_t>(input_1.z) * static_cast<int32_t>(input_2.z) +
+         static_cast<int32_t>(input_1.w) * static_cast<int32_t>(input_2.w);
+}
+
+struct __half4 {
+  __half2 xy;
+  __half2 zw;
+};
+
+union U1S2 {
+  unsigned u1;
+  short2 s2;
+};
+
+__device__ inline __half2 hmul2bk(const __half2 a, const __half2 b) {
+  #if !defined(__CUDA_ARCH__) || __CUDA_ARCH__ >= 530
+    return __hmul2(a, b);
+  #else
+    return __half2{(half)((float)a.x * (float)b.x), (half)((float)a.y * (float)b.y)};
+  #endif
+}
+
+__device__ inline char2 QuantizeHalf2Char2(const __half2 xy, const __half2 inverse_scale2) {
+  __half2 scaled_xy = hmul2bk(xy, inverse_scale2);
+  U1S2 s2xy;
+  s2xy.s2.x = __half2short_rn(scaled_xy.x);
+  s2xy.s2.y = __half2short_rn(scaled_xy.y);
+  s2xy.u1 = __vmaxs2(__vmins2(s2xy.u1, 0x007F007F), 0xFF80FF80);
+  return char2{(char)s2xy.s2.x, (char)s2xy.s2.y};
+}
+
+__device__ inline char4 QuantizeHalf4Char4(const __half4 val4, const __half2 inverse_scale2) {
+  __half2 val4_xy = hmul2bk(val4.xy, inverse_scale2);
+  __half2 val4_zw = hmul2bk(val4.zw, inverse_scale2);
+  U1S2 shortxy, shortzw;
+  shortxy.s2.x = __half2short_rn(__low2half(val4_xy));
+  shortzw.s2.x = __half2short_rn(__low2half(val4_zw));
+  shortxy.s2.y = __half2short_rn(__high2half(val4_xy));
+  shortzw.s2.y = __half2short_rn(__high2half(val4_zw));
+  shortxy.u1 = __vmaxs2(__vmins2(shortxy.u1, 0x007F007F), 0xFF80FF80);
+  shortzw.u1 = __vmaxs2(__vmins2(shortzw.u1, 0x007F007F), 0xFF80FF80);
+  return char4{(char)shortxy.s2.x, (char)shortxy.s2.y, (char)shortzw.s2.x, (char)shortzw.s2.y};
+}
+
+__device__ inline char4 QuantizeHalf4Char4Strict(const __half4 val4, const float inverse_scale) {
+  U1S2 shortxy, shortzw;
+  shortxy.s2.x = static_cast<short>(__float2int_rn(__half2float(val4.xy.x) * inverse_scale));
+  shortxy.s2.y = static_cast<short>(__float2int_rn(__half2float(val4.xy.y) * inverse_scale));
+  shortzw.s2.x = static_cast<short>(__float2int_rn(__half2float(val4.zw.x) * inverse_scale));
+  shortzw.s2.y = static_cast<short>(__float2int_rn(__half2float(val4.zw.y) * inverse_scale));
+  shortxy.u1 = __vmaxs2(__vmins2(shortxy.u1, 0x007F007F), 0xFF80FF80);
+  shortzw.u1 = __vmaxs2(__vmins2(shortzw.u1, 0x007F007F), 0xFF80FF80);
+  return char4{(char)shortxy.s2.x, (char)shortxy.s2.y, (char)shortzw.s2.x, (char)shortzw.s2.y};
+}
+
+__device__ inline __half4 DeqantizeChar4Half4(const char4 ch4, const __half2 scale2) {
+  return {hmul2bk(scale2, __half2(__short2half_rn(ch4.x), __short2half_rn(ch4.y))),
+          hmul2bk(scale2, __half2(__short2half_rn(ch4.z), __short2half_rn(ch4.w)))};
+}
+
+__device__ inline __half4 DeqantizeChar4Half4Strict(const char4 ch4, const float scale) {
+  return __half4{{__float2half_rn(scale * ch4.x), __float2half_rn(scale * ch4.y)},
+                 {__float2half_rn(scale * ch4.z), __float2half_rn(scale * ch4.w)}};
 }
 
 }  // namespace cuda

onnxruntime/contrib_ops/cuda/bert/longformer_attention_impl.cu
@@ -869,16 +869,26 @@ Status LongformerQkvToContext(
   //  Q, K, V, Global_K, Global_V, Global_Q (format 0)
   //  Q, K, V, Global_Q, Global_K, Global_V (format 1)
   if (format == 1 || max_num_global == 0 || nullptr == global_input) {
-    LaunchAddBiasTranspose(stream, 3, format, max_threads_per_block, batch_size,
-                           sequence_length, num_heads, head_size,
-                           input, bias, qkv,
-                           use_half4);
-
-    if (max_num_global > 0 && nullptr != global_input) {
+    if (bias == nullptr) {
+      ORT_RETURN_IF_ERROR(LaunchTransQkv(stream, 3, sequence_length, batch_size, head_size, num_heads,
+                                         max_threads_per_block, false, input, qkv));
+    } else {
       LaunchAddBiasTranspose(stream, 3, format, max_threads_per_block, batch_size,
                              sequence_length, num_heads, head_size,
-                             global_input, global_bias, qkv + 3 * elements,
+                             input, bias, qkv,
                              use_half4);
+    }
+
+    if (max_num_global > 0 && nullptr != global_input) {
+      if (global_bias == nullptr) {
+        ORT_RETURN_IF_ERROR(LaunchTransQkv(stream, 3, sequence_length, batch_size, head_size, num_heads,
+                                           max_threads_per_block, false, global_input, qkv + 3 * elements));
+      } else {
+        LaunchAddBiasTranspose(stream, 3, format, max_threads_per_block, batch_size,
+                               sequence_length, num_heads, head_size,
+                               global_input, global_bias, qkv + 3 * elements,
+                               use_half4);
+      }
     }
   } else {
     LaunchAddBiasTranspose(stream, 5, format, max_threads_per_block, batch_size,

onnxruntime/contrib_ops/cuda/cuda_contrib_kernels.cc
@@ -100,6 +100,10 @@ class ONNX_OPERATOR_TYPED_KERNEL_CLASS_NAME(kCudaExecutionProvider, kOnnxDomain,
 class ONNX_OPERATOR_KERNEL_CLASS_NAME(kCudaExecutionProvider, kMSDomain, 1, QOrderedMatMul);
 class ONNX_OPERATOR_KERNEL_CLASS_NAME(kCudaExecutionProvider, kMSDomain, 1, QOrderedLayerNormalization);
 class ONNX_OPERATOR_KERNEL_CLASS_NAME(kCudaExecutionProvider, kMSDomain, 1, QOrderedGelu);
+class ONNX_OPERATOR_KERNEL_CLASS_NAME(kCudaExecutionProvider, kMSDomain, 1, QuantizeWithOrder);
+class ONNX_OPERATOR_KERNEL_CLASS_NAME(kCudaExecutionProvider, kMSDomain, 1, DequantizeWithOrder);
+class ONNX_OPERATOR_KERNEL_CLASS_NAME(kCudaExecutionProvider, kMSDomain, 1, QOrderedAttention);
+class ONNX_OPERATOR_KERNEL_CLASS_NAME(kCudaExecutionProvider, kMSDomain, 1, QOrderedLongformerAttention);
 
 #ifdef ENABLE_ATEN
 class ONNX_OPERATOR_KERNEL_CLASS_NAME(kCudaExecutionProvider, kPytorchAtenDomain, 1, ATen);
@@ -207,6 +211,10 @@ Status RegisterCudaContribKernels(KernelRegistry& kernel_registry) {
     BuildKernelCreateInfo<ONNX_OPERATOR_KERNEL_CLASS_NAME(kCudaExecutionProvider, kMSDomain, 1, QOrderedMatMul)>,
     BuildKernelCreateInfo<ONNX_OPERATOR_KERNEL_CLASS_NAME(kCudaExecutionProvider, kMSDomain, 1, QOrderedLayerNormalization)>,
     BuildKernelCreateInfo<ONNX_OPERATOR_KERNEL_CLASS_NAME(kCudaExecutionProvider, kMSDomain, 1, QOrderedGelu)>,
+    BuildKernelCreateInfo<ONNX_OPERATOR_KERNEL_CLASS_NAME(kCudaExecutionProvider, kMSDomain, 1, QuantizeWithOrder)>,
+    BuildKernelCreateInfo<ONNX_OPERATOR_KERNEL_CLASS_NAME(kCudaExecutionProvider, kMSDomain, 1, DequantizeWithOrder)>,
+    BuildKernelCreateInfo<ONNX_OPERATOR_KERNEL_CLASS_NAME(kCudaExecutionProvider, kMSDomain, 1, QOrderedAttention)>,
+    BuildKernelCreateInfo<ONNX_OPERATOR_KERNEL_CLASS_NAME(kCudaExecutionProvider, kMSDomain, 1, QOrderedLongformerAttention)>,
 
 #ifdef ENABLE_ATEN
     BuildKernelCreateInfo<ONNX_OPERATOR_KERNEL_CLASS_NAME(kCudaExecutionProvider, kPytorchAtenDomain, 1, ATen)>,

commit 074e009e69b8e640e6f8947d67f2206f7a291e70

fix jetson build break caused by https://github.com/microsoft/onnxruntime/pull/12949 (#13310)
https://github.com/microsoft/onnxruntime/pull/12949 breaks Jetson/ARM
builds due to
invalid narrowing conversion from "char" to "signed char". (on ARM, char
is unsigned)
This was reported by
https://github.com/microsoft/onnxruntime/issues/13285#issuecomment-1276122505

onnxruntime/contrib_ops/cuda/quantization/qordered_ops/qordered_common.cuh
@@ -74,7 +74,7 @@ __device__ inline char2 QuantizeHalf2Char2(const __half2 xy, const __half2 inver
   s2xy.s2.x = __half2short_rn(scaled_xy.x);
   s2xy.s2.y = __half2short_rn(scaled_xy.y);
   s2xy.u1 = __vmaxs2(__vmins2(s2xy.u1, 0x007F007F), 0xFF80FF80);
-  return char2{(char)s2xy.s2.x, (char)s2xy.s2.y};
+  return char2{(signed char)s2xy.s2.x, (signed char)s2xy.s2.y};
 }
 
 __device__ inline char4 QuantizeHalf4Char4(const __half4 val4, const __half2 inverse_scale2) {
@@ -87,7 +87,7 @@ __device__ inline char4 QuantizeHalf4Char4(const __half4 val4, const __half2 inv
   shortzw.s2.y = __half2short_rn(__high2half(val4_zw));
   shortxy.u1 = __vmaxs2(__vmins2(shortxy.u1, 0x007F007F), 0xFF80FF80);
   shortzw.u1 = __vmaxs2(__vmins2(shortzw.u1, 0x007F007F), 0xFF80FF80);
-  return char4{(char)shortxy.s2.x, (char)shortxy.s2.y, (char)shortzw.s2.x, (char)shortzw.s2.y};
+  return char4{(signed char)shortxy.s2.x, (signed char)shortxy.s2.y, (signed char)shortzw.s2.x, (signed char)shortzw.s2.y};
 }
 
 __device__ inline char4 QuantizeHalf4Char4Strict(const __half4 val4, const float inverse_scale) {
@@ -98,7 +98,7 @@ __device__ inline char4 QuantizeHalf4Char4Strict(const __half4 val4, const float
   shortzw.s2.y = static_cast<short>(__float2int_rn(__half2float(val4.zw.y) * inverse_scale));
   shortxy.u1 = __vmaxs2(__vmins2(shortxy.u1, 0x007F007F), 0xFF80FF80);
   shortzw.u1 = __vmaxs2(__vmins2(shortzw.u1, 0x007F007F), 0xFF80FF80);
-  return char4{(char)shortxy.s2.x, (char)shortxy.s2.y, (char)shortzw.s2.x, (char)shortzw.s2.y};
+  return char4{(signed char)shortxy.s2.x, (signed char)shortxy.s2.y, (signed char)shortzw.s2.x, (signed char)shortzw.s2.y};
 }
 
 __device__ inline __half4 DeqantizeChar4Half4(const char4 ch4, const __half2 scale2) {

onnxruntime/contrib_ops/cuda/quantization/qordered_ops/qordered_qdq_impl.cu
@@ -42,7 +42,7 @@ struct DequantizeVec<float2> {
   static __device__ inline QuantizedVecT Quantize(const float2 fpvals, const float inv_scale) {
     float dqvalx = fmaxf(fminf(127.0f, fpvals.x * inv_scale), -128.0f);
     float dqvaly = fmaxf(fminf(127.0f, fpvals.y * inv_scale), -128.0f);
-    return char2{static_cast<char>(__float2int_rn(dqvalx)), static_cast<char>(__float2int_rn(dqvaly))};
+    return char2{static_cast<signed char>(__float2int_rn(dqvalx)), static_cast<signed char>(__float2int_rn(dqvaly))};
   }
 
   static __device__ inline float2 Dequantize(const QuantizedVecT qvals, const float scale) {
@@ -76,7 +76,7 @@ struct DequantizeVec<__half2> {
     s2xy.s2.x = __half2short_rn(xy.x);
     s2xy.s2.y = __half2short_rn(xy.y);
     s2xy.u1 = __vmaxs2(__vmins2(s2xy.u1, 0x007F007F), 0xFF80FF80);
-    return char2{(char)s2xy.s2.x, (char)s2xy.s2.y};
+    return char2{(signed char)s2xy.s2.x, (signed char)s2xy.s2.y};
   }
 
   static __device__ inline __half2 Dequantize(const QuantizedVecT qvals, const __half scale) {