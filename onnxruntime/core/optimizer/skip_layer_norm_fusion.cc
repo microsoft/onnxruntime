037b07fe08ab9e5837e0343fd94bcc9941c338b2
peixuanzuo <peixuanzuo@linmif39a000004.zvflicr54joexhdgnhvmxrxygg.phxx.internal.cloudapp.net>
2022-10-18 08:54:36 +0000

    add comment

--- onnxruntime/core/optimizer/skip_layer_norm_fusion.cc
+++ onnxruntime/core/optimizer/skip_layer_norm_fusion.cc
@@ -48,7 +48,7 @@ static bool CheckFirstAdd(Node& add, ProviderType providertype) {
     if (!utils::HasDimValue(add_input1_shape->dim(i)) ||
         !utils::HasDimValue(add_input2_shape->dim(i)) ||
         add_input1_shape->dim(i).dim_value() != add_input2_shape->dim(i).dim_value()) {
-      // dim0 is batch_size, which could be only have dim_param.
+      // Allow dim0 only has dim_param(e.g., batch_size).
       if (i == 0) {
         if (!utils::HasDimParam(add_input1_shape->dim(i)) ||
             !utils::HasDimParam(add_input2_shape->dim(i)) ||
