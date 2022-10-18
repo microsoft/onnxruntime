aae96c73008070026316b48d57db25cc5023e7eb
Prathik Rao <prathikrao@microsoft.com>
2022-10-17 18:08:10 -0700

    register resize as max_pool properly

--- orttraining/orttraining/python/training/ortmodule/_custom_op_symbolic_registry.py
+++ orttraining/orttraining/python/training/ortmodule/_custom_op_symbolic_registry.py
@@ -112,6 +112,23 @@ def multinomial(g, self, num_samples, replacement=False, generator=None):
         raise RuntimeError("Unsupported: ONNX does not support generator for multinomial")
     return g.op("org.pytorch.aten::ATen", self, num_samples, replacement, generator, operator_s="multinomial")
 
+@register_symbolic("resize")
+def resize(g, self, kernel_size, stride, padding, dilation, ceil_mode):
+    stride_val = sym_help._maybe_get_const(stride, "is")
+    if not stride_val:
+        stride = kernel_size
+    return g.op(
+        "org.pytorch.aten::ATen",
+        self,
+        kernel_size,
+        stride,
+        padding,
+        dilation,
+        ceil_mode,
+        operator_s="max_pool2d_with_indices",
+        outputs=2,
+    )[0]
+
 
 @register_symbolic("max_pool2d")
 def max_pool2d(g, self, kernel_size, stride, padding, dilation, ceil_mode):
