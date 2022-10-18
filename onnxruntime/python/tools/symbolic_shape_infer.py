bab4f3e44ae301bf673898599cc12d5226d5c641
Vincent Wang <wangwchpku@outlook.com>
2022-10-18 18:14:44 +0800

    bugfix

--- onnxruntime/python/tools/symbolic_shape_infer.py
+++ onnxruntime/python/tools/symbolic_shape_infer.py
@@ -1354,6 +1354,23 @@ class SymbolicShapeInference:
 
     def _infer_aten_group_norm(self, node):
         self._propagate_shape_and_type(node)
+        input_shape = self._get_shape(node, 0)
+        N = input_shape[0] if input_shape is not None and len(input_shape) != 0 else None
+        group = self._try_get_value(node, 6)
+        output_dtype = self.known_vi_[node.input[0]].type.tensor_type.elem_type
+        for i in [1, 2]:
+            if node.output[i]:
+                vi = self.known_vi_[node.output[i]]
+                vi.CopyFrom(
+                    helper.make_tensor_value_info(
+                        node.output[i],
+                        output_dtype,
+                        [
+                            N if N is not None else self._new_symbolic_dim_from_output(node, i, 0),
+                            as_scalar(group) if group is not None else self._new_symbolic_dim_from_output(node, i, 1),
+                        ],
+                    )
+                )
 
     def _infer_BatchNormalization(self, node):
         self._propagate_shape_and_type(node)
