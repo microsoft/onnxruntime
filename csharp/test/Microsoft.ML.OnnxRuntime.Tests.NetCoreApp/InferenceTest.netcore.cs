dc662b33a4fdddee03592042f2110a3af681df32
Yi Zhang <zhanyi@microsoft.com>
2022-10-21 13:15:18 +0800

    ignore int8 and fp16 in non-GPU operator

--- csharp/test/Microsoft.ML.OnnxRuntime.Tests.NetCoreApp/InferenceTest.netcore.cs
+++ csharp/test/Microsoft.ML.OnnxRuntime.Tests.NetCoreApp/InferenceTest.netcore.cs
@@ -425,7 +425,11 @@ namespace Microsoft.ML.OnnxRuntime.Tests
                 //var modelRoot = new DirectoryInfo(Path.Combine(modelsDir, opsetDir.Name));
                 foreach (var modelDir in opsetDir.EnumerateDirectories())
                 {
+#if USE_CUDA
                     if (!skipModels.ContainsKey(modelDir.Name))
+#else
+                    if (!(skipModels.ContainsKey(modelDir.Name) || modelDir.Name.Contains("int8", StringComparison.OrdinalIgnoreCase) || modelDir.Name.Contains("fp16", StringComparison.OrdinalIgnoreCase)))
+#endif
                     {
                         yield return new object[] { modelDir.Parent.FullName, modelDir.Name };
                     }
@@ -443,7 +447,11 @@ namespace Microsoft.ML.OnnxRuntime.Tests
             {
                 foreach (var modelDir in opsetDir.EnumerateDirectories())
                 {
+#if USE_CUDA
                     if (skipModels.ContainsKey(modelDir.Name))
+#else
+                    if (skipModels.ContainsKey(modelDir.Name) || modelDir.Name.Contains("int8", StringComparison.OrdinalIgnoreCase) || modelDir.Name.Contains("fp16", StringComparison.OrdinalIgnoreCase))
+#endif
                     {
                         //Console.WriteLine("Model {0} is skipped due to the error: {1}", modelDir.FullName, skipModels[modelDir.Name]);
                         yield return new object[] { modelDir.Parent.FullName, modelDir.Name };
