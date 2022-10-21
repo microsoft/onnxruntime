7b9736acbd49a293f2ec2f90d2b4f418b37bb6a7
Dulguun Bold <bolddulguun2@gmail.com>
2022-10-21 09:58:59 +0200

    Clone specific dldt version
    Rel-1.0.0 build fails due to it clones wrong version dldt branch

--- tools/ci_build/github/linux/docker/scripts/install_openvino.sh
+++ tools/ci_build/github/linux/docker/scripts/install_openvino.sh
@@ -8,7 +8,7 @@ esac
 done
 
 OPENVINO_VERSION=${OPENVINO_VERSION:=2019_R1.1}
-git clone https://github.com/opencv/dldt.git /data/dldt/openvino_2019.1.144
+git clone --branch releases/2019/3 https://github.com/opencv/dldt.git /data/dldt/openvino_2019.1.144
 
 export INTEL_CVSDK_DIR=/data/dldt/openvino_2019.1.144
 apt-get update && apt-get -y  install libusb-1.0-0-dev
