236796f5e30d553afe11a5727733b2054ab31979
Yi Zhang <zhanyi@microsoft.com>
2022-10-22 16:39:56 +0800

    skip some models in windows CPU ctest

--- winml/test/model/skip_model_tests.h
+++ winml/test/model/skip_model_tests.h
@@ -124,6 +124,22 @@ std::unordered_map<std::string, std::string> disabledTests(
      {"fp16_tiny_yolov2_opset8_GPU", "Bug 31005780: Result of fp16_test_tiny_yolov2_opset7 and fp16_coreml_FNS_Candy_opset7 models on DirectML aren't as accurate as on CPU https://microsoft.visualstudio.com/OS/_workitems/edit/31005780"},
      {"fp16_coreml_FNS_Candy_opset7_GPU", "Bug 31005780: Result of fp16_test_tiny_yolov2_opset7 and fp16_coreml_FNS_Candy_opset7 models on DirectML aren't as accurate as on CPU https://microsoft.visualstudio.com/OS/_workitems/edit/31005780"},
      {"mlperf_ssd_mobilenet_300_opset10_GPU", "Bug 31005624: mlperf_ssd_mobilenet_300 opset 10 model fails to evaluate in DirectML https://microsoft.visualstudio.com/OS/_workitems/edit/31005624"},
+
+     // from model zoo with source name
+     {"VGG_16_int8_opset12_zoo_CPU", disabledTestDefaultReason},
+     {"SSD_int8_opset12_zoo_CPU", disabledTestDefaultReason},
+     {"ShuffleNet_v2_int8_opset12_zoo_CPU", disabledTestDefaultReason},
+     {"ResNet50_int8_opset12_zoo_CPU", disabledTestDefaultReason},
+     {"ResNet50_qdq_opset12_zoo_CPU", disabledTestDefaultReason},
+     {"MobileNet_v2_1_0_qdq_opset12_zoo_CPU", disabledTestDefaultReason},
+     {"MobileNet_v2_1_0_int8_opset12_zoo_CPU", disabledTestDefaultReason},
+     {"Inception_1_int8_opset12_zoo_CPU", disabledTestDefaultReason},
+     {"Faster_R_CNN_R_50_FPN_int8_opset12_zoo_CPU", disabledTestDefaultReason},
+     {"BERT_Squad_int8_opset12_zoo_CPU", disabledTestDefaultReason},
+     {"FCN_ResNet_50_opset11_zoo_CPU", disabledTestDefaultReason},
+     {"FCN_ResNet_101_opset11_zoo_CPU", disabledTestDefaultReason},
+     {"EfficientNet_Lite4_qdq_opset11_zoo_CPU", disabledTestDefaultReason},
+     {"EfficientNet_Lite4_int8_opset11_zoo_CPU", disabledTestDefaultReason},
     }
 );
 
