1a9e6af6a35417ef9829658e21b0c1d524a53802
Abhishek Udupa <abhishek.udupa@microsoft.com>
2022-10-24 16:31:59 -0700

    Lint fixes

--- onnxruntime/python/tools/profile_explorer/profile_explorer.py
+++ onnxruntime/python/tools/profile_explorer/profile_explorer.py
@@ -2,15 +2,15 @@
 
 import argparse
 import json
-import pandas as pd
 import subprocess as sp
+import pandas as pd
 
 
 def _demangle(name, demangler='cu++filt'):
     try:
-        p = sp.Popen([demangler, name], stdin=sp.PIPE, stdout=sp.PIPE)
-        out, _ = p.communicate()
-        return out.decode('utf-8').strip()
+        with sp.Popen([demangler, name], stdin=sp.PIPE, stdout=sp.PIPE) as proc:
+            out, _ = proc.communicate()
+            return out.decode('utf-8').strip()
     except:
         return name
 
@@ -34,11 +34,11 @@ def _get_args():
 
 def _shape_to_string(shape):
     res = ''
-    for d in shape:
-        if len(d) > 1:
+    for dict_obj in shape:
+        if len(dict_obj) > 1:
             raise ValueError('Unhandled type in _shape_to_string()')
-        key = list(d.keys())[0]
-        value = list(d.values())[0]
+        key = list(dict_obj.keys())[0]
+        value = list(dict_obj.values())[0]
         if len(res) != 0:
             res += '__'
         res += f'{key}_{"x".join(str(v) for v in value)}'
@@ -49,12 +49,12 @@ def _json_to_df(profile_path, filter_set):
     cpu_entries = []
     gpu_entries = []
     
-    with open(profile_path, "r") as f:
-        data = json.load(f)
-    if type(data) == dict:
+    with open(profile_path, "r", encoding='utf-8') as file_obj:
+        data = json.load(file_obj)
+    if isinstance(data, dict):
         data = data['traceEvents']
 
-    for idx, item in enumerate(data):
+    for item in data:
         cat = item.get('cat')
         if cat is None:
             continue
@@ -64,11 +64,11 @@ def _json_to_df(profile_path, filter_set):
         arg = item.get('args')
         if arg is None:
             continue
-        op = arg.get("op_name")
+        op_name = arg.get("op_name")
 
         name = item['name']
 
-        if filter_set is not None and len(filter_set) > 0 and name not in filter_set and op not in filter_set:
+        if filter_set is not None and len(filter_set) > 0 and name not in filter_set and op_name not in filter_set:
             continue
         
         if cat != 'Kernel' and not name.endswith('kernel_time'):
@@ -79,7 +79,7 @@ def _json_to_df(profile_path, filter_set):
                 'name': name,
                 'duration': dur,
                 'dimensions': f'{item["args"]["block_x"]}_{item["args"]["block_y"]}_{item["args"]["block_z"]}_{item["args"]["grid_x"]}_{item["args"]["grid_y"]}_{item["args"]["grid_z"]}',
-                'op_name': op,
+                'op_name': op_name,
             })
         else:
             cpu_entries.append({
@@ -95,36 +95,36 @@ def _json_to_df(profile_path, filter_set):
     gpu_df['count'] = 1
     return cpu_df, gpu_df
 
-def _print_cpu_top_hitters(df, args):
+def _print_cpu_top_hitters(frame, args):
     top = args.count
     group_key = ['name', 'input_shape'] if args.shape_sensitive else ['name']
-    df2 = df[['duration', 'count']].sum()
-    df['pct'] = 100 * (df['duration'] / df2['duration'])
+    frame2 = frame[['duration', 'count']].sum()
+    frame['pct'] = 100 * (frame['duration'] / frame2['duration'])
     fields = group_key + ['duration', 'pct', 'count']
-    df1 = df[fields].groupby(group_key).sum().reset_index()
-    df1 = df1.sort_values(by='duration', ascending=False)[:top]
-    df1['cumulative_pct'] = df1['pct'].cumsum()
-    df1['cumulative_dur'] = df1['duration'].cumsum()
+    frame1 = frame[fields].groupby(group_key).sum().reset_index()
+    frame1 = frame1.sort_values(by='duration', ascending=False)[:top]
+    frame1['cumulative_pct'] = frame1['pct'].cumsum()
+    frame1['cumulative_dur'] = frame1['duration'].cumsum()
     print('\n------ Top CPU Kernel Times ------')
-    print(df1.round(2).to_string(index=False))
+    print(frame1.round(2).to_string(index=False))
     if args.csv:
-        df1.to_csv(f'{args.csv}_cpu_kernel_times.csv', index=False)
+        frame1.to_csv(f'{args.csv}_cpu_kernel_times.csv', index=False)
 
-def _print_gpu_top_hitters(df, args):
+def _print_gpu_top_hitters(frame, args):
     top = args.count
     group_key = ['name', 'dimensions'] if args.dimension_sensitive else ['name']
-    df2 = df[['duration', 'count']].sum()
-    df['pct'] = 100 * (df['duration'] / df2['duration'])
+    frame2 = frame[['duration', 'count']].sum()
+    frame['pct'] = 100 * (frame['duration'] / frame2['duration'])
     fields = group_key + ['duration', 'pct', 'count']
-    df1 = df[fields].groupby(group_key).sum().reset_index()
-    df1 = df1.sort_values(by='duration', ascending=False)[:top]
-    df1['cumulative_pct'] = df1['pct'].cumsum()
-    df1['cumulative_dur'] = df1['duration'].cumsum()
-    df1['name'] = df1['name'].apply(lambda x: _demangle(x, args.demangler))
+    frame1 = frame[fields].groupby(group_key).sum().reset_index()
+    frame1 = frame1.sort_values(by='duration', ascending=False)[:top]
+    frame1['cumulative_pct'] = frame1['pct'].cumsum()
+    frame1['cumulative_dur'] = frame1['duration'].cumsum()
+    frame1['name'] = frame1['name'].apply(lambda x: _demangle(x, args.demangler))
     print('\n------ Top GPU Kernel Times ------')
-    print(df1.round(2).to_string(index=False))
+    print(frame1.round(2).to_string(index=False))
     if args.csv:
-        df1.to_csv(f'{args.csv}_gpu_kernel_times.csv', index=False)
+        frame1.to_csv(f'{args.csv}_gpu_kernel_times.csv', index=False)
 
 def main():
     args = _get_args()
