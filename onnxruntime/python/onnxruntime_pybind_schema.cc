0c530ecace1bf881e3cce5c5b15c0e97cb619d1d
Dwayne Robinson <dwayner@microsoft.com>
2022-10-24 19:20:43 -0700

    Add overload to DML EP creator to avoid device software check

--- onnxruntime/python/onnxruntime_pybind_schema.cc
+++ onnxruntime/python/onnxruntime_pybind_schema.cc
@@ -62,7 +62,7 @@ void addGlobalSchemaFunctions(pybind11::module& m) {
             onnxruntime::ArmNNProviderFactoryCreator::Create(0),
 #endif
 #ifdef USE_DML
-            onnxruntime::DMLProviderFactoryCreator::Create(0),
+            onnxruntime::DMLProviderFactoryCreator::Create(0, /*skip_software_device_check*/ true),
 #endif
 #ifdef USE_NNAPI
             onnxruntime::NnapiProviderFactoryCreator::Create(0),
