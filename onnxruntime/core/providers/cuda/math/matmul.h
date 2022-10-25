5d980c02589b39363af89330f725c1b071f96ade
Hari Seshadri <hasesh@microsoft.com>
2022-10-25 14:43:05 -0700

    a

--- onnxruntime/core/providers/cuda/math/matmul.h
+++ onnxruntime/core/providers/cuda/math/matmul.h
@@ -4,6 +4,7 @@
 #pragma once
 
 #include "core/providers/cuda/cuda_kernel.h"
+#include "core/framework/float16.h"
 
 namespace onnxruntime {
 namespace cuda {
@@ -22,6 +23,14 @@ class MatMul final : public CudaKernel {
           // cudaMalloc(&left_X_ptr_, (size_t)(ceil(6291456/ 256.)) * 256);
 
           cudaMalloc(&left_X_ptr_, (size_t)(ceil(25165824/ 256.)) * 256);
+          std::vector<uint16_t> data(25165824, 3);
+
+          cudaMemcpy(data.data() , left_X_ptr_, 25165824,  cudaMemcpyDeviceToHost);
+
+          for(size_t i=0; i<25165824; ++i) {
+            std::cout << MLFloat16(data[i]).ToFloat() << std::endl;
+          }
+
           //cudaMalloc(&right_X_ptr_1_, (size_t)(ceil(1179648/ 256.)) * 256);
           //cudaMalloc(&right_X_ptr_2_, (size_t)(ceil(4718592/ 256.)) * 256);
 
