60fdef575a9ba77fa9f746e8e863fdbe2a7fef9e
Patrice Vignola <vignola.patrice@gmail.com>
2022-10-25 18:06:02 -0700

    Add logging (REVERT LATER)

--- onnxruntime/core/providers/cuda/cuda_execution_provider.cc
+++ onnxruntime/core/providers/cuda/cuda_execution_provider.cc
@@ -2480,8 +2480,14 @@ CUDAExecutionProvider::GetCapability(const onnxruntime::GraphViewer& graph,
   auto cpu_nodes = GetCpuPreferredNodes(graph, kernel_lookup, candidates);
   std::vector<std::unique_ptr<ComputeCapability>> result;
   for (auto& node_index : candidates) {
-    if (cpu_nodes.count(node_index) > 0)
+    const onnxruntime::Node& node = *graph.GetNode(nodeIndex);
+
+    if (cpu_nodes.count(node_index) > 0) {
+      printf("*************Placed on CPU: %s\n", node.Name().c_str());
       continue;
+    }
+
+    printf("*************Placed on CUDA: %s\n", node.Name().c_str());
 
     auto sub_graph = IndexedSubGraph::Create();
     sub_graph->Nodes().push_back(node_index);
