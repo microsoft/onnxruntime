5cd56e4b5affc0ff8eefac184dd3a0ab55ce0945
Hari Seshadri <hasesh@microsoft.com>
2022-10-27 11:09:09 -0700

    v

--- onnxruntime/core/framework/session_state_utils.cc
+++ onnxruntime/core/framework/session_state_utils.cc
@@ -171,9 +171,9 @@ static common::Status DeserializeTensorProto(const Env& env, const std::basic_st
     }
     // TODO!! Need a temp buffer allocator for non-escape buffers that maybe too big for stack allocation.
 
-    //Status copy_status = data_transfer_mgr.CopyTensor(*p_deserialize_tensor, *p_tensor);
+    Status copy_status = data_transfer_mgr.CopyTensor(*p_deserialize_tensor, *p_tensor);
 
-    Status copy_status = data_transfer_mgr.Randomize(*p_tensor);
+    //Status copy_status = data_transfer_mgr.Randomize(*p_tensor);
 
     if (!copy_status.IsOK()) {
       if (copy_status.ErrorMessage().empty()) {
