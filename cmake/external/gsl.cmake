b2ffbfbf69addfd66ab8f448e753cb1c281aae28
Changming Sun <chasun@microsoft.com>
2022-11-05 16:06:06 -0700

    update

--- cmake/external/gsl.cmake
+++ cmake/external/gsl.cmake
@@ -3,11 +3,21 @@
 
 include(FetchContent)
 
-FetchContent_Declare(
+if(onnxruntime_USE_CUDA)
+  FetchContent_Declare(
     GSL
     URL https://github.com/microsoft/GSL/archive/refs/tags/v4.0.0.zip
     URL_HASH SHA1=cf368104cd22a87b4dd0c80228919bb2df3e2a14
-    )
+    ${Patch_EXECUTABLE} --binary --ignore-whitespace -p1 < ${PROJECT_SOURCE_DIR}/patches/gsl/1064.patch
+  )
+else()
+  FetchContent_Declare(
+    GSL
+    URL https://github.com/microsoft/GSL/archive/refs/tags/v4.0.0.zip
+    URL_HASH SHA1=cf368104cd22a87b4dd0c80228919bb2df3e2a14
+  )
+endif()
+
 
 FetchContent_MakeAvailable(GSL)
 
