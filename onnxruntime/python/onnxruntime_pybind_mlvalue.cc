a4d849c9096c7d80845c52b89634ac0ef6bf48fd
Randy Shuai <rashuai@microsoft.com>
2022-11-04 18:36:53 -0700

    switch header inclusion

--- onnxruntime/python/onnxruntime_pybind_mlvalue.cc
+++ onnxruntime/python/onnxruntime_pybind_mlvalue.cc
@@ -1,12 +1,12 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+#include "onnxruntime_pybind_mlvalue.h"
+
 #define NO_IMPORT_ARRAY
 #define NPY_NO_DEPRECATED_API NPY_1_7_API_VERSION
 #define PY_ARRAY_UNIQUE_SYMBOL onnxruntime_python_ARRAY_API
 #include <numpy/arrayobject.h>
-
-#include "onnxruntime_pybind_mlvalue.h"
 
 #include "core/graph/graph.h"
 #include "core/framework/tensor_shape.h"
