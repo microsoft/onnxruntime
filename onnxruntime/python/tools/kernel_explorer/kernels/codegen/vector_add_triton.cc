cd2edc8dbf2fded09d91643710ce41ac4a1625ca
codego7250 <codego7250@gmail.com>
2022-11-08 22:49:45 +0000

    update vector_add triton

--- onnxruntime/python/tools/kernel_explorer/kernels/codegen/vector_add_triton.cc
+++ onnxruntime/python/tools/kernel_explorer/kernels/codegen/vector_add_triton.cc
@@ -2,7 +2,7 @@
 // Licensed under the MIT License.
 
 // This file serve as a simple example for adding a tunable op to onnxruntime.
-#include "python/tools/kernel_explorer/kernels/vector_add.h"
+#include "python/tools/kernel_explorer/kernels/vector_add_triton.h"
 
 #include <hip/hip_fp16.h>
 #include <pybind11/pybind11.h>
@@ -36,8 +36,8 @@ struct VectorAddParams : rocm::tunable::OpParams {
 // Wrap the kernel function, so that we have a unified launch interface. If the kernel has state, the state can also
 // be managed at this level via a functor
 template <typename T, int TPB, int Vec>
-Status VectorAddOp(const VectorAddParams<T>* params) {
-  return LaunchVectorAdd<T, TPB, Vec>(
+Status TritonVectorAddOp(const VectorAddParams<T>* params) {
+  return LaunchTritonVectorAdd<T, TPB, Vec>(
       params->stream,
       params->x,
       params->y,
@@ -45,26 +45,16 @@ Status VectorAddOp(const VectorAddParams<T>* params) {
       params->n);
 }
 
-#define ADD_OP(threads_per_block)                                \
-  this->ops_.emplace_back(VectorAddOp<T, threads_per_block, 1>); \
-  this->ops_.emplace_back(VectorAddOp<T, threads_per_block, 2>); \
-  this->ops_.emplace_back(VectorAddOp<T, threads_per_block, 4>); \
-  this->ops_.emplace_back(VectorAddOp<T, threads_per_block, 8>);
+#define ADD_OP(block_size)                                \
+  this->ops_.emplace_back(TritonVectorAddOp<T, block_size>);
 
 // A Tunable VectorAddOp is a collection of non-tunable VectorAddOps implementations that have variable performance
 // characteristics. Those implementations may be put into a C++ container for tuner to select.
 template <typename T>
-class VectorAddTunableOp : public rocm::tunable::TunableOp<VectorAddParams<T>> {
+class TritonVectorAddTunableOp : public rocm::tunable::TunableOp<VectorAddParams<T>> {
  public:
-  VectorAddTunableOp() {
-    ADD_OP(64);
-    ADD_OP(128);
-    ADD_OP(192);
-    ADD_OP(256);
-    ADD_OP(320);
-    ADD_OP(384);
-    ADD_OP(448);
-    ADD_OP(512);
+  TritonVectorAddTunableOp() {
+    ADD_OP(1024);
   }
 
  private:
@@ -82,7 +72,7 @@ class VectorAddTunableOp : public rocm::tunable::TunableOp<VectorAddParams<T>> {
 //#####################################################################################################################
 
 template <typename T, int TPB, int Vec>
-class VectorAdd : public IKernelExplorer {
+class TritonVectorAdd : public IKernelExplorer {
  public:
   VectorAdd(DeviceArray& x, DeviceArray& y, DeviceArray& z, int n) {
     params_.stream = Stream();
@@ -103,7 +93,7 @@ class VectorAdd : public IKernelExplorer {
 };
 
 template <typename T>
-class VectorAddTunable : public IKernelExplorer {
+class TritonVectorAddTunable : public IKernelExplorer {
  public:
   VectorAddTunable(DeviceArray& x, DeviceArray& y, DeviceArray& z, int n) {
     params_.stream = Stream();
@@ -127,41 +117,31 @@ class VectorAddTunable : public IKernelExplorer {
   VectorAddTunableOp<T> impl_;
 };
 
-#define REGISTER_OP(name, type, threads_per_block, vec_size)                                              \
-  py::class_<name<type, threads_per_block, vec_size>>(m, #name"_"#type"_"#threads_per_block"_"#vec_size)  \
+
+#define REGISTER_OP(name, type, block_size)                                             \
+  py::class_<name<type, block_size>>(m, #name"_"#type"_"#block_size)  \
     .def(py::init<DeviceArray&, DeviceArray&, DeviceArray&, int>())                                       \
-    .def("SetRepeats", &name<type, threads_per_block, vec_size>::SetRepeats)                              \
-    .def("Profile", &name<type, threads_per_block, vec_size>::Profile)                                    \
-    .def("Run", &name<type, threads_per_block, vec_size>::Run);
+    .def("SetRepeats", &name<type, block_size>::SetRepeats)                              \
+    .def("Profile", &name<type, block_size>::Profile)                                    \
+    .def("Run", &name<type, block_size>::Run);
 
-#define REGISTER_OP_FOR_ALL_VEC_SIZE(name, type, threads_per_block)  \
-  REGISTER_OP(name, type, threads_per_block, 1)                      \
-  REGISTER_OP(name, type, threads_per_block, 2)                      \
-  REGISTER_OP(name, type, threads_per_block, 4)                      \
-  REGISTER_OP(name, type, threads_per_block, 8)
+#define REGISTER_OP_FOR_ALL_BLOCK_SIZE(name, type, block_size)  \
+  REGISTER_OP(name, type, block_size)
 
-#define REGISTER_OP_FOR_ALL_THREADS_PER_BLOCK(name, type)  \
-  REGISTER_OP_FOR_ALL_VEC_SIZE(name, type, 64)             \
-  REGISTER_OP_FOR_ALL_VEC_SIZE(name, type, 128)            \
-  REGISTER_OP_FOR_ALL_VEC_SIZE(name, type, 192)            \
-  REGISTER_OP_FOR_ALL_VEC_SIZE(name, type, 256)            \
-  REGISTER_OP_FOR_ALL_VEC_SIZE(name, type, 320)            \
-  REGISTER_OP_FOR_ALL_VEC_SIZE(name, type, 384)            \
-  REGISTER_OP_FOR_ALL_VEC_SIZE(name, type, 448)            \
-  REGISTER_OP_FOR_ALL_VEC_SIZE(name, type, 512)
+
 
 #define REGISTER_TUNABLE_OP(type)                                      \
-  py::class_<VectorAddTunable<type>>(m, "VectorAdd_" #type "_Tunable") \
+  py::class_<TritonVectorAddTunable<type>>(m, "TritonVectorAdd_" #type "_Tunable") \
       .def(py::init<DeviceArray&, DeviceArray&, DeviceArray&, int>())  \
       .def("SetRepeats", &VectorAddTunable<type>::SetRepeats)          \
       .def("Profile", &VectorAddTunable<type>::Profile)                \
       .def("Run", &VectorAddTunable<type>::Run);
 
-void InitVectorAdd(py::module m) {
-  REGISTER_OP_FOR_ALL_THREADS_PER_BLOCK(VectorAdd, half);
-  REGISTER_OP_FOR_ALL_THREADS_PER_BLOCK(VectorAdd, float);
+void InitTritonVectorAdd(py::module m) {
+  //REGISTER_OP_FOR_ALL_THREADS_PER_BLOCK(VectorAdd, half);
+  REGISTER_OP_FOR_ALL_BLOCK_SIZE(TritonVectorAdd, float);
 
-  REGISTER_TUNABLE_OP(half);
+  //REGISTER_TUNABLE_OP(half);
   REGISTER_TUNABLE_OP(float)
 }
 
