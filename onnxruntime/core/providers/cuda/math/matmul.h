5a18e2dc49a98f2edfb4f8e8fa027fa5b0917b97
Hari Seshadri <hasesh@microsoft.com>
2022-11-09 16:14:25 -0800

    b

--- onnxruntime/core/providers/cuda/math/matmul.h
+++ onnxruntime/core/providers/cuda/math/matmul.h
@@ -26,12 +26,14 @@ class MatMul final : public CudaKernel {
       std::cout << "using CublasLtMatmul" << std::endl;
     }
 
+    /*
     if (flush_denormals_to_zero_) {
       std::cout << "Denormals to zero" << std::endl;
    
     } else if (flush_all_to_zero_) {
       std::cout << "All to zero" << std::endl;    
     }
+    */
   }
 
   Status ComputeInternal(OpKernelContext* context) const override;
