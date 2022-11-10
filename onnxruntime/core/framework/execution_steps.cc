1f46e23765f9ee83ad0cdc9009941160bec18b7d
Cheng Tang <chenta@microsoft.com@orttrainingdev9.d32nl1ml4oruzj4qz3bqlggovf.px.internal.cloudapp.net>
2022-11-10 03:40:43 +0000

    fix more comments

--- onnxruntime/core/framework/execution_steps.cc
+++ onnxruntime/core/framework/execution_steps.cc
@@ -1,3 +1,5 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
 #include "core/framework/execution_steps.h"
 #include "core/framework/sequential_executor.h"
 namespace onnxruntime {
