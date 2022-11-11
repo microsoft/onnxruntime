2e6ba38ab855b8e28c7ebadeb92982c677a11da5
wejoncy <wejoncy@163.com>
2022-11-11 15:15:36 +0800

    -

--- onnxruntime/test/providers/xnnpack/xnnpack_basic_test.cc
+++ onnxruntime/test/providers/xnnpack/xnnpack_basic_test.cc
@@ -1,9 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-#include <algorithm>
-#include <cstdint>
-#include <numeric>
 #include <random>
 #include <string>
 
