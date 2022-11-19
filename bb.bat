dd9161c27b86e41809e245097410a2886b9e172e
Yulong Wang <7679871+fs-eire@users.noreply.github.com>
2022-11-18 17:44:58 -0800

    w

--- bb.bat
+++ bb.bat
@@ -1,6 +1,24 @@
-call .\build.bat --config Debug --skip_submodule_sync --skip_tests --build_wasm --use_js --cmake_generator "Visual Studio 17 2022" --target onnxruntime_webassembly --cmake_extra_defines onnxruntime_DEBUG_NODE_INPUTS_OUTPUTS=1
+@echo off
+
+if ["%~1"]==["--clean"] (
+    if exist "%~dp0build\Windows\Debug" (
+        rd /s /q %~dp0build\Windows\Debug
+    )
+)
+
+setlocal
+
+if exist "%~dp0build\Windows\host_protoc\Release\protoc.exe" (
+    set protoc_path_flag=--path_to_protoc_exe %~dp0build\Windows\host_protoc\Release\protoc.exe
+) else (
+    set protoc_path_flag=
+)
+
+call .\build.bat --config Debug --skip_submodule_sync --skip_tests --build_wasm --use_js --cmake_generator "Visual Studio 17 2022" %protoc_path_flag% --target onnxruntime_webassembly --cmake_extra_defines onnxruntime_DEBUG_NODE_INPUTS_OUTPUTS=1
 
 IF %ERRORLEVEL% == 0 (
 copy /Y .\build\Windows\Debug\ort-wasm.js .\js\web\lib\wasm\binding\
 copy /Y .\build\Windows\Debug\ort-wasm.wasm .\js\web\dist\
 )
+
+endlocal
--- br.bat
+++ br.bat
@@ -1,6 +1,24 @@
-call .\build.bat --config Release --skip_submodule_sync --skip_tests --disable_wasm_exception_catching --disable_rtti --build_wasm --use_js --cmake_generator "Visual Studio 17 2022" --target onnxruntime_webassembly
+@echo off
+
+if ["%~1"]==["--clean"] (
+    if exist "%~dp0build\Windows\Release" (
+        rd /s /q %~dp0build\Windows\Release
+    )
+)
+
+setlocal
+
+if exist "%~dp0build\Windows\host_protoc\Release\protoc.exe" (
+    set protoc_path_flag=--path_to_protoc_exe %~dp0build\Windows\host_protoc\Release\protoc.exe
+) else (
+    set protoc_path_flag=
+)
+
+call .\build.bat --config Release --skip_submodule_sync --skip_tests --disable_wasm_exception_catching --disable_rtti --build_wasm --use_js --cmake_generator "Visual Studio 17 2022" %protoc_path_flag% --target onnxruntime_webassembly
 
 IF %ERRORLEVEL% == 0 (
 copy /Y .\build\Windows\Release\ort-wasm.js .\js\web\lib\wasm\binding\
 copy /Y .\build\Windows\Release\ort-wasm.wasm .\js\web\dist\
 )
+
+endlocal
