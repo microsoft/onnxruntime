a5c2047dd14f252673e6887000058c7d6290ab07
Changming Sun <chasun@microsoft.com>
2022-11-21 10:21:38 -0800

    Fix the remaining Prefast warnings in CPU EP (#13707)
    ### Description
    
    Fix the remaining Prefast warnings in CPU EP.

--- onnxruntime/core/graph/contrib_ops/bert_defs.cc
+++ onnxruntime/core/graph/contrib_ops/bert_defs.cc
@@ -6,7 +6,11 @@
 #include "core/graph/contrib_ops/quantization_defs.h"
 #include "core/graph/contrib_ops/onnx_function_util.h"
 #include "core/graph/contrib_ops/shape_inference_functions.h"
-
+// Suppress a warning: global initializer calls a non-constexpr function 'symbol' which is from
+// ONNX_OPERATOR_SET_SCHEMA_EX macro and only happens in debug build
+#if defined(_WIN32) && !defined(NDEBUG)
+#pragma warning(disable : 26426)
+#endif
 using namespace ::ONNX_NAMESPACE;
 
 namespace ONNX_NAMESPACE {
