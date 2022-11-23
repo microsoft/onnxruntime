5a8bd76dd8a8b2829497ff9e536b959c76c926a4
Tianlei Wu <tlwu@microsoft.com>
2022-11-23 00:20:04 +0000

    --batch_size and --use_io_binding in eval_squad

--- onnxruntime/python/tools/transformers/models/bert/eval_squad.py
+++ onnxruntime/python/tools/transformers/models/bert/eval_squad.py
@@ -39,7 +39,7 @@ def get_package_version(package_name: str):
         return None
 
 
-def load_onnx_model(model_id: str, onnx_path: Optional[str] = None, use_gpu: bool = True):
+def load_onnx_model(model_id: str, onnx_path: Optional[str] = None, use_gpu: bool = True, use_io_binding: bool = False):
     """Load onnx model given pretrained model name and optional ONNX model path. If onnx_path is None,
     the default onnx model from optimum will be used.
 
@@ -58,14 +58,17 @@ def load_onnx_model(model_id: str, onnx_path: Optional[str] = None, use_gpu: boo
         model.latest_model_name = Path(onnx_path).name
 
         if use_gpu:
-            model.device = torch.device("cuda")
+            model.device = torch.device("cuda:0")
             model.model = ORTModel.load_model(onnx_path, "CUDAExecutionProvider")
         else:
+            model.device = torch.device("cpu")
             model.model = ORTModel.load_model(onnx_path)
     else:
         onnx_path = os.path.join(model.model_save_dir.as_posix(), model.latest_model_name)
         if use_gpu:
             model.to("cuda")
+
+    model.use_io_binding = use_io_binding
 
     return model, onnx_path
 
@@ -85,6 +88,7 @@ def output_details(results: List[Dict[str, Any]], csv_filename: str):
             "disable_fused_attention",
             "batch_size",
             "sequence_length",
+            "use_io_binding",
             "exact",
             "f1",
             "total",
@@ -119,7 +123,13 @@ def output_summary(results: List[Dict[str, Any]], csv_filename: str, metric_name
         metric_name (str): the metric to summarize
     """
     with open(csv_filename, mode="a", newline="", encoding="ascii") as csv_file:
-        header_names = ["pretrained_model_name", "onnx_path", "provider", "disable_fused_attention"]
+        header_names = [
+            "pretrained_model_name",
+            "onnx_path",
+            "provider",
+            "disable_fused_attention",
+            "use_io_binding",
+        ]
 
         model_list = list(set([result["onnx_path"] for result in results]))
         model_list.sort()
@@ -190,19 +200,21 @@ def main():
         tokenizer.model_max_length = sequence_length
         tokenizer.doc_stride = min(sequence_length // 2, 128)
 
-        ort_model, onnx_path = load_onnx_model(pretrained_model_name, args.onnx, args.use_gpu)
+        ort_model, onnx_path = load_onnx_model(pretrained_model_name, args.onnx, args.use_gpu, args.use_io_binding)
         print(ort_model.config)
         if sequence_length > ort_model.config.max_position_embeddings:
             raise RuntimeError("sequence length should not be larger than {ort_model.config.max_position_embeddings}")
 
-        qa_pipeline = pipeline("question-answering", model=ort_model, tokenizer=tokenizer, question_first=True)
+        qa_pipeline = pipeline(
+            "question-answering", model=ort_model, tokenizer=tokenizer, question_first=True, batch_size=args.batch_size
+        )
 
         task_evaluator = evaluator("question-answering")
-        data = load_dataset("squad", split=f"validation[:{args.total}]" if args.total > 0 else "validation")
+        squad_dataset = load_dataset("squad", split=f"validation[:{args.total}]" if args.total > 0 else "validation")
 
         result = task_evaluator.compute(
             model_or_pipeline=qa_pipeline,
-            data=data,
+            data=squad_dataset,
             metric="squad_v2",
             squad_v2_format=True,
         )
@@ -211,8 +223,9 @@ def main():
         result["disable_fused_attention"] = disable_fused_attention
         result["pretrained_model_name"] = pretrained_model_name
         result["onnx_path"] = onnx_path
-        result["batch_size"] = 1
+        result["batch_size"] = args.batch_size
         result["sequence_length"] = sequence_length
+        result["use_io_binding"] = args.use_io_binding
         print(result)
 
         all_results.append(result)
@@ -244,6 +257,14 @@ def parse_arguments(argv=None):
         help="Sequence lengths for onnx model inputs. It could have multiple values.",
     )
 
+    parser.add_argument(
+        "-b",
+        "--batch_size",
+        type=int,
+        default=1,
+        help="batch size for inference.",
+    )
+
     parser.add_argument("-t", "--total", type=int, default=0, help="Total samples to test. 0 means all samples.")
 
     parser.add_argument(
@@ -256,6 +277,9 @@ def parse_arguments(argv=None):
 
     parser.add_argument("--use_gpu", required=False, action="store_true", help="Use CUDA execution provider.")
     parser.set_defaults(use_gpu=False)
+
+    parser.add_argument("--use_io_binding", required=False, action="store_true", help="Use IO Binding for GPU.")
+    parser.set_defaults(use_io_binding=False)
 
     args = parser.parse_args(argv)
 
