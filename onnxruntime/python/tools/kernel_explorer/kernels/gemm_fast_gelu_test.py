ee15b4159f70553f0bb6f89771e5ec2e71832604
peixuanzuo <peixuanzuo@linmif39a000004.zvflicr54joexhdgnhvmxrxygg.phxx.internal.cloudapp.net>
2022-11-29 03:08:43 +0000

    update python format

--- onnxruntime/python/tools/kernel_explorer/kernels/gemm_fast_gelu_test.py
+++ onnxruntime/python/tools/kernel_explorer/kernels/gemm_fast_gelu_test.py
@@ -83,12 +83,14 @@ def test_gemmfastgelu_tunable_bert_cases(dtype, size, transab):
     wrapper_name = "GemmFastGeluTunable_{}_{}".format(dtype_to_suffix(dtype), transab_to_suffix(transab))
     _test_gemmfastgelu(getattr(ke, wrapper_name), dtype, *size, *transab)
 
+
 @pytest.mark.parametrize("dtype", dtypes)
 @pytest.mark.parametrize("size", get_gemm_basic_sizes(full=False) + get_gemm_bert_sizes(full=False))
 @pytest.mark.parametrize("transab", all_transabs)
 def test_gemmfastgelu_ck_bert_cases(dtype, size, transab):
     wrapper_name = "CKGemmFastGelu_{}_{}".format(dtype_to_suffix(dtype), transab_to_suffix(transab))
     _test_gemmfastgelu(getattr(ke, wrapper_name), dtype, *size, *transab)
+
 
 def profile_gemmfastgelu_func(my_func, dtype: str, m: int, n: int, k: int, transa: bool, transb: bool):
     a_shape = (k, m) if transa else (m, k)
@@ -133,8 +135,13 @@ def profile_with_args(transa, transb, dtype, m, n, k):
     dtype_suffix = "_" + dtype_to_suffix(dtype)
     profile_gemmfastgelu_func(getattr(ke, "GemmFastGeluUnfused" + dtype_suffix), dtype, m, n, k, transa, transb)
     transab_suffix = "_" + transab_to_suffix((transa, transb))
-    profile_gemmfastgelu_func(getattr(ke, "CKGemmFastGelu" + dtype_suffix + transab_suffix), dtype, m, n, k, transa, transb)
-    profile_gemmfastgelu_func(getattr(ke, "GemmFastGeluTunable" + dtype_suffix + transab_suffix), dtype, m, n, k, transa, transb)
+    profile_gemmfastgelu_func(
+        getattr(ke, "CKGemmFastGelu" + dtype_suffix + transab_suffix), dtype, m, n, k, transa, transb
+    )
+    profile_gemmfastgelu_func(
+        getattr(ke, "GemmFastGeluTunable" + dtype_suffix + transab_suffix), dtype, m, n, k, transa, transb
+    )
+
 
 def profile():
     for dtype in dtypes:
