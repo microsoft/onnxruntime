639d285670c7f5dcbc957ecbce350c295cbe328a
Wei-Sheng Chin <wschin@outlook.com>
2022-11-30 09:23:44 -0800

    [DORT] Catch up with yesterday's PyTorch change (#13779)
    Fix recent CI failures.

--- orttraining/orttraining/python/training/torchdynamo/register_backend.py
+++ orttraining/orttraining/python/training/torchdynamo/register_backend.py
@@ -3,46 +3,21 @@
 # Licensed under the MIT License.
 # --------------------------------------------------------------------------
 
-import torch
 from functorch.compile import min_cut_rematerialization_partition
-from torch._dynamo.optimizations.backends import BACKENDS
-from torch._dynamo.optimizations.training import AotAutogradStrategy
-
+from torch._dynamo.optimizations.training import aot_autograd
 from .ort_backend import OrtBackend
 
 
-class AotOrt(AotAutogradStrategy):
-    """Implement compiler interface to plug ORT into TorchDynam.
+# This is the underlying compiler for ALL training graphs if the user uses ORT
+# to optimize training graphs.
+# A global compiler is used here, so that cached compilation results can be reused
+# across different graphs (each graph calls this compiler once).
+DEFAULT_BACKEND = OrtBackend()
 
-    Under the hood, AotOrt.compile is called inside functorch. See aot_function
-    and aot_module in aot_autograd.py in PyTorch repo for more details. Basically,
-    AotOrt.compile is mapped to forward graph compiler, fw_compile, and backward
-    graph compiler, bw_compile, in aot_autograd.py.
-    """
-
-    def __init__(self, graph_module: torch.fx.GraphModule, example_inputs):
-        super().__init__(graph_module, example_inputs)
-
-        self.ort = OrtBackend()
-
-    def candidate(self):
-        return BACKENDS["aot_autograd"](
-            # Graph to compile.
-            self.gm,
-            # Example inputs that self.gm can execute on.
-            # That it, self.gm(*example_inputs) will run.
-            self.example_inputs,
-            # Forward graph's compiler.
-            fw_compiler=self.ort,
-            # Backward graph's compiler.
-            bw_compiler=self.ort,
-            # partition_fn splits training graph into forward and backward graphs.
-            partition_fn=min_cut_rematerialization_partition,
-        )
-
-
-# Call stack:
-# AotAutogradStrategy.compile_fn
-#  AotAutogradStrategy.verified_candidate
-#   AotOrt.candidate
-aot_ort = AotOrt.compile_fn
+# Wrap ORT as a compiler in Dynamo.
+#
+# Under the hood, AotOrt.compile is called inside functorch. See aot_function
+# and aot_module in aot_autograd.py in PyTorch repo for more details. Basically,
+# AotOrt.compile is mapped to forward graph compiler, fw_compile, and backward
+# graph compiler, bw_compile, in aot_autograd.py.
+aot_ort = aot_autograd(fw_compiler=DEFAULT_BACKEND, partition_fn=min_cut_rematerialization_partition)
