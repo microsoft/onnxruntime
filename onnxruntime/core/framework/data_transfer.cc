3da7ac55aa356584d564400d7046a7c32fd301e5
Ashwini Khade <askhade@microsoft.com>
2022-12-02 15:08:49 -0800

    add a separate macro definition for enabling strided tensor support

--- onnxruntime/core/framework/data_transfer.cc
+++ onnxruntime/core/framework/data_transfer.cc
@@ -50,7 +50,7 @@ common::Status CPUDataTransfer::CopyTensor(const Tensor& src, Tensor& dst, int /
     return Status::OK();
   }
 
-#ifdef ENABLE_TRAINING
+#ifdef ENABLE_STRIDED_TENSORS
   if (!src.IsContiguous() || !dst.IsContiguous()) {
     auto dst_stride_vec = dst.Strides();
     auto src_stride_vec = src.Strides();
@@ -71,7 +71,7 @@ common::Status CPUDataTransfer::CopyTensor(const Tensor& src, Tensor& dst, int /
     }
 
     return Status::OK();
-#ifdef ENABLE_TRAINING
+#ifdef ENABLE_STRIDED_TENSORS
   }
 #endif
 }
