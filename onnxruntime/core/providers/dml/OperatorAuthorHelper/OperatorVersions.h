48004ed3b05175b53fba95b446114c53ea335b83
Numfor Mbiziwo-Tiapo <numform@microsoft.com>
2022-12-07 13:57:16 -0800

    Fix identity

--- onnxruntime/core/providers/dml/OperatorAuthorHelper/OperatorVersions.h
+++ onnxruntime/core/providers/dml/OperatorAuthorHelper/OperatorVersions.h
@@ -400,6 +400,7 @@ namespace OperatorHelper
         static const int sc_sinceVer_FusedMatMul = 1;
         static const int sc_sinceVer_QLinearSigmoid = 1;
         static const int sc_sinceVer_Attention = 1;
+        static const int sc_sinceVer_SkipLayerNormalization = 1;
     } // namespace MsftOperatorSet1
 
 } // namespace OperatorHelper
