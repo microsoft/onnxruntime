d62de05b30be9d117b4b3d4d80b5954508515e6f
Liqun Fu <liqfu@microsoft.com>
2022-12-08 08:41:52 -0800

    dep -> 12, fix android and core-ml max ver to 18
    Signed-off-by: Liqun Fu <liqfu@microsoft.com>

--- onnxruntime/core/providers/coreml/builders/impl/base_op_builder.h
+++ onnxruntime/core/providers/coreml/builders/impl/base_op_builder.h
@@ -46,7 +46,7 @@ class BaseOpBuilder : public IOpBuilder {
   virtual bool HasSupportedInputsImpl(const Node& node, const logging::Logger& logger) const;
 
   virtual int GetMinSupportedOpSet(const Node& /* node */) const { return 1; }
-  virtual int GetMaxSupportedOpSet(const Node& /* node */) const { return 17; }
+  virtual int GetMaxSupportedOpSet(const Node& /* node */) const { return 18; }
 
  private:
   bool HasSupportedOpSet(const Node& node, const logging::Logger& logger) const;
