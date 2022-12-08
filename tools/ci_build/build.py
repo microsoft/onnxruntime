cade3ab5abd6d52613cb46591b7be10945612037
Chi Lo <Chi.Lo@microsoft.com>
2022-12-08 19:33:06 +0000

    increase timeout to 10 hours

--- tools/ci_build/build.py
+++ tools/ci_build/build.py
@@ -1882,7 +1882,7 @@ def run_onnxruntime_tests(args, source_dir, ctest_path, build_dir, configs):
 
         else:
             if args.use_tensorrt:
-                ctest_cmd = [ctest_path, "--build-config", config, "--verbose", "--timeout", "18000"]
+                ctest_cmd = [ctest_path, "--build-config", config, "--verbose", "--timeout", "36000"]
             else:
                 ctest_cmd = [ctest_path, "--build-config", config, "--verbose", "--timeout", "10800"]
             run_subprocess(ctest_cmd, cwd=cwd, dll_path=dll_path)
