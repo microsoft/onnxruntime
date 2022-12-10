a1fded41736e98644a37056ba53213ba7f37f044
Chi Lo <Chi.Lo@microsoft.com>
2022-12-10 23:57:52 +0000

    test CUDA_MODULE_LOADING=LAZY

--- tools/ci_build/github/windows/setup_env_gpu.bat
+++ tools/ci_build/github/windows/setup_env_gpu.bat
@@ -3,3 +3,4 @@ REM Licensed under the MIT License.
 
 set PATH=C:\local\TensorRT-8.5.1.7.Windows10.x86_64.cuda-11.8.cudnn8.6\lib;C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v11.6\bin;C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v11.6\extras\CUPTI\lib64;C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin;%PATH%
 set GRADLE_OPTS=-Dorg.gradle.daemon=false
+set CUDA_MODULE_LOADING=LAZY
