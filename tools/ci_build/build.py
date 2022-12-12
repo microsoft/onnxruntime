f390402fa692d1ca8f53fb9ecf2e1766afb251d9
Chi Lo <Chi.Lo@microsoft.com>
2022-12-12 02:06:14 +0000

    remove timeout for TRT EP unittests

--- tools/ci_build/build.py
+++ tools/ci_build/build.py
@@ -1888,15 +1888,7 @@ def run_onnxruntime_tests(args, source_dir, ctest_path, build_dir, configs):
                     run_subprocess([os.path.join(cwd, exe)], cwd=cwd, dll_path=dll_path)
 
         else:
-            if args.use_tensorrt:
-                # Using --tensorrt_placeholder_builder can help reduce CI testing time.
-                # However, in C-API application test in package pipelines, with this flag will cause
-                # ORT TRT to deadlock, so we remove this flag for package pipelines.
-                # For package pipelines with TRT 8.5 GA, we observe increased testing time, hence
-                # increase the timeout as workaround.
-                ctest_cmd = [ctest_path, "--build-config", config, "--verbose", "--timeout", "36000"]
-            else:
-                ctest_cmd = [ctest_path, "--build-config", config, "--verbose", "--timeout", "10800"]
+            ctest_cmd = [ctest_path, "--build-config", config, "--verbose", "--timeout", "10800"]
             run_subprocess(ctest_cmd, cwd=cwd, dll_path=dll_path)
 
         if args.enable_pybind:
