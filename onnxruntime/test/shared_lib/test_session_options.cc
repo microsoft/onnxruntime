cf8b9aa61d00acecc31dc0907a20d7fac7525474
Randy Shuai <rashuai@microsoft.com>
2022-12-12 12:00:37 -0800

    add header to kMaxStrLen

--- onnxruntime/test/shared_lib/test_session_options.cc
+++ onnxruntime/test/shared_lib/test_session_options.cc
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+##include "core/common/common.h"
 #include "core/session/onnxruntime_cxx_api.h"
 #include "core/session/onnxruntime_session_options_config_keys.h"
 #include "core/optimizer/graph_transformer_level.h"
