47a11211a5b22a61534aba04abe63048d6c7f45f
Baiju Meswani <bmeswani@microsoft.com>
2022-12-13 19:15:53 +0000

    address pull request review comments

--- orttraining/orttraining/test/training_ops/cuda/fake_quant_test.cc
+++ orttraining/orttraining/test/training_ops/cuda/fake_quant_test.cc
@@ -26,7 +26,7 @@ void CompareFakeQuantKernels(const std::vector<int64_t>& tensor_dim,
   std::vector<float> scale = random.Uniform<float>(std::vector<int64_t>({1}), 0.04f, 0.1f);
   test.AddInput<float>("scale", {1}, scale);
   std::vector<float> zero_point = random.Uniform<float>(std::vector<int64_t>({1}), 0.f, 255.0f);
-  test.AddInput<float>("zero_scale", {1}, std::vector<float>({std::nearbyint(zero_point.front())}));
+  test.AddInput<float>("zero_point", {1}, std::vector<float>({std::nearbyint(zero_point.front())}));
 
   // Create output tensors
   std::vector<float> fake_quantized_data = FillZeros<float>(tensor_dim);
@@ -34,36 +34,6 @@ void CompareFakeQuantKernels(const std::vector<int64_t>& tensor_dim,
   std::unique_ptr<bool[]> quantization_mask = std::make_unique<bool[]>(detail::SizeFromDims(tensor_dim));
   test.AddOutput<bool>("quantization_mask", tensor_dim, quantization_mask.get(), detail::SizeFromDims(tensor_dim));
 
-  test.CompareWithCPU(kCudaExecutionProvider, per_sample_tolerance, relative_per_sample_tolerance);
-}
-
-void CompareFakeQuantGradKernels(const std::vector<int64_t>& tensor_dim,
-                                 double per_sample_tolerance = 2e-4,
-                                 double relative_per_sample_tolerance = 2e-4) {
-  CompareOpTester test("FakeQuantGrad", 1, onnxruntime::kMSDomain);
-
-  // Randomly generate the gradient w.r.t. the output Y.
-  RandomValueGenerator random{};
-  std::vector<float> dY_data = random.Uniform<float>(tensor_dim, -1000.0f, 1000.0f);
-
-  // Randomly generate the mask data
-  std::unique_ptr<bool[]> mask_data = [&random, &tensor_dim]() {
-    auto data_int = random.Uniform<int>(tensor_dim, 0, 2);
-    auto data_bool = std::make_unique<bool[]>(detail::SizeFromDims(tensor_dim));
-    for (size_t i = 0; i < data_int.size(); ++i) {
-      data_bool.get()[i] = data_int[i] == 0;
-    }
-    return data_bool;
-  }();
-
-  // Initialize the gradient w.r.t. the input X with 0s.
-  std::vector<float> dX_data = FillZeros<float>(tensor_dim);
-
-  test.AddInput<float>("dY", tensor_dim, dY_data);
-  test.AddInput<bool>("gradient_mask", tensor_dim, mask_data.get(), detail::SizeFromDims(tensor_dim));
-  test.AddOutput<float>("dX", tensor_dim, dX_data);
-
-  // Compare the outputs from the two kernels
   test.CompareWithCPU(kCudaExecutionProvider, per_sample_tolerance, relative_per_sample_tolerance);
 }
 
@@ -85,7 +55,7 @@ TEST(FakeQuantTest, FakeQuantComputation) {
 
   test.AddInput<float>("input_tensor", {10}, {1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f, 7.0f, 8.0f, 9.0f, 10.0f});
   test.AddInput<float>("scale", {1}, {0.075f});
-  test.AddInput<float>("zero_scale", {1}, {128.0f});
+  test.AddInput<float>("zero_point", {1}, {128.0f});
   // quantized values = nearby_int(value / scale + zero_point)
   //                  = {13.33+128, 26.66+128, 40.00+128, 53.33+128, 66.66+128, ...}
   //                  = {141.33, 154.66, 168.00, 171.33, 184.66, ...}
@@ -170,7 +140,34 @@ class FakeQuantGradKernelComparisonParameterizedTest : public ::testing::TestWit
 };
 
 TEST_P(FakeQuantGradKernelComparisonParameterizedTest, FakeQuantGradKernels) {
-  CompareFakeQuantGradKernels(GetParam());
+  CompareOpTester test("FakeQuantGrad", 1, onnxruntime::kMSDomain);
+  auto tensor_dim = GetParam();
+
+  // Randomly generate the gradient w.r.t. the output Y.
+  RandomValueGenerator random{};
+  std::vector<float> dY_data = random.Uniform<float>(tensor_dim, -1000.0f, 1000.0f);
+
+  // Randomly generate the mask data
+  std::unique_ptr<bool[]> mask_data = [&random, &tensor_dim]() {
+    auto data_int = random.Uniform<int>(tensor_dim, 0, 2);
+    auto data_bool = std::make_unique<bool[]>(detail::SizeFromDims(tensor_dim));
+    for (size_t i = 0; i < data_int.size(); ++i) {
+      data_bool.get()[i] = data_int[i] == 0;
+    }
+    return data_bool;
+  }();
+
+  // Initialize the gradient w.r.t. the input X with 0s.
+  std::vector<float> dX_data = FillZeros<float>(tensor_dim);
+
+  test.AddInput<float>("dY", tensor_dim, dY_data);
+  test.AddInput<bool>("gradient_mask", tensor_dim, mask_data.get(), detail::SizeFromDims(tensor_dim));
+  test.AddOutput<float>("dX", tensor_dim, dX_data);
+
+  // Compare the outputs from the two kernels
+  const double per_sample_tolerance = 2e-4;
+  const double relative_per_sample_tolerance = 2e-4;
+  test.CompareWithCPU(kCudaExecutionProvider, per_sample_tolerance, relative_per_sample_tolerance);
 }
 
 INSTANTIATE_TEST_SUITE_P(
