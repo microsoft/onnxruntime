66bc4ec18edb41a14e95142a6b99b925cd2cbe34
Randy Shuai <rashuai@microsoft.com>
2022-12-14 20:05:27 -0800

    tune zlib search path

--- tools/ci_build/github/windows/setup_env_zlib.bat
+++ tools/ci_build/github/windows/setup_env_zlib.bat
@@ -1,5 +1,5 @@
 @echo off
-set search_path=%1
+set search_path=%1\vcpkg\src\vcpkg\installed
 for /r %search_path% %%a in (*.dll) do (
   if "%%~nxa"=="zlib1.dll" (
     echo Found zlib1.dll under: %%~dpa
