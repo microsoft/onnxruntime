06506d9b6ed38f925d810194f528e52efc4b6681
Thomas Rondahl <thomas.rondahl@gmail.com>
2022-12-15 12:43:08 +0100

    Changes based on feedback
    * Revert migration of OrtEnv to separate file
    * Expand on documentation
    
    #13822

--- csharp/src/Microsoft.ML.OnnxRuntime/ThreadingOptions.Shared.cs
+++ csharp/src/Microsoft.ML.OnnxRuntime/ThreadingOptions.Shared.cs
@@ -5,14 +5,24 @@ namespace Microsoft.ML.OnnxRuntime
 {
     public class ThreadingOptions : SafeHandle
     {
+        /// <summary>
+        /// A pointer to a underlying native instance of ThreadingOptions
+        /// </summary>
         internal IntPtr Handle => handle;
         
-        
+        /// <summary>
+        /// Create an empty threading options.
+        /// </summary>
         public ThreadingOptions() : base(IntPtr.Zero, true)
         {
             NativeApiStatus.VerifySuccess(NativeMethods.OrtCreateThreadingOptions(out handle));
         }
 
+        /// <summary>
+        /// Set global inter-op thread count.
+        /// Setting it to 0 will allow ORT to choose the number of threads, setting it to 1 will cause the main thread to be used (i.e., no thread pools will be used).
+        /// </summary>
+        /// <param name="value">The number of threads.</param>
         public int GlobalInterOpNumThreads
         {
             get => _globalInterOpNumThreads;
@@ -24,6 +34,11 @@ namespace Microsoft.ML.OnnxRuntime
         }
         private int _globalInterOpNumThreads;
         
+        /// <summary>
+        /// Sets the number of threads available for intra-op parallelism (i.e. within a single op).
+        /// Setting it to 0 will allow ORT to choose the number of threads, setting it to 1 will cause the main thread to be used (i.e., no thread pools will be used).
+        /// </summary>
+        /// <param name="value">The number of threads.</param>
         public int GlobalIntraOpNumThreads
         {
             get => _globalIntraOpNumThreads;
@@ -35,6 +50,11 @@ namespace Microsoft.ML.OnnxRuntime
         }
         private int _globalIntraOpNumThreads;
         
+        /// <summary>
+        /// Allows spinning of thread pools when their queues are empty. This call sets the value for both inter-op and intra-op thread pools.
+        /// If the CPU usage is very high then do not enable this.
+        /// </summary>
+        /// <param name="value">If true allow the thread pools to spin.</param>
         public bool GlobalSpinControl
         {
             get => _globalSpinControl;
@@ -46,11 +66,19 @@ namespace Microsoft.ML.OnnxRuntime
         }
         private bool _globalSpinControl = true; // Default: spin is on
 
+        /// <summary>
+        /// When this is set it causes intra-op and inter-op thread pools to flush denormal values to zero.
+        /// </summary>
         public void SetGlobalDenormalAsZero()
         {
             NativeApiStatus.VerifySuccess(NativeMethods.OrtThreadingOptionsSetGlobalDenormalAsZero(handle));
         }
     
+        /// <summary>
+        /// Overrides SafeHandle.ReleaseHandle() to properly dispose of
+        /// the native instance of ThreadingOptions
+        /// </summary>
+        /// <returns>always returns true</returns>
         protected override bool ReleaseHandle()
         {
             NativeMethods.OrtReleaseThreadingOptions(handle);
