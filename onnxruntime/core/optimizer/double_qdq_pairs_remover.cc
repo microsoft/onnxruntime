c7e2cd4878a3f4851521680e8259de7dbf29a7b1
Jian Chen <cjian@microsoft.com>
2022-12-26 10:27:31 -0800

    Refactoring the name

--- onnxruntime/core/optimizer/double_qdq_pairs_remover.cc
+++ onnxruntime/core/optimizer/double_qdq_pairs_remover.cc
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 #include "double_qdq_pairs_remover.h"
+#include "core/graph/graph_utils.h"
 namespace onnxruntime {
 Status DoubleQDQPairsRemover::ApplyImpl(
     Graph& graph, bool& modified, int graph_level, const logging::Logger& logger) const {
@@ -16,4 +17,25 @@ Status DoubleQDQPairsRemover::ApplyImpl(
   }
   return Status::OK();
 }
+
+  bool DoubleQDQPairsRemover::IsPairDQQRemovable(const Node &dq_node, const Node &q_node) const {
+  auto dq_proto = dq_node.InputDefs()[1]->ToProto();
+  auto q_proto = q_node.OutputDefs()[1]->ToProto();
+  dq_proto.
+  return true;
+  }
+
+  std::vector<const Node *> DoubleQDQPairsRemover::GetQualifiedQChildren(const Node &dq_node) const {
+    auto q_children = graph_utils::FindChildrenByType(dq_node, "QuantizeLinear");
+    q_children.erase(
+        std::remove_if(
+            q_children.begin(),
+            q_children.end(),
+            [&]( const Node* q_node) {
+              return IsPairDQQRemovable(dq_node, *q_node);
+            }),
+        q_children.end());
+    return q_children;
+  }
+
 }  // namespace onnxruntime
--- onnxruntime/core/optimizer/double_qdq_pairs_remover.h
+++ onnxruntime/core/optimizer/double_qdq_pairs_remover.h
@@ -7,6 +7,10 @@
 #include "core/optimizer/graph_transformer.h"
 
 namespace onnxruntime {
+  /**
+   * @Class DoubleQDQPairsRemover
+   * @brief Remove one pair of Q-DQ Double Q-DQ pairs.
+   */
 class DoubleQDQPairsRemover : public GraphTransformer {
 public:
   DoubleQDQPairsRemover() : GraphTransformer("DoubleQDQPairsRemover", {}) {}
@@ -14,5 +18,7 @@ public:
 private:
   Status ApplyImpl(Graph& graph, bool& modified, int graph_level,
                    const logging::Logger& logger) const override;
+  bool IsPairDQQRemovable(const Node& dq_node, const Node& q_node) const;
+  std::vector<const Node*> GetQualifiedQChildren(const Node& dq_node) const;
 };
 } // namespace onnxruntime
