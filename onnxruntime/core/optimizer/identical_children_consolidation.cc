6d71a6ed62be50cf461db227bc0af2dabee7b953
Jian Chen <cjian@microsoft.com>
2022-12-29 13:17:01 -0800

    Updating split test cases

--- onnxruntime/core/optimizer/identical_children_consolidation.cc
+++ onnxruntime/core/optimizer/identical_children_consolidation.cc
@@ -7,35 +7,40 @@ namespace onnxruntime {
 Status IdenticalChildrenConsolidation::ApplyImpl(Graph& graph, bool& modified, int  /*graph_level*/, const logging::Logger&  /*logger*/) const {
   GraphViewer const graph_viewer(graph);
   for (auto node_index : graph_viewer.GetNodesInTopologicalOrder()) {
-    Node& node = *(graph.GetNode(node_index));
-    if(supported_parent_optypes.count(node.OpType()) == 0) { continue; }
-    if (graph.GetNode(node_index) == nullptr || node.GetOutputEdgesCount() < 2) { continue;}
-    auto identical_children_indexes = GetIndenticalChildrenSet(node);
-    if(identical_children_indexes.empty()) { continue;}
-    auto *first_child = graph.GetNode(identical_children_indexes[0]);
-    for (size_t i = 1; i < identical_children_indexes.size(); i++) {
-      auto *other_child = graph.GetNode(identical_children_indexes[i]);
-      //Currently Only support single output
-      graph_utils::ReplaceDownstreamNodeInput(graph, *other_child, 0, *first_child, 0);
-      graph_utils::RemoveNode(graph,*other_child);
-      modified = true;
+    Node* node = graph.GetNode(node_index);
+    if (IsSupportedParentOp(node)) {
+      auto identical_children_indexes = GetIdenticalChildrenIndexes(node);
+      if(identical_children_indexes.empty()) { continue;}
+      auto *first_child = graph.GetNode(identical_children_indexes[0]);
+      for (size_t i = 1; i < identical_children_indexes.size(); i++) {
+        auto *other_child = graph.GetNode(identical_children_indexes[i]);
+        //Currently Only support single output
+        graph_utils::ReplaceDownstreamNodeInput(graph, *other_child, 0, *first_child, 0);
+        graph_utils::RemoveNode(graph,*other_child);
+        modified = true;
+      }
     }
   }
   return Status::OK();
 }
 
-std::vector<NodeIndex> IdenticalChildrenConsolidation::GetIndenticalChildrenSet(Node &node) const {
+std::vector<NodeIndex> IdenticalChildrenConsolidation::GetIdenticalChildrenIndexes(Node *node) const {
   unordered_set<NodeIndex> identical_set;
-  for (auto i = node.OutputEdgesBegin(); i != std::prev(node.OutputEdgesEnd(),1); ++i) {
-    if(supported_children_optypes.count(i->GetNode().OpType()) == 0) {
-      continue; // this op is not currently supported
-    }
-    for (auto j = std::next(i); j != node.OutputEdgesEnd(); ++j) {
-      if (i->GetNode().OpType() == j->GetNode().OpType()) {
-        identical_set.insert({i->GetNode().Index(), j->GetNode().Index()});
+  for (auto i = node->OutputEdgesBegin(); i != std::prev(node->OutputEdgesEnd(),1); ++i) {
+    if(supported_children_optypes.count(i->GetNode().OpType()) > 0) {
+      for (auto j = std::next(i); j != node->OutputEdgesEnd(); ++j) {
+        if (i->GetNode().OpType() == j->GetNode().OpType()) {
+          identical_set.insert({i->GetNode().Index(), j->GetNode().Index()});
+        }
       }
     }
   }
-  return {identical_set.begin(),identical_set.end()};
+  return {identical_set.begin(), identical_set.end()};
 }
+
+  bool IdenticalChildrenConsolidation::IsSupportedParentOp(const Node *node) const {
+    return node != nullptr
+       && supported_parent_optypes.count(node->OpType()) != 0
+       && node->GetOutputEdgesCount() > 1;
+  }
 }
