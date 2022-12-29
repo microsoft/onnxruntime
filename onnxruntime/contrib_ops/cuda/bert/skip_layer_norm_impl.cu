0ab0ca3cef224aaf2354bb2daa61516f6e7e3e12
Ubuntu <wy@v100-2.0cdb2e52twzevn1i4fi45bylyg.jx.internal.cloudapp.net>
2022-12-29 22:25:49 +0000

    update

--- onnxruntime/contrib_ops/cuda/bert/skip_layer_norm_impl.cu
+++ onnxruntime/contrib_ops/cuda/bert/skip_layer_norm_impl.cu
@@ -152,7 +152,6 @@ Status LaunchSkipLayerNormKernel(
           <<<grid_size, block_size, 0, stream>>>(ld, input, skip, beta, gamma, bias,
                                                  maybe2half<T>(epsilon), output,
                                                  skip_input_add_output, hasBias, hasSkipInputAdditionOutput);
-      cudaDeviceSynchronize();
     } else if (ld <= 64) {
       constexpr int block_size = 64 / 2;
       SkipLayerNormKernelSmall<T, block_size, 2, Simplified>
