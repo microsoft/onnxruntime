68794d0ac168a162c0e74e1b135a600aa046b057
Adrian Lizarraga <adlizarraga@microsoft.com>
2023-01-04 17:56:29 -0800

    Improve custom op library handle cleanup (#14099)
    ### Description
    - Adds a new C API `OrtApi::RegisterCustomOpsLibrary_V2` that manages
    the lifetime of dynamic library handles (i.e., calls `dlclose` or
    `FreeLibrary`).
    - Deprecates C API `OrtApi::RegisterCustomOpsLibrary`.
    - Adds C++ API wrapper for convenient registering of custom op
    libraries.
    - `PySessionOptions` is now an alias of `OrtSessionOptions`
    
    ### Motivation and Context
    The current API for registering custom op libraries loads dynamic
    libraries but requires users to handle the release of the corresponding
    library handles. Additionally, the user has to make sure to release the
    library handle _after_ the session has been destroyed (or the program
    segfaults).
    
    The new API automatically cleans up the library and allows the user to
    write more straightforward code.

--- include/onnxruntime/core/session/onnxruntime_cxx_api.h
+++ include/onnxruntime/core/session/onnxruntime_cxx_api.h
@@ -478,6 +478,8 @@ struct SessionOptionsImpl : Base<T> {
   SessionOptionsImpl& SetCustomCreateThreadFn(OrtCustomCreateThreadFn ort_custom_create_thread_fn);  ///< Wraps OrtApi::SessionOptionsSetCustomCreateThreadFn
   SessionOptionsImpl& SetCustomThreadCreationOptions(void* ort_custom_thread_creation_options);      ///< Wraps OrtApi::SessionOptionsSetCustomThreadCreationOptions
   SessionOptionsImpl& SetCustomJoinThreadFn(OrtCustomJoinThreadFn ort_custom_join_thread_fn);        ///< Wraps OrtApi::SessionOptionsSetCustomJoinThreadFn
+
+  void RegisterCustomOpsLibrary(const ORTCHAR_T* library_name);  ///< Wraps OrtApi::RegisterCustomOpsLibrary_V2
 };
 }  // namespace detail
 
