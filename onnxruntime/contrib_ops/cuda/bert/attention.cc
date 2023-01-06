15b5d9468ec6a0075e1c37eece76d1d2e55b3601
Tianlei Wu <tlwu@microsoft.com>
2023-01-06 14:27:40 -0800

    Add CrossAttention operator (#14146)
    Move separated Q, K and V (without input projection) from Attention to a
    new operator CrossAttention.
    
    The Attention operator is hard to maintain when we need support with and
    without input projection in one class. Add a new operator according to
    feedback.
    
    Some change might need in the future, but not in this PR:
    (1) bias could be optional (We will not proceed that route unless
    experiments show that fusing Add bias with MatMul instead of this op
    could improve performance).
    (2) support packed KV. There are two ways to support it: when key and
    value are same Tensor, they are packed; or we can make value as
    optional, and use packed mode when value is empty and the key has packed
    K/V.
    (3) support cached key and value, and other (like relative position
    bias), or more attention mask format. They can be added easily without
    breaking backward compatible.
    (4) ROCm/CPU implementation of this op.

--- onnxruntime/contrib_ops/cuda/bert/attention.cc
+++ onnxruntime/contrib_ops/cuda/bert/attention.cc
@@ -16,26 +16,28 @@ namespace onnxruntime {
 namespace contrib {
 namespace cuda {
 
-#define PastSequenceLength 8
-#define REGISTER_KERNEL_TYPED(T)                                  \
-  ONNX_OPERATOR_TYPED_KERNEL_EX(                                  \
-      Attention,                                                  \
-      kMSDomain,                                                  \
-      1,                                                          \
-      T,                                                          \
-      kCudaExecutionProvider,                                     \
-      (*KernelDefBuilder::Create())                               \
-          .MayInplace(4, 1)                                       \
-          .TypeConstraint("T", DataTypeImpl::GetTensorType<T>())  \
-          .InputMemoryType(OrtMemTypeCPUInput, PastSequenceLength)\
-      ,                                                           \
+constexpr int kPastSequenceLengthInputIndex = 6;
+constexpr int kPastInputIndex = 4;
+constexpr int kPresentOutputIndex = 1;
+
+#define REGISTER_KERNEL_TYPED(T)                                               \
+  ONNX_OPERATOR_TYPED_KERNEL_EX(                                               \
+      Attention,                                                               \
+      kMSDomain,                                                               \
+      1,                                                                       \
+      T,                                                                       \
+      kCudaExecutionProvider,                                                  \
+      (*KernelDefBuilder::Create())                                            \
+          .MayInplace(kPastInputIndex, kPresentOutputIndex)                    \
+          .TypeConstraint("T", DataTypeImpl::GetTensorType<T>())               \
+          .InputMemoryType(OrtMemTypeCPUInput, kPastSequenceLengthInputIndex), \
       Attention<T>);
 
 REGISTER_KERNEL_TYPED(float)
 REGISTER_KERNEL_TYPED(MLFloat16)
 
 template <typename T>
-Attention<T>::Attention(const OpKernelInfo& info) : CudaKernel(info), AttentionBase(info, false, false) {
+Attention<T>::Attention(const OpKernelInfo& info) : CudaKernel(info), AttentionBase(info, false) {
   disable_fused_runner_ = sizeof(T) != 2 ||
                           ParseEnvironmentVariableWithDefault<bool>(attention::kDisableFusedAttention, false);
 
@@ -49,22 +51,18 @@ Status Attention<T>::ComputeInternal(OpKernelContext* context) const {
   const Tensor* weights = context->Input<Tensor>(1);
   const Tensor* bias = context->Input<Tensor>(2);
   const Tensor* mask_index = context->Input<Tensor>(3);
-  const Tensor* past = context->Input<Tensor>(4);
+  const Tensor* past = context->Input<Tensor>(kPastInputIndex);
   const Tensor* extra_add_qk = context->Input<Tensor>(5);
-  const Tensor* key = context->Input<Tensor>(6);
-  const Tensor* value = context->Input<Tensor>(7);
-  const Tensor* past_seq_len = context->Input<Tensor>(8);
+  const Tensor* past_seq_len = context->Input<Tensor>(kPastSequenceLengthInputIndex);
 
   auto& device_prop = GetDeviceProp();
   AttentionParameters parameters;
   ORT_RETURN_IF_ERROR(CheckInputs(input->Shape(),
-                                  nullptr == weights ? nullptr : &(weights->Shape()),
+                                  weights->Shape(),
                                   bias->Shape(),
                                   mask_index,
                                   past,
                                   extra_add_qk,
-                                  key,
-                                  value,
                                   &parameters,
                                   device_prop.maxThreadsPerBlock,
                                   past_seq_len));
@@ -79,25 +77,25 @@ Status Attention<T>::ComputeInternal(OpKernelContext* context) const {
   Tensor* output = context->Output(0, output_shape);
 
   std::vector<int64_t> present_dims{
-    2, parameters.batch_size, parameters.num_heads,
-    past_present_share_buffer_ ? parameters.max_sequence_length : parameters.total_sequence_length,
-    parameters.head_size};
+      2, parameters.batch_size, parameters.num_heads,
+      past_present_share_buffer_ ? parameters.max_sequence_length : parameters.total_sequence_length,
+      parameters.head_size};
   TensorShape present_shape(present_dims);
-  Tensor* present = context->Output(1, present_shape);
+  Tensor* present = context->Output(kPresentOutputIndex, present_shape);
 
   MHARunner* fused_runner = nullptr;
 
 #ifndef ENABLE_TRAINING  // Only enable fused kernel on non-training builds
   // Check whether we can use fused kernel
   int sm = device_prop.major * 10 + device_prop.minor;
-  bool is_1d_mask = nullptr != mask_index && mask_index->Shape().NumDimensions() == 1;
+  bool is_mask_1d_seq_len = parameters.mask_type == AttentionMaskType::MASK_1D_KEY_SEQ_LEN;
 
   if (is_unidirectional_) {  // GPT
     // Fused kernels requires left side padding (The mask shall be sequence lengths or no mask)
     // Fused kernels don't support different sequence lengths of q and kv, so only apply to the first token
     // where past state is empty.
     bool use_causal_fused_runner = !disable_fused_runner_ &&
-                                   (nullptr == mask_index || is_1d_mask) &&
+                                   (nullptr == mask_index || is_mask_1d_seq_len) &&
                                    nullptr == extra_add_qk &&
                                    parameters.past_sequence_length == 0 &&
                                    parameters.hidden_size == parameters.v_hidden_size &&
@@ -115,7 +113,7 @@ Status Attention<T>::ComputeInternal(OpKernelContext* context) const {
     }
   } else {  // BERT
     bool use_fused_runner = !disable_fused_runner_ &&
-                            (nullptr == mask_index || is_1d_mask) &&
+                            (nullptr == mask_index || is_mask_1d_seq_len) &&
                             nullptr == past &&
                             nullptr == present &&
                             nullptr == extra_add_qk &&
@@ -145,23 +143,21 @@ Status Attention<T>::ComputeInternal(OpKernelContext* context) const {
   typedef typename ToCudaType<T>::MappedType CudaT;
 
   IAllocatorUniquePtr<T> gemm_buffer;
-  if (weights != nullptr) {
-    int m = batch_size * sequence_length;
-    int n = (parameters.hidden_size + parameters.hidden_size + parameters.v_hidden_size);
-    int k = parameters.input_hidden_size;
-    gemm_buffer = GetScratchBuffer<T>(static_cast<size_t>(m) * n, context->GetComputeStream());
+  int m = batch_size * sequence_length;
+  int n = (parameters.hidden_size + parameters.hidden_size + parameters.v_hidden_size);
+  int k = parameters.input_hidden_size;
+  gemm_buffer = GetScratchBuffer<T>(static_cast<size_t>(m) * n, context->GetComputeStream());
 
-    CudaT one = ToCudaType<T>::FromFloat(1.0f);
-    CudaT zero = ToCudaType<T>::FromFloat(0.0f);
+  CudaT one = ToCudaType<T>::FromFloat(1.0f);
+  CudaT zero = ToCudaType<T>::FromFloat(0.0f);
 
-    // Gemm, note that CUDA assumes col-major, so result(N, M) = 1 * weights x input + 1 x bias
-    // The bias part is not included here since we fuse bias, transpose and output 3 matrice into one cuda kernel.
-    CUBLAS_RETURN_IF_ERROR(cublasGemmHelper(
-        cublas, CUBLAS_OP_N, CUBLAS_OP_N, n, m, k, &one,
-        reinterpret_cast<const CudaT*>(weights->Data<T>()), n,
-        reinterpret_cast<const CudaT*>(input->Data<T>()), k,
-        &zero, reinterpret_cast<CudaT*>(gemm_buffer.get()), n, device_prop));
-  }
+  // Gemm, note that CUDA assumes col-major, so result(N, M) = 1 * weights x input + 1 x bias
+  // The bias part is not included here since we fuse bias, transpose and output 3 matrice into one cuda kernel.
+  CUBLAS_RETURN_IF_ERROR(cublasGemmHelper(
+      cublas, CUBLAS_OP_N, CUBLAS_OP_N, n, m, k, &one,
+      reinterpret_cast<const CudaT*>(weights->Data<T>()), n,
+      reinterpret_cast<const CudaT*>(input->Data<T>()), k,
+      &zero, reinterpret_cast<CudaT*>(gemm_buffer.get()), n, device_prop));
 
   constexpr size_t element_size = sizeof(T);
   size_t workSpaceSize = GetAttentionWorkspaceSize(element_size,
@@ -177,11 +173,11 @@ Status Attention<T>::ComputeInternal(OpKernelContext* context) const {
 
   typedef typename ToCudaType<T>::MappedType CudaT;
   AttentionData<CudaT> data;
-  data.gemm_buffer = (nullptr == weights) ? nullptr : reinterpret_cast<CudaT*>(gemm_buffer.get());
+  data.gemm_buffer = reinterpret_cast<CudaT*>(gemm_buffer.get());
   data.bias = reinterpret_cast<const CudaT*>(bias->Data<T>());
-  data.query = (nullptr != weights) ? nullptr : reinterpret_cast<const CudaT*>(input->Data<T>());
-  data.key = (nullptr == key) ? nullptr : reinterpret_cast<const CudaT*>(key->Data<T>());
-  data.value = (nullptr == value) ? nullptr : reinterpret_cast<const CudaT*>(value->Data<T>());
+  data.query = nullptr;
+  data.key = nullptr;
+  data.value = nullptr;
   data.mask_index = (nullptr == mask_index) ? nullptr : mask_index->Data<int>();
   data.mask_index_dims = (nullptr == mask_index) ? gsl::span<const int64_t>() : mask_index->Shape().GetDims();
   data.past = (nullptr == past) ? nullptr : reinterpret_cast<const CudaT*>(past->Data<T>());
@@ -191,7 +187,7 @@ Status Attention<T>::ComputeInternal(OpKernelContext* context) const {
   data.present = (nullptr == present) ? nullptr : reinterpret_cast<CudaT*>(present->MutableData<T>());
 
   return QkvToContext<CudaT>(
-    device_prop, cublas, Stream(context), parameters, data, reinterpret_cast<void*>(fused_runner), past_present_share_buffer_);
+      device_prop, cublas, Stream(context), parameters, data, reinterpret_cast<void*>(fused_runner), past_present_share_buffer_);
 }
 
 }  // namespace cuda
--- onnxruntime/core/providers/shared_library/provider_bridge_provider.cc
+++ onnxruntime/core/providers/shared_library/provider_bridge_provider.cc
@@ -562,19 +562,16 @@ Status LongformerAttentionBase::CheckInputs(const TensorShape& input_shape,
 }
 
 Status AttentionBase::CheckInputs(const TensorShape& input_shape,
-                                  const TensorShape* weights_shape,
+                                  const TensorShape& weights_shape,
                                   const TensorShape& bias_shape,
                                   const Tensor*& mask_index,
                                   const Tensor* past,
                                   const Tensor* extra_add_qk,
-                                  const Tensor* key,
-                                  const Tensor* value,
                                   void* parameters,
                                   const int max_threads_per_block,
                                   const Tensor* past_seq_len) const {
   return g_host_cpu.AttentionBase__CheckInputs(this, input_shape, weights_shape, bias_shape,
-                                               mask_index, past, extra_add_qk,
-                                               key, value, parameters,
+                                               mask_index, past, extra_add_qk, parameters,
                                                max_threads_per_block, past_seq_len);
 }
 Tensor* AttentionBase::GetPresent(OpKernelContext* context, const Tensor* past, int batch_size, int head_size,
