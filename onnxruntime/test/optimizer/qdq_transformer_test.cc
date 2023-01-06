cf91ea4c3c59fef9b7dc6cabd707840ad558d1b1
Jian Chen <cjian@microsoft.com>
2023-01-06 09:03:10 -0800

    Consolidate Identical Children Nodes (#14026)
    ### Description
    In case where Q have multiple DQ children, we want to keep only 1 DQ.
    The only remaining DQ's will channel its output to deleted DQ children's
    outputs.
    
    ex
    Q->N(DQ).    => Q->DQ
    
    
    ### Motivation and Context
    <!-- - Why is this change required? What problem does it solve?
    - If it fixes an open issue, please link to the issue here. -->

--- onnxruntime/test/optimizer/qdq_transformer_test.cc
+++ onnxruntime/test/optimizer/qdq_transformer_test.cc
@@ -764,6 +764,7 @@ TEST(QDQTransformerTests, Gather) {
   test_case({12, 37}, {24, 12});
 }
 
+// Because split isn't one the supported ops, this will stay the same
 TEST(QDQTransformerTests, Split) {
   auto test_case = [&](const std::vector<int64_t>& input_shape, const int64_t& axis) {
     auto check_graph = [&](InferenceSessionWrapper& session) {
@@ -779,20 +780,55 @@ TEST(QDQTransformerTests, Split) {
   };
   test_case({6, 18, 54}, 0);
 }
+
+// Because split isn't one the supported ops, this will stay the same
+TEST(QDQTransformerTests, Split_without_IdenticalChildrenConsolidation) {
+  auto test_case = [&](const std::vector<int64_t>& input_shape, const int64_t& axis) {
+    auto check_graph = [&](InferenceSessionWrapper& session) {
+      auto op_to_count = CountOpsInGraph(session.GetGraph());
+      EXPECT_EQ(op_to_count["Split"], 1);
+      EXPECT_EQ(op_to_count["QuantizeLinear"], 1);
+      EXPECT_EQ(op_to_count["DequantizeLinear"], 3);
+    };
+    TransformerTester(BuildConsolidationTestCase<int8_t, int8_t>(input_shape, axis),
+                      check_graph,
+                      TransformerLevel::Level1,
+                      TransformerLevel::Level2, 12, {}, {}, nullptr, {},
+                      {"IdenticalChildrenConsolidation"});
+  };
+  test_case({6, 18, 54}, 0);
+}
+
+TEST(QDQTransformerTests, Split_with_IdenticalChildrenConsolidation) {
+  auto test_case = [&](const std::vector<int64_t>& input_shape, const int64_t& axis) {
+    auto check_graph = [&](InferenceSessionWrapper& session) {
+      auto op_to_count = CountOpsInGraph(session.GetGraph());
+      EXPECT_EQ(op_to_count["Split"], 1);
+      EXPECT_EQ(op_to_count["QuantizeLinear"], 1);
+      EXPECT_EQ(op_to_count["DequantizeLinear"], 3);
+    };
+    TransformerTester(BuildConsolidationTestCase<int8_t, int8_t>(input_shape, axis),
+                      check_graph,
+                      TransformerLevel::Level1,
+                      TransformerLevel::Level2);
+  };
+  test_case({6, 18, 54}, 0);
+}
+
 TEST(QDQTransformerTests, Where) {
-  auto test_case = [&](const std::vector<int64_t>& cond_shape, const std::vector<int64_t>& x_shape,const std::vector<int64_t>& y_shape) {
+  auto test_case = [&](const std::vector<int64_t>& cond_shape, const std::vector<int64_t>& x_shape, const std::vector<int64_t>& y_shape) {
     auto check_graph = [&](InferenceSessionWrapper& session) {
       auto op_to_count = CountOpsInGraph(session.GetGraph());
       EXPECT_EQ(op_to_count["com.microsoft.QLinearWhere"], 1);
       EXPECT_EQ(op_to_count["QuantizeLinear"], 0);
       EXPECT_EQ(op_to_count["DequantizeLinear"], 0);
     };
-    TransformerTester(BuildQDQWhereTestCase<int8_t>(cond_shape,x_shape,y_shape),
+    TransformerTester(BuildQDQWhereTestCase<int8_t>(cond_shape, x_shape, y_shape),
                       check_graph,
                       TransformerLevel::Level1,
                       TransformerLevel::Level2);
   };
-  test_case({1},{1},{1});
+  test_case({1}, {1}, {1});
 }
 
 TEST(QDQTransformerTests, Transpose) {
