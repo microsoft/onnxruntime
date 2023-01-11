0cd1623ba67240afbdfa3356cb0ba2f0d6f298c1
Yufeng Li <liyufeng1987@gmail.com>
2023-01-10 20:40:55 -0800

    fix type of BlockPrefixCallbackOp

--- onnxruntime/contrib_ops/cuda/transformers/generation_cuda_impl.cu
+++ onnxruntime/contrib_ops/cuda/transformers/generation_cuda_impl.cu
@@ -469,14 +469,13 @@ template void LaunchSortPairs(void* d_temp_storage,
 // A stateful callback functor that maintains a running prefix to be applied
 // during consecutive scan operations.
 struct BlockPrefixCallbackOp {
-  // Running prefix
-  int running_total;
-  // Constructor
-  __device__ BlockPrefixCallbackOp(int running_total) : running_total(running_total) {}
+  float running_total; // running prefix
+
+  __device__ BlockPrefixCallbackOp(float running_total) : running_total(running_total) {}
   // Callback operator to be entered by the first warp of threads in the block.
   // Thread-0 is responsible for returning a value for seeding the block-wide scan.
-  __device__ int operator()(int block_aggregate) {
-    int old_prefix = running_total;
+  __device__ float operator()(float block_aggregate) {
+    float old_prefix = running_total;
     running_total += block_aggregate;
     return old_prefix;
   }
