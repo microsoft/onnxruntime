868cacd2ab2b48bd813fd0acfa0d6e311fe6defa
Ubuntu <wy@v100-2.0cdb2e52twzevn1i4fi45bylyg.jx.internal.cloudapp.net>
2023-01-12 06:17:55 +0000

    update

--- onnxruntime/test/contrib_ops/sampling_test.cc
+++ onnxruntime/test/contrib_ops/sampling_test.cc
@@ -13,15 +13,26 @@ extern std::unique_ptr<Ort::Env> ort_env;
 namespace onnxruntime {
 namespace test {
 
-static void GPT2WithSamplingTest(
-    std::vector<int32_t>& input_ids,
-    std::vector<int32_t>& max_length,
-    std::vector<int32_t>& min_length,
-    std::vector<float>& repetition_penalty,
-    const int64_t batch_size,
-    const int64_t sequence_length,
-    std::vector<int32_t>& expected_output,
-    bool use_cuda) {
+#ifdef USE_CUDA
+TEST(SamplingTest, Gpt2Sampling_CUDA) {
+  std::vector<int32_t> input_ids{
+      0, 0, 0, 0, 0, 52, 195, 731, 321, 301, 734, 620,
+      41, 554, 74, 622, 206, 222, 75, 223, 221, 198, 224, 572,
+      0, 0, 0, 52, 328, 219, 328, 206, 288, 227, 896, 328};
+
+  std::vector<int32_t> max_length{15};
+  std::vector<int32_t> min_length{1};
+  std::vector<float> repetition_penalty{1.0f};
+
+
+  std::vector<int32_t> expected_output{
+      0, 0, 0, 0, 0, 52, 195, 731, 321, 301, 734, 620, 125, 543, 668,
+      41, 554, 74, 622, 206, 222, 75, 223, 221, 198, 224, 572, 776, 213, 697,
+      0, 0, 0, 52, 328, 219, 328, 206, 288, 227, 896, 328, 450};
+
+  const int64_t batch_size = 3;
+  const int64_t sequence_length = 12;
+
   std::vector<int64_t> input_ids_shape{batch_size, sequence_length};
 
   std::vector<int64_t> parameter_shape{1};
@@ -50,49 +61,28 @@ static void GPT2WithSamplingTest(
   const char* const output_names[] = {"sequences"};
 
   Ort::SessionOptions session_options;
-  if (use_cuda) {
-    Ort::ThrowOnError(OrtSessionOptionsAppendExecutionProvider_CUDA(session_options, 0));
-  }
-
-  Ort::Session session(*ort_env, ORT_TSTR("testdata/transformers/tiny_gpt2_sampling.onnx"), session_options);
-
-  auto ort_outputs = session.Run(Ort::RunOptions{}, input_names, ort_inputs.data(), ort_inputs.size(),
-                                 output_names, 1);
-
-  ASSERT_EQ(ort_outputs.size(), 1U);
-  const auto& sequences = ort_outputs[0];
-  ASSERT_TRUE(sequences.IsTensor());
-
-  auto result_ts = sequences.GetTensorTypeAndShapeInfo();
-  ASSERT_EQ(ONNX_TENSOR_ELEMENT_DATA_TYPE_INT32, result_ts.GetElementType());
-
-  ASSERT_EQ(expected_output_shape, result_ts.GetShape());
-  const auto* result_vals = sequences.GetTensorData<int32_t>();
-  auto result_span = gsl::make_span(result_vals, expected_output.size());
-
-  ASSERT_TRUE(std::equal(expected_output.cbegin(), expected_output.cend(), result_span.begin(), result_span.end()));
-}
-
-#ifdef USE_CUDA
-TEST(SamplingTest, Gpt2Sampling_CUDA) {
-  std::vector<int32_t> input_ids{
-      0, 0, 0, 0, 0, 52, 195, 731, 321, 301, 734, 620,
-      41, 554, 74, 622, 206, 222, 75, 223, 221, 198, 224, 572,
-      0, 0, 0, 52, 328, 219, 328, 206, 288, 227, 896, 328};
-
-  std::vector<int32_t> max_length{15};
-  std::vector<int32_t> min_length{1};
-  std::vector<float> repetition_penalty{1.0f};
-
-
-  std::vector<int32_t> expected_output{
-      0, 0, 0, 0, 0, 52, 195, 731, 321, 301, 734, 620, 125, 543, 668,
-      41, 554, 74, 622, 206, 222, 75, 223, 221, 198, 224, 572, 776, 213, 697,
-      0, 0, 0, 52, 328, 219, 328, 206, 288, 227, 896, 328, 450};
-
   constexpr int min_cuda_architecture = 530;
-  GPT2WithSamplingTest(input_ids, max_length, min_length, repetition_penalty, 3, 12, expected_output,
-                       HasCudaEnvironment(min_cuda_architecture));
+  if (HasCudaEnvironment(min_cuda_architecture)) {
+    Ort::ThrowOnError(OrtSessionOptionsAppendExecutionProvider_CUDA(session_options, 0));
+
+    Ort::Session session(*ort_env, ORT_TSTR("testdata/transformers/tiny_gpt2_sampling.onnx"), session_options);
+
+    auto ort_outputs = session.Run(Ort::RunOptions{}, input_names, ort_inputs.data(), ort_inputs.size(),
+                                   output_names, 1);
+
+    ASSERT_EQ(ort_outputs.size(), 1U);
+    const auto& sequences = ort_outputs[0];
+    ASSERT_TRUE(sequences.IsTensor());
+
+    auto result_ts = sequences.GetTensorTypeAndShapeInfo();
+    ASSERT_EQ(ONNX_TENSOR_ELEMENT_DATA_TYPE_INT32, result_ts.GetElementType());
+
+    ASSERT_EQ(expected_output_shape, result_ts.GetShape());
+    const auto* result_vals = sequences.GetTensorData<int32_t>();
+    auto result_span = gsl::make_span(result_vals, expected_output.size());
+
+    ASSERT_TRUE(std::equal(expected_output.cbegin(), expected_output.cend(), result_span.begin(), result_span.end()));
+  }
 }
 #endif
 
@@ -112,7 +102,53 @@ TEST(SamplingTest, Gpt2Sampling_CPU) {
       41, 554, 74, 622, 206, 222, 75, 223, 221, 198, 224, 572, 475, 944, 527,
       0, 0, 0, 52, 328, 219, 328, 206, 288, 227, 896, 328, 210};
 
-  GPT2WithSamplingTest(input_ids, max_length, min_length, repetition_penalty, 3, 12, expected_output, false);
+  const int64_t batch_size = 3;
+  const int64_t sequence_length = 12;
+  std::vector<int64_t> input_ids_shape{batch_size, sequence_length};
+
+  std::vector<int64_t> parameter_shape{1};
+
+  std::vector<int64_t> expected_output_shape{input_ids_shape[0], max_length[0]};
+
+  Ort::MemoryInfo info("Cpu", OrtDeviceAllocator, 0, OrtMemTypeDefault);
+  auto input_ids_tensor = Ort::Value::CreateTensor(
+      info, input_ids.data(), input_ids.size(), input_ids_shape.data(), input_ids_shape.size());
+
+  auto max_length_tensor = Ort::Value::CreateTensor(
+      info, max_length.data(), max_length.size(), parameter_shape.data(), parameter_shape.size());
+
+  auto min_length_tensor = Ort::Value::CreateTensor(
+      info, min_length.data(), min_length.size(), parameter_shape.data(), parameter_shape.size());
+
+  auto repetition_penalty_tensor = Ort::Value::CreateTensor(
+      info, repetition_penalty.data(), repetition_penalty.size(), parameter_shape.data(), parameter_shape.size());
+
+  std::vector<Ort::Value> ort_inputs;
+  ort_inputs.push_back(std::move(input_ids_tensor));
+  ort_inputs.push_back(std::move(max_length_tensor));
+  ort_inputs.push_back(std::move(min_length_tensor));
+  ort_inputs.push_back(std::move(repetition_penalty_tensor));
+  const char* input_names[] = {"input_ids", "max_length", "min_length", "repetition_penalty"};
+  const char* const output_names[] = {"sequences"};
+
+  Ort::SessionOptions session_options;
+  Ort::Session session(*ort_env, ORT_TSTR("testdata/transformers/tiny_gpt2_sampling.onnx"), session_options);
+
+  auto ort_outputs = session.Run(Ort::RunOptions{}, input_names, ort_inputs.data(), ort_inputs.size(),
+                                 output_names, 1);
+
+  ASSERT_EQ(ort_outputs.size(), 1U);
+  const auto& sequences = ort_outputs[0];
+  ASSERT_TRUE(sequences.IsTensor());
+
+  auto result_ts = sequences.GetTensorTypeAndShapeInfo();
+  ASSERT_EQ(ONNX_TENSOR_ELEMENT_DATA_TYPE_INT32, result_ts.GetElementType());
+
+  ASSERT_EQ(expected_output_shape, result_ts.GetShape());
+  const auto* result_vals = sequences.GetTensorData<int32_t>();
+  auto result_span = gsl::make_span(result_vals, expected_output.size());
+
+  ASSERT_TRUE(std::equal(expected_output.cbegin(), expected_output.cend(), result_span.begin(), result_span.end()));
 }
 #endif
 }  // namespace test
