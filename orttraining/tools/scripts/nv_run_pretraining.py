da3ebcfc246b2f291b74518d1e0624db543dbd0a
Justin Chu <justinchu@microsoft.com>
2023-01-16 04:37:49 +0000

    Remove future

--- orttraining/tools/scripts/nv_run_pretraining.py
+++ orttraining/tools/scripts/nv_run_pretraining.py
@@ -15,9 +15,6 @@
 # limitations under the License.
 """BERT finetuning runner."""
 
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
 
 # ==================
 import csv
