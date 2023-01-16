213cfb626bd12de2df143a8c0196028e21ee1a87
Justin Chu <justinchu@microsoft.com>
2023-01-16 17:34:08 +0000

    Fix init

--- orttraining/orttraining/python/training/__init__.py
+++ orttraining/orttraining/python/training/__init__.py
@@ -2,15 +2,16 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 # --------------------------------------------------------------------------
-
+# isort:skip_file
 from onnxruntime.capi._pybind_state import PropagateCastOpsStrategy, TrainingParameters
 from onnxruntime.capi.training.training_session import TrainingSession
 
-from . import amp, checkpoint, model_desc_validation, optim
-from .orttrainer import ORTTrainer, TrainStepInfo
-
 # Options need to be imported before `ORTTrainer`.
 from .orttrainer_options import ORTTrainerOptions
+from .orttrainer import ORTTrainer, TrainStepInfo
+
+from . import amp, checkpoint, model_desc_validation, optim
+
 
 try:
     from .ortmodule import ORTModule
