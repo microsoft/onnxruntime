adbeab8919a7113d6b3de484e7750f5844718874
Adrian Lizarraga <adlizarraga@microsoft.com>
2023-01-17 15:55:56 -0800

    Clean up TensorRT deprecations, warnings, unbounded string copy (#14148)
    ### Description
    - Updates deprecated use of `nvinfer1::___::destroy()` by using a
    `std::unique_ptr<>` instead of our own smart pointer that calls
    `destroy`. See [TensorRT deprecation
    list](https://docs.nvidia.com/deeplearning/tensorrt/api/c_api/deprecated.html#:~:text=Deprecated%20prior%20to%20TensorRT%208.0%20and%20will%20be,noexcept%20Use%20addMatrixMultiply%20instead.%20Deprecated%20in%20TensorRT%208.4.)
    and search for `destroy`.
    - Fixes warnings regarding uninitialized member variables.
    - Fixes bugs in TensorRT model ID generation:
      - Potential segfault when model path only has a root component.
      - Unbounded string copy for non-Windows builds.
    
    
    ### Motivation and Context
    Clean up

--- onnxruntime/core/providers/tensorrt/tensorrt_execution_provider.h
+++ onnxruntime/core/providers/tensorrt/tensorrt_execution_provider.h
@@ -87,33 +87,33 @@ struct TensorrtFuncState {
   DestroyFunc test_release_func = nullptr;
   AllocatorHandle allocator = nullptr;
   tensorrt_ptr::unique_pointer<nvonnxparser::IParser>* parser = nullptr;
-  tensorrt_ptr::unique_pointer<nvinfer1::ICudaEngine>* engine = nullptr;
-  tensorrt_ptr::unique_pointer<nvinfer1::IExecutionContext>* context = nullptr;
-  tensorrt_ptr::unique_pointer<nvinfer1::IBuilder>* builder = nullptr;
-  tensorrt_ptr::unique_pointer<nvinfer1::INetworkDefinition>* network = nullptr;
+  std::unique_ptr<nvinfer1::ICudaEngine>* engine = nullptr;
+  std::unique_ptr<nvinfer1::IExecutionContext>* context = nullptr;
+  std::unique_ptr<nvinfer1::IBuilder>* builder = nullptr;
+  std::unique_ptr<nvinfer1::INetworkDefinition>* network = nullptr;
   std::vector<std::unordered_map<std::string, size_t>> input_info;
   std::vector<std::unordered_map<std::string, size_t>> output_info;
   std::unordered_map<std::string, std::unordered_map<size_t, std::pair<int64_t, int64_t>>> input_shape_ranges;
   OrtMutex* tensorrt_mu_ptr = nullptr;
-  bool fp16_enable;
-  bool int8_enable;
-  bool int8_calibration_cache_available;
-  bool dla_enable;
-  int dla_core;
+  bool fp16_enable = false;
+  bool int8_enable = false;
+  bool int8_calibration_cache_available = false;
+  bool dla_enable = false;
+  int dla_core = 0;
   size_t* max_workspace_size_ptr = nullptr;
   std::string trt_node_name_with_precision;
-  bool engine_cache_enable;
+  bool engine_cache_enable = false;
   std::string engine_cache_path;
   nvinfer1::IRuntime* runtime = nullptr;
   nvinfer1::IOptimizationProfile* trt_profile = nullptr;
   AllocatorPtr scratch_allocator;
-  bool context_memory_sharing_enable;
+  bool context_memory_sharing_enable = false;
   size_t* max_context_mem_size_ptr = nullptr;
   IAllocatorUniquePtr<void>* context_memory = nullptr;
   std::unordered_map<std::string, float> dynamic_range_map;
-  bool engine_decryption_enable;
-  int (*engine_decryption)(const char*, char*, size_t*);
-  int (*engine_encryption)(const char*, char*, size_t);
+  bool engine_decryption_enable = false;
+  int (*engine_decryption)(const char*, char*, size_t*) = nullptr;
+  int (*engine_encryption)(const char*, char*, size_t) = nullptr;
 };
 
 // Logical device representation.
@@ -164,7 +164,7 @@ class TensorrtExecutionProvider : public IExecutionProvider {
   bool dump_subgraphs_ = false;
   bool engine_cache_enable_ = false;
   std::string cache_path_, engine_decryption_lib_path_;
-  tensorrt_ptr::unique_pointer<nvinfer1::IRuntime> runtime_ = nullptr;
+  std::unique_ptr<nvinfer1::IRuntime> runtime_ = nullptr;
   OrtMutex tensorrt_mu_;
   int device_id_;
   AllocatorPtr allocator_;
@@ -172,17 +172,17 @@ class TensorrtExecutionProvider : public IExecutionProvider {
   bool layer_norm_fp32_fallback_ = false;
   size_t max_ctx_mem_size_ = 0;
   IAllocatorUniquePtr<void> context_memory_ = nullptr;
-  mutable char model_path_[4096];  // Reserved for max path length
+  mutable char model_path_[4096] = {};  // Reserved for max path length
   bool engine_decryption_enable_ = false;
-  int (*engine_decryption_)(const char*, char*, size_t*);
-  int (*engine_encryption_)(const char*, char*, size_t);
+  int (*engine_decryption_)(const char*, char*, size_t*) = nullptr;
+  int (*engine_encryption_)(const char*, char*, size_t) = nullptr;
 
   std::unordered_set<std::string> control_flow_op_set_ = {"If", "Loop", "Scan"};
   std::unordered_map<std::string, tensorrt_ptr::unique_pointer<nvonnxparser::IParser>> parsers_;
-  std::unordered_map<std::string, tensorrt_ptr::unique_pointer<nvinfer1::ICudaEngine>> engines_;
-  std::unordered_map<std::string, tensorrt_ptr::unique_pointer<nvinfer1::IExecutionContext>> contexts_;
-  std::unordered_map<std::string, tensorrt_ptr::unique_pointer<nvinfer1::IBuilder>> builders_;
-  std::unordered_map<std::string, tensorrt_ptr::unique_pointer<nvinfer1::INetworkDefinition>> networks_;
+  std::unordered_map<std::string, std::unique_ptr<nvinfer1::ICudaEngine>> engines_;
+  std::unordered_map<std::string, std::unique_ptr<nvinfer1::IExecutionContext>> contexts_;
+  std::unordered_map<std::string, std::unique_ptr<nvinfer1::IBuilder>> builders_;
+  std::unordered_map<std::string, std::unique_ptr<nvinfer1::INetworkDefinition>> networks_;
   std::unordered_map<std::string, std::vector<std::unordered_map<std::string, size_t>>> input_info_;
   std::unordered_map<std::string, std::vector<std::unordered_map<std::string, size_t>>> output_info_;
   std::unordered_map<std::string, std::unordered_map<std::string, std::unordered_map<size_t, std::pair<int64_t, int64_t>>>> input_shape_ranges_;
