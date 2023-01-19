b1bfe455f08eb67aeca2f1ad501469e3d8dd8ab5
Wei-Sheng Chin <wschin@outlook.com>
2023-01-19 22:30:47 +0000

    Try format

--- java/src/main/java/ai/onnxruntime/OrtSession.java
+++ java/src/main/java/ai/onnxruntime/OrtSession.java
@@ -1001,17 +1001,12 @@ public class OrtSession implements AutoCloseable {
     }
 
     /**
-     * Adds Xnnpack as an execution backend. Needs to list all options hereif a new option
-     * supported. current supported options: {}
+     * Adds Xnnpack as an execution backend. Needs to list all options hereif a
+     * new option supported. current supported options: {}
      * The maximum number of provider options is set to 128 (see addExecutionProvider's
      * comment). This number is controlled by ORT_JAVA_MAX_ARGUMENT_ARRAY_LENGTH in
      * ai_onnxruntime_OrtSession_SessionOptions.c. If 128 is not enough, please increase
      * it or implementing an incremental way to add more options.
-     * Pseudocode for incremental way to add more options:
-     *  auto ep = addExecutionProvider("XNNPACK");
-     *  ep.add_option("key1", "value1");
-     *  ep.add_option("key2", "value2");
-     *  ep.initialize()
      *
      * @param providerOptions options pass to XNNPACK EP for initialization.
      * @throws OrtException If there was an error in native code.
