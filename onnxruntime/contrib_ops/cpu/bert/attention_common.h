d73a0ff75f7ecb034c28bd3ffb132714d0cc8810
Tianlei Wu <tlwu@microsoft.com>
2023-01-23 20:08:22 +0000

    add seq len threshold for fp32

--- onnxruntime/contrib_ops/cpu/bert/attention_common.h
+++ onnxruntime/contrib_ops/cpu/bert/attention_common.h
@@ -68,6 +68,9 @@ constexpr const char* kDisableTrtFlashAttention = "ORT_DISABLE_TRT_FLASH_ATTENTI
 // Environment variable to enable or disable cutlass memory efficient attention. Default is 0 (enabled).
 constexpr const char* kDisableMemoryEfficientAttention = "ORT_DISABLE_MEMORY_EFFICIENT_ATTENTION";
 
+// Minimum sequence length to enable memory efficient attention in FP32.
+constexpr int kMinSequenceLengthForMemoryEfficientAttentionFp32 = 256;
+
 }  // namespace attention
 
 }  // namespace contrib
