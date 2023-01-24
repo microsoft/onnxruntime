fbde54c88898c3abbb0865739f4019961126350c
Liqun Fu <liqfu@microsoft.com>
2023-01-24 14:14:30 -0800

    add more test cases
    Signed-off-by: Liqun Fu <liqfu@microsoft.com>

---
+++ onnxruntime/test/providers/cpu/nn/lp_pool_test_generator.py
@@ -0,0 +1,59 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+import numpy as np
+import torch
+import torch.nn as nn
+
+# use this code to generate test data for PoolTest.LpPool1d and PoolTest.LpPool2d
+def generate_lppool_1d_test_cases() -> None:
+    p = 2
+    x = np.array(
+        [
+            [
+                [1, 2, 3, 4],
+            ]
+        ]
+    ).astype(np.float32)
+
+    print(x)
+    kernel_sizes = [2, 3]
+    strides = [[1],[2]]
+    for kernel_size in kernel_sizes:
+        for stride in strides:
+            print(kernel_size)
+            print(stride)
+            m2 = nn.LPPool1d(p, kernel_size=kernel_size, stride=stride)
+            pt_y = m2(torch.from_numpy(x))
+            print(torch.flatten(pt_y))
+            print(pt_y.shape)
+
+def generate_lppool_2d_test_cases() -> None:
+    p = 2
+    x = np.array(
+        [
+            [
+                [
+                    [1, 2, 3, 4],
+                    [5, 6, 7, 8],
+                    [9, 10, 11, 12],
+                    [13, 14, 15, 16],
+                ]
+            ]
+        ]
+    ).astype(np.float32)
+
+    print(x)
+    kernel_sizes = [[2, 2],[3, 3]]
+    strides = [[1, 1],[2, 2]]
+    for kernel_size in kernel_sizes:
+        for stride in strides: 
+            m2 = nn.LPPool2d(p, kernel_size=kernel_size, stride=stride)
+            pt_y = m2(torch.from_numpy(x))
+            print(kernel_size)
+            print(stride)
+            print(torch.flatten(pt_y))
+            print(pt_y.shape)
+
+generate_lppool_1d_test_cases()
+generate_lppool_2d_test_cases()
