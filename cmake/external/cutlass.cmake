5213d04942fb73280cd23ca8fecdaef149bb0299
Tianlei Wu <tlwu@microsoft.com>
2023-01-24 16:17:43 +0000

    update cutlass.cmake

--- cmake/external/cutlass.cmake
+++ cmake/external/cutlass.cmake
@@ -1,12 +1,11 @@
 if (onnxruntime_USE_FLASH_ATTENTION)
   include(FetchContent)
-  FetchContent_Declare(cutlass
-    GIT_REPOSITORY https://github.com/nvidia/cutlass.git
-    GIT_TAG        66d9cddc832c1cdc2b30a8755274f7f74640cfe6
+  FetchContent_Declare(
+    cutlass
+    URL ${DEP_URL_cutlass}
+    URL_HASH SHA1=${DEP_SHA1_cutlass}
   )
 
+  onnxruntime_fetchcontent_makeavailable(cutlass)
   FetchContent_GetProperties(cutlass)
-  if(NOT cutlass_POPULATED)
-    FetchContent_Populate(cutlass)
-  endif()
 endif()
