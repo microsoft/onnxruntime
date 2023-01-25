29c47dc3ce060c655933dd958454ac2e2a682ed0
Tianlei Wu <tlwu@microsoft.com>
2023-01-25 22:04:40 +0000

    [CUDA] Add GroupNormalization operator

---
+++ onnxruntime/contrib_ops/cuda/diffusion/group_norm.cc
@@ -0,0 +1,106 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+#include "core/providers/cuda/cuda_common.h"
+#include "contrib_ops/cuda/diffusion/group_norm.h"
+#include "contrib_ops/cuda/diffusion/group_norm_impl.h"
+
+namespace onnxruntime {
+namespace contrib {
+namespace cuda {
+
+#define REGISTER_KERNEL_TYPED(T)                                  \
+  ONNX_OPERATOR_TYPED_KERNEL_EX(                                  \
+      GroupNorm,                                                  \
+      kMSDomain,                                                  \
+      1,                                                          \
+      T,                                                          \
+      kCudaExecutionProvider,                                     \
+      (*KernelDefBuilder::Create())                               \
+          .TypeConstraint("T", DataTypeImpl::GetTensorType<T>()), \
+      GroupNorm<T>);
+
+REGISTER_KERNEL_TYPED(MLFloat16);
+
+using namespace ONNX_NAMESPACE;
+
+template <typename T>
+GroupNorm<T>::GroupNorm(const OpKernelInfo& op_kernel_info) : CudaKernel(op_kernel_info) {
+  ORT_ENFORCE(op_kernel_info.GetAttr<float>("epsilon", &epsilon_).IsOK());
+  ORT_ENFORCE(epsilon_ >= 0);
+
+  int64_t num_groups;
+  ORT_ENFORCE(op_kernel_info.GetAttr<int64_t>("groups", &num_groups).IsOK());
+  ORT_ENFORCE(num_groups >= 0);
+  num_groups_ = static_cast<int>(num_groups);
+
+
+  ORT_ENFORCE(op_kernel_info.GetAttr<bool>("swish", &swish_).IsOK());
+}
+
+template <typename T>
+Status GroupNorm<T>::ComputeInternal(OpKernelContext* context) const {
+  const Tensor* input = context->Input<Tensor>(0);
+  const Tensor* gamma = context->Input<Tensor>(1);
+  const Tensor* beta = context->Input<Tensor>(2);
+  Tensor* output = context->Output(0, input->Shape());
+
+  const auto& input_dims = input->Shape().GetDims();
+  if (input_dims.size() != 4) {
+    return ORT_MAKE_STATUS(ONNXRUNTIME, INVALID_ARGUMENT,
+                           "input is expected to have 4 dimensions, got ", input_dims.size());
+  }
+
+  const auto& gamma_dims = gamma->Shape().GetDims();
+  if (gamma_dims.size() != 1) {
+    return ORT_MAKE_STATUS(ONNXRUNTIME, INVALID_ARGUMENT,
+                           "gamma is expected to have 1 dimension, got ", gamma_dims.size());
+  }
+  if (gamma_dims[0] != input_dims[2]) {
+    return ORT_MAKE_STATUS(ONNXRUNTIME, INVALID_ARGUMENT,
+                           "Last dimension of gamma and input does not match");
+  }
+
+  const auto& beta_dims = beta->Shape().GetDims();
+  if (beta_dims.size() != 1) {
+    return ORT_MAKE_STATUS(ONNXRUNTIME, INVALID_ARGUMENT,
+                            "beta is expected to have 1 dimension, got ", beta_dims.size());
+  }
+  if (beta_dims[0] != input_dims[2]) {
+    return ORT_MAKE_STATUS(ONNXRUNTIME, INVALID_ARGUMENT,
+                            "Last dimension of beta and input does not match");
+  }
+
+  int batch_size = static_cast<int>(input_dims[0]);
+  int num_channels = static_cast<int>(input_dims[1]);
+  int height = static_cast<int>(input_dims[2]);
+  int width = static_cast<int>(input_dims[3]);
+
+  if (num_channels % num_groups_ != 0) {
+    return ORT_MAKE_STATUS(ONNXRUNTIME, INVALID_ARGUMENT,
+                            "number of channels should be divisiable by num_groups");
+  }
+
+  auto workspace = GetScratchBuffer<void>(GetGroupNormWorkspaceSizeInBytes(), context->GetComputeStream());
+
+  typedef typename ToCudaType<T>::MappedType CudaT;
+
+  return LaunchGroupNormKernel<CudaT>(
+      Stream(context),
+      reinterpret_cast<CudaT*>(output->MutableData<T>()),
+      reinterpret_cast<const CudaT*>(input->Data<T>()),
+      reinterpret_cast<const float*>(gamma->Data<T>()),
+      reinterpret_cast<const float*>(beta->Data<T>()),
+      reinterpret_cast<float*>(workspace.get()),
+      epsilon_,
+      batch_size,
+      num_channels,
+      height,
+      width,
+      num_groups_,
+      swish_);
+}
+
+}  // namespace cuda
+}  // namespace contrib
+}  // namespace onnxruntime
---
+++ onnxruntime/core/graph/contrib_ops/diffusion_defs.cc
@@ -0,0 +1,67 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+#include "core/graph/constants.h"
+#include "core/graph/contrib_ops/contrib_defs.h"
+#include "core/graph/contrib_ops/onnx_function_util.h"
+#include "core/graph/contrib_ops/shape_inference_functions.h"
+
+// Suppress a warning: global initializer calls a non-constexpr function 'symbol' which is from
+// ONNX_OPERATOR_SET_SCHEMA_EX macro and only happens in debug build
+#if defined(_WIN32) && !defined(NDEBUG)
+#pragma warning(disable : 26426)
+#endif
+
+namespace onnxruntime {
+namespace contrib {
+using ONNX_NAMESPACE::AttributeProto;
+using ONNX_NAMESPACE::OpSchema;
+#ifndef NDEBUG
+using ONNX_NAMESPACE::DbgOperatorSetTracker;
+#endif
+
+constexpr const char* GroupNorm_ver1_doc = R"DOC(
+Applies Group Normalization over a mini-batch of inputs as described in the paper Group Normalization (https://arxiv.org/abs/1803.08494).
+
+This operator transforms input according to
+  y = gamma * (x - mean) / sqrt(variance + epsilon) + beta
+
+The input channels are separated into num_groups groups, each containing num_channels / num_groups channels. num_channels must be divisible by num_groups. The mean and standard-deviation are calculated separately over the each group.
+The weight and bias are per-channel affine transform parameter vectors of size num_channels.
+
+The swish attribute can be used to enable Swish activation after group normalization.
+)DOC";
+
+ONNX_MS_OPERATOR_SET_SCHEMA(
+    GroupNorm, 1,
+    OpSchema()
+        .SetDoc(GroupNorm_ver1_doc)
+        .Attr("epsilon", "The epsilon value to use to avoid division by zero", AttributeProto::FLOAT, static_cast<float>(1e-5))
+        .Attr("groups",
+              "The number of groups of channels. It should be a divisor of the number of channels C",
+              AttributeProto::INT)
+        .Attr("swish",
+              "Whether use Swish activation after group normalization",
+              AttributeProto::INT)
+        .Input(0,
+               "X",
+               "Input data tensor. Dimensions are (N x C x H x W), where N is the batch size, C is the number of channels, and H and W are the height and width of the data",
+               "T")
+        .Input(1,
+               "gamma",
+               "1D gamma tensor for normalization with shape (C), where C is number of channels",
+               "T")
+        .Input(2,
+               "beta",
+               "1D beta tensor for normalization  with shape (C), where C is number of channels",
+               "T")
+        .Output(0,
+                "Y",
+                "The output tensor of the same shape as X",
+                "T")
+        .TypeConstraint("T", {"tensor(float16)"}, "Constrain input X and output Y types to half tensors.")
+        .TypeConstraint("M", {"tensor(float)"}, "Constrain gamma and beta to float tensors.")
+        .TypeAndShapeInferenceFunction(ONNX_NAMESPACE::propagateShapeAndTypeFromFirstInput));
+
+}  // namespace contrib
+}  // namespace onnxruntime
