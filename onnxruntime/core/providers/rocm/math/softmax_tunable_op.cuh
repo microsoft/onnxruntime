fa627f63466f9cb7f63ce430cb5f52802f4efe2d
peixuanzuo <peixuanzuo@microsoft.com>
2023-01-31 07:20:48 +0000

    add SoftmaxTunableOp

--- onnxruntime/core/providers/rocm/math/softmax_tunable_op.cuh
+++ onnxruntime/core/providers/rocm/math/softmax_tunable_op.cuh
@@ -35,7 +35,7 @@ struct SoftmaxParams : onnxruntime::rocm::tunable::OpParams {
 };
 
 template <typename input_t, typename output_t, typename acc_t, int VecSize>
-Status SoftmaxBlockwiseOp(SoftmaxParams<input_t, output_t>* params) {
+Status SoftmaxBlockwiseOp(const SoftmaxParams<input_t, output_t>* params) {
   dim3 grid(params->batch_count);
   dim3 block = SoftMax_getBlockSize(VecSize, params->softmax_elements);
   if (params->is_log_softmax) {
@@ -51,6 +51,41 @@ Status SoftmaxBlockwiseOp(SoftmaxParams<input_t, output_t>* params) {
   }
   return HIP_CALL(hipGetLastError());
 }
+
+template <typename input_t, typename output_t, typename acc_t>
+Status SoftmaxBlockwiseStaticSelection(const SoftmaxParams<input_t, output_t>* params) {
+  dim3 grid(params->batch_count);
+  constexpr int ILP = sizeof(float4) / sizeof(input_t);
+  dim3 block = SoftMax_getBlockSize(ILP, params->softmax_elements);
+  if (params->is_log_softmax) {
+    softmax_block_forward<ILP, input_t, acc_t, output_t, LogSoftMaxForwardEpilogue>
+        <<<grid, block, block.x * sizeof(acc_t), params->stream>>>(params->output, const_cast<input_t*>(params->input),
+                                                                   params->softmax_elements, params->input_stride,
+                                                                   params->output_stride);
+  } else {
+    softmax_block_forward<ILP, input_t, acc_t, output_t, SoftMaxForwardEpilogue>
+        <<<grid, block, block.x * sizeof(acc_t), params->stream>>>(params->output, const_cast<input_t*>(params->input),
+                                                                   params->softmax_elements, params->input_stride,
+                                                                   params->output_stride);
+  }
+  return HIP_CALL(hipGetLastError());
+}
+
+template <typename input_t, typename output_t, typename acc_t>
+class SoftmaxTunableOp : public onnxruntime::rocm::tunable::TunableOp<SoftmaxParams<input_t, output_t>> {
+ public:
+  SoftmaxTunableOp() {
+    this->RegisterOp(SoftmaxBlockwiseStaticSelection<input_t, output_t, acc_t>);
+    this->RegisterOp(SoftmaxBlockwiseOp<input_t, output_t, acc_t, 1>);
+    this->RegisterOp(SoftmaxBlockwiseOp<input_t, output_t, acc_t, 2>);
+    this->RegisterOp(SoftmaxBlockwiseOp<input_t, output_t, acc_t, 4>);
+    this->RegisterOp(SoftmaxBlockwiseOp<input_t, output_t, acc_t, 8>);
+    this->RegisterOp(SoftmaxBlockwiseOp<input_t, output_t, acc_t, 16>);
+
+    // NOTE: the 1st kernel is SoftmaxBlockwise Original implementation.
+    this->SetDefaultId(0);
+  }
+};
 
 }  // namespace rocm
 }  // namespace onnxruntime
--- onnxruntime/python/tools/kernel_explorer/kernels/rocm/softmax.cu
+++ onnxruntime/python/tools/kernel_explorer/kernels/rocm/softmax.cu
@@ -37,6 +37,51 @@ class SoftmaxBlockwise : public IKernelExplorer {
   ParamsT params_{};
 };
 
+template <typename T>
+class SoftmaxBlockwiseStaticSelection : public IKernelExplorer {
+ public:
+  SoftmaxBlockwiseStaticSelection(DeviceArray& output, DeviceArray& input, int softmax_elements,
+                                  int input_stride, int output_stride, int batch_count, bool is_log_softmax)
+      : params_(this->Stream(), static_cast<T*>(output.ptr()), static_cast<T*>(input.ptr()),
+                softmax_elements, input_stride, output_stride, batch_count, is_log_softmax) {}
+
+  bool IsSupported() {
+    return true;
+  }
+
+  void Run() override {
+    ORT_THROW_IF_ERROR((rocm::SoftmaxBlockwiseStaticSelection<T, T, rocm::AccumulationType_t<T>>(&params_)));
+  }
+
+ private:
+  using ParamsT = rocm::SoftmaxParams<T, T>;
+  ParamsT params_{};
+};
+
+template <typename T>
+class SoftmaxTunable : public IKernelExplorer {
+ public:
+  SoftmaxTunable(DeviceArray& output, DeviceArray& input, int softmax_elements,
+                 int input_stride, int output_stride, int batch_count, bool is_log_softmax)
+      : params_(this->Stream(), static_cast<T*>(output.ptr()), static_cast<T*>(input.ptr()),
+                softmax_elements, input_stride, output_stride, batch_count, is_log_softmax) {
+    op_.EnableTuning();
+  }
+
+  void Run() override {
+    ORT_THROW_IF_ERROR(op_(&params_));
+  }
+
+  bool IsSupported() {
+    return true;
+  }
+
+ private:
+  using ParamsT = rocm::SoftmaxParams<T, T>;
+  ParamsT params_{};
+  rocm::SoftmaxTunableOp<T, T, rocm::AccumulationType_t<T>> op_{};
+};
+
 #define REGISTER_OP(name, type, vec_size)                                    \
   py::class_<name<type, vec_size>>(m, #name "_" #type "_" #vec_size)         \
       .def(py::init<DeviceArray&, DeviceArray&, int, int, int, int, bool>()) \
@@ -52,9 +97,23 @@ class SoftmaxBlockwise : public IKernelExplorer {
   REGISTER_OP(name, type, 8)                     \
   REGISTER_OP(name, type, 16)
 
+#define REGISTER_OP_TYPED(name, type)                                        \
+  py::class_<name<type>>(m, #name "_" #type)                                 \
+      .def(py::init<DeviceArray&, DeviceArray&, int, int, int, int, bool>()) \
+      .def("SetRepeats", &name<type>::SetRepeats)                            \
+      .def("Profile", &name<type>::Profile)                                  \
+      .def("Run", &name<type>::Run)                                          \
+      .def("IsSupported", &name<type>::IsSupported);
+
 void InitSoftmax(py::module m) {
   REGISTER_OP_FOR_ALL_VEC_SIZE(SoftmaxBlockwise, half);
   REGISTER_OP_FOR_ALL_VEC_SIZE(SoftmaxBlockwise, float);
+
+  REGISTER_OP_TYPED(SoftmaxBlockwiseStaticSelection, half);
+  REGISTER_OP_TYPED(SoftmaxBlockwiseStaticSelection, float);
+
+  REGISTER_OP_TYPED(SoftmaxTunable, half);
+  REGISTER_OP_TYPED(SoftmaxTunable, float);
 }
 
 }  // namespace onnxruntime
