dbfdf5e2263edd6fd12727dd6dfbe63faff85fc0
Chi Lo <Chi.Lo@microsoft.com>
2023-02-04 20:57:31 +0000

    add test

---
+++ onnxruntime/test/testdata/trt_plugin_custom_op_test.py
@@ -0,0 +1,26 @@
+import onnx
+from onnx import TensorProto, helper
+
+def GenerateModel(model_name):
+    nodes = [
+        helper.make_node("DisentangledAttention_TRT", ["input1", "input2", "input3"], ["output"], "DisentangledAttention_TRT", domain="trt.plugins", factor=0.123, span=128),
+    ]
+
+    graph = helper.make_graph(
+        nodes,
+        "trt_plugin_custom_op",
+        [  # input
+            helper.make_tensor_value_info("input1", TensorProto.FLOAT, [12, 256, 256]),
+            helper.make_tensor_value_info("input2", TensorProto.FLOAT, [12, 256, 256]),
+            helper.make_tensor_value_info("input3", TensorProto.FLOAT, [12, 256, 256]),
+        ],
+        [  # output
+            helper.make_tensor_value_info("output", TensorProto.FLOAT, [12, 256, 256]),
+        ],
+    )
+
+    model = helper.make_model(graph)
+    onnx.save(model, model_name)
+
+if __name__ == "__main__":
+    GenerateModel("trt_plugin_custom_op_test.onnx")
