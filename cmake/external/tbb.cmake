ef3d00fab01eb6f15374d18ac43fb70566e9ad31
Ran Shuai <randysheriff@hotmail.com>
2023-02-05 16:03:08 -0800

    fix cmake for linux

--- cmake/external/tbb.cmake
+++ cmake/external/tbb.cmake
@@ -9,7 +9,7 @@ include(ExternalProject)
 #set(TBB_TEST OFF CACHE INTERNAL "")
 #set(TBB_OUTPUT_DIR_BASE intel-tbb)
 
-ExternalProject_Add(tbb
+ExternalProject_Add(intel_tbb
                     GIT_REPOSITORY https://github.com/oneapi-src/oneTBB.git
                     GIT_TAG v2021.8.0
                     SOURCE_DIR ${CMAKE_CURRENT_BINARY_DIR}/_deps/tbb-src
@@ -23,8 +23,12 @@ set(TBB_INC ${CMAKE_CURRENT_BINARY_DIR}/_deps/tbb-src/include)
 include_directories(${TBB_INC})
 link_directories(${CMAKE_CURRENT_BINARY_DIR}/_deps/tbb-build/tbb_${CMAKE_BUILD_TYPE})
 
+if(WIN23)
 if(CMAKE_BUILD_TYPE STREQUAL "Debug")
 set (TBB "tbb12_debug")
 else()
 set (TBB "tbb12")
+endif()
+else()
+set (TBB "tbb")
 endif()
