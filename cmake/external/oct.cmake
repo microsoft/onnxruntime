ba5b0b13527fc8cee26e8e6600bef443c37deeaf
gunandrose4u <zhuganghuaonnet@gmail.com>
2023-02-08 21:58:33 +0000

    use TunePolling branch

--- cmake/external/oct.cmake
+++ cmake/external/oct.cmake
@@ -5,7 +5,7 @@ include(ExternalProject)
 
 ExternalProject_Add(tbb
                     GIT_REPOSITORY https://github.com/RandySheriffH/octopus.git
-                    GIT_TAG main
+                    GIT_TAG TunePolling
                     SOURCE_DIR ${CMAKE_CURRENT_BINARY_DIR}/_deps/oct-src
                     CONFIGURE_COMMAND ""
                     BUILD_COMMAND ""
