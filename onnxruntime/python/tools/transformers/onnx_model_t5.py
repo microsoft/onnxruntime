76dd29035536a52eb6f97af707786fa9c22df6cb
Ubuntu <wy@v100-2.0cdb2e52twzevn1i4fi45bylyg.jx.internal.cloudapp.net>
2023-02-09 00:22:10 +0000

    encoder

--- onnxruntime/python/tools/transformers/onnx_model_t5.py
+++ onnxruntime/python/tools/transformers/onnx_model_t5.py
@@ -50,13 +50,6 @@ class FusionT5Attention(FusionAttention):
         # Not implemented yet
         return
 
-
-# Attr("max_distance", "Max distance", AttributeProto::INT)
-# Attr("is_bidirectional", "Default value is 0.", AttributeProto::INT, static_cast<int64_t>(0))
-# Input(0, "bias_table", "2D input tensor with shape (num_buckets, num_heads), COL-major(See UT for example)", "T")
-# Input(1, "query_length", "The length of query. Self Attention requires query_length = key_length", "U")
-# Input(2, "key_length", "The length of key.", "U")
-# Output(0, "output", "4D output tensor with shape (1, num_heads, sequence_length, sequence_length)", "T")
 class FusionRelativePositionBiasBlock(Fusion):
     def __init__(self, model: OnnxModel, max_distance: int):
         super().__init__(model, "RelativePositionBias", ["Add", "Slice"])
@@ -65,6 +58,8 @@ class FusionRelativePositionBiasBlock(Fusion):
         self.is_bidirectional = False
 
     def fuse(self, node, input_name_to_nodes, output_name_to_node):
+        # TODO: optimization opportunity: Only last dimension of relative_position_bias is used in decoder.
+        # Cuda kernel can be optimized to only compute last dimension.
         if node.op_type != "Add" and node.op_type != "Slice":
             return
 
@@ -74,7 +69,13 @@ class FusionRelativePositionBiasBlock(Fusion):
             [0, 0, 0, 1]
         )
         if compute_bias_nodes is None:
-            return
+            compute_bias_nodes = self.model.match_parent_path(
+                node,
+                ["Unsqueeze", "Transpose", "Gather", "Add", "Where"],
+                [0, 0, 0, 1, 1]
+            )
+            if compute_bias_nodes is None:
+                return
 
         gather = compute_bias_nodes[2]
         where = compute_bias_nodes[-1]
@@ -82,45 +83,42 @@ class FusionRelativePositionBiasBlock(Fusion):
 
         compute_buckets_nodes = self.model.match_parent_path(
             where,
-            ["Min", "ConstantOfShape", "Shape", "Add", "Cast", "Mul", "Div", "Log", "Div", "Cast", "Neg"],
-            [2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            ["Min", "ConstantOfShape", "Shape", "Add", "Cast", "Mul", "Div", "Log", "Div"],
+            [2, 1, 0, 0, 0, 0, 0, 0, 0],
         )
         if compute_buckets_nodes is None:
             return
 
-        neg = compute_buckets_nodes[-1]
-
-        less_nodes = self.model.match_parent_path(
-            where,
-            ["Less"],
-            [0]
-        )
-        if less_nodes is None:
-            return
-
-        less = less_nodes[0]
-        if (less.input[0] != neg.output[0]):
-            return
+        div = compute_buckets_nodes[-1]
 
         range_nodes = self.model.match_parent_path(
-            neg,
-            ["Min", "ConstantOfShape", "Shape", "Sub", "Unsqueeze", "Range"],
-            [0, 1, 0, 0, 0, 0]
+            div,
+            ["Cast", "Neg", "Min", "ConstantOfShape", "Shape", "Sub", "Unsqueeze", "Range"],
+            [0, 0, 0, 1, 0, 0, 0, 0]
         )
         if range_nodes is None:
-            return
+            range_nodes = self.model.match_parent_path(
+                div,
+                ["Cast", "Abs", "Sub", "Unsqueeze", "Range"],
+                [0, 0, 0, 0, 0]
+            )
+            self.is_bidirectional = True
+            if range_nodes is None:
+                return
+
         range = range_nodes[-1]
 
         self.nodes_to_remove.extend(compute_bias_nodes)
         self.nodes_to_remove.extend(compute_buckets_nodes)
-        self.nodes_to_remove.extend(less_nodes)
         self.nodes_to_remove.extend(range_nodes)
+
+        node_name_prefix = "encoder" if self.is_bidirectional else "decoder"
 
         table_weight_i = self.model.get_initializer(gather.input[0])
         table_weight = NumpyHelper.to_array(table_weight_i)
         table_weight_t = np.transpose(table_weight)
         bias_table = helper.make_tensor(
-            name="bias_table_weight",
+            name=node_name_prefix + "bias_table_weight",
             data_type=TensorProto.FLOAT,
             dims=[np.shape(table_weight)[0], np.shape(table_weight)[1]],
             vals=table_weight_t.flatten().tolist(),
@@ -133,7 +131,7 @@ class FusionRelativePositionBiasBlock(Fusion):
             "RelativePositionBias",
             inputs=inputs,
             outputs=outputs,
-            name=self.model.create_node_name("RelativePositionBias", name_prefix="RPB"),
+            name=self.model.create_node_name("RelativePositionBias", name_prefix=node_name_prefix),
         )
         rpb_node.domain = "com.microsoft"
         rpb_node.attribute.extend([helper.make_attribute("max_distance", self.max_distance)])
