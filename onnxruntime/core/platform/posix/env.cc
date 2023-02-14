2779406a860ca631c04912eeb6791b8dd6321106
Ran Shuai <randysheriff@hotmail.com>
2023-02-14 11:01:16 -0800

    silence ex

--- onnxruntime/core/platform/posix/env.cc
+++ onnxruntime/core/platform/posix/env.cc
@@ -239,7 +239,8 @@ class PosixThread : public EnvThread {
           } else {
             // Logical processor id starts from 0 internally, but in ort API, it starts from 1,
             // that's why id need to increase by 1 when logging.
-            LOGS_DEFAULT(ERROR) << "cpu " << id + 1 << " does not exist, skipping it for affinity setting";
+            //LOGS_DEFAULT(ERROR) << "cpu " << id + 1 << " does not exist, skipping it for affinity setting";
+	    std::cout << "cpu " << id + 1 << " does not exist, skipping it for affinity setting";
           }
         }
         auto ret = pthread_setaffinity_np(pthread_self(), sizeof(cpu_set_t), &cpuset);
@@ -249,8 +250,9 @@ class PosixThread : public EnvThread {
                                 << ", mask: " << *p->affinity;
         } else {
           auto [err_no, err_msg] = GetSystemError(ret);
-          LOGS_DEFAULT(ERROR) << "pthread_setaffinity_np failed for thread: " << syscall(SYS_gettid)
-                              << ", index: " << p->index
+          //LOGS_DEFAULT(ERROR) << "pthread_setaffinity_np failed for thread: " << syscall(SYS_gettid)
+	  std::cout << "pthread_setaffinity_np failed for thread: " << syscall(SYS_gettid)
+                   	      << ", index: " << p->index
                               << ", mask: " << *p->affinity
                               << ", error code: " << err_no << " error msg: " << err_msg
                               << ". Specify the number of threads explicitly so the affinity is not set.";
@@ -264,7 +266,6 @@ class PosixThread : public EnvThread {
       // Ignore exceptions
       std::cout << "caught posix thread exception during creation: " << ex.what() << std::endl;
     }
-    pthread_exit(0);
     return nullptr;
   }
   static void CustomThreadMain(void* param) {
