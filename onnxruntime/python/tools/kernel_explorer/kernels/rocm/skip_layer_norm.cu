25bc04fe56649aab2ec6eb7f79ce99ff35cd437b
Ubuntu <ettao@ettao-amd-dev1.zvflicr54joexhdgnhvmxrxygg.phxx.internal.cloudapp.net>
2023-02-14 01:25:46 +0000

    fix build

--- onnxruntime/python/tools/kernel_explorer/kernels/rocm/skip_layer_norm.cu
+++ onnxruntime/python/tools/kernel_explorer/kernels/rocm/skip_layer_norm.cu
@@ -14,22 +14,22 @@ namespace py = pybind11;
 
 namespace onnxruntime {
 
-template <typename T, int ThreadsPerBlock, int VecSize>
+template <typename T, int ThreadsPerBlock, int VecSize, bool Simplified>
 class SkipLayerNormSmall : public IKernelExplorer {
  public:
-  SkipLayerNormSmall(DeviceArray& output, DeviceArray& input, DeviceArray& skip,
+  SkipLayerNormSmall(DeviceArray& output, DeviceArray& skip_input_bias_add_output, DeviceArray& input, DeviceArray& skip,
                      DeviceArray& gamma, DeviceArray& beta, DeviceArray& bias,
                      float epsilon, int hidden_size, int element_count)
-      : params_(TuningContext(), Stream(), static_cast<T*>(output.ptr()), static_cast<T*>(input.ptr()),
-                static_cast<T*>(skip.ptr()), static_cast<T*>(gamma.ptr()), static_cast<T*>(beta.ptr()),
-                static_cast<T*>(bias.ptr()), epsilon, hidden_size, element_count) {}
+      : params_(TuningContext(), Stream(), static_cast<T*>(output.ptr()), static_cast<T*>(skip_input_bias_add_output.ptr()), 
+                static_cast<T*>(input.ptr()), static_cast<T*>(skip.ptr()), static_cast<T*>(gamma.ptr()), 
+                static_cast<T*>(beta.ptr()), static_cast<T*>(bias.ptr()), epsilon, hidden_size, element_count) {}
 
   void Run() override {
-    ORT_THROW_IF_ERROR((contrib::rocm::SkipLayerNormSmallOp<T, ThreadsPerBlock, VecSize>(&params_)));
+    ORT_THROW_IF_ERROR((contrib::rocm::SkipLayerNormSmallOp<T, ThreadsPerBlock, VecSize, Simplified>(&params_)));
   }
 
   bool IsSupported() {
-    Status status = contrib::rocm::SkipLayerNormSmallOp<T, ThreadsPerBlock, VecSize>(&params_);
+    Status status = contrib::rocm::SkipLayerNormSmallOp<T, ThreadsPerBlock, VecSize, Simplified>(&params_);
     return status.IsOK();
   }
 
@@ -38,22 +38,22 @@ class SkipLayerNormSmall : public IKernelExplorer {
   ParamsT params_{};
 };
 
-template <typename T, int ThreadsPerBlock, int VecSize>
+template <typename T, int ThreadsPerBlock, int VecSize, bool Simplified>
 class SkipLayerNormRegular : public IKernelExplorer {
  public:
-  SkipLayerNormRegular(DeviceArray& output, DeviceArray& input, DeviceArray& skip,
+  SkipLayerNormRegular(DeviceArray& output, DeviceArray& skip_input_bias_add_output, DeviceArray& input, DeviceArray& skip,
                        DeviceArray& gamma, DeviceArray& beta, DeviceArray& bias,
                        float epsilon, int hidden_size, int element_count)
-      : params_(TuningContext(), Stream(), static_cast<T*>(output.ptr()), static_cast<T*>(input.ptr()),
-                static_cast<T*>(skip.ptr()), static_cast<T*>(gamma.ptr()), static_cast<T*>(beta.ptr()),
-                static_cast<T*>(bias.ptr()), epsilon, hidden_size, element_count) {}
+      : params_(TuningContext(), Stream(), static_cast<T*>(output.ptr()), static_cast<T*>(skip_input_bias_add_output.ptr()), 
+                static_cast<T*>(input.ptr()), static_cast<T*>(skip.ptr()), static_cast<T*>(gamma.ptr()), 
+                static_cast<T*>(beta.ptr()), static_cast<T*>(bias.ptr()), epsilon, hidden_size, element_count) {}
 
   void Run() override {
-    ORT_THROW_IF_ERROR((contrib::rocm::SkipLayerNormRegularOp<T, ThreadsPerBlock, VecSize>(&params_)));
+    ORT_THROW_IF_ERROR((contrib::rocm::SkipLayerNormRegularOp<T, ThreadsPerBlock, VecSize, Simplified>(&params_)));
   }
 
   bool IsSupported() {
-    Status status = contrib::rocm::SkipLayerNormRegularOp<T, ThreadsPerBlock, VecSize>(&params_);
+    Status status = contrib::rocm::SkipLayerNormRegularOp<T, ThreadsPerBlock, VecSize, Simplified>(&params_);
     return status.IsOK();
   }
 
@@ -62,22 +62,22 @@ class SkipLayerNormRegular : public IKernelExplorer {
   ParamsT params_{};
 };
 
-template <typename T>
+template <typename T, bool Simplified>
 class SkipLayerNormStaticSelection : public IKernelExplorer {
  public:
-  SkipLayerNormStaticSelection(DeviceArray& output, DeviceArray& input, DeviceArray& skip,
-                               DeviceArray& gamma, DeviceArray& beta, DeviceArray& bias,
+  SkipLayerNormStaticSelection(DeviceArray& output, DeviceArray& skip_input_bias_add_output, DeviceArray& input, 
+                               DeviceArray& skip, DeviceArray& gamma, DeviceArray& beta, DeviceArray& bias,
                                float epsilon, int hidden_size, int element_count)
-      : params_(TuningContext(), Stream(), static_cast<T*>(output.ptr()), static_cast<T*>(input.ptr()),
-                static_cast<T*>(skip.ptr()), static_cast<T*>(gamma.ptr()), static_cast<T*>(beta.ptr()),
-                static_cast<T*>(bias.ptr()), epsilon, hidden_size, element_count) {}
+      : params_(TuningContext(), Stream(), static_cast<T*>(output.ptr()), static_cast<T*>(skip_input_bias_add_output.ptr()), 
+                static_cast<T*>(input.ptr()), static_cast<T*>(skip.ptr()), static_cast<T*>(gamma.ptr()), 
+                static_cast<T*>(beta.ptr()), static_cast<T*>(bias.ptr()), epsilon, hidden_size, element_count) {}
 
   void Run() override {
-    ORT_THROW_IF_ERROR((contrib::rocm::SkipLayerNormStaticSelection<T>(&params_)));
+    ORT_THROW_IF_ERROR((contrib::rocm::SkipLayerNormStaticSelection<T, Simplified>(&params_)));
   }
 
   bool IsSupported() {
-    Status status = contrib::rocm::SkipLayerNormStaticSelection<T>(&params_);
+    Status status = contrib::rocm::SkipLayerNormStaticSelection<T, Simplified>(&params_);
     return status.IsOK();
   }
 
@@ -86,15 +86,15 @@ class SkipLayerNormStaticSelection : public IKernelExplorer {
   ParamsT params_{};
 };
 
-template <typename T>
+template <typename T, bool Simplified>
 class SkipLayerNormTunable : public IKernelExplorer {
  public:
-  SkipLayerNormTunable(DeviceArray& output, DeviceArray& input, DeviceArray& skip,
+  SkipLayerNormTunable(DeviceArray& output, DeviceArray& skip_input_bias_add_output, DeviceArray& input, DeviceArray& skip,
                        DeviceArray& gamma, DeviceArray& beta, DeviceArray& bias,
                        float epsilon, int hidden_size, int element_count)
-      : params_(TuningContext(), Stream(), static_cast<T*>(output.ptr()), static_cast<T*>(input.ptr()),
-                static_cast<T*>(skip.ptr()), static_cast<T*>(gamma.ptr()), static_cast<T*>(beta.ptr()),
-                static_cast<T*>(bias.ptr()), epsilon, hidden_size, element_count) {
+      : params_(TuningContext(), Stream(), static_cast<T*>(output.ptr()), static_cast<T*>(skip_input_bias_add_output.ptr()), 
+                static_cast<T*>(input.ptr()), static_cast<T*>(skip.ptr()), static_cast<T*>(gamma.ptr()), 
+                static_cast<T*>(beta.ptr()), static_cast<T*>(bias.ptr()), epsilon, hidden_size, element_count) {
 
     params_.TuningContext()->EnableTunableOp();
   }
@@ -113,52 +113,61 @@ class SkipLayerNormTunable : public IKernelExplorer {
   contrib::rocm::SkipLayerNormTunableOp<T, Simplified> op_{};
 };
 
-#define REGISTER_OP(name, type, threads_per_block, vec_size)                                                   \
-  py::class_<name<type, threads_per_block, vec_size>>(m, #name "_" #type "_" #threads_per_block "_" #vec_size) \
-      .def(py::init<DeviceArray&, DeviceArray&, DeviceArray&, DeviceArray&,                                    \
-                    DeviceArray&, DeviceArray&,                                                                \
-                    float, int, int>())                                                                        \
-      .def("SetRepeats", &name<type, threads_per_block, vec_size>::SetRepeats)                                 \
-      .def("Profile", &name<type, threads_per_block, vec_size>::Profile)                                       \
-      .def("Run", &name<type, threads_per_block, vec_size>::Run)                                               \
-      .def("IsSupported", &name<type, threads_per_block, vec_size>::IsSupported);
+#define REGISTER_OP(name, type, threads_per_block, vec_size, Simplified)                                                                    \
+  py::class_<name<type, threads_per_block, vec_size, Simplified>>(m, #name "_" #type "_" #threads_per_block "_" #vec_size "_" #Simplified)  \
+      .def(py::init<DeviceArray&, DeviceArray&, DeviceArray&, DeviceArray&,                                                                 \
+                    DeviceArray&, DeviceArray&, DeviceArray&,                                                                               \
+                    float, int, int>())                                                                                                     \
+      .def("SetRepeats", &name<type, threads_per_block, vec_size, Simplified>::SetRepeats)                                                  \
+      .def("Profile", &name<type, threads_per_block, vec_size, Simplified>::Profile)                                                        \
+      .def("Run", &name<type, threads_per_block, vec_size, Simplified>::Run)                                                                \
+      .def("IsSupported", &name<type, threads_per_block, vec_size, Simplified>::IsSupported);
 
-#define REGISTER_OP_FOR_ALL_VEC_SIZE(name, type, threads_per_block) \
-  REGISTER_OP(name, type, threads_per_block, 1)                     \
-  REGISTER_OP(name, type, threads_per_block, 2)                     \
-  REGISTER_OP(name, type, threads_per_block, 4)                     \
-  REGISTER_OP(name, type, threads_per_block, 8)                     \
-  REGISTER_OP(name, type, threads_per_block, 16)
+#define REGISTER_OP_FOR_ALL_VEC_SIZE(name, type, threads_per_block, Simplified) \
+  REGISTER_OP(name, type, threads_per_block, 1, Simplified)                     \
+  REGISTER_OP(name, type, threads_per_block, 2, Simplified)                     \
+  REGISTER_OP(name, type, threads_per_block, 4, Simplified)                     \
+  REGISTER_OP(name, type, threads_per_block, 8, Simplified)                     \
+  REGISTER_OP(name, type, threads_per_block, 16, Simplified)
 
-#define REGISTER_OP_FOR_ALL_THREADS_PER_BLOCK_ALL_VEC_SIZE(name, type) \
-  REGISTER_OP_FOR_ALL_VEC_SIZE(name, type, 64)                         \
-  REGISTER_OP_FOR_ALL_VEC_SIZE(name, type, 128)                        \
-  REGISTER_OP_FOR_ALL_VEC_SIZE(name, type, 192)                        \
-  REGISTER_OP_FOR_ALL_VEC_SIZE(name, type, 256)                        \
-  REGISTER_OP_FOR_ALL_VEC_SIZE(name, type, 320)                        \
-  REGISTER_OP_FOR_ALL_VEC_SIZE(name, type, 384)
+#define REGISTER_OP_FOR_ALL_THREADS_PER_BLOCK_ALL_VEC_SIZE(name, type, Simplified) \
+  REGISTER_OP_FOR_ALL_VEC_SIZE(name, type, 64, Simplified)                         \
+  REGISTER_OP_FOR_ALL_VEC_SIZE(name, type, 128, Simplified)                        \
+  REGISTER_OP_FOR_ALL_VEC_SIZE(name, type, 192, Simplified)                        \
+  REGISTER_OP_FOR_ALL_VEC_SIZE(name, type, 256, Simplified)                        \
+  REGISTER_OP_FOR_ALL_VEC_SIZE(name, type, 320, Simplified)                        \
+  REGISTER_OP_FOR_ALL_VEC_SIZE(name, type, 384, Simplified)
 
-#define REGISTER_OP_TYPED(name, type)                                       \
-  py::class_<name<type>>(m, #name "_" #type)                                \
+#define REGISTER_OP_TYPED(name, type, Simplified)                           \
+  py::class_<name<type, Simplified>>(m, #name "_" #type "_" #Simplified)    \
       .def(py::init<DeviceArray&, DeviceArray&, DeviceArray&, DeviceArray&, \
-                    DeviceArray&, DeviceArray&,                             \
+                    DeviceArray&, DeviceArray&, DeviceArray&,               \
                     float, int, int>())                                     \
-      .def("SetRepeats", &name<type>::SetRepeats)                           \
-      .def("Profile", &name<type>::Profile)                                 \
-      .def("Run", &name<type>::Run)                                         \
-      .def("IsSupported", &name<type>::IsSupported);
+      .def("SetRepeats", &name<type, Simplified>::SetRepeats)               \
+      .def("Profile", &name<type, Simplified>::Profile)                     \
+      .def("Run", &name<type, Simplified>::Run)                             \
+      .def("IsSupported", &name<type, Simplified>::IsSupported);
 
 void InitSkipLayerNorm(py::module m) {
-  REGISTER_OP_FOR_ALL_THREADS_PER_BLOCK_ALL_VEC_SIZE(SkipLayerNormSmall, half);
-  REGISTER_OP_FOR_ALL_THREADS_PER_BLOCK_ALL_VEC_SIZE(SkipLayerNormSmall, float);
-  REGISTER_OP_FOR_ALL_THREADS_PER_BLOCK_ALL_VEC_SIZE(SkipLayerNormRegular, half);
-  REGISTER_OP_FOR_ALL_THREADS_PER_BLOCK_ALL_VEC_SIZE(SkipLayerNormRegular, float);
+  REGISTER_OP_FOR_ALL_THREADS_PER_BLOCK_ALL_VEC_SIZE(SkipLayerNormSmall, half, true);
+  REGISTER_OP_FOR_ALL_THREADS_PER_BLOCK_ALL_VEC_SIZE(SkipLayerNormSmall, float, true);
+  REGISTER_OP_FOR_ALL_THREADS_PER_BLOCK_ALL_VEC_SIZE(SkipLayerNormSmall, half, false);
+  REGISTER_OP_FOR_ALL_THREADS_PER_BLOCK_ALL_VEC_SIZE(SkipLayerNormSmall, float, false);
 
-  REGISTER_OP_TYPED(SkipLayerNormTunable, half);
-  REGISTER_OP_TYPED(SkipLayerNormTunable, float);
+  REGISTER_OP_FOR_ALL_THREADS_PER_BLOCK_ALL_VEC_SIZE(SkipLayerNormRegular, half, true);
+  REGISTER_OP_FOR_ALL_THREADS_PER_BLOCK_ALL_VEC_SIZE(SkipLayerNormRegular, float, true);
+  REGISTER_OP_FOR_ALL_THREADS_PER_BLOCK_ALL_VEC_SIZE(SkipLayerNormRegular, half, false);
+  REGISTER_OP_FOR_ALL_THREADS_PER_BLOCK_ALL_VEC_SIZE(SkipLayerNormRegular, float, false);
 
-  REGISTER_OP_TYPED(SkipLayerNormStaticSelection, half);
-  REGISTER_OP_TYPED(SkipLayerNormStaticSelection, float);
+  REGISTER_OP_TYPED(SkipLayerNormTunable, half, true);
+  REGISTER_OP_TYPED(SkipLayerNormTunable, float, true);
+  REGISTER_OP_TYPED(SkipLayerNormTunable, half, false);
+  REGISTER_OP_TYPED(SkipLayerNormTunable, float, false);
+
+  REGISTER_OP_TYPED(SkipLayerNormStaticSelection, half, true);
+  REGISTER_OP_TYPED(SkipLayerNormStaticSelection, float, true);
+  REGISTER_OP_TYPED(SkipLayerNormStaticSelection, half, false);
+  REGISTER_OP_TYPED(SkipLayerNormStaticSelection, float, false);
 }
 
 }  // namespace onnxruntime
