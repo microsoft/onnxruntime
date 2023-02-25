c57cf374b67f72575546d7b4c69a1af4972e2b54
Patrice Vignola <vignola.patrice@gmail.com>
2023-02-24 21:59:34 -0800

    Bump ORT version number to 1.14.1 (#14826)

--- onnxruntime/core/session/onnxruntime_c_api.cc
+++ onnxruntime/core/session/onnxruntime_c_api.cc
@@ -2667,7 +2667,7 @@ static_assert(offsetof(OrtApi, ReleaseKernelInfo) / sizeof(void*) == 218, "Size 
 static_assert(offsetof(OrtApi, ReleaseCANNProviderOptions) / sizeof(void*) == 224, "Size of version 13 API cannot change");
 
 // So that nobody forgets to finish an API version, this check will serve as a reminder:
-static_assert(std::string_view(ORT_VERSION) == "1.14.0",
+static_assert(std::string_view(ORT_VERSION) == "1.14.1",
               "ORT_Version change detected, please follow below steps to ensure OrtApi is updated properly");
 // 1. Update the hardcoded version string in above static_assert to silence it
 // 2. If there were any APIs added to ort_api_1_to_14 above:
