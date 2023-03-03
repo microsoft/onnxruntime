5b427498ef0abfa7e0b8c81ebc9ce77e3b643d83
Randy Shuai <rashuai@microsoft.com>
2023-03-03 13:42:11 -0800

    add header changes

--- include/onnxruntime/core/session/onnxruntime_c_api.h
+++ include/onnxruntime/core/session/onnxruntime_c_api.h
@@ -332,6 +332,8 @@ typedef struct OrtKernelContext OrtKernelContext;
 struct OrtCustomOp;
 typedef struct OrtCustomOp OrtCustomOp;
 
+typedef void (*CustomComputeFn)(OrtKernelContext*);
+
 typedef enum OrtAllocatorType {
   OrtInvalidAllocator = -1,
   OrtDeviceAllocator = 0,
@@ -3959,6 +3961,12 @@ struct OrtApi {
 #ifdef __cplusplus
   OrtApi(const OrtApi&) = delete;  // Prevent users from accidentally copying the API structure, it should always be passed as a pointer
 #endif
+
+  ORT_API2_STATUS(LiteCustomOpResgiter,
+                  _In_ const char* domain_name,
+                  _In_ const char* op_name,
+                  _In_ const char* execution_provider,
+                  _In_ const CustomComputeFn custom_compute_fn);
 };
 
 /*
@@ -4066,6 +4074,8 @@ ORT_API_STATUS(OrtSessionOptionsAppendExecutionProvider_MIGraphX, _In_ OrtSessio
  * \param use_arena zero: false. non-zero: true.
  */
 ORT_API_STATUS(OrtSessionOptionsAppendExecutionProvider_Dnnl, _In_ OrtSessionOptions* options, int use_arena);
+
+//ORT_API_STATUS(LiteCustomOpResgiter, _In_ const char* domain, _In_ const char* op_name, _In_ void (*)(OrtKernelContext*));
 
 #ifdef __cplusplus
 }
