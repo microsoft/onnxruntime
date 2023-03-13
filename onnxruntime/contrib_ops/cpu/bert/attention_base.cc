4d4a6e39f67858a0ecee9cb19954d56a19181b99
Ye Wang <52801275+wangyems@users.noreply.github.com>
2023-03-13 14:29:16 -0700

    [t5 optimization] kernel changes to t5 (#14928)
    ### Description
    <!-- Describe your changes. -->
    
    1. support optional bias in Attention op (used in T5 encoder)
    2. support broadcasting rel_pos_bias in attention_softmax.h
    3. add scale in
    MHA op's attributes
    4. support past_key/past_value and present_key/present_value in MHA
    5. UT and parity tests are added
    6. fix an issue: https://github.com/microsoft/onnxruntime/issues/14920
    
    note: the fusions will be in another PR since mt5 needs to be tested and
    an issue from github will be investigated.
    
    Future works:
    1. support shared buffer for past/present
    2. enable trt kernels when possible and investigate (trt/cutlass)kernels
    with rel_pos_bias)
    3. support KV/QKV packing with past/present
    
    ### Motivation and Context
    <!-- - Why is this change required? What problem does it solve?
    - If it fixes an open issue, please link to the issue here. -->
    
    ---------
    
    Co-authored-by: Ubuntu <wy@v100-2.0cdb2e52twzevn1i4fi45bylyg.jx.internal.cloudapp.net>

--- onnxruntime/contrib_ops/cpu/bert/attention_base.cc
+++ onnxruntime/contrib_ops/cpu/bert/attention_base.cc
@@ -193,6 +193,7 @@ Status AttentionBase::CheckInputs(const TensorShape& input_shape,
     }
   }
 
+  bool broadcast_res_pos_bias = false;
   if (relative_position_bias != nullptr) {
     const auto& relative_position_bias_dims = relative_position_bias->Shape().GetDims();
 
@@ -202,10 +203,13 @@ Status AttentionBase::CheckInputs(const TensorShape& input_shape,
                              relative_position_bias_dims.size());
     }
 
-    if (relative_position_bias_dims[0] != batch_size) {
+    if (relative_position_bias_dims[0] != batch_size && relative_position_bias_dims[0] != 1) {
       return ORT_MAKE_STATUS(ONNXRUNTIME, INVALID_ARGUMENT,
-                             "Input 'relative_position_bias' dimension 0 should be same as batch_size, got ",
+                             "Input 'relative_position_bias' dimension 0 should be same as batch_size or 1, got ",
                              relative_position_bias_dims[0]);
+    }
+    if (relative_position_bias_dims[0] == 1) {
+      broadcast_res_pos_bias = true;
     }
     if (relative_position_bias_dims[1] != num_heads_) {
       return ORT_MAKE_STATUS(ONNXRUNTIME, INVALID_ARGUMENT,
@@ -255,6 +259,8 @@ Status AttentionBase::CheckInputs(const TensorShape& input_shape,
     output_parameters->mask_filter_value = mask_filter_value_;
     output_parameters->scale = scale_;
     output_parameters->mask_type = mask_type;
+    output_parameters->broadcast_res_pos_bias = broadcast_res_pos_bias;
+    output_parameters->pass_past_in_kv = false;
   }
 
   return Status::OK();
