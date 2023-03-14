31136b6446836013a2f37bb122368341a0054055
Lei Cao <leca@microsoft.com>
2023-03-14 11:52:46 -0700

    expose register_execution_provider in __init__.py

--- onnxruntime/__init__.py
+++ onnxruntime/__init__.py
@@ -41,6 +41,7 @@ try:
         get_available_providers,
         get_device,
         get_version_string,
+        register_execution_provider,
         set_default_logger_severity,
         set_default_logger_verbosity,
         set_seed,
