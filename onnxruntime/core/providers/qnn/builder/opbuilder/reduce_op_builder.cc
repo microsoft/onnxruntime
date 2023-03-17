9481822b4824aae85788d9f0b36262207913dd5f
adrianlizarraga <adlizarraga@microsoft.com>
2023-03-17 03:58:33 -0700

    Handle noop_with_empty_axes. Disable new opset ReduceOp tests fort TRT and OpenVINO.

--- onnxruntime/core/providers/qnn/builder/opbuilder/reduce_op_builder.cc
+++ onnxruntime/core/providers/qnn/builder/opbuilder/reduce_op_builder.cc
@@ -102,15 +102,15 @@ const std::array<std::vector<const char*>, REDUCE_OP_TYPE_COUNT> ReduceOpBuilder
 };
 
 const std::array<std::vector<const char*>, REDUCE_OP_TYPE_COUNT> ReduceOpBuilder::data_input_quant_types = {
-    std::vector<const char*>{"uint8", "int8", "uint16"},  // ReduceMax
-    std::vector<const char*>{"uint8", "int8", "uint16"},  // ReduceMax
-    std::vector<const char*>{"uint8", "int8", "uint16"},  // ReduceMean
-    std::vector<const char*>{"uint8", "int8", "uint16"},  // ReduceProd
-    std::vector<const char*>{"uint8", "int8", "uint16", "int32"},  // ReduceSum
+    std::vector<const char*>{"uint8", "int8"},  // ReduceMax
+    std::vector<const char*>{"uint8", "int8"},  // ReduceMax
+    std::vector<const char*>{"uint8", "int8"},  // ReduceMean
+    std::vector<const char*>{"uint8", "int8"},  // ReduceProd
+    std::vector<const char*>{"uint8", "int8", "int32"},  // ReduceSum
 };
 
 Status ReduceOpBuilder::IsTypeAllowed(ONNX_NAMESPACE::DataType type, const std::vector<const char*>& allowed_types,
-                     const char* err_prefix) const {
+                                      const char* err_prefix) const {
   for (auto allowed_type : allowed_types) {
     if (ONNX_NAMESPACE::Utils::DataTypeUtils::ToType(allowed_type) == type) {
       return Status::OK();
@@ -126,9 +126,9 @@ Status ReduceOpBuilder::IsTypeAllowed(ONNX_NAMESPACE::DataType type, const std::
 }
 
 Status ReduceOpBuilder::IsOpSupported(QnnModelWrapper& qnn_model_wrapper,
-                                            const NodeUnit& node_unit,
-                                            const logging::Logger& logger,
-                                            bool is_quantized_model) const {
+                                      const NodeUnit& node_unit,
+                                      const logging::Logger& logger,
+                                      bool is_quantized_model) const {
   ORT_UNUSED_PARAMETER(logger);
   ReduceOpType reduce_op_type = GetReduceOpType(node_unit.OpType());
   if (reduce_op_type == ReduceOpType::REDUCE_OP_TYPE_UNKNOWN) {
@@ -167,13 +167,20 @@ Status ReduceOpBuilder::IsOpSupported(QnnModelWrapper& qnn_model_wrapper,
       // Check that the axes shape is 1D with shape [M] where 0 < M <= rank(input_data)
       std::vector<uint32_t> axes_shape;
       ORT_RETURN_IF_NOT(qnn_model_wrapper.GetOnnxShape(axes_input.node_arg, axes_shape),
-                        "Cannot get shape of data input");
+                        "Cannot get shape of axes input");
 
-      num_axes = axes_shape[0];
-      if (axes_shape.size() != 1 || num_axes > input_data_rank) {
+      if (axes_shape.size() != 1 || axes_shape[0] > input_data_rank) {
         return ORT_MAKE_STATUS(ONNXRUNTIME, FAIL,
                                "QNN EP: \"axes\" input must have shape [M] where 0 < M <= rank(input[0])");
       }
+
+      bool noop_with_empty_axes = static_cast<bool>(node_attr_helper.Get("noop_with_empty_axes", (int64_t)0));
+      if (axes_shape[0] == 0 && noop_with_empty_axes) {
+        return ORT_MAKE_STATUS(ONNXRUNTIME, FAIL,
+                               "QNN EP: does not support NoOp for reduction operators with empty axes.");
+      }
+
+      num_axes = (axes_shape[0] == 0) ? input_data_rank : axes_shape[0];
     }
   } else {
     std::vector<int64_t> reduce_axes(input_data_rank);
@@ -238,7 +245,9 @@ Status ReduceOpBuilder::SetAxesParam(QnnModelWrapper& qnn_model_wrapper,
   const auto& inputs = node_unit.Inputs();
   std::vector<uint32_t> input_shape;
   ORT_RETURN_IF_NOT(qnn_model_wrapper.GetOnnxShape(inputs[0].node_arg, input_shape), "Cannot get shape");
-  uint32_t rank = static_cast<uint32_t>(input_shape.size());
+  const uint32_t rank = static_cast<uint32_t>(input_shape.size());
+
+  // Create an array of default axes values.
   std::vector<int64_t> reduce_axes(rank);
   for (uint32_t i = 0; i < rank; ++i) {
     reduce_axes[i] = i;
@@ -247,44 +256,59 @@ Status ReduceOpBuilder::SetAxesParam(QnnModelWrapper& qnn_model_wrapper,
   const int opset_axes_as_input = ReduceOpBuilder::opset_with_axes_as_input[reduce_op_type];
   const int opset = node_unit.SinceVersion();
 
-  if (opset < opset_axes_as_input) {
-    // Axes is in ONNX node attribute.
+  // Extract the axes values from either the attribute or initializer input (depending on opset).
+  if (opset < opset_axes_as_input) {  // Axes is in ONNX node attribute.
     NodeAttrHelper node_helper(node_unit);
     reduce_axes = node_helper.Get(qnn_def::axes, reduce_axes);
-  } else if (inputs.size() > 1) {
-    // Axes is in ONNX input[1] initializer.
-    const std::string& axes_input_name = inputs[1].node_arg.Name();
-    ORT_ENFORCE(qnn_model_wrapper.IsInitializerInput(axes_input_name),
-                "Exepect QNN Reduce* operator's axes input to be an initializer.");
-    const auto& axes_tensor = qnn_model_wrapper.GetInitializerTensors().at(axes_input_name);
-    std::vector<uint8_t> axes_bytes;
+  } else if (inputs.size() > 1) {  // Axes is in ONNX input[1] initializer.
+    const auto& axes_input = inputs[1];
 
-    ORT_RETURN_IF_ERROR(onnxruntime::utils::UnpackInitializerData(*axes_tensor, axes_bytes));
-    ORT_ENFORCE(reduce_axes.size() * sizeof(reduce_axes[0]) >= axes_bytes.size(),
-                "Expect QNN Reduce* operator to have at most rank(input[0]) axes elements.");
-    reduce_axes.resize(axes_bytes.size() / sizeof(reduce_axes[0]));
+    std::vector<uint32_t> axes_shape;
+    ORT_RETURN_IF_NOT(qnn_model_wrapper.GetOnnxShape(axes_input.node_arg, axes_shape),
+                      "Cannot get shape of axes input");
 
-    auto src_span = gsl::make_span(axes_bytes.data(), axes_bytes.size());
-    auto dst_span = gsl::make_span(reduce_axes.data(), reduce_axes.size());
+    // Empty axes means to use default axes (when noop_with_empty_axes is 0).
+    if (axes_shape[0] > 0) {
+      const std::string& axes_input_name = inputs[1].node_arg.Name();
+      ORT_ENFORCE(qnn_model_wrapper.IsInitializerInput(axes_input_name),
+                  "Exepect QNN Reduce* operator's axes input to be an initializer.");
 
-    // Copy initializer bytes (stored in little-endian order) to vector of int64_t.
-    // ReadLittleEndian returns a status error if the source and destination spans do not have
-    // matching byte sizes.
-    ORT_RETURN_IF_ERROR(onnxruntime::utils::ReadLittleEndian(src_span, dst_span));
+      // Get axes initializer bytes.
+      const auto& axes_tensor = qnn_model_wrapper.GetInitializerTensors().at(axes_input_name);
+      std::vector<uint8_t> axes_bytes;
+
+      ORT_RETURN_IF_ERROR(onnxruntime::utils::UnpackInitializerData(*axes_tensor, axes_bytes));
+      ORT_ENFORCE(reduce_axes.size() * sizeof(reduce_axes[0]) >= axes_bytes.size(),
+                  "Expect QNN Reduce* operator to have at most rank(input[0]) axes elements.");
+      reduce_axes.resize(axes_bytes.size() / sizeof(reduce_axes[0]));
+
+      auto src_span = gsl::make_span(axes_bytes.data(), axes_bytes.size());
+      auto dst_span = gsl::make_span(reduce_axes.data(), reduce_axes.size());
+
+      // Copy initializer bytes (stored in little-endian order) to vector of int64_t.
+      // ReadLittleEndian returns a status error if the source and destination spans do not have
+      // matching byte sizes.
+      ORT_RETURN_IF_ERROR(onnxruntime::utils::ReadLittleEndian(src_span, dst_span));
+    }
   }
 
+  // QNN does not support negative axes values.
+  // Fix negative values by adding the input rank.
   size_t num_axes = reduce_axes.size();
   for (size_t i = 0; i < num_axes; ++i) {
     if (reduce_axes.at(i) < 0) {
       reduce_axes[i] += rank;
     }
   }
+
+  // Truncate int64 ONNX axes values to QNN's required type (uint32_t).
   std::vector<uint32_t> axes_shape{SafeInt<uint32_t>(num_axes)};
   std::vector<uint32_t> axes_data;
   axes_data.resize(num_axes);
   std::transform(reduce_axes.begin(), reduce_axes.end(), axes_data.begin(),
                  [](int64_t item) { return SafeInt<uint32_t>(item); });
 
+  // Create the QNN axes parameter.
   QnnParamWrapper axes_param(node_unit.Index(), node_unit.Name(), qnn_def::axes,
                              std::move(axes_shape), std::move(axes_data));
   param_tensor_names.push_back(axes_param.GetParamTensorName());
