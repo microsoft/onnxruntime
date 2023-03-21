972958f0043a00e5caa517d9368c53e7b0515b85
edgchen1 <18449977+edgchen1@users.noreply.github.com>
2023-03-21 14:27:22 -0700

    update class documentation comment

--- onnxruntime/core/optimizer/qdq_transformer/ensure_unique_dq_for_node_unit.h
+++ onnxruntime/core/optimizer/qdq_transformer/ensure_unique_dq_for_node_unit.h
@@ -11,6 +11,8 @@ namespace onnxruntime {
  * Graph transformer that duplicates DQ nodes in order to ensure that each potential QDQ node unit has unique DQ nodes
  * for its inputs.
  *
+ * Having unique DQ nodes per QDQ node unit simplifies QDQ node unit processing.
+ *
  * In particular, it ensures that each output edge from a DQ to an explicit input (i.e., to a node in the same graph)
  * will have a unique DQ of which that explicit input is the only consumer.
  *
