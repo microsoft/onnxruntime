a35107d7e45fe61ed4ede1ca6379b999241afe44
xadupre <xadupre@microsoft.com>
2023-03-24 18:21:24 +0100

    ut

--- onnxruntime/test/python/onnxruntime_test_float8.py
+++ onnxruntime/test/python/onnxruntime_test_float8.py
@@ -165,6 +165,7 @@ class TestInferenceSession(unittest.TestCase):
             if to not in {TensorProto.FLOAT8E4M3FN, TensorProto.FLOAT8E5M2}:
                 # only those types are available on CUDA.
                 continue
+            onnx_model = self.model_cast_cast(to)
             sess = onnxruntime.InferenceSession(onnx_model.SerializeToString(), so, providers=["CUDAExecutionProvider"])
             y = sess.run(None, {"X": x})[0]
             assert_allclose(expect, y)
