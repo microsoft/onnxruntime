4555d1de7f4c530904e0e6e81c880602d2e22c78
xadupre <xadupre@microsoft.com>
2023-03-24 16:42:11 +0100

    better message

--- onnxruntime/test/python/onnxruntime_test_float8.py
+++ onnxruntime/test/python/onnxruntime_test_float8.py
@@ -80,9 +80,10 @@ class TestInferenceSession(unittest.TestCase):
             y = ref.run(None, {"X": x})[0]
             assert_allclose(y, expect)
             for prov in ["CPUExecutionProvider", "CUDAExecutionProvider"]:
-                print("prov")
+                print("prov1", prov)
                 if prov not in available_providers:
                     continue
+                print("prov2", prov)
                 with self.subTest(provider=prov, to=to):
                     sess = onnxruntime.InferenceSession(onnx_model.SerializeToString(), so, providers=[prov])
                     y = sess.run(None, {"X": x})[0]
