d66e5cdf7b2f20a07b92f2a2c5372cf5475b69a7
Changming Sun <chasun@microsoft.com>
2023-03-27 19:29:15 -0700

    update

--- tools/ci_build/github/windows/install_protoc.ps1
+++ tools/ci_build/github/windows/install_protoc.ps1
@@ -27,7 +27,7 @@ $Env:MSBUILDDISABLENODEREUSE=1
 New-Item -Path "$install_prefix" -ItemType Directory -Force
 
 # Setup compile flags
-$compile_flags = '/MP /guard:cf /Qspectre /DWIN32 /D_WINDOWS /DWINVER=0x0601 /D_WIN32_WINNT=0x0601 /DNTDDI_VERSION=0x06010000 /W3 '
+$compile_flags = @('/MP', '/guard:cf', '/Qspectre', '/DWIN32', '/D_WINDOWS', '/DWINVER=0x0A00', '/D_WIN32_WINNT=0x0A00', '/DNTDDI_VERSION=0x0A000000', '/W3')
 $linker_flags=@('/guard:cf')
 
 if ($use_cache) {
@@ -40,23 +40,35 @@ else {
 if($build_config -eq 'Release'){
   $compile_flags += "/O2", "/Ob2", "/DNDEBUG", "/Gw", "/GL"
 } elseif($build_config -eq 'RelWithDebInfo'){
-    $compile_flags += "$debug_info_format", "/O2", "/Ob1", "/DNDEBUG", "/Gw", "/GL"
+  $compile_flags += "$debug_info_format", "/O2", "/Ob1", "/DNDEBUG", "/Gw", "/GL"
 } elseif($build_config -eq 'Debug'){
   $compile_flags += "$debug_info_format", "/Ob0", "/Od", "/RTC1"
 } elseif($build_config -eq 'MinSizeRel'){
   $compile_flags += "/O1", "/Ob1", "/DNDEBUG", "/Gw", "/GL"
 }
-Write-Host $compile_flags
-# cmake args that applies to every 3rd-party library
-[string[]]$cmake_extra_args="-DCMAKE_CXX_STANDARD=17 `"-DCMAKE_CXX_FLAGS=$compile_flags /EHsc`" ", "`"-DCMAKE_C_FLAGS=$compile_flags`"", "--compile-no-warning-as-error", "--fresh", "-Wno-dev"
+
+
 if($cpu_arch -eq 'x86'){
   $cmake_extra_args +=  "-A", "Win32", "-T", "host=x64"
+  $compile_flags += '/Qspectre'
   $linker_flags += '/machine:x86'
 } elseif($cpu_arch -eq 'x64') {
   $linker_flags += '/machine:x64'
+  $compile_flags += '/Qspectre'
+} elseif($cpu_arch -eq 'arm') {
+  $linker_flags += '/machine:ARM'
+} elseif($cpu_arch -eq 'arm64') {
+  $linker_flags += '/machine:ARM64'
+} elseif($cpu_arch -eq 'arm64ec') {
+  $linker_flags += '/machine:ARM64EC'
 } else {
   throw "$cpu_arch is not supported"
 }
+
+Write-Host $compile_flags
+
+# cmake args that applies to every 3rd-party library
+[string[]]$cmake_extra_args="-DCMAKE_CXX_STANDARD=17 `"-DCMAKE_CXX_FLAGS=$compile_flags /EHsc`" ", "`"-DCMAKE_C_FLAGS=$compile_flags`"", "--compile-no-warning-as-error", "--fresh", "-Wno-dev"
 
 if ($use_cache) {
   if ($build_config -eq 'RelWithDebInfo') {
