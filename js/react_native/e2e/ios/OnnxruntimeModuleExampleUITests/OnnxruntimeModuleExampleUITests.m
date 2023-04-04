97785dc9e568a99489d40afbbb53c4de5bcaa480
rachguo <rachguo@rachguos-Mac-mini.local>
2023-04-04 16:47:01 -0700

    update

--- js/react_native/e2e/ios/OnnxruntimeModuleExampleUITests/OnnxruntimeModuleExampleUITests.m
+++ js/react_native/e2e/ios/OnnxruntimeModuleExampleUITests/OnnxruntimeModuleExampleUITests.m
@@ -1,27 +1,27 @@
 // NOT USING THIS TEST ANY MORE
 
-// // Copyright (c) Microsoft Corporation. All rights reserved.
-// // Licensed under the MIT License.
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
 
-// #import <XCTest/XCTest.h>
+#import <XCTest/XCTest.h>
 
-// @interface OnnxruntimeModuleExampleUITests : XCTestCase
+@interface OnnxruntimeModuleExampleUITests : XCTestCase
 
-// @end
+@end
 
-// @implementation OnnxruntimeModuleExampleUITests
+@implementation OnnxruntimeModuleExampleUITests
 
-// - (void)setUp {
-//   self.continueAfterFailure = NO;
-// }
+- (void)setUp {
+  self.continueAfterFailure = NO;
+}
 
-// - (void)testExample {
-//   XCUIApplication *app = [[XCUIApplication alloc] init];
-//   [app launch];
+- (void)testExample {
+  XCUIApplication *app = [[XCUIApplication alloc] init];
+  [app launch];
 
-//   XCTAssert([app.textFields[@"output"] waitForExistenceWithTimeout:180]);
-//   NSString* value = app.textFields[@"output"].value;
-//   XCTAssertEqualObjects(value, @"Result: 3");
-// }
+  XCTAssert([app.textFields[@"output"] waitForExistenceWithTimeout:180]);
+  NSString* value = app.textFields[@"output"].value;
+  XCTAssertEqualObjects(value, @"Result: 3");
+}
 
-// @end
+@end
