39954917d1c61a53b23317f13f46ff4c9a1806a4
stevenlix <stevli@microsoft.com>
2023-04-07 21:34:43 +0000

    FP16 support for whisper

--- onnxruntime/contrib_ops/cpu/transformers/beam_search_parameters.cc
+++ onnxruntime/contrib_ops/cpu/transformers/beam_search_parameters.cc
@@ -66,10 +66,32 @@ void BeamSearchParameters::ParseFromInputs(OpKernelContext* context) {
               "num_return_sequences (", num_return_sequences, ") shall be be no more than num_beams (", num_beams, ")");
 
   auto* length_penalty_tensor = context->Input<Tensor>(5);
-  length_penalty = length_penalty_tensor ? static_cast<float>(*length_penalty_tensor->Data<float>()) : 1;
+  if (length_penalty_tensor) {
+    if (length_penalty_tensor->DataType() == DataTypeImpl::GetType<float>()) {
+      length_penalty = static_cast<float>(*length_penalty_tensor->Data<float>());
+    }
+    else {
+      length_penalty = static_cast<MLFloat16>(*length_penalty_tensor->Data<MLFloat16>());
+    }
+  } else {
+    length_penalty = 1.0f;
+  }
+  //length_penalty = length_penalty_tensor ? static_cast<float>(*length_penalty_tensor->Data<float>()) : 1;
+  ///length_penalty = length_penalty_tensor ? static_cast<MLFloat16>(*length_penalty_tensor->Data<MLFloat16>()) : 1.0f;//slx
 
   auto* repetition_penalty_tensor = context->Input<Tensor>(6);
-  repetition_penalty = repetition_penalty_tensor ? static_cast<float>(*repetition_penalty_tensor->Data<float>()) : 1.0f;
+  if (repetition_penalty_tensor) {
+    if (repetition_penalty_tensor->DataType() == DataTypeImpl::GetType<float>()) {
+      repetition_penalty = static_cast<float>(*repetition_penalty_tensor->Data<float>());
+    }
+    else {
+      repetition_penalty = static_cast<MLFloat16>(*repetition_penalty_tensor->Data<MLFloat16>());
+    }
+  } else {
+    repetition_penalty = 1.0f;
+  }
+  //repetition_penalty = repetition_penalty_tensor ? static_cast<float>(*repetition_penalty_tensor->Data<float>()) : 1.0f;
+  ///repetition_penalty = repetition_penalty_tensor ? static_cast<MLFloat16>(*repetition_penalty_tensor->Data<MLFloat16>()) : 1.0f;//slx  
   ORT_ENFORCE(repetition_penalty > 0.0f, "repetition_penalty shall be greater than 0, got ", repetition_penalty);
 }
 
