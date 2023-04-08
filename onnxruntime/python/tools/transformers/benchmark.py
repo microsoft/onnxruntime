5cd613fec5f0c933cf87252a7a3628234a042d9e
Ryan Hill <ryanhill@microsoft.com>
2023-04-07 17:10:08 -0700

    Python format

--- onnxruntime/python/tools/transformers/benchmark.py
+++ onnxruntime/python/tools/transformers/benchmark.py
@@ -344,7 +344,7 @@ def run_pytorch(
 
         if config.model_type in ["vit", "swin"]:
             # These models don't use sequence lengths, so just pick the first sequence length so that the summary still works
-            sequence_lengths = [ sequence_lengths[0] ]
+            sequence_lengths = [sequence_lengths[0]]
         else:
             tokenizer = AutoTokenizer.from_pretrained(model_name, cache_dir=cache_dir)
 
@@ -787,7 +787,7 @@ def main():
         logger.error("int8 is for CPU only")
         return
 
-    if len(args.models)==1 and MODELS[args.models[0]][3] in ["vit", "swim"]:
+    if len(args.models) == 1 and MODELS[args.models[0]][3] in ["vit", "swim"]:
         args.sequence_lengths = [""]
 
     args.num_threads = sorted({cpu_count if x <= 0 else x for x in args.num_threads})
