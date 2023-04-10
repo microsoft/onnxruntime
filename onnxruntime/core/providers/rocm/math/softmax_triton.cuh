e74cc8686b95dbb28f3e23dad94fa2582785861c
kailums <kalu@microsoft.com>
2023-04-10 04:17:04 +0000

    fix softmax select kernel bug

--- onnxruntime/core/providers/rocm/math/softmax_triton.cuh
+++ onnxruntime/core/providers/rocm/math/softmax_triton.cuh
@@ -16,18 +16,23 @@ namespace rocm {
 #ifdef ENABLE_TRITON_LIB
 
 namespace {
-// TODO: onnxruntime softmax is not support output has different type between input
-// so actually there is only one dtype
 
 template <typename T>
-std::string GetSoftmaxTritonName(int softmax_elements) {
-  int block_size = NextPowerOf2(softmax_elements);
+std::string GetSoftmaxTritonName(int num_elements) {
   std::string ret = "softmax_";
   std::string type_name = GetDataTypeName<T>();
   ret += type_name;
   ret += "_";
-  if (block_size <= 1024) {
-    block_size = 1024;
+
+  // search for most suitable block_size
+  // the block size must larger than num_elements
+  int block_size = 1024;
+  while (num_elements > block_size) {
+    block_size *= 2;
+  }
+  // current max block_size is 16384
+  if (block_size > 16384) {
+    return ret;  // this will cause LaunchKernel return not supported error
   }
   ret += std::to_string(block_size);
   return ret;
@@ -35,8 +40,13 @@ std::string GetSoftmaxTritonName(int softmax_elements) {
 
 }  // end of namespace
 
+// TODO: onnxruntime softmax is not support output has different type between input
+// so actually T and OutputT should be same type
 template <typename T, typename OutputT>
 Status SoftmaxTritonOp(const SoftmaxParams<T, OutputT>* params) {
+  if (params->is_log_softmax) {
+    TUNABLE_OP_RETURN_UNSUPPORTED_ARGUMENT_IF(true, "softmax triton not support log-softmax");
+  }
   auto fname = GetSoftmaxTritonName<T>(params->softmax_elements);
 
   // construct args for launch kernel
