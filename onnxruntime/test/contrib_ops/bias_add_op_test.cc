7608fd79d8ead590382c6aebaeb8d005d43cfd47
Patrice Vignola <vignola.patrice@gmail.com>
2023-04-10 14:46:33 -0700

    [DML EP] Add BiasAdd (#15211)

--- onnxruntime/test/contrib_ops/bias_add_op_test.cc
+++ onnxruntime/test/contrib_ops/bias_add_op_test.cc
@@ -13,7 +13,7 @@ using namespace onnxruntime::test;
 namespace onnxruntime {
 namespace test {
 
-#if defined(USE_CUDA) || defined(USE_ROCM)
+#if defined(USE_CUDA) || defined(USE_ROCM) || defined(USE_DML)
 static std::vector<float> GetExpectedResult(const std::vector<float>& input_data,
                                             const std::vector<float>& bias_data,
                                             const std::vector<float>& skip_data) {
@@ -39,8 +39,9 @@ static void RunSkipBiasGpuTest(const std::vector<float>& input_data,
   int min_cuda_architecture = use_float16 ? 530 : 0;
   bool enable_cuda = HasCudaEnvironment(min_cuda_architecture);
   bool enable_rocm = (nullptr != DefaultRocmExecutionProvider().get());
+  bool enable_dml = (nullptr != DefaultDmlExecutionProvider().get());
 
-  if (!enable_cuda && !enable_rocm) {
+  if (!enable_cuda && !enable_rocm && !enable_dml) {
     return;
   }
 
@@ -64,6 +65,9 @@ static void RunSkipBiasGpuTest(const std::vector<float>& input_data,
   }
   if (enable_rocm) {
     execution_providers.push_back(DefaultRocmExecutionProvider());
+  }
+  if (enable_dml) {
+    execution_providers.push_back(DefaultDmlExecutionProvider());
   }
   tester.Run(OpTester::ExpectResult::kExpectSuccess, "", {}, nullptr, &execution_providers);
 }
