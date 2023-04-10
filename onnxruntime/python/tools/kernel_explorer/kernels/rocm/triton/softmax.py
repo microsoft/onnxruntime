26478f25e990fc00bd38a796941d8ba32995a871
kailums <kalu@microsoft.com>
2023-04-10 07:37:19 +0000

    add compile script

--- onnxruntime/python/tools/kernel_explorer/kernels/rocm/triton/softmax.py
+++ onnxruntime/python/tools/kernel_explorer/kernels/rocm/triton/softmax.py
@@ -30,61 +30,32 @@ def softmax_kernel(
     output_ptrs = output_row_start_ptr + col_offsets
     tl.store(output_ptrs, softmax_output, mask=col_offsets < n_cols)
 
-def compile(name_mp, lib_dir):
-    def compile_one(sig, block_size):
+
+#function_table = {'name': name, 'func': func, 'sig'=sig, kwargs={}},
+
+dtypes = ['fp32', 'fp16']
+blocks = [1024, 2048, 4096, 8192, 16384]
+name_pattern = 'softmax_{}_{}'
+sig_pattern = '*{},*{},i32,i32,i32'
+
+def GetSoftmaxFunctionTable():
+    func_table = []
+    def get_num_warps(block_size):
         num_warps = 4
         if block_size >= 2048:
             num_warps = 8
         if block_size >= 4096:
             num_warps = 16
+        return num_warps
 
-        ret = triton.compile(softmax_kernel, signature=sig, num_warps=num_warps, constants={"BLOCK_SIZE": block_size})
-        return ret
-
-    metadata = []
-    for name in name_mp:
-        sig, block_size = name_mp[name]
-        ret = compile_one(sig, block_size)
-        compile_res = {}
-        compile_res['name'] = name
-        compile_res['func_name'] = ret.metadata['name']
-        compile_res['num_warps'] = ret.metadata['num_warps']
-        compile_res['shared'] = ret.metadata['shared']
-        compile_res['BLOCK_SIZE'] = block_size
-        # move tmp hsaco file into current dir
-        lib_name = f'{name}.hasco'
-        shutil.copyfile(ret.asm['hsaco_path'], f'{lib_dir}/{lib_name}')
-        compile_res['lib_file'] = lib_name
-        metadata.append(compile_res)
-
-    return metadata
-
-dtypes = ['fp32', 'fp16']
-onnx_types = ['float', 'half']
-blocks = [1024, 2048, 4096, 8192, 16384]
-name_pattern = 'softmax_{}_{}'
-sig_pattern = '*{},*{},i32,i32,i32'
-
-def main():
-    name_mp = {}
-    for dtype, onnx_t in zip(dtypes, onnx_types):
+    for dtype in dtypes:
         for b in blocks:
-            name = name_pattern.format(onnx_t, b)
+            name = name_pattern.format(dtype, b)
             sig = sig_pattern.format(dtype, dtype)
-            name_mp[name] = [sig, b]
+            num_warps = get_num_warps(b)
+            kwargs = {'num_warps': num_warps, 'constants': {'BLOCK_SIZE': b}}
+            func_desc = {'name':name, 'func': softmax_kernel, 'sig': sig, 'kwargs': kwargs}
+            func_table.append(func_desc)
 
-    lib_dir = './libs'
-    if not os.path.exists(lib_dir):
-        os.mkdir(lib_dir)
-
-    metadata = compile(name_mp, lib_dir)
-    print('compile done.')
-
-    # save metadata into json file
-    with open(f'{lib_dir}/meta.json', 'w') as fp:
-        json.dump(metadata, fp)
-    print('save into file done.')
-
-if __name__ == '__main__':
-    main()
+    return func_table
 
