6b0daecaf769b5a83cda75221b1aa75afde38bed
Yulong Wang <7679871+fs-eire@users.noreply.github.com>
2023-04-11 15:38:27 -0700

    [build] fix default value of flag cmake_generator

--- tools/ci_build/build.py
+++ tools/ci_build/build.py
@@ -543,7 +543,7 @@ def parse_arguments():
             "NMake Makefiles",
             "Xcode",
         ],
-        default="Visual Studio 16 2019" if is_windows() else None,
+        default=None,
         help="Specify the generator that CMake invokes. ",
     )
     parser.add_argument(
@@ -699,6 +699,12 @@ def parse_arguments():
     if not args.disable_wasm_exception_catching or args.enable_wasm_api_exception_catching:
         # doesn't make sense to catch if no one throws
         args.enable_wasm_exception_throwing_override = True
+
+    if args.cmake_generator is None:
+        if args.build_wasm:
+            args.cmake_generator = "Ninja"
+        elif is_windows():
+            args.cmake_generator = "Visual Studio 16 2019"
 
     return args
 
