3be5bfe363ad602c6aa96c2d42df6028d55158d9
Patrice Vignola <vignola.patrice@gmail.com>
2023-04-11 08:31:04 -0700

    [DML EP] Add MatMul + SoftMax fusion (#15240)

--- onnxruntime/core/providers/dml/OperatorAuthorHelper/OperatorVersions.h
+++ onnxruntime/core/providers/dml/OperatorAuthorHelper/OperatorVersions.h
@@ -406,6 +406,7 @@ namespace OperatorHelper
         static const int sc_sinceVer_Gelu = 1;
         static const int sc_sinceVer_BiasGelu = 1;
         static const int sc_sinceVer_FusedMatMul = 1;
+        static const int sc_sinceVer_FusedMatMulActivation = 1;
         static const int sc_sinceVer_QLinearSigmoid = 1;
         static const int sc_sinceVer_Attention = 1;
         static const int sc_sinceVer_SkipLayerNormalization = 1;
