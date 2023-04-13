15b9725bd8cbfd262f6e11822bde6e30d9476ff7
Yi Zhang <zhanyi@microsoft.com>
2023-04-13 11:51:12 +0800

    download ccache zip

--- tools/ci_build/github/linux/docker/inference/x64/python/cpu/scripts/install_deps.sh
+++ tools/ci_build/github/linux/docker/inference/x64/python/cpu/scripts/install_deps.sh
@@ -17,13 +17,20 @@ done
 
 # No release binary for ccache aarch64, so we need to build it from source.
 if ! [ -x "$(command -v ccache)" ]; then
-    git clone --recursive --branch v4.7.4 https://github.com/ccache/ccache
+    ccache_url="https://github.com/ccache/ccache/archive/refs/tags/v4.8.tar.gz"
     pushd .
-    cd ccache
+    curl -sSL --retry 5 --retry-delay 10 --create-dirs --fail -L -o ccache_src.tar.gz $ccache_url
+    mkdir ccache_main
+    cd ccache_main
+    tar -zxf ../ccache_src.tar.gz --strip=1
+
     mkdir build
     cd build
-    cmake -DCMAKE_INSTALL_PREFIX=/usr/local _DCMAKE_BUILD_TYPE=Releae ..
+    cmake -DCMAKE_INSTALL_PREFIX=/usr/local _DCMAKE_BUILD_TYPE=Release ..
     make
     make install
     which ccache
+    popd
+    rm -f ccache_src.tar.gz
+    rm -rf ccache_src
 fi
