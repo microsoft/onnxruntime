922f51e9c43bdfd67f989cea37bfc96df6a4450a
Cheng Tang <chenta@microsoft.com@orttrainingdev9.d32nl1ml4oruzj4qz3bqlggovf.px.internal.cloudapp.net>
2023-04-16 04:22:26 +0000

    provide device holder

--- onnxruntime/core/framework/device_stream_collection.cc
+++ onnxruntime/core/framework/device_stream_collection.cc
@@ -9,7 +9,7 @@ namespace onnxruntime {
 
 class DeviceStreamCollectionImpl {
  public:
-  DeviceStreamCollectionImpl(size_t num_streams, const SessionState& sess_state) : num_streams_(num_streams) {
+  DeviceStreamCollectionImpl(size_t num_streams, const SessionState& sess_state) : num_streams_(num_streams), device_(OrtDevice()) {
     device_streams_.resize(num_streams, nullptr);
     owned_streams_.reserve(num_streams);
     auto& providers = sess_state.GetExecutionProviders();
@@ -18,7 +18,7 @@ class DeviceStreamCollectionImpl {
       eps_.push_back(ep);
     }
     is_main_graph_ = sess_state.GetGraphViewer().ParentNode() == nullptr;
-    //root_stream_ = std::make_unique<Stream>(nullptr, OrtDevice());
+    root_stream_ = std::make_unique<Stream>(nullptr, device_);
   }
 
   ~DeviceStreamCollectionImpl() {
@@ -98,6 +98,7 @@ class DeviceStreamCollectionImpl {
   // This is used in ExecutionFrame when memory pattern is enabled, to allocate the peak size memory
   // labelled this stream in the current thread, instead of the default stream which will be used in all the threads (thus caused thread safe issue)
   std::unique_ptr<Stream> root_stream_;
+  OrtDevice device_;
   void ReleaseSingleStreamBuffers();
 };
 
