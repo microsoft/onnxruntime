5965ba4fc7feed1820442afdcf118e1a3b248ff4
Tang, Cheng <souptc@gmail.com>
2023-04-20 11:22:30 -0700

    Update onnxruntime/test/python/onnxruntime_test_collective.py
    Co-authored-by: Wei-Sheng Chin <wschin@outlook.com>

--- onnxruntime/test/python/onnxruntime_test_collective.py
+++ onnxruntime/test/python/onnxruntime_test_collective.py
@@ -30,7 +30,7 @@ class ORTBertPretrainTest(unittest.TestCase):
     def _create_allgather_ut_model(self, shape, axis):
         X = helper.make_tensor_value_info("X", TensorProto.FLOAT, shape)  # noqa: N806
         rank, size = self._get_rank_size()
-        output_shape = [s * size if _ == axis else s for _, s in enumerate(shape)]
+        output_shape = [s * size if axis_index == axis else s for axis_index, s in enumerate(shape)]
         Y = helper.make_tensor_value_info("Y", TensorProto.FLOAT, output_shape)  # noqa: N806
         node_def = helper.make_node("AllGather", ["X"], ["Y"], domain="com.microsoft", group_size=size, axis=axis)
         graph_def = helper.make_graph(
