4341900fcad10a7d6226fe759fe13504a86f3820
peixuanzuo <peixuanzuo@microsoft.com>
2023-04-25 10:58:15 +0000

    update to use hipify to generate beam search file

--- cmake/onnxruntime_rocm_hipify.cmake
+++ cmake/onnxruntime_rocm_hipify.cmake
@@ -82,7 +82,6 @@ set(contrib_ops_excluded_files
   "tensor/image_scaler.h"
   "tensor/image_scaler_impl.cu"
   "tensor/image_scaler_impl.h"
-  "transformers/beam_search.cc"
   "transformers/greedy_search.cc"
   "transformers/greedy_search.h"
   "conv_transpose_with_dynamic_pads.cc"
