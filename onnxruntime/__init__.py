374cbfe5ff26d350ab0ee3e59f94f0e6dc805e64
Yuhong Guo <yuhong.gyh@antgroup.com>
2023-04-26 14:49:27 +0800

    Add get_build_info to pybind

--- onnxruntime/__init__.py
+++ onnxruntime/__init__.py
@@ -39,6 +39,7 @@ try:
     from onnxruntime.capi._pybind_state import get_all_providers  # noqa: F401
     from onnxruntime.capi._pybind_state import get_available_providers  # noqa: F401
     from onnxruntime.capi._pybind_state import get_device  # noqa: F401
+    from onnxruntime.capi._pybind_state import get_build_info  # noqa: F401
     from onnxruntime.capi._pybind_state import get_version_string  # noqa: F401
     from onnxruntime.capi._pybind_state import set_default_logger_severity  # noqa: F401
     from onnxruntime.capi._pybind_state import set_default_logger_verbosity  # noqa: F401
