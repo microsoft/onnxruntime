74ee55a4ccaf1c5f085057100c2f9139e6f05617
Wanming Lin <wanming.lin@intel.com>
2023-04-27 10:09:03 +0800

    Fixed lint errors

--- js/web/lib/wasm/session-options.ts
+++ js/web/lib/wasm/session-options.ts
@@ -69,17 +69,23 @@ const setExecutionProviders =
             if (typeof ep !== 'string') {
               const webnnOptions = ep as InferenceSession.WebNNExecutionProviderOption;
               if (webnnOptions?.deviceType) {
-                const keyDataOffset = allocWasmString("deviceType", allocs);
+                const keyDataOffset = allocWasmString('deviceType', allocs);
                 const valueDataOffset = allocWasmString(webnnOptions.deviceType.toString(), allocs);
-                if (getInstance()._OrtAddSessionConfigEntry(sessionOptionsHandle, keyDataOffset, valueDataOffset) !== 0) {
-                  throw new Error(`Can't set a session config entry: "deviceType" - ${webnnOptions.deviceType}`);
+                if (getInstance()._OrtAddSessionConfigEntry(
+                    sessionOptionsHandle, keyDataOffset, valueDataOffset) !== 0) {
+                  throw new Error(
+                    `Can't set a session config entry: 'deviceType' - ${webnnOptions.deviceType}`
+                  );
                 }
               }
               if (webnnOptions?.powerPreference) {
-                const keyDataOffset = allocWasmString("powerPreference", allocs);
+                const keyDataOffset = allocWasmString('powerPreference', allocs);
                 const valueDataOffset = allocWasmString(webnnOptions.powerPreference.toString(), allocs);
-                if (getInstance()._OrtAddSessionConfigEntry(sessionOptionsHandle, keyDataOffset, valueDataOffset) !== 0) {
-                  throw new Error(`Can't set a session config entry: "powerPreference" - ${webnnOptions.powerPreference}`);
+                if (getInstance()._OrtAddSessionConfigEntry(
+                    sessionOptionsHandle, keyDataOffset, valueDataOffset) !== 0) {
+                  throw new Error(
+                    `Can't set a session config entry: 'powerPreference' - ${webnnOptions.powerPreference}`
+                  );
                 }
               }
             }
