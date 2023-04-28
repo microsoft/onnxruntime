7481ce29646499da6ff8c7774757587a051a1445
Changming Sun <chasun@microsoft.com>
2023-04-27 20:44:26 -0700

    update

--- tools/ci_build/github/linux/docker/inference/x64/python/cpu/scripts/install_centos.sh
+++ tools/ci_build/github/linux/docker/inference/x64/python/cpu/scripts/install_centos.sh
@@ -4,4 +4,4 @@ set -e
 os_major_version=$(cat /etc/redhat-release | tr -dc '0-9.'|cut -d \. -f1)
 
 echo "installing for os major version : $os_major_version"
-yum install -y which gdb redhat-lsb-core expat-devel tar unzip zlib-devel make libunwind bzip2 bzip2-devel
+yum install -y which gdb redhat-lsb-core expat-devel tar unzip zlib-devel make bzip2 bzip2-devel
