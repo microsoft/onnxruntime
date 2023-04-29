53340ef307bc6c888756b84e96af986865bfac24
rachguo <rachguo@rachguos-Mini.attlocal.net>
2023-04-28 18:00:36 -0700

    fix

--- js/react_native/android/src/main/java/ai/onnxruntime/reactnative/OnnxruntimeExtensionsDisabled.java
+++ js/react_native/android/src/main/java/ai/onnxruntime/reactnative/OnnxruntimeExtensionsDisabled.java
@@ -8,9 +8,11 @@ import android.util.Log;
 
 class OnnxruntimeExtensions {
   public void registerOrtExtensionsIfEnabled(SessionOptions sessionOptions) {
-    Log.d("Info",
-          "ORT Extensions is not enabled in the current configuration. If you want to enable this support,"
-              + "please add \"onnxruntimeEnableExtensions\": \"true\" in your project root directory package.json.");
+    Log.d(
+        "Info",
+        "ORT Extensions is not enabled in the current configuration. If you want to enable this support,"
+            +
+            "please add \"onnxruntimeEnableExtensions\": \"true\" as a top-level entry in your project's root directory package.json.");
     return;
   }
 }
