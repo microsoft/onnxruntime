28063f1058537cc6d589e6494b85aad63b1a2819
Chen Fu <1316708+chenfucn@users.noreply.github.com>
2023-05-07 10:32:28 -0700

    post processing activation only when nchw and sum fusion present.

--- onnxruntime/core/providers/cpu/fp16/fp16_conv.cc
+++ onnxruntime/core/providers/cpu/fp16/fp16_conv.cc
@@ -336,7 +336,7 @@ Status FusedConvFp16::Compute(OpKernelContext* context) const {
   const auto* Xdata = X->Data<MLFloat16>();
   const auto* Bdata = B != nullptr ? B->Data<MLFloat16>() : nullptr;
   auto* Ydata = Y->MutableData<MLFloat16>();
-  const auto* SumData = Sum != nullptr ? Sum->Data<MLFloat16>() : nullptr;
+  const auto* sum_data = Sum != nullptr ? Sum->Data<MLFloat16>() : nullptr;
 
   BufferUniquePtr transpose_input_buffer;
   BufferUniquePtr transpose_output_buffer;
@@ -407,7 +407,7 @@ Status FusedConvFp16::Compute(OpKernelContext* context) const {
   for (int64_t image_id = 0; image_id < N; ++image_id) {
     const auto* input_data = Xdata;
     auto* output_data = Ydata;
-    const auto* add_src = SumData;
+    const auto* add_src = sum_data;
 
     if (!channels_last_) {
       // Transpose the input from channels first (CHW) to channels last (HWC).
@@ -476,7 +476,7 @@ Status FusedConvFp16::Compute(OpKernelContext* context) const {
             static_cast<size_t>(M),
             static_cast<size_t>(output_count),
             static_cast<size_t>(kernel_size),
-            channels_last_ ? &act : nullptr);
+            (!channels_last_ && sum_data) ? nullptr : &act);
       } else {
         for (int64_t group_id = 0; group_id < group_count; ++group_id) {
           // Prepare the im2col transformation or use the input buffer directly for
@@ -552,7 +552,7 @@ Status FusedConvFp16::Compute(OpKernelContext* context) const {
           gemm_params.C = worker_output + group_id * group_output_channels;
           gemm_params.ldc = static_cast<size_t>(M);
           gemm_params.Bias = Bdata;
-          gemm_params.OutputProcessor = channels_last_ ? &act : nullptr;  // process fused activation and add
+          gemm_params.OutputProcessor = (!channels_last_ && sum_data) ? nullptr : &act;  // process fused activation and add
 
           MlasHalfGemmBatch(
               static_cast<size_t>(output_count),
@@ -572,8 +572,8 @@ Status FusedConvFp16::Compute(OpKernelContext* context) const {
           Ydata,
           static_cast<size_t>(output_image_size),
           static_cast<size_t>(M));
-      if (SumData != nullptr) {
-        MLAS_HALF_GEMM_ACTIVATION_PROCESSOR proc(activation_, SumData);
+      if (sum_data != nullptr) {
+        MLAS_HALF_GEMM_ACTIVATION_PROCESSOR proc(activation_, sum_data);
         proc.Process(Ydata, 0, 0, static_cast<size_t>(M),
                      static_cast<size_t>(output_image_size),
                      static_cast<size_t>(output_image_size));
@@ -582,8 +582,8 @@ Status FusedConvFp16::Compute(OpKernelContext* context) const {
 
     Xdata += X_offset;
     Ydata += Y_offset;
-    if (SumData != nullptr) {
-      SumData += Y_offset;
+    if (sum_data != nullptr) {
+      sum_data += Y_offset;
     }
   }
 
