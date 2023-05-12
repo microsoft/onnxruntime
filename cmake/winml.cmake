e1ad3c40278579e775ab8f89a95138c650040c6c
Jian Chen <cjian@microsoft.com>
2023-05-11 22:39:41 -0700

    ignore a winml warning.

--- cmake/winml.cmake
+++ cmake/winml.cmake
@@ -344,6 +344,9 @@ if (onnxruntime_USE_DML)
   target_add_dml(winml_adapter)
 endif()
 
+#Please remove this after updating to a newer windows SDK
+target_link_options(winml_adapter PRIVATE /ignore:5038)
+
 # add it to the onnxruntime shared library
 set(onnxruntime_winml winml_adapter)
 list(APPEND onnxruntime_EXTERNAL_DEPENDENCIES winml_adapter)
