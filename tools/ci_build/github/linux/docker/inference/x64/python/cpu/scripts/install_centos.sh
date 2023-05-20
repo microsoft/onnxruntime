a7d263623e11976ba7881a227b183f1ec295d4c2
Randy Shuai <rashuai@microsoft.com>
2023-05-19 17:05:51 -0700

    install wget

--- tools/ci_build/github/linux/docker/inference/x64/python/cpu/scripts/install_centos.sh
+++ tools/ci_build/github/linux/docker/inference/x64/python/cpu/scripts/install_centos.sh
@@ -4,7 +4,7 @@ set -e
 os_major_version=$(cat /etc/redhat-release | tr -dc '0-9.'|cut -d \. -f1)
 
 echo "installing for os major version : $os_major_version"
-yum install -y which gdb redhat-lsb-core expat-devel tar unzip zlib-devel make libunwind bzip2 bzip2-devel perl-IPC-Cmd python3 openssl-devel
+yum install -y which gdb redhat-lsb-core expat-devel tar unzip zlib-devel make libunwind bzip2 bzip2-devel perl-IPC-Cmd python3 openssl-devel wget
 
 echo "installing rapidjson for AzureEP"
 wget https://github.com/Tencent/rapidjson/archive/refs/tags/v1.1.0.tar.gz
