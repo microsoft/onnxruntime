d1993ded5e3eeb465862c056a471b5ed09c16196
Randy Shuai <rashuai@microsoft.com>
2023-05-22 12:31:42 -0700

    set path to python3

--- tools/ci_build/github/linux/docker/inference/x64/python/cpu/scripts/install_centos.sh
+++ tools/ci_build/github/linux/docker/inference/x64/python/cpu/scripts/install_centos.sh
@@ -4,7 +4,8 @@ set -e
 os_major_version=$(cat /etc/redhat-release | tr -dc '0-9.'|cut -d \. -f1)
 
 echo "installing for os major version : $os_major_version"
-yum install -y which gdb redhat-lsb-core expat-devel tar unzip zlib-devel make libunwind bzip2 bzip2-devel perl-IPC-Cmd python3 openssl-devel wget
+yum install -y which gdb redhat-lsb-core expat-devel tar unzip zlib-devel make libunwind bzip2 bzip2-devel perl-IPC-Cmd openssl-devel wget
+export PATH=/opt/python/cp38-cp38/bin:$PATH
 
 echo "installing rapidjson for AzureEP"
 wget https://github.com/Tencent/rapidjson/archive/refs/tags/v1.1.0.tar.gz
