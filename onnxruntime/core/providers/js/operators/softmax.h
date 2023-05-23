b634344cc0ebfc2d916b6dff9ab2a2564dfbbff5
Jian Chen <cjian@microsoft.com>
2023-05-23 11:36:06 -0700

    Init commit for Cjian/softmax

---
+++ onnxruntime/core/providers/js/operators/softmax.h
@@ -0,0 +1,13 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+#pragma once
+
+namespace onnxruntime {
+
+    class softmax {
+
+    };
+
+} // onnxruntime
+
