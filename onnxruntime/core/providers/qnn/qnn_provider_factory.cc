efc84a43e810b05a440d7baff1b40b14e2118085
Adrian Lizarraga <adlizarraga@microsoft.com>
2023-05-23 17:56:32 -0700

    [QNN EP] Add session option to disable fallback to default CPU EP (#16016)
    ### Description
    Adds the session config option `disable_cpu_ep_fallback` to allow the
    user to prevent the CPU EP from handling
    nodes not supported by other execution providers.
    
    ```C++
    // Graph nodes that are not supported by the execution providers (EPs) explicitly added to the session are
    // assigned (i.e., "fallback") to the CPU EP by default.
    //
    // This option allows the user to disable the fallback of unsupported graph nodes to the CPU EP.
    // If this option is set to "1", session creation will fail if the execution providers other than the CPU EP cannot
    // fully support all of the nodes in the graph.
    //
    // It is invalid to set this option and explicitly add the CPU EP to the session. In this case, session creation
    // will also fail with an error.
    //
    // Option values:
    // - "0": CPU EP fallback is not disabled. [DEFAULT]
    // - "1": CPU EP fallback is disabled.
    static const char* const kOrtSessionOptionsDisableCPUEPFallback = "session.disable_cpu_ep_fallback";
    ```
    
    #### Example use
    ```C++
    #include "core/session/onnxruntime_cxx_api.h"
    #include "core/session/onnxruntime_session_options_config_keys.h"
    
    int main(int argc, char** argv) {
        Ort::SessionOptions so;
        so.AddConfigEntry(kOrtSessionOptionsDisableCPUEPFallback, "1");  // Disable fallback to the CPU EP.
    
        onnxruntime::ProviderOptions options;
    #if defined(_WIN32)
        options["backend_path"] = "QnnCpu.dll";
    #else
        options["backend_path"] = "libQnnCpu.so";
    #endif
    
        so.AppendExecutionProvider("QNN", options);
    
        const ORTCHAR_T* ort_model_path = ORT_MODEL_FOLDER "qnn_ep_partial_support.onnx";
        Ort::Session session(*ort_env, ort_model_path, so);  // Throws exception if nodes fallback to CPU
        // ...
    ```
    
    ### Motivation and Context
    Makes it easier for application developers to ensure that the entire
    model runs on specific EPs. This is critical for Qualcomm/scenarios. If
    the compute cannot be offloaded to the NPU, running on CPU is not
    acceptable. (could be the difference between 90 second inference and 6
    seconds inference)
    
    ---------
    
    Co-authored-by: Pranav Sharma <prs@microsoft.com>

--- onnxruntime/core/providers/qnn/qnn_provider_factory.cc
+++ onnxruntime/core/providers/qnn/qnn_provider_factory.cc
@@ -9,22 +9,25 @@
 
 namespace onnxruntime {
 struct QNNProviderFactory : IExecutionProviderFactory {
-  QNNProviderFactory(const ProviderOptions& provider_options_map) : provider_options_map_(provider_options_map) {
+  QNNProviderFactory(const ProviderOptions& provider_options_map, const SessionOptions* session_options)
+      : provider_options_map_(provider_options_map), session_options_(session_options) {
   }
 
   ~QNNProviderFactory() override {
   }
 
   std::unique_ptr<IExecutionProvider> CreateProvider() override {
-    return std::make_unique<QNNExecutionProvider>(provider_options_map_);
+    return std::make_unique<QNNExecutionProvider>(provider_options_map_, session_options_);
   }
 
  private:
   ProviderOptions provider_options_map_;
+  const SessionOptions* session_options_;
 };
 
-std::shared_ptr<IExecutionProviderFactory> QNNProviderFactoryCreator::Create(const ProviderOptions& provider_options_map) {
-  return std::make_shared<onnxruntime::QNNProviderFactory>(provider_options_map);
+std::shared_ptr<IExecutionProviderFactory> QNNProviderFactoryCreator::Create(const ProviderOptions& provider_options_map,
+                                                                             const SessionOptions* session_options) {
+  return std::make_shared<onnxruntime::QNNProviderFactory>(provider_options_map, session_options);
 }
 
 }  // namespace onnxruntime
