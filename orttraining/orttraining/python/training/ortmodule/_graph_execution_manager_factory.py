38eab98187bff0c5731a3a70247fb6044a5fd563
pengwa <pengwa@microsoft.com>
2023-05-25 09:28:20 +0800

    Type hint for ORTModule (#15938)
    ### Type hint for ORTModule
    
    Add Type hint for ORTModule
    Refine comments. 
    
    The reason of removing theinterface execution_session_run_forward from
    `orttraining/orttraining/python/training/ortmodule/_graph_execution_manager.py`:
    
    PR
    https://github.com/microsoft/onnxruntime/commit/535dc50762ce7eb4e75b8651ca88254cc8f479fc#diff-497e18dc8878818205b81fd80f85942548d8aa15d0f1204ce3e3d9795e3dd195
    and some commit before it breaks the function interface contracts
    between parent calss _graph_execution_manager.py and its children
    _training_manager.py and _inference_manager.py. So there is no need to
    have this interface.
    
    
    ### Other EE work opportunities
    
    1. Use logger correctly. 
    2. Remove few duplication logic parsing input/output recursively.
    3. Clean up environment variable usage.

--- orttraining/orttraining/python/training/ortmodule/_graph_execution_manager_factory.py
+++ orttraining/orttraining/python/training/ortmodule/_graph_execution_manager_factory.py
@@ -3,18 +3,21 @@
 # Licensed under the MIT License.
 # --------------------------------------------------------------------------
 
+from typing import Union
+
 from ._fallback import _FallbackManager
 from ._inference_manager import InferenceManager
+from ._io import _FlattenedModule
 from ._training_manager import TrainingManager
 from .debug_options import DebugOptions
 
 
 class GraphExecutionManagerFactory:
-    def __init__(self, module, debug_options: DebugOptions, fallback_manager: _FallbackManager):
+    def __init__(self, module: _FlattenedModule, debug_options: DebugOptions, fallback_manager: _FallbackManager):
         self._training_manager = TrainingManager(module, debug_options, fallback_manager)
         self._inference_manager = InferenceManager(module, debug_options, fallback_manager)
 
-    def __call__(self, is_training):
+    def __call__(self, is_training) -> Union[InferenceManager, TrainingManager]:
         if is_training:
             return self._training_manager
         else:
