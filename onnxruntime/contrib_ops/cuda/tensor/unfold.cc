84f29899734436ecfb14c8d5734f158dcd28d3a0
Lei Zhang <zhang.huanning@hotmail.com>
2023-05-25 16:59:26 -0700

    add unit test

--- onnxruntime/contrib_ops/cuda/tensor/unfold.cc
+++ onnxruntime/contrib_ops/cuda/tensor/unfold.cc
@@ -19,7 +19,7 @@ ONNX_OPERATOR_KERNEL_EX(
     1,
     kCudaExecutionProvider,
     (*KernelDefBuilder::Create())
-        .TypeConstraint("T", DataTypeImpl::AllFixedSizeTensorTypes()),
+        .TypeConstraint("T", DataTypeImpl::AllTensorTypes()),
     UnfoldTensor);
 
 Status UnfoldTensor::ComputeInternal(OpKernelContext* ctx) const {
@@ -31,22 +31,22 @@ Status UnfoldTensor::ComputeInternal(OpKernelContext* ctx) const {
   ORT_ENFORCE(dim < rank, "input rank:", rank, " is not bigger than attribut specified dim: ", dim);
   ORT_ENFORCE(input_dims[dim] >= size_, "dimsize:", input_dims[dim], " is less than unfold size:", size_);
 
-  std::vector<int64_t> output_dims(rank + 1, 0);
-  std::copy_n(input_dims.begin(), dim, output_dims.begin());
   int64_t leading_dims = std::accumulate(input_dims.begin(), input_dims.begin() + dim, 1LL, std::multiplies<int64_t>());
-  output_dims[dim] = (input_dims[dim] - size_) / step_ + 1;
-  output_dims[dim+1] = size_;
-  std::copy(input_dims.begin() + (dim + 1), input_dims.end(), output_dims.begin() + (dim + 2));
   int64_t tailing_dims = std::accumulate(input_dims.begin() + (dim + 1), input_dims.end(), 1LL, std::multiplies<int64_t>());
 
+  std::vector<int64_t> output_dims(rank + 1, 0);
+  std::copy(input_dims.begin(), input_dims.end(), output_dims.begin());
+  output_dims[dim] = (input_dims[dim] - size_) / step_ + 1;
+  output_dims.back() = size_;
   TensorShape output_shape(output_dims);
   Tensor* output_tensor = ctx->Output(0, output_shape);
 
   cudaStream_t stream = this->Stream(ctx);
   const cudaDeviceProp& device_prop = this->GetDeviceProp();
   size_t element_size = input_tensor.DataType()->Size();
-  return LaunchUnfoldTensor(stream, device_prop, element_size, input_tensor.DataRaw(), output_tensor->MutableDataRaw(),
-                            leading_dims, tailing_dims, input_dims[dim], unfold_size, step);
+  return onnxruntime::contrib::cuda::LaunchUnfoldTensor(
+      stream, device_prop, element_size, input_tensor.DataRaw(), output_tensor->MutableDataRaw(),
+      leading_dims, tailing_dims, input_dims[dim], size_, step_);
 }
 
 }  // namespace cuda
