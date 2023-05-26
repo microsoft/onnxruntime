7e81130b0d278994e16d67a6aa9fec71081145b9
Jian Chen <cjian@microsoft.com>
2023-05-25 22:22:37 -0700

    Lint

--- onnxruntime/core/optimizer/identical_children_consolidation.cc
+++ onnxruntime/core/optimizer/identical_children_consolidation.cc
@@ -60,7 +60,7 @@ std::vector<std::vector<NodeIndex>> IdenticalChildrenConsolidation::DivideIdenti
   return groups;
 }
 
- std::string_view IdenticalChildrenConsolidation::IdentityBuilder(const Graph& graph, const Node& node) const {
+std::string_view IdenticalChildrenConsolidation::IdentityBuilder(const Graph& graph, const Node& node) const {
   std::string identity;
   for (const auto* input_def : node.InputDefs()) {
     if (input_def->Exists() && !input_def->Name().empty()) {
@@ -118,7 +118,7 @@ std::vector<std::vector<NodeIndex>> IdenticalChildrenConsolidation::DivideIdenti
         identity.append(name);
       }
     } else {
-      return  ignore_identity  ;
+      return ignore_identity;
     }
     identity.append("####");
   }
