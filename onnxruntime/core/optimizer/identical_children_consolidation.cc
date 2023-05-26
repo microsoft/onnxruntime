b834aec04e2dabf7b2e61722456d129e4fbe14c6
Jian Chen <cjian@microsoft.com>
2023-05-26 13:20:37 -0700

    Remove unnecessary std::to_string()

--- onnxruntime/core/optimizer/identical_children_consolidation.cc
+++ onnxruntime/core/optimizer/identical_children_consolidation.cc
@@ -72,37 +72,37 @@ std::string IdenticalChildrenConsolidation::IdentityBuilder(const Graph& graph, 
           Initializer value{*data, graph.ModelPath()};
           switch (static_cast<TensorProto::DataType>(data->data_type())) {
             case TensorProto::DataType::TensorProto_DataType_INT8:
-              identity << std::to_string(value.data<int8_t>()[0]);
+              identity << value.data<int8_t>()[0];
               break;
             case TensorProto::DataType::TensorProto_DataType_INT16:
-              identity << std::to_string(value.data<int16_t>()[0]);
+              identity << value.data<int16_t>()[0];
               break;
             case TensorProto::DataType::TensorProto_DataType_INT32:
-              identity << std::to_string(value.data<int32_t>()[0]);
+              identity << value.data<int32_t>()[0];
               break;
             case TensorProto::DataType::TensorProto_DataType_UINT8:
-              identity << std::to_string(value.data<uint8_t>()[0]);
+              identity << value.data<uint8_t>()[0];
               break;
             case TensorProto::DataType::TensorProto_DataType_UINT16:
-              identity << std::to_string(value.data<uint16_t>()[0]);
+              identity << value.data<uint16_t>()[0];
               break;
             case TensorProto::DataType::TensorProto_DataType_BOOL:
-              identity << std::to_string(value.data<bool>()[0]);
+              identity << value.data<bool>()[0];
               break;
             case TensorProto::DataType::TensorProto_DataType_INT64:
-              identity << std::to_string(value.data<int64_t>()[0]);
+              identity << value.data<int64_t>()[0];
               break;
             case TensorProto::DataType::TensorProto_DataType_UINT32:
-              identity << std::to_string(value.data<uint32_t>()[0]);
+              identity << value.data<uint32_t>()[0];
               break;
             case TensorProto::DataType::TensorProto_DataType_UINT64:
-              identity << std::to_string(value.data<uint64_t>()[0]);
+              identity << value.data<uint64_t>()[0];
               break;
             case TensorProto::DataType::TensorProto_DataType_FLOAT:
-              identity << std::to_string(value.data<float>()[0]);
+              identity << value.data<float>()[0];
               break;
             case TensorProto::DataType::TensorProto_DataType_DOUBLE:
-              identity << std::to_string(value.data<double>()[0]);
+              identity << value.data<double>()[0];
               break;
             case TensorProto::DataType::TensorProto_DataType_STRING:
               identity << value.data<std::string>()[0];
