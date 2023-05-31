4db1db7f325724e35095fc91a9fc443e2fa25ee0
adamlouly <adamlouly3@gmail.com>
2023-05-31 23:27:51 +0000

    add print

--- orttraining/orttraining/test/python/orttraining_test_ortmodule_api.py
+++ orttraining/orttraining/test/python/orttraining_test_ortmodule_api.py
@@ -5733,6 +5733,10 @@ def test_runtime_inspector_label_and_embed_sparsity_detection(embed_is_sparse, l
     found_embed_is_sparse = False
     found_label_is_sparse = False
     for record in caplog.records:
+        print(" ---------------------------------------------------------- ")
+        print(record.getMessage())
+        print(" ---------------------------------------------------------- ")
+
         if "Label sparsity based optimization is on for" in record.getMessage():
             found_label_is_sparse = True
 
