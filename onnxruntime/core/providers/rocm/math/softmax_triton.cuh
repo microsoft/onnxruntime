1df0ecd27bea51e849c644d7ff9722f467031734
mindest <linminuser@gmail.com>
2023-06-01 16:41:39 +0000

    some more refactor on Triton, Softmax

--- onnxruntime/core/providers/rocm/math/softmax_triton.cuh
+++ onnxruntime/core/providers/rocm/math/softmax_triton.cuh
@@ -30,20 +30,20 @@ template <typename T, typename OutputT>
 auto GetSoftmaxTritonOps() {
   std::vector<std::pair<std::string, tunable::Op<SoftmaxParams<T, OutputT>>>> ret;
   auto group_name = GetSoftmaxTritonGroupName<T>();
-  auto *kernel_list = GetOrtTritonKernelByGroup(group_name);
+  auto* kernel_list = GetOrtTritonKernelByGroup(group_name);
   if (kernel_list == nullptr) {
     return ret;
   }
 
   for (auto i : *kernel_list) {
     // check params match
-    auto *metadata = GetOrtTritonKernelMetadata(i);
+    auto* metadata = GetOrtTritonKernelMetadata(i);
     auto block_size = -1;
     const std::string block_name = "BLOCK_SIZE";
     if (metadata->constants.count(block_name) != 0) {
       block_size = metadata->constants.at(block_name);
     }
-    auto impl = [i, block_size](const SoftmaxParams<T, OutputT> *params) -> Status {
+    auto impl = [i, block_size](const SoftmaxParams<T, OutputT>* params) -> Status {
       if (params->is_log_softmax) {
         TUNABLE_OP_RETURN_UNSUPPORTED_ARGUMENT_IF(true, "log_softmax not support");
       }
@@ -52,8 +52,8 @@ auto GetSoftmaxTritonOps() {
       }
       // construct args for launch kernel
       struct {
-        void *out;
-        const void *in;
+        void* out;
+        const void* in;
         int in_stride;
         int out_stride;
         int n_cols;
