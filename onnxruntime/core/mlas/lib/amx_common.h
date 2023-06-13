1d5d8f9e48ecacb28e90138ac4e5d6a2fa572050
Dipanjan Sengupta <dipanjan.sengupta@intel.com>
2023-06-13 03:12:40 +0000

    Fixing the unaligned backslashed for a newline

--- onnxruntime/core/mlas/lib/amx_common.h
+++ onnxruntime/core/mlas/lib/amx_common.h
@@ -33,7 +33,7 @@ Abstract:
 
 #define tile_stored(dst, base, stride) _tile_stored(dst, base, stride)
 
-#define tile_loadconfig(config)		\
+#define tile_loadconfig(config)						\
  _tile_loadconfig(config)
 
 #define tile_storeconfig(config) _tile_storeconfig(config)
@@ -42,33 +42,33 @@ Abstract:
 
 asm(".include \"x86_64/QgemmU8S8KernelAmxCommon.S\"");
 
-#define tile_int8_dp_internal1(name,dst,src1,src2)		\
-  __asm__ volatile						\
+#define tile_int8_dp_internal1(name,dst,src1,src2)			\
+  __asm__ volatile							\
   (""#name" tmm"#dst", tmm"#src1", tmm"#src2"")
 
-#define tile_dpbusd(dst,src1,src2)			\
+#define tile_dpbusd(dst,src1,src2)					\
 tile_int8_dp_internal1(TdpbusdTmmTmmTmm, dst, src1, src2)
 
 #define tile_loadd_internal1(dst,base,stride)				\
-  __asm__ volatile ("TileloaddTmmMem tmm"#dst", %0, %1" \
+  __asm__ volatile ("TileloaddTmmMem tmm"#dst", %0, %1" 		\
    :: "r" ((const void*) (base)), "r" ((long) (stride)))
 
-#define tile_loadd(dst,base,stride)		\
+#define tile_loadd(dst,base,stride)					\
   tile_loadd_internal1(dst, base, stride)
 
 
 #define tile_stored_internal1(dst,base,stride)				\
-  __asm__ volatile ("TileStoredMemTmm tmm"#dst", %0, %1" \
+  __asm__ volatile ("TileStoredMemTmm tmm"#dst", %0, %1" 		\
    :: "r" ((const void*) (base)), "r" ((long) (stride)))
 
-#define tile_stored(dst,base,stride)		\
+#define tile_stored(dst,base,stride)					\
 tile_stored_internal1(dst, base, stride)
 
 
-#define tile_loadconfig(config)		\
-__asm__ volatile ("ldtilecfgMacro %0" :: "r" (((const void *)config))) \
+#define tile_loadconfig(config)						\
+__asm__ volatile ("ldtilecfgMacro %0" :: "r" (((const void *)config)))  \
 
-#define tile_storeconfig(config)		\
-__asm__ volatile ("sttilecfgMacro %0" :: "r" (((const void *)config))) \
+#define tile_storeconfig(config)					\
+__asm__ volatile ("sttilecfgMacro %0" :: "r" (((const void *)config)))  \
 
 #endif
