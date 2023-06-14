5094bb48fccfbbf659c5e0e63e79680d964b95bc
Mengni Wang <mengni.wang@intel.com>
2023-06-14 21:35:16 +0800

    fix python format
    Signed-off-by: Mengni Wang <mengni.wang@intel.com>

--- onnxruntime/python/tools/quantization/quantize.py
+++ onnxruntime/python/tools/quantization/quantize.py
@@ -388,13 +388,14 @@ def quantize_static(
         try:
             importlib.import_module("neural_compressor.adaptor.ox_utils.smooth_quant")
         except Exception as e:
-            logger.error("{}.".format(e))
+            logger.error(f"{e}.")
             raise RuntimeError("neural-compressor is not correctly installed. Please check your environment.")
 
         import copy
 
-        from .quant_utils import save_and_reload_model
         from neural_compressor.adaptor.ox_utils.smooth_quant import ORTSmoothQuant
+
+        from .quant_utils import save_and_reload_model
 
         def inc_dataloader():
             data_reader = copy.deepcopy(calibration_data_reader)
