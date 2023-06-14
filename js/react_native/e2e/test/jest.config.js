93569aa41b8f27209e11b6ffbc07b7d8cf0ab662
rachguo <rachguo@rachguos-Mac-mini.local>
2023-06-14 14:04:32 -0700

    update to detox default jest reporter

--- js/react_native/e2e/test/jest.config.js
+++ js/react_native/e2e/test/jest.config.js
@@ -6,7 +6,7 @@ module.exports = {
   maxWorkers: 1,
   globalSetup: 'detox/runners/jest/globalSetup',
   globalTeardown: 'detox/runners/jest/globalTeardown',
-  reporters: ['jest-junit'],
+  reporters: ['detox/runners/jest/reporter'],
   testEnvironment: 'detox/runners/jest/testEnvironment',
   verbose: true,
 };
