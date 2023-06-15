9be133231fe7585ed6e3337389058bd5ca8db89e
Tianlei Wu <tlwu@microsoft.com>
2023-06-14 18:10:20 -0700

    Fix cuda graph capture (#15005)
    Fix two issues related to cuda graph capture:
    https://github.com/microsoft/onnxruntime/issues/14942 and
    https://github.com/microsoft/onnxruntime/issues/15002
    
    Issue 1: Previously, graph capture starts at the second run. However,
    memory pattern optimization will allocate memory from the second run,
    and cudamalloc is not allowed during graph capture. In this PR, the
    graph capture will start graph capture after 2 runs to avoid the issue.
    
    Issue 2: https://github.com/microsoft/onnxruntime/pull/13495 introduced
    multiple stream support. But stream cleanup will call
    cudaStreamSyncronize which is not allowed in cuda graph capture. In this
    PR, we move stream cleanup after cuda graph capture.
    
    Update the squeeze net test model with dynamic axis so that we can test
    with larger batch size. Add a test that could reproduce the bug (when
    changing min runs from 2 back to 1).

--- onnxruntime/test/python/onnxruntime_test_python_cudagraph.py
+++ onnxruntime/test/python/onnxruntime_test_python_cudagraph.py
@@ -1,20 +1,63 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
-import gc  # noqa: F401
-import os  # noqa: F401
-import sys  # noqa: F401
-import threading  # noqa: F401
-import time  # noqa: F401
-
-# -*- coding: UTF-8 -*-
 import unittest
+from typing import Dict, List
 
 import numpy as np
 from helper import get_name
 
 import onnxruntime as onnxrt
-from onnxruntime.capi.onnxruntime_pybind11_state import Fail  # noqa: F401
+
+
+class CudaGraphHelper:
+    def __init__(
+        self,
+        ort_session: onnxrt.InferenceSession,
+        input_and_output_shape: Dict[str, List[int]],
+        device_id: int = 0,
+    ):
+        self.input_names = [input.name for input in ort_session.get_inputs()]
+        self.output_names = [output.name for output in ort_session.get_outputs()]
+
+        self.input_and_output_shape = input_and_output_shape
+        self.io_numpy_type = self.get_io_numpy_type_map(ort_session)
+        self.io_binding = ort_session.io_binding()
+        self.io_ort_value = {}
+
+        for name in self.input_names + self.output_names:
+            ort_value = onnxrt.OrtValue.ortvalue_from_shape_and_type(
+                input_and_output_shape[name], self.io_numpy_type[name], "cuda", device_id
+            )
+            self.io_ort_value[name] = ort_value
+            if name in self.input_names:
+                self.io_binding.bind_ortvalue_input(name, ort_value)
+            else:
+                self.io_binding.bind_ortvalue_output(name, ort_value)
+
+    def get_io_numpy_type_map(self, ort_session: onnxrt.InferenceSession):
+        ort_type_to_numpy_type = {
+            "tensor(int64)": np.longlong,
+            "tensor(int32)": np.intc,
+            "tensor(float)": np.float32,
+            "tensor(float16)": np.float16,
+        }
+
+        name_to_numpy_type = {}
+        for _input in ort_session.get_inputs():
+            name_to_numpy_type[_input.name] = ort_type_to_numpy_type[_input.type]
+
+        for output in ort_session.get_outputs():
+            name_to_numpy_type[output.name] = ort_type_to_numpy_type[output.type]
+
+        return name_to_numpy_type
+
+    def update_inputs(self, inputs: Dict[str, np.ndarray]):
+        for input_name in self.input_names:
+            self.io_ort_value[input_name].update_inplace(inputs[input_name])
+
+    def get_output(self, output_name: str):
+        return self.io_ort_value[output_name].numpy()
 
 
 class TestInferenceSessionWithCudaGraph(unittest.TestCase):
@@ -73,6 +116,44 @@ class TestInferenceSessionWithCudaGraph(unittest.TestCase):
                 rtol=1e-05,
                 atol=1e-05,
             )
+
+    def testArenaWithCudaGraph(self):  # noqa: N802
+        if "CUDAExecutionProvider" in onnxrt.get_available_providers():
+            # To test cuda graph catpure, we set Arena extend strategy to be SameAsRequested so as to detect any
+            # potential memory allocation after the first run.
+            providers = [
+                ("CUDAExecutionProvider", {"enable_cuda_graph": True, "arena_extend_strategy": "kSameAsRequested"})
+            ]
+            test_model_path = get_name("squeezenet/model.onnx")
+
+            input_and_output_shape = {
+                "data_0": [16, 3, 224, 224],
+                "softmaxout_1": [16, 1000, 1, 1],
+            }
+
+            session_options = onnxrt.SessionOptions()
+            # It is optional to disable memory pattern since min_num_runs_before_cuda_graph_capture_ = 2.
+            session_options.enable_mem_pattern = False
+            session = onnxrt.InferenceSession(test_model_path, session_options, providers=providers)
+
+            cuda_graph_helper = CudaGraphHelper(session, input_and_output_shape)
+            io_binding = cuda_graph_helper.io_binding
+
+            # Create a random input for testing.
+            np.random.seed(0)
+            inputs = {"data_0": np.random.randint(0, 256, size=input_and_output_shape["data_0"]).astype(np.float32)}
+
+            # One regular run for the necessary memory allocation and cuda graph capturing
+            cuda_graph_helper.update_inputs(inputs)
+            session.run_with_iobinding(io_binding)
+            expected_output = cuda_graph_helper.get_output("softmaxout_1")
+
+            # After capturing, CUDA graph replay happens from this Run onwards
+            cuda_graph_helper.update_inputs(inputs)
+            session.run_with_iobinding(io_binding)
+            output = cuda_graph_helper.get_output("softmaxout_1")
+
+            np.testing.assert_allclose(expected_output, output, rtol=1e-02, atol=1e-02)
 
 
 if __name__ == "__main__":
