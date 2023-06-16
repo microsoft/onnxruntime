b7883ecac96b973c6961649d617ac415e07975b6
Jhen-Jie Hong <developer@jhen.me>
2023-06-16 17:37:02 +0800

    [js/rn] Implement blob exchange by JSI instead of use base64 (#16094)
    ### Description
    <!-- Describe your changes. -->
    
    - Create `OnnxruntimeJSIHelper` native module to provide two JSI
    functions
    - `jsiOnnxruntimeStoreArrayBuffer`: Store buffer in Blob Manager &
    return blob object (iOS: RCTBlobManager, Android: BlobModule)
      - `jsiOnnxruntimeResolveArrayBuffer`: Use blob object to get buffer
    - The part of implementation is reference to
    [react-native-blob-jsi-helper](https://github.com/mrousavy/react-native-blob-jsi-helper)
    - Replace base64 encode/decode
      - `loadModelFromBlob`: Rename from `loadModelFromBase64EncodedBuffer`
      - `run`: Use blob object to replace input.data & results[].data
    
    For [this
    context](https://github.com/microsoft/onnxruntime/issues/16031#issuecomment-1556527812),
    it saved a lot of time and avoid JS thread blocking in decode return
    type, it is 3700ms -> 5~20ms for the case. (resolve function only takes
    0.x ms)
    
    ### Motivation and Context
    <!-- - Why is this change required? What problem does it solve?
    - If it fixes an open issue, please link to the issue here. -->
    
    It’s related to #16031, but not a full implementation for migrate to
    JSI.
    
    It just uses JSI through BlobManager to replace the slow part (base64
    encode / decode).
    
    Rewriting it entirely in JSI could be complicated, like type convertion
    and threading. This PR might be considered a minor change.
    
    /cc @skottmckay

--- js/react_native/ios/OnnxruntimeModule.mm
+++ js/react_native/ios/OnnxruntimeModule.mm
@@ -5,6 +5,8 @@
 #import "TensorHelper.h"
 
 #import <Foundation/Foundation.h>
+#import <React/RCTBlobManager.h>
+#import <React/RCTBridge+Private.h>
 #import <React/RCTLog.h>
 
 // Note: Using below syntax for including ort c api and ort extensions headers to resolve a compiling error happened
@@ -44,6 +46,21 @@ static int nextSessionId = 0;
 
 RCT_EXPORT_MODULE(Onnxruntime)
 
+RCTBlobManager *blobManager = nil;
+
+- (void)checkBlobManager {
+  if (blobManager == nil) {
+    blobManager = [[RCTBridge currentBridge] moduleForClass:RCTBlobManager.class];
+    if (blobManager == nil) {
+      @throw @"RCTBlobManager is not initialized";
+    }
+  }
+}
+
+- (void)setBlobManager:(RCTBlobManager *)manager {
+  blobManager = manager;
+}
+
 /**
  * React native binding API to load a model using given uri.
  *
@@ -68,22 +85,27 @@ RCT_EXPORT_METHOD(loadModel
 }
 
 /**
- * React native binding API to load a model using BASE64 encoded model data string.
+ * React native binding API to load a model using blob object that data stored in RCTBlobManager.
  *
- * @param modelData the BASE64 encoded model data string
+ * @param modelDataBlob a model data blob object
  * @param options onnxruntime session options
  * @param resolve callback for returning output back to react native js
  * @param reject callback for returning an error back to react native js
  * @note when run() is called, the same modelPath must be passed into the first parameter.
  */
-RCT_EXPORT_METHOD(loadModelFromBase64EncodedBuffer
-                  : (NSString *)modelDataBase64EncodedString options
+RCT_EXPORT_METHOD(loadModelFromBlob
+                  : (NSDictionary *)modelDataBlob options
                   : (NSDictionary *)options resolver
                   : (RCTPromiseResolveBlock)resolve rejecter
                   : (RCTPromiseRejectBlock)reject) {
   @try {
-    NSData *modelDataDecoded = [[NSData alloc] initWithBase64EncodedString:modelDataBase64EncodedString options:0];
-    NSDictionary *resultMap = [self loadModelFromBuffer:modelDataDecoded options:options];
+    [self checkBlobManager];
+    NSString *blobId = [modelDataBlob objectForKey:@"blobId"];
+    long size = [[modelDataBlob objectForKey:@"size"] longValue];
+    long offset = [[modelDataBlob objectForKey:@"offset"] longValue];
+    auto modelData = [blobManager resolve:blobId offset:offset size:size];
+    NSDictionary *resultMap = [self loadModelFromBuffer:modelData options:options];
+    [blobManager remove:blobId];
     resolve(resultMap);
   } @catch (...) {
     reject(@"onnxruntime", @"failed to load model from buffer", nil);
@@ -255,6 +277,8 @@ RCT_EXPORT_METHOD(run
   }
   SessionInfo *sessionInfo = (SessionInfo *)[value pointerValue];
 
+  [self checkBlobManager];
+
   std::vector<Ort::Value> feeds;
   std::vector<Ort::MemoryAllocation> allocations;
   feeds.reserve(sessionInfo->inputNames.size());
@@ -265,7 +289,10 @@ RCT_EXPORT_METHOD(run
       @throw exception;
     }
 
-    Ort::Value value = [TensorHelper createInputTensor:inputTensor ortAllocator:ortAllocator allocations:allocations];
+    Ort::Value value = [TensorHelper createInputTensor:blobManager
+                                                 input:inputTensor
+                                          ortAllocator:ortAllocator
+                                           allocations:allocations];
     feeds.emplace_back(std::move(value));
   }
 
@@ -280,7 +307,7 @@ RCT_EXPORT_METHOD(run
       sessionInfo->session->Run(runOptions, sessionInfo->inputNames.data(), feeds.data(),
                                 sessionInfo->inputNames.size(), requestedOutputs.data(), requestedOutputs.size());
 
-  NSDictionary *resultMap = [TensorHelper createOutputTensor:requestedOutputs values:result];
+  NSDictionary *resultMap = [TensorHelper createOutputTensor:blobManager outputNames:requestedOutputs values:result];
 
   return resultMap;
 }
@@ -378,6 +405,7 @@ static NSDictionary *executionModeTable = @{@"sequential" : @(ORT_SEQUENTIAL), @
   while (NSString *key = [iterator nextObject]) {
     [self dispose:key];
   }
+  blobManager = nullptr;
 }
 
 @end
