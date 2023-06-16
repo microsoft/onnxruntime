bd2042b418ca709510b3795c45207951ce3dfba5
Pallavi Deshmukh <pdeshmuk@microsoft.com>
2023-06-16 15:01:58 -0700

    DORT support for custom ops

--- orttraining/orttraining/python/training/torchdynamo/__init__.py
+++ orttraining/orttraining/python/training/torchdynamo/__init__.py
@@ -2,3 +2,12 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 # --------------------------------------------------------------------------
+
+from typing import Set
+
+# set of custom ops supported in DORT
+custom_symbols: Set[str] = set()
+
+# register custom ops in DORT
+def register_custom_op_in_dort(custom_op_name: str):
+    custom_symbols.add(custom_op_name)
