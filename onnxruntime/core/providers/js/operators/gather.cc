c4bcce3de8f797dccbb5f78cc773abead05d6239
Jian Chen <cjian@microsoft.com>
2023-06-19 11:29:43 -0700

    Uploading the gather.cc and gather.h

--- onnxruntime/core/providers/js/operators/gather.cc
+++ onnxruntime/core/providers/js/operators/gather.cc
@@ -1,53 +1,50 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
+//  Copyright (c) Microsoft Corporation. All rights reserved.
+//  Licensed under the MIT License.
 
-#include "core/providers/js/js_kernel.h"
+//
+// Created by Jian Chen on 6/16/23.
+//
 
 #include "gather.h"
+#include "core/common/safeint.h"
+#include "core/framework/op_kernel.h"
+#include "core/providers/common.h"
+#include "core/providers/op_kernel_type_control.h"
 
 namespace onnxruntime {
 namespace js {
+#define REGISTER_GATHER_VERSIONED_KERNEL(GatherOp, sinceVersion, endVersion)                             \
+  ONNX_OPERATOR_VERSIONED_KERNEL_EX(                                                                                 \
+      GatherOp,                                                                                                      \
+      kOnnxDomain,                                                                                                   \
+      sinceVersion, endVersion,                                                                                      \
+      kJsExecutionProvider,                                                                                          \
+      KernelDefBuilder()                                                                                 \
+          .TypeConstraint("T", DataTypeImpl::AllFixedSizeTensorTypes())                                              \
+          .TypeConstraint("Tind", {DataTypeImpl::GetTensorType<int32_t>(), DataTypeImpl::GetTensorType<int64_t>()}), \
+      GatherOp);
+#define REGISTER_GATHER_KERNEL(GatherOp, sinceVersion)                             \
+  ONNX_OPERATOR_KERNEL_EX(                                                                                 \
+      GatherOp,                                                                                                      \
+      kOnnxDomain,                                                                                                   \
+      sinceVersion,                                                                                      \
+      kJsExecutionProvider,                                                                                          \
+      KernelDefBuilder()                                                                                 \
+          .TypeConstraint("T", DataTypeImpl::AllFixedSizeTensorTypes())                                              \
+          .TypeConstraint("Tind", {DataTypeImpl::GetTensorType<int32_t>(), DataTypeImpl::GetTensorType<int64_t>()}), \
+      GatherOp);
 
-using AllSupportedSize =
-    TypeList<
-        float,
-        double,
-        int64_t,
-        uint64_t,
-        int32_t,
-        uint32_t>;
+REGISTER_GATHER_VERSIONED_KERNEL(Gather, 1, 10);
+REGISTER_GATHER_VERSIONED_KERNEL(Gather, 11, 12);
+REGISTER_GATHER_KERNEL(Gather, 13);
 
-ONNX_OPERATOR_VERSIONED_KERNEL_EX(
-    Gather,
-    kOnnxDomain,
-    1,
-    10,
-    kJsExecutionProvider,
-    (*KernelDefBuilder::Create())
-        .TypeConstraint("T", BuildKernelDefConstraintsFromTypeList<AllSupportedSize>())
-        .TypeConstraint("Tind", BuildKernelDefConstraintsFromTypeList<TypeList<int32_t, int64_t>>()),
-    Gather);
+REGISTER_GATHER_VERSIONED_KERNEL(GatherElements, 11, 12);
+REGISTER_GATHER_KERNEL(GatherElements, 13);
 
-ONNX_OPERATOR_VERSIONED_KERNEL_EX(
-    Gather,
-    kOnnxDomain,
-    11,
-    12,
-    kJsExecutionProvider,
-    (*KernelDefBuilder::Create())
-        .TypeConstraint("T", BuildKernelDefConstraintsFromTypeList<AllSupportedSize>())
-        .TypeConstraint("Tind", BuildKernelDefConstraintsFromTypeList<TypeList<int32_t, int64_t>>()),
-    Gather);
+REGISTER_GATHER_VERSIONED_KERNEL(GatherND, 11, 11);
+REGISTER_GATHER_VERSIONED_KERNEL(GatherND, 12, 12);
+REGISTER_GATHER_KERNEL(GatherND, 13);
 
-ONNX_OPERATOR_KERNEL_EX(
-    Gather,
-    kOnnxDomain,
-    13,
-    kJsExecutionProvider,
-    (*KernelDefBuilder::Create())
-        .TypeConstraint("T", BuildKernelDefConstraintsFromTypeList<AllSupportedSize>())
-        .TypeConstraint("Tind", BuildKernelDefConstraintsFromTypeList<TypeList<int32_t, int64_t>>()),
-    Gather);
 
 }  // namespace js
 }  // namespace onnxruntime
