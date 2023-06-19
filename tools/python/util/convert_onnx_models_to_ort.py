226e67c9b9b00d2963134b54b7a22675a30c57f1
Baiju Meswani <bmeswani@microsoft.com>
2023-06-19 21:49:28 +0000

    fix annotations

--- tools/python/util/convert_onnx_models_to_ort.py
+++ tools/python/util/convert_onnx_models_to_ort.py
@@ -2,6 +2,8 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
+from __future__ import annotations
+
 import argparse
 import contextlib
 import enum
