2d058f08f1733c307cdea9eb0a530486bfc6f331
Yuhong Guo <guoyuhong1985@outlook.com>
2023-06-21 05:54:55 +0800

    Move tests from core/providers/cuda/test/* to test/providers/cuda/ and refactor CUDA UT (#16161)
    ### Description
    <!-- Describe your changes. -->
    
    1. Add a new test lib `onnxruntime_providers_cuda_ut` which is similar
    to `onnxruntime_providers_cuda` but `onnxruntime_providers_cuda_ut` is
    only built if `onnxruntime_BUILD_UNIT_TESTS` is set. We can call all
    CUDA UTs through this ut lib without affecting production lib
    `onnxruntime_providers_cuda`.
    2. Move all test cases from `core/providers/cuda/test/` to
    `test/providers/cuda/`. These test cases are built into lib
    `onnxruntime_providers_cuda_ut` and run by `./onnxruntime_test_all
    --gtest_filter="*CUDA_EP_Unittest*"`. Since the lib is only for test, we
    can use gtest macros in the test cases. Previous implementation do not
    support using gtest lib in the CUDA UT cases.
    3. The cmake code in `cmake/onnxruntime_providers.cmake` is refactored a
    bit. A new function `onnxruntime_add_object_library` is to build a
    object target. The 2 libs `onnxruntime_providers_cuda_ut` &
    `onnxruntime_providers_cuda` share most of the code, so the object files
    can be used in both libs, which helps reduce build time. Another
    function `config_cuda_provider_shared_module` is used to configure all 3
    similar
    targets(onnxruntime_providers_cuda_obj/onnxruntime_providers_cuda/onnxruntime_providers_cuda_ut).
    4. Refactored the test to call `testing::InitGoogleTest` &
    `RUN_ALL_TESTS` in `libonnxruntime_providers_cuda_ut.so`'s `TestAll`.
    After this change, we can see all the cases running in
    `CUDA_EP_Unittest.All`:
    
    ![image](https://github.com/microsoft/onnxruntime/assets/19584326/8ff80df6-060b-4ef0-90b7-657e68d3db87)
    
    
    
    
    ### Motivation and Context
    <!-- - Why is this change required? What problem does it solve?
    - If it fixes an open issue, please link to the issue here. -->
    
    After https://github.com/microsoft/onnxruntime/pull/13016, there are
    still test files in test/providers/cuda/ that are not moved to
    core/providers/cuda/test/ and the test cases are disabled. This PR helps
    to clean the unfinished TODOs.
    
    Even through onnxruntime_shared_lib_test covers some test for CUDA
    provider. onnxruntime_shared_lib_test works like a coarse grain
    end-to-end test for CUDA provider. If CUDA unittest can run cases for a
    single component, this wound be helpful for CUDA developers.
    
    ---------
    
    Co-authored-by: Yuhong Guo <yuhong.gyh@antgroup.com>

--- include/onnxruntime/core/session/onnxruntime_cxx_api.h
+++ include/onnxruntime/core/session/onnxruntime_cxx_api.h
@@ -1563,7 +1563,6 @@ struct ArenaCfg : detail::Base<OrtArenaCfg> {
    * \param arena_extend_strategy -  use -1 to allow ORT to choose the default, 0 = kNextPowerOfTwo, 1 = kSameAsRequested
    * \param initial_chunk_size_bytes - use -1 to allow ORT to choose the default
    * \param max_dead_bytes_per_chunk - use -1 to allow ORT to choose the default
-   * \param max_power_of_two_extend_bytes - use -1 to allow ORT to choose the default
    * See docs/C_API.md for details on what the following parameters mean and how to choose these values
    */
   ArenaCfg(size_t max_mem, int arena_extend_strategy, int initial_chunk_size_bytes, int max_dead_bytes_per_chunk);
