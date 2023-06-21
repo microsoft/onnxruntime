605865807680b12ba50dfbaf856ea6c2f9c25245
Hector Li <hecli@microsoft.com>
2023-06-20 23:17:45 -0700

    change to read from converted QNN model_net.json file which makes it a lot easier

--- onnxruntime/python/tools/qnn/add_trans_cast.py
+++ onnxruntime/python/tools/qnn/add_trans_cast.py
@@ -3,6 +3,7 @@
 # Licensed under the MIT License.
 # --------------------------------------------------------------------------
 
+import json
 from argparse import ArgumentParser
 
 import onnx
@@ -61,106 +62,66 @@ def graph_topological_sort(graph):
     graph.node.extend(sorted_nodes)
 
 
-def get_value_from_qnn_code_line(line_content):
-    value = line_content.split("=")
-    assert len(value) == 2, "Error: Wrong format encountered."
-    value = value[1].split(",")
-    assert len(value) == 2, "Error: Wrong format encountered."
-    return value[0].replace(" ", "").replace('"', "")
-
-
-class QNNTENSORSTRUCT:
+class QnnTensorStruct:
     def __init__(self):
         self.name = ""
-        self.qnn_data_type = ""
         self.onnx_data_type = TensorProto.FLOAT
-        self.dim_alias = ""
         self.dim = []
 
 
-def qnn_data_type_str_to_onnx_data_type(qnn_data_type_str):
-    if qnn_data_type_str == "QNN_DATATYPE_UFIXED_POINT_8" or qnn_data_type_str == "QNN_DATATYPE_UINT_8":
+def qnn_data_type_to_onnx_data_type(qnn_data_type):
+    # QNN_DATATYPE_UFIXED_POINT_8 QNN_DATATYPE_UINT_8
+    if qnn_data_type == 0x0408 or qnn_data_type == 0x0108:
         return TensorProto.UINT8
-    elif qnn_data_type_str == "QNN_DATATYPE_UFIXED_POINT_16" or qnn_data_type_str == "QNN_DATATYPE_UINT_16":
+    # QNN_DATATYPE_UFIXED_POINT_16 QNN_DATATYPE_UINT_16
+    elif qnn_data_type == 0x0416 or qnn_data_type == 0x0116:
         return TensorProto.UINT16
-    elif qnn_data_type_str == "QNN_DATATYPE_UFIXED_POINT_32" or qnn_data_type_str == "QNN_DATATYPE_UINT_32":
+    # QNN_DATATYPE_UFIXED_POINT_32 QNN_DATATYPE_UINT_32
+    elif qnn_data_type == 0x0432 or qnn_data_type == 0x0132:
         return TensorProto.UINT32
-    elif qnn_data_type_str == "QNN_DATATYPE_UINT_64":
+    # QNN_DATATYPE_UINT_64
+    elif qnn_data_type == 0x0164:
         return TensorProto.UINT64
-    elif qnn_data_type_str == "QNN_DATATYPE_UFIXED_POINT_8":
-        return TensorProto.UINT8
-    elif qnn_data_type_str == "QNN_DATATYPE_FIXED_POINT_8" or qnn_data_type_str == "QNN_DATATYPE_INT_8":
+    # QNN_DATATYPE_FIXED_POINT_8 QNN_DATATYPE_INT_8
+    elif qnn_data_type == 0x0308 or qnn_data_type == 0x0008:
         return TensorProto.INT8
-    elif qnn_data_type_str == "QNN_DATATYPE_FIXED_POINT_16" or qnn_data_type_str == "QNN_DATATYPE_INT_16":
+    # QNN_DATATYPE_FIXED_POINT_16 QNN_DATATYPE_INT_16
+    elif qnn_data_type == 0x0316 or qnn_data_type == 0x0016:
         return TensorProto.INT16
-    elif qnn_data_type_str == "QNN_DATATYPE_FIXED_POINT_32" or qnn_data_type_str == "QNN_DATATYPE_INT_32":
+    # QNN_DATATYPE_FIXED_POINT_32 QNN_DATATYPE_INT_32
+    elif qnn_data_type == 0x0332 or qnn_data_type == 0x0032:
         return TensorProto.INT32
-    elif qnn_data_type_str == "QNN_DATATYPE_INT_64":
+    # QNN_DATATYPE_INT_64
+    elif qnn_data_type == 0x0064:
         return TensorProto.INT64
-    elif qnn_data_type_str == "QNN_DATATYPE_FLOAT_16":
+    # QNN_DATATYPE_FLOAT_16
+    elif qnn_data_type == 0x0216:
         return TensorProto.FLOAT16
-    elif qnn_data_type_str == "QNN_DATATYPE_FLOAT_32":
+    # QNN_DATATYPE_FLOAT_32
+    elif qnn_data_type == 0x0232:
         return TensorProto.FLOAT
-    elif qnn_data_type_str == "QNN_DATATYPE_BOOL_8 ":
+    # QNN_DATATYPE_BOOL_8
+    elif qnn_data_type == 0x0508:
         return TensorProto.BOOL
     else:
         return TensorProto.UNDEFINED
 
 
-def parse_dims(line_content):
-    value = line_content.split("=")
-    assert len(value) == 2, "Error: Wrong format encountered."
-    value = value[1].replace(" ", "").split(";")
-    assert len(value) == 2, "Error: Wrong format encountered."
-    shape = value[0].replace("{", "").replace("}", "").split(",")
-    return [eval(item) for item in shape]
+def parse_qnn_json_file(qnn_json_file_path, qnn_input_output_tensor_dic):
+    with open(qnn_json_file_path) as qnn_json_file:
+        qnn_json = json.load(qnn_json_file)
+        for qnn_tensor_name, qnn_tensor_attribute in qnn_json["graph"]["tensors"].items():
+            # type:0 - QNN input tensor, type:1 - QNN output tensor
+            if qnn_tensor_attribute["type"] == 0 or qnn_tensor_attribute["type"] == 1:
+                qnn_tensor = QnnTensorStruct()
+                qnn_tensor.name = qnn_tensor_name
+                qnn_tensor.onnx_data_type = qnn_data_type_to_onnx_data_type(qnn_tensor_attribute["data_type"])
+                qnn_tensor.dim = qnn_tensor_attribute["dims"]
+                qnn_input_output_tensor_dic[qnn_tensor_name] = qnn_tensor
 
-
-def parse_qnn_cpp_file(qnn_cpp_file, qnn_input_output_tensor_dic):
-    qnn_tensor_names = []
-    qnn_tensor_types = []
-    qnn_tensor_data_types = []
-    qnn_tensor_dim_alias = []
-    qnn_tensor_item_count = 0
-    with open(qnn_cpp_file) as cpp_file:
-        for line in cpp_file:
-            if "(Qnn_Tensor_t)" in line:
-                qnn_tensor_item_count = 0
-            if (".name" in line) and (qnn_tensor_item_count < 4):
-                name = get_value_from_qnn_code_line(line)
-                qnn_tensor_names.append(name)
-                qnn_tensor_item_count += 1
-            if (".type" in line) and (qnn_tensor_item_count < 4):
-                qnn_tensor_types.append(get_value_from_qnn_code_line(line))
-                qnn_tensor_item_count += 1
-            if (".dataType" in line) and (qnn_tensor_item_count < 4):
-                qnn_tensor_data_types.append(get_value_from_qnn_code_line(line))
-                qnn_tensor_item_count += 1
-            if (".dimensions" in line) and (qnn_tensor_item_count < 4):
-                qnn_tensor_dim_alias.append(get_value_from_qnn_code_line(line))
-                qnn_tensor_item_count += 1
     assert (
-        len(qnn_tensor_names) == len(qnn_tensor_types)
-        and len(qnn_tensor_types) == len(qnn_tensor_data_types)
-        and len(qnn_tensor_data_types) == len(qnn_tensor_dim_alias)
-    ), "Error: Something wrong!"
-
-    for i in range(len(qnn_tensor_types)):
-        # only care about the graph input and output tensors
-        if qnn_tensor_types[i] == "QNN_TENSOR_TYPE_APP_WRITE" or qnn_tensor_types[i] == "QNN_TENSOR_TYPE_APP_READ":
-            qnn_tensor = QNNTENSORSTRUCT()
-            qnn_tensor.name = qnn_tensor_names[i]
-            qnn_tensor.qnn_data_type = qnn_tensor_data_types[i]
-            qnn_tensor.onnx_data_type = qnn_data_type_str_to_onnx_data_type(qnn_tensor.qnn_data_type)
-            qnn_tensor.dim_alias = qnn_tensor_dim_alias[i]
-            qnn_input_output_tensor_dic[qnn_tensor.name] = qnn_tensor
-
-    # Go through the cpp file again get the shape dims by the give dim alias
-    with open(qnn_cpp_file) as cpp_file:
-        for line in cpp_file:
-            for tensor in qnn_input_output_tensor_dic.values():
-                if tensor.dim_alias + "[]" in line:
-                    tensor.dim = parse_dims(line)
+        len(qnn_input_output_tensor_dic) > 1
+    ), "Converted QNN model not valid. It should have at least 1 input & 1 output."
 
 
 def compare_onnx_shape_with_qnn_shape(onnx_dims, qnn_dims):
@@ -192,7 +153,7 @@ def gen_to_channel_last_perm(rank):
 
 # Onnxruntime QNN EP can support context binary file generated by QNN tool chain. However QNN generated context binary file
 # uses channel last and 8 bits or 16 bits for input and output.
-# This script get the QNN model input & output information from QNN converted model.cpp file,
+# This script get the QNN model input & output information from QNN converted model_net.json file,
 # and insert Cast, Transpose nodes to Onnx model if required
 def main():
     parser = ArgumentParser(
@@ -200,13 +161,13 @@ def main():
     )
     parser.add_argument("-m", "--onnx_model", help="Required. Path to Onnx model file.", required=True, type=str)
     parser.add_argument(
-        "-q", "--qnn_cpp", help="Required. Path to Qnn converted model.cpp file.", required=True, type=str
+        "-q", "--qnn_json", help="Required. Path to Qnn converted model_net.json file.", required=True, type=str
     )
     args = parser.parse_args()
 
-    # Parse Qnn model.cpp file to get the graph input output information
+    # Parse Qnn model_net.json file to get the graph input output information
     qnn_input_output_tensor_dic = {}
-    parse_qnn_cpp_file(args.qnn_cpp, qnn_input_output_tensor_dic)
+    parse_qnn_json_file(args.qnn_json, qnn_input_output_tensor_dic)
 
     model = onnx.load(args.onnx_model)
 
