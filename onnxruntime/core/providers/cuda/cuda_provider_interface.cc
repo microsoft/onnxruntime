d5b606d50d5a251910a45cfcef001cb7c314c8ea
Ryan Hill <38674843+RyanUnderhill@users.noreply.github.com>
2023-06-23 09:21:03 -0700

    Remove now duplicated symbol (#16458)
    ### Description
    Change #16161 broke rocm by duplicating this symbol. This removes the
    duplicate to unblock the tests.

--- onnxruntime/core/providers/cuda/cuda_provider_interface.cc
+++ onnxruntime/core/providers/cuda/cuda_provider_interface.cc
@@ -2,6 +2,7 @@
 // Licensed under the MIT License.
 
 #include "core/session/onnxruntime_c_api.h"
+#if !defined(USE_ROCM)
 
 namespace onnxruntime {
 struct Provider;
@@ -15,3 +16,5 @@ ORT_API(onnxruntime::Provider*, GetProvider) {
   return reinterpret_cast<onnxruntime::Provider*>(onnxruntime::GetProvider());
 }
 }
+
+#endif
