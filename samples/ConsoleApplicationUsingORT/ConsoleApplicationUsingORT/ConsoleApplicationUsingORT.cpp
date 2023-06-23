faf2f751339aa24f6d16b533454eb504204c6cfe
Lei Cao <jslhcl@gmail.com>
2023-06-23 12:14:22 -0700

    more logic in Console app

--- samples/ConsoleApplicationUsingORT/ConsoleApplicationUsingORT/ConsoleApplicationUsingORT.cpp
+++ samples/ConsoleApplicationUsingORT/ConsoleApplicationUsingORT/ConsoleApplicationUsingORT.cpp
@@ -3,21 +3,25 @@
 
 #include <iostream>
 #include <vector>
-#include "core\framework\tensor_shape2.h"
+#include "core/framework/tensor_shape2.h"
+//#include "MathFunctions.h"
 
 int main()
 {
   std::vector<int64_t> dims{3, 4, 5};
-  onnxruntime::TensorShape2 tensor_shape_2(dims); 
-  //onnxruntime::TensorShape2 tensor_shape_2; 
-  std::string str = tensor_shape_2.ToString();
-  std::cout << "Hello World! " << str << "\n ";
+//  onnxruntime::TensorShape2 tensor_shape_2(dims);
+  //onnxruntime::TensorShape2 tensor_shape_2;
+//  std::string str = tensor_shape_2.ToString();
+//  std::cout << "Hello World! " << str << "\n ";
+  std::cout<<"hello dims size="<<dims.size()<<"\n";
+//  std::cout<<"sqrt of 16:"<<mathfunctions::sqrt(16.0)<<"\n";
+  std::cout<<"cube:"<<onnxruntime::cube(3)<<"\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
+// Tips for Getting Started:
 //   1. Use the Solution Explorer window to add/manage files
 //   2. Use the Team Explorer window to connect to source control
 //   3. Use the Output window to see build output and other messages
