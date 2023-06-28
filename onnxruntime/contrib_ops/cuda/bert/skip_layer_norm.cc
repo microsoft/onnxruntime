b1eec9522a798e3a39d6857ec263e95297b825c4
Khalia Spear <134653808+khspear@users.noreply.github.com>
2023-06-28 14:24:02 -0700

    Update skip_layer_norm.cc
    Added and edited same checks from cpu

--- onnxruntime/contrib_ops/cuda/bert/skip_layer_norm.cc
+++ onnxruntime/contrib_ops/cuda/bert/skip_layer_norm.cc
@@ -60,10 +60,6 @@ Status SkipLayerNorm<T, Simplified>::ComputeInternal(OpKernelContext* ctx) const
   // of the input and skip tensors
   Tensor* skip_input_bias_add_output = ctx->Output(3, input->Shape());
 
-  if (input->Shape().Size() == 0) {
-    return Status::OK();
-  }
-
   const auto& input_dims = input->Shape().GetDims();
   size_t input_dims_size = input_dims.size();
   const auto& skip_dims = skip->Shape().GetDims();
@@ -71,6 +67,30 @@ Status SkipLayerNorm<T, Simplified>::ComputeInternal(OpKernelContext* ctx) const
   if (input_dims_size != 3 && input_dims_size != 2) {
     return ORT_MAKE_STATUS(ONNXRUNTIME, INVALID_ARGUMENT,
                            "input is expected to have 3 or 2 dimensions, got ", input_dims_size);
+  }
+
+  if (input->Shape() != skip->Shape() && skip_dims[0] != 1) {
+    return ORT_MAKE_STATUS(ONNXRUNTIME, INVALID_ARGUMENT,
+                           "skip is expected to have same shape as input or a batch size of 1");
+  }
+
+  if (skip_dims_size != 3 && skip_dims_size != 2) {
+    return ORT_MAKE_STATUS(ONNXRUNTIME, INVALID_ARGUMENT,
+                           "skip is expected to have 3 or 2 dimensions, got ", skip_dims_size);
+  }
+
+  if (skip_dims[1] != input_dims[1] && skip_dims[1] != 1) {
+    return ORT_MAKE_STATUS(ONNXRUNTIME, INVALID_ARGUMENT,
+                           "sequence length needs to be 1 or same as input");
+  }
+
+  if (skip_dims[2] != input_dims[2] && skip_dims[2] != 1) {
+    return ORT_MAKE_STATUS(ONNXRUNTIME, INVALID_ARGUMENT,
+                           "hidden size needs to be 1 or same as input");
+  }
+
+  if (input->Shape().Size() == 0) {
+    return Status::OK();
   }
 
   int hidden_size = static_cast<int>(input_dims[input_dims_size - 1]);
