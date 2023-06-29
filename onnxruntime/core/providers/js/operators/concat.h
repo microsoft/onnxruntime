bfbb5e8ed92ff2dabde7157fc814e745fa17d90a
Satya Jandhyala <satya.k.jandhyala@gmail.com>
2023-06-29 11:17:06 -0700

    Added header file for the base class.

--- onnxruntime/core/providers/js/operators/concat.h
+++ onnxruntime/core/providers/js/operators/concat.h
@@ -4,11 +4,12 @@
 #pragma once
 
 #include "core/providers/js/js_kernel.h"
+#include "core/providers/cpu/tensor/concatbase.h"
 
 namespace onnxruntime {
 namespace js {
 
-class Concat : public JsKernel, public ConcatBase {
+class Concat final: public JsKernel, public ConcatBase {
  public:
   Concat(const OpKernelInfo& info) : JsKernel(info), ConcatBase(info) {
     JSEP_INIT_KERNEL_ATTRIBUTE(Concat, ({"axis", $1}), axis_);
