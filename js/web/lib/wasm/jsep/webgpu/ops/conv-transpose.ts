e62bef5048aa0a39ed6fa65aa057a2db5a319b0d
Satya Jandhyala <satya.k.jandhyala@gmail.com>
2023-06-29 15:05:54 -0700

    Formatted conv-transpose.ts.

--- js/web/lib/wasm/jsep/webgpu/ops/conv-transpose.ts
+++ js/web/lib/wasm/jsep/webgpu/ops/conv-transpose.ts
@@ -4,8 +4,7 @@
 import {DataType} from '../../../wasm-common';
 import {TensorView} from '../../tensor';
 import {createAttributeWithCacheKey} from '../attribute-with-cache-key';
-import {ComputeContext} from '../types';
-import {GpuDataType, ProgramInfoLoader, ProgramMetadata} from '../types';
+import {ComputeContext, GpuDataType, ProgramInfoLoader, ProgramMetadata} from '../types';
 
 import {createConvTranspose2DProgramInfo} from './3rd-party/conv_backprop_webgpu';
 import {ConvAttributes} from './conv';
