0903ce809fe11c2773d6c8b7f867363793d93a49
Peter McAughan <petermca@microsoft.com>
2023-06-29 23:41:21 +0000

    Whitespace

--- onnxruntime/python/tools/transformers/onnx_model_bert.py
+++ onnxruntime/python/tools/transformers/onnx_model_bert.py
@@ -2,6 +2,7 @@
 # Copyright (c) Microsoft Corporation.  All rights reserved.
 # Licensed under the MIT License.
 # --------------------------------------------------------------------------
+
 from logging import getLogger
 from typing import List, Optional
 
