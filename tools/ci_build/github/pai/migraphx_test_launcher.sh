21da6c16bbafee7e761adfaf04f05a2fe4a90c39
peixuanzuo <peixuanzuo@microsoft.com>
2023-07-04 06:33:36 +0000

    rm bug info

--- tools/ci_build/github/pai/migraphx_test_launcher.sh
+++ tools/ci_build/github/pai/migraphx_test_launcher.sh
@@ -12,5 +12,4 @@ done <$script_dir/migraphx-excluded-tests.txt
 echo ""
 
 echo "Running ./onnxruntime_test_all .."
-ls -l $build_dir/onnxruntime_test_all
 $build_dir/onnxruntime_test_all --gtest_filter=$gtest_filter
