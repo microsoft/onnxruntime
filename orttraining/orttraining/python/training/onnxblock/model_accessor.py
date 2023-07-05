20ee12984ebcc6db51f17ccd8226d377e389a469
Baiju Meswani <bmeswani@microsoft.com>
2023-07-05 20:10:45 +0000

    fix lint

--- orttraining/orttraining/python/training/onnxblock/model_accessor.py
+++ orttraining/orttraining/python/training/onnxblock/model_accessor.py
@@ -1,6 +1,8 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
+from __future__ import annotations
+
 import copy
 from contextlib import contextmanager
 import os
