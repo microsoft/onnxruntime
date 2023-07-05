b0e8260f3422044d3add6b30f8bf04bf5840bacd
Satya Jandhyala <satya.k.jandhyala@gmail.com>
2023-07-05 16:18:09 -0700

    Use UTF8ToString instead of String.formCharCode to pass string values to JS

--- onnxruntime/core/providers/js/operators/resize.h
+++ onnxruntime/core/providers/js/operators/resize.h
@@ -25,28 +25,24 @@ class Resize : public JsKernel, public UpsampleBase {
     JSEP_INIT_KERNEL_ATTRIBUTE(Resize, ({
                                  "antialias" : $1,
                                  "axes" : $2 ? Array.from(HEAP32.subarray($3, $3 + $2)) : [],
-                                 "coordinateTransformMode" : String.fromCharCode.apply(null, Array.from(HEAP8.subarray($4, $4 + $5))),
-                                 "cubicCoeffA" : $6,
-                                 "excludeOutside" : !!$7,
-                                 "extrapolationValue" : $8,
-                                 "keepAspectRatioPolicy" : String.fromCharCode.apply(null, Array.from(HEAP8.subarray($9, $9 + $10))),
-                                 "mode" : String.fromCharCode.apply(null, Array.from(HEAP8.subarray($11, $11 + $12))),
-                                 "nearestMode" : String.fromCharCode.apply(null, Array.from(HEAP8.subarray($13, $13 + $14))),
+                                 "coordinateTransformMode" : UTF8ToString($4),
+                                 "cubicCoeffA" : $5,
+                                 "excludeOutside" : $6,
+                                 "extrapolationValue" : $7,
+                                 "keepAspectRatioPolicy" : UTF8ToString($8),
+                                 "mode" : UTF8ToString($9),
+                                 "nearestMode" : UTF8ToString($10),
                                }),
                                static_cast<int32_t>(antialias_),
                                gsl::narrow_cast<int32_t>(axes.size()),
                                reinterpret_cast<int32_t>((axes.size() > 0) ? axes.data() : nullptr) >> 2,
                                resize_coordinate_transformation_mode.c_str(),
-                               resize_coordinate_transformation_mode.length(),
                                static_cast<double>(cubic_coeff_a_),
                                static_cast<int32_t>(exclude_outside_),
                                static_cast<double>(extrapolation_value_),
                                keep_aspect_ratio_policy.c_str(),
-                               keep_aspect_ratio_policy.length(),
                                mode.c_str(),
-                               mode.length(),
-                               nearest_mode.c_str(),
-                               nearest_mode.length());
+                               nearest_mode.c_str());
   }
 };
 
