4df88a95e868df11265f33ce07037e60fbcb37b5
edgchen1 <18449977+edgchen1@users.noreply.github.com>
2023-07-05 10:14:07 -0700

    use int32_t in CountDownBarrier

--- onnxruntime/core/framework/stream_execution_context.h
+++ onnxruntime/core/framework/stream_execution_context.h
@@ -37,7 +37,7 @@ class StreamExecutionContext {
    public:
     CountDownBarrier() : v_{0} {};
 
-    void Set(int_fast32_t v) {
+    void Set(int32_t v) {
       ORT_ENFORCE(v >= 0);
       v_.store(v, std::memory_order_relaxed);
     }
@@ -46,7 +46,9 @@ class StreamExecutionContext {
       return v_.fetch_sub(1, std::memory_order_relaxed) == 1;
     }
 
-    int_fast32_t Get() { return v_.load(std::memory_order_relaxed); }
+    int32_t Get() {
+      return gsl::narrow_cast<int32_t>(v_.load(std::memory_order_relaxed));
+    }
 
     void Inc() {
       ++v_;
