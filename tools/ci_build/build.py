7130d209bd89a0f5af4991a2eaf79fcb847df6a1
Chi Lo <Chi.Lo@microsoft.com>
2023-07-08 00:57:48 +0000

    add --extra-verbose to debug

--- tools/ci_build/build.py
+++ tools/ci_build/build.py
@@ -1764,7 +1764,7 @@ def run_onnxruntime_tests(args, source_dir, ctest_path, build_dir, configs):
                 test_output = f"--gtest_output=xml:{cwd}/{exe}.{config}.results.xml"
                 run_subprocess([os.path.join(cwd, exe), test_output], cwd=cwd, dll_path=dll_path)
         else:
-            ctest_cmd = [ctest_path, "--build-config", config, "--verbose --extra-verbose", "--timeout", args.test_all_timeout]
+            ctest_cmd = [ctest_path, "--build-config", config, "--extra-verbose", "--timeout", args.test_all_timeout]
             run_subprocess(ctest_cmd, cwd=cwd, dll_path=dll_path)
 
         if args.enable_pybind:
