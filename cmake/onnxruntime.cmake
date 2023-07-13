ca3e073a33859ab6b783c73ffab0f548888585fe
edgchen1 <18449977+edgchen1@users.noreply.github.com>
2023-07-13 14:26:04 -0700

    add onnxruntime_extensions to onnxruntime_INTERNAL_LIBRARIES

--- cmake/onnxruntime.cmake
+++ cmake/onnxruntime.cmake
@@ -226,6 +226,12 @@ if (onnxruntime_ENABLE_LANGUAGE_INTEROP_OPS)
   )
 endif()
 
+if (onnxruntime_USE_EXTENSIONS)
+  list(APPEND onnxruntime_INTERNAL_LIBRARIES
+    onnxruntime_extensions
+  )
+endif()
+
 # If you are linking a new library, please add it to the list onnxruntime_INTERNAL_LIBRARIES or onnxruntime_EXTERNAL_LIBRARIES,
 # Please do not add a library directly to the target_link_libraries command
 target_link_libraries(onnxruntime PRIVATE
