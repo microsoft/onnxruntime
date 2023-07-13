8ea44b47c3ba8de5e6ce2b3619f3f47be49ef633
edgchen1 <18449977+edgchen1@users.noreply.github.com>
2023-07-13 16:36:14 -0700

    remove another nodiscard

--- onnxruntime/core/providers/coreml/builders/op_builder.h
+++ onnxruntime/core/providers/coreml/builders/op_builder.h
@@ -29,8 +29,8 @@ class IOpBuilder {
   virtual void AddInitializersToSkip(ModelBuilder& model_builder, const Node& node) const = 0;
 
   // Add the operator to CoreML model
-  [[nodiscard]] virtual Status AddToModelBuilder(ModelBuilder& model_builder, const Node& node,
-                                                 const logging::Logger& logger) const = 0;
+  virtual Status AddToModelBuilder(ModelBuilder& model_builder, const Node& node,
+                                   const logging::Logger& logger) const = 0;
 #endif
 
   // Operator support related
