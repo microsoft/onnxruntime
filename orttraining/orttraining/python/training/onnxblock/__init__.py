9889f0f50754671f4df397747901974865422a12
Baiju Meswani <bmeswani@microsoft.com>
2023-07-14 11:15:51 -0700

    Add support for training apis to support custom ops (#16601)

--- orttraining/orttraining/python/training/onnxblock/__init__.py
+++ orttraining/orttraining/python/training/onnxblock/__init__.py
@@ -8,7 +8,7 @@ import onnxruntime.training.onnxblock.loss as loss
 import onnxruntime.training.onnxblock.optim as optim
 from onnxruntime.training.onnxblock.blocks import Block
 from onnxruntime.training.onnxblock.checkpoint_utils import load_checkpoint_to_model, save_checkpoint
-from onnxruntime.training.onnxblock.model_accessor import base, empty_base
+from onnxruntime.training.onnxblock.model_accessor import base, custom_op_library, empty_base
 from onnxruntime.training.onnxblock.onnxblock import ForwardBlock, TrainingBlock
 
 __all__ = [
@@ -21,5 +21,6 @@ __all__ = [
     "load_checkpoint_to_model",
     "save_checkpoint",
     "base",
+    "custom_op_library",
     "empty_base",
 ]
