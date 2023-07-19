c66f0a4f5238399e13c2d430f536a9361385d9c8
Justin Chu <justinchu@microsoft.com>
2023-07-19 17:41:29 +0000

    Revert formatting

--- winml/lib/Common/inc/winrt_headers.h
+++ winml/lib/Common/inc/winrt_headers.h
@@ -11,14 +11,16 @@
 #include "winrt/windows.foundation.collections.h"
 #include "winrt/windows.storage.streams.h"
 
+// clang-format off
 #define STRINGIFY(x) #x
 #define XSTRINGIFY(x) STRINGIFY(x)
-#define CPPWINRT_HEADER(root_ns) comp_generated / winrt / ##root_ns##.AI.MachineLearning.h
+#define CPPWINRT_HEADER(root_ns) comp_generated/winrt/##root_ns##.AI.MachineLearning.h
 #define NATIVE_HEADER(root_ns) root_ns##.AI.MachineLearning.native.h
 #define NATIVE_INTERNAL_HEADER(root_ns) root_ns##.AI.MachineLearning.native.internal.h
 #define CREATE_CPPWINRT_COMPONENT_HEADER() XSTRINGIFY(CPPWINRT_HEADER(WINML_ROOT_NS))
 #define CREATE_NATIVE_HEADER() XSTRINGIFY(NATIVE_HEADER(WINML_ROOT_NS))
 #define CREATE_NATIVE_INTERNAL_HEADER() XSTRINGIFY(NATIVE_INTERNAL_HEADER(WINML_ROOT_NS))
+// clang-format on
 
 #include CREATE_CPPWINRT_COMPONENT_HEADER()
 
