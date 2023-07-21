2aed119006b7cb2d2e203b56e97b4517db58f096
kunal-vaishnavi <115581922+kunal-vaishnavi@users.noreply.github.com>
2023-07-20 18:44:28 -0700

    Fix bug with saving model optimized by inference session (#16716)
    ### Description
    A [previous PR](https://github.com/microsoft/onnxruntime/pull/16531)
    added a temporary directory to save the model optimizations after
    loading a model into an `InferenceSession`. Many models that have an
    external data file, however, require the data file to be in the same
    directory as the ONNX model file. Because the model is saved in a
    temporary directory and the data is saved in another directory, this
    causes a `FileNotFoundError` error when trying to load the model in the
    temporary directory.
    
    This PR fixes this error by saving the external data file in the same
    directory that the optimized model is located in.
    
    ### Motivation and Context
    This PR fixes a bug with using a temporary directory while running the
    optimizer for models that have an external data file.

--- onnxruntime/core/graph/graph.cc
+++ onnxruntime/core/graph/graph.cc
@@ -3381,11 +3381,20 @@ ONNX_NAMESPACE::GraphProto Graph::ToGraphProto() const {
 }
 
 ONNX_NAMESPACE::GraphProto Graph::ToGraphProtoWithExternalInitializers(const std::string& external_file_name,
+                                                                       const PathString& destination_file_path,
                                                                        size_t initializer_size_threshold) const {
   GraphProto result;
   ToGraphProtoInternal(result);
 
-  std::ofstream external_stream(external_file_name, std::ofstream::out | std::ofstream::binary);
+  Path parent_path = Path::Parse(destination_file_path).ParentPath();
+  Path external_file_path = Path::Parse(ToPathString(external_file_name));
+  // Check if parent_path is relative path (length = 0)
+  if (parent_path.ToPathString().length()) {
+    // Save external data file in same directory as model
+    external_file_path = parent_path.Append(external_file_path);
+  }
+
+  std::ofstream external_stream(external_file_path.ToPathString(), std::ofstream::out | std::ofstream::binary);
   ORT_ENFORCE(external_stream.is_open());
   int64_t external_offset = 0;
 
