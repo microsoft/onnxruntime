1a9e389148cfcba3124c317263020a5fdaf920d1
Prasanth Pulavarthi <prasanth.pulavarthi@microsoft.com>
2023-07-21 13:18:26 -0700

    Update generative-ai.svelte

--- website/src/routes/components/generative-ai.svelte
+++ website/src/routes/components/generative-ai.svelte
@@ -2,7 +2,7 @@
     <figure><img src="" alt="Generative AI Graphic" /></figure>
     <div class="card-body">
       <h2 class="card-title">Generative AI</h2>
-      <p>Integrate the power of generative AI in your apps and services with ONNX Runtime. Broad platform support and deep optimizations enable you to benefit from state of the art generative AI models.</p>
+      <p>Integrate the power of generative AI in your apps and services with ONNX Runtime. Broad platform support and deep optimizations enable you to benefit from state of the art models for image synthesis, text generation, and more.</p>
       <div class="card-actions justify-end">
         <button class="btn btn-primary">Learn More</button>
       </div>
