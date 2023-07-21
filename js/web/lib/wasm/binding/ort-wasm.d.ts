45feab65f07a756def2d7d47f8d1e8557950130d
Ashwini Khade <askhade@microsoft.com>
2023-07-21 15:08:44 -0700

    add training type declaration for bindings for wasm

--- js/web/lib/wasm/binding/ort-wasm.d.ts
+++ js/web/lib/wasm/binding/ort-wasm.d.ts
@@ -2,78 +2,92 @@
 // Licensed under the MIT License.
 
 declare namespace JSEP {
-  type BackendType = unknown;
-  type AllocFunction = (size: number) => number;
-  type FreeFunction = (size: number) => number;
-  type UploadFunction = (dataOffset: number, gpuDataId: number, size: number) => void;
-  type DownloadFunction = (gpuDataId: number, dataOffset: number, size: number) => Promise<void>;
-  type CreateKernelFunction = (name: string, kernel: number, attribute: unknown) => void;
-  type ReleaseKernelFunction = (kernel: number) => void;
-  type RunFunction = (kernel: number, contextDataOffset: number) => number;
+    type BackendType = unknown;
+    type AllocFunction = (size: number) => number;
+    type FreeFunction = (size: number) => number;
+    type UploadFunction = (dataOffset: number, gpuDataId: number, size: number) => void;
+    type DownloadFunction = (gpuDataId: number, dataOffset: number, size: number) => Promise<void>;
+    type CreateKernelFunction = (name: string, kernel: number, attribute: unknown) => void;
+    type ReleaseKernelFunction = (kernel: number) => void;
+    type RunFunction = (kernel: number, contextDataOffset: number) => number;
 }
 
 export interface OrtWasmModule extends EmscriptenModule {
-  // #region emscripten functions
-  stackSave(): number;
-  stackRestore(stack: number): void;
-  stackAlloc(size: number): number;
+    // #region emscripten functions
+    stackSave(): number;
+    stackRestore(stack: number): void;
+    stackAlloc(size: number): number;
 
-  UTF8ToString(offset: number, maxBytesToRead?: number): string;
-  lengthBytesUTF8(str: string): number;
-  stringToUTF8(str: string, offset: number, maxBytes: number): void;
-  // #endregion
+    UTF8ToString(offset: number, maxBytesToRead?: number): string;
+    lengthBytesUTF8(str: string): number;
+    stringToUTF8(str: string, offset: number, maxBytes: number): void;
+    // #endregion
 
-  // #region ORT APIs
-  _OrtInit(numThreads: number, loggingLevel: number): number;
+    // #region ORT APIs
+    _OrtInit(numThreads: number, loggingLevel: number): number;
 
-  _OrtGetLastError(errorCodeOffset: number, errorMessageOffset: number): void;
+    _OrtGetLastError(errorCodeOffset: number, errorMessageOffset: number): void;
 
-  _OrtCreateSession(dataOffset: number, dataLength: number, sessionOptionsHandle: number): number;
-  _OrtReleaseSession(sessionHandle: number): void;
-  _OrtGetInputOutputCount(sessionHandle: number, inputCountOffset: number, outputCountOffset: number): number;
-  _OrtGetInputName(sessionHandle: number, index: number): number;
-  _OrtGetOutputName(sessionHandle: number, index: number): number;
+    _OrtCreateSession(dataOffset: number, dataLength: number, sessionOptionsHandle: number): number;
+    _OrtReleaseSession(sessionHandle: number): void;
+    _OrtGetInputOutputCount(sessionHandle: number, inputCountOffset: number, outputCountOffset: number): number;
+    _OrtGetInputName(sessionHandle: number, index: number): number;
+    _OrtGetOutputName(sessionHandle: number, index: number): number;
 
-  _OrtFree(stringHandle: number): void;
+    _OrtFree(stringHandle: number): void;
 
-  _OrtCreateTensor(dataType: number, dataOffset: number, dataLength: number, dimsOffset: number, dimsLength: number):
-      number;
-  _OrtGetTensorData(tensorHandle: number, dataType: number, dataOffset: number, dimsOffset: number, dimsLength: number):
-      number;
-  _OrtReleaseTensor(tensorHandle: number): void;
-  _OrtRun(
-      sessionHandle: number, inputNamesOffset: number, inputsOffset: number, inputCount: number,
-      outputNamesOffset: number, outputCount: number, outputsOffset: number, runOptionsHandle: number): number;
+    _OrtCreateTensor(dataType: number, dataOffset: number, dataLength: number, dimsOffset: number, dimsLength: number):
+        number;
+    _OrtGetTensorData(tensorHandle: number, dataType: number, dataOffset: number, dimsOffset: number, dimsLength: number):
+        number;
+    _OrtReleaseTensor(tensorHandle: number): void;
+    _OrtRun(
+        sessionHandle: number, inputNamesOffset: number, inputsOffset: number, inputCount: number,
+        outputNamesOffset: number, outputCount: number, outputsOffset: number, runOptionsHandle: number): number;
 
-  _OrtCreateSessionOptions(
-      graphOptimizationLevel: number, enableCpuMemArena: boolean, enableMemPattern: boolean, executionMode: number,
-      enableProfiling: boolean, profileFilePrefix: number, logId: number, logSeverityLevel: number,
-      logVerbosityLevel: number, optimizedModelFilePath: number): number;
-  _OrtAppendExecutionProvider(sessionOptionsHandle: number, name: number): number;
-  _OrtAddSessionConfigEntry(sessionOptionsHandle: number, configKey: number, configValue: number): number;
-  _OrtReleaseSessionOptions(sessionOptionsHandle: number): void;
+    _OrtCreateSessionOptions(
+        graphOptimizationLevel: number, enableCpuMemArena: boolean, enableMemPattern: boolean, executionMode: number,
+        enableProfiling: boolean, profileFilePrefix: number, logId: number, logSeverityLevel: number,
+        logVerbosityLevel: number, optimizedModelFilePath: number): number;
+    _OrtAppendExecutionProvider(sessionOptionsHandle: number, name: number): number;
+    _OrtAddSessionConfigEntry(sessionOptionsHandle: number, configKey: number, configValue: number): number;
+    _OrtReleaseSessionOptions(sessionOptionsHandle: number): void;
 
-  _OrtCreateRunOptions(logSeverityLevel: number, logVerbosityLevel: number, terminate: boolean, tag: number): number;
-  _OrtAddRunConfigEntry(runOptionsHandle: number, configKey: number, configValue: number): number;
-  _OrtReleaseRunOptions(runOptionsHandle: number): void;
+    _OrtCreateRunOptions(logSeverityLevel: number, logVerbosityLevel: number, terminate: boolean, tag: number): number;
+    _OrtAddRunConfigEntry(runOptionsHandle: number, configKey: number, configValue: number): number;
+    _OrtReleaseRunOptions(runOptionsHandle: number): void;
 
-  _OrtEndProfiling(sessionHandle: number): number;
-  // #endregion
+    _OrtEndProfiling(sessionHandle: number): number;
+    // #endregion
 
-  // #region config
-  mainScriptUrlOrBlob?: string|Blob;
-  // #endregion
+    // #region config
+    mainScriptUrlOrBlob?: string | Blob;
+    // #endregion
 
-  // #region JSEP
-  jsepInit?
-      (backend: JSEP.BackendType, alloc: JSEP.AllocFunction, free: JSEP.FreeFunction, upload: JSEP.UploadFunction,
-       download: JSEP.DownloadFunction, createKernel: JSEP.CreateKernelFunction,
-       releaseKernel: JSEP.ReleaseKernelFunction, run: JSEP.RunFunction): void;
+    // #region JSEP
+    jsepInit?
+        (backend: JSEP.BackendType, alloc: JSEP.AllocFunction, free: JSEP.FreeFunction, upload: JSEP.UploadFunction,
+            download: JSEP.DownloadFunction, createKernel: JSEP.CreateKernelFunction,
+            releaseKernel: JSEP.ReleaseKernelFunction, run: JSEP.RunFunction): void;
 
-  _JsepOutput(context: number, index: number, data: number): number;
+    _JsepOutput(context: number, index: number, data: number): number;
 
-  jsepRunPromise?: Promise<number>;
-  // #endregion
+    jsepRunPromise?: Promise<number>;
+    // #endregion
+
+    // #region ORT WASM Traning
+    _OrtLoadCheckpointForTraining(dataOffset: number, dataLength: number): number;
+    _OrtCreateTrainingSession(sessionOptionsHandle: number, trainModelOffset: number,
+        trainModelLength: number, evalModelOffset: number, evalModelLength: number, optimizerModelOffset: number,
+        optimizerModelLength: number): number;
+
+    _OrtRunTrainStep(trainingSessionHandle: number, inputsOffset: number, inputCount: number,
+        outputsOffset: number, outputCount: number, runOptionsHandle: number): number;
+
+    _OrtReleaseTrainingSession(trainingSessionHandle: number): void;
+
+    _OrtReleaseTrainingCheckpoint(checkpointStateHandle: number): void;
+    // #endregion
 }
 
 declare const moduleFactory: EmscriptenModuleFactory<OrtWasmModule>;
