0ffd4c5375104fa247322b3e6a1c2c5772df2810
Prasanth Pulavarthi <prasanth.pulavarthi@microsoft.com>
2023-07-21 13:14:36 -0700

    Update cross-platform.svelte

--- website/src/routes/components/cross-platform.svelte
+++ website/src/routes/components/cross-platform.svelte
@@ -2,7 +2,7 @@
     <figure><img src="" alt="Cross Platform Graphic" /></figure>
     <div class="card-body">
       <h2 class="card-title">Cross Platform</h2>
-      <p>Take advantage of the benefits of ONNX Runtime without changing your technology stack. Access ONNX RunTime using your preferred API - C#, C++, C, Python, or Java. Support for Linux, Windows, and Mac allows you to build and deploy applications without worry.</p>
+      <p>Take advantage of the benefits of ONNX Runtime without changing your technology stack. Access ONNX Runtime using your preferred API - C#, C++, C, Python, or Java. Support for Linux, Windows, and Mac allows you to build and deploy applications without worry.</p>
       <div class="join join-vertical lg:join-horizontal">
         <button class="btn join-item">PyTorch</button>
         <button class="btn join-item">Windows</button>
