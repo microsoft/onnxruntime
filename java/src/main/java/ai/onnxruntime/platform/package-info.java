367660fc95613768ab23b061a4b8b79a31fc3ebe
Adam Pocock <adam.pocock@oracle.com>
2023-07-24 11:53:04 -0400

    Updating the package docs.

--- java/src/main/java/ai/onnxruntime/platform/package-info.java
+++ java/src/main/java/ai/onnxruntime/platform/package-info.java
@@ -6,6 +6,8 @@
 /**
  * A package of platform specific code, used to swap out Java implementations which don't run on Android.
  *
+ * <p>Classes in this package should always have the same public methods.
+ *
  * <p>This is the Java version of the package.
  */
 package ai.onnxruntime.platform;
