b9ef4214153c51eb343735da0d0495aa210cb936
yf711 <yifanl@microsoft.com>
2023-07-25 14:48:55 -0700

    introduce valgrind

--- onnxruntime/python/tools/tensorrt/perf/mem_test/run.sh
+++ onnxruntime/python/tools/tensorrt/perf/mem_test/run.sh
@@ -14,9 +14,10 @@ s) ORT_SOURCE=${OPTARG};;
 esac
 done
 
-ONNX_MODEL_URL="https://github.com/onnx/models/raw/master/vision/classification/squeezenet/model/squeezenet1.0-7.onnx"
+ONNX_MODEL_TAR_URL="https://github.com/onnx/models/raw/main/vision/classification/squeezenet/model/squeezenet1.0-7.tar.gz"
+MODEL_TAR_NAME="squeezenet1.0-7.tar.gz"
 ONNX_MODEL="squeezenet.onnx"
-ASAN_OPTIONS="protect_shadow_gap=0:log_path=asan.log"
+ASAN_OPTIONS="protect_shadow_gap=0:new_delete_type_mismatch=0:log_path=asan.log"
 
 export LD_LIBRARY_PATH=${ORT_BINARY_PATH}
 export LIBRARY_PATH=${ORT_BINARY_PATH}
@@ -46,21 +47,65 @@ cd build
 cp ../squeezenet_calibration.flatbuffers . 
 
 cmake ..
-make -j8
-wget ${ONNX_MODEL_URL} -O ${ONNX_MODEL}
-ASAN_OPTIONS=${ASAN_OPTIONS} ./onnx_memtest
+make -j
+wget ${ONNX_MODEL_TAR_URL} -O squeezenet1.0-7.tar.gz
+tar -xzf ${MODEL_TAR_NAME} --strip-components=1
+mv model.onnx ${ONNX_MODEL}
+rm ${MODEL_TAR_NAME}
+mkdir result
 
-if [ $? -ne 0 ]
-then
-    echo "Memory test application failed."
-    exit 1
+# Run valgrind
+echo $(date +"%Y-%m-%d %H:%M:%S") '[valgrind] Starting memcheck with' ${ONNX_MODEL}
+valgrind --leak-check=full --show-leak-kinds=all --log-file=valgrind.log ${ORT_SOURCE}/build/Linux/Release/onnxruntime_perf_test -e tensorrt -r 1 ${ONNX_MODEL}
+echo $(date +"%Y-%m-%d %H:%M:%S") '[valgrind] Analyzing valgrind log'
+
+found_leak_summary=false
+is_mem_leaked=false
+while IFS= read -r line
+do
+  if echo $line | grep -q 'LEAK SUMMARY:'; then
+    found_leak_summary=true
+  elif $found_leak_summary && echo $line | grep -q 'definitely lost:'; then
+    bytes_lost=$(echo $line | grep -o -E '[0-9,]+ bytes')
+    blocks_lost=$(echo $line | grep -o -E '[0-9]+ blocks')
+    echo "Bytes lost: $bytes_lost"
+    echo "Blocks lost: $blocks_lost"
+    if [ "$blocks_lost" != "0 blocks" ]; then
+      echo $(date +"%Y-%m-%d %H:%M:%S") '[valgrind] Memory leak happened when testing squeezenet model!'
+      is_mem_leaked=true
+    fi
+    found_leak_summary=false
+  fi
+done < "valgrind.log"
+
+# Export detailed memleak log if available
+if [ "$is_mem_leaked" = "true" ]; then
+    awk '
+    # substitute "==xxxxx==" with ""
+    {sub(/==[0-9]+== /, "")}
+
+    # found=1 when keyword is found
+    /blocks are definitely lost in loss/ {found = 1}
+
+    # export this line when found and line!=""
+    found && $0 != "" {print}
+
+    # stop exporting when found and line=""
+    found && $0 == "" {found = 0; print ""}
+    ' valgrind.log > memleak_detail.log
+    echo $(date +"%Y-%m-%d %H:%M:%S") '[valgrind] Detailed memleak log saved in artifact memleak_detail.log'
+    mv memleak_detail.log result
 fi
 
-mkdir result
+mv valgrind.log result
+
+# Run AddressSanitizer 
+ASAN_OPTIONS=${ASAN_OPTIONS} ./onnx_memtest
+
 if [ -e asan.log* ]
 then
     cat asan.log*
     mv asan.log* result
 else
-    echo "No memory Leak(s) or other memory error(s) detected." > result/asan.log
+    echo $(date +"%Y-%m-%d %H:%M:%S") "[AddressSanitizer] No memory Leak(s) or other memory error(s) detected." > result/asan.log
 fi
