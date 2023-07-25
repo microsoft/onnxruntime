56e565dfa05ac8d234793630979325f444971fe4
Wanming Lin <wanming.lin@intel.com>
2023-07-25 15:49:08 +0800

    Fix a nit

--- onnxruntime/core/providers/webnn/builders/impl/reduction_op_builder.cc
+++ onnxruntime/core/providers/webnn/builders/impl/reduction_op_builder.cc
@@ -136,7 +136,7 @@ bool ReductionOpBuilder::IsOpSupportedImpl(const InitializedTensorSet& initializ
   const auto& op_type = node.OpType();
   // If the optional input 'axes' is provided, it must be an initializer.
   if (input_defs.size() > 1 && !Contains(initializers, input_defs[1]->Name())) {
-    LOGS(logger, VERBOSE) << "Input axes of " << op_type << " must be known";
+    LOGS(logger, VERBOSE) << "Input axes of " << op_type << " must be a constant";
     return false;
   }
 
