28e92b484aeb96cd14d1c155d2095715191747cd
Patrice Vignola <vignola.patrice@gmail.com>
2023-07-25 21:43:29 -0700

    [DML EP] Add NCHW and float16 gamma/beta support for GroupNorm (#16814)
    This will remove transposes that are non needed in the DML kernel. To
    keep backward compatiblity, the default behavior is to set NHWC when no
    attribute is set.

--- onnxruntime/core/providers/dml/OperatorAuthorHelper/Attributes.h
+++ onnxruntime/core/providers/dml/OperatorAuthorHelper/Attributes.h
@@ -23,6 +23,7 @@ namespace AttrName
     static constexpr const char* BlockSize = "blocksize";
     static constexpr const char* Border = "border";
     static constexpr const char* Broadcast = "broadcast";
+    static constexpr const char* ChannelsLast = "channels_last";
     static constexpr const char* CeilMode = "ceil_mode";
     static constexpr const char* Clip = "clip";
     static constexpr const char* CoordinateTransformationMode = "coordinate_transformation_mode";
