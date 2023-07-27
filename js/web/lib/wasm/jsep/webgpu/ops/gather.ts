01dc586dec7a7102237ede4d41a919480ff082f7
Jian Chen <cjian@microsoft.com>
2023-07-27 15:47:20 -0700

    Upload gather js implementation.

--- js/web/lib/wasm/jsep/webgpu/ops/gather.ts
+++ js/web/lib/wasm/jsep/webgpu/ops/gather.ts
@@ -1,116 +1,116 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
-
-import {DataType} from '../../../wasm-common';
+import {ComputeContext, GpuDataType, ProgramInfo, ProgramMetadata} from '../types';
 import {TensorView} from '../../tensor';
-import {AttributeWithCacheKey, createAttributeWithCacheKey} from '../attribute-with-cache-key';
-import {ComputeContext, GpuDataType, ProgramInfo} from '../types';
-import {ShapeUtil} from '../../util';
-import {ShaderHelper} from './common';
-// import {createTransposeProgramInfo, TransposeAttributes, transposeProgramMetadata} from "./transpose";
-// import {createTransposeProgramInfo, TransposeAttributes, transposeProgramMetadata} from './transpose';
-
-const validateInputs = (inputs: readonly TensorView[]): void => {
-	const data = inputs[0];
-	const indices = inputs[1];
-	if (!inputs || inputs.length !== 2) {
-		throw new Error('Gather op requires 2 inputs.');
-	}
-
-	if (data.dims.length < 1) {
-		throw new Error('Invalid Gather input data shape.');
-	}
-
-	if (indices.dataType !== DataType.int64 && indices.dataType !== DataType.int32) {
-		throw new Error('Invalid Gather input data input type. Accepted input data type is int32 or int64.');
-	}
-};
+import {DataType} from "../../../wasm-common";
+import {ShapeUtil} from "../../util";
+import {ShaderHelper} from "./common";
+import {AttributeWithCacheKey, createAttributeWithCacheKey} from "../attribute-with-cache-key";
 
 export interface GatherAttributes extends AttributeWithCacheKey {
-	readonly axis: number;
+    axis: number;
 }
 
-export const gatherProgramMetadata = {
-	name: 'Gather',
-	inputTypes: [GpuDataType.default,GpuDataType.default],
-};
-const createGatherProgramInfo = (
-	inputData: TensorView,
-	index: TensorView
-): ProgramInfo => {
-	// eslint-disable-next-line no-console
-	console.log('createGatherProgramInfo');
-	const inputShape = inputData.dims.slice();
-	const indexShape = index.dims.slice();
-	const outputShape = new Array(inputShape.length + indexShape.length - 1);
-	for (let i = 0; i < outputShape.length; i++) {
-		if (i < indexShape.length) {  // B
-			outputShape[i] = indexShape[i];
-		} else {                                                       // C
-			outputShape[i] = inputShape[i - indexShape.length + 1];  // skip 1 for axis
-		}
-	}
-	const outputSize = ShapeUtil.size(outputShape);
-	const indexSize = ShapeUtil.size(indexShape);
-	const dataType = 'f32';  // TODO: support other data type
-	const subOutputSize = ShapeUtil.size(outputShape.slice(1));
+const validateInputs = (inputs: readonly TensorView[]): void => {
+    if (!inputs || inputs.length !== 2) {
+        throw new Error('Gather requires 2 inputs.');
+    }
 
-	// Step 1: transpose input data along axis
-	const getShaderSource = (shaderHelper: ShaderHelper) => `
-        @group(0) @binding(0) var<storage, read> inputData : array<${dataType}>;
-        @group(0) @binding(1) var<storage, read> index : array<i32>;
-        @group(0) @binding(2) var<storage, read_write> output : array<${dataType}>;
-
-        ${shaderHelper.mainStart()}
-        	${shaderHelper.guardAgainstOutOfBoundsWorkgroupSizes(outputSize)}
-        	for (var i : i32 = 0; i < ${indexSize}; i++) {
-						for (var j : i32 = 0; j < ${subOutputSize}; j++) {
-						 let idx = index[i];
-<!--						 output[i * ${subOutputSize} + j] = inputData[idx * ${subOutputSize} + j];-->
-						}
-        	}
-    }`;
-	return {
-		...gatherProgramMetadata,
-		getShaderSource,
-		outputs: [{dims: outputShape, dataType: inputData.dataType, gpuDataType: GpuDataType.default}],
-		dispatchGroup: () => ({x: Math.ceil(outputSize / 64 /* workgroup size */)})
-	};
+    // if (inputs[0].dataType !== DataType.float || inputs[1].dataType !== DataType.float) {
+    //     throw new Error('inputs should be float type');
+    // }
 };
+
+const createGatherProgramInfo =
+    (metadata: ProgramMetadata, inputs: readonly TensorView[], attributes: GatherAttributes):
+        ProgramInfo => {
+        const inputShape = inputs[0].dims;
+        const indicesShape = inputs[1].dims;
+
+        const inputRank = inputShape.length;
+        const axis = ShapeUtil.normalizeAxis(attributes.axis, inputRank);
+
+        const outputShape = [];
+        for (let i = 0; i < axis; ++i) {
+            outputShape.push(inputShape[i]);
+        }
+
+        for (const i of indicesShape) {
+            outputShape.push(i);
+        }
+
+        for (let i = axis + 1; i < inputRank; ++i) {
+            outputShape.push(inputShape[i]);
+        }
+        const inputDataType = inputs[0].dataType;
+        const block = ShapeUtil.sizeFromDimension(inputShape, axis + 1);
+        const elementSize = [DataType.int64, DataType.uint64, DataType.double].includes(inputDataType) ? 2 : 1;
+        const indicesElementSize = inputs[1].dataType === DataType.int64 ? 2 : 1;
+        const blockSize = elementSize * block;
+        const M = ShapeUtil.sizeToDimension(inputShape, axis);
+        const N = ShapeUtil.size(indicesShape);
+        const dataBatchElements = ShapeUtil.sizeFromDimension(inputShape, axis) * elementSize;
+        const gatheredBatchElements = N * block * elementSize;
+        const axisDimLimit = inputShape[axis];
+
+        const inputSize = ShapeUtil.size(inputShape) * elementSize;
+        const outputSize = ShapeUtil.size(outputShape) * elementSize;
+
+        const totalGathers = M * N;
+        // int64 indices would be treated as little endian i32 with assumption they fall in i32 limits
+        // That assumption is safe as it's not possible to allocate >2gb buffer for input tensor
+        // Input data will be treated as u32 or two u32 for 8-byte tensors
+        const getShaderSource = (shaderHelper: ShaderHelper) => `
+  const N: u32 = ${N};
+  const elementSize: u32 = ${elementSize};
+  const indicesElementSize: u32 = ${indicesElementSize};
+
+  @group(0) @binding(0) var<storage, read> input : array<f32>;
+  @group(0) @binding(1) var<storage, read> inputIndices : array<i32>;
+  @group(0) @binding(2) var<storage, read_write> output: array<f32>;
+
+  ${shaderHelper.mainStart()}
+    let batch: u32 = global_idx / N;
+    let i: u32 = global_idx % N;
+
+    let srcOffsetBatch: u32 = batch * ${dataBatchElements};
+    let dstOffsetBatch: u32 = batch * ${gatheredBatchElements};
+    var idx = inputIndices[i * indicesElementSize];
+    if (idx < 0) {
+        idx = idx + ${axisDimLimit};
+    }
+
+    let srcOffset = srcOffsetBatch + u32(idx) * ${blockSize};
+    let dstOffset = dstOffsetBatch + i * ${blockSize};
+    if (srcOffset >= ${inputSize}) {
+        return;
+    }
+    if (dstOffset >= ${outputSize}) {
+        return;
+    }
+    for (var j: u32 = 0; j < ${blockSize}; j++) {
+        output[dstOffset + j] = input[srcOffset + j];
+    }
+  }`;
+        return {
+            ...metadata,
+            outputs: [
+                {dims: outputShape, dataType: inputs[0].dataType, gpuDataType: GpuDataType.default},
+            ],
+            getShaderSource,
+            dispatchGroup: () => ({x: Math.ceil( totalGathers / 64 /* workgroup size */)})
+        };
+    };
+
+export const parseGatherAttributes = (attributes: Record<string, unknown>): GatherAttributes =>
+    createAttributeWithCacheKey({ axis: attributes.axis as number });
 
 export const gather = (context: ComputeContext, attributes: GatherAttributes): void => {
-	validateInputs(context.inputs);
-	const inputData = context.inputs[0];
-	const inputShape = inputData.dims.slice();
-	const axis = ShapeUtil.normalizeAxis(attributes.axis, inputShape.length);
-	const perm = inputShape;
-	perm[axis] = 0;
-	perm[0] = axis;
-	// const weightTransposeAttribute: TransposeAttributes = createAttributeWithCacheKey({perm});
-	// const transposedInput = context.compute({
-	// 		...transposeProgramMetadata,
-	// 		cacheHint: weightTransposeAttribute.cacheKey,
-	// 		get: () => createTransposeProgramInfo(inputData, weightTransposeAttribute.perm)
-	// 	}
-	// )[0];
-	//
-	// const gathered = context.compute({
-	// 		...gatherProgramMetadata,
-	// 		cacheHint: attributes.cacheKey,
-	// 		get: () => createGatherProgramInfo(transposedInput, context.inputs[1])
-	// 	}
-	// )[0];
-	// context.compute({
-	// 	...transposeProgramMetadata,
-	// 	cacheHint: weightTransposeAttribute.cacheKey,
-	// 	get: () => createTransposeProgramInfo(gathered, weightTransposeAttribute.perm)
-	// });
-		context.compute({
-		...gatherProgramMetadata,
-		cacheHint: attributes.cacheKey,
-		get: () => createGatherProgramInfo(inputData, context.inputs[1])
-	});
+    validateInputs(context.inputs);
+
+    const metadata = {
+        name: 'Gather',
+        inputTypes: [GpuDataType.default, GpuDataType.default],
+        cacheHint: attributes.cacheKey,
+    };
+
+    context.compute(createGatherProgramInfo(metadata, context.inputs, attributes));
 };
-export const parseGatherAttributes = (attributes: Record<string, unknown>): GatherAttributes =>
-	createAttributeWithCacheKey(attributes as Omit<GatherAttributes, keyof AttributeWithCacheKey>);
