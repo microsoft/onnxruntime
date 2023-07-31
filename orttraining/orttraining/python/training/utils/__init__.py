a7ef090ae9e1678a9fd17bfb8a4776fa4680b7ec
Peng Wang(AI FWK) <pengwa@microsoft.com>
2023-07-31 09:42:39 +0000

    fix lint

--- orttraining/orttraining/python/training/utils/__init__.py
+++ orttraining/orttraining/python/training/utils/__init__.py
@@ -2,12 +2,22 @@
 # Licensed under the MIT License.
 # __init__.py
 
-from .torch_io_helper import (
-    PrimitiveType,
-    ORTModelInputOutputType,
-    _TensorStub,
+from onnxruntime.training.utils.torch_io_helper import (
     ORTModelInputOutputSchemaType,
-    get_schema_for_flatten_data,
+    ORTModelInputOutputType,
+    PrimitiveType,
+    _TensorStub,
     flatten_data_with_schema,
+    get_schema_for_flatten_data,
     unflatten_from_data_and_schema,
-)  # noqa: F401
+)
+
+__all__ = [
+    "PrimitiveType",
+    "ORTModelInputOutputType",
+    "_TensorStub",
+    "ORTModelInputOutputSchemaType",
+    "get_schema_for_flatten_data",
+    "flatten_data_with_schema",
+    "unflatten_from_data_and_schema",
+]
