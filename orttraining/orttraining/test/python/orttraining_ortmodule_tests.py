247ab105e3c6c9dc6e3964b38b3b69530f443b0c
Baiju Meswani <bmeswani@microsoft.com>
2023-07-31 23:34:36 +0000

    Make RNN python tests as reference tests only and avoid running them in the CI pipeline

--- orttraining/orttraining/test/python/orttraining_ortmodule_tests.py
+++ orttraining/orttraining/test/python/orttraining_ortmodule_tests.py
@@ -159,16 +159,6 @@ def run_pytorch_export_contrib_ops_tests(cwd, log):
     run_subprocess(command, cwd=cwd, log=log).check_returncode()
 
 
-def run_rnn_training_op_tests(cwd, log):
-    log.debug("Running: RNN Training Ops Tests")
-
-    command = [sys.executable, "-m", "pytest", "-sv", "orttraining_test_lstm.py"]
-    run_subprocess(command, cwd=cwd, log=log).check_returncode()
-
-    command = [sys.executable, "-m", "pytest", "-sv", "orttraining_test_gru.py"]
-    run_subprocess(command, cwd=cwd, log=log).check_returncode()
-
-
 def main():
     args = parse_arguments()
     cwd = args.cwd
@@ -211,8 +201,6 @@ def main():
 
     # TODO(bmeswani): Enable this test once it can run with latest pytorch
     # run_pytorch_export_contrib_ops_tests(cwd, log)
-
-    run_rnn_training_op_tests(cwd, log)
 
     return 0
 
