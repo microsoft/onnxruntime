33f81287b948af8b4887a72562001c2557d5dcaa
pengwa@microsoft.com <pengwa@microsoft.com>
2023-08-01 06:37:56 +0000

    fixes

--- orttraining/orttraining/python/training/ortmodule/_onnx_models.py
+++ orttraining/orttraining/python/training/ortmodule/_onnx_models.py
@@ -26,9 +26,10 @@ class ONNXModels:
     1. exported_model: Model that is exported by torch.onnx.export
     2. optimized_model: For eval mode it's exported_model with concrete input shapes set if needed,
                         for training mode, it's optimized model after gradients graph has been built.
-    3. In addition, ORTModule also saves the execution_model which is the model
-       that is being executed by ORT. It has further optimizations done by the
-       InferenceSession and is saved by the InferenceSession.
+    In addition, ORTModule also saves two other models, to the user-provided path:
+    a. the pre_grad_model which is the model before the gradients graph is built.
+    b. the execution_model which is the model that is being executed by ORT.
+      It has further optimizations done by the InferenceSession and is saved by the InferenceSession.
     """
 
     exported_model: Optional[onnx.ModelProto] = None
