cded46f32633d677941fd6ae99b4b953aa5c141a
xadupre <xadupre@microsoft.com>
2023-08-01 15:17:20 +0000

    fix float 8 conversion on CPU

--- onnxruntime/test/framework/float_8_test.cc
+++ onnxruntime/test/framework/float_8_test.cc
@@ -16,10 +16,19 @@ namespace onnxruntime {
 namespace test {
 
 TEST(Float8_Tests, CastE4M3FN) {
-  std::vector<std::pair<float,float>> cases{
-    std::pair<float,float>(1.8131605, 1.875)
-  };
-  for(auto it: cases) {
+  std::vector<std::pair<float, float>> cases{
+      std::pair<float, float>(0.00439453125, 0.00390625),
+      std::pair<float, float>(0.005859375, 0.005859375),
+      std::pair<float, float>(0.005759375, 0.005859375),
+      std::pair<float, float>(0.0046875, 0.00390625),
+      std::pair<float, float>(0.001953125, 0.001953125),
+      std::pair<float, float>(0.0029296875, 0.00390625),
+      std::pair<float, float>(0.002053125, 0.001953125),
+      std::pair<float, float>(0.00234375, 0.001953125),
+      std::pair<float, float>(0.0087890625, 0.0078125),
+      std::pair<float, float>(0.001171875, 0.001953125),
+      std::pair<float, float>(1.8131605, 1.875)};
+  for (auto it : cases) {
     auto f8 = onnxruntime::Float8E4M3FN(it.first);
     auto f8_32 = f8.ToFloat();
     EXPECT_EQ(it.second, f8_32);
