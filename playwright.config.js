9a31436579cd2a688f20d6fbc502dd0ffa173eda
Changming Sun <chasun@microsoft.com>
2023-08-02 18:52:26 -0700

    update

--- playwright.config.js
+++ playwright.config.js
@@ -4,9 +4,6 @@ const config = {
     launchOptions: {
         args: [
             '--enable-unsafe-webgpu',
-            '--enable-gpu',
-            '--use-angle=egl-angle',
-            '--use-gl=d3d11',
             '--ignore-gpu-blocklist',
 			"--gpu-vendor-id=0x10de"
         ],
