43bf2753bc8ae9fc91a3854c75c788d8d4d04364
RandySheriffH <48490400+RandySheriffH@users.noreply.github.com>
2023-08-03 16:05:54 -0700

    Move factory  (#16994)
    Move factory
    
    Co-authored-by: Randy Shuai <rashuai@microsoft.com>

---
+++ include/onnxruntime/core/providers/azure/azure_provider_factory.h
@@ -0,0 +1,17 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+#pragma once
+
+#include "core/providers/providers.h"
+
+namespace onnxruntime {
+
+struct AzureProviderFactory : public IExecutionProviderFactory {
+  AzureProviderFactory(const std::unordered_map<std::string, std::string>& config);
+  std::unique_ptr<IExecutionProvider> CreateProvider() override;
+  };
+  std::unordered_map<std::string, std::string> config_;
+};
+
+}
