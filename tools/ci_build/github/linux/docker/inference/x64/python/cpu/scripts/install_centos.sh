576a7e415cd89d493a74164589bf7e88b90ec30b
Jian Chen <cjian@microsoft.com>
2023-08-03 14:45:47 -0700

    Remove libunwind

--- tools/ci_build/github/linux/docker/inference/x64/python/cpu/scripts/install_centos.sh
+++ tools/ci_build/github/linux/docker/inference/x64/python/cpu/scripts/install_centos.sh
@@ -4,8 +4,8 @@ set -e
 os_major_version=$(cat /etc/redhat-release | tr -dc '0-9.'|cut -d \. -f1)
 
 echo "installing for os major version : $os_major_version"
-yum install -y which gdb redhat-lsb-core expat-devel tar unzip zlib-devel make bzip2 bzip2-devel
 yum install -y which gdb redhat-lsb-core expat-devel tar unzip zlib-devel make bzip2 bzip2-devel perl-IPC-Cmd openssl-devel wget
+
 # export PATH=/opt/python/cp38-cp38/bin:$PATH
 
 echo "installing rapidjson for AzureEP"
