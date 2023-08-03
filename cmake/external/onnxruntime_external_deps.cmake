3a585bf78d8c3717cfc1b71bdb21c694c41f8b2b
Changming Sun <chasun@microsoft.com>
2023-08-03 15:05:39 -0700

    update

--- cmake/external/onnxruntime_external_deps.cmake
+++ cmake/external/onnxruntime_external_deps.cmake
@@ -43,7 +43,11 @@ if (onnxruntime_BUILD_UNIT_TESTS)
     set(gtest_disable_pthreads ON)
   endif()
   set(INSTALL_GTEST OFF CACHE BOOL "" FORCE)
+if (CMAKE_SYSTEM_NAME STREQUAL "iOS")
+  set(GTEST_HAS_ABSL OFF CACHE BOOL "" FORCE)
+else()
   set(GTEST_HAS_ABSL ON CACHE BOOL "" FORCE)
+endif()
   # gtest and gmock
   FetchContent_Declare(
     googletest
