ea0f50004a7e95529eecc996c9c697c8a25d1b4a
Randy Shuai <rashuai@microsoft.com>
2023-08-06 16:55:48 -0700

    add UT for AzureEP in scharp

--- csharp/test/Microsoft.ML.OnnxRuntime.Tests.Common/InferenceTest.cs
+++ csharp/test/Microsoft.ML.OnnxRuntime.Tests.Common/InferenceTest.cs
@@ -5,6 +5,7 @@ using Microsoft.ML.OnnxRuntime.Tensors;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using Xunit;
@@ -2025,6 +2026,30 @@ namespace Microsoft.ML.OnnxRuntime.Tests
                 }
             }
         }
-    }
 
+        [Fact(DisplayName = "TestLoadAzureEP")]
+        private void TestLoadAzureEP()
+        {
+            var model = TestDataLoader.LoadModelFromEmbeddedResource("mul_1.onnx");
+
+            using (var memInfo = new OrtMemoryInfo(OrtMemoryInfo.allocatorCPU,
+                                                   OrtAllocatorType.ArenaAllocator, 0, OrtMemType.Default))
+            using (var arenaCfg = new OrtArenaCfg(0, -1, -1, -1))
+            {
+                using (var sessionOptions = new SessionOptions())
+                {
+                    sessionOptions.AppendExecutionProvider("AZURE");
+                    try {
+                        using (var session1 = new InferenceSession(model, sessionOptions))
+                        {
+
+                        }
+                    }
+                    catch (Exception) {
+                        Assert.True(false);
+                    } 
+                }
+            }
+        }
+    }
 }
