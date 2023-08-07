49791b5decd1e936ebbfcd906e296dc4647f59c9
Jeff Bloomfield <jeffbloo@microsoft.com>
2023-08-07 14:18:15 -0700

    Revert "Temporarily flush and sync weight copies before initialization"
    This reverts commit 789eb66fda893a711cf472500f4be1b26c501baa.

--- onnxruntime/core/providers/dml/DmlExecutionProvider/src/ExecutionProvider.cpp
+++ onnxruntime/core/providers/dml/DmlExecutionProvider/src/ExecutionProvider.cpp
@@ -961,13 +961,6 @@ namespace Dml
         assert(!m_closed);
         m_context->Flush();
     }
-    
-    void __stdcall ExecutionProviderImpl::FlushAndSync() const
-    {
-        Flush();
-        m_context->GetCurrentCompletionEvent().WaitForSignal();
-        m_context->ReleaseCompletedReferences();
-    }
 
     void ExecutionProviderImpl::ReleaseCompletedReferences()
     {
