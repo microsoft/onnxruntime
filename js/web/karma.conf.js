170c96651386838d6adb4f768c07450db08e1142
Yulong Wang <7679871+fs-eire@users.noreply.github.com>
2023-08-07 15:15:12 -0700

    add a single line comments to explain the flags

--- js/web/karma.conf.js
+++ js/web/karma.conf.js
@@ -90,6 +90,7 @@ module.exports = function(config) {
     hostname,
     listenAddress,
     customLaunchers: {
+      // the following flags are used to make sure Edge on CI agents to initialize WebGPU correctly.
       EdgeWebGpuTest: {base: 'Edge', flags: ['--ignore-gpu-blocklist', '--gpu-vendor-id=0x10de']},
       ChromeTest: {base: 'Chrome', flags: ['--enable-features=SharedArrayBuffer']},
       ChromeTestHeadless: {base: 'ChromeHeadless', flags: ['--enable-features=SharedArrayBuffer']},
