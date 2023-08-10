24fb065092fd1531f9a9855bbf2aef1210cc38b2
wejoncy <wejoncy@163.com>
2023-08-10 19:00:31 +0800

    rope

--- onnxruntime/contrib_ops/cuda/bert/paged_attention_impl.cu
+++ onnxruntime/contrib_ops/cuda/bert/paged_attention_impl.cu
@@ -386,6 +386,52 @@ __global__ void reshape_and_cache_kernel(
   }
 }
 
+template <typename scalar_t>
+__global__ void rotary_embedding_neox_kernel(
+    const int64_t* __restrict__ positions,       // [num_tokens]
+    scalar_t* __restrict__ query,                // [num_tokens, num_heads, head_size]
+    scalar_t* __restrict__ key,                  // [num_tokens, num_kv_heads, head_size]
+    const scalar_t* __restrict__ cos_sin_cache,  // [max_position, 2, rot_dim // 2]
+    const int rot_dim,
+    const int stride,
+    const int num_heads,
+    const int num_kv_heads,
+    const int head_size) {
+  // Each thread block is responsible for one token.
+  const int token_idx = blockIdx.x;
+  int64_t pos = positions[token_idx];
+  const scalar_t* cache_ptr = cos_sin_cache + pos * rot_dim;
+
+  const int embed_dim = rot_dim / 2;
+  const int nq = num_heads * embed_dim;
+  for (int i = threadIdx.x; i < nq; i += blockDim.x) {
+    const int head_idx = i / embed_dim;
+    const int token_head = token_idx * stride + head_idx * head_size;
+
+    const int rot_offset = i % embed_dim;
+    const int x_index = rot_offset;
+    const int y_index = embed_dim + rot_offset;
+
+    const int out_x = token_idx * stride + head_idx * head_size + x_index;
+    const int out_y = token_idx * stride + head_idx * head_size + y_index;
+
+    const scalar_t cos = __ldg(cache_ptr + x_index);
+    const scalar_t sin = __ldg(cache_ptr + y_index);
+
+    const scalar_t q_x = query[token_head + x_index];
+    const scalar_t q_y = query[token_head + y_index];
+    query[out_x] = q_x * cos - q_y * sin;
+    query[out_y] = q_y * cos + q_x * sin;
+
+    if (head_idx < num_kv_heads) {
+      const scalar_t k_x = key[token_head + x_index];
+      const scalar_t k_y = key[token_head + y_index];
+      key[out_x] = k_x * cos - k_y * sin;
+      key[out_y] = k_y * cos + k_x * sin;
+    }
+  }
+}
+
 }  // namespace vllm
 
 #define LAUNCH_ATTENTION_KERNEL(T, HEAD_SIZE, BLOCK_SIZE, NUM_THREADS)                 \
@@ -597,6 +643,52 @@ void reshape_and_cache(
         head_size,
         block_size,
         x);
+  }
+}
+
+void rotary_embedding_neox(
+    const cudaStream_t stream,
+    const int64_t* positions,  // [num_tokens]
+    void* query,               // [num_tokens, num_heads * head_size]
+    void* key,                 // [num_tokens, num_kv_heads * head_size]
+    int head_size,
+    const void* cos_sin_cache,  // [max_position, rot_dim]
+    int num_tokens,
+    int rot_dim,
+    int num_heads,
+    int num_kv_heads,
+    int dtype) {
+  //int num_tokens = query.size(0);
+  //int rot_dim = cos_sin_cache.size(1);
+  //int num_heads = query.size(1) / head_size;
+  //int num_kv_heads = key.size(1) / head_size;
+  int stride = num_heads * head_size;
+  //TORCH_CHECK(stride == key.stride(0));
+
+  dim3 grid(num_tokens);
+  dim3 block(std::min(num_heads * rot_dim / 2, 512));
+  //const cudaStream_t stream = at::cuda::getCurrentCUDAStream();
+
+  if (dtype == 0) {
+    //float
+    //CALL_KERNEL_LAUNCHER_BLOCK_SIZE(float);
+    //} else if constexpr (std::is_same_v<T, MLFloat16>) {
+  } else if (dtype == 1) {
+    // half
+    using scalar_t = half;
+    vllm::rotary_embedding_neox_kernel<scalar_t><<<grid, block, 0, stream>>>(
+        positions,
+        static_cast<scalar_t*>(query),
+        static_cast<scalar_t*>(key),
+        static_cast<const scalar_t*>(cos_sin_cache),
+        rot_dim,
+        stride,
+        num_heads,
+        num_kv_heads,
+        head_size);
+    //} else if constexpr (std::is_same_v<T, BFloat16>) {
+  } else if (dtype == 2) {
+    // CALL_KERNEL_LAUNCHER_BLOCK_SIZE(__nv_bfloat16);
   }
 }
 
