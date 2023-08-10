bc29da8ab0755f78af3dcaeeb86fe77305b72d96
Changming Sun <chasun@microsoft.com>
2023-08-10 13:51:02 -0700

    update

--- dockerfiles/scripts/install_cmake.sh
+++ dockerfiles/scripts/install_cmake.sh
@@ -3,5 +3,5 @@ mkdir -p /tmp/src
 cd /tmp/src
 
 echo "Installing cmake"
-curl https://github.com/Kitware/CMake/releases/download/v3.27.1/cmake-3.27.1-linux-`uname -m`.tar.gz -sSL --retry 5  -o /tmp/src/cmake-3.27.1-linux-`uname -m`.tar.gz
-tar -zxf /tmp/src/cmake-3.27.1-linux-`uname -m`.tar.gz --strip=1 -C /usr
+curl https://github.com/Kitware/CMake/releases/download/v3.27.2/cmake-3.27.2-linux-`uname -m`.tar.gz -sSL --retry 5  -o /tmp/src/cmake-3.27.2-linux-`uname -m`.tar.gz
+tar -zxf /tmp/src/cmake-3.27.2-linux-`uname -m`.tar.gz --strip=1 -C /usr
