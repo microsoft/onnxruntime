f2e1b91634a42091db49cebf196f6d96788ba0d5
Chen Fu <1316708+chenfucn@users.noreply.github.com>
2023-08-11 15:17:58 -0700

    add int4 quantization code in python (#17077)
    ### Description
    Adding int4 quantization code in python
    
    
    ### Motivation and Context
    Python quantization tool no-longer needs to invoke shell to call a
    native exe

--- onnxruntime/python/tools/quantization/__init__.py
+++ onnxruntime/python/tools/quantization/__init__.py
@@ -6,7 +6,6 @@ from .calibrate import (  # noqa: F401
     create_calibrator,
 )
 from .matmul_weight4_quantizer import MatMulWeight4Quantizer  # noqa: F401
-from .q4dq_wrapper import Q4dqWrapper  # noqa: F401
 from .qdq_quantizer import QDQQuantizer  # noqa: F401
 from .quant_utils import QuantFormat, QuantType, write_calibration_table  # noqa: F401
 from .quantize import DynamicQuantConfig  # noqa: F401
