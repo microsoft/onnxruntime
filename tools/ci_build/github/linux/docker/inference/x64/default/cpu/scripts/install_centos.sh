528084cff2f6efce0b8a21dc93569dbac554b54b
Jian Chen <cjian@microsoft.com>
2023-08-16 17:23:22 -0700

    Update to gcc-toolset-12

--- tools/ci_build/github/linux/docker/inference/x64/default/cpu/scripts/install_centos.sh
+++ tools/ci_build/github/linux/docker/inference/x64/default/cpu/scripts/install_centos.sh
@@ -5,6 +5,6 @@ os_major_version=$(cat /etc/redhat-release | tr -dc '0-9.'|cut -d \. -f1)
 
 echo "installing for CentOS version : $os_major_version"
 yum install -y centos-release-scl-rh
-yum install -y which gdb redhat-lsb-core expat-devel tar unzip zlib-devel make bzip2 bzip2-devel java-11-openjdk-devel graphviz devtoolset-12-binutils devtoolset-12-gcc devtoolset-12-gcc-c++ devtoolset-12-gcc-gfortran
+yum install -y which gdb redhat-lsb-core expat-devel tar unzip zlib-devel make bzip2 bzip2-devel java-11-openjdk-devel graphviz gcc-toolset-12-binutils gcc-toolset-12-gcc gcc-toolset-12-gcc-c++ gcc-toolset-12-gcc-gfortran
 
 localedef -i en_US -f UTF-8 en_US.UTF-8
