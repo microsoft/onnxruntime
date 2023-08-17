1e1dca7d0dca5de8e6e8c7f7771ccd157dea3c5f
Hector Li <hecli@microsoft.com>
2023-08-16 21:58:20 -0700

    Add QDQ test for Where op

--- onnxruntime/test/providers/qnn/simple_op_htp_test.cc
+++ onnxruntime/test/providers/qnn/simple_op_htp_test.cc
@@ -439,16 +439,12 @@ TEST_F(QnnHTPBackendTests, BinaryOp_Sub4D) {
                               17, ExpectedEPNodeAssignment::All);
 }
 
-// TODO: Certain large input sizes cause the QNN graph to fail to finalize with error 1002 (QNN_COMMON_ERROR_MEM_ALLOC).
-// Enable when this is fixed.
 TEST_F(QnnHTPBackendTests, BinaryOp_Sub4D_LargeInputs) {
   RunQDQBinaryOpTest<uint8_t>("Sub", TestInputDef<float>({1, 3, 768, 1152}, false, -1.0f, 1.0f),
                               TestInputDef<float>({1, 3, 768, 1152}, false, -1.0f, 1.0f),
                               17, ExpectedEPNodeAssignment::All);
 }
 
-// TODO: Certain large input sizes cause the QNN graph to fail to finalize with error 1002 (QNN_COMMON_ERROR_MEM_ALLOC).
-// Enable when this is fixed.
 TEST_F(QnnHTPBackendTests, BinaryOp_Sub4D_Broadcast) {
   RunQDQBinaryOpTest<uint8_t>("Sub", TestInputDef<float>({1, 3, 768, 1152}, false, -1.0f, 1.0f),
                               TestInputDef<float>({3, 1, 1}, true, {1.0f, 0.5f, -0.3f}),
@@ -474,12 +470,39 @@ TEST_F(QnnHTPBackendTests, DISABLED_BinaryOp_Div4D_LargeInputs) {
                               17, ExpectedEPNodeAssignment::All);
 }
 
-// TODO: Certain large input sizes cause the QNN graph to fail to finalize with error 1002 (QNN_COMMON_ERROR_MEM_ALLOC).
-// Enable when this is fixed.
-// Fails accuracy when input0 has dims [1,3,768,768]
 TEST_F(QnnHTPBackendTests, BinaryOp_Div4D_Broadcast) {
   RunQDQBinaryOpTest<uint8_t>("Div", TestInputDef<float>({1, 3, 768, 1152}, false, -1.0f, 1.0f),
                               TestInputDef<float>({3, 1, 1}, true, {1.0f, 0.5f, -0.3f}),
+                              17, ExpectedEPNodeAssignment::All);
+}
+
+TEST_F(QnnHTPBackendTests, BinaryOp_Div4D_Broadcast1) {
+  RunQDQBinaryOpTest<uint8_t>("Div", TestInputDef<float>({1, 3136, 128}, false, -1.0f, 1.0f),
+                              TestInputDef<float>({1, 3136, 1}, false, -1.0f, 1.0f),
+                              17, ExpectedEPNodeAssignment::All);
+}
+
+TEST_F(QnnHTPBackendTests, BinaryOp_Div4D_Broadcast2) {
+  RunQDQBinaryOpTest<uint8_t>("Div", TestInputDef<float>({1, 56, 56, 512}, false, -1.0f, 1.0f),
+                              TestInputDef<float>({1}, false, {0.5f}),
+                              17, ExpectedEPNodeAssignment::All);
+}
+
+TEST_F(QnnHTPBackendTests, BinaryOp_Div4D_Broadcast3) {
+  RunQDQBinaryOpTest<uint8_t>("Div", TestInputDef<float>({1, 784, 1024}, false, -1.0f, 1.0f),
+                              TestInputDef<float>({1}, true, -1.0f, 1.0f),
+                              17, ExpectedEPNodeAssignment::All);
+}
+
+TEST_F(QnnHTPBackendTests, BinaryOp_Div4D_Broadcast4) {
+  RunQDQBinaryOpTest<uint8_t>("Div", TestInputDef<float>({1, 1024, 49}, false, -1.0f, 1.0f),
+                              TestInputDef<float>({1, 1, 49}, false, -1.0f, 1.0f),
+                              17, ExpectedEPNodeAssignment::All);
+}
+
+TEST_F(QnnHTPBackendTests, BinaryOp_Div4D_Broadcast5) {
+  RunQDQBinaryOpTest<uint8_t>("Div", TestInputDef<float>({1, 49, 4096}, false, -1.0f, 1.0f),
+                              TestInputDef<float>({1}, false, -1.0f, 1.0f),
                               17, ExpectedEPNodeAssignment::All);
 }
 
