d3b9bab0b0841f50f598e8b9b3d64660733d87cd
Ashwini Khade <askhade@microsoft.com>
2023-08-18 21:51:59 +0000

    apply lintrunner fixes

--- onnxruntime/test/testdata/training_api/ort_format/prepare_artifacts.py
+++ onnxruntime/test/testdata/training_api/ort_format/prepare_artifacts.py
@@ -13,7 +13,7 @@ from onnxruntime.training import artifacts
 
 class SimpleNet(nn.Module):
     def __init__(self, input_size, hidden_size, output_size):
-        super(SimpleNet, self).__init__()
+        super().__init__()
         self.fc1 = nn.Linear(input_size, hidden_size)
         self.relu = nn.ReLU()
         self.fc2 = nn.Linear(hidden_size, output_size)
