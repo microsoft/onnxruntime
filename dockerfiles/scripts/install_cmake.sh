16db4e0d776434868a8a76b29b94a7b7b6c5bc04
Changming Sun <chasun@microsoft.com>
2023-08-18 17:48:57 -0700

    Update cmake to 3.27.3

--- dockerfiles/scripts/install_cmake.sh
+++ dockerfiles/scripts/install_cmake.sh
@@ -3,5 +3,8 @@ mkdir -p /tmp/src
 cd /tmp/src
 
 echo "Installing cmake"
-curl https://github.com/Kitware/CMake/releases/download/v3.27.2/cmake-3.27.2-linux-`uname -m`.tar.gz -sSL --retry 5  -o /tmp/src/cmake-3.27.2-linux-`uname -m`.tar.gz
-tar -zxf /tmp/src/cmake-3.27.2-linux-`uname -m`.tar.gz --strip=1 -C /usr
+CPU_ARCH=`uname -m`
+CMAKE_VERSION='3.27.3'
+curl https://github.com/Kitware/CMake/releases/download/v$CMAKE_VERSION/cmake-$CMAKE_VERSION-linux-$CPU_ARCH.tar.gz -sSL --retry 5  -o /tmp/src/cmake.tar.gz
+tar -zxf /tmp/src/cmake.tar.gz --strip=1 -C /usr
+rm -f /tmp/src/cmake.tar.gz
