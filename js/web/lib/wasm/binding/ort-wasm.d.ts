00f4677e5c9926e8d68b95e39835224331391913
carzh <wolfivyaura@gmail.com>
2023-08-21 20:32:36 +0000

    added updated declaration file

--- js/web/lib/wasm/binding/ort-wasm.d.ts
+++ js/web/lib/wasm/binding/ort-wasm.d.ts
@@ -60,6 +60,34 @@ export interface OrtWasmModule extends EmscriptenModule {
   _OrtEndProfiling(sessionHandle: number): number;
   // #endregion
 
+  // #region ORT Training APIs
+  _OrtTrainingLoadCheckpoint?(dataOffset: number, dataLength: number): number;
+
+  _OrtTrainingReleaseCheckpoint?(checkpointHandle: number): void;
+
+  _OrtTrainingCreateSession?(sessionOptionsHandle: number, checkpointHandle: number, trainOffset: number, trainLength: number,
+    evalOffset: number, evalLength: number, optimizerOffset: number, optimizerLength: number)
+    : number;
+
+  _OrtTrainingLazyResetGrad?(trainingHandle: number): number;
+
+  _OrtTrainingRunTrainStep?(trainingHandle: number, inputsOffset: number, inputCount: number, outputsOffset: number,
+    outputCount: number, runOptionsHandle: number): number;
+
+  _OrtTrainingOptimizerStep?(trainingHandle: number, runOptionsHandle: number): number;
+
+  _OrtTrainingEvalStep?(trainingHandle: number, inputsOffset: number, inputCount: number, outputsOffset: number,
+    outputCount: number, runOptionsHandle: number): number;
+
+  _OrtTrainingGetParametersSize?(trainingHandle: number, paramSizeT: number, trainableOnly: boolean): number;
+
+  _OrtTrainingCopyParametersToBuffer?(trainingHandle: number, parametersBuffer: number, parameterCount: number, trainableOnly: boolean): number;
+
+  _OrtTrainingCopyParametersFromBuffer?(trainingHandle: number, parametersBuffer: number, parameterCount: number, trainableOnly: boolean): number;
+
+  _OrtTrainingReleaseSession?(trainingHandle: number): void;
+  // #endregion
+
   // #region config
   mainScriptUrlOrBlob?: string|Blob;
   // #endregion
