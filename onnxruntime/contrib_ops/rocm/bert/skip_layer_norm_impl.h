5b2a9bd1105a7294d6b465b4a53073fae00206d8
peixuanzuo <peixuanzuo@microsoft.com>
2023-08-22 10:01:32 +0000

    fix build

--- onnxruntime/contrib_ops/rocm/bert/skip_layer_norm_impl.h
+++ onnxruntime/contrib_ops/rocm/bert/skip_layer_norm_impl.h
@@ -25,7 +25,7 @@ Status LaunchSkipLayerNormKernel(
     int hidden_size,                // hidden size, it is the leading dimension (ld)
     int element_count,              // number of elements in input tensor
     bool skip_broadcasted,          // whether skip tensor is broadcasted
-    int skip_size,                  // size of skip tensor
+    int skip_size                   // size of skip tensor
 );
 
 }  // namespace rocm
