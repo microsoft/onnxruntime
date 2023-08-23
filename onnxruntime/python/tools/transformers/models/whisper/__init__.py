33bb7bea5ff21584e8ad692afb681013f5f2626c
kunal-vaishnavi <115581922+kunal-vaishnavi@users.noreply.github.com>
2023-08-22 18:14:44 -0700

    Add Whisper scripts (#17043)
    ### Description
    This PR adds benchmark scripts for Whisper. It is a follow-up to [this
    PR](https://github.com/microsoft/onnxruntime/pull/17020) that adds the
    LLaMA scripts.
    
    
    
    ### Motivation and Context
    This PR enables benchmarking Whisper across various configurations.

--- onnxruntime/python/tools/transformers/models/whisper/__init__.py
+++ onnxruntime/python/tools/transformers/models/whisper/__init__.py
@@ -2,7 +2,7 @@
 # Copyright (c) Microsoft Corporation.  All rights reserved.
 # Licensed under the MIT License.
 # --------------------------------------------------------------------------
-import os.path
+import os
 import sys
 
 sys.path.append(os.path.dirname(__file__))
