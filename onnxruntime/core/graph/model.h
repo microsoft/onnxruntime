7380c7aee6bcd6598719bd539dd403ec00138b56
Artem Shilkin <89970996+reshilkin@users.noreply.github.com>
2023-08-29 20:28:26 +0300

    Fix compilation with newer flatbuffers (#17164)
    In flatbuffers@v23.5.9 was broken forward declaration for
    FlatBufferBuilder. Trying to compile onnxruntime falls with the
    following error:
    ```
    flatbuffers/include/flatbuffers/flatbuffer_builder.h:1420:38: error: typedef redefinition with different types ('FlatBufferBuilderImpl<false>' vs 'flatbuffers::FlatBufferBuilder')
    typedef FlatBufferBuilderImpl<false> FlatBufferBuilder;
                                         ^
    onnx_runtime/include/onnxruntime/core/graph/graph.h:47:11: note: previous definition is here
        class FlatBufferBuilder;
    ```
    This PR removes these declarations and puts includes instead

--- onnxruntime/core/graph/model.h
+++ onnxruntime/core/graph/model.h
@@ -7,6 +7,9 @@
 #include <memory>
 #include <climits>
 #include <string>
+
+#include "flatbuffers/flatbuffers.h"
+
 #include "core/common/path.h"
 #include "core/graph/graph_viewer.h"
 #include "core/graph/ort_format_load_options.h"
@@ -14,12 +17,6 @@
 #if !defined(ORT_MINIMAL_BUILD)
 #include "core/graph/function_template.h"
 #endif
-
-namespace flatbuffers {
-class FlatBufferBuilder;
-template <typename T>
-struct Offset;
-}  // namespace flatbuffers
 
 namespace onnxruntime {
 
