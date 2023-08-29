70b7795d014905a81963e6b6c6236cec1bf4a09f
Randy Shuai <rashuai@microsoft.com>
2023-08-28 18:58:43 -0700

    release in destructor

--- onnxruntime/python/onnxruntime_pybind_state.cc
+++ onnxruntime/python/onnxruntime_pybind_state.cc
@@ -85,7 +85,6 @@ struct AsyncResource {
   std::vector<std::string> feed_names;
   std::vector<const char*> feed_names_raw;
 
-  std::vector<OrtValue> fetches;
   std::vector<OrtValue*> fetches_raw;
 
   std::vector<std::string> fetch_names;
@@ -103,10 +102,17 @@ struct AsyncResource {
   }
 
   void ReserveFetches(size_t sz) {
-    fetches.reserve(sz);
     fetches_raw.reserve(sz);
     fetch_names.reserve(sz);
     fetch_names_raw.reserve(sz);
+  }
+
+  ~AsyncResource() {
+    for (auto* fetch : fetches_raw) {
+      if (fetch) {
+        std::unique_ptr<OrtValue> fetch_ptr(fetch);
+      }
+    }
   }
 };
 
@@ -1790,8 +1796,7 @@ including arg name, arg type (contains both type and shape).)pbdoc")
              for (auto& output_name : output_names) {
                async_resource->fetch_names.push_back(output_name);
                async_resource->fetch_names_raw.push_back(async_resource->fetch_names.back().c_str());
-               async_resource->fetches.push_back({});
-               async_resource->fetches_raw.push_back(&async_resource->fetches.back());
+               async_resource->fetches_raw.push_back({});
              }
              const RunOptions* run_async_option = run_options ? run_options : &async_resource->default_run_option;
              common::Status status = sess->GetSessionHandle()->RunAsync(run_async_option,
