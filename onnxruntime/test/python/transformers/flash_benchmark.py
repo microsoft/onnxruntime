a54a7b982bb2388009ef8f5c2b6a8a5677b06757
aciddelgado <aciddelgado@microsoft.com>
2023-08-28 19:47:45 -0700

    flash benchmark script

--- onnxruntime/test/python/transformers/flash_benchmark.py
+++ onnxruntime/test/python/transformers/flash_benchmark.py
@@ -1,11 +1,11 @@
 # Install the newest triton version with
 # pip install "git+https://github.com/openai/triton.git#egg=triton&subdirectory=python"
-import pickle
 import math
+import time
+
 import torch
-import torch.nn as nn
-from einops import rearrange, repeat
 from onnx import TensorProto, helper
+
 from onnxruntime import InferenceSession, SessionOptions
 
 
@@ -97,9 +97,10 @@ def flash_attn_func(q, k, v, config, causal=False):
     }
     sess_options = SessionOptions()
     ort_session = InferenceSession(onnx_model_str, sess_options, providers=["CUDAExecutionProvider"])
-    ort_output = ort_session.run(None, ort_inputs)
-    output = torch.tensor(ort_output)
-    return output
+    start = time.time()
+    ort_session.run(None, ort_inputs)
+    end = time.time()
+    return end - start
 
 
 def flops(batch, seqlen, headdim, nheads, causal):
@@ -111,46 +112,12 @@ def efficiency(flop, time):
     return (flop / time / 10**12) if not math.isnan(time) else 0.0
 
 
-def attention_pytorch(qkv, dropout_p=0.0, causal=True):
-    """
-    Arguments:
-        qkv: (batch_size, seqlen, 3, nheads, head_dim)
-        dropout_p: float
-    Output:
-        output: (batch_size, seqlen, nheads, head_dim)
-    """
-    batch_size, seqlen, _, nheads, d = qkv.shape
-    q, k, v = qkv.unbind(dim=2)
-    q = rearrange(q, 'b t h d -> (b h) t d')
-    k = rearrange(k, 'b s h d -> (b h) d s')
-    softmax_scale = 1.0 / math.sqrt(d)
-    # Preallocate attn_weights for `baddbmm`
-    scores = torch.empty(batch_size * nheads, seqlen, seqlen, dtype=qkv.dtype, device=qkv.device)
-    scores = rearrange(torch.baddbmm(scores, q, k, beta=0, alpha=softmax_scale),
-                       '(b h) t s -> b h t s', h=nheads)
-    if causal:
-        # "triu_tril_cuda_template" not implemented for 'BFloat16'
-        # So we have to construct the mask in float
-        causal_mask = torch.triu(torch.full((seqlen, seqlen), -10000.0, device=scores.device), 1)
-        # TD [2022-09-30]: Adding is faster than masked_fill_ (idk why, just better kernel I guess)
-        scores = scores + causal_mask.to(dtype=scores.dtype)
-    attention = torch.softmax(scores, dim=-1)
-    attention_drop = F.dropout(attention, dropout_p)
-    output = torch.einsum('bhts,bshd->bthd', attention_drop , v)
-    return output.to(dtype=qkv.dtype)
-
-
-def time_fwd_bwd(func, *args, **kwargs):
-    time_f, time_b = benchmark_fwd_bwd(func, *args, **kwargs)
-    return time_f[1].mean, time_b[1].mean
-
-
 repeats = 30
-device = 'cuda'
+device = "cuda"
 dtype = torch.float16
 
 bs_seqlen_vals = [(32, 512), (16, 1024), (8, 2048), (4, 4096), (2, 8192), (1, 16384)]
-causal_vals = [False, True]
+causal_vals = [False]  # , True]
 headdim_vals = [64, 128]
 dim = 2048
 
@@ -166,15 +133,10 @@ for causal in causal_vals:
             nheads = dim // headdim
             config = Config(batch_size, seqlen, seqlen, nheads, headdim)
             qkv = torch.randn(batch_size, seqlen, 3, nheads, headdim, device=device, dtype=dtype)
-            time_f = time_fwd(
-                flash_attn_func, *qkv.unbind(dim=2), config, causal=causal, repeats=repeats, verbose=False
-            ) # TODO: make a time fwd function to time the performance
-
+            q, k, v = qkv.unbind(dim=2)
+            time_f = flash_attn_func(q, k, v, config, causal)
             print(f"### causal={causal}, headdim={headdim}, batch_size={batch_size}, seqlen={seqlen} ###")
-            speed = efficiency(
-                flops(batch_size, seqlen, headdim, nheads, causal),
-                time_f
-            )
+            speed = efficiency(flops(batch_size, seqlen, headdim, nheads, causal), time_f)
             print(f"Flash2 fwd: {speed:.2f} TFLOPs/s")
 
 
