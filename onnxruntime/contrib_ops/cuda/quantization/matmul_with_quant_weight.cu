a186539924439e00463fcb47dee389c590ae5214
Yufeng Li <liyufeng1987@gmail.com>
2023-08-30 05:27:35 +0000

    temp opt

--- onnxruntime/contrib_ops/cuda/quantization/matmul_with_quant_weight.cu
+++ onnxruntime/contrib_ops/cuda/quantization/matmul_with_quant_weight.cu
@@ -17,6 +17,60 @@ namespace onnxruntime {
 namespace contrib {
 namespace cuda {
 
+template <class Scalar>
+struct EightElementsDequant;
+
+template <>
+struct EightElementsDequant<float>{
+  float2 values[4];
+  inline __device__ void Dequant(uint32_t values_quant, float2 scale, float2 scale_x_zp){
+    values[0] = {float(values_quant & 0xF) * scale.x + scale_x_zp.x, float((values_quant>>4) & 0xF) * scale.y + scale_x_zp.y};
+    values[1] = {float((values_quant>>8) & 0xF) * scale.x + scale_x_zp.x, float((values_quant>>12) & 0xF) * scale.y + scale_x_zp.y};
+    values[1] = {float((values_quant>>16) & 0xF) * scale.x + scale_x_zp.x, float((values_quant>>20) & 0xF) * scale.y + scale_x_zp.y};
+    values[1] = {float((values_quant>>24) & 0xF) * scale.x + scale_x_zp.x, float((values_quant>>28) & 0xF) * scale.y + scale_x_zp.y};
+  }
+};
+
+template <>
+struct EightElementsDequant<half>{
+  half2 values[4];
+  inline __device__ void Dequant(uint32_t values_quant, half2 scales, half2 scale_x_zp){
+    values[0] = __hfma2(__halves2half2(__uint2half_rn(values_quant & 0xF), __uint2half_rn((values_quant>>4) & 0xF)), scales, scale_x_zp);
+    values[1] = __hfma2(__halves2half2(__uint2half_rn((values_quant>>8) & 0xF), __uint2half_rn((values_quant>>12) & 0xF)), scales, scale_x_zp);
+    values[2] = __hfma2(__halves2half2(__uint2half_rn((values_quant>>16) & 0xF), __uint2half_rn((values_quant>>20) & 0xF)), scales, scale_x_zp);
+    values[3] = __hfma2(__halves2half2(__uint2half_rn((values_quant>>24) & 0xF), __uint2half_rn((values_quant>>28) & 0xF)), scales, scale_x_zp);
+  }
+};
+
+template<class Scalar>
+struct Scalar2;
+
+template<>
+struct Scalar2<float>{
+
+  using type = float2;
+  inline __device__ static float2 MakeScalar2(float f){
+    return float2{f, f};
+  }
+
+    inline __device__ static float2 MulAdd(float2 a, float2 b, float2 c){
+    return {a.x*b.x + c.x, a.y*b.y+c.y};
+  }
+};
+
+template<>
+struct Scalar2<half>{
+  using type = half2;
+  inline __device__ static half2 MakeScalar2(half h){
+    return __halves2half2(h, h);
+  }
+
+  inline __device__ static half2 MulAdd(half2 a, half2 b, half2 c){
+    return __hfma2(a, b, c);
+  }
+
+};
+
 template <class T, int block_size>
 __global__ void MatMul4BitsWeightKernel(
     T* output,
@@ -42,11 +96,11 @@ __global__ void MatMul4BitsWeightKernel(
   a_data += m_id * k + k_id;
 
   for (int i = threadIdx.x; i < block_size; i += blockDim.x) {
-    if (i + k_id < k) {
+    //if (i + k_id < k) {
       a_data_vec[i] = a_data[i];
-    } else {
-      a_data_vec[i] = static_cast<T>(0.f);
-    }
+    //} else {
+    //  a_data_vec[i] = static_cast<T>(0.f);
+    //}
   }
   __syncthreads();
   // dequantize a blob
@@ -54,26 +108,24 @@ __global__ void MatMul4BitsWeightKernel(
   const uint8_t* quant_data_cur = b_data_quant + (n_id * gridDim.x + k_block_id) * block_size / 2;
   T scale = *(scales_data + n_id * gridDim.x + k_block_id);
   T zero_point = static_cast<T>(zero_points ? float(zero_points[n_id * gridDim.x + k_block_id]) : 8.f);
-  for (int kk = 0; kk < block_size; kk += 2) {
-    uint8_t value = *(quant_data_cur++);
-    T x0 = static_cast<T>(float(value & 0xF));
-    T x1 = static_cast<T>(float(value >> 4));
-    if (kk + k_id < k) {
-      x0 = scale * (x0 - zero_point);
-      weight[kk] = x0;
-    }
-    if (kk + 1 + k_id < k) {
-      x1 = scale * (x1 - zero_point);
-      weight[kk + 1] = x1;
-    }
+  T scale_zero_point = -scale * zero_point;
+  typename Scalar2<T>::type scale_zero_point_pair = Scalar2<T>::MakeScalar2(scale_zero_point);
+  typename Scalar2<T>::type scale_pair = Scalar2<T>::MakeScalar2(scale);
+
+  typename Scalar2<T>::type res_pair;
+  const typename Scalar2<T>::type* a_data_vec_2 = reinterpret_cast<const typename Scalar2<T>::type*>(a_data_vec);
+  for (int kk = 0; kk < block_size; kk += 8) {
+    uint32_t value = *(reinterpret_cast<const uint32_t*>(quant_data_cur+kk));
+    EightElementsDequant<T> eight_elements;
+    eight_elements.Dequant(value, scale_pair, scale_zero_point_pair);
+
+    res_pair = Scalar2<T>::MulAdd(eight_elements.values[0], a_data_vec_2[kk/2], res_pair);
+    res_pair = Scalar2<T>::MulAdd(eight_elements.values[1], a_data_vec_2[kk/2 + 1], res_pair);
+    res_pair = Scalar2<T>::MulAdd(eight_elements.values[2], a_data_vec_2[kk/2 + 1], res_pair);
+    res_pair = Scalar2<T>::MulAdd(eight_elements.values[3], a_data_vec_2[kk/2 + 1], res_pair);
   }
 
-  T res = static_cast<T>(0.f);
-  for (int kk = 0; kk < block_size; kk++) {
-    res += weight[kk] * a_data_vec[kk];
-  }
-
-  atomicAdd(output + m_id * n + n_id, res);
+  atomicAdd(output + m_id * n + n_id, res_pair.x + res_pair.y);
 }
 
 template <class T>
