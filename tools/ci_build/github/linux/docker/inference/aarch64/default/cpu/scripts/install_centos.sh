f5e185c84ed5e34637c65d77464e5d3f81df1256
Changming Sun <chasun@microsoft.com>
2023-08-30 10:03:01 -0700

    update

--- tools/ci_build/github/linux/docker/inference/aarch64/default/cpu/scripts/install_centos.sh
+++ tools/ci_build/github/linux/docker/inference/aarch64/default/cpu/scripts/install_centos.sh
@@ -4,10 +4,5 @@ set -e -x
 os_major_version=$(cat /etc/redhat-release | tr -dc '0-9.'|cut -d \. -f1)
 
 echo "installing for CentOS version : $os_major_version"
-dnf install -y glibc-langpack-\* glibc-locale-source
-export LANG=en_US.UTF-8
-export LC_ALL=en_US.UTF-8
+dnf install -y glibc-langpack-\* glibc-locale-source which gdb redhat-lsb-core expat-devel tar unzip zlib-devel make bzip2 bzip2-devel java-11-openjdk-devel graphviz gcc-toolset-12-binutils gcc-toolset-12-gcc gcc-toolset-12-gcc-c++ gcc-toolset-12-gcc-gfortran
 locale
-dnf install -y which gdb redhat-lsb-core expat-devel tar unzip zlib-devel make bzip2 bzip2-devel java-11-openjdk-devel graphviz gcc-toolset-12-binutils gcc-toolset-12-gcc gcc-toolset-12-gcc-c++ gcc-toolset-12-gcc-gfortran
-
-localedef -i en_US -f UTF-8 en_US.UTF-8
