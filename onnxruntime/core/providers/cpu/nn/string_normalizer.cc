f367988256d54aed7ec05c1298e2a5b39c22d620
rachguo <rachguo@rachguos-Mini.attlocal.net>
2023-09-05 19:22:52 -0700

    hardcode only for iphone device

--- onnxruntime/core/providers/cpu/nn/string_normalizer.cc
+++ onnxruntime/core/providers/cpu/nn/string_normalizer.cc
@@ -202,11 +202,14 @@ class Utf8Converter {
 #endif
 
 #if defined(__APPLE__)
+#if TARGET_OS_IPHONE
 const std::string default_locale("en-US.UTF-8");
 #else
-const std::string default_locale("en_US.UTF-8"); // All non-MS and not Apple
+const std::string default_locale("en_US.UTF-8");  // Other kinds of Apple Platforms including MacOS, iphone simulator,etc
 #endif
-
+#else
+const std::string default_locale("en_US.UTF-8");  // All non-MS and not Apple
+#endif
 
 #endif  // _MSC_VER
 
