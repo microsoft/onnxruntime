6402f1aadea135c3bef214fb6fde530e1806e38e
MaanavD <maanavdalal@microsoft.com>
2023-09-07 11:23:20 -0700

    Attempting relative paths again.

--- svelte.config.js
+++ svelte.config.js
@@ -13,7 +13,7 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
 		paths: {
-			relative: true
+			base: process.env.NODE_ENV === "production" ? "/onnxruntime" : "",
         },
 	}
 };
