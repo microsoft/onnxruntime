3af1c58bab93d0214376164ff822c82c9b0d3137
MaanavD <maanavdalal@microsoft.com>
2023-09-07 11:01:05 -0700

    Attempted to add automated relative routes.

---
+++ fix_routes.sh
@@ -0,0 +1,4 @@
+node node_modules/sveltejs-adapter-ipfs/ipfs_fixes/relativize_pages.cjs
+node node_modules/sveltejs-adapter-ipfs/ipfs_fixes/inject_base.cjs
+node node_modules/sveltejs-adapter-ipfs/ipfs_fixes/inject_base_in_paths_file.cjs
+node node_modules/sveltejs-adapter-ipfs/ipfs_fixes/inject_base_in_singletons_file.cjs
