5b5e0f96d3f6ff97279a9f5e4ef6a265acef8228
Yi Zhang <zhanyi@microsoft.com>
2023-09-07 10:51:54 +0800

    install dotnet in ubuntu

--- tools/ci_build/github/linux/docker/scripts/manylinux/install_deps.sh
+++ tools/ci_build/github/linux/docker/scripts/manylinux/install_deps.sh
@@ -18,8 +18,16 @@ if [ -f /etc/redhat-release ]; then
     dnf update --refresh \
     && dnf install -y dotnet-sdk-6.0
 elif [ -f /etc/os-release ]; then
-    apt-get update \
-    && apt-get install -y dotnet-sdk-6.0
+  # Get Ubuntu version
+  declare repo_version=$(if command -v lsb_release &> /dev/null; then lsb_release -r -s; else grep -oP '(?<=^VERSION_ID=).+' /etc/os-release | tr -d '"'; fi)
+  # Download Microsoft signing key and repository
+  wget https://packages.microsoft.com/config/ubuntu/$repo_version/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+  # Install Microsoft signing key and repository
+  dpkg -i packages-microsoft-prod.deb
+  # Clean up
+  rm packages-microsoft-prod.deb
+  # Update packages
+  apt update
 else
   echo "Unsupported OS"
   exit 1
