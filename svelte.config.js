6c4fb4ceda08d63bb8a47c18814c86f6f69fd54c
MaanavD <maanavdalal@microsoft.com>
2023-09-07 15:35:08 -0700

    Attempted fix for routing pt2. TEMPORARY. WILL NOT WORK IN PROD.

--- svelte.config.js
+++ svelte.config.js
@@ -12,7 +12,9 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
-		base: process.env.NODE_ENV === 'production' ? '/onnxruntime' : '',
+		paths:{
+			base: process.env.NODE_ENV === 'production' ? '/onnxruntime' : '',
+		}
 	}
 };
 
