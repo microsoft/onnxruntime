8e6a478e42491652456dbcda3c25ee902be1ca7c
yf711 <yifanl@microsoft.com>
2023-09-08 10:20:40 -0700

    test

--- onnxruntime/core/optimizer/graph_transformer_utils.cc
+++ onnxruntime/core/optimizer/graph_transformer_utils.cc
@@ -183,8 +183,12 @@ InlinedVector<std::unique_ptr<GraphTransformer>> GenerateTransformers(
     const IExecutionProvider& cpu_execution_provider, /*required by constant folding*/
     const InlinedHashSet<std::string>& rules_and_transformers_to_disable) {
   InlinedVector<std::unique_ptr<GraphTransformer>> transformers;
+#ifdef ENABLE_TENSORRT
+  const bool disable_quant_qdq = true;
+#else 
   const bool disable_quant_qdq =
       session_options.config_options.GetConfigOrDefault(kOrtSessionOptionsDisableQuantQDQ, "0") == "1";
+#endif
 #ifndef DISABLE_CONTRIB_OPS
   const InlinedHashSet<std::string_view> cpu_ep = {onnxruntime::kCpuExecutionProvider};
 #endif
@@ -201,12 +205,10 @@ InlinedVector<std::unique_ptr<GraphTransformer>> GenerateTransformers(
 
       // no filtering on execution provider for L1 optimizations as they only use official ONNX operators
 
-#ifndef ENABLE_TENSORRT
       if (session_options.config_options.GetConfigOrDefault(kOrtSessionOptionsDisableDoubleQDQRemover, "0") == "0") {
         // We need to remove the duplicated QDQ Pairs before all other GraphTransformation.
         transformers.emplace_back(std::make_unique<DoubleQDQPairsRemover>());
       }
-#endif
       
       // Put ConstantSharing before CommonSubexpressionElimination by intention as it can create more opportunities for
       // CSE. For example, if A and B nodes both do Add operation with a same value but different initializers, by
