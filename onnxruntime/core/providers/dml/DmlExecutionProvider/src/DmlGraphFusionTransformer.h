30e86d6e7a495d4f6775e743f058ee6d9f36c80a
Patrice Vignola <vignola.patrice@gmail.com>
2023-09-11 15:30:26 -0700

    [DML EP] Add subgraph fusion support

--- onnxruntime/core/providers/dml/DmlExecutionProvider/src/DmlGraphFusionTransformer.h
+++ onnxruntime/core/providers/dml/DmlExecutionProvider/src/DmlGraphFusionTransformer.h
@@ -8,26 +8,34 @@
 
 namespace Dml
 {
-	class ExecutionProviderImpl;
+    class ExecutionProviderImpl;
 
-	class DmlGraphFusionTransformer : public onnxruntime::GraphTransformer
-	{
-	public:
-		DmlGraphFusionTransformer(
-			const std::string& name,
-			const onnxruntime::IExecutionProvider* provider
-		);
+    class DmlGraphFusionTransformer : public onnxruntime::GraphTransformer
+    {
+    public:
+        DmlGraphFusionTransformer(
+            const std::string& name,
+            const onnxruntime::IExecutionProvider* provider
+        );
 
-	public:
-		inline const static char* const DML_GRAPH_FUSION_NODE_NAME_PREFIX = "DmlFusedNode_";
-		inline const static char* const DML_GRAPH_FUSION_NODE_DOMAIN = "DmlFusedNodeDomain";
+    public:
+        inline const static char* const DML_GRAPH_FUSION_NODE_NAME_PREFIX = "DmlFusedNode_";
+        inline const static char* const DML_GRAPH_FUSION_NODE_DOMAIN = "DmlFusedNodeDomain";
 
-	private:
-		onnxruntime::common::Status ApplyImpl(onnxruntime::Graph& graph, 
-											  bool& modified, 
-											  int graph_level, 
-											  const onnxruntime::logging::Logger& logger) const final;
-	private:
-		const ExecutionProviderImpl* m_providerImpl = nullptr;
-	};
+    private:
+        onnxruntime::common::Status ApplyImpl(onnxruntime::Graph& graph,
+                                              bool& modified,
+                                              int graph_level,
+                                              const onnxruntime::logging::Logger& logger) const final;
+
+        onnxruntime::common::Status DmlGraphFusionTransformer::ApplyImplHelper(
+            onnxruntime::Graph& graph,
+            bool& modified,
+            int graph_level,
+            const onnxruntime::logging::Logger& logger,
+            const std::unordered_map<std::string, const onnxruntime::NodeArg*>& implicitInputDefs) const;
+
+    private:
+        const ExecutionProviderImpl* m_providerImpl = nullptr;
+    };
 }
