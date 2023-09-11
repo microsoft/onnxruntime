d7e6e01524fdd00adb7a1eb5caddee13d0f91536
Abhishek Jindal <abjindal@microsoft.com>
2023-09-11 13:39:29 -0700

    fix graph and input data types

--- onnxruntime/test/testdata/transform/fusion/layer_norm_weight_test_activation.py
+++ onnxruntime/test/testdata/transform/fusion/layer_norm_weight_test_activation.py
@@ -5,21 +5,20 @@ from onnx import OperatorSetIdProto, TensorProto, helper
 def GenerateModel(model_name, has_casts=False):  # noqa: N802
     nodes = [  # LayerNorm subgraph
         helper.make_node("Identity", ["A"], ["A_id_out"], "id"),
-        helper.make_node("ReduceMean", ["cast_A_id_out"], ["rd_out"], "reduce1", axes=[-1], keepdims=1),
-        helper.make_node("Sub", ["cast_A_id_out", "rd_out"], ["sub_out"], "sub"),
+        helper.make_node("ReduceMean", ["A_id_out"], ["rd_out"], "reduce1", axes=[-1], keepdims=1),
+        helper.make_node("Sub", ["A_id_out", "rd_out"], ["sub_out"], "sub"),
         helper.make_node("Pow", ["cast_sub_out" if has_casts else "sub_out", "pow_in_2"], ["pow_out"], "pow"),
         helper.make_node("ReduceMean", ["pow_out"], ["rd2_out"], "reduce2", axes=[-1], keepdims=1),
-        helper.make_node("Add", ["rd2_out", "const_e12"], ["add1_out"], "add1"),
+        helper.make_node("Add", ["rd2_out", "const_e12_f32"], ["add1_out"], "add1"),
         helper.make_node("Sqrt", ["add1_out"], ["sqrt_out"], "sqrt"),
         helper.make_node("Div", ["cast_sub_out" if has_casts else "sub_out", "sqrt_out"], ["div_out"], "div"),
         helper.make_node("Mul", ["gamma", "cast_div_out" if has_casts else "div_out"], ["mul_out"], "mul"),
-        helper.make_node("Add", ["mul_out", "const_e12"], ["C"], "add2"),
+        helper.make_node("Add", ["mul_out", "const_e12_f16"], ["C"], "add2"),
     ]
 
     if has_casts:
         nodes.extend(
             [
-                helper.make_node("Cast", ["A_id_out"], ["cast_A_id_out"], "cast_id", to=1),
                 helper.make_node("Cast", ["sub_out"], ["cast_sub_out"], "cast_sub", to=1),
                 helper.make_node("Cast", ["div_out"], ["cast_div_out"], "cast_2", to=10),
             ]
@@ -27,7 +26,8 @@ def GenerateModel(model_name, has_casts=False):  # noqa: N802
 
     initializers = [  # initializers
         helper.make_tensor("pow_in_2", TensorProto.FLOAT, [], [2]),
-        helper.make_tensor("const_e12", TensorProto.FLOAT16, [], [1e-12]),
+        helper.make_tensor("const_e12_f32", TensorProto.FLOAT, [], [1e-12]),
+        helper.make_tensor("const_e12_f16", TensorProto.FLOAT16, [], [1e-12]),
         helper.make_tensor(
             "gamma",
             TensorProto.FLOAT16 if has_casts else TensorProto.FLOAT,
