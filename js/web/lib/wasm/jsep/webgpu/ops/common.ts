fe5b70c0928bacfa53c5596a8425d6d869306b2a
Arthur Islamov <arthur@islamov.ai>
2023-09-14 00:11:17 +0400

    [js/webgpu] FP16 extension registration (#17493)
    ### Description
    First small change to support FP16
    
    ---------
    
    Co-authored-by: Yulong Wang <7679871+fs-eire@users.noreply.github.com>

--- js/web/lib/wasm/jsep/webgpu/ops/common.ts
+++ js/web/lib/wasm/jsep/webgpu/ops/common.ts
@@ -192,11 +192,14 @@ export interface IndicesHelper {
 }
 
 const getWgslMappedType = (type: number, components: 1|2|3|4): string|[string, string] => {
+  if (components === 3) {
+    throw new Error('vec3 has same alignment as vec4, use vec4 instead');
+  }
+
   // return type is [ storage type, runtime type ] or a single string for both
   switch (type) {
-    // TODO: enable after "shader-f16" WSGL extension release
-    // case DataType.float16:
-    //   return components > 1 ? `vec${components}<f16>` : 'f16';
+    case DataType.float16:
+      return components > 1 ? `vec${components}<f16>` : 'f16';
     case DataType.float:
       return components > 1 ? `vec${components}<f32>` : 'f32';
     case DataType.int32:
