4ebd6fb79b5bd6b038077753308ae1ad62486bb2
Justin Chu <justinchuby@users.noreply.github.com>
2023-09-14 12:17:46 -0700

    Remove reference to internals in torch.onnx
    Fixes https://github.com/microsoft/onnxruntime/issues/11901

--- onnxruntime/test/python/test_pytorch_export_contrib_ops.py
+++ onnxruntime/test/python/test_pytorch_export_contrib_ops.py
@@ -49,14 +49,12 @@ def ort_test_with_input(ort_sess, input, output, rtol, atol):
 # These set of tests verify ONNX model export and compares outputs between
 # PyTorch and ORT.
 class ONNXExporterTest(unittest.TestCase):
-    from torch.onnx.symbolic_helper import _export_onnx_opset_version
-
-    opset_version = _export_onnx_opset_version
-    keep_initializers_as_inputs = True  # For IR version 3 type export.
-
     def setUp(self):
         torch.manual_seed(0)
         pytorch_export_contrib_ops.register()
+        opset_version = 17
+        # For IR version 3 type export.
+        keep_initializers_as_inputs = True
 
     def run_test(
         self,
