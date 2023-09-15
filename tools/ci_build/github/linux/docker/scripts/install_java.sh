5afc78c8e3cfee31f98d4a006dd987beafc6f884
Yi Zhang <zhanyi@microsoft.com>
2023-09-15 09:12:40 +0800

    update

--- tools/ci_build/github/linux/docker/scripts/install_java.sh
+++ tools/ci_build/github/linux/docker/scripts/install_java.sh
@@ -2,8 +2,7 @@
 set -e -x
 
 if [ -f /etc/redhat-release ]; then
-    dnf update --refresh -y \
-    && dnf install -y java-11-openjdk-devel \
+    dnf install -y java-11-openjdk-devel \
     && dnf clean dbcache
 elif [ -f /etc/os-release ]; then
     apt-get update && apt-get install -y openjdk-11-jdk
