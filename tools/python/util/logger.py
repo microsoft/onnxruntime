caae13cc389de12725a8a5aabfa15715910a71cf
Bowen Bao <bowbao@microsoft.com>
2023-09-19 10:42:27 -0700

    Avoid `get_logger` overriding root logger level (#17569)
    ### Description
    Instead, set level to DEBUG for the logger returned.
    
    
    
    ### Motivation and Context
    <!-- - Why is this change required? What problem does it solve?
    - If it fixes an open issue, please link to the issue here. -->
    
    Otherwise, this function call overrides root logger level setting, which
    affects logging facility of other python packages.

--- tools/python/util/logger.py
+++ tools/python/util/logger.py
@@ -5,6 +5,7 @@ import logging
 
 
 def get_logger(name):
-    logging.basicConfig(format="%(asctime)s %(name)s [%(levelname)s] - %(message)s", level=logging.DEBUG)
-
-    return logging.getLogger(name)
+    logging.basicConfig(format="%(asctime)s %(name)s [%(levelname)s] - %(message)s")
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+    return logger
