8df5f4e0df1f3b9ceeb0f1f2561b09727ace9b37
Tianlei Wu <tlwu@microsoft.com>
2023-09-18 23:49:37 -0700

    build script for windows and cuda 11.8

---
+++ build_debug.cmd
@@ -0,0 +1,9 @@
+IF {%VCToolsVersion%}=={} call "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" amd64
+
+set CudaToolkitDir=C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v11.8
+
+.\build.bat --config Debug --build_dir .\build --build_shared_lib --parallel --build_wheel ^
+            --cmake_generator "Visual Studio 17 2022" ^
+            --use_cuda --cuda_version 11.8 --cuda_home "C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v11.8" ^
+            --cudnn_home C:\nvidia\cudnn-windows-x86_64-8.9.4.25_cuda11-archive ^
+            --cmake_extra_defines CMAKE_CUDA_ARCHITECTURES=89 --skip_tests
---
+++ build_trt.cmd
@@ -0,0 +1,12 @@
+IF {%VCToolsVersion%}=={} call "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" amd64
+
+set CudaToolkitDir=C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v11.8
+
+.\build.bat --config Release --build_dir .\build --build_shared_lib --parallel --build_wheel ^
+            --cmake_generator "Visual Studio 17 2022" ^
+            --use_cuda --cuda_version 11.8 --cuda_home "C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v11.8" ^
+            --cudnn_home C:\nvidia\cudnn-windows-x86_64-8.9.4.25_cuda11-archive ^
+            --use_tensorrt --tensorrt_home "C:\nvidia\TensorRT-8.6.1.6.Windows10.x86_64.cuda-11.8\TensorRT-8.6.1.6" ^
+            --cmake_extra_defines CMAKE_CUDA_ARCHITECTURES=89 ^
+            --cmake_extra_defines onnxruntime_BUILD_UNIT_TESTS=OFF  ^
+            --skip_tests
