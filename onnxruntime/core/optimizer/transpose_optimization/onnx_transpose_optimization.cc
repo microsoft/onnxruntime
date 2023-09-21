1c961d8a4756bde52e450fea0413e4770b25d754
Scott McKay <Scott.McKay@microsoft.com>
2023-09-22 07:52:10 +1000

    Fix bad paste

--- onnxruntime/core/optimizer/transpose_optimization/onnx_transpose_optimization.cc
+++ onnxruntime/core/optimizer/transpose_optimization/onnx_transpose_optimization.cc
@@ -690,7 +690,7 @@ static void TransposeInputImpl(api::GraphRef& graph,
                      const auto& input_idxs = entry.second;
                      // id matches and the entry is for input 0 of the DQ node
                      return id == inp_node->Id() &&
-                            std::find(input_idxs.begin(), input_idxs.end(), size_t(0) 0) != input_idxs.end();
+                            std::find(input_idxs.begin(), input_idxs.end(), size_t(0)) != input_idxs.end();
                    }) != nodes_using_updated_shared_initializer->end()) {
     // set things up so we can look past the DQ node to the Transpose that was inserted in front of the reshaped
     // constant initializer that was shared with this node.
