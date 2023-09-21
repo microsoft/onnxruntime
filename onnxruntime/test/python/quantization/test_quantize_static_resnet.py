2be30345f48c5e6b9fa26af4e1eb5beba102f84a
Xavier Dupre <xadupre@microsoft.com>
2023-09-21 13:50:48 +0200

    move to python code

--- onnxruntime/test/python/quantization/test_quantize_static_resnet.py
+++ onnxruntime/test/python/quantization/test_quantize_static_resnet.py
@@ -4,15 +4,18 @@
 # Licensed under the MIT License. See License.txt in the project root for
 # license information.
 # --------------------------------------------------------------------------
-import unittest
 import os
 import random
 import tempfile
+import unittest
+
 import numpy as np
+from numpy.testing import assert_allclose
+
 from onnxruntime import InferenceSession
-from onnxruntime.quantization.calibrate import CalibrationDataReader
 from onnxruntime.quantization import QuantFormat, QuantType, quantize_static
-from onnxruntime.quantization.calibrate import CalibrationMethod
+from onnxruntime.quantization.calibrate import CalibrationDataReader, CalibrationMethod
+from resnet_code import create_model
 
 
 class FakeResnetCalibrationDataReader(CalibrationDataReader):
@@ -32,11 +35,6 @@ class FakeResnetCalibrationDataReader(CalibrationDataReader):
 
 class TestStaticQuantizationResNet(unittest.TestCase):
     def test_quantize_static_resnet(self):
-        folder = os.path.join(os.path.dirname(__file__), "..", "..", "testdata")
-        model = os.path.join(folder, "resnet_first_nodes.onnx")
-        if not os.path.exists(model):
-            raise FileNotFoundError(f"Unable to find {model!r} in testdata.")
-
         kwargs = {
             "activation_type": QuantType.QUInt8,
             "calibrate_method": CalibrationMethod.Percentile,
@@ -51,15 +49,21 @@ class TestStaticQuantizationResNet(unittest.TestCase):
             "nodes_to_exclude": None,
             "nodes_to_quantize": None,
             "op_types_to_quantize": None,
-            "per_channel": True,
+            # if per_channel is True, it raises an exception.
+            # QLinearConv : zero point of per-channel filter must be same
+            "per_channel": False,
             "quant_format": QuantFormat.QDQ,
             "reduce_range": False,
             "weight_type": QuantType.QUInt8,
         }
 
         dataloader = FakeResnetCalibrationDataReader(16)
+        proto = create_model()
 
-        with tempfile.TemporaryDirectory(prefix="test_calibration.") as temp:
+        with tempfile.TemporaryDirectory() as temp:
+            model = os.path.join(temp, "resnet_first_nodes.onnx")
+            with open(model, "wb") as f:
+                f.write(proto.SerializeToString())
             qdq_file = os.path.join(temp, "preprocessed-small-qdq.onnx")
             quantize_static(
                 model_input=model,
@@ -74,7 +78,19 @@ class TestStaticQuantizationResNet(unittest.TestCase):
             size = np.prod(shape)
             dummy = (np.arange(size) / float(size)).astype(np.float32).reshape(shape)
             got = sess.run(None, {"input": dummy})
-            print(got[0])
+            self.assertEqual(got[0].shape, (1, 64, 8, 8))
+            self.assertEqual(got[0].dtype, np.float32)
+            expected = np.array(
+                [
+                    [[1.4244736433029175, 1.256888508796692], [1.3406810760498047, 1.2149922847747803]],
+                    [[0.8798219561576843, 1.131199598312378], [1.131199598312378, 1.173095941543579]],
+                    [[0.0, 0.0], [0.0, 0.0]],
+                    [[0.0, 0.0], [1.2149922847747803, 1.0474071502685547]],
+                ],
+                dtype=np.float32,
+            )
+            print(got[0][0, :4, :2, :2].tolist())
+            assert_allclose(expected, got[0][0, :4, :2, :2], atol=1e-2)
 
 
 if __name__ == "__main__":
