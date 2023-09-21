f2ff72310ee7b15ddcc363236de37a344deab305
Xavier Dupre <xadupre@microsoft.com>
2023-09-21 15:38:17 +0200

    remove shebang

--- onnxruntime/test/python/quantization/test_quantize_static_resnet.py
+++ onnxruntime/test/python/quantization/test_quantize_static_resnet.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 # -------------------------------------------------------------------------
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for
