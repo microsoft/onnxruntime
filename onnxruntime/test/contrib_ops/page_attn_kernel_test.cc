947d0d3ad8ff8b35acd1271fe1edb4b00f2154f9
Lei Zhang <zhang.huanning@hotmail.com>
2023-09-29 00:07:44 +0000

    build ok. meet link issue. need another way for cuda kernel only test.

--- onnxruntime/test/contrib_ops/page_attn_kernel_test.cc
+++ onnxruntime/test/contrib_ops/page_attn_kernel_test.cc
@@ -2,6 +2,8 @@
 // Licensed under the MIT License.
 // #ifdef USE_CUDA
 
+#include <memory>
+
 #include "test/util/include/default_providers.h"
 #include "cuda_runtime_api.h"
 
@@ -13,7 +15,7 @@
 #include "test/providers/provider_test_utils.h"
 #include "test/util/include/scoped_env_vars.h"
 
-#include "onnxruntime/contrib_ops/cuda/bert/paged_attention_impl.h"
+#include "contrib_ops/cuda/bert/paged_attention_impl.h"
 
 namespace onnxruntime {
 
@@ -42,10 +44,10 @@ void RefQuantizeReshapeAndCache(
         auto max_abs = [](MLFloat16 a, MLFloat16 b) {
           float abs_a = fabsf((float)a);
           float abs_b = fabsf((float)b);
-          return MLFloat16 { fmaxf(abs_a, abs_b); };
+          return MLFloat16(fmaxf(abs_a, abs_b));
         };
-        float max_abs_k = (float)std::accumulate(k, k + kv_quant_chunk_size, MLFloat16{0.0f}, max_abs);
-        float max_abs_v = (float)std::accumulate(v, v + kv_quant_chunk_size, MLFloat16{0.0f}, max_abs);
+        float max_abs_k = (float)std::accumulate(k, k + kv_quant_chunk_size, MLFloat16(0.0f), max_abs);
+        float max_abs_v = (float)std::accumulate(v, v + kv_quant_chunk_size, MLFloat16(0.0f), max_abs);
         float inv_unit_scale_k = max_abs_k ? (127.0f / max_abs_k) : 0.0f;
         float inv_unit_scale_v = max_abs_v ? (127.0f / max_abs_v) : 0.0f;
 
@@ -58,8 +60,8 @@ void RefQuantizeReshapeAndCache(
           int f = i + j;
           auto* dst_k = base_dst_k + ((f / x) * (block_size * x) + tib * x + (f % x));
           auto* dst_v = base_dst_v + (f * block_size + tib);
-          *dst_k = (int8_t)std::max(-128, std::min(127, std::nearbyintf((float)k[j] * inv_unit_scale_k)));
-          *dst_v = (int8_t)std::max(-128, std::min(127, std::nearbyintf((float)v[j] * inv_unit_scale_v)));
+          *dst_k = (int8_t)std::max(-128, std::min(127, (int)std::nearbyintf((float)k[j] * inv_unit_scale_k)));
+          *dst_v = (int8_t)std::max(-128, std::min(127, (int)std::nearbyintf((float)v[j] * inv_unit_scale_v)));
         }
 
         ScaleT* param_k = kv_params_cache.data() + ((b * 2 * num_heads + h) * (head_size / kv_quant_chunk_size * block_size) + (i / kv_quant_chunk_size) * block_size + tib);
@@ -71,7 +73,6 @@ void RefQuantizeReshapeAndCache(
   }
 }
 
-
 template <typename T>
 std::unique_ptr<T, std::function<void(T*)>> move_to_cuda(const std::vector<T>& cpu_vec) {
   void* dev_ptr = nullptr;
@@ -80,9 +81,9 @@ std::unique_ptr<T, std::function<void(T*)>> move_to_cuda(const std::vector<T>& c
   return std::unique_ptr<T, std::function<void(T*)>>(
       (T*)dev_ptr,
       [](T* p) {
-        if (dev_ptr) {
-          cudaFree(dev_ptr);
-          dev_ptr = nullptr;
+        if (p) {
+          cudaFree(p);
+          p = nullptr;
         }
       });
 }
@@ -106,56 +107,52 @@ void CudaQuantizeReshapeAndCache(
     const std::vector<int>& slot_mapping,    // [num_tokens]
     std::vector<int8_t>& k_cache,            // [num_blocks, num_heads, head_size/x, block_size, x]
     std::vector<int8_t>& v_cache,            // [num_blocks, num_heads, head_size, block_size]
-    std::vector<ScaleT>& kv_params_cache)
-{  // [num_blocks, 2, num_heads, head_size / kv_quant_chunk_size, block_size]
+    std::vector<ScaleT>& kv_params_cache) {  // [num_blocks, 2, num_heads, head_size / kv_quant_chunk_size, block_size]
 
-    auto k_cache_dev = move_to_cuda(k_cache);
-    auto v_cache_dev = move_to_cuda(v_cache);
-    auto kv_params_dev = move_to_cuda(kv_params_cache);
-    auto slot_mapping_dev = move_to_cuda(slot_mapping);
-    auto value_dev = move_to_cuda(value);
-    auto key_dev = move_to_cuda(key);
+  auto k_cache_dev = move_to_cuda(k_cache);
+  auto v_cache_dev = move_to_cuda(v_cache);
+  auto kv_params_dev = move_to_cuda(kv_params_cache);
+  auto slot_mapping_dev = move_to_cuda(slot_mapping);
+  auto value_dev = move_to_cuda(value);
+  auto key_dev = move_to_cuda(key);
 
-    std::vector<int64_t> kv_dims{num_tokens, num_heads, head_size};
-    reshape_and_cache(
-        nullptr,
-        key_dev.get(),
-        value_dev.get(),
-        k_cache_dev.get(),
-        v_cache_dev.get(),
-        slot_mapping_dev.get(),
-        kv_dims.data(),
-        kv_dims.data(),
-        block_size,
-        x,
-        1,
-        kv_params_dev.get(),
-        kv_quant_chunk_size,
-        1);
+  std::vector<int64_t> kv_dims{num_tokens, num_heads, head_size};
+  onnxruntime::contrib::cuda::reshape_and_cache(
+      nullptr,
+      key_dev.get(),
+      value_dev.get(),
+      k_cache_dev.get(),
+      v_cache_dev.get(),
+      slot_mapping_dev.get(),
+      kv_dims.data(),
+      kv_dims.data(),
+      block_size,
+      x,
+      1,
+      kv_params_dev.get(),
+      kv_quant_chunk_size,
+      1);
 
-    ASSERT_TRUE(cudaPeekAtLastError() == cudaSuccess);
+  ASSERT_TRUE(cudaPeekAtLastError() == cudaSuccess);
 
-    back_to_cpu(k_cache_dev.get(), k_cache);
-    back_to_cpu(v_cache_dev.get(), v_cache);
-    back_to_cpu(kv_params_dev.get(), kv_params_cache);
+  back_to_cpu(k_cache_dev.get(), k_cache);
+  back_to_cpu(v_cache_dev.get(), v_cache);
+  back_to_cpu(kv_params_dev.get(), kv_params_cache);
 }
 
-void check_eq(std::vector<T>& gold, std::vector<T>& r, T threshold) {
-};
-
 void check_eq(std::vector<int8_t>& gold, std::vector<int8_t>& r, int threshold = 1) {
-    ASSERT_EQUAL(gold.size(), r.size());
-    for (size_t i = 0; i < gold.size(); i++) {
-        EXPECT_NEAR(gold[i], r[i], threshold) << " at:" << i;
-    }
+  ASSERT_EQ(gold.size(), r.size());
+  for (size_t i = 0; i < gold.size(); i++) {
+    EXPECT_NEAR(gold[i], r[i], threshold) << " at:" << i;
+  }
 }
 
 void check_eq(std::vector<MLFloat16>& gold, std::vector<MLFloat16>& r, float abs_err, float rel_err) {
-    ASSERT_EQUAL(gold.size(), r.size());
-    for (size_t i = 0; i < gold.size(); i++) {
-        EXPECT_NEAR((float)gold[i], (float)r[i], abs_err) << " at:" << i;
-        EXPECT_NEAR((float)gold[i], (float)r[i], rel_err * std::fabsf((float)gold[i])) << " at:" << i;
-    }
+  ASSERT_EQ(gold.size(), r.size());
+  for (size_t i = 0; i < gold.size(); i++) {
+    EXPECT_NEAR((float)gold[i], (float)r[i], abs_err) << " at:" << i;
+    EXPECT_NEAR((float)gold[i], (float)r[i], rel_err * std::fabs((float)gold[i])) << " at:" << i;
+  }
 }
 
 template <typename ScaleT>
@@ -172,8 +169,8 @@ void RunQuantizeReshapeAndCacheKernelTest(
   int num_blocks = std::min(8 + num_tokens / block_size, 2 * (num_tokens / block_size));
   RandomValueGenerator random{seed};
   std::vector<int64_t> kv_dims{num_blocks, num_heads, head_size};
-  std::vector<MLFloat16> key = random.Uniform<MLFloat16>(kv_shape, MLFloat16{-2.0f}, MLFloat16{2.0f});
-  std::vector<MLFloat16> value = random.Uniform<MLFloat16>(kv_shape, MLFloat16{-2.0f}, MLFloat16{2.0f});
+  std::vector<MLFloat16> key = random.Uniform<MLFloat16>(kv_dims, MLFloat16(-2.0f), MLFloat16(2.0f));
+  std::vector<MLFloat16> value = random.Uniform<MLFloat16>(kv_dims, MLFloat16(-2.0f), MLFloat16(2.0f));
 
   std::vector<int64_t> k_cache_dims{num_blocks, num_heads, head_size / x, block_size, x};
   auto kv_cache_elem_count = std::accumulate(k_cache_dims.begin(), k_cache_dims.end(), 1LL, std::multiplies<int64_t>());
@@ -192,9 +189,6 @@ void RunQuantizeReshapeAndCacheKernelTest(
     int b = blk_mapping[t / block_size];
     slot_mapping[t] = b * block_size + (t % block_size);
   }
-
-  int key_stride = num_heads * head_size * block_size;
-  int value_stride = num_heads * head_size * block_size;
 
   std::vector<int64_t> kv_params_dims{num_blocks, 2, num_heads, head_size / kv_quant_chunk_size, block_size};
   auto kv_params_elem_count = std::accumulate(kv_params_dims.begin(), kv_params_dims.end(), 1LL, std::multiplies<int64_t>());
@@ -231,9 +225,9 @@ void RunQuantizeReshapeAndCacheKernelTest(
       v_cache,
       kv_params_cache);
 
-    check_eq(k_cache_ref, k_cache, 1);
-    check_eq(v_cache_ref, v_cache, 1);
-    check_eq(kv_params_cache_erf, kv_params_cache, 0.001f, 0.05f);
+  check_eq(k_cache_ref, k_cache, 1);
+  check_eq(v_cache_ref, v_cache, 1);
+  check_eq(kv_params_cache_ref, kv_params_cache, 0.001f, 0.05f);
 };
 
 TEST(PagedAttention, QuantizeReshapeAndCache) {
