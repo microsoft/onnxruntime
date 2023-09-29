b5303bd732acc5331846337d55cd729b31e8dbcd
shaahji <96227573+shaahji@users.noreply.github.com>
2023-09-29 14:11:05 -0700

    Python API to check whether collective ops are available or not (#17730)
    Python API to check whether collective ops are available or not
    
    ### Description
    <!-- Describe your changes. -->
    
    Adding an API to check whether collective ops are available or not.
    Since there is no independent MPI enabled build, this flag can be used
    on Python front for branching. Specifically, to conditionally enable
    tests.
    
    ### Motivation and Context
    <!-- - Why is this change required? What problem does it solve?
    - If it fixes an open issue, please link to the issue here. -->
    
    Flag to be used in Python to check whether onnxruntime supports
    collective ops or not. Handy for conditionally enabling/disabling tests
    and for other branching decisions.

--- onnxruntime/python/onnxruntime_pybind_module.cc
+++ onnxruntime/python/onnxruntime_pybind_module.cc
@@ -10,6 +10,12 @@ namespace onnxruntime {
 namespace python {
 namespace py = pybind11;
 
+#if defined(USE_MPI) && defined(ORT_USE_NCCL)
+static constexpr bool HAS_COLLECTIVE_OPS = true;
+#else
+static constexpr bool HAS_COLLECTIVE_OPS = false;
+#endif
+
 void CreateInferencePybindStateModule(py::module& m);
 
 PYBIND11_MODULE(onnxruntime_pybind11_state, m) {
@@ -23,6 +29,7 @@ PYBIND11_MODULE(onnxruntime_pybind11_state, m) {
 
   m.def("get_version_string", []() -> std::string { return ORT_VERSION; });
   m.def("get_build_info", []() -> std::string { return ORT_BUILD_INFO; });
+  m.def("has_collective_ops", []() -> bool { return HAS_COLLECTIVE_OPS; });
 }
 }  // namespace python
 }  // namespace onnxruntime
