013ab8fa05dd68aa7be2ba1e45994f4a096e3075
Arthur Islamov <arthur@islamov.ai>
2023-09-30 11:00:23 +0400

    [js/web] FP16 Conv, ConvTranspose and MatMul (#17514)
    ### Description
    Another three ops for fp16
    
    ---------
    
    Co-authored-by: Guenther Schmuelling <guschmue@microsoft.com>
    Co-authored-by: Yulong Wang <7679871+fs-eire@users.noreply.github.com>

--- onnxruntime/core/providers/js/operators/matmul.cc
+++ onnxruntime/core/providers/js/operators/matmul.cc
@@ -9,11 +9,11 @@ namespace js {
 JSEP_KERNEL_IMPL(MatMul, MatMul)
 
 ONNX_OPERATOR_VERSIONED_KERNEL_EX(MatMul, kOnnxDomain, 1, 12, kJsExecutionProvider,
-                                  KernelDefBuilder().TypeConstraint("T", DataTypeImpl::GetTensorType<float>()),
+                                  KernelDefBuilder().TypeConstraint("T", JsepSupportedFloatTypes()),
                                   MatMul);
 
 ONNX_OPERATOR_KERNEL_EX(MatMul, kOnnxDomain, 13, kJsExecutionProvider,
-                        KernelDefBuilder().TypeConstraint("T", DataTypeImpl::GetTensorType<float>()),
+                        KernelDefBuilder().TypeConstraint("T", JsepSupportedFloatTypes()),
                         MatMul);
 
 }  // namespace js
