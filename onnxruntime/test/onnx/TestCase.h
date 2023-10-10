06afe7b9f0c1c92077e7b2c41b649ae0a6ba3e8a
Yi Zhang <zhanyi@microsoft.com>
2023-10-10 13:03:58 +0800

    Onnx_test_runner and onnxruntime_test_all use the same broken test list. (#17840)

--- onnxruntime/test/onnx/TestCase.h
+++ onnxruntime/test/onnx/TestCase.h
@@ -106,3 +106,18 @@ void LoadTests(const std::vector<std::basic_string<PATH_CHAR_TYPE>>& input_paths
                const TestTolerances& tolerances,
                const std::unordered_set<std::basic_string<ORTCHAR_T>>& disabled_tests,
                const std::function<void(std::unique_ptr<ITestCase>)>& process_function);
+
+struct BrokenTest {
+  std::string test_name_;
+  std::string reason_;
+  std::set<std::string> broken_opset_versions_ = {};  // apply to all versions if empty
+  BrokenTest(std::string name, std::string reason) : test_name_(std::move(name)), reason_(std::move(reason)) {}
+  BrokenTest(std::string name, std::string reason, const std::initializer_list<std::string>& versions) : test_name_(std::move(name)), reason_(std::move(reason)), broken_opset_versions_(versions) {}
+  bool operator<(const struct BrokenTest& test) const {
+    return strcmp(test_name_.c_str(), test.test_name_.c_str()) < 0;
+  }
+};
+
+std::unique_ptr<std::set<BrokenTest>> GetBrokenTests(const std::string& provider_name);
+
+std::unique_ptr<std::set<std::string>> GetBrokenTestsKeyWordSet(const std::string& provider_name);
