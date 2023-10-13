93838657de4b6c91b2c07d53ff9f82abdf204ef4
Liqun Fu <liqfu@microsoft.com>
2023-10-13 08:13:00 -0700

    brocken tests
    Signed-off-by: Liqun Fu <liqfu@microsoft.com>

--- onnxruntime/test/onnx/TestCase.cc
+++ onnxruntime/test/onnx/TestCase.cc
@@ -944,6 +944,19 @@ std::unique_ptr<std::set<BrokenTest>> GetBrokenTests(const std::string& provider
       {"simple_rnn_batchwise", "type error", {}},
       {"mod_float_mixed_sign_example", "fmod attribute must be true for floating point types", {}},
       {"col2im_pads", "result mismatch", {"opset18"}},
+      {"gridsample_volumetric_nearest_align_corners_0", "result differs", {}},
+      {"gridsample_volumetric_nearest_align_corners_1", "result differs", {}},
+      {"reduce_l1_empty_set", "unknown version", {}},
+      {"reduce_l1_empty_set_expanded", "unknown version", {}},
+      {"reduce_l2_empty_set", "unknown version", {}},
+      {"reduce_l2_empty_set_expanded", "unknown version", {}},
+      {"reduce_log_sum_empty_set", "unknown version", {}},
+      {"reduce_log_sum_empty_set_expanded", "unknown version", {}},
+      {"reduce_log_sum_exp_empty_set", "unknown version", {}},
+      {"reduce_log_sum_exp_empty_set_expanded", "unknown version", {}},
+      {"reduce_prod_empty_set", "unknown version", {}},
+      {"reduce_sum_square_empty_set", "unknown version", {}},
+      {"reduce_sum_square_empty_set_expanded", "unknown version", {}},
 #ifdef ENABLE_TRAINING_CORE
       {"adagrad", "not a registered function/op", {}},                  // Op not registered.
       {"adagrad_multiple", "not a registered function/op", {}},         // Op not registered.
@@ -1338,19 +1351,6 @@ std::unique_ptr<std::set<BrokenTest>> GetBrokenTests(const std::string& provider
     broken_tests->insert({"sce_sum_log_prob_expanded", "result differs"});
     broken_tests->insert({"gridsample_reflection_padding", "result differs"});
     broken_tests->insert({"spacetodepth", "result differs"});
-    broken_tests->insert({"gridsample_volumetric_nearest_align_corners_0", "result differs"});
-    broken_tests->insert({"gridsample_volumetric_nearest_align_corners_1", "result differs"});
-    broken_tests->insert({"reduce_l1_empty_set", "unknown version"});
-    broken_tests->insert({"reduce_l1_empty_set_expanded", "unknown version"});
-    broken_tests->insert({"reduce_l2_empty_set", "unknown version"});
-    broken_tests->insert({"reduce_l2_empty_set_expanded", "unknown version"});
-    broken_tests->insert({"reduce_log_sum_empty_set", "unknown version"});
-    broken_tests->insert({"reduce_log_sum_empty_set_expanded", "unknown version"});
-    broken_tests->insert({"reduce_log_sum_exp_empty_set", "unknown version"});
-    broken_tests->insert({"reduce_log_sum_exp_empty_set_expanded", "unknown version"});
-    broken_tests->insert({"reduce_prod_empty_set", "unknown version"});
-    broken_tests->insert({"reduce_sum_square_empty_set", "unknown version"});
-    broken_tests->insert({"reduce_sum_square_empty_set_expanded", "unknown version"});
   }
 
 #ifdef DISABLE_CONTRIB_OPS
