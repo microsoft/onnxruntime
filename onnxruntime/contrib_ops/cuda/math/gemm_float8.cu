1de2c1db3d86e6cf5abd1491297eb2cc5a7db07b
Xavier Dupre <xadupre@microsoft.com>
2023-10-16 13:45:47 +0000

    fix lint issues

--- onnxruntime/contrib_ops/cuda/math/gemm_float8.cu
+++ onnxruntime/contrib_ops/cuda/math/gemm_float8.cu
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-#include "core/providers/cuda/math/gemm_float8.h"
+#include "contrib_ops/cuda/math/gemm_float8.h"
 #include "core/providers/cuda/cu_inc/common.cuh"
 #include "core/providers/cuda/shared_inc/cuda_utils.h"
 #include <algorithm>
