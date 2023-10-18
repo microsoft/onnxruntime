c945f964fdb9ae1553543227445d0911c94b2b3a
Dmitri Smirnov <dmitrism@microsoft.com>
2023-10-18 13:59:20 -0700

    Revert back to underscore

--- onnxruntime/core/graph/function_utils.cc
+++ onnxruntime/core/graph/function_utils.cc
@@ -374,7 +374,7 @@ class Inliner {
   // renaming-binding in current scope.
   void make_unique(std::string& name) {
     auto new_name{prefix_};
-    new_name.append("/").append(name);
+    new_name.append("_").append(name);
     auto& current_scope = rename_scopes_.back();
     current_scope[name] = new_name;
     name = std::move(new_name);
@@ -411,7 +411,7 @@ class Inliner {
       std::string rename_as = actuals.Get(i);
       if constexpr (isOutput) {
         if (rename_as.empty())
-          rename_as.assign(prefix_).append("/").append(formal);
+          rename_as.assign(prefix_).append("_").append(formal);
       }
       current_scope[formal] = rename_as;
       if (!rename_as.empty())
@@ -421,7 +421,7 @@ class Inliner {
       std::string& formal = *formals.Mutable(i);
       std::string rename_as;
       if constexpr (isOutput) {
-        rename_as.assign(prefix_).append("/").append(formal);
+        rename_as.assign(prefix_).append("_").append(formal);
       }
       current_scope[formal] = rename_as;
       if (!rename_as.empty())
@@ -432,7 +432,7 @@ class Inliner {
   // Process a node:
   void transform(NodeProto& n) {
     if (!n.name().empty())
-      n.set_name(prefix_ + "/" + n.name());
+      n.set_name(prefix_ + n.name());
 
     for (auto& x : *n.mutable_input()) {
       rename(x, false);
