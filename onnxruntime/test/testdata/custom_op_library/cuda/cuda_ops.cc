b2fdbfe1de1ed2cf9e730d89df81591a4a0166dd
Randy Shuai <rashuai@microsoft.com>
2023-10-18 18:03:01 -0700

    tune case

--- onnxruntime/test/testdata/custom_op_library/cuda/cuda_ops.cc
+++ onnxruntime/test/testdata/custom_op_library/cuda/cuda_ops.cc
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-#ifdef USE_CUDA
+#if defined(USE_CUDA) && !defined(ENABLE_TRAINING)
 
 #define ORT_API_MANUAL_INIT
 #include "onnxruntime_cxx_api.h"
