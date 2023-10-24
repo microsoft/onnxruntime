70505c4b0ca667cdfbddc244fc91e2fbf17c49b0
carzh <wolfivyaura@gmail.com>
2023-10-24 11:27:23 -0700

    format + lint

--- js/web/lib/wasm/binding/ort-wasm.d.ts
+++ js/web/lib/wasm/binding/ort-wasm.d.ts
@@ -102,9 +102,10 @@ export interface OrtWasmModule extends EmscriptenModule {
   _OrtTrainingCopyParametersFromBuffer?
       (trainingHandle: number, parametersBuffer: number, parameterCount: number, trainableOnly: boolean): number;
 
-  _OrtTrainingGetInputOutputCount?(trainingHandle: number, inputCount: number, outputCount: number,
-    isEvalModel: boolean): number;
-  _OrtTrainingGetInputOutputName?(trainingHandle: number, index: number, isInput: boolean, isEvalModel: boolean): number;
+  _OrtTrainingGetInputOutputCount?
+      (trainingHandle: number, inputCount: number, outputCount: number, isEvalModel: boolean): number;
+  _OrtTrainingGetInputOutputName?
+      (trainingHandle: number, index: number, isInput: boolean, isEvalModel: boolean): number;
 
   _OrtTrainingReleaseSession?(trainingHandle: number): void;
   // #endregion
