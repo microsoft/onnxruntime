5666cbc517cbb0cbd1e22ec22213c054c97e5ddd
MaanavD <maanavdalal@gmail.com>
2023-10-23 22:22:08 -0700

    Added videos to quickstart page.

--- src/routes/getting-started/+page.svelte
+++ src/routes/getting-started/+page.svelte
@@ -1,14 +1,15 @@
+<script lang="ts">
+	import Table from './table.svelte';
+	import githublogo from '../../images/logos/GitHub-Logo.svg';
+	import ImageTutorials from '../../images/undraw/image_tutorials.svelte';
+</script>
+
 <svelte:head>
 	<meta
 		name="description"
 		content="Quickly ramp up with ONNX Runtime, using a variety of platforms to deploy on hardware of your choice."
 	/>
 </svelte:head>
-<script lang="ts">
-	import Table from './table.svelte';
-	import githublogo from '../../images/logos/GitHub-Logo.svg';
-	import ImageTutorials from '../../images/undraw/image_tutorials.svelte';
-</script>
 
 <div class="container mx-auto px-10 my-10">
 	<h1 class="text-4xl pb-4">Installation Instructions</h1>
@@ -53,19 +54,40 @@
 	</div>
 </div>
 <div class="container mx-auto px-10 my-10">
+	<h1 class="text-4xl">Videos</h1>
+	<h1 class="text-2xl">Check out some of our videos to help you get started!</h1>
+	<div class="grid grid-cols-1 lg:grid-cols-3 gap-8 mx-auto my-10">
+		<div >
+			<iframe class="w-full h-64 pb-2" src="https://www.youtube.com/embed/F22zYGJdpB0?si=G00YdjyDSUA_akE8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+			<p class="text-center text-xl">Finetuning T5 Model in AzureML using Azure Container for PyTorch Curated Environment</p>
+		</div>
+		<div >
+			<iframe class="w-full h-64 pb-2" src="https://www.youtube.com/embed/2dbXFQxsIDY?si=XFWUx8wbqgs6JAZ-" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+			<p class="text-center text-xl">ONNX Runtime Azure EP for Hybrid Inferencing on Edge and Cloud</p>
+		</div>
+		<div >
+			<iframe class="w-full h-64 pb-2" src="https://www.youtube.com/embed/waIeC3OIn70?si=jFs5SztQx4v9KBNj" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+			<p class="text-center text-xl">v1.14 ONNX Runtime - Release Review</p>
+		</div>
+	</div>
+	<a href="https://www.youtube.com/@ONNXRuntime" class="btn btn-primary mr-4"
+		>ONNX Runtime YouTube channel →</a
+	>
+</div>
+<div class="container mx-auto px-10 my-10">
 	<div class="grid grid-cols-3">
 		<div class="col-span-2">
 			<h1 class="text-4xl">Tutorials</h1>
-			<h1 class="text-2xl">Learn more about ONNX Runtime with tutorials from our documentation.</h1>
-		
+			<h1 class="text-2xl pb-2">Learn more about ONNX Runtime with tutorials from our documentation.</h1>
+
 			<a href="/docs/tutorials/" rel="external" class="btn btn-primary mr-4"
-							>ONNX Runtime Tutorials →</a
-						>
+				>ONNX Runtime Tutorials →</a
+			>
 		</div>
 		<div class="col-span-1">
 			<div class="m-auto">
-				<ImageTutorials widthscale={0.5} heightscale={0.5}/>
+				<ImageTutorials widthscale={0.5} heightscale={0.5} />
 			</div>
 		</div>
-	</div> 
+	</div>
 </div>
