80ff642448b6e6fa1d8319b3b00bdd9f2aa40f4b
Changming Sun <chasun@microsoft.com>
2023-10-24 17:10:43 -0700

    Revert "Install CUDA 12.2 on Windows (#18044)"
    This reverts commit e63ccd3cbb9e2479af94a69a0e2c9bb9b59a54e4.

--- tools/ci_build/github/windows/setup_env_cuda_11.bat
+++ tools/ci_build/github/windows/setup_env_cuda_11.bat
@@ -6,10 +6,4 @@ if exist PATH=%AGENT_TEMPDIRECTORY%\v11.8\ {
 } else {
     set PATH=C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v11.8\bin;C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v11.8\extras\CUPTI\lib64;%PATH%
 }
-@REM The default version is still cuda v11.8, because set cuda v12.2 after it
-if exist PATH=%AGENT_TEMPDIRECTORY%\v12.2\ {
-    set PATH=%PATH%;%AGENT_TEMPDIRECTORY%\v12.2\bin;%AGENT_TEMPDIRECTORY%\v12.2\extras\CUPTI\lib64
-} else {
-    set PATH=%PATH%;C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v12.2\bin;C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v12.2\extras\CUPTI\lib64
-}
 set GRADLE_OPTS=-Dorg.gradle.daemon=false
