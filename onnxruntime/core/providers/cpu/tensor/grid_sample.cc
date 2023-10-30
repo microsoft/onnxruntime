86fae0df6b4323287ffedca0713be03188eebf21
Liqun Fu <liqfu@microsoft.com>
2023-10-30 15:41:23 -0700

    REGISTER_KERNEL_TYPED_20
    Signed-off-by: Liqun Fu <liqfu@microsoft.com>

--- onnxruntime/core/providers/cpu/tensor/grid_sample.cc
+++ onnxruntime/core/providers/cpu/tensor/grid_sample.cc
@@ -18,7 +18,7 @@ namespace onnxruntime {
                                               .TypeConstraint("T2", DataTypeImpl::GetTensorType<T>()), \
                                           GridSample<T>);
 
-#define REGISTER_KERNEL_TYPED20(T)                                                           \
+#define REGISTER_KERNEL_TYPED_20(T)                                                           \
   ONNX_OPERATOR_TYPED_KERNEL_EX(GridSample, kOnnxDomain, 20, T, kCpuExecutionProvider,       \
                                 KernelDefBuilder()                                           \
                                     .TypeConstraint("T1", DataTypeImpl::GetTensorType<T>())  \
@@ -26,8 +26,8 @@ namespace onnxruntime {
                                 GridSample<T>);
 
 REGISTER_KERNEL_TYPED(float)
-REGISTER_KERNEL_TYPED20(float)
-REGISTER_KERNEL_TYPED20(double)
+REGISTER_KERNEL_TYPED_20(float)
+REGISTER_KERNEL_TYPED_20(double)
 
 // Restore normalized location to actual image location
 //   When align_corners is true:
