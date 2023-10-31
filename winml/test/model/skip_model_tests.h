6ae7c51a2f1ec3d7beb4a360bb633a36f7e8d052
Changming Sun <chasun@microsoft.com>
2023-10-30 21:57:00 -0700

    Revert "Disable dml stage in windows GPU pipeline temporarily. (#18034)" (#18150) (#18170)
    This reverts commit 99b8dcaae2ac033b10880bc5bc7b0e89b37fe466.
    
    
    ### Motivation and Context
    Restore the dml stage in windows GPU  pipeline.
    Agent issue is solved by adding Feature.DisableGpuDriver in pool
    properties.
    
    ---------
    
    Co-authored-by: Yi Zhang <zhanyi@microsoft.com>

--- winml/test/model/skip_model_tests.h
+++ winml/test/model/skip_model_tests.h
@@ -146,6 +146,8 @@ std::unordered_map<std::string, std::string> disabledTests({
    "Bug 31005780: Result of fp16_test_tiny_yolov2_opset7 and fp16_coreml_FNS_Candy_opset7 models on DirectML aren't as accurate as on CPU https://microsoft.visualstudio.com/OS/_workitems/edit/31005780"},
   {                                           "mlperf_ssd_mobilenet_300_opset10_GPU",
    "Bug 31005624: mlperf_ssd_mobilenet_300 opset 10 model fails to evaluate in DirectML https://microsoft.visualstudio.com/OS/_workitems/edit/31005624"                                                  },
+  {                                           "mlperf_ssd_resnet34_1200_opset10_GPU",
+   "Bug 31005624: mlperf_ssd_resnet34_1200_opset10_GPU opset 10 model fails to evaluate in DirectML https://microsoft.visualstudio.com/OS/_workitems/edit/31005624"                                                  },
 });
 
 /*
