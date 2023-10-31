d2cb6c36e6d7b4144bdb00113ed81461f469fad4
sophies927 <107952697+sophies927@users.noreply.github.com>
2023-10-31 18:05:04 +0000

    Update Hugging Face landing page

--- src/images/undraw/image_HF2.svelte
+++ src/images/undraw/image_HF2.svelte
@@ -1,7 +1,11 @@
+<script>
+	export let widthscale = 1;
+	export let heightscale = 1;
+</script>
 <svg
 	stroke="currentColor"
-	width="109"
-	height="79"
+	width={109 * widthscale}
+	height={79 * heightscale}
 	viewBox="0 0 109 79"
 	fill="none"
 	xmlns="http://www.w3.org/2000/svg"
