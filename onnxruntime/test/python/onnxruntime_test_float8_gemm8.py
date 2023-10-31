cbb8ac517fe2ff9b703a8bb37ed4169c398f00dd
Xavier Dupre <xadupre@microsoft.com>
2023-10-31 17:47:00 +0100

    improving message

--- onnxruntime/test/python/onnxruntime_test_float8_gemm8.py
+++ onnxruntime/test/python/onnxruntime_test_float8_gemm8.py
@@ -194,27 +194,27 @@ class TestFloat8Gemm8(unittest.TestCase):
         self.assertEqual(expected.shape, y.shape)
         self.assertEqual(expected.dtype, y.dtype)
 
-    @unittest.skipIf("CUDAExecutionProvider" not in available_providers, reason="Not running on CUDA.")
+    @unittest.skipIf("CUDAExecutionProvider" not in available_providers, reason="Not running without CUDA.")
     def test_model_gemm_float(self):
         self.common_test_model_gemm("FLOAT", transA=1, rtol=1e-3)
 
-    @unittest.skipIf("CUDAExecutionProvider" not in available_providers, reason="Not running on CUDA.")
+    @unittest.skipIf("CUDAExecutionProvider" not in available_providers, reason="Not running without CUDA.")
     def test_model_gemm_float_default_values(self):
         self.common_test_model_gemm("FLOAT", transA=1, rtol=1e-3, activation=None)
 
-    @unittest.skipIf("CUDAExecutionProvider" not in available_providers, reason="Not running on CUDA.")
+    @unittest.skipIf("CUDAExecutionProvider" not in available_providers, reason="Not running without CUDA.")
     def test_model_gemm_float_relu(self):
         self.common_test_model_gemm("FLOAT", transA=1, rtol=1e-3, activation="RELU")
 
-    @unittest.skipIf("CUDAExecutionProvider" not in available_providers, reason="Not running on CUDA.")
+    @unittest.skipIf("CUDAExecutionProvider" not in available_providers, reason="Not running without CUDA.")
     def test_model_gemm_float_gelu(self):
         self.common_test_model_gemm("FLOAT", transA=1, rtol=1e-3, activation="GELU")
 
-    @unittest.skipIf("CUDAExecutionProvider" not in available_providers, reason="Not running on CUDA.")
+    @unittest.skipIf("CUDAExecutionProvider" not in available_providers, reason="Not running without CUDA.")
     def test_model_gemm_float_bias(self):
         self.common_test_model_gemm("FLOAT", transA=1, beta=1.0, rtol=1e-3)
 
-    @unittest.skipIf("CUDAExecutionProvider" not in available_providers, reason="Not running on CUDA.")
+    @unittest.skipIf("CUDAExecutionProvider" not in available_providers, reason="Not running without CUDA.")
     def test_model_gemm_float16(self):
         self.common_test_model_gemm(
             "FLOAT16",
@@ -223,7 +223,7 @@ class TestFloat8Gemm8(unittest.TestCase):
             transB=1,
         )
 
-    @unittest.skipIf("CUDAExecutionProvider" not in available_providers, reason="Not running on CUDA.")
+    @unittest.skipIf("CUDAExecutionProvider" not in available_providers, reason="Not running without CUDA.")
     @unittest.skipIf(not hasattr(TensorProto, "FLOAT8E4M3FN"), reason="needs onnx>=1.14.0")
     def test_model_gemm_float8_e4m3(self):
         self.common_test_model_gemm(
@@ -236,7 +236,7 @@ class TestFloat8Gemm8(unittest.TestCase):
         )
 
     @parameterized.parameterized.expand(list(itertools.product([0, 1], [0, 1])))
-    @unittest.skipIf("CUDAExecutionProvider" not in available_providers, reason="Not running on CUDA.")
+    @unittest.skipIf("CUDAExecutionProvider" not in available_providers, reason="Not running without CUDA.")
     def test_combinations_square_matrices(self, transA, transB):
         self.common_test_model_gemm("FLOAT", transA=transA, transB=transB, rtol=1e-3)
 
@@ -248,7 +248,7 @@ class TestFloat8Gemm8(unittest.TestCase):
             ((2, 3), (2, 5), 1, 0),
         ]
     )
-    @unittest.skipIf("CUDAExecutionProvider" not in available_providers, reason="Not running on CUDA.")
+    @unittest.skipIf("CUDAExecutionProvider" not in available_providers, reason="Not running without CUDA.")
     def test_combinations(self, shapeA, shapeB, transA, transB):
         model = make_model(
             make_graph(
