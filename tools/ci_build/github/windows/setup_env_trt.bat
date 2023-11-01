3ccbbc5ae022a2b08bc1c98d6b8cadff074fe6af
Jian Chen <cjian@microsoft.com>
2023-11-01 15:44:49 -0700

    Change {} to ()

--- tools/ci_build/github/windows/setup_env_trt.bat
+++ tools/ci_build/github/windows/setup_env_trt.bat
@@ -1,11 +1,11 @@
 REM Copyright (c) Microsoft Corporation. All rights reserved.
 REM Licensed under the MIT License.
 
-if exist %AGENT_TEMPDIRECTORY%\v11.8\ {
+if exist %AGENT_TEMPDIRECTORY%\v11.8\ (
     set PATH=%AGENT_TEMPDIRECTORY%\v11.8\bin;%AGENT_TEMPDIRECTORY%\v11.8\extras\CUPTI\lib64;%PATH%
-} else {
+) else (
     set PATH=C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v11.8\bin;C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v11.8\extras\CUPTI\lib64;%PATH%
-}
+)
 set PATH=C:\local\TensorRT-8.6.1.6.Windows10.x86_64.cuda-11.8\lib;%PATH%
 set GRADLE_OPTS=-Dorg.gradle.daemon=false
 set CUDA_MODULE_LOADING=LAZY
