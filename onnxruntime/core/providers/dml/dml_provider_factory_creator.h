bc533a6723950cf9e21f5ec65cb7941d593d6d29
Patrice Vignola <vignola.patrice@gmail.com>
2023-11-02 01:29:59 -0700

    [DML EP] Add dynamic graph compilation (#18199)

--- onnxruntime/core/providers/dml/dml_provider_factory_creator.h
+++ onnxruntime/core/providers/dml/dml_provider_factory_creator.h
@@ -7,14 +7,22 @@
 
 #include <wrl/client.h>
 #include <d3d12.h>
+#include "core/framework/provider_options.h"
 #include "core/providers/providers.h"
 #include "core/providers/dml/dml_provider_factory.h"
 
 namespace onnxruntime {
 
 struct DMLProviderFactoryCreator {
-  static std::shared_ptr<IExecutionProviderFactory> Create(int device_id);
-  static std::shared_ptr<IExecutionProviderFactory> Create(int device_id, bool skip_software_device_check);
+  static std::shared_ptr<IExecutionProviderFactory> Create(
+    int device_id,
+    bool skip_software_device_check,
+    bool disable_metacommands,
+    bool enable_dynamic_graph_fusion);
+
+  static std::shared_ptr<IExecutionProviderFactory> CreateFromProviderOptions(
+    const ProviderOptions& provider_options_map);
+
   static Microsoft::WRL::ComPtr<ID3D12Device> CreateD3D12Device(int device_id, bool skip_software_device_check);
   static Microsoft::WRL::ComPtr<IDMLDevice> CreateDMLDevice(ID3D12Device* d3d12_device);
 };
