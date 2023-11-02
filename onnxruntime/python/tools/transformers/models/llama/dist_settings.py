dcddb283b071580d174f2801e7e371ecd8b0c15c
Frank Dong <frdong@microsoft.com>
2023-11-02 06:00:34 +0000

    fix benchmark_all and change dist_settings mpi is only required for multi-gpus

--- onnxruntime/python/tools/transformers/models/llama/dist_settings.py
+++ onnxruntime/python/tools/transformers/models/llama/dist_settings.py
@@ -2,41 +2,41 @@ import os
 
 import torch
 import torch.distributed as dist
-from mpi4py import MPI
 
+
+comm = None
 
 def init_dist():
     if "LOCAL_RANK" in os.environ:
         local_rank = int(os.environ["LOCAL_RANK"])
         rank = int(os.environ["RANK"])
         world_size = int(os.environ["WORLD_SIZE"])
+
+        dist.init_process_group("nccl", init_method="tcp://127.0.0.1:7645", world_size=world_size, rank=rank)
     elif "OMPI_COMM_WORLD_LOCAL_RANK" in os.environ:
+        from mpi4py import MPI
+        comm = MPI.COMM_WORLD
+
         local_rank = int(os.environ.get("OMPI_COMM_WORLD_LOCAL_RANK", 0))
         rank = int(os.environ.get("OMPI_COMM_WORLD_RANK", 0))
         world_size = int(os.environ.get("OMPI_COMM_WORLD_SIZE", 1))
+
+        dist.init_process_group("nccl", init_method="tcp://127.0.0.1:7647", world_size=world_size, rank=rank)
     else:
-        local_rank = 0
-        rank = 0
-        world_size = 1
-
-    dist.init_process_group("nccl", init_method="tcp://127.0.0.1:7647", world_size=world_size, rank=rank)
-    device = torch.device(local_rank)
-    return device
-
-
-comm = MPI.COMM_WORLD
-
+        # don't need to do init for single process
+        pass
 
 def get_rank():
-    return comm.Get_rank()
+    return comm.Get_rank() if comm is not None else 0
 
 
 def get_size():
-    return comm.Get_size()
+    return comm.Get_size() if comm is not None else 1
 
 
 def barrier():
-    comm.Barrier()
+    if comm is not None:
+        comm.Barrier()
 
 
 def print_out(*args):
