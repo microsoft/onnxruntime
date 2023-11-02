47c45aa7b5e598cfb052d558f4fb9e2a5b7a68da
Chi Lo <Chi.Lo@microsoft.com>
2023-11-02 21:11:15 +0000

    fix format

--- onnxruntime/test/testdata/trt_reshape_test.py
+++ onnxruntime/test/testdata/trt_reshape_test.py
@@ -20,8 +20,14 @@ def generate_model(model_name):
         nodes,
         "trt_engine_wrapper",
         [  # input
-            helper.make_tensor_value_info("data", TensorProto.FLOAT, ["N",2]),
-            helper.make_tensor_value_info("shape", TensorProto.INT64, [2,]),
+            helper.make_tensor_value_info("data", TensorProto.FLOAT, ["N", 2]),
+            helper.make_tensor_value_info(
+                "shape",
+                TensorProto.INT64,
+                [
+                    2,
+                ],
+            ),
         ],
         [  # output
             helper.make_tensor_value_info("reshaped", TensorProto.FLOAT, [4, 1]),
