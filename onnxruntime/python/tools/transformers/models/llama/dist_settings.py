aa8ffb68a2d41de4fa16ba2703dc3989ca340d82
Frank Dong <frdong@microsoft.com>
2023-11-02 06:08:09 +0000

    fix lint error

--- onnxruntime/python/tools/transformers/models/llama/dist_settings.py
+++ onnxruntime/python/tools/transformers/models/llama/dist_settings.py
@@ -1,23 +1,23 @@
 import os
 
-import torch
 import torch.distributed as dist
-
 
 comm = None
 
+
 def init_dist():
     if "LOCAL_RANK" in os.environ:
-        local_rank = int(os.environ["LOCAL_RANK"])
+        int(os.environ["LOCAL_RANK"])
         rank = int(os.environ["RANK"])
         world_size = int(os.environ["WORLD_SIZE"])
 
         dist.init_process_group("nccl", init_method="tcp://127.0.0.1:7645", world_size=world_size, rank=rank)
     elif "OMPI_COMM_WORLD_LOCAL_RANK" in os.environ:
         from mpi4py import MPI
-        comm = MPI.COMM_WORLD
 
-        local_rank = int(os.environ.get("OMPI_COMM_WORLD_LOCAL_RANK", 0))
+        comm = MPI.COMM_WORLD  # noqa: F841
+
+        int(os.environ.get("OMPI_COMM_WORLD_LOCAL_RANK", 0))
         rank = int(os.environ.get("OMPI_COMM_WORLD_RANK", 0))
         world_size = int(os.environ.get("OMPI_COMM_WORLD_SIZE", 1))
 
@@ -25,6 +25,7 @@ def init_dist():
     else:
         # don't need to do init for single process
         pass
+
 
 def get_rank():
     return comm.Get_rank() if comm is not None else 0
