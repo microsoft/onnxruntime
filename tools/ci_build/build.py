9782c26e44108d7e4b986dd0a8bfc2d991db696f
Xavier Dupre <xadupre@microsoft.com>
2023-11-02 12:54:34 +0100

    check compatibility between cuda version and float 8

--- tools/ci_build/build.py
+++ tools/ci_build/build.py
@@ -6,6 +6,7 @@ import argparse
 import contextlib
 import json
 import os
+import packaging.version as pv
 import platform
 import re
 import shlex
@@ -1085,6 +1086,12 @@ def generate_build_tree(
     if args.use_cuda:
         nvcc_threads = number_of_nvcc_threads(args)
         cmake_args.append("-Donnxruntime_NVCC_THREADS=" + str(nvcc_threads))
+        if not args.disable_float8_types and args.cuda_version:
+            if pv.Version(args.cuda_version) < pv.Version("11.8"):
+                raise BuildError(
+                    f"Float 8 types require CUDA>=11.8. They must be disabled on CUDA=={args.cuda_version}. "
+                    f"See option disable_types."
+                )
     if args.use_rocm:
         cmake_args.append("-Donnxruntime_ROCM_HOME=" + rocm_home)
         cmake_args.append("-Donnxruntime_ROCM_VERSION=" + args.rocm_version)
