1e9265412aa3e53559241ce5fefc36381308bff1
Xu Xing <xing.xu@intel.com>
2023-11-07 11:05:09 +0800

    [js/webgpu] Fix scalar uniform uniform

--- js/web/lib/wasm/jsep/backend-webgpu.ts
+++ js/web/lib/wasm/jsep/backend-webgpu.ts
@@ -345,6 +345,9 @@ export class WebGpuBackend {
       let maxAlignmentOfField = 1;
       programUniforms.forEach(v => {
         const data = typeof v.data === 'number' ? [v.data] : v.data;
+        if (data.length === 0) {
+          return;
+        }
         // https://www.w3.org/TR/WGSL/#alignof
         let baseAlignment: number;
         switch (data.length) {
