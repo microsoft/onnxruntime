baf990e94da36d7374c7ce259ac3a92778b697ac
Jian Chen <cjian@microsoft.com>
2023-11-07 15:46:43 -0800

    Update TRT location

--- tools/ci_build/github/windows/setup_env_trt.bat
+++ tools/ci_build/github/windows/setup_env_trt.bat
@@ -4,7 +4,7 @@ REM Licensed under the MIT License.
 @REM set PATH=%AGENT_TEMPDIRECTORY%\v11.8\bin;%AGENT_TEMPDIRECTORY%\v11.8\extras\CUPTI\lib64;%PATH%
 @REM set PATH=C:\local\TensorRT-8.6.1.6.Windows10.x86_64.cuda-11.8\lib;%PATH%
 
-set PATH=%PATH%;C:\local\TensorRT-8.6.1.6.Windows10.x86_64.cuda-12.0\lib
+set PATH=%PATH%;%AGENT_TEMPDIRECTORY%\TensorRT-8.6.1.6.Windows10.x86_64.cuda-12.0\lib
 set PATH=%PATH%;%AGENT_TEMPDIRECTORY%\v12.2\bin;%AGENT_TEMPDIRECTORY%\v12.2\extras\CUPTI\lib64
 
 set GRADLE_OPTS=-Dorg.gradle.daemon=false
