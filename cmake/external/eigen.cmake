276df9f309759a50a77cbf2261b75ad411479d28
Scott McKay <Scott.McKay@microsoft.com>
2023-11-07 18:19:55 +1000

    Require `patch` to be found. Can't `git apply` when we download the source as a zip.

--- cmake/external/eigen.cmake
+++ cmake/external/eigen.cmake
@@ -1,25 +1,18 @@
-
 if (onnxruntime_USE_PREINSTALLED_EIGEN)
     add_library(eigen INTERFACE)
     file(TO_CMAKE_PATH ${eigen_SOURCE_PATH} eigen_INCLUDE_DIRS)
     target_include_directories(eigen INTERFACE ${eigen_INCLUDE_DIRS})
 else ()
-    if(Patch_FOUND)
-        set(EIGEN_PATCH_COMMAND ${Patch_EXECUTABLE} --ignore-whitespace -p1 -i
-                                ${PROJECT_SOURCE_DIR}/patches/eigen/3.4.0_to_ORT_1.16_src.patch)
-    else()
-        set(EIGEN_PATCH_COMMAND git apply --ignore-space-change --ignore-whitespace
-                                ${PROJECT_SOURCE_DIR}/patches/eigen/3.4.0_to_ORT_1.16_src.patch)
+    if(NOT Patch_FOUND)
+        message(FATAL_ERROR "Patch is required but was not found.")
     endif()
 
-    # Why does an ACL have different args which imply `git apply` is intended to be used
-    # (which AFAIK doesn't work with the change to download source instead of do a git checkout)
-    # https://github.com/microsoft/onnxruntime/issues/15248
     FetchContent_Declare(
         eigen
         URL ${DEP_URL_eigen}
         URL_HASH SHA1=${DEP_SHA1_eigen}
-        PATCH_COMMAND ${EIGEN_PATCH_COMMAND}
+        PATCH_COMMAND ${Patch_EXECUTABLE} --ignore-whitespace -p1 -i
+                      ${PROJECT_SOURCE_DIR}/patches/eigen/3.4.0_to_ORT_1.16_src.patch
     )
 
     FetchContent_Populate(eigen)
