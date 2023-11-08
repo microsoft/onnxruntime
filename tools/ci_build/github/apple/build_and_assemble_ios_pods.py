9dfbfae16d220e67a9809d68865343654a11e062
rachguo <rachguo@rachguos-Mini.attlocal.net>
2023-11-07 16:41:36 -0800

    fix input argument

--- tools/ci_build/github/apple/build_and_assemble_ios_pods.py
+++ tools/ci_build/github/apple/build_and_assemble_ios_pods.py
@@ -58,13 +58,15 @@ def parse_args():
 
     parser.add_argument("--test", action="store_true", help="Run tests on the framework and pod package files.")
 
+    parser.add_argument("--build-for-macosx", action="store_true", help="Enable macos arch for pod package.")
+
     build_framework_group = parser.add_argument_group(
         title="iOS framework build arguments",
         description="See the corresponding arguments in build_ios_framework.py for details.",
     )
 
     build_framework_group.add_argument("--include-ops-by-config")
-    build_framework_group.add_argument("--build-for-macosx")
+
     build_framework_group.add_argument(
         "--build-settings-file", required=True, help="The positional argument of build_ios_framework.py."
     )
