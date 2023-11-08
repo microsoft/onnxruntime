c8def0cc5193783492c0ed153f0d71527a80857e
kunal-vaishnavi <115581922+kunal-vaishnavi@users.noreply.github.com>
2023-11-08 09:36:28 -0800

    Add LLaMA GQA ragged batching (#18337)
    This PR updates replacing MHA with GQA and updates the LLaMA scripts for
    the modified GQA op. It is related to the changes in [this
    PR](https://github.com/microsoft/onnxruntime/pull/18283).
    
    ### Motivation and Context
    This PR allows us to run LLaMA with the GQA op end-to-end using ragged
    batching (i.e. batched inputs of different lengths).

--- onnxruntime/python/tools/transformers/models/llama/llama_inputs.py
+++ onnxruntime/python/tools/transformers/models/llama/llama_inputs.py
@@ -2,7 +2,7 @@ from typing import List, Tuple
 
 import numpy as np
 import torch
-from transformers import LlamaConfig
+from transformers import AutoConfig
 
 from onnxruntime import InferenceSession, OrtValue
 
@@ -24,7 +24,7 @@ def get_position_ids(attention_mask: torch.Tensor, use_past_kv: bool):
 #   attention_mask: (batch_size, sequence_length)
 #   position_ids: (batch_size, sequence_length)
 def get_sample_inputs(
-    config: LlamaConfig,
+    config: AutoConfig,
     device: torch.device,
     batch_size: int,
     seq_len: int,
@@ -59,7 +59,7 @@ def get_sample_inputs(
 #   past_key: (batch_size, num_heads, past_sequence_length, head_size)
 #   past_value: (batch_size, num_heads, past_sequence_length, head_size)
 def get_sample_with_past_kv_inputs(
-    config: LlamaConfig,
+    config: AutoConfig,
     device: torch.device,
     batch_size: int,
     past_seq_len: int,
@@ -115,13 +115,14 @@ def get_sample_with_past_kv_inputs(
 #      For models with GQA, kv_sequence_length = max_sequence_length
 #      For models without GQA, kv_sequence_length = past_sequence_length
 def get_merged_sample_with_past_kv_inputs(
-    config: LlamaConfig,
+    config: AutoConfig,
     device: torch.device,
     batch_size: int,
     seq_len: int,
     past_seq_len: int,
     max_seq_len: int,
     use_fp16: bool = False,
+    use_gqa: bool = False,
     engine: str = "pt",
     return_dict: bool = False,
     world_size: int = 1,
@@ -156,9 +157,7 @@ def get_merged_sample_with_past_kv_inputs(
         assert isinstance(past_kv, dict)
         inputs.update(past_kv)
 
-        if use_fp16:  # If model has GQA
-            del inputs["attention_mask"]
-            inputs["past_sequence_length"] = np.array([past_seq_len], dtype=np.int64)
+        if use_gqa:
             inputs = enable_past_present_share_buffer(inputs, past_seq_len, max_seq_len)
 
     else:
@@ -170,12 +169,13 @@ def get_merged_sample_with_past_kv_inputs(
 
 # Inputs for Microsoft export from https://github.com/microsoft/Llama-2-Onnx
 def get_msft_sample_inputs(
-    config: LlamaConfig,
+    config: AutoConfig,
     batch_size: int,
     past_seq_len: int,
     seq_len: int,
     max_seq_len: int,
     use_fp16: bool,
+    use_gqa: bool,
     split_kv: bool,
 ):
     np_dtype = np.float16 if use_fp16 else np.float32
@@ -213,8 +213,7 @@ def get_msft_sample_inputs(
                 }
             )
 
-        if use_fp16:  # If model has GQA
-            del ort_inputs["attn_mask"]
+        if use_gqa:
             ort_inputs = enable_past_present_share_buffer(ort_inputs, past_seq_len, max_seq_len)
 
     return ort_inputs
@@ -222,7 +221,7 @@ def get_msft_sample_inputs(
 
 # Create past_key_values
 # Each is of shape (batch_size, num_heads, past_sequence_length, head_size)
-def get_past_kv_inputs(config: LlamaConfig, batch_size: int, past_seq_len: int, use_fp16: bool, world_size: int = 1):
+def get_past_kv_inputs(config: AutoConfig, batch_size: int, past_seq_len: int, use_fp16: bool, world_size: int = 1):
     num_heads, head_size = config.num_key_value_heads // world_size, config.hidden_size // config.num_attention_heads
     torch_dtype = torch.float16 if use_fp16 else torch.float32
     past_kv = [
@@ -247,8 +246,7 @@ def flatten_past_kv_inputs(past_key_values: List[Tuple[torch.Tensor, torch.Tenso
 # Format PyTorch inputs to ONNX Runtime inputs
 def convert_inputs_for_ort(
     pt_inputs: dict,
-    use_fp16: bool,
-    use_buffer_share: bool = False,
+    use_gqa: bool = False,
     past_seq_len: int = 0,
     max_seq_len: int = 2048,
     device: str = "",
@@ -260,17 +258,11 @@ def convert_inputs_for_ort(
             ort_inputs[k] = v
         elif k == "past_key_values":
             ort_inputs.update(flatten_past_kv_inputs(v))
-        elif k == "attention_mask" and use_fp16 and use_buffer_share:
-            # Skip because FP16 model has GroupQueryAttention, uses buffer sharing,
-            # and GQA supports a causal mask by default
-
-            # Instead, add the past sequence length input for GQA
-            ort_inputs["past_sequence_length"] = np.array([past_seq_len], dtype=np.int64)
         else:
             ort_inputs[k] = v.detach().cpu().numpy()
 
-    # Reshape kv caches if using past-present-share-buffer
-    if use_buffer_share and device != "" and device != "cpu" and device_id > -1:
+    # Reshape KV caches if using past-present-share-buffer
+    if use_gqa and device != "" and device != "cpu" and device_id > -1:
         ort_inputs = enable_past_present_share_buffer(ort_inputs, past_seq_len, max_seq_len)
 
     return ort_inputs
@@ -289,17 +281,14 @@ def enable_past_present_share_buffer(ort_inputs: dict, past_seq_len: int, max_se
 
 
 # Add IO bindings for execution providers
-def add_io_bindings(model: InferenceSession, ort_inputs: dict, device: str, device_id: int, kv_cache_ortvalues: dict):
-    use_fp16 = False
+def add_io_bindings(
+    model: InferenceSession, ort_inputs: dict, device: str, device_id: int, use_gqa: bool, kv_cache_ortvalues: dict
+):
     io_binding = model.io_binding()
 
     for k, v in ort_inputs.items():
-        # Detect if model is in FP16
-        if v.dtype == np.float16:
-            use_fp16 = True
-
         # Bind OrtValue inputs to device
-        if use_fp16 and ("cache" in k or "past_key_values" in k):
+        if use_gqa and ("cache" in k or "past_key_values" in k):
             if k not in kv_cache_ortvalues:
                 v_device = OrtValue.ortvalue_from_numpy(v, device_type=device, device_id=device_id)
                 io_binding.bind_ortvalue_input(k, v_device)
@@ -313,7 +302,7 @@ def add_io_bindings(model: InferenceSession, ort_inputs: dict, device: str, devi
 
     for output in model.get_outputs():
         name = output.name
-        if use_fp16 and ("out" in name or "present" in name):
+        if use_gqa and ("out" in name or "present" in name):
             # Bind present KV cache outputs to past KV cache inputs in order to buffer share
             input_name = name.replace("out", "cache").replace("present", "past_key_values")
             io_binding.bind_ortvalue_output(name, kv_cache_ortvalues[input_name])
