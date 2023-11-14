183832933953e6923cd654b9f4ee3c139cf1c8cf
Jian Chen <cjian@microsoft.com>
2023-11-13 17:07:51 -0800

    Update shell script

--- tools/ci_build/github/linux/run_python_dockerbuild.sh
+++ tools/ci_build/github/linux/run_python_dockerbuild.sh
@@ -2,7 +2,7 @@
 set -e -x
 BUILD_CONFIG="Release"
 
-while getopts "i:d:xc:u:" parameter_Option
+while getopts "i:d:xc:u" parameter_Option
 do case "${parameter_Option}"
 in
 i) DOCKER_IMAGE=${OPTARG};;
