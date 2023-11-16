ba3c6823e4b374a21e7e4bce3172199fc4715ef6
adrianlizarraga <adlizarraga@microsoft.com>
2023-11-15 17:39:11 -0800

    Try to override for MatMul, Sigmoid, Tanh

--- onnxruntime/python/tools/quantization/execution_providers/qnn/quant_config.py
+++ onnxruntime/python/tools/quantization/execution_providers/qnn/quant_config.py
@@ -3,17 +3,49 @@
 # Licensed under the MIT License. See License.txt in the project root for
 # license information.
 # --------------------------------------------------------------------------
+from pathlib import Path
+
 import onnx
 
 from ...calibrate import CalibrationDataReader, CalibrationMethod
+from ...quant_utils import QuantType
 from ...quantize import StaticQuantConfig
 
-def get_qnn_qdq_config(model_input: Path,
-                       calibration_data_reader: CalibrationDataReader,
-                       calibrate_method=CalibrationMethod.MinMax):
+
+def get_qnn_qdq_config(
+    model_input: Path,
+    calibration_data_reader: CalibrationDataReader,
+    calibrate_method=CalibrationMethod.MinMax,
+    activation_type=QuantType.QUInt8,
+    weight_type=QuantType.QUInt8,
+):
+    # Parse model nodes to setup overrides.
     model = onnx.load_model(model_input)
-    # TODO: Parse model nodes to setup overrides.
-    return StaticQuantConfig(calibration_data_reader,
-                             calibrate_method=calibrate_method,
-                             extra_options={"MinimumRealRange": 0.0001,
-                                 "DedicatedQDQPair": True})
+
+    tensor_quant_overrides = {}
+
+    for node in model.graph.node:
+        if node.op_type == "MatMul" and activation_type == QuantType.QUInt16:
+            tensor_quant_overrides[node.input[1]] = {"quant_type": QuantType.QUInt8}
+        elif node.op_type == "Sigmoid":
+            if activation_type == QuantType.QUInt16:
+                tensor_quant_overrides[node.output[0]] = {"scale": 1.0 / 65536.0, "zero_point": 0}
+            elif activation_type == QuantType.QInt16:
+                tensor_quant_overrides[node.output[0]] = {"scale": 1.0 / 32768.0, "zero_point": 0}
+        elif node.op_type == "Tanh":
+            if activation_type == QuantType.QUInt16:
+                tensor_quant_overrides[node.output[0]] = {"scale": 1.0 / 32768.0, "zero_point": 32768}
+            elif activation_type == QuantType.QInt16:
+                tensor_quant_overrides[node.output[0]] = {"scale": 1.0 / 32768.0, "zero_point": 0}
+
+    return StaticQuantConfig(
+        calibration_data_reader,
+        calibrate_method=calibrate_method,
+        activation_type=activation_type,
+        weight_type=weight_type,
+        extra_options={
+            "MinimumRealRange": 0.0001,
+            "DedicatedQDQPair": True,
+            "TensorQuantOverrides": tensor_quant_overrides,
+        },
+    )
