52d0395b591149f97f7c0377a71606b001d7751f
Christian Larson <chrilaMSFT@users.noreply.github.com>
2023-11-17 18:19:29 -0800

    Fix TryConvertTensorToBroadcastScalar build break (#18497)
    ### Description
    Fix TryConvertTensorToBroadcastScalar build break, caused by incorrect
    method call.
    
    -        else if (!IsCpuData())
    +        else if (!constExpTensor->IsCpuData())
    
    ### Motivation and Context
    Previous build causing build break.
    
    Co-authored-by: Christian Larson <28911437+chrilaMSFT@users.noreply.github.com>

--- onnxruntime/core/providers/dml/DmlExecutionProvider/src/Operators/DmlOperator.cpp
+++ onnxruntime/core/providers/dml/DmlExecutionProvider/src/Operators/DmlOperator.cpp
@@ -842,7 +842,7 @@ namespace Dml
         {
             return;
         }
-        else if (!IsCpuData())
+        else if (!constExpTensor->IsCpuData())
         {
             return;
         }
