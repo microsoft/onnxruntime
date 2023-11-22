e7f22efdb942b0c487c8c04d63cbe31384e7ae59
rachguo <rachguo@rachguos-Mac-mini.local>
2023-11-22 10:04:06 -0800

    formatting

--- onnxruntime/test/platform/apple/apple_package_test/macos_package_testUITests/macos_package_uitest_cpp_api.mm
+++ onnxruntime/test/platform/apple/apple_package_test/macos_package_testUITests/macos_package_uitest_cpp_api.mm
@@ -76,16 +76,16 @@ void testSigmoid(const char* modelPath, bool useCoreML) {
 @implementation macos_package_testUITests
 
 - (void)setUp {
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+  // Put setup code here. This method is called before the invocation of each test method in the class.
 
-    // In UI tests it is usually best to stop immediately when a failure occurs.
-    self.continueAfterFailure = NO;
+  // In UI tests it is usually best to stop immediately when a failure occurs.
+  self.continueAfterFailure = NO;
 
-    // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+  // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
 }
 
 - (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
+  // Put teardown code here. This method is called after the invocation of each test method in the class.
 }
 
 - (NSString*)getFilePath {
