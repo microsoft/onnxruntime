c2f7a9945c6d4a7930e060a14ebfd582fc239b0e
adrianlizarraga <adlizarraga@microsoft.com>
2023-11-25 16:48:53 -0800

    Fix Softmax QDQ handling for 16-bit

--- onnxruntime/python/tools/quantization/operators/softmax.py
+++ onnxruntime/python/tools/quantization/operators/softmax.py
@@ -1,6 +1,14 @@
 import onnx
 
-from ..quant_utils import TENSOR_NAME_QUANT_SUFFIX, QuantizedValue, QuantizedValueType, attribute_to_kwarg, ms_domain
+from ..quant_utils import (
+    TENSOR_NAME_QUANT_SUFFIX,
+    QuantizedValue,
+    QuantizedValueType,
+    attribute_to_kwarg,
+    compute_scale_zp,
+    get_qmin_qmax_for_qType,
+    ms_domain,
+)
 from .base_operator import QuantOperatorBase
 from .qdq_base_operator import QDQOperatorBase
 
@@ -77,15 +85,23 @@ class QLinearSoftmax(QuantOperatorBase):
 class QDQSoftmax(QDQOperatorBase):
     def quantize(self):
         super().quantize()
-        if self.quantizer.activation_qType == onnx.onnx_pb.TensorProto.UINT8:
-            out_scale = 1 / 256.0
-            out_zero_point = 0
-        elif self.quantizer.is_activation_symmetric:
-            # results are all greater or equal to 0, so we can only use
-            # half of the range
-            out_scale = 1 / 127.0
-            out_zero_point = 0
+        output_name = self.node.output[0]
+
+        quant_overrides = self.quantizer.tensor_quant_overrides.get(output_name, {})
+
+        quant_type = self.quantizer.activation_qType
+        if "quant_type" in quant_overrides:
+            quant_type = quant_overrides["quant_type"].tensor_type
+
+        if "scale" in quant_overrides and "zero_point" in quant_overrides:
+            out_zero_point, out_scale = quant_overrides["zero_point"], quant_overrides["scale"]
         else:
-            out_scale = 1 / 256.0
-            out_zero_point = -128
-        self.quantizer.set_quant_scale_zp(self.node.output[0], (out_scale, out_zero_point))
+            # Unless overridden by the user, force Softmax to range from 0.0 to 1.0
+            rmin = quant_overrides.get("rmin", 0.0)
+            rmax = quant_overrides.get("rmax", 1.0)
+            symmetric = quant_overrides.get("symmetric", self.quantizer.is_activation_symmetric)
+            reduce_range = quant_overrides.get("reduce_range", False)
+            qmin, qmax = get_qmin_qmax_for_qType(quant_type, reduce_range=reduce_range, symmetric=symmetric)
+            out_zero_point, out_scale = compute_scale_zp(rmin, rmax, qmin, qmax, symmetric=symmetric)
+
+        self.quantizer.set_quant_scale_zp(output_name, (out_scale, out_zero_point))
