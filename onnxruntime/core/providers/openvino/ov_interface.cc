789e5d5a610c3931aadc252e884cdbb88fbb63ce
Suryaprakash Shanmugam <suryaprakash.shanmugam@intel.com>
2023-11-27 03:13:24 -0800

    Add logging for OpenVINO device properties in debug mode

--- onnxruntime/core/providers/openvino/ov_interface.cc
+++ onnxruntime/core/providers/openvino/ov_interface.cc
@@ -6,6 +6,7 @@
 #define ORT_API_MANUAL_INIT
 #include "core/session/onnxruntime_cxx_api.h"
 #include "core/providers/shared_library/provider_api.h"
+#include "backend_utils.h"
 
 #if defined(OV_API_20)
 using Exception = ov::Exception;
@@ -36,6 +37,34 @@ OVExeNetwork OVCore::LoadNetwork(std::shared_ptr<OVNetwork>& ie_cnn_network,
   ov::CompiledModel obj;
   try {
     obj = oe.compile_model(ie_cnn_network, hw_target, device_config);
+    
+   if (onnxruntime::openvino_ep::backend_utils::IsDebugEnabled()) {
+      // output of the actual settings that the device selected
+      auto supported_properties = obj.get_property(ov::supported_properties);
+      std::cout << "Model:" << std::endl;
+      for (const auto& cfg : supported_properties) {
+          if (cfg == ov::supported_properties)
+              continue;
+          auto prop = obj.get_property(cfg);
+          if (cfg == ov::device::properties) {
+              auto devices_properties = prop.as<ov::AnyMap>();
+              for (auto& item : devices_properties) {
+                  std::cout << "  " << item.first << ": " << std::endl;
+                  for (auto& item2 : item.second.as<ov::AnyMap>()) {
+                      OPENVINO_SUPPRESS_DEPRECATED_START
+                      if (item2.first == ov::supported_properties || item2.first == "SUPPORTED_CONFIG_KEYS)" ||
+                          item2.first == "SUPPORTED_METRICS")
+                          continue;
+                      OPENVINO_SUPPRESS_DEPRECATED_END
+                      std::cout << "    " << item2.first << ": " << item2.second.as<std::string>() << std::endl;
+                  }
+              }
+          } else {
+              std::cout << "  " << cfg << ": " << prop.as<std::string>() << std::endl;
+          }
+      }
+    }
+
     OVExeNetwork exe(obj);
     return exe;
   } catch (const Exception& e) {
