c75bc3388492cd105dee59174ffe623a31aca643
Rachel Guo <35738743+YUNQIUGUO@users.noreply.github.com>
2023-12-05 21:06:55 -0800

    Update onnxruntime/core/providers/nnapi/nnapi_builtin/builders/impl/split_op_builder.cc
    Co-authored-by: Edward Chen <18449977+edgchen1@users.noreply.github.com>

--- onnxruntime/core/providers/nnapi/nnapi_builtin/builders/impl/split_op_builder.cc
+++ onnxruntime/core/providers/nnapi/nnapi_builtin/builders/impl/split_op_builder.cc
@@ -133,7 +133,7 @@ bool SplitOpBuilder::IsOpSupportedImpl(const InitializedTensorSet& initializers,
       }
       num_outputs = SafeInt<uint32_t>(*num_outputs_attr);
       if (num_outputs != SafeInt<uint32_t>(node_unit.Outputs().size()) || num_outputs > split_dims_at_axis) {
-        LOGS_DEFAULT(VERBOSE) << "Invalid num_outputs provided.\n."
+        LOGS_DEFAULT(VERBOSE) << "Invalid num_outputs provided. "
                               << "The value should be less than or equal to the size of dimension being split "
                               << "and align with the size of output nodes. Current num_outputs: "
                               << num_outputs;
