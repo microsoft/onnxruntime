eb93e810de49271d07f4a6c2fc8b5f69863bb17f
Tianlei Wu <tlwu@microsoft.com>
2023-12-06 00:22:37 +0000

    Add benchmark script

--- onnxruntime/python/tools/transformers/models/stable_diffusion/benchmark_turbo.py
+++ onnxruntime/python/tools/transformers/models/stable_diffusion/benchmark_turbo.py
@@ -1,14 +1,22 @@
 import gc
+import importlib.util
 import time
 from statistics import mean
 
 import torch
-from diffusers import AutoencoderKL, DiffusionPipeline, EulerAncestralDiscreteScheduler
+from diffusers import (
+    AutoencoderKL,
+    ControlNetModel,
+    DiffusionPipeline,
+    EulerAncestralDiscreteScheduler,
+    StableDiffusionXLControlNetPipeline,
+)
 
 """
-Benchmark script for SDXL-Turbo.
+Benchmark script for SDXL-Turbo or SD-Turbo with PyTorch or Stable Fast.
+For SDXL-Turbo, we can also benchmark canny control net with --use_control_net flag.
 
-Setup of Linux before running Stable Fast:
+Setup for Stable Fast:
     git clone https://github.com/chengzeyi/stable-fast.git
     cd stable-fast
     git submodule update --init
@@ -19,50 +27,80 @@ Setup of Linux before running Stable Fast:
 """
 
 
-def compile_stable_fast(pipeline):
+def get_canny_image():
+    import cv2
+    import numpy as np
+    from PIL import Image
+
+    # Test Image can be downloaded from https://hf.co/datasets/huggingface/documentation-images/resolve/main/diffusers/input_image_vermeer.png
+    image = Image.open("input_image_vermeer.png").convert("RGB")
+
+    image = np.array(image)
+    image = cv2.Canny(image, 100, 200)
+    image = image[:, :, None]
+    image = np.concatenate([image, image, image], axis=2)
+    return Image.fromarray(image)
+
+
+def compile_stable_fast(pipeline, enable_cuda_graph=True):
     from sfast.compilers.stable_diffusion_pipeline_compiler import CompilationConfig, compile
 
     config = CompilationConfig.Default()
-    try:
-        import xformers
 
+    if importlib.util.find_spec("xformers") is not None:
         config.enable_xformers = True
-    except ImportError:
-        print("xformers not installed, skip")
 
-    try:
-        import triton
-
+    if importlib.util.find_spec("triton") is not None:
         config.enable_triton = True
-    except ImportError:
-        print("triton not installed, skip")
 
-    config.enable_cuda_graph = True
+    config.enable_cuda_graph = enable_cuda_graph
 
     pipeline = compile(pipeline, config)
     return pipeline
 
 
-def compile_torch(pipeline):
+def compile_torch(pipeline, use_nhwc=False):
     pipeline.unet = torch.compile(pipeline.unet, mode="reduce-overhead", fullgraph=True)
+    if use_nhwc:
+        pipeline.unet.to(memory_format=torch.channels_last)
+
+    if hasattr(pipeline, "controlnet"):
+        if use_nhwc:
+            pipeline.controlnet.to(memory_format=torch.channels_last)
+        pipeline.controlnet = torch.compile(pipeline.controlnet, mode="reduce-overhead", fullgraph=True)
     return pipeline
 
 
-def load_pipeline(name, engine):
+def load_pipeline(name, engine, use_control_net=False, use_nhwc=False, enable_cuda_graph=True):
     gc.collect()
     torch.cuda.empty_cache()
     before_memory = torch.cuda.memory_allocated()
 
     scheduler = EulerAncestralDiscreteScheduler.from_pretrained(name, subfolder="scheduler")
     vae = AutoencoderKL.from_pretrained("madebyollin/sdxl-vae-fp16-fix", torch_dtype=torch.float16).to("cuda")
-    pipeline = DiffusionPipeline.from_pretrained(
-        name,
-        vae=vae,
-        scheduler=scheduler,
-        torch_dtype=torch.float16,
-        variant="fp16",
-        use_safetensors=True,
-    ).to("cuda")
+
+    if use_control_net:
+        assert "xl" in name
+        controlnet = ControlNetModel.from_pretrained("diffusers/controlnet-canny-sdxl-1.0", torch_dtype=torch.float16)
+        pipeline = StableDiffusionXLControlNetPipeline.from_pretrained(
+            name,
+            controlnet=controlnet,
+            vae=vae,
+            scheduler=scheduler,
+            variant="fp16",
+            use_safetensors=True,
+            torch_dtype=torch.float16,
+        ).to("cuda")
+    else:
+        pipeline = DiffusionPipeline.from_pretrained(
+            name,
+            vae=vae,
+            scheduler=scheduler,
+            controlnet=controlnet,
+            variant="fp16",
+            use_safetensors=True,
+            torch_dtype=torch.float16,
+        ).to("cuda")
     pipeline.safety_checker = None
 
     gc.collect()
@@ -70,15 +108,22 @@ def load_pipeline(name, engine):
     print(f"Loaded model with {after_memory - before_memory} bytes allocated")
 
     if engine == "stable_fast":
-        pipeline = compile_stable_fast(pipeline)
+        pipeline = compile_stable_fast(pipeline, enable_cuda_graph=enable_cuda_graph)
     elif engine == "torch":
-        pipeline = compile_torch(pipeline)
+        pipeline = compile_torch(pipeline, use_nhwc=use_nhwc)
 
     pipeline.set_progress_bar_config(disable=True)
     return pipeline
 
 
-def test(pipeline, batch_size=1, steps=4, warmup_runs=3, test_runs=10, seed=123, verbose=False, display=False):
+def test(pipeline, batch_size=1, steps=4, control_image=None, warmup_runs=3, test_runs=10, seed=123, verbose=False):
+    control_net_args = {}
+    if hasattr(pipeline, "controlnet"):
+        control_net_args = {
+            "image": control_image,
+            "controlnet_conditioning_scale": 0.5,
+        }
+
     warmup_prompt = "warm up"
     for _ in range(warmup_runs):
         image = pipeline(
@@ -86,6 +131,7 @@ def test(pipeline, batch_size=1, steps=4, warmup_runs=3, test_runs=10, seed=123,
             num_inference_steps=steps,
             num_images_per_prompt=batch_size,
             guidance_scale=0.0,
+            **control_net_args,
         ).images
         assert len(image) == batch_size
 
@@ -105,6 +151,7 @@ def test(pipeline, batch_size=1, steps=4, warmup_runs=3, test_runs=10, seed=123,
             num_images_per_prompt=batch_size,
             guidance_scale=0.0,
             generator=generator,
+            **control_net_args,
         ).images[0]
         torch.cuda.synchronize()
         seconds = time.perf_counter() - start_time
@@ -113,16 +160,7 @@ def test(pipeline, batch_size=1, steps=4, warmup_runs=3, test_runs=10, seed=123,
     if verbose:
         print(latency_list)
 
-    print(f"batch_size={batch_size}, steps={steps}, average_latency_in_ms={mean(latency_list) * 1000:.1f}")
-
-    if image:
-        image.save(f"stable_fast_xl_turbo_{batch_size}_{steps}.png")
-
-    if display:
-        from sfast.utils.term_image import print_image
-
-        print_image(image, max_width=120)
-
+    return image, latency_list
 
 def arguments():
     import argparse
@@ -140,7 +178,25 @@ def arguments():
         "--name",
         type=str,
         default="stabilityai/sdxl-turbo",
-        help="stable diffusion model name like stabilityai/sdxl-turbo",
+        help="stable diffusion model name. Default is stabilityai/sdxl-turbo",
+    )
+
+    parser.add_argument(
+        "--use_control_net",
+        action="store_true",
+        help="test control net diffusers/controlnet-canny-sdxl-1.0",
+    )
+
+    parser.add_argument(
+        "--use_nhwc",
+        action="store_true",
+        help="use channel last format for torch compile",
+    )
+
+    parser.add_argument(
+        "--enable_cuda_graph",
+        action="store_true",
+        help="enable cuda graph for stable fast",
     )
 
     parser.add_argument(
@@ -157,8 +213,6 @@ def arguments():
         help="unet steps",
     )
 
-    parser.add_argument("--display", action="store_true", help="Display image in terminal.")
-
     args = parser.parse_args()
     return args
 
@@ -167,15 +221,33 @@ def main():
     args = arguments()
 
     with torch.no_grad():
-        pipeline = load_pipeline(args.name, args.engine)
+        pipeline = load_pipeline(
+            args.name, args.engine, use_control_net=args.use_control_net, use_nhwc=args.use_nhwc, enable_cuda_graph=args.enable_cuda_graph
+        )
+
+        canny_image = get_canny_image()
 
         if args.engine == "stable_fast":
             from sfast.utils.compute_precision import low_compute_precision
 
             with low_compute_precision():
-                test(pipeline, args.batch_size, args.steps, display=args.display)
+                image, latency_list = test(pipeline, args.batch_size, args.steps, control_image=canny_image)
         else:
-            test(pipeline, args.batch_size, args.steps, display=args.display)
+            image, latency_list = test(pipeline, args.batch_size, args.steps, control_image=canny_image)
 
+        # Save the first output image to inspect the result.
+        if image:
+            image.save(f"{args.engine}_{args.name.replace('/', '_')}_{args.batch_size}_{args.steps}_c{int(args.use_control_net)}.png")
+
+        result = {
+            "engine": args.engine,
+            "batch_size": args.batch_size,
+            "steps" : args.steps,
+            "control_net" : args.use_control_net,
+            "nhwc": args.use_nhwc,
+            "enable_cuda_graph": args.enable_cuda_graph,
+            "average_latency_in_ms": mean(latency_list) * 1000
+        }
+        print(result)
 
 main()
