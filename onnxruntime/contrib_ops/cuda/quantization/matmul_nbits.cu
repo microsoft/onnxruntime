3340cb41c61c10558f9b9f259cae450b634f3942
Yufeng Li <liyufeng1987@gmail.com>
2023-12-07 17:15:13 +0000

    optimize int4 on gpu

--- onnxruntime/contrib_ops/cuda/quantization/matmul_nbits.cu
+++ onnxruntime/contrib_ops/cuda/quantization/matmul_nbits.cu
@@ -17,6 +17,20 @@ namespace onnxruntime {
 namespace contrib {
 namespace cuda {
 
+template <typename T>
+__device__ __forceinline__ T WarpUniform(T value) {
+  struct {
+    union {
+      T value;
+      uint32_t asInt;
+    };
+  } p;
+  p.value = value;
+  p.asInt = __shfl_sync(0xffffffff, (unsigned)p.asInt, 0);
+  return p.value;
+}
+
+/*
 __device__ __forceinline__ float AccumulateEightElements(uint32_t values_quant, half scale, uint8_t zp, const half* a) {
   half2 scale_half2 = {scale, scale};
   half zp_adjust = -scale * __short2half_rn(zp);
@@ -34,6 +48,80 @@ __device__ __forceinline__ float AccumulateEightElements(uint32_t values_quant, 
 
   half2 element67 = __halves2half2(__uint2half_rn((values_quant >> 24) & 0xF), __uint2half_rn((values_quant >> 28) & 0xF));
   half2 v3 = element67 * scale_half2 + zp_adjust2;
+
+  v0 = v0 * (*(reinterpret_cast<half2*>(&(vec_a.x))));
+  v1 = v1 * (*(reinterpret_cast<half2*>(&(vec_a.y))));
+  v2 = v2 * (*(reinterpret_cast<half2*>(&(vec_a.z)))) + v0;
+  v3 = v3 * (*(reinterpret_cast<half2*>(&(vec_a.w)))) + v1;
+  v3 = v2 + v3;
+  return float(v3.x) + float(v3.y);
+}
+*/
+
+__device__ __forceinline__ float AccumulateEightElements(uint32_t values_quant, half scale, uint8_t zp, const half* a) {
+  half2 scale_half2 = {scale, scale};
+  half zp_adjust = -scale * __short2half_rn(zp);
+  half2 zp_adjust2 = {zp_adjust, zp_adjust};
+  uint4 vec_a = *(reinterpret_cast<const uint4*>(a));
+
+  half2 elements[4];
+  uint32_t*      h   = reinterpret_cast<uint32_t*>(&elements);
+
+  // First, we extract the i4s and construct an intermediate fp16 number.
+  static constexpr uint32_t immLut                = (0xf0 & 0xcc) | 0xaa;
+  static constexpr uint32_t BOTTOM_MASK           = 0x000f000f;
+  static constexpr uint32_t TOP_MASK              = 0x00f000f0;
+  static constexpr uint32_t I4s_TO_F16s_MAGIC_NUM = 0x64006400;
+
+  // Note that the entire sequence only requires 1 shift instruction. This is thanks to the register packing
+  // format and the fact that we force our integers to be unsigned, and account for this in the fp16 subtractions.
+  // In addition, I exploit the fact that sub and fma have the same throughput in order to convert elt_23 and
+  // elt_67 to fp16 without having to shift them to the bottom bits before hand.
+
+  // Shift right by 8 to now consider elt_45 and elt_67. Issue first to hide RAW dependency if we issue
+  // immediately before required.
+  const uint32_t top_i4s = values_quant >> 8;
+  // Extract elt_01 - (i4s & 0x000f000f) | 0x64006400
+  asm volatile("lop3.b32 %0, %1, %2, %3, %4;\n"
+                : "=r"(h[0])
+                : "r"(values_quant), "n"(BOTTOM_MASK), "n"(I4s_TO_F16s_MAGIC_NUM), "n"(immLut));
+  // Extract elt_23 (i4s & 0x00f000f0) | 0x64006400
+  asm volatile("lop3.b32 %0, %1, %2, %3, %4;\n"
+                : "=r"(h[1])
+                : "r"(values_quant), "n"(TOP_MASK), "n"(I4s_TO_F16s_MAGIC_NUM), "n"(immLut));
+  // Extract elt_45 (top_i4s & 0x000f000f) | 0x64006400
+  asm volatile("lop3.b32 %0, %1, %2, %3, %4;\n"
+                : "=r"(h[2])
+                : "r"(top_i4s), "n"(BOTTOM_MASK), "n"(I4s_TO_F16s_MAGIC_NUM), "n"(immLut));
+  // Extract elt_67 (top_i4s & 0x00f000f0) | 0x64006400
+  asm volatile("lop3.b32 %0, %1, %2, %3, %4;\n"
+                : "=r"(h[3])
+                : "r"(top_i4s), "n"(TOP_MASK), "n"(I4s_TO_F16s_MAGIC_NUM), "n"(immLut));
+
+  // I use inline PTX below because I am not sure if the compiler will emit float2half instructions if I use the
+  // half2 ctor. In this case, I chose performance reliability over code readability.
+
+  // This is the half2 {1032, 1032} represented as an integer.
+  static constexpr uint32_t FP16_TOP_MAGIC_NUM = 0x64086408;
+  // This is the half2 {1 / 16, 1 / 16} represented as an integer.
+  static constexpr uint32_t ONE_SIXTEENTH = 0x2c002c00;
+  // This is the half2 {-72, -72} represented as an integer.
+  static constexpr uint32_t NEG_72 = 0xd480d480;
+
+  // Finally, we construct the output numbers.
+  // Convert elt_01
+  asm volatile("sub.f16x2 %0, %1, %2;\n" : "=r"(h[0]) : "r"(h[0]), "r"(FP16_TOP_MAGIC_NUM));
+  // Convert elt_23
+  asm volatile("fma.rn.f16x2 %0, %1, %2, %3;\n" : "=r"(h[1]) : "r"(h[1]), "r"(ONE_SIXTEENTH), "r"(NEG_72));
+  // Convert elt_45
+  asm volatile("sub.f16x2 %0, %1, %2;\n" : "=r"(h[2]) : "r"(h[2]), "r"(FP16_TOP_MAGIC_NUM));
+  // Convert elt_67
+  asm volatile("fma.rn.f16x2 %0, %1, %2, %3;\n" : "=r"(h[3]) : "r"(h[3]), "r"(ONE_SIXTEENTH), "r"(NEG_72));
+
+  half2 v0 = elements[0] * scale_half2 + zp_adjust2;
+  half2 v1 = elements[1] * scale_half2 + zp_adjust2;
+  half2 v2 = elements[2] * scale_half2 + zp_adjust2;
+  half2 v3 = elements[3] * scale_half2 + zp_adjust2;
 
   v0 = v0 * (*(reinterpret_cast<half2*>(&(vec_a.x))));
   v1 = v1 * (*(reinterpret_cast<half2*>(&(vec_a.y))));
@@ -90,7 +178,7 @@ __global__ void MatMulFloatInt4Kernel(
   int n_block_id = blockIdx.x;
   int m_id = blockIdx.y;
   int lane_id = threadIdx.x;
-  int warp_id = threadIdx.y;
+  int warp_id = WarpUniform(threadIdx.y);
   int n_id = n_block_id * kColsPerThreadBlock + warp_id;
   int thread_id = warp_id * kWarpSize + lane_id;
   constexpr int k_per_iter = 256;
