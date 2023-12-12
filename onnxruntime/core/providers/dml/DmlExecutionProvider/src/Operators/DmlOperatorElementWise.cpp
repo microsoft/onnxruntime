a4aa89340c56daa4c2d3e2f9487d510d78f5dded
Jake Mathern <jamather@microsoft.com>
2023-12-11 17:02:22 -0800

    fix constantpow when the constant is not cpu data

--- onnxruntime/core/providers/dml/DmlExecutionProvider/src/Operators/DmlOperatorElementWise.cpp
+++ onnxruntime/core/providers/dml/DmlExecutionProvider/src/Operators/DmlOperatorElementWise.cpp
@@ -480,7 +480,7 @@ public:
         ML_CHECK_VALID_ARGUMENT(kernelInfo.GetOutputCount() == 1);
 
         auto constExpTensor = kernelInfo.TryGetConstantInputTensor(1);
-        if (constExpTensor && constExpTensor->GetTotalElementCount() == 1)
+        if (constExpTensor && constExpTensor->IsCpuData() && constExpTensor->GetTotalElementCount() == 1)
         {
             std::vector<std::optional<uint32_t>> kernelInputIndices = {0};
 
