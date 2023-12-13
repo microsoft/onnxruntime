ece0d7bf4befb5c5e739dfc97b0f2d1c3add2ae2
Your <wangye@microsoft.com>
2023-12-13 22:40:25 +0000

    update

--- onnxruntime/python/tools/transformers/onnx_model_phi.py
+++ onnxruntime/python/tools/transformers/onnx_model_phi.py
@@ -23,7 +23,7 @@ class FissionTransformerBlockPhi(Fusion):
                                              "model_modeling_mixformer_sequential_ParallelBlock_sub2_1"])
         
     def uname(self, layer_id, name):
-        return name + "_" + layer_id
+        return name + "_" + str(layer_id)
 
     def fuse(
             self,
@@ -52,78 +52,87 @@ class FissionTransformerBlockPhi(Fusion):
 
         # opt graph construction. TODO: replace ? with layer id
         layer_id = 0
-        helper.make_node(
-            'LayerNormalization',
-            inputs=[i_hidden_states, ln_weight, ln_bias],
-            outputs=[self.uname(layer_id, 'ln_out')],
-            name=self.uname(layer_id, 'LayerNormalization'),
-            epsilon=9.999999747378752e-06,
-        )
-        helper.make_node(
-            'Attention',
-            inputs=[self.uname(layer_id, 'ln_out'), attn_qkv_weight, attn_qkv_bias, i_attn_mask, i_kv_cache],
-            outputs=[self.uname(layer_id, 'attn_out'), o_kv_cache],
-            name=self.uname(layer_id, 'Attention'),
-            domain='com.microsoft',
-            num_heads=32,
-            unidirectional=1,
-            do_rotary=1,
-        )       
-        helper.make_node(
-            'MatMul',
-            inputs=[self.uname(layer_id, 'attn_out'), attn_out_weight],
-            outputs=[self.uname(layer_id, 'matmul_out')],
-            name=self.uname(layer_id, 'OutProj_MatMul'),
-        )
-        helper.make_node(
-            'Add',
-            inputs=[self.uname(layer_id, 'matmul_out'), attn_out_bias],
-            outputs=[self.uname(layer_id, 'add_out')],
-            name=self.uname(layer_id, 'OutProj_Add'),
-        )
-        helper.make_node(
-            'MatMul',
-            inputs=[self.uname(layer_id, 'ln_out'), mlp_fc1_weight],
-            outputs=[self.uname(layer_id, 'fc1_w_out')],
-            name=self.uname(layer_id, 'FC1_MatMul'),
-        )
-        helper.make_node(
-            'Add',
-            inputs=[self.uname(layer_id, 'fc1_w_out'), mlp_fc1_bias],
-            outputs=[self.uname(layer_id, 'fc1_b_out')],
-            name=self.uname(layer_id, 'FC1_Bias'),
-        )
-        helper.make_node(
-            'NewGelu', # check what is it and use onnx inlining if needed
-            inputs=[self.uname(layer_id, 'fc1_b_out')],
-            outputs=[self.uname(layer_id, 'new_gelu_out')],
-            name=self.uname(layer_id, 'NewGelu'),
-            domain='com.microsoft', # fix later
-        )
-        helper.make_node(
-            'MatMul',
-            inputs=[self.uname(layer_id, 'new_gelu_out'), mlp_fc2_weight],
-            outputs=[self.uname(layer_id, 'fc2_w_out')],
-            name=self.uname(layer_id, 'FC2_MatMul'),
-        )
-        helper.make_node(
-            'Add',
-            inputs=[self.uname(layer_id, 'fc2_w_out'), mlp_fc2_bias],
-            outputs=[self.uname(layer_id, 'fc2_b_out')],
-            name=self.uname(layer_id, 'FC2_Bias'),
-        )
-        helper.make_node(
-            'Add',
-            inputs=[self.uname(layer_id, 'attn_out'), self.uname(layer_id, 'fc2_b_out')],
-            outputs=[self.uname(layer_id, 'residual_1_out')],
-            name=self.uname(layer_id, 'Residual_Add_1'),
-        )
-        helper.make_node(
-            'Add',
-            inputs=[i_hidden_states, self.uname(layer_id, 'residual_1_out')],
-            outputs=[o_hidden_states],
-            name=self.uname(layer_id, 'Residual_Add_2'),
-        )
+        subgraph_nodes = [
+            helper.make_node(
+                'LayerNormalization',
+                inputs=[i_hidden_states, ln_weight, ln_bias],
+                outputs=[self.uname(layer_id, 'ln_out')],
+                name=self.uname(layer_id, 'LayerNormalization'),
+                epsilon=9.999999747378752e-06,
+            ),
+            helper.make_node(
+                'Attention',
+                inputs=[self.uname(layer_id, 'ln_out'), attn_qkv_weight, attn_qkv_bias, i_attn_mask, i_kv_cache],
+                outputs=[self.uname(layer_id, 'attn_out'), o_kv_cache],
+                name=self.uname(layer_id, 'Attention'),
+                domain='com.microsoft',
+                num_heads=32,
+                unidirectional=1,
+                do_rotary=1,
+            ),      
+            helper.make_node(
+                'MatMul',
+                inputs=[self.uname(layer_id, 'attn_out'), attn_out_weight],
+                outputs=[self.uname(layer_id, 'matmul_out')],
+                name=self.uname(layer_id, 'OutProj_MatMul'),
+            ),
+            helper.make_node(
+                'Add',
+                inputs=[self.uname(layer_id, 'matmul_out'), attn_out_bias],
+                outputs=[self.uname(layer_id, 'add_out')],
+                name=self.uname(layer_id, 'OutProj_Add'),
+            ),
+            helper.make_node(
+                'MatMul',
+                inputs=[self.uname(layer_id, 'ln_out'), mlp_fc1_weight],
+                outputs=[self.uname(layer_id, 'fc1_w_out')],
+                name=self.uname(layer_id, 'FC1_MatMul'),
+            ),
+            helper.make_node(
+                'Add',
+                inputs=[self.uname(layer_id, 'fc1_w_out'), mlp_fc1_bias],
+                outputs=[self.uname(layer_id, 'fc1_b_out')],
+                name=self.uname(layer_id, 'FC1_Bias'),
+            ),
+            helper.make_node(
+                'NewGelu', # check what is it and use onnx inlining if needed
+                inputs=[self.uname(layer_id, 'fc1_b_out')],
+                outputs=[self.uname(layer_id, 'new_gelu_out')],
+                name=self.uname(layer_id, 'NewGelu'),
+                domain='com.microsoft', # fix later
+            ),
+            helper.make_node(
+                'MatMul',
+                inputs=[self.uname(layer_id, 'new_gelu_out'), mlp_fc2_weight],
+                outputs=[self.uname(layer_id, 'fc2_w_out')],
+                name=self.uname(layer_id, 'FC2_MatMul'),
+            ),
+            helper.make_node(
+                'Add',
+                inputs=[self.uname(layer_id, 'fc2_w_out'), mlp_fc2_bias],
+                outputs=[self.uname(layer_id, 'fc2_b_out')],
+                name=self.uname(layer_id, 'FC2_Bias'),
+            ),
+            helper.make_node(
+                'Add',
+                inputs=[self.uname(layer_id, 'attn_out'), self.uname(layer_id, 'fc2_b_out')],
+                outputs=[self.uname(layer_id, 'residual_1_out')],
+                name=self.uname(layer_id, 'Residual_Add_1'),
+            ),
+            helper.make_node(
+                'Add',
+                inputs=[i_hidden_states, self.uname(layer_id, 'residual_1_out')],
+                outputs=[o_hidden_states],
+                name=self.uname(layer_id, 'Residual_Add_2'),
+            ),
+        ]
+
+        for new_node in subgraph_nodes:
+            self.nodes_to_add.append(new_node)
+            self.node_name_to_graph_name[new_node.name] = self.this_graph_name
+
+        self.nodes_to_remove.append(node)
+        self.prune_graph = True
 
 
 class PhiOnnxModel(OnnxModel):
