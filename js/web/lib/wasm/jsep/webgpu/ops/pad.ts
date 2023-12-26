7f53621cf7678dca95fc89a7234b755f6a28b13e
Xu Xing <xing.xu@intel.com>
2023-12-26 08:27:03 +0800

    evert constant value

--- js/web/lib/wasm/jsep/webgpu/ops/pad.ts
+++ js/web/lib/wasm/jsep/webgpu/ops/pad.ts
@@ -1,12 +1,12 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-import {DataType} from '../../../wasm-common';
+import {DataType, tensorDataTypeEnumToString} from '../../../wasm-common';
 import {TensorView} from '../../tensor-view';
 import {ShapeUtil} from '../../util';
 import {ComputeContext, ProgramInfo, ProgramInputTensorInfoDependency, ProgramUniform} from '../types';
 
-import {createTensorShapeVariables, getElementAt, IndicesHelper, inputVariable, outputVariable, ShaderHelper, UniformsArrayType} from './common';
+import {createTensorShapeVariables, getElementAt, IndicesHelper, inputVariable, outputVariable, ShaderHelper, UniformDataElementType, UniformsArrayType} from './common';
 
 interface PadAttributes {
   // 0-constant, 1-reflect, 2-edge, 3-wrap
@@ -34,11 +34,10 @@ const validateInputs = (inputs: readonly TensorView[]): void => {
   }
 };
 
-const getPadConstant =
-    (output: IndicesHelper, inputRank: number, padsLength: number, constantValue: number): string => {
-      let block = '';
-      for (let i = inputRank - 1; i >= 0; --i) {
-        block += `
+const getPadConstant = (output: IndicesHelper, inputRank: number, padsLength: number): string => {
+  let block = '';
+  for (let i = inputRank - 1; i >= 0; --i) {
+    block += `
             k = i32(${output.indicesGet('indices', i)}) - ${getElementAt('uniforms.pads', i, padsLength)};
             if (k < 0) {
               break;
@@ -48,10 +47,10 @@ const getPadConstant =
             }
             offset += k * i32(${getElementAt('uniforms.x_strides', i, inputRank)});
         `;
-      }
+  }
 
-      return `
-          value = ${output.type.value}(${constantValue});
+  return `
+          value = ${output.type.value}(uniforms.constant_value);
           for (var i = 0; i < 1; i++) {
             var offset = 0;
             var k = 0;
@@ -59,7 +58,7 @@ const getPadConstant =
             value = x[offset];
           }
       `;
-    };
+};
 
 const getPadReflect = (output: IndicesHelper, inputRank: number, padsLength: number): string => {
   let block = '';
@@ -134,29 +133,32 @@ const getPadWrap = (output: IndicesHelper, inputRank: number, padsLength: number
           `;
 };
 
-const getPadSnippet =
-    (output: IndicesHelper, inputRank: number, attributes: PadAttributes, constantValue: number): string => {
-      switch (attributes.mode) {
-        case 0:
-          return getPadConstant(output, inputRank, attributes.pads.length, constantValue);
-        case 1:
-          return getPadReflect(output, inputRank, attributes.pads.length);
-        case 2:
-          return getPadEdge(output, inputRank, attributes.pads.length);
-        case 3:
-          return getPadWrap(output, inputRank, attributes.pads.length);
-        default:
-          throw new Error('Invalid mode');
-      }
-    };
+const getPadSnippet = (output: IndicesHelper, inputRank: number, attributes: PadAttributes): string => {
+  switch (attributes.mode) {
+    case 0:
+      return getPadConstant(output, inputRank, attributes.pads.length);
+    case 1:
+      return getPadReflect(output, inputRank, attributes.pads.length);
+    case 2:
+      return getPadEdge(output, inputRank, attributes.pads.length);
+    case 3:
+      return getPadWrap(output, inputRank, attributes.pads.length);
+    default:
+      throw new Error('Invalid mode');
+  }
+};
 
 const createPadProgramInfo = (inputs: readonly TensorView[], attributes: PadAttributes): ProgramInfo => {
   const outputShape = ShapeUtil.padShape(inputs[0].dims.slice(), attributes.pads);
   const inputDims = inputs[0].dims;
   const outputSize = ShapeUtil.size(outputShape);
-  // TODO: attributes.value maybe of type f16, currently f16 is not supported in uniform.
   const programUniforms: ProgramUniform[] =
       [{type: 'uint32', data: outputSize}, {type: 'uint32', data: attributes.pads}];
+  if (attributes.mode === 0) {
+    const tensorDataType = tensorDataTypeEnumToString(inputs[0].dataType) as ProgramUniform['type'];
+    programUniforms.push({type: tensorDataType, data: attributes.value});
+  }
+
   programUniforms.push(...createTensorShapeVariables(inputs[0].dims), ...createTensorShapeVariables(outputShape));
   const inputDependencies: ProgramInputTensorInfoDependency[] = ['rank'];
 
@@ -164,9 +166,13 @@ const createPadProgramInfo = (inputs: readonly TensorView[], attributes: PadAttr
     const output = outputVariable('output', inputs[0].dataType, outputShape.length);
     const input = inputVariable('x', inputs[0].dataType, inputDims.length);
     const dataType = input.type.value;
-    const padSnippet = getPadSnippet(output, inputDims.length, attributes, attributes.value);
+    const padSnippet = getPadSnippet(output, inputDims.length, attributes);
     const uniforms: UniformsArrayType =
         [{name: 'output_size', type: 'u32'}, {name: 'pads', type: 'i32', length: attributes.pads.length}];
+    if (attributes.mode === 0) {
+      uniforms.push({name: 'constant_value', type: dataType as UniformDataElementType});
+    }
+
     return `
             ${shaderHelper.registerUniforms(uniforms).declareVariables(input, output)}
             ${shaderHelper.mainStart()}
@@ -182,7 +188,7 @@ const createPadProgramInfo = (inputs: readonly TensorView[], attributes: PadAttr
 
   return {
     name: 'Pad',
-    shaderCache: {hint: `${attributes.mode};${attributes.value}`, inputDependencies},
+    shaderCache: {hint: `${attributes.mode}`, inputDependencies},
     getRunData: () => ({
       outputs: [{dims: outputShape, dataType: inputs[0].dataType}],
       dispatchGroup: {x: Math.ceil(ShapeUtil.size(outputShape) / 64 /* workgroup size */)},
