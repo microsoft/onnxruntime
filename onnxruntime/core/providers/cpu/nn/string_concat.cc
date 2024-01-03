42858bb48c384c289eb5e01f0ab47cd6c0a0f7c8
Aditya Goel <agoel4512@gmail.com>
2024-01-03 18:04:42 +0000

    Apply suggestions

--- onnxruntime/core/providers/cpu/nn/string_concat.cc
+++ onnxruntime/core/providers/cpu/nn/string_concat.cc
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 #include "string_concat.h"
 #include "core/providers/cpu/math/element_wise_ops.h"
 #include "core/common/common.h"
@@ -16,28 +19,33 @@ Status StringConcat::Compute(OpKernelContext* context) const {
         auto x = broadcast_helper.ScalarInput0<std::string>();
         auto Y = broadcast_helper.SpanInput1<std::string>();
         auto output = broadcast_helper.OutputSpan<std::string>();
-        std::transform(Y.begin(), Y.end(), output.begin(),
-                       [&x](const std::string& y) {
-                         return x + y;
-                       });
+        const auto x_size = x.length();
+        for (size_t i = 0; i < Y.size(); ++i) {
+          output[i].reserve(x_size + Y[i].length());
+          output[i].append(x);
+          output[i].append(Y[i]);
+        }
       },
       [](BroadcastHelper& broadcast_helper) {
         auto X = broadcast_helper.SpanInput0<std::string>();
         auto y = broadcast_helper.ScalarInput1<std::string>();
         auto output = broadcast_helper.OutputSpan<std::string>();
-        std::transform(X.begin(), X.end(), output.begin(),
-                       [&y](const std::string& x) {
-                         return x + y;
-                       });
+        const auto y_size = y.length();
+        for (size_t i = 0; i < X.size(); ++i) {
+          output[i].reserve(y_size + X[i].length());
+          output[i].append(X[i]);
+          output[i].append(y);
+        }
       },
       [](BroadcastHelper& broadcast_helper) {
         auto X = broadcast_helper.SpanInput0<std::string>();
         auto Y = broadcast_helper.SpanInput1<std::string>();
         auto output = broadcast_helper.OutputSpan<std::string>();
-        std::transform(X.begin(), X.end(), Y.begin(), output.begin(),
-                       [](const std::string& x, const std::string& y) {
-                         return x + y;
-                       });
+        for (size_t i = 0; i < X.size(); ++i) {
+          output[i].reserve(X[i].length() + Y[i].length());
+          output[i].append(X[i]);
+          output[i].append(Y[i]);
+        }
       }};
   UntypedBroadcastTwo(*context, broadcast_funcs);
   return Status::OK();
