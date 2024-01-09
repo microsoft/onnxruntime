e0d33461fc6cc59d08eb9a834a56464c8107c3e2
Chi Lo <Chi.Lo@microsoft.com>
2024-01-09 01:44:08 +0000

    remove kernelcontext_setoutput

--- onnxruntime/core/framework/op_kernel.cc
+++ onnxruntime/core/framework/op_kernel.cc
@@ -186,7 +186,7 @@ AllocatorPtr OpKernelContext::GetAllocator(const OrtDevice& device) const {
   return execution_frame_->GetAllocator(device);
 }
 
-#if defined(ENABLE_ATEN) || defined(USE_TENSORRT)
+#ifdef ENABLE_ATEN
 Status OpKernelContext::SetOutputMLValue(int index, const OrtValue& ort_value) {
   if (index < 0 || index >= OutputCount()) {
     return Status(common::ONNXRUNTIME, common::FAIL,
