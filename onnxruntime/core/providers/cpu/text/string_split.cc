434546a3078b372c739181e9941025795a5be179
Aditya Goel <agoel4512@gmail.com>
2024-01-10 11:30:52 +0000

    Move to text directory

--- onnxruntime/core/providers/cpu/text/string_split.cc
+++ onnxruntime/core/providers/cpu/text/string_split.cc
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-#include "core/providers/cpu/nn/string_split.h"
+#include "string_split.h"
 #include <algorithm>
 #include <limits>
 #include <string>
