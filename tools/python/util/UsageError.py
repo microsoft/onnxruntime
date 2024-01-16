6d07a50d10e1ef5cc50314e36521dfbe7373b44c
Jian Chen <cjian@microsoft.com>
2024-01-15 22:07:30 -0800

    Adding dot to import files

--- tools/python/util/UsageError.py
+++ tools/python/util/UsageError.py
@@ -1,6 +1,6 @@
 #  // Copyright (c) Microsoft Corporation. All rights reserved.
 #  // Licensed under the MIT License.
-import BaseError
+from .BaseError import BaseError
 
 
 class UsageError(BaseError):
