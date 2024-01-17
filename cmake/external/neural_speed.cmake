9f0c403a13948b2975a4d238c8b5e28fcc705743
Luo, Yu <yu.luo@intel.com>
2024-01-17 16:25:55 +0800

    use url instead of git for neural_speed.

--- cmake/external/neural_speed.cmake
+++ cmake/external/neural_speed.cmake
@@ -1,6 +1,3 @@
-set(NEURAL_SPEED_URL https://github.com/intel/neural-speed.git)
-set(NEURAL_SPEED_TAG 44c05babb9bf01c8b26f8697526f380677cb6800) # kernel-only release v0.1
-
 if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU" AND onnxruntime_target_platform STREQUAL "x86_64")
   set(USE_NEURAL_SPEED TRUE)
 elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "MSVC" AND onnxruntime_target_platform STREQUAL "x64")
@@ -10,9 +7,12 @@ endif()
 if(USE_NEURAL_SPEED)
   FetchContent_Declare(
       neural_speed
-      GIT_REPOSITORY ${NEURAL_SPEED_URL}
-      GIT_TAG        ${NEURAL_SPEED_TAG}
+      URL ${DEP_URL_neural_speed}
+      URL_HASH SHA1=${DEP_SHA1_neural_speed}
   )
   set(BTLA_USE_OPENMP OFF)
   FetchContent_MakeAvailable(neural_speed)
+  if(NOT neural_speed)
+    FetchContent_Populate(neural_speed)
+  endif()
 endif()
