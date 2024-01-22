178a893419a88cd0a37f529cdd559c63ed8deee6
Yi Zhang <zhanyi@microsoft.com>
2024-01-22 22:39:34 +0800

    wording

--- onnxruntime/test/providers/cpu/model_tests.cc
+++ onnxruntime/test/providers/cpu/model_tests.cc
@@ -105,7 +105,7 @@ TEST_P(ModelTest, Run) {
         model_path.find(ORT_TSTR("yolov3")) > 0 ||
         model_path.find(ORT_TSTR("mask_rcnn")) > 0 ||
         model_path.find(ORT_TSTR("FNS")) > 0) {
-        SkipTest("Skipping SSD test for big tolearance failure or shape error");
+        SkipTest("Skipping SSD test for big tolearance failure or other errors");
         return;
       }
   }
