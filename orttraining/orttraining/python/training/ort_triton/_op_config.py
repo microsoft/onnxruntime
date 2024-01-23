e2d03b155d5c2634f73bd764b6b75e90bccd1670
Prathik Rao <prathikrao@microsoft.com@orttrainingdev8.d32nl1ml4oruzj4qz3bqlggovf.px.internal.cloudapp.net>
2024-01-23 00:53:25 +0000

    triton integration notes

--- orttraining/orttraining/python/training/ort_triton/_op_config.py
+++ orttraining/orttraining/python/training/ort_triton/_op_config.py
@@ -31,17 +31,28 @@ _ELEMENTWISE_OPS = {
     "Sqrt": {"versions": [13]},
     "Exp": {"versions": [13]},
     "Where": {"versions": [9, 16]},
+    # CastOp in v19 has updates for FP8:
+    # - Saturate attribute was added and it defined the behavior if FP8 conversion
+    #   goes out of range. Does this need to be addressed?
     "Cast": {"versions": [13]},
     "Dropout": {"versions": [13]},
     "DropoutGrad": {"domain": "com.microsoft", "versions": [1]},
+    # IdentityOp has had changes in v14/v16/v19 but is_no_op attribute seems to
+    # essentially disable this op. Does this need to be addressed?
     "Identity": {"versions": [13], "is_no_op": True},
     "Sum": {"versions": [13]},
+    # GeluOp was released on v20, might need to switch domain from msft -> onnx?
     "Gelu": {"domain": "com.microsoft", "versions": [1]},
     "QuickGelu": {"domain": "com.microsoft", "versions": [1]},
     "GeluGrad": {"domain": "com.microsoft", "versions": [1]},
     "QuickGeluGrad": {"domain": "com.microsoft", "versions": [1]},
 }
 
+# ReduceMean/Sum/Max/Min have all changed similarly. Instead of axes being an attribute, it is now an input
+# Additionally, there is a new attribute called noop_with_empty_axis that defines behavior
+# if 'axes' is empty. Default behavior with 'false' is to reduce all axes. When axes is
+# empty and this attribute is set to true, input tensor will not be reduced, and the output
+# tensor would be equivalent to input tensor.
 _REDUCTION_OPS = {
     "ReduceMean": {"versions": [13], "conditions": {"axes": "[-1]"}},
     "ReduceSum": {"versions": [13], "conditions": {"axes": "[-1]"}},
