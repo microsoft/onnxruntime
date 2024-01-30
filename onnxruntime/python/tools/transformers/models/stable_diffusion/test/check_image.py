0f2041e81757b7c9dfce5c9a32ed3ae8ca8f22de
Yi Zhang <zhanyi@microsoft.com>
2024-01-30 11:14:53 +0800

    only cp generated image

--- onnxruntime/python/tools/transformers/models/stable_diffusion/test/check_image.py
+++ onnxruntime/python/tools/transformers/models/stable_diffusion/test/check_image.py
@@ -62,7 +62,7 @@ def main():
     cache_dir = args.cache_dir
     score = round(generate_score(image1, image2, cache_dir), 2)
     print("similarity Score: ", {score})
-    if score < 95:
+    if score < 97:
         print(f"{image1} and {image2} are different")
         raise SystemExit(1)
     else:
