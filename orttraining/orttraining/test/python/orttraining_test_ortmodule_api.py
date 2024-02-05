cd58e283730e83275995ba50c182f4d9bb02667c
Prathik Rao <prathik.rao@gmail.com>
2024-02-05 13:11:37 -0800

    add ATen support for bicubic interpolation (#19380)
    ### Description
    <!-- Describe your changes. -->
    
    Add ATen fallback support for bicubic interpolation algorithm.
    
    ### Motivation and Context
    <!-- - Why is this change required? What problem does it solve?
    - If it fixes an open issue, please link to the issue here. -->
    
    Required for facebook/dinov2 model architecture as part of ONNX Runtime
    integration with AML Vision models.

--- orttraining/orttraining/test/python/orttraining_test_ortmodule_api.py
+++ orttraining/orttraining/test/python/orttraining_test_ortmodule_api.py
@@ -1805,6 +1805,34 @@ def test_resize_grad_correctness_bilinear_2d(interpolate_size_scale, align_corne
     _test_helpers.assert_values_are_close(ort_input.grad, pt_input.grad)
 
 
+def test_aten_upsample_bicubic():
+    class _NeuralNetUpsampleBicubic(torch.nn.Module):
+        def __init__(self):
+            super().__init__()
+
+        def forward(self, input):
+            return torch.nn.functional.interpolate(input, size=(8, 12), mode="bicubic")
+
+    device = "cuda"
+    pt_model = _NeuralNetUpsampleBicubic().to(device)
+    ort_model = ORTModule(copy.deepcopy(pt_model))
+
+    def run_step(model, input):
+        prediction = model(input)
+        prediction.sum().backward()
+        return prediction
+
+    # reset manual seed to reset the generator
+    torch.manual_seed(2333)
+    pt_input = torch.randn([2, 4, 6, 8], dtype=torch.float, device=device, requires_grad=True)
+    ort_input = copy.deepcopy(pt_input)
+    pt_prediction = run_step(pt_model, pt_input)
+    ort_prediction = run_step(ort_model, ort_input)
+
+    _test_helpers.assert_values_are_close(ort_prediction, pt_prediction)
+    _test_helpers.assert_values_are_close(ort_input.grad, pt_input.grad)
+
+
 def test_gradient_correctness_cast_chain():
     class NeuralNetCast(torch.nn.Module):
         def __init__(self, D):
