6df843003885280874eb43bc807ec0c84692ea17
Kunal Vaishnavi <kvaishnavi@microsoft.com>
2024-02-13 22:39:06 +0000

    Add copyright to Whisper scripts

--- onnxruntime/python/tools/transformers/models/whisper/whisper_chain.py
+++ onnxruntime/python/tools/transformers/models/whisper/whisper_chain.py
@@ -1,3 +1,9 @@
+# -------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation.  All rights reserved.
+# Licensed under the MIT License.  See License.txt in the project root for
+# license information.
+# --------------------------------------------------------------------------
+
 import logging
 import os
 
