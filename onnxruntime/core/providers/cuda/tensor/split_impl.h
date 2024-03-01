ce84e252beb39ff23160399f25fe9cb4ea11006f
kailums <kalu@microsoft.com>
2024-03-01 18:29:57 +0800

    add split3inner

--- onnxruntime/core/providers/cuda/tensor/split_impl.h
+++ onnxruntime/core/providers/cuda/tensor/split_impl.h
@@ -19,5 +19,9 @@ Status SplitImpl(cudaStream_t stream, const size_t element_size, const int block
                  const int64_t* axis_dimension_input_output_mapping, const int num_outputs, const void* input_data,
                  void** output_data, const size_t input_size);
 
+Status Split3Inner(cudaStream_t stream, const size_t element_size, const int64_t size0, const int64_t size1,
+                   const int64_t size2, const void* input_data, void* output_data0, void* output_data1,
+                   void* output_data2, const gsl::span<const int64_t>& input_shape);
+
 }  // namespace cuda
 }  // namespace onnxruntime
