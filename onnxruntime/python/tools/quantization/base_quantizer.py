dd2b9e346315d72f6797867b3f276716cbce4181
adrianlizarraga <adlizarraga@microsoft.com>
2024-03-06 23:49:07 -0800

    Remove convert logic from overrides checker

--- onnxruntime/python/tools/quantization/base_quantizer.py
+++ onnxruntime/python/tools/quantization/base_quantizer.py
@@ -595,46 +595,6 @@ class BaseQuantizer:
                                     f"Tensor override option '{key}' is invalid with 'scale' and 'zero_point'"
                                 )
 
-                    if "convert" in quant_overrides:
-                        if index > 0:
-                            raise ValueError(
-                                f"Per-channel overrides (tensor '{tensor_name}') do not support 'convert'."
-                            )
-
-                        if is_initializer:
-                            raise ValueError("Cannot use 'convert' override for initializers")
-
-                        if "quant_type" not in quant_overrides["convert"]:
-                            raise ValueError(f"'convert' options (tensor '{tensor_name}') must specify a 'quant_type'")
-
-                        convert_quant_type = quant_overrides["convert"]["quant_type"].tensor_type
-                        original_quant_type = (
-                            quant_type.tensor_type if quant_type is not None else self.activation_qType
-                        )
-                        if convert_quant_type == original_quant_type:
-                            raise ValueError(
-                                f"'convert' quant_type must differ from original quant_type (tensor '{tensor_name}')"
-                            )
-
-                        convert_has_scale = "scale" in quant_overrides["convert"]
-                        convert_has_zero_point = "zero_point" in quant_overrides["convert"]
-
-                        if (convert_has_scale and not convert_has_zero_point) or (
-                            convert_has_zero_point and not convert_has_scale
-                        ):
-                            raise ValueError(
-                                f"Must provide both 'scale' and 'zero_point' if one of the overrides is provided (tensor '{tensor_name}')"
-                            )
-
-                        if convert_has_scale:
-                            for key in keys_unsupported_with_scale_zp:
-                                if key in quant_overrides["convert"]:
-                                    raise ValueError(
-                                        f"Tensor override option '{key}' is invalid with 'scale' and 'zero_point' (tensor '{tensor_name}')"
-                                    )
-
-                        tensor_quant_override_types.add(convert_quant_type)
-
         return tensor_quant_overrides, tensor_quant_override_types
 
     def get_per_tensor_quant_overrides(self, tensor_name):
