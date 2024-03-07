604b1defb8e30e5231c216a873148e78f3d148bb
Scott McKay <Scott.McKay@microsoft.com>
2024-03-07 20:16:58 +1000

    Address PR comment

--- onnxruntime/test/util/default_providers.cc
+++ onnxruntime/test/util/default_providers.cc
@@ -226,7 +226,6 @@ std::unique_ptr<IExecutionProvider> DefaultRocmExecutionProvider(bool test_tunab
 std::unique_ptr<IExecutionProvider> DefaultCoreMLExecutionProvider(bool use_mlprogram) {
   // To manually test CoreML model generation on a non-macOS platform, comment out the `&& defined(__APPLE__)` below.
   // The test will create a model but execution of it will obviously fail.
-  // To test creating an ML Program, set the environment variable COREML_EP_TEST_MLPROGRAM to any value.
 #if defined(USE_COREML) && defined(__APPLE__)
   // We want to run UT on CPU only to get output value without losing precision
   uint32_t coreml_flags = 0;
