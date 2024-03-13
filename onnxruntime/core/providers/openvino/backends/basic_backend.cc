f71ca3adc7c9432857cf5c5cd2243eb7f39d4b70
Suryaprakash Shanmugam <suryaprakash.shanmugam@intel.com>
2024-03-12 23:15:14 -0700

    num_of_threads mapped to inference_num_threads property of CPU

--- onnxruntime/core/providers/openvino/backends/basic_backend.cc
+++ onnxruntime/core/providers/openvino/backends/basic_backend.cc
@@ -176,6 +176,10 @@ void BasicBackend::SetNumThreads(ov::AnyMap& device_config) {
     device_config.emplace(ov::inference_num_threads(global_context_.num_of_threads));
 }
 
+void BasicBackend::SetNumThreads(ov::AnyMap& device_config) {
+    device_config.emplace(ov::inference_num_threads(global_context_.num_of_threads));
+}
+
 // Starts an asynchronous inference request for data in slice indexed by batch_slice_idx on
 // an Infer Request indexed by infer_req_idx
 void BasicBackend::StartAsyncInference(Ort::KernelContext& context, OVInferRequestPtr infer_request) {
