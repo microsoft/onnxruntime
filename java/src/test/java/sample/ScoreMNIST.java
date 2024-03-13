6e6851facf57df4ec876d5510a65b95a7ec0278f
Adam Pocock <adam.pocock@oracle.com>
2024-03-12 20:37:34 -0400

    Bumping spotless and gradle wrapper to support building with Java 21.

--- java/src/test/java/sample/ScoreMNIST.java
+++ java/src/test/java/sample/ScoreMNIST.java
@@ -30,6 +30,7 @@ import java.util.regex.Pattern;
 public class ScoreMNIST {
 
   private static final Logger logger = Logger.getLogger(ScoreMNIST.class.getName());
+
   /** Pattern for splitting libsvm format files. */
   private static final Pattern splitPattern = Pattern.compile("\\s+");
 
