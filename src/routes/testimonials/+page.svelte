7e63a71122f80a681d61e709b70beb97ac10f474
MaanavD <maanavdalal@microsoft.com>
2024-03-19 14:04:31 -0700

    Small fix #2.

--- src/routes/testimonials/+page.svelte
+++ src/routes/testimonials/+page.svelte
@@ -31,19 +31,7 @@
 	import vespalogo from '../../images/logos/vespa-logo.png';
 	import writerlogo from '../../images/logos/writer-logo.png';
 	import xilinxlogo from '../../images/logos/xilinx-logo.png';
-	function offsetAnchor() {
-    if(location.hash.length !== 0) {
-        window.scrollTo(window.scrollX, window.scrollY - 100);
-    }
-}
 
-// This will capture hash changes while on the page
-window.addEventListener("hashchange", offsetAnchor);
-
-// This is here so that when you enter the page with a hash,
-// it can provide the offset in that case too. Having a timeout
-// seems necessary to allow the browser to jump to the anchor first.
-window.setTimeout(offsetAnchor, 1); 
 	const quotes = [
 		{
 			title: 'Adobe',
