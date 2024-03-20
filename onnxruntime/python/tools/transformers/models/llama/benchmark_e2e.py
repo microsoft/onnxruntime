f0ebf4dc553e1ecd479c57af61f93ba1600ef1b1
Kunal Vaishnavi <kvaishnavi@microsoft.com>
2024-03-20 21:03:22 +0000

    Add changes suggested by linter

--- onnxruntime/python/tools/transformers/models/llama/benchmark_e2e.py
+++ onnxruntime/python/tools/transformers/models/llama/benchmark_e2e.py
@@ -383,7 +383,11 @@ def main():
                 inputs["attention_mask"] = torch.cat(
                     [inputs["attention_mask"], (~has_eos).to(torch.int64).reshape(batch_size, 1)], 1
                 )
-                inputs["position_ids"] = None if "position_ids" not in inputs else torch.max(inputs["position_ids"], dim=1)[0].reshape(batch_size, 1) + 1
+                inputs["position_ids"] = (
+                    None
+                    if "position_ids" not in inputs
+                    else torch.max(inputs["position_ids"], dim=1)[0].reshape(batch_size, 1) + 1
+                )
 
                 # Set logits to zeros for next inference run and re-use memory buffer
                 if outputs["logits"].shape[1] != 1:
