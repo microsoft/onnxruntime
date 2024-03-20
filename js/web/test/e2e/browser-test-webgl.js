9dfc4460a87ea09773c58bc3cfe5a1149d130432
sevenc-nanashi <sevenc7c@sevenc7c.com>
2024-03-20 09:20:23 +0900

    Fix: Fix unit test

--- js/web/test/e2e/browser-test-webgl.js
+++ js/web/test/e2e/browser-test-webgl.js
@@ -6,3 +6,18 @@
 it('Browser E2E testing - WebGL backend', async function() {
   await testFunction(ort, {executionProviders: ['webgl']});
 });
+
+it('Browser E2E testing - invalid buffer', async () => {
+  try {
+    await ort.InferenceSession.create(
+      new Uint8Array(Array.from({length: 100}, () => 42)),
+      {executionProviders: ['webgl']}
+    );
+
+    // Should not reach here.
+    assert(false);
+  } catch (e) {
+    assert(e.message.includes("as ONNX format"));
+    assert(e.message.includes("as ORT format"));
+  }
+});
