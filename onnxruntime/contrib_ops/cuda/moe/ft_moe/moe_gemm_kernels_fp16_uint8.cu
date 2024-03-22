abec9b5b35a202c3d66e1d6397f0b2756662255c
Your <wangye@microsoft.com>
2024-03-22 16:34:25 +0000

    surpass 4200

--- onnxruntime/contrib_ops/cuda/moe/ft_moe/moe_gemm_kernels_fp16_uint8.cu
+++ onnxruntime/contrib_ops/cuda/moe/ft_moe/moe_gemm_kernels_fp16_uint8.cu
@@ -17,8 +17,11 @@
 #pragma warning(push)
 #pragma warning(disable : 4100)
 #pragma warning(disable : 4244)
+#pragma warning(disable : 4200)
 #endif
+
 #include "moe_gemm_kernels_template.h"
+
 #if defined(_MSC_VER)
 #pragma warning(pop)
 #endif
--- onnxruntime/contrib_ops/cuda/moe/ft_moe/moe_gemm_kernels_fp32_fp32.cu
+++ onnxruntime/contrib_ops/cuda/moe/ft_moe/moe_gemm_kernels_fp32_fp32.cu
@@ -17,6 +17,7 @@
 #pragma warning(push)
 #pragma warning(disable : 4100)
 #pragma warning(disable : 4244)
+#pragma warning(disable : 4200)
 #endif
 
 #include "moe_gemm_kernels_template.h"
