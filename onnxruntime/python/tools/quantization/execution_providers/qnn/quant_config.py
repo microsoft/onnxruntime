b8968b923adf5877f8cbc6ce7f408e1648e9c168
adrianlizarraga <adlizarraga@microsoft.com>
2024-03-22 01:03:48 -0700

    Add unit tests for qnn compatibility overrides

--- onnxruntime/python/tools/quantization/execution_providers/qnn/quant_config.py
+++ onnxruntime/python/tools/quantization/execution_providers/qnn/quant_config.py
@@ -72,118 +72,25 @@ def get_qnn_qdq_config(
     overrides_helper = TensorQuantOverridesHelper(copy.deepcopy(init_overrides) if init_overrides else {})
 
     if not overrides_helper.empty() and add_qtype_converts:
+        # Fix mixed-precision overrides.
         overrides_fixer = MixedPrecisionTensorQuantOverridesFixer.create_from_model(
             overrides_helper, model, activation_type
         )
         overrides_fixer.apply(activation_type, activation_symmetric)
 
-    # Setup quantization overrides for specific operator types
+    # Setup quantization overrides for specific operator types to ensure compatibility with QNN EP.
+    compat_overrides = QnnCompatibilityOverrides(
+        activation_type,
+        weight_type,
+        activation_symmetric,
+        weight_symmetric,
+        overrides_helper,
+        name_to_initializer,
+    )
+
     for node in model.graph.node:
         op_types.add(node.op_type)
-
-        if node.op_type == "MatMul" and weight_type in Q8_TYPES:
-            input_16bit_act = None
-            input_wgt = None
-
-            for input_name in node.input:
-                if input_name not in name_to_initializer:
-                    qtype = overrides_helper.get_node_input_qtype_info(
-                        input_name, node.name, activation_type
-                    ).quant_type
-                    if qtype in Q16_TYPES:
-                        input_16bit_act = input_name
-                else:
-                    input_wgt = input_name
-
-            # Override initializer to use the weight_type
-            if input_16bit_act and input_wgt:
-                did_update = overrides_helper.update_tensor_overrides(
-                    input_wgt,
-                    {"quant_type": weight_type, "symmetric": weight_symmetric},
-                    overwrite=False,
-                )
-
-                if not did_update:
-                    warn_unable_to_override(node, "quant_type/symmetric", input_wgt, "input weight")
-        elif node.op_type == "LayerNormalization" and weight_type in Q8_TYPES:
-            has_q16_activation = False
-            for input_name in node.input:
-                if input_name not in name_to_initializer:
-                    qtype = overrides_helper.get_node_input_qtype_info(
-                        input_name, node.name, activation_type
-                    ).quant_type
-                    if qtype in Q16_TYPES:
-                        has_q16_activation = True
-                        break
-
-            # Override initializers to use the weight_type. Don't override the bias input.
-            if has_q16_activation:
-                for i in range(2):
-                    input_name = node.input[i]
-                    if input_name in name_to_initializer:
-                        did_update = overrides_helper.update_tensor_overrides(
-                            input_name,
-                            {"quant_type": weight_type, "symmetric": weight_symmetric},
-                            overwrite=False,
-                        )
-
-                        if not did_update:
-                            warn_unable_to_override(node, "quant_type/symmetric", input_name, "input weight")
-
-        elif node.op_type == "Sigmoid":
-            output_type = overrides_helper.get_node_output_qtype_info(node.output[0], activation_type).quant_type
-
-            if output_type == QuantType.QUInt16:
-                did_update = overrides_helper.update_tensor_overrides(
-                    node.output[0],
-                    {
-                        "quant_type": output_type,
-                        "scale": np.array(1.0 / 65536.0, dtype=np.float32),
-                        "zero_point": np.array(0, dtype=np.uint16),
-                    },
-                    overwrite=False,
-                )
-                if not did_update:
-                    warn_unable_to_override(node, "quant_type/scale/zero_point", node.output[0], "output")
-            elif output_type == QuantType.QInt16:
-                did_update = overrides_helper.update_tensor_overrides(
-                    node.output[0],
-                    {
-                        "quant_type": output_type,
-                        "scale": np.array(1.0 / 32768.0, dtype=np.float32),
-                        "zero_point": np.array(0, dtype=np.int16),
-                    },
-                    overwrite=False,
-                )
-                if not did_update:
-                    warn_unable_to_override(node, "quant_type/scale/zero_point", node.output[0], "output")
-        elif node.op_type == "Tanh":
-            output_type = overrides_helper.get_node_output_qtype_info(node.output[0]).quant_type
-
-            if output_type == QuantType.QUInt16:
-                did_update = overrides_helper.update_tensor_overrides(
-                    node.output[0],
-                    {
-                        "quant_type": output_type,
-                        "scale": np.array(1.0 / 32768.0, dtype=np.float32),
-                        "zero_point": np.array(32768, dtype=np.uint16),
-                    },
-                    overwrite=False,
-                )
-                if not did_update:
-                    warn_unable_to_override(node, "quant_type/scale/zero_point", node.output[0], "output")
-            elif output_type == QuantType.QInt16:
-                did_update = overrides_helper.update_tensor_overrides(
-                    node.output[0],
-                    {
-                        "quant_type": output_type,
-                        "scale": np.array(1.0 / 32768.0, dtype=np.float32),
-                        "zero_point": np.array(0, dtype=np.int16),
-                    },
-                    overwrite=False,
-                )
-                if not did_update:
-                    warn_unable_to_override(node, "quant_type/scale/zero_point", node.output[0], "output")
+        compat_overrides.process_node(node)
 
     extra_options = {
         "MinimumRealRange": 0.0001,
@@ -207,3 +114,164 @@ def get_qnn_qdq_config(
         use_external_data_format=(model_has_external_data or model.ByteSize() >= MODEL_SIZE_THRESHOLD),
         extra_options=extra_options,
     )
+
+
+class QnnCompatibilityOverrides:
+    """
+    Helper that processes nodes to generate quantization overrides that make the resulting QDQ model
+    compatible with QNN EP.
+    """
+
+    def __init__(
+        self,
+        default_activation_qtype: QuantType,
+        default_weight_qtype: QuantType,
+        activation_symmetric: bool,
+        weight_symmetric: bool,
+        overrides: TensorQuantOverridesHelper,
+        initializers: dict[str, onnx.TensorProto],
+    ):
+        self.default_activation_qtype = default_activation_qtype
+        self.default_weight_qtype = default_weight_qtype
+        self.activation_symmetric = activation_symmetric
+        self.weight_symmetric = weight_symmetric
+        self.overrides = overrides
+        self.initializers = initializers
+
+        self.process_fns = {
+            "MatMul": self._process_matmul,
+            "LayerNormalization": self._process_layernorm,
+            "Sigmoid": self._process_sigmoid,
+            "Tanh": self._process_tanh,
+        }
+
+    def process_node(self, node: onnx.NodeProto):
+        process_fn = self.process_fns.get(node.op_type)
+
+        if process_fn is not None:
+            print(f"Processing {node.op_type}")
+            process_fn(node)
+
+    def _process_matmul(self, node: onnx.NodeProto):
+        """
+        Overrides MatMul's initializer input(s) to use the default weight type if:
+        - The default weight type is 8-bit
+        - One of the inputs is a 16-bit activation
+        """
+        assert node.op_type == "MatMul", f"Expected MatMul, but got {node.op_type}"
+        if self.default_weight_qtype not in Q8_TYPES:
+            return
+
+        input_16bit_act = None
+        input_wgt = None
+
+        for input_name in node.input:
+            if input_name and input_name not in self.initializers:
+                qtype = self.overrides.get_node_input_qtype_info(
+                    input_name, node.name, self.default_activation_qtype
+                ).quant_type
+                if qtype in Q16_TYPES:
+                    input_16bit_act = input_name
+            else:
+                input_wgt = input_name
+
+        # Override initializer to use the default weight type.
+        if input_16bit_act and input_wgt:
+            did_update = self.overrides.update_tensor_overrides(
+                input_wgt,
+                {"quant_type": self.default_weight_qtype, "symmetric": self.weight_symmetric},
+                overwrite=False,
+            )
+
+            if not did_update:
+                warn_unable_to_override(node, "quant_type/symmetric", input_wgt, "input weight")
+
+    def _process_layernorm(self, node: onnx.NodeProto):
+        """
+        Overrides LayerNormalization's initializer input(s), except for bias, to use the default weight type if:
+        - The default weight type is 8-bit
+        - One of the inputs is a 16-bit activation
+        """
+        assert node.op_type == "LayerNormalization", f"Expected LayerNormalization, but got {node.op_type}"
+        if self.default_weight_qtype not in Q8_TYPES:
+            return
+
+        has_q16_activation = False
+        for input_name in node.input:
+            if input_name and input_name not in self.initializers:
+                qtype = self.overrides.get_node_input_qtype_info(
+                    input_name, node.name, self.default_activation_qtype
+                ).quant_type
+                if qtype in Q16_TYPES:
+                    has_q16_activation = True
+                    break
+
+        # Override initializers to use the self.default_weight_qtype. Don't override the bias input.
+        if has_q16_activation:
+            for i in range(2):
+                input_name = node.input[i]
+                if input_name and input_name in self.initializers:
+                    did_update = self.overrides.update_tensor_overrides(
+                        input_name,
+                        {"quant_type": self.default_weight_qtype, "symmetric": self.weight_symmetric},
+                        overwrite=False,
+                    )
+
+                    if not did_update:
+                        warn_unable_to_override(node, "quant_type/symmetric", input_name, "input weight")
+
+    def _process_sigmoid(self, node: onnx.NodeProto):
+        """
+        Overrides 16-bit Sigmoid's output scale and zero-point as per QNN requirements.
+        """
+        assert node.op_type == "Sigmoid", f"Expected Sigmoid, but got {node.op_type}"
+        output_type = self.overrides.get_node_output_qtype_info(
+            node.output[0], self.default_activation_qtype
+        ).quant_type
+
+        if output_type == QuantType.QUInt16:
+            self.overrides.update_tensor_overrides(
+                node.output[0],
+                {
+                    "quant_type": output_type,
+                    "scale": np.array(1.0 / 65536.0, dtype=np.float32),
+                    "zero_point": np.array(0, dtype=np.uint16),
+                },
+            )
+        elif output_type == QuantType.QInt16:
+            self.overrides.update_tensor_overrides(
+                node.output[0],
+                {
+                    "quant_type": output_type,
+                    "scale": np.array(1.0 / 32768.0, dtype=np.float32),
+                    "zero_point": np.array(0, dtype=np.int16),
+                },
+            )
+
+    def _process_tanh(self, node: onnx.NodeProto):
+        """
+        Overrides 16-bit Tanh's output scale and zero-point as per QNN requirements.
+        """
+        assert node.op_type == "Tanh", f"Expected Tanh, but got {node.op_type}"
+        output_type = self.overrides.get_node_output_qtype_info(
+            node.output[0], self.default_activation_qtype
+        ).quant_type
+
+        if output_type == QuantType.QUInt16:
+            self.overrides.update_tensor_overrides(
+                node.output[0],
+                {
+                    "quant_type": output_type,
+                    "scale": np.array(1.0 / 32768.0, dtype=np.float32),
+                    "zero_point": np.array(32768, dtype=np.uint16),
+                },
+            )
+        elif output_type == QuantType.QInt16:
+            self.overrides.update_tensor_overrides(
+                node.output[0],
+                {
+                    "quant_type": output_type,
+                    "scale": np.array(1.0 / 32768.0, dtype=np.float32),
+                    "zero_point": np.array(0, dtype=np.int16),
+                },
+            )
