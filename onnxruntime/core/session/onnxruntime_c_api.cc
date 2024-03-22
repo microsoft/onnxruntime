dbf1a8cd39446528a31bf05b59fcc1c383d99c99
Yulong Wang <7679871+fs-eire@users.noreply.github.com>
2024-03-21 18:08:11 -0700

    Rebase rel-1.17.3 (#20006)
    ### Description
    
    the release branch `rel-1.17.3` is created based on `rel-1.17.2` last
    week. However, there are latest code change merged into `rel-1.17.2`:
    #19897. The branch `rel-1.17.3` is protected so no push or delete can be
    performed on it.
    
    This PR cherry-picks the commit 633c22f based on 6bc6adc to make sure
    the base of `rel-1.17.3` matches `rel-1.17.2`.
    
    @snnn @pranavsharma This operation will ensure the code base contains
    same code, but the git history will not be exactly same. If you want it
    to be exactly same, I need your help to do a git rebase or delete and
    recreate the branch.
    
    Co-authored-by: Rachel Guo <35738743+YUNQIUGUO@users.noreply.github.com>
    Co-authored-by: rachguo <rachguo@rachguos-Mini.attlocal.net>
    Co-authored-by: George Wu <jywu@microsoft.com>

--- onnxruntime/core/session/onnxruntime_c_api.cc
+++ onnxruntime/core/session/onnxruntime_c_api.cc
@@ -2724,6 +2724,7 @@ static constexpr OrtApi ort_api_1_to_17 = {
     &OrtApis::SetDeterministicCompute,
     &OrtApis::KernelContext_ParallelFor,
     &OrtApis::SessionOptionsAppendExecutionProvider_OpenVINO_V2,
+    // End of Version 17 - DO NOT MODIFY ABOVE (see above text for more information)
 };
 
 // OrtApiBase can never change as there is no way to know what version of OrtApiBase is returned by OrtGetApiBase.
@@ -2753,7 +2754,7 @@ static_assert(offsetof(OrtApi, ReleaseCANNProviderOptions) / sizeof(void*) == 22
 static_assert(offsetof(OrtApi, GetSessionConfigEntry) / sizeof(void*) == 238, "Size of version 14 API cannot change");
 static_assert(offsetof(OrtApi, GetBuildInfoString) / sizeof(void*) == 254, "Size of version 15 API cannot change");
 static_assert(offsetof(OrtApi, KernelContext_GetResource) / sizeof(void*) == 265, "Size of version 16 API cannot change");
-static_assert(offsetof(OrtApi, SetUserLoggingFunction) / sizeof(void*) == 266, "Size of version 17 API cannot change");
+static_assert(offsetof(OrtApi, SessionOptionsAppendExecutionProvider_OpenVINO_V2) / sizeof(void*) == 275, "Size of version 17 API cannot change");
 
 // So that nobody forgets to finish an API version, this check will serve as a reminder:
 static_assert(std::string_view(ORT_VERSION) == "1.17.2",
