046d06ff2694c2ef267dbf612871578d03481fbc
Rachel Guo <35738743+YUNQIUGUO@users.noreply.github.com>
2024-03-29 13:10:13 -0700

    Cherry-pick for 1.17.3 (#20013)
    ### Description
    <!-- Describe your changes. -->
    
    Web prs are not included yet.
    
    ### Motivation and Context
    <!-- - Why is this change required? What problem does it solve?
    - If it fixes an open issue, please link to the issue here. -->
    
    ---------
    
    Co-authored-by: Yufeng Li <liyufeng1987@gmail.com>
    Co-authored-by: Maximilian Müller <44298237+gedoensmax@users.noreply.github.com>
    Co-authored-by: Yi Zhang <zhanyi@microsoft.com>
    Co-authored-by: Your Name <your@email.com>
    Co-authored-by: Edward Chen <18449977+edgchen1@users.noreply.github.com>
    Co-authored-by: enximi <70036307+enximi@users.noreply.github.com>
    Co-authored-by: George Wu <jywu@microsoft.com>
    Co-authored-by: Markus Tavenrath <mtavenrath@users.noreply.github.com>
    Co-authored-by: Tianlei Wu <tlwu@microsoft.com>
    Co-authored-by: rachguo <rachguo@rachguos-Mini.attlocal.net>
    Co-authored-by: Adam Pocock <adam.pocock@oracle.com>
    Co-authored-by: aciddelgado <139922440+aciddelgado@users.noreply.github.com>
    Co-authored-by: kunal-vaishnavi <115581922+kunal-vaishnavi@users.noreply.github.com>
    Co-authored-by: Changming Sun <chasun@microsoft.com>
    Co-authored-by: Chi Lo <54722500+chilo-ms@users.noreply.github.com>

--- onnxruntime/core/session/onnxruntime_c_api.cc
+++ onnxruntime/core/session/onnxruntime_c_api.cc
@@ -2757,7 +2757,7 @@ static_assert(offsetof(OrtApi, KernelContext_GetResource) / sizeof(void*) == 265
 static_assert(offsetof(OrtApi, SessionOptionsAppendExecutionProvider_OpenVINO_V2) / sizeof(void*) == 275, "Size of version 17 API cannot change");
 
 // So that nobody forgets to finish an API version, this check will serve as a reminder:
-static_assert(std::string_view(ORT_VERSION) == "1.17.2",
+static_assert(std::string_view(ORT_VERSION) == "1.17.3",
               "ORT_Version change detected, please follow below steps to ensure OrtApi is updated properly");
 // 1. Update the hardcoded version string in above static_assert to silence it
 // 2. If there were any APIs added to ort_api_1_to_17 above:
