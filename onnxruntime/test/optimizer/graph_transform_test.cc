9d06e1bfa469bce92d7aa2ebfa36c7a9714ba119
Atanas Dimitrov <70822030+neNasko1@users.noreply.github.com>
2024-04-01 17:41:10 +0100

    Label encoder fusion (#19761)
    ### Description
    Created a new `LabelEncoderFusion` pass. This is useful in model that
    result from automatic conversion tools related to data-science:
    sometimes the produced model contains consecutive `LabelEncoder`-s.
    To merge 2 `LabelEncoder`-s the optimizer propagates the outputs of the
    first encoder through the second one.
    
    
    ### Motivation and Context
    This enhances the capabilities of the `onnxruntime::optimizer` by fusing
    consecutive `LabelEncoder` nodes.
    
    
    ### Fusion examples
    ```
    Applying fusion
    node1: (a,C) (b,B) (c,A) -> Default: _Unused
    node2: (A,1) (B,2) (C,3) -> Default: -1
    fused: (a,3) (b,2) (c,1) -> Default: -1
    Applying fusion
    node1: (a,C) (b,B) (c,A) -> Default: D
    node2: (A,a) (B,b) (C,c) (D,d) -> Default: default
    fused: (a,c) (b,b) (c,a) -> Default: d
    Applying fusion
    node1: (a,0) (b,1) (c,2) -> Default: -1
    node2: (2,a) (1,b) (0,c) -> Default: default
    fused: (a,c) (b,b) (c,a) -> Default: default
    Applying fusion
    node1: (a,3) (b,2) (c,1) -> Default: -1
    node2: (1,a) (2,b) (3,c) -> Default: d
    fused: (a,c) (b,b) (c,a) -> Default: d
    ```
    
    ---------
    
    Co-authored-by: Justin Chu <justinchuby@users.noreply.github.com>

--- onnxruntime/test/optimizer/graph_transform_test.cc
+++ onnxruntime/test/optimizer/graph_transform_test.cc
@@ -69,6 +69,7 @@
 #include "core/optimizer/slice_elimination.h"
 #include "core/optimizer/unsqueeze_elimination.h"
 #include "core/optimizer/utils.h"
+#include "core/optimizer/label_encoder_fusion.h"
 #include "core/platform/env.h"
 #include "core/session/inference_session.h"
 #include "core/session/onnxruntime_session_options_config_keys.h"
@@ -1899,6 +1900,68 @@ TEST_F(GraphTransformationTests, DivMulFusion) {
   op_to_count = CountOpsInGraph(graph);
   ASSERT_TRUE(op_to_count["Div"] == 5);
   ASSERT_TRUE(op_to_count["Mul"] == 2);
+}
+
+TEST_F(GraphTransformationTests, LabelEncoderFusion) {
+  using common::INVALID_GRAPH;
+  constexpr const ORTCHAR_T* model_uri = MODEL_FOLDER "fusion/label_encoder.onnx";
+
+  NameMLValMap feeds;
+
+  constexpr size_t ALPH = 26;
+  OrtValue mlvalue_a;
+  std::vector<int64_t> dims_a = {ALPH};
+  std::vector<std::string> values_a = {};
+  for (char letter = 'a'; letter <= 'z'; letter++) {
+    values_a.emplace_back(1, letter);
+  }
+  CreateMLValue<std::string>(TestCPUExecutionProvider()->CreatePreferredAllocators()[0], dims_a,
+                             values_a, &mlvalue_a);
+  feeds.insert(std::make_pair("A", mlvalue_a));
+
+  bool is_implemented = true;
+
+  auto run_model_test = [&](TransformerLevel level, std::vector<OrtValue>& fetches, const int requiredLabelEncoderCount) {
+    SessionOptions session_options;
+    session_options.graph_optimization_level = level;
+    session_options.session_logid = "OptimizerTests";
+    InferenceSessionWrapper session{session_options, GetEnvironment()};
+
+    // If we did not initialize the session correctly, the operator is missing.
+    if (!session.Load(model_uri).IsOK() || !session.Initialize().IsOK()) {
+      is_implemented = false;
+      return;
+    }
+
+    // Count if the number of LabelEncoders is as expected
+    std::map<std::string, int> op_to_count = CountOpsInGraph(session.GetGraph());
+    ASSERT_TRUE(op_to_count["ai.onnx.ml.LabelEncoder"] == requiredLabelEncoderCount);
+
+    std::vector<std::string> output_names = {};
+    for (const auto& output : session.GetGraph().GetOutputs()) {
+      output_names.push_back(output->Name());
+    }
+
+    RunOptions run_options;
+    ASSERT_STATUS_OK(session.Run(run_options, feeds, output_names, &fetches));
+  };
+
+  // run model with and w/o optimizations and compare the results
+  std::vector<OrtValue> unoptimized_fetches;
+  run_model_test(TransformerLevel::Default, unoptimized_fetches, 11);
+
+  std::vector<OrtValue> optimized_fetches;
+  run_model_test(TransformerLevel::MaxLevel, optimized_fetches, 7);
+
+  // If there was a problem loading the model, do not compare the 2 results
+  if (!is_implemented) {
+    GTEST_SKIP();
+    return;
+  }
+
+  // Compare results
+  auto ret = CompareOrtValue(optimized_fetches[0], unoptimized_fetches[0], 0.0, 0.0, false);
+  EXPECT_EQ(ret.first, COMPARE_RESULT::SUCCESS) << ret.second;
 }
 
 TEST_F(GraphTransformationTests, NotWhereFusion) {
