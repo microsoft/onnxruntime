d243fb1706d9c8f59c9269c812db5e1deececf6a
yf711 <yifanl@microsoft.com>
2024-04-08 22:41:53 -0700

    test

--- onnxruntime/test/providers/cpu/model_tests.cc
+++ onnxruntime/test/providers/cpu/model_tests.cc
@@ -612,7 +612,7 @@ static constexpr ORT_STRING_VIEW provider_name_dml = ORT_TSTR("dml");
       ORT_TSTR("SSD-MobilenetV1-12"),  // symbolic shape inference shape error
       ORT_TSTR("SSD"),                 // needs to run symbolic shape inference shape first
       ORT_TSTR("size")                 // INVALID_ARGUMENT: Cannot find binding of given name: x
-      ORT_TSTR("resnet_v2_50")         // TensorRT EP output tensor data type: 0 not supported
+      ORT_TSTR("resnet")         // TensorRT EP output tensor data type: 0 not supported
   };
   std::vector<std::filesystem::path> paths;
 
