546c26edca96b39bda999d883ffb6e49fdf24661
Satya Jandhyala <satya.k.jandhyala@gmail.com>
2024-04-09 18:10:50 -0700

    Revert "Added bBlocksPerCol and blobSize to attributes to avoid recalculating."
    This reverts commit ce73fc37ca3faad87f3e1d2602067f9da6adee92.

--- js/web/lib/wasm/jsep/webgpu/ops/matmulnbits.ts
+++ js/web/lib/wasm/jsep/webgpu/ops/matmulnbits.ts
@@ -16,8 +16,6 @@ export interface MatMulNBitsAttributes extends AttributeWithCacheKey {
   accuracyLevel: number;
   bits: number;
   blockSize: number;
-  blobSize: number;
-  nBlocksPerCol: number;
 }
 
 const validateInputs = (inputs: readonly TensorView[], attributes: MatMulNBitsAttributes): void => {
@@ -29,20 +27,22 @@ const validateInputs = (inputs: readonly TensorView[], attributes: MatMulNBitsAt
   if (a.dims[aRank - 1] !== attributes.k) {
     throw new Error('The last dim of input shape does not match the k value');
   }
+  const nBlocksPerCol = Math.floor((attributes.k + attributes.blockSize - 1) / attributes.blockSize);
+  const blobSize = attributes.blockSize / 8 * attributes.bits;
   const b = inputs[1];
-  if (!ShapeUtil.areEqual(b.dims, [attributes.n, attributes.nBlocksPerCol, attributes.blobSize])) {
+  if (!ShapeUtil.areEqual(b.dims, [attributes.n, nBlocksPerCol, blobSize])) {
     throw new Error('The second inputs must be 3D tensor with shape N X nBlocksPerCol X blobSize');
   }
   const scales = inputs[2];
   const scalesShape = scales.dims;
-  if (ShapeUtil.size(scalesShape) !== attributes.n * attributes.nBlocksPerCol) {
+  if (ShapeUtil.size(scalesShape) !== attributes.n * nBlocksPerCol) {
     throw new Error('scales input size error.');
   }
   if (inputs.length === 4) {
     const zeroPoints = inputs[3];
     const zeroPointsShape = zeroPoints.dims;
-    const expectedZeroPointsSize = attributes.bits > 4 ? (attributes.n * attributes.nBlocksPerCol) :
-                                                         attributes.n * Math.floor((attributes.nBlocksPerCol + 1) / 2);
+    const expectedZeroPointsSize =
+        attributes.bits > 4 ? (attributes.n * nBlocksPerCol) : attributes.n * Math.floor((nBlocksPerCol + 1) / 2);
     if (ShapeUtil.size(zeroPointsShape) !== expectedZeroPointsSize) {
       throw new Error('zeroPoints input size error.');
     }
@@ -54,13 +54,13 @@ export const createMatMulNBitsProgramInfo =
      maxComputeWorkgroupSizes: [number, number, number], maxComputeWorkgroupStorageSize: number): ProgramInfo => {
       const inputShape = inputs[0].dims;
       const aRank = inputShape.length;
-      const nBlocksPerCol = attributes.nBlocksPerCol;
+      const nBlocksPerCol = Math.floor((attributes.k + attributes.blockSize - 1) / attributes.blockSize);
       const dimAOuter = inputShape[aRank - 2];
       const dimInner = attributes.k;
       const dimBOuter = attributes.n;
       const batchDims = inputShape.slice(0, aRank - 2);
       const batchSize = ShapeUtil.size(batchDims);
-      const blobSize = attributes.blobSize;
+      const blobSize = attributes.blockSize / 8 * attributes.bits;
       const blobSizeInWords = blobSize / 4;
       const dataType = inputs[0].dataType;
       const outputNumber = getMaxComponents(dimAOuter);
@@ -68,7 +68,7 @@ export const createMatMulNBitsProgramInfo =
       const bComponents = getMaxComponents(blobSizeInWords);
       const elementSize = getTensorElementSize(dataType)!;
       const workgroupOutputSize = dimAOuter * nBlocksPerCol * elementSize;
-      const maxNumberOfComponents = Math.ceil(maxComputeWorkgroupStorageSize / workgroupOutputSize);
+      const maxNumberOfComponents = Math.floor(maxComputeWorkgroupStorageSize / workgroupOutputSize);
       const components = getMaxComponents(dimBOuter, maxNumberOfComponents);
       const useBlockwiseMatMulNBits = (nBlocksPerCol <= maxComputeWorkgroupSizes[0]) &&
           (workgroupOutputSize * components) <= maxComputeWorkgroupStorageSize;
@@ -299,13 +299,5 @@ export const matMulNBits = (context: ComputeContext, attributes: MatMulNBitsAttr
       context.inputs, attributes, maxComputeWorkgroupSizes, maxComputeWorkgroupStorageSize));
 };
 
-export const parseMatMulNBitsAttributes = (attributes: Record<string, unknown>): MatMulNBitsAttributes => {
-  const k = attributes.k as number;
-  const n = attributes.n as number;
-  const accuracyLevel = attributes.accuracyLevel as number;
-  const bits = attributes.bits as number;
-  const blockSize = attributes.blockSize as number;
-  const blobSize = blockSize / 8 * bits;
-  const nBlocksPerCol = Math.floor((k + blockSize - 1) / blockSize);
-  return createAttributeWithCacheKey({k, n, accuracyLevel, bits, blockSize, blobSize, nBlocksPerCol});
-};
+export const parseMatMulNBitsAttributes = (attributes: Record<string, unknown>): MatMulNBitsAttributes =>
+    createAttributeWithCacheKey(attributes as Omit<MatMulNBitsAttributes, keyof AttributeWithCacheKey>);
