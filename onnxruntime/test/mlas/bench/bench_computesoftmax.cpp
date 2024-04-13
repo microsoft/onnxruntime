a61d82ecc535eac65b4cbfa4c06d7e19fb5ce82a
Yi-Hong Lyu <yilyu@microsoft.com>
2024-04-13 10:23:22 +0000

    Fix Lint C++ warnings

--- onnxruntime/test/mlas/bench/bench_computesoftmax.cpp
+++ onnxruntime/test/mlas/bench/bench_computesoftmax.cpp
@@ -1,9 +1,9 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-#include "bench_util.h"
 #include "core/mlas/lib/mlasi.h"
 #include "core/util/thread_utils.h"
+#include "test/mlas/bench/bench_util.h"
 
 using onnxruntime::narrow;
 
