2cfd84bbfcd62bd9f965c49f980d55e948c96001
aamajumder <150728138+aamajumder@users.noreply.github.com>
2024-04-22 12:01:59 -0700

    [DML EP] Register opset 20 operators (#20092)
    ### Description
    This PR registers the following opset 20 operators to the DML EP:
    -IsNaN-20
    -IsInf-20
    -ReduceMax-20
    
    
    ### Motivation and Context
    <!-- - Why is this change required? What problem does it solve?
    - If it fixes an open issue, please link to the issue here. -->

--- onnxruntime/core/providers/dml/OperatorAuthorHelper/OperatorVersions.h
+++ onnxruntime/core/providers/dml/OperatorAuthorHelper/OperatorVersions.h
@@ -429,6 +429,13 @@ namespace OperatorHelper
         static const int sc_sinceVer_Size = 19;
     }
 
+    namespace OnnxOperatorSet20
+    {
+        static const int sc_sinceVer_IsNaN = 20;
+        static const int sc_sinceVer_IsInf = 20;
+        static const int sc_sinceVer_ReduceMax = 20;
+    }
+
     namespace MsftOperatorSet1
     {
         static const int sc_sinceVer_DmlFusedConv = 1;
