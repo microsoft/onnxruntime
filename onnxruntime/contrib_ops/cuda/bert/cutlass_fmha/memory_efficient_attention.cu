6a760e79ec9b392309d59a1104dbde4512aabb29
Your <wangye@microsoft.com>
2024-04-23 11:41:55 -0700

    Revert "conditionally upgrade to cutlass 3.3.0"
    This reverts commit 3d6a1f18c3eea6a10f512bd5429a5d4f09a28edc.

--- onnxruntime/contrib_ops/cuda/bert/cutlass_fmha/memory_efficient_attention.cu
+++ onnxruntime/contrib_ops/cuda/bert/cutlass_fmha/memory_efficient_attention.cu
@@ -9,19 +9,18 @@ namespace contrib {
 namespace cuda {
 
 void run_memory_efficient_attention(const MemoryEfficientAttentionParams& params) {
-  // const int32_t& sm = params.sm;
-  // if (sm >= 80) {
-  //   run_memory_efficient_attention_sm80(params);
-  // } else if (sm >= 75) {
-  //   run_memory_efficient_attention_sm75(params);
-  // } else if (sm >= 70) {
-  //   run_memory_efficient_attention_sm70(params);
-  // } else if (sm >= 50) {
-  //   run_memory_efficient_attention_sm50(params);
-  // } else {
-  //   assert(false);  // shall not reach here.
-  // }
-  ;
+  const int32_t& sm = params.sm;
+  if (sm >= 80) {
+    run_memory_efficient_attention_sm80(params);
+  } else if (sm >= 75) {
+    run_memory_efficient_attention_sm75(params);
+  } else if (sm >= 70) {
+    run_memory_efficient_attention_sm70(params);
+  } else if (sm >= 50) {
+    run_memory_efficient_attention_sm50(params);
+  } else {
+    assert(false);  // shall not reach here.
+  }
 }
 
 }  // namespace cuda
