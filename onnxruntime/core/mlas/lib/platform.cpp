1d88398eca3ea0d4cc9def4778d02b015c60649b
liqunfu <liqun.fu@microsoft.com>
2024-04-23 18:14:28 -0700

    bring back avx512 and vnni
    Signed-off-by: liqunfu <liqun.fu@microsoft.com>

--- onnxruntime/core/mlas/lib/platform.cpp
+++ onnxruntime/core/mlas/lib/platform.cpp
@@ -440,7 +440,7 @@ Return Value:
                         this->GemmU8U8Kernel = MlasGemmU8U8KernelAvx512Core;
                         this->ConvSymU8S8Dispatch = &MlasConvSymDispatchAvx512Core;
                         this->FpQ4GemmDispatch = &MlasFpQ4GemmDispatchAvx512;
-                        // this->SQNBitGemmDispatch = &MlasSQNBitGemmDispatchAvx512;
+                        this->SQNBitGemmDispatch = &MlasSQNBitGemmDispatchAvx512;
 
                         //
                         // Check if the processor supports AVX512VNNI.
@@ -453,7 +453,7 @@ Return Value:
                             this->GemvU8S8Kernel = MlasGemvU8S8KernelAvx512Vnni;
                             this->ConvSymU8S8Dispatch = &MlasConvSymDispatchAvx512Vnni;
                             this->Q8Q4GemmDispatch = &MlasQ8Q4GemmDispatchAvx512vnni;
-                            // this->SQNBitGemmDispatch = &MlasSQNBitGemmDispatchAvx512vnni;
+                            this->SQNBitGemmDispatch = &MlasSQNBitGemmDispatchAvx512vnni;
                         }
                     }
                 }
