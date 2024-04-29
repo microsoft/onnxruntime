0c5af3ed9438fbde7d04d47ee8e31ace240b5706
Satya Jandhyala <satya.k.jandhyala@gmail.com>
2024-04-29 11:32:29 -0700

    Distribute writing-to-output work over all threads.

--- js/web/lib/wasm/jsep/webgpu/ops/matmulnbits.ts
+++ js/web/lib/wasm/jsep/webgpu/ops/matmulnbits.ts
@@ -211,17 +211,20 @@ export const createMatMulNBitsProgramInfo =
           var output_indices: ${output.type.indices};
           ${output.indicesSet('output_indices', '0', 'batch')};
           ${output.indicesSet('output_indices', outputRank - 1, 'col')};
-          ${output.indicesSet('output_indices', outputRank - 2, '0')};
-          var output_offset = ${output.indicesToOffset('output_indices')};
-          for (var m: u32 = 0u; m < ${dimAOuter}u; m++) {
-            var output_value: ${output.type.value} = ${output.type.value}(0);
-            var workgroup_shared_offset: u32 = m;
-            for (var b: u32 = 0u; b < ${nBlocksPerCol}u; b++) {
-              output_value += workgroup_shared[workgroup_shared_offset];
-              workgroup_shared_offset += ${dimAOuter};
+          var loop_count: u32 = ${Math.ceil(dimAOuter / nBlocksPerCol)};
+          for (var m: u32 = 0u; m < loop_count; m++) {
+            var row = m + local_id.x * loop_count;
+            if (row < ${dimAOuter}) {
+              ${output.indicesSet('output_indices', outputRank - 2, 'row')};
+              var output_offset = ${output.indicesToOffset('output_indices')};
+              var output_value: ${output.type.value} = ${output.type.value}(0);
+              var workgroup_shared_offset: u32 = row;
+              for (var b: u32 = 0u; b < ${nBlocksPerCol}u; b++) {
+                output_value += workgroup_shared[workgroup_shared_offset];
+                workgroup_shared_offset += ${dimAOuter};
+              }
+              ${output.setByOffset('output_offset', 'output_value')};
             }
-            ${output.setByOffset('output_offset', 'output_value')};
-            output_offset += ${dimBOuter / components};
           }
         }` :
                                          `
