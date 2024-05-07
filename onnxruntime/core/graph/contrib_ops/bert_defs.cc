c99518fc9c88a211315d519e33289775a2f254a1
aciddelgado <139922440+aciddelgado@users.noreply.github.com>
2024-05-07 15:19:26 -0700

    fix gqa cpu nan bug (#20521)
    ### Description
    There was a bug with gqa on cpu where on token case, with batch_size >
    1, and with past_present_share_buffer off, the output would occasionally
    contain nans. this pr fixes that. it also updates documentation and
    fixes posid gen for rotary in cuda in prompt case.
    
    
    
    ### Motivation and Context
    this pr solves the GQA CPU bug as well as updates the documentation and
    makes seqlens_k irrelevant for prompt case, which is useful to prevent
    user error.

--- onnxruntime/core/graph/contrib_ops/bert_defs.cc
+++ onnxruntime/core/graph/contrib_ops/bert_defs.cc
@@ -1046,10 +1046,11 @@ ONNX_MS_OPERATOR_SET_SCHEMA(
 constexpr const char* GroupQueryAttention_ver1_doc = R"DOC(
 Group Query Self/Cross Attention.
 
-Supports different number of heads for q and kv. Only supports causal or local attention.
-Supports rotary position embedding.
-Supports k-v cache.
-CPU EP supports fp32... CUDA EP supports fp16.
+*Highly recommend using k-v cache share buffer for both CPU and CUDA. Enabled through IOBinding past and present kv.
+Supports different number of heads for q and kv for CPU and CUDA.
+Only supports causal and local attention.
+Supports rotary position embedding for CPU and CUDA.
+Supports packed input for CPU and CUDA.
 )DOC";
 
 ONNX_MS_OPERATOR_SET_SCHEMA(
