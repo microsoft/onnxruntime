bd520fa11a0661c742993f33f0751964077598ab
maggie1059 <34173352+maggie1059@users.noreply.github.com>
2024-05-08 09:48:29 -0700

    Revert compute queue default for DML (#20604)
    ### Description
    <!-- Describe your changes. -->
    
    
    
    ### Motivation and Context
    <!-- - Why is this change required? What problem does it solve?
    - If it fixes an open issue, please link to the issue here. -->

--- onnxruntime/core/providers/dml/dml_provider_factory.cc
+++ onnxruntime/core/providers/dml/dml_provider_factory.cc
@@ -542,10 +542,7 @@ static D3D12_COMMAND_LIST_TYPE CalculateCommandListType(ID3D12Device* d3d12_devi
       sizeof(feature_levels)
       ));
 
-  // Use compute queue whenever possible on supported hardware to avoid TDR and maintain UI QoS
-  // Core and generic devices only have compute queues, DX11 has "immediate" submission, DX12 has both
-  auto use_compute_command_list = (feature_levels.MaxSupportedFeatureLevel <= D3D_FEATURE_LEVEL_1_0_CORE) ||
-                                  (feature_levels.MaxSupportedFeatureLevel >= D3D_FEATURE_LEVEL_12_0);
+  auto use_compute_command_list = (feature_levels.MaxSupportedFeatureLevel <= D3D_FEATURE_LEVEL_1_0_CORE);
 
   if (use_compute_command_list)
   {
