e73b69ac40e043b8eaa056c885a7ab2f56b7ff28
Jian Chen <cjian@microsoft.com>
2024-05-10 13:59:13 -0700

    --gpus all -e NVIDIA_VISIBLE_DEVICES=all

--- tools/ci_build/github/linux/build_cuda_c_api_package.sh
+++ tools/ci_build/github/linux/build_cuda_c_api_package.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e -x
-docker run --gpus all -e NVIDIA_VISIBLE_DEVICES=all --rm --volume \
+docker run --rm --volume \
 $BUILD_SOURCESDIRECTORY:/onnxruntime_src --volume $BUILD_BINARIESDIRECTORY:/build \
 --volume /data/models:/build/models:ro --volume /data/onnx:/data/onnx:ro -e NIGHTLY_BUILD onnxruntimecuda${CUDA_VERSION_MAJOR}build \
 /usr/bin/python3.9 /onnxruntime_src/tools/ci_build/build.py --enable_lto --build_java --build_nodejs --build_dir /build --config Release \
