3601ddeaa42b5d7d3a2c2c8ed2a706f2d9415af1
Jian Chen <cjian@microsoft.com>
2024-05-10 15:14:33 -0700

    nuget/templates/test_linux.yml

--- tools/ci_build/github/linux/build_cuda_c_api_package.sh
+++ tools/ci_build/github/linux/build_cuda_c_api_package.sh
@@ -6,4 +6,5 @@ $BUILD_SOURCESDIRECTORY:/onnxruntime_src --volume $BUILD_BINARIESDIRECTORY:/buil
 /usr/bin/python3.9 /onnxruntime_src/tools/ci_build/build.py --enable_lto --build_java --build_nodejs --build_dir /build --config Release \
 --skip_submodule_sync  --parallel --use_binskim_compliant_compile_flags --build_shared_lib --use_cuda --cuda_version=$CUDA_VERSION \
 --cuda_home=/usr/local/cuda-$CUDA_VERSION --cudnn_home=/usr/local/cuda-$CUDA_VERSION \
+--skip_tests \
 --cmake_extra_defines 'CMAKE_CUDA_ARCHITECTURES=60;61;70;75;80'
