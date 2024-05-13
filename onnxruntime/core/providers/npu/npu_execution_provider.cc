db5a0a0ecdb3d5809bd519b0153cb8292118361a
wilderfield <b.lozano.havoc@gmail.com>
2024-05-12 22:00:24 -0700

    Fix bug

--- onnxruntime/core/providers/npu/npu_execution_provider.cc
+++ onnxruntime/core/providers/npu/npu_execution_provider.cc
@@ -1,17 +1,19 @@
 
 
 #include "core/providers/npu/npu_execution_provider.h"
+#include "core/graph/graph_viewer.h"
+#include "core/framework/compute_capability.h"
 
 namespace onnxruntime {
 NPUExecutionProvider::NPUExecutionProvider(const NPUExecutionProviderInfo& info)
     : IExecutionProvider{onnxruntime::kNpuExecutionProvider}, info_{info} {}
 
 std::vector<std::unique_ptr<ComputeCapability>>
-NPUExecutionProvider::GetCapability(const onnxruntime::GraphViewer& graph,
-              const IKernelLookup& kernel_lookup) const {
-  for (const auto& node : graph.Nodes()) {
+NPUExecutionProvider::GetCapability(const onnxruntime::GraphViewer& graph_viewer, const IKernelLookup& /*kernel_lookup*/) const {
+
+  for (const auto& node : graph_viewer.Nodes()) {
     std::cout << " Node Name : " << node.Name() << std::endl;
-    std::cout << " Node OpType : " << node.opType() << std::endl;
+    std::cout << " Node OpType : " << node.OpType() << std::endl;
     std::cout << " Node Domain : " << node.Domain() << std::endl;
     std::cout << " Input Count : " << node.InputDefs().size() << std::endl;
     std::cout << " Output Count : " << node.OutputDefs().size() << std::endl;
