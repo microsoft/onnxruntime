935fb347f5c6243399f1761c5af9fdab01993aa4
Suryaprakash Shanmugam <suryaprakash.shanmugam@intel.com>
2024-05-15 22:46:26 -0700

    Fix ordering of value info in GraphProto creation

--- onnxruntime/core/graph/graph.cc
+++ onnxruntime/core/graph/graph.cc
@@ -4062,7 +4062,11 @@ void Graph::ToGraphProtoInternal(ONNX_NAMESPACE::GraphProto& graph_proto) const 
     *(graph_proto.mutable_output()->Add()) = output_arg->ToProto();
   }
 
-  for (const auto* value_info : value_info_) {
+  auto sort_predicate = [](const NodeArg* v1, const NodeArg* v2) {return v1->Name() < v2->Name();};
+  std::vector<const NodeArg*> value_info_set{value_info_.begin(), value_info_.end()};
+  std::sort(value_info_set.begin(), value_info_set.end(), sort_predicate);
+
+  for (const auto* value_info : value_info_set) {
     *(graph_proto.mutable_value_info()->Add()) = value_info->ToProto();
   }
 
