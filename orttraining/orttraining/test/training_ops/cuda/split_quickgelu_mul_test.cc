7bf00027907cfcd2f600980d19eb11b9b1fac7bf
Abhishek Jindal <abjindal@microsoft.com>
2024-05-21 15:39:59 -0700

    modify test name

--- orttraining/orttraining/test/training_ops/cuda/split_quickgelu_mul_test.cc
+++ orttraining/orttraining/test/training_ops/cuda/split_quickgelu_mul_test.cc
@@ -9,7 +9,7 @@ namespace test {
 
 // #if defined(USE_CUDA) || defined(USE_ROCM)
 
-TEST(S2SModelSplitQuickGelu, Int32Type2D) {
+TEST(S2SModelSplitQuickGeluTest, Int32Type2D) {
   std::cout << "Starting test" << std::endl;
   std::vector<float> input = {1, 1, 3, 2,
                                 0, 3, 0, 4,
