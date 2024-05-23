32a96ec5054bba0c7c7467a2fff5f4df89a260d7
Abhishek Jindal <abjindal@microsoft.com>
2024-05-22 17:18:06 -0700

    change test

--- orttraining/orttraining/test/training_ops/cuda/split_quickgelu_mul_test.cc
+++ orttraining/orttraining/test/training_ops/cuda/split_quickgelu_mul_test.cc
@@ -12,14 +12,14 @@ namespace test {
 TEST(S2SModelSplitQuickGeluTest, Int32Type2D) {
   std::cout << "Starting test" << std::endl;
   std::vector<float> input = {1, 1, 3, 2,
-                                0, 3, 0, 4,
-                                0, 5, 0, 6,
-                                0, 0, 0, 2,
+                                -1, 3, -2, 4,
+                                -0.5, 5, -3, 6,
+                                -2, -1, -4, 2,
                                 1, -1, 2, -2};
   // std::vector<float> output = {0.9940, 0.9678, 1.5, 1.9880,
   //                                0.0000, 2.9967, 0.0000, 3.9992};
-  std::vector<float> output = {2.9819, 1.9357, 0.0000, 11.9868,
-                                 0.0000, 29.9989, 0.0000, 0.0000, 1.9357, 0.0643};
+  std::vector<float> output = {2.9819, 1.9357, 6.4341, 1.1987,
+                                 9.0357, 2.9999, 8.8294, -1.9357, 1.9357, 6.4341};
 
   OpTester test("S2SModelSplitQuickGelu", 1, onnxruntime::kMSDomain);
   test.AddInput<float>("input", {5, 4}, input);
