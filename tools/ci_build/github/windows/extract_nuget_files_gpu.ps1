9c18125f6242412195cefc6c798344335faeca06
Jian Chen <cjian@microsoft.com>
2024-05-23 12:53:47 -0700

    USERPROFILE

--- tools/ci_build/github/windows/extract_nuget_files_gpu.ps1
+++ tools/ci_build/github/windows/extract_nuget_files_gpu.ps1
@@ -31,7 +31,7 @@ New-Item -Path $Env:BUILD_BINARIESDIRECTORY\RelWithDebInfo\_deps\protobuf-build\
 Copy-Item -Path $Env:BUILD_BINARIESDIRECTORY\RelWithDebInfo\RelWithDebInfo\nuget-artifacts\onnxruntime-win-x64-cuda-*\lib\* -Destination $Env:BUILD_BINARIESDIRECTORY\RelWithDebInfo\RelWithDebInfo
 dotnet new console
 dotnet add package Google.Protobuf.Tools --version 3.21.12
-[System.Environment]::SetEnvironmentVariable('PATH', $env:PATH + ";$env:USERPROFILE/.nuget/packages/google.protobuf.tools/**/tools/windows_x64", [System.EnvironmentVariableTarget]::User)
+Copy-Item -Path $Env:USERPROFILE\.nuget\packages\google.protobuf.tools\**\tools\windows_x64\protoc.exe $Env:BUILD_BINARIESDIRECTORY\RelWithDebInfo\_deps\protobuf-build\RelWithDebInfo
 
 $ort_dirs = Get-ChildItem -Path $Env:BUILD_BINARIESDIRECTORY\RelWithDebInfo\RelWithDebInfo\nuget-artifacts\onnxruntime-* -Directory
 foreach ($ort_dir in $ort_dirs)
