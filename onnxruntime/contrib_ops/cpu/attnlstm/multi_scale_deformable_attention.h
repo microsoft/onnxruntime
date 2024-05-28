b2e6b3e4c79ec3b1b74383d93b2368da25a1e9e7
Qingnan Duan <duanqn@proton.me>
2024-05-28 17:41:59 +0800

    Add generic implementation

--- onnxruntime/contrib_ops/cpu/attnlstm/multi_scale_deformable_attention.h
+++ onnxruntime/contrib_ops/cpu/attnlstm/multi_scale_deformable_attention.h
@@ -13,7 +13,29 @@ class MultiScaleDeformableAttention final : public OpKernel {
   MultiScaleDeformableAttention(const OpKernelInfo& info);
   [[nodiscard]] Status Compute(_Inout_ OpKernelContext* context) const override;
   private:
+  enum class ImplementationRoute {
+    AVX512,
+    Generic
+  };
+
+  ImplementationRoute route;
+
   void ComputeAVX512(
+    const float* value,
+    const int64_t* value_spatial_shapes,
+    const float* reference_points,
+    const float* sampling_locations,
+    const float* attention_weights,
+    float* output,
+    int64_t M,
+    int64_t L,
+    int64_t P,
+    int64_t D,
+    int64_t Q,
+    concurrency::ThreadPool* thread_pool,
+    AllocatorPtr alloc) const;
+
+  void ComputeGeneric(
     const float* value,
     const int64_t* value_spatial_shapes,
     const float* reference_points,
