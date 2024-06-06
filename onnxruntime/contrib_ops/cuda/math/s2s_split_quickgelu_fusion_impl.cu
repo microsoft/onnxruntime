36977384b0e60f1163cd2cbd057ae6403b522de2
Abhishek Jindal <abjindal@microsoft.com>
2024-06-06 15:09:00 -0700

    update logs

--- onnxruntime/contrib_ops/cuda/math/s2s_split_quickgelu_fusion_impl.cu
+++ onnxruntime/contrib_ops/cuda/math/s2s_split_quickgelu_fusion_impl.cu
@@ -23,12 +23,12 @@ constexpr int kThreadsPerBlock = GridDim::maxThreadsPerBlock;
 
 template <typename T>
 __device__ inline T QuickGeluCompute(const T inp1, const T inp2, const T alpha_val) {
-  if (std::is_same<T, float>::value) {
-    printf("Input is float: %f\n", static_cast<float>(inp1));
-  } else if (std::is_same<T, double>::value) {
-    printf("Input is double: %lf\n", static_cast<double>(inp1));
-  } else if (std::is_same<T, int>::value) {
-    printf("Input is int: %d\n", static_cast<int>(inp1));
+  // if (std::is_same<T, float>::value) {
+  //   printf("Input is float: %f\n", static_cast<float>(inp1));
+  // } else if (std::is_same<T, double>::value) {
+  //   printf("Input is double: %lf\n", static_cast<double>(inp1));
+  // } else if (std::is_same<T, int>::value) {
+  //   printf("Input is int: %d\n", static_cast<int>(inp1));
   // } else if (std::is_same<T, long>::value) {
   //   printf("Input is long: %ld\n", static_cast<long>(inp1));
   // } else if (std::is_same<T, long long>::value) {
@@ -37,22 +37,22 @@ __device__ inline T QuickGeluCompute(const T inp1, const T inp2, const T alpha_v
   //   printf("Input is half: %f\n", __half2float(inp1));
   // } else if (std::is_same<T, nv_bfloat16>::value) {
   //   printf("Input is bfloat16: %f\n", __bfloat162float(inp1));
-  } else {
-    // Add more types if necessary
-    printf("Unknown type\n");
-  }
+  // } else {
+  //   // Add more types if necessary
+  //   printf("Unknown type\n");
+  // }
   T v = inp2 * alpha_val;
   T one = static_cast<T>(1.f);
   T zero = static_cast<T>(0.f);
   T sigmoid = v >= zero ? one / (one + _Exp(-v)) : one - one / (one + _Exp(v));
   T quickgelu_out = inp2 * sigmoid;
-  printf("Inp1 val: %f\n", static_cast<float>(inp1));
-  printf("Inp2 val: %f\n", static_cast<float>(inp2));
-  printf("Alpha val: %f\n", static_cast<float>(alpha_val));
-  printf("v val: %f\n", static_cast<float>(v));
-  printf("Sigmoid val: %f\n", static_cast<float>(sigmoid));
-  printf("quickgelu_out: %f\n", static_cast<float>(quickgelu_out));  // Using %f for float, change format specifier if using double
-  printf("Final out: %f\n", static_cast<float>(inp1 * quickgelu_out));
+  // printf("Inp1 val: %f\n", static_cast<float>(inp1));
+  // printf("Inp2 val: %f\n", static_cast<float>(inp2));
+  // printf("Alpha val: %f\n", static_cast<float>(alpha_val));
+  // printf("v val: %f\n", static_cast<float>(v));
+  // printf("Sigmoid val: %f\n", static_cast<float>(sigmoid));
+  // printf("quickgelu_out: %f\n", static_cast<float>(quickgelu_out));  // Using %f for float, change format specifier if using double
+  // printf("Final out: %f\n", static_cast<float>(inp1 * quickgelu_out));
   return inp1 * quickgelu_out;
 }
 
@@ -71,13 +71,13 @@ __global__ void S2SModelSplitQuickGeluKernel(const int dim, float alpha, const T
   // float alpha = 1.702f;
   T alpha_val = static_cast<T>(alpha);
   // Separate QuickGelu code in another fn
-  printf("Curr kElementsPerThread %d\n", kElementsPerThread);
-  printf("Curr blockIdx.x %d\n", blockIdx.x);
-  printf("Curr threadIdx.x %d\n", threadIdx.x);
-  printf("Curr offset_in1 %d\n", offset_in1);
-  printf("Curr offset_in2 %d\n", offset_in2);
-  printf("Curr offset_out %d\n", offset_out);
-  printf("Curr dim %d\n", dim);
+  // printf("Curr kElementsPerThread %d\n", kElementsPerThread);
+  // printf("Curr blockIdx.x %d\n", blockIdx.x);
+  // printf("Curr threadIdx.x %d\n", threadIdx.x);
+  // printf("Curr offset_in1 %d\n", offset_in1);
+  // printf("Curr offset_in2 %d\n", offset_in2);
+  // printf("Curr offset_out %d\n", offset_out);
+  // printf("Curr dim %d\n", dim);
   // std::cout << "Curr kElementsPerThread:" << kElementsPerThread << std::endl;
   // input_size - dim
   // 5x4 (input_size = 20), dim = 2
@@ -93,11 +93,11 @@ __global__ void S2SModelSplitQuickGeluKernel(const int dim, float alpha, const T
   // What about this condition? (Removing if condition should improve Warp Divergence?)
   // for (int i = 0; i < kElementsPerThread; i++) {
   for (int i = 0; i < kElementsPerThread && threadIdx.x*kElementsPerThread + i < dim; i++){
-    int curr_in = offset_in1 + i;
+    // int curr_in = offset_in1 + i;
     // int curr_half = curr_in / dim;
-    printf("Curr Inp Outside %d\n", curr_in);
+    // printf("Curr Inp Outside %d\n", curr_in);
     if (threadIdx.x*kElementsPerThread + i < dim) {
-      printf("Curr Inp inside %d\n", curr_in);
+      // printf("Curr Inp inside %d\n", curr_in);
       output[offset_out + i] = QuickGeluCompute(input[offset_in1 + i], input[offset_in2+i], alpha_val);
       // T v = input[offset_in2+i] * alpha_val;
       // T sigmoid = v >= zero ? one / (one + _Exp(-v)) : one - one / (one + _Exp(v));
@@ -126,7 +126,7 @@ void LaunchS2SModelSplitQuickGeluKernel(cudaStream_t stream, int dim, int64_t in
   // ElemWiseKernel
   // [1000][1]
   int num_blocks = static_cast<int>(N/(2*dim));
-  printf("Num blocks %d\n", num_blocks);
+  // printf("Num blocks %d\n", num_blocks);
   // num_blocks = 1;
   // printf("Final number threads per block %d\n", num_threads_per_block);
   // printf("Final num blocks %d\n", num_blocks);
