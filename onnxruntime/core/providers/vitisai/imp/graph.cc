5f3080eeb157724d7503b02d5fbf5b21937b99e6
Changming Sun <chasun@microsoft.com>
2024-06-06 20:53:13 +0000

    update

--- onnxruntime/core/providers/vitisai/imp/graph.cc
+++ onnxruntime/core/providers/vitisai/imp/graph.cc
@@ -109,7 +109,7 @@ void graph_save(const Graph& graph, const std::string& filename, const std::stri
   if (initializer_size_threshold == std::numeric_limits<size_t>::max()) {
     model_proto = model.ToProto();
   } else {
-    model_proto = model.ToGraphProtoWithExternalInitializers(filename_dat, graph.ModelPath().ToPathString(), initializer_size_threshold);
+    model_proto = model.ToGraphProtoWithExternalInitializers(filename_dat, graph.ModelPath(), initializer_size_threshold);
   }
   auto& metadata = model.MetaData();
   if (!metadata.empty()) {
