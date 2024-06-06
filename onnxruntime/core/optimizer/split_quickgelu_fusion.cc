85b1f07ee07aba9b11740b827beff9c7d17e6c06
Abhishek Jindal <abjindal@microsoft.com>
2024-06-06 15:11:58 -0700

    update logs

--- onnxruntime/core/optimizer/split_quickgelu_fusion.cc
+++ onnxruntime/core/optimizer/split_quickgelu_fusion.cc
@@ -125,13 +125,13 @@ bool GetSplitQuickGeluParams(
     return false;
   }
   auto& quickgelu_attr = quickgelu_node.GetAttributes();
-  for (const auto& pair : quickgelu_attr){
-    std::cout << "Key:" << pair.first << std::endl;
-  }
+  // for (const auto& pair : quickgelu_attr){
+  //   std::cout << "Key:" << pair.first << std::endl;
+  // }
   if (quickgelu_attr.find("alpha") != quickgelu_attr.end()) {
     auto& alpha_attr = quickgelu_attr.at("alpha");
     alpha = utils::HasFloat(alpha_attr) ? (float)alpha_attr.f() : alpha;
-    printf("Got ALPHA Value as: %f\n", alpha);
+    // printf("Got ALPHA Value as: %f\n", alpha);
   } else {
     return false;
   }
