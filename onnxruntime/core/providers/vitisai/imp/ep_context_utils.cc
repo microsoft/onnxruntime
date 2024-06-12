6fa47a725e03bff189e15b01417db4c6a61ba875
Glen Cao <glen.cao@amd.com>
2024-06-11 23:16:55 -0700

    More logging for debugging

--- onnxruntime/core/providers/vitisai/imp/ep_context_utils.cc
+++ onnxruntime/core/providers/vitisai/imp/ep_context_utils.cc
@@ -266,9 +266,10 @@ std::unique_ptr<Model> CreateEPContexModel(
 
   ep_ctx_graph.AddNode(kEPContextOp, kEPContextOp, "", input_node_arg_ptrs, output_node_arg_ptrs, p_node_attrs.get(), kEPContextOpDomain);
   LOGS_DEFAULT(VERBOSE) << "EP context node created and added to graph";
-  // ORT_ENFORCE(ep_ctx_graph.Resolve().IsOK());
+  ORT_ENFORCE(ep_ctx_graph.Resolve().IsOK());
   auto p_ep_ctx_graph_viewer = ep_ctx_graph.CreateGraphViewer();
-  LOGS_DEFAULT(VERBOSE) << "EP context graph viewer created with EP context node added: " << p_ep_ctx_graph_viewer->Name();
+  LOGS_DEFAULT(VERBOSE) << "EP context graph viewer created with EP context node added";
+  LOGS_DEFAULT(VERBOSE) << "EP context graph viewer name " << p_ep_ctx_graph_viewer->Name();
   auto p_ep_ctx_model = p_ep_ctx_graph_viewer->CreateModel(*p_logger);
   LOGS_DEFAULT(VERBOSE) << "EP context model created";
   auto p_ep_ctx_model_proto = p_ep_ctx_model->ToProto();
