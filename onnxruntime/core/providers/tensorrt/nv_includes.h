facd228666104a6fb2123f519eb341ce57b5169a
yf711 <yifanl@microsoft.com>
2024-06-12 10:00:36 -0700

    remove 4996 in nv_includes.h & test_main.cc

--- onnxruntime/core/providers/tensorrt/nv_includes.h
+++ onnxruntime/core/providers/tensorrt/nv_includes.h
@@ -2,11 +2,12 @@
 // Licensed under the MIT License.
 #pragma once
 
-// File to include the required TRT headers with workarounds for warnings we can't fix or not fixed yet.
+// File to include the required TRT headers with workarounds for warnings we can't fix.
+
+// Ignore warning C4100: unreferenced formal parameter
 #if defined(_MSC_VER)
 #pragma warning(push)
-#pragma warning(disable : 4100)  // Ignore warning C4100: unreferenced formal parameter
-#pragma warning(disable : 4996)  // Ignore warning C4996: 'nvinfer1::IPluginV2' was declared deprecated
+#pragma warning(disable : 4100)
 #endif
 
 #include <NvInfer.h>
