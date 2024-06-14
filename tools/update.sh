6ba79281236759dd36ce2096f058c5df43775e3f
Changming Sun <chasun@microsoft.com>
2024-06-14 06:57:48 +0000

    update

---
+++ tools/update.sh
@@ -0,0 +1,2 @@
+find . -name \*.yml -exec sed -i 's/onnxruntime-Win2022-GPU-A10/onnxruntime-Win2022-GPU-A10-beta/g' {} \;
+find . -name \*.yml -exec sed -i 's/onnxruntime-Win-CPU-2022'/onnxruntime-Win2022-CPU-beta'/g' {} \;
