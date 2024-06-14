acbf7b1075e43456d23755ce4ff6c6ba9cfcba87
Changming Sun <chasun@microsoft.com>
2024-06-14 14:31:33 +0000

    update

--- tools/update.sh
+++ tools/update.sh
@@ -1,3 +1,4 @@
 find . -name \*.yml -exec sed -i 's/onnxruntime-Win2022-GPU-A10/onnxruntime-Win2022-GPU-A10-beta/g' {} \;
 find . -name \*.yml -exec sed -i 's/onnxruntime-Win-CPU-2022'/onnxruntime-Win2022-CPU-beta'/g' {} \;
 find . -name \*.yml -exec sed -i 's/onnxruntime-Win2022-CPU-training-AMD/onnxruntime-Win2022-CPU-beta/g' {} \;
+find . -name \*.yml -exec sed -i 's/onnxruntime-Win2022-Intel-CPU/onnxruntime-Win2022-CPU-beta/g' {} \;
