1b737213cac561e94779e8d95a8c1ec0db8ed64a
Jian Chen <cjian@microsoft.com>
2024-06-14 11:37:05 -0700

    using timestamp_ns::operator<<;

--- onnxruntime/test/util/include/capturing_sink.h
+++ onnxruntime/test/util/include/capturing_sink.h
@@ -17,7 +17,7 @@ class CapturingSink : public logging::ISink {
  public:
   void SendImpl(const Timestamp& timestamp, const std::string& logger_id, const Capture& message) override {
     // operator for formatting of timestamp in ISO8601 format including microseconds
-    using date::operator<<;
+    using timestamp_ns::operator<<;
     std::ostringstream msg;
 
     msg << timestamp << " [" << message.SeverityPrefix() << ":" << message.Category() << ":" << logger_id << ", "
