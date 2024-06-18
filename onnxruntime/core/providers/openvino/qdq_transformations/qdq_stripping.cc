82ab184010929150af69d4b158440b1954ed1bfa
saurabhkale17 <saurabh1.kale@intel.com>
2024-06-18 04:16:35 -0700

    fix standalone dq conversion

--- onnxruntime/core/providers/openvino/qdq_transformations/qdq_stripping.cc
+++ onnxruntime/core/providers/openvino/qdq_transformations/qdq_stripping.cc
@@ -219,8 +219,8 @@ static bool DQFeedsASupportedOp(const Node* dq_node, const onnxruntime::GraphVie
       return true;
     }
   } else if (op_type == "Add") {
-    // Add keeps all DQs except if it has const inits
-    return !IsAnyDQAConstantInitializer(&target_node, src_graph);
+    // Add => keeps all DQs 
+    return true
   }
   return false;
 }
