6ba970254363e9423f3c0fe587792a640c4beac2
tianchiyang <tianchiyang@microsoft.com>
2024-06-18 09:35:19 +0000

    fix

--- ort_build.sh
+++ ort_build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+rm -rf build/Linux/Release/dist/*.whl
 bash ./build.sh \
     --config Release --enable_training --build_wheel --update --build --cmake_generator Ninja --skip_tests \
     --use_rocm --rocm_version=5.4.0 --rocm_home /opt/rocm --enable_nccl \
@@ -9,5 +10,6 @@ bash ./build.sh \
     --cmake_extra_defines CMAKE_HIP_ARCHITECTURES=gfx90a
 
 pip uninstall -y onnxruntime-training onnxruntime-gpu onnxruntime
-pip install build/Linux/Release/dist/onnxruntime_training-1.19.0+rocm540.profiling-cp38-cp38-linux_x86_64.whl
+pip install build/Linux/Release/dist/*.whl
+cd /tmp
 python -m torch_ort.configure
