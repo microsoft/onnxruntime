9371e319a25d8b8e1fbbec4c4930f3b9f85a7ec4
Tianlei Wu <tlwu@microsoft.com>
2024-06-19 17:43:50 -0700

    Add header

---
+++ onnxruntime/contrib_ops/cpu/sparse/sparse_attention.h
@@ -0,0 +1,23 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+#pragma once
+
+#include "core/common/common.h"
+#include "core/framework/op_kernel.h"
+#include "contrib_ops/cpu/bert/gqa_attention_base.h"
+
+namespace onnxruntime {
+namespace contrib {
+
+template <typename T>
+class SparseAttention final : public OpKernel, public GQAAttentionBase {
+ public:
+  SparseAttention(const OpKernelInfo& info);
+  Status Compute(OpKernelContext* context) const override;
+ private:
+    int sparse_block_size_;
+};
+
+}  // namespace contrib
+}  // namespace onnxruntime
