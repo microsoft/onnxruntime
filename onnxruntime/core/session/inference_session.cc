c669ead5e891b4e3f13f3aa2ac8179be74d41cbf
Jian Chen <cjian@microsoft.com>
2024-06-20 16:46:27 -0700

    std::function<void()> run_fn = [this]()  {

--- onnxruntime/core/session/inference_session.cc
+++ onnxruntime/core/session/inference_session.cc
@@ -2751,7 +2751,7 @@ common::Status InferenceSession::RunAsync(const RunOptions* run_options,
 #if __cplusplus >= 202002L
   std::function<void()> run_fn = [=, this]() {
 #else
-  std::function<void()> run_fn = [this]() {
+  std::function<void()> run_fn = [=]() {
 #endif
     Status status = Status::OK();
     ORT_TRY {
