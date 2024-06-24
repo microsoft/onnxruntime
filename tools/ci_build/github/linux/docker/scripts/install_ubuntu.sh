5da1efcd5532c1d3c009a495d1d2e11bbf89243b
Jian Chen <cjian@microsoft.com>
2024-06-24 11:59:02 -0700

    remove python2

--- tools/ci_build/github/linux/docker/scripts/install_ubuntu.sh
+++ tools/ci_build/github/linux/docker/scripts/install_ubuntu.sh
@@ -49,7 +49,7 @@ PACKAGE_LIST="autotools-dev \
 	unzip \
 	zip \
 	rsync libunwind8 libpng-dev libexpat1-dev \
-	python3-setuptools python3-numpy python3-wheel python python3-pip python3-pytest \
+	python3-setuptools python3-numpy python3-wheel python3-pip python3-pytest \
 	openjdk-11-jdk \
 	graphviz"
 
