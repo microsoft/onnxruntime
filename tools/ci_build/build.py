6baaaf516538f9059da3558b2cd22128a9e42c07
Preetha Veeramalai <preetha.veeramalai@intel.com>
2024-06-28 08:31:02 -0700

    OVEP options to disable CPU fallback at compile time (#21166)
    ### Description
    Provide user level options to control the fallback on CPU for models not
    supported on Intel's NPU hardware.
    
    
    ### Motivation and Context
    - Current workflow of OVEP allows safe fallback from OV NPU to OV CPU on
    compilation failures. Also supports MLAS CPU fallback in presence of
    unsupported custom ops.
    - The PR provides a build-time option to disable fallback from OV NPU to
    OV CPU.
    - The session Option "kOrtSessionOptionsDisableCPUEPFallback" disables
    OV CPU and MLAS CPU fallback.
    - Also has bug fix for proto creation.
    
    ---------
    
    Co-authored-by: jatinwadhwa921 <jatin.wadhwa@intel.com>
    Co-authored-by: ankitm3k <ankit.maheshkar@intel.com>

--- tools/ci_build/build.py
+++ tools/ci_build/build.py
@@ -79,6 +79,7 @@ def _openvino_verify_device_type(device_read):
         "CPU_NO_PARTITION",
         "GPU_NO_PARTITION",
         "NPU_NO_PARTITION",
+        "NPU_NO_CPU_FALLBACK",
     ]
     status_hetero = True
     res = False
@@ -1227,6 +1228,7 @@ def generate_build_tree(
     if args.use_openvino:
         cmake_args += [
             "-Donnxruntime_USE_OPENVINO=ON",
+            "-Donnxruntime_NPU_NO_FALLBACK=" + ("ON" if args.use_openvino == "NPU_NO_CPU_FALLBACK" else "OFF"),
             "-Donnxruntime_USE_OPENVINO_GPU=" + ("ON" if args.use_openvino == "GPU" else "OFF"),
             "-Donnxruntime_USE_OPENVINO_CPU=" + ("ON" if args.use_openvino == "CPU" else "OFF"),
             "-Donnxruntime_USE_OPENVINO_NPU=" + ("ON" if args.use_openvino == "NPU" else "OFF"),
