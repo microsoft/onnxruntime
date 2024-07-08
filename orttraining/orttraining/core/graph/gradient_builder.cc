f5005289ba1253bfee01fdc99221bfe1efd7eb1c
root <prathik.rao@gmail.com>
2024-07-08 19:15:45 +0000

    peng fix, xavier suggestion

--- orttraining/orttraining/core/graph/gradient_builder.cc
+++ orttraining/orttraining/core/graph/gradient_builder.cc
@@ -1795,11 +1795,15 @@ IMPLEMENT_GRADIENT_BUILDER(GetExternalGradient) {
 
     std::vector<ArgDef> output_args;
     for (size_t output_index = 0; output_index < node_def.outputs.size(); ++output_index) {
-      if (!IsGradientRequiredForSrcNodeInput(output_index)) {
+      // If the input is not used in the forward computation, we don't need it for gradient computation
+      // Required for ORTMODULE_ATEN_SDPA_FALLBACK
+      if (output_index >= GetSrcNodeInputSize() or !IsGradientRequiredForSrcNodeInput(output_index)) {
         output_args.emplace_back(ArgDef());
         continue;
       }
+
       const auto& output = node_def.outputs[output_index];
+
       if (output.find("GI(") == 0) {
         size_t index = static_cast<size_t>(std::stoi(output.substr(3, output.length() - 4)));
         output_args.emplace_back(GI(index));
