f5adf885572d64824e9092273914491029982fc9
Changming Sun <chasun@microsoft.com>
2024-07-11 22:40:50 +0000

    update

--- tools/ci_build/github/linux/docker/scripts/manylinux/install_centos.sh
+++ tools/ci_build/github/linux/docker/scripts/manylinux/install_centos.sh
@@ -6,7 +6,7 @@ os_major_version=$(tr -dc '0-9.' < /etc/redhat-release |cut -d \. -f1)
 echo "installing for os major version : $os_major_version"
 if [ "$os_major_version" -gt 7 ]; then
     PACKAGE_MANAGER="dnf"
-    $PACKAGE_MANAGER install -y which redhat-lsb-core expat-devel tar unzip zlib-devel make bzip2 bzip2-devel perl-IPC-Cmd openssl-devel wget
+    $PACKAGE_MANAGER install -y which redhat-lsb-core expat-devel tar unzip zlib-devel make bzip2 bzip2-devel perl-IPC-Cmd openssl-devel wget libuuid-devel
 fi
 if [ ! -f /etc/yum.repos.d/microsoft-prod.repo ]; then
   rpm -Uvh https://packages.microsoft.com/config/centos/$os_major_version/packages-microsoft-prod.rpm
