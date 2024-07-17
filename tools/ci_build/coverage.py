2cd5fc8484e4ac00c7236beae3725a805e5a3bc9
Jian Chen <cjian@microsoft.com>
2024-07-17 11:35:35 -0400

    adding verbose and debug the location of testcase_driver.cc.gcda

--- tools/ci_build/coverage.py
+++ tools/ci_build/coverage.py
@@ -52,11 +52,13 @@ def main():
     adb_shell("cd /data/local/tmp && tar -zcf gcda_files.tar.gz *.dir")
     adb_pull("/data/local/tmp/gcda_files.tar.gz", cwd)
     os.chdir(cwd)
-    run_subprocess("tar -zxf gcda_files.tar.gz -C CMakeFiles".split(" "))
+    run_subprocess("tar -zxf gcda_files.tar.gz -C CMakeFiles".split(" "),cwd=cwd)
+    run_subprocess(f"find {source_dir} -name testcase_driver.cc.gcda",cwd=cwd)
     cmd = ["gcovr", "-s", "-r"]
     cmd.append(os.path.join(source_dir, "onnxruntime"))
     cmd.extend([".", "-o"])
     cmd.append(os.path.join(cwd, "coverage_rpt.txt"))
+    cmd.append("--verbose")
     run_subprocess(cmd, cwd=os.path.join(cwd, "CMakeFiles"))
 
 
