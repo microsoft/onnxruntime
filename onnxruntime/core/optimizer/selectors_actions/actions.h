e8ce6b9d7873e6382d5b83c0218a42f3573574c1
Jing Fang <fajin@microsoft.com>
2024-07-17 13:57:03 -0700

    add transform part of the dq matmul tool chain

--- onnxruntime/core/optimizer/selectors_actions/actions.h
+++ onnxruntime/core/optimizer/selectors_actions/actions.h
@@ -158,6 +158,8 @@ struct ReplaceWithNew : public Action {
   // specifies how the inputs and outputs for the replaced nodes are moved to the new node
   virtual std::vector<NodeAndMoveInfo> ValueMoves(const RuntimeState&) const = 0;
 
+  virtual Status ProcessNewNode(Graph&, const NodesToOptimize&, Node&) const { return Status::OK(); }
+
   RemoveNodes node_remover_;
 };
 
@@ -187,5 +189,4 @@ struct ReplaceWithNewFixed : public ReplaceWithNew {
   const NodeAttributes extra_attrs_;
   const std::vector<NodeAndMoveInfo> value_moves_;
 };
-
 }  // namespace onnxruntime
