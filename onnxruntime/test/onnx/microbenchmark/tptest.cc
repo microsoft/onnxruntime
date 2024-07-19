33d08f3a231febfe7e3894c2890479d7ba34c698
Jian Chen <cjian@microsoft.com>
2024-07-19 16:19:40 -0400

    #pragma GCC diagnostic ignored "-Wdeprecated"

--- onnxruntime/test/onnx/microbenchmark/tptest.cc
+++ onnxruntime/test/onnx/microbenchmark/tptest.cc
@@ -102,7 +102,9 @@ static void BM_ThreadPoolSimpleParallelFor(benchmark::State& state) {
   for (auto _ : state) {
     for (int j = 0; j < 100; j++) {
       ThreadPool::TrySimpleParallelFor(tp.get(), len, [&](size_t) {
-        for (volatile size_t x = 0; x < body; x++) {
+        volatile size_t x = 0;
+        for (size_t i = 0; i < body; i++) {
+          x++;
         }
       });
     }
