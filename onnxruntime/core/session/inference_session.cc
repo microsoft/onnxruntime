7386423669b4d71d36932050a6e877d62eb8a86b
Jian Chen <cjian@microsoft.com>
2024-07-22 15:22:55 -0400

    if(UNIX)

--- onnxruntime/core/session/inference_session.cc
+++ onnxruntime/core/session/inference_session.cc
@@ -2745,15 +2745,10 @@ common::Status InferenceSession::RunAsync(const RunOptions* run_options,
   if (!tp || concurrency::ThreadPool::DegreeOfParallelism(tp) < 2) {
     return ORT_MAKE_STATUS(ONNXRUNTIME, INVALID_ARGUMENT, "intra op thread pool must have at least one thread for RunAsync");
   }
-#ifdef __GNUC__
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wdeprecated"
-  std::function<void()> run_fn = [=]() {
-#pragma GCC diagnostic pop
+#if __cplusplus >= 202002L
+  std::function<void()> run_fn = [=, this]() {
 #else
-  #ifdef __cplusplus >= 202002L
-    std::function<void()> run_fn = [=, this]() {
-  #endif
+  std::function<void()> run_fn = [=]() {
 #endif
     Status status = Status::OK();
     ORT_TRY {
