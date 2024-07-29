797e6fb120531d9809b028d4a3d97b9847525696
Preetha Veeramalai <preetha.veeramalai@intel.com>
2024-07-29 17:08:14 +0530

    Add checks for EPCtx file path and create epctx model part of session creation

--- onnxruntime/core/providers/openvino/openvino_provider_factory.cc
+++ onnxruntime/core/providers/openvino/openvino_provider_factory.cc
@@ -298,7 +298,21 @@ struct OpenVINO_Provider : Provider {
       // The path to dump epctx model is valid only when epctx is enabled.
       // Overrides the cache_dir option to dump model cache files from OV.
       if (export_ep_ctx_blob) {
-        cache_dir = provider_options_map.at("so_epctx_path").c_str();
+        auto ep_context_file_path_ = provider_options_map.at("so_epctx_path");
+        auto file_path = std::filesystem::path(ep_context_file_path_);
+        // ep_context_file_path_ file extension must be .onnx
+        if (!ep_context_file_path_.empty() &&
+            file_path.extension().generic_string() == ".onnx") {
+          // ep_context_file_path_ must be provided as a directory, create it if doesn't exist
+          auto parent_path = file_path.parent_path();
+          if (!std::filesystem::is_directory(parent_path) &&
+              !std::filesystem::create_directory(parent_path)) {
+            ORT_THROW("[ERROR] [OpenVINO] Failed to create directory : " +file_path.parent_path().generic_string()+ " \n");
+          }
+          cache_dir = ep_context_file_path_.c_str();
+        } else {
+          ORT_THROW("[ERROR] [OpenVINO] Invalid ep_ctx_file_path" + ep_context_file_path_ + " \n");
+        }
       }
     }
 
