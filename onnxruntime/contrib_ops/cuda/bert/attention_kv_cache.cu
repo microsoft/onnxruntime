a194bf78f940df4dd773c745b0510d0cce6cfd7c
Tianlei Wu <tlwu@microsoft.com>
2024-07-30 18:12:53 +0000

    move ORT_ENFORCE

--- onnxruntime/contrib_ops/cuda/bert/attention_kv_cache.cu
+++ onnxruntime/contrib_ops/cuda/bert/attention_kv_cache.cu
@@ -270,9 +270,9 @@ Status ConcatPastToPresent(int batch_size, int num_heads, int qk_head_size, int 
     data.k = data.present;
     data.v = data.present + batch_size * num_heads * total_sequence_length * qk_head_size;
   } else  { // MultiHeadAttention op
-    ORT_ENFORCE(data.qkv_format == AttentionQkvFormat::Q_K_V_BNSH ||
-                data.qkv_format == AttentionQkvFormat::Q_K_V_BSNH_BNSH_BNSH);
     if (nullptr != data.present_key) {
+      ORT_ENFORCE(data.qkv_format == AttentionQkvFormat::Q_K_V_BNSH ||
+                  data.qkv_format == AttentionQkvFormat::Q_K_V_BSNH_BNSH_BNSH);
       if (nullptr != data.past_key) {
         assert(data.past_key != data.k);
         assert(data.past_value != data.v);
