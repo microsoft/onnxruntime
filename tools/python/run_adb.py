383ed7f970cad1fd0fa24efac0ec74204ed19776
Jian Chen <cjian@microsoft.com>
2024-07-31 10:43:49 -0700

    Do not allow clearing Android logs if the emulator is not running

--- tools/python/run_adb.py
+++ tools/python/run_adb.py
@@ -13,7 +13,21 @@ from util.android import get_sdk_tool_paths
 
 def run_adb(android_sdk_root: str, args: typing.List[str]):
     sdk_tool_paths = get_sdk_tool_paths(android_sdk_root)
-    run(sdk_tool_paths.adb, *args)
+    if is_emulator_running(sdk_tool_paths.adb):
+        run(sdk_tool_paths.adb, *args)
+    else:
+        print("No emulator is running.")
+
+
+def is_emulator_running(adb_path: str) -> bool:
+    result = run([adb_path, 'devices'], capture_stdout=True)
+    output = result.stdout
+    lines = output.strip().split('\n')
+    if len(lines) > 1:
+        for line in lines[1:]:
+            if "emulator" in line:
+                return True
+    return False
 
 
 def main():
