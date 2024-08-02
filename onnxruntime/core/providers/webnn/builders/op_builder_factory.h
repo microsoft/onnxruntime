8c641d718250cd839844316eff2aed082c022227
Wanming Lin <wanming.lin@intel.com>
2024-08-03 07:25:04 +0800

    [WebNN EP] Support Dropout op (#21586)
    ### Description
    WebNN only supports test mode, so we don't care about other inputs or
    attributes about training mode, use WebNN's identity op to implement the
    Dropout op directly.

--- onnxruntime/core/providers/webnn/builders/op_builder_factory.h
+++ onnxruntime/core/providers/webnn/builders/op_builder_factory.h
@@ -26,6 +26,7 @@ void CreateCastOpBuilder(const std::string& op_type, OpBuilderRegistrations& op_
 void CreateClipOpBuilder(const std::string& op_type, OpBuilderRegistrations& op_registrations);
 void CreateConvOpBuilder(const std::string& op_type, OpBuilderRegistrations& op_registrations);
 void CreateConcatOpBuilder(const std::string& op_type, OpBuilderRegistrations& op_registrations);
+void CreateDropoutOpBuilder(const std::string& op_type, OpBuilderRegistrations& op_registrations);
 void CreateDynamicQuantizeLinearOpBuilder(const std::string& op_type, OpBuilderRegistrations& op_registrations);
 void CreateDequantizeLinearOpBuilder(const std::string& op_type, OpBuilderRegistrations& op_registrations);
 void CreateExpandOpBuilder(const std::string& op_type, OpBuilderRegistrations& op_registrations);
