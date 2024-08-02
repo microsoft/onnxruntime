843fead426f835e64204f194ce09002f6278fe27
Ted Themistokleous <107195283+TedThemistokleous@users.noreply.github.com>
2024-08-02 11:34:26 -0400

    Add support for MIGraphX Exhaustive tune flag in MIGraphX EP (#46)
    * Add support for MIGraphX Exhaustive tune flag in MIGraphX EP
    
    Enable exhaustive tune by either python interface of environment env in bash
    
    * Apply lintrunner pass
    
    * Fix compile errors.
    
    * Lintrunner pass

--- onnxruntime/core/providers/migraphx/migraphx_execution_provider.h
+++ onnxruntime/core/providers/migraphx/migraphx_execution_provider.h
@@ -25,6 +25,7 @@ static const char kSaveCompiledModel[] = "ORT_MIGRAPHX_SAVE_COMPILED_MODEL";
 static const char kSavedModelPath[] = "ORT_MIGRAPHX_SAVE_COMPILE_PATH";
 static const char kLoadCompiledModel[] = "ORT_MIGRAPHX_LOAD_COMPILED_MODEL";
 static const char kLoadModelPath[] = "ORT_MIGRAPHX_LOAD_COMPILE_PATH";
+static const char kExhaustiveTune[] = "ORT_MIGRAPHX_EXHAUSTIVE_TUNE";
 
 };  // namespace migraphx_env_vars
 
@@ -49,6 +50,7 @@ struct MIGraphXFuncState {
   bool load_compiled_mode = false;
   std::string load_compiled_path;
   bool dump_model_ops = false;
+  bool exhaustive_tune = false;
 };
 
 // Logical device representation.
@@ -100,6 +102,7 @@ class MIGraphXExecutionProvider : public IExecutionProvider {
   migraphx::target t_;
   OrtMutex mgx_mu_;
   hipStream_t stream_ = nullptr;
+  bool exhaustive_tune_ = false;
 
   std::unordered_map<std::string, migraphx::program> map_progs_;
   std::unordered_map<std::string, std::string> map_onnx_string_;
