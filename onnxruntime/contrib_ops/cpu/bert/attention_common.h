bd83af799c00b05b4bb6252545f8e85488c78c34
aciddelgado <aciddelgado@microsoft.com>
2024-08-08 10:44:22 -0700

    changes

--- onnxruntime/contrib_ops/cpu/bert/attention_common.h
+++ onnxruntime/contrib_ops/cpu/bert/attention_common.h
@@ -103,8 +103,8 @@ struct GroupQueryAttentionParameters {
   int local_window_size;
   bool kv_share_buffer;
   bool is_packed_qkv;
-  bool is_interactive;  // indicates whether seqlens_k is 1 or 2 dimensional. 2-d case enables interactive decoding
-  bool is_prompt;       // determines if seqlens_k is past or kv sequence length tensor
+  bool is_interactive;  // indicates whether we have past context and seqlen > 1
+  bool is_prompt;       // indicates whether this is first decoding step
   bool do_rotary;
   bool rotary_interleaved;
   float scale;
