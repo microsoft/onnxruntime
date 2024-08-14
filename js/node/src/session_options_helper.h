0d8eba79485a23bdf89420a59cccccd3c91caf6b
Yulong Wang <7679871+fs-eire@users.noreply.github.com>
2024-08-13 20:13:52 -0700

    more formatted files

--- js/node/src/session_options_helper.h
+++ js/node/src/session_options_helper.h
@@ -10,4 +10,4 @@ struct SessionOptions;
 }
 
 // parse a Javascript session options object and fill the native SessionOptions object.
-void ParseSessionOptions(const Napi::Object options, Ort::SessionOptions &sessionOptions);
+void ParseSessionOptions(const Napi::Object options, Ort::SessionOptions& sessionOptions);
