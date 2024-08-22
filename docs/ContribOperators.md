bedd34229d5155691b6e7c1a9258599ad54b34e8
Your Name <you@example.com>
2024-08-22 20:25:51 +0000

    refactor and docs

--- docs/ContribOperators.md
+++ docs/ContribOperators.md
@@ -80,8 +80,7 @@ Do not modify directly.*
   * <a href="#com.microsoft.QLinearSigmoid">com.microsoft.QLinearSigmoid</a>
   * <a href="#com.microsoft.QLinearSoftmax">com.microsoft.QLinearSoftmax</a>
   * <a href="#com.microsoft.QLinearWhere">com.microsoft.QLinearWhere</a>
-  * <a href="#com.microsoft.QMoE4Bits">com.microsoft.QMoE4Bits</a>
-  * <a href="#com.microsoft.QMoE8Bits">com.microsoft.QMoE8Bits</a>
+  * <a href="#com.microsoft.QMoE">com.microsoft.QMoE</a>
   * <a href="#com.microsoft.QOrderedAttention">com.microsoft.QOrderedAttention</a>
   * <a href="#com.microsoft.QOrderedGelu">com.microsoft.QOrderedGelu</a>
   * <a href="#com.microsoft.QOrderedLayerNormalization">com.microsoft.QOrderedLayerNormalization</a>
@@ -4397,72 +4396,9 @@ This version of the operator has been available since version 1 of the 'com.micr
 </dl>
 
 
-### <a name="com.microsoft.QMoE4Bits"></a><a name="com.microsoft.qmoe4bits">**com.microsoft.QMoE4Bits**</a>
+### <a name="com.microsoft.QMoE"></a><a name="com.microsoft.qmoe">**com.microsoft.QMoE**</a>
 
-  Int4 MoE
-
-#### Version
-
-This version of the operator has been available since version 1 of the 'com.microsoft' operator set.
-
-#### Attributes
-
-<dl>
-<dt><tt>activation_type</tt> : string</dt>
-<dd>Activation function to use. Choose from relu, gelu, silu and identity. Default is relu</dd>
-<dt><tt>k</tt> : int</dt>
-<dd>Number of top experts to select from expert pool</dd>
-<dt><tt>normalize_routing_weights</tt> : int</dt>
-<dd>Whether to normalize routing weights</dd>
-</dl>
-
-#### Inputs (7 - 11)
-
-<dl>
-<dt><tt>input</tt> : T</dt>
-<dd>2D input tensor with shape (num_rows, hidden_size) or 3D input tensor with shape (batch_size, sequence_length, hidden_size)</dd>
-<dt><tt>router_probs</tt> : T</dt>
-<dd>2D input tensor with shape (num_rows, num_experts)</dd>
-<dt><tt>fc1_experts_weights</tt> : T1</dt>
-<dd>3D input tensor with shape (num_experts, hidden_size, inter_size / 2)</dd>
-<dt><tt>fc1_scales</tt> : T</dt>
-<dd>2D input tensor with shape (num_experts, inter_size)</dd>
-<dt><tt>fc1_experts_bias</tt> (optional) : T</dt>
-<dd>2D optional input tensor with shape (num_experts, inter_size)</dd>
-<dt><tt>fc2_experts_weights</tt> : T1</dt>
-<dd>3D input tensor with shape (num_experts, inter_size, hidden_size / 2)</dd>
-<dt><tt>fc2_scales</tt> : T</dt>
-<dd>2D input tensor with shape (num_experts, hidden_size)</dd>
-<dt><tt>fc2_experts_bias</tt> (optional) : T</dt>
-<dd>2D optional input tensor with shape (num_experts, hidden_size)</dd>
-<dt><tt>fc3_experts_weights</tt> (optional) : T1</dt>
-<dd>3D optional input tensor with shape (num_experts, hidden_size, inter_size / 2)</dd>
-<dt><tt>fc3_scales</tt> (optional) : T</dt>
-<dd>2D optional input tensor with shape (num_experts, inter_size)</dd>
-<dt><tt>fc3_experts_bias</tt> (optional) : T</dt>
-<dd>2D optional input tensor with shape (num_experts, inter_size)</dd>
-</dl>
-
-#### Outputs
-
-<dl>
-<dt><tt>output</tt> : T</dt>
-<dd>2D input tensor with shape (num_rows, hidden_size) or 3D input tensor with shape (batch_size, sequence_length, hidden_size)</dd>
-</dl>
-
-#### Type Constraints
-
-<dl>
-<dt><tt>T</tt> : tensor(float16)</dt>
-<dd>Constrain input and output types to float or float16 tensors.</dd>
-<dt><tt>T1</tt> : tensor(uint8)</dt>
-<dd>Constrain weights type to uint8 tensors.</dd>
-</dl>
-
-
-### <a name="com.microsoft.QMoE8Bits"></a><a name="com.microsoft.qmoe8bits">**com.microsoft.QMoE8Bits**</a>
-
-  Int8 MoE
+  Quantized MoE
 
 #### Version
 
@@ -4473,6 +4409,8 @@ This version of the operator has been available since version 1 of the 'com.micr
 <dl>
 <dt><tt>activation_type</tt> : string</dt>
 <dd>Activation function to use. Choose from relu, gelu, silu and identity. Default is relu</dd>
+<dt><tt>expert_weight_bits</tt> : int</dt>
+<dd>Number of bits used in quantized weights. Default is 4 bits</dd>
 <dt><tt>k</tt> : int</dt>
 <dd>Number of top experts to select from expert pool</dd>
 <dt><tt>normalize_routing_weights</tt> : int</dt>
@@ -4489,19 +4427,19 @@ This version of the operator has been available since version 1 of the 'com.micr
 <dt><tt>router_probs</tt> : T</dt>
 <dd>2D input tensor with shape (num_rows, num_experts)</dd>
 <dt><tt>fc1_experts_weights</tt> : T1</dt>
-<dd>3D input tensor with shape (num_experts, hidden_size, inter_size)</dd>
+<dd>3D input tensor with shape (num_experts, hidden_size, inter_size) or (num_experts, hidden_size, inter_size / 2)</dd>
 <dt><tt>fc1_scales</tt> : T</dt>
 <dd>2D input tensor with shape (num_experts, inter_size)</dd>
 <dt><tt>fc1_experts_bias</tt> (optional) : T</dt>
 <dd>2D optional input tensor with shape (num_experts, inter_size)</dd>
 <dt><tt>fc2_experts_weights</tt> : T1</dt>
-<dd>3D input tensor with shape (num_experts, inter_size, hidden_size)</dd>
+<dd>3D input tensor with shape (num_experts, inter_size, hidden_size) or (num_experts, inter_size, hidden_size / 2)</dd>
 <dt><tt>fc2_scales</tt> : T</dt>
 <dd>2D input tensor with shape (num_experts, hidden_size)</dd>
 <dt><tt>fc2_experts_bias</tt> (optional) : T</dt>
 <dd>2D optional input tensor with shape (num_experts, hidden_size)</dd>
 <dt><tt>fc3_experts_weights</tt> (optional) : T1</dt>
-<dd>3D optional input tensor with shape (num_experts, hidden_size, inter_size)</dd>
+<dd>3D optional input tensor with shape (num_experts, hidden_size, inter_size) or (num_experts, hidden_size, inter_size / 2)</dd>
 <dt><tt>fc3_scales</tt> (optional) : T</dt>
 <dd>2D optional input tensor with shape (num_experts, inter_size)</dd>
 <dt><tt>fc3_experts_bias</tt> (optional) : T</dt>
--- onnxruntime/contrib_ops/cuda/quantization/moe_quantization.cc
+++ onnxruntime/contrib_ops/cuda/quantization/moe_quantization.cc
@@ -37,12 +37,103 @@ template <>
 struct ToCudaTypeWrapper<uint8_t, true> {
   using MappedType = cutlass::uint4b_t;
 };
+
 }  // anonymous namespace
 
 QMoE::QMoE(const OpKernelInfo& op_kernel_info) : CudaKernel(op_kernel_info), MoEBase(op_kernel_info) {
     ORT_ENFORCE(op_kernel_info.GetAttr<int64_t>("expert_weight_bits", &expert_weight_bits_).IsOK());
     ORT_ENFORCE(expert_weight_bits_ == 8 || expert_weight_bits_ == 4,
                 "expert_weight_bits must be 4 or 8, but got ", expert_weight_bits_);
+}
+
+template<typename CudaWeightT>
+Status QMoE::QuantizedMoEImpl(OpKernelContext* context, 
+                              MoEParameters& moe_params,
+                              const Tensor* input, 
+                              const Tensor* router_probs, 
+                              const Tensor* fc1_experts_weights,
+                              const Tensor* fc1_experts_bias_optional, 
+                              const Tensor* fc2_experts_weights,
+                              const Tensor* fc2_experts_bias_optional, 
+                              const Tensor* fc3_experts_weights_optional,
+                              const Tensor* fc3_experts_bias_optional, 
+                              const Tensor* fc1_scales, 
+                              const Tensor* fc2_scales,
+                              const Tensor* fc3_scales_optional, 
+                              const cudaDeviceProp& device_prop) const {
+  auto stream = context->GetComputeStream();
+
+  const int sm = device_prop.major * 10 + device_prop.minor;
+
+  AllocatorPtr allocator;
+  ORT_RETURN_IF_ERROR(context->GetTempSpaceAllocator(&allocator));
+
+  using T = MLFloat16;
+  using CudaT = typename ToCudaType<T>::MappedType;
+
+  ort_fastertransformer::CutlassMoeFCRunner<CudaT, CudaWeightT> moe_runner(sm, 
+                                                                           fc3_experts_weights_optional != nullptr,
+                                                                           normalize_routing_weights_,
+                                                                           use_sparse_mixer_);
+
+  size_t ws_size = moe_runner.getWorkspaceSize(
+      static_cast<size_t>(moe_params.num_rows), static_cast<size_t>(moe_params.hidden_size),
+      static_cast<size_t>(moe_params.inter_size), static_cast<size_t>(moe_params.num_experts), 
+      static_cast<size_t>(k_));
+  size_t fc2_output_size = k_ * moe_params.num_rows * moe_params.hidden_size * sizeof(CudaT);
+  size_t expert_scales_size = k_ * moe_params.num_rows * sizeof(CudaT);
+  size_t expanded_source_row_to_expanded_dest_row_size = k_ * moe_params.num_rows * sizeof(int);
+  size_t expert_for_source_row_size = k_ * moe_params.num_rows * sizeof(int);
+
+  IAllocatorUniquePtr<void> work_space = IAllocator::MakeUniquePtr<void>(allocator, ws_size, false, stream);
+  IAllocatorUniquePtr<void> fc2_output = IAllocator::MakeUniquePtr<void>(allocator, fc2_output_size, false, stream);
+  IAllocatorUniquePtr<void> expert_scales =
+      IAllocator::MakeUniquePtr<void>(allocator, expert_scales_size, false, stream);
+  IAllocatorUniquePtr<void> expanded_source_row_to_expanded_dest_row =
+      IAllocator::MakeUniquePtr<void>(allocator, expanded_source_row_to_expanded_dest_row_size, false, stream);
+  IAllocatorUniquePtr<void> expert_for_source_row =
+      IAllocator::MakeUniquePtr<void>(allocator, expert_for_source_row_size, false, stream);
+
+  moe_runner.run_moe_fc(
+      reinterpret_cast<const CudaT*>(input->template Data<T>()),
+      reinterpret_cast<const CudaT*>(router_probs->template Data<T>()),
+      reinterpret_cast<const CudaWeightT*>(fc1_experts_weights->DataRaw()),
+      fc1_scales == nullptr ? nullptr : reinterpret_cast<const CudaT*>(fc1_scales->template Data<T>()),
+      fc1_experts_bias_optional == nullptr
+          ? nullptr
+          : reinterpret_cast<const CudaT*>(fc1_experts_bias_optional->template Data<T>()),
+      activation_type_,
+      fc3_experts_weights_optional == nullptr
+          ? nullptr
+          : reinterpret_cast<const CudaWeightT*>(fc3_experts_weights_optional->DataRaw()),
+      fc3_scales_optional == nullptr ? nullptr
+                                     : reinterpret_cast<const CudaT*>(fc3_scales_optional->template Data<T>()),
+      fc3_experts_bias_optional == nullptr
+          ? nullptr
+          : reinterpret_cast<const CudaT*>(fc3_experts_bias_optional->template Data<T>()),
+      reinterpret_cast<const CudaWeightT*>(fc2_experts_weights->DataRaw()),
+      fc2_scales == nullptr ? nullptr : reinterpret_cast<const CudaT*>(fc2_scales->template Data<T>()),
+      static_cast<int>(moe_params.num_rows), static_cast<int>(moe_params.hidden_size),
+      static_cast<int>(moe_params.inter_size), static_cast<int>(moe_params.num_experts),
+      static_cast<int>(moe_params.local_num_experts), 0 /*local_experts_start_index_ used in sharded MoE*/,
+      static_cast<int>(k_), reinterpret_cast<char*>(work_space.get()), reinterpret_cast<CudaT*>(fc2_output.get()),
+      reinterpret_cast<CudaT*>(expert_scales.get()),
+      reinterpret_cast<int*>(expanded_source_row_to_expanded_dest_row.get()),
+      reinterpret_cast<int*>(expert_for_source_row.get()), Stream(context));
+
+  Tensor* output = context->Output(0, input->Shape());
+
+  ort_fastertransformer::finalize_moe_routing_kernelLauncher(
+      reinterpret_cast<CudaT*>(fc2_output.get()), reinterpret_cast<CudaT*>(output->template MutableData<T>()),
+      fc2_experts_bias_optional == nullptr
+          ? nullptr
+          : reinterpret_cast<const CudaT*>(fc2_experts_bias_optional->template Data<T>()),
+      reinterpret_cast<CudaT*>(expert_scales.get()),
+      reinterpret_cast<int*>(expanded_source_row_to_expanded_dest_row.get()),
+      reinterpret_cast<int*>(expert_for_source_row.get()), static_cast<int>(moe_params.num_rows),
+      static_cast<int>(moe_params.hidden_size), static_cast<int>(k_), Stream(context));
+
+  return Status::OK();
 }
 
 Status QMoE::ComputeInternal(OpKernelContext* context) const {
@@ -58,159 +149,39 @@ Status QMoE::ComputeInternal(OpKernelContext* context) const {
   const Tensor* fc3_scales_optional = context->Input<Tensor>(9);
   const Tensor* fc3_experts_bias_optional = context->Input<Tensor>(10);
 
-#if defined(__GNUC__)
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wmaybe-uninitialized"  // Mute "maybe used uninitialized" warning for MoEParameters.
-#endif
-
-  MoEParameters moe_params;
   MoEQuantType quant_type = expert_weight_bits_ == 4 ? MoEQuantType::UINT4 : MoEQuantType::UINT8;
+  MoEParameters moe_params;
   ORT_RETURN_IF_ERROR(CheckInputs(moe_params, quant_type, input, router_probs, fc1_experts_weights,
                                   fc1_experts_bias_optional, fc2_experts_weights, fc2_experts_bias_optional,
                                   fc3_experts_weights_optional, fc3_experts_bias_optional));
   ORT_RETURN_IF_ERROR(CheckInputScales(fc1_scales, fc2_scales, fc3_scales_optional, moe_params.num_experts,
                                        moe_params.hidden_size, moe_params.inter_size));
 
-  using T = MLFloat16;
-  using CudaT = typename ToCudaType<T>::MappedType;
-
-  auto stream = context->GetComputeStream();
-
-  auto& device_prop = GetDeviceProp();
-  const int sm = device_prop.major * 10 + device_prop.minor;
-
-  AllocatorPtr allocator;
-  ORT_RETURN_IF_ERROR(context->GetTempSpaceAllocator(&allocator));
-
-  Tensor* output = context->Output(0, input->Shape());
+#if defined(__GNUC__)
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wmaybe-uninitialized"  // Mute "maybe used uninitialized" warning for MoEParameters.
+#endif
 
   if (quant_type == MoEQuantType::UINT4) {
     using CudaWeightT = typename ToCudaTypeWrapper<uint8_t, true>::MappedType;
-
-    ort_fastertransformer::CutlassMoeFCRunner<CudaT, CudaWeightT> moe_runner(sm, fc3_experts_weights_optional != nullptr,
-                                                                             normalize_routing_weights_,
-                                                                             use_sparse_mixer_);
-
-    size_t ws_size = moe_runner.getWorkspaceSize(
-        static_cast<size_t>(moe_params.num_rows), static_cast<size_t>(moe_params.hidden_size),
-        static_cast<size_t>(moe_params.inter_size), static_cast<size_t>(moe_params.num_experts), static_cast<size_t>(k_));
-    size_t fc2_output_size = k_ * moe_params.num_rows * moe_params.hidden_size * sizeof(CudaT);
-    size_t expert_scales_size = k_ * moe_params.num_rows * sizeof(CudaT);
-    size_t expanded_source_row_to_expanded_dest_row_size = k_ * moe_params.num_rows * sizeof(int);
-    size_t expert_for_source_row_size = k_ * moe_params.num_rows * sizeof(int);
-
-    IAllocatorUniquePtr<void> work_space = IAllocator::MakeUniquePtr<void>(allocator, ws_size, false, stream);
-    IAllocatorUniquePtr<void> fc2_output = IAllocator::MakeUniquePtr<void>(allocator, fc2_output_size, false, stream);
-    IAllocatorUniquePtr<void> expert_scales =
-        IAllocator::MakeUniquePtr<void>(allocator, expert_scales_size, false, stream);
-    IAllocatorUniquePtr<void> expanded_source_row_to_expanded_dest_row =
-        IAllocator::MakeUniquePtr<void>(allocator, expanded_source_row_to_expanded_dest_row_size, false, stream);
-    IAllocatorUniquePtr<void> expert_for_source_row =
-        IAllocator::MakeUniquePtr<void>(allocator, expert_for_source_row_size, false, stream);
-
-    moe_runner.run_moe_fc(
-        reinterpret_cast<const CudaT*>(input->template Data<T>()),
-        reinterpret_cast<const CudaT*>(router_probs->template Data<T>()),
-        reinterpret_cast<const CudaWeightT*>(fc1_experts_weights->DataRaw()),
-        fc1_scales == nullptr ? nullptr : reinterpret_cast<const CudaT*>(fc1_scales->template Data<T>()),
-        fc1_experts_bias_optional == nullptr
-            ? nullptr
-            : reinterpret_cast<const CudaT*>(fc1_experts_bias_optional->template Data<T>()),
-        activation_type_,
-        fc3_experts_weights_optional == nullptr
-            ? nullptr
-            : reinterpret_cast<const CudaWeightT*>(fc3_experts_weights_optional->DataRaw()),
-        fc3_scales_optional == nullptr ? nullptr
-                                       : reinterpret_cast<const CudaT*>(fc3_scales_optional->template Data<T>()),
-        fc3_experts_bias_optional == nullptr
-            ? nullptr
-            : reinterpret_cast<const CudaT*>(fc3_experts_bias_optional->template Data<T>()),
-        reinterpret_cast<const CudaWeightT*>(fc2_experts_weights->DataRaw()),
-        fc2_scales == nullptr ? nullptr : reinterpret_cast<const CudaT*>(fc2_scales->template Data<T>()),
-        static_cast<int>(moe_params.num_rows), static_cast<int>(moe_params.hidden_size),
-        static_cast<int>(moe_params.inter_size), static_cast<int>(moe_params.num_experts),
-        static_cast<int>(moe_params.local_num_experts), 0 /*local_experts_start_index_ used in sharded MoE*/,
-        static_cast<int>(k_), reinterpret_cast<char*>(work_space.get()), reinterpret_cast<CudaT*>(fc2_output.get()),
-        reinterpret_cast<CudaT*>(expert_scales.get()),
-        reinterpret_cast<int*>(expanded_source_row_to_expanded_dest_row.get()),
-        reinterpret_cast<int*>(expert_for_source_row.get()), Stream(context));
-
-    ort_fastertransformer::finalize_moe_routing_kernelLauncher(
-        reinterpret_cast<CudaT*>(fc2_output.get()), reinterpret_cast<CudaT*>(output->template MutableData<T>()),
-        fc2_experts_bias_optional == nullptr
-            ? nullptr
-            : reinterpret_cast<const CudaT*>(fc2_experts_bias_optional->template Data<T>()),
-        reinterpret_cast<CudaT*>(expert_scales.get()),
-        reinterpret_cast<int*>(expanded_source_row_to_expanded_dest_row.get()),
-        reinterpret_cast<int*>(expert_for_source_row.get()), static_cast<int>(moe_params.num_rows),
-        static_cast<int>(moe_params.hidden_size), static_cast<int>(k_), Stream(context));
+    return QuantizedMoEImpl<CudaWeightT>(context, moe_params, input, router_probs,
+                                         fc1_experts_weights, fc1_experts_bias_optional, fc2_experts_weights,
+                                         fc2_experts_bias_optional, fc3_experts_weights_optional,
+                                         fc3_experts_bias_optional, fc1_scales, fc2_scales, fc3_scales_optional, 
+                                         GetDeviceProp());
   } else {
     using CudaWeightT = typename ToCudaTypeWrapper<uint8_t, false>::MappedType;
-
-    ort_fastertransformer::CutlassMoeFCRunner<CudaT, CudaWeightT> moe_runner(sm, fc3_experts_weights_optional != nullptr,
-                                                                             normalize_routing_weights_,
-                                                                             use_sparse_mixer_);
-
-    size_t ws_size = moe_runner.getWorkspaceSize(
-        static_cast<size_t>(moe_params.num_rows), static_cast<size_t>(moe_params.hidden_size),
-        static_cast<size_t>(moe_params.inter_size), static_cast<size_t>(moe_params.num_experts), static_cast<size_t>(k_));
-    size_t fc2_output_size = k_ * moe_params.num_rows * moe_params.hidden_size * sizeof(CudaT);
-    size_t expert_scales_size = k_ * moe_params.num_rows * sizeof(CudaT);
-    size_t expanded_source_row_to_expanded_dest_row_size = k_ * moe_params.num_rows * sizeof(int);
-    size_t expert_for_source_row_size = k_ * moe_params.num_rows * sizeof(int);
-
-    IAllocatorUniquePtr<void> work_space = IAllocator::MakeUniquePtr<void>(allocator, ws_size, false, stream);
-    IAllocatorUniquePtr<void> fc2_output = IAllocator::MakeUniquePtr<void>(allocator, fc2_output_size, false, stream);
-    IAllocatorUniquePtr<void> expert_scales =
-        IAllocator::MakeUniquePtr<void>(allocator, expert_scales_size, false, stream);
-    IAllocatorUniquePtr<void> expanded_source_row_to_expanded_dest_row =
-        IAllocator::MakeUniquePtr<void>(allocator, expanded_source_row_to_expanded_dest_row_size, false, stream);
-    IAllocatorUniquePtr<void> expert_for_source_row =
-        IAllocator::MakeUniquePtr<void>(allocator, expert_for_source_row_size, false, stream);
-
-    moe_runner.run_moe_fc(
-        reinterpret_cast<const CudaT*>(input->template Data<T>()),
-        reinterpret_cast<const CudaT*>(router_probs->template Data<T>()),
-        reinterpret_cast<const CudaWeightT*>(fc1_experts_weights->DataRaw()),
-        fc1_scales == nullptr ? nullptr : reinterpret_cast<const CudaT*>(fc1_scales->template Data<T>()),
-        fc1_experts_bias_optional == nullptr
-            ? nullptr
-            : reinterpret_cast<const CudaT*>(fc1_experts_bias_optional->template Data<T>()),
-        activation_type_,
-        fc3_experts_weights_optional == nullptr
-            ? nullptr
-            : reinterpret_cast<const CudaWeightT*>(fc3_experts_weights_optional->DataRaw()),
-        fc3_scales_optional == nullptr ? nullptr
-                                       : reinterpret_cast<const CudaT*>(fc3_scales_optional->template Data<T>()),
-        fc3_experts_bias_optional == nullptr
-            ? nullptr
-            : reinterpret_cast<const CudaT*>(fc3_experts_bias_optional->template Data<T>()),
-        reinterpret_cast<const CudaWeightT*>(fc2_experts_weights->DataRaw()),
-        fc2_scales == nullptr ? nullptr : reinterpret_cast<const CudaT*>(fc2_scales->template Data<T>()),
-        static_cast<int>(moe_params.num_rows), static_cast<int>(moe_params.hidden_size),
-        static_cast<int>(moe_params.inter_size), static_cast<int>(moe_params.num_experts),
-        static_cast<int>(moe_params.local_num_experts), 0 /*local_experts_start_index_ used in sharded MoE*/,
-        static_cast<int>(k_), reinterpret_cast<char*>(work_space.get()), reinterpret_cast<CudaT*>(fc2_output.get()),
-        reinterpret_cast<CudaT*>(expert_scales.get()),
-        reinterpret_cast<int*>(expanded_source_row_to_expanded_dest_row.get()),
-        reinterpret_cast<int*>(expert_for_source_row.get()), Stream(context));
-
-    ort_fastertransformer::finalize_moe_routing_kernelLauncher(
-        reinterpret_cast<CudaT*>(fc2_output.get()), reinterpret_cast<CudaT*>(output->template MutableData<T>()),
-        fc2_experts_bias_optional == nullptr
-            ? nullptr
-            : reinterpret_cast<const CudaT*>(fc2_experts_bias_optional->template Data<T>()),
-        reinterpret_cast<CudaT*>(expert_scales.get()),
-        reinterpret_cast<int*>(expanded_source_row_to_expanded_dest_row.get()),
-        reinterpret_cast<int*>(expert_for_source_row.get()), static_cast<int>(moe_params.num_rows),
-        static_cast<int>(moe_params.hidden_size), static_cast<int>(k_), Stream(context));
+    return QuantizedMoEImpl<CudaWeightT>(context, moe_params, input, router_probs,
+                                         fc1_experts_weights, fc1_experts_bias_optional, fc2_experts_weights,
+                                         fc2_experts_bias_optional, fc3_experts_weights_optional,
+                                         fc3_experts_bias_optional, fc1_scales, fc2_scales, fc3_scales_optional, 
+                                         GetDeviceProp());
   }
 
 #if defined(__GNUC__)
 #pragma GCC diagnostic pop
 #endif
 
-  return Status::OK();
 }
 
 }  // namespace cuda
