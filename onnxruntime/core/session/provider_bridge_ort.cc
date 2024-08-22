37a7dd7d63b2170f7c91def94f659e4736384303
Yueqing Zhang <yuz75@Pitt.edu>
2024-08-22 15:28:31 -0500

    [VitisAI] optimize model clone (#21706)
    ### Description
    <!-- Describe your changes. -->
    Optimize the memory consumption for model_clone which is a crucial part
    of our model preparation
    
    
    ### Motivation and Context
    <!-- - Why is this change required? What problem does it solve?
    - If it fixes an open issue, please link to the issue here. -->
    This is curcial for meeting the requirement for Microsoft's 8.15
    release.
    
    ---------
    
    Co-authored-by: Yueqing Zhang <yueqingz@amd.com>
    Co-authored-by: Chunye Wang <chunywan@amd.com>

--- onnxruntime/core/session/provider_bridge_ort.cc
+++ onnxruntime/core/session/provider_bridge_ort.cc
@@ -1180,7 +1180,7 @@ struct ProviderHostImpl : ProviderHost {
   std::unique_ptr<GraphViewer> Graph__CreateGraphViewer(const Graph* p) override { return std::make_unique<GraphViewer>(*p); }
   std::unique_ptr<ONNX_NAMESPACE::GraphProto> Graph__ToGraphProto(const Graph* p) override { return std::make_unique<ONNX_NAMESPACE::GraphProto>(p->ToGraphProto()); }
   void Graph__SetInputs(Graph* p, gsl::span<const NodeArg* const> inputs) override { p->SetInputs(inputs); }
-
+  const std::unordered_map<std::string, int>& Graph__DomainToVersionMap(const Graph* p) const noexcept override { return p->DomainToVersionMap(); };
   NodeArg& Graph__GetOrCreateNodeArg(Graph* p, const std::string& name, const ONNX_NAMESPACE::TypeProto* p_arg_type) override { return p->GetOrCreateNodeArg(name, p_arg_type); }
   void Graph__AddOuterScopeNodeArg(Graph* p, const std::string& name) override { p->AddOuterScopeNodeArg(name); }
 
