b9a22210a63da83acd8d83e217cb195f3b76aa11
zz002 <zhenze.wang@amd.com>
2024-08-27 12:16:44 +0800

    [VitisAI] add OpSchema, VitisAI use IKernelLookup to check supported ops, VitisAI def_builder adds TypeConstraint related processing (#21688)
    ### Description
    <!-- Describe your changes. -->
    1. add OpSchema
    2. VitisAI use IKernelLookup to check supported ops
    3. VitisAI def_builder adds TypeConstraint related processing
    
    ### Motivation and Context
    <!-- - Why is this change required? What problem does it solve?
    - If it fixes an open issue, please link to the issue here. -->
    
    ---------
    
    Co-authored-by: Zhenze Wang <zhenzew@xilinx.com>

--- onnxruntime/core/providers/vitisai/vitisai_execution_provider.h
+++ onnxruntime/core/providers/vitisai/vitisai_execution_provider.h
@@ -41,7 +41,6 @@ class VitisAIExecutionProvider : public IExecutionProvider {
   const InlinedVector<const Node*> GetEpContextNodes() const override;
 
  private:
-  void CreateKernelRegistry();
   using my_ep_t = vaip_core::DllSafe<std::vector<std::unique_ptr<vaip_core::ExecutionProvider>>>;
   using my_ep_uptr_t = std::shared_ptr<my_ep_t>;
   // we have to hide the implementation by forward declaration.
