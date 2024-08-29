119e7c3a18edddfff81818b57dce653f96b1c75b
Yi Zhang <your@email.com>
2024-08-29 11:11:43 +0800

    add reminder

--- tools/ci_build/set-trigger-rules.py
+++ tools/ci_build/set-trigger-rules.py
@@ -51,10 +51,12 @@ def add_trigger_filter(file_name, trigger_lines):
         lines = f.readlines()
 
     start_marker = f"##### start trigger Don't edit it manually, Please do edit {os.path.basename(__file__)} ####"
+    reminder = "### please do rerun {os.path.basename(__file__)} ###"
     end_marker = "#### end trigger ####\n"
 
     if lines[0].startswith(start_marker):
-        for i in range(1, len(lines)):
+        lines[1] = reminder + "\n"
+        for i in range(2, len(lines)):
             if lines[i].startswith(end_marker):
                 lines[1:i] = trigger_lines
                 break
