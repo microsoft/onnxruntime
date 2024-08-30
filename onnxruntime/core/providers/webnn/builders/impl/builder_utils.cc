7550fec4aab4271aaaf9a789d074c3d243bd856a
Wanming Lin <wanming.lin@intel.com>
2024-08-30 09:01:56 +0800

    Revert "[WebNN EP] Remove NHWC preferred layout" (#21905)
    Reverts microsoft/onnxruntime#21570

--- onnxruntime/core/providers/webnn/builders/impl/builder_utils.cc
+++ onnxruntime/core/providers/webnn/builders/impl/builder_utils.cc
@@ -19,9 +19,10 @@ common::Status ComputeConvPads(const std::vector<int64_t> input_shape,
                                const std::vector<int64_t>& onnx_strides,
                                const std::vector<int64_t>& onnx_dilations,
                                AutoPadType auto_pad_type,
-                               std::vector<int64_t>& pads_out) {
-  const int64_t input_size_y = input_shape[2];
-  const int64_t input_size_x = input_shape[3];
+                               std::vector<int64_t>& pads_out,
+                               bool use_nchw) {
+  const int64_t input_size_y = use_nchw ? input_shape[2] : input_shape[1];
+  const int64_t input_size_x = use_nchw ? input_shape[3] : input_shape[2];
   const int64_t stride_y = onnx_strides[0];
   const int64_t stride_x = onnx_strides[1];
   const int64_t dilation_y = onnx_dilations[0];
@@ -53,15 +54,16 @@ common::Status HandleAutoPad(const std::vector<int64_t> input_shape,
                              const std::vector<int64_t>& onnx_strides,
                              const std::vector<int64_t>& onnx_dilations,
                              AutoPadType auto_pad_type,
-                             std::vector<int64_t>& pads_out) {
+                             std::vector<int64_t>& pads_out,
+                             bool use_nchw) {
   if (AutoPadType::SAME_UPPER == auto_pad_type) {
     ORT_RETURN_IF_ERROR(ComputeConvPads(input_shape, weight_size_y, weight_size_x,
                                         onnx_pads, onnx_strides, onnx_dilations,
-                                        AutoPadType::SAME_UPPER, pads_out));
+                                        AutoPadType::SAME_UPPER, pads_out, use_nchw));
   } else {
     ORT_RETURN_IF_ERROR(ComputeConvPads(input_shape, weight_size_y, weight_size_x,
                                         onnx_pads, onnx_strides, onnx_dilations,
-                                        AutoPadType::SAME_LOWER, pads_out));
+                                        AutoPadType::SAME_LOWER, pads_out, use_nchw));
   }
   return Status::OK();
 }
@@ -109,9 +111,10 @@ common::Status ComputeConvTransposePadsAndOutputShape(const std::vector<int64_t>
                                                       const std::vector<int64_t>& onnx_output_padding,
                                                       AutoPadType auto_pad_type,
                                                       std::vector<int64_t>& pads_out,
-                                                      std::vector<int64_t>& output_shape_out) {
-  const int64_t input_size_y = input_shape[2];
-  const int64_t input_size_x = input_shape[3];
+                                                      std::vector<int64_t>& output_shape_out,
+                                                      bool use_nchw) {
+  const int64_t input_size_y = use_nchw ? input_shape[2] : input_shape[1];
+  const int64_t input_size_x = use_nchw ? input_shape[3] : input_shape[2];
   const int64_t stride_y = onnx_strides[0];
   const int64_t stride_x = onnx_strides[1];
   const int64_t dilation_y = onnx_dilations[0];
