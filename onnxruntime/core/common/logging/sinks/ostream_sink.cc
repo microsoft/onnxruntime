cd1380c1be1148e573e677281e41466a3171cbb6
Jian Chen <cjian@microsoft.com>
2024-08-30 14:17:59 -0700

    change ifndef to ifdef

--- onnxruntime/core/common/logging/sinks/ostream_sink.cc
+++ onnxruntime/core/common/logging/sinks/ostream_sink.cc
@@ -31,7 +31,7 @@ void OStreamSink::SendImpl(const Timestamp& timestamp, const std::string& logger
   //
   // Going with #2 as it should scale better at the cost of creating the message in memory first
   // before sending to the stream.
-#ifndef _WIN32
+#ifdef _WIN32
   std::wostringstream msg;
 #else
   std::ostringstream msg;
