8ac2c0e01d6a0217b320d28b41069cd2384efadf
Tianlei Wu <tlwu@microsoft.com>
2024-08-30 21:52:59 +0000

    upgrade cutlass to 3.5.1

--- cmake/external/cutlass.cmake
+++ cmake/external/cutlass.cmake
@@ -3,7 +3,6 @@ FetchContent_Declare(
   cutlass
   URL ${DEP_URL_cutlass}
   URL_HASH SHA1=${DEP_SHA1_cutlass}
-  PATCH_COMMAND ${Patch_EXECUTABLE} --binary --ignore-whitespace -p1 < ${PROJECT_SOURCE_DIR}/patches/cutlass/cutlass_3.5.0.patch
 )
 
 FetchContent_GetProperties(cutlass)
