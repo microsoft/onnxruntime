6ad89c56bfe85794c50f1e547446d649083be53a
Yulong Wang <7679871+fs-eire@users.noreply.github.com>
2024-09-01 23:32:51 -0700

    add some unary ops

--- onnxruntime/core/providers/webgpu/math/unary_elementwise_ops.cc
+++ onnxruntime/core/providers/webgpu/math/unary_elementwise_ops.cc
@@ -24,43 +24,172 @@ Status UnaryElementwiseProgram::GenerateShaderCode(ShaderHelper& shader) const {
   return Status::OK();
 }
 
-#define WEBGPU_ELEMENTWISE_IMPL(OP_TYPE, ...)                                  \
-  class OP_TYPE final : public WebGpuKernel {                                  \
-   public:                                                                     \
-    OP_TYPE(const OpKernelInfo& info) : WebGpuKernel{info} {}                  \
-                                                                               \
-   protected:                                                                  \
-    Status ComputeInternal(ComputeContext& context) const override {           \
-      const auto* input_tensor = context.Input(0);                             \
-      auto* output_tensor = context.Output(0, input_tensor->Shape());          \
-      SafeInt<uint32_t> vec_size = (input_tensor->Shape().Size() + 3) / 4;     \
-      UnaryElementwiseProgram program{#OP_TYPE, __VA_ARGS__};                  \
-      program                                                                  \
-          .Inputs({{input_tensor, ProgramInputTensorDependency::Type}})        \
-          .Outputs({output_tensor})                                            \
-          .DispatchGroupSize((vec_size + WORKGROUP_SIZE - 1) / WORKGROUP_SIZE) \
-          .UniformVariables({                                                  \
-              {static_cast<uint32_t>(vec_size)},                               \
-          });                                                                  \
-      return context.RunProgram(program);                                      \
-    }                                                                          \
+Status UnaryElementwise::ComputeInternal(ComputeContext& context) const {
+  const auto* input_tensor = context.Input(0);
+  auto* output_tensor = context.Output(0, input_tensor->Shape());
+  SafeInt<uint32_t> vec_size = (input_tensor->Shape().Size() + 3) / 4;
+  UnaryElementwiseProgram program{kernel_name_, expression_, additional_impl_};
+  program
+      .Inputs({{input_tensor, ProgramInputTensorDependency::Type}})
+      .Outputs({output_tensor})
+      .DispatchGroupSize((vec_size + WORKGROUP_SIZE - 1) / WORKGROUP_SIZE)
+      .UniformVariables({
+          {static_cast<uint32_t>(vec_size)},
+      });
+  ORT_RETURN_IF_ERROR(ConfigureProgram(program));
+  return context.RunProgram(program);
+}
+
+#define WEBGPU_ELEMENTWISE_IMPL(OP_TYPE, ...)                                            \
+  class OP_TYPE final : public UnaryElementwise {                                        \
+   public:                                                                               \
+    OP_TYPE(const OpKernelInfo& info) : UnaryElementwise{info, #OP_TYPE, __VA_ARGS__} {} \
   };
 
-#define WEBGPU_ELEMENTWISE_KERNEL(OP_TYPE, VERSION, KERNEL_CLASS, TYPE) \
-  ONNX_OPERATOR_KERNEL_EX(                                              \
-      OP_TYPE, kOnnxDomain, VERSION, kWebGpuExecutionProvider,          \
-      KernelDefBuilder().TypeConstraint("T", TYPE),                     \
-      KERNEL_CLASS);
+#define WEBGPU_ELEMENTWISE_KERNEL(OP_TYPE_AND_CLASS_NAME, VERSION, TYPE)      \
+  ONNX_OPERATOR_KERNEL_EX(                                                    \
+      OP_TYPE_AND_CLASS_NAME, kOnnxDomain, VERSION, kWebGpuExecutionProvider, \
+      KernelDefBuilder().TypeConstraint("T", TYPE),                           \
+      OP_TYPE_AND_CLASS_NAME);
 
-#define WEBGPU_ELEMENTWISE_VERSIONED_KERNEL(OP_TYPE, VERSION_FROM, VERSION_TO, KERNEL_CLASS, TYPE) \
-  ONNX_OPERATOR_VERSIONED_KERNEL_EX(                                                               \
-      OP_TYPE, kOnnxDomain, VERSION_FROM, VERSION_TO, kWebGpuExecutionProvider,                    \
-      KernelDefBuilder().TypeConstraint("T", TYPE),                                                \
-      KERNEL_CLASS);
+#define WEBGPU_ELEMENTWISE_VERSIONED_KERNEL(OP_TYPE_AND_CLASS_NAME, VERSION_FROM, VERSION_TO, TYPE) \
+  ONNX_OPERATOR_VERSIONED_KERNEL_EX(                                                                \
+      OP_TYPE_AND_CLASS_NAME, kOnnxDomain, VERSION_FROM, VERSION_TO, kWebGpuExecutionProvider,      \
+      KernelDefBuilder().TypeConstraint("T", TYPE),                                                 \
+      OP_TYPE_AND_CLASS_NAME);
+
+//
+// math
+//
 
 WEBGPU_ELEMENTWISE_IMPL(Abs, "abs(a)")
-WEBGPU_ELEMENTWISE_VERSIONED_KERNEL(Abs, 6, 12, Abs, WebGpuSupportedFloatTypes())
-WEBGPU_ELEMENTWISE_KERNEL(Abs, 13, Abs, WebGpuSupportedFloatTypes())
+WEBGPU_ELEMENTWISE_VERSIONED_KERNEL(Abs, 6, 12, WebGpuSupportedFloatTypes())
+WEBGPU_ELEMENTWISE_KERNEL(Abs, 13, WebGpuSupportedFloatTypes())
+
+WEBGPU_ELEMENTWISE_IMPL(Neg, "-a")
+WEBGPU_ELEMENTWISE_VERSIONED_KERNEL(Neg, 6, 12, WebGpuSupportedFloatTypes())
+WEBGPU_ELEMENTWISE_KERNEL(Neg, 13, WebGpuSupportedFloatTypes())
+
+WEBGPU_ELEMENTWISE_IMPL(Floor, "floor(a)")
+WEBGPU_ELEMENTWISE_VERSIONED_KERNEL(Floor, 6, 12, WebGpuSupportedFloatTypes())
+WEBGPU_ELEMENTWISE_KERNEL(Floor, 13, WebGpuSupportedFloatTypes())
+
+WEBGPU_ELEMENTWISE_IMPL(Ceil, "ceil(a)")
+WEBGPU_ELEMENTWISE_VERSIONED_KERNEL(Ceil, 6, 12, WebGpuSupportedFloatTypes())
+WEBGPU_ELEMENTWISE_KERNEL(Ceil, 13, WebGpuSupportedFloatTypes())
+
+WEBGPU_ELEMENTWISE_IMPL(Reciprocal, "1.0/a")
+WEBGPU_ELEMENTWISE_VERSIONED_KERNEL(Reciprocal, 6, 12, WebGpuSupportedFloatTypes())
+WEBGPU_ELEMENTWISE_KERNEL(Reciprocal, 13, WebGpuSupportedFloatTypes())
+
+WEBGPU_ELEMENTWISE_IMPL(Sqrt, "sqrt(a)")
+WEBGPU_ELEMENTWISE_VERSIONED_KERNEL(Sqrt, 6, 12, WebGpuSupportedFloatTypes())
+WEBGPU_ELEMENTWISE_KERNEL(Sqrt, 13, WebGpuSupportedFloatTypes())
+
+WEBGPU_ELEMENTWISE_IMPL(Exp, "exp(a)")
+WEBGPU_ELEMENTWISE_VERSIONED_KERNEL(Exp, 6, 12, WebGpuSupportedFloatTypes())
+WEBGPU_ELEMENTWISE_KERNEL(Exp, 13, WebGpuSupportedFloatTypes())
+
+constexpr char ErfImpl[] = R"(
+const r0: x_value_t = 0.3275911;
+const r1: x_value_t = 0.254829592;
+const r2: x_value_t = -0.284496736;
+const r3: x_value_t = 1.421413741;
+const r4: x_value_t = -1.453152027;
+const r5: x_value_t = 1.061405429;
+
+fn erf_v(v: vec4<x_value_t>) -> vec4<x_value_t> {
+  let absv = abs(v);
+  let x = 1.0 / (1.0 + r0 * absv);
+  return sign(v) * (1.0 - ((((r5 * x + r4) * x + r3) * x + r2) * x + r1) * x * exp(-absv * absv));
+}
+)";
+
+WEBGPU_ELEMENTWISE_IMPL(Erf, "erf_v(a)", ErfImpl)
+WEBGPU_ELEMENTWISE_VERSIONED_KERNEL(Erf, 9, 12, WebGpuSupportedFloatTypes())
+WEBGPU_ELEMENTWISE_KERNEL(Erf, 13, WebGpuSupportedFloatTypes())
+
+WEBGPU_ELEMENTWISE_IMPL(Log, "log(a)")
+WEBGPU_ELEMENTWISE_VERSIONED_KERNEL(Log, 6, 12, WebGpuSupportedFloatTypes())
+WEBGPU_ELEMENTWISE_KERNEL(Log, 13, WebGpuSupportedFloatTypes())
+
+WEBGPU_ELEMENTWISE_IMPL(Sigmoid, "1.0 / (1.0 + exp(-a))")
+WEBGPU_ELEMENTWISE_VERSIONED_KERNEL(Sigmoid, 6, 12, WebGpuSupportedFloatTypes())
+WEBGPU_ELEMENTWISE_KERNEL(Sigmoid, 13, WebGpuSupportedFloatTypes())
+
+class HardSigmoid final : public UnaryElementwise {
+ public:
+  HardSigmoid(const OpKernelInfo& info)
+      : UnaryElementwise{
+            info,
+            "HardSigmoid",
+            // alpha = uniforms.f32_attr[0]
+            // beta = uniforms.f32_attr[1]
+            "max(vec4<x_value_t>(0.0), min(vec4<x_value_t>(1.0), x_value_t(uniforms.f32_attr[0]) * a + vec4<x_value_t>(uniforms.f32_attr[1])))"} {
+    info.GetAttrOrDefault("alpha", attr, 0.2f);
+    info.GetAttrOrDefault("beta", attr + 1, 0.5f);
+  }
+
+  Status ConfigureProgram(UnaryElementwiseProgram& program) const override {
+    program.UniformVariables({gsl::make_span(attr, 2), {}});
+    return Status::OK();
+  }
+
+ protected:
+  float attr[2];
+};
+
+WEBGPU_ELEMENTWISE_KERNEL(HardSigmoid, 6, WebGpuSupportedFloatTypes())
+
+WEBGPU_ELEMENTWISE_IMPL(Sin, "sin(a)")
+WEBGPU_ELEMENTWISE_KERNEL(Sin, 7, WebGpuSupportedFloatTypes())
+
+WEBGPU_ELEMENTWISE_IMPL(Cos, "cos(a)")
+WEBGPU_ELEMENTWISE_KERNEL(Cos, 7, WebGpuSupportedFloatTypes())
+
+WEBGPU_ELEMENTWISE_IMPL(Tan, "tan(a)")
+WEBGPU_ELEMENTWISE_KERNEL(Tan, 7, WebGpuSupportedFloatTypes())
+
+WEBGPU_ELEMENTWISE_IMPL(Asin, "asin(a)")
+WEBGPU_ELEMENTWISE_KERNEL(Asin, 7, WebGpuSupportedFloatTypes())
+
+WEBGPU_ELEMENTWISE_IMPL(Acos, "acos(a)")
+WEBGPU_ELEMENTWISE_KERNEL(Acos, 7, WebGpuSupportedFloatTypes())
+
+WEBGPU_ELEMENTWISE_IMPL(Atan, "atan(a)")
+WEBGPU_ELEMENTWISE_KERNEL(Atan, 7, WebGpuSupportedFloatTypes())
+
+WEBGPU_ELEMENTWISE_IMPL(Sinh, "sinh(a)")
+WEBGPU_ELEMENTWISE_KERNEL(Sinh, 9, WebGpuSupportedFloatTypes())
+
+WEBGPU_ELEMENTWISE_IMPL(Cosh, "cosh(a)")
+WEBGPU_ELEMENTWISE_KERNEL(Cosh, 9, WebGpuSupportedFloatTypes())
+
+WEBGPU_ELEMENTWISE_IMPL(Tanh, "tanh(a)")
+WEBGPU_ELEMENTWISE_VERSIONED_KERNEL(Tanh, 6, 12, WebGpuSupportedFloatTypes())
+WEBGPU_ELEMENTWISE_KERNEL(Tanh, 13, WebGpuSupportedFloatTypes())
+
+WEBGPU_ELEMENTWISE_IMPL(Asinh, "asinh(a)")
+WEBGPU_ELEMENTWISE_KERNEL(Asinh, 9, WebGpuSupportedFloatTypes())
+
+WEBGPU_ELEMENTWISE_IMPL(Acosh, "acosh(a)")
+WEBGPU_ELEMENTWISE_KERNEL(Acosh, 9, WebGpuSupportedFloatTypes())
+
+WEBGPU_ELEMENTWISE_IMPL(Atanh, "atanh(a)")
+WEBGPU_ELEMENTWISE_KERNEL(Atanh, 9, WebGpuSupportedFloatTypes())
+
+// todo: logical ops
+
+//
+// activation
+//
+
+// todo: clip
+
+// constexpr char EluImpl[] = R"(
+//)";
+//
+// WEBGPU_ELEMENTWISE_IMPL(Elu, "elu_v(a)", )
 
 // TODO: add other unary elementwise ops
 
--- onnxruntime/core/providers/webgpu/math/unary_elementwise_ops.h
+++ onnxruntime/core/providers/webgpu/math/unary_elementwise_ops.h
@@ -11,15 +11,44 @@ namespace webgpu {
 
 class UnaryElementwiseProgram final : public Program<UnaryElementwiseProgram> {
  public:
-  UnaryElementwiseProgram(const std::string& kernel_name, const std::string& expression, const std::string& additional_impl = "")
+  UnaryElementwiseProgram(const std::string& kernel_name, std::string_view expression, std::string_view additional_impl)
       : Program{kernel_name}, expression_{expression}, additional_impl_{additional_impl} {
   }
 
   Status GenerateShaderCode(ShaderHelper& sh) const override;
 
-  WEBGPU_PROGRAM_DEFINE_UNIFORM_VARIABLES({"vec_size", ProgramUniformVariableDataType::Uint32});
+  WEBGPU_PROGRAM_DEFINE_UNIFORM_VARIABLES(
+      {"vec_size", ProgramUniformVariableDataType::Uint32},   // output size
+      {"f32_attr", ProgramUniformVariableDataType::Float32},  // float type attribute(s)
+      {"i32_attr", ProgramUniformVariableDataType::Int32});   // int type attribute(s)
 
  private:
+  std::string_view expression_;
+  std::string_view additional_impl_;
+};
+
+// TODO: after upgrading to C++20, use consteval to make a compile-time constructor so that it will be safe to switch
+//       the std::string to std::string_view. This will avoid the cost of copying the string.
+
+class UnaryElementwise : public WebGpuKernel {
+ public:
+  UnaryElementwise(const OpKernelInfo& info,
+                   const std::string& kernel_name,
+                   const std::string& expression,
+                   const std::string& additional_impl = "") : WebGpuKernel{info},
+                                                              kernel_name_{kernel_name},
+                                                              expression_{expression},
+                                                              additional_impl_{additional_impl} {}
+
+ protected:
+  Status ComputeInternal(ComputeContext& context) const final;
+  virtual Status ConfigureProgram(UnaryElementwiseProgram& program) const {
+    program.UniformVariables({{}, {}});  // empty for both float and int attribute(s)
+    return Status::OK();
+  }
+
+ private:
+  std::string kernel_name_;
   std::string expression_;
   std::string additional_impl_;
 };
