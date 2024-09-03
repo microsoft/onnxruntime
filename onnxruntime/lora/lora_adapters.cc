2c29f64c0cbf74e55d8c61f2395ffbacd6ab2a9b
Dmitri Smirnov <dmitrism@microsoft.com>
2024-09-03 14:34:49 -0700

    Implement LoraAdapter and public APIs

--- onnxruntime/lora/lora_adapters.cc
+++ onnxruntime/lora/lora_adapters.cc
@@ -9,43 +9,49 @@
 
 namespace onnxruntime {
 namespace lora {
-namespace details {
 
-LoraParam::LoraParam(std::string name, OrtValue ort_value)
+LoraAdapter::LoraParam::LoraParam(std::string name, OrtValue ort_value) noexcept
     : name_(std::move(name)), ort_value_(std::move(ort_value)) {}
 
-BinaryFormatHolder::~BinaryFormatHolder() = default;
-
-void BinaryFormatHolder::Load(const std::filesystem::path& file_path) {
+void LoraAdapter::Load(const std::filesystem::path& file_path) {
   auto buffer = utils::LoadLoraAdapterBytes(file_path);
   adapter_ = utils::ValidateAndGetAdapterFromBytes(buffer);
   buffer_.emplace<BufferHolder>(std::move(buffer));
+
+  InitializeParamsValues();
 }
 
-void BinaryFormatHolder::MemoryMap(const std::filesystem::path& file_path) {
+void LoraAdapter::MemoryMap(const std::filesystem::path& file_path) {
   auto [mapped_memory, file_size] = utils::MemoryMapAdapterFile(file_path);
   auto u8_span = ReinterpretAsSpan<const uint8_t>(gsl::make_span(mapped_memory.get(), file_size));
   adapter_ = utils::ValidateAndGetAdapterFromBytes(u8_span);
   buffer_.emplace<MemMapHolder>(std::move(mapped_memory), file_size);
+
+  InitializeParamsValues();
 }
 
-size_t BinaryFormatHolder::GetSize() const {
+void LoraAdapter::InitializeParamsValues() {
+  if (adapter_ == nullptr) {
+    ORT_THROW("Adapter is not loaded yet.");
+  }
+
+  const auto* params = adapter_->parameters();
+  InlinedHashMap<std::string, LoraParam> params_values;
+  params_values.reserve(params->size());
+  for (const auto* param : *params) {
+    auto [name, ort_value] = utils::CreateOrtValueOverLoraParameter(*param);
+    params_values.emplace(name, LoraParam(std::move(name), std::move(ort_value)));
+  }
+  params_values_.swap(params_values);
+}
+
+size_t LoraAdapter::GetSize() const {
   if (std::holds_alternative<MemMapHolder>(buffer_)) {
-    return std::get<0>(buffer_).file_size_;
+    return std::get<1>(buffer_).file_size_;
   } else if (std::holds_alternative<BufferHolder>(buffer_)) {
-    return std::get<1>(buffer_).buffer_.size();
+    return std::get<2>(buffer_).buffer_.size();
   }
   ORT_THROW("Non-exhaustive visitor for BinaryFormatHolder::GetSize()");
-}
-
-}  // namespace details
-
-void LoraAdapter::Load(const std::filesystem::path& file_path) {
-  binary_format_holder_.Load(file_path);
-}
-
-void LoraAdapter::MemoryMap(const std::filesystem::path& file_path) {
-  binary_format_holder_.MemoryMap(file_path);
 }
 
 }  // namespace lora
