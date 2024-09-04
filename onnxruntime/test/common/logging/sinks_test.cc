63bb6be7fd4145747b8d1592d7cee6e60931dcdb
Jian Chen <cjian@microsoft.com>
2024-09-04 12:52:50 -0700

    Add core/common/common.h

--- onnxruntime/test/common/logging/sinks_test.cc
+++ onnxruntime/test/common/logging/sinks_test.cc
@@ -1,6 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-
+#include "core/common/common.h"
 #include "core/common/logging/capture.h"
 #include "core/common/logging/logging.h"
 #include "core/common/logging/sinks/cerr_sink.h"
