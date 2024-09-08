c41b40dd5a7b10b10eb13459211f270980f84c69
Yi Zhang <your@email.com>
2024-09-08 17:26:43 +0800

    one message

--- cmake/external/xnnpack.cmake
+++ cmake/external/xnnpack.cmake
@@ -42,6 +42,7 @@ set(onnxruntime_EXTERNAL_LIBRARIES_XNNPACK XNNPACK pthreadpool)
 if(onnxruntime_target_platform MATCHES "ARM-like")
   FetchContent_Declare(KleidiAI URL ${DEP_URL_kleidiai} URL_HASH SHA1=${DEP_SHA1_kleidiai})
   onnxruntime_fetchcontent_makeavailable(kleidiai)
+  message(STATUS, "Adding KleidiAI to XNNPACK")
   set(onnxruntime_EXTERNAL_LIBRARIES_XNNPACK ${onnxruntime_EXTERNAL_LIBRARIES_XNNPACK} kleidiai)
 
 # the XNNPACK CMake setup doesn't include the WASM kernels so we have to manually set those up
