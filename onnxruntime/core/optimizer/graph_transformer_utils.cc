2cdc05f189bb34259deb3e3daef3289f1565558c
Peishen Yan <peishen.yan@intel.com>
2024-09-09 11:27:52 +0800

    Move Gelu and LayerNorm fusion to L1 optimization (#21332)
    According to https://github.com/microsoft/onnxruntime/issues/20915, we
    move the Gelu and LayerNorm fusion to L1 with a condition on the ONNX
    opset the model imports (LayerNorm requires opset 16+ and Gelu requires
    opset 20+.) If the opset version doesn't meet the requirements, the
    fusion is delayed to L2 optimization since the internal contrib op
    doesn't have a requirement for any specific ONNX opset.
    
    ---------
    
    Co-authored-by: Scott McKay <Scott.McKay@microsoft.com>
    Co-authored-by: Edward Chen <18449977+edgchen1@users.noreply.github.com>

--- onnxruntime/core/optimizer/graph_transformer_utils.cc
+++ onnxruntime/core/optimizer/graph_transformer_utils.cc
@@ -235,6 +235,9 @@ InlinedVector<std::unique_ptr<GraphTransformer>> GenerateTransformers(
       transformers.emplace_back(std::make_unique<FreeDimensionOverrideTransformer>(
           session_options.free_dimension_overrides));
 
+      transformers.emplace_back(std::make_unique<GeluFusion>());
+      transformers.emplace_back(std::make_unique<LayerNormFusion>());
+
       if (!disable_quant_qdq) {
         transformers.emplace_back(std::make_unique<QDQPropagationTransformer>());
 
@@ -325,8 +328,8 @@ InlinedVector<std::unique_ptr<GraphTransformer>> GenerateTransformers(
 
       transformers.emplace_back(std::make_unique<ConvActivationFusion>(cpu_rocm_acl_armnn_js_eps));
 
-      transformers.emplace_back(std::make_unique<GeluFusion>(cpu_cuda_dml_rocm_eps));
-      transformers.emplace_back(std::make_unique<LayerNormFusion>(cpu_cuda_dml_rocm_eps));
+      transformers.emplace_back(std::make_unique<GeluFusion>(cpu_cuda_dml_rocm_eps, level));
+      transformers.emplace_back(std::make_unique<LayerNormFusion>(cpu_cuda_dml_rocm_eps, level));
       transformers.emplace_back(std::make_unique<SimplifiedLayerNormFusion>(cpu_cuda_rocm_eps));
       transformers.emplace_back(std::make_unique<AttentionFusion>(cpu_cuda_dml_rocm_eps));
       transformers.emplace_back(std::make_unique<EmbedLayerNormFusion>(cpu_cuda_dml_rocm_eps));
--- onnxruntime/core/optimizer/layer_norm_fusion.h
+++ onnxruntime/core/optimizer/layer_norm_fusion.h
@@ -17,9 +17,26 @@ The formula corresponding to LayerNorm activation subgraph:
 
 */
 class LayerNormFusion : public GraphTransformer {
+ private:
+  TransformerLevel optimization_level_ = TransformerLevel::Level1;
+  bool allow_contrib_op_in_level_1_ = false;
+  std::string GetLayerNormFusionName(TransformerLevel level) {
+    switch (level) {
+      case TransformerLevel::Level1:
+        return "LayerNormFusionL1";
+      case TransformerLevel::Level2:
+        return "LayerNormFusionL2";
+      default:
+        return "LayerNormFusion";
+    }
+  }
+
  public:
-  LayerNormFusion(const InlinedHashSet<std::string_view>& compatible_execution_providers = {}) noexcept
-      : GraphTransformer("LayerNormFusion", compatible_execution_providers) {}
+  LayerNormFusion(const InlinedHashSet<std::string_view>& compatible_execution_providers = {},
+                  TransformerLevel level = TransformerLevel::Level1, bool allow_contrib_op_in_level_1 = false) noexcept
+      : GraphTransformer(GetLayerNormFusionName(level), compatible_execution_providers),
+        optimization_level_(level),
+        allow_contrib_op_in_level_1_(allow_contrib_op_in_level_1) {}
 
   Status ApplyImpl(Graph& graph, bool& modified, int graph_level, const logging::Logger& logger) const override;
 };
