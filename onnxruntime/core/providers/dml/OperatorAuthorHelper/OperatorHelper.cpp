6215cd6043915c397218c4b5e4ab24130e81a566
Anagha Rao <anagrao@microsoft.com>
2024-09-10 10:49:28 -0700

    Fix for 1D Tensor Tranposing

--- onnxruntime/core/providers/dml/OperatorAuthorHelper/OperatorHelper.cpp
+++ onnxruntime/core/providers/dml/OperatorAuthorHelper/OperatorHelper.cpp
@@ -606,8 +606,7 @@ namespace OperatorHelper
 
     std::pair<std::vector<uint32_t>, std::vector<uint32_t>> GetFusedMatMulSizesAndStrides(
         gsl::span<const uint32_t> sizes,
-        int32_t transBatch,
-        int32_t transpose)
+        int32_t transBatch)
     {
         const uint32_t dimensionCount = gsl::narrow_cast<uint32_t>(sizes.size());
         std::vector<uint32_t> newStrides(dimensionCount);
