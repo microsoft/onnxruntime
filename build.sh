57bea3980c3c6bf5d7ed3d46fdfd5fcd51920dc9
Jingyan Wang <jingywa@microsoft.com>
2024-09-10 23:01:11 +0000

    Remove unintended change

--- build.sh
+++ build.sh
@@ -18,5 +18,4 @@ elif [[ "$*" == *"--android"* ]]; then
     DIR_OS="Android"
 fi
 
-# python3 $DIR/tools/ci_build/build.py --build_dir $DIR/build_trt86/$DIR_OS "$@"
-python3 $DIR/tools/ci_build/build.py --build_dir $DIR/build_trt104/$DIR_OS "$@"
+python3 $DIR/tools/ci_build/build.py --build_dir $DIR/build/$DIR_OS "$@"
