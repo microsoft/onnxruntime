bfb939a42369c228326fc5c35709e01455f99e24
Prathik Rao <prathik.rao@gmail.com>
2024-09-11 12:10:39 -0700

    move constructor test

--- js/common/test/type-tests/tensor/create-new-uint8.ts
+++ js/common/test/type-tests/tensor/create-new-uint8.ts
@@ -12,8 +12,3 @@ new ort.Tensor(new Uint8Array(1));
 //
 // {type-tests}|pass
 new ort.Tensor(new Uint8ClampedArray(1));
-
-// construct from type (bool), data (Uint8ClampedArray) and shape (number array)
-//
-// {type-tests}|fail
-new ort.Tensor('bool', new Uint8ClampedArray([255, 256]), [2]);
