68df34db9475722d8423e45b81fd0f790578b0f1
Adrian Lizarraga <adlizarraga@microsoft.com>
2024-09-12 12:53:52 -0700

    Update onnxruntime/core/providers/qnn/builder/qnn_model_wrapper.cc

--- onnxruntime/core/providers/qnn/builder/qnn_model_wrapper.cc
+++ onnxruntime/core/providers/qnn/builder/qnn_model_wrapper.cc
@@ -308,7 +308,6 @@ bool QnnModelWrapper::GetOnnxShape(const NodeArg& node_arg, std::vector<uint32_t
     return true;
   }
 
-  // We already checked the shape has no dynamic dimension
   for (const auto& dim : shape_proto->dim()) {
     if (!dim.has_dim_value()) {
       return false;  // Do not support dynamic shapes.
