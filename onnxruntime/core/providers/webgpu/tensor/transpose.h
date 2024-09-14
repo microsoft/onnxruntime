ff34dd94d875f35de7aaeefffad1d3257a45cd51
Xu Xing <xing.xu@intel.com>
2024-09-14 10:41:07 +0800

    [webgpu native] Add transpose shared

--- onnxruntime/core/providers/webgpu/tensor/transpose.h
+++ onnxruntime/core/providers/webgpu/tensor/transpose.h
@@ -13,8 +13,8 @@ namespace webgpu {
 
 class TransposeProgram final : public Program<TransposeProgram> {
  public:
-  TransposeProgram(const gsl::span<const size_t>& permutations)
-      : Program{"Transpose"}, perm_(permutations.begin(), permutations.end()) {
+  TransposeProgram(const gsl::span<const size_t>& permutations, bool use_shared, const int tile_size)
+      : Program{"Transpose"}, perm_(permutations.begin(), permutations.end()), use_shared_(use_shared), tile_size_(tile_size) {
   }
 
   Status GenerateShaderCode(ShaderHelper& sh) const override;
@@ -22,7 +22,9 @@ class TransposeProgram final : public Program<TransposeProgram> {
   WEBGPU_PROGRAM_DEFINE_UNIFORM_VARIABLES({"output_size", ProgramUniformVariableDataType::Uint32});
 
  private:
-  InlinedVector<size_t> perm_;
+  InlinedVector<int64_t> perm_;
+  const bool use_shared_;
+  const uint32_t tile_size_;
 };
 
 class Transpose final : public WebGpuKernel, public TransposeBase {
