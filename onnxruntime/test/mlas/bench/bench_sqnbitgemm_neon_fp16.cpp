76a4172b41ebaa8de655f59c048871463c025b20
Jing Fang <fajin@microsoft.com>
2024-09-23 16:36:07 -0700

    added benchmarks

--- onnxruntime/test/mlas/bench/bench_sqnbitgemm_neon_fp16.cpp
+++ onnxruntime/test/mlas/bench/bench_sqnbitgemm_neon_fp16.cpp
@@ -1,13 +1,15 @@
 #include "bench_util.h"
-#include "mlasi.h"
+#include "core/mlas/lib/mlasi.h"
+
+#if defined(MLAS_F16VEC_INTRINSICS_SUPPORTED) && defined(MLAS_TARGET_ARM64)
 
 void BM_ConvertF16ToF32(benchmark::State& state) {
     bool aligned = static_cast<bool>(state.range(0));
     const size_t count = 1 << 18;
-    const auto src = RandomVectorUniform<unsigned short>(count, 0, 60000);
-    const auto dst = std::vector<float>(count + 16);
+    auto src = RandomVectorUniform<unsigned short>(count, 0, 60000);
+    auto dst = std::vector<float>(count + 16);
     auto aligned_dst = (reinterpret_cast<intptr_t>(dst.data()) + 15) & (~15);
-    const float* dst_start = aligned ? reinterpret_cast<float*>(aligned_dst)
+    float* dst_start = aligned ? reinterpret_cast<float*>(aligned_dst)
                                      : reinterpret_cast<float*>(aligned_dst + 1);
 
     // Warm up
@@ -21,10 +23,10 @@ void BM_ConvertF16ToF32(benchmark::State& state) {
 void BM_ConvertF32ToF16(benchmark::State& state) {
     bool aligned = static_cast<bool>(state.range(0));
     const size_t count = 1 << 18;
-    const auto src = RandomVectorUniform(count, -30000.0f, 30000.0f);
-    const auto dst = std::vector<unsigned short>(count + 16);
+    auto src = RandomVectorUniform(count, -30000.0f, 30000.0f);
+    auto dst = std::vector<unsigned short>(count + 16);
     auto aligned_dst = (reinterpret_cast<intptr_t>(dst.data()) + 15) & (~15);
-    const unsigned short* dst_start = aligned ? reinterpret_cast<unsigned short*>(aligned_dst)
+    unsigned short* dst_start = aligned ? reinterpret_cast<unsigned short*>(aligned_dst)
                                               : reinterpret_cast<unsigned short*>(aligned_dst + 1);
 
     // Warm up
@@ -39,12 +41,14 @@ BENCHMARK(BM_ConvertF16ToF32)
     ->UseRealTime()
     ->Apply([](benchmark::internal::Benchmark* b) {
       b->ArgNames({"aligned"});
-      b->Args({0, 1});
+      b->Argsproduct({{0, 1}});
     });
 
 BENCHMARK(BM_ConvertF32ToF16)
     ->UseRealTime()
     ->Apply([](benchmark::internal::Benchmark* b) {
       b->ArgNames({"aligned"});
-      b->Args({0, 1});
+      b->ArgsProduct({{0, 1}});
     });
+
+#endif  // defined(MLAS_F16VEC_INTRINSICS_SUPPORTED) && defined(MLAS_TARGET_ARM64)
