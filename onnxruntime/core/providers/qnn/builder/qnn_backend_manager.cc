e4e617f3ab40c718afb48cbd7df4e398b90a1e1a
Hector Li <hecli@microsoft.com>
2024-09-25 10:45:25 -0700

    Update code regarding some QNN bug fix 1. QnnProfile_ExtendedEventData_t.version is not initialized in Qnn 2. Failed to finalize the graph for HardSigmoid with FP16 precision

--- onnxruntime/core/providers/qnn/builder/qnn_backend_manager.cc
+++ onnxruntime/core/providers/qnn/builder/qnn_backend_manager.cc
@@ -1179,8 +1179,7 @@ Status QnnBackendManager::ExtractProfilingEventExtended(
 #endif
 
   if (!tracelogging_provider_ep_enabled) {
-    // QNN issue, the version number not correct, ticket created
-    // if (event_data_extended.version == QNN_PROFILE_DATA_VERSION_1) {
+    if (event_data_extended.version == QNN_PROFILE_DATA_VERSION_1) {
     outfile << event_data_extended.v1.timestamp << ","
             << message << ","
             << ExtractQnnScalarValue(event_data_extended.v1.value) << ","
@@ -1189,7 +1188,7 @@ Status QnnBackendManager::ExtractProfilingEventExtended(
             << ","
             << eventLevel << ","
             << (event_data_extended.v1.identifier ? event_data_extended.v1.identifier : "NULL") << "\n";
-    //}
+    }
   } else {
 #ifdef _WIN32
     LogQnnProfileEventAsTraceLogging(
