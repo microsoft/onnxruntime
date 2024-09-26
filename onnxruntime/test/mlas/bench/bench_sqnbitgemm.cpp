46e0941a44429a5fc58f551d9a7a036c4935c623
Yufeng Li <liyufeng1987@gmail.com>
2024-09-26 14:07:51 -0700

    remove neural-speed

--- onnxruntime/test/mlas/bench/bench_sqnbitgemm.cpp
+++ onnxruntime/test/mlas/bench/bench_sqnbitgemm.cpp
@@ -118,8 +118,8 @@ static void SQNBitGemmArgs(benchmark::internal::Benchmark* b) {
   b->ArgNames({"BlkLen", "M", "N", "K", "Threads", "Symmetric", "HasBias", "ComputeType"});
 
   b->ArgsProduct({
-      {16, 32, 64, 128, 256},                  // BlkLen
-      {1, 1024, 2048},                         // M
+      {128},                  // BlkLen
+      {1},                         // M
       {4096, 11008},                           // N
       {4096, 11008},                           // K
       {1, 8},                                  // Threads
