42e13fd86b600f99aa8b4d0ea673426096132ee2
Adam Pocock <adam.pocock@oracle.com>
2024-09-27 10:56:40 -0400

    Fixing copyright.

--- java/src/main/java/ai/onnxruntime/OrtLoraAdapter.java
+++ java/src/main/java/ai/onnxruntime/OrtLoraAdapter.java
@@ -1,5 +1,6 @@
 /*
- * Copyright © 2024, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2024, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) Microsoft Corporation. All rights reserved.
  * Licensed under the MIT License.
  */
 package ai.onnxruntime;
