b930f41fad92630a00bf53df799dc8477e154276
Qin Jiajia <jiajia.qin@intel.com>
2024-09-27 13:42:54 +0800

    format indent

--- onnxruntime/contrib_ops/webgpu/quantization/matmul_nbits.cc
+++ onnxruntime/contrib_ops/webgpu/quantization/matmul_nbits.cc
@@ -61,70 +61,70 @@ Status MatMulNBitsProgram::GenerateShaderCode(ShaderHelper& shader) const {
   const int output_element_number = y.NumComponents() * SafeInt<int>(output_number_);
   std::ostringstream prepare_scale_and_zero_point;
   prepare_scale_and_zero_point.imbue(std::locale::classic());
-  prepare_scale_and_zero_point << "var col_index = col * " << y.NumComponents() << ";\n";
+  prepare_scale_and_zero_point << "    var col_index = col * " << y.NumComponents() << ";\n";
   if (has_zero_points_) {
     const auto& zero_points = shader.AddInput("zero_points", ShaderUsage::UseUniform);
-    prepare_scale_and_zero_point << "let zero_point_bytes_per_col = (n_blocks_per_col + 1) / 2;\n"
-                                 << "var zero_point_byte_count: u32;\n"
-                                 << "var zero_point_word_index: u32;\n"
-                                 << "var zero_point_byte_offset: u32;\n"
-                                 << "let zero_point_nibble_offset: u32 = block & 0x1u;\n"
-                                 << "var zero_point_bits_offset: u32;\n"
-                                 << "var zero_point_word: u32;\n";
+    prepare_scale_and_zero_point << "    let zero_point_bytes_per_col = (n_blocks_per_col + 1) / 2;\n"
+                                 << "    var zero_point_byte_count: u32;\n"
+                                 << "    var zero_point_word_index: u32;\n"
+                                 << "    var zero_point_byte_offset: u32;\n"
+                                 << "    let zero_point_nibble_offset: u32 = block & 0x1u;\n"
+                                 << "    var zero_point_bits_offset: u32;\n"
+                                 << "    var zero_point_word: u32;\n";
     for (int c = 0; c < output_element_number; c++) {
-      prepare_scale_and_zero_point << "let scale" << c << " = " << scales.GetByOffset("col_index * n_blocks_per_col + block") << ";\n";
-      prepare_scale_and_zero_point << "zero_point_byte_count = col_index * zero_point_bytes_per_col + (block >> 0x1u);\n"
-                                   << "zero_point_word_index = zero_point_byte_count >> 0x2u;\n"
-                                   << "zero_point_byte_offset = zero_point_byte_count & 0x3u;\n"
-                                   << "zero_point_bits_offset = (zero_point_byte_offset << 3) + (zero_point_nibble_offset << 2);\n"
-                                   << "zero_point_word = " << zero_points.GetByOffset("zero_point_word_index") << " >> zero_point_bits_offset;\n"
-                                   << "let zero_point" << c << " = output_element_t((zero_point_word) & 0xFu);\n";
-      prepare_scale_and_zero_point << "col_index += 1;\n";
+      prepare_scale_and_zero_point << "    let scale" << c << " = " << scales.GetByOffset("col_index * n_blocks_per_col + block") << ";\n";
+      prepare_scale_and_zero_point << "    zero_point_byte_count = col_index * zero_point_bytes_per_col + (block >> 0x1u);\n"
+                                   << "    zero_point_word_index = zero_point_byte_count >> 0x2u;\n"
+                                   << "    zero_point_byte_offset = zero_point_byte_count & 0x3u;\n"
+                                   << "    zero_point_bits_offset = (zero_point_byte_offset << 3) + (zero_point_nibble_offset << 2);\n"
+                                   << "    zero_point_word = " << zero_points.GetByOffset("zero_point_word_index") << " >> zero_point_bits_offset;\n"
+                                   << "    let zero_point" << c << " = output_element_t((zero_point_word) & 0xFu);\n";
+      prepare_scale_and_zero_point << "    col_index += 1;\n";
     }
   } else {
-    prepare_scale_and_zero_point << "let zero_point = output_element_t(8.0);\n";
+    prepare_scale_and_zero_point << "    let zero_point = output_element_t(8.0);\n";
     for (int c = 0; c < output_element_number; c++) {
-      prepare_scale_and_zero_point << "let scale" << c << " = " << scales.GetByOffset("col_index * n_blocks_per_col + block") << ";\n";
-      prepare_scale_and_zero_point << "col_index += 1;\n";
+      prepare_scale_and_zero_point << "    let scale" << c << " = " << scales.GetByOffset("col_index * n_blocks_per_col + block") << ";\n";
+      prepare_scale_and_zero_point << "    col_index += 1;\n";
     }
   }
 
   std::ostringstream prepare_b_data;
   prepare_b_data.imbue(std::locale::classic());
-  prepare_b_data << "col_index = col * " << y.NumComponents() << ";\n";
+  prepare_b_data << "      col_index = col * " << y.NumComponents() << ";\n";
   for (int c = 0; c < output_element_number; c++) {
-    prepare_b_data << "let b" << c << "_data = " << b.GetByIndices("input_b_indices_t(col_index, block, word)") << ";\n"
-                   << "col_index += 1;\n";
+    prepare_b_data << "      let b" << c << "_data = " << b.GetByIndices("input_b_indices_t(col_index, block, word)") << ";\n"
+                   << "      col_index += 1;\n";
   }
-  prepare_b_data << "var b_value : u32;\n"
-                 << "let b_mask : u32 = 0x0F0F0F0Fu;\n"
-                 << "var b_value_lower : vec4<u32>;\n"
-                 << "var b_value_upper : vec4<u32>;\n"
-                 << "var b_quantized_values : " << quantized_data_type << ";\n "
-                 << "var b_dequantized_values : " << quantized_data_type << ";\n ";
+  prepare_b_data << "      var b_value : u32;\n"
+                 << "      let b_mask : u32 = 0x0F0F0F0Fu;\n"
+                 << "      var b_value_lower : vec4<u32>;\n"
+                 << "      var b_value_upper : vec4<u32>;\n"
+                 << "      var b_quantized_values : " << quantized_data_type << ";\n"
+                 << "      var b_dequantized_values : " << quantized_data_type << ";\n";
 
   std::ostringstream process_one_word;
   process_one_word.imbue(std::locale::classic());
-  process_one_word << "var input_offset = " << a.IndicesToOffset("input_a_indices_t(batch, row, word_offset)") << ";\n"
-                   << "var a_data: " << quantized_data_type << ";\n "
-                   << "for (var j: u32 = 0; j < " << (8 / a.NumComponents()) << "; j++) {\n"
-                   << "  if (word_offset + j < uniforms.input_a_shape[2]) {\n"
-                   << "    a_data[j] = " << a.GetByOffset("input_offset") << ";\n"
-                   << "    input_offset++;\n"
-                   << "  } else {\n"
-                   << "    a_data[j] = input_a_value_t(0);\n"
-                   << "  }\n"
-                   << "}\n";
+  process_one_word << "        var input_offset = " << a.IndicesToOffset("input_a_indices_t(batch, row, word_offset)") << ";\n"
+                   << "        var a_data: " << quantized_data_type << ";\n"
+                   << "        for (var j: u32 = 0; j < " << (8 / a.NumComponents()) << "; j++) {\n"
+                   << "          if (word_offset + j < uniforms.input_a_shape[2]) {\n"
+                   << "            a_data[j] = " << a.GetByOffset("input_offset") << ";\n"
+                   << "            input_offset++;\n"
+                   << "          } else {\n"
+                   << "            a_data[j] = input_a_value_t(0);\n"
+                   << "          }\n"
+                   << "        }\n";
   for (int c = 0; c < output_element_number; c++) {
-    process_one_word << "b_value = " << "b" << c << "_data";
+    process_one_word << "        b_value = " << "b" << c << "_data";
     if (components_b_ > 1) {
       process_one_word << "[i]";
     }
     process_one_word << ";\n"
-                     << "b_value_lower = unpack4xU8(b_value & b_mask);\n"
-                     << "b_value_upper = unpack4xU8((b_value >> 4) & b_mask);\n"
-                     << "b_quantized_values = " << quantized_data_type << "(output_element_t(b_value_lower[0]), output_element_t(b_value_upper[0]), output_element_t(b_value_lower[1]), output_element_t(b_value_upper[1]), output_element_t(b_value_lower[2]), output_element_t(b_value_upper[2]), output_element_t(b_value_lower[3]), output_element_t(b_value_upper[3]));\n"
-                     << "b_dequantized_values = ";
+                     << "        b_value_lower = unpack4xU8(b_value & b_mask);\n"
+                     << "        b_value_upper = unpack4xU8((b_value >> 4) & b_mask);\n"
+                     << "        b_quantized_values = " << quantized_data_type << "(output_element_t(b_value_lower[0]), output_element_t(b_value_upper[0]), output_element_t(b_value_lower[1]), output_element_t(b_value_upper[1]), output_element_t(b_value_lower[2]), output_element_t(b_value_upper[2]), output_element_t(b_value_lower[3]), output_element_t(b_value_upper[3]));\n"
+                     << "        b_dequantized_values = ";
     if (a.NumComponents() == 1) {
       if (has_zero_points_) {
         process_one_word << quantized_data_type << "((b_quantized_values[0] - zero_point" << c << ") * scale" << c << ", "
@@ -160,7 +160,7 @@ Status MatMulNBitsProgram::GenerateShaderCode(ShaderHelper& shader) const {
       process_one_word << ")) * scale" << c << ";\n";
     }
 
-    process_one_word << "workgroup_shared[local_id.x * " << output_number_ << " + " << c / y.NumComponents() << "]";
+    process_one_word << "        workgroup_shared[local_id.x * " << output_number_ << " + " << c / y.NumComponents() << "]";
     if (y.NumComponents() > 1) {
       process_one_word << "[" << c % y.NumComponents() << "]";
     }
@@ -188,45 +188,46 @@ Status MatMulNBitsProgram::GenerateShaderCode(ShaderHelper& shader) const {
   const uint32_t shared_memory_size = output_number_ * WORKGROUP_SIZE;
   std::string offset = "workgroup_idx * " + std::to_string(output_number_);
   shader.AppendImplementation("var<workgroup> workgroup_shared : array<output_value_t," + std::to_string(shared_memory_size) + ">;\n");
-  shader.SetMainFunctionBody("let output_indices = ", y.OffsetToIndices(offset),
+  shader.SetMainFunctionBody("  let output_indices = ", y.OffsetToIndices(offset),
                              ";\n"
-                             "let col = output_indices[2];\n"
-                             "let row = output_indices[1];\n"
-                             "let batch = output_indices[0];\n"
-                             "let n_blocks_per_col = uniforms.input_b_shape[1];\n"
-                             "let blob_size = uniforms.input_b_shape[2]"
+                             "  let col = output_indices[2];\n"
+                             "  let row = output_indices[1];\n"
+                             "  let batch = output_indices[0];\n"
+                             "  let n_blocks_per_col = uniforms.input_b_shape[1];\n"
+                             "  let blob_size = uniforms.input_b_shape[2]"
                              ";\n"
-                             "for (var block = local_id.x; block < n_blocks_per_col; block += workgroup_size_x) {\n"
-                             "  var word_offset = block * uniforms.block_size / ",
+                             "  for (var block = local_id.x; block < n_blocks_per_col; block += workgroup_size_x) {\n"
+                             "    var word_offset = block * uniforms.block_size / ",
                              a.NumComponents(), ";\n",
                              prepare_scale_and_zero_point.str(),
-                             "  for (var word: u32 = 0; word < blob_size; word += 1) {\n",
+                             "    for (var word: u32 = 0; word < blob_size; word += 1) {\n",
                              prepare_b_data.str(),
-                             "    for (var i: u32 = 0; i < ", components_b_, "; i++) {\n",
+                             "      for (var i: u32 = 0; i < ", components_b_, "; i++) {\n",
                              process_one_word.str(),
-                             "      word_offset += ", 8 / a.NumComponents(),
+                             "        word_offset += ", 8 / a.NumComponents(),
                              ";\n"
+                             "      }\n"
                              "    }\n"
                              "  }\n"
-                             "}\n"
-                             "workgroupBarrier();\n"
-                             "if (local_id.x < ",
+                             "  workgroupBarrier();\n"
+                             "  if (local_id.x < ",
                              output_number_,
                              ") {\n"
-                             "  var output_value = output_value_t(0);\n"
-                             "  var workgroup_shared_offset = local_id.x;\n"
-                             "  let blocks_num = min(",
+                             "    var output_value = output_value_t(0);\n"
+                             "    var workgroup_shared_offset = local_id.x;\n"
+                             "    let blocks_num = min(",
                              shared_memory_size,
                              ", n_blocks_per_col);\n"
-                             "  for (var b = 0u; b < blocks_num; b++) {\n"
-                             "    output_value += workgroup_shared[workgroup_shared_offset];\n"
-                             "    workgroup_shared_offset += ",
+                             "    for (var b = 0u; b < blocks_num; b++) {\n"
+                             "      output_value += workgroup_shared[workgroup_shared_offset];\n"
+                             "      workgroup_shared_offset += ",
                              output_number_,
                              ";\n"
-                             "  }\n",
+                             "    }\n",
+                             "    ",
                              y.SetByIndices("output_indices_t(batch, row, col + local_id.x)", "output_value"),
                              "\n"
-                             "}\n");
+                             "  }\n");
 
   return Status::OK();
 }
