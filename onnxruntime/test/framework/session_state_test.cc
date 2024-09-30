46b9bace48aa508b074854e408dc7f3ffe49e80c
Frank Dong <frdong@microsoft.com>
2024-09-30 15:24:33 -0700

    fix CI failure in Android

--- onnxruntime/test/framework/session_state_test.cc
+++ onnxruntime/test/framework/session_state_test.cc
@@ -394,7 +394,7 @@ class PrePackingTestOpKernel : public OpKernel {
     return Status::OK();
   }
 
-  Tensor* GetPrePackTensors() {
+  Tensor* GetPrePackTensors() override {
     ++get_prepack_tensors_count;
 
     std::shared_ptr<CPUAllocator> alloc = std::make_shared<CPUAllocator>();
