9758b34a56a9b5e60075b42e54eb7388906f5dd4
Jian Chen <cjian@microsoft.com>
2024-10-03 15:56:24 -0700

    Update cpu_execution_provider.cc

--- onnxruntime/core/providers/cpu/cpu_execution_provider.cc
+++ onnxruntime/core/providers/cpu/cpu_execution_provider.cc
@@ -1703,7 +1703,7 @@ Status RegisterOnnxOperatorKernels(KernelRegistry& kernel_registry) {
       BuildKernelCreateInfo<ONNX_OPERATOR_KERNEL_CLASS_NAME(kCpuExecutionProvider, kOnnxDomain, 10, ConvInteger)>,
 
       BuildKernelCreateInfo<ONNX_OPERATOR_TYPED_KERNEL_CLASS_NAME(kCpuExecutionProvider, kOnnxDomain, 10, uint8_t_int8_t,
-                                                                ConvInteger)>,
+                                                                  ConvInteger)>,
       BuildKernelCreateInfo<ONNX_OPERATOR_TYPED_KERNEL_CLASS_NAME(kCpuExecutionProvider, kOnnxDomain, 10, uint8_t,
                                                                   QLinearConv)>,
       BuildKernelCreateInfo<ONNX_OPERATOR_TYPED_KERNEL_CLASS_NAME(kCpuExecutionProvider, kOnnxDomain, 10, int8_t,
