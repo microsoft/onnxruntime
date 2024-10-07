e4b825ab9b54281d0a0d52b499df5463dda26383
xadupre <xadupre@microsoft.com>
2024-10-07 18:50:41 +0200

    restore cc

--- onnxruntime/core/providers/cpu/ml/tree_ensemble_helper.h
+++ onnxruntime/core/providers/cpu/ml/tree_ensemble_helper.h
@@ -2,44 +2,14 @@
 // Licensed under the MIT License.
 
 #pragma once
-
-#if !defined(ORT_MINIMAL_BUILD)
-
 #include "core/common/common.h"
 #include "core/framework/op_kernel.h"
-#include "core/framework/tensorprotoutils.h"
-#include "core/common/safeint.h"
 
 namespace onnxruntime {
 namespace ml {
-namespace detail {
 
-template <typename T>
-Status GetVectorAttrsOrDefault(const OpKernelInfo& info, const std::string& name, std::vector<T>& data) {
-  ONNX_NAMESPACE::TensorProto proto;
-  auto result = info.GetAttr(name, &proto);
+Status GetVectorAttrsOrDefault(const OpKernelInfo& info, const std::string& name, std::vector<double>& data);
+Status GetVectorAttrsOrDefault(const OpKernelInfo& info, const std::string& name, std::vector<float>& data);
 
-  SafeInt<int64_t> n_elements(1);
-  for (auto dim : proto.dims()) {
-    n_elements *= dim;
-  }
-
-  if (proto.dims().empty()) {
-    return Status::OK();
-  }
-
-  const SafeInt<size_t> tensor_size(n_elements);
-  data.clear();
-  data.resize(tensor_size);
-
-  result = utils::UnpackTensor<T>(proto, std::filesystem::path(), data.data(), tensor_size);
-  ORT_ENFORCE(result.IsOK(), "TreeEnsemble could not unpack tensor attribute ", name);
-
-  return Status::OK();
-}
-
-}  // namespace detail
 }  // namespace ml
 }  // namespace onnxruntime
-
-#endif  // !defined(ORT_MINIMAL_BUILD)
