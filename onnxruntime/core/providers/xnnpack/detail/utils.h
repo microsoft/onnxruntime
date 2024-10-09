73da3d115be6c2d3a4082838e0af9496bf2dd16b
Ubuntu <azureuser@zhanyi-Linux-ARM64.ojylseg20mkujpnbz4gzdju3tb.jx.internal.cloudapp.net>
2024-10-09 11:05:36 +0000

    temp chnage

--- onnxruntime/core/providers/xnnpack/detail/utils.h
+++ onnxruntime/core/providers/xnnpack/detail/utils.h
@@ -81,13 +81,13 @@ bool IsPaddingTypeSupported(AutoPadType auto_pad);
 using ComputeTypeSet = std::unordered_set<ONNX_NAMESPACE::TensorProto_DataType>;
 #ifdef XNNPACK_FP16_SUPPORTED
 bool IsComputeTypeSupported(int32_t compute_type,
-                            const ComputeTypeSet compute_type_set = {ONNX_NAMESPACE::TensorProto_DataType_FLOAT,
+                            const ComputeTypeSet& compute_type_set = {ONNX_NAMESPACE::TensorProto_DataType_FLOAT,
                                                                      ONNX_NAMESPACE::TensorProto_DataType_FLOAT16,
                                                                      ONNX_NAMESPACE::TensorProto_DataType_UINT8,
                                                                      ONNX_NAMESPACE::TensorProto_DataType_INT8});
 #else
 bool IsComputeTypeSupported(int32_t compute_type,
-                            const ComputeTypeSet compute_type_set = {ONNX_NAMESPACE::TensorProto_DataType_FLOAT,
+                            const ComputeTypeSet& compute_type_set = {ONNX_NAMESPACE::TensorProto_DataType_FLOAT,
                                                                      ONNX_NAMESPACE::TensorProto_DataType_UINT8,
                                                                      ONNX_NAMESPACE::TensorProto_DataType_INT8});
 #endif
