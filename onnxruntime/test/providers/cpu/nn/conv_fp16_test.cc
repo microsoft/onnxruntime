b07d11df5238b0ce35ecabd55fbd6a66f3bd2573
Ubuntu <azureuser@zhanyi-Linux-ARM64.ojylseg20mkujpnbz4gzdju3tb.jx.internal.cloudapp.net>
2024-10-09 10:49:01 +0000

    add one comment

--- onnxruntime/test/providers/cpu/nn/conv_fp16_test.cc
+++ onnxruntime/test/providers/cpu/nn/conv_fp16_test.cc
@@ -4,6 +4,7 @@
 #include "core/mlas/inc/mlas.h"
 #include "core/providers/xnnpack/xnnpack_init.h"
 
+// XNNPACK_FP16_SUPPORTED scope is too big, so add USE_XNNPACK to avoid the FP16 tests enabled for other EP
 #if defined(MLAS_F16VEC_INTRINSICS_SUPPORTED) || defined(COREML_ENABLE_MLPROGRAM) || (defined(USE_XNNPACK) && defined(XNNPACK_FP16_SUPPORTED))
 
 #include "gtest/gtest.h"
