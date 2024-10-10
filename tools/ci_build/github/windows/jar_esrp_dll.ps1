6734254f15bb74015fcc7a63c308e05428544dfa
Kyle Zhang <zhanxi@microsoft.com>
2024-10-10 17:55:15 +0800

    remove codesignsummary file

--- tools/ci_build/github/windows/jar_esrp_dll.ps1
+++ tools/ci_build/github/windows/jar_esrp_dll.ps1
@@ -8,17 +8,23 @@ $extracted_file_directory = "$original_jar_file_directory\jar_extracted_full_fil
 if ($instruction -eq "extract") {
     Write-Host "Extracting the jar file..."
     & 7z x $original_jar_file_full_path -o"$extracted_file_directory"
-    Write-Host "Extracted file directory: $extracted_file_directory"
+    Write-Host "Extracted files directory: $extracted_file_directory"
 
-    Rename-Item "$original_jar_file_full_path" "$original_jar_file_full_path.original"
+    Write-Host "Removing the original jar file..."
+    Remove-Item -Path "$original_jar_file_full_path" -Force
+    Write-Host "Removed the original jar file."
 }
 elseif ($instruction -eq "repack") {
+    Write-Host "Removing ESRP's CodeSignSummary file..."
+    Remove-Item -Path "$extracted_file_directory/CodeSignSummary*.md" -Force
+    Write-Host "Removed ESRP's CodeSignSummary file."
+
     Write-Host "Repacking the jar file..."
     & 7z a "$original_jar_file_full_path" "$extracted_file_directory\*"
     Write-Host "Repacked the jar file."
 
+    Write-Host "Removing the extracted files..."
     Remove-Item -Path "$extracted_file_directory" -Recurse -Force
-    Remove-Item -Path "$original_jar_file_full_path.original" -Force
     Write-Host "Removed the extracted files."
 }
 else {
