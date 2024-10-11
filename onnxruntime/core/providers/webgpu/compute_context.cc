613ad6d88087100c7c650f485c1653df2b1c8bdb
Yulong Wang <7679871+fs-eire@users.noreply.github.com>
2024-10-11 01:57:43 -0700

    check ValidationMode for push/pop error scope

--- onnxruntime/core/providers/webgpu/compute_context.cc
+++ onnxruntime/core/providers/webgpu/compute_context.cc
@@ -14,22 +14,26 @@ ComputeContext::ComputeContext(OpKernelContext& kernel_context)
 }
 
 void ComputeContext::PushErrorScope() {
-  webgpu_context_.Device().PushErrorScope(wgpu::ErrorFilter::Validation);
+  if (webgpu_context_.ValidationMode() >= ValidationMode::Basic) {
+    webgpu_context_.Device().PushErrorScope(wgpu::ErrorFilter::Validation);
+  }
 }
 
 Status ComputeContext::PopErrorScope() {
   Status status{};
 
-  ORT_RETURN_IF_ERROR(webgpu_context_.Wait(
-      webgpu_context_.Device().PopErrorScope(
-          wgpu::CallbackMode::WaitAnyOnly, [](wgpu::PopErrorScopeStatus pop_status, wgpu::ErrorType error_type, char const* message, Status* status) {
-            ORT_ENFORCE(pop_status == wgpu::PopErrorScopeStatus::Success, "Instance dropped.");
-            if (error_type == wgpu::ErrorType::NoError) {
-              return;
-            }
-            *status = ORT_MAKE_STATUS(ONNXRUNTIME, FAIL, "WebGPU validation failed. ", message);
-          },
-          &status)));
+  if (webgpu_context_.ValidationMode() >= ValidationMode::Basic) {
+    ORT_RETURN_IF_ERROR(webgpu_context_.Wait(
+        webgpu_context_.Device().PopErrorScope(
+            wgpu::CallbackMode::WaitAnyOnly, [](wgpu::PopErrorScopeStatus pop_status, wgpu::ErrorType error_type, char const* message, Status* status) {
+              ORT_ENFORCE(pop_status == wgpu::PopErrorScopeStatus::Success, "Instance dropped.");
+              if (error_type == wgpu::ErrorType::NoError) {
+                return;
+              }
+              *status = ORT_MAKE_STATUS(ONNXRUNTIME, FAIL, "WebGPU validation failed. ", message);
+            },
+            &status)));
+  }
   return status;
 }
 
