70a56284e30e0cd3c6249ca4ff4dfc0bcf99c8ef
wejoncy <wejoncy@163.com>
2024-10-10 22:54:18 -0700

    add comments for flag

--- include/onnxruntime/core/providers/coreml/coreml_provider_factory.h
+++ include/onnxruntime/core/providers/coreml/coreml_provider_factory.h
@@ -32,6 +32,9 @@ enum COREMLFlags {
   COREML_FLAG_CREATE_MLPROGRAM = 0x010,
 
   // Exclude ANE as sometimes this decrease performance
+  // https://developer.apple.com/documentation/coreml/mlcomputeunits?language=objc
+  // there are four compute units:
+  // MLComputeUnitsCPUAndNeuralEngine|MLComputeUnitsCPUAndGPU|MLComputeUnitsCPUOnly|MLComputeUnitsAll
   COREML_FLAG_USE_CPU_AND_GPU = 0x020,
   // Keep COREML_FLAG_LAST at the end of the enum definition
   // And assign the last COREMLFlag to it
