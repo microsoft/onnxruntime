6f34b5f05122fd34f3763533ce6b34eb5f9c994e
Sheetal Kadam <shekadam@microsoft.com>
2024-10-10 17:14:29 -0700

    add qnn browserstack

--- java/src/test/android/app/src/main/java/ai/onnxruntime/example/javavalidator/MainActivity.kt
+++ java/src/test/android/app/src/main/java/ai/onnxruntime/example/javavalidator/MainActivity.kt
@@ -1,11 +1,13 @@
 package ai.onnxruntime.example.javavalidator
 
 import android.os.Bundle
+import android.system.Os
 import androidx.appcompat.app.AppCompatActivity
 
 /*Empty activity app mainly used for testing*/
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        //Os.setenv("ADSP_LIBRARY_PATH", adspLibraryPath, true)
         super.onCreate(savedInstanceState)
     }
 }
