9a43d427ea9ada567d111fe1490d9d96b30678b6
Patrice Vignola <vignola.patrice@gmail.com>
2024-10-11 18:17:09 -0700

    Support partial rotary embedding

--- onnxruntime/test/contrib_ops/rotary_embedding_op_test.cc
+++ onnxruntime/test/contrib_ops/rotary_embedding_op_test.cc
@@ -135,8 +135,7 @@ static void RunTests(const std::vector<float>& input_data,
                      int max_sequence_length = 0,
                      int64_t interleaved = 0,
                      int64_t is_packed_batching = 0,
-                     bool use_float16 = true,
-                     bool disable_dml = false) {
+                     bool use_float16 = true) {
   // FP32 test for CPU
   RunTest(input_data,
           position_ids,
@@ -173,7 +172,7 @@ static void RunTests(const std::vector<float>& input_data,
           TensorType::kFloat,
           false, /* disable_cpu */
           false, /* disable_cuda */
-          disable_dml || false /* disable_dml */);
+          false /* disable_dml */);
 
   // FP16 test for CUDA and DML
   if (use_float16) {
@@ -193,7 +192,7 @@ static void RunTests(const std::vector<float>& input_data,
             TensorType::kFloat16,
             true,  /* disable_cpu */
             false, /* disable_cuda*/
-            disable_dml || false /* disable_dml */);
+            false /* disable_dml */);
 
     // RunTest(input_data,
     //         position_ids,
@@ -744,8 +743,7 @@ TEST(RotaryEmbeddingTest, RotaryEmbedding_CustomRotaryDim_SmallData_Phi) {
            max_sequence_length,
            interleaved,
            0,    // is_packed_batching
-           true, /*use_fp16*/
-           true /*disable_dml*/);
+           true /*use_fp16*/);
 }
 
 TEST(RotaryEmbeddingTest, RotaryEmbedding_CustomRotaryDim_SmallData_Phi_Packed_Batching) {
@@ -786,8 +784,7 @@ TEST(RotaryEmbeddingTest, RotaryEmbedding_CustomRotaryDim_SmallData_Phi_Packed_B
            max_sequence_length,
            interleaved,
            1,    // is_packed_batching
-           true, /*use_fp16*/
-           true /*disable_dml*/);
+           true /*use_fp16*/);
 }
 
 }  // namespace test
