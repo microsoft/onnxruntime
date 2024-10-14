942554116f1542d1393bad8361987d1b3803a690
Guangyun Han <guangyunhan@microsoft.com>
2024-10-14 14:15:55 +0800

    Fix Clip removal logic

--- onnxruntime/core/optimizer/qdq_transformer/qdq_util.h
+++ onnxruntime/core/optimizer/qdq_transformer/qdq_util.h
@@ -3,7 +3,9 @@
 
 #pragma once
 
+#include <cmath>
 #include <functional>
+#include <limits>
 #include <string>
 #include <filesystem>
 
@@ -76,5 +78,46 @@ bool MatchQNode(const Node& node);
 // Check DQ node op type, version, and domain.
 bool MatchDQNode(const Node& node);
 #endif  // !defined(ORT_MINIMAL_BUILD) || defined(ORT_EXTENDED_MINIMAL_BUILD)
+
+// 1. Clip can be removed iff the codoamin of QuantizeLinear remain unchanged.
+// 2. To remain unchanged, y=QuantizeLinear(Clip(x)) must span the full range of values that can be represented by the
+//    integer type of y. We can use this precondition to eval QuantizeLinear backward to to get the domain.
+// 3. Indicates the domain of QuantizeLinear is strict subset of the codomain of Clip. We can use this to test if a
+//    removal is valid or not.
+// 4. Due to rounding effect, we can be get the upperbound and lowerbound of min or max
+//    - Which one to use?
+//      upperbound of min and lowerbound of max
+//    - Why?
+//      We want the codomain to be unchanged, so as long as the domain genreate a codomain that fill the integer value
+//      range will be fine.
+
+// The quantization formula is y = saturate((x / y_scale) + y_zero_point)
+// For (x / y_scale), it rounds to the nearest even. So the allowed quantize limits before saturate need to be taken
+// care of.
+//
+// The following struct provides a wrapper to compute the domain from codomain (Q output dtype).
+template <typename T>
+struct QuantizeDomain {
+  inline static float MinUpper(float scale, int zp) {
+    int64_t codomain_min = std::numeric_limits<T>::lowest();
+    int64_t biased = codomain_min - zp;
+    float before_round = static_cast<float>(biased) + 0.5f;  // move to upperbound
+    if (biased % 2 == 1) {                                   // cannot be exact ?.5 because of rounding to even
+      before_round = std::nextafterf(before_round, static_cast<float>(biased));
+    }
+    return before_round * scale;
+  }
+
+  inline static float MaxLower(float scale, int zp) {
+    int64_t codomain_max = std::numeric_limits<T>::max();
+    int64_t biased = codomain_max - zp;
+    float before_round = static_cast<float>(biased) - 0.5f;  // move to lowerbound
+    if (biased % 2 == 1) {                                   // cannot be exact ?.5 because of rounding to even
+      before_round = std::nextafterf(before_round, static_cast<float>(biased));
+    }
+    return before_round * scale;
+  }
+};
+
 }  // namespace QDQ
 }  // namespace onnxruntime
