d97f4f3b866dae3d009cf49dca267297d36870ac
yf711 <yifanl@microsoft.com>
2024-10-14 10:41:41 -0700

    adjustment

--- samples/build_and_test.sh
+++ samples/build_and_test.sh
@@ -4,24 +4,24 @@
 ../build.sh --parallel --build_shared_lib --cuda_home /usr/local/cuda --cudnn_home /usr/lib/x86_64-linux-gnu/ --use_tensorrt --tensorrt_home /usr/lib/x86_64-linux-gnu/ --config Debug --skip_tests --skip_submodule_sync --cmake_extra_defines 'CMAKE_CUDA_ARCHITECTURES=80' --allow_running_as_root --compile_no_warning_as_error
 
 # 2. build test module
-cd outTreeEp && mkdir -p build && cd build
+cd outTreeEp && rm -r build && mkdir -p build && cd build
 cmake -S ../ -B ./ -DCMAKE_BUILD_TYPE=Debug
 cmake --build ./
 cd ../../
 
-cd outTreeEp_kernel && mkdir -p build && cd build
+cd outTreeEp_kernel && rm -r build && mkdir -p build && cd build
 cmake -S ../ -B ./ -DCMAKE_BUILD_TYPE=Debug
 cmake --build ./
 cd ../../
 
-cd tensorRTEp && mkdir -p build && cd build
+cd tensorRTEp && rm -r build && mkdir -p build && cd build
 # Download TensorRT with correct cuda version selected
 cmake -S ../ -B ./ -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CUDA_ARCHITECTURES=80 -DCMAKE_CUDA_COMPILER=/usr/local/cuda/bin/nvcc -DTENSORRT_HOME=/home/yifanl/TensorRT-10.4.0.26/
 cmake --build ./
 cd ../../
 
 cp ../build/Linux/Debug/libonnxruntime.so c_test
-cd c_test && mkdir -p build && cd build
+cd c_test && rm -r build && mkdir -p build && cd build
 cmake -S ../ -B ./ -DCMAKE_BUILD_TYPE=Debug
 cmake --build ./
 cd ../../
