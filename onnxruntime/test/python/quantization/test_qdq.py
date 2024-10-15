23cb7b62f2a6bd502c68906c5770950e255bcdee
adrianlizarraga <adlizarraga@microsoft.com>
2024-10-15 01:56:16 -0700

    Save unittest model to tmp dir

--- onnxruntime/test/python/quantization/test_qdq.py
+++ onnxruntime/test/python/quantization/test_qdq.py
@@ -1732,8 +1732,8 @@ class TestAdjustWeightScaleForInt32Bias(unittest.TestCase):
         cls._tmp_model_dir = tempfile.TemporaryDirectory(prefix="ort.qdq.adj_int32_bias_")
 
         # Note: swap with the commented line if you want to see the models in local test dir.
-        # cls._tmp_dir_path = cls._tmp_model_dir.name
-        cls._tmp_dir_path = "."
+        cls._tmp_dir_path = cls._tmp_model_dir.name
+        # cls._tmp_dir_path = "."
 
     @classmethod
     def tearDownClass(cls):
