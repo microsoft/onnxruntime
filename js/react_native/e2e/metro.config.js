c12e201ff8bd85150c109464fae010328df2a4fb
Jian Chen <cjian@microsoft.com>
2024-10-15 20:10:19 -0700

    new branch module.exports = {   presets: ['module:@react-native/babel-preset'], };

--- js/react_native/e2e/metro.config.js
+++ js/react_native/e2e/metro.config.js
@@ -1,39 +1,10 @@
-'use strict';
+const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+/**
+ * Metro configuration
+ * https://reactnative.dev/docs/metro
+ *
+ * @type {import('metro-config').MetroConfig}
+ */
+const config = {};
+module.exports = mergeConfig(getDefaultConfig(__dirname), config);
 
-const path = require('path');
-const exclusionlist = require('metro-config/src/defaults/exclusionList');
-const escape = require('escape-string-regexp');
-const pak = require('../package.json');
-
-const root = path.resolve(__dirname, '..');
-
-const modules = Object.keys({
-  ...pak.peerDependencies,
-});
-
-module.exports = {
-  projectRoot: __dirname,
-  watchFolders: [root],
-
-  // We need to make sure that only one version is loaded for peerDependencies
-  // So we exclusionlist them at the root, and alias them to the versions in example's node_modules
-  resolver: {
-    exclusionlistRE: exclusionlist(
-      modules.map((m) => new RegExp(`^${escape(path.join(root, 'node_modules', m))}\\/.*$`)),
-    ),
-
-    extraNodeModules: modules.reduce((acc, name) => {
-      acc[name] = path.join(__dirname, 'node_modules', name);
-      return acc;
-    }, {}),
-  },
-
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: true,
-      },
-    }),
-  },
-};
