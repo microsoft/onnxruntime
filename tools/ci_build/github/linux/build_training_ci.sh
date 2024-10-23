ded6c9f3db7deff7042a2dddedb70a3260faa473
Changming Sun <chasun@microsoft.com>
2024-10-23 11:57:15 -0700

    Move ORT Training pipeline to github actions (#22543)
    Move ORT Training pipeline to github actions and enable CodeQL scan for the code(including inference code).
    We will move all pull request pipelines to Github Actions.

--- tools/ci_build/github/linux/build_training_ci.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -e -x
-python3.12 -m pip install -r /onnxruntime_src/tools/ci_build/github/linux/python/requirements.txt
-python3.12 /onnxruntime_src/tools/ci_build/build.py --build_dir /build --config Release --enable_training --skip_submodule_sync --parallel
