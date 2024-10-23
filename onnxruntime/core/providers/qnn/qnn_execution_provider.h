0028d3f3328c9147261e28c85dbc70d36319485d
ivberg <ivberg@microsoft.com>
2024-10-22 20:45:44 -0700

    Fix crash in QNN EP - ResetQnnLogLevel (#22456)
    ### Description
    Fix crash with extra checks ResetQnnLogLevel. 
    
    From the dump it looks like during ETW callbacks, while the provider is stopping, we attempt to reset the QNN log level.
    While the QNN BackEndMgr (this) is alive logger_ is not valid
    
    ### Motivation and Context
    ORT should not crash

--- onnxruntime/core/providers/qnn/qnn_execution_provider.h
+++ onnxruntime/core/providers/qnn/qnn_execution_provider.h
@@ -151,7 +151,7 @@ class QNNExecutionProvider : public IExecutionProvider {
   bool enable_HTP_FP16_precision_ = true;
   bool share_ep_contexts_ = false;
 #ifdef _WIN32
-  onnxruntime::logging::EtwRegistrationManager::EtwInternalCallback callback_ETWSink_provider_;
+  onnxruntime::logging::EtwRegistrationManager::EtwInternalCallback callback_ETWSink_provider_ = nullptr;
 #endif
   qnn::ModelSettings model_settings_ = {};
 
