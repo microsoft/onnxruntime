d31985a99d22f0290b5eb798e20aa0bac8976603
kailums <kalu@microsoft.com>
2024-10-24 03:35:09 +0000

    update all cp38 and cp39 to cp312 in tools dir

--- tools/ci_build/github/linux/docker/inference/x86_64/python/cpu/scripts/install_centos.sh
+++ tools/ci_build/github/linux/docker/inference/x86_64/python/cpu/scripts/install_centos.sh
@@ -7,7 +7,7 @@ echo "installing for os major version : $os_major_version"
 dnf install -y glibc-langpack-\*
 yum install -y which redhat-lsb-core expat-devel tar unzip zlib-devel make bzip2 bzip2-devel perl-IPC-Cmd openssl-devel wget
 
-# export PATH=/opt/python/cp38-cp38/bin:$PATH
+# export PATH=/opt/python/cp312-cp312/bin:$PATH
 
 echo "installing rapidjson for AzureEP"
 wget https://github.com/Tencent/rapidjson/archive/refs/tags/v1.1.0.tar.gz
--- tools/ci_build/github/linux/ort_minimal/build_minimal_ort_and_run_tests.sh
+++ tools/ci_build/github/linux/ort_minimal/build_minimal_ort_and_run_tests.sh
@@ -7,7 +7,7 @@
 
 set -e
 set -x
-export PATH=/opt/python/cp38-cp38/bin:$PATH
+export PATH=/opt/python/cp312-cp312/bin:$PATH
 USAGE_TEXT="Usage:
   -b|--build-directory <build directory>
     Specifies the build directory. Required.
