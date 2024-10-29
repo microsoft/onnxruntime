e608d77e261bee919ccfe24a13cfb1dd6db84314
Maanav Dalal <maanavdalal@gmail.com>
2024-10-29 15:13:30 -0700

    Moved blog to featuredblogs

--- src/routes/blogs/+page.svelte
+++ src/routes/blogs/+page.svelte
@@ -46,6 +46,16 @@
 	}
 	let featuredblog = [
 		{
+			title: 'Enhancing team collaboration during AI model optimization with the Olive Shared Cache',
+			date: 'October 30th, 2024',
+			blurb:
+				"Learn how to use Olive's shared cache to enhance team collaboration when optimizing AI models",
+			link: 'blogs/olive-shared-cache',
+			image: Phi3SmallMediumImage,
+			imgalt:
+				'Chart comparing model size (in GB) of ONNX Phi-3-medium between PyTorch and ONNX Runtime'
+		},
+		{
 			title: 'Accelerating LightGlue Inference with ONNX Runtime and TensorRT',
 			date: 'July 17th, 2024',
 			blurb:
@@ -65,6 +75,10 @@
 			imgalt:
 				'Image of the different steps of an ML pipeline on a mobile device, running using NimbleEdge and ONNX Runtime.'
 		},
+		
+		
+	];
+	let blogs = [
 		{
 			title: 'Background Removal in the Browser Using ONNX Runtime with WebGPU',
 			date: 'June 12th, 2024',
@@ -74,19 +88,6 @@
 			image: 'https://imgly-blog-prod.storage.googleapis.com/2024/06/onnx-runtime-imgly.jpg',
 			imgalt:
 				'Image of a skateboarder with a sky background, with half of the background being alternating grey and white squares indicating it has been removed.'
-		},
-		
-	];
-	let blogs = [
-		{
-			title: 'Enhancing team collaboration during AI model optimization with the Olive Shared Cache',
-			date: 'October 30th, 2024',
-			blurb:
-				"Learn how to use Olive's shared cache to enhance team collaboration when optimizing AI models",
-			link: 'blogs/olive-shared-cache',
-			image: Phi3SmallMediumImage,
-			imgalt:
-				'Chart comparing model size (in GB) of ONNX Phi-3-medium between PyTorch and ONNX Runtime'
 		},
 		{
 			title: 'Phi-3 Small and Medium Models are now Optimized with ONNX Runtime and DirectML',
