19c4a054a3e88120e97d63ab13b46bbab4eafd1f
Yi Zhang <your@email.com>
2024-10-31 08:55:45 +0800

    revert onnxruntime_java_unittests.cmake

--- cmake/onnxruntime_java_unittests.cmake
+++ cmake/onnxruntime_java_unittests.cmake
@@ -6,42 +6,26 @@
 FILE(TO_NATIVE_PATH ${GRADLE_EXECUTABLE} GRADLE_NATIVE_PATH)
 FILE(TO_NATIVE_PATH ${BIN_DIR} BINDIR_NATIVE_PATH)
 
-function(run_java_unit_test SYSTEM_PROPERTY_DEFINITION)
-  set(GRADLE_TEST_ARGS
-      ${GRADLE_NATIVE_PATH}
-      test --rerun
-      cmakeCheck
-      --console=plain
-      -DcmakeBuildDir=${BINDIR_NATIVE_PATH}
-      -Dorg.gradle.daemon=false
-      ${SYSTEM_PROPERTY_DEFINITIONS})
-
-  if(WIN32)
-  list(PREPEND GRADLE_TEST_ARGS cmd /C)
-  endif()
-
-  message(STATUS "gradle test command args: ${GRADLE_TEST_ARGS}")
-
-  execute_process(COMMAND ${GRADLE_TEST_ARGS}
-                  WORKING_DIRECTORY ${REPO_ROOT}/java
-                  RESULT_VARIABLE HAD_ERROR)
-endfunction()
-
 message(STATUS "gradle additional system property definitions: ${GRADLE_SYSTEM_PROPERTY_DEFINITIONS}")
 
-string(FIND "${GRADLE_SYSTEM_PROPERTY_DEFINITIONS}" "-DUSE_CUDA=1" INDEX_CUDA)
-string(FIND "${GRADLE_SYSTEM_PROPERTY_DEFINITIONS}" "-DUSE_DML=1" INDEX_DML)
+set(GRADLE_TEST_ARGS
+    ${GRADLE_NATIVE_PATH}
+    test --rerun
+    cmakeCheck
+    --console=plain
+    -DcmakeBuildDir=${BINDIR_NATIVE_PATH}
+    -Dorg.gradle.daemon=false
+    ${GRADLE_SYSTEM_PROPERTY_DEFINITIONS})
 
-
-if((INDEX_CUDA GREATER -1) AND (INDEX_DML GREATER -1))
-  string(REPLACE "-DUSE_CUDA=1" "" GRADLE_DML_YSTEM_PROPERTY_DEFINITIONS ${GRADLE_SYSTEM_PROPERTY_DEFINITIONS})
-  run_java_unit_test("${GRADLE_DML_YSTEM_PROPERTY_DEFINITIONS}")
-
-  string(REPLACE "-DUSE_DML=1" "" GRADLE_CUDA_YSTEM_PROPERTY_DEFINITIONS ${GRADLE_SYSTEM_PROPERTY_DEFINITIONS})
-  run_java_unit_test("${GRADLE_CUDA_YSTEM_PROPERTY_DEFINITIONS}")
-else()
-  run_java_unit_test("${GRADLE_SYSTEM_PROPERTY_DEFINITIONS}")
+if(WIN32)
+  list(PREPEND GRADLE_TEST_ARGS cmd /C)
 endif()
+
+message(STATUS "gradle test command args: ${GRADLE_TEST_ARGS}")
+
+execute_process(COMMAND ${GRADLE_TEST_ARGS}
+    WORKING_DIRECTORY ${REPO_ROOT}/java
+    RESULT_VARIABLE HAD_ERROR)
 
 if(HAD_ERROR)
   message(FATAL_ERROR "Java Unitests failed")
