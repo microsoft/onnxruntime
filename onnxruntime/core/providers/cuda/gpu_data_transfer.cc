cadcce3511ef4098d1f7dc5a20c3a179c0a2a51e
Tianlei Wu <tlwu@microsoft.com>
2024-10-31 09:52:50 -0700

    [CUDA/ROCm/Migraphx] consolidate gpu data transfer (#22609)
    ### Description
    Consolidate the gpu data transfer in CUDA, ROCm and Migraphx EP.
    (1) Remove some redundant stream synchronize on default stream according
    to spec of cudaMemcpy
    (2) consolidate CUDA, ROCm and MigrphaX to try use same logic.
    
    ### Motivation
    This is a follow up on reviewing
    https://github.com/microsoft/onnxruntime/pull/22589.
    
    ### Context
    
    
    https://docs.nvidia.com/cuda/cuda-runtime-api/api-sync-behavior.html#api-sync-behavior
    ##### cudaMemcpy()
    * For transfers from pageable host memory to device memory, a stream
    sync is performed before the copy is initiated. The function will return
    once the pageable buffer has been copied to the staging memory for DMA
    transfer to device memory, **but the DMA to final destination may not
    have completed**.
    * For transfers from pinned host memory to device memory, the function
    is synchronous with respect to the host.
    * For transfers from device to either pageable or pinned host memory,
    the function returns only once the copy has completed.
    * For transfers from device memory to device memory, **no host-side
    synchronization is performed**.
    * For transfers from any host memory to any host memory, the function is
    fully synchronous with respect to the host.
    
    #### cudaMemcpyAsync
    
    * For transfers between device memory and pageable host memory, the
    function might be synchronous with respect to host.
    * For transfers from any host memory to any host memory, the function is
    fully synchronous with respect to the host.
    * If pageable memory must first be staged to pinned memory, the driver
    may synchronize with the stream and stage the copy into pinned memory.
     * For all other transfers, the function should be fully asynchronous.
    
    
    https://rocm.docs.amd.com/projects/HIP/en/latest/doxygen/html/group___memory.html
    
    ##### hipMemcpyAsync()
    
    If host or dest are not pinned, the memory copy will be performed
    synchronously. For best performance, use hipHostMalloc to allocate host
    memory that is transferred asynchronously.
    on HCC hipMemcpyAsync does not support overlapped H2D and D2H copies.
    For hipMemcpy, the copy is always performed by the device associated
    with the specified stream.
    
    ##### hipMemcpy()
    For hipMemcpy, the copy is always performed by the current device (set
    by hipSetDevice).
    
    https://github.com/ROCm/ROCm/blob/roc-5.7.x/tools/autotag/templates/rocm_changes/5.6.1.md
    
    ROCm 5.6.1 release note: hipMemcpy device-to-device (intra device) is
    now asynchronous with respect to the host

--- onnxruntime/core/providers/cuda/gpu_data_transfer.cc
+++ onnxruntime/core/providers/cuda/gpu_data_transfer.cc
@@ -7,10 +7,6 @@
 #include "cuda_common.h"
 
 namespace onnxruntime {
-GPUDataTransfer::GPUDataTransfer() {}
-
-GPUDataTransfer::~GPUDataTransfer() {}
-
 bool GPUDataTransfer::CanCopy(const OrtDevice& src_device, const OrtDevice& dst_device) const {
   return src_device.Type() == OrtDevice::GPU || src_device.MemType() == OrtDevice::MemType::CUDA_PINNED ||
          dst_device.Type() == OrtDevice::GPU || dst_device.MemType() == OrtDevice::MemType::CUDA_PINNED;
@@ -30,19 +26,25 @@ common::Status GPUDataTransfer::CopyTensor(const Tensor& src, Tensor& dst) const
       // Copy only if the two addresses are different.
       if (dst_data != src_data) {
         CUDA_RETURN_IF_ERROR(cudaMemcpy(dst_data, src_data, bytes, cudaMemcpyDeviceToDevice));
+        // For device memory to device memory copy, no host-side synchronization is performed by cudaMemcpy.
+        // see https://docs.nvidia.com/cuda/cuda-runtime-api/api-sync-behavior.html
         CUDA_RETURN_IF_ERROR(cudaStreamSynchronize(nullptr));
       }
     } else {
       // copy from other CPU memory to GPU, this is blocking
       CUDA_RETURN_IF_ERROR(cudaMemcpy(dst_data, src_data, bytes, cudaMemcpyHostToDevice));
-      CUDA_RETURN_IF_ERROR(cudaStreamSynchronize(nullptr));
+      if (src_device.MemType() != OrtDevice::MemType::CUDA_PINNED) {
+        // For cudaMemcpy from pageable host memory to device memory, DMA to final destination may not have completed.
+        // see https://docs.nvidia.com/cuda/cuda-runtime-api/api-sync-behavior.html
+        CUDA_RETURN_IF_ERROR(cudaStreamSynchronize(nullptr));
+      }
     }
   } else if (src_device.Type() == OrtDevice::GPU) {
     // copying from GPU to CPU memory, this is blocking
     CUDA_RETURN_IF_ERROR(cudaMemcpy(dst_data, src_data, bytes, cudaMemcpyDeviceToHost));
-    CUDA_RETURN_IF_ERROR(cudaStreamSynchronize(nullptr));
   } else {
     // copying between cpu memory
+    ORT_ENFORCE(dst_data != src_data);
     memcpy(dst_data, src_data, bytes);
   }
 
@@ -59,7 +61,7 @@ common::Status GPUDataTransfer::CopyTensorAsync(const Tensor& src, Tensor& dst, 
 
   if (dst_device.Type() == OrtDevice::GPU) {
     if (src_device.Type() == OrtDevice::CPU) {
-      // copy from pinned memory to GPU, this is non-blocking
+      // copy from pinned or non-pinned CPU memory to GPU
       CUDA_RETURN_IF_ERROR(cudaMemcpyAsync(dst_data, src_data, bytes, cudaMemcpyHostToDevice, static_cast<cudaStream_t>(stream.GetHandle())));
     } else if (src_device.Type() == OrtDevice::GPU) {
       // copying between GPU, this is non-blocking
@@ -69,7 +71,7 @@ common::Status GPUDataTransfer::CopyTensorAsync(const Tensor& src, Tensor& dst, 
     }
   } else if (src_device.Type() == OrtDevice::GPU) {
     if (dst_device.Type() == OrtDevice::CPU) {
-      // copying from GPU to pinned memory, this is non-blocking
+      // copy from GPU to pinned or non-pinned CPU memory.
       CUDA_RETURN_IF_ERROR(cudaMemcpyAsync(dst_data, src_data, bytes, cudaMemcpyDeviceToHost, static_cast<cudaStream_t>(stream.GetHandle())));
     }
   } else {
@@ -77,6 +79,8 @@ common::Status GPUDataTransfer::CopyTensorAsync(const Tensor& src, Tensor& dst, 
       // sync the stream first to make sure the data arrived
       CUDA_RETURN_IF_ERROR(cudaStreamSynchronize(static_cast<cudaStream_t>(stream.GetHandle())));
     }
+
+    ORT_ENFORCE(dst_data != src_data);
     memcpy(dst_data, src_data, bytes);
   }
 
