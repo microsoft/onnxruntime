9630aebfe63c7f796522bfc4c993717115a4f353
Yi Zhang <your@email.com>
2024-10-31 18:40:02 +0800

    lint

--- onnxruntime/test/common/cuda_op_test_utils.h
+++ onnxruntime/test/common/cuda_op_test_utils.h
@@ -5,9 +5,9 @@
 
 #include "test/util/include/default_providers.h"
 
-#define SKIP_CUDA_TEST_WITH_DML                                                                                           \
-  if (DefaultCudaExecutionProvider() == nullptr) {                                                                        \
-    GTEST_SKIP() << "CUDA Tests are not supported while DML is enabled";                                                                                                          \
+#define SKIP_CUDA_TEST_WITH_DML                                          \
+  if (DefaultCudaExecutionProvider() == nullptr) {                       \
+    GTEST_SKIP() << "CUDA Tests are not supported while DML is enabled"; \
   }
 
 namespace onnxruntime {
