781ee7efd554cef27d177d9b4b934a19c183bde7
shiyi <shiyi.zou@intel.com>
2024-10-31 13:22:25 +0800

    Revert "[WebNN] Fallback the node when its output doesn't have shape info (#2…"
    This reverts commit c547306d5fd7ce04219eab93666837d11a732edd.

--- onnxruntime/core/providers/webnn/builders/impl/base_op_builder.cc
+++ onnxruntime/core/providers/webnn/builders/impl/base_op_builder.cc
@@ -34,7 +34,7 @@ bool BaseOpBuilder::IsOpSupported(const InitializedTensorSet& initializers, cons
   if (!HasSupportedInputs(node, wnn_limits, logger))
     return false;
 
-  if (!HasSupportedOutputs(node, wnn_limits, logger))
+  if (!HasSupportedOutputsImpl(node, wnn_limits, logger))
     return false;
 
   if (!HasSupportedOpSet(node, logger))
@@ -47,7 +47,7 @@ bool BaseOpBuilder::HasSupportedInputs(const Node& node, const emscripten::val& 
                                        const logging::Logger& logger) const {
   const auto node_name = MakeString("Node [", node.Name(), "] type [", node.OpType(), "]");
   for (const auto* input : node.InputDefs()) {
-    if (!IsTensorShapeSupported(*input, node_name, logger)) {
+    if (!IsInputSupported(*input, node_name, logger)) {
       return false;
     }
   }
@@ -66,18 +66,6 @@ bool BaseOpBuilder::HasSupportedInputsImpl(const Node& node,
     return false;
 
   return IsDataTypeSupportedByOp(op_type, input_type, wnn_limits, "input", "Input", logger);
-}
-
-bool BaseOpBuilder::HasSupportedOutputs(const Node& node, const emscripten::val& wnn_limits,
-                                        const logging::Logger& logger) const {
-  const auto node_name = MakeString("Node [", node.Name(), "] type [", node.OpType(), "]");
-  for (const auto* output : node.OutputDefs()) {
-    if (!IsTensorShapeSupported(*output, node_name, logger)) {
-      return false;
-    }
-  }
-
-  return HasSupportedOutputsImpl(node, wnn_limits, logger);
 }
 
 bool BaseOpBuilder::HasSupportedOutputsImpl(const Node& node,
