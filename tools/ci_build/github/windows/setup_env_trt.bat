c3353b1ab9c2cbcf0fbb6be3011943e577267049
yf711 <yifanl@microsoft.com>
2024-11-04 14:38:45 -0800

    fix

--- tools/ci_build/github/windows/setup_env_trt.bat
+++ tools/ci_build/github/windows/setup_env_trt.bat
@@ -3,12 +3,18 @@ REM Licensed under the MIT License.
 
 @echo off
 setlocal
-REM Check if the user provided TensorRT path as arguments
+REM Check if the user provided TensorRT paths as arguments
 if "%~1"=="" (
     echo Please provide the TensorRT cuda 12 directory path as the first argument.
     exit /b 1
 )
+if "%~2"=="" (
+    echo Please provide the TensorRT cuda 11 directory path as the second argument.
+    exit /b 1
+)
+
 set TENSORRT_CUDA12_PATH=%~1
+set TENSORRT_CUDA11_PATH=%~2
 
 if exist PATH=%AGENT_TEMPDIRECTORY%\v12.2\ (
     set PATH=%PATH%;%AGENT_TEMPDIRECTORY%\v12.2\bin;%AGENT_TEMPDIRECTORY%\v12.2\extras\CUPTI\lib64
