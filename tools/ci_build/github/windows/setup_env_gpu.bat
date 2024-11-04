19523b9d68e222af3bc469f01dbe24f3d796a14e
yf711 <yifanl@microsoft.com>
2024-11-04 14:09:35 -0800

    wrap trt binary paths as external param for batch scripts

--- tools/ci_build/github/windows/setup_env_gpu.bat
+++ tools/ci_build/github/windows/setup_env_gpu.bat
@@ -1,21 +1,37 @@
 REM Copyright (c) Microsoft Corporation. All rights reserved.
 REM Licensed under the MIT License.
 
+@echo off
+setlocal
+REM Check if the user provided TensorRT paths as arguments
+if "%~1"=="" (
+    echo Please provide the TensorRT cuda 12 directory path as the first argument.
+    exit /b 1
+)
+if "%~2"=="" (
+    echo Please provide the TensorRT cuda 11 directory path as the second argument.
+    exit /b 1
+)
+
+set TENSORRT_CUDA12_PATH=%~1
+set TENSORRT_CUDA11_PATH=%~2
+
 if exist PATH=%AGENT_TEMPDIRECTORY%\v12.2\ (
     set PATH=%AGENT_TEMPDIRECTORY%\v12.2\bin;%AGENT_TEMPDIRECTORY%\v12.2\extras\CUPTI\lib64;%PATH%
 ) else (
     set PATH=C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v12.2\bin;C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v12.2\extras\CUPTI\lib64;%PATH%
 )
-set PATH=%AGENT_TEMPDIRECTORY%\TensorRT-10.5.0.18.Windows10.x86_64.cuda-12.6\lib;%PATH%
+set PATH=%AGENT_TEMPDIRECTORY%\%TENSORRT_CUDA12_PATH%\lib;%PATH%
 
 @REM The default version is still cuda v12.2, because set cuda v11.8 after it
-set PATH=%PATH%;%AGENT_TEMPDIRECTORY%\TensorRT-10.5.0.18.Windows10.x86_64.cuda-11.8\lib
+set PATH=%PATH%;%AGENT_TEMPDIRECTORY%\%TENSORRT_CUDA11_PATH%\lib
 if exist PATH=%AGENT_TEMPDIRECTORY%\v11.8\ (
     set PATH=%PATH%;%AGENT_TEMPDIRECTORY%\v11.8\bin;%AGENT_TEMPDIRECTORY%\v11.8\extras\CUPTI\lib64
 ) else (
     set PATH=%PATH%;C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\\bin;C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\\extras\CUPTI\lib64
 )
 
-
 set GRADLE_OPTS=-Dorg.gradle.daemon=false
 set CUDA_MODULE_LOADING=LAZY
+
+endlocal
--- tools/ci_build/github/windows/setup_env_trt.bat
+++ tools/ci_build/github/windows/setup_env_trt.bat
@@ -1,11 +1,22 @@
 REM Copyright (c) Microsoft Corporation. All rights reserved.
 REM Licensed under the MIT License.
 
+@echo off
+setlocal
+REM Check if the user provided TensorRT path as arguments
+if "%~1"=="" (
+    echo Please provide the TensorRT cuda 12 directory path as the first argument.
+    exit /b 1
+)
+set TENSORRT_CUDA12_PATH=%~1
+
 if exist PATH=%AGENT_TEMPDIRECTORY%\v12.2\ (
     set PATH=%PATH%;%AGENT_TEMPDIRECTORY%\v12.2\bin;%AGENT_TEMPDIRECTORY%\v12.2\extras\CUPTI\lib64
 ) else (
     set PATH=%PATH%;C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v12.2\bin;C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v12.2\extras\CUPTI\lib64
 )
-set PATH=%AGENT_TEMPDIRECTORY%\TensorRT-10.5.0.18.Windows10.x86_64.cuda-12.6\lib;%PATH%
+set PATH=%AGENT_TEMPDIRECTORY%\%TENSORRT_CUDA12_PATH%\lib;%PATH%
 set GRADLE_OPTS=-Dorg.gradle.daemon=false
 set CUDA_MODULE_LOADING=LAZY
+
+endlocal
