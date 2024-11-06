653b12bfa0711ff0772354b020a99d5b0f967c88
Kyle Zhang <xi.zhang@microsoft.com>
2024-11-06 17:32:35 +0800

    debug

--- tools/ci_build/kyle_test.py
+++ tools/ci_build/kyle_test.py
@@ -2,5 +2,6 @@ import os
 
 print("===================OUTPUT ENVRONMENT VARIABLES START===================")
 print(os.environ['ReleaseVersionSuffix'])
+print(os.environ['ReleaseVersionSuffixSTRING'])
 print(os.environ['KYLE_ENV'])
 print("===================OUTPUT ENVRONMENT VARIABLES END===================")
