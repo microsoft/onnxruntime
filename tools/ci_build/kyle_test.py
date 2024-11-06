28ffd01174c31aabb5f8c688f60c530fa48962dd
Kyle Zhang <xi.zhang@microsoft.com>
2024-11-06 17:36:22 +0800

    debug

--- tools/ci_build/kyle_test.py
+++ tools/ci_build/kyle_test.py
@@ -1,7 +1,18 @@
 import os
 
 print("===================OUTPUT ENVRONMENT VARIABLES START===================")
-print(os.environ['ReleaseVersionSuffix'])
-print(os.environ['ReleaseVersionSuffixSTRING'])
-print(os.environ['KYLE_ENV'])
+if os.environ.get('ReleaseVersionSuffix') is None:
+    print("ReleaseVersionSuffix is not set")
+else:
+    print(os.environ['ReleaseVersionSuffix'])
+
+if os.environ.get('ReleaseVersionSuffixSTRING') is None:
+    print("ReleaseVersionSuffixSTRING is not set")
+else:
+    print(os.environ['ReleaseVersionSuffixSTRING'])
+
+if os.environ.get('KYLE_ENV') is None:
+    print("KYLE_ENV is not set")
+else:
+    print(os.environ['KYLE_ENV'])
 print("===================OUTPUT ENVRONMENT VARIABLES END===================")
