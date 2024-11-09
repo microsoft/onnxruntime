265b3642949d3c8b18a32a6224065d192a1277b7
Yi Zhang <zhanyi@microsoft.com>
2024-11-09 11:26:19 +0800

    Add XNNPack build on Linux ARM64 and improve Linux CPU (#22773)
    ### Description
    1. Add XNNPack build on Linux ARM64
    2. Build only one python wheel for PR request.
    
    [AB#49763](https://aiinfra.visualstudio.com/6a833879-cd9b-44a4-a9de-adc2d818f13c/_workitems/edit/49763)
    
    
    
    ### Motivation and Context
    Why I add xnnpack build on Linux ARM64  rather than Windows ARM64.
    Becuase KleidiAI  doesn't support Windows
    
    ```
    IF(XNNPACK_TARGET_PROCESSOR STREQUAL "arm64" AND XNNPACK_ENABLE_ARM_I8MM AND NOT CMAKE_C_COMPILER_ID STREQUAL "MSVC")
      IF (XNNPACK_ENABLE_KLEIDIAI)
        MESSAGE(STATUS "Enabling KleidiAI for Arm64")
      ENDIF()
    ELSE()
      SET(XNNPACK_ENABLE_KLEIDIAI OFF)
    ENDIF()
    ```
    
    ---------

--- tools/ci_build/github/linux/run_python_dockerbuild.sh
+++ tools/ci_build/github/linux/run_python_dockerbuild.sh
@@ -2,14 +2,15 @@
 set -e -x
 BUILD_CONFIG="Release"
 
-while getopts "i:d:x:c:" parameter_Option
+while getopts "i:d:x:c:p:" parameter_Option
 do case "${parameter_Option}"
 in
 i) DOCKER_IMAGE=${OPTARG};;
 d) DEVICE=${OPTARG};;
 x) BUILD_EXTR_PAR=${OPTARG};;
 c) BUILD_CONFIG=${OPTARG};;
-*) echo "Usage: $0 -i <docker_image> -d <GPU|CPU> [-x <extra_build_arg>] [-c <build_config>]"
+p) PYTHON_EXES=${OPTARG};;
+*) echo "Usage: $0 -i <docker_image> -d <GPU|CPU> [-x <extra_build_arg>] [-c <build_config>] [-p <python_exe_path>]"
    exit 1;;
 esac
 done
@@ -33,7 +34,7 @@ docker run --rm \
     -e ORT_DISABLE_PYTHON_PACKAGE_LOCAL_VERSION \
     -e DEFAULT_TRAINING_PACKAGE_DEVICE \
     $ADDITIONAL_DOCKER_PARAMETER \
-    $DOCKER_IMAGE tools/ci_build/github/linux/build_linux_python_package.sh $DOCKER_SCRIPT_OPTIONS
+    $DOCKER_IMAGE tools/ci_build/github/linux/build_linux_python_package.sh -c $BUILD_CONFIG -p $PYTHON_EXES $DOCKER_SCRIPT_OPTIONS
 
 sudo rm -rf "${BUILD_BINARIESDIRECTORY}/${BUILD_CONFIG}/onnxruntime" "${BUILD_BINARIESDIRECTORY}/${BUILD_CONFIG}/pybind11" \
     "${BUILD_BINARIESDIRECTORY}/${BUILD_CONFIG}/models" "${BUILD_BINARIESDIRECTORY}/${BUILD_CONFIG}/_deps" \
