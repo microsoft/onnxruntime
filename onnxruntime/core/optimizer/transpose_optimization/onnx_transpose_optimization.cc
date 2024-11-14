146fad61e1c75003d6c160c22dacdc3005d4ed89
Yi-Hong Lyu <yilyu@microsoft.com>
2024-11-13 23:06:49 -0800

    Optimize QLinearSoftmax Transpose
    - Improved transpose around QLinearSoftmax in Level 3 NHWC Transformer.
    - Removed redundant code HandleQLinearConcat, HandleQLinearBinaryOp.

--- onnxruntime/core/optimizer/transpose_optimization/onnx_transpose_optimization.cc
+++ onnxruntime/core/optimizer/transpose_optimization/onnx_transpose_optimization.cc
@@ -1654,14 +1654,14 @@ static bool HandleSplit(HandlerArgs& args) {
 
 constexpr HandlerInfo split_handler = {&FirstInput, &HandleSplit};
 
-static bool HandleConcat(HandlerArgs& args) {
+bool HandleConcat(HandlerArgs& args) {
   return HandleSimpleNodeWithAxis(args);
 }
 
 constexpr HandlerInfo concat_handler = {&AllInputs, &HandleConcat};
 
 // Handles Softmax, Hardmax, and LogSoftmax
-static bool HandleSoftHardMax(HandlerArgs& args) {
+bool HandleSoftHardMax(HandlerArgs& args) {
   if (args.ctx.opset >= 13) {
     return HandleSimpleNodeWithAxis(args, /*default_axis*/ -1);
   }
@@ -2585,6 +2585,7 @@ static const std::unordered_map<std::string_view, const HandlerInfo&> handler_ma
     {"Softmax", soft_hard_max_handler},
     {"Hardmax", soft_hard_max_handler},
     {"LogSoftmax", soft_hard_max_handler},
+    //{"com.microsoft.QLinearSoftmax", soft_hard_max_handler},
 
     {"QuantizeLinear", quantize_dequantize_linear_handler},
     {"DequantizeLinear", quantize_dequantize_linear_handler},
@@ -2601,6 +2602,8 @@ static const HandlerInfo* GetHandler(api::NodeRef& node, const HandlerMap& exten
   } else {
     key = onnxruntime::MakeString(domain, ".", op_type);
   }
+
+  //std::cout << "key: " << key << std::endl;
 
   // extended map is higher priority
   auto match = extended_handlers.find(key);
