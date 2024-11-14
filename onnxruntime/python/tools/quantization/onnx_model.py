b70729d7a151b4d5f846c416e5742412b0b2b410
adrianlizarraga <adlizarraga@microsoft.com>
2024-11-14 01:39:22 -0800

    import annotations in onnx_model.py

--- onnxruntime/python/tools/quantization/onnx_model.py
+++ onnxruntime/python/tools/quantization/onnx_model.py
@@ -2,6 +2,8 @@
 # Copyright (c) Microsoft Corporation.  All rights reserved.
 # Licensed under the MIT License.
 # --------------------------------------------------------------------------
+from __future__ import annotations
+
 from pathlib import Path
 
 import numpy as np
