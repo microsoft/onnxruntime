eb5d7b4eed471363cbecc0be72f4e842215d4cf8
Satya Kumar Jandhyala <satya.k.jandhyala@gmail.com>
2024-11-18 00:01:00 -0800

    Use kv_num_heads instead num_heads for key/value input shape conversion.

--- onnxruntime/contrib_ops/webgpu/bert/group_query_attention.cc
+++ onnxruntime/contrib_ops/webgpu/bert/group_query_attention.cc
@@ -64,8 +64,13 @@ Status GroupQueryAttention::ComputeInternal(onnxruntime::webgpu::ComputeContext&
   output_shape[1] = static_cast<int64_t>(parameters.sequence_length_);
   output_shape[2] = static_cast<int64_t>(parameters.hidden_size_);
   Tensor* output = context.Output(0, output_shape);
-  const int present_kv_seqlen = parameters.seqlen_present_kv_cache_;
-  std::vector<int64_t> present_kv_shape({static_cast<int64_t>(parameters.batch_size_), static_cast<int64_t>(kv_num_heads_), static_cast<int64_t>(present_kv_seqlen), static_cast<int64_t>(parameters.head_size_)});
+  std::vector<int64_t> present_dims{
+    parameters.batch_size_,
+    kv_num_heads_,
+    parameters.seqlen_present_kv_cache_,
+    parameters.head_size_
+  };
+  std::vector<int64_t> present_kv_shape(present_dims);
   Tensor* present_key = context.Output(1, present_kv_shape);
   Tensor* present_value = context.Output(2, present_kv_shape);
   parameters.past_present_share_buffer_ = present_key != nullptr && present_value != nullptr && past_key != nullptr && past_value != nullptr && past_key->DataRaw() == present_key->DataRaw() && past_value->DataRaw() == present_value->DataRaw();
@@ -76,23 +81,24 @@ Status GroupQueryAttention::ComputeInternal(onnxruntime::webgpu::ComputeContext&
   Tensor Q = context.CreateGPUTensor(query->DataType(), q_new_shape);
   ORT_RETURN_IF_ERROR(TransferBSDToBNSH(
       context, parameters.num_heads_, parameters.sequence_length_, parameters.head_size_, query, nullptr, 0, &Q));
-  TensorShapeVector k_new_dims({parameters.batch_size_, parameters.num_heads_,
-                                parameters.kv_sequence_length_, parameters.head_size_});
   if (parameters.qkv_format_ == Q_K_V_BSNH_BNSH_BNSH) {  // key and value in BNSH format
     return ApplyAttention(&Q, key, value, nullptr, past_key, past_value, output, present_key,
                           present_value, parameters, context, seqlen_k);
   }
+
+  TensorShapeVector k_new_dims({parameters.batch_size_, parameters.kv_num_heads_,
+                                parameters.kv_sequence_length_, parameters.head_size_});
   TensorShape k_new_shape(k_new_dims);
   Tensor K = context.CreateGPUTensor(key->DataType(), k_new_shape);
-  ORT_RETURN_IF_ERROR(TransferBSDToBNSH(context, parameters.num_heads_, parameters.kv_sequence_length_,
-                                        parameters.head_size_, key, nullptr, parameters.hidden_size_, &K));
+  ORT_RETURN_IF_ERROR(TransferBSDToBNSH(context, parameters.kv_num_heads_, parameters.kv_sequence_length_,
+                                        parameters.head_size_, key, nullptr, 0, &K));
 
-  TensorShapeVector v_new_dims({parameters.batch_size_, parameters.num_heads_,
+  TensorShapeVector v_new_dims({parameters.batch_size_, parameters.kv_num_heads_,
                                 parameters.kv_sequence_length_, parameters.v_head_size_});
   TensorShape v_new_shape(v_new_dims);
   Tensor V = context.CreateGPUTensor(value->DataType(), v_new_shape);
-  ORT_RETURN_IF_ERROR(TransferBSDToBNSH(context, parameters.num_heads_, parameters.kv_sequence_length_,
-                                        parameters.v_head_size_, value, nullptr, 2 * parameters.hidden_size_, &V));
+  ORT_RETURN_IF_ERROR(TransferBSDToBNSH(context, parameters.kv_num_heads_, parameters.kv_sequence_length_,
+                                        parameters.v_head_size_, value, nullptr, 0, &V));
   return ApplyAttention(&Q, &K, &V, nullptr, past_key, past_value, output, present_key,
                         present_value, parameters, context, seqlen_k);
 }
