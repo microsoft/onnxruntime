b81160fc7ce99859410b0a096bb72060da93789d
wejoncy <wejoncy@163.com>
2024-11-21 16:28:32 +0800

    Update objectivec/ort_coreml_execution_provider.mm
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- objectivec/ort_coreml_execution_provider.mm
+++ objectivec/ort_coreml_execution_provider.mm
@@ -44,7 +44,7 @@ BOOL ORTIsCoreMLExecutionProviderAvailable() {
 }
 
 - (BOOL)appendCoreMLExecutionProviderWithOptionsV2:(NSDictionary*)provider_options
-                                              error:(NSError**)error {
+                                             error:(NSError**)error {
 #if ORT_OBJC_API_COREML_EP_AVAILABLE
   try {
     return [self appendExecutionProvider:@"CoreML" providerOptions:provider_options error:error];
