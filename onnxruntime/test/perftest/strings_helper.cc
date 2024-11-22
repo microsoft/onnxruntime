7ac7cfd86b0496615bf6f7da4800772e13155951
wejoncy <wejoncy@163.com>
2024-11-21 21:36:31 -0800

    remove key suggestion

--- onnxruntime/test/perftest/strings_helper.cc
+++ onnxruntime/test/perftest/strings_helper.cc
@@ -12,59 +12,6 @@
 namespace onnxruntime {
 namespace perftest {
 
-/**
- * @brief Computes the Levenshtein distance between two strings.
- *
- * The Levenshtein distance is a metric for measuring the difference between two strings.
- * It represents the minimum number of single-character edits required to change one string
- * into the other. The possible edits are insertion, deletion, or substitution of a character.
- *
- * This function uses a dynamic programming approach with optimized space complexity.
- * It maintains only two vectors representing the previous and current rows of the distance matrix.
- *
- * @param str1 The first input string.
- * @param str2 The second input string.
- * @return The Levenshtein distance between the two strings.
- */
-static size_t LevenShtein(const std::string& str1, const std::string& str2) {
-  size_t m = str1.length();
-  size_t n = str2.length();
-
-  std::vector<size_t> prevRow(n + 1, 0);
-  std::vector<size_t> currRow(n + 1, 0);
-
-  for (size_t j = 0; j <= n; j++) {
-    prevRow[j] = j;
-  }
-
-  for (size_t i = 1; i <= m; i++) {
-    currRow[0] = i;
-    for (size_t j = 1; j <= n; j++) {
-      if (str1[i - 1] == str2[j - 1]) {
-        currRow[j] = prevRow[j - 1];
-      } else {
-        currRow[j] = 1 + std::min(currRow[j - 1], std::min(prevRow[j], prevRow[j - 1]));
-      }
-    }
-    prevRow = currRow;
-  }
-
-  return currRow[n];
-}
-
-static std::string FindNearestKey(const std::string& key, const std::unordered_set<std::string>& available_keys) {
-  std::string similar_key;
-  size_t min_distance = std::numeric_limits<int>::max();
-  for (const auto& av_key : available_keys) {
-    size_t distance = LevenShtein(av_key, key);
-    if (distance < min_distance) {
-      min_distance = distance;
-      similar_key = av_key;
-    }
-  }
-  return similar_key;
-}
-
 bool ParseSessionConfigs(const std::string& configs_string,
                          std::unordered_map<std::string, std::string>& session_configs,
                          const std::unordered_set<std::string>& available_keys) {
@@ -93,8 +40,8 @@ bool ParseSessionConfigs(const std::string& configs_string,
         available_keys_str += av_key;
         available_keys_str += ", ";
       }
-      ORT_THROW("[ERROR] wrong key type entered : `", key, "`. Did you mean: `", FindNearestKey(key, available_keys),
-                "`?. The following runtime key options are avaible: [", available_keys_str, "]");
+      ORT_THROW("[ERROR] wrong key type entered : `", key,
+                "`. The following runtime key options are avaible: [", available_keys_str, "]");
     }
 
     auto it = session_configs.find(key);
