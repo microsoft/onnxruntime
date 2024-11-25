dffabcd3e69c01bce58be3001e06cc2a1b6fb8f4
Kyle Zhang <xi.zhang@microsoft.com>
2024-11-25 14:07:16 +0800

    fix jar packaing ps1

--- tools/ci_build/github/windows/jar_gpu_packaging.ps1
+++ tools/ci_build/github/windows/jar_gpu_packaging.ps1
@@ -1,19 +1,20 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
+$onnx_runtime_release_version = $Env:ONNXRUNTIMEVERSION + $Env:RELEASEVERSIONSUFFIX
 $ErrorActionPreference = "Stop"
 Write-Output "Start"
 dir
-Copy-Item -Path $Env:BUILD_BINARIESDIRECTORY\java-artifact\onnxruntime-java-linux-x64\ai\onnxruntime\native\linux-x64\libonnxruntime_providers_cuda.so -Destination $Env:BUILD_BINARIESDIRECTORY\java-artifact\onnxruntime-java-linux-x64-tensorrt\ai\onnxruntime\native\linux-x64 
+Copy-Item -Path $Env:BUILD_BINARIESDIRECTORY\java-artifact\onnxruntime-java-linux-x64\ai\onnxruntime\native\linux-x64\libonnxruntime_providers_cuda.so -Destination $Env:BUILD_BINARIESDIRECTORY\java-artifact\onnxruntime-java-linux-x64-tensorrt\ai\onnxruntime\native\linux-x64
 pushd onnxruntime-java-linux-x64-tensorrt
 Write-Output "Run 7z"
 7z a $Env:BUILD_BINARIESDIRECTORY\java-artifact\onnxruntime-java-win-x64\testing.jar libcustom_op_library.so
 Remove-Item -Path libcustom_op_library.so
-7z a $Env:BUILD_BINARIESDIRECTORY\java-artifact\onnxruntime-java-win-x64\onnxruntime-$Env:ONNXRUNTIMEVERSION.jar .
+7z a $Env:BUILD_BINARIESDIRECTORY\java-artifact\onnxruntime-java-win-x64\onnxruntime-$onnx_runtime_release_version.jar .
 popd
 pushd onnxruntime-java-win-x64
-ren onnxruntime-$Env:ONNXRUNTIMEVERSION.jar onnxruntime_gpu-$Env:ONNXRUNTIMEVERSION.jar
-ren onnxruntime-$Env:ONNXRUNTIMEVERSION-javadoc.jar onnxruntime_gpu-$Env:ONNXRUNTIMEVERSION-javadoc.jar
-ren onnxruntime-$Env:ONNXRUNTIMEVERSION-sources.jar onnxruntime_gpu-$Env:ONNXRUNTIMEVERSION-sources.jar
-ren onnxruntime-$Env:ONNXRUNTIMEVERSION.pom onnxruntime_gpu-$Env:ONNXRUNTIMEVERSION.pom
+ren onnxruntime-$onnx_runtime_release_version.jar onnxruntime_gpu-$onnx_runtime_release_version.jar
+ren onnxruntime-$onnx_runtime_release_version-javadoc.jar onnxruntime_gpu-$onnx_runtime_release_version-javadoc.jar
+ren onnxruntime-$onnx_runtime_release_version-sources.jar onnxruntime_gpu-$onnx_runtime_release_version-sources.jar
+ren onnxruntime-$onnx_runtime_release_version.pom onnxruntime_gpu-$onnx_runtime_release_version.pom
 popd
--- tools/ci_build/github/windows/jar_packaging.ps1
+++ tools/ci_build/github/windows/jar_packaging.ps1
@@ -1,6 +1,7 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
+$onnx_runtime_release_version = $Env:ONNXRUNTIMEVERSION + $Env:RELEASEVERSIONSUFFIX
 $ErrorActionPreference = "Stop"
 Write-Output "Start"
 dir
@@ -8,18 +9,18 @@ pushd onnxruntime-java-linux-x64
 Write-Output "Run 7z"
 7z a $Env:BUILD_BINARIESDIRECTORY\java-artifact\onnxruntime-java-win-x64\testing.jar libcustom_op_library.so
 Remove-Item -Path libcustom_op_library.so
-7z a $Env:BUILD_BINARIESDIRECTORY\java-artifact\onnxruntime-java-win-x64\onnxruntime-$Env:ONNXRUNTIMEVERSION.jar .
+7z a $Env:BUILD_BINARIESDIRECTORY\java-artifact\onnxruntime-java-win-x64\onnxruntime-$onnx_runtime_release_version.jar .
 popd
 pushd onnxruntime-java-osx-x86_64
 7z a $Env:BUILD_BINARIESDIRECTORY\java-artifact\onnxruntime-java-win-x64\testing.jar libcustom_op_library.dylib
 Remove-Item -Path libcustom_op_library.dylib
-7z a $Env:BUILD_BINARIESDIRECTORY\java-artifact\onnxruntime-java-win-x64\onnxruntime-$Env:ONNXRUNTIMEVERSION.jar .
+7z a $Env:BUILD_BINARIESDIRECTORY\java-artifact\onnxruntime-java-win-x64\onnxruntime-$onnx_runtime_release_version.jar .
 popd
 pushd onnxruntime-java-linux-aarch64
 Remove-Item -Path libcustom_op_library.so
-7z a $Env:BUILD_BINARIESDIRECTORY\java-artifact\onnxruntime-java-win-x64\onnxruntime-$Env:ONNXRUNTIMEVERSION.jar .
+7z a $Env:BUILD_BINARIESDIRECTORY\java-artifact\onnxruntime-java-win-x64\onnxruntime-$onnx_runtime_release_version.jar .
 popd
 pushd onnxruntime-java-osx-arm64
 Remove-Item -Path libcustom_op_library.dylib
-7z a $Env:BUILD_BINARIESDIRECTORY\java-artifact\onnxruntime-java-win-x64\onnxruntime-$Env:ONNXRUNTIMEVERSION.jar .
+7z a $Env:BUILD_BINARIESDIRECTORY\java-artifact\onnxruntime-java-win-x64\onnxruntime-$onnx_runtime_release_version.jar .
 popd
