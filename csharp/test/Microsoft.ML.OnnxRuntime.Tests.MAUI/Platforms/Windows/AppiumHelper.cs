400057823d25213c52fa83da3b1df9b076b83df6
Scott McKay <Scott.McKay@microsoft.com>
2024-11-25 20:46:29 +1000

    Fix app name

--- csharp/test/Microsoft.ML.OnnxRuntime.Tests.MAUI/Platforms/Windows/AppiumHelper.cs
+++ csharp/test/Microsoft.ML.OnnxRuntime.Tests.MAUI/Platforms/Windows/AppiumHelper.cs
@@ -17,7 +17,9 @@ internal static partial class AppiumHelper
             // Always Windows for Windows
             PlatformName = "Windows",
             // The identifier of the deployed application to test
-            App = "ORT.CSharp.Tests.MAUI", 
+            // Note sure where this is defined, but log from the 'Deployment' tab in the Visual Studio 'Output' window
+            // shows the full package name.
+            App = "ORT.CSharp.Tests.MAUI_1.0.0.1_x64__9zz4h110yvjzm", 
         };
 
         // Note there are many more options that you can use to influence the app under test according to your needs
