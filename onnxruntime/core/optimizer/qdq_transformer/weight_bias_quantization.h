9bc8c5c450eb3cd62e20a7ff547cddf382319269
Vincent Wang <wangwchpku@outlook.com>
2024-12-04 11:31:41 +0800

    resolve comments

--- onnxruntime/core/optimizer/qdq_transformer/weight_bias_quantization.h
+++ onnxruntime/core/optimizer/qdq_transformer/weight_bias_quantization.h
@@ -11,12 +11,10 @@ namespace onnxruntime {
  * @class WeightBiasQuantization
  *
  * Some quantized models do not have Gemm/Conv/ConvTranspose's weight and/or bias quantized. This optimization adds
- * subgraphs to quantize the weight and/or bias.
- *   For weight, it's quantized to symmetric per-tensor INT8 tensor.
- *   For bias, it's quantized to a INT32 tensor with scale = scale_input_0 * scale_input_1 and zero_point = 0.
- *
- * Normally the ConstantFolding optimizer would fold the weight and/or bias initializers into the target initializers,
- * which is consumed by a DequantizeLinear node respectively.
+ * subgraphs with Q->DQ after weight and/or bias. It's possible that the ConstantFolding optimizer would fold the Q Op
+ * so that weight and/or bias initializers are folded to initializers in target data types, followed by DQ Op.
+ *   For weight, the Q output is a symmetric per-tensor INT8 tensor.
+ *   For bias, the Q's scale = scale_input_0 * scale_input_1 and zero_point = (INT32)0.
  */
 class WeightBiasQuantization : public GraphTransformer {
  public:
