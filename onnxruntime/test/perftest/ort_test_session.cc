46993a3e2544a459ae24a2b1c3cf5e484ecd7c40
Hector Li <hecli@microsoft.com>
2024-12-05 19:52:48 -0800

    resolve issue cause by merge conflict

--- onnxruntime/test/perftest/ort_test_session.cc
+++ onnxruntime/test/perftest/ort_test_session.cc
@@ -202,7 +202,7 @@ OnnxRuntimeTestSession::OnnxRuntimeTestSession(Ort::Env& env, std::random_device
                         {"backend_path", "profiling_file_path", "profiling_level", "rpc_control_latency",
                          "vtcm_mb", "soc_model", "device_id", "htp_performance_mode", "qnn_saver_path",
                          "htp_graph_finalization_optimization_mode", "qnn_context_priority", "htp_arch",
-                         "enable_htp_fp16_precision", "offload_graph_io_quantization"});
+                         "enable_htp_fp16_precision", "offload_graph_io_quantization", "enable_htp_spill_fill_buffer"});
     for (const auto& provider_option : provider_options) {
       const std::string& key = provider_option.first;
       const std::string& value = provider_option.second;
