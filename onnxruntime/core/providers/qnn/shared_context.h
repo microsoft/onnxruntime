45ef88371923f4597f9e897d36137f59aa3738e8
edgchen1 <18449977+edgchen1@users.noreply.github.com>
2024-12-11 11:05:21 -0800

    remove shared mem handles from shared context

--- onnxruntime/core/providers/qnn/shared_context.h
+++ onnxruntime/core/providers/qnn/shared_context.h
@@ -1,12 +1,9 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License
 
-#include <functional>
 #include <memory>
 #include <mutex>
 #include <vector>
-
-#include <QnnInterface.h>
 
 #include "core/common/common.h"
 #include "core/providers/qnn/builder/qnn_model.h"
@@ -14,36 +11,6 @@
 #pragma once
 
 namespace onnxruntime {
-
-class SharedMemHandles {
- public:
-  Qnn_MemHandle_t Get(const void* addr) {
-    std::lock_guard g{mutex_};
-    const auto it = qnn_mem_handles_.find(addr);
-    ORT_ENFORCE(it != qnn_mem_handles_.end(), "Failed to find mem handle associated with address (", addr, ").");
-    return it->second;
-  }
-
-  void Add(const void* addr, Qnn_MemHandle_t mem_handle) {
-    std::lock_guard g{mutex_};
-    auto [it, added] = qnn_mem_handles_.emplace(addr, mem_handle);
-    ORT_ENFORCE(added,
-                "There is already a mem handle (", mem_handle, ") associated with the address (", addr, ").");
-  }
-
-  Qnn_MemHandle_t GetAndRemove(const void* addr) {
-    std::lock_guard g{mutex_};
-    const auto it = qnn_mem_handles_.find(addr);
-    ORT_ENFORCE(it != qnn_mem_handles_.end(), "Failed to find mem handle associated with address (", addr, ").");
-    const auto qnn_mem_handle = it->second;
-    qnn_mem_handles_.erase(it);
-    return qnn_mem_handle;
-  }
-
- private:
-  std::unordered_map<const void*, Qnn_MemHandle_t> qnn_mem_handles_;
-  std::mutex mutex_;
-};
 
 class SharedContext {
  public:
@@ -94,8 +61,6 @@ class SharedContext {
     return graph_exist;
   }
 
-  SharedMemHandles& GetSharedMemHandles() { return shared_mem_handles_; }
-
  private:
   SharedContext() = default;
   ~SharedContext() = default;
@@ -106,9 +71,6 @@ class SharedContext {
   // Producer sessions can be in parallel
   // Consumer sessions have to be after producer sessions initialized
   std::mutex mtx_;
-
-  // TODO can we avoid keeping mem handles in SharedContext?
-  SharedMemHandles shared_mem_handles_;
 };
 
 }  // namespace onnxruntime
