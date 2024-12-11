d8de3c4096d61c70e1c7efd94b39068f44152139
amancini-N <63410090+amancini-N@users.noreply.github.com>
2024-12-11 01:20:47 +0100

    [CUDA EP] Fix BeamSearch on T5 with sequence_as_input_ids (#20667) (#20668)
    ### Description
    Change the implementation of BeamSearch op when using CUDA EP: in case
    of T5 model, and in case the decoder input_ids are sequences, copy the
    sequences device-to-device instead of host-to-device
    
    ### Motivation and Context
    - Fixes #20667

--- onnxruntime/test/contrib_ops/beam_search_test.cc
+++ onnxruntime/test/contrib_ops/beam_search_test.cc
@@ -424,5 +424,19 @@ TEST(BeamSearchTest, DummyT5WithOuterScopeInitializers) {
   tester.RunWithConfig();
 }
 
+TEST(BeamSearchTest, DummyT5WithSequenceInputIds) {
+#if defined(USE_CUDA) && defined(USE_DML)
+  SKIP_CUDA_TEST_WITH_DML;
+#endif
+  ModelTester tester(CurrentTestName(), ORT_TSTR("testdata/dummy_t5_with_sequence_input_ids.onnx"));
+  tester.ConfigEp(DefaultCpuExecutionProvider());
+  tester.AddInput("encoder_input_ids", {1, 5}, {16, 17, 1, 0, 8});
+  tester.AddOutput("sequences", {1, 3, 10}, {2, 19, 18, 3, 8, 8, 8, 8, 8, 8, 2, 19, 18, 3, 10, 19, 18, 3, 8, 8, 2, 19, 18, 15, 13, 13, 13, 13, 13, 13});
+#ifdef USE_CUDA
+  tester.ConfigEp(DefaultCudaExecutionProvider());
+#endif
+  tester.RunWithConfig();
+}
+
 }  // namespace test
 }  // namespace onnxruntime
