cbea59ac9b651cecb45153880625167f20f09fc6
genmingz@AMD <danyue333@163.com>
2024-12-13 13:13:43 +0800

    Add attrProto.release_s interface (#22977)
    ### Description
    Add AttributeProto.release_s interface, which is used to obtain the
    string in the attribute using move semantics instead of copying it
    
    
    
    ### Motivation and Context
    The ep_context node stores a lot of information in attributes, which may
    cause the memory usage to increase. Use this interface to avoid memory
    waste
    
    ---------
    
    Co-authored-by: GenMing Zhong <genmingz@xlnx.xilinx.com>
    Co-authored-by: genmingz <genmingz@amd.com>

--- onnxruntime/core/session/provider_bridge_ort.cc
+++ onnxruntime/core/session/provider_bridge_ort.cc
@@ -497,6 +497,7 @@ struct ProviderHostImpl : ProviderHost {
   void AttributeProto__set_name(ONNX_NAMESPACE::AttributeProto* p, const ::std::string& value) override { return p->set_name(value); }
   void AttributeProto__set_type(ONNX_NAMESPACE::AttributeProto* p, ONNX_NAMESPACE::AttributeProto_AttributeType value) override { return p->set_type(value); }
   ONNX_NAMESPACE::TensorProto* AttributeProto__add_tensors(ONNX_NAMESPACE::AttributeProto* p) override { return p->add_tensors(); }
+  std::string* AttributeProto__release_s(ONNX_NAMESPACE::AttributeProto* p) override { return p->release_s(); }
 
   // GraphProto (wrapped)
   std::unique_ptr<ONNX_NAMESPACE::GraphProto> GraphProto__construct() override { return std::make_unique<ONNX_NAMESPACE::GraphProto>(); }
