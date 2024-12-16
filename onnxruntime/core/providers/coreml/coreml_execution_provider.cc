e82f3e41c5f10f633eda08dbb9e896d0a248301a
wejoncy <wejoncy@.com>
2024-12-16 19:07:11 +0800

    fix

--- onnxruntime/core/providers/coreml/coreml_execution_provider.cc
+++ onnxruntime/core/providers/coreml/coreml_execution_provider.cc
@@ -66,7 +66,7 @@ CoreMLExecutionProvider::GetCapability(const onnxruntime::GraphViewer& graph_vie
         if (main_graph->GetModel().MetaData().count(kCOREML_CACHE_KEY) > 0) {
           user_provided_key = graph_viewer.GetGraph().GetModel().MetaData().at(kCOREML_CACHE_KEY);
           if (user_provided_key.size() > 32 ||
-              std::any_if(user_provided_key.begin(), user_provided_key.end(),
+              std::any_of(user_provided_key.begin(), user_provided_key.end(),
                             [](unsigned char c) { return !std::isalnum(c); })) {
             user_provided_key = std::to_string(std::hash<std::string>{}(user_provided_key));
           }
