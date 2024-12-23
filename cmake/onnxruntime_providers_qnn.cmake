b707c46acfa93c40ffd85e062888f31add5fbc8a
adrianlizarraga <adlizarraga@microsoft.com>
2024-12-22 17:13:02 -0800

    prepend _ to linker arg

--- cmake/onnxruntime_providers_qnn.cmake
+++ cmake/onnxruntime_providers_qnn.cmake
@@ -34,7 +34,7 @@
                           "LINKER:--version-script=${ONNXRUNTIME_ROOT}/core/providers/qnn/version_script.lds"
                           "LINKER:--gc-sections"
                           "LINKER:-rpath=\$ORIGIN"
-                          "LINKER:--undefined=Provider_GetHost"
+                          "LINKER:-u,_Provider_GetHost"
       )
     else()
       target_link_options(onnxruntime_providers_qnn PRIVATE
