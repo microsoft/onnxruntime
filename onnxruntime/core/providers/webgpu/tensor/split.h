64de1135f4b2e2b89ee302499c9b338f49ab47d2
Jie Chen <jie.a.chen@intel.com>
2024-12-31 11:18:49 +0800

    Remove the duplicated validation

--- onnxruntime/core/providers/webgpu/tensor/split.h
+++ onnxruntime/core/providers/webgpu/tensor/split.h
@@ -26,42 +26,7 @@ class SplitProgram final : public Program<SplitProgram> {
 
 class Split : public WebGpuKernel, public SplitBase {
  public:
-  Split(const OpKernelInfo& info, uint32_t opset) : WebGpuKernel(info), SplitBase(info, opset) {
-    std::vector<int32_t> split_sizes;
-    // Check if split_sizes is provided as an attribute.
-    if (split_sizes_.size() > 0) {
-      ORT_ENFORCE(split_sizes_.size() == info.node().OutputDefs().size(), "Number of outputs (",
-                  info.node().OutputDefs().size(), ") does not match split_sizes (", split_sizes_.size(), ")");
-      split_sizes.resize(split_sizes_.size());
-      for (size_t i = 0; i < split_sizes_.size(); ++i) {
-        split_sizes[i] = gsl::narrow_cast<int32_t>(split_sizes_[i]);
-      }
-    } else if (info.GetInputCount() < 2) {
-      // No valid split_sizes is providede as an attribute or input tensor. In this case, we try to compute it from input, output shapes and
-      // num_outputs.
-
-      // Handle negative axis.
-      const auto num_dimensions = gsl::narrow_cast<int64_t>(info.node().InputDefs()[0]->Shape()->dim_size());
-      const auto axis = HandleNegativeAxis(axis_, num_dimensions);
-
-      auto total_split_size = info.node().InputDefs()[0]->Shape()->dim(gsl::narrow_cast<int32_t>(axis)).dim_value();
-      int64_t split_size_sum = 0;
-      if (num_outputs_ >= 0) {
-        ORT_ENFORCE(num_outputs_ == gsl::narrow_cast<int64_t>(info.node().OutputDefs().size()),
-                    "Invalid num_outputs value of ", num_outputs_, ". Size of dimension being split is ",
-                    info.node().OutputDefs().size());
-      }
-
-      // Compute split_sizes from the output shapes.
-      for (auto output : info.node().OutputDefs()) {
-        auto split_size = output->Shape()->dim(gsl::narrow_cast<int32_t>(axis)).dim_value();
-        split_sizes.push_back(gsl::narrow_cast<int32_t>(split_size));
-        split_size_sum += split_size;
-      }
-      ORT_ENFORCE(split_size_sum == total_split_size, "Sum of split sizes (", split_size_sum,
-                  ") does not match input size (", total_split_size, ")");
-    }
-  }
+  Split(const OpKernelInfo& info, uint32_t opset) : WebGpuKernel(info), SplitBase(info, opset) {}
 
  protected:
   Status ComputeInternal(ComputeContext& context) const override;
