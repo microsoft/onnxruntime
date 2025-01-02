7d703a3051b1fdbfeba08c69497a4d3849f45827
Changming Sun <chasun@microsoft.com>
2025-01-02 22:44:39 +0000

    update

--- cmake/external/cudnn_frontend.cmake
+++ cmake/external/cudnn_frontend.cmake
@@ -1,12 +1,12 @@
-include(FetchContent)
+
 FetchContent_Declare(
   cudnn_frontend
   URL ${DEP_URL_cudnn_frontend}
   URL_HASH SHA1=${DEP_SHA1_cudnn_frontend}
 )
 
-set(CUDNN_FRONTEND_BUILD_SAMPLES OFF)
-set(CUDNN_FRONTEND_BUILD_UNIT_TESTS OFF)
-set(CUDNN_FRONTEND_BUILD_PYTHON_BINDINGS OFF)
+set(CUDNN_FRONTEND_BUILD_SAMPLES OFF CACHE BOOL "" FORCE)
+set(CUDNN_FRONTEND_BUILD_TESTS OFF CACHE BOOL "" FORCE)
+set(CUDNN_FRONTEND_BUILD_PYTHON_BINDINGS OFF CACHE BOOL "" FORCE)
 set(CUDNN_PATH ${onnxruntime_CUDNN_HOME})
-FetchContent_MakeAvailable(cudnn_frontend)
+onnxruntime_fetchcontent_makeavailable(cudnn_frontend)
