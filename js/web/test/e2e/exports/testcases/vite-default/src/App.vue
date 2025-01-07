2f85ddc3ae15b74c7ad0b1062ac961ef554c3eff
Yulong Wang <7679871+fs-eire@users.noreply.github.com>
2025-01-06 22:06:36 -0800

    simplify vite-default

--- js/web/test/e2e/exports/testcases/vite-default/src/App.vue
+++ js/web/test/e2e/exports/testcases/vite-default/src/App.vue
@@ -3,14 +3,6 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
   <HelloWorld msg="Vite + Vue" />
 </template>
 
