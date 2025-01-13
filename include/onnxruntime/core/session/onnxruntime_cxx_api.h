7ca45523a3afc9adf1d19b6b0e514b95f09cf472
edgchen1 <18449977+edgchen1@users.noreply.github.com>
2025-01-13 11:49:10 -0800

    fix typo

--- include/onnxruntime/core/session/onnxruntime_cxx_api.h
+++ include/onnxruntime/core/session/onnxruntime_cxx_api.h
@@ -2133,7 +2133,7 @@ struct KernelContext {
   // If input is optional and is not present, the method returns an empty ConstValue
   // which can be compared to nullptr.
   ConstValue GetInput(size_t index) const;
-  // If outout is optional and is not present, the method returns an empty UnownedValue
+  // If output is optional and is not present, the method returns an empty UnownedValue
   // which can be compared to nullptr.
   UnownedValue GetOutput(size_t index, const int64_t* dim_values, size_t dim_count) const;
   UnownedValue GetOutput(size_t index, const std::vector<int64_t>& dims) const;
