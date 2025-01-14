e5e6fcef22d74cb7986df4ab3158eb643fc2008d
Yifan Li <109183385+yf711@users.noreply.github.com>
2025-01-14 14:27:34 -0800

    Update range of gpu arch (#23309)
    ### Description
    <!-- Describe your changes. -->
    * Remove deprecated gpu arch to control nuget/python package size
    (latest TRT supports sm75 Turing and newer arch)
    * Add 90 to support blackwell series in next release (86;89 not
    considered as adding them will rapidly increase package size)
    
    | arch_range | Python-cuda12 | Nuget-cuda12 |
    | -------------- |
    ------------------------------------------------------------ |
    ---------------------------------- |
    | 60;61;70;75;80 | Linux: 279MB Win: 267MB | Linux: 247MB Win: 235MB |
    | 75;80 | Linux: 174MB Win: 162MB | Linux: 168MB Win: 156MB |
    | **75;80;90** | **Linux: 299MB Win: 277MB** | **Linux: 294MB Win:
    271MB** |
    | 75;80;86;89 | [Linux: MB Win:
    390MB](https://aiinfra.visualstudio.com/Lotus/_build/results?buildId=647457&view=results)
    | Linux: 416MB Win: 383MB |
    | 75;80;86;89;90 | [Linux: MB Win:
    505MB](https://aiinfra.visualstudio.com/Lotus/_build/results?buildId=646536&view=results)
    | Linux: 541MB Win: 498MB |
    
    ### Motivation and Context
    <!-- - Why is this change required? What problem does it solve?
    - If it fixes an open issue, please link to the issue here. -->
    
    Callout: While adding sm90 support, the build of cuda11.8+cudnn8 will be
    dropped in the coming ORT release,
    as the build has issue with blackwell (mentioned in comments) and demand
    on cuda 11 is minor, according to internal ort-cuda11 repo.

--- tools/ci_build/github/linux/build_linux_python_package.sh
+++ tools/ci_build/github/linux/build_linux_python_package.sh
@@ -70,7 +70,7 @@ fi
 if [ "$BUILD_DEVICE" == "GPU" ]; then
     SHORT_CUDA_VERSION=$(echo $CUDA_VERSION | sed   's/\([[:digit:]]\+\.[[:digit:]]\+\)\.[[:digit:]]\+/\1/')
     #Enable CUDA and TRT EPs.
-    BUILD_ARGS+=("--use_cuda" "--use_tensorrt" "--cuda_version=$SHORT_CUDA_VERSION" "--tensorrt_home=/usr" "--cuda_home=/usr/local/cuda-$SHORT_CUDA_VERSION" "--cudnn_home=/usr/local/cuda-$SHORT_CUDA_VERSION" "--cmake_extra_defines" "CMAKE_CUDA_ARCHITECTURES=52;60;61;70;75;80")
+    BUILD_ARGS+=("--use_cuda" "--use_tensorrt" "--cuda_version=$SHORT_CUDA_VERSION" "--tensorrt_home=/usr" "--cuda_home=/usr/local/cuda-$SHORT_CUDA_VERSION" "--cudnn_home=/usr/local/cuda-$SHORT_CUDA_VERSION" "--cmake_extra_defines" "CMAKE_CUDA_ARCHITECTURES=75;80;90")
 fi
 
 if [ "$BUILD_DEVICE" == "NPU" ]; then
