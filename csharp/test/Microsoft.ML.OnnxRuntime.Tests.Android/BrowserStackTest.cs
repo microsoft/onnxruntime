eedad80fe94a1920eeb65735de8f4c6bd8d42ea4
carzh <wolfivyaura@gmail.com>
2025-01-15 12:33:56 -0800

    Working android MAUI app + project that runs android browserstack test

---
+++ csharp/test/Microsoft.ML.OnnxRuntime.Tests.Android/BrowserStackTest.cs
@@ -0,0 +1,74 @@
+﻿using Newtonsoft.Json;
+using NUnit.Framework.Interfaces;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Android;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.ML.OnnxRuntime.Tests.Android
+{
+    public class BrowserStackTest
+    {
+        public AndroidDriver driver;
+        public BrowserStackTest() { }
+
+        [SetUp]
+        public void Init()
+        {
+            var androidOptions = new AppiumOptions
+            {
+                AutomationName = "UIAutomator2",
+                PlatformName = "Android",
+            };
+
+            driver = new AndroidDriver(new Uri("http://127.0.0.1:4723/wd/hub"), androidOptions);
+        }
+
+        /// <summary>
+        /// Sends a log to BrowserStack that is visible in the text logs and labelled as ANNOTATION.
+        /// </summary>
+        /// <param name="text">Log text to send.</param>
+        /// <param name="logLevel">Log level -- choose between info, debug, warning, and error</param>
+        public void browserStackLog(String text, String logLevel = "info")
+        {
+            String jsonToSend = String.Format("browserstack_executor: {\"action\": \"annotate\", \"arguments\": {\"data\": {0}, \"level\": {1}}}", JsonConvert.ToString(text), JsonConvert.ToString(logLevel));
+
+            ((IJavaScriptExecutor)driver).ExecuteScript(jsonToSend);
+        }
+
+        /// <summary>
+        /// Passes the correct test status to BrowserStack and ensures the driver quits.
+        /// </summary>
+        [TearDown]
+        public void Dispose()
+        {
+            try
+            {
+                // According to https://www.browserstack.com/docs/app-automate/appium/set-up-tests/mark-tests-as-pass-fail
+                // BrowserStack doesn't know whether test assertions have passed or failed. Below handles
+                // passing the test status to BrowserStack along with any relevant information.
+                if (TestContext.CurrentContext.Result.Outcome.Status == TestStatus.Failed)
+                {
+                    String failureMessage = TestContext.CurrentContext.Result.Message;
+                    String jsonToSendFailure = String.Format("browserstack_executor: {\"action\": \"setSessionStatus\", \"arguments\": {\"status\":\"failed\", \"reason\": {0}}}", JsonConvert.ToString(failureMessage));
+
+                    ((IJavaScriptExecutor)driver).ExecuteScript(jsonToSendFailure);
+                }
+                else
+                {
+                    ((IJavaScriptExecutor)driver).ExecuteScript("browserstack_executor: {\"action\": \"setSessionStatus\", \"arguments\": {\"status\":\"passed\", \"reason\": \"\"}}");
+                }
+            }
+            finally
+            {
+                // will run even if exception is thrown by previous block
+                ((AndroidDriver)driver).Quit();
+            }
+        }
+    }
+}
---
+++ csharp/test/Microsoft.ML.OnnxRuntime.Tests.Android/RunAllTest.cs
@@ -0,0 +1,107 @@
+﻿using OpenQA.Selenium.Appium;
+using OpenQA.Selenium;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.ML.OnnxRuntime.Tests.Android
+{
+    [TestFixture]
+    public class RunAllTest : BrowserStackTest
+    {
+        public AppiumElement FindAppiumElement(String xpathQuery, String text)
+        {
+            IReadOnlyCollection<AppiumElement> appiumElements = driver.FindElements(By.XPath(xpathQuery));
+
+            foreach (var element in appiumElements)
+            {
+                if (element.Text.Contains(text))
+                {
+                    return element;
+                }
+            }
+            // was unable to find given element
+            throw new Exception(String.Format("Could not find {0}: {1} on the page.", xpathQuery, text));
+        }
+
+        public AppiumElement FindAppiumElementThenClick(String xpathQuery, String text)
+        {
+            AppiumElement appiumElement = FindAppiumElement(xpathQuery, text);
+            appiumElement.Click();
+            return appiumElement;
+        }
+
+        public (int, int) GetPassFailCount()
+        {
+            int numPassed = -1;
+            int numFailed = -1;
+
+            IReadOnlyCollection<AppiumElement> labelElements = driver.FindElements(By.XPath("//android.widget.TextView"));
+
+            for (int i = 0; i < labelElements.Count; i++)
+            {
+                AppiumElement element = labelElements.ElementAt(i);
+
+                if (element.Text.Equals("✔"))
+                {
+                    i++;
+                    numPassed = int.Parse(labelElements.ElementAt(i).Text);
+                }
+
+                if (element.Text.Equals("⛔"))
+                {
+                    i++;
+                    numFailed = int.Parse(labelElements.ElementAt(i).Text);
+                    break;
+                }
+            }
+
+            Assert.That(numPassed, Is.GreaterThanOrEqualTo(0), "Could not find number passed label.");
+            Assert.That(numFailed, Is.GreaterThanOrEqualTo(0), "Could not find number failed label.");
+
+            return (numPassed, numFailed);
+        }
+
+        [Test]
+        public async Task ClickRunAllTest()
+        {
+            AppiumElement runAllButton = FindAppiumElementThenClick("//android.widget.Button", "Run All");
+            
+            while (!runAllButton.Enabled)
+            {
+                // waiting for unit tests to execute
+                await Task.Delay(500);
+            }
+
+            var (numPassed, numFailed) = GetPassFailCount();
+
+            if (numFailed == 0)
+            {
+                Assert.Pass();
+                return;
+            }
+
+            // click into test results if tests have failed
+            FindAppiumElementThenClick("//android.widget.TextView", "⛔");
+            await Task.Delay(500);
+            FindAppiumElementThenClick("//android.widget.EditText", "All");
+            await Task.Delay(100);
+            FindAppiumElementThenClick("//android.widget.TextView", "Failed");
+            await Task.Delay(500);
+
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("PASSED TESTS: " + numPassed + " | FAILED TESTS: " + numFailed);
+
+            IReadOnlyCollection<AppiumElement> textResults = driver.FindElements(By.XPath("//android.widget.TextView"));
+            foreach (var element in textResults)
+            {
+                sb.AppendLine(element.Text);
+            }
+
+            Assert.That(numFailed, Is.EqualTo(0), sb.ToString());
+        }
+    }
+}
