8b7f688b75ab22cbb6d89a698988c49182664081
Tianlei Wu <tlwu@microsoft.com>
2025-01-16 12:54:46 -0800

    Dump nodes with potential overflow in half conversion  (#23363)
    Add a tool to generate node_block_list used in [float16 conversion tool](https://github.com/microsoft/onnxruntime/blob/04030f64be10e020d3ac9aa5ba7d0f2917cbd14e/onnxruntime/python/tools/transformers/float16.py#L175).
    
    Previously, we have a feature to dump statistics data (like min, max) of
    each node input/output. However, it is time consuming to generate a list
    of nodes that need to be kept in float32 when model is large.
    
    This could help speed up the process by outputting a list of nodes that
    have potential overflow in float-to-half conversion.
    
    Usage is to build onnxruntime from source with ` --cmake_extra_defines
    onnxruntime_DEBUG_NODE_INPUTS_OUTPUTS=1`, then set some environment
    variables before running float32 optimized onnx model like:
    ```
    export ORT_DEBUG_NODE_IO_DUMP_HALF_CONVERSION_OVERFLOW=1
    export ORT_DEBUG_NODE_IO_HALF_OVERFLOW_THRESHOLD=50000
    
    python benchmark.py -e optimum --height 1024 --width 1024 --steps 3 -b 1 -v Flux.1D -p flux1_dev_onnx/fp32_opt --skip_warmup
    ```
    
    The threshold `ORT_DEBUG_NODE_IO_HALF_OVERFLOW_THRESHOLD` shall be <=
    65504. The default value is 50000 if the environment variable is not
    set. It is better to leave some margin if number of samples are not
    large enough in the test.
    
    As a demo, we add an option --skip_warmup to benchmark.py for Flux, so
    that we can reduce the time on dumping warm-up runs.
    
    Example snippet of stdout (each inference session has such a summary
    when session ended):
    ```
    Total counter in node dumping: 141
    Found 2 nodes cannot be converted to half precision due to potential input/output overflow.
    Operator frequencies for these nodes:
    Softmax : 1
    MatMul : 1
    # -------
    # Example python script for float16 conversion
    # For details, search `node_block_list` in https://github.com/microsoft/onnxruntime/blob/main/onnxruntime/python/tools/transformers/float16.py
    # -------
    from onnxruntime.transformers.onnx_model import OnnxModel
    m = OnnxModel(onnx.load('flux1_dev_onnx/fp32_opt/vae_decoder/model.onnx'))
    node_block_list = [
      '/decoder/mid_block/attentions.0/Softmax',
      '/decoder/mid_block/attentions.0/MatMul',
    ]
    m.convert_float_to_float16(keep_io_types=False, node_block_list=node_block_list)
    m.save_model_to_file('fp16/optimized.onnx', use_external_data_format=False)
    ```
    Then you can use the python script to convert corresponding model to
    float16.
    
    ### Motivation and Context
    
    It is a tool used to generate node_block_list used in float16 conversion
    of stable diffusion 3.x and flux models in
    https://github.com/microsoft/onnxruntime/pull/22986.
    
    In stable diffusion or Flux pipeline, there are multiple models and
    there could be multiple session runs for each model. Without a proper
    tool, it is time consuming to get node_block_list for each model.

--- onnxruntime/core/framework/debug_node_inputs_outputs_utils.h
+++ onnxruntime/core/framework/debug_node_inputs_outputs_utils.h
@@ -19,6 +19,9 @@
 #include "core/framework/op_kernel.h"
 #include "core/framework/session_state.h"
 #include "core/graph/graph.h"
+#include <unordered_set>
+#include <mutex>
+#include <string>
 
 namespace onnxruntime {
 namespace utils {
@@ -37,6 +40,8 @@ constexpr const char* kDumpInputData = "ORT_DEBUG_NODE_IO_DUMP_INPUT_DATA";
 constexpr const char* kDumpOutputData = "ORT_DEBUG_NODE_IO_DUMP_OUTPUT_DATA";
 // Output statistics data like min, max, count of NaN, count of infinity etc.
 constexpr const char* kDumpStatisticsData = "ORT_DEBUG_NODE_IO_DUMP_STATISTICS_DATA";
+// Output node name when any float input or output exceeds a threshold for float16 conversion overflow.
+constexpr const char* kDumpHalfConversionOverflow = "ORT_DEBUG_NODE_IO_DUMP_HALF_CONVERSION_OVERFLOW";
 
 // specify a node name filter to limit the nodes that are dumped
 // see NodeDumpOptions::FilterOptions
@@ -61,6 +66,10 @@ constexpr const char* kSnippetThreshold = "ORT_DEBUG_NODE_IO_SNIPPET_THRESHOLD";
 // Number of array items in snippet at beginning and end of each dimension (default 3)
 constexpr const char* kSnippetEdgeItems = "ORT_DEBUG_NODE_IO_SNIPPET_EDGE_ITEMS";
 
+// Threshold for float to float16 conversion overflow detection (default 50000).
+// It is a positive integer that <= 65504, and it is recommended to add some margin for new inputs.
+constexpr const char* kHalfOverflowThreshold = "ORT_DEBUG_NODE_IO_HALF_OVERFLOW_THRESHOLD";
+
 }  // namespace debug_node_inputs_outputs_env_vars
 
 constexpr char kFilterPatternDelimiter = ';';
@@ -73,7 +82,8 @@ struct NodeDumpOptions {
     OutputData = 1 << 2,
     NodePlacement = 1 << 3,
     StatisticsData = 1 << 4,
-    AllData = Shape | InputData | OutputData | NodePlacement | StatisticsData,
+    HalfConversionOverflow = 1 << 5,
+    AllData = Shape | InputData | OutputData | NodePlacement | StatisticsData | HalfConversionOverflow,
   };
 
   // specifies the information to dump per node
@@ -117,6 +127,9 @@ struct NodeDumpOptions {
 
   // Number of array items in snippet at beginning and end of each dimension for Stdout.
   int snippet_edge_items;
+
+  // Threshold for float16 conversion overflow.
+  float half_overflow_threshold;
 };
 
 struct NodeDumpContext {
@@ -124,6 +137,16 @@ struct NodeDumpContext {
   size_t iteration;
   // which node are we on?
   size_t program_counter;
+};
+
+// A session level analysis of node dumps. It can be used to collect some statistics or analysis during node dumps.
+struct NodeDumpAnalysis {
+  std::mutex set_mutex;
+  std::unordered_set<std::string> half_overflow_nodes;
+  std::unordered_map<std::string, int> half_overflow_ops;
+  int counter{0};
+  void Add(const std::string& node_name, const std::string& op_name, bool is_half_overflow);
+  void PrintToStdOut(const std::string& model_path);
 };
 
 // gets NodeDumpOptions instance configured from environment variable values
@@ -135,13 +158,15 @@ void DumpNodeInputs(
     const NodeDumpContext& dump_context,
     const OpKernelContext& context,
     const Node& node,
-    const SessionState& session_state);
+    const SessionState& session_state,
+    NodeDumpAnalysis& dump_analysis);
 
 void DumpNodeInputs(
     const NodeDumpContext& dump_context,
     const OpKernelContext& context,
     const Node& node,
-    const SessionState& session_state);
+    const SessionState& session_state,
+    NodeDumpAnalysis& dump_analysis);
 
 // dumps outputs for a node
 void DumpNodeOutputs(
@@ -149,13 +174,15 @@ void DumpNodeOutputs(
     const NodeDumpContext& dump_context,
     OpKernelContext& context,
     const Node& node,
-    const SessionState& session_state);
+    const SessionState& session_state,
+    NodeDumpAnalysis& dump_analysis);
 
 void DumpNodeOutputs(
     const NodeDumpContext& dump_context,
     OpKernelContext& context,
     const Node& node,
-    const SessionState& session_state);
+    const SessionState& session_state,
+    NodeDumpAnalysis& dump_analysis);
 
 }  // namespace utils
 }  // namespace onnxruntime
--- onnxruntime/core/framework/print_tensor_statistics_utils.h
+++ onnxruntime/core/framework/print_tensor_statistics_utils.h
@@ -8,6 +8,14 @@
 namespace onnxruntime {
 namespace utils {
 
+// Currently we only store statistics data for float tensors that printed to stdout.
+// It can be extended to other types if needed.
+struct TensorStatisticsData {
+  bool is_float = false;
+  float float_min;
+  float float_max;
+};
+
 template <typename T>
 int my_fpclassify(const T& val) {
   return std::fpclassify(val);
@@ -30,7 +38,7 @@ void PrintFloatStats(const T* data, size_t count) {
   size_t zero = 0;
   size_t subnormal = 0;
   for (size_t i = 0; i < count; i++) {
-    switch (my_fpclassify(*data)) {
+    switch (my_fpclassify(data[i])) {
       case FP_INFINITE:
         inf++;
         break;
@@ -59,7 +67,7 @@ void PrintFloatStats(const T* data, size_t count) {
 }
 
 template <typename T>
-void PrintCommonStats(const T* data, size_t count) {
+void PrintCommonStats(const T* data, size_t count, TensorStatisticsData& tensor_statistics) {
   T min = data[0];
   T max = min;
   for (size_t i = 1; i < count; i++) {
@@ -77,30 +85,38 @@ void PrintCommonStats(const T* data, size_t count) {
 
   std::cout << ",Max=";
   PrintValue(max);
+
+  // Statistics for float and double only for now.
+  if constexpr (std::is_same<T, float>::value) {
+    tensor_statistics.is_float = true;
+    tensor_statistics.float_min = static_cast<double>(min);
+    tensor_statistics.float_max = static_cast<double>(max);
+  }
 }
 
-#define DEF_PRINT_COMMON_STATS_INT4(INT4_TYPE)                                   \
-  template <>                                                                    \
-  inline void PrintCommonStats<INT4_TYPE>(const INT4_TYPE* data, size_t count) { \
-    using UnpackedType = typename INT4_TYPE::UnpackedType;                       \
-    UnpackedType min = data[0].GetElem(0);                                       \
-    UnpackedType max = min;                                                      \
-    for (size_t i = 1; i < count; i++) {                                         \
-      auto indices = INT4_TYPE::GetTensorElemIndices(i);                         \
-      auto value = data[indices.first].GetElem(indices.second);                  \
-      if (value > max) {                                                         \
-        max = value;                                                             \
-      }                                                                          \
-      if (value < min) {                                                         \
-        min = value;                                                             \
-      }                                                                          \
-    }                                                                            \
-                                                                                 \
-    std::cout << "Min=";                                                         \
-    PrintValue(min);                                                             \
-                                                                                 \
-    std::cout << ",Max=";                                                        \
-    PrintValue(max);                                                             \
+#define DEF_PRINT_COMMON_STATS_INT4(INT4_TYPE)                      \
+  template <>                                                       \
+  inline void PrintCommonStats<INT4_TYPE>(                          \
+      const INT4_TYPE* data, size_t count, TensorStatisticsData&) { \
+    using UnpackedType = typename INT4_TYPE::UnpackedType;          \
+    UnpackedType min = data[0].GetElem(0);                          \
+    UnpackedType max = min;                                         \
+    for (size_t i = 1; i < count; i++) {                            \
+      auto indices = INT4_TYPE::GetTensorElemIndices(i);            \
+      auto value = data[indices.first].GetElem(indices.second);     \
+      if (value > max) {                                            \
+        max = value;                                                \
+      }                                                             \
+      if (value < min) {                                            \
+        min = value;                                                \
+      }                                                             \
+    }                                                               \
+                                                                    \
+    std::cout << "Min=";                                            \
+    PrintValue(min);                                                \
+                                                                    \
+    std::cout << ",Max=";                                           \
+    PrintValue(max);                                                \
   }
 
 DEF_PRINT_COMMON_STATS_INT4(Int4x2)
@@ -129,36 +145,36 @@ void PrintHalfStats(const T* data, size_t count) {
 }
 
 template <typename T>
-void PrintTensorStats(const T* tensor, size_t count) {
-  PrintCommonStats<T>(tensor, count);
+void PrintTensorStats(const T* tensor, size_t count, TensorStatisticsData& tensor_statistics) {
+  PrintCommonStats<T>(tensor, count, tensor_statistics);
 }
 
 template <>
-void PrintTensorStats<float>(const float* tensor, size_t count) {
-  PrintCommonStats<float>(tensor, count);
+void PrintTensorStats<float>(const float* tensor, size_t count, TensorStatisticsData& tensor_statistics) {
+  PrintCommonStats<float>(tensor, count, tensor_statistics);
   PrintFloatStats<float>(tensor, count);
 }
 
 template <>
-void PrintTensorStats<double>(const double* tensor, size_t count) {
-  PrintCommonStats<double>(tensor, count);
+void PrintTensorStats<double>(const double* tensor, size_t count, TensorStatisticsData& tensor_statistics) {
+  PrintCommonStats<double>(tensor, count, tensor_statistics);
   PrintFloatStats<double>(tensor, count);
 }
 
 template <>
-void PrintTensorStats<MLFloat16>(const MLFloat16* tensor, size_t count) {
+void PrintTensorStats<MLFloat16>(const MLFloat16* tensor, size_t count, TensorStatisticsData&) {
   PrintHalfStats<MLFloat16>(tensor, count);
   PrintFloatStats<MLFloat16>(tensor, count);
 }
 
 template <>
-void PrintTensorStats<BFloat16>(const BFloat16* tensor, size_t count) {
+void PrintTensorStats<BFloat16>(const BFloat16* tensor, size_t count, TensorStatisticsData&) {
   PrintHalfStats<BFloat16>(tensor, count);
   PrintFloatStats<BFloat16>(tensor, count);
 }
 
 template <typename T>
-void PrintCpuTensorStats(const Tensor& tensor) {
+void PrintCpuTensorStats(const Tensor& tensor, TensorStatisticsData& tensor_statistics) {
   const auto& shape = tensor.Shape();
   auto num_items = shape.Size();
   if (num_items == 0) {
@@ -166,12 +182,12 @@ void PrintCpuTensorStats(const Tensor& tensor) {
   }
 
   const T* data = tensor.Data<T>();
-  PrintTensorStats<T>(data, num_items);
+  PrintTensorStats<T>(data, num_items, tensor_statistics);
   std::cout << std::endl;
 }
 
 template <>
-void PrintCpuTensorStats<std::string>(const Tensor&) {
+void PrintCpuTensorStats<std::string>(const Tensor&, TensorStatisticsData&) {
 }
 
 }  // namespace utils
