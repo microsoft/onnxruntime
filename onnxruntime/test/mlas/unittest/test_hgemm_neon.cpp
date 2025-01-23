5d16333fb4dccda7feb76f00546f6ece8e200d20
Jing Fang <fajin@microsoft.com>
2025-01-23 23:05:33 +0000

    added mlas hgemm ut

--- onnxruntime/test/mlas/unittest/test_hgemm_neon.cpp
+++ onnxruntime/test/mlas/unittest/test_hgemm_neon.cpp
@@ -25,6 +25,7 @@ Abstract:
 
 class MlasNeonHGemmPackBTest : public MlasTestBase {
  private:
+  std::random_device rd_;
   unsigned int seed_;
   std::mt19937 gen_;  // mersenne_twister_engine seeded with rd()
   std::uniform_real_distribution<float> distrib_;
@@ -87,7 +88,7 @@ class MlasNeonHGemmPackBTest : public MlasTestBase {
 
  public:
   MlasNeonHGemmPackBTest()
-    : seed_(std::rand()), gen_(seed_), distrib_(-100.f, 100.f) {
+    : seed_(rd_()), gen_(seed_), distrib_(-100.f, 100.f) {
   }
 
   static const char* GetTestSuiteName() {
@@ -111,6 +112,7 @@ class MlasNeonHGemmPackBTest : public MlasTestBase {
 
 class MlasNeonHGemmTransposedBTest : public MlasTestBase {
  private:
+  std::random_device rd_;
   unsigned int seed_;
   std::mt19937 gen_;  // mersenne_twister_engine seeded with rd()
   std::uniform_real_distribution<float> distrib_;
@@ -167,7 +169,7 @@ class MlasNeonHGemmTransposedBTest : public MlasTestBase {
 
  public:
   MlasNeonHGemmTransposedBTest()
-    : seed_(192872), gen_(seed_), distrib_(-1.f, 1.f) {
+    : seed_(1928375), gen_(seed_), distrib_(-1.f, 1.f) {
   }
 
   static const char* GetTestSuiteName() {
@@ -191,6 +193,7 @@ class MlasNeonHGemmTransposedBTest : public MlasTestBase {
 
 class MlasNeonHGemmTransposedPackedBTest : public MlasTestBase {
  private:
+  std::random_device rd_;
   unsigned int seed_;
   std::mt19937 gen_;  // mersenne_twister_engine seeded with rd()
   std::uniform_real_distribution<float> distrib_;
@@ -273,7 +276,7 @@ class MlasNeonHGemmTransposedPackedBTest : public MlasTestBase {
 
  public:
   MlasNeonHGemmTransposedPackedBTest()
-    : seed_(1928378), gen_(seed_), distrib_(-1.f, 1.f) {
+    : seed_(1928372), gen_(seed_), distrib_(-1.f, 1.f) {
   }
 
   static const char* GetTestSuiteName() {
@@ -295,7 +298,86 @@ class MlasNeonHGemmTransposedPackedBTest : public MlasTestBase {
   }
 };
 
-// TODO: add mlas call UT
+class MlasNeonHGemmTest : public MlasTestBase {
+ private:
+  std::random_device rd_;
+  unsigned int seed_;
+  std::mt19937 gen_;  // mersenne_twister_engine seeded with rd()
+  std::uniform_real_distribution<float> distrib_;
+  MatrixGuardBuffer<MLAS_FP16> A_, B_, ref_, C_;
+
+  template <size_t M, size_t K, size_t N>
+  MLAS_FORCEINLINE void HGemm(const MLAS_FP16* A, const MLAS_FP16* B, MLAS_FP16* C, MLAS_FP16 alpha, MLAS_FP16 beta) {
+    float alphaf = alpha.ToFloat();
+    float betaf = beta.ToFloat();
+    for (size_t i = 0; i < M; ++i) {
+      for (size_t j = 0; j < N; ++j) {
+        float accu = 0.0f;
+        for (size_t k = 0; k < K; ++k) {
+          accu += (A[i * K + k].ToFloat()) * (B[j * K + k].ToFloat());
+        }
+        C[i * N + j] = MLAS_FP16(accu * alphaf + C[i * N + j].ToFloat() * betaf);
+      }
+    }
+  }
+
+  MLAS_FORCEINLINE
+  bool FloatEqual(MLAS_FP16 v0, MLAS_FP16 v1, float rtol, float atol) {
+    float f0 = v0.ToFloat(), f1 = v1.ToFloat();
+    return std::abs(f0 - f1) <= std::abs(f1 * rtol) + atol;
+  }
+
+  template <size_t M, size_t K, size_t N>
+  MLAS_FORCEINLINE void Check(const MLAS_FP16* C, const MLAS_FP16* ref) {
+    for (size_t i = 0; i < M; ++i) {
+      for (size_t j = 0; j < N; ++j) {
+        ASSERT_TRUE(FloatEqual(C[i * N + j], ref[i * N + j], 0.02f, 0.055f))
+          << " seed " << seed_ << " i " << i << " j " << j
+          << " M " << M << " K " << K << " N " << N
+          << " v0 " << C[i * N + j] << " v1 " << ref[i * N + j];
+      }
+    }
+  }
+
+  template <size_t M, size_t K, size_t N>
+  void TestHGemm(MLAS_FP16 alpha, MLAS_FP16 beta) {
+    auto InitializeBuffer = [this](MLAS_FP16* buffer, size_t count) {
+      for (size_t i = 0; i < count; i++) {
+        buffer[i] = MLAS_FP16(distrib_(gen_));
+      }
+    };
+
+    const auto* A = A_.GetFilledBuffer(M * K, InitializeBuffer);
+    const auto* B = B_.GetFilledBuffer(K * N, InitializeBuffer);
+    auto* C = C_.GetBuffer(M * N, true);
+    auto* ref = ref_.GetBuffer(M * N, true);
+    MlasGemm(CblasNoTrans, CblasTrans, M, N, K, A, K, B, K, C, N, alpha.val, beta.val, nullptr);
+    HGemm<M, K, N>(A, B, ref, alpha, beta);
+    Check<M, K, N>(C, ref);
+  }
+
+ public:
+  MlasNeonHGemmTest()
+    : seed_(192837), gen_(seed_), distrib_(-0.25f, 0.25f) {
+  }
+
+  static const char* GetTestSuiteName() {
+    return "NeonHGemm";
+  }
+
+  void ExecuteShort(void) override {
+    TestHGemm<2, 1, 1>(MLAS_FP16(1.0f), MLAS_FP16(0.0f));
+    TestHGemm<1, 128, 512>(MLAS_FP16(0.5f), MLAS_FP16(1.0f));
+    TestHGemm<2, 128, 513>(MLAS_FP16(1.5f), MLAS_FP16(0.5f));
+    TestHGemm<1, 128, 511>(MLAS_FP16(1.0f), MLAS_FP16(0.0f));
+    TestHGemm<2, 129, 512>(MLAS_FP16(0.5f), MLAS_FP16(1.0f));
+    TestHGemm<1, 127, 512>(MLAS_FP16(1.5f), MLAS_FP16(0.5f));
+    TestHGemm<1, 513, 1023>(MLAS_FP16(0.5f), MLAS_FP16(1.0f));
+    TestHGemm<2, 511, 1025>(MLAS_FP16(1.5f), MLAS_FP16(0.5f));
+    TestHGemm<127, 513, 1023>(MLAS_FP16(1.0f), MLAS_FP16(0.0f));
+    TestHGemm<129, 511, 1025>(MLAS_FP16(0.5f), MLAS_FP16(1.0f));
+  }
+};
 
 static UNUSED_VARIABLE bool added_to_main = AddTestRegister([](bool is_short_execute) {
   size_t count = 0;
@@ -303,6 +385,7 @@ static UNUSED_VARIABLE bool added_to_main = AddTestRegister([](bool is_short_exe
     count += MlasDirectShortExecuteTests<MlasNeonHGemmPackBTest>::RegisterShortExecute();
     count += MlasDirectShortExecuteTests<MlasNeonHGemmTransposedBTest>::RegisterShortExecute();
     count += MlasDirectShortExecuteTests<MlasNeonHGemmTransposedPackedBTest>::RegisterShortExecute();
+    count += MlasDirectShortExecuteTests<MlasNeonHGemmTest>::RegisterShortExecute();
   }
   return count;
 });
