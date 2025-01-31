5c5e7f45ea19b1264140c491dc78e0022f2fad3e
Changming Sun <chasun@microsoft.com>
2025-01-31 13:05:58 -0800

    update

--- cmake/external/pybind11.cmake
+++ cmake/external/pybind11.cmake
@@ -5,7 +5,7 @@ onnxruntime_fetchcontent_declare(
     pybind11_project
     URL ${DEP_URL_pybind11}
     URL_HASH SHA1=${DEP_SHA1_pybind11}
-    FIND_PACKAGE_ARGS 2.6 NAMES pybind11
+    FIND_PACKAGE_ARGS 2.13 NAMES pybind11
     EXCLUDE_FROM_ALL
 )
 onnxruntime_fetchcontent_makeavailable(pybind11_project)
