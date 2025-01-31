c8ff2f175869ac2ea93e66a2bb3118cd41238c4d
kunal-vaishnavi <115581922+kunal-vaishnavi@users.noreply.github.com>
2025-01-31 09:17:49 -0800

    Add fusions for SigLIP and Conformer-Encoder (#23528)
    ### Description
    This PR adds fusions for [Google's SigLIP
    model](https://huggingface.co/google/siglip-base-patch16-224/) and
    Microsoft's internal conformer-encoder model.
    
    Here is an example of how to run the ORT transformer optimizer for the
    SigLIP model.
    ```
    $ git clone https://github.com/microsoft/onnxruntime
    $ cd onnxruntime/onnxruntime/python/tools/transformers
    $ python3 optimizer.py --input /path/to/model.onnx --output /path/to/model_opt.onnx --model_type clip --num_heads 16 --hidden_size 1152 --use_external_data_format --opt_level 0 --disable_shape_inference
    ```
    
    Here is an example of how to run the ORT transformer optimizer for the
    conformer-encoder model.
    ```
    $ git clone https://github.com/microsoft/onnxruntime
    $ cd onnxruntime/onnxruntime/python/tools/transformers
    $ python3 optimizer.py --input /path/to/model.onnx --output /path/to/model_opt.onnx --model_type conformer --num_heads 16 --hidden_size 1024 --use_external_data_format --opt_level 0 --disable_shape_inference --convert_attribute
    ```
    
    ### Motivation and Context
    This PR helps optimize multi-modal models that use SigLIP for the vision
    encoder and conformer-encoder for the speech encoder.
    
    This PR uses changes from the following PRs:
    - https://github.com/pytorch/pytorch/pull/144801
    - https://github.com/microsoft/onnxscript/pull/2018
    - https://github.com/microsoft/onnxscript/pull/2019
    - https://github.com/microsoft/onnxscript/pull/2020
    - https://github.com/microsoft/onnxscript/pull/2021
    - https://github.com/microsoft/onnxscript/pull/2022
    - https://github.com/microsoft/onnxscript/pull/2024
    - https://github.com/microsoft/onnxscript/pull/2025
    - https://github.com/microsoft/onnxscript/pull/2029
    - https://github.com/microsoft/onnxscript/pull/2033
    
    ### Introduction of ONNX Script
    
    This PR introduces [ONNX
    Script](https://github.com/microsoft/onnxscript) into the ORT
    transformer optimizer as an optional step via the
    `fold_transpose_initializers()` method of the `DynamoOnnxHelper` class.

--- onnxruntime/python/tools/transformers/fusion_fastgelu.py
+++ onnxruntime/python/tools/transformers/fusion_fastgelu.py
@@ -363,7 +363,9 @@ class FusionFastGelu(Fusion):
 
     def fuse_4(self, tanh_node, input_name_to_nodes: dict, output_name_to_node: dict) -> bool | None:
         """
-        This pattern is from stable diffusion 3.5 model.
+        PyTorch's gelu implementation with tanh approximation:
+           Gelu(x) = 0.5 * x * (1 + torch.tanh(0.7978845834732056 * (x + 0.044714998453855515 * x * x * x)))
+
         Fuse Gelu with tanh into one node:
               +-----------------+------------------+
               |                 |                  |
@@ -398,6 +400,7 @@ class FusionFastGelu(Fusion):
         if len(children) != 1 or children[0].op_type != "Mul":
             return
         mul_half = children[0]
+
         if not self.model.has_constant_input(mul_half, 0.5):
             return
 
@@ -407,11 +410,11 @@ class FusionFastGelu(Fusion):
         if mul_before_tanh is None:
             return
 
-        i = self.model.find_constant_input(mul_before_tanh, 0.7978, delta=0.0001)
-        if i < 0:
+        k = self.model.find_constant_input(mul_before_tanh, 0.7978, delta=0.01)
+        if k < 0:
             return
 
-        add_before_tanh = self.model.match_parent(mul_before_tanh, "Add", 0 if i == 1 else 1, output_name_to_node)
+        add_before_tanh = self.model.match_parent(mul_before_tanh, "Add", 0 if k == 1 else 1, output_name_to_node)
         if add_before_tanh is None:
             return
 
@@ -426,31 +429,31 @@ class FusionFastGelu(Fusion):
         if mul_after_pow is None:
             return
 
-        i = self.model.find_constant_input(mul_after_pow, 0.0447, delta=0.0001)
-        if i < 0:
+        m = self.model.find_constant_input(mul_after_pow, 0.0447, delta=0.01)
+        if m < 0:
             return
 
-        mul = self.model.match_parent(mul_after_pow, "Mul", 0 if i == 1 else 1, output_name_to_node)
-        if mul is None:
+        mul_cubed = self.model.match_parent(mul_after_pow, "Mul", 0 if m == 1 else 1, output_name_to_node)
+        if mul_cubed is None:
             return
 
-        if mul.input[0] == root_input:
+        if mul_cubed.input[0] == root_input:
             another = 1
-        elif mul.input[1] == root_input:
+        elif mul_cubed.input[1] == root_input:
             another = 0
         else:
             return
 
-        mul2 = self.model.match_parent(mul, "Mul", another, output_name_to_node)
-        if mul2 is None:
+        mul_squared = self.model.match_parent(mul_cubed, "Mul", another, output_name_to_node)
+        if mul_squared is None:
             return
 
-        if mul2.input[0] != root_input or mul2.input[1] != root_input:
+        if mul_squared.input[0] != root_input or mul_squared.input[1] != root_input:
             return
 
         subgraph_nodes = [
-            mul2,
-            mul,
+            mul_squared,
+            mul_cubed,
             mul_after_pow,
             add_before_tanh,
             mul_before_tanh,
@@ -478,4 +481,5 @@ class FusionFastGelu(Fusion):
         fused_node.domain = "com.microsoft"
         self.nodes_to_add.append(fused_node)
         self.node_name_to_graph_name[fused_node.name] = self.this_graph_name
+        self.increase_counter("FastGelu")
         return True
