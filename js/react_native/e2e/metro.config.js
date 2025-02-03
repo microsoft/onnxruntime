38c4d85537062c5f7cb99310c1d8cc1e4279eb28
Jian Chen <cjian@microsoft.com>
2025-02-02 17:57:06 -0800

    Update metro.config.js

--- js/react_native/e2e/metro.config.js
+++ js/react_native/e2e/metro.config.js
@@ -1,3 +1,4 @@
+const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
 /**
  * Metro configuration
  * https://facebook.github.io/metro/docs/configuration
@@ -5,5 +6,4 @@
  * @type {import('metro-config').MetroConfig}
  */
 const config = {};
-
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
