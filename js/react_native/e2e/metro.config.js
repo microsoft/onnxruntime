1c45f9d69683bef3f69d85641675b3c5e6cc0002
Jian Chen <cjian@microsoft.com>
2025-02-02 19:35:07 -0800

    Update android projects

--- js/react_native/e2e/metro.config.js
+++ js/react_native/e2e/metro.config.js
@@ -1,9 +1,17 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config')
+const path = require('path');
 /**
  * Metro configuration
  * https://facebook.github.io/metro/docs/configuration
  *
  * @type {import('metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+  watchFolders: [
+    path.resolve(__dirname, '..'), // Ensure Metro watches the lib folder
+  ],
+  resolver: {
+    sourceExts: ['tsx', 'ts', 'jsx', 'js','json'], // Ensure TypeScript files are recognized
+  },
+};
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
