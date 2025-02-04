e2fdb22483e6b7525d265be50cfa0b27d1854003
Jian Chen <cjian@microsoft.com>
2025-02-03 18:17:02 -0800

    Testing RN-73

--- js/react_native/babel.config.js
+++ js/react_native/babel.config.js
@@ -1,5 +1,5 @@
 'use strict';
 
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+  presets: ['module:@react-native/babel-preset'],
 };
--- js/react_native/e2e/babel.config.js
+++ js/react_native/e2e/babel.config.js
@@ -4,7 +4,7 @@ const path = require('path');
 const pak = require('../package.json');
 
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+  presets: ['module:@react-native/babel-preset'],
   plugins: [
     [
       'module-resolver',
