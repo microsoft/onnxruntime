59e276091a7899cd76aa2f3d30694d73e1862bc4
liqunfu <liqun.fu@microsoft.com>
2025-02-06 01:52:25 +0000

    from patch
    Signed-off-by: liqunfu <liqun.fu@microsoft.com>

---
+++ onnxruntime/core/mlas/lib/rotary_embedding_kernel_avx2.cpp
@@ -0,0 +1,30 @@
+/*++
+
+Copyright (c) Microsoft Corporation. All rights reserved.
+
+Licensed under the MIT License.
+
+Module Name:
+
+    rotary_embedding_kernel_avx2.cpp
+
+Abstract:
+
+    This module implements the rotary embedding kernels for AVX2 supported h/w.
+
+--*/
+
+#include "rotary_embedding.h"
+#include "rotary_embedding_kernel_avx2.h"
+
+//
+// Kernel dispatch structure definition.
+//
+const MLAS_ROPE_DISPATCH MlasRopeDispatchAvx2 = []() {
+    MLAS_ROPE_DISPATCH d;
+
+#if defined(MLAS_TARGET_AMD64)
+        d.SRope = rope_avx2::RopeKernel_Avx2;
+#endif
+    return d;
+}();
