e264af34aa5505ba0d0d2acdc388414a40d044db
Changming Sun <chasun@microsoft.com>
2025-02-05 22:20:43 -0800

    test change

--- tools/ci_build/build.py
+++ tools/ci_build/build.py
@@ -2719,7 +2719,8 @@ def main():
     args = parse_arguments()
 
     print(args)
-
+    if not args.use_vcpkg:
+        sys.exit(1)
     if os.getenv("ORT_BUILD_WITH_CACHE") == "1":
         args.use_cache = True
 
