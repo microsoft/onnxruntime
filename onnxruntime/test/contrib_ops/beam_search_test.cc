microsoft/onnxruntime

commit 1001ec93a7362bf717f368d3084846c13690e960

Ryanunderhill/beamscorer gpu (#16272)
### Description
Make BeamScorer run on the GPU vs the CPU.

Brief overview:
  Adds a CUDA 'CudaBeamSearchScorer' implementation of IBeamScorer
Instead of a 'done' flag per beam, there is one single 'not done'
variable that is copied to the CPU every iteration
Removes some of the extra CPU side buffers and parameters that are no
longer needed

Remaining future optimizations:
CPU copied beam indices is still used in the non
DecoderMaskedSelfAttention case. An extra kernel can be written to avoid
PickGptPasteState needing CPU copied beam indices (called from
UpdateGptFeeds).

### Motivation and Context
It's faster to keep the work on the GPU to avoid GPU->CPU->GPU copies of
data.

onnxruntime/contrib_ops/cpu/transformers/generation_shared.h
@@ -39,8 +39,7 @@ struct IBeamSearchState {
                                        // in total, it will be:
                                        // 2 * (batch_size * num_beams * (parts_vocab + 1), 2 * num_beams)
 
-  // The final chosen indices after BeamScorer has finished processing
-  gsl::span<int32_t> chosen_indices;  // shape (batch_size, num_beams)
+  gsl::span<int32_t> sequences_device;  // shape (2 * batch_size * max_length)
 
   Tensor staging_for_past_state_reorder;  // Tensor of shape (batch_size * num_beams, num_heads, max_length, head_size)
 };
@@ -96,6 +95,8 @@ struct ISamplingState {
 struct ISequences {
   virtual ~ISequences() {}
   virtual gsl::span<const int32_t> GetSequence(int beam_index) const = 0;
+  virtual gsl::span<const int32_t> GetCurrentDeviceSequences() const = 0;  // Get all current beam_index sequences in one continuous block (to pass to CUDA)
+  virtual gsl::span<int32_t> GetNextDeviceSequences() = 0;                 // Get all next beam_index sequences in one continuous block (to pass to CUDA)
   virtual int GetSequenceLength() const = 0;
 };
 
@@ -118,7 +119,13 @@ struct IBeamScorer {
                         Tensor* output_sequences,
                         Tensor* output_sequence_scores) = 0;
 
-  virtual gsl::span<int32_t>& GetNextIndices() = 0;
+  virtual bool IsDone() const = 0;                    // GPU version will return false here, as it asynchronously queues up the event
+  virtual bool IsDoneLater() const { return false; }  // GPU version waits for the asynchous result to complete here
+
+  virtual gsl::span<float> GetNextScores() = 0;
+  virtual gsl::span<int32_t> GetNextTokens() = 0;
+  virtual gsl::span<int32_t> GetNextIndicesCPU() = 0;
+  virtual gsl::span<int32_t> GetNextIndicesGPU() { return {}; }  // If this is non CPU, returns the device buffer of the indices
 };
 
 struct IGenerationParameters {

onnxruntime/contrib_ops/cpu/transformers/generation_device_helper.cc
@@ -280,7 +280,6 @@ void InitGreedyState(transformers::IGreedySearchState<T>* greedy_state,
 template <typename T>
 Status ProcessLogits(const OrtValue& logits,                                 // logits output of subgraph
                      transformers::IBeamSearchState<T>* beam_state,          // state
-                     transformers::IBeamSearchCpuState* cpu_state,           // state in CPU
                      transformers::ISequences* sequences,                    // sequences
                      AllocatorPtr& allocator,                                // default allocator
                      onnxruntime::concurrency::ThreadPool* thread_pool,      // thread pool (for CPU only)
@@ -290,7 +289,6 @@ Status ProcessLogits(const OrtValue& logits,                                 // 
                      int step,                                               // iteration counter
                      Stream* stream,                                         // cuda stream (for CUDA only)
                      const transformers::IConsoleDumper* dumper) {           // tensor dumper
-  ORT_UNUSED_PARAMETER(cpu_state);
 #ifndef DEBUG_GENERATION
   ORT_UNUSED_PARAMETER(dumper);
 #endif
@@ -946,7 +944,6 @@ template void InitGreedyState<float>(
 template Status ProcessLogits<float>(
     const OrtValue& logits,
     transformers::IBeamSearchState<float>* beam_state,
-    transformers::IBeamSearchCpuState* cpu_state,
     transformers::ISequences* sequences,
     AllocatorPtr& allocator,
     onnxruntime::concurrency::ThreadPool* thread_pool,

onnxruntime/contrib_ops/cpu/transformers/beam_search.cc
@@ -215,7 +215,8 @@ Status BeamSearch::Compute(OpKernelContext* ctx) const {
           init_beam_state_func_ ? init_beam_state_func_ : GenerationCpuDeviceHelper::InitBeamState<float>,
           device_copy_func_ ? device_copy_func_ : GenerationCpuDeviceHelper::DeviceCopy<float>,
           device_copy_int32_func_ ? device_copy_int32_func_ : GenerationCpuDeviceHelper::DeviceCopy<int32_t>,
-          update_gpt_feeds_func_ ? update_gpt_feeds_func_ : GenerationCpuDeviceHelper::UpdateGptFeeds<float>};
+          update_gpt_feeds_func_ ? update_gpt_feeds_func_ : GenerationCpuDeviceHelper::UpdateGptFeeds<float>,
+          create_beam_scorer_func_};
 #ifdef USE_CUDA
       ORT_RETURN_IF_ERROR(impl.InitializeCuda(reorder_past_state_func_, cuda_device_prop_, cuda_device_arch_));
 #endif
@@ -237,7 +238,8 @@ Status BeamSearch::Compute(OpKernelContext* ctx) const {
           init_beam_state_fp16_func_,
           device_copy_func_,
           device_copy_int32_func_,
-          update_gpt_feeds_fp16_func_};
+          update_gpt_feeds_fp16_func_,
+          create_beam_scorer_func_};
 #ifdef USE_CUDA
       ORT_RETURN_IF_ERROR(impl.InitializeCuda(reorder_past_state_func_, cuda_device_prop_, cuda_device_arch_));
 #endif
@@ -267,7 +269,8 @@ Status BeamSearch::Compute(OpKernelContext* ctx) const {
           update_decoder_feeds_func_ ? update_decoder_feeds_func_ : GenerationCpuDeviceHelper::UpdateDecoderFeeds<float>,
           expand_buffer_int32_func_ ? expand_buffer_int32_func_ : GenerationCpuDeviceHelper::ExpandBuffer<int32_t>,
           expand_buffer_float_func_ ? expand_buffer_float_func_ : GenerationCpuDeviceHelper::ExpandBuffer<float>,
-          expand_buffer_float16_func_ ? expand_buffer_float16_func_ : GenerationCpuDeviceHelper::ExpandBuffer<MLFloat16>};
+          expand_buffer_float16_func_ ? expand_buffer_float16_func_ : GenerationCpuDeviceHelper::ExpandBuffer<MLFloat16>,
+          create_beam_scorer_func_};
 #ifdef USE_CUDA
       ORT_RETURN_IF_ERROR(impl.InitializeCuda(reorder_past_state_func_, init_cache_indir_func_, cuda_device_prop_, cuda_device_arch_));
 #endif
@@ -288,7 +291,8 @@ Status BeamSearch::Compute(OpKernelContext* ctx) const {
           update_decoder_feeds_fp16_func_,
           expand_buffer_int32_func_,
           expand_buffer_float_func_,
-          expand_buffer_float16_func_};
+          expand_buffer_float16_func_,
+          create_beam_scorer_func_};
 #ifdef USE_CUDA
       ORT_RETURN_IF_ERROR(impl.InitializeCuda(reorder_past_state_func_, init_cache_indir_func_, cuda_device_prop_, cuda_device_arch_));
 #endif
@@ -314,7 +318,8 @@ Status BeamSearch::Compute(OpKernelContext* ctx) const {
           create_whisper_encoder_inputs_func_ ? create_whisper_encoder_inputs_func_ : GenerationCpuDeviceHelper::CreateWhisperEncoderInputs<float>,
           update_decoder_feeds_func_ ? update_decoder_feeds_func_ : GenerationCpuDeviceHelper::UpdateDecoderFeeds<float>,
           expand_buffer_float_func_ ? expand_buffer_float_func_ : GenerationCpuDeviceHelper::ExpandBuffer<float>,
-          expand_buffer_float16_func_ ? expand_buffer_float16_func_ : GenerationCpuDeviceHelper::ExpandBuffer<MLFloat16>};
+          expand_buffer_float16_func_ ? expand_buffer_float16_func_ : GenerationCpuDeviceHelper::ExpandBuffer<MLFloat16>,
+          create_beam_scorer_func_};
 #ifdef USE_CUDA
       ORT_RETURN_IF_ERROR(impl.InitializeCuda(reorder_past_state_func_, init_cache_indir_func_, cuda_device_prop_, cuda_device_arch_));
 #endif
@@ -334,7 +339,8 @@ Status BeamSearch::Compute(OpKernelContext* ctx) const {
           create_whisper_encoder_inputs_func_ ? create_whisper_encoder_inputs_func_ : GenerationCpuDeviceHelper::CreateWhisperEncoderInputs<MLFloat16>,
           update_decoder_feeds_fp16_func_ ? update_decoder_feeds_fp16_func_ : GenerationCpuDeviceHelper::UpdateDecoderFeeds<MLFloat16>,
           expand_buffer_float_func_,
-          expand_buffer_float16_func_};
+          expand_buffer_float16_func_,
+          create_beam_scorer_func_};
 #ifdef USE_CUDA
       ORT_RETURN_IF_ERROR(impl.InitializeCuda(reorder_past_state_func_, init_cache_indir_func_, cuda_device_prop_, cuda_device_arch_));
 #endif

onnxruntime/contrib_ops/cpu/transformers/beam_search.h
@@ -53,7 +53,8 @@ class BeamSearch : public IControlFlowKernel {
       const GenerationDeviceHelper::ProcessLogitsFunc<float>& process_logits_func,
       const GenerationDeviceHelper::ProcessLogitsFunc<MLFloat16>& process_logits_fp16_func,
       const GenerationDeviceHelper::InitBeamStateFunc<float>& init_beam_state_func,
-      const GenerationDeviceHelper::InitBeamStateFunc<MLFloat16>& init_beam_state_fp16_func) {
+      const GenerationDeviceHelper::InitBeamStateFunc<MLFloat16>& init_beam_state_fp16_func,
+      const GenerationDeviceHelper::CreateBeamScorer& create_beam_scorer_func) {
     add_to_feeds_func_ = add_to_feeds_func;
     topk_func_ = topk_func;
     device_copy_func_ = device_copy_func;
@@ -62,6 +63,7 @@ class BeamSearch : public IControlFlowKernel {
     process_logits_fp16_func_ = process_logits_fp16_func;
     init_beam_state_func_ = init_beam_state_func;
     init_beam_state_fp16_func_ = init_beam_state_fp16_func;
+    create_beam_scorer_func_ = create_beam_scorer_func;
   }
 
 #ifdef USE_CUDA
@@ -111,6 +113,7 @@ class BeamSearch : public IControlFlowKernel {
 
   GenerationDeviceHelper::InitBeamStateFunc<float> init_beam_state_func_;
   GenerationDeviceHelper::InitBeamStateFunc<MLFloat16> init_beam_state_fp16_func_;
+  GenerationDeviceHelper::CreateBeamScorer create_beam_scorer_func_;
 
 #ifdef USE_CUDA
   GenerationDeviceHelper::ReorderPastStateFunc reorder_past_state_func_;

onnxruntime/contrib_ops/cpu/transformers/beam_search_impl_base.h
@@ -23,16 +23,18 @@ struct BeamSearchState : IBeamSearchState<T> {
     size_t next_token_size = SafeInt<size_t>(batch_beam_size) * parameters.vocab_size;
     this->next_token_logits = AllocateBuffer<T>(allocator, next_token_logits_buffer_, next_token_size);
     this->next_token_scores = AllocateBuffer<float>(allocator, next_token_scores_buffer_, next_token_size);
-
     this->next_tokens = AllocateBuffer<int32_t>(allocator, next_tokens_buffer_, SafeInt<size_t>(2) * batch_beam_size);
-
     this->next_indices = AllocateBuffer<int32_t>(allocator, next_indices_buffer_, SafeInt<size_t>(2) * batch_beam_size);
-
     this->next_scores = AllocateBuffer<float>(allocator, next_scores_buffer_, SafeInt<size_t>(2) * batch_beam_size);
 
     constexpr size_t max_parts_of_vocab = 128;
     size_t topk_buffer_size = SafeInt<size_t>(batch_beam_size) * (max_parts_of_vocab + 1) * parameters.num_beams * 2 * 2;
     this->topk_buffer = AllocateBuffer<float>(allocator, topk_temp_buffer_, topk_buffer_size);
+
+    if (allocator->Info().device.Type() == OrtDevice::GPU) {
+      size_t sequences_elements = SafeInt<size_t>(2) * batch_beam_size * parameters.max_length;
+      this->sequences_device = AllocateBuffer<int32_t>(allocator, sequences_device_buffer_, sequences_elements);
+    }
 
     if (use_position) {
       this->next_positions = AllocateBuffer<int32_t>(allocator, next_positions_buffer_, batch_beam_size);
@@ -54,11 +56,6 @@ struct BeamSearchState : IBeamSearchState<T> {
       Tensor temp(DataTypeImpl::GetType<T>(), staging_for_past_state_reorder_buffer_shape, allocator);
 
       this->staging_for_past_state_reorder = std::move(temp);
-
-      // We need a buffer on GPU to hold the final chosen indices after BeamScorer has finished processing
-      // TODO: This is a temporary work-around as BeamScorer currently only runs on CPU.
-      // We can remove these kinds of work-arounds once BeamScorer runs on CUDA eventually.
-      this->chosen_indices = AllocateBuffer<int32_t>(allocator, chosen_indices_buffer_, batch_beam_size);
     }
   }
 
@@ -80,7 +77,7 @@ struct BeamSearchState : IBeamSearchState<T> {
   BufferUniquePtr beam_scores_buffer_;
   BufferUniquePtr scores_buffer_;
   BufferUniquePtr topk_temp_buffer_;
-  BufferUniquePtr chosen_indices_buffer_;
+  BufferUniquePtr sequences_device_buffer_;
 };
 
 struct BeamSearchCpuState : IBeamSearchCpuState {
@@ -174,7 +171,6 @@ class BeamSearchBase : public GenerateBase {
   // Process logits and append next tokens to sequences.
   Status GenerateNextToken(const OrtValue& logits,
                            gsl::span<int32_t>& beam_next_tokens,
-                           gsl::span<int32_t>& beam_indices,
                            BeamSearchState<T>& beam_state,
                            BeamSearchCpuState& cpu_state,
                            int counter);
@@ -188,7 +184,7 @@ class BeamSearchBase : public GenerateBase {
 
   BeamSearchParameters* parameters_;
 
-  std::unique_ptr<BeamSearchScorer> beam_scorer_;
+  std::unique_ptr<IBeamScorer> beam_scorer_;
 
   // Device specific functions
   GenerationDeviceHelper::ProcessLogitsFunc<T> process_logits_func_;
@@ -246,7 +242,7 @@ Status BeamSearchBase<T>::ProcessLogits(
     BeamSearchCpuState& cpu_state,
     AllocatorPtr& allocator,
     int counter) {
-  return process_logits_func_(logits, &beam_state, &cpu_state, &(cpu_state.sequences), allocator,
+  return process_logits_func_(logits, &beam_state, &(cpu_state.sequences), allocator,
                               thread_pool_, &logits_processors_, beam_scorer_.get(),
                               parameters_, counter, ort_stream_, GetConsoleDumper());
 }
@@ -255,36 +251,69 @@ template <typename T>
 Status BeamSearchBase<T>::GenerateNextToken(
     const OrtValue& logits,
     gsl::span<int32_t>& beam_next_tokens,
-    gsl::span<int32_t>& beam_indices,
     BeamSearchState<T>& beam_state,
     BeamSearchCpuState& cpu_state,
     int counter) {
   // Process logits to get next token scores
   ORT_RETURN_IF_ERROR(ProcessLogits(logits, beam_state, cpu_state, temp_space_allocator_, counter));
 
-  gsl::span<float>& beam_scores = beam_scorer_->GetNextScores();
-  // It is optional to clone beam_scores. Change it to use same buffer also works for CPU:
-  //    beam_state.beam_scores = beam_scores
-  // Here we make a copy to reduce the coupling with little cost (the buffer size is small).
-  ORT_RETURN_IF_ERROR(device_copy_func_(beam_state.beam_scores,
-                                        beam_scores,
-                                        ort_stream_,
-                                        DeviceCopyDirection::hostToDevice));
+  if (this->IsCuda()) {
+    auto beam_scores = beam_scorer_->GetNextScores();
+    // It is optional to clone beam_scores. Change it to use same buffer also works:
+    //    beam_state.beam_scores = beam_scores
+    // Here we make a copy to reduce the coupling with little cost (the buffer size is small).
+    ORT_RETURN_IF_ERROR(device_copy_func_(beam_state.beam_scores,
+                                          beam_scores,
+                                          ort_stream_,
+                                          DeviceCopyDirection::deviceToDevice));
 
-  beam_next_tokens = beam_scorer_->GetNextTokens();
-  beam_indices = beam_scorer_->GetNextIndices();
-
-#ifdef DEBUG_GENERATION
-  cpu_dumper_.Print("beam_scores from scorer", beam_scores.data(), parameters_->batch_size, parameters_->num_beams);
-  cpu_dumper_.Print("beam_next_tokens", beam_next_tokens.data(), parameters_->batch_size, parameters_->num_beams);
-  cpu_dumper_.Print("beam_indices", beam_indices.data(), parameters_->batch_size, parameters_->num_beams);
-#endif
-
-  cpu_state.sequences.AppendNextTokenToSequences(beam_indices, beam_next_tokens);
+    beam_next_tokens = beam_scorer_->GetNextTokens();
 
 #ifdef DEBUG_GENERATION
-  cpu_state.sequences.PrintSequences(&cpu_dumper_);
+    auto beam_indices = beam_scorer_->GetNextIndicesGPU();
+    cuda_dumper_->Print("beam_scores from scorer", beam_state.beam_scores.data(), parameters_->batch_size, parameters_->num_beams);
+    cuda_dumper_->Print("beam_next_tokens", beam_next_tokens.data(), parameters_->batch_size, parameters_->num_beams);
+    cuda_dumper_->Print("beam_indices", beam_indices.data(), parameters_->batch_size, parameters_->num_beams);
 #endif
+
+    // the Cuda beam scorer does the AppendNextTokenSequences, all that's left for Cuda is a this small step
+    cpu_state.sequences.AfterDeviceAppendedNextToken();
+
+#ifdef DEBUG_GENERATION
+    // CUDA equivalent of cpu_state.sequences.PrintSequences
+    auto sequences_buffer = cpu_state.sequences.GetCurrentDeviceSequences();
+    for (int i = 0; i < parameters_->batch_size * parameters_->num_beams; i++) {
+      gsl::span<const int32_t> sequence = sequences_buffer.subspan(i * parameters_->max_length, cpu_state.sequences.GetSequenceLength());
+      cuda_dumper_->Print("sequences", i, false);
+      cuda_dumper_->Print(nullptr, sequence.data(), 1, static_cast<int>(sequence.size()));
+    }
+#endif
+  } else {
+    auto beam_scores = beam_scorer_->GetNextScores();
+    // It is optional to clone beam_scores. Change it to use same buffer also works for CPU:
+    //    beam_state.beam_scores = beam_scores
+    // Here we make a copy to reduce the coupling with little cost (the buffer size is small).
+    ORT_RETURN_IF_ERROR(device_copy_func_(beam_state.beam_scores,
+                                          beam_scores,
+                                          ort_stream_,
+                                          DeviceCopyDirection::hostToDevice));
+
+    beam_next_tokens = beam_scorer_->GetNextTokens();
+    auto beam_indices = beam_scorer_->GetNextIndicesCPU();
+
+#ifdef DEBUG_GENERATION
+    cpu_dumper_.Print("beam_scores from scorer", beam_scores.data(), parameters_->batch_size, parameters_->num_beams);
+    cpu_dumper_.Print("beam_next_tokens", beam_next_tokens.data(), parameters_->batch_size, parameters_->num_beams);
+    cpu_dumper_.Print("beam_indices", beam_indices.data(), parameters_->batch_size, parameters_->num_beams);
+#endif
+
+    cpu_state.sequences.AppendNextTokenToSequences(beam_indices, beam_next_tokens);
+
+#ifdef DEBUG_GENERATION
+    cpu_state.sequences.PrintSequences(&cpu_dumper_);
+#endif
+  }
+
   return Status::OK();
 }
 

commit 09e5724f3b50a6912561eda4bd45725dae8c8596

[CUDA] Fix beam search of num_beams > 32 (#23599)
### Description
* Pass topk_scores to beam scorer in slow topk path.
* Add an env variable `ORT_BEAM_SEARCH_USE_FAST_TOPK` to enable/disable fast topk.
* Add a test case for slow topk path.

### Motivation and Context

This bug was introduced in
https://github.com/microsoft/onnxruntime/pull/16272

Beam search uses fast cuda kernel when number of beams <= 32. When beam
size is larger than that threshold, we use another code path (slower
cuda kernel) to get topk. In such `slow topk path`, topk_scores shall be
passed to beam scorer but it is not.

This bug will cause incorrect result when num_beams > 32. It was not
found previously since such large beam size is rarely used.

onnxruntime/contrib_ops/cpu/transformers/beam_search_parameters.cc
@@ -2,6 +2,7 @@
 // Licensed under the MIT License.
 
 #include "contrib_ops/cpu/transformers/beam_search_parameters.h"
+#include "core/platform/env_var_utils.h"
 
 namespace onnxruntime {
 namespace contrib {
@@ -136,7 +137,11 @@ void BeamSearchParameters::ParseFromInputs(OpKernelContext* context) {
       temperature = 1.0f;
     }
   }
+
+  // The following parameter is read from environment variable for testing purpose.
+  use_fast_topk = ParseEnvironmentVariableWithDefault<bool>(kBeamSearchUseFastTopK, true);
 }
+
 void BeamSearchParameters::SetSubgraphParameters(int vocabulary_size, int heads, int hidden_size_per_head, int layers) {
   // Override vocab_size using the inferred shape from the decoder subgraph ONLY IF
   // the vocab_size hasn't been explicitly specified by the user (as an attribute of BeamSearch)

onnxruntime/contrib_ops/cpu/transformers/generation_shared.h
@@ -199,7 +199,13 @@ struct IGenerationParameters {
   int extra_decoding_ids_input_id = -1;
   int cross_qk_output_id = -1;
   int no_speech_probs_output_id = -1;
+
+  // Parameter for testing slow topk path. It can be updated by the below environment variable.
+  bool use_fast_topk = true;
 };
+
+// Environment variable to enable/disable fast topk kernel on GPU. Default is 1 (enabled).
+constexpr const char* kBeamSearchUseFastTopK = "ORT_BEAM_SEARCH_USE_FAST_TOPK";
 
 }  // namespace transformers
 }  // namespace contrib

onnxruntime/contrib_ops/cuda/transformers/generation_device_helper.cc
@@ -524,7 +524,8 @@ Status ProcessLogits(const OrtValue& logits,                                 // 
     beam_state->remaining_scores = beam_state->remaining_scores.subspan(next_token_scores.size());
   }
 
-  if (num_beams <= 32) {
+  gsl::span<float> scores_to_process = beam_state->next_scores;
+  if (parameters->use_fast_topk && num_beams <= 32) {
     constexpr size_t max_parts_of_vocab = 128;
     size_t candidate_count = SafeInt<size_t>(batch_beam_size) * 2 * num_beams;
     float* topk_tmp_buffer = beam_state->topk_buffer.data();
@@ -546,13 +547,6 @@ Status ProcessLogits(const OrtValue& logits,                                 // 
                          beam_state->next_tokens.data(),
                          beam_state->next_indices.data(),
                          cuda_stream);
-
-    // Select [batch_size, 2 * num_beams] from [batch_size * num_beams, 2 * num_beams]
-#ifdef DEBUG_GENERATION
-    dumper->Print("next_tokens before scorer", beam_state->next_tokens.data(), batch_size, 2 * num_beams);
-    dumper->Print("next_indices before scorer", beam_state->next_indices.data(), batch_size, 2 * num_beams);
-    dumper->Print("next_scores before scorer", beam_state->next_scores.data(), batch_size, 2 * num_beams);
-#endif
   } else {
     // Apply top-k selection like the following:
     //   next_token_scores = next_token_scores.view(batch_size, num_beams * vocab_size)
@@ -588,17 +582,19 @@ Status ProcessLogits(const OrtValue& logits,                                 // 
     cuda::LaunchNextTokenKernel(next_token_indices, beam_state->next_indices.data(), beam_state->next_tokens.data(),
                                 batch_size, top_k, vocab_size, cuda_stream);
 
-#ifdef DEBUG_GENERATION
-    dumper->Print("next_scores before scorer", topk_scores->Data<float>(), batch_size, top_k);
-    dumper->Print("next_tokens before scorer", beam_state->next_tokens.data(), batch_size, top_k);
-    dumper->Print("next_indices before scorer", beam_state->next_indices.data(), batch_size, top_k);
-#endif
+    scores_to_process = gsl::span<float>(topk_scores->MutableData<float>(), batch_size * top_k);
   }
 
   // gsl::span doesn't convert from non const to const, so all we're doing here is making each const.
-  gsl::span<const float> next_scores(beam_state->next_scores.data(), beam_state->next_scores.size());
+  gsl::span<const float> next_scores(scores_to_process.data(), scores_to_process.size());
   gsl::span<const int32_t> next_tokens(beam_state->next_tokens.data(), beam_state->next_tokens.size());
   gsl::span<const int32_t> next_indices(beam_state->next_indices.data(), beam_state->next_indices.size());
+
+#ifdef DEBUG_GENERATION
+  dumper->Print("next_scores before scorer", next_scores.data(), batch_size, 2 * num_beams);
+  dumper->Print("next_tokens before scorer", next_tokens.data(), batch_size, 2 * num_beams);
+  dumper->Print("next_indices before scorer", next_indices.data(), batch_size, 2 * num_beams);
+#endif
 
   beam_scorer->Process(
       *sequences,
@@ -735,6 +731,7 @@ void CudaBeamSearchScorer::Process(transformers::ISequences& sequences,
                                        next_tokens,
                                        next_indices,
                                        stream_);
+
   CUDA_CALL_THROW(cudaEventRecord(event_process_complete_.Get(), stream_));
 
   cuda::LaunchBeamSearchScorer_AppendNextTokenToSequences(*state_cpu_,

onnxruntime/test/contrib_ops/beam_search_test.cc
@@ -9,6 +9,8 @@
 #include "test/common/cuda_op_test_utils.h"
 #include "test/providers/model_tester.h"
 #include "test/util/include/current_test_name.h"
+#include "test/util/include/scoped_env_vars.h"
+#include "contrib_ops/cpu/transformers/generation_shared.h"
 
 #ifdef USE_CUDA
 #include "core/providers/cuda/cuda_provider_options.h"
@@ -19,7 +21,7 @@ extern std::unique_ptr<Ort::Env> ort_env;
 namespace onnxruntime {
 namespace test {
 
-TEST(BeamSearchTest, GptBeamSearchFp32) {
+void RunGptBeamSearchFp32() {
   std::vector<int64_t> input_ids_shape{3, 12};
   std::vector<int32_t> input_ids{
       0, 0, 0, 0, 0, 52, 195, 731, 321, 301, 734, 620,
@@ -105,6 +107,16 @@ TEST(BeamSearchTest, GptBeamSearchFp32) {
   const auto* result_vals = sequences.GetTensorData<int32_t>();
   auto result_span = gsl::make_span(result_vals, expected_output.size());
   ASSERT_TRUE(std::equal(expected_output.cbegin(), expected_output.cend(), result_span.begin(), result_span.end()));
+}
+
+TEST(BeamSearchTest, GptBeamSearchFp32) {
+  RunGptBeamSearchFp32();
+}
+
+TEST(BeamSearchTest, GptBeamSearchFp32_DisableFastTopK) {
+  ScopedEnvironmentVariables scoped_env_vars{
+      EnvVarMap{{onnxruntime::contrib::transformers::kBeamSearchUseFastTopK, "0"}}};
+  RunGptBeamSearchFp32();
 }
 
 TEST(BeamSearchTest, GptBeamSearchFp16) {