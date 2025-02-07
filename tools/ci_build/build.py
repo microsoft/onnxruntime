758342aaa38a67b0f3f09c28ac5055811714a58a
Changming Sun <chasun@microsoft.com>
2025-02-06 17:26:38 -0800

    update

--- tools/ci_build/build.py
+++ tools/ci_build/build.py
@@ -1077,9 +1077,9 @@ def generate_vcpkg_install_options(source_dir, args):
                 "--x-asset-sources=x-azurl,https://vcpkg.storage.devpackages.microsoft.io/artifacts/\\;x-block-origin"
             )
     if SYSTEM_COLLECTIONURI == "https://dev.azure.com/onnxruntime/":
-        vcpkg_install_options.append("--binarysource=x-az-universal,onnxruntime,onnxruntime,onnxruntime,rw")
+        vcpkg_install_options.append("--binarysource=x-az-universal,onnxruntime,onnxruntime,onnxruntime,readwrite")
     elif SYSTEM_COLLECTIONURI == "https://dev.azure.com/aiinfra/" or SYSTEM_COLLECTIONURI == "https://aiinfra.visualstudio.com/":
-        vcpkg_install_options.append("--binarysource=x-az-universal,aiinfra,Lotus,Lotus,rw")
+        vcpkg_install_options.append("--binarysource=x-az-universal,aiinfra,Lotus,Lotus,readwrite")
     # Config binary cache
     return vcpkg_install_options
 
