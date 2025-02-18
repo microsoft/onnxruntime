53c9ec80974e5eceb24ae739151a110a8736af16
Jian Chen <cjian@microsoft.com>
2025-02-18 12:45:39 -0800

    Adding MNISTPackage.java

--- js/react_native/e2e/android/app/src/main/java/com/reactnativeonnxruntimemodule/MainApplication.kt
+++ js/react_native/e2e/android/app/src/main/java/com/reactnativeonnxruntimemodule/MainApplication.kt
@@ -27,6 +27,7 @@ class MainApplication : Application(), ReactApplication {
             PackageList(this).packages.apply {
               // Packages that cannot be autolinked yet can be added manually here, for example:
               // add(MyReactNativePackage())
+              add(MNISTPackage())
             }
 
         override fun getJSMainModuleName(): String = "index"
