079588b59ffe20f339bf20cb7d181adc7c0127a9
Jie Chen <jie.a.chen@intel.com>
2025-03-04 14:50:26 +0800

    Fix adapter

--- onnxruntime/core/providers/webgpu/webgpu_context.h
+++ onnxruntime/core/providers/webgpu/webgpu_context.h
@@ -75,7 +75,6 @@ class WebGpuContext final {
 
   Status Wait(wgpu::Future f);
 
-  const wgpu::Adapter& Adapter() const { return adapter_; }
   const wgpu::Device& Device() const { return device_; }
 
   const wgpu::AdapterInfo& AdapterInfo() const { return adapter_info_; }
@@ -200,7 +199,6 @@ class WebGpuContext final {
   LibraryHandles modules_;
 
   wgpu::Instance instance_;
-  wgpu::Adapter adapter_;
   wgpu::Device device_;
 
   webgpu::ValidationMode validation_mode_;
