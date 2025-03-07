bf4e42e20bb035d0852a879a1b5e87d44f665812
Sheetal Kadam <shekadam@microsoft.com>
2025-03-06 17:53:17 -0800

    java api for webgpu

--- java/src/test/android/app/src/androidTest/java/ai/onnxruntime/example/javavalidator/SimpleTest.kt
+++ java/src/test/android/app/src/androidTest/java/ai/onnxruntime/example/javavalidator/SimpleTest.kt
@@ -52,6 +52,11 @@ class SimpleTest {
         runSigmoidModelTestImpl(1, OrtProvider.QNN)
     }
 
+    @Test
+    fun runSigmoidModelTestWEBGPU() {
+        runSigmoidModelTestImpl(1, OrtProvider.WEBGPU)
+    }
+
     @Throws(IOException::class)
     private fun readModel(fileName: String): ByteArray {
         return InstrumentationRegistry.getInstrumentation().context.assets.open(fileName)
@@ -76,6 +81,15 @@ class SimpleTest {
                         opts.addNnapi()
                     } else {
                         Log.println(Log.INFO, TAG, "NO NNAPI EP available, skip the test")
+                        return
+                    }
+                }
+
+                OrtProvider.WEBGPU -> {
+                    if (OrtEnvironment.getAvailableProviders().contains(OrtProvider.WEBGPU)) {
+                        opts.addWebGPU()
+                    } else {
+                        Log.println(Log.INFO, TAG, "NO WEBGPU EP available, skip the test")
                         return
                     }
                 }
