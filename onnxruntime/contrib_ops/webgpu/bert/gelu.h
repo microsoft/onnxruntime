8bf16469a613616ea8baef4eac0409b529e745dc
Prathik Rao <prathik.rao@gmail.com>
2025-03-09 17:06:29 -0700

    reuse gelu in unary_elementwise_ops.cc

--- onnxruntime/contrib_ops/webgpu/bert/gelu.h
+++ onnxruntime/contrib_ops/webgpu/bert/gelu.h
@@ -1,31 +1,31 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
+// // Copyright (c) Microsoft Corporation. All rights reserved.
+// // Licensed under the MIT License.
 
-#pragma once
+// #pragma once
 
-#include "core/providers/webgpu/program.h"
-#include "core/providers/webgpu/webgpu_kernel.h"
+// #include "core/providers/webgpu/program.h"
+// #include "core/providers/webgpu/webgpu_kernel.h"
 
-namespace onnxruntime {
-namespace contrib {
-namespace webgpu {
+// namespace onnxruntime {
+// namespace contrib {
+// namespace webgpu {
 
-using namespace onnxruntime::webgpu;
-using onnxruntime::webgpu::ComputeContext;
+// using namespace onnxruntime::webgpu;
+// using onnxruntime::webgpu::ComputeContext;
 
-class GeluProgram final : public Program<GeluProgram> {
- public:
-  GeluProgram() : Program{"Gelu"} {}
-  Status GenerateShaderCode(ShaderHelper& sh) const override;
-  WEBGPU_PROGRAM_DEFINE_UNIFORM_VARIABLES({"vec_size", ProgramUniformVariableDataType::Uint32});
-};
+// class GeluProgram final : public Program<GeluProgram> {
+//  public:
+//   GeluProgram() : Program{"Gelu"} {}
+//   Status GenerateShaderCode(ShaderHelper& sh) const override;
+//   WEBGPU_PROGRAM_DEFINE_UNIFORM_VARIABLES({"vec_size", ProgramUniformVariableDataType::Uint32});
+// };
 
-class Gelu final : public WebGpuKernel {
- public:
-  Gelu(const OpKernelInfo& info) : WebGpuKernel(info) {}
-  Status ComputeInternal(ComputeContext& context) const override;
-};
+// class Gelu final : public WebGpuKernel {
+//  public:
+//   Gelu(const OpKernelInfo& info) : WebGpuKernel(info) {}
+//   Status ComputeInternal(ComputeContext& context) const override;
+// };
 
-}  // namespace webgpu
-}  // namespace contrib
-}  // namespace onnxruntime
+// }  // namespace webgpu
+// }  // namespace contrib
+// }  // namespace onnxruntime
