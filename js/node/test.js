e98ad7cd87e2e0d4e20af97b00ccb225f78c2155
Yulong Wang <7679871+fs-eire@users.noreply.github.com>
2025-03-10 22:31:14 -0700

    3

--- js/node/test.js
+++ js/node/test.js
@@ -1,14 +1,8 @@
 const ort = require('.');
 
 async function main() {
-  const session0 = await ort.InferenceSession.create('D:\\pg\\2025-02-18\\mobilenetv2-12.onnx');
-  console.log(session0.inputNames);
-  console.log(session0.outputNames);
-  console.log(session0.inputMetadata);
-  console.log(session0.outputMetadata);
-
   const session1 = await ort.InferenceSession.create(
-    'C:\\Users\\fs\\Downloads\\op_test_generated_model_abs_no_shape.onnx',
+    'op_test_generated_model_abs_no_shape.onnx',
   );
   console.log(session1.inputNames);
   console.log(session1.outputNames);
@@ -16,7 +10,7 @@ async function main() {
   console.log(session1.outputMetadata);
 
   const session2 = await ort.InferenceSession.create(
-    'C:\\Users\\fs\\Downloads\\op_test_generated_model_abs_symbol.onnx',
+    'op_test_generated_model_abs_symbol.onnx',
   );
   console.log(session2.inputNames);
   console.log(session2.outputNames);
@@ -24,7 +18,7 @@ async function main() {
   console.log(session2.outputMetadata);
 
   const session3 = await ort.InferenceSession.create(
-    'C:\\Users\\fs\\Downloads\\op_test_generated_model_abs_fixed.onnx',
+    'op_test_generated_model_abs_fixed.onnx',
   );
   console.log(session3.inputNames);
   console.log(session3.outputNames);
