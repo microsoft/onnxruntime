3317b3099cc18d0043597b77e599951967b73598
Yulong Wang <7679871+fs-eire@users.noreply.github.com>
2025-03-10 22:33:29 -0700

    4

--- js/node/test.js
+++ js/node/test.js
@@ -1,29 +1,17 @@
 const ort = require('.');
 
 async function main() {
-  const session1 = await ort.InferenceSession.create(
-    'op_test_generated_model_abs_no_shape.onnx',
-  );
-  console.log(session1.inputNames);
-  console.log(session1.outputNames);
-  console.log(session1.inputMetadata);
-  console.log(session1.outputMetadata);
-
   const session2 = await ort.InferenceSession.create(
     'op_test_generated_model_abs_symbol.onnx',
   );
   console.log(session2.inputNames);
   console.log(session2.outputNames);
-  console.log(session2.inputMetadata);
-  console.log(session2.outputMetadata);
 
-  const session3 = await ort.InferenceSession.create(
-    'op_test_generated_model_abs_fixed.onnx',
-  );
-  console.log(session3.inputNames);
-  console.log(session3.outputNames);
-  console.log(session3.inputMetadata);
-  console.log(session3.outputMetadata);
+
+  const input_0 = new ort.Tensor('float32', [1, -2, 3, -4, 5, -6, 7, -8], [2, 4]);
+  const {output_0} = await session2.run({input_0});
+
+  console.log(output_0.data);
 }
 
 main();
