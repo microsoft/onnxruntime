08dee61cca9b5818d16e0aa449605ff690c07a93
Hector Li <hecli@microsoft.com>
2025-03-11 14:18:47 -0700

    resolve build issue on Linux

--- onnxruntime/test/providers/qnn/qnn_ep_context_test.cc
+++ onnxruntime/test/providers/qnn/qnn_ep_context_test.cc
@@ -1114,6 +1114,7 @@ static void DumpModelWithSharedCtx(ProviderOptions provider_options,
   Ort::Session session2(*ort_env, ToPathString(onnx_model_path2).c_str(), so);
 }
 
+#if defined(__aarch64__) || defined(_M_ARM64)
 static void GetModelInputNames(const std::string& model_path,
                                std::vector<std::string>& input_names,
                                std::vector<std::string>& output_names,
@@ -1133,6 +1134,7 @@ static void GetModelInputNames(const std::string& model_path,
     output_names.push_back(output->Name());
   }
 }
+#endif
 
 // 1. Create 2 QDQ models
 // 2. Initialize 2 Ort sessions which share the same QNN EP from these 2 QDQ models
