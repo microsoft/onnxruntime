e1b87da19186e256a280c2133b25be6951953be5
Sheetal Kadam <shekadam@microsoft.com>
2025-03-12 13:23:22 -0700

    remove params

--- java/src/main/java/ai/onnxruntime/OrtSession.java
+++ java/src/main/java/ai/onnxruntime/OrtSession.java
@@ -1330,8 +1330,8 @@ public class OrtSession implements AutoCloseable {
     /**
      * Adds WEBGPU as an execution backend.
      *
-     * @param providerOptions Configuration options for the WEBGPU backend. Refer to the WEBGPU execution
-     *     provider's documentation.
+     * This method does not accept any parameters. The `providerOptions` is set internally
+     * to an empty map for the WEBGPU execution provider configuration.
      * @throws OrtException If there was an error in native code.
      */
     public void addWebGPU() throws OrtException {
