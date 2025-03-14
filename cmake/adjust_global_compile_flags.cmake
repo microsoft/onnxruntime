0034289a39591342d6315387d6b5a579d96707a5
Sheetal Kadam <shekadam@microsoft.com>
2025-03-14 15:07:08 -0700

    increase timeout to test completion

--- cmake/adjust_global_compile_flags.cmake
+++ cmake/adjust_global_compile_flags.cmake
@@ -193,7 +193,7 @@ endif()
 # Due to many dependencies have different symbol visibility settings, set global compile flags here.
 if (${CMAKE_SYSTEM_NAME} MATCHES "Darwin|iOS|visionOS|tvOS")
   foreach(flags CMAKE_CXX_FLAGS CMAKE_OBJC_FLAGS CMAKE_OBJCXX_FLAGS)
-    string(APPEND ${flags} " -fvisibility=hidden -fvisibility-inlines-hidden -Wno-unsafe-buffer-usage")
+    string(APPEND ${flags} " -fvisibility=hidden -fvisibility-inlines-hidden")
   endforeach()
 endif()
 
