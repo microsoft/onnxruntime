ad4ad3aa6b58ee9112a4a74ec3fc2026b2674eda
Prathik Rao <prathik.rao@gmail.com>
2025-03-14 11:21:59 -0700

    cum sum impl

---
+++ onnxruntime/core/providers/webgpu/math/cum_sum.cc
@@ -0,0 +1,84 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+#include "core/common/inlined_containers.h"
+#include "core/providers/webgpu/tensor/gather_elements.h"
+#include "core/providers/cpu/tensor/utils.h"
+#include "core/providers/webgpu/shader_helper.h"
+#include "core/providers/webgpu/webgpu_supported_types.h"
+
+namespace onnxruntime {
+namespace webgpu {
+
+ONNX_OPERATOR_VERSIONED_KERNEL_EX(
+    GatherElements,
+    kOnnxDomain,
+    11, 13,
+    kWebGpuExecutionProvider,
+    (*KernelDefBuilder::Create()).TypeConstraint("T", WebGpuSupportedFloatTypes()),
+    GatherElements);
+
+ONNX_OPERATOR_KERNEL_EX(
+    GatherElements,
+    kOnnxDomain,
+    14,
+    kWebGpuExecutionProvider,
+    (*KernelDefBuilder::Create()).TypeConstraint("T", WebGpuSupportedFloatTypes()),
+    GatherElements);
+
+Status GatherElementsProgram::GenerateShaderCode(ShaderHelper& shader) const {
+  const ShaderVariableHelper& input = shader.AddInput("input", ShaderUsage::UseUniform);
+  const ShaderVariableHelper& indices = shader.AddInput("indices", ShaderUsage::UseUniform);
+  const ShaderVariableHelper& output = shader.AddOutput("output", ShaderUsage::UseUniform);
+
+  std::string index = "i32(" + input.IndicesGet("uniforms.input_shape", "uniforms.axis", input.Rank()) + ")";
+  std::string max = GetElementAt("uniforms.input_shape", "uniforms.axis", input.Rank());
+  std::string lowerLimit = reverse_ ? index + (exclusive_ ? " + 1" : "") : "0";
+  std::string upperLimit = reverse_ ? max : index + (exclusive_ ? "" : " + 1");
+  shader.MainFunctionBody() << shader.GuardAgainstOutOfBoundsWorkgroupSizes("uniforms.output_size")
+                            << "var input_indices = " << input.OffsetToIndices("global_idx") << ";\n"
+                            << "var sum = output_indices_t(0);\n"
+                            << "let first : i32 = " << lowerLimit << ";\n"
+                            << "let last : i32 = " << upperLimit << ";\n"
+                            << "for (var i : i32 = first; i < last; i++) {\n"
+                            << "  " << input.IndicesSet("input_indices", "uniforms.axis", "u32(i)") << ";\n"
+                            << "  sum = sum + " << input.GetByIndices("input_indices") << ";\n"
+                            << "}\n"
+                            << output.SetByOffset("global_idx", "sum") << ";\n";
+
+  return Status::OK();
+}
+
+Status GatherElements::ComputeInternal(ComputeContext& context) const {
+  const auto* input_tensor = context.Input(0);
+  const TensorShape& input_shape = input_tensor->Shape();
+  int64_t input_rank = input_shape.NumDimensions();
+
+  const auto* axis_tensor = context.Input(1);
+  const auto* axis_data = axis_tensor->Data<int64_t>();
+
+  // Handle negative axis
+  int64_t axis = axis_data[0];
+  if (axis < 0) {
+    axis += input_rank;
+  }
+
+  auto* output_tensor = context.Output(0, input_shape);
+  int64_t output_size = output_tensor->Shape().Size();
+
+  if (output_size == 0) {
+    return Status::OK();
+  }
+
+  GatherElementsProgram program{exclusive_, reverse_};
+  program
+      .AddInput({input_tensor})
+      .AddOutput({output_tensor, ProgramTensorMetadataDependency::TypeAndRank})
+      .SetDispatchGroupSize((output_size + WORKGROUP_SIZE - 1) / WORKGROUP_SIZE)
+      .AddUniformVariables({{static_cast<uint32_t>(output_size)},
+                            {static_cast<uint32_t>(axis)}});
+  return context.RunProgram(program);
+}
+
+}  // namespace webgpu
+}  // namespace onnxruntime
---
+++ onnxruntime/core/providers/webgpu/math/cum_sum.h
@@ -0,0 +1,41 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+#pragma once
+
+#include "core/providers/webgpu/webgpu_kernel.h"
+#include "core/providers/webgpu/program.h"
+
+namespace onnxruntime {
+namespace webgpu {
+
+class CumSumProgram final : public Program<CumSumProgram> {
+ public:
+ CumSumProgram(int64_t exclusive, int64_t reverse) : Program{"CumSum"}, exclusive_{exclusive}, reverse_{reverse} {}
+
+  Status GenerateShaderCode(ShaderHelper& sh) const override;
+
+  WEBGPU_PROGRAM_DEFINE_UNIFORM_VARIABLES({"output_size", ProgramUniformVariableDataType::Uint32,
+                                           "axis", ProgramUniformVariableDataType::Uint32});
+
+ private:
+  int64_t exclusive_;
+  int64_t reverse_;
+};
+
+class GatherElements final : public WebGpuKernel {
+ public:
+  GatherElements(const OpKernelInfo& info) : WebGpuKernel(info) {
+    exclusive_ = info.GetAttrOrDefault<int64_t>("exclusive", 0);
+    reverse_ = info.GetAttrOrDefault<int64_t>("reverse", 0);
+  }
+
+  Status ComputeInternal(ComputeContext& context) const override;
+
+ private:
+  int64_t exclusive_;
+    int64_t reverse_;
+};
+
+}  // namespace webgpu
+}  // namespace onnxruntime
