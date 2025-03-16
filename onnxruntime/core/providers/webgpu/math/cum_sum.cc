248494bca84a98bfff6ed62832f129c7a21499cd
Prathik Rao <prathik.rao@gmail.com>
2025-03-16 15:11:05 -0700

    satya comments

--- onnxruntime/core/providers/webgpu/math/cum_sum.cc
+++ onnxruntime/core/providers/webgpu/math/cum_sum.cc
@@ -13,7 +13,7 @@ ONNX_OPERATOR_VERSIONED_KERNEL_EX(
     kOnnxDomain,
     11, 13,
     kWebGpuExecutionProvider,
-    (*KernelDefBuilder::Create()).TypeConstraint("T", WebGpuSupportedFloatTypes()).TypeConstraint("T2", WebGpuSupportedNumberTypes()),
+    (*KernelDefBuilder::Create()).TypeConstraint("T", WebGpuSupportedFloatTypes()).TypeConstraint("T2", WebGpuSupportedNumberTypes()).InputMemoryType(OrtMemTypeCPU, 1),
     CumSum);
 
 ONNX_OPERATOR_KERNEL_EX(
@@ -21,7 +21,7 @@ ONNX_OPERATOR_KERNEL_EX(
     kOnnxDomain,
     14,
     kWebGpuExecutionProvider,
-    (*KernelDefBuilder::Create()).TypeConstraint("T", WebGpuSupportedFloatTypes()).TypeConstraint("T2", WebGpuSupportedNumberTypes()),
+    (*KernelDefBuilder::Create()).TypeConstraint("T", WebGpuSupportedFloatTypes()).TypeConstraint("T2", WebGpuSupportedNumberTypes()).InputMemoryType(OrtMemTypeCPU, 1),
     CumSum);
 
 Status CumSumProgram::GenerateShaderCode(ShaderHelper& shader) const {
@@ -30,13 +30,27 @@ Status CumSumProgram::GenerateShaderCode(ShaderHelper& shader) const {
 
   std::string index = "i32(" + input.IndicesGet("input_indices", "uniforms.axis") + ")";
   std::string max = GetElementAt("uniforms.input_shape", "uniforms.axis", input.Rank());
-  std::string lowerLimit = reverse_ ? index + (exclusive_ ? " + 1" : "") : "0";
-  std::string upperLimit = reverse_ ? max : index + (exclusive_ ? "" : " + 1");
+  std::stringstream lowerLimit;
+  lowerLimit << "let first : i32 = 0;\n"
+             << "if (uniforms.reverse == 1) {\n"
+             << "  first = " << index << ";\n"
+             << "  if (uniforms.exclusive == 1) { first += 1; }\n"
+             << "}\n";
+  std::string upperLimit;
+  upperLimit << "let last : i32 = 0;\n"
+             << "if (uniforms.reverse == 1) {\n"
+             << "  last = " << max << ";\n"
+             << "} else {\n"
+             << "  last = " << index << ";\n"
+             << "  if (uniforms.exclusive == 0) { last += 1; }\n"
+             << "}\n";
+  // std::string lowerLimit = reverse_ ? index + (exclusive_ ? " + 1" : "") : "0";
+  // std::string upperLimit = reverse_ ? max : index + (exclusive_ ? "" : " + 1");
   shader.MainFunctionBody() << shader.GuardAgainstOutOfBoundsWorkgroupSizes("uniforms.output_size")
                             << "var input_indices = " << input.OffsetToIndices("global_idx") << ";\n"
                             << "var sum = output_indices_t(0);\n"
-                            << "let first : i32 = " << lowerLimit << ";\n"
-                            << "let last : i32 = " << upperLimit << ";\n"
+                            << lowerLimit.str()
+                            << upperLimit.str()
                             << "for (var i : i32 = first; i < last; i++) {\n"
                             << "  " << input.IndicesSet("input_indices", "uniforms.axis", "u32(i)") << ";\n"
                             << "  sum = sum + " << input.GetByIndices("input_indices") << ";\n"
@@ -53,9 +67,14 @@ Status CumSum::ComputeInternal(ComputeContext& context) const {
 
   const auto* axis_tensor = context.Input(1);
   const auto* axis_data = axis_tensor->Data<int64_t>();
-
-  // Handle negative axis
   int64_t axis = axis_data[0];
+
+  bool valid_axis = true;
+  if (axis < -input_rank || axis >= input_rank) {
+    valid_axis = false;
+  }
+  ORT_ENFORCE(valid_axis, "Axes attribute must be within range -input_rank <= axis < input_rank.");
+  // Handle negative axis
   if (axis < 0) {
     axis += input_rank;
   }
@@ -67,13 +86,15 @@ Status CumSum::ComputeInternal(ComputeContext& context) const {
     return Status::OK();
   }
 
-  CumSumProgram program{exclusive_, reverse_};
+  CumSumProgram program{};
   program
       .AddInput({input_tensor})
       .AddOutput({output_tensor, ProgramTensorMetadataDependency::TypeAndRank})
       .SetDispatchGroupSize((output_size + WORKGROUP_SIZE - 1) / WORKGROUP_SIZE)
       .AddUniformVariables({{static_cast<uint32_t>(output_size)},
-                            {static_cast<uint32_t>(axis)}});
+                            {static_cast<uint32_t>(axis)},
+                            {static_cast<uint32_t>(exclusive_)},
+                            {static_cast<uint32_t>(reverse_)}});
   return context.RunProgram(program);
 }
 
