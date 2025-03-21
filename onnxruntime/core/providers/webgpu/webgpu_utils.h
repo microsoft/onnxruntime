b6fd71067a15de02efc1b79066ad40a79bac2063
Yulong Wang <7679871+fs-eire@users.noreply.github.com>
2025-03-20 19:46:48 -0700

    [webgpu EP] put GetMaxComponents and SumVector to one place. (#24122)
    ### Description
    
    put `GetMaxComponents` and `SumVector` to one place.
    
    fix a bug in `SumVector`:
    
    ```diff
    -      return "(" + x + ".x + " + x + ".y + " + x + ".w + " + x + ".z" + ")";
    +      return "(" + x + ".x + " + x + ".y + " + x + ".z + " + x + ".w" + ")";
    ```

--- onnxruntime/core/providers/webgpu/webgpu_utils.h
+++ onnxruntime/core/providers/webgpu/webgpu_utils.h
@@ -7,13 +7,26 @@
 namespace onnxruntime {
 namespace webgpu {
 
-inline int64_t GetMaxComponents(int64_t size) {
+inline int GetMaxComponents(int64_t size) {
   if (size % 4 == 0) {
     return 4;
   } else if (size % 2 == 0) {
     return 2;
   }
   return 1;
+}
+
+inline std::string SumVector(std::string x, int components) {
+  switch (components) {
+    case 1:
+      return x;
+    case 2:
+      return "(" + x + ".x + " + x + ".y" + ")";
+    case 4:
+      return "(" + x + ".x + " + x + ".y + " + x + ".z + " + x + ".w" + ")";
+    default:
+      ORT_THROW("Unsupported number of components: ", components);
+  }
 }
 
 }  // namespace webgpu
