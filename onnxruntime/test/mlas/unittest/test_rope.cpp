a96105f252e9155626baa4e3d5bda914d4830095
Liqun Fu <liqun.fu@microsoft.com>
2025-03-20 21:36:20 -0700

    review comment update: avoid magic number and proper casting
    Signed-off-by: Liqun Fu <liqun.fu@microsoft.com>

--- onnxruntime/test/mlas/unittest/test_rope.cpp
+++ onnxruntime/test/mlas/unittest/test_rope.cpp
@@ -37,9 +37,10 @@ class MlasRoPETest : public MlasTestBase {
       input[i] = static_cast<T>(1.f * i + 1);
     }
     for (size_t i = 0; i < table_len; ++i) {
-      float theta = (float)i / 1000 * Pi;
-      sin_data[i] = (T)std::sin(theta);
-      cos_data[i] = (T)std::cos(theta);
+      // https://arxiv.org/pdf/2104.09864 section 3.4.3
+      float theta_i = static_cast<float>(pow(10000, -2.0f * i / rotary_emb_dim));
+      sin_data[i] = static_cast<T>(std::sin(theta_i));
+      cos_data[i] = static_cast<T>(std::cos(theta_i));
     }
 
     // Call the function
