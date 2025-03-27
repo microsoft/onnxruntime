f83962c615eb61d3b20ef2b81b16c830e0ff2109
Prathik Rao <prathik.rao@gmail.com>
2025-03-27 11:22:21 -0700

    adjust include

--- onnxruntime/core/providers/webgpu/controlflow/if.cc
+++ onnxruntime/core/providers/webgpu/controlflow/if.cc
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-#include "if.h"
+#include "core/providers/webgpu/controlflow/if.h"
 
 using namespace ONNX_NAMESPACE;
 using namespace onnxruntime::common;
