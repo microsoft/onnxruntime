f57282d3082946b90ffd67486ac4393c626ee25f
Ubuntu <yilyu@yilyu-x64-d8ads-v5-linux.gp4c2wlhwjtu5hyrq4fgdaludg.jx.internal.cloudapp.net>
2025-03-27 21:22:16 +0000

    Add support
    - Support cmake option.
    - Thread safe warning.
    - Define CheckSaturation macro

--- onnxruntime/core/mlas/lib/saturation_check.cpp
+++ onnxruntime/core/mlas/lib/saturation_check.cpp
@@ -15,70 +15,16 @@ Abstract:
 
 --*/
 
+#include <atomic>
 #include <immintrin.h>
 #include <stdint.h>
 #include <iostream>
 
-void print_m256i_16(const char* label, __m256i reg) {
-    alignas(32) int16_t values[16];  // __m256i has 16x int16_t elements
-    _mm256_store_si256(reinterpret_cast<__m256i*>(values), reg);
+namespace onnxruntime {
 
-    std::cout << label << ": ";
-    for (int i = 0; i < 16; ++i) {
-        std::cout << std::hex << values[i] << " ";
-    }
-    std::cout << std::endl;
+std::atomic<int> saturation_count{0};
+
 }
-
-void print_m256i_8(const char* label, __m256i reg) {
-    alignas(32) int8_t values[32];  // __m256i has 16x int16_t elements
-    _mm256_store_si256(reinterpret_cast<__m256i*>(values), reg);
-
-    std::cout << label << ": ";
-    for (int i = 0; i < 32; ++i) {
-        std::cout << std::dec << static_cast<uint32_t>(values[i]) << " ";
-    }
-    std::cout << std::endl;
-}
-
-#if 0
-//#ifdef SATURATION_DETECTION_ENABLED
-extern "C" void CheckForSaturationBeforeMul(const __m256i* unsigned_ptr, const __m256i* signed_ptr) {
-    static  int counter = 0;
-
-    __m256i unsigned_data = _mm256_loadu_si256(unsigned_ptr);
-    __m256i signed_data = _mm256_loadu_si256(signed_ptr);
-
-    __m256i result = _mm256_maddubs_epi16(unsigned_data, signed_data);
-
-    alignas(32) uint16_t unsigned_bytes[16];  // __m256i has 16x int16_t elements
-    _mm256_store_si256(reinterpret_cast<__m256i*>(unsigned_bytes), unsigned_data);
-    alignas(32) int16_t signed_bytes[16];  // __m256i has 16x int16_t elements
-    _mm256_store_si256(reinterpret_cast<__m256i*>(signed_bytes), signed_data);
-
-    //(void)input;  // Suppress unused variable warning
-    //(void)multiplier;  // Suppress unused variable warning
-
-    // Example saturation check: print values
-    if (counter < 10) {
-        print_m256i_8("unsigned_data:\t", unsigned_data);
-        print_m256i_8("signed_data:\t", signed_data);
-        print_m256i_16("result:\t", result);
-        counter++;
-        int32_t dest =
-            static_cast<int32_t>(signed_bytes[0]) * static_cast<int32_t>(static_cast<uint32_t>(unsigned_bytes[0])) +
-            static_cast<int32_t>(signed_bytes[1]) * static_cast<int32_t>(static_cast<uint32_t>(unsigned_bytes[1]));
-        std::cout << "Checking for saturation..." << std::hex << "0x" << dest << std::endl;
-    }
-    
-    return;  // Modify this to return true if saturation is detected
-}
-//#endif  // SATURATION_DETECTION_ENABLED
-#endif
-
-#if 1
-
-int saturate_counter = 0;
 
 extern "C" void CheckForSaturationBeforeMul(const __m256i* unsigned_ptr, const __m256i* signed_ptr) {
 
@@ -120,10 +66,8 @@ extern "C" void CheckForSaturationBeforeMul(const __m256i* unsigned_ptr, const _
     //std::cout << std::endl;
 
     // Log a warning if saturation is detected
-    if (saturation_detected && saturate_counter < 2) {
-        std::cerr << "[WARNING] Saturation detected in _mm256_maddubs_epi16 operation!" << std::endl;
-        saturate_counter++;
+    if (saturation_detected && ++onnxruntime::saturation_count < 2) {
+        std::cerr << "[WARNING] Saturation detected in VPMADDUBSW instruction!" << std::endl;
     }
 
 }
-#endif
--- onnxruntime/core/session/inference_session.cc
+++ onnxruntime/core/session/inference_session.cc
@@ -96,8 +96,6 @@
 using namespace ONNX_NAMESPACE;
 using namespace onnxruntime::common;
 
-extern int saturate_counter;
-
 namespace onnxruntime {
 namespace {
 template <typename T>
@@ -250,6 +248,8 @@ Status GetMinimalBuildOptimizationHandling(
 #endif  // !defined(ORT_MINIMAL_BUILD)
 
 }  // namespace
+
+extern std::atomic<int> saturation_count;
 
 std::atomic<uint32_t> InferenceSession::global_session_id_{1};
 std::map<uint32_t, InferenceSession*> InferenceSession::active_sessions_;
@@ -2841,7 +2841,7 @@ Status InferenceSession::Run(const RunOptions& run_options,
   }
 #endif
 
-  saturate_counter = 0;
+  saturation_count = 0;
 
   // As N+1 inference runs (N for memory allocation and 1 for graph capturing)
   // are needed before replaying the captured graph, here run N inference runs recursively until graph captured,
