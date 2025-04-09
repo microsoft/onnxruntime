c7fb6836b849b9a94f3ca2548edb1ab85ee39744
Jeff Kilpatrick <jkilpat@qti.qualcomm.com>
2025-04-09 16:34:41 -0700

    [AISW-134328] Check in copy of AI Hub build script
    To kick of building a fast and debuggable CI system, we begin by
    checking in a stripped-down copy of the top-level script used by
    the AI Hub team for both local development and CI.
    
    Please note that script does not yet do anything useful. Tasks
    will be added as subsequent items in epic AISW-134319 are completed.

---
+++ qcom/ep_build/github.py
@@ -0,0 +1,27 @@
+import os
+
+from .util import Colors, echo, get_env_bool
+
+
+def on_github():
+    return "GITHUB_ACTION" in os.environ or get_env_bool(
+        "EP_BUILD_FORCE_ON_CI", False
+    )
+
+
+def start_group(group_name):
+    if on_github():
+        print(f"::group::{group_name}", flush=True)
+    else:
+        echo(f"{Colors.GREEN}{group_name}{Colors.OFF}")
+
+
+def end_group():
+    if on_github():
+        print("::endgroup::", flush=True)
+
+
+def set_github_output(key, value):
+    if on_github():
+        with open(os.environ["GITHUB_OUTPUT"], "a") as fh:
+            print(f"{key}={value}", file=fh)
---
+++ qcom/ep_build/self_test.py
@@ -0,0 +1,64 @@
+"""
+Self-tests for build_and_test.py.
+"""
+
+from typing import Optional, Type
+
+from . import plan
+
+
+def assert_task_dependencies_exist(task_library: Type) -> None:
+    # Check ALL_TASKS for consistency
+    for task_name in plan.ALL_TASKS:
+        task_func = getattr(task_library, task_name, None)
+        assert task_func is not None, f"{task_name} is not defined in TaskLibrary"
+        assert callable(task_func), f"{task_name} is not a function"
+    task_set = set(plan.ALL_TASKS)
+    assert len(task_set) == len(plan.ALL_TASKS), "Some task is defined twice"
+
+    # Make sure ancillary task collections at least refer to real tasks
+    unknown_publics = [t for t in plan.PUBLIC_TASKS if t not in task_set]
+    assert (
+        len(unknown_publics) == 0
+    ), f"These public tasks are not defined as tasks: {', '.join(unknown_publics)}"
+    unknown_descriptions = [t for t in plan.TASK_DESCRIPTIONS if t not in task_set]
+    assert (
+        len(unknown_publics) == 0
+    ), f"These descriptions are associated with undefined tasks: {', '.join(unknown_descriptions)}"
+
+    # Ensure that summarizers exist
+    unknown_summarizers = [
+        s for s in plan.SUMMARIZERS if getattr(task_library, s, None) is None
+    ]
+    assert (
+        len(unknown_publics) == 0
+    ), f"These summarizers have no implementing function: {', '.join(unknown_summarizers)}"
+
+    # Verify that all dependency keys and values are tasks
+    for task, deps in plan.TASK_DEPENDENCIES.items():
+        assert (
+            task in task_set
+        ), f"Task {task} has declared dependencies, but it does not exist"
+        unknown_deps = [d for d in deps if d not in task_set]
+        assert (
+            len(unknown_deps) == 0
+        ), f"Task {task} depends on these unknown dependencies: {', '.join(unknown_deps)}"
+        assert task not in deps, f"Task {task} depends on itself."
+
+
+def assert_tasks_sorted() -> None:
+    """Ensure that tasks are sorted alphabetically with ci_ functions following all others."""
+    last_task: Optional[str] = None
+    in_ci_section = False
+    for task_name in plan.ALL_TASKS:
+        if not last_task:
+            last_task = task_name
+            continue
+        if task_name.startswith("ci_") and not in_ci_section:
+            in_ci_section = True
+            last_task = task_name
+            continue
+        assert (
+            task_name > last_task
+        ), f"TaskLibrary functions are sorted incorrectly. {task_name} must come before {last_task}."
+        last_task = task_name
