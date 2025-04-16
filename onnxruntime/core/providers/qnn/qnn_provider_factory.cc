4574c803b1bb4e2e1353fd44b1b3625cdc5964d4
adrianlizarraga <adlizarraga@microsoft.com>
2025-04-15 22:26:51 -0700

    remove unnecessary headers

--- onnxruntime/core/providers/qnn/qnn_provider_factory.cc
+++ onnxruntime/core/providers/qnn/qnn_provider_factory.cc
@@ -1,8 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License
 
-#include <algorithm>
-#include <cctype>
 #include <string>
 #include <unordered_map>
 #include "core/providers/qnn/qnn_provider_factory_creator.h"
