650fbae81ebff135ecfe8e04d289b9e60b30aa0b
Ankit Maheshkar <ankit.maheshkar@intel.com>
2025-04-18 10:27:20 +0530

    [OVEP] update: Introduce enable_causallm provider option in OVEP (preview) (#24457)
    ### Description
    This PR introduces a new provider option called `enable_causallm` for
    OVEP.
    
    This provider option will serve as a entry gate towards enabling
    inference using ORT GenAI integration with OVEP in the upcoming PR ahead
    inside OVEP.

--- onnxruntime/core/session/provider_bridge_ort.cc
+++ onnxruntime/core/session/provider_bridge_ort.cc
@@ -2021,6 +2021,7 @@ ProviderOptions OrtOpenVINOProviderOptionsToOrtOpenVINOProviderOptionsV2(const O
   ov_options_converted_map["load_config"] = "";
   ov_options_converted_map["model_priority"] = "DEFAULT";
   ov_options_converted_map["enable_qdq_optimizer"] = "false";
+  ov_options_converted_map["enable_causallm"] = "false";
   return ov_options_converted_map;
 }
 
