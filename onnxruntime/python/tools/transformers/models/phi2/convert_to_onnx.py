0d26928b57ea55d6ece7e98be834dbb0d8b6c0a0
Tianlei Wu <tlwu@microsoft.com>
2025-04-18 23:09:06 -0700

    Rename matmul_4bits_quantizer.py to matmul_nbits_quantizer.py (#24472)
    ### Description
    
    * Rename  filename and class name since it supports 4 and 8 bits.
    * Update HQQWeightOnlyQuantizer to support 8 bits.
    * Update some comments.
    
    ### Motivation and Context
    https://github.com/microsoft/onnxruntime/pull/24384 added 8 bits support
    for the default weight only quantizer.

--- onnxruntime/python/tools/transformers/models/phi2/convert_to_onnx.py
+++ onnxruntime/python/tools/transformers/models/phi2/convert_to_onnx.py
@@ -14,9 +14,15 @@ import torch
 from benchmark_helper import Precision
 from fusion_options import AttentionOpType
 from onnx_model import OnnxModel
+from packaging import version
 from transformers import AutoConfig, AutoModelForCausalLM
 
-from onnxruntime.quantization.matmul_4bits_quantizer import MatMul4BitsQuantizer
+from onnxruntime import __version__ as ort_version
+
+if version.parse(ort_version) < version.parse("1.22.0"):
+    from onnxruntime.quantization.matmul_4bits_quantizer import MatMul4BitsQuantizer as MatMulNBitsQuantizer
+else:
+    from onnxruntime.quantization.matmul_nbits_quantizer import MatMulNBitsQuantizer
 
 
 class ConvertPhi2ToONNX:
@@ -160,7 +166,7 @@ class ConvertPhi2ToONNX:
             return
         else:
             assert self.precision == Precision.INT4
-            quant = MatMul4BitsQuantizer(
+            quant = MatMulNBitsQuantizer(
                 model=optimizer.model,
                 block_size=self.block_size,
                 is_symmetric=True,
@@ -351,7 +357,7 @@ def parse_arguments():
         required=False,
         default=16,
         type=int,
-        help="Block size to quantize with. See https://github.com/microsoft/onnxruntime/blob/main/onnxruntime/python/tools/quantization/matmul_4bits_quantizer.py for details.",
+        help="Block size to quantize with. See https://github.com/microsoft/onnxruntime/blob/main/onnxruntime/python/tools/quantization/matmul_nbits_quantizer.py for details.",
     )
 
     parser.add_argument(
