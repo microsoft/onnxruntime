3df48110777b9515bb80590122359bff8a423374
Wanming Lin <wanming.lin@intel.com>
2025-04-20 09:12:52 +0800

    [WebNN] Fallback unsupported integer input and output of a WebNN graph to int32 (#24425)
    Some WebNN backends support limited data types for the input and output
    of a WebNN graph. However, they can support more data types for
    intermediate nodes. To address this limitation, we implement a data type
    fallback mechanism. (Note: Currently, we only support fallback to int32
    for certain integer data types.)
    
    If a data type is not supported for a graph's input or output but is
    supported for intermediate nodes, we will:
    1. Save the input MLTensor as 'int32' data type,
    2. Convert the input data from ORT to int32,
    3. Insert a cast operation to WebNN graph to convert the input back to
    its original data type,
    4. Insert a cast operation to WebNN graph to convert the output back to
    'int32',
    5. Convert the output data from int32 to its original data type.

--- onnxruntime/core/providers/webnn/builders/helper.h
+++ onnxruntime/core/providers/webnn/builders/helper.h
@@ -164,7 +164,7 @@ inline bool ReadScalarTensorData(const onnx::TensorProto& tensor, emscripten::va
       scalar = emscripten::val{*reinterpret_cast<uint64_t*>(unpacked_tensor.data())};
       break;
     default:
-      LOGS(logger, ERROR) << "Unsupported data type : " << tensor.data_type();
+      LOGS(logger, ERROR) << "WebNN backend does not support data type: " << tensor.data_type();
       return false;
       break;
   }
@@ -362,6 +362,15 @@ const std::map<ONNX_NAMESPACE::TensorProto_DataType, std::string_view> onnx_to_w
     {ONNX_NAMESPACE::TensorProto_DataType_INT64, "int64"},
     {ONNX_NAMESPACE::TensorProto_DataType_UINT32, "uint32"},
     {ONNX_NAMESPACE::TensorProto_DataType_UINT64, "uint64"},
+};
+
+// This array contains the input/output data types of a WebNN graph that are allowed to be fallback to int32.
+constexpr std::array<ONNX_NAMESPACE::TensorProto_DataType, 5> supported_fallback_integer_data_types = {
+    ONNX_NAMESPACE::TensorProto_DataType_BOOL,
+    ONNX_NAMESPACE::TensorProto_DataType_INT8,
+    ONNX_NAMESPACE::TensorProto_DataType_UINT8,
+    ONNX_NAMESPACE::TensorProto_DataType_UINT32,
+    ONNX_NAMESPACE::TensorProto_DataType_INT64,
 };
 
 bool AreDataTypesSame(const std::string_view op_type,
