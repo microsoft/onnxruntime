5e973df84055345f6f76502fe51dfba95f73d91d
fajin-corp <fajin@microsoft.com>
2025-04-23 18:26:45 +0000

    fix linting

--- onnxruntime/test/contrib_ops/matmul_8bits_test.cc
+++ onnxruntime/test/contrib_ops/matmul_8bits_test.cc
@@ -252,7 +252,7 @@ TEST(MatMulNBits, Float32_8b_Accuracy4) {
   TestMatMul8BitsTyped<float, 100, 288, 93, 128, 4>();
   TestMatMul8BitsTyped<float, 100, 288, 1234, 16, 4>();
 }
-#endif // defined(MLAS_TARGET_AMD64_IX86) && !defined(USE_DML) && !defined(USE_WEBGPU) && !defined(USE_COREML)
+#endif  // defined(MLAS_TARGET_AMD64_IX86) && !defined(USE_DML) && !defined(USE_WEBGPU) && !defined(USE_COREML)
 }  // namespace test
 }  // namespace onnxruntime
 
