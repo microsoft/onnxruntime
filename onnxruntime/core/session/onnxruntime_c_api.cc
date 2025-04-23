e01fd7ac61a974899b84088da2ab99bb0b4eac34
amarin16 <91211819+amarin16@users.noreply.github.com>
2025-04-23 13:13:33 -0700

    Bump version to 1.21.2 (#24523)
    Bump the versions to `1.21.2` in order to publish the patch release for
    the required onnxruntime packages.

--- onnxruntime/core/session/onnxruntime_c_api.cc
+++ onnxruntime/core/session/onnxruntime_c_api.cc
@@ -2847,7 +2847,7 @@ static_assert(offsetof(OrtApi, AddExternalInitializersFromFilesInMemory) / sizeo
 static_assert(offsetof(OrtApi, SetEpDynamicOptions) / sizeof(void*) == 284, "Size of version 20 API cannot change");
 
 // So that nobody forgets to finish an API version, this check will serve as a reminder:
-static_assert(std::string_view(ORT_VERSION) == "1.21.1",
+static_assert(std::string_view(ORT_VERSION) == "1.21.2",
               "ORT_Version change detected, please follow below steps to ensure OrtApi is updated properly");
 // 1. Update the hardcoded version string in above static_assert to silence it
 // 2. If there were any APIs added to ort_api_1_to_21 above:
