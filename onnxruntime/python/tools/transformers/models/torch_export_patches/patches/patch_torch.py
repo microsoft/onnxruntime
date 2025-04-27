1df40d6c1979cc263bdf7544df8fb9dd272736e9
Jian Chen <cjian@microsoft.com>
2025-04-27 15:37:21 -0700

    Merge with main

--- onnxruntime/python/tools/transformers/models/torch_export_patches/patches/patch_torch.py
+++ onnxruntime/python/tools/transformers/models/torch_export_patches/patches/patch_torch.py
@@ -1,9 +1,3 @@
-# -------------------------------------------------------------------------
-# Copyright (c) Microsoft Corporation.  All rights reserved.
-# Licensed under the MIT License.  See License.txt in the project root for
-# license information.
-# --------------------------------------------------------------------------
-
 import inspect
 import os
 from collections.abc import Callable, Sequence
