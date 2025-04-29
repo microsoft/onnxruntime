758f91d7f1e824e974f350315518eca51d2f459c
adrianlizarraga <adlizarraga@microsoft.com>
2025-04-29 15:38:32 -0700

    Use C++ ORT API in unit test for RAII

--- onnxruntime/test/shared_lib/test_session_options.cc
+++ onnxruntime/test/shared_lib/test_session_options.cc
@@ -40,20 +40,14 @@ TEST(CApiTest, session_options_oversized_affinity_string) {
 // Test that loading OpenVINO EP when only the interface is built (but not the full EP) fails.
 TEST(CApiTest, session_options_provider_interface_fail_add_openvino) {
   const OrtApi& api = Ort::GetApi();
-  OrtSessionOptions* session_options = nullptr;
-  OrtStatus* status = nullptr;
+  Ort::SessionOptions session_options;
 
-  status = api.CreateSessionOptions(&session_options);
-  ASSERT_TRUE(status == nullptr);
-
-  status = api.SessionOptionsAppendExecutionProvider(session_options,
-                                                     kOpenVINOExecutionProvider, nullptr, nullptr, 0);
-  ASSERT_TRUE(status != nullptr);
-  OrtErrorCode err_code = api.GetErrorCode(status);
-  EXPECT_EQ(err_code, ORT_FAIL);
-
-  const char* err_msg = api.GetErrorMessage(status);
-  EXPECT_THAT(err_msg, testing::HasSubstr("Failed to load"));
+  Ort::Status status = Ort::Status{api.SessionOptionsAppendExecutionProvider(session_options,
+                                                                             kOpenVINOExecutionProvider,
+                                                                             nullptr, nullptr, 0)};
+  ASSERT_TRUE(!status.IsOK());
+  EXPECT_EQ(status.GetErrorCode(), ORT_FAIL);
+  EXPECT_THAT(status.GetErrorMessage(), testing::HasSubstr("Failed to load"));
 }
 #endif  // defined(USE_OPENVINO_PROVIDER_INTERFACE)
 
@@ -61,22 +55,19 @@ TEST(CApiTest, session_options_provider_interface_fail_add_openvino) {
 // Test that loading CUDA EP when only the interface is built (but not the full EP) fails.
 TEST(CApiTest, session_options_provider_interface_fail_add_cuda) {
   const OrtApi& api = Ort::GetApi();
-  OrtSessionOptions* session_options = nullptr;
-  OrtStatus* status = nullptr;
-
-  status = api.CreateSessionOptions(&session_options);
-  ASSERT_TRUE(status == nullptr);
+  Ort::SessionOptions session_options;
 
   OrtCUDAProviderOptionsV2* cuda_options = nullptr;
-  status = api.CreateCUDAProviderOptions(&cuda_options);
-  status = api.SessionOptionsAppendExecutionProvider_CUDA_V2(session_options, cuda_options);
-  ASSERT_TRUE(status != nullptr);
+  Ort::Status status1 = Ort::Status{api.CreateCUDAProviderOptions(&cuda_options)};
+  ASSERT_TRUE(status1.IsOK());
 
-  OrtErrorCode err_code = api.GetErrorCode(status);
-  EXPECT_EQ(err_code, ORT_FAIL);
+  Ort::Status status2 = Ort::Status{api.SessionOptionsAppendExecutionProvider_CUDA_V2(session_options,
+                                                                                      cuda_options)};
+  ASSERT_FALSE(status2.IsOK());
+  EXPECT_EQ(status2.GetErrorCode(), ORT_FAIL);
+  EXPECT_THAT(status2.GetErrorMessage(), testing::HasSubstr("Failed to load"));
 
-  const char* err_msg = api.GetErrorMessage(status);
-  EXPECT_THAT(err_msg, testing::HasSubstr("Failed to load"));
+  api.ReleaseCUDAProviderOptions(cuda_options);
 }
 #endif  // defined(USE_CUDA_PROVIDER_INTERFACE)
 
@@ -84,20 +75,14 @@ TEST(CApiTest, session_options_provider_interface_fail_add_cuda) {
 // Test that loading NV EP when only the interface is built (but not the full EP) fails.
 TEST(CApiTest, session_options_provider_interface_fail_add_nv) {
   const OrtApi& api = Ort::GetApi();
-  OrtSessionOptions* session_options = nullptr;
-  OrtStatus* status = nullptr;
+  Ort::SessionOptions session_options;
 
-  status = api.CreateSessionOptions(&session_options);
-  ASSERT_TRUE(status == nullptr);
-
-  status = api.SessionOptionsAppendExecutionProvider(session_options,
-                                                     kNvTensorRTRTXExecutionProvider, nullptr, nullptr, 0);
-  ASSERT_TRUE(status != nullptr);
-  OrtErrorCode err_code = api.GetErrorCode(status);
-  EXPECT_EQ(err_code, ORT_FAIL);
-
-  const char* err_msg = api.GetErrorMessage(status);
-  EXPECT_THAT(err_msg, testing::HasSubstr("Failed to load"));
+  Ort::Status status = Ort::Status{api.SessionOptionsAppendExecutionProvider(session_options,
+                                                                             kNvTensorRTRTXExecutionProvider,
+                                                                             nullptr, nullptr, 0)};
+  ASSERT_TRUE(!status.IsOK());
+  EXPECT_EQ(status.GetErrorCode(), ORT_FAIL);
+  EXPECT_THAT(status.GetErrorMessage(), testing::HasSubstr("Failed to load"));
 }
 #endif  // defined(USE_OPENVINO_PROVIDER_INTERFACE)
 
@@ -105,22 +90,19 @@ TEST(CApiTest, session_options_provider_interface_fail_add_nv) {
 // Test that loading TensorRT EP when only the interface is built (but not the full EP) fails.
 TEST(CApiTest, session_options_provider_interface_fail_add_tensorrt) {
   const OrtApi& api = Ort::GetApi();
-  OrtSessionOptions* session_options = nullptr;
-  OrtStatus* status = nullptr;
-
-  status = api.CreateSessionOptions(&session_options);
-  ASSERT_TRUE(status == nullptr);
+  Ort::SessionOptions session_options;
 
   OrtTensorRTProviderOptionsV2* trt_options = nullptr;
-  status = api.CreateTensorRTProviderOptions(&trt_options);
-  status = api.SessionOptionsAppendExecutionProvider_TensorRT_V2(session_options, trt_options);
-  ASSERT_TRUE(status != nullptr);
+  Ort::Status status1 = Ort::Status{api.CreateTensorRTProviderOptions(&trt_options)};
+  ASSERT_TRUE(status1.IsOK());
 
-  OrtErrorCode err_code = api.GetErrorCode(status);
-  EXPECT_EQ(err_code, ORT_FAIL);
+  Ort::Status status2 = Ort::Status{api.SessionOptionsAppendExecutionProvider_TensorRT_V2(session_options,
+                                                                                          trt_options)};
+  ASSERT_FALSE(status2.IsOK());
+  EXPECT_EQ(status2.GetErrorCode(), ORT_FAIL);
+  EXPECT_THAT(status2.GetErrorMessage(), testing::HasSubstr("Failed to load"));
 
-  const char* err_msg = api.GetErrorMessage(status);
-  EXPECT_THAT(err_msg, testing::HasSubstr("Failed to load"));
+  api.ReleaseTensorRTProviderOptions(trt_options);
 }
 #endif  // defined(USE_TENSORRT_PROVIDER_INTERFACE)
 
@@ -128,20 +110,14 @@ TEST(CApiTest, session_options_provider_interface_fail_add_tensorrt) {
 // Test that loading VitisAI EP when only the interface is built (but not the full EP) fails.
 TEST(CApiTest, session_options_provider_interface_fail_vitisai) {
   const OrtApi& api = Ort::GetApi();
-  OrtSessionOptions* session_options = nullptr;
-  OrtStatus* status = nullptr;
+  Ort::SessionOptions session_options;
 
-  status = api.CreateSessionOptions(&session_options);
-  ASSERT_TRUE(status == nullptr);
-
-  status = api.SessionOptionsAppendExecutionProvider(session_options,
-                                                     kVitisAIExecutionProvider, nullptr, nullptr, 0);
-  ASSERT_TRUE(status != nullptr);
-  OrtErrorCode err_code = api.GetErrorCode(status);
-  EXPECT_EQ(err_code, ORT_FAIL);
-
-  const char* err_msg = api.GetErrorMessage(status);
-  EXPECT_THAT(err_msg, testing::HasSubstr("Failed to load"));
+  Ort::Status status = Ort::Status{api.SessionOptionsAppendExecutionProvider(session_options,
+                                                                             kVitisAIExecutionProvider,
+                                                                             nullptr, nullptr, 0)};
+  ASSERT_TRUE(!status.IsOK());
+  EXPECT_EQ(status.GetErrorCode(), ORT_FAIL);
+  EXPECT_THAT(status.GetErrorMessage(), testing::HasSubstr("Failed to load"));
 }
 #endif  // defined(USE_VITISAI_PROVIDER_INTERFACE)
 
@@ -149,19 +125,13 @@ TEST(CApiTest, session_options_provider_interface_fail_vitisai) {
 // Test that loading QNN EP when only the interface is built (but not the full EP) fails.
 TEST(CApiTest, session_options_provider_interface_fail_qnn) {
   const OrtApi& api = Ort::GetApi();
-  OrtSessionOptions* session_options = nullptr;
-  OrtStatus* status = nullptr;
+  Ort::SessionOptions session_options;
 
-  status = api.CreateSessionOptions(&session_options);
-  ASSERT_TRUE(status == nullptr);
-
-  status = api.SessionOptionsAppendExecutionProvider(session_options,
-                                                     kQnnExecutionProvider, nullptr, nullptr, 0);
-  ASSERT_TRUE(status != nullptr);
-  OrtErrorCode err_code = api.GetErrorCode(status);
-  EXPECT_EQ(err_code, ORT_FAIL);
-
-  const char* err_msg = api.GetErrorMessage(status);
-  EXPECT_THAT(err_msg, testing::HasSubstr("Failed to load"));
+  Ort::Status status = Ort::Status{api.SessionOptionsAppendExecutionProvider(session_options,
+                                                                             kOpenVINOExecutionProvider,
+                                                                             nullptr, nullptr, 0)};
+  ASSERT_TRUE(!status.IsOK());
+  EXPECT_EQ(status.GetErrorCode(), ORT_FAIL);
+  EXPECT_THAT(status.GetErrorMessage(), testing::HasSubstr("Failed to load"));
 }
 #endif  // defined(USE_QNN_PROVIDER_INTERFACE)
