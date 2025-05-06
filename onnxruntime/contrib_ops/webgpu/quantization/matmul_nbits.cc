07181a60047880257976987787390d28513de668
Jiajia Qin <jiajiaqin@microsoft.com>
2025-05-06 14:19:40 +0800

    fix the warning

--- onnxruntime/contrib_ops/webgpu/quantization/matmul_nbits.cc
+++ onnxruntime/contrib_ops/webgpu/quantization/matmul_nbits.cc
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+#include <string>
 #include <string_view>
 
 #include "contrib_ops/webgpu/quantization/matmul_nbits.h"
