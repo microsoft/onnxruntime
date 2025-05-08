a7647d22a27e10156f761b6ad55a414047341495
Dmitri Smirnov <dmitrism@microsoft.com>
2025-05-08 16:03:15 -0700

    Fix the doc

--- include/onnxruntime/core/session/onnxruntime_c_api.h
+++ include/onnxruntime/core/session/onnxruntime_c_api.h
@@ -5255,7 +5255,7 @@ struct OrtApi {
   /** \brief Compute total size in bytes of the tensor data contained in an OrtValue.
    *
    * Returns the total number of bytes used to store the tensor data. For numeric tensors,
-   * this is sizeof(element_type) * total_element_count. OrtValues that are not tensors
+   * this is sizeof(element_type) * total_element_count. OrtValues that are not tensors or
    * that are tensors that contain strings will cause an error to be returned.
    *
    * \param[in] ort_value OrtValue instance containing a tensor
