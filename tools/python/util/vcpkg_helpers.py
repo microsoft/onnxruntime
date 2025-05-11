44b40c25f707e543bd45bdabec6c92aed0651b64
Yulong Wang <7679871+fs-eire@users.noreply.github.com>
2025-05-10 23:12:42 -0700

    static

--- tools/python/util/vcpkg_helpers.py
+++ tools/python/util/vcpkg_helpers.py
@@ -78,13 +78,6 @@ endif()
 endif() # End ONNX-specific block
 """)
 
-    # Add Dawn specific flags
-    f.write(r"""
-if(PORT STREQUAL "dawn")
-    set(VCPKG_LIBRARY_LINKAGE dynamic) # Always use dynamic link for Dawn
-endif()
-""")
-
 
 def add_copyright_header(f) -> None:
     """
