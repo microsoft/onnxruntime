8f0865d454b0bf08eff35eee583d4f5856d35164
Prathik Rao <prathik.rao@gmail.com>
2025-05-14 06:07:03 -0700

    [WebGPU EP] add output_size==0 check in transpose kernel (#24746)
    fixes error for https://huggingface.co/Xenova/musicgen-small on webgpu
    native
    
    
    
    ![4574e02e-fb31-41a9-9257-bd389373f64f](https://github.com/user-attachments/assets/d71b07db-863b-40ad-8478-08d94ac74f69)

--- onnxruntime/core/providers/webgpu/tensor/transpose.cc
+++ onnxruntime/core/providers/webgpu/tensor/transpose.cc
@@ -104,6 +104,10 @@ Status Transpose::DoTranspose(onnxruntime::webgpu::ComputeContext& context,
                               gsl::span<const size_t> permutations,
                               const Tensor& input, Tensor& output) {
   const auto& input_shape = input.Shape();
+  uint32_t output_size = onnxruntime::narrow<int32_t>(input_shape.Size());
+  if (output_size == 0) {
+    return Status::OK();
+  }
   const auto& input_dims = input_shape.GetDims();
   int32_t rank = static_cast<int32_t>(input_shape.NumDimensions());
 
@@ -130,8 +134,6 @@ Status Transpose::DoTranspose(onnxruntime::webgpu::ComputeContext& context,
                           : new_shape;
     new_output_shape = TensorShape({new_input_shape[1], new_input_shape[0]});
   }
-
-  uint32_t output_size = onnxruntime::narrow<int32_t>(input_shape.Size());
   TransposeProgram program{permutations, use_shared};
 
   if (use_shared) {
