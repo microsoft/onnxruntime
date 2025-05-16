4f734ab7f579a902bf4ce2458db947877fd5f2c0
Hector Li <hecli@microsoft.com>
2025-05-16 10:47:00 -0700

    disable the test for QNN EP build only

--- onnxruntime/test/providers/cpu/nn/pool_op_test.cc
+++ onnxruntime/test/providers/cpu/nn/pool_op_test.cc
@@ -213,8 +213,7 @@ TEST(PoolTest, MaxPool1D_case1) {
   test.Run(OpTester::ExpectResult::kExpectSuccess, "", {kTensorrtExecutionProvider});
 }
 
-// Caused by a combination of most recent changes, will fix it
-TEST(PoolTest, DISABLED_MaxPool1D_case2) {
+TEST(PoolTest, MaxPool1D_case2) {
   OpTester test("MaxPool");
   // no padding
   test.AddAttribute("auto_pad", "VALID");
@@ -230,7 +229,9 @@ TEST(PoolTest, DISABLED_MaxPool1D_case2) {
 
   test.AddInput<float>("X", x_dims, x_vals);
   test.AddOutput<float>("Y", expected_dims, expected_vals);
-  test.Run(OpTester::ExpectResult::kExpectSuccess, "", {kTensorrtExecutionProvider});
+
+  // QNN test failed. Caused by a combination of most recent changes, will fix it
+  test.Run(OpTester::ExpectResult::kExpectSuccess, "", {kTensorrtExecutionProvider, kQnnExecutionProvider});
 }
 
 TEST(PoolTest, MaxPool1D_case3) {
