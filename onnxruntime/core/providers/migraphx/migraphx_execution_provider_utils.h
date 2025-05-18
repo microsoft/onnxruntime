c52539b60a78e55a9521e2a02181bcd630e38047
Artur Wojcik <artur.wojcik@amd.com>
2025-05-18 11:03:24 +0200

    improve output name index extraction

--- onnxruntime/core/providers/migraphx/migraphx_execution_provider_utils.h
+++ onnxruntime/core/providers/migraphx/migraphx_execution_provider_utils.h
@@ -322,20 +322,28 @@ inline std::string GenerateGraphId(const GraphViewer& graph_viewer) {
   return std::string{s.data(), ptr};
 }
 
-inline void TrimLeft(std::string& s, int (*fn)(int) = std::isspace) {
-  s.erase(s.begin(), std::find_if(s.begin(), s.end(), [fn](int ch) {
+inline std::string_view TrimLeft(std::string_view sv, int (*fn)(int) = std::isspace) {
+  return sv.substr(0, sv.end() - std::find_if(sv.begin(), sv.end(), [fn](int ch) {
     return fn(ch);
   }));
 }
 
-inline void TrimRight(std::string& s, int (*fn)(int) = std::isspace) {
-  s.erase(std::find_if(s.rbegin(), s.rend(), [fn](int ch) {
+inline std::string_view TrimRight(std::string_view sv, int (*fn)(int) = std::isspace) {
+  return sv.substr(sv.end() - std::find_if(sv.rbegin(), sv.rend(), [fn](int ch) {
     return fn(ch);
-  }).base(), s.end());
+  }).base());
 }
 
-inline void Trim(std::string& s, int (*fn)(int) = std::isspace) {
-  TrimLeft(s, fn); TrimRight(s, fn);
+inline std::string_view Trim(std::string_view sv, int (*fn)(int) = std::isspace) {
+  return TrimRight(TrimLeft(sv, fn), fn);
+}
+
+inline int ToInteger(const std::string_view sv) {
+  int result = 0;
+  if (auto [_, ec] = std::from_chars(sv.data(), sv.data() + sv.length(), result); ec == std::errc()) {
+    return result;
+  }
+  ORT_THROW("invalid input for conversion to integer");
 }
 
 }  // namespace onnxruntime
