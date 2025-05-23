79163faa6055bd3e559b9d82577753f835b9c38e
Ken Hoover <ken.hoover@instacart.com>
2025-05-23 20:32:12 +0000

    Lintrunner

--- onnxruntime/test/python/onnxruntime_test_python_global_threadpool.py
+++ onnxruntime/test/python/onnxruntime_test_python_global_threadpool.py
@@ -14,7 +14,9 @@ class TestGlobalThreadPool(unittest.TestCase):
         session_opts.execution_mode = onnxrt.ExecutionMode.ORT_PARALLEL
         session_opts.graph_optimization_level = onnxrt.GraphOptimizationLevel.ORT_DISABLE_ALL
         session_opts.use_per_session_threads = False
-        session = onnxrt.InferenceSession(get_name("mnist.onnx"), session_opts, providers=onnxrt.get_available_providers())
+        session = onnxrt.InferenceSession(
+            get_name("mnist.onnx"), session_opts, providers=onnxrt.get_available_providers()
+        )
         input = np.ones([1, 1, 28, 28], np.float32)
         session.run(None, {"Input3": input})
 
