2bdb57bb0a02316e8eb2a5bad03d91711bd79ff2
Tianlei Wu <tlwu@microsoft.com>
2025-05-23 12:26:54 -0700

    Update MatMulBNits spec and Add Input Checks (#24828)
    ### Description
    
    Major changes of spec:
    * 2D scale shape: [N * n_blocks_per_col] => [N, n_blocks_per_col]
    * 2D zero shape: [N * CeilDiv(n_blocks_per_col * bits, 8)] => [N,
    CeilDiv(n_blocks_per_col * bits, 8)]
    * For B, drop int32 type and only allow uint8.
    * allow bfloat16 as input/output type.
    * Mark input g_idx as deprecated (since it has no benefit on model size
    and performance in inference).
    
    Add a function CheckInputs to verify the input shape.
    
    The reason of the shape change is to make scale and zero compatible with
    other operators like DequantizeLinear and GatherBlockQuantized. That
    will make it easy for graph fusion and model builder.
    
    Note that ORT can still handle the legacy 1D format for scale and zero
    points, and CUDA/CPU could still handle g_idx. However, they are
    deprecated, and our tools shall generate 2D scale and zeros, and avoid
    using g_idx going forward.
    
    This change is backward compatible. Model from old spec can run in
    latest ORT (CheckInputs handles 1D scale and zero points), and model
    from latest spec can still run in older ORT (since older ORT does not
    check dimension of scale and zero points)
    
    ### Motivation and Context
    
    CUDA and CPU provider does not check inputs for MatMulNBits. It could
    cause out of boundary access.
    
    We are going to share the lm_head weights of MatMulNBits to
    GatherBlockQuantized. 2D shape can be used in Gather directly, and we
    can avoid Reshape nodes.
    
    Our latest models published for foundry use 2D scale and zero points. So
    I update the spec to reflect that.

--- onnxruntime/test/contrib_ops/matmul_4bits_test.cc
+++ onnxruntime/test/contrib_ops/matmul_4bits_test.cc
@@ -81,6 +81,8 @@ struct TestOptions {
   bool has_g_idx{false};
   bool has_bias{false};
 
+  bool legacy_shape{false};  // for backward compatibility
+
   std::optional<float> output_abs_error{};
   std::optional<float> output_rel_error{};
 };
@@ -107,28 +109,20 @@ void RunTest(const TestOptions& opts,
 
   const bool zp_is_4bit = opts.zp_is_4bit || opts.has_g_idx;
 
-  const int64_t M = opts.M,
-                K = opts.K,
-                N = opts.N;
+  const int64_t M = opts.M;
+  const int64_t K = opts.K;
+  const int64_t N = opts.N;
 
   RandomValueGenerator random{1234};
   std::vector<float> input0_vals(random.Gaussian<float>(AsSpan({M, K}), 0.0f, 0.25f));
   std::vector<float> input1_f_vals(random.Gaussian<float>(AsSpan({K, N}), 0.0f, 0.25f));
 
-#if 0  // for Debugging
-  std::vector<float> input1_f_vals_trans(N * K);
-  MlasTranspose(input1_f_vals.data(), input1_f_vals_trans.data(), K, N);
-#endif
-
-  int q_rows, q_cols;
-  MlasBlockwiseQuantizedShape<float, QBits>(static_cast<int>(opts.block_size), /* columnwise */ true,
-                                            static_cast<int>(K), static_cast<int>(N),
-                                            q_rows, q_cols);
-
-  size_t q_data_size_in_bytes, q_scale_size, q_zp_size_in_bytes;
-  MlasBlockwiseQuantizedBufferSizes<QBits>(static_cast<int>(opts.block_size), /* columnwise */ true,
-                                           static_cast<int>(K), static_cast<int>(N),
-                                           q_data_size_in_bytes, q_scale_size, &q_zp_size_in_bytes);
+  int64_t k_blocks = (K + opts.block_size - 1) / opts.block_size;
+  int64_t blob_size = (opts.block_size * QBits + 7) / 8;
+  size_t q_scale_size = static_cast<size_t>(N * k_blocks);
+  size_t q_data_size_in_bytes = static_cast<size_t>(N * k_blocks * blob_size);  // packed as UInt4x2
+  const int64_t zero_point_blob_size = (k_blocks * QBits + 7) / 8;
+  size_t q_zp_size_in_bytes = static_cast<size_t>(N * zero_point_blob_size);  // packed as UInt4x2
 
   std::vector<uint8_t> input1_vals(q_data_size_in_bytes);
   std::vector<float> scales(q_scale_size);
@@ -141,16 +135,6 @@ void RunTest(const TestOptions& opts,
                      static_cast<int32_t>(N),
                      static_cast<int32_t>(K),
                      static_cast<int32_t>(opts.block_size));
-
-#if 0
-  for (int i = 0; i < input1_vals.size(); i++)
-  {
-    uint8_t byte = input1_vals[i];
-    uint8_t val_lo = byte & 0x0f;
-    uint8_t val_hi = byte >> 4;
-    std::cout << (int)val_lo << ", " << (int)val_hi << ", ";
-  }
-#endif
 
   const std::vector<int64_t> bias_shape = {N};
   const auto bias = [&]() -> std::optional<std::vector<float>> {
@@ -184,17 +168,22 @@ void RunTest(const TestOptions& opts,
     test.AddInput<T1>("A", {M, K}, input0_vals, false);
   }
 
-  test.AddInput<uint8_t>("B", {q_cols, q_rows}, input1_vals, true);
+  test.AddInput<uint8_t>("B", {N, k_blocks, blob_size}, input1_vals, true);
+
+  auto scales_shape = opts.legacy_shape ? std::vector<int64_t>{N * k_blocks}
+                                        : std::vector<int64_t>{N, k_blocks};
 
   if constexpr (use_float16) {
-    test.AddInput<T1>("scales", {static_cast<int64_t>(q_scale_size)}, ToFloat16(scales), true);
+    test.AddInput<T1>("scales", scales_shape, ToFloat16(scales), true);
   } else {
-    test.AddInput<T1>("scales", {static_cast<int64_t>(q_scale_size)}, scales, true);
+    test.AddInput<T1>("scales", scales_shape, scales, true);
   }
 
   if (opts.has_zero_point) {
     if (zp_is_4bit) {
-      test.AddInput<uint8_t>("zero_points", {static_cast<int64_t>(q_zp_size_in_bytes)}, zp, true);
+      auto zp_shape = opts.legacy_shape ? std::vector<int64_t>{N * zero_point_blob_size}
+                                        : std::vector<int64_t>{N, zero_point_blob_size};
+      test.AddInput<uint8_t>("zero_points", zp_shape, zp, true);
     } else {
       std::vector<float> zp_f;
       zp_f.reserve(q_zp_size_in_bytes * 2);
@@ -209,9 +198,9 @@ void RunTest(const TestOptions& opts,
       }
 
       if constexpr (use_float16) {
-        test.AddInput<T1>("zero_points", {static_cast<int64_t>(q_scale_size)}, ToFloat16(zp_f), true);
+        test.AddInput<T1>("zero_points", scales_shape, ToFloat16(zp_f), true);
       } else {
-        test.AddInput<T1>("zero_points", {static_cast<int64_t>(q_scale_size)}, zp_f, true);
+        test.AddInput<T1>("zero_points", scales_shape, zp_f, true);
       }
     }
   } else {
@@ -267,7 +256,7 @@ void RunTest(const TestOptions& opts,
 
 }  // namespace
 
-template <typename AType, int M, int N, int K, int block_size, int accuracy_level>
+template <typename AType, int M, int N, int K, int block_size, int accuracy_level, bool legacy_shape = false>
 void TestMatMulNBitsTyped() {
   TestOptions base_opts{};
   base_opts.M = M, base_opts.N = N, base_opts.K = K;
@@ -464,6 +453,13 @@ TEST(MatMulNBits, Float16_Accuracy4) {
   TestMatMulNBitsTyped<MLFloat16, 100, 288, 93, 128, 4>();
   TestMatMulNBitsTyped<MLFloat16, 100, 288, 1234, 16, 4>();
 }
+
+TEST(MatMulNBits, LegacyShape) {
+  constexpr bool legacy_shape = true;
+  TestMatMulNBitsTyped<float, 4, 2, 16, 16, 4, legacy_shape>();
+  TestMatMulNBitsTyped<MLFloat16, 1, 2, 16, 16, 4, legacy_shape>();
+}
+
 #endif
 #endif
 #endif
