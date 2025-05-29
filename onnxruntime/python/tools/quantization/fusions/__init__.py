onnxruntime/onnxruntime-qnn

[AISW-134068] Replace Upsample with Resize

onnxruntime/python/tools/quantization/fusions/__init__.py
@@ -1,3 +1,4 @@
 from .fusion import Fusion  # noqa: F401
 from .fusion_gelu import FusionGelu  # noqa: F401
 from .fusion_layernorm import FusionLayerNormalization  # noqa: F401
+from .replace_upsample_with_resize import replace_upsample_with_resize  # noqa: F401

microsoft/onnxruntime

onnxruntime/python/tools/quantization/fusions/__init__.py
@@ -1,3 +1,4 @@
 from .fusion import Fusion  # noqa: F401
 from .fusion_gelu import FusionGelu  # noqa: F401
 from .fusion_layernorm import FusionLayerNormalization  # noqa: F401
+from .replace_upsample_with_resize import ReplaceUpsampleWithResize  # noqa: F401