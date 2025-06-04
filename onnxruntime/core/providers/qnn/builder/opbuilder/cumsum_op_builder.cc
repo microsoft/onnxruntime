15e80042c17aa5791007659ebfddf51f7c7a563b
hungjuiw <hungjuiw@qti.qualcomm.com>
2025-06-03 19:01:50 -0700

    Revert "[AISW-134231] Addressed feedback comments"
    This reverts commit 1c5a97fc1024fed199da480d94d63c0aa4dbd879.

--- onnxruntime/core/providers/qnn/builder/opbuilder/cumsum_op_builder.cc
+++ onnxruntime/core/providers/qnn/builder/opbuilder/cumsum_op_builder.cc
@@ -8,50 +8,10 @@
 
 namespace onnxruntime {
 namespace qnn {
-namespace {
-
-Status GetOnnxAxis(QnnModelWrapper& qnn_model_wrapper, const NodeUnit& node_unit, uint32_t& onnx_axis) {
-  const auto& inputs = node_unit.Inputs();
-  TensorInfo axis_input_info = {};
-  ORT_RETURN_IF_ERROR(qnn_model_wrapper.GetTensorInfo(inputs[1], axis_input_info));
-  ORT_RETURN_IF_NOT(axis_input_info.is_initializer, "axis must be initializers");
-  std::vector<uint8_t> axis_unpacked_tensor;
-  ORT_RETURN_IF_ERROR(qnn_model_wrapper.UnpackInitializerData(*axis_input_info.initializer_tensor, axis_unpacked_tensor));
-  ORT_RETURN_IF_NOT(1 == static_cast<uint32_t>(axis_unpacked_tensor.size() / sizeof(axis_input_info.qnn_data_type)),
-                    "axis should be a single element");
-
-  int32_t axis = 0;
-  if (axis_input_info.qnn_data_type == QNN_DATATYPE_INT_64) {
-    axis = static_cast<int32_t>(*reinterpret_cast<const int64_t*>(axis_unpacked_tensor.data()));
-  } else {
-    axis = static_cast<int32_t>(*reinterpret_cast<const int32_t*>(axis_unpacked_tensor.data()));
-  }
-
-  std::vector<uint32_t> input_shape;
-  ORT_RETURN_IF_NOT(qnn_model_wrapper.GetOnnxShape(inputs[0].node_arg, input_shape), "Cannot get shape");
-
-  auto rank = static_cast<int32_t>(input_shape.size());
-  if (axis < 0) {
-    axis += rank;
-  }
-
-  ORT_RETURN_IF_NOT((axis >= 0 && axis < static_cast<int32_t>(input_shape.size())), "QNN requires axis range [0, rank-1].");
-
-  onnx_axis = static_cast<uint32_t>(axis);
-
-  return Status::OK();
-}
-
-}  // namespace
-
 class CumSumOpBuilder : public BaseOpBuilder {
  public:
   CumSumOpBuilder() : BaseOpBuilder("CumSumOpBuilder") {}
   ORT_DISALLOW_COPY_ASSIGNMENT_AND_MOVE(CumSumOpBuilder);
-
-  Status IsOpSupported(QnnModelWrapper& qnn_model_wrapper,
-                       const NodeUnit& node_unit,
-                       const logging::Logger& logger) const override ORT_MUST_USE_RESULT;
 
   Status ProcessInputs(QnnModelWrapper& qnn_model_wrapper,
                        const NodeUnit& node_unit,
@@ -64,11 +24,12 @@ class CumSumOpBuilder : public BaseOpBuilder {
                                      std::vector<std::string>&& input_names,
                                      const logging::Logger& logger,
                                      bool do_op_validation) const override ORT_MUST_USE_RESULT;
+
+ private:
+  Status ExplicitOpCheck(QnnModelWrapper& qnn_model_wrapper, const NodeUnit& node_unit) const;
 };
 
-Status CumSumOpBuilder::IsOpSupported(QnnModelWrapper& qnn_model_wrapper,
-                                      const NodeUnit& node_unit,
-                                      const logging::Logger& logger) const {
+Status CumSumOpBuilder::ExplicitOpCheck(QnnModelWrapper& qnn_model_wrapper, const NodeUnit& node_unit) const {
   const auto& inputs = node_unit.Inputs();
   ORT_RETURN_IF_NOT(qnn_model_wrapper.IsConstantInput(inputs[1].node_arg.Name()),
                     "QNN CumSum needs axis as a param, hence input[1] must be a constant.");
@@ -77,12 +38,10 @@ Status CumSumOpBuilder::IsOpSupported(QnnModelWrapper& qnn_model_wrapper,
   int64_t exclusive = node_helper.Get("exclusive", static_cast<int64_t>(0));
   int64_t reverse = node_helper.Get("reverse", static_cast<int64_t>(0));
 
-  // QNN HTP op validation passes for non-default values of attributes but fails in finalize.
-  // Hence adding the checks here.
   ORT_RETURN_IF_NOT(exclusive == 0, "QNN only supports default value 0 for exclusive attribute");
   ORT_RETURN_IF_NOT(reverse == 0, "QNN only supports default value 0 for reverse attribute");
 
-  return AddToModelBuilder(qnn_model_wrapper, node_unit, logger, true);
+  return Status::OK();
 }
 
 Status CumSumOpBuilder::ProcessInputs(QnnModelWrapper& qnn_model_wrapper,
@@ -90,9 +49,42 @@ Status CumSumOpBuilder::ProcessInputs(QnnModelWrapper& qnn_model_wrapper,
                                       const logging::Logger& logger,
                                       std::vector<std::string>& input_names,
                                       bool do_op_validation) const {
-  ORT_UNUSED_PARAMETER(do_op_validation);
+  if (do_op_validation) {
+    ORT_RETURN_IF_ERROR(ExplicitOpCheck(qnn_model_wrapper, node_unit));
+  }
+
   const auto& inputs = node_unit.Inputs();
   ORT_RETURN_IF_ERROR(ProcessInput(qnn_model_wrapper, inputs[0], logger, input_names));
+  return Status::OK();
+}
+
+Status GetOnnxAxis(QnnModelWrapper& qnn_model_wrapper, const NodeUnit& node_unit, int32_t& onnx_axis) {
+  const auto& inputs = node_unit.Inputs();
+  TensorInfo axis_input_info = {};
+  ORT_RETURN_IF_ERROR(qnn_model_wrapper.GetTensorInfo(inputs[1], axis_input_info));
+  ORT_RETURN_IF_NOT(axis_input_info.is_initializer, "axis must be initializers");
+  std::vector<uint8_t> axis_unpacked_tensor;
+  ORT_RETURN_IF_ERROR(qnn_model_wrapper.UnpackInitializerData(*axis_input_info.initializer_tensor, axis_unpacked_tensor));
+  ORT_RETURN_IF_NOT(1 == static_cast<uint32_t>(axis_unpacked_tensor.size() / sizeof(axis_input_info.qnn_data_type)),
+                    "axis should be a single element");
+  ORT_RETURN_IF_NOT((axis_input_info.qnn_data_type == QNN_DATATYPE_INT_32 || axis_input_info.qnn_data_type == QNN_DATATYPE_INT_64),
+                    "axis datatype should be either int32 or int64");
+  if (axis_input_info.qnn_data_type == QNN_DATATYPE_INT_64) {
+    onnx_axis = static_cast<int32_t>(*reinterpret_cast<const int64_t*>(axis_unpacked_tensor.data()));
+  } else {
+    onnx_axis = static_cast<int32_t>(*reinterpret_cast<const int32_t*>(axis_unpacked_tensor.data()));
+  }
+
+  std::vector<uint32_t> input_shape;
+  ORT_RETURN_IF_NOT(qnn_model_wrapper.GetOnnxShape(inputs[0].node_arg, input_shape), "Cannot get shape");
+
+  auto rank = static_cast<int32_t>(input_shape.size());
+  if (onnx_axis < 0) {
+    onnx_axis += rank;
+  }
+
+  ORT_RETURN_IF_NOT((onnx_axis >= 0 && onnx_axis < static_cast<int32_t>(input_shape.size())), "QNN requires axis range [0, rank-1].");
+
   return Status::OK();
 }
 
@@ -107,10 +99,10 @@ Status CumSumOpBuilder::ProcessAttributesAndOutputs(QnnModelWrapper& qnn_model_w
 
   // Add axis param
   Qnn_Scalar_t axis_qnn_scalar = QNN_SCALAR_INIT;
-  uint32_t onnx_axis = 0;
+  int32_t onnx_axis = 0;
   ORT_RETURN_IF_ERROR(GetOnnxAxis(qnn_model_wrapper, node_unit, onnx_axis));
   axis_qnn_scalar.dataType = QNN_DATATYPE_UINT_32;
-  axis_qnn_scalar.uint32Value = onnx_axis;
+  axis_qnn_scalar.uint32Value = static_cast<uint32_t>(onnx_axis);
   QnnParamWrapper axis_param(node_unit.Index(), node_unit.Name(), QNN_OP_CUMULATIVE_SUM_PARAM_AXIS, axis_qnn_scalar);
   param_tensor_names.push_back(axis_param.GetParamTensorName());
   qnn_model_wrapper.AddParamWrapper(std::move(axis_param));
