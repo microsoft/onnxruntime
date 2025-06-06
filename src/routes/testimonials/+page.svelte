225347c4c6bf440c9749d4765bea8d271673c3bf
Baiju Meswani <bmeswani@microsoft.com>
2025-06-06 20:12:06 +0000

    Correct typo

--- src/routes/testimonials/+page.svelte
+++ src/routes/testimonials/+page.svelte
@@ -139,7 +139,7 @@
 		{
 			title: 'Graiphic',
 			quote:
-				""With SOTA, we have developed the first complete ecosystem fully based on ONNX and ONNX Runtime. More than just supporting AI workloads, SOTA orchestrates graph-based computation at its core, enabling modular, scalable, and transparent execution across AI and non-AI domains alike. We believe ONNX is not just a format, it is the foundation for the future of graph-native computation.",
+				"With SOTA, we have developed the first complete ecosystem fully based on ONNX and ONNX Runtime. More than just supporting AI workloads, SOTA orchestrates graph-based computation at its core, enabling modular, scalable, and transparent execution across AI and non-AI domains alike. We believe ONNX is not just a format, it is the foundation for the future of graph-native computation.",
 			author: 'Youssef Menjour, CTO and Co-founder, Graiphic',
 			imgsrc: graiphicLogo,
 			imgalt: 'Graiphic logo'
