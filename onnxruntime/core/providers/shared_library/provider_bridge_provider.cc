b2673dfeac6bfb12dab72bbc6d82795eb7b4c528
Karim Vadsariya <kvadsariya@microsoft.com>
2025-06-09 14:45:19 -0700

    [MIGRAPHX] Add ORT generic interface build support for MigraphX

--- onnxruntime/core/providers/shared_library/provider_bridge_provider.cc
+++ onnxruntime/core/providers/shared_library/provider_bridge_provider.cc
@@ -361,7 +361,7 @@ std::unique_ptr<IDataTransfer> CreateGPUDataTransfer() {
 }
 #endif
 
-#ifdef USE_MIGRAPHX
+#if defined(USE_MIGRAPHX) || defined(USE_MIGRAPHX_INTERFACE)
 std::unique_ptr<IAllocator> CreateMIGraphXAllocator(int16_t device_id, const char* name) {
   return g_host->CreateMIGraphXAllocator(device_id, name);
 }
