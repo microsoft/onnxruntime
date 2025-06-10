1e4b6fe40182ce208c519d876863b212c28f037f
Ken Hoover <ken.hoover@instacart.com>
2025-06-10 16:34:04 +0000

    Add test for raising an exception when setting use_per_session_threads to True

--- onnxruntime/test/python/onnxruntime_test_python_global_threadpool.py
+++ onnxruntime/test/python/onnxruntime_test_python_global_threadpool.py
@@ -8,8 +8,11 @@ import onnxruntime as onnxrt
 
 
 class TestGlobalThreadPool(unittest.TestCase):
-    def test_global_threadpool(self):
+    @classmethod
+    def setUpClass(cls):
         onnxrt.set_global_thread_pool_sizes(2, 2)
+
+    def test_global_threadpool(self):
         session_opts = onnxrt.SessionOptions()
         session_opts.execution_mode = onnxrt.ExecutionMode.ORT_PARALLEL
         session_opts.graph_optimization_level = onnxrt.GraphOptimizationLevel.ORT_DISABLE_ALL
@@ -19,6 +22,14 @@ class TestGlobalThreadPool(unittest.TestCase):
         )
         input = np.ones([1, 1, 28, 28], np.float32)
         session.run(None, {"Input3": input})
+
+    def test_raise_error_if_use_per_session_threads(self):
+        session_opts = onnxrt.SessionOptions()
+        session_opts.execution_mode = onnxrt.ExecutionMode.ORT_PARALLEL
+        session_opts.graph_optimization_level = onnxrt.GraphOptimizationLevel.ORT_DISABLE_ALL
+        session_opts.use_per_session_threads = True
+        with self.assertRaises(RuntimeError):
+            onnxrt.InferenceSession(get_name("mnist.onnx"), session_opts, providers=onnxrt.get_available_providers())
 
 
 if __name__ == "__main__":
