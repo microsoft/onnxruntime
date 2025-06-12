7584a8aefd89578659e8f07265c456d1ab945cc4
Prathik Rao <prathik.rao@gmail.com>
2025-06-12 14:09:43 -0700

    c++ dtype check

--- onnxruntime/core/providers/webgpu/math/binary_elementwise_ops.cc
+++ onnxruntime/core/providers/webgpu/math/binary_elementwise_ops.cc
@@ -289,22 +289,21 @@ std::string GetPowImpl(int lhs_element_type, int /* rhs_element_type */) {
   if (lhs_element_type == ONNX_TENSOR_ELEMENT_DATA_TYPE_INT32) {
     round_str = "round";
   }
-  std::string is_float;
-  if (lhs_element_type == ONNX_TENSOR_ELEMENT_DATA_TYPE_FLOAT ||
-      lhs_element_type == ONNX_TENSOR_ELEMENT_DATA_TYPE_FLOAT16) {
-    is_float = "true";
-  } else {
-    is_float = "false";
+
+  SS(float_cond, 1024);
+  if (lhs_element_type == ONNX_TENSOR_ELEMENT_DATA_TYPE_FLOAT || lhs_element_type == ONNX_TENSOR_ELEMENT_DATA_TYPE_FLOAT16) {
+    float_cond << " else if (a >= input_a_element_t(0.0) && b == 0.5) {\n"
+                  "  return sqrt(a);\n"
+                  "  }\n";
   }
 
   s << "fn pow_custom(a : input_a_element_t, b : f32) -> input_a_element_t {\n"
        "  if (b == 0.0) {\n"
        "    return input_a_element_t(1.0);\n"
-       "  } else if (" << is_float << " && (a >= input_a_element_t(0.0) && b == 0.5)) {\n"
-       "    return sqrt(a);\n"
        "  } else if (a < input_a_element_t(0.0) && b != floor(b)) {\n"
        "    return input_a_element_t(pow(f32(a), b)); // NaN\n"
        "  }\n"
+    << SS_GET(float_cond)
     << "  return select(sign(a), input_a_element_t(1.0), round(abs(b) % 2.0) != 1.0) * input_a_element_t(" << round_str << "(pow(f32(abs(a)), b)));\n"
     << "}\n"
        "fn pow_v(a : vec4<input_a_element_t>, b : vec4<input_b_element_t>) -> vec4<input_a_element_t> {\n"
