7cf40b4949dc81b97b6bb5db6a2b3b296292cba4
Ken Hoover <k.hoover3@gmail.com>
2025-06-16 01:15:26 -0400

    Add python bindings to the global thread pool functionality (#24238)
    ### Description
    Allows users to configure and enable the global thread pool via Python,
    and have inference sessions use it instead of session-local thread
    pools.
    
    ### Motivation and Context
    Forked off of #23495 to take over implementation, see issue #23523.
    
    Our particular use case involves a single service instance serving
    thousands of individual models, each relatively small (e.g. small
    decision trees). Creating individual services for each model is too much
    overhead, and attempting to start several thousand thread-pools is a
    non-starter. We could possibly have each session be single-threaded, but
    we would like to be able to separate the request handler thread count
    from the compute thread count (e.g. 2 handler threads but 4 intra-op
    ones).
    <!-- - Why is this change required? What problem does it solve?
    - If it fixes an open issue, please link to the issue here. -->
    
    ---------
    
    Co-authored-by: alex-halpin <alex.halpin@prizepicks.com>

--- onnxruntime/core/session/environment.cc
+++ onnxruntime/core/session/environment.cc
@@ -208,17 +208,7 @@ Status Environment::Initialize(std::unique_ptr<logging::LoggingManager> logging_
 
   // create thread pools
   if (create_global_thread_pools) {
-    create_global_thread_pools_ = true;
-    OrtThreadPoolParams to = tp_options->intra_op_thread_pool_params;
-    if (to.name == nullptr) {
-      to.name = ORT_TSTR("intra-op");
-    }
-    intra_op_thread_pool_ = concurrency::CreateThreadPool(&Env::Default(), to, concurrency::ThreadPoolType::INTRA_OP);
-    to = tp_options->inter_op_thread_pool_params;
-    if (to.name == nullptr) {
-      to.name = ORT_TSTR("inter-op");
-    }
-    inter_op_thread_pool_ = concurrency::CreateThreadPool(&Env::Default(), to, concurrency::ThreadPoolType::INTER_OP);
+    ORT_RETURN_IF_ERROR(SetGlobalThreadingOptions(*tp_options));
   }
 
   ORT_TRY {
@@ -343,6 +333,24 @@ Internal copy node
     status = Status{ONNXRUNTIME, common::RUNTIME_EXCEPTION};
   }
   return status;
+}
+
+Status Environment::SetGlobalThreadingOptions(const OrtThreadingOptions& tp_options) {
+  if (create_global_thread_pools_) {
+    return ORT_MAKE_STATUS(ONNXRUNTIME, FAIL, "Global thread pools have already been created, cannot replace them");
+  }
+  create_global_thread_pools_ = true;
+  OrtThreadPoolParams to = tp_options.intra_op_thread_pool_params;
+  if (to.name == nullptr) {
+    to.name = ORT_TSTR("intra-op");
+  }
+  intra_op_thread_pool_ = concurrency::CreateThreadPool(&Env::Default(), to, concurrency::ThreadPoolType::INTRA_OP);
+  to = tp_options.inter_op_thread_pool_params;
+  if (to.name == nullptr) {
+    to.name = ORT_TSTR("inter-op");
+  }
+  inter_op_thread_pool_ = concurrency::CreateThreadPool(&Env::Default(), to, concurrency::ThreadPoolType::INTER_OP);
+  return Status::OK();
 }
 
 Status Environment::CreateAndRegisterAllocatorV2(const std::string& provider_type, const OrtMemoryInfo& mem_info,
