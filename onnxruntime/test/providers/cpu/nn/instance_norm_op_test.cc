3eeff82f1bc624a9734b748459cfd2d8cb245090
Prathik Rao <prathik.rao@gmail.com>
2025-06-18 08:47:06 -0700

    [WebGPU EP] fixes bugs in NCHW version of instance norm operator (#25092)
    The instance norm operator suffered from the following issues that this
    PR addresses:
    
    1. If {2, 80, 2} is the tensor shape, then there are 320 numbers. {2,
    80, 1} is the logical shape where each element is a vec2, so there are
    320 numbers as well. The InstanceNorm\<false\> code path was not passing
    the logical shape into the shader generation function causing incorrect
    output.
    2. The output_size was being divided by components which affects how
    many workers are dispatched. In the case of components=4, 75% of outputs
    for the InstanceNorm\<false\> code path were not updated and remained 0
    causing correctness issues.
    3. All the tests, including ones explicitly marked NCHW, were being run
    on the preferred data layout (NHWC).
    4. Typos and some implicit typing was fixed as well.
    
    P.S. Fixes pyannote model

--- onnxruntime/test/providers/cpu/nn/instance_norm_op_test.cc
+++ onnxruntime/test/providers/cpu/nn/instance_norm_op_test.cc
@@ -4,6 +4,7 @@
 #include "gtest/gtest.h"
 #include "test/providers/provider_test_utils.h"
 #include "test/common/tensor_op_test_utils.h"
+#include "test/util/include/default_providers.h"
 
 using namespace std;
 namespace onnxruntime {
@@ -289,6 +290,52 @@ TEST(InstanceNormalizationOpTest, InstanceNormNCHW) {
                                                            kTensorrtExecutionProvider,
                                                        });
 }
+
+#ifdef USE_WEBGPU
+TEST(InstanceNormalizationOpTest, InstanceNormNCHW_webgpu) {
+  OpTester test("InstanceNormalization");
+  test.AddAttribute("epsilon", 0.009999999776482582f);
+
+  vector<float> input = {1.0f, 2.0f, 3.0f, 2.0f, 2.0f, 2.0f};
+  vector<int64_t> input_dims = {1, 2, 1, 3};
+  test.AddInput<float>("input", input_dims, input);
+
+  vector<float> scale = {1.0f, 1.0f};
+  vector<int64_t> scale_dims = {2};
+  test.AddInput<float>("scale", scale_dims, scale);
+
+  vector<float> B = {0.0f, 2.0f};
+  vector<int64_t> B_dims = {2};
+  test.AddInput<float>("B", B_dims, B);
+
+  vector<float> expected_output = {-1.21566f, 0.0f, 1.21566f, 2.0f, 2.0f, 2.0f};
+  test.AddOutput<float>("Y", input_dims, expected_output);
+
+  test.ConfigEp(DefaultWebGpuExecutionProvider(false)).RunWithConfig();
+}
+
+TEST(InstanceNormalizationOpTest, InstanceNormNCHW_webgpu_2) {
+  OpTester test("InstanceNormalization");
+  test.AddAttribute("epsilon", 0.009999999776482582f);
+
+  vector<float> input = {1.0f, 2.0f, 3.0f, 2.0f, 2.0f, 2.0f, 2.0f, 2.0f};
+  vector<int64_t> input_dims = {1, 2, 2, 2};
+  test.AddInput<float>("input", input_dims, input);
+
+  vector<float> scale = {1.0f, 1.0f};
+  vector<int64_t> scale_dims = {2};
+  test.AddInput<float>("scale", scale_dims, scale);
+
+  vector<float> B = {0.0f, 2.0f};
+  vector<int64_t> B_dims = {2};
+  test.AddInput<float>("B", B_dims, B);
+
+  vector<float> expected_output = {-1.40028f, 0.0f, 1.40028f, 0.0f, 2.0f, 2.0f, 2.0f, 2.0f};
+  test.AddOutput<float>("Y", input_dims, expected_output);
+
+  test.ConfigEp(DefaultWebGpuExecutionProvider(false)).RunWithConfig();
+}
+#endif
 
 }  // namespace test
 }  // namespace onnxruntime
