e44601ccd1961f989c52afcc9701a5efaae53024
kunal-vaishnavi <115581922+kunal-vaishnavi@users.noreply.github.com>
2025-06-24 00:46:20 -0700

    Fix default value for Whisper's buffer sharing

--- onnxruntime/python/tools/transformers/models/whisper/whisper_helper.py
+++ onnxruntime/python/tools/transformers/models/whisper/whisper_helper.py
@@ -596,7 +596,7 @@ class WhisperHelper:
                 "no_repeat_ngram_size": 0,
                 "num_beams": 1,
                 "num_return_sequences": 1,
-                "past_present_share_buffer": True,
+                "past_present_share_buffer": provider == "cuda",  # Required for DMMHA CUDA kernel. Otherwise must be false for beam search to work in ORT GenAI.
                 "repetition_penalty": 1.0,
                 "temperature": 1.0,
                 "top_k": 1,
