87c5c8cd8b6bde9f0c17a6a9835b27a9f3ed474f
Tianlei Wu <tlwu@microsoft.com>
2025-06-26 15:47:14 -0700

    [CUDA] bfloat16 MatMulNBits (#25161)
    ### Description
    
    Support bfloat16 for MatMulNBits in CUDA.
    
    
    ### Motivation and Context
    
    For LLM model with bfloat16 data type.

---
+++ onnxruntime/core/providers/cuda/cuda_type_conversion.h
@@ -0,0 +1,92 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+#pragma once
+
+#include <cuda_fp16.h>
+#include <cuda_bf16.h>
+#if defined(ENABLE_FP8) && !defined(DISABLE_FLOAT8_TYPES)
+#include <cuda_fp8.h>
+#endif
+#include <type_traits>
+#include <cstdint>
+#include "core/framework/int4.h"
+#include "core/framework/float8.h"
+#include "core/framework/float16.h"
+
+namespace onnxruntime {
+namespace cuda {
+
+// Type mapping for ORT Type to CUDA Type
+template <typename T>
+struct OrtToCudaType {
+  using type = T;
+
+  static type FromFloat(float f) {
+    static_assert(std::is_same_v<T, float> || std::is_same_v<T, double>);
+    return static_cast<T>(f);
+  }
+};
+
+template <>
+struct OrtToCudaType<Int4x2> {
+  using type = int8_t;
+};
+
+template <>
+struct OrtToCudaType<UInt4x2> {
+  using type = uint8_t;
+};
+
+template <>
+struct OrtToCudaType<MLFloat16> {
+  using type = __half;
+  static type FromFloat(float f) {
+    return type(f);
+  }
+};
+
+template <>
+struct OrtToCudaType<BFloat16> {
+  using type = __nv_bfloat16;
+  static type FromFloat(float f) {
+    return type(f);
+  }
+};
+
+#if defined(ENABLE_FP8) && !defined(DISABLE_FLOAT8_TYPES)
+template <>
+struct OrtToCudaType<Float8E4M3FN> {
+  using type = __nv_fp8_e4m3;
+  static type FromFloat(float f) {
+    return type(f);
+  }
+};
+
+template <>
+struct OrtToCudaType<Float8E4M3FNUZ> {
+  using type = __nv_fp8_e4m3;
+  static type FromFloat(float f) {
+    return type(f);
+  }
+};
+
+template <>
+struct OrtToCudaType<Float8E5M2> {
+  using type = __nv_fp8_e5m2;
+  static type FromFloat(float f) {
+    return type(f);
+  }
+};
+
+template <>
+struct OrtToCudaType<Float8E5M2FNUZ> {
+  using type = __nv_fp8_e5m2;
+  static type FromFloat(float f) {
+    return type(f);
+  }
+};
+#endif
+
+}  // namespace cuda
+}  // namespace onnxruntime
