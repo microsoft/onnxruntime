cc25212988b4c76b71e4205d6f955e1b70e263c9
edgchen1 <18449977+edgchen1@users.noreply.github.com>
2025-06-26 19:27:33 -0700

    clean up code for linux gpu discovery

--- onnxruntime/core/platform/linux/device_discovery.cc
+++ onnxruntime/core/platform/linux/device_discovery.cc
@@ -30,23 +30,42 @@ OrtHardwareDevice GetCpuDevice() {
   return cpu_device;
 }
 
-bool ParseGpuSysfsPath(const fs::path& sysfs_path, size_t& idx) {
-  const auto filename = sysfs_path.filename();
-  const auto filename_str = std::string_view{filename.native()};
+std::vector<fs::path> DetectGpuSysfsPaths() {
+  const fs::path sysfs_class_drm_path = "/sys/class/drm";
 
-  // Look for a filename matching "cardN". N is a number.
-  constexpr std::string_view prefix = "card";
-  if (filename_str.find(prefix) != 0) {
-    return false;
+  if (!fs::exists(sysfs_class_drm_path)) {
+    return {};
   }
 
-  size_t parsed_idx{};
-  if (!TryParseStringWithClassicLocale<size_t>(filename_str.substr(prefix.size()), parsed_idx)) {
-    return false;
+  const auto detect_card_path = [](const fs::path& sysfs_path, size_t& idx) -> bool {
+    const auto filename = sysfs_path.filename();
+    const auto filename_str = std::string_view{filename.native()};
+
+    // Look for a filename matching "cardN". N is a number.
+    constexpr std::string_view prefix = "card";
+    if (filename_str.find(prefix) != 0) {
+      return false;
+    }
+
+    size_t parsed_idx{};
+    if (!TryParseStringWithClassicLocale<size_t>(filename_str.substr(prefix.size()), parsed_idx)) {
+      return false;
+    }
+
+    idx = parsed_idx;
+    return true;
+  };
+
+  std::vector<fs::path> gpu_sysfs_paths{};
+  for (const auto& dir_item : fs::directory_iterator{sysfs_class_drm_path}) {
+    auto dir_item_path = dir_item.path();
+
+    if (size_t idx{}; detect_card_path(dir_item_path, idx)) {
+      gpu_sysfs_paths.emplace_back(std::move(dir_item_path));
+    }
   }
 
-  idx = parsed_idx;
-  return true;
+  return gpu_sysfs_paths;
 }
 
 std::string ReadFileContents(const fs::path& file_path) {
@@ -57,14 +76,19 @@ std::string ReadFileContents(const fs::path& file_path) {
   return contents;
 }
 
+template <typename ValueType>
+ValueType ReadValueFromFile(const fs::path& file_path) {
+  const auto file_text = utils::TrimString(ReadFileContents(file_path));
+  return ParseStringWithClassicLocale<ValueType>(file_text);
+}
+
 OrtHardwareDevice GetGpuDevice(const fs::path& sysfs_path) {
   OrtHardwareDevice gpu_device{};
 
   // vendor id
   {
     const auto vendor_id_path = sysfs_path / "device" / "vendor";
-    const auto vendor_id_text = utils::TrimString(ReadFileContents(vendor_id_path));
-    gpu_device.vendor_id = ParseStringWithClassicLocale<uint32_t>(vendor_id_text);
+    gpu_device.vendor_id = ReadValueFromFile<uint32_t>(vendor_id_path);
   }
 
   // TODO vendor name
@@ -72,11 +96,10 @@ OrtHardwareDevice GetGpuDevice(const fs::path& sysfs_path) {
   // device id
   {
     const auto device_id_path = sysfs_path / "device" / "device";
-    const auto device_id_text = utils::TrimString(ReadFileContents(device_id_path));
-    gpu_device.device_id = ParseStringWithClassicLocale<uint32_t>(device_id_text);
+    gpu_device.device_id = ReadValueFromFile<uint32_t>(device_id_path);
   }
 
-  // TODO metadata["Discrete"]
+  // TODO metadata? e.g., is the device discrete?
 
   gpu_device.type = OrtHardwareDeviceType_GPU;
 
@@ -84,21 +107,13 @@ OrtHardwareDevice GetGpuDevice(const fs::path& sysfs_path) {
 }
 
 std::vector<OrtHardwareDevice> GetGpuDevices() {
+  const auto gpu_sysfs_paths = DetectGpuSysfsPaths();
   std::vector<OrtHardwareDevice> gpu_devices{};
+  gpu_devices.reserve(gpu_sysfs_paths.size());
 
-  const fs::path sysfs_class_drm_path = "/sys/class/drm";
-
-  if (!fs::exists(sysfs_class_drm_path)) {
-    return gpu_devices;
-  }
-
-  for (const auto& dir_item : fs::directory_iterator{sysfs_class_drm_path}) {
-    const auto& dir_item_path = dir_item.path();
-
-    if (size_t idx{}; ParseGpuSysfsPath(dir_item_path, idx)) {
-      auto gpu_device = GetGpuDevice(dir_item_path);
-      gpu_devices.emplace_back(std::move(gpu_device));
-    }
+  for (const auto& gpu_sysfs_path : gpu_sysfs_paths) {
+    auto gpu_device = GetGpuDevice(gpu_sysfs_path);
+    gpu_devices.emplace_back(std::move(gpu_device));
   }
 
   return gpu_devices;
@@ -120,6 +135,7 @@ std::unordered_set<OrtHardwareDevice> DeviceDiscovery::DiscoverDevicesForPlatfor
   }
 
   // get NPU devices
+  // TODO figure out how to discover these
 
   return devices;
 }
