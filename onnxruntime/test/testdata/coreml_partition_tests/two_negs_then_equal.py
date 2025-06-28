d41169a39dfe14e3dc383b45f8a4aa9d1bfdfaaf
carzh <wolfivyaura@gmail.com>
2025-06-27 22:53:08 -0700

    forgot to add test model generation scripts to lintrunner sigh

--- onnxruntime/test/testdata/coreml_partition_tests/two_negs_then_equal.py
+++ onnxruntime/test/testdata/coreml_partition_tests/two_negs_then_equal.py
@@ -1,53 +1,34 @@
 import onnx
-from onnx import helper, TensorProto, save_model
 
 # Define inputs
-input1 = helper.make_tensor_value_info("x1", TensorProto.INT64, ["dim"])
-input2 = helper.make_tensor_value_info("x2", TensorProto.INT64, ["dim"])
+input1 = onnx.helper.make_tensor_value_info("x1", onnx.TensorProto.INT64, ["dim"])
+input2 = onnx.helper.make_tensor_value_info("x2", onnx.TensorProto.INT64, ["dim"])
 
 # Intermediate outputs
 neg1_out = "neg1_out"
 neg2_out = "neg2_out"
 
 # Final output
-equal_out = helper.make_tensor_value_info("out", TensorProto.BOOL, ["dim"])
+equal_out = onnx.helper.make_tensor_value_info("out", onnx.TensorProto.BOOL, ["dim"])
 
 # Create the nodes
-neg1 = helper.make_node(
-    "Neg",
-    inputs=["x1"],
-    outputs=[neg1_out],
-    name="Neg_1"
-)
+neg1 = onnx.helper.make_node("Neg", inputs=["x1"], outputs=[neg1_out], name="Neg_1")
 
-neg2 = helper.make_node(
-    "Neg",
-    inputs=["x2"],
-    outputs=[neg2_out],
-    name="Neg_2"
-)
+neg2 = onnx.helper.make_node("Neg", inputs=["x2"], outputs=[neg2_out], name="Neg_2")
 
-equal = helper.make_node(
-    "Equal",
-    inputs=[neg1_out, neg2_out],
-    outputs=["out"],
-    name="Equal"
-)
+equal = onnx.helper.make_node("Equal", inputs=[neg1_out, neg2_out], outputs=["out"], name="Equal")
 
 # Create the graph
-graph = helper.make_graph(
-    nodes=[neg1, neg2, equal],
-    name="NegEqualGraph",
-    inputs=[input1, input2],
-    outputs=[equal_out]
+graph = onnx.helper.make_graph(
+    nodes=[neg1, neg2, equal], name="NegEqualGraph", inputs=[input1, input2], outputs=[equal_out]
 )
 
 # Create the model
-model = helper.make_model(graph, opset_imports=[helper.make_opsetid("", 13)])
+model = onnx.helper.make_model(graph, opset_imports=[onnx.helper.make_opsetid("", 13)])
 model.ir_version = onnx.IR_VERSION
 
 # Save to file
-save_model(model, "two_negs_then_equal.onnx")
+onnx.save_model(model, "two_negs_then_equal.onnx")
 print("Model saved as 'two_negs_then_equal.onnx'")
 
 onnx.checker.check_model(model)
--- onnxruntime/test/testdata/coreml_partition_tests/where_add_equal.py
+++ onnxruntime/test/testdata/coreml_partition_tests/where_add_equal.py
@@ -1,79 +1,63 @@
-import onnx
-from onnx import helper, TensorProto, checker, save_model
 import numpy as np
+import onnx
 
 # === Inputs ===
-cond_input = helper.make_tensor_value_info("cond", TensorProto.BOOL, [2, 3])
-x_input = helper.make_tensor_value_info("X", TensorProto.FLOAT, [2, 3])
+cond_input = onnx.helper.make_tensor_value_info("cond", onnx.TensorProto.BOOL, [2, 3])
+x_input = onnx.helper.make_tensor_value_info("X", onnx.TensorProto.FLOAT, [2, 3])
 
 # === Outputs ===
-neg_output = helper.make_tensor_value_info("neg_out", TensorProto.FLOAT, [2, 3])
-final_output = helper.make_tensor_value_info("final_out", TensorProto.FLOAT, [2, 3])
+neg_output = onnx.helper.make_tensor_value_info("neg_out", onnx.TensorProto.FLOAT, [2, 3])
+final_output = onnx.helper.make_tensor_value_info("final_out", onnx.TensorProto.FLOAT, [2, 3])
 
 # === Initializers ===
-Y_const = helper.make_tensor("Y_const", TensorProto.FLOAT, [2, 3], np.random.randn(2, 3).astype(np.float32).flatten())
-cmp_const = helper.make_tensor("cmp_const", TensorProto.FLOAT, [2, 3], np.random.randn(2, 3).astype(np.float32).flatten())
-true_val = helper.make_tensor("true_val", TensorProto.FLOAT, [2, 3], np.random.randn(2, 3).astype(np.float32).flatten())
-false_val = helper.make_tensor("false_val", TensorProto.FLOAT, [2, 3], np.random.randn(2, 3).astype(np.float32).flatten())
+Y_const = onnx.helper.make_tensor(
+    "Y_const", onnx.TensorProto.FLOAT, [2, 3], np.random.randn(2, 3).astype(np.float32).flatten()
+)
+cmp_const = onnx.helper.make_tensor(
+    "cmp_const", onnx.TensorProto.FLOAT, [2, 3], np.random.randn(2, 3).astype(np.float32).flatten()
+)
+true_val = onnx.helper.make_tensor(
+    "true_val", onnx.TensorProto.FLOAT, [2, 3], np.random.randn(2, 3).astype(np.float32).flatten()
+)
+false_val = onnx.helper.make_tensor(
+    "false_val", onnx.TensorProto.FLOAT, [2, 3], np.random.randn(2, 3).astype(np.float32).flatten()
+)
 
 # === Nodes ===
 
 # Where(cond, X, Y_const)
-where1 = helper.make_node(
-    "Where",
-    inputs=["cond", "X", "Y_const"],
-    outputs=["where1_out"],
-    name="Where1"
-)
+where1 = onnx.helper.make_node("Where", inputs=["cond", "X", "Y_const"], outputs=["where1_out"], name="Where1")
 
 # Neg(X)
-neg = helper.make_node(
-    "Neg",
-    inputs=["X"],
-    outputs=["neg_out"],
-    name="Neg"
-)
+neg = onnx.helper.make_node("Neg", inputs=["X"], outputs=["neg_out"], name="Neg")
 
 # Add(where1_out, X)
-add = helper.make_node(
-    "Add",
-    inputs=["where1_out", "X"],
-    outputs=["add_out"],
-    name="Add"
-)
+add = onnx.helper.make_node("Add", inputs=["where1_out", "X"], outputs=["add_out"], name="Add")
 
 # Equal(add_out, cmp_const)
-equal = helper.make_node(
-    "Equal",
-    inputs=["add_out", "cmp_const"],
-    outputs=["equal_out"],
-    name="Equal"
-)
+equal = onnx.helper.make_node("Equal", inputs=["add_out", "cmp_const"], outputs=["equal_out"], name="Equal")
 
 # Final Where(equal_out, true_val, false_val)
-where2 = helper.make_node(
-    "Where",
-    inputs=["equal_out", "true_val", "false_val"],
-    outputs=["final_out"],
-    name="Where2"
+where2 = onnx.helper.make_node(
+    "Where", inputs=["equal_out", "true_val", "false_val"], outputs=["final_out"], name="Where2"
 )
 
 # === Graph ===
-graph = helper.make_graph(
+graph = onnx.helper.make_graph(
     nodes=[where1, neg, add, equal, where2],
     name="WhereAddEqualGraph",
     inputs=[cond_input, x_input],
     outputs=[neg_output, final_output],
-    initializer=[Y_const, cmp_const, true_val, false_val]
+    initializer=[Y_const, cmp_const, true_val, false_val],
 )
 
 # === Model ===
-model = helper.make_model(graph, opset_imports=[helper.make_opsetid("", 18)])
+model = onnx.helper.make_model(graph, opset_imports=[onnx.helper.make_opsetid("", 18)])
 model.ir_version = onnx.IR_VERSION
 
 # === Check ===
-checker.check_model(model)
+onnx.checker.check_model(model)
 
 # === Save ===
-save_model(model, "where_add_equal.onnx")
+onnx.save_model(model, "where_add_equal.onnx")
 print("Model saved as 'where_add_equal.onnx'")
