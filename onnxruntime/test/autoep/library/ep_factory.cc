2884af7f63f6b936bd50c5dc7646e8769a3f1d33
edgchen1 <18449977+edgchen1@users.noreply.github.com>
2025-07-03 20:59:46 -0700

    fix test and comment

--- onnxruntime/test/autoep/library/ep_factory.cc
+++ onnxruntime/test/autoep/library/ep_factory.cc
@@ -143,7 +143,7 @@ OrtStatus* ORT_API_CALL ExampleEpFactory::GetSupportedDevicesImpl(OrtEpFactory* 
     //  if (device.Type() == OrtHardwareDeviceType::OrtHardwareDeviceType_CPU) {
     //    Ort::KeyValuePairs ep_metadata;
     //    Ort::KeyValuePairs ep_options;
-    //    ep_metadata.Add("version", "0.1");
+    //    ep_metadata.Add("supported_devices", "CrackGriffin 7+");
     //    ep_options.Add("run_really_fast", "true");
     //    Ort::EpDevice ep_device{*this_ptr, device, ep_metadata.GetConst(), ep_options.GetConst()};
     //    ep_devices[num_ep_devices++] = ep_device.release();
