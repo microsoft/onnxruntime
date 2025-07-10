3a72e7ae668d098a2f9fc62450ea37af48241c6e
Jianhui Dai <jianhui.j.dai@intel.com>
2025-07-10 11:33:47 +0800

    Fix TODOs and pass unittests

--- onnxruntime/contrib_ops/webgpu/quantization/matmul_nbits.h
+++ onnxruntime/contrib_ops/webgpu/quantization/matmul_nbits.h
@@ -15,7 +15,7 @@ using namespace onnxruntime::webgpu;
 class MatMulNBitsWideTileProgram final : public Program<MatMulNBitsWideTileProgram> {
  public:
   MatMulNBitsWideTileProgram(bool has_zero_points, uint32_t tile_m, uint32_t tile_n, uint32_t nbits)
-      : Program{"MatMulNBitsWideTileProgram"}, has_zero_points_{has_zero_points}, tile_m_(tile_m), tile_n_(tile_n), nbits_(nbits) {}
+      : Program{"MatMulNBitsWideTile"}, has_zero_points_{has_zero_points}, tile_m_(tile_m), tile_n_(tile_n), nbits_(nbits) {}
 
   Status GenerateShaderCode(ShaderHelper& sh) const override;
   WEBGPU_PROGRAM_DEFINE_UNIFORM_VARIABLES({"Batch", ProgramUniformVariableDataType::Uint32},
