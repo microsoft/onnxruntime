d4e737a79c2de4961ec35aa7bb9076dbda347421
Umang Bhatt <umangb@nvidia.com>
2025-07-17 19:44:37 +0530

    Comments addressed

--- onnxruntime/core/providers/cuda/cuda_graph.cc
+++ onnxruntime/core/providers/cuda/cuda_graph.cc
@@ -81,8 +81,10 @@ Status CUDAGraphManager::Replay(CudaGraphAnnotation_t cuda_graph_annotation_id) 
   if (!cuda_graph_set_.Contains(cuda_graph_annotation_id)) {
     return ORT_MAKE_STATUS(ONNXRUNTIME, FAIL, "No CUDA graph found for annotation ID: ", cuda_graph_annotation_id);
   }
-
+  cudaGraphExec_t graph_exec = cuda_graph_set_.Get(cuda_graph_annotation_id);
   CUDA_RETURN_IF_ERROR(cudaGraphLaunch(graph_exec, stream_));
+
+  CUDA_RETURN_IF_ERROR(cudaStreamSynchronize(stream_));
   return Status::OK();
 }
 
