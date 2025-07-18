88facb6e967d0953a28fe1ac647cdd92bd919d9b
iraut <iraut@nvidia.com>
2025-07-18 22:40:48 +0530

    !. added Ep name

--- onnxruntime/core/providers/nv_tensorrt_rtx/nv_provider_factory.cc
+++ onnxruntime/core/providers/nv_tensorrt_rtx/nv_provider_factory.cc
@@ -159,7 +159,12 @@ struct NvTensorRtRtxEpFactory : OrtEpFactory {
                          const OrtLogger& default_logger_in,
                          const char* ep_name,
                          OrtHardwareDeviceType hw_type)
+<<<<<<< HEAD
       : ort_api{ort_api_in}, default_logger{default_logger_in}, ep_name{ep_name}, ort_hw_device_type{hw_type} {
+=======
+      : ort_api{ort_api_in}, ep_name{ep_name}, ort_hw_device_type{hw_type} {
+    ort_version_supported = ORT_API_VERSION;
+>>>>>>> 81084a829 (!. added Ep name)
     GetName = GetNameImpl;
     GetVendor = GetVendorImpl;
     GetVersion = GetVersionImpl;
@@ -185,7 +190,6 @@ struct NvTensorRtRtxEpFactory : OrtEpFactory {
     const auto* factory = static_cast<const NvTensorRtRtxEpFactory*>(this_ptr);
     return factory->vendor_id;
   }
-
 
   static const char* GetVersionImpl(const OrtEpFactory* /*this_ptr*/) noexcept {
     return ORT_VERSION;
@@ -237,7 +241,7 @@ struct NvTensorRtRtxEpFactory : OrtEpFactory {
 
   const OrtApi& ort_api;
   const OrtLogger& default_logger;
-  const std::string ep_name;
+  const std::string ep_name{kNvTensorRTRTXExecutionProvider};
   const std::string vendor{"NVIDIA"};
 
   // NVIDIA vendor ID. Refer to the ACPI ID registry (search NVIDIA): https://uefi.org/ACPI_ID_List
