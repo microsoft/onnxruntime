ff13ed73f35c9cb6622db78c79e1fb242b68f0d8
Changming Sun <chasun@microsoft.com>
2025-07-19 23:50:36 -0700

    update

--- tools/ci_build/build.py
+++ tools/ci_build/build.py
@@ -188,9 +188,6 @@ def use_dev_mode(args):
         return False
     if is_macOS() and (args.ios or args.visionos or args.tvos):
         return False
-    SYSTEM_COLLECTIONURI = os.getenv("SYSTEM_COLLECTIONURI")  # noqa: N806
-    if SYSTEM_COLLECTIONURI and SYSTEM_COLLECTIONURI != "https://dev.azure.com/onnxruntime/":
-        return False
     return True
 
 
