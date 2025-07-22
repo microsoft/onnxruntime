0bc4189e58d3529590bbbcb0f70593c20e5ba734
Prathik Rao <prathik.rao@gmail.com>
2025-07-22 10:41:06 -0700

    fix

--- onnxruntime/test/providers/cpu/tensor/split_op_test.cc
+++ onnxruntime/test/providers/cpu/tensor/split_op_test.cc
@@ -397,6 +397,23 @@ TEST(SplitOperatorTest, ZeroSizeInput) {
   RunTest<float>(axis, {}, input, outputs, {kTensorrtExecutionProvider, kQnnExecutionProvider, kCoreMLExecutionProvider});
 }
 
+TEST(SplitOperatorTest, ZeroSizeOutput) {
+  constexpr int64_t axis = 1;
+  std::vector<ShapeAndFloatData> outputs;
+
+  // Non-zero input that will be split to produce zero-size outputs
+  ShapeAndFloatData input = {{2, 3}, {1.f, 2.f, 3.f, 4.f, 5.f, 6.f}};
+  
+  // Split sizes: 0, 2, 1 - first output will have zero size
+  std::vector<int64_t> splits{0, 2, 1};
+  
+  outputs.push_back({{2, 0}, {}});  // Zero-size output
+  outputs.push_back({{2, 2}, {1.f, 2.f, 4.f, 5.f}});
+  outputs.push_back({{2, 1}, {3.f, 6.f}});
+
+  RunTest<float>(axis, splits, input, outputs, {kTensorrtExecutionProvider, kQnnExecutionProvider, kCoreMLExecutionProvider}, false, true);
+}
+
 // test a split of a dimension that has leading and trailing dimensions
 TEST(SplitOperatorTest, Axis1SplitMiddleDimensionEqually) {
   constexpr int64_t axis = 1;
