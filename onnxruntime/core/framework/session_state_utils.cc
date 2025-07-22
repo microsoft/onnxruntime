f003c28f2733f036c05d988c27902f12c398ed62
Uros Petkovic <urpektov@amd.com>
2025-07-22 13:41:34 +0200

    Transfering memory to CPU

--- onnxruntime/core/framework/session_state_utils.cc
+++ onnxruntime/core/framework/session_state_utils.cc
@@ -126,7 +126,7 @@ static common::Status DeserializeTensorProto(const Env& env, const std::basic_st
       // 2. load initializer into CPU memory - deserialized_value,
       //    we will use mmap so no need to allocate memory on CPU in advance
       // 3. copy tensor from CPU to device - deserialized_value -> tensor -> ort_value
-      ORT_RETURN_IF_ERROR(AllocateTensor(memory_buffer, tensor, type, tensor_shape, use_device_allocator_for_initializers, alloc));
+      /* ORT_RETURN_IF_ERROR(AllocateTensor(memory_buffer, tensor, type, tensor_shape, use_device_allocator_for_initializers, alloc));
 
       OrtValue deserialized_value;
       ORT_RETURN_IF_ERROR(utils::GetExtDataFromTensorProto(env, proto_path, tensor_proto,
@@ -134,7 +134,12 @@ static common::Status DeserializeTensorProto(const Env& env, const std::basic_st
                                                            &prepacked_for_graph));
 
       return CopyTensorFromCPUToDevice(data_transfer_mgr, deserialized_value.Get<Tensor>(),
-                                       std::move(tensor), ort_value);
+                                       std::move(tensor), ort_value); */
+
+      ORT_RETURN_IF_ERROR(utils::GetExtDataFromTensorProto(env, proto_path, tensor_proto,
+                                                           ort_value,
+                                                           &prepacked_for_graph));
+      return common::Status::OK();
     }
   } else {
     // for internal initializer, always allocate memory on device - tensor
