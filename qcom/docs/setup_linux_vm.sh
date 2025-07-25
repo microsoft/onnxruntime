5e7f9aca963dd73db7da3f317f901edb232fb054
Romero, Kyle <kromero@qti.qualcomm.com>
2025-07-25 08:43:07 -0700

    [AISW-141517] Add automatic PR creation for merging upstream changes (#132)
    - Add dispatchable GitHub action that merges changes from upstream repository automatically

--- qcom/docs/setup_linux_vm.sh
+++ qcom/docs/setup_linux_vm.sh
@@ -18,6 +18,17 @@ set -x
 
 sudo apt install libc++-dev python3.10-dev python3.10-venv
 
+# Configure GPG keyrings and other dependencies for GitHub CLI
+(type -p wget >/dev/null || (sudo apt update && sudo apt-get install wget -y)) \
+	&& sudo mkdir -p -m 755 /etc/apt/keyrings \
+        && out=$(mktemp) && wget -nv -O$out https://cli.github.com/packages/githubcli-archive-keyring.gpg \
+        && cat $out | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
+	&& sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
+	&& sudo mkdir -p -m 755 /etc/apt/sources.list.d \
+	&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+	&& sudo apt update \
+	&& sudo apt install gh -y
+
 sudo -u ortqnnepci mkdir -p "${runner_home}"
 
 ##########################################
