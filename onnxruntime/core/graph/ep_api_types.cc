ba84595d2e59e467115868507a886dd4bc9a21d5
Chi Lo <Chi.Lo@microsoft.com>
2025-07-29 16:10:56 -0700

    remove redundnat code

--- onnxruntime/core/graph/ep_api_types.cc
+++ onnxruntime/core/graph/ep_api_types.cc
@@ -282,7 +282,6 @@ Status EpNode::GetTensorAttributeAsOrtValue(const OrtOpAttr* attribute, const Or
     return Status::OK();
   };
 
-  const auto* attr_proto = reinterpret_cast<const ONNX_NAMESPACE::AttributeProto*>(attribute);
   const auto& tensor_proto = attr_proto->t();
 
   // Create and cache an OrtValue for the 'TENSOR' attribute
