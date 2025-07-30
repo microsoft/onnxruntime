ce4e3931ac73b39462368ab0482b00bb942fbbe6
yifei <y.zhou@xilinx.com>
2025-07-29 18:42:05 -0700

    VitisAI: bump MAJOR VERSION

--- onnxruntime/core/providers/vitisai/include/vaip/vaip_ort_api.h
+++ onnxruntime/core/providers/vitisai/include/vaip/vaip_ort_api.h
@@ -13,7 +13,7 @@ struct OrtApi;
 
 namespace vaip_core {
 
-#define VAIP_ORT_API_MAJOR (17u)
+#define VAIP_ORT_API_MAJOR (18u)
 #define VAIP_ORT_API_MINOR (0u)
 #define VAIP_ORT_API_PATCH (0u)
 struct OrtApiForVaip {
