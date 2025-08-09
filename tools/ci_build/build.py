27f4dcd4a1860a52f1601b72f9c8a6767efebcf0
Adrian Lizarraga <adlizarraga@microsoft.com>
2025-08-09 06:12:29 -0700

    ORT 1.23.0 cherry-pick PRs 25629, 25583 (#25709)
    ### Description
    Cherry-pick the following PRs into `rel-1.23.0`:
    - https://github.com/microsoft/onnxruntime/pull/25629
    - https://github.com/microsoft/onnxruntime/pull/25583
    
    
    
    ### Motivation and Context
    <!-- - Why is this change required? What problem does it solve?
    - If it fixes an open issue, please link to the issue here. -->
    
    ---------
    
    Co-authored-by: Chunye Wang@AMD <Chunye.wang@amd.com>
    Co-authored-by: mingyue <mingyue@amd.com>
    Co-authored-by: Artur Wojcik <artur.wojcik@amd.com>
    Co-authored-by: urpetkov-amd <127323899+urpetkov-amd@users.noreply.github.com>
    Co-authored-by: Ted Themistokleous <107195283+TedThemistokleous@users.noreply.github.com>
    Co-authored-by: Ted Themistokleous <tedthemistokleous@amd.com>
    Co-authored-by: Scott McKay <Scott.McKay@microsoft.com>

--- tools/ci_build/build.py
+++ tools/ci_build/build.py
@@ -723,8 +723,6 @@ def generate_build_tree(
             cmake_args += ["-Donnxruntime_ENABLE_WEBASSEMBLY_RELAXED_SIMD=ON"]
     if args.use_migraphx:
         cmake_args.append("-Donnxruntime_MIGRAPHX_HOME=" + migraphx_home)
-        cmake_args.append("-Donnxruntime_ROCM_HOME=" + rocm_home)
-        cmake_args.append("-Donnxruntime_ROCM_VERSION=" + args.rocm_version)
 
     if args.use_tensorrt:
         cmake_args.append("-Donnxruntime_TENSORRT_HOME=" + tensorrt_home)
@@ -1994,6 +1992,7 @@ def build_nuget_package(
     use_winml,
     use_qnn,
     use_dml,
+    use_migraphx,
     enable_training_apis,
     msbuild_extra_options,
 ):
@@ -2031,6 +2030,9 @@ def build_nuget_package(
     elif use_tensorrt:
         execution_provider = "/p:ExecutionProvider=tensorrt"
         package_name = "/p:OrtPackageId=Microsoft.ML.OnnxRuntime.TensorRT"
+    elif use_migraphx:
+        execution_provider = "/p:ExecutionProvider=migraphx"
+        package_name = "/p:OrtPackageId=Microsoft.ML.OnnxRuntime.MIGraphX"
     elif use_dnnl:
         execution_provider = "/p:ExecutionProvider=dnnl"
         package_name = "/p:OrtPackageId=Microsoft.ML.OnnxRuntime.DNNL"
@@ -2622,6 +2624,7 @@ def main():
                 getattr(args, "use_winml", False),
                 args.use_qnn,
                 getattr(args, "use_dml", False),
+                args.use_migraphx,
                 args.enable_training_apis,
                 normalize_arg_list(args.msbuild_extra_options),
             )
