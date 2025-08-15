3890f20548861bcf391928a39d694b239fd1b51d
Sushanth Rajasankar <44513542+sushraja-msft@users.noreply.github.com>
2025-08-15 16:59:29 -0700

    Generation shader works

--- onnxruntime/contrib_ops/webgpu/quantization/dp4a_matmul_nbits.cc
+++ onnxruntime/contrib_ops/webgpu/quantization/dp4a_matmul_nbits.cc
@@ -68,6 +68,7 @@ Status ApplyDP4AMatrixMatMulNBits(const Tensor* a, const Tensor* b, const Tensor
                                   onnxruntime::webgpu::ComputeContext& context,
                                   Tensor* y) {
   constexpr uint32_t kVec4Components = 4;
+  constexpr uint32_t kVec2Components = 2;
   constexpr uint32_t kU32Components = 4;
 
   constexpr uint32_t kBlockSizeA = 128;
@@ -83,7 +84,7 @@ Status ApplyDP4AMatrixMatMulNBits(const Tensor* a, const Tensor* b, const Tensor
                    {&a_scale, ProgramTensorMetadataDependency::Rank, a_scale.Shape(), 1}});
   ORT_RETURN_IF_ERROR(context.RunProgram(quantize_program));
   const bool has_zero_points = zero_points != nullptr;
-  if (M < min_M_for_tile_optimization && nbits != 2) {
+  if (M < min_M_for_tile_optimization) {
     uint32_t tile_size_k_vec = 16;
     uint32_t tile_size = 32;
 
@@ -91,14 +92,14 @@ Status ApplyDP4AMatrixMatMulNBits(const Tensor* a, const Tensor* b, const Tensor
       tile_size_k_vec = 32;
       tile_size = 4;
     }
-
+    const uint32_t b_components = (nbits == 2 ? kVec2Components : kVec4Components);
     DP4AMatMulNBitsSmallMProgram mul_program{tile_size_k_vec, tile_size, nbits, has_zero_points};
     uint32_t num_N_tile = (N + tile_size - 1) / tile_size;
     mul_program.SetWorkgroupSize(128);
     mul_program.SetDispatchGroupSize(M * num_N_tile);
     mul_program.AddInputs({{&a_quant, ProgramTensorMetadataDependency::TypeAndRank, static_cast<int>(kVec4Components)},
                            {&a_scale, ProgramTensorMetadataDependency::TypeAndRank, 1},
-                           {b, ProgramTensorMetadataDependency::TypeAndRank, static_cast<int>(kVec4Components * kU32Components)},
+                           {b, ProgramTensorMetadataDependency::TypeAndRank, static_cast<int>(b_components * kU32Components)},
                            {scales, ProgramTensorMetadataDependency::TypeAndRank, 1}})
         .AddUniformVariables({M, N, K, K / 16, K / 32, block_size, num_N_tile, zero_blocks_per_col})
         .AddOutput({y, ProgramTensorMetadataDependency::TypeAndRank, 1})
