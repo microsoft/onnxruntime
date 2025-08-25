607c2d0fc3f5d98c69d42922edc4ec17c8430c76
Tianlei Wu <tlwu@microsoft.com>
2025-08-25 13:36:30 -0700

    link CUDA::nvrtc

--- cmake/onnxruntime_providers_cuda.cmake
+++ cmake/onnxruntime_providers_cuda.cmake
@@ -237,7 +237,7 @@
           message( WARNING "To compile with NHWC ops enabled please compile against cuDNN 9 or newer." )
         endif()
       endif()
-      target_link_libraries(${target} PRIVATE CUDA::cublasLt CUDA::cublas CUDNN::cudnn_all cudnn_frontend CUDA::curand CUDA::cufft CUDA::cudart
+      target_link_libraries(${target} PRIVATE CUDA::cublasLt CUDA::cublas CUDNN::cudnn_all cudnn_frontend CUDA::curand CUDA::cufft CUDA::cudart CUDA::nvrtc
               ${ABSEIL_LIBS} ${ONNXRUNTIME_PROVIDERS_SHARED} Boost::mp11 safeint_interface)
     endif()
 
