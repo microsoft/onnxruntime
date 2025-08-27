3a576b71446f86041913e46b16cfd007aa3e925b
Hari Seshadri <hasesh@microsoft.com>
2025-08-26 18:14:53 -0700

    Fix test

--- onnxruntime/test/framework/float_4_test.cc
+++ onnxruntime/test/framework/float_4_test.cc
@@ -37,7 +37,7 @@ TEST(Float4_Tests, BasicFloatConversion) {
       {std::pair<float, float>(-2.5f, 2.5),
        std::pair<float, float>(-2.f, 2.f)},
       {std::pair<float, float>(-1.25f, 1.25f),
-       std::pair<float, float>(-1.25f, 1.25f)},
+       std::pair<float, float>(-1.f, 1.f)},
       {std::pair<float, float>(std::numeric_limits<float>::infinity(),
                                -std::numeric_limits<float>::infinity()),
        std::pair<float, float>(6.f, -6.f)}};
