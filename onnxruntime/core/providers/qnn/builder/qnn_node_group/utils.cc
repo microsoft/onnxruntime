0a87431ce80c2016a2b378f47a8d594af25eeedf
quic-tirupath <quic_tirupath@quicinc.com>
2025-08-26 17:51:51 -0700

    [QNN EP] Add Case-2 LPBQ pattern support for Gemm and Matmul nodes
     - Case-2 LPBQ pattern omits QuantizeLinear node in LPBQ packing pattern
     - Modify LPBQ fusion logic in QNN EP implemented for Gemma and MatMul
       nodes to gracefully handle the optional QuantizeLinear node in LPBQ
       packing pattern.
     - Add unit tests to verify Case-2 LPBQ pattern fusion for Gemm and
       MatMul nodes.

--- onnxruntime/core/providers/qnn/builder/qnn_node_group/utils.cc
+++ onnxruntime/core/providers/qnn/builder/qnn_node_group/utils.cc
@@ -314,5 +314,69 @@ const NodeUnit* GetOnlyChildOfOutput(const GraphViewer& graph_viewer,
   return (child_count == 1) ? p_child_node_unit : nullptr;
 }
 
+const NodeUnit* GetParentOfInputByName(const GraphViewer& graph_viewer,
+                                       const NodeUnit& node_unit,
+                                       const std::string& input_name,
+                                       const std::unordered_map<const Node*, const NodeUnit*>& node_unit_map,
+                                       const std::unordered_map<const NodeUnit*, const IQnnNodeGroup*>& qnn_node_group_map) {
+  const Node* p_child_node = nullptr;
+
+  for (auto node : node_unit.GetAllNodesInGroup()) {
+    for (auto node_input : node->InputDefs()) {
+      if (node_input->Name() == input_name) {
+        p_child_node = node;
+        break;
+      }
+
+      if (p_child_node != nullptr) {
+        break;
+      }
+    }
+  }
+
+  if (p_child_node == nullptr) {
+    return nullptr;
+  }
+
+  const Node& child_node = *p_child_node;
+
+  for (auto edge = child_node.InputEdgesBegin(); edge != child_node.InputEdgesEnd(); ++edge) {
+    const Node& parent_node = edge->GetNode();
+    if (parent_node.OutputDefs()[0]->Name() != input_name) {
+      continue;
+    }
+
+    if (graph_viewer.GetNode(parent_node.Index()) == nullptr) {
+      // Node is not in this GraphViewer
+      return nullptr;
+    }
+
+    if (graph_viewer.NodeProducesGraphOutput(parent_node)) {
+      // Node is producing a graph output
+      return nullptr;
+    }
+
+    const auto parent_node_unit_it = node_unit_map.find(&parent_node);
+    if (parent_node_unit_it == node_unit_map.end()) {
+      return nullptr;
+    }
+    const NodeUnit* p_parent_node_unit = parent_node_unit_it->second;
+
+    // Check if parent node has already been handled. Should not be the case if the calling
+    // fusion function has been called in topological order, but check to be safe.
+    if (qnn_node_group_map.count(p_parent_node_unit) != 0) {
+      return nullptr;
+    }
+
+    // parent must not already be part of a QDQ NodeUnit (i.e., be standalone).
+    if (p_parent_node_unit->UnitType() != NodeUnit::Type::SingleNode) {
+      return nullptr;
+    }
+
+    return p_parent_node_unit;
+  }
+  return nullptr;
+}
+
 }  // namespace qnn
 }  // namespace onnxruntime
