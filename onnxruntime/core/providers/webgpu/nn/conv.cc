87eddf753e5d7209dc5d30ffbfe9ceec98792cae
Jianhui Dai <jianhui.j.dai@intel.com>
2025-09-01 15:35:35 +0800

    [webgpu] Normalize `Transpose` dispatch group size

--- onnxruntime/core/providers/webgpu/nn/conv.cc
+++ onnxruntime/core/providers/webgpu/nn/conv.cc
@@ -8,6 +8,15 @@
 #include "core/providers/webgpu/nn/grouped_conv.h"
 #include "core/providers/webgpu/webgpu_utils.h"
 #include "core/providers/webgpu/math/matmul.h"
+
+namespace {
+
+inline uint32_t ceil_div(int64_t numerator, int32_t denominator) {
+  return static_cast<uint32_t>((numerator + denominator - 1) / denominator);
+}
+
+}  // namespace
+
 namespace onnxruntime {
 namespace webgpu {
 
@@ -19,6 +28,13 @@ Status TransposeKernel(ComputeContext& context, const Tensor* kernel, const Tens
     transposed_kernel_shape_vector[i] = kernel_shape[perm[i]];
   }
   uint32_t output_size = onnxruntime::narrow<uint32_t>(kernel_shape.Size());
+
+  // Normalize dispatch = ceil_div(output_size, 64)
+  ORT_ENFORCE(rank == static_cast<size_t>(4), "Input tensor must have rank 4.");
+  uint32_t dispatch_x = onnxruntime::narrow<uint32_t>(transposed_kernel_shape_vector[0] * transposed_kernel_shape_vector[1]);
+  uint32_t dispatch_y = ceil_div(transposed_kernel_shape_vector[2], 8);
+  uint32_t dispatch_z = ceil_div(transposed_kernel_shape_vector[3], 8);
+
   TensorShape transposed_kernel_shape(transposed_kernel_shape_vector);
   *transposed_kernel = context.CreateGPUTensor(kernel->DataType(), transposed_kernel_shape);
   bool use_shared = false;
@@ -28,7 +44,8 @@ Status TransposeKernel(ComputeContext& context, const Tensor* kernel, const Tens
       .AddInput({kernel, ProgramTensorMetadataDependency::TypeAndRank, kernel_shape, 1})
       .AddOutput({transposed_kernel, ProgramTensorMetadataDependency::TypeAndRank})
       .AddUniformVariable({output_size})
-      .SetDispatchGroupSize((output_size + WORKGROUP_SIZE - 1) / WORKGROUP_SIZE);
+      .SetWorkgroupSize(64)
+      .SetDispatchGroupSize(dispatch_x, dispatch_y, dispatch_z);
   return context.RunProgram(program);
 }
 
