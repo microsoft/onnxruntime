e337c8897e8248739ae15a31590d588dc280c08d
Lifang Zhang <lifang.zhang@nokia.com>
2025-09-11 13:14:19 +0300

    Add void to empty parameter list to fix -Werror=strict-prototypes error
    Compiling C application dependent on onnxruntime with flag
    -Werror=strict-prototypes flag results in an error because the function
    declarations without parameters are considered to have an implicit int
    parameter list, which is not allowed in strict prototype mode. To fix
    this, we need to explicitly specify void in the parameter list of
    functions that do not take any arguments.
    
    Signed-off-by: Lifang Zhang <lifang.zhang@nokia.com>

--- include/onnxruntime/core/session/onnxruntime_c_api.h
+++ include/onnxruntime/core/session/onnxruntime_c_api.h
@@ -5108,7 +5108,7 @@ struct OrtApi {
    *
    * \since Version 1.22.
    */
-  const OrtModelEditorApi*(ORT_API_CALL* GetModelEditorApi)();
+  const OrtModelEditorApi*(ORT_API_CALL* GetModelEditorApi)(void);
 
   /** \brief Create an OrtValue for a Tensor that uses pre-existing memory.
    *
@@ -5166,7 +5166,7 @@ struct OrtApi {
    *
    * \since Version 1.22.
    */
-  const OrtCompileApi*(ORT_API_CALL* GetCompileApi)();
+  const OrtCompileApi*(ORT_API_CALL* GetCompileApi)(void);
 
   //
   // OrtKeyValuePairs
@@ -5434,6 +5434,7 @@ struct OrtApi {
    *
    * \since Version 1.22.
    */
+<<<<<<< Updated upstream
   const OrtEpApi*(ORT_API_CALL* GetEpApi)();
 
   /** \brief Compute total size in bytes of the tensor data contained in an OrtValue.
@@ -6580,6 +6581,9 @@ struct OrtApi {
                   _In_ size_t byte_size, _Outptr_ OrtExternalInitializerInfo** out);
 
   /// @}
+=======
+  const OrtEpApi*(ORT_API_CALL* GetEpApi)(void);
+>>>>>>> Stashed changes
 };
 
 /*
