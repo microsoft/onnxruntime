1db8d8b8a1242dbc9b6786cd49f22dcedd5aeaed
edgchen1 <18449977+edgchen1@users.noreply.github.com>
2025-09-11 18:38:54 -0700

    update include path

--- onnxruntime/test/unittest_util/framework_test_utils.cc
+++ onnxruntime/test/unittest_util/framework_test_utils.cc
@@ -1,6 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-#include "framework_test_utils.h"
+#include "test/unittest_util/framework_test_utils.h"
 #include "core/graph/graph.h"
 
 namespace onnxruntime {
