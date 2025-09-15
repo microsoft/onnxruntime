2c2ffef196cc77ca243ce3ecc1e73eca9ddf6bae
Justin Chu <justinchuby@users.noreply.github.com>
2025-09-15 10:57:05 -0700

    Update layer_norm.cc

--- onnxruntime/contrib_ops/cpu/layer_norm.cc
+++ onnxruntime/contrib_ops/cpu/layer_norm.cc
@@ -21,7 +21,7 @@ namespace contrib {
                                     .TypeConstraint("T", DataTypeImpl::GetTensorType<T>())                  \
                                     .TypeConstraint("U", DataTypeImpl::GetTensorType<T>())                  \
                                     .TypeConstraint("V", DataTypeImpl::GetTensorType<T>()),                 \
-                                LayerNorm<true>);
+                                LayerNorm<true>);                                                           \
   ONNX_OPERATOR_TYPED_KERNEL_EX(SimplifiedLayerNormalization, kMSDomain, 1, T, kCpuExecutionProvider,       \
                                 KernelDefBuilder()                                                          \
                                     .TypeConstraint("T", DataTypeImpl::GetTensorType<T>())                  \
