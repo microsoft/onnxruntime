c99838a4337f05fc00baf02f6b742f07a07e1275
Justin Chu <justinchuby@users.noreply.github.com>
2025-09-15 10:41:45 -0700

    Add SimplifiedLayerNormalization operator

--- onnxruntime/contrib_ops/cpu/layer_norm.cc
+++ onnxruntime/contrib_ops/cpu/layer_norm.cc
@@ -22,6 +22,12 @@ namespace contrib {
                                     .TypeConstraint("U", DataTypeImpl::GetTensorType<T>())                  \
                                     .TypeConstraint("V", DataTypeImpl::GetTensorType<T>()),                 \
                                 LayerNorm<true>);
+  ONNX_OPERATOR_TYPED_KERNEL_EX(SimplifiedLayerNormalization, kMSDomain, 1, T, kCpuExecutionProvider,       \
+                                KernelDefBuilder()                                                          \
+                                    .TypeConstraint("T", DataTypeImpl::GetTensorType<T>())                  \
+                                    .TypeConstraint("U", DataTypeImpl::GetTensorType<T>())                  \
+                                    .TypeConstraint("V", DataTypeImpl::GetTensorType<T>()),                 \
+                                LayerNorm<true>);
 
 REGISTER_CONTRIB_KERNELS(float)
 REGISTER_CONTRIB_KERNELS(double)
