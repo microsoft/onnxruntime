d2e64f8c1bcd0777289ed90cc97703b8549af047
wenjieh <wenjieh@amd.com>
2025-09-20 03:34:55 -0500

    format

--- csharp/src/Microsoft.ML.OnnxRuntime/SessionOptions.shared.cs
+++ csharp/src/Microsoft.ML.OnnxRuntime/SessionOptions.shared.cs
@@ -5,7 +5,6 @@ using System;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using System.Text;
-using System.Linq;
 
 namespace Microsoft.ML.OnnxRuntime
 {
@@ -429,7 +428,7 @@ namespace Microsoft.ML.OnnxRuntime
         /// <summary>
         /// Select execution providers from the list of available execution providers and devices returned by 
         /// GetEpDevices.
-        ///
+        /// 
         /// One or more OrtEpDevice instances may be provided in epDevices, but must all be for the same 
         /// execution provider.
         /// 
@@ -438,7 +437,7 @@ namespace Microsoft.ML.OnnxRuntime
         /// e.g. 
         ///   - if execution provider 'A' has an OrtEpDevice for NPU and one for GPU and you wish to use it for
         ///     both devices, pass the two OrtEpDevice instances in the epDevices list in one call.
-        ///   - if you wish to use execution provider 'B' for GPU and execution provider 'C' for CPU,
+        ///   - if you wish to use execution provider 'B' for GPU and execution provider 'C' for CPU, 
         ///     make two calls to AppendExecutionProvider, with one OrtEpDevice in the epDevices list in each call.
         ///     
         /// The priority of the execution providers is set by the order in which they are appended.
@@ -482,7 +481,7 @@ namespace Microsoft.ML.OnnxRuntime
                         env.Handle,
                         epDevicePtrs,
                         (UIntPtr)epDevices.Count,
-                        epOptionsKeys, 
+                        epOptionsKeys,  
                         epOptionsValues,
                         epOptionsCount));
             }
