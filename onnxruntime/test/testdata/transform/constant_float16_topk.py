f4afb107e0c197c926005d4dd0c7bdaa991840a9
nikhil sharma <nikhil.r.sharma@fujitsu.com>
2025-09-23 23:04:28 +0530

    Added the  script to generate constant_float16_topk.onnx

---
+++ onnxruntime/test/testdata/transform/constant_float16_topk.py
@@ -0,0 +1,19 @@
+import onnx
+from onnx import helper, TensorProto
+
+# input tensor
+X = helper.make_tensor_value_info("X", TensorProto.FLOAT16, [3, 4])
+
+# output tensors
+Values = helper.make_tensor_value_info("Values", TensorProto.FLOAT16, [3, 2])
+Indices = helper.make_tensor_value_info("Indices", TensorProto.INT64, [3, 2])
+
+# constant for k
+k_const = helper.make_tensor("k", TensorProto.INT64, [1], [2])
+
+node = helper.make_node("TopK", ["X", "k"], ["Values", "Indices"], axis=1)
+
+graph = helper.make_graph([node], "TopKGraph", [X], [Values, Indices], [k_const])
+model = helper.make_model(graph, opset_imports=[helper.make_opsetid("", 13)])
+
+onnx.save(model, "constant_float16_topk.onnx")
