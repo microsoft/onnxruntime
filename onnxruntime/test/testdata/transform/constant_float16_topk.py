347cd988a5bd431faba6734c772eccb99adf4792
nikhil sharma <nikhil.r.sharma@fujitsu.com>
2025-09-24 22:03:09 +0530

    Bot suggestion Fixup.

--- onnxruntime/test/testdata/transform/constant_float16_topk.py
+++ onnxruntime/test/testdata/transform/constant_float16_topk.py
@@ -1,19 +1,18 @@
 import onnx
-from onnx import TensorProto, helper
 
 # input tensor
-X = helper.make_tensor_value_info("X", TensorProto.FLOAT16, [3, 4])
+X = onnx.helper.make_tensor_value_info("X", onnx.TensorProto.FLOAT16, [3, 4])
 
 # output tensors
-Values = helper.make_tensor_value_info("Values", TensorProto.FLOAT16, [3, 2])
-Indices = helper.make_tensor_value_info("Indices", TensorProto.INT64, [3, 2])
+Values = onnx.helper.make_tensor_value_info("Values", onnx.TensorProto.FLOAT16, [3, 2])
+Indices = onnx.helper.make_tensor_value_info("Indices", onnx.TensorProto.INT64, [3, 2])
 
 # constant for k
-k_const = helper.make_tensor("k", TensorProto.INT64, [1], [2])
+k_const = onnx.helper.make_tensor("k", onnx.TensorProto.INT64, [1], [2])
 
-node = helper.make_node("TopK", ["X", "k"], ["Values", "Indices"], axis=1)
+node = onnx.helper.make_node("TopK", ["X", "k"], ["Values", "Indices"], axis=1)
 
-graph = helper.make_graph([node], "TopKGraph", [X], [Values, Indices], [k_const])
-model = helper.make_model(graph, opset_imports=[helper.make_opsetid("", 13)])
+graph = onnx.helper.make_graph([node], "TopKGraph", [X], [Values, Indices], [k_const])
+model = onnx.helper.make_model(graph, opset_imports=[onnx.helper.make_opsetid("", 13)])
 
 onnx.save(model, "constant_float16_topk.onnx")
