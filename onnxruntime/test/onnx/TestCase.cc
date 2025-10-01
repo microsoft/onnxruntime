67ce51b87ecbfba64e0aba8785681a1974cff92c
Ti-Tai Wang <titaiwang@microsoft.com>
2025-10-01 16:44:26 +0000

    add disable tests to QNN

--- onnxruntime/test/onnx/TestCase.cc
+++ onnxruntime/test/onnx/TestCase.cc
@@ -1435,6 +1435,20 @@ std::unique_ptr<std::set<BrokenTest>> GetBrokenTests(const std::string& provider
     broken_tests->insert({"scatter_elements_with_negative_indices", "unknown version"});
     // Fails since ONNX==1.19.0
     broken_tests->insert({"l2normalization_axis_0", "unknown version"});
+    broken_tests->insert({"attention_3d_gqa_attn_mask", "unknown version"});
+    broken_tests->insert({"attention_3d_gqa_causal", "unknown version"});
+    broken_tests->insert({"attention_3d_gqa_scaled", "unknown version"});
+    broken_tests->insert({"attention_3d_gqa_softcap", "unknown version"});
+    broken_tests->insert({"attention_3d_gqa_with_past_and_present", "unknown version"});
+    broken_tests->insert({"attention_4d_gqa", "unknown version"});
+    broken_tests->insert({"attention_4d_gqa_attn_mask", "unknown version"});
+    broken_tests->insert({"attention_4d_gqa_causal", "unknown version"});
+    broken_tests->insert({"attention_4d_gqa_scaled", "unknown version"});
+    broken_tests->insert({"attention_4d_gqa_softcap", "unknown version"});
+    broken_tests->insert({"attention_4d_gqa_with_past_and_present", "unknown version"});
+    broken_tests->insert({"attention_4d_gqa_with_past_and_present_fp16", "unknown version"});
+    broken_tests->insert({"attention_4d_with_past_and_present_qk_matmul_bias_3d_mask_causal", "unknown version"});
+    broken_tests->insert({"attention_4d_with_past_and_present_qk_matmul_bias_4d_mask_causal", "unknown version"});
     broken_tests->insert({"attention_4d_diff_heads_mask4d_padded_kv", "need nonpad_kv_seqlen "});
   }
 
