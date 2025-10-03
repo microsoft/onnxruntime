3915d7291de5b9ab8254f0e28a79c26a1dd0533d
edgchen1 <18449977+edgchen1@users.noreply.github.com>
2025-10-03 09:13:17 -0700

    check for target in cmake file and fix more build issues

--- cmake/onnxruntime_test_pch.cmake
+++ cmake/onnxruntime_test_pch.cmake
@@ -2,13 +2,13 @@
 
 if(CMAKE_CXX_COMPILER_ID MATCHES "MSVC")
   # Visual Studio PCH
-  if(NOT onnxruntime_MINIMAL_BUILD)
-    target_precompile_headers(onnxruntime_test_all PRIVATE
-      "${CMAKE_CURRENT_SOURCE_DIR}/test_pch.h"
-    )
+  target_precompile_headers(onnxruntime_test_all PRIVATE
+    "${CMAKE_CURRENT_SOURCE_DIR}/test_pch.h"
+  )
+  if (TARGET onnxruntime_provider_test)
     target_precompile_headers(onnxruntime_provider_test PRIVATE
       "${CMAKE_CURRENT_SOURCE_DIR}/test_pch.h"
-   )
+    )
   endif()
 endif()
 
