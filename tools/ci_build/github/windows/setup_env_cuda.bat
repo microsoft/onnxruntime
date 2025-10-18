7153cbc304fe78d94b5ae3ca9ac5b7eaa66e7c72
Tianlei WU <tlwu@microsoft.com>
2025-10-17 17:29:13 -0700

    Initial version

--- tools/ci_build/github/windows/setup_env_cuda.bat
+++ tools/ci_build/github/windows/setup_env_cuda.bat
@@ -1,17 +1,40 @@
 REM Copyright (c) Microsoft Corporation. All rights reserved.
 REM Licensed under the MIT License.
 
-if exist PATH=%AGENT_TEMPDIRECTORY%\v12.8\ (
-    set PATH=%AGENT_TEMPDIRECTORY%\v12.8\bin;%AGENT_TEMPDIRECTORY%\v12.8\extras\CUPTI\lib64;%PATH%
+REM --- Setup CUDA 12.8 ---
+REM Check if a local/agent-specific version exists
+if exist "%AGENT_TEMPDIRECTORY%\v12.8\" (
+    echo Using CUDA 12.8 from AGENT_TEMPDIRECTORY.
+    set "PATH=%AGENT_TEMPDIRECTORY%\v12.8\bin;%AGENT_TEMPDIRECTORY%\v12.8\extras\CUPTI\lib64;%PATH%"
 ) else (
-    set PATH=C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v12.8\bin;C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v12.8\extras\CUPTI\lib64;%PATH%
+    echo Using system default CUDA 12.8.
+    set "PATH=C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v12.8\bin;C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v12.8\extras\CUPTI\lib64;%PATH%"
 )
 
-@REM The default version is still cuda v12.8, because set cuda v11.8 after it
-if exist PATH=%AGENT_TEMPDIRECTORY%\v11.8\ (
-    set PATH=%PATH%;%AGENT_TEMPDIRECTORY%\v11.8\bin;%AGENT_TEMPDIRECTORY%\v11.8\extras\CUPTI\lib64
+REM --- Setup TensorRT for CUDA 12.8 ---
+set "TRT_12_8_PATH=%AGENT_TEMPDIRECTORY%\TensorRT-10.9.0.34.Windows10.x86_64.cuda-12.8\lib"
+if exist "%TRT_12_8_PATH%\" (
+    echo Adding TensorRT 10.9.0 (for CUDA 12.8) to PATH.
+    set "PATH=%TRT_12_8_PATH%;%PATH%"
 ) else (
-    set PATH=%PATH%;C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v11.8\bin;C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v11.8\extras\CUPTI\lib64
+    echo Warning: TensorRT 10.9.0 directory not found at %TRT_12_8_PATH%
 )
+
+
+REM --- [DISABLED] Setup for CUDA 13.0 ---
+@REM If uncommented, CUDA 13.0 would become the default, overriding 12.8.
+@REM set "TRT_13_0_PATH=%AGENT_TEMPDIRECTORY%\TensorRT-10.13.3.9.Windows.win10.cuda-13.0\lib"
+@REM if exist "%TRT_13_0_PATH%\" (
+@REM     set "PATH=%TRT_13_0_PATH%;%PATH%"
+@REM )
+@REM
+@REM if exist "%AGENT_TEMPDIRECTORY%\v13.0\" (
+@REM     set "PATH=%AGENT_TEMPDIRECTORY%\v13.0\bin;%AGENT_TEMPDIRECTORY%\v13.0\extras\CUPTI\lib64;%PATH%"
+@REM ) else (
+@REM     set "PATH=C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v13.0\bin;C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v13.0\extras\CUPTI\lib64;%PATH%"
+@REM )
+@REM --- [END DISABLED] ---
+
 
 set GRADLE_OPTS=-Dorg.gradle.daemon=false
+set CUDA_MODULE_LOADING=LAZY
