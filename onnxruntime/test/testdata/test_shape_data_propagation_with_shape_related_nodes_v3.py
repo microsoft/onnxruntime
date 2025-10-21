40c8d0979d625fceb5bbf1d766b6d0ac46a01bae
Chi Lo <Chi.Lo@microsoft.com>
2025-10-21 16:47:24 -0700

    lintrunner -a

--- onnxruntime/test/testdata/test_shape_data_propagation_with_shape_related_nodes_v3.py
+++ onnxruntime/test/testdata/test_shape_data_propagation_with_shape_related_nodes_v3.py
@@ -42,52 +42,63 @@ unsq2 = helper.make_node("Unsqueeze", ["mul_out", "axes_unsq2"], ["u2_out"])
 concat = helper.make_node("Concat", ["u0_out", "u1_out", "u2_out"], ["concat_out"], axis=0)
 
 # Axes initializers
-axes_u1 = helper.make_tensor('axes_u1', TensorProto.INT64, [1], [1])
-axes_u2 = helper.make_tensor('axes_u2', TensorProto.INT64, [1], [1])
-axes_s1 = helper.make_tensor('axes_s1', TensorProto.INT64, [1], [1])
-axes_s2 = helper.make_tensor('axes_s2', TensorProto.INT64, [1], [1])
+axes_u1 = helper.make_tensor("axes_u1", TensorProto.INT64, [1], [1])
+axes_u2 = helper.make_tensor("axes_u2", TensorProto.INT64, [1], [1])
+axes_s1 = helper.make_tensor("axes_s1", TensorProto.INT64, [1], [1])
+axes_s2 = helper.make_tensor("axes_s2", TensorProto.INT64, [1], [1])
 
 # First Unsqueeze
-unsqueeze1 = helper.make_node(
-    'Unsqueeze',
-    inputs=['concat_out', 'axes_u1'],
-    outputs=['u1'],
-    name='Unsqueeze_1'
-)
+unsqueeze1 = helper.make_node("Unsqueeze", inputs=["concat_out", "axes_u1"], outputs=["u1"], name="Unsqueeze_1")
 
 # Second Unsqueeze
-unsqueeze2 = helper.make_node(
-    'Unsqueeze',
-    inputs=['u1', 'axes_u2'],
-    outputs=['u2'],
-    name='Unsqueeze_2'
-)
+unsqueeze2 = helper.make_node("Unsqueeze", inputs=["u1", "axes_u2"], outputs=["u2"], name="Unsqueeze_2")
 
 # First Squeeze
-squeeze1 = helper.make_node(
-    'Squeeze',
-    inputs=['u2', 'axes_s1'],
-    outputs=['s1'],
-    name='Squeeze_1'
-)
+squeeze1 = helper.make_node("Squeeze", inputs=["u2", "axes_s1"], outputs=["s1"], name="Squeeze_1")
 
 # Second Squeeze
-squeeze2 = helper.make_node(
-    'Squeeze',
-    inputs=['s1', 'axes_s2'],
-    outputs=['squeeze_output'],
-    name='Squeeze_2'
-)
+squeeze2 = helper.make_node("Squeeze", inputs=["s1", "axes_s2"], outputs=["squeeze_output"], name="Squeeze_2")
 
 reshape = helper.make_node("Reshape", ["div_out", "squeeze_output"], ["output"])
 
 # === Graph ===
 graph = helper.make_graph(
-    [div, shape_left, shape_right, gather0, gather1, gather2, gather3, mul, unsq0, unsq1, unsq2, concat, unsqueeze1, unsqueeze2, squeeze1, squeeze2, reshape],
+    [
+        div,
+        shape_left,
+        shape_right,
+        gather0,
+        gather1,
+        gather2,
+        gather3,
+        mul,
+        unsq0,
+        unsq1,
+        unsq2,
+        concat,
+        unsqueeze1,
+        unsqueeze2,
+        squeeze1,
+        squeeze2,
+        reshape,
+    ],
     "Div_Shape_Gather_Concat_Reshape",
     [input_tensor],
     [output_tensor],
-    initializer=[B, g0_idx, g1_idx, g2_idx, g3_idx, axes_unsq0, axes_unsq1, axes_unsq2, axes_u1, axes_u2, axes_s1, axes_s2],
+    initializer=[
+        B,
+        g0_idx,
+        g1_idx,
+        g2_idx,
+        g3_idx,
+        axes_unsq0,
+        axes_unsq1,
+        axes_unsq2,
+        axes_u1,
+        axes_u2,
+        axes_s1,
+        axes_s2,
+    ],
 )
 
 # === Model ===
