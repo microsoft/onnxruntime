7d00f65ec37df58eb4fabe04a8b6c4f23522d38a
Changming Sun <chasun@microsoft.com>
2025-10-23 18:23:37 -0700

    Fix: Disable KleidiAI on systems with SME1 but not SME2
    This PR fixes an issue where KleidiAI was incorrectly enabled on systems that support SME1 but not SME2, leading to crashes.
    
    The issue was reported in #26377.
    
    The fix involves changing the check in onnxruntime/core/mlas/lib/platform.cpp to use HasArm_SME2() instead of HasArm_SME(). This ensures that KleidiAI is only enabled on systems that have SME2 support.

--- onnxruntime/core/mlas/lib/platform.cpp
+++ onnxruntime/core/mlas/lib/platform.cpp
@@ -601,7 +601,7 @@ Return Value:
     }
 
 #if defined(USE_KLEIDIAI) && !defined(_MSC_VER)
-    if(MLAS_CPUIDINFO::GetCPUIDInfo().HasArm_SME()){
+    if(MLAS_CPUIDINFO::GetCPUIDInfo().HasArm_SME2()){
         this->MlasGemmBatchOverride = ArmKleidiAI::MlasGemmBatch;
         this->MlasGemmPackBSizeOverride = ArmKleidiAI::MlasGemmPackBSize;
         this->MlasGemmPackBOverride = ArmKleidiAI::MlasGemmPackB;
