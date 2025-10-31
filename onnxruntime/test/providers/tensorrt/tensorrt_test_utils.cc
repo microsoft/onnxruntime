186c938c014019b2ca5d80320631db6419bc687c
Chi Lo <Chi.Lo@microsoft.com>
2025-10-31 12:30:11 -0700

    fix build warning

--- onnxruntime/test/providers/tensorrt/tensorrt_test_utils.cc
+++ onnxruntime/test/providers/tensorrt/tensorrt_test_utils.cc
@@ -1,6 +1,5 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-#pragma once
 
 #include "onnxruntime_cxx_api.h"
 #include "core/common/path_string.h"
