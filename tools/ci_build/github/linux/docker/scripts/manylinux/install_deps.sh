946adb643b89b57f8e795a50acfcb0410ad8c8c2
Tianlei Wu <tlwu@microsoft.com>
2025-11-03 13:27:49 -0800

    undo onnxscript install for python 3.14

--- tools/ci_build/github/linux/docker/scripts/manylinux/install_deps.sh
+++ tools/ci_build/github/linux/docker/scripts/manylinux/install_deps.sh
@@ -37,13 +37,6 @@ export CMAKE_ARGS="-DONNX_GEN_PB_TYPE_STUBS=OFF -DONNX_WERROR=OFF"
 
 for PYTHON_EXE in "${PYTHON_EXES[@]}"
 do
-  # For python 3.14, install onnxscript without dependencies (It depends on onnx, which does not suppport python 3.14 yet)
-  # This can be removed once onnx supports python 3.14: update requirements.txt to replace onnx_weekly with onnx.
-  if [[ "$PYTHON_EXE" == *cp314* ]]; then
-    ${PYTHON_EXE} -m pip install --upgrade pip setuptools wheel
-    ${PYTHON_EXE} -m pip install onnxscript ml_dtypes onnx_ir onnx_weekly typing_extensions packaging --no-deps
-  fi
-
   ${PYTHON_EXE} -m pip install -r "${0/%install_deps\.sh/requirements\.txt}"
 done
 
