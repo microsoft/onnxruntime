67a974e0bbb9d2d297a90cb435f9c22e26a8f4b4
Guenther Schmuelling <guschmue@microsoft.com>
2025-11-03 18:29:35 -0800

    webgpu qmoe

---
+++ onnxruntime/contrib_ops/webgpu/moe/moe_base.h
@@ -0,0 +1,40 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+#pragma once
+
+#include "core/providers/webgpu/program.h"
+#include "core/providers/webgpu/webgpu_kernel.h"
+
+namespace onnxruntime {
+namespace contrib {
+namespace webgpu {
+
+using namespace onnxruntime::webgpu;
+using onnxruntime::webgpu::ComputeContext;
+
+enum class MoEActivationType {
+  Relu,
+  Gelu,
+  Silu,
+  Identity,
+  SwiGLU,
+
+};
+
+enum class MoEQuantType {
+  None = 0,
+  UINT4 = 1,
+  UINT8 = 2,
+};
+
+enum class MoEParallelType {
+  None = 0,
+  EP = 1,
+  TP = 2,
+  EPAndTP = 3,
+};
+
+}  // namespace webgpu
+}  // namespace contrib
+}  // namespace onnxruntime
