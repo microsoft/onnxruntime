1f35c2e6182dc02da634212cbd0b2a4bb01e2ee5
Hari Seshadri <hasesh@microsoft.com>
2025-11-14 16:19:26 -0800

    Try keep reviewdog happy

--- tools/ci_build/github/linux/copy_strip_binary.sh
+++ tools/ci_build/github/linux/copy_strip_binary.sh
@@ -17,27 +17,27 @@ EXIT_CODE=1
 
 uname -a
 cd "$BINARY_DIR"
-mv installed/usr/local $ARTIFACT_NAME
+mv installed/usr/local "$ARTIFACT_NAME"
 # Do not ship onnx_test_runner
-rm -rf $ARTIFACT_NAME/bin
+rm -rf "$ARTIFACT_NAME"/bin
 echo "Copy debug symbols in a separate file and strip the original binary."
-if [[ $LIB_NAME == *.dylib ]]
+if [[ "$LIB_NAME" == *.dylib ]]
 then
-    dsymutil $BINARY_DIR/$ARTIFACT_NAME/lib/$LIB_NAME -o $BINARY_DIR/$ARTIFACT_NAME/lib/$LIB_NAME.dSYM
-    strip -S $BINARY_DIR/$ARTIFACT_NAME/lib/$LIB_NAME
+    dsymutil "$BINARY_DIR"/"$ARTIFACT_NAME"/lib/"$LIB_NAME" -o "$BINARY_DIR"/"$ARTIFACT_NAME"/lib/"$LIB_NAME".dSYM
+    strip -S "$BINARY_DIR"/"$ARTIFACT_NAME"/lib/"$LIB_NAME"
     # copy the CoreML EP header for macOS build (libs with .dylib ext)
-    cp $SOURCE_DIR/include/onnxruntime/core/providers/coreml/coreml_provider_factory.h  $BINARY_DIR/$ARTIFACT_NAME/include
+    cp "$SOURCE_DIR"/include/onnxruntime/core/providers/coreml/coreml_provider_factory.h  "$BINARY_DIR"/"$ARTIFACT_NAME"/include
 fi
 
-# copy the README, licence and TPN
-cp $SOURCE_DIR/README.md $BINARY_DIR/$ARTIFACT_NAME/README.md
-cp $SOURCE_DIR/docs/Privacy.md $BINARY_DIR/$ARTIFACT_NAME/Privacy.md
-cp $SOURCE_DIR/LICENSE $BINARY_DIR/$ARTIFACT_NAME/LICENSE
-cp $SOURCE_DIR/ThirdPartyNotices.txt $BINARY_DIR/$ARTIFACT_NAME/ThirdPartyNotices.txt
-cp $SOURCE_DIR/VERSION_NUMBER $BINARY_DIR/$ARTIFACT_NAME/VERSION_NUMBER
+# copy the README, license and TPN
+cp "$SOURCE_DIR"/README.md "$BINARY_DIR"/"$ARTIFACT_NAME"/README.md
+cp "$SOURCE_DIR"/docs/Privacy.md "$BINARY_DIR"/"$ARTIFACT_NAME"/Privacy.md
+cp "$SOURCE_DIR"/LICENSE "$BINARY_DIR"/"$ARTIFACT_NAME"/LICENSE
+cp "$SOURCE_DIR"/ThirdPartyNotices.txt "$BINARY_DIR"/"$ARTIFACT_NAME"/ThirdPartyNotices.txt
+cp "$SOURCE_DIR"/VERSION_NUMBER "$BINARY_DIR"/"$ARTIFACT_NAME"/VERSION_NUMBER
 
 
-echo $COMMIT_ID > $BINARY_DIR/$ARTIFACT_NAME/GIT_COMMIT_ID
+echo "$COMMIT_ID" > "$BINARY_DIR"/"$ARTIFACT_NAME"/GIT_COMMIT_ID
 
 EXIT_CODE=$?
 
