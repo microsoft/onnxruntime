f449e5be7e9bae36c2be276e951233d0a0e7bc31
Ti-Tai Wang <titaiwang@microsoft.com>
2025-11-18 03:11:57 +0000

    try always using lite

--- tools/python/util/vcpkg_helpers.py
+++ tools/python/util/vcpkg_helpers.py
@@ -82,14 +82,6 @@ endif()
         "-DONNX_USE_LITE_PROTO=ON"
     )"""
         )
-    else:
-        # keep default OFF for non-WebAssembly builds.
-        f.write(
-            r"""
-    list(APPEND VCPKG_CMAKE_CONFIGURE_OPTIONS
-        "-DONNX_USE_LITE_PROTO=OFF"
-    )"""
-        )
 
     f.write(r"""
 endif() # End ONNX-specific block
