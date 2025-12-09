e3f3cac4faead30ef648b05aeaf9b4fc7ca6902d
Romero, Kyle <kromero@qti.qualcomm.com>
2025-12-08 16:00:36 -0800

    [QNN] Add Genie libraries to QNN EP (#434)

--- setup.py
+++ setup.py
@@ -382,6 +382,7 @@ if platform.system() == "Linux" or platform.system() == "AIX":
     libs.append(providers_qnn)
     # QNN
     qnn_deps = [
+        "libGenie.so",
         "libQnnCpu.so",
         "libQnnGpu.so",
         "libQnnHtp.so",
@@ -443,6 +444,7 @@ else:
     libs.extend(["dxcompiler.dll", "dxil.dll"])
     # QNN V68/V73/V81 dependencies
     qnn_deps = [
+        "Genie.dll",
         "QnnCpu.dll",
         "QnnGpu.dll",
         "QnnHtp.dll",
