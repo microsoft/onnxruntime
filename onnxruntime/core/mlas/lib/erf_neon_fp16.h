add23db660315e8689b7be5cc7e663deb8e84cfe
akote123 <Aruna.K@fujitsu.com>
2025-12-09 17:46:55 +0530

    Enable Gelu Fp16
    Seperate platform dependant code

---
+++ onnxruntime/core/mlas/lib/erf_neon_fp16.h
@@ -0,0 +1,24 @@
+/*++
+
+Copyright 2025 FUJITSU LIMITED
+
+Module Name:
+
+   erf_neon_fp16.h
+
+Abstract:
+
+    This module contains the procedure prototypes for the ERF NEON FP16 intrinsics.
+
+--*/
+
+#pragma once
+
+#include <arm_neon.h>
+
+#include "mlasi.h"
+#include "fp16_common.h"
+#include "softmax_kernel_neon.h"
+
+using _mlas_fp16_ = uint16_t;
+void MlasNeonErfKernelFp16(const _mlas_fp16_* Input, _mlas_fp16_* Output, size_t N);
