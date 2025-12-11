9982f811a219772780e71b838b4fa8898eb8f9c6
Kyle Romero <kromero@qti.qualcomm.com>
2025-12-10 16:18:47 -0800

    [AISW-FIX] Fix issues with merge from upstream
    - Fix issue with new constructor of OrtEpGraphSupportInfo
    - Fix issue with session options not throwing in new EP plugin API

--- onnxruntime/test/providers/qnn-abi/qnn_basic_test.cc
+++ onnxruntime/test/providers/qnn-abi/qnn_basic_test.cc
@@ -202,7 +202,7 @@ TEST(QnnABIEP, TestDisableCPUFallback_ConflictingConfig) {
 TEST(QnnABIEP, TestInvalidSpecificationOfBothBackendTypeAndBackendPath) {
   Ort::SessionOptions so{};
 
-  onnxruntime::ProviderOptions options{};
+  onnxruntime::ProviderOptions options;
   options["backend_type"] = "cpu";
 #if defined(_WIN32)
   options["backend_path"] = "QnnCpu.dll";
@@ -215,13 +215,10 @@ TEST(QnnABIEP, TestInvalidSpecificationOfBothBackendTypeAndBackendPath) {
 
   const ORTCHAR_T* ort_model_path = ORT_MODEL_FOLDER "constant_floats.onnx";
 
-  try {
-    Ort::Session session(*ort_env, ort_model_path, so);
-    FAIL();
-  } catch (const Ort::Exception& e) {
-    ASSERT_EQ(e.GetOrtErrorCode(), ORT_FAIL);
-    ASSERT_THAT(e.what(), testing::HasSubstr("Only one of 'backend_type' and 'backend_path' should be set."));
-  }
+  Ort::Session session(*ort_env, ort_model_path, so);
+  ASSERT_FALSE(SessionHasEp(session, onnxruntime::kQnnABIExecutionProvider))
+      << "QNN EP was found in registered providers for session "
+      << "when both backend_type and backend_path were specified, which should not happen.";
 }
 
 #if defined(__aarch64__) || defined(_M_ARM64) || defined(__linux__)
