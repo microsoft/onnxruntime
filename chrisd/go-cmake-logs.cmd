788df42ab0321ae941475a45b78269a33e09bc0d
Chris Dern <chrisd@microsoft.com>
2025-12-14 10:26:12 -0700

    Working, with review comments.

---
+++ chrisd/go-cmake-logs.cmd
@@ -0,0 +1,6 @@
+mkdir build\.cmake\api\v1\query\
+echo > build\.cmake\api\v1\query\cache-v2
+echo > build\.cmake\api\v1\query\cmakeFiles-v1
+echo > build\.cmake\api\v1\query\codemodel-v2
+echo > build\.cmake\api\v1\query\toolchains-v1
+start build\.cmake\api\v1\query\
