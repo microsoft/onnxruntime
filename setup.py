2dfef6929ca8b39f1aa5a4383f3cb2248c1e9ac7
Edward Chen <18449977+edgchen1@users.noreply.github.com>
2025-12-15 08:57:21 -0800

    Revert "[QNN EP] Add support for building aarch64 wheels (#26634)"
    This reverts commit c244eb89ba432300a37074c84466005d985ceb15.

--- setup.py
+++ setup.py
@@ -117,8 +117,6 @@ manylinux_tags = [
     "manylinux2014_s390x",
     "manylinux_2_28_x86_64",
     "manylinux_2_28_aarch64",
-    "manylinux_2_34_x86_64",
-    "manylinux_2_34_aarch64",
 ]
 is_manylinux = environ.get("AUDITWHEEL_PLAT", None) in manylinux_tags
 
@@ -278,8 +276,6 @@ try:
 
                 cann_dependencies = ["libascendcl.so", "libacl_op_compiler.so", "libfmk_onnx_parser.so"]
 
-                qnn_dependencies = ["libcdsprpc.so"]
-
                 dest = "onnxruntime/capi/libonnxruntime_providers_openvino.so"
                 if path.isfile(dest):
                     subprocess.run(
@@ -298,18 +294,12 @@ try:
                 pass
 
             _bdist_wheel.run(self)
-            if is_manylinux and not disable_auditwheel_repair and not is_openvino:
+            if is_manylinux and not disable_auditwheel_repair and not is_openvino and not is_qnn:
                 assert self.dist_dir is not None
                 file = glob(path.join(self.dist_dir, "*linux*.whl"))[0]
                 logger.info("repairing %s for manylinux1", file)
                 auditwheel_cmd = ["auditwheel", "-v", "repair", "-w", self.dist_dir, file]
-                for i in (
-                    cuda_dependencies
-                    + migraphx_dependencies
-                    + tensorrt_dependencies
-                    + cann_dependencies
-                    + qnn_dependencies
-                ):
+                for i in cuda_dependencies + migraphx_dependencies + tensorrt_dependencies + cann_dependencies:
                     auditwheel_cmd += ["--exclude", i]
                 logger.info("Running %s", " ".join([shlex.quote(arg) for arg in auditwheel_cmd]))
                 try:
@@ -386,12 +376,7 @@ if platform.system() == "Linux" or platform.system() == "AIX":
     # QNN
     qnn_deps = [
         "libQnnCpu.so",
-        "libQnnGpu.so",
         "libQnnHtp.so",
-        "libQnnHtpPrepare.so",
-        "libQnnHtpV68Skel.so",
-        "libQnnHtpV68Stub.so",
-        "libQnnIr.so",
         "libQnnSaver.so",
         "libQnnSystem.so",
         "libHtpPrepare.so",
@@ -449,7 +434,6 @@ else:
         "QnnCpu.dll",
         "QnnGpu.dll",
         "QnnHtp.dll",
-        "QnnIr.dll",
         "QnnSaver.dll",
         "QnnSystem.dll",
         "QnnHtpPrepare.dll",
