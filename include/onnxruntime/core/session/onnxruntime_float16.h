352fb8ee5551584897638c2a7cf0c71f248b7f58
Xavier Dupré <xadupre@microsoft.com>
2025-12-16 09:14:35 +0000

    fix comment

--- include/onnxruntime/core/session/onnxruntime_float16.h
+++ include/onnxruntime/core/session/onnxruntime_float16.h
@@ -76,7 +76,7 @@ struct Float16Impl {
   static constexpr uint16_t kPositiveQNaNBits = 0x7E00U;
   static constexpr uint16_t kNegativeQNaNBits = 0xFE00U;
   static constexpr uint16_t kMaxValueBits = 0x7BFFU;  // Largest normal number
-  static constexpr uint16_t kMinValueBits = 0xFBFFU;  // Largest normal number
+  static constexpr uint16_t kMinValueBits = 0xFBFFU;  // Lowest normal number
   static constexpr uint16_t kOneBits = 0x3C00U;
   static constexpr uint16_t kMinusOneBits = 0xBC00U;
 
