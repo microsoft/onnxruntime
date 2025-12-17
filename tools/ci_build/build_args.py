d1848837e615579b6ebdd394e30875c3a6186d37
Tianlei Wu <tlwu@microsoft.com>
2025-12-17 12:21:19 -0800

    Update tools/ci_build/build_args.py
    Co-authored-by: Edward Chen <18449977+edgchen1@users.noreply.github.com>

--- tools/ci_build/build_args.py
+++ tools/ci_build/build_args.py
@@ -764,8 +764,8 @@ def add_execution_provider_args(parser: argparse.ArgumentParser) -> None:
     migx_group.add_argument("--use_migraphx", action="store_true", help="Enable MIGraphX EP.")
     migx_group.add_argument("--migraphx_home", help="Path to MIGraphX installation directory.")
     # --rocm_version and --rocm_home are deprecated. See https://github.com/microsoft/onnxruntime/issues/26801.
-    migx_group.add_argument("--rocm_version", help="ROCm stack version.")
-    migx_group.add_argument("--rocm_home", help="Path to ROCm installation directory.")
+    migx_group.add_argument("--rocm_version", help="ROCm stack version. This option is deprecated and has no effect.")
+    migx_group.add_argument("--rocm_home", help="Path to ROCm installation directory. This option is deprecated and has no effect.")
 
     # --- WebNN ---
     webnn_group = parser.add_argument_group("WebNN Execution Provider")
