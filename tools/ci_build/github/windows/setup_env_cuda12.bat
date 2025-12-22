71f23316ad4c08eac0d470b67abc63cd72d11364
Erik <erscor@microsoft.com>
2025-12-22 13:13:57 -0500

    Windows path fix

--- tools/ci_build/github/windows/setup_env_cuda12.bat
+++ tools/ci_build/github/windows/setup_env_cuda12.bat
@@ -12,7 +12,7 @@ if exist "%AGENT_TEMPDIRECTORY%\v12.9\" (
 )
 
 @REM --- Setup TensorRT for CUDA 12.9 ---
-set "TRT_12_9_PATH=%AGENT_TEMPDIRECTORY%\TensorRT-10.14.1.48.Windows10.x86_64.cuda-12.9\lib"
+set "TRT_12_9_PATH=%AGENT_TEMPDIRECTORY%\TensorRT-10.14.1.48.Windows.win10.x86_64.cuda-12.9\lib"
 if exist "%TRT_12_9_PATH%\" (
     echo "Adding TensorRT 10.14.1.48 for CUDA 12.9 to PATH."
     set "PATH=%TRT_12_9_PATH%;%PATH%"
