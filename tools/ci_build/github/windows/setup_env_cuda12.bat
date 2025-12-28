31f7702cc44c707ae8cfc14727f77e964ba6d62a
Erik <erscor@microsoft.com>
2025-12-28 11:33:17 -0500

    TRT Update, no 12.8 -> 12.9 bump.

--- tools/ci_build/github/windows/setup_env_cuda12.bat
+++ tools/ci_build/github/windows/setup_env_cuda12.bat
@@ -12,12 +12,12 @@ if exist "%AGENT_TEMPDIRECTORY%\v12.8\" (
 )
 
 @REM --- Setup TensorRT for CUDA 12.8 ---
-set "TRT_12_8_PATH=%AGENT_TEMPDIRECTORY%\TensorRT-10.9.0.34.Windows10.x86_64.cuda-12.8\lib"
+set "TRT_12_8_PATH=%AGENT_TEMPDIRECTORY%\TensorRT-10.14.1.48.Windows.win10.cuda-12.9\lib"
 if exist "%TRT_12_8_PATH%\" (
-    echo "Adding TensorRT 10.9.0 for CUDA 12.8 to PATH."
+    echo "Adding TensorRT 10.14.1.48 for CUDA 12.9 to PATH."
     set "PATH=%TRT_12_8_PATH%;%PATH%"
 ) else (
-    echo "Warning: TensorRT 10.9.0 directory not found at %TRT_12_8_PATH%"
+    echo "Warning: TensorRT 10.14.1.48 directory not found at %TRT_12_8_PATH%"
 )
 
 
