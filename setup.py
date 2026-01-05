648efff2967ca4ddfd66124f4ed4f17d480ee793
Pinapaka <apinapak@amd.com>
2026-01-05 18:13:52 +0530

    add headers in onnxrt wheel

--- setup.py
+++ setup.py
@@ -499,6 +499,14 @@ examples = [path.join("datasets", x) for x in examples_names]
 # Extra files such as EULA and ThirdPartyNotices (and Qualcomm License, only for QNN release packages)
 extra = ["LICENSE", "ThirdPartyNotices.txt", "Privacy.md", "Qualcomm_LICENSE.pdf"]
 
+# Header files from include/onnxruntime/core/session
+header_files = []
+session_headers_src = path.join("include", "onnxruntime", "core", "session")
+if path.isdir(session_headers_src):
+    header_files = [
+        path.join("include", "onnxruntime", "core", "session", "*.h")
+    ]
+
 # Description
 readme_file = "docs/python/ReadMeOV.rst" if is_openvino else "docs/python/README.rst"
 README = path.join(getcwd(), readme_file)
@@ -672,7 +680,7 @@ if enable_training or enable_training_apis:
 if package_name == "onnxruntime-tvm":
     packages += ["onnxruntime.providers.tvm"]
 
-package_data["onnxruntime"] = data + examples + extra
+package_data["onnxruntime"] = data + examples + extra + header_files
 
 version_number = ""
 with open("VERSION_NUMBER") as f:
