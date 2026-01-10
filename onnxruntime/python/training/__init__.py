0feeb245446a6b47e699526e5b7060d4a23258d3
Justin Chu <justinchuby@users.noreply.github.com>
2026-01-10 09:49:55 -0800

    Move source around
    Signed-off-by: Justin Chu <justinchuby@users.noreply.github.com>

--- onnxruntime/python/training/__init__.py
+++
@@ -1,6 +0,0 @@
-# -------------------------------------------------------------------------
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License.
-# --------------------------------------------------------------------------
-
-# This is a placeholder file. It is deployed with inference only wheel.
--- onnxruntime/test/testdata/logicaland.onnx
+++
@@ -1,16 +0,0 @@
-tf2onnx0.0.2.0:
-*
-input:0
-input1:0output:0output"Andtf2onnxRtestZ
-input1:0
-	
-
-Z
-input:0
-	
-
-b
-output:0
-	
-
-B
