448a7a410e306f195a2a0af181cb9631f95f8348
vraspar <vrajang@outlook.com>
2026-01-12 13:14:52 -0800

    Fix include issues

--- include/onnxruntime/core/framework/to_tensor_proto_element_type.h
+++ include/onnxruntime/core/framework/to_tensor_proto_element_type.h
@@ -13,6 +13,7 @@
 #include "core/framework/float4.h"
 #include "core/common/float8.h"
 #include "core/common/float16.h"
+#include "core/framework/int2.h"
 #include "core/framework/int4.h"
 
 namespace onnxruntime {
