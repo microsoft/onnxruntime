d67bb8808dc5b9d137d7902ca38336d19803978b
Vaisakh K V <vaiskv@qti.qualcomm.com>
2026-01-13 11:10:16 +0530

    Added build config to use QMX kernels

--- tools/ci_build/build.py
+++ tools/ci_build/build.py
@@ -886,6 +886,8 @@ def generate_build_tree(
 
     if not args.no_kleidiai:
         cmake_args += ["-Donnxruntime_USE_KLEIDIAI=ON"]
+        if args.use_qmx
+            cmake_args += ["-DENABLE_KLEIDIAI_QMX_COEXIST=ON"]
 
     if args.enable_arm_neon_nchwc:
         cmake_args += ["-Donnxruntime_USE_ARM_NEON_NCHWC=ON"]
