fc8e803d3d20ae9d53ea7a8bc7fbb8d0750f49cc
tirupath-qti <tirupath@qti.qualcomm.com>
2026-01-15 10:27:43 -0800

    [QNN EP] Add BFloat16 dtype support in QNN EP (#26987)
    ### Description
    - QNN NPU backend supports BFloat16 dtype for many operators
    - QNN EP adds a new session option "htp_bf16_enable" to enable Users to
    signal processing the Float32 graph in BFloat16 precision
    - When User specifies "htp_bf16_enable", the QNN EP lowers incoming
    Float32 Ort graph into BFloat16 QNN graph.
     - The ORT CPU fallback still receives Float32 partitions.
    - The lowered QNN graph still accepts float32 inputs, outputs and
    constant initializers. The QNN EP inserts Cast operators to do the
    necessary precision switch.
    
    ### Motivation and Context
    - This enables computing accuracy sensitive float32 models in bfloat16
    precision on Qualcomm NPU accelerator to improve inference time w.r.t
    computing in float32 precision.
    
    ---------
    
    Co-authored-by: Ashwath Shankarnarayan <ashwshan@qti.qualcomm.com>
    (cherry picked from commit 530a1fbbb07a79a7e37aa6f37bda8e677faa6caf)

--- onnxruntime/core/providers/qnn/builder/qnn_utils.cc
+++ onnxruntime/core/providers/qnn/builder/qnn_utils.cc
@@ -34,6 +34,7 @@ size_t GetElementSizeByType(const Qnn_DataType_t& data_type) {
       {QNN_DATATYPE_UINT_64, 8},
       {QNN_DATATYPE_FLOAT_16, 2},
       {QNN_DATATYPE_FLOAT_32, 4},
+      {QNN_DATATYPE_BFLOAT_16, 2},
       {QNN_DATATYPE_BOOL_8, 1},
       {QNN_DATATYPE_SFIXED_POINT_8, 1},
       {QNN_DATATYPE_SFIXED_POINT_16, 2},
@@ -201,6 +202,9 @@ std::ostream& operator<<(std::ostream& out, const Qnn_DataType_t& data_type) {
       break;
     case QNN_DATATYPE_FLOAT_32:
       out << "QNN_DATATYPE_FLOAT_32";
+      break;
+    case QNN_DATATYPE_BFLOAT_16:
+      out << "QNN_DATATYPE_BFLOAT_16";
       break;
     case QNN_DATATYPE_SFIXED_POINT_8:
       out << "QNN_DATATYPE_SFIXED_POINT_8";
