9fa0f9636fc29e0bd9cdcc13f3a78e95e2164bdb
Erik <erscor@microsoft.com>
2026-01-14 22:37:56 -0500

    Package validation

--- tools/ci_build/github/windows/extract_zip_files_gpu.ps1
+++ tools/ci_build/github/windows/extract_zip_files_gpu.ps1
@@ -11,15 +11,14 @@ Foreach-Object {
 
 # Rename tensorrt directory for later use in bundle_dlls_gpu.bat
 Get-ChildItem $Env:BUILD_BINARIESDIRECTORY\zip-artifacts | Where-Object { $_.Name -match 'onnxruntime-win-x64-tensorrt-cuda_\d{2}-\d{1,}\.\d{1,}\.\d{1,}$' } | Rename-Item -NewName $Env:BUILD_BINARIESDIRECTORY\zip-artifacts\onnxruntime-win-x64-tensorrt
-Get-ChildItem $Env:BUILD_BINARIESDIRECTORY\zip-artifacts | Where-Object { $_.Name -match 'onnxruntime-win-x64-cuda_\d{2}-\d{1,}\.\d{1,}\.\d{1,}$' } | Rename-Item -NewName $Env:BUILD_BINARIESDIRECTORY\zip-artifacts\onnxruntime-win-x64-cuda
 
 Remove-Item $Env:BUILD_BINARIESDIRECTORY\zip-artifacts\*.zip
 
 # Rename cuda directory to gpu directory and re-compress it for later use in bundle_dlls_gpu.bat
 Get-ChildItem $Env:BUILD_BINARIESDIRECTORY\zip-artifacts -Filter *cuda* |
 Foreach-Object {
- $($_.FullName) -match '.*onnxruntime-win-x64-cuda-(.*)'
- $version=$matches[1]
+ $($_.FullName) -match '.*onnxruntime-win-x64-cuda_(.*)-(.*)'
+ $version=$matches[2]
  Rename-Item -Path $($_.FullName) -NewName onnxruntime-win-x64-gpu-$version
  $cmd = "7z.exe a $Env:BUILD_BINARIESDIRECTORY\zip-artifacts\onnxruntime-win-x64-gpu-$version.zip $Env:BUILD_BINARIESDIRECTORY\zip-artifacts\onnxruntime-win-x64-gpu-$version"
  Write-Output $cmd
