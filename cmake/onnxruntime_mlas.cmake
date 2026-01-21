378e7cb92567e2f26ef6647ddf6fce00c4c52fe5
milpuz01 <milos.puzovic@arm.com>
2026-01-21 13:19:09 +0000

    mlas/arm64: add NEON conv asm kernels and tune NCHWC kernel selection
    Signed-off-by: Milos Puzovic <milos.puzovic@arm.com>

--- cmake/onnxruntime_mlas.cmake
+++ cmake/onnxruntime_mlas.cmake
@@ -466,6 +466,8 @@ else()
           ${MLAS_SRC_DIR}/aarch64/QgemmS8S8KernelSdot.S
           ${MLAS_SRC_DIR}/aarch64/SgemmKernelNeon.S
           ${MLAS_SRC_DIR}/aarch64/SgemvKernelNeon.S
+          ${MLAS_SRC_DIR}/aarch64/SconvDepthwiseKernelNeon.S
+          ${MLAS_SRC_DIR}/aarch64/SconvPointwiseKernelNeon.S
           ${MLAS_SRC_DIR}/aarch64/SymQgemmS8KernelNeon.S
           ${MLAS_SRC_DIR}/aarch64/SymQgemmS8KernelSdot.S
           ${MLAS_SRC_DIR}/aarch64/SymQgemmS8KernelSdotLd64.S
