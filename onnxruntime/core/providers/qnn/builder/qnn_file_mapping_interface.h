b507234ca6742631492da940250eb0acb0c8fa26
quic_calvnguy <quic_calvnguy@quic_inc.com>
2026-01-23 15:50:53 -0800

    linting

--- onnxruntime/core/providers/qnn/builder/qnn_file_mapping_interface.h
+++ onnxruntime/core/providers/qnn/builder/qnn_file_mapping_interface.h
@@ -15,20 +15,20 @@ namespace qnn {
 
 class FileMappingInterface {
  public:
-   typedef struct MappedWeightInfo {
+  typedef struct MappedWeightInfo {
     size_t aligned_offset = 0;
     size_t delta = 0;
     void* aligned_data_ptr = nullptr;
     void* unaligned_data_ptr = nullptr;
-   } MappedWeightInfo_t;
+  } MappedWeightInfo_t;
 
-   virtual ~FileMappingInterface() = default;
+  virtual ~FileMappingInterface() = default;
 
-   virtual Status GetContextBinMappedMemoryPtr(const std::string& bin_filepath,
-                                               void** mapped_data_ptr) = 0;
+  virtual Status GetContextBinMappedMemoryPtr(const std::string& bin_filepath,
+                                              void** mapped_data_ptr) = 0;
 
-   virtual MappedWeightInfo_t GetMappedWeightMemoryPtr(void* mapped_base_ptr,
-                                                       const size_t offset) = 0;
+  virtual MappedWeightInfo_t GetMappedWeightMemoryPtr(void* mapped_base_ptr,
+                                                      const size_t offset) = 0;
 };
 
 }  // namespace qnn
