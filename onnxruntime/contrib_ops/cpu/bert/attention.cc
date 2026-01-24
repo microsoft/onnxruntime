c2d0d2aced7e2a12e64bd9755a3583cd96559494
Hariharan Seshadri <shariharan91@gmail.com>
2026-01-24 13:12:06 -0800

    Update onnxruntime/contrib_ops/cpu/bert/attention.cc
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- onnxruntime/contrib_ops/cpu/bert/attention.cc
+++ onnxruntime/contrib_ops/cpu/bert/attention.cc
@@ -315,11 +315,12 @@ Status Attention<T>::Compute(OpKernelContext* context) const {
         } else {
           math::GemmEx<float, ThreadPool>(
               CblasNoTrans,                   // TransA = no
-              CblasNoTrans,                   // TransB = no
-              sequence_length,                // M      = S
-              head_size,                      // N      = H
-              input_hidden_size,              // K      = D
-              1.0f,                           // alpha
+              CblasNoTrans,                            // TransA = no
+              CblasNoTrans,                            // TransB = no
+              sequence_length,                         // M      = S
+              head_size,                               // N      = H
+              input_hidden_size,                       // K      = D
+              1.0f,                                    // alpha
               CblasNoTrans,                            // TransA = no
               CblasNoTrans,                            // TransB = no
               sequence_length,                         // M      = S
