8e26ebf60ac03aaf9a5fed1362bdaf46251e036f
Adam Pocock <adam.pocock@oracle.com>
2026-01-27 22:38:46 -0500

    Adding OrtAllocator.getStats() support to Java.

--- java/src/main/native/ai_onnxruntime_OrtAllocator.c
+++ java/src/main/native/ai_onnxruntime_OrtAllocator.c
@@ -1,11 +1,37 @@
 /*
- * Copyright (c) 2019, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2019, 2026, Oracle and/or its affiliates. All rights reserved.
  * Licensed under the MIT License.
  */
 #include <jni.h>
 #include "onnxruntime/core/session/onnxruntime_c_api.h"
 #include "OrtJniUtil.h"
 #include "ai_onnxruntime_OrtAllocator.h"
+
+/*
+ * Class:     ai_onnxruntime_OrtAllocator
+ * Method:    getStats
+ * Signature: (JJ)[[Ljava/lang/String;
+ */
+JNIEXPORT jobjectArray JNICALL Java_ai_onnxruntime_OrtAllocator_getStats
+  (JNIEnv * jniEnv, jclass jclazz, jlong apiHandle, jlong nativeHandle) {
+  (void) jclazz; // Required JNI parameter not needed by functions which don't need to access their host object.
+  const OrtApi* api = (const OrtApi*) apiHandle;
+  OrtAllocator* allocator = (OrtAllocator*) nativeHandle;
+  if (allocator->GetStats == NULL) {
+    // Throw invalid argument exception
+    throwOrtException(jniEnv, 2, "This allocator does not support GetStats.");
+    return NULL;
+  }
+  OrtKeyValuePairs* kvp;
+  OrtErrorCode code = checkOrtStatus(jniEnv, api, allocator->GetStats(allocator, &kvp));
+  if (code != ORT_OK) {
+    return NULL;
+  } else {
+    jobjectArray pair = convertOrtKeyValuePairsToArrays(jniEnv, api, kvp);
+    return pair;
+  }
+}
+
 /*
  * Class:     ai_onnxruntime_OrtAllocator
  * Method:    closeAllocator
