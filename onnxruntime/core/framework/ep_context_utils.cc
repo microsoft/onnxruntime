7164a083d112d71a5f3149b40bb2c7301a51bcf1
adrianlizarraga <adlizarraga@microsoft.com>
2026-01-28 17:05:31 -0800

    Early exit if ext initializer files are missing when compiling

--- onnxruntime/core/framework/ep_context_utils.cc
+++ onnxruntime/core/framework/ep_context_utils.cc
@@ -3,9 +3,11 @@
 
 #if !defined(ORT_MINIMAL_BUILD)
 #include <limits>
+#include <filesystem>
 #include <utility>
 #include "core/framework/ep_context_utils.h"
 #include "core/framework/error_code_helper.h"
+#include "core/framework/tensorprotoutils.h"
 #include "core/graph/model_saving_options.h"
 
 namespace onnxruntime {
@@ -52,6 +54,49 @@ Status EpContextModelToProto(const onnxruntime::Model& ep_context_model,
 
   return ORT_MAKE_STATUS(ONNXRUNTIME, FAIL, "Unexpected location for initializers while generating ",
                          validated_model_path);
+}
+
+static Status GetMissingExternalInitializerFilesImpl(const onnxruntime::Graph& graph,
+                                                     const std::filesystem::path& model_dir,
+                                                     std::unordered_set<std::filesystem::path>& checked_files,
+                                                     std::unordered_set<std::filesystem::path>& invalid_files) {
+  for (const auto& node : graph.Nodes()) {
+    if (node.ContainsSubgraph()) {
+      for (const auto& subgraph : node.GetSubgraphs()) {
+        ORT_RETURN_IF_ERROR(GetMissingExternalInitializerFilesImpl(*subgraph, model_dir,
+                                                                   checked_files, invalid_files));
+      }
+    }
+  }
+
+  for (const auto& [initializer_name, initializer_proto] : graph.GetAllInitializedTensors()) {
+    if (utils::HasExternalDataInFile(*initializer_proto)) {
+      std::unique_ptr<ExternalDataInfo> external_data_info;
+      ORT_RETURN_IF_ERROR(ExternalDataInfo::Create(initializer_proto->external_data(), external_data_info));
+
+      std::filesystem::path initializer_file_path = model_dir / external_data_info->GetRelPath();
+
+      if (checked_files.find(initializer_file_path) != checked_files.end()) {
+        continue;  // Already checked this file
+      }
+
+      if (!std::filesystem::exists(initializer_file_path)) {
+        invalid_files.insert(initializer_file_path);
+      }
+
+      checked_files.insert(std::move(initializer_file_path));
+    }
+  }
+
+  return Status::OK();
+}
+
+Status GetMissingExternalInitializerFiles(const onnxruntime::Graph& graph,
+                                          std::unordered_set<std::filesystem::path>& invalid_files) {
+  std::unordered_set<std::filesystem::path> checked_files;
+  const std::filesystem::path model_dir = graph.GetModel().ModelPath().parent_path();
+
+  return GetMissingExternalInitializerFilesImpl(graph, model_dir, checked_files, invalid_files);
 }
 
 //
