f88218e58e0e0a2571b482fe2e616000d047fcd3
Yulong Wang <7679871+fs-eire@users.noreply.github.com>
2026-02-12 16:55:16 -0800

    resolve comments - missing macro guard in allocator.h

--- include/onnxruntime/ep/adapter/allocator.h
+++ include/onnxruntime/ep/adapter/allocator.h
@@ -3,6 +3,10 @@
 
 #pragma once
 
+#if !defined(ORT_EP_API_ADAPTER_HEADER_INCLUDED)
+#error "This header should not be included directly. Include ep/_pch.h instead."
+#endif
+
 #include "core/framework/allocator.h"
 
 namespace onnxruntime {
