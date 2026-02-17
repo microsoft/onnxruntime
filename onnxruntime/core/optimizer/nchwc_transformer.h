abcec4633f814de7aa6c6b12ba9aa671cdfd9a69
Hari Seshadri <shariharan91@gmail.com>
2026-02-17 15:42:18 -0800

    Fix build

--- onnxruntime/core/optimizer/nchwc_transformer.h
+++ onnxruntime/core/optimizer/nchwc_transformer.h
@@ -16,14 +16,14 @@ and inserts nodes to reorder tensors as needed.
 */
 class NchwcTransformer : public GraphTransformer {
  public:
-  explicit NchwcTransformer(bool use_nchw_layout_for_large_conv = false) noexcept
+  explicit NchwcTransformer(bool disable_nchwc_layout_for_large_conv = false) noexcept
       : GraphTransformer("NchwcTransformer"),
-        use_nchw_layout_for_large_conv_(use_nchw_layout_for_large_conv) {}
+        disable_nchwc_layout_for_large_conv_(disable_nchwc_layout_for_large_conv) {}
 
  private:
   Status ApplyImpl(Graph& graph, bool& modified, int graph_level, const logging::Logger& logger) const override;
 
-  const bool use_nchw_layout_for_large_conv_;
+  const bool disable_nchwc_layout_for_large_conv_;
 };
 
 }  // namespace onnxruntime
