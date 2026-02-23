33609e3cda45884f46bfe48a6685c865d7ed0d19
Hari Seshadri <hasesh@microsoft.com>
2026-02-23 01:18:36 -0800

    Fixes

--- onnxruntime/core/providers/cpu/utils.h
+++ onnxruntime/core/providers/cpu/utils.h
@@ -3,13 +3,23 @@
 
 #pragma once
 
+#ifndef SHARED_PROVIDER
+#include "core/framework/config_options.h"
+#else
+#include "core/providers/shared_library/provider_api.h"
+#endif
+
 #include "core/mlas/inc/mlas.h"
 
 namespace onnxruntime {
 
-struct ConfigOptions;
+inline void SetupMlasBackendKernelSelectorFromConfigOptions(MLAS_BACKEND_KERNEL_SELECTOR_CONFIG* config,
+                                                            const ConfigOptions& config_options) {
+    if (config == nullptr) {
+        return;
+    }
 
-void SetupMlasBackendKernelSelectorFromConfigOptions(MLAS_BACKEND_KERNEL_SELECTOR_CONFIG* config,
-                                                     const ConfigOptions& config_options);
+    config->use_kleidiai = config_options.GetConfigEntry("mlas.disable_kleidiai") != "1";
+}
 
 }  // namespace onnxruntime
