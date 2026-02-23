c101131133d389883f4bb7da6c88c522fd3c9a95
Hariharan Seshadri <shariharan91@gmail.com>
2026-02-23 11:43:29 -0800

    Update onnxruntime/test/contrib_ops/matmul_4bits_test.cc
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- onnxruntime/test/contrib_ops/matmul_4bits_test.cc
+++ onnxruntime/test/contrib_ops/matmul_4bits_test.cc
@@ -499,7 +499,6 @@ TEST(MatMulNBits, Float16_4b_Accuracy4) {
   // See PR #27412 for details on the following test case,
   // which is added to cover a specific failure case in the past.
   TestMatMulNBitsTyped<MLFloat16, 369, 6144, 2048, 32, 4>();
-
 }
 
 TEST(MatMulNBits, LegacyShape_4b) {
