9e2940a894192876374486f19dde0ca61a4a4cb5
Hari Seshadri <hasesh@microsoft.com>
2026-02-27 12:51:12 -0800

    Add a simple test

--- onnxruntime/test/providers/cpu/cpu_execution_provider_test.cc
+++ onnxruntime/test/providers/cpu/cpu_execution_provider_test.cc
@@ -2,6 +2,7 @@
 // Licensed under the MIT License.
 
 #include "core/providers/cpu/cpu_execution_provider.h"
+#include "core/providers/cpu/mlas_backend_kernel_selector_config_utils.h"
 #include "gtest/gtest.h"
 
 namespace onnxruntime {
@@ -11,6 +12,25 @@ TEST(CPUExecutionProviderTest, MetadataTest) {
   auto provider = std::make_unique<CPUExecutionProvider>(info);
   EXPECT_TRUE(provider != nullptr);
   ASSERT_EQ(provider->GetOrtDeviceByMemType(OrtMemTypeDefault).Type(), OrtDevice::CPU);
+}
+
+TEST(CPUExecutionProviderTest, MlasBackendKernelSelectorDefaultsToKleidiAiEnabled) {
+  MLAS_BACKEND_KERNEL_SELECTOR_CONFIG config;
+  ConfigOptions config_options;
+
+  SetupMlasBackendKernelSelectorFromConfigOptions(config, config_options);
+
+  EXPECT_TRUE(config.use_kleidiai);
+}
+
+TEST(CPUExecutionProviderTest, MlasBackendKernelSelectorCanDisableKleidiAi) {
+  MLAS_BACKEND_KERNEL_SELECTOR_CONFIG config;
+  ConfigOptions config_options;
+  ASSERT_STATUS_OK(config_options.AddConfigEntry(kOrtSessionOptionsMlasDisableKleidiAi, "1"));
+
+  SetupMlasBackendKernelSelectorFromConfigOptions(config, config_options);
+
+  EXPECT_FALSE(config.use_kleidiai);
 }
 }  // namespace test
 }  // namespace onnxruntime
