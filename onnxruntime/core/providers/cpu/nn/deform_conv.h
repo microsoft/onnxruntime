08a33f47631b4d0ea5ee07442753cf9e62e64222
Shirasawa <764798966@qq.com>
2026-03-02 02:22:03 +0800

    Extract DeformConvAttributes to shared header

--- onnxruntime/core/providers/cpu/nn/deform_conv.h
+++ onnxruntime/core/providers/cpu/nn/deform_conv.h
@@ -6,31 +6,9 @@
 #include "core/common/common.h"
 #include "core/framework/op_kernel.h"
 #include "core/framework/op_node_proto_helper.h"
-#include "core/framework/tensor_shape.h"
+#include "deform_conv_attributes.h"
 
 namespace onnxruntime {
-
-// Attributes for ONNX DeformConv (opset 19+).
-// See https://onnx.ai/onnx/operators/onnx__DeformConv.html
-struct DeformConvAttributes {
-  explicit DeformConvAttributes(const OpKernelInfo& info) {
-    // Optional attributes.
-    // If not present, they will be empty/default, and handled in Compute.
-    (void)info.GetAttrs("kernel_shape", kernel_shape);
-    (void)info.GetAttrs("strides", strides);
-    (void)info.GetAttrs("pads", pads);
-    (void)info.GetAttrs("dilations", dilations);
-    group = info.GetAttrOrDefault<int64_t>("group", 1);
-    offset_group = info.GetAttrOrDefault<int64_t>("offset_group", 1);
-  }
-
-  TensorShapeVector kernel_shape;
-  TensorShapeVector strides;
-  TensorShapeVector pads;
-  TensorShapeVector dilations;
-  int64_t group{1};
-  int64_t offset_group{1};
-};
 
 template <typename T>
 class DeformConv : public OpKernel {
