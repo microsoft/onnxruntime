dc8070fc2215df0065144909c16133bcdf18a988
Yulong Wang <7679871+fs-eire@users.noreply.github.com>
2026-03-10 15:10:29 -0700

    remove unnecessary headers

--- js/node/src/ort_instance_data.cc
+++ js/node/src/ort_instance_data.cc
@@ -1,9 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-#include <atomic>
-#include <mutex>
-
 #include "common.h"
 #include "ort_instance_data.h"
 #include "ort_singleton_data.h"
