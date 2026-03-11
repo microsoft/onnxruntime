96923b4e0f09176462796bf7725ccc9d75e8e714
Sanaa Hamel <sanaahamel@microsoft.com>
2026-03-11 15:38:38 -0400

    feat: use ccache for reusable linux

--- tools/ci_build/github/linux/docker/scripts/manylinux/install_centos.sh
+++ tools/ci_build/github/linux/docker/scripts/manylinux/install_centos.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-os_major_version=$(tr -dc '0-9.' < /etc/redhat-release |cut -d \. -f1)
+os_major_version=$(tr -dc '0-9.' </etc/redhat-release | cut -d \. -f1)
 
 echo "installing for os major version : $os_major_version"
 if [ "$os_major_version" -gt 7 ]; then
@@ -11,3 +11,4 @@ fi
 
 # Install automatic documentation generation dependencies
 $PACKAGE_MANAGER install -y graphviz
+$PACKAGE_MANAGER install -y ccache
--- tools/ci_build/github/linux/docker/scripts/manylinux/install_ubuntuos.sh
+++ tools/ci_build/github/linux/docker/scripts/manylinux/install_ubuntuos.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -ex
 
-os_major_version=$(cat /etc/os-release | tr -dc '0-9.'|cut -d \. -f1)
+os_major_version=$(cat /etc/os-release | tr -dc '0-9.' | cut -d \. -f1)
 
 apt-get update -y && apt-get upgrade -y
 apt-get install -y software-properties-common
 
 echo "installing for os major version : $os_major_version"
-apt-get install -y gdb  build-essential tar unzip  make  aria2  bzip2
+apt-get install -y gdb build-essential ccache tar unzip make aria2 bzip2
 
 # Install Java
 # Install automatic documentation generation dependencies
