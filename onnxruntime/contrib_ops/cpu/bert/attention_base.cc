c01a8495189672f335ac6edf8a4bde2b13ba56d3
Tianlei Wu <tlwu@microsoft.com>
2026-03-11 19:10:18 -0700

    remove unused files

--- onnxruntime/contrib_ops/cpu/bert/attention_base.cc
+++
@@ -1,8 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
-// Implementations of AttentionBase member functions are now inline in attention_base.h
-// (under #ifndef SHARED_PROVIDER). This file is kept for the SHARED_PROVIDER build
-// where the provider bridge provides the definitions.
-
-#include "contrib_ops/cpu/bert/attention_base.h"
--- onnxruntime/contrib_ops/cpu/bert/longformer_attention_base.cc
+++
@@ -1,8 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
-// Implementation of LongformerAttentionBase::CheckInputs is now inline in longformer_attention_base.h
-// (under #ifndef SHARED_PROVIDER). This file is kept for the SHARED_PROVIDER build
-// where the provider bridge provides the definition.
-
-#include "longformer_attention_base.h"
