0edd61c66e0803bfcba7643ac7b5025239529d12
Hariharan Seshadri <shariharan91@gmail.com>
2026-03-14 14:25:41 -0700

    Update onnxruntime/test/mlas/unittest/test_conv2d_nchwc.cpp
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- onnxruntime/test/mlas/unittest/test_conv2d_nchwc.cpp
+++ onnxruntime/test/mlas/unittest/test_conv2d_nchwc.cpp
@@ -41,7 +41,7 @@ static size_t Conv2dNchwcRegistShortExecute() {
       count += Conv2dShortExecuteTest<MlasNchwcConv2DFilterSetTuningTest<true>>::RegisterSingleTest(
           1, 1, 96, 56, 56, 96, 3, 3, 1, 1, 1, 1, 1, 1, 1, 1);
       count += Conv2dShortExecuteTest<MlasNchwcConv2DMaxInputChannelTuningTest<true>>::RegisterSingleTest(
-        1, 1, 192, 28, 28, 96, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1);
+          1, 1, 192, 28, 28, 96, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1);
       count += Conv2dShortExecuteTest<MlasNchwcConv2DMaxInputChannelTuningTest<true>>::RegisterSingleTest(
           1, 1, 384, 14, 14, 96, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1);
     }
