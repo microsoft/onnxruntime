57505111d8f2c3863b546c0cf518a329a3e904ad
derdeljan-msft <derdeljan@microsoft.com>
2026-03-14 06:43:03 +0100

    Revert "[QNN EP] Fix error messages being logged as VERBOSE instead o… (#27650)
    ### Description
    
    Revert QNN SDK logging verbosity changes introduced in
    https://github.com/microsoft/onnxruntime/pull/24931.
    
    This reverts commit ec4f6bfa1f68ec7aee1608fb5ee3f7f0917330fe.
    
    
    ### Motivation and Context
    
    Loggin used to fail on QNN backend destruction (when releasing QNN
    context handles) with segmentation faults (even with empty user logging
    functions), hence reverting the changes.
    
    Co-authored-by: Tianlei Wu <tlwu@microsoft.com>

--- onnxruntime/core/providers/qnn/builder/qnn_backend_manager.h
+++ onnxruntime/core/providers/qnn/builder/qnn_backend_manager.h
@@ -270,9 +270,6 @@ class QnnBackendManager : public std::enable_shared_from_this<QnnBackendManager>
   Qnn_ErrorHandle_t ReleaseDmaData(Qnn_ContextBinaryDmaDataMem_t data_mem, void* mapped_base_ptr);
 #endif
 
-  QnnLog_Level_t MapOrtSeverityToQNNLogLevel(logging::Severity ort_log_level);
-  static logging::Severity MapQNNLogLevelToOrtSeverity(QnnLog_Level_t qnn_log_level);
-
 #ifdef QNN_FILE_MAPPED_WEIGHTS_AVAILABLE
   typedef struct FileMappingCallbackInfo {
     void* const mapped_file_ptr;
@@ -379,6 +376,7 @@ class QnnBackendManager : public std::enable_shared_from_this<QnnBackendManager>
 
   const char* QnnProfileErrorToString(QnnProfile_Error_t error);
   std::string QnnErrorHandleToString(Qnn_ErrorHandle_t error);
+  QnnLog_Level_t MapOrtSeverityToQNNLogLevel(logging::Severity ort_log_level);
 
   // Adds a new QNN context.
   // Transfers ownership of `context_handle` (i.e., responsibility of freeing it) to this instance
