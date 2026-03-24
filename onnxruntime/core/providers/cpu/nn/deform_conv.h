484b5b5c3c1f5b1a4b8b50187e6df0cf96818ae0
Shirasawa <764798966@qq.com>
2026-03-25 02:37:25 +0800

    Optimize DeformConv CPU kernel by removing mutex and heap allocations

--- onnxruntime/core/providers/cpu/nn/deform_conv.h
+++ onnxruntime/core/providers/cpu/nn/deform_conv.h
@@ -7,13 +7,8 @@
 #include "core/framework/op_kernel.h"
 #include "core/framework/op_node_proto_helper.h"
 #include "deform_conv_attributes.h"
-#include <memory>
-#include <mutex>
 
 namespace onnxruntime {
-
-template <typename T>
-struct DeformConvKernelMetaCacheData;
 
 template <typename T>
 class DeformConv : public OpKernel {
@@ -24,8 +19,6 @@ class DeformConv : public OpKernel {
 
  private:
   DeformConvAttributes attrs_;
-  mutable std::mutex kernel_meta_cache_mu_;
-  mutable std::shared_ptr<const DeformConvKernelMetaCacheData<T>> kernel_meta_cache_;
 };
 
 }  // namespace onnxruntime
