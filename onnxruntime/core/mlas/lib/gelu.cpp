cfec75557c46f489d84669c5b6d8f67796377aff
Sanket Kale <sanketk.kale@fujitsu.com>
2026-04-01 10:24:42 +0530

    Resolved merge conflicts

--- onnxruntime/core/mlas/lib/gelu.cpp
+++ onnxruntime/core/mlas/lib/gelu.cpp
@@ -62,3 +62,37 @@ MlasComputeGeluErf(
     MlasGeluErfKernel(Input, Output, N);
 #endif
 }
+
+void
+MLASCALL
+MlasComputeFP16Gelu(const MLAS_FP16* input,
+                    MLAS_FP16* output,
+                    MLAS_FP16* temp,
+                    size_t count,
+                    MLAS_GELU_ALGORITHM algo)
+{
+    if(GetMlasPlatform().GeluFP16KernelRoutine){
+        GetMlasPlatform().GeluFP16KernelRoutine(input, output, temp, count, algo);
+        return;
+    }
+    MLAS_UNREFERENCED_PARAMETER(temp); // 'temp' is only used by vectorized kernel implementations and it is unused in the scalar fallback path.
+    for (size_t i = 0; i < count; ++i) {
+        float x = static_cast<float>(input[i]);
+        float gelu_val;
+
+        if (algo == MlasGeluTanh) {
+            // GELU approximation (tanh)
+            const float B = 0.7978845608f;
+            const float C = 0.044715f * B;
+            float tanh_arg = x * (B + C * x * x);
+            float tanh_res = std::tanh(tanh_arg);
+            gelu_val = 0.5f * x * (1.0f + tanh_res);
+        } else {
+            // GELU exact (erf)
+            gelu_val = 0.5f * x *
+                (1.0f + std::erf(x * static_cast<float>(M_SQRT1_2)));
+        }
+
+        output[i] = MLAS_FP16(gelu_val);
+    }
+}
