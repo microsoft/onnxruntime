45d64fcc7c6f67a4c65a9e89d52d757ae30f84cd
Yuzo <youzow3@yahoo.co.jp>
2026-04-01 12:59:57 +0900

    fix link error

--- cmake/onnxruntime_graph.cmake
+++ cmake/onnxruntime_graph.cmake
@@ -112,7 +112,7 @@ endif()
 if (onnxruntime_ENABLE_TRAINING)
   #TODO: the graph library should focus on ONNX IR, it shouldn't depend on math libraries like MKLML/OpenBlas
   target_include_directories(onnxruntime_graph PRIVATE ${MKLML_INCLUDE_DIR})
-  target_link_libraries(onnxruntime_graph PRIVATE nlohmann_json::nlohmann_json)
+  target_link_libraries(onnxruntime_graph PRIVATE onnxruntime_training nlohmann_json::nlohmann_json)
 endif()
 
 target_include_directories(onnxruntime_graph PRIVATE ${ONNXRUNTIME_ROOT})
