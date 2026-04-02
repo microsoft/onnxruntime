05c1eea6ba6bccefe8d30e1d96c3fa5c4cea6995
Hari Seshadri <hasesh@microsoft.com>
2026-04-01 20:27:30 -0700

    Fix test

--- onnxruntime/test/flatbuffers/flatbuffer_utils_test.cc
+++ onnxruntime/test/flatbuffers/flatbuffer_utils_test.cc
@@ -326,8 +326,7 @@ TEST(FlatbufferUtilsTest, LoadInitializerRejectsNullStringDataEntry) {
   ASSERT_NE(fbs_string_data, nullptr);
   auto* mutable_string_data =
       const_cast<flatbuffers::Vector<flatbuffers::Offset<flatbuffers::String>>*>(fbs_string_data);
-  auto* string_offsets = mutable_string_data->data();
-  string_offsets[1] = flatbuffers::Offset<flatbuffers::String>();
+  ASSERT_TRUE(mutable_string_data->MutateOffset(1, 0));
 
   ONNX_NAMESPACE::TensorProto initializer;
   OrtFormatLoadOptions options;
