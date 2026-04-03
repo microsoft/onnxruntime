15e60b3a9cc48b68b24ecaf2fdf1685dad9bedeb
Xavier Dupré <xadupre@microsoft.com>
2026-04-03 17:44:05 +0000

    use size_t

--- onnxruntime/core/providers/cpu/ml/svmclassifier.h
+++ onnxruntime/core/providers/cpu/ml/svmclassifier.h
@@ -120,9 +120,9 @@ class SVMClassifier final : public OpKernel, private SVMCommon {
   Status ComputeImpl(OpKernelContext& ctx, gsl::span<const float> x_data, const TensorShape& x_shape) const;
 
   bool weights_are_all_positive_;
-  ptrdiff_t feature_count_;
-  ptrdiff_t class_count_;
-  ptrdiff_t vector_count_;
+  size_t feature_count_;
+  size_t class_count_;
+  size_t vector_count_;
   bool using_strings_;
   std::vector<int64_t> vectors_per_class_;
   std::vector<int64_t> starting_vector_;
