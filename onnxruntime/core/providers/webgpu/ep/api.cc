6186e1517ffc2a8276379b30b5ab7dc9bc60d9e8
edgchen1 <18449977+edgchen1@users.noreply.github.com>
2026-04-22 16:49:53 -0700

    Initial fix - don't rely on Ort::Status usage before calling ApiInit().

--- onnxruntime/core/providers/webgpu/ep/api.cc
+++ onnxruntime/core/providers/webgpu/ep/api.cc
@@ -5,6 +5,7 @@
 #include "onnxruntime_cxx_api.h"
 #undef ORT_API_MANUAL_INIT
 
+#include <iostream>
 #include <memory>
 
 #include "core/providers/webgpu/ep/factory.h"
@@ -36,10 +37,33 @@ extern "C" {
 EXPORT_SYMBOL OrtStatus* CreateEpFactories(const char* /*registration_name*/, const OrtApiBase* ort_api_base,
                                            const OrtLogger* default_logger,
                                            OrtEpFactory** factories, size_t max_factories, size_t* num_factories) noexcept {
-  EXCEPTION_TO_RETURNED_STATUS_BEGIN
+  {
+    // Note: We can't use the EXCEPTION_TO_RETURNED_STATUS_BEGIN/EXCEPTION_TO_RETURNED_STATUS_END macros before the
+    // call to `onnxruntime::ep::ApiInit()`. We need to create an OrtStatus more conservatively.
 
-  // Manual init for the C++ API
-  onnxruntime::ep::ApiInit(ort_api_base);
+    // Creates an OrtStatus* for the error or falls back to printing an error message.
+    auto report_error = [](const OrtApiBase* ort_api_base, const char* message) -> OrtStatus* {
+      if (ort_api_base != nullptr) {
+        // Note: CreateStatus has been around since the v1 API, so we'll try to obtain it with the v1 API.
+        if (const OrtApi* ort_api_v1 = ort_api_base->GetApi(1); ort_api_v1 != nullptr) {
+          return ort_api_v1->CreateStatus(OrtErrorCode::ORT_FAIL, message);
+        }
+      }
+      std::cerr << "Error: " << message << "\n";
+      return nullptr;
+    };
+
+    try {
+      // Manual init for the C++ API
+      onnxruntime::ep::ApiInit(ort_api_base);
+    } catch (std::exception& e) {
+      return report_error(ort_api_base, e.what());
+    } catch (...) {
+      return report_error(ort_api_base, "Unknown exception");
+    }
+  }
+
+  EXCEPTION_TO_RETURNED_STATUS_BEGIN
 
   if (max_factories < 1) {
     return onnxruntime::ep::Api().ort.CreateStatus(ORT_INVALID_ARGUMENT,
