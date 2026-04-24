5a0a728f30a42ab6953173bf999b678d80f0137c
Hariharan Seshadri <shariharan91@gmail.com>
2026-04-23 17:15:48 -0700

    Update onnxruntime/contrib_ops/cpu/bert/rotary_embedding.cc
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- onnxruntime/contrib_ops/cpu/bert/rotary_embedding.cc
+++ onnxruntime/contrib_ops/cpu/bert/rotary_embedding.cc
@@ -113,7 +113,7 @@ Status RunRotaryEmbedding(concurrency::ThreadPool* tp, RotaryParameters paramete
   std::ptrdiff_t max_block_offset = 0;
   std::ptrdiff_t max_b_s_index = 0;
   [[maybe_unused]] std::ptrdiff_t max_cache_offset = 0;
-    ORT_RETURN_IF_ERROR(onnxruntime::rotary_embedding_int32_utils::CheckedMulToPtrdiff(
+  ORT_RETURN_IF_ERROR(onnxruntime::rotary_embedding_int32_utils::CheckedMulToPtrdiff(
       std::max(batch_size - 1, 0), batch_stride, "max_batch_offset", max_batch_offset));
     ORT_RETURN_IF_ERROR(onnxruntime::rotary_embedding_int32_utils::CheckedMulToPtrdiff(
       std::max(sequence_length - 1, 0), seq_stride, "max_seq_offset", max_seq_offset));
