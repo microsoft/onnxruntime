bfd897178ae255f4f6ee387d59b54d94fe129a25
Tianlei Wu <tlwu@microsoft.com>
2026-05-05 17:07:49 -0700

    test: fix QMoETest_CPU_Int2_MLAS missing swiglu_fusion attribute
    The CPU QMoE kernel enforces swiglu_fusion==1 when activation_type is swiglu.
    Add the missing attribute to avoid ORT_ENFORCE failure.

--- onnxruntime/test/contrib_ops/moe_test.cc
+++ onnxruntime/test/contrib_ops/moe_test.cc
@@ -1473,6 +1473,7 @@ TEST(MoETest, QMoETest_CPU_Int2_MLAS) {
   OpTester cpu_tester("QMoE", 1, onnxruntime::kMSDomain);
   cpu_tester.AddAttribute<int64_t>("k", 2);
   cpu_tester.AddAttribute<std::string>("activation_type", "swiglu");
+  cpu_tester.AddAttribute<int64_t>("swiglu_fusion", static_cast<int64_t>(1));
   cpu_tester.AddAttribute<int64_t>("normalize_routing_weights", 1);
   cpu_tester.AddAttribute<int64_t>("expert_weight_bits", expert_weight_bits);
 
