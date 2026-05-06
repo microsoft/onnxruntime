d393f2bc4a7a923ff6869c388a490c1d24769d00
Adrian Lizarraga <adlizarraga@microsoft.com>
2026-05-06 09:49:25 -0700

    Remove duplicate static assert for version 26 (no additions)

--- onnxruntime/core/session/onnxruntime_c_api.cc
+++ onnxruntime/core/session/onnxruntime_c_api.cc
@@ -4933,7 +4933,7 @@ static_assert(offsetof(OrtApi, GetEpApi) / sizeof(void*) == 317, "Size of versio
 static_assert(offsetof(OrtApi, CreateExternalInitializerInfo) / sizeof(void*) == 389, "Size of version 23 API cannot change");
 static_assert(offsetof(OrtApi, GetTensorElementTypeAndShapeDataReference) / sizeof(void*) == 414, "Size of version 24 API cannot change");
 static_assert(offsetof(OrtApi, SetPerSessionThreadPoolCallbacks) / sizeof(void*) == 418, "Size of version 25 API cannot change");
-static_assert(offsetof(OrtApi, SetPerSessionThreadPoolCallbacks) / sizeof(void*) == 418, "Size of version 26 API cannot change");  // no additions in version 26
+// no additions in version 26
 
 // So that nobody forgets to finish an API version, this check will serve as a reminder:
 static_assert(std::string_view(ORT_VERSION) == "1.27.0",
