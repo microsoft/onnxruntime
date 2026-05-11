363d98c5d85af026264bee3941b7fb4496015b53
Eric Crawford <eric.r.crawford@intel.com>
2026-05-11 15:39:06 -0700

    Remove OrtEp::GetMemoryInfoByMemType

--- onnxruntime/core/session/plugin_ep/ep_plugin_provider_interfaces.h
+++ onnxruntime/core/session/plugin_ep/ep_plugin_provider_interfaces.h
@@ -151,8 +151,6 @@ class PluginExecutionProvider : public IExecutionProvider {
   common::Status ReplayGraph(int graph_annotation_id) override;
   OrtGraphCaptureNodeAssignmentPolicy GetGraphCaptureNodeAssignmentPolicy() const override;
 
-  OrtDevice GetOrtDeviceByMemType(OrtMemType mem_type) const override;
-
  private:
   const logging::Logger& GetEpLoggerOrDefault() const;
 
