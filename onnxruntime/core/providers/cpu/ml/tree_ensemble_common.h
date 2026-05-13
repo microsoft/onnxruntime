ad0585bca711c3e4c5ce1f17d20824279294d98a
Xavier Dupré <xadupre@users.noreply.github.com>
2026-05-13 10:16:55 +0200

    Update onnxruntime/core/providers/cpu/ml/tree_ensemble_common.h
    Co-authored-by: Tianlei Wu <tlwu@microsoft.com>

--- onnxruntime/core/providers/cpu/ml/tree_ensemble_common.h
+++ onnxruntime/core/providers/cpu/ml/tree_ensemble_common.h
@@ -331,7 +331,7 @@ Status TreeEnsembleCommon<InputType, ThresholdType, OutputType>::Init(
     w.value = attributes.target_class_weights_as_tensor.empty()
                   ? static_cast<ThresholdType>(attributes.target_class_weights[i])
                   : attributes.target_class_weights_as_tensor[i];
-    // AddNodes already maded sure that w.i >= 0 && w.i < n_targets_or_classes_.
+    // TreeEnsembleAttributesV3 already made sure that w.i >= 0 && w.i < n_targets_or_classes_.
     if (leaf.truenode_or_weight.weight_data.n_weights == 0) {
       leaf.truenode_or_weight.weight_data.weight = static_cast<int32_t>(weights_.size());
       leaf.value_or_unique_weight = w.value;
