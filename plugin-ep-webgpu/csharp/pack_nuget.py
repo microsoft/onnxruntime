576529ced802d85219d721db79a7858816760b7a
edgchen1 <18449977+edgchen1@users.noreply.github.com>
2026-05-15 11:26:19 -0700

    consistent whitespace

--- plugin-ep-webgpu/csharp/pack_nuget.py
+++ plugin-ep-webgpu/csharp/pack_nuget.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
+
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
+
 """Build the Microsoft.ML.OnnxRuntime.EP.WebGpu NuGet package.
 
 Stages native binaries from build artifacts into the runtimes/ layout expected
