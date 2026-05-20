6d03671a2cf433b7fda2cfc1aab18549b01a6a9e
Tianlei Wu <tlwu@microsoft.com>
2026-05-20 11:14:01 -0700

    add benchmark

--- onnxruntime/contrib_ops/cpu/quantization/matmul_nbits_impl.h
+++ onnxruntime/contrib_ops/cpu/quantization/matmul_nbits_impl.h
@@ -23,14 +23,14 @@ void DequantizeBlockwise(
 // Does not support reorder_idx (g_idx).
 template <typename inputT, typename zeroT>
 void DequantizeBlockwise2Bits(
-    inputT* output,              // dequantized output
-    const uint8_t* quant_data,   // quantized input
-    const inputT* scales_data,   // quantization scales
-    const zeroT* zero_points,    // quantization zero points
-    int32_t block_size,          // quantization block size
-    bool,                        // columnwise quantization or row-wise
-    int32_t K,                   // number of rows in quantized input
-    int32_t N,                   // number of columns in quantized input
+    inputT* output,             // dequantized output
+    const uint8_t* quant_data,  // quantized input
+    const inputT* scales_data,  // quantization scales
+    const zeroT* zero_points,   // quantization zero points
+    int32_t block_size,         // quantization block size
+    bool,                       // columnwise quantization or row-wise
+    int32_t K,                  // number of rows in quantized input
+    int32_t N,                  // number of columns in quantized input
     onnxruntime::concurrency::ThreadPool* thread_pool);
 
 }  // namespace contrib
