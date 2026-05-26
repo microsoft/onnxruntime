af805ba0f559a4bd151c8e57d0fe184d0c1a550b
Hari Seshadri <shariharan91@gmail.com>
2026-05-26 14:06:55 -0700

    [WebGPU] Default non-debug validation mode to disabled

--- onnxruntime/core/providers/webgpu/webgpu_context.h
+++ onnxruntime/core/providers/webgpu/webgpu_context.h
@@ -92,7 +92,7 @@ struct WebGpuContextConfig {
 #ifndef NDEBUG
       webgpu::ValidationMode::Full  // for debug build, enable full validation by default
 #else
-      webgpu::ValidationMode::Basic  // for release build, enable basic validation by default
+  webgpu::ValidationMode::Disabled  // for release build, disable validation by default
 #endif  // !NDEBUG
   };
   bool preserve_device{false};
