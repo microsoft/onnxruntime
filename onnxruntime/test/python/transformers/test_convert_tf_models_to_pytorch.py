4958a8d6453bd87339e3238a6da0f057cde718eb
Tianlei Wu <tlwu@microsoft.com>
2026-06-04 14:56:53 -0700

    Add license header

--- onnxruntime/test/python/transformers/test_convert_tf_models_to_pytorch.py
+++ onnxruntime/test/python/transformers/test_convert_tf_models_to_pytorch.py
@@ -1,3 +1,10 @@
+#!/usr/bin/env python
+# -------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation.  All rights reserved.
+# Licensed under the MIT License.  See License.txt in the project root for
+# license information.
+# --------------------------------------------------------------------------
+
 import io
 import tarfile
 import zipfile
