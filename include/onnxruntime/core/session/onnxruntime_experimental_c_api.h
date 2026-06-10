f1fd062fda2428fcef6eb46dce7f5bf84c50f0b1
edgchen1 <18449977+edgchen1@users.noreply.github.com>
2026-06-09 21:41:05 -0700

    update experimental header comments

--- include/onnxruntime/core/session/onnxruntime_experimental_c_api.h
+++ include/onnxruntime/core/session/onnxruntime_experimental_c_api.h
@@ -6,8 +6,8 @@
 // This header provides typedefs, name constants, and (for C++) typed inline accessors for experimental ORT functions.
 // It should be used together with the experimental header lookup function `OrtApi::GetExperimentalFunction()`.
 //
-// This header is generated from onnxruntime_experimental_c_api.inc, which defines the list of experimental API
-// functions.
+// This header contains code generated from onnxruntime_experimental_c_api.inc, which defines the list of experimental
+// API functions.
 //
 // IMPORTANT: Experimental functions are NOT part of the stable ABI. They may be added, changed, or removed between
 // releases without notice. A function's availability should always be checked at runtime (the lookup returns nullptr
@@ -30,8 +30,19 @@
 
 #include "onnxruntime_c_api.h"
 
-// --- C: function pointer typedefs and name constants ---
 //
+// Auxiliary declarations
+//
+// Declarations of auxiliary types or typedefs required by experimental APIs go here.
+//
+// For example, if an experimental API uses a new type, OrtExperimentalType, we would declare it in this file:
+//   ORT_RUNTIME_CLASS(ExperimentalType);
+//
+
+//
+// C: function pointer typedefs and name constants
+//
+
 // For each ORT_EXPERIMENTAL_API(VER, RET, NAME, ...) entry in the .inc file, this generates:
 //
 //   // Function pointer typedef:
@@ -40,13 +51,11 @@
 //   // Name constant for lookup:
 //   static const char* const kOrtExperimental_<NAME>_SinceV<VER>_FnName = "<NAME>_SinceV<VER>";
 //
-// Example: ORT_EXPERIMENTAL_API(28, OrtStatusPtr, OrtApi_ExperimentalApiTest, _Out_ int64_t* out)
-// produces:
+// Example: ORT_EXPERIMENTAL_API(28, OrtStatusPtr, OrtApi_ExperimentalApiTest, _Out_ int64_t* out) produces:
 //   typedef OrtStatusPtr(ORT_API_CALL* OrtExperimental_OrtApi_ExperimentalApiTest_SinceV28_Fn)(
 //       _Out_ int64_t* out) NO_EXCEPTION;
 //   static const char* const kOrtExperimental_OrtApi_ExperimentalApiTest_SinceV28_FnName =
 //       "OrtApi_ExperimentalApiTest_SinceV28";
-
 #define ORT_EXPERIMENTAL_API(VER, RET, NAME, ...)                                                 \
   typedef RET(ORT_API_CALL* OrtExperimental_##NAME##_SinceV##VER##_Fn)(__VA_ARGS__) NO_EXCEPTION; \
   static const char* const kOrtExperimental_##NAME##_SinceV##VER##_FnName = #NAME "_SinceV" #VER;
@@ -55,24 +64,25 @@
 
 #undef ORT_EXPERIMENTAL_API
 
+//
+// C++: typed inline accessors
+//
+
 #ifdef __cplusplus
 
 namespace Ort {
 namespace Experimental {
 
-// --- C++: typed inline accessors (reuses the C typedefs above) ---
-//
 // For each .inc entry, this generates a typed accessor in Ort::Experimental:
 //
 //   inline OrtExperimental_<NAME>_SinceV<VER>_Fn Get_<NAME>_SinceV<VER>_Fn(const OrtApi* api);
 //
-// Example: for ORT_EXPERIMENTAL_API(28, OrtStatusPtr, OrtApi_ExperimentalApiTest, ...) this produces:
+// Example: ORT_EXPERIMENTAL_API(28, OrtStatusPtr, OrtApi_ExperimentalApiTest, ...) produces:
 //   inline OrtExperimental_OrtApi_ExperimentalApiTest_SinceV28_Fn
 //   Get_OrtApi_ExperimentalApiTest_SinceV28_Fn(const OrtApi* api) {
 //     return reinterpret_cast<OrtExperimental_OrtApi_ExperimentalApiTest_SinceV28_Fn>(
 //         api->GetExperimentalFunction(kOrtExperimental_OrtApi_ExperimentalApiTest_SinceV28_FnName));
 //   }
-
 #define ORT_EXPERIMENTAL_API(VER, RET, NAME, ...)                                      \
   inline OrtExperimental_##NAME##_SinceV##VER##_Fn Get_##NAME##_SinceV##VER##_Fn(      \
       const OrtApi* api) {                                                             \
