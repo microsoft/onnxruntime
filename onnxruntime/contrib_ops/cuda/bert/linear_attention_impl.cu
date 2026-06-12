74945fd0adc157a6b1addc5812d0ce9c32141d6e
Tianlei Wu <tlwu@microsoft.com>
2026-06-11 18:20:00 -0700

    refactoring

--- onnxruntime/contrib_ops/cuda/bert/linear_attention_impl.cu
+++ onnxruntime/contrib_ops/cuda/bert/linear_attention_impl.cu
@@ -27,43 +27,13 @@
 #include <type_traits>
 #include "contrib_ops/cuda/bert/linear_attention_impl.h"
 #include "core/providers/cuda/cu_inc/common.cuh"
+#include "core/providers/cuda/cu_inc/cuda_type_helper.cuh"
 
 namespace onnxruntime {
 namespace contrib {
 namespace cuda {
 
 namespace {
-
-// Convert half/bfloat16 to float
-template <typename T>
-__device__ __forceinline__ float to_float(T val);
-
-template <>
-__device__ __forceinline__ float to_float(float val) { return val; }
-
-template <>
-__device__ __forceinline__ float to_float(half val) { return __half2float(val); }
-
-#if __CUDA_ARCH__ >= 800 || !defined(__CUDA_ARCH__)
-template <>
-__device__ __forceinline__ float to_float(__nv_bfloat16 val) { return __bfloat162float(val); }
-#endif
-
-// Convert float to half/bfloat16/float
-template <typename T>
-__device__ __forceinline__ T from_float(float val);
-
-template <>
-__device__ __forceinline__ float from_float(float val) { return val; }
-
-template <>
-__device__ __forceinline__ half from_float(float val) { return __float2half(val); }
-
-#if __CUDA_ARCH__ >= 800 || !defined(__CUDA_ARCH__)
-template <>
-__device__ __forceinline__ __nv_bfloat16 from_float(float val) { return __float2bfloat16(val); }
-#endif
-
 // Full-warp sum reduction (result broadcast to all lanes).
 __device__ __forceinline__ float warp_reduce_sum(float v) {
 #pragma unroll
