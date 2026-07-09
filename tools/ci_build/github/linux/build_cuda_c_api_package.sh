667712cdb56ad0990d99dac164215eb4d89f2013
Sanaa Hamel <sanaahamel@microsoft.com>
2026-07-09 15:54:49 -0400

    feat(ci): ccache and vcpkg cache for ADO pipelines

--- tools/ci_build/github/linux/build_cuda_c_api_package.sh
+++ tools/ci_build/github/linux/build_cuda_c_api_package.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e -x
+set -euxo pipefail
 
 if [ "$CUDA_VERSION" == "12.8" ]; then
     CUDA_ARCHS="60-real;70-real;75-real;80-real;90a-real;90-virtual"
@@ -10,18 +10,50 @@ else
     exit 1
 fi
 
-docker run -e SYSTEM_COLLECTIONURI --rm \
---volume "$BUILD_SOURCESDIRECTORY:/onnxruntime_src" --volume "$BUILD_BINARIESDIRECTORY:/build" \
--e NPM_CONFIG_USERCONFIG=/tmp/.npmrc \
--e PIP_INDEX_URL \
---volume "${NPM_CONFIG_USERCONFIG}:/tmp/.npmrc:ro" \
---volume "$HOME/.m2:/home/onnxruntimedev/.m2:ro" \
---volume "$HOME/.gradle:/home/onnxruntimedev/.gradle" \
--e NIGHTLY_BUILD "onnxruntimecuda${CUDA_VERSION_MAJOR}build" \
-/bin/bash -c "/usr/bin/python3 /onnxruntime_src/tools/ci_build/build.py --enable_lto --build_java --build_nodejs \
---build_dir /build --config Release --skip_submodule_sync --use_binskim_compliant_compile_flags --build_shared_lib \
---parallel --nvcc_threads 1 --flash_nvcc_threads 1 \
---use_cuda --cuda_version=$CUDA_VERSION --cuda_home=/usr/local/cuda-$CUDA_VERSION --cudnn_home=/usr/local/cuda-$CUDA_VERSION \
---skip_tests --use_vcpkg --use_vcpkg_ms_internal_asset_cache \
---cmake_extra_defines 'CMAKE_CUDA_ARCHITECTURES=${CUDA_ARCHS}' 'onnxruntime_USE_FPA_INTB_GEMM=OFF' \
-&& cd /build/Release && make install DESTDIR=/build/installed"
+# HACK: so the docker user can write to the caches
+mkdir -p "${CCACHE_DIR}"
+mkdir -p "${VCPKG_DEFAULT_BINARY_CACHE}"
+chmod -R 777 "${CCACHE_DIR}"
+chmod -R 777 "${VCPKG_DEFAULT_BINARY_CACHE}"
+
+docker run --rm \
+    --volume "${BUILD_BINARIESDIRECTORY}:/build" \
+    --volume "${BUILD_SOURCESDIRECTORY}:/onnxruntime_src" \
+    --volume "${CCACHE_DIR}:/cache/ccache" \
+    --volume "${HOME}/.gradle:/home/onnxruntimedev/.gradle" \
+    --volume "${HOME}/.m2:/home/onnxruntimedev/.m2:ro" \
+    --volume "${NPM_CONFIG_USERCONFIG}:/tmp/.npmrc:ro" \
+    --volume "${VCPKG_DEFAULT_BINARY_CACHE}:/cache/vcpkg" \
+    -e CCACHE_DIR=/cache/ccache \
+    -e NIGHTLY_BUILD "onnxruntimecuda${CUDA_VERSION_MAJOR}build" \
+    -e NPM_CONFIG_USERCONFIG=/tmp/.npmrc \
+    -e PIP_INDEX_URL \
+    -e SYSTEM_COLLECTIONURI \
+    -e VCPKG_DEFAULT_BINARY_CACHE=/cache/vcpkg \
+    "onnxruntimecuda${CUDA_VERSION_MAJOR}build" \
+    /bin/bash -c " \
+ccache --zero-stats \
+&& /usr/bin/python3 /onnxruntime_src/tools/ci_build/build.py \
+    --build_dir /build \
+    --build_java \
+    --build_nodejs \
+    --build_shared_lib \
+    --cmake_extra_defines 'CMAKE_CUDA_ARCHITECTURES=${CUDA_ARCHS}' 'onnxruntime_USE_FPA_INTB_GEMM=OFF' \
+    --config Release \
+    --cuda_home     '/usr/local/cuda-$CUDA_VERSION' \
+    --cuda_version  '$CUDA_VERSION' \
+    --cudnn_home    '/usr/local/cuda-$CUDA_VERSION' \
+    --enable_lto \
+    --flash_nvcc_threads 1 \
+    --nvcc_threads 1 \
+    --parallel \
+    --skip_submodule_sync \
+    --skip_tests \
+    --use_binskim_compliant_compile_flags \
+    --use_cache \
+    --use_cuda \
+    --use_vcpkg \
+    --use_vcpkg_ms_internal_asset_cache \
+&& ccache --show-stats -vv \
+&& cd /build/Release \
+&& make install DESTDIR=/build/installed"
--- tools/ci_build/github/linux/build_tensorrt_c_api_package.sh
+++ tools/ci_build/github/linux/build_tensorrt_c_api_package.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e -x
+set -euxo pipefail
 
 if [ "$CUDA_VERSION" == "12.8" ]; then
     CUDA_ARCHS="60-real;70-real;75-real;80-real;90a-real;90-virtual"
@@ -10,18 +10,53 @@ else
     exit 1
 fi
 
+# HACK: so the docker user can write to the caches
+mkdir -p "${CCACHE_DIR}"
+mkdir -p "${VCPKG_DEFAULT_BINARY_CACHE}"
+chmod -R 777 "${CCACHE_DIR}"
+chmod -R 777 "${VCPKG_DEFAULT_BINARY_CACHE}"
+
 mkdir -p "$HOME/.onnx"
-docker run -e SYSTEM_COLLECTIONURI --rm --volume /data/onnx:/data/onnx:ro --volume "$BUILD_SOURCESDIRECTORY:/onnxruntime_src" \
-  --volume "$BUILD_BINARIESDIRECTORY:/build" --volume /data/models:/build/models:ro \
-  -e NPM_CONFIG_USERCONFIG=/tmp/.npmrc \
-  -e PIP_INDEX_URL \
-  --volume "${NPM_CONFIG_USERCONFIG}:/tmp/.npmrc:ro" \
-  --volume "$HOME/.m2:/home/onnxruntimedev/.m2:ro" \
-  --volume "$HOME/.gradle:/home/onnxruntimedev/.gradle" \
-  --volume "$HOME/.onnx:/home/onnxruntimedev/.onnx" -e NIGHTLY_BUILD "onnxruntimecuda${CUDA_VERSION_MAJOR}xtrt86build" \
-  /bin/bash -c "/usr/bin/python3 /onnxruntime_src/tools/ci_build/build.py --build_dir /build --config Release --skip_tests \
-  --skip_submodule_sync --use_binskim_compliant_compile_flags --build_shared_lib --build_java --build_nodejs \
-  --parallel --nvcc_threads 1 --flash_nvcc_threads 1 \
-  --use_tensorrt --cuda_version=$CUDA_VERSION --cuda_home=/usr/local/cuda-$CUDA_VERSION --cudnn_home=/usr --tensorrt_home=/usr \
-  --cmake_extra_defines 'CMAKE_CUDA_ARCHITECTURES=${CUDA_ARCHS}' 'onnxruntime_USE_FPA_INTB_GEMM=OFF' \
-  --use_vcpkg --use_vcpkg_ms_internal_asset_cache && cd /build/Release && make install DESTDIR=/build/installed"
+
+docker run \
+    --rm \
+    --volume "${HOME}/.gradle:/home/onnxruntimedev/.gradle" \
+    --volume "${HOME}/.m2:/home/onnxruntimedev/.m2:ro" \
+    --volume "${HOME}/.onnx:/home/onnxruntimedev/.onnx" \
+    --volume "${NPM_CONFIG_USERCONFIG}:/tmp/.npmrc:ro" \
+    --volume "${BUILD_BINARIESDIRECTORY}:/build" \
+    --volume "${BUILD_SOURCESDIRECTORY}:/onnxruntime_src" \
+    --volume /data/models:/build/models:ro \
+    --volume /data/onnx:/data/onnx:ro \
+    -e CCACHE_DIR=/cache/ccache \
+    -e NIGHTLY_BUILD \
+    -e NPM_CONFIG_USERCONFIG=/tmp/.npmrc \
+    -e PIP_INDEX_URL \
+    -e SYSTEM_COLLECTIONURI \
+    -e VCPKG_DEFAULT_BINARY_CACHE=/cache/vcpkg \
+    "onnxruntimecuda${CUDA_VERSION_MAJOR}xtrt86build" \
+    /bin/bash -c "\
+ccache --zero-stats \
+&& /usr/bin/python3 /onnxruntime_src/tools/ci_build/build.py \
+    --build_dir /build \
+    --config Release \
+    --skip_tests \
+    --skip_submodule_sync \
+    --use_binskim_compliant_compile_flags \
+    --build_shared_lib \
+    --build_java \
+    --build_nodejs \
+    --parallel \
+    --nvcc_threads 1 \
+    --flash_nvcc_threads 1 \
+    --use_tensorrt \
+    --cuda_version  '$CUDA_VERSION' \
+    --cuda_home     '/usr/local/cuda-$CUDA_VERSION' \
+    --cudnn_home    '/usr' \
+    --tensorrt_home '/usr' \
+    --cmake_extra_defines 'CMAKE_CUDA_ARCHITECTURES=${CUDA_ARCHS}' 'onnxruntime_USE_FPA_INTB_GEMM=OFF' \
+    --use_vcpkg \
+    --use_vcpkg_ms_internal_asset_cache \
+&& ccache --show-stats -vv \
+&& cd /build/Release \
+&& make install DESTDIR=/build/installed"
