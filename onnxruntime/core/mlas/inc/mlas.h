f744ec9b7ae503354dfbdd9a7c6e5ec0f3a93dd1
akote123 <Aruna.K@fujitsu.com>
2026-07-21 10:16:44 +0530

    Resolved Review comments

--- onnxruntime/core/mlas/inc/mlas.h
+++ onnxruntime/core/mlas/inc/mlas.h
@@ -2432,7 +2432,7 @@ struct MlasFlashAttentionThreadedArgs {
     const float* key;
     const float* value;
     float* output;
-    const MLAS_BACKEND_KERNEL_SELECTOR_CONFIG* BackendKernelSelectorConfig;
+    const MLAS_BACKEND_KERNEL_SELECTOR_CONFIG* BackendKernelSelectorConfig = nullptr;
 };
 
 /**
@@ -2491,7 +2491,7 @@ struct MlasFlashAttentionGQAArgs {
     // per-(batch, head, q_block) partitioning is used.
     float* flash_decoding_partials;
     int kv_chunk_count;
-    const MLAS_BACKEND_KERNEL_SELECTOR_CONFIG* BackendKernelSelectorConfig;
+    const MLAS_BACKEND_KERNEL_SELECTOR_CONFIG* BackendKernelSelectorConfig = nullptr;
 };
 
 /**
