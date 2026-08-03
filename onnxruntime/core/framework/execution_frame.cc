7bf17d359fc5981e11eb5123940d42668032b124
Beheshti <nazanin.beheshti@intel.com>
2026-08-03 12:27:18 -0700

    resolve build issue

--- onnxruntime/core/framework/execution_frame.cc
+++ onnxruntime/core/framework/execution_frame.cc
@@ -177,14 +177,13 @@ Status IExecutionFrame::GetOrCreateNodeOutputMLValue(const int output_index, int
       bool shape_matched = true;
 
       if (p_ort_value->IsTensor()) {
-        const TensorShape& existing_shape = p_ort_value->IsTensor()
-                                                ? p_ort_value->Get<Tensor>().Shape()
-#if !defined(DISABLE_SPARSE_TENSORS)
-                                                : p_ort_value->Get<SparseTensor>().DenseShape();
-#endif
-        LOGS_DEFAULT(INFO) << "existing_shape: " << existing_shape.ToString() << ", computed_shape: " << (shape ? shape->ToString() : "<null>");
+      
         ORT_RETURN_IF_NOT(shape != nullptr, "shape must not be null for tensor output that is already allocated");
         const Tensor& tensor = p_ort_value->Get<Tensor>();
+        const TensorShape& existing_shape = tensor.Shape();
+
+        LOGS_DEFAULT(INFO) << "existing_shape: " << existing_shape.ToString() << ", computed_shape: " << (shape ? shape->ToString() : "<null>");
+        ORT_RETURN_IF_NOT(shape != nullptr, "shape must not be null for tensor output that is already allocated");
         LOGS_DEFAULT(INFO) << "existing_shape size: " << existing_shape.Size() << ", computed_shape size: " << shape->Size() << std::endl;
         // Compare number of elements
         if (existing_shape.Size() == shape->Size()) {
