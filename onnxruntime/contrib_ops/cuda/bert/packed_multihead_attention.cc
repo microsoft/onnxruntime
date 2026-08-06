f6fad55741cf467f99735126ff797d546086dcb9
Tianlei Wu <tlwu@microsoft.com>
2026-08-06 02:19:52 +0000

    address feedbacks

--- onnxruntime/contrib_ops/cuda/bert/packed_multihead_attention.cc
+++ onnxruntime/contrib_ops/cuda/bert/packed_multihead_attention.cc
@@ -168,8 +168,6 @@ Status PackedMultiHeadAttention<T>::CheckInputs(const TensorShape& query_shape,
   parameters.v_head_size = static_cast<int>(v_hidden_size) / num_heads;
   parameters.num_heads = num_heads;
   parameters.num_heads_kv = num_heads;
-  parameters.k_hidden_size = hidden_size;
-  parameters.v_input_hidden_size = hidden_size;
   parameters.scale = this->GetScale();
   parameters.token_count = static_cast<int32_t>(token_count);
 
