5dd1462b9eaaad47a3b87d42308342abb7b35def
Tianlei Wu <tlwu@microsoft.com>
2026-08-11 12:53:20 -0700

    Bump ORT Version to 1.30 (#31972)
    Do not merge this PR until 1.29 release branch is cut.

--- include/onnxruntime/core/session/onnxruntime_c_api.h
+++ include/onnxruntime/core/session/onnxruntime_c_api.h
@@ -40,7 +40,7 @@
  *
  * This value is used by some API functions to behave as this version of the header expects.
  */
-#define ORT_API_VERSION 29
+#define ORT_API_VERSION 30
 
 #ifdef __cplusplus
 extern "C" {
