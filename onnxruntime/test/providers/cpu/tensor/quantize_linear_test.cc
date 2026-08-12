207cbe32ee1b26ea66f97158d1344d74af5db8ff
Akshay Sonawane <asonawane@microsoft.com>
2026-08-12 14:14:00 -0700

    Include limits in CUDA QDQ test

--- onnxruntime/test/providers/cpu/tensor/quantize_linear_test.cc
+++ onnxruntime/test/providers/cpu/tensor/quantize_linear_test.cc
@@ -1,6 +1,8 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+#include <limits>
+
 #include "gtest/gtest.h"
 #include "test/common/cuda_op_test_utils.h"
 #include "test/providers/provider_test_utils.h"
