d46aecdf98b25c426b82c06ce6e4b4bb9cac84f8
Tianlei Wu <tlwu@microsoft.com>
2026-09-01 09:30:56 -0700

    Fix MSVC C4459 errors in speculative paged XQA

--- onnxruntime/contrib_ops/cuda/bert/xqa/xqa_paged_spec_dec_fp16_fp8_256.cu
+++ onnxruntime/contrib_ops/cuda/bert/xqa/xqa_paged_spec_dec_fp16_fp8_256.cu
@@ -11,4 +11,11 @@
 #define XQA_PAGED_GROUP6_ONLY 1
 #define XQA_PAGED_SPEC_DEC 1
 
+// SPEC_DEC helper parameters intentionally use the same terminology as the
+// compile-time XQA configuration constants. MSVC reports those parameters as
+// C4459 when CUDA host code is compiled with warnings treated as errors.
+#ifdef _MSC_VER
+#pragma warning(disable : 4459)
+#endif
+
 #include "xqa_paged_loader_impl.cuh"
