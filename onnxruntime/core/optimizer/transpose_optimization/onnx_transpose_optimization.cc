188ac6aa947229dc4e5316f76f170645a6979f03
Akshay Sonawane <asonawane@microsoft.com>
2026-09-21 22:57:05 +0000

    Fix lint

--- onnxruntime/core/optimizer/transpose_optimization/onnx_transpose_optimization.cc
+++ onnxruntime/core/optimizer/transpose_optimization/onnx_transpose_optimization.cc
@@ -425,7 +425,7 @@ static bool CanTransposeInputWithQDQ(const api::GraphRef& graph, std::string_vie
 
   auto quant_info = GetQuantizationInfo(graph, *producer);
   return quant_info && (quant_info->mode != QuantizationMode::kPerAxis ||
-                         IsAxisInRangeForPerm(quant_info->norm_axis, perm));
+                        IsAxisInRangeForPerm(quant_info->norm_axis, perm));
 }
 
 /// <summary>
