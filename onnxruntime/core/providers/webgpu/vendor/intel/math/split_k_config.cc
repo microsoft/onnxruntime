c9b6a60fc8fa418c28192a512d52dfdc15807b7f
Jiajia Qin <jiajiaqin@microsoft.com>
2026-09-22 10:42:32 +0800

    Format Intel WebGPU Split-K profiles

--- onnxruntime/core/providers/webgpu/vendor/intel/math/split_k_config.cc
+++ onnxruntime/core/providers/webgpu/vendor/intel/math/split_k_config.cc
@@ -22,21 +22,18 @@ SplitKConfig CreateSplitKConfig(std::string_view architecture) {
       architecture == "gen-12hp") {
     // These thresholds are verified on Intel discrete GPUs and Lunar Lake iGPUs.
     return SplitKConfig{
-        max_batch_size, split_dim_inner, min_dim_inner_with_split_k,
-        {{768, 52.0}, {2304, 35.0}, {3072, 21.5}, {4096, 16.0}}};
+        max_batch_size, split_dim_inner, min_dim_inner_with_split_k, {{768, 52.0}, {2304, 35.0}, {3072, 21.5}, {4096, 16.0}}};
   }
 
   if (architecture == "xe-3lpg") {
     // These thresholds are verified on Intel Panther Lake iGPUs (12Xe).
     return SplitKConfig{
-        max_batch_size, split_dim_inner, min_dim_inner_with_split_k,
-        {{768, 40.0}, {1792, 22.0}, {3072, 18.0}, {4096, 10.0}}};
+        max_batch_size, split_dim_inner, min_dim_inner_with_split_k, {{768, 40.0}, {1792, 22.0}, {3072, 18.0}, {4096, 10.0}}};
   }
 
   // Default thresholds for newer Intel GPUs, chosen on a gen-12lp GPU with 32 EUs.
   return SplitKConfig{
-      max_batch_size, split_dim_inner, min_dim_inner_with_split_k,
-      {{768, 20.0}, {1792, 13.0}, {3072, 8.0}, {4096, 6.0}}};
+      max_batch_size, split_dim_inner, min_dim_inner_with_split_k, {{768, 20.0}, {1792, 13.0}, {3072, 8.0}, {4096, 6.0}}};
 }
 
 }  // namespace intel
